g0odi3z2j/cilium

commit d4e7f3bbcd27c1017f48275adb170966b1709ff2

Fixed race conditions
Replaced simple Mutex with RWMutex and dealt with some race conditions
caught by go build -race.

Acked-by: Thomas Graf <thomas@cilium.io>
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon/daemon.go
@@ -37,27 +37,24 @@ type Daemon struct {
 	ipamConf                  map[types.IPAMType]*types.IPAMConfig
 	consul                    *consulAPI.Client
 	containers                map[string]*types.Container
-	containersMU              sync.Mutex
+	containersMU              sync.RWMutex
 	endpoints                 map[uint16]*types.Endpoint
 	endpointsDocker           map[string]*types.Endpoint
 	endpointsDockerEP         map[string]*types.Endpoint
-	endpointsMU               sync.Mutex
+	endpointsMU               sync.RWMutex
 	endpointsLearning         map[uint16]types.LearningLabel
-	endpointsLearningMU       sync.Mutex
+	endpointsLearningMU       sync.RWMutex
 	endpointsLearningRegister chan types.LearningLabel
-	validLabelPrefixesMU      sync.Mutex
 	dockerClient              *dClient.Client
 	k8sClient                 *k8sClient.Client
 	conf                      *Config
 	policyTree                types.PolicyTree
-	policyTreeMU              sync.Mutex
+	policyTreeMU              sync.RWMutex
 	cacheIteration            int
 	reservedConsumables       []*types.Consumable
 	uiTopo                    types.UITopo
 	uiListeners               map[*Conn]bool
-	uiListenersMU             sync.Mutex
 	registerUIListener        chan *Conn
-	configMutex               sync.Mutex
 }
 
 func createConsulClient(config *consulAPI.Config) (*consulAPI.Client, error) {
@@ -115,7 +112,9 @@ func (d *Daemon) writeNetdevHeader(dir string) error {
 	defer f.Close()
 
 	fw := bufio.NewWriter(f)
+	d.conf.OptsMU.RLock()
 	fw.WriteString(d.conf.Opts.GetFmtList())
+	d.conf.OptsMU.RUnlock()
 
 	return fw.Flush()
 }
@@ -328,12 +327,12 @@ func changedOption(key string, value bool, data interface{}) {
 }
 
 func (d *Daemon) Update(opts types.OptionMap) error {
+	d.conf.OptsMU.Lock()
+	defer d.conf.OptsMU.Unlock()
+
 	if err := d.conf.Opts.Validate(opts); err != nil {
 		return err
 	}
-
-	d.configMutex.Lock()
-	defer d.configMutex.Unlock()
 
 	changes := d.conf.Opts.Apply(opts, changedOption, d)
 	if changes > 0 {

common/types/ui.go
@@ -25,7 +25,7 @@ const (
 )
 
 type UITopo struct {
-	uiTopoMU *sync.Mutex
+	uiTopoMU *sync.RWMutex
 	uiNodes  map[int]*UINode
 	uiEdges  map[string]*UIEdge
 	UIChan   chan UIUpdateMsg
@@ -33,7 +33,7 @@ type UITopo struct {
 
 func NewUITopo() UITopo {
 	return UITopo{
-		uiTopoMU: &sync.Mutex{},
+		uiTopoMU: &sync.RWMutex{},
 		uiNodes:  map[int]*UINode{},
 		uiEdges:  map[string]*UIEdge{},
 		UIChan:   make(chan UIUpdateMsg, 10),
@@ -117,8 +117,8 @@ func (t *UITopo) DeleteNode(id32 uint32) {
 }
 
 func (t *UITopo) GetNodes() []UINode {
-	t.uiTopoMU.Lock()
-	defer t.uiTopoMU.Unlock()
+	t.uiTopoMU.RLock()
+	defer t.uiTopoMU.RUnlock()
 	nodes := []UINode{}
 	for _, node := range t.uiNodes {
 		node.Build()
@@ -267,8 +267,8 @@ func (t *UITopo) DeleteEdge(from, to int) {
 }
 
 func (t *UITopo) GetEdges() []UIEdge {
-	t.uiTopoMU.Lock()
-	defer t.uiTopoMU.Unlock()
+	t.uiTopoMU.RLock()
+	defer t.uiTopoMU.RUnlock()
 	edges := []UIEdge{}
 	for _, edge := range t.uiEdges {
 		edge.Build()

daemon/daemon/daemon_config.go
@@ -2,6 +2,7 @@ package daemon
 
 import (
 	"net"
+	"sync"
 
 	"github.com/noironetworks/cilium-net/bpf/lxcmap"
 	"github.com/noironetworks/cilium-net/common/types"
@@ -31,24 +32,26 @@ var (
 
 // Config is the configuration used by Daemon.
 type Config struct {
-	LibDir             string                // Cilium library directory
-	RunDir             string                // Cilium runtime directory
-	LXCMap             *lxcmap.LXCMap        // LXCMap where all LXCs are stored
-	NodeAddress        net.IP                // Node IPv6 Address
-	IPv4Range          *net.IPNet            // Containers IPv4 Address range
-	IPv4Prefix         string                // IPv4 prefix
-	Device             string                // Receive device
-	ConsulConfig       *consulAPI.Config     // Consul configuration
-	DockerEndpoint     string                // Docker endpoint
-	K8sEndpoint        string                // Kubernetes endpoint
-	ValidLabelPrefixes *types.LabelPrefixCfg // Label prefixes used to filter from all labels
-	UIServerAddr       string                // TCP address for UI server
-	Tunnel             string                // Tunnel mode
+	LibDir               string                // Cilium library directory
+	RunDir               string                // Cilium runtime directory
+	LXCMap               *lxcmap.LXCMap        // LXCMap where all LXCs are stored
+	NodeAddress          net.IP                // Node IPv6 Address
+	IPv4Range            *net.IPNet            // Containers IPv4 Address range
+	IPv4Prefix           string                // IPv4 prefix
+	Device               string                // Receive device
+	ConsulConfig         *consulAPI.Config     // Consul configuration
+	DockerEndpoint       string                // Docker endpoint
+	K8sEndpoint          string                // Kubernetes endpoint
+	ValidLabelPrefixes   *types.LabelPrefixCfg // Label prefixes used to filter from all labels
+	ValidLabelPrefixesMU sync.RWMutex
+	UIServerAddr         string // TCP address for UI server
+	Tunnel               string // Tunnel mode
 
 	RestoreState bool // RestoreState restores the state from previous running daemons.
 
 	// Options changeable at runtime
-	Opts *types.BoolOptions
+	Opts   *types.BoolOptions
+	OptsMU sync.RWMutex
 }
 
 func NewConfig() *Config {

daemon/daemon/docker_watcher.go
@@ -129,8 +129,8 @@ func (d *Daemon) getFilteredLabels(allLabels map[string]string) types.Labels {
 
 	ciliumLabels.MergeLabels(k8sLabels)
 
-	d.validLabelPrefixesMU.Lock()
-	defer d.validLabelPrefixesMU.Unlock()
+	d.conf.ValidLabelPrefixesMU.RLock()
+	defer d.conf.ValidLabelPrefixesMU.RUnlock()
 	return d.conf.ValidLabelPrefixes.FilterLabels(ciliumLabels)
 }
 

daemon/daemon/endpoint.go
@@ -128,8 +128,8 @@ func (d *Daemon) setEndpointSecLabel(endpointID *uint16, dockerID, dockerEPID st
 // EndpointGetByDockerID returns a copy of the endpoint for the given dockerEPID, or nil
 // if the endpoint was not found.
 func (d *Daemon) EndpointGetByDockerID(dockerID string) (*types.Endpoint, error) {
-	d.endpointsMU.Lock()
-	defer d.endpointsMU.Unlock()
+	d.endpointsMU.RLock()
+	defer d.endpointsMU.RUnlock()
 
 	if ep := d.lookupDockerID(dockerID); ep != nil {
 		epCopy := *ep
@@ -141,8 +141,8 @@ func (d *Daemon) EndpointGetByDockerID(dockerID string) (*types.Endpoint, error)
 // EndpointGetByDockerEPID returns a copy of the endpoint for the given dockerEPID, or nil
 // if the endpoint was not found.
 func (d *Daemon) EndpointGetByDockerEPID(dockerEPID string) (*types.Endpoint, error) {
-	d.endpointsMU.Lock()
-	defer d.endpointsMU.Unlock()
+	d.endpointsMU.RLock()
+	defer d.endpointsMU.RUnlock()
 
 	if ep := d.lookupDockerEndpoint(dockerEPID); ep != nil {
 		epCopy := *ep
@@ -154,8 +154,8 @@ func (d *Daemon) EndpointGetByDockerEPID(dockerEPID string) (*types.Endpoint, er
 // EndpointGet returns a copy of the endpoint for the given endpointID, or nil if the
 // endpoint was not found.
 func (d *Daemon) EndpointGet(endpointID uint16) (*types.Endpoint, error) {
-	d.endpointsMU.Lock()
-	defer d.endpointsMU.Unlock()
+	d.endpointsMU.RLock()
+	defer d.endpointsMU.RUnlock()
 
 	if ep := d.lookupCiliumEndpoint(endpointID); ep != nil {
 		epCopy := *ep
@@ -167,8 +167,8 @@ func (d *Daemon) EndpointGet(endpointID uint16) (*types.Endpoint, error) {
 
 // EndpointsGet returns a copy of all the endpoints or nil if there are no endpoints.
 func (d *Daemon) EndpointsGet() ([]types.Endpoint, error) {
-	d.endpointsMU.Lock()
-	defer d.endpointsMU.Unlock()
+	d.endpointsMU.RLock()
+	defer d.endpointsMU.RUnlock()
 
 	eps := []types.Endpoint{}
 	epsSet := map[*types.Endpoint]bool{}
@@ -375,6 +375,7 @@ func (d *Daemon) EndpointJoin(ep types.Endpoint) error {
 		ep.Opts = types.NewBoolOptions(&types.EndpointOptionLibrary)
 	}
 
+	d.conf.OptsMU.Lock()
 	ep.Opts.InheritDefault(d.conf.Opts, types.OptionConntrack)
 	ep.Opts.InheritDefault(d.conf.Opts, types.OptionConntrackAccounting)
 	ep.Opts.InheritDefault(d.conf.Opts, types.OptionPolicy)
@@ -382,6 +383,7 @@ func (d *Daemon) EndpointJoin(ep types.Endpoint) error {
 	ep.Opts.InheritDefault(d.conf.Opts, types.OptionDropNotify)
 	ep.Opts.InheritDefault(d.conf.Opts, types.OptionNAT46)
 	ep.Opts.SetIfUnset(types.OptionLearnTraffic, false)
+	d.conf.OptsMU.Unlock()
 
 	d.InsertEndpoint(&ep)
 
@@ -515,10 +517,10 @@ func (d *Daemon) EndpointSave(ep types.Endpoint) error {
 }
 
 func (d *Daemon) EndpointLabelsGet(epID uint16) (*types.OpLabels, error) {
-	d.containersMU.Lock()
-	defer d.containersMU.Unlock()
-	d.endpointsMU.Lock()
-	defer d.endpointsMU.Unlock()
+	d.containersMU.RLock()
+	defer d.containersMU.RUnlock()
+	d.endpointsMU.RLock()
+	defer d.endpointsMU.RUnlock()
 
 	ep := d.lookupCiliumEndpoint(epID)
 	if ep == nil {
@@ -543,7 +545,9 @@ func (d *Daemon) EndpointLabelsUpdate(epID uint16, op types.LabelOP, labels type
 		return fmt.Errorf("Endpoint %d not found", epID)
 	}
 
+	d.conf.ValidLabelPrefixesMU.RLock()
 	labels = d.conf.ValidLabelPrefixes.FilterLabels(labels)
+	d.conf.ValidLabelPrefixesMU.RUnlock()
 
 	d.containersMU.Lock()
 	cont := d.containers[ep.DockerID]

daemon/daemon/ipam.go
@@ -49,9 +49,14 @@ func releaseIPCNI(cniReq types.IPAMReq, ipamConf *types.IPAMConfig) error {
 
 // allocateIPLibnetwork allocates an IP for the libnetwork plugin.
 func allocateIPLibnetwork(ln types.IPAMReq, ipamConf *types.IPAMConfig) (*types.IPAMRep, error) {
+	log.Debugf("ipamConf.IPAllocatorMU locking...")
 	ipamConf.IPAllocatorMU.Lock()
-	defer ipamConf.IPAllocatorMU.Unlock()
-
+	log.Debugf("ipamConf.IPAllocatorMU Locked")
+	defer func() {
+		log.Debugf("Unlocking ipamConf.IPAllocatorMU")
+		ipamConf.IPAllocatorMU.Unlock()
+		log.Debugf("Unlocked ipamConf.IPAllocatorMU")
+	}()
 	if ln.IP != nil {
 		err := ipamConf.IPAllocator.Allocate(*ln.IP)
 		return nil, err

daemon/daemon/labels_test.go
@@ -63,7 +63,9 @@ func (ds *DaemonSuite) SetUpTest(c *C) {
 	daemonConf.K8sEndpoint = "tcp://127.0.0.1"
 	daemonConf.ValidLabelPrefixes = nil
 	daemonConf.IPv4Range = ipv4range
+	daemonConf.OptsMU.Lock()
 	daemonConf.Opts.Set(types.OptionDropNotify, true)
+	daemonConf.OptsMU.Unlock()
 	daemonConf.Device = "undefined"
 
 	d1 := []byte("#!/usr/bin/env bash\necho \"OK\"\n")

daemon/daemon/monitor.go
@@ -23,7 +23,7 @@ func (d *Daemon) receiveEvent(msg *bpf.PerfEventSample, cpu int) {
 			log.Warningf("Error while parsing drop notification message: %s\n", err)
 			return
 		}
-		d.endpointsLearningMU.Lock()
+		d.endpointsLearningMU.RLock()
 		for _, v := range d.endpointsLearning {
 			if dn.DstID == uint32(v.EndpointID) {
 				go func(epID uint16, lblID uint32) {
@@ -42,7 +42,7 @@ func (d *Daemon) receiveEvent(msg *bpf.PerfEventSample, cpu int) {
 				}(v.EndpointID, dn.SrcLabel)
 			}
 		}
-		d.endpointsLearningMU.Unlock()
+		d.endpointsLearningMU.RUnlock()
 	}
 }
 

commit 1090d3064fc56567206e6b8e89316e6ef4ca4281

daemon: fixed wrongly locker location
Fixes: d4e7f3bbcd27c1017f48275adb170966b1709ff2

Acked-by: Thomas Graf <thomas@cilium.io>
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon/daemon.go
@@ -112,9 +112,7 @@ func (d *Daemon) writeNetdevHeader(dir string) error {
 	defer f.Close()
 
 	fw := bufio.NewWriter(f)
-	d.conf.OptsMU.RLock()
 	fw.WriteString(d.conf.Opts.GetFmtList())
-	d.conf.OptsMU.RUnlock()
 
 	return fw.Flush()
 }
@@ -198,9 +196,12 @@ func (d *Daemon) init() error {
 	f.Close()
 
 	if !d.conf.DryMode {
+		d.conf.OptsMU.RLock()
 		if err := d.compileBase(); err != nil {
+			d.conf.OptsMU.RUnlock()
 			return err
 		}
+		d.conf.OptsMU.RUnlock()
 
 		d.conf.LXCMap, err = lxcmap.OpenMap(common.BPFMap)
 		if err != nil {