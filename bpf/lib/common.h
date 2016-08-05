g0odi3z2j/cilium

commit a205b8db13133a65ff34d160bbeb690234e73364

bpf: Use container ID to tail call into egress policy programs
Egress policy inserts into the CT of the receiving container and
must thus be unique.

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/lib/common.h
@@ -11,9 +11,8 @@
 #define EVENT_SOURCE 0
 #endif
 
-
-/* Maximum number of supported local security labels */
-#define POLICY_MAP_SIZE	65536
+#define POLICY_MAP_SIZE	MAX 65536
+#define RESERVED_POLICY_SIZE 128
 
 #define __inline__ __attribute__((always_inline))
 
@@ -47,7 +46,8 @@ struct portmap {
 
 struct lxc_info {
 	__u32		ifindex;
-	__u32		sec_label;
+	__u16		sec_label;
+	__u16           lxc_id;
 	mac_t		mac;
 	mac_t		node_mac;
 	union v6addr	ip;

bpf/bpf_lxc.c
@@ -24,6 +24,8 @@
 #include "lib/drop.h"
 #include "lib/dbg.h"
 
+#define POLICY_ID ((LXC_ID << 16) | SECLABEL)
+
 __BPF_MAP(CT_MAP6, BPF_MAP_TYPE_HASH, 0, sizeof(struct ipv6_ct_tuple),
 	  sizeof(struct ct_entry), PIN_GLOBAL_NS, CT_MAP_SIZE);
 __BPF_MAP(CT_MAP4, BPF_MAP_TYPE_HASH, 0, sizeof(struct ipv4_ct_tuple),
@@ -214,7 +216,7 @@ to_host:
 		policy_mark_skip(skb);
 #endif
 
-		tail_call(skb, &cilium_policy, HOST_ID);
+		tail_call(skb, &cilium_reserved_policy, HOST_ID);
 		return DROP_MISSED_TAIL_CALL;
 #endif
 	}
@@ -237,7 +239,7 @@ pass_to_stack:
 	skb->cb[CB_SRC_LABEL] = SECLABEL;
 	skb->cb[CB_IFINDEX] = 0; /* Indicate passing to stack */
 
-	tail_call(skb, &cilium_policy, WORLD_ID);
+	tail_call(skb, &cilium_reserved_policy, WORLD_ID);
 	return DROP_MISSED_TAIL_CALL;
 #endif
 }
@@ -401,7 +403,7 @@ to_host:
 		policy_mark_skip(skb);
 #endif
 
-		tail_call(skb, &cilium_policy, HOST_ID);
+		tail_call(skb, &cilium_reserved_policy, HOST_ID);
 		return DROP_MISSED_TAIL_CALL;
 #endif
 	}
@@ -426,7 +428,7 @@ pass_to_stack:
 	skb->cb[CB_SRC_LABEL] = SECLABEL;
 	skb->cb[CB_IFINDEX] = 0; /* Indicate passing to stack */
 
-	tail_call(skb, &cilium_policy, WORLD_ID);
+	tail_call(skb, &cilium_reserved_policy, WORLD_ID);
 	return DROP_MISSED_TAIL_CALL;
 #endif
 }
@@ -551,7 +553,7 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 	return 0;
 }
 
-__section_tail(CILIUM_MAP_POLICY, SECLABEL) int handle_policy(struct __sk_buff *skb)
+__section_tail(CILIUM_MAP_POLICY, LXC_ID) int handle_policy(struct __sk_buff *skb)
 {
 	int ret, ifindex = skb->cb[CB_IFINDEX];
 	__u32 src_label = skb->cb[CB_SRC_LABEL];

bpf/bpf_netdev.c
@@ -226,7 +226,7 @@ int from_netdev(struct __sk_buff *skb)
 __BPF_MAP(POLICY_MAP, BPF_MAP_TYPE_HASH, 0, sizeof(__u32),
 	  sizeof(struct policy_entry), PIN_GLOBAL_NS, 1024);
 
-__section_tail(CILIUM_MAP_POLICY, SECLABEL) int handle_policy(struct __sk_buff *skb)
+__section_tail(CILIUM_MAP_RES_POLICY, SECLABEL) int handle_policy(struct __sk_buff *skb)
 {
 	__u32 src_label = skb->cb[CB_SRC_LABEL];
 	int ifindex = skb->cb[CB_IFINDEX];

bpf/lib/l3.h
@@ -152,7 +152,7 @@ static inline int ipv6_local_delivery(struct __sk_buff *skb, int nh_off,
 		skb->cb[CB_SRC_LABEL] = seclabel;
 		skb->cb[CB_IFINDEX] = dst_lxc->ifindex;
 
-		tail_call(skb, &cilium_policy, ntohl(dst_lxc->sec_label));
+		tail_call(skb, &cilium_policy, lxc_id);
 		return DROP_MISSED_TAIL_CALL;
 	}
 
@@ -188,7 +188,7 @@ static inline int __inline__ ipv4_local_delivery(struct __sk_buff *skb, int l3_o
 		skb->cb[CB_SRC_LABEL] = seclabel;
 		skb->cb[CB_IFINDEX] = dst_lxc->ifindex;
 
-		tail_call(skb, &cilium_policy, ntohl(dst_lxc->sec_label));
+		tail_call(skb, &cilium_policy, dst_lxc->lxc_id);
 		return DROP_MISSED_TAIL_CALL;
 	}
 

bpf/lib/maps.h
@@ -6,11 +6,13 @@
 #define CILIUM_MAP_LXC		0
 #define CILIUM_MAP_POLICY	1
 #define CILIUM_MAP_CALLS	2
+#define CILIUM_MAP_RES_POLICY	3
 
 __BPF_MAP(cilium_lxc, BPF_MAP_TYPE_HASH, CILIUM_MAP_LXC, sizeof(__u32), sizeof(struct lxc_info), PIN_GLOBAL_NS, 1024);
 
 /* Global map to jump into policy enforcement of receiving endpoint */
 BPF_PROG_ARRAY(cilium_policy, CILIUM_MAP_POLICY, PIN_GLOBAL_NS, POLICY_MAP_SIZE);
+BPF_PROG_ARRAY(cilium_reserved_policy, CILIUM_MAP_RES_POLICY, PIN_GLOBAL_NS, RESERVED_POLICY_SIZE);
 
 /* Private map for internal tail calls */
 BPF_PROG_ARRAY(cilium_calls, CILIUM_MAP_CALLS, PIN_OBJECT_NS, CILIUM_CALL_SIZE);

bpf/lxcmap/lxcmap.go
@@ -77,7 +77,8 @@ func (v6 v6Addr) String() string {
 // programs.
 type LXCInfo struct {
 	IfIndex    uint32
-	SecLabelID uint32
+	SecLabelID uint16
+	LxcID      uint16
 	MAC        MAC
 	NodeMAC    MAC
 	V6Addr     v6Addr
@@ -94,12 +95,13 @@ func (lxc LXCInfo) String() string {
 	if len(portMaps) == 0 {
 		portMaps = append(portMaps, "(empty)")
 	}
-	return fmt.Sprintf("ifindex=%d mac=%s nodemac=%s ip=%s seclabel=0x%x portMaps=%s",
+	return fmt.Sprintf("id=%d ifindex=%d mac=%s nodemac=%s ip=%s seclabel=0x%x portMaps=%s",
+		lxc.LxcID,
 		lxc.IfIndex,
 		lxc.MAC,
 		lxc.NodeMAC,
 		lxc.V6Addr,
-		common.Swab32(lxc.SecLabelID),
+		common.Swab16(lxc.SecLabelID),
 		strings.Join(portMaps, " "),
 	)
 }
@@ -128,7 +130,8 @@ func (m *LXCMap) WriteEndpoint(ep *types.Endpoint) error {
 		// Store security label in network byte order so it can be
 		// written into the packet without an additional byte order
 		// conversion.
-		SecLabelID: common.Swab32(ep.SecLabel.ID),
+		SecLabelID: common.Swab16(uint16(ep.SecLabel.ID)),
+		LxcID:      ep.ID,
 		MAC:        MAC(mac),
 		NodeMAC:    MAC(nodeMAC),
 	}

commit 0c811b67ea043b8068229da8c3b8db54a318761a

bpf: Fix typo in POLICY_MAP_SIZE
Fixes: a205b8db13133a65ff34d160bbeb690234e73364
Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/lib/common.h
@@ -11,7 +11,7 @@
 #define EVENT_SOURCE 0
 #endif
 
-#define POLICY_MAP_SIZE	MAX 65536
+#define POLICY_MAP_SIZE	65536
 #define RESERVED_POLICY_SIZE 128
 
 #define __inline__ __attribute__((always_inline))