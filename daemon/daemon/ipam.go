g0odi3z2j/cilium

commit 8f6ea7c53fce21963e4615282181976b52c97de9

cilium-docker: provide real subnet if IPv4 is enabled
Acked-by: Thomas Graf <thomas@cilium.io>
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon/ipam.go
@@ -178,19 +178,21 @@ func (d *Daemon) ReleaseIP(ipamType ipam.IPAMType, options ipam.IPAMReq) error {
 }
 
 // getIPAMConfLibnetwork returns the Libnetwork specific IPAM configuration.
-func getIPAMConfLibnetwork(ln ipam.IPAMReq, ipamConf *ipam.IPAMConfig) (*ipam.IPAMConfigRep, error) {
+func (d *Daemon) getIPAMConfLibnetwork(ln ipam.IPAMReq) (*ipam.IPAMConfigRep, error) {
 	if ln.RequestPoolRequest != nil {
 		var poolID, pool, gw string
 
 		if ln.RequestPoolRequest.V6 == false {
-			poolID = ipam.LibnetworkDefaultPoolV4
-			pool = ipam.LibnetworkDummyV4AllocPool
-			gw = ipam.LibnetworkDummyV4Gateway
+			if d.conf.IPv4Enabled {
+				poolID = ipam.LibnetworkDefaultPoolV4
+				pool = d.conf.NodeAddress.IPv4AllocRange().String()
+				gw = d.conf.NodeAddress.IPv4Address.IP().String() + "/32"
+			}
 		} else {
-			subnetGo := net.IPNet(ipamConf.IPAMConfig.Subnet)
+			subnetGo := net.IPNet(d.ipamConf.IPAMConfig.Subnet)
 			poolID = ipam.LibnetworkDefaultPoolV6
 			pool = subnetGo.String()
-			gw = ipamConf.IPAMConfig.Gateway.String() + "/128"
+			gw = d.ipamConf.IPAMConfig.Gateway.String() + "/128"
 		}
 
 		return &ipam.IPAMConfigRep{
@@ -205,7 +207,7 @@ func getIPAMConfLibnetwork(ln ipam.IPAMReq, ipamConf *ipam.IPAMConfig) (*ipam.IP
 	}
 
 	ciliumRoutes := []ipam.Route{}
-	for _, r := range ipamConf.IPAMConfig.Routes {
+	for _, r := range d.ipamConf.IPAMConfig.Routes {
 		ciliumRoute := ipam.NewRoute(r.Dst, r.GW)
 		ciliumRoutes = append(ciliumRoutes, *ciliumRoute)
 	}
@@ -213,7 +215,7 @@ func getIPAMConfLibnetwork(ln ipam.IPAMReq, ipamConf *ipam.IPAMConfig) (*ipam.IP
 	return &ipam.IPAMConfigRep{
 		IPAMConfig: &ipam.IPAMRep{
 			IP6: &ipam.IPConfig{
-				Gateway: ipamConf.IPAMConfig.Gateway,
+				Gateway: d.ipamConf.IPAMConfig.Gateway,
 				Routes:  ciliumRoutes,
 			},
 		},
@@ -224,7 +226,7 @@ func getIPAMConfLibnetwork(ln ipam.IPAMReq, ipamConf *ipam.IPAMConfig) (*ipam.IP
 func (d *Daemon) GetIPAMConf(ipamType ipam.IPAMType, options ipam.IPAMReq) (*ipam.IPAMConfigRep, error) {
 	switch ipamType {
 	case ipam.LibnetworkIPAMType:
-		return getIPAMConfLibnetwork(options, d.ipamConf)
+		return d.getIPAMConfLibnetwork(options)
 	}
 	return nil, fmt.Errorf("unknown IPAM Type %s", ipamType)
 }

common/ipam/ipam.go
@@ -37,10 +37,6 @@ const (
 	LibnetworkDefaultPoolV4 = "CiliumPoolv4"
 	// LibnetworkDefaultPoolV6 is the IPv6 pool name for libnetwork.
 	LibnetworkDefaultPoolV6 = "CiliumPoolv6"
-	// LibnetworkDummyV4AllocPool is never exposed, makes libnetwork happy.
-	LibnetworkDummyV4AllocPool = "0.0.0.0/0"
-	// LibnetworkDummyV4Gateway is never exposed, makes libnetwork happy.
-	LibnetworkDummyV4Gateway = "1.1.1.1/32"
 )
 
 // IPAMConfig is the IPAM configuration used for a particular IPAM type.

commit 9d52b65191c635fe83e47e53c3c2f53cb1669e3a

daemon: Respond dummy IPv4 CIDR to libnetwork
Cilium creates a veth (cilium_host) in the host with one of the given
ipv4-range's values that the user uses.

When the user creates a docker network, docker makes a
RequestPoolRequest to the cilium docker plugin where cilium replies with
the ipv4-range set up. Unfortunatly, docker checks if the given Pool
overlaps with any IP already set, which it does with the cilium_host,
and stays in an infinite loop.

This hack allows the RequestPoolReply to be accepted by docker and the
cilium docker plugin to continue to give the necessary IPv4 addresses to
the RequestPoolRequest.

Fixes: 8f6ea7c53f ("cilium-docker: provide real subnet if IPv4 is enabled")
Acked-by: Thomas Graf <thomas@cilium.io>
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon/ipam.go
@@ -188,13 +188,8 @@ func (d *Daemon) getIPAMConfLibnetwork(ln ipam.IPAMReq) (*ipam.IPAMConfigRep, er
 
 		if ln.RequestPoolRequest.V6 == false {
 			poolID = ipam.LibnetworkDefaultPoolV4
-			if d.conf.IPv4Enabled {
-				pool = d.conf.NodeAddress.IPv4AllocRange().String()
-				gw = d.conf.NodeAddress.IPv4Address.IP().String() + "/32"
-			} else {
-				pool = ipam.LibnetworkDummyV4AllocPool
-				gw = ipam.LibnetworkDummyV4Gateway
-			}
+			pool = ipam.LibnetworkDummyV4AllocPool
+			gw = ipam.LibnetworkDummyV4Gateway
 		} else {
 			subnetGo := net.IPNet(d.ipamConf.IPAMConfig.Subnet)
 			poolID = ipam.LibnetworkDefaultPoolV6