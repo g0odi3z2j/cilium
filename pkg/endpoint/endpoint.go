g0odi3z2j/cilium

commit 2dc7fcdaabe07ae4070a13275bc05af97e385e9d

endpoint: Closing EP FD bpf maps on Leave
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/endpoint.go
@@ -763,6 +763,25 @@ func (e *Endpoint) LeaveLocked(owner Owner) {
 		c.Mutex.RUnlock()
 	}
 
+	if e.PolicyMap != nil {
+		if err := e.PolicyMap.Close(); err != nil {
+			log.Warningf("Unable to close policy map %s: %s", e.PolicyMapPathLocked(), err)
+		}
+	}
+
+	if f, err := os.Open(e.Ct6MapPathLocked()); err != nil {
+		log.Debugf("Unable to close IPv6 CT map %s: %s", e.Ct6MapPathLocked(), err)
+	} else {
+		f.Close()
+	}
+
+	log.Debugf("Closing e.Ct4MapPathLocked")
+	if f, err := os.Open(e.Ct4MapPathLocked()); err != nil {
+		log.Debugf("Unable to close IPv4 CT map %s: %s", e.Ct4MapPathLocked(), err)
+	} else {
+		f.Close()
+	}
+
 	e.removeDirectory()
 }
 

commit 9932da85986fc1d163f4d98c2d8d83259a77d81f

endpoint: removing unnecessary os.Open
The CT maps are only used by GC, where their FD are properly closed
there. Thus it is unnecessary to have them Open and Close when the
endpoint is being removed.

Fixes: 2dc7fcdaab ("endpoint: Closing EP FD bpf maps on Leave")

Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/endpoint.go
@@ -769,19 +769,6 @@ func (e *Endpoint) LeaveLocked(owner Owner) {
 		}
 	}
 
-	if f, err := os.Open(e.Ct6MapPathLocked()); err != nil {
-		log.Debugf("Unable to close IPv6 CT map %s: %s", e.Ct6MapPathLocked(), err)
-	} else {
-		f.Close()
-	}
-
-	log.Debugf("Closing e.Ct4MapPathLocked")
-	if f, err := os.Open(e.Ct4MapPathLocked()); err != nil {
-		log.Debugf("Unable to close IPv4 CT map %s: %s", e.Ct4MapPathLocked(), err)
-	} else {
-		f.Close()
-	}
-
 	e.removeDirectory()
 }
 