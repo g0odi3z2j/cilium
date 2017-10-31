g0odi3z2j/cilium

commit ce23e1082059e82d969833f2ebf7c2a9d37c992b

node: Install individual per node routes
The route pointing packets from the host to IPs managed by other nodes
has been covered by a single cluster wide route prefix which covered all
nodes. This did not allow to have any non Cilium IPs unless the IPs were
associated to the local host.

 * Merge node and nodeaddress package together as they depend on each
   other.

 * Move route and address addition from the init.sh script to golang
   native code in pkg/node to allow tiggering route additions based
   on events.

 * Add a per node route for each node discovered in the cluster

 * Per node routes are enabled by default when running on Kubernetes but
   can be disabled with --single-cluster-route.

Fixes: #1846

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/node/manager.go
@@ -40,18 +40,41 @@ const (
 	DirectRoute
 )
 
-var (
-	mutex lock.RWMutex
-	nodes = map[Identity]*Node{}
-)
+type clusterConfiguation struct {
+	lock.RWMutex
+
+	nodes                 map[Identity]*Node
+	ciliumHostInitialized bool
+	usePerNodeRoutes      bool
+	auxPrefixes           []*net.IPNet
+}
+
+var clusterConf = newClusterConfiguration()
+
+func newClusterConfiguration() clusterConfiguation {
+	return clusterConfiguation{
+		nodes:       map[Identity]*Node{},
+		auxPrefixes: []*net.IPNet{},
+	}
+}
+
+func (cc *clusterConfiguation) getNode(ni Identity) *Node {
+	cc.RLock()
+	n := cc.nodes[ni]
+	cc.RUnlock()
+	return n
+}
+
+func (cc *clusterConfiguation) addAuxPrefix(prefix *net.IPNet) {
+	cc.Lock()
+	cc.auxPrefixes = append(cc.auxPrefixes, prefix)
+	cc.Unlock()
+}
 
 // GetNode returns the node with the given identity, if exists, from the nodes
 // map.
 func GetNode(ni Identity) *Node {
-	mutex.RLock()
-	n := nodes[ni]
-	mutex.RUnlock()
-	return n
+	return clusterConf.getNode(ni)
 }
 
 func deleteNodeCIDR(ip *net.IPNet) {
@@ -64,12 +87,210 @@ func deleteNodeCIDR(ip *net.IPNet) {
 	}
 }
 
-func updateNodeCIDR(ip *net.IPNet, host net.IP) {
+func ipFamily(ip net.IP) int {
+	if ip.To4() == nil {
+		return netlink.FAMILY_V6
+	}
+
+	return netlink.FAMILY_V4
+}
+
+// findAddress finds a particular IP address assigned to the specified link
+func findAddress(link netlink.Link, ip net.IP) *netlink.Addr {
+	addrs, err := netlink.AddrList(link, ipFamily(ip))
+	if err != nil {
+		log.WithError(err).WithFields(log.Fields{
+			logfields.IPAddr:    ip,
+			logfields.Interface: link.Attrs().Name,
+		}).Warn("Listing of addresses failed")
+
+		// return address not found on error
+		return nil
+	}
+
+	for _, a := range addrs {
+		if ip.Equal(a.IP) {
+			return &a
+		}
+	}
+
+	return nil
+}
+
+// findRoute finds a particular route as specified by the filter which points
+// to the specified device. The filter route can have the following fields set:
+//  - Dst
+//  - LinkIndex
+//  - Scope
+//  - Gw
+func findRoute(link netlink.Link, route *netlink.Route) *netlink.Route {
+	routes, err := netlink.RouteList(link, ipFamily(route.Dst.IP))
+	if err != nil {
+		return nil
+	}
+
+	for _, r := range routes {
+		aMaskLen, aMaskBits := r.Dst.Mask.Size()
+		bMaskLen, bMaskBits := route.Dst.Mask.Size()
+		if r.LinkIndex == route.LinkIndex && r.Scope == route.Scope &&
+			aMaskLen == bMaskLen && aMaskBits == bMaskBits &&
+			r.Dst.IP.Equal(route.Dst.IP) && r.Gw.Equal(route.Gw) {
+			return &r
+		}
+	}
+
+	return nil
+}
+
+// replaceNodeRoute verifies that the L2 route for the router IP which is used
+// as nexthop for all node routes is properly installed. If unavailable or
+// incorrect, it will be replaced with the proper L2 route.
+func replaceNexthopRoute(link netlink.Link, routerIP, routerNet *net.IPNet) error {
+	// Add the Cilium router IP as address to the "cilium_host" if not
+	// already assigned
+	if routerIP != nil && findAddress(link, routerIP.IP) == nil {
+		addr := &netlink.Addr{IPNet: routerIP}
+		if err := netlink.AddrReplace(link, addr); err != nil {
+			return fmt.Errorf("unable to add nexthop address \"%s\": %q", routerIP, err)
+		}
+
+		log.WithFields(log.Fields{
+			logfields.IPAddr:    routerIP,
+			logfields.Interface: link.Attrs().Name,
+		}).Info("Added Cilium router IP address")
+	}
+
+	// This is the L2 route which makes the Cilium router IP available behind
+	// the "cilium_host" interface. All other routes will use this router IP
+	// as nexthop.
+	route := &netlink.Route{
+		LinkIndex: link.Attrs().Index,
+		Dst:       routerNet,
+		Scope:     netlink.SCOPE_LINK,
+	}
+
+	if findRoute(link, route) == nil {
+		scopedLog := log.WithField(logfields.Route, route)
+
+		if err := netlink.RouteReplace(route); err != nil {
+			scopedLog.WithError(err).Error("Unable to add L2 nexthop route")
+			return fmt.Errorf("unable to add L2 nexthop route")
+		}
+
+		scopedLog.Info("Added L2 nexthop route")
+	}
+
+	return nil
+}
+
+// replaceNodeRoute verifies whether the specified node CIDR is properly
+// covered by a route installed in the host's routing table. If unavailable,
+// the route is installed on the host.
+func replaceNodeRoute(ip *net.IPNet) {
 	if ip == nil {
 		return
 	}
 
-	if err := tunnel.SetTunnelEndpoint(ip.IP, host); err != nil {
+	link, err := netlink.LinkByName(HostDevice)
+	if err != nil {
+		log.WithError(err).WithField(logfields.Interface, HostDevice).Error("Unable to lookup interface")
+		return
+	}
+
+	var routerIP, routerNet *net.IPNet
+	var via, local net.IP
+	if ip.IP.To4() != nil {
+		via = net.IPv4(169, 254, 254, 1)
+		routerIP = &net.IPNet{IP: via, Mask: net.CIDRMask(32, 32)}
+		routerNet = &net.IPNet{IP: net.IPv4(169, 254, 254, 0), Mask: net.CIDRMask(24, 32)}
+		local = GetInternalIPv4()
+	} else {
+		via = GetIPv6Router()
+		// IPv6 does not require the router IP to be added as address, leaving
+		routerIP = nil
+		routerNet = &net.IPNet{IP: via, Mask: net.CIDRMask(128, 128)}
+		local = GetIPv6()
+	}
+
+	if err := replaceNexthopRoute(link, routerIP, routerNet); err != nil {
+		log.WithError(err).Error("Unable to add nexthop route")
+	}
+
+	route := netlink.Route{LinkIndex: link.Attrs().Index, Dst: ip, Gw: via, Src: local}
+	scopedLog := log.WithField(logfields.Route, route)
+
+	if err := netlink.RouteReplace(&route); err != nil {
+		scopedLog.WithError(err).Error("Unable to add node route")
+	} else {
+		scopedLog.Info("Installed node route")
+	}
+}
+
+func (cc *clusterConfiguation) replaceHostRoutes() {
+	if !cc.ciliumHostInitialized {
+		log.Debug("Deferring node routes installation, host device not present yet")
+		return
+	}
+
+	// We have the option to use per node routes if a control plane is in
+	// place which gives us a list of all nodes and their node CIDRs. This
+	// allows to share a CIDR with legacy endpoints outside of the cluster
+	// but requires individual routes to be installed which creates an
+	// overhead with many nodes.
+	if cc.usePerNodeRoutes {
+		for _, n := range cc.nodes {
+			replaceNodeRoute(n.IPv4AllocCIDR)
+			replaceNodeRoute(n.IPv6AllocCIDR)
+		}
+	} else {
+		replaceNodeRoute(GetIPv4AllocRange())
+		replaceNodeRoute(GetIPv6AllocRange())
+	}
+
+	for _, prefix := range cc.auxPrefixes {
+		replaceNodeRoute(prefix)
+	}
+}
+
+func (cc *clusterConfiguation) installHostRoutes() {
+	cc.Lock()
+	cc.ciliumHostInitialized = true
+	cc.replaceHostRoutes()
+	cc.Unlock()
+}
+
+// InstallHostRoutes installs all required routes to make the following IP
+// spaces available from the local host:
+//  - node CIDR of local and remote nodes
+//  - service CIDR range
+//
+// This may only be called after the cilium_host interface has been initialized
+// for the first time
+func InstallHostRoutes() {
+	clusterConf.installHostRoutes()
+}
+
+// AddAuxPrefix adds additional prefixes for which routes should be installed
+// that point to the Cilium network. This function does not directly install
+// the route but schedules it for addition by InstallHostRoutes
+func AddAuxPrefix(prefix *net.IPNet) {
+	clusterConf.addAuxPrefix(prefix)
+}
+
+// EnablePerNodeRoutes enables use of per node routes. This function must be called
+// at init time before any routes are installed.
+func EnablePerNodeRoutes() {
+	clusterConf.Lock()
+	clusterConf.usePerNodeRoutes = true
+	clusterConf.Unlock()
+}
+
+func updateNodeCIDR(n *Node, ip *net.IPNet) {
+	if ip == nil {
+		return
+	}
+
+	if err := tunnel.SetTunnelEndpoint(ip.IP, n.GetNodeIP(false)); err != nil {
 		log.WithError(err).WithField(logfields.IPAddr, ip).Error("bpf: Unable to update in tunnel endpoint map")
 	}
 }
@@ -78,10 +299,10 @@ func updateNodeCIDR(ip *net.IPNet, host net.IP) {
 // When using DirectRoute RouteType the field ownAddr should contain the IPv6
 // address of the interface that can reach the other nodes.
 func UpdateNode(ni Identity, n *Node, routesTypes RouteType, ownAddr net.IP) {
-	mutex.Lock()
-	defer mutex.Unlock()
+	clusterConf.Lock()
+	defer clusterConf.Unlock()
 
-	oldNode, oldNodeExists := nodes[ni]
+	oldNode, oldNodeExists := clusterConf.nodes[ni]
 	if (routesTypes & TunnelRoute) != 0 {
 		if oldNodeExists {
 			deleteNodeCIDR(oldNode.IPv4AllocCIDR)
@@ -94,23 +315,23 @@ func UpdateNode(ni Identity, n *Node, routesTypes RouteType, ownAddr net.IP) {
 			logfields.V6Prefix: n.IPv6AllocCIDR,
 		}).Debug("bpf: Setting tunnel endpoint")
 
-		nodeIP := n.GetNodeIP(false)
-		updateNodeCIDR(n.IPv4AllocCIDR, nodeIP)
-		updateNodeCIDR(n.IPv6AllocCIDR, nodeIP)
+		updateNodeCIDR(n, n.IPv4AllocCIDR)
+		updateNodeCIDR(n, n.IPv6AllocCIDR)
 	}
 	if (routesTypes & DirectRoute) != 0 {
 		updateIPRoute(oldNode, n, ownAddr)
 	}
 
-	nodes[ni] = n
+	clusterConf.nodes[ni] = n
+	clusterConf.replaceHostRoutes()
 }
 
 // DeleteNode remove the node from the nodes' maps and / or the L3 routes to
 // reach that node.
 func DeleteNode(ni Identity, routesTypes RouteType) {
 	var err1, err2 error
-	mutex.Lock()
-	if n, ok := nodes[ni]; ok {
+	clusterConf.Lock()
+	if n, ok := clusterConf.nodes[ni]; ok {
 		if (routesTypes & TunnelRoute) != 0 {
 			log.WithFields(log.Fields{
 				logfields.IPAddr:   n.GetNodeIP(false),
@@ -137,12 +358,12 @@ func DeleteNode(ni Identity, routesTypes RouteType) {
 		}
 	}
 
-	// Keep node around
 	if err1 == nil && err2 == nil {
-		delete(nodes, ni)
+		delete(clusterConf.nodes, ni)
 	}
 
-	mutex.Unlock()
+	clusterConf.replaceHostRoutes()
+	clusterConf.Unlock()
 }
 
 // updateIPRoute updates the IP routing entry for the given node n via the

bpf/init.sh
@@ -16,18 +16,13 @@
 
 LIB=$1
 RUNDIR=$2
-IP6_ROUTER=$3
-IP4_HOST=$4
-IP6_HOST=$5
-IP4_RANGE=$6
-IP6_RANGE=$7
-IP4_SVC_RANGE=$8
-IP6_SVC_RANGE=$9
-MODE=${10}
+IP4_HOST=$3
+IP6_HOST=$4
+MODE=$5
 # Only set if MODE = "direct" or "lb"
-NATIVE_DEV=${11}
-XDP_DEV=${12}
-XDP_MODE=${13}
+NATIVE_DEV=$6
+XDP_DEV=$7
+XDP_MODE=$8
 
 HOST_ID="host"
 WORLD_ID="world"
@@ -259,36 +254,10 @@ echo "#define HOST_IFINDEX_MAC { .addr = ${HOST_MAC}}" >> $RUNDIR/globals/node_c
 	ip -6 addr add $IP6_HOST dev $HOST_DEV1
 }
 
-ip route del $IP6_ROUTER/128 2> /dev/null || true
-ip route add $IP6_ROUTER/128 dev $HOST_DEV1
-ip route del $IP6_RANGE 2> /dev/null || true
-ip route add $IP6_RANGE via $IP6_ROUTER src $IP6_HOST
-
-if [ "$IP6_SVC_RANGE" != "auto" ]; then
-	ip route del $IP6_SVC_RANGE 2> /dev/null || true
-	ip route add $IP6_SVC_RANGE via $IP6_ROUTER src $IP6_HOST
-fi
-
 if [[ "$IP4_HOST" != "<nil>" ]]; then
 	[ -n "$(ip -4 addr show to $IP4_HOST)" ] || {
 		ip -4 addr add $IP4_HOST dev $HOST_DEV1 scope link
 	}
-fi
-
-ip addr del 169.254.254.1/32 dev $HOST_DEV1 2> /dev/null || true
-ip addr add 169.254.254.1/32 dev $HOST_DEV1 scope link
-ip route del 169.254.254.0/24 dev $HOST_DEV1 2> /dev/null || true
-ip route add 169.254.254.0/24 dev $HOST_DEV1 scope link
-ip route del $IP4_RANGE 2> /dev/null || true
-if [[ "$IP4_HOST" != "<nil>" ]]; then
-  ip route add $IP4_RANGE via 169.254.254.1 src $IP4_HOST
-fi
-
-if [ "$IP4_SVC_RANGE" != "auto" ]; then
-	ip route del $IP4_SVC_RANGE 2> /dev/null || true
-        if [[ "$IP4_HOST" != "<nil>" ]]; then
-          ip route add $IP4_SVC_RANGE via 169.254.254.1 src $IP4_HOST
-        fi
 fi
 
 # Decrease priority of the rule to identify local addresses

common/plugins/ipam.go
@@ -19,7 +19,7 @@ import (
 	"net"
 
 	"github.com/cilium/cilium/api/v1/models"
-	"github.com/cilium/cilium/pkg/nodeaddress"
+	"github.com/cilium/cilium/pkg/node"
 )
 
 // IPv6Gateway returns the IPv6 gateway address for endpoints.
@@ -66,11 +66,11 @@ func IPv6Routes(addr *models.NodeAddressing) ([]Route, error) {
 		{
 			Prefix: net.IPNet{
 				IP:   ip,
-				Mask: nodeaddress.ContainerIPv6Mask,
+				Mask: node.ContainerIPv6Mask,
 			},
 		},
 		{
-			Prefix:  nodeaddress.IPv6DefaultRoute,
+			Prefix:  node.IPv6DefaultRoute,
 			Nexthop: &ip,
 		},
 	}, nil
@@ -86,11 +86,11 @@ func IPv4Routes(addr *models.NodeAddressing) ([]Route, error) {
 		{
 			Prefix: net.IPNet{
 				IP:   ip,
-				Mask: nodeaddress.ContainerIPv4Mask,
+				Mask: node.ContainerIPv4Mask,
 			},
 		},
 		{
-			Prefix:  nodeaddress.IPv4DefaultRoute,
+			Prefix:  node.IPv4DefaultRoute,
 			Nexthop: &ip,
 		},
 	}, nil

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=3fc90f93eb8197014a31fdd600944d44de9d91e6
-GO_VERSION_USED=go1.9.2
+GO_BINDATA_SHA1SUM=2a37ccd03224d98ffe24b79edb05bca13947f564
+GO_VERSION_USED=go1.8.3

daemon/daemon.go
@@ -52,7 +52,6 @@ import (
 	"github.com/cilium/cilium/pkg/maps/policymap"
 	"github.com/cilium/cilium/pkg/maps/tunnel"
 	"github.com/cilium/cilium/pkg/node"
-	"github.com/cilium/cilium/pkg/nodeaddress"
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/proxy"
 	"github.com/cilium/cilium/pkg/workloads"
@@ -76,13 +75,8 @@ const (
 const (
 	initArgLib int = iota
 	initArgRundir
-	initArgIPv6Router
 	initArgIPv4NodeIP
 	initArgIPv6NodeIP
-	initArgIPv4Range
-	initArgIPv6Range
-	initArgIPv4ServiceRange
-	initArgIPv6ServiceRange
 	initArgMode
 	initArgDevice
 	initArgDevicePreFilter
@@ -548,10 +542,10 @@ func (d *Daemon) installMasqRule() error {
 	if err := runProg("iptables", []string{
 		"-t", "nat",
 		"-A", ciliumPostNatChain,
-		"!", "-s", nodeaddress.GetHostMasqueradeIPv4().String(),
+		"!", "-s", node.GetHostMasqueradeIPv4().String(),
 		"-o", "cilium_host",
 		"-m", "comment", "--comment", "cilium host->cluster masquerade",
-		"-j", "SNAT", "--to-source", nodeaddress.GetHostMasqueradeIPv4().String()}, false); err != nil {
+		"-j", "SNAT", "--to-source", node.GetHostMasqueradeIPv4().String()}, false); err != nil {
 		return err
 	}
 
@@ -560,8 +554,8 @@ func (d *Daemon) installMasqRule() error {
 	if err := runProg("iptables", []string{
 		"-t", "nat",
 		"-A", "CILIUM_POST",
-		"-s", nodeaddress.GetIPv4AllocRange().String(),
-		"!", "-d", nodeaddress.GetIPv4AllocRange().String(),
+		"-s", node.GetIPv4AllocRange().String(),
+		"!", "-d", node.GetIPv4AllocRange().String(),
 		"!", "-o", "cilium_+",
 		"-m", "comment", "--comment", "cilium masquerade non-cluster",
 		"-j", "MASQUERADE"}, false); err != nil {
@@ -623,11 +617,8 @@ func (d *Daemon) compileBase() error {
 
 	args[initArgLib] = d.conf.BpfDir
 	args[initArgRundir] = d.conf.StateDir
-	args[initArgIPv6Router] = nodeaddress.GetIPv6NoZeroComp()
-	args[initArgIPv4NodeIP] = nodeaddress.GetInternalIPv4().String()
-	args[initArgIPv6NodeIP] = nodeaddress.GetIPv6().String()
-	args[initArgIPv4ServiceRange] = v4ServicePrefix
-	args[initArgIPv6ServiceRange] = v6ServicePrefix
+	args[initArgIPv4NodeIP] = node.GetInternalIPv4().String()
+	args[initArgIPv6NodeIP] = node.GetIPv6().String()
 
 	if d.conf.Device != "undefined" {
 		_, err := netlink.LinkByName(d.conf.Device)
@@ -649,11 +640,6 @@ func (d *Daemon) compileBase() error {
 			mode = "direct"
 		}
 
-		// in direct routing mode, only packets to the local node
-		// prefix should go to cilium_host
-		args[initArgIPv4Range] = nodeaddress.GetIPv4AllocRange().String()
-		args[initArgIPv6Range] = nodeaddress.GetIPv6NodeRange().String()
-
 		args[initArgMode] = mode
 		args[initArgDevice] = d.conf.Device
 
@@ -663,10 +649,6 @@ func (d *Daemon) compileBase() error {
 			//FIXME: allow LBMode in tunnel
 			return fmt.Errorf("Unable to run LB mode with tunnel mode")
 		}
-
-		// in tunnel mode, all packets in the cluster should go to cilium_host
-		args[initArgIPv4Range] = nodeaddress.GetIPv4ClusterRange().String()
-		args[initArgIPv6Range] = nodeaddress.GetIPv6ClusterRange().String()
 
 		args[initArgMode] = d.conf.Tunnel
 	}
@@ -692,6 +674,7 @@ func (d *Daemon) compileBase() error {
 	}
 
 	ipam.ReserveLocalRoutes()
+	node.InstallHostRoutes()
 
 	if !d.conf.IPv4Disabled {
 		// Always remove masquerade rule and then re-add it if required
@@ -729,8 +712,8 @@ func (d *Daemon) init() error {
 	}
 	fw := bufio.NewWriter(f)
 
-	routerIP := nodeaddress.GetIPv6Router()
-	hostIP := nodeaddress.GetIPv6()
+	routerIP := node.GetIPv6Router()
+	hostIP := node.GetIPv6()
 
 	fmt.Fprintf(fw, ""+
 		"/*\n"+
@@ -745,13 +728,13 @@ func (d *Daemon) init() error {
 			" * Host-IPv4: %s\n"+
 			" */\n\n"+
 			"#define ENABLE_IPV4\n",
-			nodeaddress.GetInternalIPv4().String())
+			node.GetInternalIPv4().String())
 	}
 
 	fw.WriteString(common.FmtDefineComma("ROUTER_IP", routerIP))
 
 	if !d.conf.IPv4Disabled {
-		ipv4GW := nodeaddress.GetInternalIPv4()
+		ipv4GW := node.GetInternalIPv4()
 		fmt.Fprintf(fw, "#define IPV4_GATEWAY %#x\n", byteorder.HostSliceToNetwork(ipv4GW, reflect.Uint32).(uint32))
 		fmt.Fprintf(fw, "#define IPV4_LOOPBACK %#x\n", byteorder.HostSliceToNetwork(d.loopbackIPv4, reflect.Uint32).(uint32))
 	} else {
@@ -760,10 +743,10 @@ func (d *Daemon) init() error {
 		fmt.Fprintf(fw, "#define IPV4_LOOPBACK %#x\n", 0)
 	}
 
-	ipv4Range := nodeaddress.GetIPv4AllocRange()
+	ipv4Range := node.GetIPv4AllocRange()
 	fmt.Fprintf(fw, "#define IPV4_MASK %#x\n", byteorder.HostSliceToNetwork(ipv4Range.Mask, reflect.Uint32).(uint32))
 
-	ipv4ClusterRange := nodeaddress.GetIPv4ClusterRange()
+	ipv4ClusterRange := node.GetIPv4ClusterRange()
 	fmt.Fprintf(fw, "#define IPV4_CLUSTER_RANGE %#x\n", byteorder.HostSliceToNetwork(ipv4ClusterRange.IP, reflect.Uint32).(uint32))
 	fmt.Fprintf(fw, "#define IPV4_CLUSTER_MASK %#x\n", byteorder.HostSliceToNetwork(ipv4ClusterRange.Mask, reflect.Uint32).(uint32))
 
@@ -796,10 +779,10 @@ func (d *Daemon) init() error {
 		}
 
 		localIPs := []net.IP{
-			nodeaddress.GetInternalIPv4(),
-			nodeaddress.GetExternalIPv4(),
-			nodeaddress.GetIPv6(),
-			nodeaddress.GetIPv6Router(),
+			node.GetInternalIPv4(),
+			node.GetExternalIPv4(),
+			node.GetIPv6(),
+			node.GetIPv6Router(),
 		}
 		for _, ip := range localIPs {
 			log.WithField(logfields.IPAddr, ip).Debug("Adding local ip to endpoint map")
@@ -910,6 +893,10 @@ func NewDaemon(c *Config) (*Daemon, error) {
 			log.Info("k8s mode: Allowing localhost to reach local endpoints")
 			config.alwaysAllowLocalhost = true
 		}
+
+		if !singleClusterRoute {
+			node.EnablePerNodeRoutes()
+		}
 	}
 	// If the device has been specified, the IPv4AllocPrefix and the
 	// IPv6AllocPrefix were already allocated before the k8s.Init().
@@ -920,24 +907,26 @@ func NewDaemon(c *Config) (*Daemon, error) {
 	// Then, we will calculate the IPv4 or IPv6 alloc prefix based on the IPv6
 	// or IPv4 alloc prefix, respectively, retrieved by k8s node annotations.
 	if config.Device == "undefined" {
-		nodeaddress.InitDefaultPrefix("")
+		node.InitDefaultPrefix("")
 	}
 
-	nodeaddress.SetIPv4ClusterCidrMaskSize(v4ClusterCidrMaskSize)
+	node.SetIPv4ClusterCidrMaskSize(v4ClusterCidrMaskSize)
 
 	if v4Prefix != AutoCIDR {
 		_, net, err := net.ParseCIDR(v4Prefix)
 		if err != nil {
 			log.WithError(err).WithField(logfields.V4Prefix, v4Prefix).Fatal("Invalid IPv4 allocation prefix")
 		}
-		nodeaddress.SetIPv4AllocRange(net)
+		node.SetIPv4AllocRange(net)
 	}
 
 	if v4ServicePrefix != AutoCIDR {
-		_, _, err := net.ParseCIDR(v4ServicePrefix)
+		_, ipnet, err := net.ParseCIDR(v4ServicePrefix)
 		if err != nil {
 			log.WithError(err).WithField(logfields.V4Prefix, v4ServicePrefix).Fatal("Invalid IPv4 service prefix")
 		}
+
+		node.AddAuxPrefix(ipnet)
 	}
 
 	if v6Prefix != AutoCIDR {
@@ -946,30 +935,27 @@ func NewDaemon(c *Config) (*Daemon, error) {
 			log.WithError(err).WithField(logfields.V6Prefix, v6ServicePrefix).Fatal("Invalid IPv6 allocation prefix")
 		}
 
-		if err := nodeaddress.SetIPv6NodeRange(net); err != nil {
+		if err := node.SetIPv6NodeRange(net); err != nil {
 			log.WithError(err).WithField(logfields.V6Prefix, net).Fatal("Invalid per node IPv6 allocation prefix")
 		}
 	}
 
 	if v6ServicePrefix != AutoCIDR {
-		_, _, err := net.ParseCIDR(v6ServicePrefix)
+		_, ipnet, err := net.ParseCIDR(v6ServicePrefix)
 		if err != nil {
 			log.WithError(err).WithField(logfields.V6Prefix, v6ServicePrefix).Fatal("Invalid IPv6 service prefix")
 		}
+
+		node.AddAuxPrefix(ipnet)
 	}
 
-	if err := nodeaddress.AutoComplete(); err != nil {
+	if err := node.AutoComplete(); err != nil {
 		log.WithError(err).Fatal("Cannot autocomplete node IPv6 address")
 	}
 
-	// Populate list of nodes with local node entry
-	ni, n := nodeaddress.GetNode()
-	node.UpdateNode(ni, n, node.TunnelRoute, nil)
-
 	if k8s.IsEnabled() {
-		err := k8s.AnnotateNodeCIDR(k8s.Client(), nodeaddress.GetName(),
-			nodeaddress.GetIPv4AllocRange(),
-			nodeaddress.GetIPv6NodeRange())
+		err := k8s.AnnotateNodeCIDR(k8s.Client(), node.GetName(),
+			node.GetIPv4AllocRange(), node.GetIPv6NodeRange())
 		if err != nil {
 			log.WithError(err).Fatal("Cannot annotate node CIDR range data")
 		}
@@ -980,21 +966,25 @@ func NewDaemon(c *Config) (*Daemon, error) {
 		log.WithError(err).Fatal("IPAM init failed")
 	}
 
-	if err := nodeaddress.ValidatePostInit(); err != nil {
+	if err := node.ValidatePostInit(); err != nil {
 		log.WithError(err).Fatal("postinit failed")
 	}
 
 	// REVIEW should these be changed? they seem intended for humans
-	log.Infof("Local node-name: %s", nodeaddress.GetName())
-	log.Infof("Node-IPv6: %s", nodeaddress.GetIPv6())
-	log.Infof("External-Node IPv4: %s", nodeaddress.GetExternalIPv4())
-	log.Infof("Internal-Node IPv4: %s", nodeaddress.GetInternalIPv4())
-	log.Infof("Cluster IPv6 prefix: %s", nodeaddress.GetIPv6ClusterRange())
-	log.Infof("Cluster IPv4 prefix: %s", nodeaddress.GetIPv4ClusterRange())
-	log.Infof("IPv6 node prefix: %s", nodeaddress.GetIPv6NodeRange())
-	log.Infof("IPv6 allocation prefix: %s", nodeaddress.GetIPv6AllocRange())
-	log.Infof("IPv4 allocation prefix: %s", nodeaddress.GetIPv4AllocRange())
-	log.Debugf("IPv6 router address: %s", nodeaddress.GetIPv6Router())
+	log.Infof("Local node-name: %s", node.GetName())
+	log.Infof("Node-IPv6: %s", node.GetIPv6())
+	log.Infof("External-Node IPv4: %s", node.GetExternalIPv4())
+	log.Infof("Internal-Node IPv4: %s", node.GetInternalIPv4())
+	log.Infof("Cluster IPv6 prefix: %s", node.GetIPv6ClusterRange())
+	log.Infof("Cluster IPv4 prefix: %s", node.GetIPv4ClusterRange())
+	log.Infof("IPv6 node prefix: %s", node.GetIPv6NodeRange())
+	log.Infof("IPv6 allocation prefix: %s", node.GetIPv6AllocRange())
+	log.Infof("IPv4 allocation prefix: %s", node.GetIPv4AllocRange())
+	log.Debugf("IPv6 router address: %s", node.GetIPv6Router())
+
+	// Populate list of nodes with local node entry
+	ni, n := node.GetLocalNode()
+	node.UpdateNode(ni, n, node.TunnelRoute, nil)
 
 	if !d.conf.IPv4Disabled {
 		// Allocate IPv4 service loopback IP
@@ -1230,13 +1220,13 @@ func (d *Daemon) getNodeAddressing() *models.NodeAddressing {
 	return &models.NodeAddressing{
 		IPV6: &models.NodeAddressingElement{
 			Enabled:    true,
-			IP:         nodeaddress.GetIPv6Router().String(),
-			AllocRange: nodeaddress.GetIPv6AllocRange().String(),
+			IP:         node.GetIPv6Router().String(),
+			AllocRange: node.GetIPv6AllocRange().String(),
 		},
 		IPV4: &models.NodeAddressingElement{
 			Enabled:    !d.conf.IPv4Disabled,
-			IP:         nodeaddress.GetInternalIPv4().String(),
-			AllocRange: nodeaddress.GetIPv4AllocRange().String(),
+			IP:         node.GetInternalIPv4().String(),
+			AllocRange: node.GetIPv4AllocRange().String(),
 		},
 	}
 }

daemon/k8s_watcher.go
@@ -28,7 +28,6 @@ import (
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logfields"
 	"github.com/cilium/cilium/pkg/node"
-	"github.com/cilium/cilium/pkg/nodeaddress"
 
 	log "github.com/sirupsen/logrus"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
@@ -1219,8 +1218,8 @@ func (d *Daemon) nodesAddFn(obj interface{}) {
 	var ownAddr net.IP
 	if autoIPv6NodeRoutes && d.conf.Device != "undefined" {
 		// ignore own node
-		if n.Name != nodeaddress.GetName() {
-			ownAddr = nodeaddress.GetIPv6()
+		if n.Name != node.GetName() {
+			ownAddr = node.GetIPv6()
 			routeTypes |= node.DirectRoute
 		}
 	}
@@ -1257,8 +1256,8 @@ func (d *Daemon) nodesModFn(oldObj interface{}, newObj interface{}) {
 	var ownAddr net.IP
 	if autoIPv6NodeRoutes && d.conf.Device != "undefined" {
 		// ignore own node
-		if newNode.Name != nodeaddress.GetName() {
-			ownAddr = nodeaddress.GetIPv6()
+		if newNode.Name != node.GetName() {
+			ownAddr = node.GetIPv6()
 			routeTypes |= node.DirectRoute
 		}
 	}

daemon/labels.go
@@ -28,7 +28,7 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logfields"
-	"github.com/cilium/cilium/pkg/nodeaddress"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/policy"
 
 	"github.com/go-openapi/runtime/middleware"
@@ -51,7 +51,7 @@ func gasNewSecLabelID(id *policy.Identity) error {
 }
 
 func (d *Daemon) CreateOrUpdateIdentity(lbls labels.Labels, epid string) (*policy.Identity, bool, error) {
-	clusterEndpointID := nodeaddress.GetExternalIPv4().String() + ":" + epid
+	clusterEndpointID := node.GetExternalIPv4().String() + ":" + epid
 
 	log.WithFields(log.Fields{
 		logfields.EndpointID:     epid,
@@ -306,7 +306,7 @@ func (d *Daemon) DeleteIdentity(id policy.NumericIdentity, epid string) error {
 // DeleteIdentityBySHA256 deletes the SecCtxLabels that belong to the labels' sha256Sum.
 func (d *Daemon) DeleteIdentityBySHA256(sha256Sum string, epid string) error {
 	if epid != "" {
-		epid = nodeaddress.GetExternalIPv4().String() + ":" + epid
+		epid = node.GetExternalIPv4().String() + ":" + epid
 	}
 
 	if sha256Sum == "" {

daemon/main.go
@@ -39,7 +39,7 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logfields"
-	"github.com/cilium/cilium/pkg/nodeaddress"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/pprof"
@@ -94,6 +94,7 @@ var (
 	k8sAPIServer          string
 	k8sKubeConfigPath     string
 	dockerEndpoint        string
+	singleClusterRoute    bool
 )
 
 var logOpts = make(map[string]string)
@@ -330,7 +331,7 @@ func init() {
 	flags.Uint32Var(&logstashProbeTimer,
 		"logstash-probe-timer", 10, "Logstash probe timer (seconds)")
 	flags.StringVar(&nat46prefix,
-		"nat46-range", nodeaddress.DefaultNAT46Prefix, "IPv6 prefix to map IPv4 addresses to")
+		"nat46-range", node.DefaultNAT46Prefix, "IPv6 prefix to map IPv4 addresses to")
 	flags.BoolVar(&masquerade,
 		"masquerade", true, "Masquerade packets from endpoints leaving the host")
 	flags.StringVar(&v6Address,
@@ -339,6 +340,8 @@ func init() {
 		"ipv4-node", "auto", "IPv4 address of node")
 	flags.BoolVar(&config.RestoreState,
 		"restore", true, "Restores state, if possible, from previous daemon")
+	flags.BoolVar(&singleClusterRoute, "single-cluster-route", false,
+		"Use a single cluster route instead of per node routes")
 	flags.StringVar(&socketPath,
 		"socket-path", defaults.SockPath, "Sets daemon's socket path to listen for connections")
 	flags.StringVar(&config.RunDir,
@@ -413,7 +416,7 @@ func initConfig() {
 
 	if config.IPv4Disabled {
 		endpoint.IPv4Enabled = false
-		nodeaddress.EnableIPv4 = false
+		node.EnableIPv4 = false
 	}
 
 	scopedLog := log.WithFields(log.Fields{
@@ -527,7 +530,7 @@ func initEnv() {
 	// If device has been specified, use it to derive better default
 	// allocation prefixes
 	if config.Device != "undefined" {
-		nodeaddress.InitDefaultPrefix(config.Device)
+		node.InitDefaultPrefix(config.Device)
 	}
 
 	if v6Address != "auto" {
@@ -538,7 +541,7 @@ func initEnv() {
 				log.WithField(logfields.IPAddr, ip).Fatal("Invalid IPv6 node address: not a global unicast address")
 			}
 
-			nodeaddress.SetIPv6(ip)
+			node.SetIPv6(ip)
 		}
 	}
 
@@ -546,7 +549,7 @@ func initEnv() {
 		if ip := net.ParseIP(v4Address); ip == nil {
 			log.WithField(logfields.IPAddr, v4Address).Fatal("Invalid IPv4 node address")
 		} else {
-			nodeaddress.SetExternalIPv4(ip)
+			node.SetExternalIPv4(ip)
 		}
 	}
 

commit 90d31015ed8484ff0e3007788d42ada940d9f6ef

node: Account for nil route.Dst
Fixes: ce23e108205 ("node: Install individual per node routes")
Reported-by: Ashwin Paranjpe <ashwinp.work@gmail.com>
Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/node/manager.go
@@ -130,6 +130,14 @@ func findRoute(link netlink.Link, route *netlink.Route) *netlink.Route {
 	}
 
 	for _, r := range routes {
+		if r.Dst != nil && route.Dst == nil {
+			continue
+		}
+
+		if route.Dst != nil && r.Dst == nil {
+			continue
+		}
+
 		aMaskLen, aMaskBits := r.Dst.Mask.Size()
 		bMaskLen, bMaskBits := route.Dst.Mask.Size()
 		if r.LinkIndex == route.LinkIndex && r.Scope == route.Scope &&