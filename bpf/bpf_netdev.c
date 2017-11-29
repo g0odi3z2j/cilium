g0odi3z2j/cilium

commit 5fbd5b5d46e9a307db80db1fcc634c9d0467683f

proxy: Distinguish between proxy and other local processes
The datapath used a simple sip == HOST_IP to detect local proxy traffic
to bypass the proxy on the second pass through. This logic is flawed and
causes the proxy to be bypassed in the following legitimate situations:
 * local process uses HOST_IP as source and talks to local endpoint
 * both egress and ingress proxy are injected between two local
   endpoints

This commit fixes this situation by:

 * Introducing a new custom dialer for the proxy. This is required
   because net.Dial() does not allow to set a socket option between
   creating the socket and calling connect(). Access to the socket
   is required to set the SO_MARK before the connect() to ensure
   that the first SYN packet contains the proper SO_MARK.

 * Replace net.Listen() with custom code so we can set the SO_MARK
   before calling syscall.Listen() to ensure that all child sockets
   inherit the SO_MARK. This ensures that even the SYN+ACK generated
   by the kernel will haver proper packet markings.

 * Extending the SO_MARK to contain a magic marker in the lower 12 bits
   of skb->mark (0xFEA for ingress proxy, 0xFEB for egress proxy). Thus
   allowing to detect packets from the proxy by matching against the
   magic marker. The identity is moved to the upper 16 bits of skb->mark

   The skb->mark is cleared on veth traversal so we match against the
   magic marker at egress on the way out to cilium_host and set a flag
   in tc_index to indicate skipping the proxy. tc_index is preserved
   across veth boundaries.

   The mark is matched in a IP routing rule and causes packets from the
   proxies to use a different routing table. This allows to route all
   packets from proxies through cilium_host regardless of their
   destination. The BPF program attached to cilium_host can then perform
   the proxy reverse translation and route accordingly.

 * Only skipping the ingress proxy if the packet is coming from the

bpf/bpf_netdev.c
@@ -59,6 +59,7 @@ static inline __u32 derive_sec_ctx(struct __sk_buff *skb, const union v6addr *no
 #endif
 }
 
+#ifdef FROM_HOST
 static inline int __inline__
 reverse_proxy6(struct __sk_buff *skb, int l4_off, struct ipv6hdr *ip6, __u8 nh)
 {
@@ -110,21 +111,67 @@ reverse_proxy6(struct __sk_buff *skb, int l4_off, struct ipv6hdr *ip6, __u8 nh)
 			return DROP_CSUM_L4;
 	}
 
+	/* Packets which have been translated back from the proxy must
+	 * skip any potential ingress proxy at the endpoint
+	 */
+	skb->tc_index |= TC_INDEX_F_SKIP_PROXY;
+
 	return 0;
 }
+#endif
+
+#ifdef FROM_HOST
+static inline void __inline__ handle_identity_from_proxy(struct __sk_buff *skb, __u32 *identity)
+{
+	int ret;
+
+	/* For packets from the proxy the identity can be specified via
+	 * skb->mark */
+	if ((ret = mark_is_from_proxy(skb))) {
+		*identity = get_identity_via_proxy(skb);
+
+		/* Packets from the ingress proxy must skip the proxy when the
+		 * destination endpoint evaluates the policy. As the packet
+		 * would loop otherwise. */
+		if (ret == SOURCE_INGRESS_PROXY)
+			skb->tc_index |= TC_INDEX_F_SKIP_PROXY;
+	}
+
+	/* Reset packet mark to avoid hitting routing rules again */
+	skb->mark = 0;
+}
+#endif
+
+#ifdef FROM_HOST
+static inline int rewrite_dmac_to_host(struct __sk_buff *skb)
+{
+	/* When attached to cilium_host, we rewrite the DMAC to the mac of
+	 * cilium_host (peer) to ensure the packet is being considered to be
+	 * addressed to the host (PACKET_HOST) */
+	union macaddr cilium_net_mac = CILIUM_NET_MAC;
+
+	/* Rewrite to destination MAC of cilium_net (remote peer) */
+	if (eth_store_daddr(skb, (__u8 *) &cilium_net_mac.addr, 0) < 0)
+		return send_drop_notify_error(skb, DROP_WRITE_ERROR, TC_ACT_OK);
+
+	return TC_ACT_OK;
+}
+#endif
 
 static inline int handle_ipv6(struct __sk_buff *skb)
 {
 	union v6addr node_ip = { };
-	void *data = (void *) (long) skb->data;
-	void *data_end = (void *) (long) skb->data_end;
-	struct ipv6hdr *ip6 = data + ETH_HLEN;
-	union v6addr *dst = (union v6addr *) &ip6->daddr;
+	void *data, *data_end;
+	struct ipv6hdr *ip6;
+	union v6addr *dst;
 	int l4_off, l3_off = ETH_HLEN;
 	struct endpoint_info *ep;
 	__u8 nexthdr;
 	__u32 flowlabel;
-	int ret;
+
+	data = (void *) (long) skb->data;
+	data_end = (void *) (long) skb->data_end;
+	ip6 = data + ETH_HLEN;
 
 	if (data + l3_off + sizeof(*ip6) > data_end)
 		return DROP_INVALID;
@@ -141,50 +188,61 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 #endif
 
 	BPF_V6(node_ip, ROUTER_IP);
-
 	flowlabel = derive_sec_ctx(skb, &node_ip, ip6);
-#ifdef FROM_HOST
-	/* For packets from the host, the identity can be specified via skb->mark */
-	if (skb->mark) {
-		flowlabel = skb->mark;
-	}
-#endif
 
-	if (likely(ipv6_match_prefix_96(dst, &node_ip))) {
+#ifdef FROM_HOST
+	if (1) {
+		int ret;
+
+		handle_identity_from_proxy(skb, &flowlabel);
+
 		ret = reverse_proxy6(skb, l4_off, ip6, ip6->nexthdr);
+		/* DIRECT PACKET READ INVALID */
 		if (IS_ERR(ret))
 			return ret;
 
-		data = (void *) (long) skb->data;
-		data_end = (void *) (long) skb->data_end;
-		ip6 = data + ETH_HLEN;
-		if (data + sizeof(*ip6) + ETH_HLEN > data_end)
-			return DROP_INVALID;
-
-		/* Lookup IPv4 address in list of local endpoints */
-		if ((ep = lookup_ip6_endpoint(ip6)) != NULL) {
-			/* Let through packets to the node-ip so they are
-			 * processed by the local ip stack */
-			if (ep->flags & ENDPOINT_F_HOST)
-				return TC_ACT_OK;
-
-			return ipv6_local_delivery(skb, l3_off, l4_off, flowlabel, ip6, nexthdr, ep);
-		} else {
-#ifdef ENCAP_IFINDEX
-			struct endpoint_key key = {};
-
-			/* IPv6 lookup key: daddr/96 */
-			dst = (union v6addr *) &ip6->daddr;
-			key.ip6.p1 = dst->p1;
-			key.ip6.p2 = dst->p2;
-			key.ip6.p3 = dst->p3;
-			key.ip6.p4 = 0;
-			key.family = ENDPOINT_KEY_IPV6;
-
-			return encap_and_redirect(skb, &key, flowlabel);
-#endif
-		}
+		/* If we are attached to cilium_host at egress, this will
+		 * rewrite the destination mac address to the MAC of cilium_net */
+		ret = rewrite_dmac_to_host(skb);
+		/* DIRECT PACKET READ INVALID */
+		if (IS_ERR(ret))
+			return ret;
 	}
+
+	data = (void *) (long) skb->data;
+	data_end = (void *) (long) skb->data_end;
+	ip6 = data + ETH_HLEN;
+
+	if (data + sizeof(*ip6) + ETH_HLEN > data_end)
+		return DROP_INVALID;
+#endif
+
+	/* Lookup IPv4 address in list of local endpoints */
+	if ((ep = lookup_ip6_endpoint(ip6)) != NULL) {
+		/* Let through packets to the node-ip so they are
+		 * processed by the local ip stack */
+		if (ep->flags & ENDPOINT_F_HOST)
+			return TC_ACT_OK;
+
+		return ipv6_local_delivery(skb, l3_off, l4_off, flowlabel, ip6, nexthdr, ep);
+	}
+
+#ifdef ENCAP_IFINDEX
+	dst = (union v6addr *) &ip6->daddr;
+	if (likely(ipv6_match_prefix_96(dst, &node_ip))) {
+		struct endpoint_key key = {};
+
+		/* IPv6 lookup key: daddr/96 */
+		dst = (union v6addr *) &ip6->daddr;
+		key.ip6.p1 = dst->p1;
+		key.ip6.p2 = dst->p2;
+		key.ip6.p3 = dst->p3;
+		key.ip6.p4 = 0;
+		key.family = ENDPOINT_KEY_IPV6;
+
+		return encap_and_redirect(skb, &key, flowlabel);
+	}
+#endif
 
 	return TC_ACT_OK;
 }
@@ -204,6 +262,7 @@ static inline __u32 derive_ipv4_sec_ctx(struct __sk_buff *skb, struct iphdr *ip4
 #endif
 }
 
+#ifdef FROM_HOST
 static inline int __inline__
 reverse_proxy(struct __sk_buff *skb, int l4_off, struct iphdr *ip4,
 	      struct ipv4_ct_tuple *tuple)
@@ -258,71 +317,85 @@ reverse_proxy(struct __sk_buff *skb, int l4_off, struct iphdr *ip4,
 	    csum_l4_replace(skb, l4_off, &csum, old_saddr, new_saddr, 4 | BPF_F_PSEUDO_HDR) < 0)
 		return DROP_CSUM_L4;
 
+	/* Packets which have been translated back from the proxy must
+	 * skip any potential ingress proxy at the endpoint
+	 */
+	skb->tc_index |= TC_INDEX_F_SKIP_PROXY;
+
 	cilium_trace_capture(skb, DBG_CAPTURE_PROXY_POST, 0);
 
 	return 0;
 }
+#endif
 
 static inline int handle_ipv4(struct __sk_buff *skb)
 {
-	void *data = (void *) (long) skb->data;
-	void *data_end = (void *) (long) skb->data_end;
-	struct iphdr *ip4 = data + ETH_HLEN;
+	struct ipv4_ct_tuple tuple = {};
+	struct endpoint_info *ep;
+	void *data, *data_end;
+	struct iphdr *ip4;
+	int l4_off;
+	__u32 secctx;
+
+	data = (void *) (long) skb->data;
+	data_end = (void *) (long) skb->data_end;
+	ip4 = data + ETH_HLEN;
 
 	if (data + sizeof(*ip4) + ETH_HLEN > data_end)
 		return DROP_INVALID;
 
-#ifdef ENABLE_IPV4
-	/* Check if destination is within our cluster prefix */
-	if ((ip4->daddr & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE) {
-		struct ipv4_ct_tuple tuple = {};
-		struct endpoint_info *ep;
-		__u32 secctx;
-		int ret, l4_off;
+	l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
+	secctx = derive_ipv4_sec_ctx(skb, ip4);
+	tuple.nexthdr = ip4->protocol;
 
-		l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
-		secctx = derive_ipv4_sec_ctx(skb, ip4);
 #ifdef FROM_HOST
-		if (skb->mark) {
-			/* For packets from the host, the identity can be specified via skb->mark */
-			secctx = skb->mark;
-		}
-#endif
-		tuple.nexthdr = ip4->protocol;
+	if (1) {
+		int ret;
 
-		cilium_trace(skb, DBG_NETDEV_IN_CLUSTER, secctx, 0);
+		handle_identity_from_proxy(skb, &secctx);
 
 		ret = reverse_proxy(skb, l4_off, ip4, &tuple);
 		/* DIRECT PACKET READ INVALID */
 		if (IS_ERR(ret))
 			return ret;
 
-		data = (void *) (long) skb->data;
-		data_end = (void *) (long) skb->data_end;
-		ip4 = data + ETH_HLEN;
-		if (data + sizeof(*ip4) + ETH_HLEN > data_end)
-			return DROP_INVALID;
+		/* If we are attached to cilium_host at egress, this will
+		 * rewrite the destination mac address to the MAC of cilium_net */
+		ret = rewrite_dmac_to_host(skb);
+		/* DIRECT PACKET READ INVALID */
+		if (IS_ERR(ret))
+			return ret;
+	}
 
-		/* Lookup IPv4 address in list of local endpoints */
-		if ((ep = lookup_ip4_endpoint(ip4)) != NULL) {
-			/* Let through packets to the node-ip so they are
-			 * processed by the local ip stack */
-			if (ep->flags & ENDPOINT_F_HOST)
-				return TC_ACT_OK;
+	data = (void *) (long) skb->data;
+	data_end = (void *) (long) skb->data_end;
+	ip4 = data + ETH_HLEN;
 
-			return ipv4_local_delivery(skb, ETH_HLEN, l4_off, secctx, ip4, ep);
-		} else {
-#ifdef ENCAP_IFINDEX
-			/* IPv4 lookup key: daddr & IPV4_MASK */
-			struct endpoint_key key = {};
-
-			key.ip4 = ip4->daddr & IPV4_MASK;
-			key.family = ENDPOINT_KEY_IPV4;
-
-			cilium_trace(skb, DBG_NETDEV_ENCAP4, key.ip4, secctx);
-			return encap_and_redirect(skb, &key, secctx);
+	if (data + sizeof(*ip4) + ETH_HLEN > data_end)
+		return DROP_INVALID;
 #endif
-		}
+
+	/* Lookup IPv4 address in list of local endpoints and host IPs */
+	if ((ep = lookup_ip4_endpoint(ip4)) != NULL) {
+		/* Let through packets to the node-ip so they are
+		 * processed by the local ip stack */
+		if (ep->flags & ENDPOINT_F_HOST)
+			return TC_ACT_OK;
+
+		return ipv4_local_delivery(skb, ETH_HLEN, l4_off, secctx, ip4, ep);
+	}
+
+#ifdef ENCAP_IFINDEX
+	/* Check if destination is within our cluster prefix */
+	if ((ip4->daddr & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE) {
+		/* IPv4 lookup key: daddr & IPV4_MASK */
+		struct endpoint_key key = {};
+
+		key.ip4 = ip4->daddr & IPV4_MASK;
+		key.family = ENDPOINT_KEY_IPV4;
+
+		cilium_trace(skb, DBG_NETDEV_ENCAP4, key.ip4, secctx);
+		return encap_and_redirect(skb, &key, secctx);
 	}
 #endif
 

bpf/lib/common.h
@@ -23,6 +23,7 @@
 #include <linux/ipv6.h>
 #include <linux/in.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifndef HAVE_LRU_MAP_TYPE
 #define NEEDS_TIMEOUT 1
@@ -220,6 +221,66 @@ struct drop_notify {
 #define DROP_NO_SERVICE		-158
 #define DROP_POLICY_L4		-159
 #define DROP_NO_TUNNEL_ENDPOINT -160
+
+
+/* Magic skb->mark markers which identify packets originating from the proxy
+ *
+ * The uppper 16 bits contain the magic marker values which indicate whether
+ * the packet is coming from an ingress or egress proxy.
+ *
+ * The lower 16 bits may contain the security identity of the original source
+ * endpoint.
+ */
+#define MARK_MAGIC_PROXY_MASK		(0xFFFF << 16)
+#define MARK_MAGIC_PROXY_INGRESS	(0xFEFA << 16)
+#define MARK_MAGIC_PROXY_EGRESS		(0xFEFB << 16)
+#define MARK_IDENTITY_MASK		0xFFFF
+
+#define SOURCE_INGRESS_PROXY 1
+#define SOURCE_EGRESS_PROXY 2
+
+/**
+ * mark_is_from_proxy - returns > 0 if packet is coming from proxy
+ *
+ * Returns:
+ *  - SOURCE_INGRESS_PROXY if from ingress proxy
+ *  - SOURCE_EGRESS_PROXY if from egress proxy
+ */
+static inline int __inline__ mark_is_from_proxy(struct __sk_buff *skb)
+{
+	int magic = skb->mark & MARK_MAGIC_PROXY_MASK;
+
+	if (magic == MARK_MAGIC_PROXY_INGRESS)
+		return SOURCE_INGRESS_PROXY;
+	else if (magic == MARK_MAGIC_PROXY_EGRESS)
+		return SOURCE_EGRESS_PROXY;
+	else
+		return 0;
+}
+
+/**
+ * get_identity_via_proxy - returns source identity as specified by the proxy
+ */
+static inline int __inline__ get_identity_via_proxy(struct __sk_buff *skb)
+{
+	return skb->mark & MARK_IDENTITY_MASK;
+}
+
+/*
+ * skb->tc_index uses
+ *
+ * cilium_host @egress
+ *   bpf_host -> bpf_lxc
+ */
+#define TC_INDEX_F_SKIP_PROXY		1
+
+/**
+ * tc_index_is_from_proxy - returns true if packet originates from egress proxy
+ */
+static inline bool __inline__ tc_index_skip_proxy(struct __sk_buff *skb)
+{
+	return skb->tc_index & TC_INDEX_F_SKIP_PROXY;
+}
 
 /* skb->cb[] usage: */
 enum {

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=402289ef6d128d9603e4fac389603b03793e0245
+GO_BINDATA_SHA1SUM=63b6ab21b164dc1cf33e5cbb853cbe8d45afada2
 GO_VERSION_USED=go1.8.3

bpf/bpf_lxc.c
@@ -801,11 +801,10 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	void *data_end = (void *) (long) skb->data_end;
 	struct ipv6hdr *ip6 = data + ETH_HLEN;
 	struct csum_offset csum_off = {};
-	union v6addr host_ip = {};
 	int ret, l4_off, verdict;
 	struct ct_state ct_state = {};
 	struct ct_state ct_state_new = {};
-	bool orig_was_proxy;
+	bool skip_proxy;
 	union v6addr orig_dip = {};
 
 	if (data + sizeof(struct ipv6hdr) + ETH_HLEN > data_end)
@@ -818,8 +817,9 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	ipv6_addr_copy(&tuple.saddr, (union v6addr *) &ip6->saddr);
 	ipv6_addr_copy(&orig_dip, (union v6addr *) &ip6->daddr);
 
-	BPF_V6(host_ip, HOST_IP);
-	orig_was_proxy = ipv6_addrcmp((union v6addr *) &ip6->saddr, &host_ip) == 0;
+	/* If packet is coming from the egress proxy we have to skip
+	 * redirection to the egress proxy as we would loop forever. */
+	skip_proxy = tc_index_skip_proxy(skb);
 
 	l4_off = ETH_HLEN + ipv6_hdrlen(skb, ETH_HLEN, &tuple.nexthdr);
 	csum_l4_offset_and_flags(tuple.nexthdr, &csum_off);
@@ -869,7 +869,7 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 		ct_state_new.orig_dport = tuple.dport;
 		ct_state_new.src_sec_id = src_label;
 		ret = ct_create6(&CT_MAP6, &tuple, skb, CT_INGRESS, &ct_state_new,
-				 orig_was_proxy);
+				 skip_proxy);
 		if (IS_ERR(ret))
 			return ret;
 
@@ -879,6 +879,9 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 	if (ct_state.proxy_port && (ret == CT_NEW || ret == CT_ESTABLISHED)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 		union macaddr router_mac = NODE_MAC;
+		union v6addr host_ip = {};
+
+		BPF_V6(host_ip, HOST_IP);
 
 		ret = ipv6_redirect_to_host_port(skb, &csum_off, l4_off,
 						 ct_state.proxy_port, tuple.dport,
@@ -909,7 +912,7 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 	int ret, verdict, l4_off;
 	struct ct_state ct_state = {};
 	struct ct_state ct_state_new = {};
-	bool orig_was_proxy;
+	bool skip_proxy;
 	__be32 orig_dip;
 
 	if (data + sizeof(*ip4) + ETH_HLEN > data_end)
@@ -918,7 +921,10 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 	policy_clear_mark(skb);
 	tuple.nexthdr = ip4->protocol;
 
-	orig_was_proxy = ip4->saddr == IPV4_GATEWAY;
+	/* If packet is coming from the egress proxy we have to skip
+	 * redirection to the egress proxy as we would loop forever. */
+	skip_proxy = tc_index_skip_proxy(skb);
+
 	tuple.daddr = ip4->daddr;
 	tuple.saddr = ip4->saddr;
 	orig_dip = ip4->daddr;
@@ -959,7 +965,7 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 		ct_state_new.orig_dport = tuple.dport;
 		ct_state_new.src_sec_id = src_label;
 		ret = ct_create4(&CT_MAP4, &tuple, skb, CT_INGRESS, &ct_state_new,
-				 orig_was_proxy);
+				 skip_proxy);
 		if (IS_ERR(ret))
 			return ret;
 

bpf/init.sh
@@ -30,6 +30,8 @@ NATIVE_DEV=${11}
 HOST_ID="host"
 WORLD_ID="world"
 
+PROXY_RT_TABLE=2005
+
 set -e
 set -x
 
@@ -51,6 +53,100 @@ sysctl -w net.ipv6.conf.all.disable_ipv6=0
 
 # This directory was created by the daemon and contains the per container header file
 DIR="$PWD/globals"
+
+function delete_old_ip_rules_af()
+{
+	IP=$1
+	TBL=$2
+	for i in $($IP rule list | grep "lookup $TBL" | awk -F: '{print $1}'); do
+		$IP rule del pref $i;
+	done
+}
+
+function delete_old_ip_rules()
+{
+	delete_old_ip_rules_af "ip -4" $1
+	delete_old_ip_rules_af "ip -6" $1
+}
+
+function setup_veth()
+{
+	local -r NAME=$1
+
+	ip link set $NAME up
+	sysctl -w net.ipv4.conf.${NAME}.forwarding=1
+	sysctl -w net.ipv6.conf.${NAME}.forwarding=1
+	sysctl -w net.ipv4.conf.${NAME}.rp_filter=0
+	sysctl -w net.ipv4.conf.${NAME}.accept_local=1
+	sysctl -w net.ipv4.conf.${NAME}.send_redirects=0
+}
+
+function setup_veth_pair()
+{
+	local -r NAME1=$1
+	local -r NAME2=$2
+
+	ip link del $NAME1 2> /dev/null || true
+	ip link add $NAME1 type veth peer name $NAME2
+
+	setup_veth $NAME1
+	setup_veth $NAME2
+}
+
+function move_local_rules_af()
+{
+	IP=$1
+
+	# move the local table lookup rule from pref 0 to pref 100 so we can
+	# insert the cilium ip rules before the local table. It is strictly
+	# required to add the new local rule before deleting the old one as
+	# otherwise local addresses will not be reachable for a short period of
+	# time.
+	$IP rule list from all lookup local pref 100 | grep "lookup local" || {
+		$IP rule add from all lookup local pref 100
+	}
+	$IP rule del from all lookup local pref 0 2> /dev/null || true
+
+	# check if the move of the local table move was successful and restore
+	# it otherwise
+	if [ "$($IP rule list lookup local | wc -l)" -eq "0" ]; then
+		$IP rule add from all lookup local pref 0
+		$IP rule del from all lookup local pref 100
+		echo "Error: The kernel does not support moving the local table routing rule"
+		echo "Local routing rules:"
+		$IP rule list lookup local
+		exit 1
+	fi
+}
+
+function move_local_rules()
+{
+	move_local_rules_af "ip -4"
+	move_local_rules_af "ip -6"
+}
+
+function setup_proxy_rules()
+{
+	# delete old ip rules and flush table
+	delete_old_ip_rules $PROXY_RT_TABLE
+	ip -4 route flush table $PROXY_RT_TABLE
+	ip -6 route flush table $PROXY_RT_TABLE
+
+	# Any packet from a proxy uses a separate routing table
+	ip -4 rule add fwmark 0xFEF00000/0xFFF00000 pref 10 lookup $PROXY_RT_TABLE
+	ip -6 rule add fwmark 0xFEF00000/0xFFF00000 pref 10 lookup $PROXY_RT_TABLE
+
+	if [ -n "$IP4_HOST" ]; then
+		ip route add table $PROXY_RT_TABLE $IP4_HOST/32 dev $HOST_DEV1
+		ip route add table $PROXY_RT_TABLE default via $IP4_HOST
+	fi
+
+	IP6_LLADDR=$(ip -6 addr show dev $HOST_DEV2 | grep inet6 | head -1 | awk '{print $2}' | awk -F'/' '{print $1}')
+	if [ -n "$IP6_LLADDR" ]; then
+		ip -6 route add table $PROXY_RT_TABLE ${IP6_LLADDR}/128 dev $HOST_DEV1
+		ip -6 route add table $PROXY_RT_TABLE default via $IP6_LLADDR dev $HOST_DEV1
+	fi
+}
 
 function mac2array()
 {
@@ -102,13 +198,21 @@ HOST_DEV2="cilium_net"
 
 $LIB/run_probes.sh $LIB $RUNDIR
 
-ip link del $HOST_DEV1 2> /dev/null || true
-ip link add $HOST_DEV1 type veth peer name $HOST_DEV2
+setup_veth_pair $HOST_DEV1 $HOST_DEV2
 
-ip link set $HOST_DEV1 up
 ip link set $HOST_DEV1 arp off
-ip link set $HOST_DEV2 up
 ip link set $HOST_DEV2 arp off
+
+sed -i '/^#.*CILIUM_NET_MAC.*$/d' $RUNDIR/globals/node_config.h
+CILIUM_NET_MAC=$(ip link show $HOST_DEV2 | grep ether | awk '{print $2}')
+CILIUM_NET_MAC=$(mac2array $CILIUM_NET_MAC)
+
+# Remove the entire '#ifndef ... #endif block
+# Each line must contain the string '#.*CILIUM_NET_MAC.*'
+sed -i '/^#.*CILIUM_NET_MAC.*$/d' $RUNDIR/globals/node_config.h
+echo "#ifndef CILIUM_NET_MAC" >> $RUNDIR/globals/node_config.h
+echo "#define CILIUM_NET_MAC { .addr = ${CILIUM_NET_MAC}}" >> $RUNDIR/globals/node_config.h
+echo "#endif /* CILIUM_NET_MAC */" >> $RUNDIR/globals/node_config.h
 
 sed -i '/^#.*HOST_IFINDEX.*$/d' $RUNDIR/globals/node_config.h
 HOST_IDX=$(cat /sys/class/net/${HOST_DEV2}/ifindex)
@@ -121,7 +225,7 @@ echo "#define HOST_IFINDEX_MAC { .addr = ${HOST_MAC}}" >> $RUNDIR/globals/node_c
 
 # If the host does not have an IPv6 address assigned, assign our generated host
 # IP to make the host accessible to endpoints
-ip -6 addr show $IP6_HOST || {
+[ -n "$(ip -6 addr show to $IP6_HOST)" ] || {
 	ip -6 addr add $IP6_HOST dev $HOST_DEV1
 }
 
@@ -136,9 +240,9 @@ if [ "$IP6_SVC_RANGE" != "auto" ]; then
 fi
 
 if [[ "$IP4_HOST" != "<nil>" ]]; then
-  ip -4 addr show $IP4_HOST || {
-  ip -4 addr add $IP4_HOST dev $HOST_DEV1
-}
+	[ -n "$(ip -4 addr show to $IP4_HOST)" ] || {
+		ip -4 addr add $IP4_HOST dev $HOST_DEV1
+	}
 fi
 
 ip addr del 169.254.254.1/32 dev $HOST_DEV1 2> /dev/null || true
@@ -156,6 +260,13 @@ if [ "$IP4_SVC_RANGE" != "auto" ]; then
           ip route add $IP4_SVC_RANGE via 169.254.254.1 src $IP4_HOST
         fi
 fi
+
+# Decrease priority of the rule to identify local addresses
+move_local_rules
+
+# Install new rules before local rule to ensure that packets from the proxy are
+# using a separate routing table
+setup_proxy_rules
 
 sed '/ENCAP_GENEVE/d' $RUNDIR/globals/node_config.h
 sed '/ENCAP_VXLAN/d' $RUNDIR/globals/node_config.h

bpf/lib/conntrack.h
@@ -21,8 +21,6 @@
 #include <linux/icmpv6.h>
 #include <linux/icmp.h>
 
-#include <stdbool.h>
-
 #include "common.h"
 #include "utils.h"
 #include "ipv6.h"

pkg/proxy/dialer.go
@@ -1,0 +1,64 @@
+// Copyright 2016-2017 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package proxy
+
+import (
+	"context"
+	"net"
+	"os"
+	"syscall"
+)
+
+func ciliumDialer(ctx context.Context, network, address string) (net.Conn, error) {
+	addr, err := net.ResolveTCPAddr(network, address)
+	if err != nil {
+		return nil, err
+	}
+
+	family := syscall.AF_INET
+	if addr.IP.To4() == nil {
+		family = syscall.AF_INET6
+	}
+
+	fd, err := syscall.Socket(family, syscall.SOCK_STREAM, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	f := os.NewFile(uintptr(fd), addr.String())
+	defer f.Close()
+
+	c, err := net.FileConn(f)
+	if err != nil {
+		return nil, err
+	}
+
+	if id, ok := identityFromContext(ctx); ok {
+		setSocketMark(c, id)
+	}
+
+	sockAddr, err := ipToSockaddr(family, addr.IP, addr.Port, addr.Zone)
+	if err != nil {
+		c.Close()
+		return nil, err
+	}
+
+	if err := syscall.Connect(fd, sockAddr); err != nil {
+		c.Close()
+		return nil, err
+	}
+
+	return c, nil
+}

pkg/proxy/ipsock_posix.go
@@ -1,0 +1,52 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin dragonfly freebsd linux nacl netbsd openbsd solaris windows
+
+package proxy
+
+import (
+	"net"
+	"syscall"
+)
+
+func ipToSockaddr(family int, ip net.IP, port int, zone string) (syscall.Sockaddr, error) {
+	switch family {
+	case syscall.AF_INET:
+		if len(ip) == 0 {
+			ip = net.IPv4zero
+		}
+		ip4 := ip.To4()
+		if ip4 == nil {
+			return nil, &net.AddrError{Err: "non-IPv4 address", Addr: ip.String()}
+		}
+		sa := &syscall.SockaddrInet4{Port: port}
+		copy(sa.Addr[:], ip4)
+		return sa, nil
+	case syscall.AF_INET6:
+		// In general, an IP wildcard address, which is either
+		// "0.0.0.0" or "::", means the entire IP addressing
+		// space. For some historical reason, it is used to
+		// specify "any available address" on some operations
+		// of IP node.
+		//
+		// When the IP node supports IPv4-mapped IPv6 address,
+		// we allow an listener to listen to the wildcard
+		// address of both IP addressing spaces by specifying
+		// IPv6 wildcard address.
+		if len(ip) == 0 || ip.Equal(net.IPv4zero) {
+			ip = net.IPv6zero
+		}
+		// We accept any IPv6 address including IPv4-mapped
+		// IPv6 address.
+		ip6 := ip.To16()
+		if ip6 == nil {
+			return nil, &net.AddrError{Err: "non-IPv6 address", Addr: ip.String()}
+		}
+		sa := &syscall.SockaddrInet6{Port: port, ZoneId: 0}
+		copy(sa.Addr[:], ip6)
+		return sa, nil
+	}
+	return nil, &net.AddrError{Err: "invalid address family", Addr: ip.String()}
+}

commit f900f6a1777a39b865e82b3a88cd69714bf3ec27

cilium: bpf: open code mark_is_from_proxy to fix proxy error
On 4.14 kernel with llvm 4.0 or greater the current implementation of
mark_is_from_proxy() is returning zero _always_ regardless of actual
mark field.

The result is "ingress" policy tests are failing. This can be seen
with the 10-proxy.sh test, giving an "Internal Server Error" (error
code 500). This is because if we do not mark the skb correctly it
will be sent to the proxy repeatedly if the configuration/packet
would have checked the skip bit and avoided forwarding to the proxy.

At the moment its unclear to me why the original version is tripping
up, but kicking through the testing cycle now. This should not change
the logic any AFAICT but still it fixes the issue.

Some ideas, arithmatic overflow (not seeing it now though),
compiler bug, artifact of my bare metal ubuntu setup, ...

Also for what its worth I like the open-coded version better anyways.
We probably don't need an inline function for a single caller.

Fixes: 5fbd5b5d46e9 ("proxy: Distinguish between proxy and other local processes")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bpf/bpf_netdev.c
@@ -123,18 +123,16 @@ reverse_proxy6(struct __sk_buff *skb, int l4_off, struct ipv6hdr *ip6, __u8 nh)
 #ifdef FROM_HOST
 static inline void __inline__ handle_identity_from_proxy(struct __sk_buff *skb, __u32 *identity)
 {
-	int ret;
+	__u32 magic = skb->mark & MARK_MAGIC_PROXY_MASK;
 
-	/* For packets from the proxy the identity can be specified via
-	 * skb->mark */
-	if ((ret = mark_is_from_proxy(skb))) {
+	/* Packets from the ingress proxy must skip the proxy when the
+	 * destination endpoint evaluates the policy. As the packet
+	 * would loop otherwise. */
+	if (magic == MARK_MAGIC_PROXY_INGRESS) {
 		*identity = get_identity_via_proxy(skb);
-
-		/* Packets from the ingress proxy must skip the proxy when the
-		 * destination endpoint evaluates the policy. As the packet
-		 * would loop otherwise. */
-		if (ret == SOURCE_INGRESS_PROXY)
-			skb->tc_index |= TC_INDEX_F_SKIP_PROXY;
+		skb->tc_index |= TC_INDEX_F_SKIP_PROXY;
+	} else if (magic == MARK_MAGIC_PROXY_EGRESS) {
+		*identity = get_identity_via_proxy(skb);
 	}
 
 	/* Reset packet mark to avoid hitting routing rules again */

bpf/lib/common.h
@@ -242,24 +242,6 @@ enum {
 #define SOURCE_EGRESS_PROXY 2
 
 /**
- * mark_is_from_proxy - returns > 0 if packet is coming from proxy
- *
- * Returns:
- *  - SOURCE_INGRESS_PROXY if from ingress proxy
- *  - SOURCE_EGRESS_PROXY if from egress proxy
- */
-static inline int __inline__ mark_is_from_proxy(struct __sk_buff *skb)
-{
-	int magic = skb->mark & MARK_MAGIC_PROXY_MASK;
-
-	if (magic == MARK_MAGIC_PROXY_INGRESS)
-		return SOURCE_INGRESS_PROXY;
-	else if (magic == MARK_MAGIC_PROXY_EGRESS)
-		return SOURCE_EGRESS_PROXY;
-	return 0;
-}
-
-/**
  * get_identity_via_proxy - returns source identity as specified by the proxy
  */
 static inline int __inline__ get_identity_via_proxy(struct __sk_buff *skb)

daemon/bpf.sha
@@ -1,3 +1,3 @@
-GO_BINDATA_SHA1SUM=2a37ccd03224d98ffe24b79edb05bca13947f564
-GO_VERSION_USED=go1.9.2
+GO_BINDATA_SHA1SUM=f7bb27ac7c6e8f3acfa243ca78fc60e917290c5f
+GO_VERSION_USED=go1.8.1
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/geneve.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh 