g0odi3z2j/cilium

commit e9dccf70a9c26108b4d1fad519816dff97d8c0dd

bpf: Refactor POLICY_MAP to lib/maps.h
Three paths were defining and using a POLICY_MAP with the same helper
functions, consolidate the declaration in one place. This fixes the
following issue reported by André when switching between `--lb` and
`--device` mode.

    + tc filter add dev cilium_vxlan ingress prio 1 handle 1 bpf da obj bpf_overlay.o sec from-overlay
    Map specification differs from pinned file!
     - Size key:     4 (obj) != 8 (pin)
    Map 'cilium_policy_reserved_2' self-check failed!
    Error loading maps into kernel!
    Error fetching ELF ancillary data!
    Note: 8 bytes struct bpf_elf_map fixup performed due to size mismatch!

Signed-off-by: Joe Stringer <joe@covalent.io>

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=c630fcc786c2016af194c2e3f8afafa480d3231a
+GO_BINDATA_SHA1SUM=92afe45730b4d52f78174ec96a26df6f4532f55d
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/geneve.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

bpf/bpf_lxc.c
@@ -760,14 +760,6 @@ int handle_ingress(struct __sk_buff *skb)
 		return ret;
 }
 
-struct bpf_elf_map __section_maps POLICY_MAP = {
-	.type		= BPF_MAP_TYPE_HASH,
-	.size_key	= sizeof(struct policy_key),
-	.size_value	= sizeof(struct policy_entry),
-	.pinning	= PIN_GLOBAL_NS,
-	.max_elem	= 1024,
-};
-
 static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u32 src_label,
 					 int *forwarding_reason)
 {

bpf/bpf_netdev.c
@@ -468,14 +468,6 @@ int from_netdev(struct __sk_buff *skb)
 	return ret;
 }
 
-struct bpf_elf_map __section_maps POLICY_MAP = {
-	.type		= BPF_MAP_TYPE_HASH,
-	.size_key	= sizeof(__u32),
-	.size_value	= sizeof(struct policy_entry),
-	.pinning	= PIN_GLOBAL_NS,
-	.max_elem	= 1024,
-};
-
 __section_tail(CILIUM_MAP_RES_POLICY, SECLABEL) int handle_policy(struct __sk_buff *skb)
 {
 	__u32 src_label = skb->cb[CB_SRC_LABEL];

bpf/bpf_overlay.c
@@ -199,14 +199,6 @@ int from_overlay(struct __sk_buff *skb)
 		return ret;
 }
 
-struct bpf_elf_map __section_maps POLICY_MAP = {
-	.type		= BPF_MAP_TYPE_HASH,
-	.size_key	= sizeof(__u32),
-	.size_value	= sizeof(struct policy_entry),
-	.pinning	= PIN_GLOBAL_NS,
-	.max_elem	= 1024,
-};
-
 __section_tail(CILIUM_MAP_RES_POLICY, SECLABEL) int handle_policy(struct __sk_buff *skb)
 {
 	__u32 src_label = skb->cb[CB_SRC_LABEL];

bpf/lib/maps.h
@@ -52,6 +52,17 @@ struct bpf_elf_map __section_maps cilium_reserved_policy = {
 	.max_elem	= RESERVED_POLICY_SIZE,
 };
 
+/* Per-endpoint policy enforcement map */
+#ifdef POLICY_MAP
+struct bpf_elf_map __section_maps POLICY_MAP = {
+	.type		= BPF_MAP_TYPE_HASH,
+	.size_key	= sizeof(struct policy_key),
+	.size_value	= sizeof(struct policy_entry),
+	.pinning	= PIN_GLOBAL_NS,
+	.max_elem	= 1024,
+};
+#endif
+
 struct bpf_elf_map __section_maps cilium_proxy4 = {
 	.type		= BPF_MAP_TYPE_HASH,
 	.size_key	= sizeof(struct proxy4_tbl_key),

bpf/netdev_config.h
@@ -25,3 +25,4 @@
 #define HANDLE_NS
 #define FROM_HOST
 #define ENCAP_IFINDEX 1
+#define POLICY_MAP cilium_policy_foo

commit e87749e3bc956719e94bdaec90bc61c7af0e04d6

bpf: Remove policy maps of programs loaded in init.sh
This allows to change the format of the maps across restarts of the agent.
This fixes an upgrade bug introduced in commit e9dccf7.

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/init.sh
@@ -199,6 +199,11 @@ function xdp_load()
 	ip link set dev $DEV $MODE obj $OUT sec $SEC
 }
 
+function remove_non_persistent_map()
+{
+	rm "/sys/fs/bpf/tc/globals/$1" 2> /dev/null || true
+}
+
 function bpf_load()
 {
 	DEV=$1
@@ -217,7 +222,7 @@ function bpf_load()
 
 	tc qdisc del dev $DEV clsact 2> /dev/null || true
 	tc qdisc add dev $DEV clsact
-	rm "/sys/fs/bpf/tc/globals/$CALLS_MAP" 2> /dev/null || true
+	remove_non_persistent_map $CALLS_MAP
 	tc filter add dev $DEV $WHERE prio 1 handle 1 bpf da obj $OUT sec $SEC
 }
 
@@ -298,7 +303,9 @@ if [ "$MODE" = "vxlan" -o "$MODE" = "geneve" ]; then
 	echo "#define ENCAP_IFINDEX $ENCAP_IDX" >> $RUNDIR/globals/node_config.h
 
 	CALLS_MAP="cilium_calls_overlay_${ID_WORLD}"
-	OPTS="-DSECLABEL=${ID_WORLD} -DPOLICY_MAP=cilium_policy_reserved_${ID_WORLD}"
+	POLICY_MAP="cilium_policy_reserved_${ID_WORLD}"
+	OPTS="-DSECLABEL=${ID_WORLD} -DPOLICY_MAP=${POLICY_MAP}"
+	remove_non_persistent_map $POLICY_MAP
 	bpf_load $ENCAP_DEV "$OPTS" "ingress" bpf_overlay.c bpf_overlay.o from-overlay ${CALLS_MAP}
 	echo "$ENCAP_DEV" > $RUNDIR/encap.state
 else
@@ -318,7 +325,9 @@ if [ "$MODE" = "direct" ]; then
 		echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
 
 		CALLS_MAP=cilium_calls_netdev_${ID_WORLD}
-		OPTS="-DSECLABEL=${ID_WORLD} -DPOLICY_MAP=cilium_policy_reserved_${ID_WORLD}"
+		POLICY_MAP="cilium_policy_reserved_${ID_WORLD}"
+		OPTS="-DSECLABEL=${ID_WORLD} -DPOLICY_MAP=${POLICY_MAP}"
+		remove_non_persistent_map $POLICY_MAP
 		bpf_load $NATIVE_DEV "$OPTS" "ingress" bpf_netdev.c bpf_netdev.o from-netdev $CALLS_MAP
 
 		echo "$NATIVE_DEV" > $RUNDIR/device.state
@@ -347,7 +356,9 @@ fi
 
 # bpf_host.o requires to see an updated node_config.h which includes ENCAP_IFINDEX
 CALLS_MAP="cilium_calls_netdev_ns_${ID_HOST}"
-OPTS="-DFROM_HOST -DFIXED_SRC_SECCTX=${ID_HOST} -DSECLABEL=${ID_HOST} -DPOLICY_MAP=cilium_policy_reserved_${ID_HOST}"
+POLICY_MAP="cilium_policy_reserved_${ID_HOST}"
+OPTS="-DFROM_HOST -DFIXED_SRC_SECCTX=${ID_HOST} -DSECLABEL=${ID_HOST} -DPOLICY_MAP=${POLICY_MAP}"
+remove_non_persistent_map $POLICY_MAP
 bpf_load $HOST_DEV1 "$OPTS" "egress" bpf_netdev.c bpf_host.o from-netdev $CALLS_MAP
 
 if [ -n "$XDP_DEV" ]; then

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=394a6c00d1fb63ab89621c6f5a67e0ef40370acd
+GO_BINDATA_SHA1SUM=22f6870d19de9643645c6b2b4a1409ca817d081d
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/geneve.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 