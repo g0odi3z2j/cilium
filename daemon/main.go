g0odi3z2j/cilium

commit 5c2ae35cc55b93d7f0d064e364337897ed71c3cd

ct: enforce CT clean up from L3 to L7 policy changes on ingress
This commit fixes some CT clean up problems such as:
 - Clean up CT entries when removing a L3-only policy, if the policy no
   longer allows it;
 - Modify the CT entries when a L7 policy is loaded so the traffic will
   immediatlly go to the L7 proxy. By sending the traffic directly to
   the proxy instead of deleting it, like the old behavior, the proxy
   will send a RST to the client, forcing the client to restart the TCP
   connection and sending the traffic to the proxy;
 - Do the CT clean up only after the BPF program is regenerated to avoid
   loopholes in the CT. First security enforcement by BPF then cleanup.
 - Allow connections to continue to exist in the CT when running with
   policy enforcement mode "Default" and a policy that was selecting an
   endpoint be removed.

Signed-off-by: André Martins <andre@cilium.io>

daemon/main.go
@@ -510,7 +510,7 @@ func initEnv(cmd *cobra.Command) {
 	}
 	// Make sure Envoy version matches ours
 	if !strings.HasPrefix(envoyVersionArray[2], version.GetCiliumVersion().Revision) {
-		log.Fatal("Envoy version mismatch, aborting.")
+		log.Warn("Envoy version mismatch, aborting.")
 	}
 
 	if viper.GetBool("pprof") {

daemon/daemon.go
@@ -251,21 +251,21 @@ func (d *Daemon) DebugEnabled() bool {
 	return d.conf.Opts.IsEnabled(endpoint.OptionDebug)
 }
 
-// CleanCTEntries cleans the connection tracking of the given endpoint
-// where the given endpoint IPs' and the idsToRm match the CT entry fields.
-// isCTLocal should bet set as true if the endpoint's CT table is either
-// local or not (if is not local then is assumed to be global).
+// ResetProxyPort cleans the connection tracking of the given endpoint
+// where the given endpoint IPs and the idsToRm match the CT entry fields.
+// isCTLocal should be set as true if the endpoint's CT table is either
+// local or not (if it is not local then it is assumed to be global).
 // Implementation of pkg/endpoint.Owner interface
-func (d *Daemon) CleanCTEntries(e *endpoint.Endpoint, isCTLocal bool, ips []net.IP, idsToRm policy.RuleContexts) {
-	endpointmanager.RmCTEntriesOf(!d.conf.IPv4Disabled, e, isCTLocal, ips, idsToRm)
+func (d *Daemon) ResetProxyPort(e *endpoint.Endpoint, isCTLocal bool, ips []net.IP, idsToMod policy.SecurityIDContexts) {
+	endpointmanager.ResetProxyPort(!d.conf.IPv4Disabled, e, isCTLocal, ips, idsToMod)
 }
 
 // FlushCTEntries flushes the connection tracking of the given endpoint
-// where the given endpoint IPs' match the CT entry fields.
-// isCTLocal should bet set as true if the endpoint's CT table is either
-// local or not (if is not local then is assumed to be global).
+// where the given endpoint IPs match the CT entry fields.
+// isCTLocal should be set as true if the endpoint's CT table is either
+// local or not (if it is not local then it is assumed to be global).
 // Implementation of pkg/endpoint.Owner interface
-func (d *Daemon) FlushCTEntries(e *endpoint.Endpoint, isCTLocal bool, ips []net.IP, idsToKeep policy.RuleContexts) {
+func (d *Daemon) FlushCTEntries(e *endpoint.Endpoint, isCTLocal bool, ips []net.IP, idsToKeep policy.SecurityIDContexts) {
 	endpointmanager.FlushCTEntriesOf(!d.conf.IPv4Disabled, e, isCTLocal, ips, idsToKeep)
 }
 

daemon/daemon_test.go
@@ -52,8 +52,8 @@ type DaemonSuite struct {
 	OnRemoveFromEndpointQueue         func(epID uint64)
 	OnDebugEnabled                    func() bool
 	OnGetCompilationLock              func() *lock.RWMutex
-	OnCleanCTEntries                  func(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToRm policy.RuleContexts)
-	OnFlushCTEntries                  func(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToKeep policy.RuleContexts)
+	OnResetProxyPort                  func(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToMod policy.SecurityIDContexts)
+	OnFlushCTEntries                  func(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToKeep policy.SecurityIDContexts)
 	OnSendNotification                func(typ monitor.AgentNotification, text string) error
 	OnNewProxyLogRecord               func(l *accesslog.LogRecord) error
 }
@@ -179,15 +179,15 @@ func (ds *DaemonSuite) GetCompilationLock() *lock.RWMutex {
 	panic("GetCompilationLock should not have been called")
 }
 
-func (ds *DaemonSuite) CleanCTEntries(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToRm policy.RuleContexts) {
-	if ds.OnCleanCTEntries != nil {
-		ds.OnCleanCTEntries(e, isCTLocal, ips, idsToRm)
+func (ds *DaemonSuite) ResetProxyPort(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToMod policy.SecurityIDContexts) {
+	if ds.OnResetProxyPort != nil {
+		ds.OnResetProxyPort(e, isCTLocal, ips, idsToMod)
 		return
 	}
-	panic("CleanCTEntries should not have been called")
+	panic("ResetProxyPort should not have been called")
 }
 
-func (ds *DaemonSuite) FlushCTEntries(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToKeep policy.RuleContexts) {
+func (ds *DaemonSuite) FlushCTEntries(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToKeep policy.SecurityIDContexts) {
 	if ds.OnFlushCTEntries != nil {
 		ds.OnFlushCTEntries(e, isCTLocal, ips, idsToKeep)
 		return

pkg/endpoint/bpf.go
@@ -375,25 +375,32 @@ func (ep *epInfoCache) GetBPFValue() (*lxcmap.EndpointInfo, error) {
 	return ep.value, nil
 }
 
-// updateCT update the CT by flushing it completely for the given endpoint or by removing the entries that have
-// the list of consumers to remove.
-func (e *Endpoint) updateCT(owner Owner, flushEndpointCT bool, consumersAdd, consumersToRm policy.RuleContexts) *sync.WaitGroup {
+// updateCT updates the Connection Tracking based on the endpoint's policy
+// enforcement. If the policy enforcement is true, all CT entries will be
+// removed except the ones matched by idsToKeep. If the policy enforcement
+// is not being enforced then all CT entries that match idsToMod will be
+// modified, by resetting its proxy_port to 0 since there is no proxy running
+// with policy enforcement disabled.
+// It returns a sync.WaitGroup that will signalize when the CT entry table
+// is updated.
+func updateCT(owner Owner, e *Endpoint, epIPs []net.IP,
+	isPolicyEnforced, isLocal bool,
+	idsToKeep, idsToMod policy.SecurityIDContexts) *sync.WaitGroup {
+
 	wg := &sync.WaitGroup{}
-
-	isLocal := e.Opts.IsEnabled(OptionConntrackLocal)
-	ip4 := e.IPv4.IP()
-	ip6 := e.IPv6.IP()
-
-	if flushEndpointCT {
+	if isPolicyEnforced {
 		wg.Add(1)
 		go func(wg *sync.WaitGroup) {
-			owner.FlushCTEntries(e, isLocal, []net.IP{ip4, ip6}, consumersAdd)
+			// consumers added, so we need to flush all CT entries except the idsToKeep
+			owner.FlushCTEntries(e, isLocal, epIPs, idsToKeep)
 			wg.Done()
 		}(wg)
-	} else if len(consumersToRm) != 0 {
+	} else {
 		wg.Add(1)
 		go func(wg *sync.WaitGroup) {
-			owner.CleanCTEntries(e, isLocal, []net.IP{ip4, ip6}, consumersToRm)
+			// consumers removed, so we need to modify all CT entries with ids To Mod
+			// because there's on policy being enforced
+			owner.ResetProxyPort(e, isLocal, epIPs, idsToMod)
 			wg.Done()
 		}(wg)
 	}
@@ -469,7 +476,7 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 		// Regenerate policy and apply any options resulting in the
 		// policy change.
 		// Note that e.PolicyMap is not initialized!
-		if _, _, _, _, err = e.regeneratePolicy(owner, nil); err != nil {
+		if _, _, _, err = e.regeneratePolicy(owner, nil); err != nil {
 			e.Mutex.Unlock()
 			return 0, fmt.Errorf("Unable to regenerate policy: %s", err)
 		}
@@ -542,6 +549,10 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 		}
 	}
 
+	var (
+		modifiedRules, deletedRules policy.SecurityIDContexts
+		policyChanged               bool
+	)
 	// Only generate & populate policy map if a seclabel and consumer model is set up
 	if c != nil {
 		c.AddMap(e.PolicyMap)
@@ -549,19 +560,55 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 		// Regenerate policy and apply any options resulting in the
 		// policy change.
 		// This also populates e.PolicyMap
-		var (
-			consumersToRm, consumersAdd policy.RuleContexts
-			flushEndpointCT             bool
-		)
-		_, flushEndpointCT, consumersAdd, consumersToRm, err = e.regeneratePolicy(owner, nil)
+		policyChanged, modifiedRules, deletedRules, err = e.regeneratePolicy(owner, nil)
 		if err != nil {
 			e.Mutex.Unlock()
-			err = fmt.Errorf("Unable to regenerate policy for '%s': %s",
-				e.PolicyMap.String(), err)
-			return 0, err
+			return 0, fmt.Errorf("Unable to regenerate policy for '%s': %s", e.PolicyMap.String(), err)
 		}
-		wg := e.updateCT(owner, flushEndpointCT, consumersAdd, consumersToRm)
-		defer wg.Wait()
+		// policyChanged can still be true and, at the same time,
+		// the modifiedRules be nil. If this happens it means
+		// the L7 was changed so we need to update the
+		// L3L4Policy map with the new proxyport.
+		//
+		// modifiedRules contains if new L4 ports were added/modified and/or
+		// L3 rules were changed
+		if policyChanged &&
+			modifiedRules == nil &&
+			c.L4Policy != nil &&
+			c.L4Policy.Ingress != nil &&
+			c.L3L4Policy != nil {
+
+			// Only update CT if the RedirectPort was changed.
+			policyChanged = false
+
+			newSecIDCtxs := policy.NewSecurityIDContexts()
+			p := *c.L3L4Policy
+			for identity, l4RuleContexts := range p {
+
+				for l4RuleContext, l7RuleContexts := range l4RuleContexts {
+					pp := l4RuleContext.PortProto()
+					l4Filter, ok := c.L4Policy.Ingress[pp]
+
+					if ok {
+						_, l7Host := policy.ParseL4Filter(l4Filter)
+						l7RuleContexts.RedirectPort = l7Host.RedirectPort
+						if _, ok := newSecIDCtxs[identity]; !ok {
+							newSecIDCtxs[identity] = policy.NewL4RuleContexts()
+						}
+						newSecIDCtxs[identity][l4RuleContext] = l7RuleContexts
+						policyChanged = true
+					}
+				}
+			}
+
+			if policyChanged {
+				for ni, ruleContexts := range newSecIDCtxs {
+					p[ni] = ruleContexts
+				}
+			}
+
+			modifiedRules = c.L3L4Policy.DeepCopy()
+		}
 	}
 
 	if err = e.writeHeaderfile(epdir, owner); err != nil {
@@ -607,6 +654,14 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 			e.L3Maps.DestroyBpfMap(IPv4Egress, e.IPv4EgressMapPathLocked())
 		}
 	}
+
+	// Since the endpoint's lock will be unlocked, we need to
+	// store the current endpoint state so we can later on
+	// update the CT without requiring to lock the endpoint again.
+	isPolicyEnforced := e.IngressOrEgressIsEnforced()
+	isLocal := e.Opts.IsEnabled(OptionConntrackLocal)
+	epIPs := []net.IP{e.IPv4.IP(), e.IPv6.IP()}
+
 	e.Mutex.Unlock()
 
 	libdir := owner.GetBpfDir()
@@ -614,13 +669,20 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 	debug := strconv.FormatBool(owner.DebugEnabled())
 
 	err = e.runInit(libdir, rundir, epdir, epInfoCache.ifName, debug)
-	if err == nil {
-		// The last operation hooks the endpoint into the endpoint table and exposes it
-		err = lxcmap.WriteEndpoint(epInfoCache)
-		if err != nil {
-			log.WithField(logfields.EndpointID, e.ID).WithError(err).Error("Exposing new bpf failed!")
-		}
+	// CT entry clean up should always happen
+	// even if the bpf program build has failed
+	if policyChanged {
+		wg := updateCT(owner, e, epIPs, isPolicyEnforced, isLocal, modifiedRules, deletedRules)
+		defer wg.Wait()
+	}
+	if err != nil {
+		return epInfoCache.revision, err
 	}
 
+	// The last operation hooks the endpoint into the endpoint table and exposes it
+	err = lxcmap.WriteEndpoint(epInfoCache)
+	if err != nil {
+		log.WithField(logfields.EndpointID, e.ID).WithError(err).Error("Exposing new bpf failed!")
+	}
 	return epInfoCache.revision, err
 }

pkg/endpoint/owner.go
@@ -76,17 +76,18 @@ type Owner interface {
 	// of BPF programs.
 	GetCompilationLock() *lock.RWMutex
 
-	// CleanCTEntries cleans the connection tracking of the given endpoint
-	// where the given endpoint IPs' and the idsToRm match the CT entry fields.
+	// ResetProxyPort modifies the entries in the Connection Tracking table for
+	// the given endpoint where the given endpoint IPs and the idsToMod match
+	// the CT entry fields.
 	// isCTLocal should be set as true if the endpoint's CT table is either
-	// local or not (if is not local then is assumed to be global).
-	CleanCTEntries(e *Endpoint, isCTLocal bool, ips []net.IP, idsToRm policy.RuleContexts)
+	// local or not (if is not local then it is assumed to be global).
+	ResetProxyPort(e *Endpoint, isCTLocal bool, ips []net.IP, idsToMod policy.SecurityIDContexts)
 
-	// FlushCTEntries flushes the connection tracking of the given endpoint
-	// where the given endpoint IPs' and the idsToKeep don't match any of the CT entry fields.
+	// FlushCTEntries flushes all entries in the Connection Tracking table for
+	// where the given endpoint IPs except the entries that match the idsToKeep
 	// isCTLocal should be set as true if the endpoint's CT table is either
-	// local or not (if is not local then is assumed to be global).
-	FlushCTEntries(e *Endpoint, isCTLocal bool, ips []net.IP, idsToKeep policy.RuleContexts)
+	// local or not (if is not local then it is assumed to be global).
+	FlushCTEntries(e *Endpoint, isCTLocal bool, ips []net.IP, idsToKeep policy.SecurityIDContexts)
 
 	// UpdateSecLabels add and deletes the given labels on given endpoint ID.
 	// The received `add` and `del` labels will be filtered with the valid label

commit a796ad29d54391c9a3d27e498e1e0a38cac291ce

daemon: Fix Envoy version check and add hidden option to skip it
Cilium agent should abort on mismatching Envoy version. The log level
was inadvertently changed from Fatal to Warn in commit 5c2ae35cc5
("ct: enforce CT clean up from L3 to L7 policy changes on ingress").

Envoy has not updated it's Bazel configuration to correctly set the
linkstamp for the version. Workaround by manually deleteting the
version_linkstamp.o file before build.

Add a new hidden boolean command line option
"--disable-envoy-version-check" that bypasses the version check on
startup. This option is bound to the environment variable
"CILIUM_DISABLE_ENVOY_BUILD" so that if that is defined as "true" or
non-zero the version check will also be skipped. If this environment
variable is defined (at all) when invoking "make" all make actions
within the envoy directory are skipped.

Fixes: 5c2ae35cc5 ("ct: enforce CT clean up from L3 to L7 policy changes on ingress")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

daemon/main.go
@@ -346,6 +346,10 @@ func init() {
 	flags.BoolVar(&useEnvoy,
 		"envoy-proxy", true, "This flag is deprecated and will be removed in the next release")
 	flags.MarkHidden("envoy-proxy")
+	flags.Bool("disable-envoy-version-check", false, "Do not perform Envoy binary version check on startup")
+	flags.MarkHidden("disable-envoy-version-check")
+	// Disable version check if Envoy build is disabled
+	viper.BindEnv("disable-envoy-version-check", "CILIUM_DISABLE_ENVOY_BUILD")
 	flags.IntVar(&v4ClusterCidrMaskSize,
 		"ipv4-cluster-cidr-mask-size", 8, "Mask size for the cluster wide CIDR")
 	flags.StringVar(&v4Prefix,
@@ -515,16 +519,20 @@ func initEnv(cmd *cobra.Command) {
 	log.Info("|___|_|_|_|___|_|_|_|")
 	log.Infof("Cilium %s", version.Version)
 
-	envoyVersion := envoy.GetEnvoyVersion()
-	log.Infof("%s", envoyVersion)
+	if viper.GetBool("disable-envoy-version-check") {
+		log.Info("Envoy version check disabled")
+	} else {
+		envoyVersion := envoy.GetEnvoyVersion()
+		log.Infof("%s", envoyVersion)
 
-	envoyVersionArray := strings.Fields(envoyVersion)
-	if len(envoyVersionArray) < 3 {
-		log.Fatal("Truncated Envoy version string, cannot verify version match.")
-	}
-	// Make sure Envoy version matches ours
-	if !strings.HasPrefix(envoyVersionArray[2], version.GetCiliumVersion().Revision) {
-		log.Warn("Envoy version mismatch, aborting.")
+		envoyVersionArray := strings.Fields(envoyVersion)
+		if len(envoyVersionArray) < 3 {
+			log.Fatal("Truncated Envoy version string, cannot verify version match.")
+		}
+		// Make sure Envoy version matches ours
+		if !strings.HasPrefix(envoyVersionArray[2], version.GetCiliumVersion().Revision) {
+			log.Fatal("Envoy version mismatch, aborting.")
+		}
 	}
 
 	if viper.GetBool("pprof") {

envoy/Makefile
@@ -27,6 +27,11 @@ CLANG_FORMAT ?= clang-format
 BUILDIFIER ?= buildifier
 STRIP ?= strip
 
+ifdef CILIUM_DISABLE_ENVOY_BUILD
+all install clean:
+	echo "Envoy build is disabled by environment variable CILIUM_DISABLE_ENVOY_BUILD"
+else
+
 # Dockerfile builds require special options
 ifdef PKG_BUILD
 BAZEL_OPTS ?= --batch
@@ -48,13 +53,16 @@ api: force-non-root Makefile.api
 
 envoy: force-non-root
 	@$(ECHO_BAZEL)
+	-rm -f bazel-out/k8-fastbuild/bin/_objs/envoy/external/envoy/source/common/common/version_linkstamp.o
 	$(BAZEL) $(BAZEL_OPTS) build $(BAZEL_BUILD_OPTS) //:envoy
 
 # Allow root build for release
 $(ENVOY_BIN) envoy-release: force
+	-rm -f bazel-out/k8-opt/bin/_objs/envoy/external/envoy/source/common/common/version_linkstamp.o
 	$(BAZEL) $(BAZEL_OPTS) build $(BAZEL_BUILD_OPTS) -c opt //:envoy
 
 envoy-debug: force-non-root
+	-rm -f bazel-out/k8-dbg/bin/_objs/envoy/external/envoy/source/common/common/version_linkstamp.o
 	$(BAZEL) $(BAZEL_OPTS) build $(BAZEL_BUILD_OPTS) -c dbg //:envoy
 
 $(CHECK_FORMAT): force-non-root
@@ -122,4 +130,6 @@ endif
 force-non-root:
 ifeq ($(USER),root)
 	$(error This target cannot be run as root!)
+endif
+
 endif