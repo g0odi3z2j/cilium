g0odi3z2j/cilium

commit 6281c2ffe741a494c206a8d9aa938650ac7053b8

endpoint: Skip BPF compilation if headerfile is unchanged
Fixes: https://github.com/cilium/cilium/issues/3671
Signed-off-by: Romain Lenglet <romain@covalent.io>

pkg/endpoint/bpf.go
@@ -18,7 +18,10 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path"
@@ -326,6 +329,33 @@ func (e *Endpoint) writeHeaderfile(prefix string, owner Owner) error {
 	}
 
 	return fw.Flush()
+}
+
+// hashHeaderfile returns the MD5 hash of the BPF headerfile with the given prefix.
+// This ignores all lines that don't start with "#", incl. all comments, since
+// they have no effect on the BPF compilation.
+func (e *Endpoint) hashHeaderfile(prefix string) (string, error) {
+	headerPath := filepath.Join(prefix, common.CHeaderFileName)
+	file, err := os.Open(headerPath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	hashWriter := md5.New()
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			io.WriteString(hashWriter, line)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	hash := hashWriter.Sum(nil)
+	return hex.EncodeToString(hash[:]), nil
 }
 
 // FIXME: Clean this function up
@@ -731,7 +761,20 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 	// BPF programs for this endpoint.
 	if err = e.writeHeaderfile(epdir, owner); err != nil {
 		e.Mutex.Unlock()
-		return 0, fmt.Errorf("Unable to write header file: %s", err)
+		return 0, fmt.Errorf("unable to write header file: %s", err)
+	}
+
+	// Avoid BPF program compilation and installation if the headerfile hasn't changed.
+	bpfHeaderfileHash, err := e.hashHeaderfile(epdir)
+	var bpfHeaderfileChanged bool
+	if err != nil {
+		e.getLogger().WithError(err).Warn("Unable to hash header file")
+		bpfHeaderfileHash = ""
+		bpfHeaderfileChanged = true
+	} else {
+		bpfHeaderfileChanged = (bpfHeaderfileHash != e.bpfHeaderfileHash)
+		e.getLogger().WithField(logfields.BPFHeaderfileHash, bpfHeaderfileHash).
+			Debugf("BPF header file hashed (was: %q)", e.bpfHeaderfileHash)
 	}
 
 	// Cache endpoint information
@@ -791,10 +834,18 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 		return 0, fmt.Errorf("Error while configuring proxy redirects: %s", err)
 	}
 
-	// Compile and install BPF programs for this endpoint
-	err = e.runInit(libdir, rundir, epdir, epInfoCache.ifName, debug)
-	if err != nil {
-		return epInfoCache.revision, err
+	if bpfHeaderfileChanged {
+		// Compile and install BPF programs for this endpoint
+		err = e.runInit(libdir, rundir, epdir, epInfoCache.ifName, debug)
+		if err != nil {
+			return epInfoCache.revision, err
+		}
+		e.bpfHeaderfileHash = bpfHeaderfileHash
+	} else {
+		e.Mutex.RLock()
+		e.getLogger().WithField(logfields.BPFHeaderfileHash, bpfHeaderfileHash).
+			Debug("BPF header file unchanged, skipping BPF compilation and installation")
+		e.Mutex.RUnlock()
 	}
 
 	// To avoid traffic loss, wait for the policy to be pushed into BPF before

pkg/endpoint/endpoint.go
@@ -375,7 +375,7 @@ type Endpoint struct {
 	ID uint16
 
 	// Mutex protects write operations to this endpoint structure
-	Mutex lock.RWMutex
+	Mutex lock.RWMutex `json:"-"`
 
 	// ContainerName is the name given to the endpoint by the container runtime
 	ContainerName string
@@ -473,6 +473,10 @@ type Endpoint struct {
 	// bypass policy while it is still being resolved.
 	PolicyCalculated bool `json:"-"`
 
+	// bpfHeaderfileHash is the hash of the last BPF headerfile that has been
+	// compiled and installed.
+	bpfHeaderfileHash string
+
 	k8sPodName   string
 	k8sNamespace string
 
@@ -512,7 +516,7 @@ type Endpoint struct {
 	//
 	// FIXME: Mark private once endpoint deletion can be moved into
 	// `pkg/endpoint`
-	BuildMutex lock.Mutex
+	BuildMutex lock.Mutex `json:"-"`
 
 	// logger is a logrus object with fields set to report an endpoints information.
 	// You must hold Endpoint.Mutex to read or write it (but not to log with it).

pkg/logging/logfields/logfields.go
@@ -226,4 +226,7 @@ const (
 
 	// BPFMapName is the name of a BPF map.
 	BPFMapName = "bpfMapName"
+
+	// BPFHeaderHash is the hash of the BPF header.
+	BPFHeaderfileHash = "bpfHeaderfileHash"
 )

commit 9f3a28e0110adc99736c5558f53a0a84ef6e2280

endpoint: Support hashing C headers with very long lines
Avoid hashing failures due to very long lines, which resulted in
those warning log messages:
... level=warning msg="Unable to hash header file"
... error="bufio.Scanner: token too long"

Fixes: 6281c2ffe741a494c206a8d9aa938650ac7053b8
Signed-off-by: Romain Lenglet <romain@covalent.io>

pkg/endpoint/bpf.go
@@ -334,7 +334,7 @@ func (e *Endpoint) writeHeaderfile(prefix string, owner Owner) error {
 // hashHeaderfile returns the MD5 hash of the BPF headerfile with the given prefix.
 // This ignores all lines that don't start with "#", incl. all comments, since
 // they have no effect on the BPF compilation.
-func (e *Endpoint) hashHeaderfile(prefix string) (string, error) {
+func hashHeaderfile(prefix string) (string, error) {
 	headerPath := filepath.Join(prefix, common.CHeaderFileName)
 	file, err := os.Open(headerPath)
 	if err != nil {
@@ -342,16 +342,29 @@ func (e *Endpoint) hashHeaderfile(prefix string) (string, error) {
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
 	hashWriter := md5.New()
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.HasPrefix(line, "#") {
-			io.WriteString(hashWriter, line)
+	reader := bufio.NewReader(file)
+	firstFragmentOfLine := true
+	lineToHash := false
+	for {
+		fragment, isPrefix, err := reader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "", err
 		}
-	}
-	if err := scanner.Err(); err != nil {
-		return "", err
+		if firstFragmentOfLine && len(fragment) > 0 && fragment[0] == '#' {
+			lineToHash = true
+		}
+		if lineToHash {
+			hashWriter.Write(fragment)
+		}
+		firstFragmentOfLine = !isPrefix
+		if firstFragmentOfLine {
+			// The next fragment is the beginning of a new line.
+			lineToHash = false
+		}
 	}
 
 	hash := hashWriter.Sum(nil)
@@ -765,7 +778,7 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, err
 	}
 
 	// Avoid BPF program compilation and installation if the headerfile hasn't changed.
-	bpfHeaderfileHash, err := e.hashHeaderfile(epdir)
+	bpfHeaderfileHash, err := hashHeaderfile(epdir)
 	var bpfHeaderfileChanged bool
 	if err != nil {
 		e.getLogger().WithError(err).Warn("Unable to hash header file")

pkg/endpoint/bpf_test.go
@@ -1,0 +1,81 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package endpoint
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/cilium/cilium/common"
+
+	. "gopkg.in/check.v1"
+)
+
+func (s *EndpointSuite) TestHashHeaderfile(c *C) {
+	// Create a line much longer than 4096, the buffer size used by ReadLine.
+	manyBytes := make([]byte, 100000)
+	for i := range manyBytes {
+		manyBytes[i] = 'a'
+	}
+	longString := string(manyBytes)
+
+	dir := c.MkDir()
+	headerPath := filepath.Join(dir, common.CHeaderFileName)
+	var text string
+	var err error
+
+	text = "ignored long line: " + longString + `
+
+#line1
+ignored
+  #ignored
+
+#line2
+
+`
+	ioutil.WriteFile(headerPath, []byte(text), 0644)
+	hash1, err := hashHeaderfile(dir)
+	c.Assert(hash1, Not(Equals), "")
+	c.Assert(err, IsNil)
+
+	// Same as above but with all the ignored lines removed.
+	text = `#line1
+#line2`
+	ioutil.WriteFile(headerPath, []byte(text), 0644)
+	hash2, err := hashHeaderfile(dir)
+	c.Assert(hash2, Not(Equals), "")
+	c.Assert(err, IsNil)
+
+	c.Assert(hash1, Equals, hash2)
+
+	// Only non-ignored lines, including one line that is longer than the ReadLine buffer.
+	text = "#line1\n" +
+		"#line2 " + longString + "\n#line3"
+	ioutil.WriteFile(headerPath, []byte(text), 0644)
+	hash3, err := hashHeaderfile(dir)
+	c.Assert(hash3, Not(Equals), "")
+	c.Assert(err, IsNil)
+
+	// Same as above but with an extra character at the end of the long line.
+	// That character shouldn't be ignored and should result into a different hash.
+	text = "#line1\n" +
+		"#line2 " + longString + "z\n#line3"
+	ioutil.WriteFile(headerPath, []byte(text), 0644)
+	hash4, err := hashHeaderfile(dir)
+	c.Assert(hash4, Not(Equals), "")
+	c.Assert(err, IsNil)
+
+	c.Assert(hash3, Not(Equals), hash4)
+}