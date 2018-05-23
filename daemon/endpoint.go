g0odi3z2j/cilium

commit 37bd5caeb7fd25489437e40d8fcb93212c436529

pkg/maps: add BPF ipcache map
This new map maps IPs to their corresponding numerical security identities.
Whenever the IPCache is updated, a callback is made to update this new map
as well. There is only one such map per cilium-agent. This map is used
by the datapath to get the identity which corresponds to an IP of a packet
upon egress to make a decision whether to allow or deny a packet upon
egress.

Also starts the watcher for ip-identity in the key-value store after the map
is cleaned up in daemon.init(), otherwise the contents of the map will be
cleaned after the watcher starts, which will result in inconsistent
state in the agent.

Signed-off by: Ian Vernon <ian@cilium.io>

daemon/endpoint.go
@@ -25,12 +25,17 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
 	"github.com/cilium/cilium/pkg/apierror"
+	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/completion"
+	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
+	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipam"
+	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	ipCacheBPF "github.com/cilium/cilium/pkg/maps/ipcache"
 	"github.com/cilium/cilium/pkg/maps/lxcmap"
 
 	"github.com/go-openapi/runtime/middleware"
@@ -681,7 +686,89 @@ func (h *putEndpointIDLabels) Handle(params PutEndpointIDLabelsParams) middlewar
 }
 
 // OnIPIdentityCacheChange is called whenever there is a change of state in the
-// IPCache (pkg/ipcache). Currently does nothing as of now.
-func (d *Daemon) OnIPIdentityCacheChange() {
+// IPCache (pkg/ipcache).
+// TODO (FIXME): GH-3161.
+func (d *Daemon) OnIPIdentityCacheChange(modType ipcache.CacheModification, ipIDPair identity.IPIdentityPair) {
+	// TODO - see if we can factor this into an interface under something like
+	// pkg/datapath instead of in the daemon directly so that the code is more
+	// logically located.
 
+	// Update BPF Maps.
+	key := ipCacheBPF.NewEndpointKey(ipIDPair.IP)
+	value := ipCacheBPF.RemoteEndpointInfo{SecurityIdentity: uint16(ipIDPair.ID)}
+
+	switch modType {
+	case ipcache.Upsert:
+		err := ipCacheBPF.IPCache.Update(key, value)
+		if err != nil {
+			log.WithError(err).WithFields(logrus.Fields{"key": key.String(),
+				"value": value.String()}).
+				Warning("unable to update bpf map")
+		}
+	case ipcache.Delete:
+		err := ipCacheBPF.IPCache.Delete(key)
+		if err != nil {
+			log.WithError(err).WithFields(logrus.Fields{"key": key.String()}).
+				Warning("unable to delete from bpf map")
+		}
+	default:
+		log.WithField("modificationType", modType).Warning("cache modification type not supported")
+	}
+}
+
+// OnIPIdentityCacheGC spawns a controller which synchronizes the BPF IPCache Map
+// with the in-memory IP-Identity cache.
+func (d *Daemon) OnIPIdentityCacheGC() {
+
+	// This controller ensures that the in-memory IP-identity cache is in-sync
+	// with the BPF map on disk. These can get out of sync if the cilium-agent
+	// is offline for some time, as the maps persist on the BPF filesystem.
+	// In the case that there is some loss of event history in the key-value
+	// store (e.g., compaction in etcd), we cannot rely upon the key-value store
+	// fully to give us the history of all events. As such, periodically check
+	// for inconsistencies in the data-path with that in the agent to ensure
+	// consistent state.
+	controller.NewManager().UpdateController("ipcache-bpf-garbage-collection",
+		controller.ControllerParams{
+			DoFunc: func() error {
+
+				// Since controllers run asynchronously, need to make sure
+				// IPIdentityCache is not being updated concurrently while we do
+				// GC;
+				ipcache.IPIdentityCache.RLock()
+				defer ipcache.IPIdentityCache.RUnlock()
+
+				keysToRemove := map[ipCacheBPF.EndpointKey]struct{}{}
+
+				// Add all keys which are in BPF map but not in in-memory cache
+				// to set of keys to remove from BPF map.
+				cb := func(key bpf.MapKey, value bpf.MapValue) {
+					k := key.(ipCacheBPF.EndpointKey)
+					keyToIP := k.String()
+
+					// Don't RLock as part of the same goroutine.
+					if _, exists := ipcache.IPIdentityCache.LookupByIPRLocked(keyToIP); !exists {
+						// Cannot delete from map during callback because DumpWithCallback
+						// RLocks the map.
+						keysToRemove[k] = struct{}{}
+					}
+				}
+
+				if err := ipCacheBPF.IPCache.DumpWithCallback(cb); err != nil {
+					return fmt.Errorf("error dumping ipcache BPF map: %s", err)
+				}
+
+				// Remove all keys which are not in in-memory cache from BPF map
+				// for consistency.
+				for k := range keysToRemove {
+					log.WithFields(logrus.Fields{logfields.BPFMapKey: k}).
+						Debug("deleting from ipcache BPF map")
+					if err := ipCacheBPF.IPCache.Delete(k); err != nil {
+						return fmt.Errorf("error deleting key %s from ipcache BPF map: %s", k, err)
+					}
+				}
+				return nil
+			},
+			RunInterval: time.Duration(5) * time.Minute,
+		})
 }

pkg/maps/ipcache/ipcache.go
@@ -1,0 +1,89 @@
+// Copyright 2016-2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ipcache
+
+import (
+	"fmt"
+	"net"
+	"unsafe"
+
+	"github.com/cilium/cilium/pkg/bpf"
+)
+
+const (
+	// MaxKeys is the maximum number of keys that can be present in the
+	// RemoteEndpointMap.
+	MaxKeys = 0x7D000
+)
+
+// EndpointKey implements the bpf.MapKey interface.
+type EndpointKey struct {
+	bpf.EndpointKey
+}
+
+// GetKeyPtr returns the unsafe pointer to the BPF key
+func (k EndpointKey) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(&k) }
+
+// NewValue returns a new empty instance of the structure representing the BPF
+// map value
+func (k EndpointKey) NewValue() bpf.MapValue { return &RemoteEndpointInfo{} }
+
+// NewEndpointKey returns an EndpointKey based on the provided IP address. The
+// address family is automatically detected
+func NewEndpointKey(ip net.IP) EndpointKey {
+	return EndpointKey{
+		EndpointKey: bpf.NewEndpointKey(ip),
+	}
+}
+
+// RemoteEndpointInfo implements the bpf.MapValue interface. It contains the
+// security identity of a remote endpoint.
+type RemoteEndpointInfo struct {
+	SecurityIdentity uint16
+	Pad              [3]uint16
+}
+
+func (v RemoteEndpointInfo) String() string {
+	return fmt.Sprintf("%d", v.SecurityIdentity)
+}
+
+// GetValuePtr returns the unsafe pointer to the BPF value.
+func (v RemoteEndpointInfo) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(&v) }
+
+var (
+	// IPCache is a mapping of all endpoint IPs in the cluster which this
+	// Cilium agent is a part of to their corresponding security identities.
+	// It is a singleton; there is only one such map per agent.
+	IPCache = bpf.NewMap(
+		"cilium_ipcache",
+		bpf.BPF_MAP_TYPE_HASH,
+		int(unsafe.Sizeof(EndpointKey{})),
+		int(unsafe.Sizeof(RemoteEndpointInfo{})),
+		MaxKeys,
+		0,
+		func(key []byte, value []byte) (bpf.MapKey, bpf.MapValue, error) {
+			k, v := EndpointKey{}, RemoteEndpointInfo{}
+
+			if err := bpf.ConvertKeyValue(key, value, &k, &v); err != nil {
+				return nil, nil, err
+			}
+			return k, v, nil
+		},
+	).WithNonPersistent()
+)
+
+func init() {
+	bpf.OpenAfterMount(IPCache)
+}

pkg/ipcache/ipcache.go
@@ -70,6 +70,26 @@ func NewIPCache() *IPCache {
 	}
 }
 
+// Lock locks the IPCache's mutex.
+func (ipc *IPCache) Lock() {
+	ipc.mutex.Lock()
+}
+
+// Unlock unlocks the IPCache's mutex.
+func (ipc *IPCache) Unlock() {
+	ipc.mutex.Unlock()
+}
+
+// RLock RLocks the IPCache's mutex.
+func (ipc *IPCache) RLock() {
+	ipc.mutex.RLock()
+}
+
+// RUnlock RUnlocks the IPCache's mutex.
+func (ipc *IPCache) RUnlock() {
+	ipc.mutex.RUnlock()
+}
+
 // upsert adds / updates the provided IP and identity into both caches contained
 // within ipc.
 func (ipc *IPCache) upsert(endpointIP string, identity identity.NumericIdentity) {
@@ -121,6 +141,15 @@ func (ipc *IPCache) LookupByIP(endpointIP string) (identity.NumericIdentity, boo
 	return identity, exists
 }
 
+// LookupByIPRLocked returns the corresponding security identity that endpoint IP maps
+// to within the provided IPCache, as well as if the corresponding entry exists
+// in the IPCache.
+func (ipc *IPCache) LookupByIPRLocked(endpointIP string) (identity.NumericIdentity, bool) {
+
+	identity, exists := ipc.ipToIdentityCache[endpointIP]
+	return identity, exists
+}
+
 // LookupByIdentity returns the set of endpoint IPs that have security identity
 // ID, as well as if the corresponding entry exists in the IPCache.
 func (ipc *IPCache) LookupByIdentity(id identity.NumericIdentity) (map[string]struct{}, bool) {
@@ -135,7 +164,11 @@ func (ipc *IPCache) LookupByIdentity(id identity.NumericIdentity) (map[string]st
 type IPIdentityMappingOwner interface {
 	// OnIPIdentityCacheChange will be called whenever there the state of the
 	// IPCache has changed.
-	OnIPIdentityCacheChange()
+	OnIPIdentityCacheChange(modType CacheModification, ipIDPair identity.IPIdentityPair)
+
+	// OnIPIdentityCacheGC will be called to sync other components which are
+	// reliant upon the IPIdentityCache with the IPIdentityCache.
+	OnIPIdentityCacheGC()
 }
 
 // GetIPIdentityMapModel returns all known endpoint IP to security identity mappings
@@ -145,22 +178,36 @@ func GetIPIdentityMapModel() {
 	// see GH-2555
 }
 
+// CacheModification represents the type of operation performed upon IPCache.
+type CacheModification string
+
+const (
+	// Upsert represents Upsertion into IPCache.
+	Upsert CacheModification = "Upsert"
+
+	// Delete represents deletion of an entry in IPCache.
+	Delete CacheModification = "Delete"
+)
+
 func ipIdentityWatcher(owner IPIdentityMappingOwner) {
 
 	for {
+
 		watcher := kvstore.ListAndWatch("endpointIPWatcher", IPIdentitiesPath, 512)
 
 		// Get events from channel as they come in.
 		for event := range watcher.Events {
 
 			var (
-				cacheChanged bool
-				ipIDPair     identity.IPIdentityPair
+				cacheChanged      bool
+				cacheModification CacheModification
+				ipIDPair          identity.IPIdentityPair
 			)
 
 			// Key and value are empty for ListDone event types, so just continue.
 			// See GH-3159.
 			if event.Typ == kvstore.EventTypeListDone {
+				owner.OnIPIdentityCacheGC()
 				continue
 			}
 
@@ -182,6 +229,7 @@ func ipIdentityWatcher(owner IPIdentityMappingOwner) {
 				if !exists || cachedIdentity != ipIDPair.ID {
 					IPIdentityCache.upsert(ipStr, ipIDPair.ID)
 					cacheChanged = true
+					cacheModification = Upsert
 
 					endpointIPs, _ := IPIdentityCache.LookupByIdentity(ipIDPair.ID)
 
@@ -197,6 +245,7 @@ func ipIdentityWatcher(owner IPIdentityMappingOwner) {
 				if exists {
 					IPIdentityCache.delete(ipStr)
 					cacheChanged = true
+					cacheModification = Delete
 
 					endpointIPs, exists := IPIdentityCache.LookupByIdentity(ipIDPair.ID)
 					if !exists {
@@ -220,8 +269,10 @@ func ipIdentityWatcher(owner IPIdentityMappingOwner) {
 					"endpoint-ip":      ipIDPair.IP,
 					"cached-identity":  cachedIdentity,
 					logfields.Identity: ipIDPair.ID,
-				}).Debugf("endpoint IP cache changed state")
-				owner.OnIPIdentityCacheChange()
+				}).Debugf("endpoint IP cache %s", cacheModification)
+
+				// Callback upon cache updates.
+				owner.OnIPIdentityCacheChange(cacheModification, ipIDPair)
 			}
 		}
 

cilium/cmd/bpf_ipcache_list.go
@@ -1,0 +1,60 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/pkg/command"
+
+	"fmt"
+	"github.com/cilium/cilium/pkg/maps/ipcache"
+	"github.com/spf13/cobra"
+)
+
+const (
+	ipAddrTitle   = "IP ADDRESS"
+	identityTitle = "IDENTITY"
+)
+
+var bpfIPCacheListCmd = &cobra.Command{
+	Use:   "ipcache list",
+	Short: "List remote endpoint IPs and their corresponding security identities",
+	Run: func(cmd *cobra.Command, args []string) {
+		common.RequireRootPrivilege("cilium bpf ipcache list")
+
+		bpfIPCacheList := make(map[string][]string)
+		if err := ipcache.IPCache.Dump(bpfIPCacheList); err != nil {
+			fmt.Fprintf(os.Stderr, "error dumping contents of map: %s\n", err)
+			os.Exit(1)
+		}
+
+		if command.OutputJSON() {
+			if err := command.PrintOutput(bpfIPCacheList); err != nil {
+				fmt.Fprintf(os.Stderr, "error getting output of map in JSON: %s\n", err)
+				os.Exit(1)
+			}
+			return
+		}
+
+		TablePrinter(ipAddrTitle, identityTitle, bpfIPCacheList)
+	},
+}
+
+func init() {
+	bpfCmd.AddCommand(bpfIPCacheListCmd)
+	command.AddJSONOutput(bpfIPCacheListCmd)
+}

commit 790206aea8d31058ab4acec9ea769a962d46c3d3

pkg/bpf: Use pointer receivers for MapKeys types
bpf.MapValue interface function GetValuePtr() returns a pointer to a
new temporary if the function receiver is a value rather than a
pointer.

endpoint, lxcmap, ipcache, and lbmap were also using value receivers
for their implementations of MapValue interface. The problem with this
is that any lookups would fail to return the actual value, as the
bpf.LookupElement would write the value into a temporary unaccessible
to the caller. No such lookups were performed, so this did not cause
any problems in practice. Fix the implementations to prevent future
problems.

This fix is otherwise low risk, but it has happened earlier in
development that GetValuePtr() implmentations were not fixed properly
and a pointer to the pointer receiver was returned. This is not
noticed by the compiler, and would result in garbage data being
written to/read from the bpf maps.

Fixes: 37bd5caeb7 (bpf: Refactor EndpointKey)
Fixes: cb1208ee0b (pkg/maps: add BPF ipcache map)
Signed-off-by: André Martins <andre@cilium.io>

daemon/endpoint.go
@@ -775,14 +775,14 @@ func (d *Daemon) OnIPIdentityCacheChange(modType ipcache.CacheModification, oldI
 	switch modType {
 	case ipcache.Upsert:
 		value := ipCacheBPF.RemoteEndpointInfo{SecurityIdentity: uint16(newIPIDPair.ID)}
-		err := ipCacheBPF.IPCache.Update(key, &value)
+		err := ipCacheBPF.IPCache.Update(&key, &value)
 		if err != nil {
 			log.WithError(err).WithFields(logrus.Fields{"key": key.String(),
 				"value": value.String()}).
 				Warning("unable to update bpf map")
 		}
 	case ipcache.Delete:
-		err := ipCacheBPF.IPCache.Delete(key)
+		err := ipCacheBPF.IPCache.Delete(&key)
 		if err != nil {
 			log.WithError(err).WithFields(logrus.Fields{"key": key.String()}).
 				Warning("unable to delete from bpf map")
@@ -814,19 +814,19 @@ func (d *Daemon) OnIPIdentityCacheGC() {
 				ipcache.IPIdentityCache.RLock()
 				defer ipcache.IPIdentityCache.RUnlock()
 
-				keysToRemove := map[ipCacheBPF.Key]struct{}{}
+				keysToRemove := map[string]*ipCacheBPF.Key{}
 
 				// Add all keys which are in BPF map but not in in-memory cache
 				// to set of keys to remove from BPF map.
 				cb := func(key bpf.MapKey, value bpf.MapValue) {
-					k := key.(ipCacheBPF.Key)
+					k := key.(*ipCacheBPF.Key)
 					keyToIP := k.String()
 
 					// Don't RLock as part of the same goroutine.
 					if _, exists := ipcache.IPIdentityCache.LookupByPrefixRLocked(keyToIP); !exists {
 						// Cannot delete from map during callback because DumpWithCallback
 						// RLocks the map.
-						keysToRemove[k] = struct{}{}
+						keysToRemove[keyToIP] = k
 					}
 				}
 
@@ -836,7 +836,7 @@ func (d *Daemon) OnIPIdentityCacheGC() {
 
 				// Remove all keys which are not in in-memory cache from BPF map
 				// for consistency.
-				for k := range keysToRemove {
+				for _, k := range keysToRemove {
 					log.WithFields(logrus.Fields{logfields.BPFMapKey: k}).
 						Debug("deleting from ipcache BPF map")
 					if err := ipCacheBPF.IPCache.Delete(k); err != nil {

pkg/bpf/endpoint.go
@@ -38,7 +38,7 @@ type EndpointKey struct {
 }
 
 // GetKeyPtr returns the unsafe pointer to the BPF key
-func (k EndpointKey) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(&k) }
+func (k *EndpointKey) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
 
 // GetValuePtr returns the unsafe pointer to the BPF key for users that
 // use EndpointKey as a value in bpf maps

pkg/endpoint/bpf.go
@@ -374,7 +374,7 @@ func (e *Endpoint) runInit(libdir, rundir, epdir, ifName, debug string) error {
 // after releasing that lock to push the entries into the datapath.
 // Functions below implement the EndpointFrontend interface with this cached information.
 type epInfoCache struct {
-	keys     []lxcmap.EndpointKey
+	keys     []*lxcmap.EndpointKey
 	value    *lxcmap.EndpointInfo
 	ifName   string
 	revision uint64
@@ -395,7 +395,7 @@ func (e *Endpoint) createEpInfoCache() *epInfoCache {
 
 // GetBPFKeys returns all keys which should represent this endpoint in the BPF
 // endpoints map
-func (ep *epInfoCache) GetBPFKeys() []lxcmap.EndpointKey {
+func (ep *epInfoCache) GetBPFKeys() []*lxcmap.EndpointKey {
 	return ep.keys
 }
 

pkg/endpoint/endpoint.go
@@ -1367,15 +1367,15 @@ func (e *Endpoint) RemoveFromGlobalPolicyMap() error {
 
 // GetBPFKeys returns all keys which should represent this endpoint in the BPF
 // endpoints map
-func (e *Endpoint) GetBPFKeys() []lxcmap.EndpointKey {
+func (e *Endpoint) GetBPFKeys() []*lxcmap.EndpointKey {
 	key := lxcmap.NewEndpointKey(e.IPv6.IP())
 
 	if e.IPv4 != nil {
 		key4 := lxcmap.NewEndpointKey(e.IPv4.IP())
-		return []lxcmap.EndpointKey{key, key4}
+		return []*lxcmap.EndpointKey{key, key4}
 	}
 
-	return []lxcmap.EndpointKey{key}
+	return []*lxcmap.EndpointKey{key}
 }
 
 // GetBPFValue returns the value which should represent this endpoint in the

pkg/maps/ipcache/ipcache.go
@@ -56,7 +56,7 @@ type Key struct {
 }
 
 // GetKeyPtr returns the unsafe pointer to the BPF key
-func (k Key) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(&k) }
+func (k *Key) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
 
 // NewValue returns a new empty instance of the structure representing the BPF
 // map value
@@ -150,7 +150,7 @@ func NewMap() *Map {
 				if err := bpf.ConvertKeyValue(key, value, &k, &v); err != nil {
 					return nil, nil, err
 				}
-				return k, &v, nil
+				return &k, &v, nil
 			},
 		),
 	}

pkg/maps/lxcmap/lxcmap.go
@@ -51,7 +51,7 @@ var (
 				return nil, nil, err
 			}
 
-			return k, &v, nil
+			return &k, &v, nil
 		},
 	)
 )
@@ -96,7 +96,7 @@ const (
 // with the endpoint BPF map
 type EndpointFrontend interface {
 	// GetBPFKeys must return a slice of EndpointKey which all represent the endpoint
-	GetBPFKeys() []EndpointKey
+	GetBPFKeys() []*EndpointKey
 
 	// GetBPFValue must return an EndpointInfo structure representing the frontend
 	GetBPFValue() (*EndpointInfo, error)
@@ -128,8 +128,8 @@ func (k EndpointKey) NewValue() bpf.MapValue { return &EndpointInfo{} }
 
 // NewEndpointKey returns an EndpointKey based on the provided IP address. The
 // address family is automatically detected
-func NewEndpointKey(ip net.IP) EndpointKey {
-	return EndpointKey{
+func NewEndpointKey(ip net.IP) *EndpointKey {
+	return &EndpointKey{
 		EndpointKey: bpf.NewEndpointKey(ip),
 	}
 }
@@ -158,8 +158,8 @@ func WriteEndpoint(f EndpointFrontend) error {
 	}
 
 	// FIXME: Revert on failure
-	for _, k := range f.GetBPFKeys() {
-		if err := LXCMap.Update(k, info); err != nil {
+	for _, v := range f.GetBPFKeys() {
+		if err := LXCMap.Update(v, info); err != nil {
 			return err
 		}
 	}

pkg/maps/tunnel/tunnel.go
@@ -51,7 +51,7 @@ var (
 				return nil, nil, err
 			}
 
-			return k, &v, nil
+			return &k, &v, nil
 		})
 )
 
@@ -67,8 +67,8 @@ type tunnelEndpoint struct {
 	bpf.EndpointKey
 }
 
-func newTunnelEndpoint(ip net.IP) tunnelEndpoint {
-	return tunnelEndpoint{
+func newTunnelEndpoint(ip net.IP) *tunnelEndpoint {
+	return &tunnelEndpoint{
 		EndpointKey: bpf.NewEndpointKey(ip),
 	}
 }
@@ -84,7 +84,7 @@ func SetTunnelEndpoint(prefix net.IP, endpoint net.IP) error {
 		fieldEndpoint: endpoint,
 	}).Debug("Updating tunnel map entry")
 
-	return TunnelMap.Update(key, &val)
+	return TunnelMap.Update(key, val)
 }
 
 // DeleteTunnelEndpoint removes a prefix => tunnel-endpoint mapping