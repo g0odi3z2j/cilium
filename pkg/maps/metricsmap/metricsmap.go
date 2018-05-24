g0odi3z2j/cilium

commit b361943179f1f6314a75e279157fd50f036e49b8

pkg/maps/metricsmap: Add a new userspace pkg/maps/metricsmap to access BPF metrics maps.
This commit adds a new pkg/maps/metricsmap which defines the userspace interface and
functions to access the BPF metrics maps.

Fixes: #4155
Related-to: #3284
Related-to: #3337
Signed-Off-By: Manali Bhutiyani <manali@covalent.io>

pkg/maps/metricsmap/metricsmap.go
@@ -1,0 +1,94 @@
+// Copyright 2016-2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metricsmap
+
+import (
+	"fmt"
+	"unsafe"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/logging"
+)
+
+var log = logging.DefaultLogger
+
+const (
+	// MapName for metrics map.
+	MapName = "cilium_metrics"
+	// MaxEntries is the maximum number of keys that can be present in the
+	// Metrics Map.
+	MaxEntries = 65536
+)
+
+// Key must be in sync with struct metrics_key in <bpf/lib/common.h>
+type Key struct {
+	Reason uint8
+	Dir    uint8
+	Pad1   uint16
+	Pad2   uint32
+}
+
+// Value must be in sync with struct metrics_value in <bpf/lib/common.h>
+type Value struct {
+	Count uint64
+	Bytes uint64
+}
+
+// String converts the key into a human readable string format
+func (k *Key) String() string {
+	return fmt.Sprintf("reason:%d dir:%d", k.Reason, k.Dir)
+}
+
+// GetKeyPtr returns the unsafe pointer to the BPF key
+func (k *Key) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+
+// String converts the value into a human readable string format
+func (v *Value) String() string {
+	return fmt.Sprintf("count:%d bytes:%d", v.Count, v.Bytes)
+}
+
+// NewValue returns a new empty instance of the structure representing the BPF
+// map value
+func (k *Key) NewValue() bpf.MapValue { return &Value{} }
+
+// GetValuePtr returns the unsafe pointer to the BPF value.
+func (v *Value) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(v) }
+
+var (
+	// Metrics is a mapping of all packet drops and forwards associated with
+	// the node on ingress/egress direction
+	Metrics = bpf.NewMap(
+		MapName,
+		bpf.BPF_MAP_TYPE_HASH,
+		int(unsafe.Sizeof(Value{})),
+		int(unsafe.Sizeof(Value{})),
+		MaxEntries,
+		0,
+		func(key []byte, value []byte) (bpf.MapKey, bpf.MapValue, error) {
+			k, v := Key{}, Value{}
+
+			if err := bpf.ConvertKeyValue(key, value, &k, &v); err != nil {
+				return nil, nil, err
+			}
+			return &k, &v, nil
+		})
+)
+
+func init() {
+	err := bpf.OpenAfterMount(Metrics)
+	if err != nil {
+		log.WithError(err).Error("unable to open metrics map")
+	}
+}

commit 615472768cc2550da8891a11632fb17db1538c23

metricsmap: Set the key size properly
The key size when creating this map type from userspace was set to the
size of the value instead of the key. Fix it.

Fixes: b361943179f1 ("pkg/maps/metricsmap: Add a new userspace pkg/maps/metricsmap to access BPF metrics maps.")
Fixes: #4228

CC: Manali Bhutiyani <manali@covalent.io>
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/maps/metricsmap/metricsmap.go
@@ -72,7 +72,7 @@ var (
 	Metrics = bpf.NewMap(
 		MapName,
 		bpf.BPF_MAP_TYPE_HASH,
-		int(unsafe.Sizeof(Value{})),
+		int(unsafe.Sizeof(Key{})),
 		int(unsafe.Sizeof(Value{})),
 		MaxEntries,
 		0,