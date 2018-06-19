g0odi3z2j/cilium

commit e662da3227d76750c341c59bb9e48bb1c58047bf

utils: Refactor reaching into EndpointSelector
The k8s utils helpers were previously reaching directly into the
EndpointSelector and manipulating it after creation, which tightly
couples the implementations. Add some helpers so that these libraries
can become more independent.

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/k8s/apis/cilium.io/utils/utils.go
@@ -85,10 +85,7 @@ func getEndpointSelector(namespace string, labelSelector *metav1.LabelSelector, 
 	// Don't add a namespace label to those endpoint selectors, or we wouldn't be
 	// able to match on those pods.
 	if !matchesInit && !es.HasKey(podPrefixLbl) {
-		if es.MatchLabels == nil {
-			es.MatchLabels = map[string]string{}
-		}
-		es.MatchLabels[podPrefixLbl] = namespace
+		es.AddMatch(podPrefixLbl, namespace)
 	}
 
 	return es
@@ -193,17 +190,14 @@ func ParseToCiliumRule(namespace, name string, r *api.Rule) *api.Rule {
 		// The PodSelector should only reflect to the same namespace
 		// the policy is being stored, thus we add the namespace to
 		// the MatchLabels map.
-		if retRule.EndpointSelector.LabelSelector.MatchLabels == nil {
-			retRule.EndpointSelector.LabelSelector.MatchLabels = map[string]string{}
-		}
-
+		//
 		// Policies applying on initializing pods are a special case.
 		// Those pods don't have any labels, so they don't have a namespace label either.
 		// Don't add a namespace label to those endpoint selectors, or we wouldn't be
 		// able to match on those pods.
-		if _, ok := retRule.EndpointSelector.LabelSelector.MatchLabels[podInitLbl]; !ok {
-			userNamespace, ok := retRule.EndpointSelector.LabelSelector.MatchLabels[podPrefixLbl]
-			if ok && userNamespace != namespace {
+		if !retRule.EndpointSelector.HasKey(podInitLbl) {
+			userNamespace, ok := retRule.EndpointSelector.GetMatch(podPrefixLbl)
+			if ok && (len(userNamespace) > 1 || (len(userNamespace) == 1 && userNamespace[1] != namespace)) {
 				log.WithFields(logrus.Fields{
 					logfields.K8sNamespace:              namespace,
 					logfields.CiliumNetworkPolicyName:   name,
@@ -211,7 +205,7 @@ func ParseToCiliumRule(namespace, name string, r *api.Rule) *api.Rule {
 				}).Warn("CiliumNetworkPolicy contains illegal namespace match in EndpointSelector." +
 					" EndpointSelector always applies in namespace of the policy resource, removing illegal namespace match'.")
 			}
-			retRule.EndpointSelector.LabelSelector.MatchLabels[podPrefixLbl] = namespace
+			retRule.EndpointSelector.AddMatch(podPrefixLbl, namespace)
 		}
 	}
 

pkg/policy/api/selector.go
@@ -132,6 +132,20 @@ func (n EndpointSelector) HasKey(key string) bool {
 	return false
 }
 
+// GetMatch checks for a match on the specified key, and returns the value that
+// the key must match, and true. If a match cannot be found, returns nil, false.
+func (n EndpointSelector) GetMatch(key string) ([]string, bool) {
+	if value, ok := n.MatchLabels[key]; ok {
+		return []string{value}, true
+	}
+	for _, v := range n.MatchExpressions {
+		if v.Key == key && v.Operator == metav1.LabelSelectorOpIn {
+			return v.Values, true
+		}
+	}
+	return nil, false
+}
+
 // NewESFromLabels creates a new endpoint selector from the given labels.
 func NewESFromLabels(lbls ...*labels.Label) EndpointSelector {
 	ml := map[string]string{}
@@ -200,6 +214,14 @@ func NewESFromK8sLabelSelector(srcPrefix string, lss ...*metav1.LabelSelector) E
 			MatchExpressions: matchExpressions,
 		},
 	}
+}
+
+// AddMatch adds a match for 'key' == 'value' to the endpoint selector.
+func (n *EndpointSelector) AddMatch(key, value string) {
+	if n.MatchLabels == nil {
+		n.MatchLabels = map[string]string{}
+	}
+	n.MatchLabels[key] = value
 }
 
 // Matches returns true if the endpoint selector Matches the `lblsToMatch`.

commit 799c338ecc016678d15f157e7d155854ef9e70dd

k8s: Fix index error in CiliumRule parsing
Recent refactoring introduced an indexing error:

panic: runtime error: index out of range

goroutine 743 [running]:
github.com/cilium/cilium/pkg/k8s/apis/cilium.io/utils.ParseToCiliumRule(...)
	/go/src/github.com/cilium/cilium/pkg/k8s/apis/cilium.io/utils/utils.go:200 +0x9ee
github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2.(*CiliumNetworkPolicy).Parse(...)
	/go/src/github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2/types.go:161 +0x1b9
main.(*Daemon).addCiliumNetworkPolicyV2(...)
	/go/src/github.com/cilium/cilium/daemon/k8s_watcher.go:1308 +0x2d3
main.(*Daemon).EnableK8sWatcher.func13.1(...)
	/go/src/github.com/cilium/cilium/daemon/k8s_watcher.go:414 +0x45
github.com/cilium/cilium/pkg/serializer.(*functionQueue).run(...)
	/go/src/github.com/cilium/cilium/pkg/serializer/func_queue.go:65 +0x70
created by github.com/cilium/cilium/pkg/serializer.NewFunctionQueue
	/go/src/github.com/cilium/cilium/pkg/serializer/func_queue.go:45 +0xca

Fix the issue and introduce unit tests to catch this in future.

Fixes: #4548
Fixes: e662da3227d7 ("utils: Refactor reaching into EndpointSelector")

Reported-by: Michi Mutsuzaki <michi@covalent.io>
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/k8s/apis/cilium.io/utils/utils.go
@@ -197,7 +197,7 @@ func ParseToCiliumRule(namespace, name string, r *api.Rule) *api.Rule {
 		// able to match on those pods.
 		if !retRule.EndpointSelector.HasKey(podInitLbl) {
 			userNamespace, ok := retRule.EndpointSelector.GetMatch(podPrefixLbl)
-			if ok && (len(userNamespace) > 1 || (len(userNamespace) == 1 && userNamespace[1] != namespace)) {
+			if ok && (len(userNamespace) > 1 || (len(userNamespace) == 1 && userNamespace[0] != namespace)) {
 				log.WithFields(logrus.Fields{
 					logfields.K8sNamespace:              namespace,
 					logfields.CiliumNetworkPolicyName:   name,

pkg/k8s/apis/cilium.io/utils/utils_test.go
@@ -1,0 +1,178 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package utils
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cilium/cilium/pkg/comparator"
+	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/policy/api"
+
+	. "gopkg.in/check.v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type CiliumV2Suite struct{}
+
+var _ = Suite(&CiliumV2Suite{})
+
+func Test_ParseToCiliumRule(t *testing.T) {
+	role := fmt.Sprintf("%s.role", labels.LabelSourceAny)
+	namespace := fmt.Sprintf("%s.%s", labels.LabelSourceK8s, k8sConst.PodNamespaceLabel)
+	type args struct {
+		namespace string
+		rule      *api.Rule
+	}
+	tests := []struct {
+		name string
+		args args
+		want *api.Rule
+	}{
+		{
+			// When the rule has no namespace match, the namespace
+			// is inherited from the namespace where the rule is
+			// added.
+			name: "parse-in-namespace",
+			args: args{
+				namespace: metav1.NamespaceDefault,
+				rule: &api.Rule{
+					EndpointSelector: api.NewESFromMatchRequirements(
+						map[string]string{
+							role: "backend",
+						},
+						nil,
+					),
+				},
+			},
+			want: &api.Rule{
+				EndpointSelector: api.NewESFromMatchRequirements(
+					map[string]string{
+						role:      "backend",
+						namespace: "default",
+					},
+					nil,
+				),
+				Labels: labels.LabelArray{
+					{
+						Key:    "io.cilium.k8s.policy.name",
+						Value:  "parse-in-namespace",
+						Source: labels.LabelSourceK8s,
+					},
+					{
+						Key:    "io.cilium.k8s.policy.namespace",
+						Value:  "default",
+						Source: labels.LabelSourceK8s,
+					},
+				},
+			},
+		},
+		{
+			// When the rule specifies a namespace, it is overridden
+			// by the namespace where the rule was inserted.
+			name: "parse-in-namespace-with-ns-selector",
+			args: args{
+				namespace: metav1.NamespaceDefault,
+				rule: &api.Rule{
+					EndpointSelector: api.NewESFromMatchRequirements(
+						map[string]string{
+							role:      "backend",
+							namespace: "foo",
+						},
+						nil,
+					),
+				},
+			},
+			want: &api.Rule{
+				EndpointSelector: api.NewESFromMatchRequirements(
+					map[string]string{
+						role:      "backend",
+						namespace: "default",
+					},
+					nil,
+				),
+				Labels: labels.LabelArray{
+					{
+						Key:    "io.cilium.k8s.policy.name",
+						Value:  "parse-in-namespace-with-ns-selector",
+						Source: labels.LabelSourceK8s,
+					},
+					{
+						Key:    "io.cilium.k8s.policy.namespace",
+						Value:  "default",
+						Source: labels.LabelSourceK8s,
+					},
+				},
+			},
+		},
+		{
+			// Don't insert a namespace selection when the rule
+			// is for init policies.
+			name: "parse-init-policy",
+			args: args{
+				namespace: metav1.NamespaceDefault,
+				rule: &api.Rule{
+					EndpointSelector: api.NewESFromMatchRequirements(
+						map[string]string{
+							role:       "backend",
+							podInitLbl: "",
+						},
+						nil,
+					),
+				},
+			},
+			want: &api.Rule{
+				EndpointSelector: api.NewESFromMatchRequirements(
+					map[string]string{
+						role:       "backend",
+						podInitLbl: "",
+						// No namespace because it's init.
+						// namespace: "default",
+					},
+					nil,
+				),
+				Labels: labels.LabelArray{
+					{
+						Key:    "io.cilium.k8s.policy.name",
+						Value:  "parse-init-policy",
+						Source: labels.LabelSourceK8s,
+					},
+					{
+						Key:    "io.cilium.k8s.policy.namespace",
+						Value:  "default",
+						Source: labels.LabelSourceK8s,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ParseToCiliumRule(tt.args.namespace, tt.name, tt.args.rule)
+			args := []interface{}{got, tt.want}
+			names := []string{"obtained", "expected"}
+			if equal, err := comparator.DeepEquals.Check(args, names); !equal {
+				t.Errorf("Failed to ParseToCiliumRule():\n%s", err)
+			}
+		})
+	}
+}