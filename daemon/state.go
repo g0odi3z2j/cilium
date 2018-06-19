g0odi3z2j/cilium

commit 5abdb4134d6847148b94b4384d5426730a039b08

agent: Reserve existing endpoint IPs before allocating auxiliary IPs
Several IPs are allocated out of the main CIDR block for the cilium_host
interface as well as the loopback redirection use case. Because these IPs were
allocated before the endpoints were restored, it was possible for IPs of to be
restored endpoints to be already allocated.  Due to the conflict, restoring of
these endpoints would fail.

Fixes: #4317

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/state.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Authors of Cilium
+// Copyright 2016-2018 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -34,16 +34,36 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-// SyncState syncs cilium state against the containers running in the host. dir is the
-// cilium's running directory. If clean is set, the endpoints that don't have its
-// container in running state are deleted.
-func (d *Daemon) SyncState(dir string, clean bool) error {
+type endpointRestoreState struct {
+	restored []*endpoint.Endpoint
+	toClean  []*endpoint.Endpoint
+}
+
+// restoreOldEndpoints reads the list of existing endpoints previously managed
+// Cilium when it was last run and associated it with container workloads. This
+// function performs the first step in restoring the endpoint structure,
+// allocating their existing IP out of the CIDR block and then inserting the
+// endpoints into the endpoints list. It needs to be followed by a call to
+// regenerateRestoredEndpoints() once the endpoint builder is ready.
+//
+// If clean is true, endpoints which cannot be associated with a container
+// workloads are deleted.
+func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreState, error) {
+	state := &endpointRestoreState{
+		restored: []*endpoint.Endpoint{},
+		toClean:  []*endpoint.Endpoint{},
+	}
+
+	if !option.Config.RestoreState {
+		log.Info("Endpoint restore is disabled, skipping restore step")
+		return state, nil
+	}
 
 	log.Info("Restoring endpoints from former life...")
 
 	dirFiles, err := ioutil.ReadDir(dir)
 	if err != nil {
-		return err
+		return state, err
 	}
 	eptsID := endpoint.FilterEPDir(dirFiles)
 
@@ -51,62 +71,77 @@ func (d *Daemon) SyncState(dir string, clean bool) error {
 
 	if len(possibleEPs) == 0 {
 		log.Info("No old endpoints found.")
-		return nil
+		return state, nil
 	}
 
-	nEndpoints := len(possibleEPs)
+	for _, ep := range possibleEPs {
+		scopedLog := log.WithField(logfields.EndpointID, ep.ID)
+		skipRestore := false
+		if _, err := netlink.LinkByName(ep.IfName); err != nil {
+			scopedLog.Infof("Interface %s could not be found for endpoint being restored, ignoring", ep.IfName)
+			skipRestore = true
+		} else if !workloads.IsRunning(ep) {
+			scopedLog.Info("No workload could be associated with endpoint being restored, ignoring")
+			skipRestore = true
+		}
 
-	// we need to signalize when the endpoints are restored, i.e., when the
-	// essential attributes were populated on the daemon, such as: IPs allocated
-	// and endpoint default options.
-	epRestored := make(chan bool, nEndpoints)
+		if clean && skipRestore {
+			state.toClean = append(state.toClean, ep)
+			continue
+		}
+
+		ep.Mutex.Lock()
+		scopedLog.Debug("Restoring endpoint")
+		ep.LogStatusOKLocked(endpoint.Other, "Restoring endpoint from previous cilium instance")
+
+		if err := d.allocateIPsLocked(ep); err != nil {
+			ep.Mutex.Unlock()
+			scopedLog.WithError(err).Error("Failed to re-allocate IP of endpoint. Not restoring endpoint.")
+			state.toClean = append(state.toClean, ep)
+			continue
+		}
+
+		if option.Config.KeepConfig {
+			ep.SetDefaultOpts(nil)
+		} else {
+			ep.SetDefaultOpts(option.Config.Opts)
+			alwaysEnforce := policy.GetPolicyEnabled() == option.AlwaysEnforce
+			ep.Opts.Set(option.IngressPolicy, alwaysEnforce)
+			ep.Opts.Set(option.EgressPolicy, alwaysEnforce)
+		}
+
+		endpointmanager.Insert(ep)
+		ep.Mutex.Unlock()
+
+		state.restored = append(state.restored, ep)
+	}
+
+	log.WithFields(logrus.Fields{
+		"count.restored": len(state.restored),
+		"count.total":    len(possibleEPs),
+	}).Info("Endpoints restored, endpoints will be regenerated in background")
+
+	return state, nil
+}
+
+func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
+	log.Infof("Regenerating %d restored endpoints", len(state.restored))
+
 	// we need to signalize when the endpoints are regenerated, i.e., when
 	// they have finished to rebuild after being restored.
-	epRegenerated := make(chan bool, nEndpoints)
+	epRegenerated := make(chan bool, len(state.restored))
 
-	for _, ep := range possibleEPs {
-		go func(ep *endpoint.Endpoint, epRestored, epRegenerated chan<- bool) {
-			scopedLog := log.WithField(logfields.EndpointID, ep.ID)
-			skipRestore := false
-			if _, err := netlink.LinkByName(ep.IfName); err != nil {
-				scopedLog.Infof("Interface %s could not be found for endpoint being restored, ignoring", ep.IfName)
-				skipRestore = true
-			} else if !workloads.IsRunning(ep) {
-				scopedLog.Info("No workload could be associated with endpoint being restored, ignoring")
-				skipRestore = true
-			}
-
-			if clean && skipRestore {
-				d.deleteEndpointQuiet(ep)
-				epRegenerated <- false
-				epRestored <- false
-				return
-			}
-
+	for _, ep := range state.restored {
+		go func(ep *endpoint.Endpoint, epRegenerated chan<- bool) {
 			ep.Mutex.Lock()
-			scopedLog.Debug("Restoring endpoint")
-			ep.LogStatusOKLocked(endpoint.Other, "Restoring endpoint from previous cilium instance")
+			scopedLog := log.WithField(logfields.EndpointID, ep.ID)
 
-			if err := d.allocateIPsLocked(ep); err != nil {
+			state := ep.GetStateLocked()
+			if state == endpoint.StateDisconnected || state == endpoint.StateDisconnecting {
+				scopedLog.Warn("Endpoint to restore has been deleted")
 				ep.Mutex.Unlock()
-				scopedLog.WithError(err).Error("Failed to re-allocate IP of endpoint. Not restoring endpoint.")
-				d.deleteEndpointQuiet(ep)
-				epRegenerated <- false
-				epRestored <- false
 				return
 			}
-
-			if option.Config.KeepConfig {
-				ep.SetDefaultOpts(nil)
-			} else {
-				ep.SetDefaultOpts(option.Config.Opts)
-				alwaysEnforce := policy.GetPolicyEnabled() == option.AlwaysEnforce
-				ep.Opts.Set(option.IngressPolicy, alwaysEnforce)
-				ep.Opts.Set(option.EgressPolicy, alwaysEnforce)
-			}
-
-			endpointmanager.Insert(ep)
-			epRestored <- true
 
 			ep.LogStatusOKLocked(endpoint.Other, "Synchronizing endpoint labels with KVStore")
 			if err := d.syncLabels(ep); err != nil {
@@ -133,18 +168,22 @@ func (d *Daemon) SyncState(dir string, clean bool) error {
 			scopedLog.WithField(logfields.IPAddr, []string{ep.IPv4.String(), ep.IPv6.String()}).Info("Restored endpoint")
 			ep.Mutex.RUnlock()
 			epRegenerated <- true
-		}(ep, epRestored, epRegenerated)
+		}(ep, epRegenerated)
+	}
+
+	for _, ep := range state.toClean {
+		go d.deleteEndpointQuiet(ep)
 	}
 
 	go func() {
 		regenerated, total := 0, 0
-		if nEndpoints > 0 {
+		if len(state.restored) > 0 {
 			for buildSuccess := range epRegenerated {
 				if buildSuccess {
 					regenerated++
 				}
 				total++
-				if total >= nEndpoints {
+				if total >= len(state.restored) {
 					break
 				}
 			}
@@ -156,26 +195,6 @@ func (d *Daemon) SyncState(dir string, clean bool) error {
 			"total":       total,
 		}).Info("Finished regenerating restored endpoints")
 	}()
-
-	if nEndpoints > 0 {
-		// We need to wait for the endpoints IPs and labels
-		// are properly synced before continuing.
-		nEPsRestored := 0
-		for range epRestored {
-			nEPsRestored++
-			if nEPsRestored >= nEndpoints {
-				break
-			}
-		}
-		close(epRestored)
-
-		log.WithFields(logrus.Fields{
-			"count.restored": nEPsRestored,
-			"count.total":    nEndpoints,
-		}).Info("Endpoints restored, endpoints will continue to regenerate in background")
-	}
-
-	return nil
 }
 
 func (d *Daemon) allocateIPsLocked(ep *endpoint.Endpoint) error {

daemon/daemon.go
@@ -1144,7 +1144,17 @@ func NewDaemon() (*Daemon, error) {
 
 	// Set up ipam conf after init() because we might be running d.conf.KVStoreIPv4Registration
 	log.Info("Initializing IPAM")
-	switch err := ipam.Init(); err.(type) {
+	ipam.Init()
+
+	// restore endpoints before any IPs are allocated to avoid eventual IP
+	// conflicts later on, otherwise any IP conflict will result in the
+	// endpoint not being able to be restored.
+	restoredEndpoints, err := d.restoreOldEndpoints(option.Config.StateDir, true)
+	if err != nil {
+		log.WithError(err).Error("Unable to restore existing endpoints")
+	}
+
+	switch err := ipam.AllocateInternalIPs(); err.(type) {
 	case ipam.ErrAllocation:
 		if v4Prefix == AutoCIDR || v6Prefix == AutoCIDR {
 			log.WithError(err).Fatalf(
@@ -1217,10 +1227,8 @@ func NewDaemon() (*Daemon, error) {
 		option.Config.AccessLog, &d, option.Config.AgentLabels)
 
 	if option.Config.RestoreState {
-		log.Info("Restoring state...")
-		if err := d.SyncState(option.Config.StateDir, true); err != nil {
-			log.WithError(err).Warn("Error while recovering endpoints")
-		}
+		d.regenerateRestoredEndpoints(restoredEndpoints)
+
 		go func() {
 			if err := d.SyncLBMap(); err != nil {
 				log.WithError(err).Warn("Error while recovering endpoints")

pkg/ipam/init.go
@@ -96,7 +96,7 @@ func ReserveLocalRoutes() {
 }
 
 // Init initializes the IPAM package
-func Init() error {
+func Init() {
 	ipamSubnets := net.IPNet{
 		IP:   node.GetIPv6Router(),
 		Mask: node.StateIPv6Mask,
@@ -134,7 +134,12 @@ func Init() error {
 			Dst: node.IPv4DefaultRoute,
 			GW:  node.GetInternalIPv4(),
 		})
+}
 
+// AllocateInternalIPs allocates all non endpoint IPs in the CIDR required for
+// operation. This mustbe called *after* endpoints have been restored to avoid
+// allocation conflicts
+func AllocateInternalIPs() error {
 	// Reserve the IPv4 router IP if it is part of the IPv4
 	// allocation range to ensure that we do not hand out the
 	// router IP to a container.

pkg/ipam/ipam_test.go
@@ -33,7 +33,8 @@ var _ = Suite(&IPAMSuite{})
 
 func (s *IPAMSuite) TestLock(c *C) {
 	node.InitDefaultPrefix("")
-	err := Init()
+	Init()
+	err := AllocateInternalIPs()
 	c.Assert(err, IsNil)
 
 	// Since the IPs we have allocated to the endpoints might or might not

commit 24d2034df593c9543a4e2d5217551d6ee83b4ffd

daemon: Fix endpoint restore log regarding health endpoint
On each restart, the health endpoint of the previous run cannot be restored.
This is fine as it is supposed to be recreated but it triggers an unnecessary
info message about restore not succeeding.

The cleaning of the state was already taking place as part of
health endpoint being added to `state.toClean`.
We simply need to get rid of the unnecessary log printed.

Fixes: #4325
Fixes: 5abdb4134d68 agent: Reserve existing endpoint IPs before allocating auxiliary IPs
Signed-off-by: Manali Bhutiyani <manali@covalent.io>

daemon/state.go
@@ -77,12 +77,19 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 	for _, ep := range possibleEPs {
 		scopedLog := log.WithField(logfields.EndpointID, ep.ID)
 		skipRestore := false
-		if _, err := netlink.LinkByName(ep.IfName); err != nil {
-			scopedLog.Infof("Interface %s could not be found for endpoint being restored, ignoring", ep.IfName)
+
+		// On each restart, the health endpoint is supposed to be recreated.
+		// Hence we need to clean health endpoint state unconditionally.
+		if ep.SecurityIdentity.ID == identityPkg.ReservedIdentityHealth {
 			skipRestore = true
-		} else if !workloads.IsRunning(ep) {
-			scopedLog.Info("No workload could be associated with endpoint being restored, ignoring")
-			skipRestore = true
+		} else {
+			if _, err := netlink.LinkByName(ep.IfName); err != nil {
+				scopedLog.Infof("Interface %s could not be found for endpoint being restored, ignoring", ep.IfName)
+				skipRestore = true
+			} else if !workloads.IsRunning(ep) {
+				scopedLog.Info("No workload could be associated with endpoint being restored, ignoring")
+				skipRestore = true
+			}
 		}
 
 		if clean && skipRestore {