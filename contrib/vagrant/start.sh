g0odi3z2j/cilium

commit 12bc14ba119874e91d59d3c091fdc62efb9a6bc2

start.sh: set RELOAD=1 if VM already exists
Signed-off-by: André Martins <andre@cilium.io>

contrib/vagrant/start.sh
@@ -53,6 +53,16 @@ export 'CILIUM_TEMP'="${dir}"
 # Required bazel version
 export 'BAZEL_VERSION'=$(<envoy/BAZEL_VERSION)
 
+# Sets the RELOAD env variable with 1 if there is any VM printed by
+# vagrant status.
+function set_reload_if_vm_exists(){
+    if [ -z "${RELOAD}" ]; then
+        if [[ $(vagrant status 2>/dev/null | wc -l) -gt 1 ]]; then
+            RELOAD=1
+        fi
+    fi
+}
+
 # split_ipv4 splits an IPv4 address into a bash array and assigns it to ${1}.
 # Exits if ${2} is an invalid IPv4 address.
 function split_ipv4(){
@@ -551,6 +561,8 @@ ipv6_public_workers_addrs=()
 
 split_ipv4 ipv4_array "${MASTER_IPV4}"
 MASTER_IPV6="${IPV6_INTERNAL_CIDR}$(printf '%02X' ${ipv4_array[3]})"
+
+set_reload_if_vm_exists
 
 create_master
 create_workers

commit e026ea6e7b455dd1bca42f6a0ec5b98c7be0baa6

start.sh: fix RELOAD=1 if VM is not created
Fixes: 12bc14ba11 ("start.sh: set RELOAD=1 if VM already exists")
Signed-off-by: André Martins <andre@cilium.io>

contrib/vagrant/start.sh
@@ -57,7 +57,8 @@ export 'BAZEL_VERSION'=$(<envoy/BAZEL_VERSION)
 # vagrant status.
 function set_reload_if_vm_exists(){
     if [ -z "${RELOAD}" ]; then
-        if [[ $(vagrant status 2>/dev/null | wc -l) -gt 1 ]]; then
+        if [[ $(vagrant status 2>/dev/null | wc -l) -gt 1 && \
+                ! $(vagrant status 2>/dev/null | grep "not created") ]]; then
             RELOAD=1
         fi
     fi