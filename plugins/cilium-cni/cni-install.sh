g0odi3z2j/cilium

commit 2c6c2a27e844aa621b521dd55cc190c3c8fc9a61

mtu: Introduce device MTU concept
Introduces a device MTU to guarantee ability to receive of arbitrarily big
frames. Existing MTU is renamed to RouteMTU and used for all configured routes.

Provide both the route and device MTU via the API so all plugins can use the
MTU configured via the cilium-agent.

Signed-off-by: Thomas Graf <thomas@cilium.io>

plugins/cilium-cni/00-cilium-cni.conf
@@ -1,5 +1,4 @@
 {
     "name": "cilium",
     "type": "cilium-cni",
-    "mtu": 1450
 }

api/v1/models/daemon_configuration_status.go
@@ -23,6 +23,9 @@ type DaemonConfigurationStatus struct {
 	// addressing
 	Addressing *NodeAddressing `json:"addressing,omitempty"`
 
+	// MTU on workload facing devices
+	DeviceMTU int64 `json:"deviceMTU,omitempty"`
+
 	// Immutable configuration (read-only)
 	Immutable ConfigurationMap `json:"immutable,omitempty"`
 
@@ -40,9 +43,14 @@ type DaemonConfigurationStatus struct {
 
 	// Currently applied configuration
 	Realized *DaemonConfigurationSpec `json:"realized,omitempty"`
+
+	// MTU for network facing routes
+	RouteMTU int64 `json:"routeMTU,omitempty"`
 }
 
 /* polymorph DaemonConfigurationStatus addressing false */
+
+/* polymorph DaemonConfigurationStatus deviceMTU false */
 
 /* polymorph DaemonConfigurationStatus immutable false */
 
@@ -55,6 +63,8 @@ type DaemonConfigurationStatus struct {
 /* polymorph DaemonConfigurationStatus nodeMonitor false */
 
 /* polymorph DaemonConfigurationStatus realized false */
+
+/* polymorph DaemonConfigurationStatus routeMTU false */
 
 // Validate validates this daemon configuration status
 func (m *DaemonConfigurationStatus) Validate(formats strfmt.Registry) error {

api/v1/openapi.yaml
@@ -1341,6 +1341,12 @@ definitions:
         "$ref": "#/definitions/MonitorStatus"
       kvstoreConfiguration:
         "$ref": "#/definitions/KVstoreConfiguration"
+      deviceMTU:
+        description: MTU on workload facing devices
+        type: integer
+      routeMTU:
+        description: MTU for network facing routes
+        type: integer
   EndpointConfigurationSpec:
     description: An endpoint's configuration
     type: object

api/v1/server/embedded_spec.go
@@ -1168,6 +1168,10 @@ func init() {
         "addressing": {
           "$ref": "#/definitions/NodeAddressing"
         },
+        "deviceMTU": {
+          "description": "MTU on workload facing devices",
+          "type": "integer"
+        },
         "immutable": {
           "description": "Immutable configuration (read-only)",
           "$ref": "#/definitions/ConfigurationMap"
@@ -1188,6 +1192,10 @@ func init() {
         "realized": {
           "description": "Currently applied configuration",
           "$ref": "#/definitions/DaemonConfigurationSpec"
+        },
+        "routeMTU": {
+          "description": "MTU for network facing routes",
+          "type": "integer"
         }
       }
     },

cilium-health/launch/endpoint.go
@@ -85,13 +85,13 @@ func logFromCommand(cmd *exec.Cmd, netns string) error {
 
 func configureHealthRouting(netns, dev string, addressing *models.NodeAddressing) error {
 	routes := []plugins.Route{}
-	v4Routes, err := plugins.IPv4Routes(addressing, mtu.StandardMTU)
+	v4Routes, err := plugins.IPv4Routes(addressing, mtu.GetRouteMTU())
 	if err == nil {
 		routes = append(routes, v4Routes...)
 	} else {
 		log.Debugf("Couldn't get IPv4 routes for health routing")
 	}
-	v6Routes, err := plugins.IPv6Routes(addressing, mtu.StandardMTU)
+	v6Routes, err := plugins.IPv6Routes(addressing, mtu.GetRouteMTU())
 	if err != nil {
 		return fmt.Errorf("Failed to get IPv6 routes")
 	}
@@ -183,7 +183,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 		},
 	}
 
-	if _, _, err := plugins.SetupVethWithNames(vethName, vethPeerName, mtu.StandardMTU, info); err != nil {
+	if _, _, err := plugins.SetupVethWithNames(vethName, vethPeerName, mtu.GetDeviceMTU(), info); err != nil {
 		return fmt.Errorf("Error while creating veth: %s", err)
 	}
 

common/plugins/ipam.go
@@ -19,7 +19,6 @@ import (
 	"net"
 
 	"github.com/cilium/cilium/api/v1/models"
-	"github.com/cilium/cilium/pkg/mtu"
 	"github.com/cilium/cilium/pkg/node"
 )
 
@@ -91,7 +90,7 @@ func IPv6Routes(addr *models.NodeAddressing, linkMTU int) ([]Route, error) {
 		{
 			Prefix:  node.IPv6DefaultRoute,
 			Nexthop: &ip,
-			MTU:     linkMTU - mtu.TunnelOverhead,
+			MTU:     linkMTU,
 		},
 	}, nil
 }
@@ -112,7 +111,7 @@ func IPv4Routes(addr *models.NodeAddressing, linkMTU int) ([]Route, error) {
 		{
 			Prefix:  node.IPv4DefaultRoute,
 			Nexthop: &ip,
-			MTU:     linkMTU - mtu.TunnelOverhead,
+			MTU:     linkMTU,
 		},
 	}, nil
 }

daemon/daemon.go
@@ -681,7 +681,7 @@ func (d *Daemon) compileBase() error {
 	args[initArgRundir] = option.Config.StateDir
 	args[initArgIPv4NodeIP] = node.GetInternalIPv4().String()
 	args[initArgIPv6NodeIP] = node.GetIPv6().String()
-	args[initArgMTU] = fmt.Sprintf("%d", mtu.StandardMTU)
+	args[initArgMTU] = fmt.Sprintf("%d", mtu.GetDeviceMTU())
 
 	if option.Config.Device != "undefined" {
 		_, err := netlink.LinkByName(option.Config.Device)
@@ -1510,7 +1510,9 @@ func (h *getConfig) Handle(params GetConfigParams) middleware.Responder {
 			Type:    kvStore,
 			Options: kvStoreOpts,
 		},
-		Realized: spec,
+		Realized:  spec,
+		DeviceMTU: int64(mtu.GetDeviceMTU()),
+		RouteMTU:  int64(mtu.GetRouteMTU()),
 	}
 
 	cfg := &models.DaemonConfiguration{

pkg/mtu/mtu.go
@@ -63,3 +63,14 @@ func UseMTU(mtu int) {
 	StandardMTU = mtu
 	TunnelMTU = mtu - TunnelOverhead
 }
+
+// GetRouteMTU returns the MTU to be used on the network. When running in
+// tunneling mode, this will have tunnel overhead accounted for.
+func GetRouteMTU() int {
+	return TunnelMTU
+}
+
+// GetDeviceMTU returns the MTU to be used on workload facing devices.
+func GetDeviceMTU() int {
+	return StandardMTU
+}

commit 8b44a62be99e5927929241674dcddcc0a3db6be4

plugins: fix CNI configuration typo
Fixes: 2c6c2a27e844 ("mtu: Introduce device MTU concept")
Signed-off-by: André Martins <andre@cilium.io>

plugins/cilium-cni/00-cilium-cni.conf
@@ -1,4 +1,4 @@
 {
     "name": "cilium",
-    "type": "cilium-cni",
+    "type": "cilium-cni"
 }

plugins/cilium-cni/cni-install.sh
@@ -36,8 +36,7 @@ else
 	cat > ${CNI_CONF_NAME} <<EOF
 {
     "name": "cilium",
-    "type": "cilium-cni",
-    "mtu": ${MTU}
+    "type": "cilium-cni"
 }
 EOF
 	mkdir -p $(dirname $CILIUM_CNI_CONF)

test/runtime/connectivity.go
@@ -240,8 +240,7 @@ var _ = Describe("RuntimeValidatedConnectivityTest", func() {
 		It("Basic connectivity test", func() {
 			filename := "00-cilium-cni.conf"
 			cniConf := `{"name": "cilium",
-				"type": "cilium-cni",
-				"mtu": 1450}`
+				"type": "cilium-cni"}`
 			err := helpers.RenderTemplateToFile(filename, cniConf, os.ModePerm)
 			Expect(err).To(BeNil())
 