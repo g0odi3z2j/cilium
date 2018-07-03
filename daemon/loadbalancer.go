g0odi3z2j/cilium

commit 7548a1090f1d9ff6d72eaa938fa2d4c5049fb1e9

service: Introduce local service ID allocation
Global service ID allocation is only required when we perform load-balancing
with DSR (direct server return). We currently pay the cost of global service ID
allocation for all services. This introduces a new local service ID allocation
layer which becomes the new default. Global service ID allocation is only
enabled when the --lb flag is provided to the agent which indicates use of DSR.

Future versions can enable/disable this on a per service basis as well.

This dramatically reduces the cost of service ID allocation and removes the
requirement of a kvstore for normal load-balancing operations.

PASS: <autogenerated>:1: ServiceEtcdSuite.BenchmarkAllocation       1000           2822479 ns/op
PASS: <autogenerated>:1: ServiceConsulSuite.BenchmarkAllocation      200          10702444 ns/op
PASS: <autogenerated>:1: ServiceLocalSuite.BenchmarkAllocation   1000000              2625 ns/op

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/service/id.go
@@ -16,19 +16,52 @@ package service
 
 import (
 	"github.com/cilium/cilium/common/types"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
 // AcquireID acquires a service ID
 func AcquireID(l3n4Addr types.L3n4Addr, baseID uint32) (*types.L3n4AddrID, error) {
-	return acquireGlobalID(l3n4Addr, baseID)
+	log.WithField(logfields.L3n4Addr, logfields.Repr(l3n4Addr)).Debug("Resolving service")
+
+	if enableGlobalServiceIDs {
+		return acquireGlobalID(l3n4Addr, baseID)
+	}
+
+	return acquireLocalID(l3n4Addr, baseID)
 }
 
 // GetID returns the L3n4AddrID that belongs to the given id.
 func GetID(id uint32) (*types.L3n4AddrID, error) {
-	return getGlobalID(id)
+	if enableGlobalServiceIDs {
+		return getGlobalID(id)
+	}
+
+	return getLocalID(id)
 }
 
 // DeleteID deletes the L3n4AddrID belonging to the given id from the kvstore.
 func DeleteID(id uint32) error {
-	return deleteGlobalID(id)
+	log.WithField(logfields.L3n4AddrID, id).Debug("deleting L3n4Addr by ID")
+
+	if enableGlobalServiceIDs {
+		return deleteGlobalID(id)
+	}
+
+	return deleteLocalID(id)
+}
+
+func setIDSpace(next, max uint32) error {
+	if enableGlobalServiceIDs {
+		return setGlobalIDSpace(next, max)
+	}
+
+	return setLocalIDSpace(next, max)
+}
+
+func getMaxServiceID() (uint32, error) {
+	if enableGlobalServiceIDs {
+		return getGlobalMaxServiceID()
+	}
+
+	return getLocalMaxServiceID()
 }

common/types/loadbalancer.go
@@ -400,6 +400,13 @@ func (a *L3n4Addr) String() string {
 	return fmt.Sprintf("%s:%d", a.IP.String(), a.Port)
 }
 
+// StringID returns the L3n4Addr as string to be used for unique identification
+func (a *L3n4Addr) StringID() string {
+	// This does not include the protocol right now as the datapath does
+	// not include the protocol in the lookup of the service IP.
+	return a.String()
+}
+
 // DeepCopy returns a DeepCopy of the given L3n4Addr.
 func (a *L3n4Addr) DeepCopy() *L3n4Addr {
 	copyIP := make(net.IP, len(a.IP))

daemon/main.go
@@ -49,6 +49,7 @@ import (
 	"github.com/cilium/cilium/pkg/pidfile"
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/pprof"
+	"github.com/cilium/cilium/pkg/service"
 	"github.com/cilium/cilium/pkg/version"
 	"github.com/cilium/cilium/pkg/workloads"
 
@@ -547,6 +548,10 @@ func initEnv(cmd *cobra.Command) {
 		logfields.Path + ".RunDir": option.Config.RunDir,
 		logfields.Path + ".LibDir": option.Config.LibDir,
 	})
+
+	if option.Config.LBInterface != "" {
+		service.EnableGlobalServiceID(true)
+	}
 
 	option.Config.BpfDir = filepath.Join(option.Config.LibDir, defaults.BpfDir)
 	scopedLog = scopedLog.WithField(logfields.Path+".BPFDir", defaults.BpfDir)

pkg/service/config.go
@@ -1,0 +1,27 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package service
+
+var (
+	enableGlobalServiceIDs bool
+)
+
+// EnableGlobalServiceID enables or disables the use of global service IDs.
+// When global service IDs are enabled, a kvstore interaction is used to
+// allocate service IDs in the scope of the entire kvstore domain to enable
+// transfer of service ID context between nodes.
+func EnableGlobalServiceID(val bool) {
+	enableGlobalServiceIDs = val
+}

pkg/service/id_kvstore.go
@@ -25,6 +25,10 @@ import (
 	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
+var (
+	serviceKvstorePrefix = common.OperationalPath + "/ServicesV2/"
+)
+
 func updateL3n4AddrIDRef(id types.ServiceID, l3n4AddrID types.L3n4AddrID) error {
 	key := path.Join(common.ServiceIDKeyPath, strconv.FormatUint(uint64(id), 10))
 	return kvstore.Client().SetValue(key, l3n4AddrID)
@@ -35,7 +39,7 @@ func updateL3n4AddrIDRef(id types.ServiceID, l3n4AddrID types.L3n4AddrID) error 
 func gasNewL3n4AddrID(l3n4AddrID *types.L3n4AddrID, baseID uint32) error {
 	if baseID == 0 {
 		var err error
-		baseID, err = getMaxServiceID()
+		baseID, err = getGlobalMaxServiceID()
 		if err != nil {
 			return err
 		}
@@ -48,8 +52,6 @@ func gasNewL3n4AddrID(l3n4AddrID *types.L3n4AddrID, baseID uint32) error {
 // created for the given l3n4Addr. If baseID is different than 0, it tries to acquire that
 // ID to the l3n4Addr.
 func acquireGlobalID(l3n4Addr types.L3n4Addr, baseID uint32) (*types.L3n4AddrID, error) {
-	log.WithField(logfields.L3n4Addr, logfields.Repr(l3n4Addr)).Debug("Resolving service")
-
 	// Retrieve unique SHA256Sum for service
 	sha256Sum := l3n4Addr.SHA256Sum()
 	svcPath := path.Join(common.ServicesKeyPath, sha256Sum)
@@ -111,7 +113,6 @@ func getGlobalID(id uint32) (*types.L3n4AddrID, error) {
 
 // deleteGlobalID deletes the L3n4AddrID belonging to the given id from the kvstore.
 func deleteGlobalID(id uint32) error {
-	log.WithField(logfields.L3n4AddrID, id).Debug("deleting L3n4Addr by ID")
 	l3n4AddrID, err := getGlobalID(id)
 	if err != nil {
 		return err
@@ -161,7 +162,10 @@ func deleteL3n4AddrIDBySHA256(sha256Sum string) error {
 	return kvstore.Client().SetValue(svcPath, l3n4AddrID)
 }
 
-// getMaxServiceID returns the maximum possible free UUID stored in the kvstore.
-func getMaxServiceID() (uint32, error) {
+func getGlobalMaxServiceID() (uint32, error) {
 	return kvstore.Client().GetMaxID(common.LastFreeServiceIDKeyPath, common.FirstFreeServiceID)
+}
+
+func setGlobalIDSpace(next, max uint32) error {
+	return kvstore.Client().SetMaxID(common.LastFreeServiceIDKeyPath, next, max)
 }

pkg/service/id_local.go
@@ -1,0 +1,140 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package service
+
+import (
+	"fmt"
+
+	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/common/types"
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+var (
+	// mutex protects servicesID, services, nextID and maxID
+	mutex lock.RWMutex
+
+	// servicesID is a map of all services indexed by service ID
+	servicesID = map[uint32]*types.L3n4AddrID{}
+
+	// services is a map of all services indexed by L3n4Addr.StringID()
+	services = map[string]uint32{}
+
+	// nextID is the next service ID to attempt to allocate
+	nextID = common.FirstFreeServiceID
+
+	// maxID is the maximum service ID available for allocation
+	maxID = common.MaxSetOfServiceID
+)
+
+func newServiceID(svc types.L3n4Addr, id uint32) *types.L3n4AddrID {
+	return &types.L3n4AddrID{
+		L3n4Addr: svc,
+		ID:       types.ServiceID(id),
+	}
+}
+
+func addServiceID(svc types.L3n4Addr, id uint32) *types.L3n4AddrID {
+	svcID := newServiceID(svc, id)
+	servicesID[id] = svcID
+	services[svc.StringID()] = id
+
+	return svcID
+}
+
+func acquireLocalID(svc types.L3n4Addr, desiredID uint32) (*types.L3n4AddrID, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if svcID, ok := services[svc.StringID()]; ok {
+		if svc, ok := servicesID[svcID]; ok {
+			return svc, nil
+		}
+	}
+
+	if desiredID != 0 {
+		if _, ok := servicesID[desiredID]; !ok {
+			return addServiceID(svc, desiredID), nil
+		}
+	}
+
+	startingID := nextID
+	rollover := false
+	for {
+		if nextID == startingID && rollover {
+			break
+		} else if nextID == maxID {
+			nextID = common.FirstFreeServiceID
+			rollover = true
+		}
+
+		if _, ok := servicesID[nextID]; !ok {
+			svcID := addServiceID(svc, nextID)
+			nextID++
+			return svcID, nil
+		}
+
+		nextID++
+	}
+
+	return nil, fmt.Errorf("no service ID available")
+}
+
+func getLocalID(id uint32) (*types.L3n4AddrID, error) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+
+	if svc, ok := servicesID[id]; ok {
+		return svc, nil
+	}
+
+	return nil, nil
+}
+
+func deleteLocalID(id uint32) error {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if svc, ok := servicesID[id]; ok {
+		delete(servicesID, id)
+		delete(services, svc.StringID())
+	}
+
+	return nil
+}
+
+func setLocalIDSpace(next, max uint32) error {
+	mutex.Lock()
+	nextID = next
+	maxID = max
+	mutex.Unlock()
+
+	return nil
+}
+
+func getLocalMaxServiceID() (uint32, error) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	return nextID, nil
+}
+
+func resetLocalID() {
+	mutex.Lock()
+	servicesID = map[uint32]*types.L3n4AddrID{}
+	services = map[string]uint32{}
+	nextID = common.FirstFreeServiceID
+	maxID = common.MaxSetOfServiceID
+	mutex.Unlock()
+}

pkg/service/id_test.go
@@ -92,7 +92,7 @@ func (ds *ServiceTestSuite) TestServices(c *C) {
 	err = DeleteID(common.FirstFreeServiceID)
 	c.Assert(err, Equals, nil)
 
-	err = kvstore.Client().SetMaxID(common.LastFreeServiceIDKeyPath, common.FirstFreeServiceID, common.FirstFreeServiceID)
+	err = setIDSpace(common.FirstFreeServiceID, common.FirstFreeServiceID)
 	c.Assert(err, Equals, nil)
 
 	err = DeleteID(common.FirstFreeServiceID)
@@ -105,8 +105,7 @@ func (ds *ServiceTestSuite) TestServices(c *C) {
 	c.Assert(err, Equals, nil)
 	c.Assert(gotL3n4AddrID.ID, Equals, types.ServiceID(common.FirstFreeServiceID+1))
 
-	sha256sum := l3n4Addr2.SHA256Sum()
-	err = deleteL3n4AddrIDBySHA256(sha256sum)
+	err = DeleteID(uint32(gotL3n4AddrID.ID))
 	c.Assert(err, Equals, nil)
 	err = DeleteID(common.FirstFreeServiceID + 1)
 	c.Assert(err, Equals, nil)
@@ -135,10 +134,32 @@ func (ds *ServiceTestSuite) TestServices(c *C) {
 
 func (ds *ServiceTestSuite) TestGetMaxServiceID(c *C) {
 	lastID := uint32(common.MaxSetOfServiceID - 1)
-	err := kvstore.Client().SetValue(common.LastFreeServiceIDKeyPath, lastID)
-	c.Assert(err, Equals, nil)
+
+	if enableGlobalServiceIDs {
+		err := kvstore.Client().SetValue(common.LastFreeServiceIDKeyPath, lastID)
+		c.Assert(err, IsNil)
+	} else {
+		err := setIDSpace(lastID, common.MaxSetOfServiceID)
+		c.Assert(err, IsNil)
+	}
 
 	id, err := getMaxServiceID()
 	c.Assert(err, Equals, nil)
 	c.Assert(id, Equals, (common.MaxSetOfServiceID - 1))
+}
+
+func (ds *ServiceTestSuite) BenchmarkAllocation(c *C) {
+	addr := types.L3n4Addr{
+		IP:     net.IPv6loopback,
+		L4Addr: types.L4Addr{Port: 0, Protocol: "UDP"},
+	}
+
+	c.ResetTimer()
+	for i := 0; i < c.N; i++ {
+		addr.L4Addr.Port = uint16(c.N)
+		_, err := AcquireID(addr, 0)
+		c.Assert(err, IsNil)
+	}
+	c.StopTimer()
+
 }

pkg/service/service_test.go
@@ -17,7 +17,6 @@ package service
 import (
 	"testing"
 
-	"github.com/cilium/cilium/common"
 	"github.com/cilium/cilium/pkg/kvstore"
 
 	. "gopkg.in/check.v1"
@@ -37,13 +36,13 @@ type ServiceEtcdSuite struct {
 var _ = Suite(&ServiceEtcdSuite{})
 
 func (e *ServiceEtcdSuite) SetUpTest(c *C) {
+	EnableGlobalServiceID(true)
 	kvstore.SetupDummy("etcd")
-	kvstore.DeletePrefix(common.OperationalPath)
-	kvstore.DeletePrefix(kvstore.BaseKeyPrefix)
+	kvstore.DeletePrefix(serviceKvstorePrefix)
 }
 
 func (e *ServiceEtcdSuite) TearDownTest(c *C) {
-	// FIXME: Cleanup test prefix
+	kvstore.DeletePrefix(serviceKvstorePrefix)
 	kvstore.Close()
 }
 
@@ -54,12 +53,27 @@ type ServiceConsulSuite struct {
 var _ = Suite(&ServiceConsulSuite{})
 
 func (e *ServiceConsulSuite) SetUpTest(c *C) {
+	EnableGlobalServiceID(true)
 	kvstore.SetupDummy("consul")
-	kvstore.DeletePrefix(common.OperationalPath)
-	kvstore.DeletePrefix(kvstore.BaseKeyPrefix)
+	kvstore.DeletePrefix(serviceKvstorePrefix)
 }
 
 func (e *ServiceConsulSuite) TearDownTest(c *C) {
-	// FIXME: Cleanup test prefix
+	kvstore.DeletePrefix(serviceKvstorePrefix)
 	kvstore.Close()
+}
+
+type ServiceLocalSuite struct {
+	ServiceTestSuite
+}
+
+var _ = Suite(&ServiceLocalSuite{})
+
+func (e *ServiceLocalSuite) SetUpTest(c *C) {
+	EnableGlobalServiceID(false)
+	resetLocalID()
+}
+
+func (e *ServiceLocalSuite) TearDownTest(c *C) {
+	resetLocalID()
 }

commit 194c00a68f8fcb872f24e521b79a16dc9bce35d6

service: Fix local service ID restore
Ensuring that the service ID is restored was taken care of by the state in the
kvstore. When using local service ID allocation, IDs were always allocated from
scratch as the existing service ID was not passed in as desired service ID.

Fixes: 7548a1090f1 ("service: Introduce local service ID allocation")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/loadbalancer.go
@@ -573,9 +573,7 @@ func (d *Daemon) SyncLBMap() error {
 	// are modifying the BPF maps, and calling Dump on a Map RLocks the maps.
 	log.Debug("iterating over services read from BPF LB Map and seeing if they have the same ID set in the KV store")
 	for _, svc := range newSVCList {
-		// Check if the services read from the lbmap have the same ID set in the
-		// KVStore.
-		kvL3n4AddrID, err := service.AcquireID(svc.FE.L3n4Addr, 0)
+		kvL3n4AddrID, err := service.RestoreID(svc.FE.L3n4Addr, uint32(svc.FE.ID))
 		if err != nil {
 			log.WithError(err).WithFields(logrus.Fields{
 				logfields.L3n4Addr: logfields.Repr(svc.FE.L3n4Addr),

pkg/service/id.go
@@ -30,6 +30,20 @@ func AcquireID(l3n4Addr types.L3n4Addr, baseID uint32) (*types.L3n4AddrID, error
 	return acquireLocalID(l3n4Addr, baseID)
 }
 
+// RestoreID restores  previously used service ID
+func RestoreID(l3n4Addr types.L3n4Addr, baseID uint32) (*types.L3n4AddrID, error) {
+	log.WithField(logfields.L3n4Addr, logfields.Repr(l3n4Addr)).Debug("Restoring service")
+
+	if enableGlobalServiceIDs {
+		// global service IDs do not require to pass in the existing
+		// service ID. The global state will guarantee that the same
+		// service will resolve to the same service ID again.
+		return acquireGlobalID(l3n4Addr, 0)
+	}
+
+	return acquireLocalID(l3n4Addr, baseID)
+}
+
 // GetID returns the L3n4AddrID that belongs to the given id.
 func GetID(id uint32) (*types.L3n4AddrID, error) {
 	if enableGlobalServiceIDs {