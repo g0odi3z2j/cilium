g0odi3z2j/cilium

commit aa4b5e8f9af44e6e31244bc90b25544f43161e34

bpf: If available, use ipcache for encapsulation
This is a first step away from requiring understanding of the PodCIDR in the
datapath. The tunnel map lookup is replaced with the ipcache map lookup if
available. If the entry is populated, the cost is the same.

This will allow to no longer require understanding of the PodCIDR to build a
tunnel mesh but directly base it off the PodIP and HostIP.

The ipcache is extended to include the hostIP and the BPF map is automatically
populated with it.

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=a3d989cab90a413a9f379d7bbc6aa18fc75ca849
+GO_BINDATA_SHA1SUM=a5d85e2077ef6d50161230f8f13b6a0882e73946
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

bpf/bpf_lxc.c
@@ -95,6 +95,7 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	void *data, *data_end;
 	union v6addr *daddr, orig_dip;
 	uint16_t dstID = WORLD_ID;
+	__u32 tunnel_endpoint = 0;
 
 	if (unlikely(!is_valid_lxc_src_mac(eth)))
 		return DROP_INVALID_SMAC;
@@ -166,8 +167,20 @@ skip_service_lookup:
 
 	/* Determine the destination category for policy fallback. */
 	BPF_V6(router_ip, ROUTER_IP);
-	if (ipv6_match_prefix_64(daddr, &router_ip))
-		dstID = CLUSTER_ID;
+
+	if (1) {
+		struct remote_endpoint_info *info;
+
+		info = lookup_ip6_remote_endpoint(&orig_dip);
+		if (info != NULL) {
+			dstID = info->sec_label;
+			tunnel_endpoint = info->tunnel_endpoint;
+		} else if (ipv6_match_prefix_64(daddr, &router_ip))
+			dstID = CLUSTER_ID;
+
+		cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
+			   orig_dip.p4, dstID);
+	}
 
 	/* If the packet is in the establishing direction and it's destined
 	 * within the cluster, it must match policy or be dropped. If it's
@@ -268,7 +281,9 @@ skip_service_lookup:
 
 	/* The packet goes to a peer not managed by this agent instance */
 #ifdef ENCAP_IFINDEX
-	if (1) {
+	if (tunnel_endpoint) {
+		return encap_and_redirect_with_nodeid(skb, tunnel_endpoint, SECLABEL);
+	} else {
 		/* FIXME GH-1391: Get rid of the initializer */
 		struct endpoint_key key = {};
 
@@ -406,6 +421,7 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb)
 	struct ct_state ct_state = {};
 	__be32 orig_dip;
 	uint16_t dstID = WORLD_ID;
+	__u32 tunnel_endpoint = 0;
 
 	if (!revalidate_data(skb, &data, &data_end, &ip4))
 		return DROP_INVALID;
@@ -466,14 +482,24 @@ skip_service_lookup:
 	forwarding_reason = ret;
 
 	/* Determine the destination category for policy fallback. */
-	if ((orig_dip & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE)
-		dstID = CLUSTER_ID;
+	if (1) {
+		struct remote_endpoint_info *info;
+
+		info = lookup_ip4_remote_endpoint(orig_dip);
+		if (info != NULL) {
+			dstID = info->sec_label;
+			tunnel_endpoint = info->tunnel_endpoint;
+		} else if ((orig_dip & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE)
+			dstID = CLUSTER_ID;
+
+		cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED4 : DBG_IP_ID_MAP_FAILED4,
+			   orig_dip, dstID);
+	}
 
 	/* If the packet is in the establishing direction and it's destined
 	 * within the cluster, it must match policy or be dropped. If it's
 	 * bound for the host/outside, perform the CIDR policy check. */
-	verdict = policy_can_egress4(skb, &tuple, dstID,
-				     ipv4_ct_tuple_get_daddr(&tuple));
+	verdict = policy_can_egress4(skb, &tuple, dstID, ipv4_ct_tuple_get_daddr(&tuple));
 	if (ret != CT_REPLY && ret != CT_RELATED && verdict < 0) {
 		/* If the connection was previously known and packet is now
 		 * denied, remove the connection tracking entry */
@@ -563,7 +589,9 @@ skip_service_lookup:
 	}
 
 #ifdef ENCAP_IFINDEX
-	if (1) {
+	if (tunnel_endpoint) {
+		return encap_and_redirect_with_nodeid(skb, tunnel_endpoint, SECLABEL);
+	} else {
 		/* FIXME GH-1391: Get rid of the initializer */
 		struct endpoint_key key = {};
 

bpf/bpf_netdev.c
@@ -171,6 +171,9 @@ static inline int rewrite_dmac_to_host(struct __sk_buff *skb)
 
 static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 {
+#ifdef ENCAP_IFINDEX
+	struct remote_endpoint_info *info;
+#endif
 	union v6addr node_ip = { };
 	void *data, *data_end;
 	struct ipv6hdr *ip6;
@@ -235,7 +238,10 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 
 #ifdef ENCAP_IFINDEX
 	dst = (union v6addr *) &ip6->daddr;
-	if (likely(ipv6_match_prefix_96(dst, &node_ip))) {
+	info = ipcache_lookup6(&cilium_ipcache, dst, V6_CACHE_KEY_LEN);
+	if (info != NULL && info->tunnel_endpoint != 0) {
+		return encap_and_redirect_with_nodeid(skb, info->tunnel_endpoint, flowlabel);
+	} else if (likely(ipv6_match_prefix_96(dst, &node_ip))) {
 		struct endpoint_key key = {};
 		int ret;
 
@@ -338,6 +344,9 @@ reverse_proxy(struct __sk_buff *skb, int l4_off, struct iphdr *ip4, __u8 nh)
 
 static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 {
+#ifdef ENCAP_IFINDEX
+	struct remote_endpoint_info *info;
+#endif
 	struct ipv4_ct_tuple tuple = {};
 	struct endpoint_info *ep;
 	void *data, *data_end;
@@ -385,8 +394,10 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 	}
 
 #ifdef ENCAP_IFINDEX
-	/* Check if destination is within our cluster prefix */
-	if ((ip4->daddr & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE) {
+	info = ipcache_lookup4(&cilium_ipcache, ip4->daddr, V4_CACHE_KEY_LEN);
+	if (info != NULL && info->tunnel_endpoint != 0) {
+		return encap_and_redirect_with_nodeid(skb, info->tunnel_endpoint, secctx);
+	} else if ((ip4->daddr & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE) {
 		/* IPv4 lookup key: daddr & IPV4_MASK */
 		struct endpoint_key key = {};
 		int ret;

bpf/lib/common.h
@@ -170,7 +170,8 @@ struct endpoint_info {
 
 struct remote_endpoint_info {
 	__u16		sec_label;
-	__u16		pad[3];
+	__u16		pad;
+	__u32		tunnel_endpoint;
 };
 
 struct policy_key {

bpf/lib/encap.h
@@ -22,19 +22,13 @@
 #include "dbg.h"
 
 #ifdef ENCAP_IFINDEX
-static inline int __inline__ encap_and_redirect(struct __sk_buff *skb, struct endpoint_key *k,
-						__u32 seclabel)
+static inline int __inline__ encap_and_redirect_with_nodeid(struct __sk_buff *skb, __u32 tunnel_endpoint, __u32 seclabel)
 {
-	struct endpoint_key *tunnel;
 	struct bpf_tunnel_key key = {};
 	__u32 node_id;
 	int ret;
 
-	if ((tunnel = map_lookup_elem(&cilium_tunnel_map, k)) == NULL) {
-		return DROP_NO_TUNNEL_ENDPOINT;
-	}
-
-	node_id = bpf_htonl(tunnel->ip4);
+	node_id = bpf_htonl(tunnel_endpoint);
 	key.tunnel_id = seclabel;
 	key.remote_ipv4 = node_id;
 
@@ -49,5 +43,16 @@ static inline int __inline__ encap_and_redirect(struct __sk_buff *skb, struct en
 	return redirect(ENCAP_IFINDEX, 0);
 }
 
+static inline int __inline__ encap_and_redirect(struct __sk_buff *skb, struct endpoint_key *k,
+						__u32 seclabel)
+{
+	struct endpoint_key *tunnel;
+
+	if ((tunnel = map_lookup_elem(&cilium_tunnel_map, k)) == NULL) {
+		return DROP_NO_TUNNEL_ENDPOINT;
+	}
+
+	return encap_and_redirect_with_nodeid(skb, tunnel->ip4, seclabel);
+}
 #endif /* ENCAP_IFINDEX */
 #endif /* __LIB_ENCAP_H_ */

bpf/lib/eps.h
@@ -45,12 +45,13 @@ lookup_ip4_endpoint(struct iphdr *ip4)
 	return map_lookup_elem(&cilium_lxc, &key);
 }
 
-#if (defined POLICY_EGRESS || defined POLICY_INGRESS) && defined LXC_ID
 /* IPCACHE_STATIC_PREFIX gets sizeof non-IP, non-prefix part of ipcache_key */
 #define IPCACHE_STATIC_PREFIX							\
 	(8 * (sizeof(struct ipcache_key) - sizeof(struct bpf_lpm_trie_key)	\
 	      - sizeof(union v6addr)))
 #define IPCACHE_PREFIX_LEN(PREFIX) (IPCACHE_STATIC_PREFIX + PREFIX)
+
+#define V6_CACHE_KEY_LEN IPCACHE_PREFIX_LEN(sizeof(union v6addr)*8)
 
 static __always_inline struct remote_endpoint_info *
 ipcache_lookup6(struct bpf_elf_map *map, union v6addr *addr, __u32 prefix)
@@ -64,6 +65,8 @@ ipcache_lookup6(struct bpf_elf_map *map, union v6addr *addr, __u32 prefix)
 	return map_lookup_elem(map, &key);
 }
 
+#define V4_CACHE_KEY_LEN IPCACHE_PREFIX_LEN(sizeof(__u32)*8)
+
 static __always_inline struct remote_endpoint_info *
 ipcache_lookup4(struct bpf_elf_map *map, __be32 addr, __u32 prefix)
 {
@@ -76,6 +79,7 @@ ipcache_lookup4(struct bpf_elf_map *map, __be32 addr, __u32 prefix)
 	return map_lookup_elem(map, &key);
 }
 
+#if defined LXC_ID
 #ifndef HAVE_LPM_MAP_TYPE
 /* Define a function with the following NAME which iterates through PREFIXES
  * (a list of integers ordered from high to low representing prefix length),
@@ -104,14 +108,11 @@ LPM_LOOKUP_FN(lookup_ip4_remote_endpoint, __be32, IPCACHE4_PREFIXES,
 	      cilium_ipcache, ipcache_lookup4)
 #undef LPM_LOOKUP_FN
 #else /* HAVE_LPM_MAP_TYPE */
-#define V6_CACHE_KEY_LEN (sizeof(union v6addr)*8)
 #define lookup_ip6_remote_endpoint(addr) \
 	ipcache_lookup6(&cilium_ipcache, addr, V6_CACHE_KEY_LEN)
-#define V4_CACHE_KEY_LEN (sizeof(__u32)*8)
 #define lookup_ip4_remote_endpoint(addr) \
 	ipcache_lookup4(&cilium_ipcache, addr, V4_CACHE_KEY_LEN)
 #endif /* HAVE_LPM_MAP_TYPE */
-
-#endif /* POLICY_EGRESS */
+#endif /* LXC_ID */
 
 #endif /* __LIB_EPS_H_ */

bpf/lib/maps.h
@@ -107,8 +107,6 @@ struct bpf_elf_map __section_maps cilium_tunnel_map = {
 #define LPM_MAP_TYPE BPF_MAP_TYPE_HASH
 #endif
 
-#if defined POLICY_INGRESS || defined POLICY_EGRESS
-
 #ifndef HAVE_LPM_MAP_TYPE
 /* Define a function with the following NAME which iterates through PREFIXES
  * (a list of integers ordered from high to low representing prefix length),
@@ -130,13 +128,9 @@ _Pragma("unroll")							\
 }
 #endif /* HAVE_LPM_MAP_TYPE */
 
-#endif /* POLICY_INGRESS || POLICY_EGRESS */
-
-#if defined(POLICY_INGRESS) || defined(POLICY_EGRESS)
 #ifndef SKIP_UNDEF_LPM_LOOKUP_FN
 #undef LPM_LOOKUP_FN
 #endif
-#endif /* POLICY_INGRESS || POLICY_EGRESS */
 
 struct ipcache_key {
 	struct bpf_lpm_trie_key lpm_key;

bpf/lib/policy.h
@@ -216,41 +216,22 @@ policy_can_egress(struct __sk_buff *skb, __u16 identity, __u16 dport, __u8 proto
 
 static inline int policy_can_egress6(struct __sk_buff *skb,
 				     struct ipv6_ct_tuple *tuple,
-				     __u16 default_identity,
-				     union v6addr *daddr)
+				     __u16 identity, union v6addr *daddr)
 {
 #ifdef DROP_ALL
 	return DROP_POLICY;
 #else
-	struct remote_endpoint_info *info;
-	__u16 identity = default_identity;
-
-	info = lookup_ip6_remote_endpoint(daddr);
-	if (info)
-		identity = info->sec_label;
-	cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
-		   daddr->p4, identity);
-
 	return policy_can_egress(skb, identity, tuple->dport, tuple->nexthdr);
 #endif /* DROP_ALL */
 }
 
 static inline int policy_can_egress4(struct __sk_buff *skb,
 				     struct ipv4_ct_tuple *tuple,
-				     __u16 default_identity, __be32 daddr)
+				     __u16 identity, __be32 daddr)
 {
 #ifdef DROP_ALL
 	return DROP_POLICY;
 #else
-	struct remote_endpoint_info *info;
-	__u16 identity = default_identity;
-
-	info = lookup_ip4_remote_endpoint(daddr);
-	if (info)
-		identity = info->sec_label;
-	cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED4 : DBG_IP_ID_MAP_FAILED4,
-		   daddr, identity);
-
 	return policy_can_egress(skb, identity, tuple->dport, tuple->nexthdr);
 #endif /* DROP_ALL */
 }
@@ -259,7 +240,7 @@ static inline int policy_can_egress4(struct __sk_buff *skb,
 
 static inline int
 policy_can_egress6(struct __sk_buff *skb, struct ipv6_ct_tuple *tuple,
-		   __u16 default_identity, union v6addr *daddr)
+		   __u16 identity, union v6addr *daddr)
 {
 #ifdef DROP_ALL
 	return DROP_POLICY;
@@ -270,7 +251,7 @@ policy_can_egress6(struct __sk_buff *skb, struct ipv6_ct_tuple *tuple,
 
 static inline int
 policy_can_egress4(struct __sk_buff *skb, struct ipv4_ct_tuple *tuple,
-		   __u16 default_identity, __be32 daddr)
+		   __u16 identity, __be32 daddr)
 {
 #ifdef DROP_ALL
 	return DROP_POLICY;

commit 3c5279989fbcc7649677c8e62b9d9e0707636421

bpf: Support fall back to routing layer for overlay ingress
Commit aa4b5e8f9af ("bpf: If available, use ipcache for encapsulation")
introduced pod resource based encapsulation setup. A side effect of this is
that this enabled encapsulation based and identity enabled multi host
networking for non Cilium managed pods as well.

However, the overlay receive code is currently dropping all traffic to
destinations other than local endpoints or host IPs. This commits changes this
to fall back to the regular routing layer to re-establish connectivity to pods
connected through networking layers other than Cilium such as a bridge.

Fixes: aa4b5e8f9af ("bpf: If available, use ipcache for encapsulation")

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/bpf_overlay.c
@@ -64,8 +64,6 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 
 		l4_off = l3_off + hdrlen;
 		return ipv6_local_delivery(skb, l3_off, l4_off, key.tunnel_id, ip6, nexthdr, ep, METRIC_INGRESS);
-	} else {
-		return DROP_NON_LOCAL;
 	}
 
 to_host:
@@ -115,8 +113,6 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 			goto to_host;
 
 		return ipv4_local_delivery(skb, ETH_HLEN, l4_off, key.tunnel_id, ip4, ep, METRIC_INGRESS);
-	} else {
-		return DROP_NON_LOCAL;
 	}
 
 to_host:

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=6b79fafa0ac79c56b56980ce6de92a02cdb45438
+GO_BINDATA_SHA1SUM=8858949a2e86dbe5a688596b98e23f8ba34fbd19
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 