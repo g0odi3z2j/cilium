g0odi3z2j/cilium

commit b1897cc947457fe5b97bd6cf9a8766da395de7c6

endpoint: Fix sidecar proxy deadlock during BPF generation
Signed-off-by: Romain Lenglet <romain@covalent.io>

pkg/endpoint/bpf.go
@@ -331,26 +331,33 @@ func (e *Endpoint) addNewRedirectsFromMap(owner Owner, m policy.L4PolicyMap, des
 
 	for _, l4 := range m {
 		if l4.IsRedirect() {
-			// Ignore the redirect if the proxy is running in a sidecar container.
-			if l4.L7Parser == policy.ParserTypeHTTP && e.hasSidecarProxy {
-				continue
+			var redirectPort uint16
+			var err error
+			// Only create a redirect if the proxy is NOT running in a sidecar
+			// container. If running in a sidecar container, just allow traffic
+			// to the port at L4 by setting the proxy port to 0.
+			if !e.hasSidecarProxy || l4.L7Parser != policy.ParserTypeHTTP {
+				redirectPort, err = owner.UpdateProxyRedirect(e, &l4, proxyWaitGroup)
+				if err != nil {
+					return err
+				}
+
+				proxyID := e.ProxyID(&l4)
+				if e.realizedRedirects == nil {
+					e.realizedRedirects = make(map[string]uint16)
+				}
+				e.realizedRedirects[proxyID] = redirectPort
+				desiredRedirects[proxyID] = true
+
+				// Update the endpoint API model to report that Cilium manages a
+				// redirect for that port.
+				e.proxyStatisticsMutex.Lock()
+				proxyStats := e.getProxyStatisticsLocked(string(l4.L7Parser), uint16(l4.Port), l4.Ingress)
+				proxyStats.AllocatedProxyPort = int64(redirectPort)
+				e.proxyStatisticsMutex.Unlock()
 			}
 
-			redirectPort, err := owner.UpdateProxyRedirect(e, &l4, proxyWaitGroup)
-			if err != nil {
-				return err
-			}
-
-			proxyID := e.ProxyID(&l4)
-			if e.realizedRedirects == nil {
-				e.realizedRedirects = make(map[string]uint16)
-			}
-			e.realizedRedirects[proxyID] = redirectPort
-			desiredRedirects[proxyID] = true
-
-			// Update the proxy port in the policy map.
-			// This may override proxy ports written by
-			// e.computeDesiredPolicyMapState.
+			// Set the proxy port in the policy map.
 			var direction policymap.TrafficDirection
 			if l4.Ingress {
 				direction = policymap.Ingress
@@ -361,13 +368,6 @@ func (e *Endpoint) addNewRedirectsFromMap(owner Owner, m policy.L4PolicyMap, des
 			for _, keyFromFilter := range keysFromFilter {
 				e.desiredMapState[keyFromFilter] = PolicyMapStateEntry{ProxyPort: redirectPort}
 			}
-
-			// Update the endpoint API model to report that Cilium manages a
-			// redirect for that port.
-			e.proxyStatisticsMutex.Lock()
-			proxyStats := e.getProxyStatisticsLocked(string(l4.L7Parser), uint16(l4.Port), l4.Ingress)
-			proxyStats.AllocatedProxyPort = int64(redirectPort)
-			e.proxyStatisticsMutex.Unlock()
 		}
 	}
 	return nil
@@ -388,22 +388,6 @@ func (e *Endpoint) addNewRedirects(owner Owner, m *policy.L4Policy, proxyWaitGro
 		return desiredRedirects, fmt.Errorf("Unable to allocate egress redirects: %s", err)
 	}
 	return desiredRedirects, nil
-}
-
-func (e *Endpoint) removeOldRedirectPolicyMapEntries(desiredRedirects map[string]bool) {
-	for id, redirectPort := range e.realizedRedirects {
-		// Remove only the redirects that are not required.
-		if desiredRedirects[id] {
-			continue
-		}
-		// Clear all occurrences of this proxy port in the policy map.
-		for k, v := range e.desiredMapState {
-			if v.ProxyPort == redirectPort {
-				v.ProxyPort = 0
-				e.desiredMapState[k] = v
-			}
-		}
-	}
 }
 
 // Must be called with endpoint.Mutex held.
@@ -509,10 +493,6 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 		return e.nextPolicyRevision, compilationExecuted, nil
 	}
 
-	// The set of IDs of proxy redirects that are required to implement the
-	// policy.
-	var desiredRedirects map[string]bool
-
 	// Anything below this point must be reverted upon failure as we are
 	// changing live BPF maps
 	createdPolicyMap := false
@@ -547,6 +527,11 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 		}
 	}
 
+	// Set up a context to wait for proxy completions.
+	completionCtx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	proxyWaitGroup := completion.NewWaitGroup(completionCtx)
+	defer cancel()
+
 	// Only generate & populate policy map if a security identity is set up for
 	// this endpoint.
 	if e.SecurityIdentity != nil {
@@ -560,53 +545,22 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 			return 0, compilationExecuted, fmt.Errorf("unable to regenerate policy for '%s': %s", e.PolicyMap.String(), err)
 		}
 
-		// Now that policy has been regenerated, set up a context to
-		// wait for proxy completions.
-		completionCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		proxyWaitGroup := completion.NewWaitGroup(completionCtx)
-		defer func() {
-			cancel()
-		}()
-
-		// Walk the L4Policy for ports that require
-		// an L7 redirect and add them to the endpoint.
-		// Also update the desired policy map state to set the newly allocated proxy ports.
-		if e.DesiredL4Policy != nil {
-			desiredRedirects, err = e.addNewRedirects(owner, e.DesiredL4Policy, proxyWaitGroup)
-			if err != nil {
-				e.Mutex.Unlock()
-				return 0, compilationExecuted, err
-			}
-		}
-
-		// Update policies after adding redirects, otherwise we will not wait for
-		// acks for the first policy updates for the first added redirects.
-		if err = e.updateNetworkPolicy(owner, proxyWaitGroup); err != nil {
-			e.Mutex.Unlock()
-			return 0, compilationExecuted, err
-		}
-
-		// To avoid traffic loss, wait for the proxy to be ready to accept
-		// traffic on new redirect ports, before we update the policy map that
-		// will redirect traffic to those ports.
-		err = e.WaitForProxyCompletions(proxyWaitGroup)
-		if err != nil {
-			return 0, compilationExecuted, fmt.Errorf("Error while configuring proxy redirects: %s", err)
-		}
-
 		// Synchronously try to update PolicyMap for this endpoint. If any
 		// part of updating the PolicyMap fails, bail out and do not generate
 		// BPF. Unfortunately, this means that the map will be in an inconsistent
 		// state with the current program (if it exists) for this endpoint.
 		// GH-3897 would fix this by creating a new map to do an atomic swap
 		// with the old one.
-		//
-		// This must be done after adding the new redirects, because the newly
-		// allocated proxy ports have to be updated in the policy map.
 		err := e.syncPolicyMap()
 		if err != nil {
 			e.Mutex.Unlock()
 			return 0, compilationExecuted, fmt.Errorf("unable to regenerate policy because PolicyMap synchronization failed: %s", err)
+		}
+
+		// Configure the new network policy with the proxies.
+		if err = e.updateNetworkPolicy(owner, proxyWaitGroup); err != nil {
+			e.Mutex.Unlock()
+			return 0, compilationExecuted, err
 		}
 	}
 
@@ -667,8 +621,26 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 		e.Mutex.RUnlock()
 	}
 
-	// Remove all obsolete redirects from the desired policy map.
-	e.removeOldRedirectPolicyMapEntries(desiredRedirects)
+	e.Mutex.Lock()
+	// Walk the L4Policy to add new redirects and update the desired policy map
+	// state to set the newly allocated proxy ports.
+	var desiredRedirects map[string]bool
+	if e.DesiredL4Policy != nil {
+		desiredRedirects, err = e.addNewRedirects(owner, e.DesiredL4Policy, proxyWaitGroup)
+		if err != nil {
+			e.Mutex.Unlock()
+			return 0, compilationExecuted, err
+		}
+	}
+	// At this point, traffic is no longer redirected to the proxy for
+	// now-obsolete redirects, since we synced the updated policy map above.
+	// It's now safe to remove the redirects from the proxy's configuration.
+	e.removeOldRedirects(owner, desiredRedirects, proxyWaitGroup)
+	e.Mutex.Unlock()
+	err = e.WaitForProxyCompletions(proxyWaitGroup)
+	if err != nil {
+		return 0, compilationExecuted, fmt.Errorf("Error while configuring proxy redirects: %s", err)
+	}
 
 	// Synchronously try to update PolicyMap for this endpoint. If any
 	// part of updating the PolicyMap fails, bail out and do not generate
@@ -677,26 +649,12 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 	// GH-3897 would fix this by creating a new map to do an atomic swap
 	// with the old one.
 	//
-	// This must be done before removing the old redirects below, because the
-	// allocated proxy ports are to be cleared from the policy map first.
+	// This must be done after allocating the new redirects, to update the
+	// policy map with the new proxy ports.
 	err = e.syncPolicyMap()
 	if err != nil {
 		e.Mutex.Unlock()
 		return 0, compilationExecuted, fmt.Errorf("unable to regenerate policy because PolicyMap synchronization failed: %s", err)
-	}
-
-	// To avoid traffic loss, wait for the policy map to be updated before
-	// deleting obsolete redirects, to make sure no packets are redirected to
-	// those ports.
-	completionCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	proxyWaitGroup := completion.NewWaitGroup(completionCtx)
-	defer cancel()
-	e.Mutex.Lock()
-	e.removeOldRedirects(owner, desiredRedirects, proxyWaitGroup)
-	e.Mutex.Unlock()
-	err = e.WaitForProxyCompletions(proxyWaitGroup)
-	if err != nil {
-		return 0, compilationExecuted, fmt.Errorf("Error while deleting obsolete proxy redirects: %s", err)
 	}
 
 	// The last operation hooks the endpoint into the endpoint table and exposes it

pkg/endpoint/policy.go
@@ -136,20 +136,40 @@ func (e *Endpoint) computeDesiredL4PolicyMapEntries(keysToAdd PolicyMapState) {
 	for _, filter := range e.DesiredL4Policy.Ingress {
 		keysFromFilter := e.convertL4FilterToPolicyMapKeys(&filter, policymap.Ingress)
 		for _, keyFromFilter := range keysFromFilter {
-			// This proxy port may get overwritten later by
-			// e.addNewRedirectsFromMap. We don't know at this point about new
-			// redirect ports that are to be allocated.
-			keysToAdd[keyFromFilter] = PolicyMapStateEntry{ProxyPort: e.lookupRedirectPort(&filter)}
+			var proxyPort uint16
+			// Preserve the already-allocated proxy ports for redirects that
+			// already exist.
+			if filter.IsRedirect() {
+				proxyPort = e.lookupRedirectPort(&filter)
+				// If the currently allocated proxy port is 0, this is a new
+				// redirect, for which no port has been allocated yet. Ignore
+				// it for now. This will be configured by
+				// e.addNewRedirectsFromMap once the port has been allocated.
+				if proxyPort == 0 {
+					continue
+				}
+			}
+			keysToAdd[keyFromFilter] = PolicyMapStateEntry{ProxyPort: proxyPort}
 		}
 	}
 
 	for _, filter := range e.DesiredL4Policy.Egress {
 		keysFromFilter := e.convertL4FilterToPolicyMapKeys(&filter, policymap.Egress)
 		for _, keyFromFilter := range keysFromFilter {
-			// This proxy port may get overwritten later by
-			// e.addNewRedirectsFromMap. We don't know at this point about new
-			// redirect ports that are to be allocated.
-			keysToAdd[keyFromFilter] = PolicyMapStateEntry{ProxyPort: e.lookupRedirectPort(&filter)}
+			var proxyPort uint16
+			// Preserve the already-allocated proxy ports for redirects that
+			// already exist.
+			if filter.IsRedirect() {
+				proxyPort = e.lookupRedirectPort(&filter)
+				// If the currently allocated proxy port is 0, this is a new
+				// redirect, for which no port has been allocated yet. Ignore
+				// it for now. This will be configured by
+				// e.addNewRedirectsFromMap once the port has been allocated.
+				if proxyPort == 0 {
+					continue
+				}
+			}
+			keysToAdd[keyFromFilter] = PolicyMapStateEntry{ProxyPort: proxyPort}
 		}
 	}
 	return

commit dcdb5df79f70afc8b71a2731dcb36df0895065d3

endpoint: Fix deadlock and missing lock during syncPolicyMap()
Fixes a deadlock in the error path of regenerateBPF() and fixes a missing lock
on the endpoint while e.syncPolicyMap() is called which writes to the endpoint
structure.

Fixes: #4821
Fixes: b1897cc9474 ("endpoint: Fix sidecar proxy deadlock during BPF generation")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/endpoint/bpf.go
@@ -642,6 +642,13 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 		return 0, compilationExecuted, fmt.Errorf("Error while configuring proxy redirects: %s", err)
 	}
 
+	e.Mutex.Lock()
+	defer e.Mutex.Unlock()
+
+	if e.IsDisconnecting() {
+		return 0, compilationExecuted, fmt.Errorf("endpoint was removed while waiting for proxy to be configured")
+	}
+
 	// Synchronously try to update PolicyMap for this endpoint. If any
 	// part of updating the PolicyMap fails, bail out and do not generate
 	// BPF. Unfortunately, this means that the map will be in an inconsistent
@@ -653,7 +660,6 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir, reason string) (uint64, boo
 	// policy map with the new proxy ports.
 	err = e.syncPolicyMap()
 	if err != nil {
-		e.Mutex.Unlock()
 		return 0, compilationExecuted, fmt.Errorf("unable to regenerate policy because PolicyMap synchronization failed: %s", err)
 	}
 

pkg/endpoint/endpoint.go
@@ -2537,3 +2537,14 @@ func (e *Endpoint) syncPolicyMapController() {
 		},
 	)
 }
+
+// IsDisconnecting returns true if the endpoint is being disconnected or
+// already disconnected
+//
+// This function must be called after re-aquiring the endpoint mutex to verify
+// that the endpoint has not been removed in the meantime.
+//
+// endpoint.Mutex must be held
+func (e *Endpoint) IsDisconnecting() bool {
+	return e.state == StateDisconnected || e.state == StateDisconnecting
+}