g0odi3z2j/cilium

commit 652a6ff6bcbc24752b50d97a3a27dac861df969f

ipcache: Populate ipcache based on Pod events
This provides early population of the ipcache based on pod events. This enables
to build a tunnel mesh and provide basic network connectivity prior to
establishing the kvstore connectivity.

The ipcache entry is extended with the concept of a source with the following
sources defined:
 - kubernetes: entries derived from pods, can be overwritten by everyone else
 - kvstore: entries derived from kvstore, can be overwritten by agent entries
 - agent: bootstrap entries, cannot be overwritten by kvstore or kubernetes

An ipcache entry can start with kubernetes being the source and then transition
to be owned by the kvstore. Depending on whether the transition happened, the
entry will be removed by either the kvstore deletion event or the pod removal
event.

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/k8s_watcher.go
@@ -28,6 +28,8 @@ import (
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpointmanager"
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/k8s"
 	k8sUtils "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/utils"
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
@@ -453,6 +455,15 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 		&v1.Pod{},
 		reSyncPeriod,
 		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(newObj interface{}) {
+				metrics.SetTSValue(metrics.EventTSK8s, time.Now())
+				if newK8sPod := copyObjToV1Pod(newObj); newK8sPod != nil {
+					serPods.Enqueue(func() error {
+						d.addK8sPodV1(newK8sPod)
+						return nil
+					}, serializer.NoRetry)
+				}
+			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
 				metrics.SetTSValue(metrics.EventTSK8s, time.Now())
 				if oldK8sPod := copyObjToV1Pod(oldObj); oldK8sPod != nil {
@@ -462,6 +473,15 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 							return nil
 						}, serializer.NoRetry)
 					}
+				}
+			},
+			DeleteFunc: func(oldObj interface{}) {
+				metrics.SetTSValue(metrics.EventTSK8s, time.Now())
+				if oldK8sPod := copyObjToV1Pod(oldObj); oldK8sPod != nil {
+					serPods.Enqueue(func() error {
+						d.deleteK8sPodV1(oldK8sPod)
+						return nil
+					}, serializer.NoRetry)
 				}
 			},
 		},
@@ -1465,10 +1485,101 @@ func (d *Daemon) updateCiliumNetworkPolicyV2(ciliumV2Store cache.Store,
 	d.addCiliumNetworkPolicyV2(ciliumV2Store, newRuleCpy)
 }
 
+func (d *Daemon) updatePodHostIP(pod *v1.Pod) (bool, error) {
+	if pod.Spec.HostNetwork {
+		return true, fmt.Errorf("pod is using host networking")
+	}
+
+	hostIP := net.ParseIP(pod.Status.HostIP)
+	if hostIP == nil {
+		return true, fmt.Errorf("no/invalid HostIP: %s", pod.Status.HostIP)
+	}
+
+	podIP := net.ParseIP(pod.Status.PodIP)
+	if podIP == nil {
+		return true, fmt.Errorf("no/invalid PodIP: %s", pod.Status.PodIP)
+	}
+
+	updated := ipcache.IPIdentityCache.Upsert(pod.Status.PodIP, ipcache.Identity{
+		ID:     identity.ReservedIdentityCluster,
+		Source: ipcache.FromKubernetes,
+	})
+	if !updated {
+		return true, fmt.Errorf("ipcache entry owned by kvstore or agent")
+	}
+
+	id := identity.IPIdentityPair{
+		IP:     podIP,
+		HostIP: hostIP,
+		ID:     identity.ReservedIdentityCluster,
+	}
+
+	for _, listener := range d.ipcacheListeners {
+		listener.OnIPIdentityCacheChange(ipcache.Upsert, nil, id)
+	}
+
+	return false, nil
+}
+
+func (d *Daemon) deletePodHostIP(pod *v1.Pod) (bool, error) {
+	if pod.Spec.HostNetwork {
+		return true, fmt.Errorf("pod is using host networking")
+	}
+
+	podIP := net.ParseIP(pod.Status.PodIP)
+	if podIP == nil {
+		return true, fmt.Errorf("no/invalid PodIP: %s", pod.Status.PodIP)
+	}
+
+	// a small race condition exists here as deletion could occur in
+	// parallel based on another event but it doesn't matter as the
+	// identity is going away
+	id, exists := ipcache.IPIdentityCache.LookupByIP(pod.Status.PodIP)
+	if !exists {
+		return true, fmt.Errorf("identity for IP does not exist in case")
+	}
+
+	if id.Source != ipcache.FromKubernetes {
+		return true, fmt.Errorf("ipcache entry not owned by kubernetes source")
+	}
+
+	ipcache.IPIdentityCache.Delete(pod.Status.PodIP)
+
+	idPair := identity.IPIdentityPair{IP: podIP}
+	for _, listener := range d.ipcacheListeners {
+		listener.OnIPIdentityCacheChange(ipcache.Delete, nil, idPair)
+	}
+
+	return false, nil
+}
+
+func (d *Daemon) addK8sPodV1(pod *v1.Pod) {
+	logger := log.WithFields(logrus.Fields{
+		logfields.K8sPodName:   pod.ObjectMeta.Name,
+		logfields.K8sNamespace: pod.ObjectMeta.Namespace,
+		"podIP":                pod.Status.PodIP,
+		"hostIP":               pod.Status.HostIP,
+	})
+
+	skipped, err := d.updatePodHostIP(pod)
+	switch {
+	case skipped:
+		logger.WithError(err).Debug("Skipped ipcache map update on pod add")
+	case err != nil:
+		logger.WithError(err).Warning("Unable to update ipcache map entry on pod add ")
+	default:
+		logger.Debug("Updated ipcache map entry on pod add")
+	}
+}
+
 func (d *Daemon) updateK8sPodV1(oldK8sPod, newK8sPod *v1.Pod) {
 	if oldK8sPod == nil || newK8sPod == nil {
 		return
 	}
+
+	// The pod IP can never change, it can only switch from unassigned to
+	// assigned
+	d.addK8sPodV1(newK8sPod)
 
 	// We only care about label updates
 	oldPodLabels := oldK8sPod.GetLabels()
@@ -1500,4 +1611,24 @@ func (d *Daemon) updateK8sPodV1(oldK8sPod, newK8sPod *v1.Pod) {
 		logfields.EndpointID: podEP.GetID(),
 		logfields.Labels:     logfields.Repr(newIdtyLabels),
 	}).Debug("Update endpoint with new labels")
+}
+
+func (d *Daemon) deleteK8sPodV1(pod *v1.Pod) {
+	logger := log.WithFields(logrus.Fields{
+		logfields.K8sPodName:   pod.ObjectMeta.Name,
+		logfields.K8sNamespace: pod.ObjectMeta.Namespace,
+		"podIP":                pod.Status.PodIP,
+		"hostIP":               pod.Status.HostIP,
+	})
+
+	skipped, err := d.deletePodHostIP(pod)
+	switch {
+	case skipped:
+		logger.WithError(err).Debug("Skipped ipcache map delete on pod delete")
+	case err != nil:
+		logger.WithError(err).Warning("Unable to delete ipcache map entry on pod delete")
+	default:
+		logger.Debug("Deleted ipcache map entry on pod delete")
+	}
+
 }

pkg/ipcache/ipcache.go
@@ -32,11 +32,37 @@ var (
 	IPIdentityCache = NewIPCache()
 )
 
+// Source is the description of the source of an identity
+type Source string
+
+const (
+	// FromKubernetes is the source used for identities derived from k8s
+	// resources (pods)
+	FromKubernetes Source = "k8s"
+
+	// FromKVStore is the source used for identities derived from the
+	// kvstore
+	FromKVStore Source = "kvstore"
+
+	// FromAgentLocal is the source used for identities derived during the
+	// agent bootup process. This includes identities for host IPs.
+	FromAgentLocal Source = "agent-local"
+)
+
+// Identity is the identity representation of an IP<->Identity cache.
+type Identity struct {
+	// ID is the numeric identity
+	ID identity.NumericIdentity
+
+	// Source is the source of the identity in the cache
+	Source Source
+}
+
 // IPCache is a caching of endpoint IP to security identity (and vice-versa) for
 // all endpoints which are part of the same cluster.
 type IPCache struct {
 	mutex             lock.RWMutex
-	ipToIdentityCache map[string]identity.NumericIdentity
+	ipToIdentityCache map[string]Identity
 	identityToIPCache map[identity.NumericIdentity]map[string]struct{}
 
 	// prefixLengths reference-count the number of CIDRs that use
@@ -56,7 +82,7 @@ type Implementation interface {
 // identity (and vice-versa) initialized.
 func NewIPCache() *IPCache {
 	return &IPCache{
-		ipToIdentityCache: map[string]identity.NumericIdentity{},
+		ipToIdentityCache: map[string]Identity{},
 		identityToIPCache: map[identity.NumericIdentity]map[string]struct{}{},
 		v4PrefixLengths:   map[int]int{},
 		v6PrefixLengths:   map[int]int{},
@@ -138,11 +164,31 @@ func unrefPrefixLength(prefixLengths map[int]int, length int) {
 	}
 }
 
+func allowOverwrite(existing, new Source) bool {
+	switch existing {
+	case FromKubernetes:
+		// k8s entries can be overwritten by everyone else
+		return true
+	case FromKVStore:
+		return new == FromKVStore || new == FromAgentLocal
+	case FromAgentLocal:
+		return new == FromAgentLocal
+	}
+
+	return true
+}
+
 // Upsert adds / updates the provided IP (endpoint or CIDR prefix) and identity
 // into the IPCache.
-func (ipc *IPCache) Upsert(IP string, identity identity.NumericIdentity) {
+func (ipc *IPCache) Upsert(IP string, identity Identity) bool {
 	ipc.mutex.Lock()
 	defer ipc.mutex.Unlock()
+
+	if existingIdentity, found := ipc.ipToIdentityCache[IP]; found {
+		if !allowOverwrite(existingIdentity.Source, identity.Source) {
+			return false
+		}
+	}
 
 	// An update is treated as a deletion and then an insert.
 	ipc.deleteLocked(IP)
@@ -155,11 +201,11 @@ func (ipc *IPCache) Upsert(IP string, identity identity.NumericIdentity) {
 	// Update both maps.
 	ipc.ipToIdentityCache[IP] = identity
 
-	_, found := ipc.identityToIPCache[identity]
+	_, found := ipc.identityToIPCache[identity.ID]
 	if !found {
-		ipc.identityToIPCache[identity] = map[string]struct{}{}
+		ipc.identityToIPCache[identity.ID] = map[string]struct{}{}
 	}
-	ipc.identityToIPCache[identity][IP] = struct{}{}
+	ipc.identityToIPCache[identity.ID][IP] = struct{}{}
 
 	// Add a reference for the prefix length if this is a CIDR.
 	if _, cidr, err := net.ParseCIDR(IP); err == nil {
@@ -171,6 +217,8 @@ func (ipc *IPCache) Upsert(IP string, identity identity.NumericIdentity) {
 			refPrefixLength(ipc.v4PrefixLengths, pl)
 		}
 	}
+
+	return true
 }
 
 // deleteLocked removes removes the provided IP-to-security-identity mapping
@@ -183,9 +231,9 @@ func (ipc *IPCache) deleteLocked(IP string) {
 	identity, found := ipc.ipToIdentityCache[IP]
 	if found {
 		delete(ipc.ipToIdentityCache, IP)
-		delete(ipc.identityToIPCache[identity], IP)
-		if len(ipc.identityToIPCache[identity]) == 0 {
-			delete(ipc.identityToIPCache, identity)
+		delete(ipc.identityToIPCache[identity.ID], IP)
+		if len(ipc.identityToIPCache[identity.ID]) == 0 {
+			delete(ipc.identityToIPCache, identity.ID)
 		}
 
 		// Remove a reference for the prefix length if this is a CIDR.
@@ -201,8 +249,8 @@ func (ipc *IPCache) deleteLocked(IP string) {
 	}
 }
 
-// delete removes the provided IP-to-security-identity mapping from the IPCache.
-func (ipc *IPCache) delete(IP string) {
+// Delete removes the provided IP-to-security-identity mapping from the IPCache.
+func (ipc *IPCache) Delete(IP string) {
 	ipc.mutex.Lock()
 	defer ipc.mutex.Unlock()
 	ipc.deleteLocked(IP)
@@ -211,7 +259,7 @@ func (ipc *IPCache) delete(IP string) {
 // LookupByIP returns the corresponding security identity that endpoint IP maps
 // to within the provided IPCache, as well as if the corresponding entry exists
 // in the IPCache.
-func (ipc *IPCache) LookupByIP(IP string) (identity.NumericIdentity, bool) {
+func (ipc *IPCache) LookupByIP(IP string) (Identity, bool) {
 	ipc.mutex.RLock()
 	defer ipc.mutex.RUnlock()
 	return ipc.LookupByIPRLocked(IP)
@@ -220,7 +268,7 @@ func (ipc *IPCache) LookupByIP(IP string) (identity.NumericIdentity, bool) {
 // LookupByIPRLocked returns the corresponding security identity that endpoint IP maps
 // to within the provided IPCache, as well as if the corresponding entry exists
 // in the IPCache.
-func (ipc *IPCache) LookupByIPRLocked(IP string) (identity.NumericIdentity, bool) {
+func (ipc *IPCache) LookupByIPRLocked(IP string) (Identity, bool) {
 
 	identity, exists := ipc.ipToIdentityCache[IP]
 	return identity, exists
@@ -230,7 +278,7 @@ func (ipc *IPCache) LookupByIPRLocked(IP string) (identity.NumericIdentity, bool
 // prefix is fully specified (ie, w.x.y.z/32 for IPv4), find the host for the
 // identity in the provided IPCache, and returns the corresponding security
 // identity as well as whether the entry exists in the IPCache.
-func (ipc *IPCache) LookupByPrefixRLocked(prefix string) (identity identity.NumericIdentity, exists bool) {
+func (ipc *IPCache) LookupByPrefixRLocked(prefix string) (identity Identity, exists bool) {
 	if _, cidr, err := net.ParseCIDR(prefix); err == nil {
 		// If it's a fully specfied prefix, attempt to find the host
 		ones, bits := cidr.Mask.Size()
@@ -248,7 +296,7 @@ func (ipc *IPCache) LookupByPrefixRLocked(prefix string) (identity identity.Nume
 // LookupByPrefix returns the corresponding security identity that endpoint IP
 // maps to within the provided IPCache, as well as if the corresponding entry
 // exists in the IPCache.
-func (ipc *IPCache) LookupByPrefix(IP string) (identity.NumericIdentity, bool) {
+func (ipc *IPCache) LookupByPrefix(IP string) (Identity, bool) {
 	ipc.mutex.RLock()
 	defer ipc.mutex.RUnlock()
 	return ipc.LookupByPrefixRLocked(IP)

daemon/daemon.go
@@ -1027,27 +1027,30 @@ func (d *Daemon) syncLXCMap() error {
 		}
 		prefix := ipIDPair.PrefixString()
 		cachedIdentity, exists := ipcache.IPIdentityCache.LookupByIP(prefix)
-		if !exists || cachedIdentity != ipIDPair.ID {
+		if !exists || cachedIdentity.ID != ipIDPair.ID {
 			// Upsert will not propagate (reserved:foo->ID) mappings across the cluster,
 			// and we specifically don't want to do so.
 			// Manually push it into each IPIdentityMappingListener.
 			log.WithFields(logrus.Fields{
 				logfields.IPAddr:       ipIDPair.IP,
 				logfields.IPMask:       ipIDPair.Mask,
-				logfields.OldIdentity:  cachedIdentity,
+				logfields.OldIdentity:  cachedIdentity.ID,
 				logfields.Identity:     ipIDPair.ID,
 				logfields.Modification: ipcache.Upsert,
 			}).Debug("Adding special identity to ipcache")
-			ipcache.IPIdentityCache.Upsert(prefix, ipIDPair.ID)
+			ipcache.IPIdentityCache.Upsert(prefix, ipcache.Identity{
+				ID:     ipIDPair.ID,
+				Source: ipcache.FromAgentLocal,
+			})
 
 			var oldIPIDPair *identity.IPIdentityPair
-			if cachedIdentity != ipIDPair.ID {
+			if cachedIdentity.ID != ipIDPair.ID {
 				// If an existing mapping is updated,
 				// provide the existing mapping to the
 				// listener so it can easily clean up
 				// the old mapping.
 				pair := ipIDPair
-				pair.ID = cachedIdentity
+				pair.ID = cachedIdentity.ID
 				oldIPIDPair = &pair
 			}
 			for _, listener := range d.ipcacheListeners {

pkg/ipcache/ipcache_test.go
@@ -43,25 +43,39 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 	c.Assert(len(IPIdentityCache.identityToIPCache), Equals, 0)
 
 	// Deletion of key that doesn't exist doesn't cause panic.
-	IPIdentityCache.delete(endpointIP)
+	IPIdentityCache.Delete(endpointIP)
 
-	IPIdentityCache.Upsert(endpointIP, identity)
+	IPIdentityCache.Upsert(endpointIP, Identity{
+		ID:     identity,
+		Source: FromKVStore,
+	})
 
 	// Assure both caches are updated..
 	c.Assert(len(IPIdentityCache.ipToIdentityCache), Equals, 1)
 	c.Assert(len(IPIdentityCache.identityToIPCache), Equals, 1)
 
 	cachedIdentity, exists := IPIdentityCache.LookupByIP(endpointIP)
-	c.Assert(cachedIdentity, Equals, identity)
 	c.Assert(exists, Equals, true)
+	c.Assert(cachedIdentity.ID, Equals, identity)
+	c.Assert(cachedIdentity.Source, Equals, FromKVStore)
 
-	IPIdentityCache.Upsert(endpointIP, identity)
+	// kubernetes source cannot update kvstore source
+	updated := IPIdentityCache.Upsert(endpointIP, Identity{
+		ID:     identity,
+		Source: FromKubernetes,
+	})
+	c.Assert(updated, Equals, false)
+
+	IPIdentityCache.Upsert(endpointIP, Identity{
+		ID:     identity,
+		Source: FromKVStore,
+	})
 
 	// No duplicates.
 	c.Assert(len(IPIdentityCache.ipToIdentityCache), Equals, 1)
 	c.Assert(len(IPIdentityCache.identityToIPCache), Equals, 1)
 
-	IPIdentityCache.delete(endpointIP)
+	IPIdentityCache.Delete(endpointIP)
 
 	// Assure deletion occurs across both mappings.
 	c.Assert(len(IPIdentityCache.ipToIdentityCache), Equals, 0)
@@ -71,10 +85,16 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 
 	c.Assert(exists, Equals, false)
 
-	IPIdentityCache.Upsert(endpointIP, identity)
+	IPIdentityCache.Upsert(endpointIP, Identity{
+		ID:     identity,
+		Source: FromKVStore,
+	})
 
 	newIdentity := identityPkg.NumericIdentity(69)
-	IPIdentityCache.Upsert(endpointIP, newIdentity)
+	IPIdentityCache.Upsert(endpointIP, Identity{
+		ID:     newIdentity,
+		Source: FromKVStore,
+	})
 
 	// Ensure that update of cache with new identity doesn't keep old identity-to-ip
 	// mapping around.
@@ -87,7 +107,7 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 		c.Assert(cachedIP, Equals, endpointIP)
 	}
 
-	IPIdentityCache.delete(endpointIP)
+	IPIdentityCache.Delete(endpointIP)
 
 	// Assure deletion occurs across both mappings.
 	c.Assert(len(IPIdentityCache.ipToIdentityCache), Equals, 0)
@@ -98,9 +118,12 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 	identities := []identityPkg.NumericIdentity{5, 67, 29, 29, 29}
 
 	for index := range endpointIPs {
-		IPIdentityCache.Upsert(endpointIPs[index], identities[index])
+		IPIdentityCache.Upsert(endpointIPs[index], Identity{
+			ID:     identities[index],
+			Source: FromKVStore,
+		})
 		cachedIdentity, _ := IPIdentityCache.LookupByIP(endpointIPs[index])
-		c.Assert(cachedIdentity, Equals, identities[index])
+		c.Assert(cachedIdentity.ID, Equals, identities[index])
 	}
 
 	expectedIPList := map[string]struct{}{
@@ -111,7 +134,7 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 	cachedEndpointIPs, _ := IPIdentityCache.LookupByIdentity(29)
 	c.Assert(reflect.DeepEqual(cachedEndpointIPs, expectedIPList), Equals, true)
 
-	IPIdentityCache.delete("27.2.2.2")
+	IPIdentityCache.Delete("27.2.2.2")
 
 	expectedIPList = map[string]struct{}{
 		"127.0.0.1": {},
@@ -121,12 +144,12 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 	c.Assert(reflect.DeepEqual(cachedEndpointIPs, expectedIPList), Equals, true)
 
 	cachedIdentity, exists = IPIdentityCache.LookupByIP("127.0.0.1")
-	c.Assert(cachedIdentity, Equals, identityPkg.NumericIdentity(29))
+	c.Assert(cachedIdentity.ID, Equals, identityPkg.NumericIdentity(29))
 
 	cachedIdentity, exists = IPIdentityCache.LookupByPrefix("127.0.0.1/32")
-	c.Assert(cachedIdentity, Equals, identityPkg.NumericIdentity(29))
+	c.Assert(cachedIdentity.ID, Equals, identityPkg.NumericIdentity(29))
 
-	IPIdentityCache.delete("127.0.0.1")
+	IPIdentityCache.Delete("127.0.0.1")
 
 	_, exists = IPIdentityCache.LookupByIdentity(29)
 	c.Assert(exists, Equals, false)
@@ -136,7 +159,7 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 
 	// Clean up.
 	for index := range endpointIPs {
-		IPIdentityCache.delete(endpointIPs[index])
+		IPIdentityCache.Delete(endpointIPs[index])
 		_, exists = IPIdentityCache.LookupByIP(endpointIPs[index])
 		c.Assert(exists, Equals, false)
 
@@ -214,4 +237,16 @@ func (s *IPCacheTestSuite) TestKeyToIPNet(c *C) {
 	c.Assert(nilIP, IsNil)
 	c.Assert(err, Not(IsNil))
 	c.Assert(isHost, Equals, false)
+}
+
+func (s *IPCacheTestSuite) TestAllowOverwrite(c *C) {
+	c.Assert(allowOverwrite(FromKubernetes, FromKubernetes), Equals, true)
+	c.Assert(allowOverwrite(FromKubernetes, FromKVStore), Equals, true)
+	c.Assert(allowOverwrite(FromKubernetes, FromAgentLocal), Equals, true)
+	c.Assert(allowOverwrite(FromKVStore, FromKubernetes), Equals, false)
+	c.Assert(allowOverwrite(FromKVStore, FromKVStore), Equals, true)
+	c.Assert(allowOverwrite(FromKVStore, FromAgentLocal), Equals, true)
+	c.Assert(allowOverwrite(FromAgentLocal, FromKubernetes), Equals, false)
+	c.Assert(allowOverwrite(FromAgentLocal, FromKVStore), Equals, false)
+	c.Assert(allowOverwrite(FromAgentLocal, FromAgentLocal), Equals, true)
 }

pkg/ipcache/kvstore.go
@@ -282,9 +282,9 @@ func deleteIPNetsFromKVStore(prefixes []*net.IPNet) (err error) {
 // for IPv4) which matches the IP in the specified pair. Only performs the
 // search if the pair's IP represents a host IP.
 // Returns the identity and whether the IP was found.
-func findShadowedCIDR(pair *identity.IPIdentityPair) (identity.NumericIdentity, bool) {
+func findShadowedCIDR(pair *identity.IPIdentityPair) (Identity, bool) {
 	if !pair.IsHost() {
-		return identity.InvalidIdentity, false
+		return Identity{ID: identity.InvalidIdentity}, false
 	}
 	bits := net.IPv6len * 8
 	if pair.IP.To4() != nil {
@@ -338,7 +338,7 @@ restart:
 				cacheChanged      bool
 				cacheModification CacheModification
 				ipIDPair          identity.IPIdentityPair
-				cachedIdentity    identity.NumericIdentity
+				cachedIdentity    Identity
 				ipIsInCache       bool
 			)
 
@@ -382,8 +382,11 @@ restart:
 				if !ipIDPair.IsHost() {
 					ones, bits := ipIDPair.Mask.Size()
 					if ipIsInCache && ones == bits {
-						if cachedIdentity != ipIDPair.ID {
-							IPIdentityCache.Upsert(ipStr, ipIDPair.ID)
+						if cachedIdentity.ID != ipIDPair.ID {
+							IPIdentityCache.Upsert(ipStr, Identity{
+								ID:     ipIDPair.ID,
+								Source: FromKVStore,
+							})
 							scopedLog.WithField(logfields.IPAddr, ipIDPair.IP).
 								Infof("Received KVstore update for CIDR overlapping with endpoint IP.")
 						}
@@ -392,8 +395,11 @@ restart:
 				}
 
 				// Insert or update the IP -> ID mapping.
-				if !ipIsInCache || cachedIdentity != ipIDPair.ID {
-					IPIdentityCache.Upsert(ipStr, ipIDPair.ID)
+				if !ipIsInCache || cachedIdentity.ID != ipIDPair.ID {
+					IPIdentityCache.Upsert(ipStr, Identity{
+						ID:     ipIDPair.ID,
+						Source: FromKVStore,
+					})
 					cacheChanged = true
 					cacheModification = Upsert
 				}
@@ -417,17 +423,17 @@ restart:
 
 				if ipIsInCache {
 					cacheChanged = true
-					IPIdentityCache.delete(ipStr)
+					IPIdentityCache.Delete(ipStr)
 
 					// Set up the IPIDPair and cacheModification for listener callbacks
 					prefixIdentity, shadowedCIDR := findShadowedCIDR(&ipIDPair)
 					if shadowedCIDR {
 						scopedLog.WithField(logfields.IPAddr, ipIDPair.IP).
 							Infof("Received KVstore deletion for endpoint IP shadowing CIDR, restoring CIDR.")
-						ipIDPair.ID = prefixIdentity
+						ipIDPair.ID = prefixIdentity.ID
 						cacheModification = Upsert
 					} else {
-						ipIDPair.ID = cachedIdentity
+						ipIDPair.ID = cachedIdentity.ID
 						cacheModification = Delete
 					}
 				}
@@ -449,7 +455,7 @@ restart:
 					// listener so it can easily clean up
 					// the old mapping.
 					pair := ipIDPair
-					pair.ID = cachedIdentity
+					pair.ID = cachedIdentity.ID
 					oldIPIDPair = &pair
 				}
 				// Callback upon cache updates.

commit 2a9010ead40a44a5d74435c14346109da3688f38

k8s: Update HostIP even if identity is unchanged
The current code made ipcache.Upsert() return false if the update was skipped.
This resulted in OnIPIdentityChange() to not be invokved if the identity was
unchanged. Instead, have Upsert() indicate whether the entry is owned by
another owner to properly skip updates.

Fixes: 652a6ff6bcb ("ipcache: Populate ipcache based on Pod events")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/k8s_watcher.go
@@ -1543,11 +1543,11 @@ func (d *Daemon) updatePodHostIP(pod *v1.Pod) (bool, error) {
 		return true, fmt.Errorf("no/invalid PodIP: %s", pod.Status.PodIP)
 	}
 
-	updated := ipcache.IPIdentityCache.Upsert(pod.Status.PodIP, ipcache.Identity{
+	selfOwned := ipcache.IPIdentityCache.Upsert(pod.Status.PodIP, ipcache.Identity{
 		ID:     identity.ReservedIdentityCluster,
 		Source: ipcache.FromKubernetes,
 	})
-	if !updated {
+	if !selfOwned {
 		return true, fmt.Errorf("ipcache entry owned by kvstore or agent")
 	}
 

pkg/envoy/resources.go
@@ -67,6 +67,11 @@ func (cache *NPHDSCache) OnIPIdentityCacheChange(modType ipcache.CacheModificati
 	// An upsert where an existing pair exists should translate into a
 	// delete (for the old Identity) followed by an upsert (for the new).
 	if oldIPIDPair != nil && modType == ipcache.Upsert {
+		// Skip update if identity is identical
+		if oldIPIDPair.ID == newIPIDPair.ID {
+			return
+		}
+
 		cache.OnIPIdentityCacheChange(ipcache.Delete, nil, *oldIPIDPair)
 	}
 

pkg/ipcache/ipcache.go
@@ -180,6 +180,10 @@ func allowOverwrite(existing, new Source) bool {
 
 // Upsert adds / updates the provided IP (endpoint or CIDR prefix) and identity
 // into the IPCache.
+//
+// Returns false if the entry is not owned by the self declared source, i.e.
+// returns false if the kubernetes layer is trying to upsert an entry now
+// managed by the kvstore layer. See allowOverwrite() for rules on ownership.
 func (ipc *IPCache) Upsert(IP string, identity Identity) bool {
 	ipc.mutex.Lock()
 	defer ipc.mutex.Unlock()
@@ -191,7 +195,7 @@ func (ipc *IPCache) Upsert(IP string, identity Identity) bool {
 
 		// Skip update if IP is already mapped to the given identity
 		if existingIdentity == identity {
-			return false
+			return true
 		}
 	}
 