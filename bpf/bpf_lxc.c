g0odi3z2j/cilium

commit 5232629e3f2a8b7f196ab09dfa34d38363a834e8

bpf: Shift ingress ipcache source lookup to netdev
Ingress paths may be one of the following:
* From a local endpoint. In this case the source is known.
* From the overlay. In this case the source is taken from the overlay.
* From a local netdev. In this case, we don't know the source and should
  perform a lookup to determine what we can about the source.

This final case was being implemented in the ingress policy lookup
stage, which adds complexity to the ingress BPF program and also makes
monitor output less consistent, as the actual source is not known until
that point and may not be propagated to the correct parts of the program
so that the real identity is reported in monitor output.

Solve these problems by shifting this ipcache lookup all the way back
into the netdev program.

Changes in instruction complexity on Linux 4.9.17 with GCC 7.2.0:

bpf_lxc ingress:        49458 -> 26197 (-47%)
bpf_netdev handle_ipv4: 3471 -> 4682   (+35%)
bpf_netdev handle_ipv6: 8847 -> 13326  (+50%)

Note that the complexity limit in this case is 64K on Linux 4.9, so
although we increase the complexity in each of the netdev programs by
fair margin, the decrease in complexity in the ingress lxc program
provides us significantly more margin for feature development.
Furthermore, depending on where the ipcache is called from, it clearly
has a different impact on complexity limit; In the lxc program from the
ingress policy function, it represents ~23K instructions of complexity
due to the nesting of where it is called from. In the netdev programs,
this is only 1.2K or 4.5K.

Signed-off-by: Joe Stringer <joe@covalent.io>

bpf/bpf_lxc.c
@@ -780,7 +780,7 @@ static inline int __inline__ ipv6_policy(struct __sk_buff *skb, int ifindex, __u
 			return ret2;
 	}
 
-	verdict = policy_can_access_ingress(skb, &src_label, tuple.dport,
+	verdict = policy_can_access_ingress(skb, src_label, tuple.dport,
 					    tuple.nexthdr, sizeof(tuple.saddr),
 					    &tuple.saddr);
 
@@ -910,7 +910,7 @@ static inline int __inline__ ipv4_policy(struct __sk_buff *skb, int ifindex, __u
 			return ret2;
 	}
 
-	verdict = policy_can_access_ingress(skb, &src_label, tuple.dport,
+	verdict = policy_can_access_ingress(skb, src_label, tuple.dport,
 					    tuple.nexthdr, sizeof(orig_sip),
 					    &orig_sip);
 

bpf/bpf_netdev.c
@@ -171,9 +171,7 @@ static inline int rewrite_dmac_to_host(struct __sk_buff *skb)
 
 static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 {
-#ifdef ENCAP_IFINDEX
 	struct remote_endpoint_info *info;
-#endif
 	union v6addr node_ip = { };
 	void *data, *data_end;
 	struct ipv6hdr *ip6;
@@ -200,6 +198,16 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 			return ret;
 	}
 #endif
+
+	/* Packets from the proxy will already have a real identity. */
+	if (identity_is_reserved(src_identity)) {
+		union v6addr *src = (union v6addr *) &ip6->saddr;
+		info = ipcache_lookup6(&cilium_ipcache, src, V6_CACHE_KEY_LEN);
+		if (!info)
+			src_identity = info->sec_label;
+		cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
+			   ((__u32 *) src)[3], src_identity);
+	}
 
 	BPF_V6(node_ip, ROUTER_IP);
 	flowlabel = derive_sec_ctx(skb, &node_ip, ip6);
@@ -344,9 +352,7 @@ reverse_proxy(struct __sk_buff *skb, int l4_off, struct iphdr *ip4, __u8 nh)
 
 static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 {
-#ifdef ENCAP_IFINDEX
 	struct remote_endpoint_info *info;
-#endif
 	struct ipv4_ct_tuple tuple = {};
 	struct endpoint_info *ep;
 	void *data, *data_end;
@@ -360,6 +366,15 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 	l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
 	secctx = derive_ipv4_sec_ctx(skb, ip4);
 	tuple.nexthdr = ip4->protocol;
+
+	/* Packets from the proxy will already have a real identity. */
+	if (identity_is_reserved(src_identity)) {
+		info = ipcache_lookup4(&cilium_ipcache, ip4->saddr, V4_CACHE_KEY_LEN);
+		if (!info)
+			src_identity = info->sec_label;
+		cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED4 : DBG_IP_ID_MAP_FAILED4,
+			   ip4->saddr, src_identity);
+	}
 
 #ifdef FROM_HOST
 	if (1) {

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=62a45e49c02110faaa812bce5270f58a8d48af9d
+GO_BINDATA_SHA1SUM=240086b01df50052613e1e92c6de1661ca557cb4
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

bpf/lib/policy.h
@@ -128,7 +128,7 @@ allow:
  *   - Negative error code if the packet should be dropped
  */
 static inline int __inline__
-policy_can_access_ingress(struct __sk_buff *skb, __u32 *src_identity,
+policy_can_access_ingress(struct __sk_buff *skb, __u32 src_identity,
 			  __u16 dport, __u8 proto, size_t cidr_addr_size,
 			  void *cidr_addr)
 {
@@ -137,37 +137,13 @@ policy_can_access_ingress(struct __sk_buff *skb, __u32 *src_identity,
 #else
 	int ret;
 
-	if (identity_is_reserved(*src_identity)) {
-		if (cidr_addr_size == sizeof(union v6addr)) {
-			struct remote_endpoint_info *info;
-
-			info = lookup_ip6_remote_endpoint(cidr_addr);
-			if (info)
-				*src_identity = info->sec_label;
-
-			cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
-				   ((__u32 *) cidr_addr)[3], *src_identity);
-		}
-
-		if (cidr_addr_size == sizeof(__be32)) {
-			struct remote_endpoint_info *info;
-			__be32 saddr = *(__be32 *)cidr_addr;
-
-			if ((info = lookup_ip4_remote_endpoint(saddr)))
-				*src_identity = info->sec_label;
-
-			cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED4 : DBG_IP_ID_MAP_FAILED4,
-				   saddr, *src_identity);
-		}
-	}
-
-	ret = __policy_can_access(&POLICY_MAP, skb, *src_identity, dport,
+	ret = __policy_can_access(&POLICY_MAP, skb, src_identity, dport,
 				      proto, cidr_addr_size, cidr_addr,
 				      CT_INGRESS);
 	if (ret >= TC_ACT_OK)
 		return ret;
 
-	cilium_dbg(skb, DBG_POLICY_DENIED, *src_identity, SECLABEL);
+	cilium_dbg(skb, DBG_POLICY_DENIED, src_identity, SECLABEL);
 
 #ifndef IGNORE_DROP
 	return DROP_POLICY;
@@ -180,7 +156,7 @@ policy_can_access_ingress(struct __sk_buff *skb, __u32 *src_identity,
 #else /* POLICY_INGRESS */
 
 static inline int
-policy_can_access_ingress(struct __sk_buff *skb, __u32 *src_label,
+policy_can_access_ingress(struct __sk_buff *skb, __u32 src_label,
 			  __u16 dport, __u8 proto, size_t cidr_addr_size,
 			  void *cidr_addr)
 {

commit 6e2676cf1c3c5bd616713b345b2a64db0911a6f6

bpf: Fix ipcache lookup in bpf_netdev
Commit 5232629e3f2 introduces an incorrect NULL check of the return value of
ipcache_lookup(). Fix this and only derive the security identity if the ipcache
entry if the security identity is in the entry. This allows to work around the
missing delete operations in certain kernels by updating the entry with a
zeroed out value instead of deleting it.

Fixes: 5232629e3f2 ("bpf: Shift ingress ipcache source lookup to netdev")
Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/bpf_lxc.c
@@ -172,7 +172,7 @@ skip_service_lookup:
 		struct remote_endpoint_info *info;
 
 		info = lookup_ip6_remote_endpoint(&orig_dip);
-		if (info != NULL) {
+		if (info != NULL && info->sec_label) {
 			dstID = info->sec_label;
 			tunnel_endpoint = info->tunnel_endpoint;
 		} else if (ipv6_match_prefix_64(daddr, &router_ip))
@@ -476,7 +476,7 @@ skip_service_lookup:
 		struct remote_endpoint_info *info;
 
 		info = lookup_ip4_remote_endpoint(orig_dip);
-		if (info != NULL) {
+		if (info != NULL && info->sec_label) {
 			dstID = info->sec_label;
 			tunnel_endpoint = info->tunnel_endpoint;
 		} else if ((orig_dip & IPV4_CLUSTER_MASK) == IPV4_CLUSTER_RANGE)

bpf/bpf_netdev.c
@@ -203,7 +203,7 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 	if (identity_is_reserved(src_identity)) {
 		union v6addr *src = (union v6addr *) &ip6->saddr;
 		info = ipcache_lookup6(&cilium_ipcache, src, V6_CACHE_KEY_LEN);
-		if (!info)
+		if (info != NULL && info->sec_label)
 			src_identity = info->sec_label;
 		cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED6 : DBG_IP_ID_MAP_FAILED6,
 			   ((__u32 *) src)[3], src_identity);
@@ -370,7 +370,7 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 	/* Packets from the proxy will already have a real identity. */
 	if (identity_is_reserved(src_identity)) {
 		info = ipcache_lookup4(&cilium_ipcache, ip4->saddr, V4_CACHE_KEY_LEN);
-		if (!info)
+		if (info != NULL && info->sec_label)
 			src_identity = info->sec_label;
 		cilium_dbg(skb, info ? DBG_IP_ID_MAP_SUCCEED4 : DBG_IP_ID_MAP_FAILED4,
 			   ip4->saddr, src_identity);

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=240086b01df50052613e1e92c6de1661ca557cb4
+GO_BINDATA_SHA1SUM=e8b3ad7cd7a6aa097638cfdf49e7c85b6e01ded5
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 