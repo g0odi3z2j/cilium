g0odi3z2j/cilium

commit 3505d0f1c9c83381c949d7db42399638c747b6ea

kvstore: New kvstore abstraction API
 - Basic lockless operations:
    - Get, GetPrefix, ListPrefix, Set, Delete
    - CreateOnly
    - Watch, ListAndWatch CreateLease, KeepAlive

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/kvstore/consul.go
@@ -93,22 +93,20 @@ func newConsulClient(config *consulAPI.Config) (KVClient, error) {
 }
 
 func (c *ConsulClient) LockPath(path string) (KVLocker, error) {
-	log.Debugf("Creating lock for %s", path)
+	trace("Creating lock for %s", path)
 	opts := &consulAPI.LockOptions{
-		Key: GetLockPath(path),
+		Key: getLockPath(path),
 	}
 	lockKey, err := c.LockOpts(opts)
 	if err != nil {
 		return nil, err
 	}
 	ch, err := lockKey.Lock(nil)
-	defer func() {
-		if err == nil {
-			log.Debugf("Locked for %s", path)
-		}
-	}()
 	if ch == nil {
 		return nil, fmt.Errorf("locker is nil\n")
+	}
+	if err == nil {
+		trace("Locked %s", path)
 	}
 	return lockKey, err
 }
@@ -124,7 +122,7 @@ func (c *ConsulClient) InitializeFreeID(path string, firstID uint32) error {
 	}
 
 	p := &consulAPI.KVPair{Key: path, Value: freeIDByte}
-	lockPair := &consulAPI.KVPair{Key: GetLockPath(path), Session: session}
+	lockPair := &consulAPI.KVPair{Key: getLockPath(path), Session: session}
 	log.Debug("Trying to acquire lock for free ID...")
 	acq, _, err := c.KV().Acquire(lockPair, nil)
 	if err != nil {
@@ -144,12 +142,11 @@ func (c *ConsulClient) InitializeFreeID(path string, firstID uint32) error {
 		// FreeID already set
 		return nil
 	}
-	log.Debugf("Trying to put free ID...")
 	_, err = c.KV().Put(p, nil)
 	if err != nil {
 		return err
 	}
-	log.Debugf("Free ID for path %s successfully initialized", path)
+	trace("Free ID for path %s successfully initialized", path)
 
 	return nil
 }
@@ -184,7 +181,7 @@ func (c *ConsulClient) GetMaxID(key string, firstID uint32) (uint32, error) {
 	}
 	if k == nil {
 		// FreeID is empty? We should set it out!
-		log.Debugf("Empty FreeID, setting it up with default value %d", firstID)
+		trace("Empty FreeID, setting it up with default value %d", firstID)
 		if err := c.InitializeFreeID(key, firstID); err != nil {
 			return 0, err
 		}
@@ -200,7 +197,7 @@ func (c *ConsulClient) GetMaxID(key string, firstID uint32) (uint32, error) {
 		}
 	}
 	var freeID uint32
-	log.Debugf("Retrieving max free ID %v", k.Value)
+	trace("Retrieving max free ID %v", k.Value)
 	if err := json.Unmarshal(k.Value, &freeID); err != nil {
 		return 0, err
 	}
@@ -233,11 +230,6 @@ func (c *ConsulClient) SetMaxID(key string, firstID, maxID uint32) error {
 	return err
 }
 
-func (c *ConsulClient) updateSecLabelIDRef(id policy.Identity) error {
-	key := path.Join(common.LabelIDKeyPath, strconv.FormatUint(uint64(id.ID), 10))
-	return c.SetValue(key, id)
-}
-
 func (c *ConsulClient) setMaxLabelID(maxID uint32) error {
 	return c.SetMaxID(common.LastFreeLabelIDKeyPath, uint32(policy.MinimalNumericIdentity), maxID)
 }
@@ -258,10 +250,10 @@ func (c *ConsulClient) GASNewSecLabelID(basePath string, baseID uint32, pI *poli
 	}
 
 	acquireFreeID := func(firstID uint32, incID *uint32) (bool, error) {
-		log.Debugf("Trying to acquire a new free seclabel ID %d", *incID)
+		trace("Trying to acquire a new free seclabel ID %d", *incID)
 		keyPath := path.Join(basePath, strconv.FormatUint(uint64(*incID), 10))
 
-		lockPair := &consulAPI.KVPair{Key: GetLockPath(keyPath), Session: session}
+		lockPair := &consulAPI.KVPair{Key: getLockPath(keyPath), Session: session}
 		acq, _, err := c.KV().Acquire(lockPair, nil)
 		if err != nil {
 			return false, err
@@ -327,10 +319,10 @@ func (c *ConsulClient) GASNewL3n4AddrID(basePath string, baseID uint32, lAddrID 
 	}
 
 	acquireFreeID := func(firstID uint32, incID *uint32) (bool, error) {
-		log.Debugf("Trying to acquire a new free service ID %d", *incID)
+		trace("Trying to acquire a new free service ID %d", *incID)
 		keyPath := path.Join(basePath, strconv.FormatUint(uint64(*incID), 10))
 
-		lockPair := &consulAPI.KVPair{Key: GetLockPath(keyPath), Session: session}
+		lockPair := &consulAPI.KVPair{Key: getLockPath(keyPath), Session: session}
 		acq, _, err := c.KV().Acquire(lockPair, nil)
 		if err != nil {
 			return false, err
@@ -375,6 +367,96 @@ func (c *ConsulClient) GASNewL3n4AddrID(basePath string, baseID uint32, lAddrID 
 			return nil
 		}
 	}
+}
+
+// Watch starts watching for changes in a prefix
+func (c *ConsulClient) Watch(w *Watcher, list bool) {
+	go func() {
+		// Last known state of all KVPairs matching the prefix
+		localState := map[string]consulAPI.KVPair{}
+		nextIndex := uint64(0)
+
+		// block Get() calls for 5 seconds maximum
+		qo := consulAPI.QueryOptions{
+			WaitTime: 5 * time.Second,
+		}
+
+		for {
+			// Initialize sleep time to a millisecond as we don't
+			// want to sleep in between successful watch cycles
+			sleepTime := 1 * time.Millisecond
+
+			qo.WaitIndex = nextIndex
+			pairs, q, err := c.KV().List(w.prefix, &qo)
+			if err != nil {
+				// in case of error, sleep for 15 seconds before retrying
+				sleepTime = 15 * time.Second
+				log.Debugf("watcher %s failed (consul): %s", w.name, err)
+			}
+
+			if q != nil {
+				nextIndex = q.LastIndex
+			}
+
+			// WaitIndex == 0 means that this was the first ever
+			// List() and there is no change, trigger a follow-up
+			// List() with updated WaitIndex immediately
+			if qo.WaitIndex == 0 && !list {
+				continue
+			}
+
+			// timeout while watching for changes, re-schedule
+			if q == nil || q.LastIndex == qo.WaitIndex {
+				continue
+			}
+
+			for _, newPair := range pairs {
+				oldPair, ok := localState[newPair.Key]
+
+				// Keys reported for the first time must be new
+				if !ok {
+					if newPair.CreateIndex == newPair.ModifyIndex {
+						w.Events <- KeyValueEvent{
+							Typ:   EventTypeCreate,
+							Key:   newPair.Key,
+							Value: newPair.Value,
+						}
+					} else {
+						log.Warning("consul: Previously unknown key %s received with CreateIndex(%d) != ModifyIndex(%d)",
+							newPair.Key, newPair.CreateIndex, newPair.ModifyIndex)
+					}
+				} else if oldPair.ModifyIndex != newPair.ModifyIndex {
+					w.Events <- KeyValueEvent{
+						Typ:   EventTypeModify,
+						Key:   newPair.Key,
+						Value: newPair.Value,
+					}
+				}
+
+				delete(localState, newPair.Key)
+			}
+
+			for k, deletedPair := range localState {
+				w.Events <- KeyValueEvent{
+					Typ:   EventTypeDelete,
+					Key:   deletedPair.Key,
+					Value: deletedPair.Value,
+				}
+				delete(localState, k)
+			}
+
+			for _, newPair := range pairs {
+				localState[newPair.Key] = *newPair
+
+			}
+
+			select {
+			case <-time.After(sleepTime):
+			case <-w.stopWatch:
+				return
+			}
+		}
+	}()
 }
 
 // GetWatcher watches for kvstore changes in the given key. Triggers the returned channel
@@ -422,4 +504,122 @@ func (c *ConsulClient) Status() (string, error) {
 func (c *ConsulClient) DeleteTree(path string) error {
 	_, err := c.Client.KV().DeleteTree(path, nil)
 	return err
+}
+
+// Set sets value of key
+func (c *ConsulClient) Set(key string, value []byte) error {
+	_, err := c.KV().Put(&consulAPI.KVPair{Key: key, Value: value}, nil)
+	return err
+}
+
+// Delete deletes a key
+func (c *ConsulClient) Delete(key string) error {
+	_, err := c.KV().Delete(key, nil)
+	return err
+}
+
+// Get returns value of key
+func (c *ConsulClient) Get(key string) ([]byte, error) {
+	pair, _, err := c.KV().Get(key, nil)
+	if err != nil {
+		return nil, err
+	}
+	if pair == nil {
+		return nil, nil
+	}
+	return pair.Value, nil
+}
+
+// GetPrefix returns the first key which matches the prefix
+func (c *ConsulClient) GetPrefix(prefix string) ([]byte, error) {
+	pairs, _, err := c.KV().List(prefix, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(pairs) == 0 {
+		return nil, nil
+	}
+
+	return pairs[0].Value, nil
+}
+
+// CreateOnly creates a key with the value and will fail if the key already exists
+func (c *ConsulClient) CreateOnly(key string, value []byte, lease bool) error {
+	k := &consulAPI.KVPair{
+		Key:         key,
+		Value:       value,
+		CreateIndex: 0,
+	}
+
+	if lease {
+		id, ok := leaseInstance.(string)
+		if !ok {
+			return fmt.Errorf("argument not a LeaseID")
+		}
+
+		k.Session = id
+	}
+
+	success, _, err := c.KV().CAS(k, nil)
+	if err != nil {
+		return fmt.Errorf("unable to compare-and-swap: %s", err)
+	}
+	if !success {
+		return fmt.Errorf("compare-and-swap unsuccessful")
+	}
+
+	return nil
+}
+
+// ListPrefix returns a map of matching keys
+func (c *ConsulClient) ListPrefix(prefix string) (KeyValuePairs, error) {
+	pairs, _, err := c.KV().List(prefix, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	p := KeyValuePairs(make(map[string][]byte, len(pairs)))
+	for i := 0; i < len(pairs); i++ {
+		p[pairs[i].Key] = []byte(pairs[i].Value)
+	}
+
+	return p, nil
+}
+
+// CreateLease creates a new lease with the given ttl
+func (c *ConsulClient) CreateLease(ttl time.Duration) (interface{}, error) {
+	entry := &consulAPI.SessionEntry{
+		TTL:      fmt.Sprintf("%ds", int(ttl.Seconds())),
+		Behavior: consulAPI.SessionBehaviorDelete,
+	}
+
+	id, _, err := c.Session().Create(entry, nil)
+	return id, err
+}
+
+// KeepAlive keeps a lease created with CreateLease alive
+func (c *ConsulClient) KeepAlive(lease interface{}) error {
+	id, ok := lease.(string)
+	if !ok {
+		return fmt.Errorf("argument not a LeaseID")
+	}
+
+	_, _, err := c.Session().Renew(id, nil)
+	return err
+}
+
+// DeleteLease deletes a lease
+func (c *ConsulClient) DeleteLease(lease interface{}) error {
+	id, ok := lease.(string)
+	if !ok {
+		return fmt.Errorf("argument not a LeaseID")
+	}
+
+	_, err := c.Session().Destroy(id, nil)
+	return err
+}
+
+// Close closes the kvstore client
+func (c *ConsulClient) Close() {
 }

daemon/kvstore_watcher.go
@@ -30,14 +30,14 @@ func (d *Daemon) EnableKVStoreWatcher(maxSeconds time.Duration) {
 	if maxID, err := GetMaxLabelID(); err == nil {
 		d.setCachedMaxLabelID(maxID)
 	}
-	ch := kvstore.Client.GetWatcher(common.LastFreeLabelIDKeyPath, maxSeconds)
+	ch := kvstore.Client().GetWatcher(common.LastFreeLabelIDKeyPath, maxSeconds)
 	go func() {
 		for {
 			select {
 			case updates, updateOk := <-ch:
 				if !updateOk {
 					log.Debugf("Watcher for %s closed, reacquiring it", common.LastFreeLabelIDKeyPath)
-					ch = kvstore.Client.GetWatcher(common.LastFreeLabelIDKeyPath, maxSeconds)
+					ch = kvstore.Client().GetWatcher(common.LastFreeLabelIDKeyPath, maxSeconds)
 				}
 				if len(updates) != 0 {
 					d.setCachedMaxLabelID(updates[0])

daemon/labels.go
@@ -36,7 +36,7 @@ import (
 
 func updateSecLabelIDRef(id policy.Identity) error {
 	key := path.Join(common.LabelIDKeyPath, strconv.FormatUint(uint64(id.ID), 10))
-	return kvstore.Client.SetValue(key, id)
+	return kvstore.Client().SetValue(key, id)
 }
 
 // gasNewSecLabelID gets and sets a New SecLabel ID.
@@ -46,7 +46,7 @@ func gasNewSecLabelID(id *policy.Identity) error {
 		return err
 	}
 
-	return kvstore.Client.GASNewSecLabelID(common.LabelIDKeyPath, uint32(baseID), id)
+	return kvstore.Client().GASNewSecLabelID(common.LabelIDKeyPath, uint32(baseID), id)
 }
 
 // type putIdentity struct {
@@ -85,14 +85,14 @@ func (d *Daemon) CreateOrUpdateIdentity(lbls labels.Labels, epid string) (*polic
 	identityPath := path.Join(common.LabelsKeyPath, lbls.SHA256Sum())
 
 	// Lock the identity
-	lockKey, err := kvstore.Client.LockPath(identityPath)
+	lockKey, err := kvstore.Client().LockPath(identityPath)
 	if err != nil {
 		return nil, false, err
 	}
 	defer lockKey.Unlock()
 
 	// Retrieve current identity for labels
-	rmsg, err := kvstore.Client.GetValue(identityPath)
+	rmsg, err := kvstore.Client().GetValue(identityPath)
 	if err != nil {
 		return nil, false, err
 	}
@@ -138,7 +138,7 @@ func (d *Daemon) CreateOrUpdateIdentity(lbls labels.Labels, epid string) (*polic
 	}
 
 	// store updated identity entry
-	if err = kvstore.Client.SetValue(identityPath, identity); err != nil {
+	if err = kvstore.Client().SetValue(identityPath, identity); err != nil {
 		return nil, false, err
 	}
 
@@ -206,7 +206,7 @@ func (d *Daemon) LookupIdentity(id policy.NumericIdentity) (*policy.Identity, er
 	}
 
 	strID := strconv.FormatUint(uint64(id), 10)
-	rmsg, err := kvstore.Client.GetValue(path.Join(common.LabelIDKeyPath, strID))
+	rmsg, err := kvstore.Client().GetValue(path.Join(common.LabelIDKeyPath, strID))
 	if err != nil {
 		return nil, apierror.Error(GetIdentityUnreachableCode, err)
 	}
@@ -215,7 +215,7 @@ func (d *Daemon) LookupIdentity(id policy.NumericIdentity) (*policy.Identity, er
 }
 
 func LookupIdentityBySHA256(sha256sum string) (*policy.Identity, error) {
-	rmsg, err := kvstore.Client.GetValue(path.Join(common.LabelsKeyPath, sha256sum))
+	rmsg, err := kvstore.Client().GetValue(path.Join(common.LabelsKeyPath, sha256sum))
 	if err != nil {
 		return nil, apierror.Error(GetIdentityUnreachableCode, err)
 	}
@@ -329,14 +329,14 @@ func (d *Daemon) DeleteIdentityBySHA256(sha256Sum string, epid string) error {
 	}
 	lblPath := path.Join(common.LabelsKeyPath, sha256Sum)
 	// Lock that sha256Sum
-	lockKey, err := kvstore.Client.LockPath(lblPath)
+	lockKey, err := kvstore.Client().LockPath(lblPath)
 	if err != nil {
 		return err
 	}
 	defer lockKey.Unlock()
 
 	// After lock complete, get label's path
-	rmsg, err := kvstore.Client.GetValue(lblPath)
+	rmsg, err := kvstore.Client().GetValue(lblPath)
 	if err != nil {
 		return err
 	}
@@ -365,7 +365,7 @@ func (d *Daemon) DeleteIdentityBySHA256(sha256Sum string, epid string) error {
 
 	log.Debugf("Decremented label %d ref-count to %d", dbSecCtxLbls.ID, dbSecCtxLbls.RefCount())
 
-	if err := kvstore.Client.SetValue(lblPath, dbSecCtxLbls); err != nil {
+	if err := kvstore.Client().SetValue(lblPath, dbSecCtxLbls); err != nil {
 		return err
 	}
 
@@ -380,6 +380,6 @@ func (d *Daemon) DeleteIdentityBySHA256(sha256Sum string, epid string) error {
 
 // GetMaxLabelID returns the maximum possible free UUID stored in consul.
 func GetMaxLabelID() (policy.NumericIdentity, error) {
-	n, err := kvstore.Client.GetMaxID(common.LastFreeLabelIDKeyPath, policy.MinimalNumericIdentity.Uint32())
+	n, err := kvstore.Client().GetMaxID(common.LastFreeLabelIDKeyPath, policy.MinimalNumericIdentity.Uint32())
 	return policy.NumericIdentity(n), err
 }

daemon/labels_test.go
@@ -82,7 +82,7 @@ func (ds *DaemonSuite) SetUpTest(c *C) {
 	d, err := NewDaemon(daemonConf)
 	c.Assert(err, IsNil)
 	ds.d = d
-	kvstore.Client.DeleteTree(common.OperationalPath)
+	kvstore.Client().DeleteTree(common.OperationalPath)
 	// Needs to be less than 1 second otherwise GetCachedMaxLabelID might
 	// not work properly
 	d.EnableKVStoreWatcher(time.Nanosecond)
@@ -172,7 +172,7 @@ func (ds *DaemonSuite) TestLabels(c *C) {
 	var emptySecCtxLblPtr *policy.Identity
 	c.Assert(gotSecCtxLbl, Equals, emptySecCtxLblPtr)
 
-	err = kvstore.Client.SetMaxID(common.LastFreeLabelIDKeyPath, policy.MinimalNumericIdentity.Uint32(), policy.MinimalNumericIdentity.Uint32())
+	err = kvstore.Client().SetMaxID(common.LastFreeLabelIDKeyPath, policy.MinimalNumericIdentity.Uint32(), policy.MinimalNumericIdentity.Uint32())
 	c.Assert(err, IsNil)
 
 	err = ds.d.DeleteIdentity(policy.MinimalNumericIdentity, "containerLabel1-non-existent")
@@ -214,7 +214,7 @@ func (ds *DaemonSuite) TestLabels(c *C) {
 
 func (ds *DaemonSuite) TestGetMaxID(c *C) {
 	lastID := policy.NumericIdentity(common.MaxSetOfLabels - 1)
-	err := kvstore.Client.SetValue(common.LastFreeLabelIDKeyPath, lastID)
+	err := kvstore.Client().SetValue(common.LastFreeLabelIDKeyPath, lastID)
 	c.Assert(err, IsNil)
 
 	id, err := GetMaxLabelID()

daemon/services.go
@@ -28,7 +28,7 @@ import (
 
 func updateL3n4AddrIDRef(id types.ServiceID, l3n4AddrID types.L3n4AddrID) error {
 	key := path.Join(common.ServiceIDKeyPath, strconv.FormatUint(uint64(id), 10))
-	return kvstore.Client.SetValue(key, l3n4AddrID)
+	return kvstore.Client().SetValue(key, l3n4AddrID)
 }
 
 // gasNewL3n4AddrID gets and sets a new L3n4Addr ID. If baseID is different than zero,
@@ -42,7 +42,7 @@ func gasNewL3n4AddrID(l3n4AddrID *types.L3n4AddrID, baseID uint32) error {
 		}
 	}
 
-	return kvstore.Client.GASNewL3n4AddrID(common.ServiceIDKeyPath, baseID, l3n4AddrID)
+	return kvstore.Client().GASNewL3n4AddrID(common.ServiceIDKeyPath, baseID, l3n4AddrID)
 }
 
 // PutL3n4Addr stores the given service in the kvstore and returns the L3n4AddrID
@@ -56,14 +56,14 @@ func PutL3n4Addr(l3n4Addr types.L3n4Addr, baseID uint32) (*types.L3n4AddrID, err
 	svcPath := path.Join(common.ServicesKeyPath, sha256Sum)
 
 	// Lock that sha256Sum
-	lockKey, err := kvstore.Client.LockPath(svcPath)
+	lockKey, err := kvstore.Client().LockPath(svcPath)
 	if err != nil {
 		return nil, err
 	}
 	defer lockKey.Unlock()
 
 	// After lock complete, get svc's path
-	rmsg, err := kvstore.Client.GetValue(svcPath)
+	rmsg, err := kvstore.Client().GetValue(svcPath)
 	if err != nil {
 		return nil, err
 	}
@@ -79,14 +79,14 @@ func PutL3n4Addr(l3n4Addr types.L3n4Addr, baseID uint32) (*types.L3n4AddrID, err
 		if err := gasNewL3n4AddrID(&sl4KV, baseID); err != nil {
 			return nil, err
 		}
-		err = kvstore.Client.SetValue(svcPath, sl4KV)
+		err = kvstore.Client().SetValue(svcPath, sl4KV)
 	}
 
 	return &sl4KV, err
 }
 
 func getL3n4AddrID(keyPath string) (*types.L3n4AddrID, error) {
-	rmsg, err := kvstore.Client.GetValue(keyPath)
+	rmsg, err := kvstore.Client().GetValue(keyPath)
 	if err != nil {
 		return nil, err
 	}
@@ -133,14 +133,14 @@ func DeleteL3n4AddrIDBySHA256(sha256Sum string) error {
 	}
 	svcPath := path.Join(common.ServicesKeyPath, sha256Sum)
 	// Lock that sha256Sum
-	lockKey, err := kvstore.Client.LockPath(svcPath)
+	lockKey, err := kvstore.Client().LockPath(svcPath)
 	if err != nil {
 		return err
 	}
 	defer lockKey.Unlock()
 
 	// After lock complete, get label's path
-	rmsg, err := kvstore.Client.GetValue(svcPath)
+	rmsg, err := kvstore.Client().GetValue(svcPath)
 	if err != nil {
 		return err
 	}
@@ -160,10 +160,10 @@ func DeleteL3n4AddrIDBySHA256(sha256Sum string) error {
 		return err
 	}
 
-	return kvstore.Client.SetValue(svcPath, l3n4AddrID)
+	return kvstore.Client().SetValue(svcPath, l3n4AddrID)
 }
 
 // GetMaxServiceID returns the maximum possible free UUID stored in the kvstore.
 func GetMaxServiceID() (uint32, error) {
-	return kvstore.Client.GetMaxID(common.LastFreeServiceIDKeyPath, common.FirstFreeServiceID)
+	return kvstore.Client().GetMaxID(common.LastFreeServiceIDKeyPath, common.FirstFreeServiceID)
 }

daemon/services_test.go
@@ -91,7 +91,7 @@ func (ds *DaemonSuite) TestServices(c *C) {
 	err = DeleteL3n4AddrIDByUUID(common.FirstFreeServiceID)
 	c.Assert(err, Equals, nil)
 
-	err = kvstore.Client.SetMaxID(common.LastFreeServiceIDKeyPath, common.FirstFreeServiceID, common.FirstFreeServiceID)
+	err = kvstore.Client().SetMaxID(common.LastFreeServiceIDKeyPath, common.FirstFreeServiceID, common.FirstFreeServiceID)
 	c.Assert(err, Equals, nil)
 
 	err = DeleteL3n4AddrIDByUUID(common.FirstFreeServiceID)
@@ -138,7 +138,7 @@ func (ds *DaemonSuite) TestServices(c *C) {
 
 func (ds *DaemonSuite) TestGetMaxServiceID(c *C) {
 	lastID := uint32(common.MaxSetOfServiceID - 1)
-	err := kvstore.Client.SetValue(common.LastFreeServiceIDKeyPath, lastID)
+	err := kvstore.Client().SetValue(common.LastFreeServiceIDKeyPath, lastID)
 	c.Assert(err, Equals, nil)
 
 	id, err := GetMaxServiceID()

daemon/status.go
@@ -68,7 +68,7 @@ func (h *getHealthz) Handle(params GetHealthzParams) middleware.Responder {
 	d := h.daemon
 	sr := models.StatusResponse{}
 
-	if info, err := kvstore.Client.Status(); err != nil {
+	if info, err := kvstore.Client().Status(); err != nil {
 		sr.Kvstore = &models.Status{State: models.StatusStateFailure, Msg: fmt.Sprintf("Err: %s - %s", err, info)}
 	} else {
 		sr.Kvstore = &models.Status{State: models.StatusStateOk, Msg: info}

pkg/kvstore/client.go
@@ -20,8 +20,13 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-// Client is the instance of the key-value as configured
-var Client KVClient
+var (
+	clientInstance KVClient
+
+	// leaseInstance is the backend specific lease object. The lease is
+	// created by initClient()
+	leaseInstance interface{}
+)
 
 func initClient() error {
 	switch backend {
@@ -36,7 +41,7 @@ func initClient() error {
 		}
 
 		log.Infof("Using consul as key-value store")
-		Client = c
+		clientInstance = c
 
 	case Etcd:
 		if etcdCfgPath == "" && etcdConfig == nil {
@@ -49,11 +54,23 @@ func initClient() error {
 		}
 
 		log.Infof("Using etcd as key-value store")
-		Client = c
+		clientInstance = c
 
 	default:
 		panic("BUG: kvstore backend not specified")
 	}
 
+	l, err := CreateLease(LeaseTTL)
+	if err != nil {
+		clientInstance = nil
+		return fmt.Errorf("Unable to create lease: %s", err)
+	}
+	leaseInstance = l
+
 	return nil
+}
+
+// Client returns the global kvstore client or nil if the client is not configured yet
+func Client() KVClient {
+	return clientInstance
 }

commit af202c0596138c9258a7316fc9fc005ebd30012a

pkg/kvstore: fix high-cpu usage when Cilium loses Consul connectivity
Fixes: 3505d0f1c9c8 ("kvstore: New kvstore abstraction API")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/consul.go
@@ -436,7 +436,7 @@ func (c *consulClient) Watch(w *Watcher) {
 
 		// timeout while watching for changes, re-schedule
 		if qo.WaitIndex != 0 && (q == nil || q.LastIndex == qo.WaitIndex) {
-			continue
+			goto wait
 		}
 
 		for _, newPair := range pairs {
@@ -487,6 +487,7 @@ func (c *consulClient) Watch(w *Watcher) {
 			w.Events <- KeyValueEvent{Typ: EventTypeListDone}
 		}
 
+	wait:
 		select {
 		case <-time.After(sleepTime):
 		case <-w.stopWatch: