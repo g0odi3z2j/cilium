g0odi3z2j/cilium

commit f17e7e08ed000a5059a778790c415da03e93305b

examples: Add MONITOR_AGGREGATION_LEVEL option
Add the MONITOR_AGGREGATION_LEVEL option to the kubernetes YAMLs, to
allow users to specify how much aggregation should occur in the datapath
for monitor output.

Signed-off-by: Joe Stringer <joe@covalent.io>

examples/kubernetes/1.10/cilium-crio.yaml
@@ -29,6 +29,11 @@ data:
   clean-cilium-state: "false"
   legacy-host-allows-world: "false"
 
+  # If you want cilium monitor to aggregate tracing for packets, set this level
+  # to "low", "medium", or "maximum". The higher the level, the less packets
+  # that will be seen in monitor output.
+  monitor-aggregation-level: "none"
+
   # Regular expression matching compatible Istio sidecar istio-proxy
   # container image names
   sidecar-istio-proxy-image: "cilium/istio_proxy"

examples/kubernetes/1.11/cilium-crio.yaml
@@ -29,6 +29,11 @@ data:
   clean-cilium-state: "false"
   legacy-host-allows-world: "false"
 
+  # If you want cilium monitor to aggregate tracing for packets, set this level
+  # to "low", "medium", or "maximum". The higher the level, the less packets
+  # that will be seen in monitor output.
+  monitor-aggregation-level: "none"
+
   # Regular expression matching compatible Istio sidecar istio-proxy
   # container image names
   sidecar-istio-proxy-image: "cilium/istio_proxy"

examples/kubernetes/1.12/cilium-crio.yaml
@@ -29,6 +29,11 @@ data:
   clean-cilium-state: "false"
   legacy-host-allows-world: "false"
 
+  # If you want cilium monitor to aggregate tracing for packets, set this level
+  # to "low", "medium", or "maximum". The higher the level, the less packets
+  # that will be seen in monitor output.
+  monitor-aggregation-level: "none"
+
   # Regular expression matching compatible Istio sidecar istio-proxy
   # container image names
   sidecar-istio-proxy-image: "cilium/istio_proxy"

examples/kubernetes/1.7/cilium-crio.yaml
@@ -29,6 +29,11 @@ data:
   clean-cilium-state: "false"
   legacy-host-allows-world: "false"
 
+  # If you want cilium monitor to aggregate tracing for packets, set this level
+  # to "low", "medium", or "maximum". The higher the level, the less packets
+  # that will be seen in monitor output.
+  monitor-aggregation-level: "none"
+
   # Regular expression matching compatible Istio sidecar istio-proxy
   # container image names
   sidecar-istio-proxy-image: "cilium/istio_proxy"

examples/kubernetes/1.8/cilium-crio.yaml
@@ -29,6 +29,11 @@ data:
   clean-cilium-state: "false"
   legacy-host-allows-world: "false"
 
+  # If you want cilium monitor to aggregate tracing for packets, set this level
+  # to "low", "medium", or "maximum". The higher the level, the less packets
+  # that will be seen in monitor output.
+  monitor-aggregation-level: "none"
+
   # Regular expression matching compatible Istio sidecar istio-proxy
   # container image names
   sidecar-istio-proxy-image: "cilium/istio_proxy"

examples/kubernetes/1.9/cilium-crio.yaml
@@ -29,6 +29,11 @@ data:
   clean-cilium-state: "false"
   legacy-host-allows-world: "false"
 
+  # If you want cilium monitor to aggregate tracing for packets, set this level
+  # to "low", "medium", or "maximum". The higher the level, the less packets
+  # that will be seen in monitor output.
+  monitor-aggregation-level: "none"
+
   # Regular expression matching compatible Istio sidecar istio-proxy
   # container image names
   sidecar-istio-proxy-image: "cilium/istio_proxy"

examples/kubernetes/1.10/cilium-cm.yaml
@@ -29,6 +29,11 @@ data:
   clean-cilium-state: "false"
   legacy-host-allows-world: "false"
 
+  # If you want cilium monitor to aggregate tracing for packets, set this level
+  # to "low", "medium", or "maximum". The higher the level, the less packets
+  # that will be seen in monitor output.
+  monitor-aggregation-level: "none"
+
   # Regular expression matching compatible Istio sidecar istio-proxy
   # container image names
   sidecar-istio-proxy-image: "cilium/istio_proxy"

examples/kubernetes/1.10/cilium-ds.yaml
@@ -112,6 +112,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

commit 278c46d6b0ae73ada2f25665b258f313b443f4fd

kubernetes: Add CILIUM_MONITOR_AGGREGATION_LEVEL to crio-o DaemonSet
Fixes: f17e7e08ed00 ("examples: Add MONITOR_AGGREGATION_LEVEL option")

Signed-off-by: Thomas Graf <thomas@cilium.io>

examples/kubernetes/1.10/cilium-crio-ds.yaml
@@ -113,6 +113,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.10/cilium-crio.yaml
@@ -160,6 +160,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.11/cilium-crio-ds.yaml
@@ -113,6 +113,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.11/cilium-crio.yaml
@@ -160,6 +160,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.12/cilium-crio-ds.yaml
@@ -113,6 +113,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.12/cilium-crio.yaml
@@ -160,6 +160,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.7/cilium-crio-ds.yaml
@@ -113,6 +113,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.7/cilium-crio.yaml
@@ -160,6 +160,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.8/cilium-crio-ds.yaml
@@ -113,6 +113,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.8/cilium-crio.yaml
@@ -160,6 +160,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.9/cilium-crio-ds.yaml
@@ -113,6 +113,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/1.9/cilium-crio.yaml
@@ -160,6 +160,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command:

examples/kubernetes/templates/v1/cilium-crio-ds.yaml.sed
@@ -113,6 +113,12 @@ spec:
                 key: tunnel
                 name: cilium-config
                 optional: true
+          - name: "CILIUM_MONITOR_AGGREGATION_LEVEL"
+            valueFrom:
+              configMapKeyRef:
+                key: monitor-aggregation-level
+                name: cilium-config
+                optional: true
         livenessProbe:
           exec:
             command: