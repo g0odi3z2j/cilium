g0odi3z2j/cilium

commit a8ed4c239763612862ee9fb7f7d12b3636ecd8f1

endpoint: Use controller pattern to sync identity to k8s pod
This resolves a variety issues:
 - No arbitrary number of retries, retry attempts will occur forever
 - The endpoint is properly locked on continued use in the background
 - Synchronization attempts will stop when the endpoint is deleted
 - No overlapping synchronization for an individual endpoint,
   previously, if updates would fail and back off time grows, a previous
   backed off update could still succeed with an old identity value
   after the endpoint of an identity has already been changed.
 - Do not attempt to update k8s pod if endpoint does not represent a k8s
   pod
 - Two go subroutines where spawned for each async operation

Fixes: #2593

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/endpoint/policy.go
@@ -20,10 +20,13 @@ import (
 	"path/filepath"
 	"reflect"
 	"sync"
+	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common"
 	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
@@ -809,6 +812,29 @@ func (e *Endpoint) TriggerPolicyUpdatesLocked(owner Owner, opts models.Configura
 	return changed, ctCleaned, nil
 }
 
+func (e *Endpoint) runIdentityToK8sPodSync() {
+	e.controllers.UpdateController("sync-identity-to-k8s-pod",
+		controller.ControllerParams{
+			DoFunc: func() error {
+				id := ""
+
+				e.Mutex.RLock()
+				if e.SecLabel != nil {
+					id = e.SecLabel.ID.String()
+				}
+				e.Mutex.RUnlock()
+
+				if id != "" && e.GetK8sNamespace() != "" && e.GetK8sPodName() != "" {
+					return k8s.AnnotatePod(e, common.CiliumIdentityAnnotation, id)
+				}
+
+				return nil
+			},
+			RunInterval: time.Duration(1) * time.Minute,
+		},
+	)
+}
+
 // SetIdentity resets endpoint's policy identity to 'id'.
 // Caller triggers policy regeneration if needed.
 // Called with e.Mutex Locked
@@ -841,8 +867,8 @@ func (e *Endpoint) SetIdentity(owner Owner, id *policy.Identity) {
 		e.SetStateLocked(StateReady, "Set identity for this endpoint")
 	}
 
-	// Annotate pod that this endpoint represents with its security identity
-	go owner.AnnotateEndpoint(e, common.CiliumIdentityAnnotation, e.SecLabel.ID.String())
+	e.runIdentityToK8sPodSync()
+
 	e.Consumable.Mutex.RLock()
 	e.getLogger().WithFields(logrus.Fields{
 		logfields.Identity: id,

daemon/daemon.go
@@ -64,7 +64,6 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/vishvananda/netlink"
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -255,62 +254,6 @@ func (d *Daemon) PolicyEnforcement() string {
 // DebugEnabled returns if debug mode is enabled.
 func (d *Daemon) DebugEnabled() bool {
 	return d.conf.Opts.IsEnabled(endpoint.OptionDebug)
-}
-
-// AnnotateEndpoint adds a Kubernetes annotation with key annotationKey and value
-// annotationValue if Kubernetes is being utilized in tandem with Cilium.
-func (d *Daemon) AnnotateEndpoint(e *endpoint.Endpoint, annotationKey, annotationValue string) {
-
-	if !k8s.IsEnabled() {
-		return // Don't error out if k8s is not enabled; treat as a no-op.
-	}
-
-	go func(e *endpoint.Endpoint) {
-		// TODO: Retry forever?
-		n := 0
-		for {
-			// Endpoint's PodName is in the format namespace:pod-name
-			split := strings.Split(e.PodName, ":")
-			if len(split) < 2 {
-				log.WithFields(logrus.Fields{
-					logfields.EndpointID:            e.ID,
-					logfields.K8sPodName:            e.PodName,
-					logfields.K8sIdentityAnnotation: common.CiliumIdentityAnnotation,
-				}).Error("k8s: unable to update pod with annotation: namespace and pod name should be delimited by :")
-				return
-			}
-
-			scopedLog := log.WithFields(logrus.Fields{
-				logfields.EndpointID:            e.ID,
-				logfields.K8sNamespace:          split[0],
-				logfields.K8sPodName:            split[1],
-				logfields.K8sIdentityAnnotation: common.CiliumIdentityAnnotation,
-			})
-
-			pod, err := k8s.Client().CoreV1().Pods(split[0]).Get(split[1], meta_v1.GetOptions{})
-			if err != nil {
-				scopedLog.WithError(err).Error("error getting pod for endpoint")
-				return
-			}
-
-			if pod.Annotations == nil {
-				pod.Annotations = make(map[string]string)
-			}
-			pod.Annotations[annotationKey] = annotationValue
-			pod, err = k8s.Client().CoreV1().Pods(split[0]).Update(pod)
-			if err == nil {
-				scopedLog.Debug("added annotation to endpoint / pods")
-				break
-			}
-
-			scopedLog.Warn("k8s: unable to update  endpoint / pod  with annotation, retrying...")
-
-			if n < 30 {
-				n++
-			}
-			time.Sleep(time.Duration(n) * time.Second)
-		}
-	}(e)
 }
 
 // CleanCTEntries cleans the connection tracking of the given endpoint

daemon/daemon_test.go
@@ -49,7 +49,6 @@ type DaemonSuite struct {
 	OnQueueEndpointBuild              func(r *e.Request)
 	OnRemoveFromEndpointQueue         func(epID uint64)
 	OnDebugEnabled                    func() bool
-	OnAnnotateEndpoint                func(e *e.Endpoint, annotationKey, annotationValue string)
 	OnGetCompilationLock              func() *lock.RWMutex
 	OnCleanCTEntries                  func(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToRm policy.RuleContexts)
 	OnFlushCTEntries                  func(e *e.Endpoint, isCTLocal bool, ips []net.IP, idsToKeep policy.RuleContexts)
@@ -74,14 +73,6 @@ func (ds *DaemonSuite) DryModeEnabled() bool {
 		return ds.OnDryModeEnabled()
 	}
 	panic("DryModeEnabled should not have been called")
-}
-
-func (ds *DaemonSuite) AnnotateEndpoint(e *e.Endpoint, annotationKey, annotationValue string) {
-	if ds.OnAnnotateEndpoint != nil {
-		ds.OnAnnotateEndpoint(e, annotationKey, annotationValue)
-	}
-	panic("OnAnnotateEndpoint should not have been called")
-
 }
 
 func (ds *DaemonSuite) EnableEndpointPolicyEnforcement(e *e.Endpoint) (bool, bool) {

pkg/endpoint/endpoint.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/controller"
 	pkgLabels "github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -308,9 +309,8 @@ type Endpoint struct {
 	// bypass policy while it is still being resolved.
 	PolicyCalculated bool `json:"-"`
 
-	// PodName is the name of the Kubernetes pod if the endpoint is managed
-	// by Kubernetes
-	PodName string
+	k8sPodName   string
+	k8sNamespace string
 
 	// policyRevision is the policy revision this endpoint is currently on
 	policyRevision uint64
@@ -334,6 +334,10 @@ type Endpoint struct {
 	// logger is a logrus object with fields set to report an endpoints information.
 	// You must hold Endpoint.Mutex to read or write it (but not to log with it).
 	logger *logrus.Entry
+
+	// controllers is the list of async controllers syncing the endpoint to
+	// other resources
+	controllers controller.Manager
 }
 
 // NewEndpointWithState creates a new endpoint useful for testing purposes
@@ -460,7 +464,7 @@ func (e *Endpoint) GetModelRLocked() *models.Endpoint {
 		},
 		Mac:            e.LXCMAC.String(),
 		HostMac:        e.NodeMAC.String(),
-		PodName:        e.PodName,
+		PodName:        e.GetK8sNamespaceAndPodNameLocked(),
 		State:          currentState, // TODO: Validate
 		Status:         statusLog,
 		Health:         e.getHealthModel(),
@@ -1270,8 +1274,8 @@ func (e *Endpoint) LeaveLocked(owner Owner) {
 	}
 
 	e.L3Maps.Close()
-
 	e.removeDirectory()
+	e.controllers.RemoveAll()
 
 	e.SetStateLocked(StateDisconnected, "Endpoint removed")
 }
@@ -1314,10 +1318,41 @@ func (e *Endpoint) SetContainerName(name string) {
 	e.Mutex.Unlock()
 }
 
-// SetPodName modifies the endpoint's pod name
-func (e *Endpoint) SetPodName(name string) {
+// GetK8sNamespace returns the name of the pod if the endpoint represents a
+// Kubernetes pod
+func (e *Endpoint) GetK8sNamespace() string {
+	e.Mutex.RLock()
+	defer e.Mutex.RUnlock()
+
+	return e.k8sNamespace
+}
+
+// SetK8sNamespace modifies the endpoint's pod name
+func (e *Endpoint) SetK8sNamespace(name string) {
 	e.Mutex.Lock()
-	e.PodName = name
+	e.k8sNamespace = name
+	e.Mutex.Unlock()
+}
+
+// GetK8sPodName returns the name of the pod if the endpoint represents a
+// Kubernetes pod
+func (e *Endpoint) GetK8sPodName() string {
+	e.Mutex.RLock()
+	defer e.Mutex.RUnlock()
+
+	return e.k8sPodName
+}
+
+// GetK8sNamespaceAndPodNameLocked returns the namespace and pod name.  This
+// function requires e.Mutex to be held.
+func (e *Endpoint) GetK8sNamespaceAndPodNameLocked() string {
+	return e.k8sNamespace + ":" + e.k8sPodName
+}
+
+// SetK8sPodName modifies the endpoint's pod name
+func (e *Endpoint) SetK8sPodName(name string) {
+	e.Mutex.Lock()
+	e.k8sPodName = name
 	e.Mutex.Unlock()
 }
 

pkg/endpoint/owner.go
@@ -71,9 +71,6 @@ type Owner interface {
 	// Returns true if debugging has been enabled
 	DebugEnabled() bool
 
-	// Annotates endpoint e with an annotation with key annotationKey, and value annotationValue.
-	AnnotateEndpoint(e *Endpoint, annotationKey, annotationValue string)
-
 	// GetCompilationLock returns the mutex responsible for synchronizing compilation
 	// of BPF programs.
 	GetCompilationLock() *lock.RWMutex

pkg/endpointmanager/manager.go
@@ -158,8 +158,8 @@ func Remove(ep *endpoint.Endpoint) {
 		delete(endpointsAux, endpoint.NewID(endpoint.ContainerNamePrefix, ep.ContainerName))
 	}
 
-	if ep.PodName != "" {
-		delete(endpointsAux, endpoint.NewID(endpoint.PodNamePrefix, ep.PodName))
+	if podName := ep.GetK8sNamespaceAndPodNameLocked(); podName != "" {
+		delete(endpointsAux, endpoint.NewID(endpoint.PodNamePrefix, podName))
 	}
 }
 
@@ -229,8 +229,8 @@ func updateReferences(ep *endpoint.Endpoint) {
 		endpointsAux[endpoint.NewID(endpoint.ContainerNamePrefix, ep.ContainerName)] = ep
 	}
 
-	if ep.PodName != "" {
-		endpointsAux[endpoint.NewID(endpoint.PodNamePrefix, ep.PodName)] = ep
+	if podName := ep.GetK8sNamespaceAndPodNameLocked(); podName != "" {
+		endpointsAux[endpoint.NewID(endpoint.PodNamePrefix, podName)] = ep
 	}
 }
 

pkg/k8s/annotate.go
@@ -1,0 +1,67 @@
+// Copyright 2016-2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package k8s
+
+import (
+	"fmt"
+
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/uuid"
+
+	"github.com/sirupsen/logrus"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// PodEndpoint is the interface that the endpoint representing a pod has to implement
+type PodEndpoint interface {
+	// GetK8sNamespace returns the name of the namespace
+	GetK8sNamespace() string
+
+	// GetK8sPodName returns the name of the pod
+	GetK8sPodName() string
+
+	// StringID returns the ID of the endpoint
+	StringID() string
+}
+
+// AnnotatePod adds a Kubernetes annotation with key annotationKey and value
+// annotationValue
+func AnnotatePod(e PodEndpoint, annotationKey, annotationValue string) error {
+	scopedLog := log.WithFields(logrus.Fields{
+		logfields.EndpointID:            e.StringID(),
+		logfields.K8sNamespace:          e.GetK8sNamespace(),
+		logfields.K8sPodName:            e.GetK8sPodName(),
+		logfields.K8sIdentityAnnotation: annotationKey,
+		logfields.RetryUUID:             uuid.NewUUID(),
+	})
+
+	pod, err := Client().CoreV1().Pods(e.GetK8sNamespace()).Get(e.GetK8sPodName(), meta_v1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("unable to annotate pod, cannot retrieve pod: %s", err)
+	}
+
+	if pod.Annotations == nil {
+		pod.Annotations = make(map[string]string)
+	}
+
+	pod.Annotations[annotationKey] = annotationValue
+	pod, err = Client().CoreV1().Pods(e.GetK8sNamespace()).Update(pod)
+	if err != nil {
+		return fmt.Errorf("unable to annotate pod, cannot update pod: %s", err)
+	}
+
+	scopedLog.Debug("Successfully annotated pod with %s=%s", annotationKey, annotationValue)
+	return nil
+}

pkg/logging/logfields/logfields.go
@@ -136,6 +136,9 @@ const (
 	// Route is a L2 or L3 Linux route
 	Route = "route"
 
+	// RetryUUID is an UUID identical for all retries of a set
+	RetryUUID = "retryUUID"
+
 	// K8s specific
 
 	// K8sNodeID is the k8s ID of a K8sNode

commit f195dbfb98ef556f02cb7a9a091cfae7168bca81

pkg/endpoint: annotate pod with numeric identity
As the pod annotation value is expected to be numeric, Cilium should
always set that value with a numeric one instead of the string
representation.

Example of the expected output:

$ kubectl get pods -n kube-system kube-dns-7dcc557ddd-vl9s2 -o yaml
apiVersion: v1
kind: Pod
metadata:
  annotations:
    cilium.io/identity: "129"
    scheduler.alpha.kubernetes.io/critical-pod: ""

Fixes: a8ed4c239763 ("endpoint: Use controller pattern to sync identity to k8s pod")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/policy.go
@@ -886,7 +886,7 @@ func (e *Endpoint) runIdentityToK8sPodSync() {
 
 				e.Mutex.RLock()
 				if e.SecurityIdentity != nil {
-					id = e.SecurityIdentity.ID.String()
+					id = e.SecurityIdentity.ID.StringID()
 				}
 				e.Mutex.RUnlock()
 