g0odi3z2j/cilium

commit cdbd318d582140c5dcea3686021aadc01d020907

Daemon: Update Health endpoints IP on localNode.
On commit `52916c2555f5a43153d8b30dcec987d5dea99b86` the GetLocalNode
changed and the values of the Health endpoints were now no updated
correctly into the localnode.

This commit updates HealthIps for localnode.

Fix #5111

Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

pkg/node/local_node.go
@@ -17,8 +17,9 @@ package node
 import (
 	"github.com/cilium/cilium/pkg/option"
 
-	"k8s.io/api/core/v1"
 	"time"
+
+	"k8s.io/api/core/v1"
 )
 
 var localNode Node
@@ -67,4 +68,10 @@ func ConfigureLocalNode() error {
 		}
 	}()
 	return nil
+}
+
+// NotifyLocalNodeUpdated Update local node information in the key-value
+// storage
+func NotifyLocalNodeUpdated() error {
+	return nodeStore.UpdateLocalKeySync(GetLocalNode())
 }

daemon/daemon.go
@@ -1351,10 +1351,20 @@ func NewDaemon() (*Daemon, error) {
 	if err != nil {
 		log.WithError(err).Fatal("Error while allocating cilium-health IP")
 	}
-	node.SetIPv4HealthIP(health4)
-	node.SetIPv6HealthIP(health6)
+
+	err = node.SetIPv4HealthIP(health4)
+	if err != nil {
+		log.WithError(err).Fatal("Error while set health IPv4 ip on the local node.")
+	}
+
+	err = node.SetIPv6HealthIP(health6)
+	if err != nil {
+		log.WithError(err).Fatal("Error while set health IPv6 ip on the local node.")
+	}
+
 	log.Debugf("IPv4 health endpoint address: %s", node.GetIPv4HealthIP())
 	log.Debugf("IPv6 health endpoint address: %s", node.GetIPv6HealthIP())
+	node.NotifyLocalNodeUpdated()
 
 	d.startStatusCollector()
 	d.dnsPoller = fqdn.NewDNSPoller(fqdn.DNSPollerConfig{

pkg/node/node_address.go
@@ -356,8 +356,15 @@ func GetIPv4HealthIP() net.IP {
 }
 
 // SetIPv4HealthIP sets the IPv4 address of the local cilium-health endpoint
-func SetIPv4HealthIP(ip net.IP) {
+func SetIPv4HealthIP(ip net.IP) error {
 	ipv4HealthAddress = ip
+
+	localNode := GetLocalNode()
+	if localNode == nil {
+		return fmt.Errorf("cannot get local node configuration")
+	}
+	localNode.IPv4HealthIP = ip
+	return nil
 }
 
 // GetIPv6HealthIP returns the IPv6 address of the local cilium-health endpoint
@@ -366,8 +373,15 @@ func GetIPv6HealthIP() net.IP {
 }
 
 // SetIPv6HealthIP sets the IPv6 address of the local cilium-health endpoint
-func SetIPv6HealthIP(ip net.IP) {
+func SetIPv6HealthIP(ip net.IP) error {
 	ipv6HealthAddress = ip
+
+	localNode := GetLocalNode()
+	if localNode == nil {
+		return fmt.Errorf("cannot get local node configuration")
+	}
+	localNode.IPv6HealthIP = ip
+	return nil
 }
 
 // GetIPv6NodeRoute returns a route pointing to the IPv6 node address

commit 2c9ce912e3c4ddfa7a2e1cbac2307bd01e8d6ec2

node: Fix panic when node store is not initialized yet
Because node store registration happens in the background, the nodeStore
pointer can still be nil at the time the local node is updated first.

Fixes: cdbd318d5821 ("Daemon: Update Health endpoints IP on localNode.")
Fixes: #5137

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/node/local_node.go
@@ -15,14 +15,19 @@
 package node
 
 import (
-	"github.com/cilium/cilium/pkg/option"
-
+	"sync"
 	"time"
+
+	"github.com/cilium/cilium/pkg/option"
 
 	"k8s.io/api/core/v1"
 )
 
-var localNode Node
+var (
+	localNode      Node
+	nodeRegistered = make(chan struct{})
+	registerOnce   sync.Once
+)
 
 // GetLocalNode returns the identity and node spec for the local node
 func GetLocalNode() *Node {
@@ -33,6 +38,11 @@ func GetLocalNode() *Node {
 // startup to configure the local node based on the configuration options
 // passed to the agent
 func ConfigureLocalNode() error {
+	registerOnce.Do(configureLocalNode)
+	return nil
+}
+
+func configureLocalNode() {
 	localNode = Node{
 		Name:    nodeName,
 		Cluster: option.Config.ClusterName,
@@ -53,7 +63,6 @@ func ConfigureLocalNode() error {
 
 	UpdateNode(&localNode, TunnelRoute, nil)
 
-	nodeRegistered := make(chan struct{})
 	go func() {
 		if err := registerNode(); err != nil {
 			log.WithError(err).Fatal("Unable to initialize local node")
@@ -67,11 +76,15 @@ func ConfigureLocalNode() error {
 			log.Fatalf("Unable to initialize local node due timeout")
 		}
 	}()
-	return nil
 }
 
 // NotifyLocalNodeUpdated Update local node information in the key-value
 // storage
-func NotifyLocalNodeUpdated() error {
-	return nodeStore.UpdateLocalKeySync(GetLocalNode())
+func NotifyLocalNodeUpdated() {
+	go func() {
+		<-nodeRegistered
+		if err := nodeStore.UpdateLocalKeySync(GetLocalNode()); err != nil {
+			log.WithError(err).Error("Unable to propagate local node change to kvstore")
+		}
+	}()
 }