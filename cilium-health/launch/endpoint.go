g0odi3z2j/cilium

commit c33f92a7cb92b7adf4b2c2afaf81e412d8a0dfee

daemon: Controllerize cilium-health endpoint
Add a controller to manage the lifecycle of the cilium-health endpoint.

Fixes: #4242

Signed-off-by: Joe Stringer <joe@covalent.io>

cilium-health/launch/endpoint.go
@@ -30,6 +30,8 @@ import (
 	"github.com/cilium/cilium/pkg/endpoint"
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/endpointmanager"
+	healthPkg "github.com/cilium/cilium/pkg/health/client"
+	"github.com/cilium/cilium/pkg/health/defaults"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -50,6 +52,9 @@ var (
 
 	// healthPidfile
 	healthPidfile = "health-endpoint.pid"
+
+	// client is used to ping the cilium-health endpoint as a health check.
+	client *healthPkg.Client
 )
 
 func logFromCommand(cmd *exec.Cmd, netns string) error {
@@ -112,6 +117,16 @@ func configureHealthRouting(netns, dev string, addressing *models.NodeAddressing
 	return err
 }
 
+// PingEndpoint attempts to make an API ping request to the local cilium-health
+// endpoint, and returns whether this was successful.
+func PingEndpoint() error {
+	if client == nil {
+		return fmt.Errorf("cilium-health endpoint hasn't yet been initialized")
+	}
+	_, err := client.Restapi.GetHello(nil)
+	return err
+}
+
 // CleanupEndpoint attempts to kill any existing cilium-health endpoint and
 // clean up its devices and pidfiles.
 func CleanupEndpoint(owner endpoint.Owner) {
@@ -135,9 +150,10 @@ func CleanupEndpoint(owner endpoint.Owner) {
 // LaunchAsEndpoint launches the cilium-health agent in a nested network
 // namespace and attaches it to Cilium the same way as any other endpoint,
 // but with special reserved labels.
+//
+// CleanupEndpoint() must be called before calling LaunchAsEndpoint() to ensure
+// cleanup of prior cilium-health endpoint instances.
 func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressing) error {
-
-	CleanupEndpoint(owner)
 
 	ip4 := node.GetIPv4HealthIP()
 	ip6 := node.GetIPv6HealthIP()
@@ -214,6 +230,11 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 		if err != nil {
 			return fmt.Errorf("Cannot annotate node CIDR range data: %s", err)
 		}
+	}
+
+	client, err = healthPkg.NewClient(fmt.Sprintf("tcp://%s:%d", ip4, defaults.HTTPPathPort))
+	if err != nil {
+		return fmt.Errorf("Cannot establish connection to health endpoint: %s", err)
 	}
 
 	return nil

daemon/main.go
@@ -30,7 +30,9 @@ import (
 	"github.com/cilium/cilium/api/v1/server/restapi"
 	health "github.com/cilium/cilium/cilium-health/launch"
 	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/envoy"
@@ -692,6 +694,34 @@ func initEnv(cmd *cobra.Command) {
 
 	log.Infof("Container runtime options set: %s", workloads.GetRuntimeOptions())
 }
+
+// runCiliumHealthEndpoint attempts to contact the cilium-health endpoint, and
+// if it cannot be reached, restarts it.
+func runCiliumHealthEndpoint(d *Daemon) error {
+	// PingEndpoint will always fail the first time (initialization).
+	if err := health.PingEndpoint(); err != nil {
+		// Delete the process
+		health.CleanupEndpoint(d)
+		// Clean up agent resources
+		ip6 := node.GetIPv6HealthIP()
+		id := addressing.CiliumIPv6(ip6).EndpointID()
+		ep := endpointmanager.LookupCiliumID(id)
+		if ep == nil {
+			log.WithField(logfields.EndpointID, id).Debug("Didn't find existing cilium-health endpoint to delete")
+		} else {
+			log.Debug("Removing existing cilium-health endpoint")
+			errs := d.deleteEndpointQuiet(ep, false)
+			for _, err := range errs {
+				log.WithError(err).Debug("Error occurred while deleting cilium-health endpoint")
+			}
+		}
+		addressing := d.getNodeAddressing()
+		// Launch new instance
+		return health.LaunchAsEndpoint(d, addressing)
+	}
+	return nil
+}
+
 func runDaemon() {
 	log.Info("Initializing daemon")
 	d, err := NewDaemon()
@@ -718,11 +748,18 @@ func runDaemon() {
 
 	// Launch another cilium-health as an endpoint, managed by cilium.
 	log.Info("Launching Cilium health endpoint")
-	addressing := d.getNodeAddressing()
-	if err = health.LaunchAsEndpoint(d, addressing); err != nil {
-		log.WithError(err).Fatal("Error while launching cilium-health endpoint")
-	}
-	defer health.CleanupEndpoint(d)
+	controller.NewManager().UpdateController("cilium-health-ep",
+		controller.ControllerParams{
+			DoFunc: func() error {
+				return runCiliumHealthEndpoint(d)
+			},
+			StopFunc: func() error {
+				err = health.PingEndpoint()
+				health.CleanupEndpoint(d)
+				return err
+			},
+			RunInterval: 30 * time.Second,
+		})
 
 	eventsCh, err := workloads.EnableEventListener()
 	if err != nil {

commit cc492914338faee3fe7e72fc9062e7a76abfd3db

daemon: Remove health-ep before deleting its devices
Previously, the health endpoint's veth devices would be removed before
it was removed from the endpointmanager. This meant that it was possible
for the health-ep controller to check (and fail to connect to) the health
endpoint then interrupt an ongoing regeneration for the health endpoint.
To avoid this, separate the veth cleanup step from the killing of the
process such that this ordering should be followed when the health
endpoint becomes unresponsive:

* Kill the process
* Remove the endpoint
* Delete the devices
* Re-launch the endpoint

Fixes: c33f92a7cb92 ("daemon: Controllerize cilium-health endpoint")
Fixes: #5152

Signed-off-by: Joe Stringer <joe@covalent.io>

cilium-health/launch/endpoint.go
@@ -133,22 +133,28 @@ func PingEndpoint() error {
 	return err
 }
 
-// CleanupEndpoint attempts to kill any existing cilium-health endpoint and
-// clean up its devices and pidfiles. If any existing cilium-health endpoint
-// exists in Cilium, it is removed from the endpoint manager.
+// KillEndpoint attempts to kill any existing cilium-health endpoint if it
+// exists.
 //
 // This is intended to be invoked in multiple situations:
 // * The health endpoint has never been run before
 // * The health endpoint was run during a previous run of the Cilium agent
 // * The health endpoint crashed during the current run of the Cilium agent
 //   and needs to be cleaned up before it is restarted.
-func CleanupEndpoint(owner endpoint.Owner) {
+func KillEndpoint() {
 	path := filepath.Join(option.Config.StateDir, healthPidfile)
 	if err := pidfile.Kill(path); err != nil {
 		scopedLog := log.WithField(logfields.Path, path).WithError(err)
 		scopedLog.Info("Failed to kill previous cilium-health instance")
 	}
+}
 
+// CleanupEndpoint cleans up remaining resources associated with the health
+// endpoint.
+//
+// This is expected to be called after the process is killed and the endpoint
+// is removed from the endpointmanager.
+func CleanupEndpoint() {
 	scopedLog := log.WithField(logfields.Veth, vethName)
 	if link, err := netlink.LinkByName(vethName); err == nil {
 		err = netlink.LinkDel(link)

daemon/main.go
@@ -767,7 +767,7 @@ func initEnv(cmd *cobra.Command) {
 
 func cleanupHealthEndpoint(d *Daemon) {
 	// Delete the process
-	health.CleanupEndpoint(d)
+	health.KillEndpoint()
 	// Clean up agent resources
 	ip6 := node.GetIPv6HealthIP()
 	id := addressing.CiliumIPv6(ip6).EndpointID()
@@ -781,6 +781,7 @@ func cleanupHealthEndpoint(d *Daemon) {
 			log.WithError(err).Debug("Error occurred while deleting cilium-health endpoint")
 		}
 	}
+	health.CleanupEndpoint()
 }
 
 // runCiliumHealthEndpoint attempts to contact the cilium-health endpoint, and