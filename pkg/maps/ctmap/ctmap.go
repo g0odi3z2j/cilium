g0odi3z2j/cilium

commit 468845486ac2e19da872f8b15dbf6c47e7d9aa10

daemon: Upgrade CT map properties on startup
If the CT map has properties that differ from the target properties as
configured in the daemon code (for example, different value size), then
remove that map during endpoint restore.

When the map is removed, any existing BPF program that refers to the map
should continue to operate as normal. When the endpoints being restored
are regenerated, they will cause automatic recreation of the new
conntrack map (if it doesn't yet exist), and begin to use the new
endpoint.

Note that when an endpoint switches from the old CT map to the new CT
map, as it would in the above case, existing connections may be
temporarily disrupted.

Once all endpoints have finished using the old CT map, the kernel will
automatically relinquish its resources.

Fixes: #5070

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/maps/ctmap/ctmap.go
@@ -100,6 +100,16 @@ func GetMapPath(e CtEndpoint, isIPv6 bool) string {
 	return file
 }
 
+// getMaps fetches all paths for conntrack maps associated with the specified
+// endpoint, and returns a map from these paths to the keySize used for that
+// map.
+func getMapPathsToKeySize(e CtEndpoint) map[string]uint32 {
+	return map[string]uint32{
+		GetMapPath(e, true):  uint32(unsafe.Sizeof(CtKey6{})),
+		GetMapPath(e, false): uint32(unsafe.Sizeof(CtKey4{})),
+	}
+}
+
 type CtType int
 
 // CtKey is the interface describing keys to the conntrack maps.
@@ -478,5 +488,60 @@ func Flush(m *bpf.Map, mapName string) int {
 		return doGC4(m, filter)
 	default:
 		return 0
+	}
+}
+
+// checkAndUpgrade determines whether the ctmap on the filesystem has different
+// map properties than this version of Cilium, and if so, removes the map from
+// the filesystem so that a subsequent BPF prog install will recreate it with
+// the correct map properties.
+//
+// Returns true if the map was upgraded.
+func checkAndUpgrade(m *bpf.Map, e CtEndpoint, keySize uint32) bool {
+	desiredMapInfo := &bpf.MapInfo{
+		MapType:   bpf.MapTypeHash,
+		KeySize:   keySize,
+		ValueSize: uint32(unsafe.Sizeof(CtEntry{})),
+	}
+
+	if e == nil {
+		desiredMapInfo.MaxEntries = MapNumEntriesGlobal
+	} else {
+		desiredMapInfo.MaxEntries = MapNumEntriesLocal
+	}
+
+	return m.CheckAndUpgrade(desiredMapInfo)
+}
+
+// DeleteIfUpgradeNeeded attempts to open the conntrack maps associated with
+// the specified endpoint, and delete the maps from the filesystem if any
+// properties do not match the properties defined in this package.
+//
+// The typical trigger for this is when, for example, the CT entry size changes
+// from one version of Cilium to the next. When Cilium restarts, it may opt
+// to restore endpoints from the prior life. Existing endpoints that use the
+// old map style are incompatible with the new version, so the CT map must be
+// destroyed and recreated during upgrade. By removing the old map location
+// from the filesystem, we ensure that the next time that the endpoint is
+// regenerated, it will recreate a new CT map with the new properties.
+//
+// Note that if an existing BPF program refers to the map at the canonical
+// paths (as fetched via the getMapPathsToKeySize() call below), then that BPF
+// program will continue to operate on the old map, even once the map is
+// removed from the filesystem. The old map will only be completely cleaned up
+// once all referenced to the map are cleared - that is, all BPF programs which
+// refer to the old map and removed/reloaded.
+func DeleteIfUpgradeNeeded(e CtEndpoint) {
+	for path, keySize := range getMapPathsToKeySize(e) {
+		scopedLog := log.WithField(logfields.Path, path)
+		oldMap, err := bpf.OpenMap(path)
+		if err != nil {
+			scopedLog.WithError(err).Debug("Couldn't open CT map for upgrade")
+			continue
+		}
+		if checkAndUpgrade(oldMap, e, keySize) {
+			scopedLog.Info("CT Map upgraded, expect brief disruption of ongoing connections")
+		}
+		oldMap.Close()
 	}
 }

daemon/state.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/maps/ctmap"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/workloads"
@@ -133,11 +134,20 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 	log.Infof("Regenerating %d restored endpoints", len(state.restored))
 
+	// Before regenerating, check whether the CT map has properties that
+	// match this Cilium userspace instance. If not, it must be removed
+	ctmap.DeleteIfUpgradeNeeded(nil)
+
 	// we need to signalize when the endpoints are regenerated, i.e., when
 	// they have finished to rebuild after being restored.
 	epRegenerated := make(chan bool, len(state.restored))
 
 	for _, ep := range state.restored {
+		// If the endpoint has local conntrack option enabled, then
+		// check whether the CT map needs upgrading (and do so).
+		if ep.Options.IsEnabled(option.ConntrackLocal) {
+			ctmap.DeleteIfUpgradeNeeded(ep)
+		}
 
 		// Insert into endpoint manager so it can be regenerated when calls to
 		// TriggerPolicyUpdates() are made. This must be done synchronously (i.e.,

pkg/bpf/map.go
@@ -867,3 +867,20 @@ func (m *Map) resolveErrors() error {
 
 	return nil
 }
+
+// CheckAndUpgrade checks the received map's properties (for the map currently
+// loaded into the kernel) against the desired properties, and if they do not
+// match, deletes the map.
+//
+// Returns true if the map was upgraded.
+func (m *Map) CheckAndUpgrade(desired *MapInfo) bool {
+	return objCheck(
+		m.fd,
+		m.path,
+		int(desired.MapType),
+		desired.KeySize,
+		desired.ValueSize,
+		desired.MaxEntries,
+		desired.Flags,
+	)
+}

commit 71e73579e3b35feac4811e1737bd25f3184b2220

ctmap: Detect support for LRU before upgrading
Fixes: 468845486ac2 ("daemon: Upgrade CT map properties on startup")

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/maps/ctmap/ctmap.go
@@ -499,7 +499,7 @@ func Flush(m *bpf.Map, mapName string) int {
 // Returns true if the map was upgraded.
 func checkAndUpgrade(m *bpf.Map, e CtEndpoint, keySize uint32) bool {
 	desiredMapInfo := &bpf.MapInfo{
-		MapType:   bpf.MapTypeHash,
+		MapType:   bpf.GetLRUMapType(),
 		KeySize:   keySize,
 		ValueSize: uint32(unsafe.Sizeof(CtEntry{})),
 	}