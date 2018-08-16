g0odi3z2j/cilium

commit 09bc1d5226137dd20d038a0be9262635f069e38f

ctmap: Add accessor method for path per endpoint
This code was previously tucked in the endpoint manager, which doesn't
seem right. Refactor it and make it available in the ctmap package so it
can be reused by future commits.

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/endpointmanager/conntrack.go
@@ -16,7 +16,6 @@ package endpointmanager
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 
 	"github.com/cilium/cilium/pkg/bpf"
@@ -44,23 +43,14 @@ func RunGC(e *endpoint.Endpoint, isIPv6 bool, filter *ctmap.GCFilter) {
 	var file string
 	var mapType string
 
-	// Choose whether to garbage collect the local or global conntrack map
-	if e != nil {
-		if isIPv6 {
-			mapType = ctmap.MapName6
-		} else {
-			mapType = ctmap.MapName4
-		}
-		file = bpf.MapPath(mapType + strconv.Itoa(int(e.ID)))
+	// Even if the pointer points to nil, passing it directly to a function
+	// that receives an interface doesn't pass the nil through, so to avoid
+	// a segfault we check the pointer and directly pass nil here.
+	if e == nil {
+		file = ctmap.GetMapPath(nil, isIPv6)
 	} else {
-		if isIPv6 {
-			mapType = ctmap.MapName6Global
-		} else {
-			mapType = ctmap.MapName4Global
-		}
-		file = bpf.MapPath(mapType)
+		file = ctmap.GetMapPath(e, isIPv6)
 	}
-
 	m, err := bpf.OpenMap(file)
 	if err != nil {
 		log.WithError(err).WithField(logfields.Path, file).Warn("Unable to open map")

pkg/maps/ctmap/ctmap.go
@@ -66,6 +66,40 @@ const (
 	deleteEntry
 )
 
+// CtEndpoint represents an endpoint for the functions required to manage
+// conntrack maps for the endpoint.
+type CtEndpoint interface {
+	StringID() string
+}
+
+// GetMapPath returns the path for the CT map for the specified endpoint.
+// Returns the global map path if e is nil.
+func GetMapPath(e CtEndpoint, isIPv6 bool) string {
+	var (
+		file    string
+		mapType string
+	)
+
+	// Choose whether to garbage collect the local or global conntrack map
+	if e != nil {
+		if isIPv6 {
+			mapType = MapName6
+		} else {
+			mapType = MapName4
+		}
+		file = bpf.MapPath(mapType + e.StringID())
+	} else {
+		if isIPv6 {
+			mapType = MapName6Global
+		} else {
+			mapType = MapName4Global
+		}
+		file = bpf.MapPath(mapType)
+	}
+
+	return file
+}
+
 type CtType int
 
 // CtKey is the interface describing keys to the conntrack maps.

commit 299f0c1cc925f735723d9ea829837f3adea59d8f

endpointmanager: Fix conntrack GC
Commit 09bc1d522613 ("ctmap: Add accessor method for path per endpoint")
inadvertently hid the map type (IPv4 or IPv6) from the endpointmanager,
which was relying on this map type to call the correct ctmap garbage
collection function.

This commit does a quick fix to fetch the maptype so that it can be used
in the same way as it was in previous releases. A followup commit will
refactor this to avoid leaking CT map details into the callers.

Fixes: 09bc1d522613 ("ctmap: Add accessor method for path per endpoint")
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/endpointmanager/conntrack.go
@@ -47,9 +47,9 @@ func RunGC(e *endpoint.Endpoint, isIPv6 bool, filter *ctmap.GCFilter) {
 	// that receives an interface doesn't pass the nil through, so to avoid
 	// a segfault we check the pointer and directly pass nil here.
 	if e == nil {
-		file = ctmap.GetMapPath(nil, isIPv6)
+		mapType, file = ctmap.GetMapTypeAndPath(nil, isIPv6)
 	} else {
-		file = ctmap.GetMapPath(e, isIPv6)
+		mapType, file = ctmap.GetMapTypeAndPath(e, isIPv6)
 	}
 	m, err := bpf.OpenMap(file)
 	if err != nil {

pkg/maps/ctmap/ctmap.go
@@ -72,9 +72,9 @@ type CtEndpoint interface {
 	StringID() string
 }
 
-// GetMapPath returns the path for the CT map for the specified endpoint.
-// Returns the global map path if e is nil.
-func GetMapPath(e CtEndpoint, isIPv6 bool) string {
+// GetMapTypeAndPath returns the map type and path for the CT map for the
+// specified endpoint. Returns the global map path if e is nil.
+func GetMapTypeAndPath(e CtEndpoint, isIPv6 bool) (string, string) {
 	var (
 		file    string
 		mapType string
@@ -97,7 +97,12 @@ func GetMapPath(e CtEndpoint, isIPv6 bool) string {
 		file = bpf.MapPath(mapType)
 	}
 
-	return file
+	return mapType, file
+}
+
+func getMapPath(e CtEndpoint, isIPv6 bool) string {
+	_, path := GetMapTypeAndPath(e, isIPv6)
+	return path
 }
 
 // getMaps fetches all paths for conntrack maps associated with the specified
@@ -105,8 +110,8 @@ func GetMapPath(e CtEndpoint, isIPv6 bool) string {
 // map.
 func getMapPathsToKeySize(e CtEndpoint) map[string]uint32 {
 	return map[string]uint32{
-		GetMapPath(e, true):  uint32(unsafe.Sizeof(CtKey6{})),
-		GetMapPath(e, false): uint32(unsafe.Sizeof(CtKey4{})),
+		getMapPath(e, true):  uint32(unsafe.Sizeof(CtKey6{})),
+		getMapPath(e, false): uint32(unsafe.Sizeof(CtKey4{})),
 	}
 }
 