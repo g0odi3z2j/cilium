g0odi3z2j/cilium

commit de8cec72abfe60899b5655fa3b4164691b00933f

envoy: use local_resources parameter during bazel build
This sets the resources for memory, CPU, and I/O for the bazel build.
This avoids errors like the following when building Envoy:

```
23:27:39     virtualbox-iso: ERROR: /home/vagrant/.cache/bazel/_bazel_vagrant/502ef5068e38073dd9828a920a71f484/external/envoy/source/server/http/BUILD:11:1: C++ compilation of rule '@envoy//source/server/http:admin_lib' failed (Exit 4)
23:27:39     virtualbox-iso: gcc: internal compiler error: Killed (program cc1plus)
23:27:39     virtualbox-iso: Please submit a full bug report,
23:27:39     virtualbox-iso: with preprocessed source if appropriate.
23:27:39     virtualbox-iso: See <file:///usr/share/doc/gcc-7/README.Bugs> for instructions.
23:27:39     virtualbox-iso: Target //:envoy failed to build
23:27:39     virtualbox-iso: Use --verbose_failures to see the command lines of failed build steps.
23:27:39     virtualbox-iso: INFO: Elapsed time: 444.469s, Critical Path: 74.62s
23:27:39     virtualbox-iso: INFO: 1544 processes, local.
23:27:39     virtualbox-iso: FAILED: Build did NOT complete successfully
23:27:39     virtualbox-iso: FAILED: Build did NOT complete successfully
23:27:40     virtualbox-iso: make: *** [envoy-release] Error 1
23:27:40     virtualbox-iso: Makefile:68: recipe for target 'envoy-release' failed
23:27:41 ==> virtualbox-iso: Deregistering and deleting VM...
23:27:41 ==> virtualbox-iso: Deleting output directory...
23:27:41 Build 'virtualbox-iso' errored: Script exited with non-zero exit status: 2
```

Signed-off by: Ian Vernon <ian@cilium.io>

envoy/Makefile
@@ -43,11 +43,11 @@ else
 # Dockerfile builds require special options
 ifdef PKG_BUILD
 BAZEL_OPTS ?= --batch
-BAZEL_BUILD_OPTS = --spawn_strategy=standalone --genrule_strategy=standalone
+BAZEL_BUILD_OPTS = --spawn_strategy=standalone --genrule_strategy=standalone --local_resources 2048,2.0,1.0
 all: clean-bins release
 else
 BAZEL_OPTS ?=
-BAZEL_BUILD_OPTS = --experimental_strict_action_env
+BAZEL_BUILD_OPTS = --experimental_strict_action_env --local_resources 2048,2.0,1.0
 all: clean-bins envoy-default api
 endif
 

commit ca6e8dfdfaaccb73ae90ff4dced25571a3b4f08b

envoy: Increase Memory available to Bazel to 4GB, limit jobs to 4
Packer-io-build build fails on gcc internal error. Typically this is
due to gcc running out of memory. Try to see if increasing the memory
available to Bazel fixes this.

This may be caused by behavior change (e.g., a regression) in Bazel
0.16.1.

With 2 available cores Bazel was still running upto 8 parallel build
actions. If all those are C++, they may need more than 0.5 GM RAM
each. Limit the number of parallel builds explicitly with a '--jobs=4'
option.

Fixes: de8cec72abfe60899b5655fa3b4164691b00933f ("envoy: use local_resources parameter during bazel build")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

contrib/vagrant/start.sh
@@ -33,7 +33,7 @@ export 'IPV6_INTERNAL_CIDR'=${IPV4+"FD01::"}
 #   worker 1: FD02::1:0:0/96
 export 'CILIUM_IPV6_NODE_CIDR'=${CILIUM_IPV6_NODE_CIDR:-"FD02::"}
 # VM memory
-export 'VM_MEMORY'=${MEMORY:-3072}
+export 'VM_MEMORY'=${MEMORY:-4096}
 # Number of CPUs
 export 'VM_CPUS'=${CPUS:-2}
 # VM_BASENAME tag is only set if K8S option is active

envoy/Makefile
@@ -45,10 +45,10 @@ else
 
 # Dockerfile builds require special options
 ifdef PKG_BUILD
-BAZEL_BUILD_OPTS = --spawn_strategy=standalone --genrule_strategy=standalone --local_resources 2048,2.0,1.0
+BAZEL_BUILD_OPTS = --spawn_strategy=standalone --genrule_strategy=standalone --local_resources 4096,2.0,1.0 --jobs=4
 all: clean-bins release shutdown-bazel
 else
-BAZEL_BUILD_OPTS = --experimental_strict_action_env --local_resources 2048,2.0,1.0
+BAZEL_BUILD_OPTS = --experimental_strict_action_env --local_resources 4096,2.0,1.0 --jobs=4
 all: clean-bins envoy-default api shutdown-bazel
 endif
 

ginkgo-kubernetes-all.Jenkinsfile
@@ -33,7 +33,7 @@ pipeline {
 
     environment {
         PROJ_PATH = "src/github.com/cilium/cilium"
-        MEMORY = "3072"
+        MEMORY = "4096"
         TESTDIR="${WORKSPACE}/${PROJ_PATH}/test"
         GOPATH="${WORKSPACE}"
         SERVER_BOX = "cilium/ubuntu"

ginkgo.Jenkinsfile
@@ -8,7 +8,7 @@ pipeline {
     environment {
         PROJ_PATH = "src/github.com/cilium/cilium"
         TESTDIR = "${WORKSPACE}/${PROJ_PATH}/"
-        MEMORY = "3072"
+        MEMORY = "4096"
         SERVER_BOX = "cilium/ubuntu"
     }
 