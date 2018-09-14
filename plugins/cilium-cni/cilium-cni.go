g0odi3z2j/cilium

commit c9dacb15e32ed47ad81c131d45820f3cefdebfa8

Ignore non-existing link error in cni del
Signed-off-by: Maciej Kwiek <maciej@covalent.io>

plugins/cilium-cni/cilium-cni.go
@@ -478,7 +478,7 @@ func cmdDel(args *skel.CmdArgs) error {
 		log.WithError(err).Warn("Deletion of endpoint failed")
 	}
 
-	return ns.WithNetNSPath(args.Netns, func(_ ns.NetNS) error {
-		return link.DeleteByName(args.IfName)
+	return ns.WithNetNSPath(args.Netns, func(netNs ns.NetNS) error {
+		return removeIfFromNSIfExists(netNs, args.IfName)
 	})
 }

commit ccea5f334205aab001f2b9a0074f8d141678b91c

cni: use right network NS while removing link
WithNetNSPath(removeIfFromNSIfExists) == removeIfFromNSIfExists(hostNS,
..). removeIfFromNSIfExists runs in expected network namespace but
it gets hostNS as a argument

Fixes: c9dacb15e32e ("Ignore non-existing link error in cni del")

Signed-off-by: Nirmoy Das <ndas@suse.de>

plugins/cilium-cni/cilium-cni.go
@@ -478,7 +478,11 @@ func cmdDel(args *skel.CmdArgs) error {
 		log.WithError(err).Warn("Deletion of endpoint failed")
 	}
 
-	return ns.WithNetNSPath(args.Netns, func(netNs ns.NetNS) error {
-		return removeIfFromNSIfExists(netNs, args.IfName)
-	})
+	netNs, err := ns.GetNS(args.Netns)
+	if err != nil {
+		return fmt.Errorf("failed to open netns %q: %s", args.Netns, err)
+	}
+	defer netNs.Close()
+
+	return removeIfFromNSIfExists(netNs, args.IfName)
 }