g0odi3z2j/cilium

commit 43edf53526f5cc108a62266a7217d9546c56a7e2

bpf: Rework monitor tracing to specify capture len
Previously, the 'monitor' argument to the tracing function would only
allow to trace (true) or not (false). Modify it to use this parameter as
a trace length so that an upcoming change can dynamically choose how
much of the packet to send into the monitor channel.

Signed-off-by: Joe Stringer <joe@covalent.io>

bpf/lib/conntrack.h
@@ -200,7 +200,7 @@ static inline int __inline__ __ct_lookup(void *map, struct __sk_buff *skb,
 					 void *tuple, int action, int dir,
 					 struct ct_state *ct_state,
 					 bool is_tcp, union tcp_flags seen_flags,
-					 bool *monitor)
+					 __u32 *monitor)
 {
 	struct ct_entry *entry;
 	int ret;
@@ -248,7 +248,7 @@ static inline int __inline__ __ct_lookup(void *map, struct __sk_buff *skb,
 			else
 				entry->tx_closing = 1;
 
-			*monitor = true;
+			*monitor = TRACE_PAYLOAD_LEN;
 			if (ct_entry_alive(entry))
 				break;
 			__ct_update_timeout(entry, CT_CLOSE_TIMEOUT, dir, seen_flags);
@@ -258,7 +258,7 @@ static inline int __inline__ __ct_lookup(void *map, struct __sk_buff *skb,
 		return CT_ESTABLISHED;
 	}
 
-	*monitor = true;
+	*monitor = TRACE_PAYLOAD_LEN;
 	return CT_NEW;
 }
 
@@ -287,7 +287,7 @@ static inline void __inline__ ipv6_ct_tuple_reverse(struct ipv6_ct_tuple *tuple)
 /* Offset must point to IPv6 */
 static inline int __inline__ ct_lookup6(void *map, struct ipv6_ct_tuple *tuple,
 					struct __sk_buff *skb, int l4_off, int dir,
-					struct ct_state *ct_state, bool *monitor)
+					struct ct_state *ct_state, __u32 *monitor)
 {
 	int ret = CT_NEW, action = ACTION_UNSPEC;
 	bool is_tcp = tuple->nexthdr == IPPROTO_TCP;
@@ -441,7 +441,7 @@ static inline void ct4_cilium_dbg_tuple(struct __sk_buff *skb, __u8 type,
 /* Offset must point to IPv4 header */
 static inline int __inline__ ct_lookup4(void *map, struct ipv4_ct_tuple *tuple,
 					struct __sk_buff *skb, int off, int dir,
-					struct ct_state *ct_state, bool *monitor)
+					struct ct_state *ct_state, __u32 *monitor)
 {
 	int ret = CT_NEW, action = ACTION_UNSPEC;
 	bool is_tcp = tuple->nexthdr == IPPROTO_TCP;
@@ -746,14 +746,14 @@ static inline int __inline__ ct_create4(void *map, struct ipv4_ct_tuple *tuple,
 #else /* !CONNTRACK */
 static inline int __inline__ ct_lookup6(void *map, struct ipv6_ct_tuple *tuple,
 					struct __sk_buff *skb, int off, int dir,
-					struct ct_state *ct_state, bool *monitor)
+					struct ct_state *ct_state, __u32 *monitor)
 {
 	return 0;
 }
 
 static inline int __inline__ ct_lookup4(void *map, struct ipv4_ct_tuple *tuple,
 					struct __sk_buff *skb, int off, int dir,
-					struct ct_state *ct_state, bool *monitor)
+					struct ct_state *ct_state, __u32 *monitor)
 {
 	return 0;
 }

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=4dc0edbdede9f597c5b49624fc464004f65ce90f
+GO_BINDATA_SHA1SUM=41f34972e641c74e1f8cb438eab72f1dcff174f7
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

bpf/bpf_lxc.c
@@ -96,7 +96,7 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	void *data, *data_end;
 	union v6addr *daddr, orig_dip;
 	__u32 tunnel_endpoint = 0;
-	bool monitor = false;
+	__u32 monitor = 0;
 
 	if (unlikely(!is_valid_lxc_src_mac(eth)))
 		return DROP_INVALID_SMAC;
@@ -213,7 +213,7 @@ skip_service_lookup:
 		ret = ct_create6(&CT_MAP6, tuple, skb, CT_EGRESS, &ct_state_new);
 		if (IS_ERR(ret))
 			return ret;
-		monitor = true;
+		monitor = TRACE_PAYLOAD_LEN;
 		break;
 
 	case CT_ESTABLISHED:
@@ -421,7 +421,7 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 	struct ct_state ct_state = {};
 	__be32 orig_dip;
 	__u32 tunnel_endpoint = 0;
-	bool monitor = false;
+	__u32 monitor = 0;
 
 	if (!revalidate_data(skb, &data, &data_end, &ip4))
 		return DROP_INVALID;
@@ -725,8 +725,9 @@ ipv6_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 	int ret, l4_off, verdict, hdrlen;
 	struct ct_state ct_state = {};
 	struct ct_state ct_state_new = {};
-	bool skip_proxy, monitor = false;
+	bool skip_proxy = false;
 	union v6addr orig_dip = {};
+	__u32 monitor = 0;
 
 	if (!revalidate_data(skb, &data, &data_end, &ip6))
 		return DROP_INVALID;
@@ -869,9 +870,10 @@ ipv4_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 	int ret, verdict, l4_off;
 	struct ct_state ct_state = {};
 	struct ct_state ct_state_new = {};
-	bool skip_proxy, monitor = false;
+	bool skip_proxy = false;
 	__be32 orig_dip, orig_sip;
 	bool is_fragment = false;
+	__u32 monitor = 0;
 
 	if (!revalidate_data(skb, &data, &data_end, &ip4))
 		return DROP_INVALID;

bpf/lib/encap.h
@@ -24,7 +24,7 @@
 #ifdef ENCAP_IFINDEX
 static inline int __inline__
 encap_and_redirect_with_nodeid(struct __sk_buff *skb, __u32 tunnel_endpoint,
-			       __u32 seclabel, bool monitor)
+			       __u32 seclabel, __u32 monitor)
 {
 	struct bpf_tunnel_key key = {};
 	__u32 node_id;
@@ -48,7 +48,7 @@ encap_and_redirect_with_nodeid(struct __sk_buff *skb, __u32 tunnel_endpoint,
 
 static inline int __inline__
 encap_and_redirect(struct __sk_buff *skb, struct endpoint_key *k,
-		   __u32 seclabel, bool monitor)
+		   __u32 seclabel, __u32 monitor)
 {
 	struct endpoint_key *tunnel;
 

bpf/lib/lb.h
@@ -428,7 +428,7 @@ static inline int __inline__ lb6_local(void *map, struct __sk_buff *skb, int l3_
 				       struct ipv6_ct_tuple *tuple, struct lb6_service *svc,
 				       struct ct_state *state)
 {
-	bool monitor; // Deliberately ignored; regular CT will determine monitoring.
+	__u32 monitor; // Deliberately ignored; regular CT will determine monitoring.
 	union v6addr *addr;
 	__u8 flags = tuple->flags;
 	int ret;
@@ -703,7 +703,7 @@ static inline int __inline__ lb4_local(void *map, struct __sk_buff *skb,
 				       struct ipv4_ct_tuple *tuple, struct lb4_service *svc,
 				       struct ct_state *state, __be32 saddr)
 {
-	bool monitor; // Deliberately ignored; regular CT will determine monitoring.
+	__u32 monitor; // Deliberately ignored; regular CT will determine monitoring.
 	__be32 new_saddr = 0, new_daddr;
 	__u8 flags = tuple->flags;
 	int ret;

bpf/lib/lxc.h
@@ -97,7 +97,7 @@ static inline int __inline__
 ipv4_redirect_to_host_port(struct __sk_buff *skb, struct csum_offset *csum,
 			  int l4_off, __be16 new_port, __be16 old_port, __be32 old_ip,
 			  struct ipv4_ct_tuple *tuple, __u32 identity,
-			  int forwarding_reason, bool monitor)
+			  int forwarding_reason, __u32 monitor)
 {
 	__be32 host_ip = IPV4_GATEWAY;
 	struct proxy4_tbl_key key = {
@@ -150,7 +150,7 @@ static inline int __inline__
 ipv6_redirect_to_host_port(struct __sk_buff *skb, struct csum_offset *csum,
 			  int l4_off, __be16 new_port, __be16 old_port,
 			  union v6addr old_ip, struct ipv6_ct_tuple *tuple, union v6addr *host_ip,
-			  __u32 identity, int forwarding_reason, bool monitor)
+			  __u32 identity, int forwarding_reason, __u32 monitor)
 {
 	struct proxy6_tbl_key key = {
 		.saddr = tuple->daddr,

bpf/lib/trace.h
@@ -89,13 +89,13 @@ struct trace_notify {
  * @dst_id:	designated destination endpoint ID
  * @ifindex:	designated destination ifindex
  * @reason:	reason for forwarding the packet (TRACE_REASON_*)
- * @monitor:	whether to send a notification (true) or only metrics (false)
+ * @monitor:	length of notification to send (0 means don't send)
  *
  * Generate a notification to indicate a packet was forwarded at an observation point.
  */
 static inline void
 send_trace_notify(struct __sk_buff *skb, __u8 obs_point, __u32 src, __u32 dst,
-		  __u16 dst_id, __u32 ifindex, __u8 reason, bool monitor)
+		  __u16 dst_id, __u32 ifindex, __u8 reason, __u32 monitor)
 {
 	switch (obs_point) {
 		case TRACE_TO_LXC:
@@ -131,7 +131,9 @@ send_trace_notify(struct __sk_buff *skb, __u8 obs_point, __u32 src, __u32 dst,
 	if (MONITOR_AGGREGATION >= TRACE_AGGREGATE_ACTIVE_CT && !monitor)
 		return;
 
-	uint64_t skb_len = (uint64_t)skb->len, cap_len = min((uint64_t)TRACE_PAYLOAD_LEN, (uint64_t)skb_len);
+	if (!monitor)
+		monitor = TRACE_PAYLOAD_LEN;
+	uint64_t skb_len = (uint64_t)skb->len, cap_len = min((uint64_t)monitor, (uint64_t)skb_len);
 	uint32_t hash = get_hash_recalc(skb);
 	struct trace_notify msg = {
 		.type = CILIUM_NOTIFY_TRACE,
@@ -155,7 +157,7 @@ send_trace_notify(struct __sk_buff *skb, __u8 obs_point, __u32 src, __u32 dst,
 #else
 
 static inline void send_trace_notify(struct __sk_buff *skb, __u8 obs_point, __u32 src, __u32 dst,
-				     __u16 dst_id, __u32 ifindex, __u8 reason, bool monitor)
+				     __u16 dst_id, __u32 ifindex, __u8 reason, __u32 monitor)
 {
 	switch (obs_point) {
 		case TRACE_TO_LXC:

commit abfa112df1f16910ccc28af3ce94c697b9951999

bpf: Fix only monitoring one byte of packets
Under higher MonitorAggregationLevels, the logic which determines when
packets should be monitored or not inadvertently truncated the monitor
packet size to 1B instead of TRACE_PAYLOAD_LEN. Fix this.

Fixes: 43edf53526f5 ("bpf: Rework monitor tracing to specify capture len")

Signed-off-by: Joe Stringer <joe@covalent.io>

bpf/lib/conntrack.h
@@ -114,13 +114,17 @@ union tcp_flags {
  * updates of the entry and returning true for each CPU. The BPF architecture
  * guarantees that entire 8-bit or 32-bit values will be set within the entry,
  * so although the CPUs may race, the worst result is that multiple executions
- * of this function return 'true' for the same connection within short
+ * of this function return non-zero for the same connection within short
  * succession, leading to multiple trace notifications being sent when one
  * might otherwise expect such notifications to be aggregated.
+ *
+ * Returns how many bytes of the packet should be monitored:
+ * - Zero if this flow was recently monitored.
+ * - Non-zero if this flow has not been monitored recently.
  */
-static inline bool __inline__ __ct_update_timeout(struct ct_entry *entry,
-						  __u32 lifetime, int dir,
-						  union tcp_flags flags)
+static inline __u32 __inline__ __ct_update_timeout(struct ct_entry *entry,
+						   __u32 lifetime, int dir,
+						   union tcp_flags flags)
 {
 	__u32 now = bpf_ktime_get_sec();
 	__u8 *accumulated_flags;
@@ -173,9 +177,9 @@ static inline bool __inline__ __ct_update_timeout(struct ct_entry *entry,
 	    *accumulated_flags != seen_flags) {
 		*last_report = now;
 		*accumulated_flags = seen_flags;
-		return true;
+		return TRACE_PAYLOAD_LEN;
 	}
-	return false;
+	return 0;
 }
 
 /**

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=83e0bb81d9c516bb6b2365ced9fd8b342690a9e5
+GO_BINDATA_SHA1SUM=87929b5009d2af06eb4548e180a9bed476576eb9
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 