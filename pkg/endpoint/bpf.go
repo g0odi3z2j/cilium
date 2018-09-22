g0odi3z2j/cilium

commit 363fb547fdb030a830c48d19135d35df8b0bf7cb

endpoint: Skip compilation if unneeded
When the periodic IPCache garbage collector replaces the LPM map on
kernels 4.11-4.14, it only requires a datapath reload and not a complete
recompilation. Skip compilation in this case.

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/endpoint/bpf.go
@@ -549,16 +549,20 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir string, regenContext *Regene
 		closeChan := loadinfo.LogPeriodicSystemLoad(log.WithFields(logrus.Fields{logfields.EndpointID: epID}).Debugf, time.Second)
 
 		// Compile and install BPF programs for this endpoint
-		stats.bpfCompilation.Start()
 		ctx, cancel := context.WithTimeout(context.Background(), ExecTimeout)
-		err = loader.CompileAndLoad(ctx, epInfoCache)
-		stats.bpfCompilation.End()
+		if bpfHeaderfilesChanged {
+			stats.bpfCompilation.Start()
+			err = loader.CompileAndLoad(ctx, epInfoCache)
+			stats.bpfCompilation.End()
+			e.Logger().WithError(err).
+				WithField(logfields.BPFCompilationTime, stats.bpfCompilation.Total().String()).
+				Info("Recompiled endpoint BPF program")
+		} else {
+			err = loader.ReloadDatapath(ctx, epInfoCache)
+			e.Logger().WithError(err).Info("Reloaded endpoint BPF program")
+		}
 		cancel()
 		close(closeChan)
-
-		e.Logger().WithError(err).
-			WithField(logfields.BPFCompilationTime, stats.bpfCompilation.Total().String()).
-			Info("Recompiled endpoint BPF program")
 
 		if err != nil {
 			return epInfoCache.revision, compilationExecuted, err

pkg/datapath/loader/loader.go
@@ -79,12 +79,7 @@ func compileDatapath(ctx context.Context, ep endpoint, dirs *directoryInfo, debu
 	return nil
 }
 
-func compileAndLoad(ctx context.Context, ep endpoint, dirs *directoryInfo) error {
-	debug := viper.GetBool(option.BPFCompileDebugName)
-	if err := compileDatapath(ctx, ep, dirs, debug); err != nil {
-		return err
-	}
-
+func reloadDatapath(ctx context.Context, ep endpoint, dirs *directoryInfo) error {
 	// Replace the current program
 	objPath := path.Join(dirs.Output, endpointObj)
 	if err := replaceDatapath(ctx, ep.InterfaceName(), objPath, symbolFromEndpoint); err != nil {
@@ -97,6 +92,15 @@ func compileAndLoad(ctx context.Context, ep endpoint, dirs *directoryInfo) error
 	}
 
 	return nil
+}
+
+func compileAndLoad(ctx context.Context, ep endpoint, dirs *directoryInfo) error {
+	debug := viper.GetBool(option.BPFCompileDebugName)
+	if err := compileDatapath(ctx, ep, dirs, debug); err != nil {
+		return err
+	}
+
+	return reloadDatapath(ctx, ep, dirs)
 }
 
 // CompileAndLoad compiles the BPF datapath programs for the specified endpoint
@@ -114,4 +118,13 @@ func CompileAndLoad(ctx context.Context, ep endpoint) error {
 		Output:  ep.StateDir(),
 	}
 	return compileAndLoad(ctx, ep, &dirs)
+}
+
+func ReloadDatapath(ctx context.Context, ep endpoint) error {
+	dirs := directoryInfo{
+		Library: option.Config.BpfDir,
+		Runtime: option.Config.StateDir,
+		Output:  ep.StateDir(),
+	}
+	return reloadDatapath(ctx, ep, &dirs)
 }

commit 871b4d41577061bd4b8f4e41478efcc0fc037bb5

endpoint: Reload using existing compiled prog path
We observed that the wrong path was being used for reloading BPF
programs without compilation: ReloadDatapath() was attempting to load
from the new "xxxxx_next" directory rather than the existing state path.

Fixes: 363fb547fdb0 ("endpoint: Skip compilation if unneeded")
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/endpoint/bpf.go
@@ -359,7 +359,7 @@ func (e *Endpoint) removeOldRedirects(owner Owner, desiredRedirects map[string]b
 // Must be called with endpoint.Mutex not held and endpoint.BuildMutex held.
 // Returns the policy revision number when the regeneration has called, a
 // boolean if the BPF compilation was executed and an error in case of an error.
-func (e *Endpoint) regenerateBPF(owner Owner, epdir string, regenContext *RegenerationContext) (revnum uint64, compiled bool, reterr error) {
+func (e *Endpoint) regenerateBPF(owner Owner, currentDir, nextDir string, regenContext *RegenerationContext) (revnum uint64, compiled bool, reterr error) {
 	var (
 		err                 error
 		compilationExecuted bool
@@ -411,7 +411,7 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir string, regenContext *Regene
 			return 0, compilationExecuted, err
 		}
 
-		if err = e.writeHeaderfile(epdir, owner); err != nil {
+		if err = e.writeHeaderfile(nextDir, owner); err != nil {
 			return 0, compilationExecuted, fmt.Errorf("Unable to write header file: %s", err)
 		}
 
@@ -501,14 +501,14 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir string, regenContext *Regene
 
 	// Generate header file specific to this endpoint for use in compiling
 	// BPF programs for this endpoint.
-	if err = e.writeHeaderfile(epdir, owner); err != nil {
+	if err = e.writeHeaderfile(nextDir, owner); err != nil {
 		e.Unlock()
 		return 0, compilationExecuted, fmt.Errorf("unable to write header file: %s", err)
 	}
 
 	// Avoid BPF program compilation and installation if the headerfile for the endpoint
 	// or the node have not changed.
-	bpfHeaderfilesHash, err := hashEndpointHeaderfiles(epdir)
+	bpfHeaderfilesHash, err := hashEndpointHeaderfiles(nextDir)
 	var bpfHeaderfilesChanged bool
 	if err != nil {
 		e.Logger().WithError(err).Warn("Unable to hash header file")
@@ -522,7 +522,12 @@ func (e *Endpoint) regenerateBPF(owner Owner, epdir string, regenContext *Regene
 
 	// Cache endpoint information
 	// TODO (ianvernon): why do we need to do this?
-	epInfoCache := e.createEpInfoCache(epdir)
+	var epInfoCache *epInfoCache
+	if bpfHeaderfilesChanged {
+		epInfoCache = e.createEpInfoCache(nextDir)
+	} else {
+		epInfoCache = e.createEpInfoCache(currentDir)
+	}
 	if epInfoCache == nil {
 		e.Unlock()
 		err = fmt.Errorf("Unable to cache endpoint information")

pkg/endpoint/policy.go
@@ -765,7 +765,7 @@ func (e *Endpoint) regenerate(owner Owner, context *RegenerationContext) (retErr
 		e.Unlock()
 	}()
 
-	revision, compilationExecuted, err = e.regenerateBPF(owner, tmpDir, context)
+	revision, compilationExecuted, err = e.regenerateBPF(owner, origDir, tmpDir, context)
 	if err != nil {
 		failDir := e.FailedDirectoryPath()
 		e.Logger().WithFields(logrus.Fields{