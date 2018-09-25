g0odi3z2j/cilium

commit 967e44a5e62a5c1654e31694a8f9afd87ccc51fb

add consistent usage of logfields.LogSubsys
Use this field consistently across the cilium codebase to allow
logs to be more easily filtered and searched.

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/endpoint/log.go
@@ -21,7 +21,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var log = logging.DefaultLogger
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "endpoint")
 
 // logger returns a logrus object with EndpointID, ContainerID and the Endpoint
 // revision fields.
@@ -61,7 +61,7 @@ func (e *Endpoint) updateLogger() {
 	}
 
 	// default to using the log var set above
-	baseLogger := log
+	baseLogger := log.Logger
 
 	// If this endpoint is set to debug ensure it will print debug by giving it
 	// an independent logger

pkg/envoy/xds/log.go
@@ -14,6 +14,9 @@
 
 package xds
 
-import "github.com/cilium/cilium/pkg/logging"
+import (
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
 
-var log = logging.DefaultLogger
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "xds")

pkg/fqdn/log.go
@@ -16,6 +16,7 @@ package fqdn
 
 import (
 	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
-var log = logging.DefaultLogger
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "fqdn")

cilium-health/cmd/root.go
@@ -43,7 +43,7 @@ var (
 	client    *clientPkg.Client
 	cmdRefDir string
 	server    *serverPkg.Server
-	log       = logging.DefaultLogger
+	log       = logging.DefaultLogger.WithField(logfields.LogSubsys, targetName)
 )
 
 // rootCmd represents the base command when called without any subcommands

cilium-health/launch/launcher.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cilium/cilium/pkg/health/defaults"
 	"github.com/cilium/cilium/pkg/launcher"
 	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
 // CiliumHealth is used to wrap the node executable binary.
@@ -34,7 +35,7 @@ type CiliumHealth struct {
 }
 
 var (
-	log = logging.DefaultLogger
+	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "cilium-health-launcher")
 
 	connectRetryInterval = 1 * time.Second
 	statusProbeInterval  = 5 * time.Second

common/types/loadbalancer.go
@@ -29,7 +29,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var log = logging.DefaultLogger
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "loadbalancer")
 
 const (
 	NONE = L4Type("NONE")

daemon/main.go
@@ -82,7 +82,7 @@ const (
 )
 
 var (
-	log = logging.DefaultLogger
+	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "daemon")
 
 	bootstrapTimestamp = time.Now()
 

monitor/launch/launcher.go
@@ -29,9 +29,10 @@ import (
 	"github.com/cilium/cilium/pkg/launcher"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
-var log = logging.DefaultLogger
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "monitor-launcher")
 
 const (
 	targetName = "cilium-node-monitor"

commit a379fbb309d7edb3c7f5747265f54e26bb24b000

endpoint: Fix debug logging hook
Previously, in many circumstances the endpoint logs wouldn't properly
hit the errors_warnings metrics and this message is printed when running
in debug mode:

  Failed to fire hook: log entry doesn't contain 'subsys' field:
  level=warning msg="Regeneration of endpoint failed" ...

Fix this by properly associating a subsystem with the endpoint logger.

Fixes: #5679
Fixes: 967e44a5e62a ("add consistent usage of logfields.LogSubsys")

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/endpoint/log.go
@@ -25,7 +25,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "endpoint")
+var (
+	Subsystem = "endpoint"
+	log       = logging.DefaultLogger.WithField(logfields.LogSubsys, Subsystem)
+)
 
 // Logger returns a logrus object with EndpointID, ContainerID and the Endpoint
 // revision fields.
@@ -76,6 +79,7 @@ func (e *Endpoint) UpdateLogger(fields map[string]interface{}) {
 	// When adding new fields, make sure they are abstracted by a setter
 	// and update the logger when the value is set.
 	l := baseLogger.WithFields(logrus.Fields{
+		logfields.LogSubsys:              Subsystem,
 		logfields.EndpointID:             e.ID,
 		logfields.ContainerID:            e.getShortContainerID(),
 		logfields.DatapathPolicyRevision: e.policyRevision,