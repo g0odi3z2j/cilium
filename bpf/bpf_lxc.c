g0odi3z2j/cilium

commit 449c9d974e820fa6234b679a140776f5ee1c2e80

bpf: Do not redirect replies from a pod to a proxyport.
Replies going into a pod are already not redirected to a proxy. Do the
same for replies going out of a pod. Only original direction packets
should be redirected to a proxy.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

bpf/bpf_lxc.c
@@ -106,9 +106,9 @@ get_ct_map4(struct ipv4_ct_tuple *tuple)
 	return &CT_MAP_ANY4;
 }
 
-static inline bool redirect_to_proxy(int verdict)
+static inline bool redirect_to_proxy(int verdict, int dir)
 {
-	return verdict > 0;
+	return verdict > 0 && (dir == CT_NEW || dir == CT_ESTABLISHED);
 }
 
 static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
@@ -265,7 +265,7 @@ skip_service_lookup:
 		return DROP_POLICY;
 	}
 
-	if (redirect_to_proxy(verdict)) {
+	if (redirect_to_proxy(verdict, ret)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 		union v6addr host_ip = {};
 
@@ -569,7 +569,7 @@ skip_service_lookup:
 		return DROP_POLICY;
 	}
 
-	if (redirect_to_proxy(verdict)) {
+	if (redirect_to_proxy(verdict, ret)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 
 		ret = ipv4_redirect_to_host_port(skb, &csum_off, l4_off,
@@ -839,7 +839,7 @@ ipv6_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 		/* NOTE: tuple has been invalidated after this */
 	}
 
-	if (redirect_to_proxy(verdict) && (ret == CT_NEW || ret == CT_ESTABLISHED)) {
+	if (redirect_to_proxy(verdict, ret)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 		union macaddr router_mac = NODE_MAC;
 		union v6addr host_ip = {};
@@ -973,7 +973,7 @@ ipv4_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 		/* NOTE: tuple has been invalidated after this */
 	}
 
-	if (redirect_to_proxy(verdict) && (ret == CT_NEW || ret == CT_ESTABLISHED)) {
+	if (redirect_to_proxy(verdict, ret)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 		union macaddr router_mac = NODE_MAC;
 

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=adaaa63d3fe66d8621006f069baa3ffa38c5f37f
+GO_BINDATA_SHA1SUM=1fd0bb17ed1a729e240ac07f77ed8b72e8d27e2b
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

commit f568ae81113dc24d0fc5d2576784a0b37e1893ef

bpf: Use 'forwarding_reason' instead of potentially overwritten 'ret'
'ret' is overwritten in some code paths, so use 'forwarding_reason' instead.

Fixes: 449c9d974e82 ("bpf: Do not redirect replies from a pod to a proxyport.")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

bpf/bpf_lxc.c
@@ -265,7 +265,7 @@ skip_service_lookup:
 		return DROP_POLICY;
 	}
 
-	if (redirect_to_proxy(verdict, ret)) {
+	if (redirect_to_proxy(verdict, forwarding_reason)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 		union v6addr host_ip = {};
 
@@ -569,7 +569,7 @@ skip_service_lookup:
 		return DROP_POLICY;
 	}
 
-	if (redirect_to_proxy(verdict, ret)) {
+	if (redirect_to_proxy(verdict, forwarding_reason)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 
 		ret = ipv4_redirect_to_host_port(skb, &csum_off, l4_off,
@@ -839,7 +839,7 @@ ipv6_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 		/* NOTE: tuple has been invalidated after this */
 	}
 
-	if (redirect_to_proxy(verdict, ret)) {
+	if (redirect_to_proxy(verdict, *forwarding_reason)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 		union macaddr router_mac = NODE_MAC;
 		union v6addr host_ip = {};
@@ -973,7 +973,7 @@ ipv4_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 		/* NOTE: tuple has been invalidated after this */
 	}
 
-	if (redirect_to_proxy(verdict, ret)) {
+	if (redirect_to_proxy(verdict, *forwarding_reason)) {
 		union macaddr host_mac = HOST_IFINDEX_MAC;
 		union macaddr router_mac = NODE_MAC;
 

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=1fd0bb17ed1a729e240ac07f77ed8b72e8d27e2b
+GO_BINDATA_SHA1SUM=374def1b2ed1ed39b660a30ff93e9b735e628dd9
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 