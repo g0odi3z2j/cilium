g0odi3z2j/cilium

commit 73a0b51c44bfd9d079e6bf596e9baf0f31faae5b

travis: change target to run unit-tests
Signed-off-by: André Martins <andre@cilium.io>

.travis.yml
@@ -3,8 +3,8 @@ language: go
 dist: trusty
 sudo: required
 
-#go:
-# - tip
+go:
+ - 1.11.1
 
 addons:
   apt:
@@ -15,5 +15,6 @@ before_install: ./.travis/prepare.sh
 
 before_script:
   - export PATH=/usr/local/clang/bin:$PATH
+  - export GO_NOQUIET=/home/travis/.gimme/versions/go1.11.1.linux.amd64/bin/go
 
 script: ./.travis/build.sh

Makefile
@@ -2,9 +2,9 @@ include Makefile.defs
 include daemon/bpf.sha
 
 SUBDIRS = proxylib envoy plugins bpf cilium daemon monitor cilium-health bugtool tools
-GOFILES ?= $(subst _$(ROOT_DIR)/,,$(shell go list ./... | grep -v -e /vendor/ -e /contrib/ -e envoy/envoy))
-TESTPKGS ?= $(subst _$(ROOT_DIR)/,,$(shell go list ./... | grep -v -e /api/v1 -e /vendor/ -e /contrib/ -e envoy/envoy -e test))
-GOLANGVERSION = $(shell go version 2>/dev/null | grep -Eo '(go[0-9].[0-9])')
+GOFILES ?= $(subst _$(ROOT_DIR)/,,$(shell $(GO) list ./... | grep -v -e /vendor/ -e /contrib/ -e envoy/envoy))
+TESTPKGS ?= $(subst _$(ROOT_DIR)/,,$(shell $(GO) list ./... | grep -v -e /api/v1 -e /vendor/ -e /contrib/ -e envoy/envoy -e test))
+GOLANGVERSION = $(shell $(GO) version 2>/dev/null | grep -Eo '(go[0-9].[0-9])')
 GOLANG_SRCFILES=$(shell for pkg in $(subst github.com/cilium/cilium/,,$(GOFILES)); do find $$pkg -name *.go -print; done | grep -v vendor)
 BPF_FILES ?= $(shell git ls-files ../bpf/ | tr "\n" ' ')
 BPF_SRCFILES=$(subst ../,,$(BPF_FILES))
@@ -15,8 +15,9 @@ SWAGGER_VERSION = 0.12.0
 SWAGGER = $(DOCKER) run --rm -v $(CURDIR):$(CURDIR) -w $(CURDIR) -e GOPATH=$(GOPATH) --entrypoint swagger quay.io/goswagger/swagger:$(SWAGGER_VERSION)
 
 COVERPKG ?= ./...
-GOTEST_OPTS = -test.v -check.vv -timeout 360s -coverprofile=coverage.out -covermode=count -coverpkg $(COVERPKG)
-GOTEST_PRIV_OPTS = $(GOTEST_OPTS) -tags=privileged_tests
+GOTEST_BASE = -test.v -check.vv -timeout 360s
+GOTEST_PRIV_OPTS = $(GOTEST_BASE) -tags=privileged_tests
+GOTEST_COVER_OPTS = -coverprofile=coverage.out -covermode=count -coverpkg $(COVERPKG)
 
 UTC_DATE=$(shell date -u "+%Y-%m-%d")
 
@@ -49,17 +50,10 @@ TEST_LDFLAGS=-ldflags "-X github.com/cilium/cilium/pkg/kvstore.consulDummyAddres
 
 # invoked from ginkgo compose file after starting kvstore backends
 tests-ginkgo-real:
-	echo "mode: count" > coverage-all.out
-	echo "mode: count" > coverage.out
 	$(QUIET)$(foreach pkg,$(TESTPKGS),\
-		go test $(TEST_LDFLAGS) $(pkg) $(GOTEST_OPTS) || exit 1; \
-		tail -n +2 coverage.out >> coverage-all.out;)
+		$(GO) test $(TEST_LDFLAGS) $(pkg) $(GOTEST_BASE) || exit 1;)
 	$(QUIET)$(foreach pkg,$(TESTPKGS),\
-		go test $(TEST_LDFLAGS) $(pkg) $(GOTEST_PRIV_OPTS) || exit 1; \
-		tail -n +2 coverage.out >> coverage-all.out;)
-	$(GO) tool cover -html=coverage-all.out -o=coverage-all.html
-	rm coverage-all.out
-	rm coverage.out
+		$(GO) test $(TEST_LDFLAGS) $(pkg) $(GOTEST_PRIV_OPTS) || exit 1;)
 	@rmdir ./daemon/1 ./daemon/1_backup 2> /dev/null || true
 
 tests-envoy: proxylib
@@ -94,10 +88,10 @@ unit-tests: start-kvstores
 	$(QUIET) echo "mode: count" > coverage-all.out
 	$(QUIET) echo "mode: count" > coverage.out
 	$(QUIET)$(foreach pkg,$(TESTPKGS),\
-		go test $(pkg) $(GOTEST_OPTS) || exit 1; \
+		$(GO) test $(pkg) $(GOTEST_BASE) $(GOTEST_COVER_OPTS) || exit 1; \
 		tail -n +2 coverage.out >> coverage-all.out;)
 	$(QUIET)$(foreach pkg,$(TESTPKGS),\
-		sudo -E go test $(pkg) $(GOTEST_PRIV_OPTS) || exit 1; \
+		sudo $(GO) test $(pkg) $(GOTEST_PRIV_OPTS) $(GOTEST_COVER_OPTS) || exit 1; \
 		tail -n +2 coverage.out >> coverage-all.out;)
 	$(GO) tool cover -html=coverage-all.out -o=coverage-all.html
 	$(QUIET) rm coverage.out
@@ -216,7 +210,7 @@ release:
 	git archive --format tar $(BRANCH) | gzip > ../cilium_$(VERSION).orig.tar.gz
 
 gofmt:
-	for pkg in $(GOFILES); do go fmt $$pkg; done
+	for pkg in $(GOFILES); do $(GO) fmt $$pkg; done
 
 govet:
 	@$(ECHO_CHECK) vetting all GOFILES...

Makefile.defs
@@ -8,7 +8,9 @@ RUNDIR?=/var/run
 CONFDIR?=/etc
 
 # GO_NOQUIET should not have any Makefile directives.
-GO_NOQUIET = go
+ifeq ($(GO_NOQUIET),)
+	GO_NOQUIET = go
+endif
 GO = $(QUIET)$(GO_NOQUIET)
 INSTALL = $(QUIET)install
 

.travis/build.sh
@@ -7,7 +7,8 @@ llc --version
 export CFLAGS="-Werror"
 export CGO_CFLAGS="-DCI_BUILD"
 
-make
-make -B tests
+make unit-tests
+
+$HOME/gopath/bin/goveralls -coverprofile=coverage-all.out -service=travis-ci -repotoken ${COVERALLS_REPO_TOKEN}
 
 exit 0

.travis/prepare.sh
@@ -13,3 +13,7 @@ rm $CLANG_FILE
 
 NEWPATH="/usr/local/clang/bin"
 export PATH="$NEWPATH:$PATH"
+
+go get -u github.com/jteeuwen/go-bindata/...
+go get golang.org/x/tools/cmd/cover
+go get github.com/mattn/goveralls

commit 36a67b68ac2a05cf295df9b8b6ad220d761a7204

Makefiles: Fix up GO references
Previously, GO was defined to include the quiet make directive '@' if
V=0 is specified on the commandline. However, this caused confusion
because it forced GO to only be used directly from Makefile statements
rather than inside nested shell statements (which are commonly used in
Makefiles across the codebase).

Standardize GO to mean only the binary and update all references across
the tree to provide $(QUIET) beforehand directly if they are guaranteed
to be Makefile statements.

Fixes: 73a0b51c44bf ("travis: change target to run unit-tests")
Signed-off-by: Joe Stringer <joe@cilium.io>

.travis.yml
@@ -20,6 +20,6 @@ before_install: ./.travis/prepare.sh
 
 before_script:
   - export PATH=/usr/local/clang/bin:$PATH
-  - export GO_NOQUIET=/home/travis/.gimme/versions/go1.11.1.linux.amd64/bin/go
+  - export GO=/home/travis/.gimme/versions/go1.11.1.linux.amd64/bin/go
 
 script: ./.travis/build.sh

Makefile
@@ -93,7 +93,7 @@ unit-tests: start-kvstores
 	$(QUIET)$(foreach pkg,$(TESTPKGS),\
 		sudo $(GO) test $(pkg) $(GOTEST_PRIV_OPTS) $(GOTEST_COVER_OPTS) || exit 1; \
 		tail -n +2 coverage.out >> coverage-all.out;)
-	$(GO) tool cover -html=coverage-all.out -o=coverage-all.html
+	$(QUIET)$(GO) tool cover -html=coverage-all.out -o=coverage-all.html
 	$(QUIET) rm coverage.out
 	@rmdir ./daemon/1 ./daemon/1_backup 2> /dev/null || true
 	$(DOCKER) rm -f "cilium-etcd-test-container"
@@ -212,11 +212,11 @@ release:
 	git archive --format tar $(BRANCH) | gzip > ../cilium_$(VERSION).orig.tar.gz
 
 gofmt:
-	for pkg in $(GOFILES); do $(GO) fmt $$pkg; done
+	$(QUIET)for pkg in $(GOFILES); do $(GO) fmt $$pkg; done
 
 govet:
 	@$(ECHO_CHECK) vetting all GOFILES...
-	$(GO) tool vet api pkg test $(SUBDIRS)
+	$(QUIET)$(GO) tool vet api pkg test $(SUBDIRS)
 
 ineffassign:
 	@$(ECHO_CHECK) ineffassign
@@ -243,20 +243,20 @@ pprof-help:
 	@echo "  pprof-mutex"
 
 pprof-heap:
-	$(GO) tool pprof http://localhost:6060/debug/pprof/heap
+	$(QUIET)$(GO) tool pprof http://localhost:6060/debug/pprof/heap
 
 pprof-profile:
-	$(GO) tool pprof http://localhost:6060/debug/pprof/profile
+	$(QUIET)$(GO) tool pprof http://localhost:6060/debug/pprof/profile
 
 
 pprof-block:
-	$(GO) tool pprof http://localhost:6060/debug/pprof/block
+	$(QUIET)$(GO) tool pprof http://localhost:6060/debug/pprof/block
 
 pprof-trace-5s:
 	curl http://localhost:6060/debug/pprof/trace?seconds=5
 
 pprof-mutex:
-	$(GO) tool pprof http://localhost:6060/debug/pprof/mutex
+	$(QUIET)$(GO) tool pprof http://localhost:6060/debug/pprof/mutex
 
 update-authors:
 	@echo "Updating AUTHORS file..."

Makefile.defs
@@ -7,11 +7,9 @@ LIBDIR?=$(PREFIX)/lib
 RUNDIR?=/var/run
 CONFDIR?=/etc
 
-# GO_NOQUIET should not have any Makefile directives.
-ifeq ($(GO_NOQUIET),)
-	GO_NOQUIET = go
+ifeq ($(GO),)
+	GO = go
 endif
-GO = $(QUIET)$(GO_NOQUIET)
 INSTALL = $(QUIET)install
 
 VERSION = $(shell cat $(dir $(lastword $(MAKEFILE_LIST)))/VERSION)

bugtool/Makefile
@@ -18,14 +18,14 @@ TARGET=cilium-bugtool
 SOURCES := $(shell find ../common . -name '*.go')
 $(TARGET): $(SOURCES)
 	@$(ECHO_GO)
-	$(GO) build $(GOBUILD) -o $(TARGET)
+	$(QUIET)$(GO) build $(GOBUILD) -o $(TARGET)
 
 all: $(TARGET)
 
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
 	-$(QUIET)rm -f .cilium-bugtool.config $(TARGET)
-	$(GO) clean
+	$(QUIET)$(GO) clean
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)

cilium-health/Makefile
@@ -4,14 +4,14 @@ TARGET=cilium-health
 SOURCES := $(shell find ../api/v1/health ../pkg/health cmd . \( -name '*.go' ! -name '*_test.go' \))
 $(TARGET): $(SOURCES)
 	@$(ECHO_GO)
-	$(GO) build $(GOBUILD) -o $(TARGET)
+	$(QUIET)$(GO) build $(GOBUILD) -o $(TARGET)
 
 all: $(TARGET)
 
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
 	-$(QUIET)rm -f $(TARGET)
-	$(GO) clean
+	$(QUIET)$(GO) clean
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)

cilium/Makefile
@@ -5,14 +5,14 @@ TARGET=cilium
 SOURCES := $(shell find ../api ../daemon ../common ../pkg cmd . \( -name '*.go' ! -name '*_test.go' \))
 $(TARGET): $(SOURCES)
 	@$(ECHO_GO)
-	$(GO) build $(GOBUILD) -o $(TARGET)
+	$(QUIET)$(GO) build $(GOBUILD) -o $(TARGET)
 
 all: $(TARGET)
 
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
 	-$(QUIET)rm -f $(TARGET)
-	$(GO) clean
+	$(QUIET)$(GO) clean
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)

daemon/Makefile
@@ -11,7 +11,7 @@ TARGET=cilium-agent
 SOURCES := $(shell find ../api ../common ../daemon ../pkg ../monitor . \( -name '*.go'  ! -name '*_test.go' \))
 $(TARGET): $(SOURCES) check-bindata
 	@$(ECHO_GO)
-	$(GO) build $(GOBUILD) -o $(TARGET)
+	$(QUIET)$(GO) build $(GOBUILD) -o $(TARGET)
 
 GO_BINDATA := $(QUIET) go-bindata -prefix ../ -mode 0640 -modtime 1450269211 \
 	-ignore Makefile -ignore bpf_features.h -ignore lxc_config.h \
@@ -24,7 +24,7 @@ all: $(TARGET)
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
 	$(QUIET)rm -f $(TARGET)
-	$(GO) clean
+	$(QUIET)$(GO) clean
 
 ifeq ("$(PKG_BUILD)","")
 

monitor/Makefile
@@ -1,4 +1,4 @@
-# Copyright 2017 Authors of Cilium
+# Copyright 2017-2018 Authors of Cilium
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,14 +18,14 @@ TARGET=cilium-node-monitor
 SOURCES := $(shell find ../monitor ../common ../pkg ../api . \( -name '*.go' ! -name '*_test.go' \))
 $(TARGET): $(SOURCES)
 	@$(ECHO_GO)
-	$(GO) build $(GOBUILD) -o $(TARGET)
+	$(QUIET)$(GO) build $(GOBUILD) -o $(TARGET)
 
 all: $(TARGET)
 
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
 	$(QUIET)rm -f $(TARGET)
-	$(GO) clean
+	$(QUIET)$(GO) clean
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)

plugins/cilium-cni/Makefile
@@ -6,7 +6,7 @@ TARGET=cilium-cni
 
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
-	$(GO) clean
+	$(QUIET)$(GO) clean
 	-$(QUIET)rm -f $(TARGET)
 
 SOURCES := $(shell find ../../api/v1/models ../../common ../../pkg/client ../../pkg/endpoint . -name '*.go')
@@ -14,7 +14,7 @@ SOURCES := $(shell find ../../api/v1/models ../../common ../../pkg/client ../../
 $(TARGET): $(SOURCES)
 	@$(ECHO_GO)
 	# Compile without cgo to allow use of cilium-cni on non-glibc platforms - see GH-5055
-	$(QUIET)CGO_ENABLED=0 $(GO_NOQUIET) build $(GOBUILD) -o $(TARGET) ./cilium-cni.go
+	$(QUIET)CGO_ENABLED=0 $(GO) build $(GOBUILD) -o $(TARGET) ./cilium-cni.go
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)/etc/cni/net.d

plugins/cilium-docker/Makefile
@@ -8,14 +8,14 @@ SOURCES := $(shell find ../../api/v1 ../../common ../../pkg/client ../../pkg/end
 
 $(TARGET): $(SOURCES)
 	@$(ECHO_GO)
-	$(GO) build $(GOBUILD) -o $(TARGET)
+	$(QUIET)$(GO) build $(GOBUILD) -o $(TARGET)
 
 run:
 	./cilium-docker -d
 
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
-	$(GO) clean
+	$(QUIET)$(GO) clean
 	-$(QUIET) rm -f $(TARGET)
 
 install:

proxylib/Makefile
@@ -5,14 +5,14 @@ TARGET=libcilium.so
 DEPS := $(shell find ../pkg accesslog npds test . \( -name '*.go' ! -name '*_test.go' \))
 $(TARGET): $(DEPS)
 	@$(ECHO_GO)
-	$(GO) build $(GOBUILD) -o $@ -buildmode=c-shared
+	$(QUIET)$(GO) build $(GOBUILD) -o $@ -buildmode=c-shared
 
 all: $(TARGET)
 
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
 	-$(QUIET)rm -f $(TARGET)
-	$(GO) clean
+	$(QUIET)$(GO) clean
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)$(LIBDIR)

tools/ring-dump/Makefile
@@ -4,14 +4,14 @@ TARGET=cilium-ring-dump
 SOURCES := $(shell find ../../pkg/bpf cmd . \( -name '*.go' ! -name '*_test.go' \))
 $(TARGET): $(SOURCES)
 	@$(ECHO_GO)
-	$(GO) build $(GOBUILD) -o $(TARGET)
+	$(QUIET)$(GO) build $(GOBUILD) -o $(TARGET)
 
 all: $(TARGET)
 
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
 	-$(QUIET)rm -f $(TARGET)
-	$(GO) clean
+	$(QUIET)$(GO) clean
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)

test/bpf/Makefile
@@ -14,7 +14,7 @@ all: $(TARGETS)
 
 perf-event-test: perf-event-test.go
 	@$(ECHO_GO)
-	$(GO) build $(GOBUILD) -o $@ $<
+	$(QUIET)$(GO) build $(GOBUILD) -o $@ $<
 
 bpf-event-test.o: bpf-event-test.c
 	@$(ECHO_CC)