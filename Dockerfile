g0odi3z2j/cilium

commit c07078cce43368c6e0f5c4dc5adcd9424db7b417

envoy: Make Envoy dependency version explicit.
Add a new variable `envoy.RequiredEnvoyVersionSHA` and check that the
running Envoy version matches that instead of the current Cilium SHA.

This requires that Envoy version dependency is manually updated, but
allows Cilium SHA to advance without requiring to rebuild Envoy.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

Dockerfile
@@ -1,4 +1,13 @@
 #
+# cilium-envoy from github.com/cilium/proxy
+#
+# This ARG is used in FROM below and must be placed before ALL FROM lines!
+# Otherwise its value is not passed in.
+#
+ARG CILIUM_ENVOY_SHA
+FROM cilium/cilium-envoy:${CILIUM_ENVOY_SHA:-latest} as cilium-envoy
+
+#
 # Cilium incremental build. Should be fast given builder-deps is up-to-date!
 #
 # cilium-builder tag is the date on which the compatible build image
@@ -8,7 +17,7 @@
 # versions to be built while allowing the new versions to make changes
 # that are not backwards compatible.
 #
-FROM quay.io/cilium/cilium-builder:2018-10-29 as builder
+FROM quay.io/cilium/cilium-builder:2018-11-01 as builder
 LABEL maintainer="maintainer@cilium.io"
 WORKDIR /go/src/github.com/cilium/cilium
 COPY . ./
@@ -33,6 +42,7 @@ RUN make LOCKDEBUG=$LOCKDEBUG PKG_BUILD=1 V=$V SKIP_DOCS=true DESTDIR=/tmp/insta
 FROM quay.io/cilium/cilium-runtime:2018-10-29
 LABEL maintainer="maintainer@cilium.io"
 COPY --from=builder /tmp/install /
+COPY --from=cilium-envoy / /
 COPY plugins/cilium-cni/cni-install.sh /cni-install.sh
 COPY plugins/cilium-cni/cni-uninstall.sh /cni-uninstall.sh
 WORKDIR /root

envoy/Makefile
@@ -14,173 +14,23 @@
 
 include ../Makefile.defs
 
-CILIUM_ENVOY_BIN = ./bazel-bin/envoy
-ISTIO_ENVOY_BIN = ./bazel-bin/istio-envoy
-ISTIO_ENVOY_RELEASE_BIN = ./istio-envoy
-ENVOY_BINS = \
-	$(CILIUM_ENVOY_BIN) \
-	$(ISTIO_ENVOY_BIN) \
-	$(ISTIO_ENVOY_RELEASE_BIN) \
-	./bazel-bin/cilium_integration_test
-CHECK_FORMAT ?= ./bazel-bin/check_format.py.runfiles/envoy/tools/check_format.py
-
-SHELL=/bin/bash -o pipefail
-BAZEL ?= $(QUIET) bazel
-BAZEL_FILTER ?= 2>&1 | grep -v -e "bazel-out/.*/genfiles/external/.*: warning: directory does not exist."
-BAZEL_OPTS ?=
-BAZEL_TEST_OPTS ?= --jobs=1
-BAZEL_CACHE ?= ~/.cache/bazel
-BAZEL_ARCHIVE ?= ~/bazel-cache.tar.bz2
-CLANG ?= clang
-CLANG_FORMAT ?= clang-format
-BUILDIFIER ?= buildifier
-STRIP ?= $(QUIET) strip
-
-ISTIO_VERSION = 1.0.2
-
-ifdef CILIUM_DISABLE_ENVOY_BUILD
-all install clean:
-	echo "Envoy build is disabled by environment variable CILIUM_DISABLE_ENVOY_BUILD"
-else
-
 # Dockerfile builds require special options
 ifdef PKG_BUILD
-BAZEL_BUILD_OPTS = --spawn_strategy=standalone --genrule_strategy=standalone --local_resources 4096,2.0,1.0 --jobs=3
-all: clean-bins release shutdown-bazel
+
+all:
+	echo "cilium-envoy has been moved to github.com/cilium/proxy"
+
+install:
+	echo "cilium-envoy must be installed separately"
+
 else
-BAZEL_BUILD_OPTS = --experimental_strict_action_env --local_resources 4096,2.0,1.0 --jobs=3
-all: clean-bins envoy-default api shutdown-bazel
-endif
 
-ifdef KEEP_BAZEL_RUNNING
-shutdown-bazel:
-else
-shutdown-bazel:
-	$(BAZEL) shutdown
-endif
+all:
+	echo "cilium-envoy has been moved to github.com/cilium/proxy"
 
-debug: envoy-debug api
-
-release: envoy-release api
-
-api: force-non-root Makefile.api
-	$(MAKE) -f Makefile.api all
-
-proxylib-hdrs: ../proxylib/libcilium.h ../proxylib/proxylib/types.h
-	-mkdir proxylib
-	cp $^ proxylib/.
-
-proxylib-bin: ../proxylib/libcilium.so
-	-mkdir proxylib
-	cp $^ proxylib/.
-
-envoy-default: force-non-root proxylib-hdrs
-	@$(ECHO_BAZEL)
-	-rm -f bazel-out/k8-fastbuild/bin/_objs/envoy/external/envoy/source/common/common/version_linkstamp.o
-	$(BAZEL) $(BAZEL_OPTS) build $(BAZEL_BUILD_OPTS) //:envoy $(BAZEL_FILTER)
-
-# Allow root build for release
-$(CILIUM_ENVOY_BIN) envoy-release: force proxylib-hdrs
-	@$(ECHO_BAZEL)
-	-rm -f bazel-out/k8-opt/bin/_objs/envoy/external/envoy/source/common/common/version_linkstamp.o
-	$(BAZEL) $(BAZEL_OPTS) build $(BAZEL_BUILD_OPTS) -c opt //:envoy $(BAZEL_FILTER)
-
-# Allow root build for release
-$(ISTIO_ENVOY_BIN) $(ISTIO_ENVOY_RELEASE_BIN): force proxylib-hdrs
-	@$(ECHO_BAZEL)
-	-rm -f bazel-out/k8-opt/bin/_objs/istio-envoy/external/envoy/source/common/common/version_linkstamp.o
-	$(BAZEL) $(BAZEL_OPTS) build $(BAZEL_BUILD_OPTS) -c opt //:istio-envoy $(BAZEL_FILTER)
-	$(STRIP) -o $(ISTIO_ENVOY_RELEASE_BIN) $(ISTIO_ENVOY_BIN)
-
-Dockerfile.%: Dockerfile.%.in
-	-sed "s/@ISTIO_VERSION@/$(ISTIO_VERSION)/" <$< >$@
-
-docker-istio-proxy: Dockerfile.istio_proxy $(ISTIO_ENVOY_RELEASE_BIN) envoy_bootstrap_tmpl.json
-	-docker build -f $< -t cilium/istio_proxy:$(ISTIO_VERSION) .
-
-docker-istio-proxy-debug: Dockerfile.istio_proxy_debug $(ISTIO_ENVOY_RELEASE_BIN) envoy_bootstrap_tmpl.json
-	-docker build -f $< -t cilium/istio_proxy_debug:$(ISTIO_VERSION) .
-
-envoy-debug: force-non-root proxylib-hdrs
-	@$(ECHO_BAZEL)
-	-rm -f bazel-out/k8-dbg/bin/_objs/envoy/external/envoy/source/common/common/version_linkstamp.o
-	$(BAZEL) $(BAZEL_OPTS) build $(BAZEL_BUILD_OPTS) -c dbg //:envoy $(BAZEL_FILTER)
-
-$(CHECK_FORMAT): force-non-root
-	$(BAZEL) $(BAZEL_OPTS) build $(BAZEL_BUILD_OPTS) //:check_format.py
-
-install: force-root
-	$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)
-	$(INSTALL) -m 0755 -T $(CILIUM_ENVOY_BIN) $(DESTDIR)$(BINDIR)/cilium-envoy
-# Strip only non-debug builds
-ifeq "$(findstring -dbg,$(realpath bazel-bin))" ""
-	$(STRIP) $(DESTDIR)$(BINDIR)/cilium-envoy
-endif
-
-bazel-archive: force-non-root tests clean-bins
-	-sudo rm -f $(BAZEL_ARCHIVE)
-	echo "Archiving bazel cache ($(BAZEL_CACHE)), this will take some time..."
-	cd $(dir $(BAZEL_CACHE)) && sudo tar cjf $(BAZEL_ARCHIVE) --atime-preserve=system $(notdir $(BAZEL_CACHE))
-
-bazel-clean-archive: force-non-root veryclean bazel-archive
-
-bazel-restore: $(BAZEL_ARCHIVE)
-	echo "Clearing & restoring bazel archive ($(BAZEL_ARCHIVE))..."
-	-sudo rm -Rf $(BAZEL_CACHE)
-	-mkdir $(dir $(BAZEL_CACHE))
-	cd $(dir $(BAZEL_CACHE)) && sudo tar xjf $(BAZEL_ARCHIVE) --warning=no-timestamp
-
-# Remove the binaries to get fresh version SHA
-clean-bins: force
-	@$(ECHO_CLEAN) $(notdir $(shell pwd))
-	-$(QUIET) rm -f $(ENVOY_BINS) \
-		Dockerfile.istio_proxy \
-		Dockerfile.istio_proxy_debug
-
-clean: force clean-bins
-	@$(ECHO_CLEAN) $(notdir $(shell pwd))
-	@echo "Bazel clean skipped, try 'make veryclean' instead."
-
-veryclean: force clean-bins
-	-sudo $(BAZEL) $(BAZEL_OPTS) clean
-	-sudo rm -Rf $(BAZEL_CACHE)
-
-check: $(CHECK_FORMAT) force-non-root
-	CLANG_FORMAT=$(CLANG_FORMAT) BUILDIFIER=$(BUILDIFIER) $(CHECK_FORMAT) --add-excluded-prefixes="./linux/" check
-
-fix: $(CHECK_FORMAT) force-non-root
-	CLANG_FORMAT=$(CLANG_FORMAT) BUILDIFIER=$(BUILDIFIER) $(CHECK_FORMAT) --add-excluded-prefixes="./linux/" fix
-
-# Run tests using the fastbuild by default.
-tests: force-non-root proxylib-hdrs proxylib-bin
-	$(BAZEL) $(BAZEL_OPTS) test $(BAZEL_BUILD_OPTS) -c fastbuild $(BAZEL_TEST_OPTS) //:envoy_binary_test $(BAZEL_FILTER)
-	$(BAZEL) $(BAZEL_OPTS) test $(BAZEL_BUILD_OPTS) -c fastbuild $(BAZEL_TEST_OPTS) //:cilium_integration_test $(BAZEL_FILTER)
-
-debug-tests: force-non-root proxylib-hdrs proxylib-bin
-	$(BAZEL) $(BAZEL_OPTS) test $(BAZEL_BUILD_OPTS) -c debug $(BAZEL_TEST_OPTS) //:envoy_binary_test $(BAZEL_FILTER)
-	$(BAZEL) $(BAZEL_OPTS) test $(BAZEL_BUILD_OPTS) -c debug $(BAZEL_TEST_OPTS) //:cilium_integration_test $(BAZEL_FILTER)
-
-.PHONY: \
-	bazel-restore \
-	docker-istio-proxy \
-	docker-istio-proxy-debug \
-	docker-istio-proxy-init \
-	force \
-	force-non-root \
-	force-root
-
-force :;
-
-force-root:
-ifndef PKG_BUILD
-ifneq ($(USER),root)
-	$(error This target must be run as root!)
-endif
-endif
-
-force-non-root:
-ifeq ($(USER),root)
-	$(error This target cannot be run as root!)
-endif
+install:
+	docker create -ti --name cilium-envoy cilium/cilium-envoy:${CILIUM_ENVOY_SHA} bash
+	docker cp cilium-envoy:/usr/bin/cilium-envoy $(BINDIR)/cilium-envoy
+	docker rm -fv cilium-envoy
 
 endif

Makefile
@@ -2,8 +2,8 @@ include Makefile.defs
 include daemon/bpf.sha
 
 SUBDIRS = proxylib envoy plugins bpf cilium daemon monitor cilium-health bugtool tools
-GOFILES ?= $(subst _$(ROOT_DIR)/,,$(shell $(GO) list ./... | grep -v -e /vendor/ -e /contrib/ -e envoy/envoy))
-TESTPKGS ?= $(subst _$(ROOT_DIR)/,,$(shell $(GO) list ./... | grep -v -e /api/v1 -e /vendor/ -e /contrib/ -e envoy/envoy -e test))
+GOFILES ?= $(subst _$(ROOT_DIR)/,,$(shell $(GO) list ./... | grep -v -e /vendor/ -e /contrib/))
+TESTPKGS ?= $(subst _$(ROOT_DIR)/,,$(shell $(GO) list ./... | grep -v -e /api/v1 -e /vendor/ -e /contrib/ -e test))
 GOLANGVERSION = $(shell $(GO) version 2>/dev/null | grep -Eo '(go[0-9].[0-9])')
 GOLANG_SRCFILES=$(shell for pkg in $(subst github.com/cilium/cilium/,,$(GOFILES)); do find $$pkg -name *.go -print; done | grep -v vendor)
 BPF_FILES ?= $(shell git ls-files ../bpf/ | tr "\n" ' ')
@@ -56,9 +56,6 @@ tests-ginkgo-real:
 		$(GO) test $(TEST_LDFLAGS) $(pkg) $(GOTEST_PRIV_OPTS) || exit 1;)
 	@rmdir ./daemon/1 ./daemon/1_backup 2> /dev/null || true
 
-tests-envoy: proxylib
-	@ $(MAKE) -C envoy tests
-
 start-kvstores:
 	@echo Starting key-value store containers...
 	-$(DOCKER) rm -f "cilium-etcd-test-container" 2> /dev/null
@@ -81,7 +78,7 @@ start-kvstores:
            agent -client=0.0.0.0 -server -bootstrap-expect 1
 
 tests: force
-	$(MAKE) unit-tests tests-envoy
+	$(MAKE) unit-tests
 
 unit-tests: start-kvstores
 	$(QUIET) $(MAKE) -C daemon/ check-bindata
@@ -127,17 +124,14 @@ install:
 GIT_VERSION: .git
 	echo "$(GIT_VERSION)" >GIT_VERSION
 
-envoy/SOURCE_VERSION: .git
-	git rev-parse HEAD >envoy/SOURCE_VERSION
-
 docker-image: clean docker-image-no-clean
 
-docker-image-no-clean: GIT_VERSION envoy/SOURCE_VERSION
-	$(QUIET)grep -v -E "(SOURCE|GIT)_VERSION" .gitignore >.dockerignore
+docker-image-no-clean: GIT_VERSION
+	$(QUIET)grep -v -E "GIT_VERSION" .gitignore >.dockerignore
 	$(QUIET)echo ".*" >>.dockerignore # .git pruned out
 	$(QUIET)echo "Documentation" >>.dockerignore # Not needed
 	@$(ECHO_GEN) docker-image
-	$(DOCKER) build --build-arg LOCKDEBUG=${LOCKDEBUG} --build-arg V=${V} -t "cilium/cilium:$(DOCKER_IMAGE_TAG)" .
+	$(DOCKER) build --build-arg CILIUM_ENVOY_SHA=${CILIUM_ENVOY_SHA} --build-arg LOCKDEBUG=${LOCKDEBUG} --build-arg V=${V} -t "cilium/cilium:$(DOCKER_IMAGE_TAG)" .
 	$(QUIET)echo "Push like this when ready:"
 	$(QUIET)echo "docker push cilium/cilium:$(DOCKER_IMAGE_TAG)"
 
@@ -145,7 +139,7 @@ docker-image-runtime:
 	cd contrib/packaging/docker && docker build -t "cilium/cilium-runtime:$(UTC_DATE)" -f Dockerfile.runtime .
 
 docker-image-builder:
-	cd envoy && docker build -t "quay.io/cilium/cilium-builder:$(UTC_DATE)" .
+	docker build -t "cilium/cilium-builder:$(UTC_DATE)" -f Dockerfile.builder .
 
 build-deb:
 	$(MAKE) -C ./contrib/packaging/deb

envoy/Dockerfile
@@ -1,91 +1,0 @@
-#
-# Builder dependencies. This takes a long time to build from scratch!
-# Also note that if build fails due to C++ internal error or similar,
-# it is possible that the image build needs more RAM than available by
-# default on non-Linux docker installs.
-#
-FROM ubuntu:18.04
-
-LABEL maintainer="maintainer@cilium.io"
-
-WORKDIR /go/src/github.com/cilium/cilium/envoy
-
-#
-# Env setup for Go (installed below)
-#
-ENV GOROOT /usr/local/go
-ENV GOPATH /go
-ENV PATH "$GOROOT/bin:$GOPATH/bin:$PATH"
-ENV GO_VERSION 1.11.1
-
-#
-# Build dependencies
-#
-RUN apt-get update \
-    && DEBIAN_FRONTEND=noninteractive apt-get upgrade -y --no-install-recommends \
-	&& DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-		apt-utils \
-		automake \
-		binutils \
-		ca-certificates \
-		cmake \
-		coreutils \
-		curl \
-		g++ \
-		gcc \
-		git \
-		libelf-dev \
-		libtool \
-		m4 \
-		make \
-		ninja-build \
-		pkg-config \
-		python \
-		rsync \
-		unzip \
-		wget \
-		zip \
-		zlib1g-dev \
-	&& apt-get clean \
-	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-#
-# Install Go
-#
-RUN curl -sfL https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz | tar -xzC /usr/local \
-	&& go get -u github.com/cilium/go-bindata/... \
-	&& go get -u github.com/golang/protobuf/protoc-gen-go \
-	&& go get -d github.com/lyft/protoc-gen-validate \
-	&& (cd /go/src/github.com/lyft/protoc-gen-validate ; git checkout 930a67cf7ba41b9d9436ad7a1be70a5d5ff6e1fc ; make build) \
-	&& go get -u github.com/gordonklaus/ineffassign
-
-#
-# Extract the needed Bazel version from the repo
-#
-COPY BAZEL_VERSION ./
-#
-# Install Bazel
-#
-RUN export BAZEL_VERSION=`cat BAZEL_VERSION` \
-	&& curl -sfL https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh -o bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh \
-	&& chmod +x bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh \
-	&& ./bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh \
-	&& mv /usr/local/bin/bazel /usr/bin \
-	&& rm bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
-
-#
-# Add minimum Envoy files needed for the deps build. Touching any of these
-# in the cilium repo will trigger this stage to be re-built.
-#
-COPY Makefile.deps WORKSPACE tools bazel ./
-COPY BUILD_DEPS BUILD
-
-RUN \
-	# Extract Envoy source version (git SHA) from WORKSPACE
-	grep "ENVOY_SHA[ \t]*=" WORKSPACE | cut -d \" -f 2 > SOURCE_VERSION \
-	# Build only Envoy dependencies
-	&& make PKG_BUILD=1 -f Makefile.deps
-
-#
-# Absolutely nothing after making envoy deps!
-#

CILIUM_ENVOY_SHA
@@ -1,0 +1,1 @@
+fd3866831d8f85e27120f34d98133343fc9c4e0e

Dockerfile.builder
@@ -1,0 +1,50 @@
+#
+# Cilium build-time dependencies.
+# Image created from this file is used to build Cilium.
+#
+FROM ubuntu:18.04
+
+LABEL maintainer="maintainer@cilium.io"
+
+WORKDIR /go/src/github.com/cilium/cilium
+
+#
+# Env setup for Go (installed below)
+#
+ENV GOROOT /usr/local/go
+ENV GOPATH /go
+ENV PATH "$GOROOT/bin:$GOPATH/bin:$PATH"
+ENV GO_VERSION 1.11.1
+
+#
+# Build dependencies
+#
+RUN apt-get update \
+    && DEBIAN_FRONTEND=noninteractive apt-get upgrade -y --no-install-recommends \
+	&& DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+		apt-utils \
+		binutils \
+		ca-certificates \
+		coreutils \
+		curl \
+		gcc \
+		git \
+		libelf-dev \
+		m4 \
+		make \
+		pkg-config \
+		python \
+		rsync \
+		unzip \
+		wget \
+		zip \
+		zlib1g-dev \
+	&& apt-get clean \
+	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+#
+# Install Go
+#
+RUN curl -sfL https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz | tar -xzC /usr/local \
+	&& go get -u github.com/cilium/go-bindata/... \
+	&& go get -u github.com/gordonklaus/ineffassign

Makefile.defs
@@ -36,3 +36,5 @@ endif
 ifeq ($(DOCKER_IMAGE_TAG),)
     DOCKER_IMAGE_TAG="latest"
 endif
+
+CILIUM_ENVOY_SHA=$(shell cat $(ROOT_DIR)/CILIUM_ENVOY_SHA)

Makefile.quiet
@@ -4,7 +4,6 @@ ifeq ($(V),0)
 	ECHO_GEN=echo "  GEN   $(notdir $(shell pwd))/"
 	ECHO_GO=echo "  GO    $(notdir $(shell pwd))/$@"
 	ECHO_CHECK=echo "  CHECK"
-	ECHO_BAZEL=echo "  BAZEL $(notdir $(shell pwd))/$(notdir $(shell dirname $(ENVOY_BIN)))/$@"
 	ECHO_GINKGO=echo "  GINKG $(notdir $(shell pwd))"
 	ECHO_CLEAN=echo "  CLEAN"
 	SPHINXOPTS+="-q"
@@ -14,7 +13,6 @@ else
 	ECHO_GEN=: 
 	ECHO_GO=: 
 	ECHO_CHECK=: 
-	ECHO_BAZEL=: 
 	ECHO_GINKGO=: 
 	ECHO_CLEAN=: 
 endif

commit ac122032be6764afec4ee8ba22f1b9d15e0c39c3

Dockerfile: point envoy image to an existent repository
Fixes: c07078cce433 ("envoy: Make Envoy dependency version explicit.")
Signed-off-by: André Martins <andre@cilium.io>

Dockerfile
@@ -5,7 +5,7 @@
 # Otherwise its value is not passed in.
 #
 ARG CILIUM_ENVOY_SHA
-FROM cilium/cilium-envoy:${CILIUM_ENVOY_SHA:-"fd3866831d8f85e27120f34d98133343fc9c4e0e"} as cilium-envoy
+FROM quay.io/cilium/cilium-envoy:${CILIUM_ENVOY_SHA:-"971e018d05c0fce542d315e320b7b5d9e7480d24"} as cilium-envoy
 
 #
 # Cilium incremental build. Should be fast given builder-deps is up-to-date!

envoy/Makefile
@@ -31,7 +31,7 @@ all:
 	echo "cilium-envoy has been moved to github.com/cilium/proxy"
 
 install:
-	docker create -ti --name cilium-envoy cilium/cilium-envoy:${CILIUM_ENVOY_SHA} bash
+	docker create -ti --name cilium-envoy quay.io/cilium/cilium-envoy:${CILIUM_ENVOY_SHA} bash
 	docker cp cilium-envoy:/usr/bin/cilium-envoy $(BINDIR)/cilium-envoy
 	docker rm -fv cilium-envoy
 