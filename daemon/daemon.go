g0odi3z2j/cilium

commit 97f19e86ccc2effde75ce4bed2ec6da8c9319115

Endpoint: set a new context per endpoint regeneration
On endpointmanager we call to endpoint.Regenerate with the same
regenerationContext, that means that all the spanStats are updated to a
new ones instead of the correct values.

With this change, all endpoint.RegenerationContext are different per
each endpoint and the metrics will be correct.

Fix #6021
Related to commit: 1a577dcc9860c3663ee15455a9ad2c08d1163b41

Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

pkg/endpointmanager/manager.go
@@ -266,6 +266,8 @@ func updateReferences(ep *endpoint.Endpoint) {
 // RegenerateAllEndpoints calls a SetStateLocked for each endpoint and
 // regenerates if state transaction is valid. During this process, the endpoint
 // list is locked and cannot be modified.
+// The endpoint.RegenerationContext will be cloned to send a new context to
+// each endpoint to avoid issue on endpoint regenerations statistics.
 // Returns a waiting group that can be used to know when all the endpoints are
 // regenerated.
 func RegenerateAllEndpoints(owner endpoint.Owner, regenContext *endpoint.RegenerationContext) *sync.WaitGroup {
@@ -285,7 +287,9 @@ func RegenerateAllEndpoints(owner endpoint.Owner, regenContext *endpoint.Regener
 				ep.Unlock()
 				if regen {
 					// Regenerate logs status according to the build success/failure
-					<-ep.Regenerate(owner, regenContext)
+					// Create a new regenContext to not overwrite the spanStats
+					// values on the endpoint regeneration.
+					<-ep.Regenerate(owner, endpoint.NewRegenerationContext(regenContext.Reason))
 				}
 			}
 			wg.Done()

commit 0970a5691a5bde81b81d2d2fa65e9ceb80c6f552

set reloadDatapath flag as a function parameter
To avoid overwritting of the RegenerationContext, as it should only be
used for statistics, we should make use of a function parameters to pass
around flags that performs a datapath reload.

Fixes: 97f19e86ccc2 ("Endpoint: set a new context per endpoint regeneration")
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon.go
@@ -1080,10 +1080,9 @@ func (d *Daemon) TriggerReloadWithoutCompile(reason string) (*sync.WaitGroup, er
 		return nil, fmt.Errorf("Unable to recompile base programs from %s: %s", reason, err)
 	}
 	regenContext := &endpoint.RegenerationContext{
-		Reason:         reason,
-		ReloadDatapath: true,
+		Reason: reason,
 	}
-	return endpointmanager.RegenerateAllEndpoints(d, regenContext), nil
+	return endpointmanager.RegenerateAllEndpoints(d, regenContext, true), nil
 }
 
 func changedOption(key string, value option.OptionSetting, data interface{}) {

daemon/policy.go
@@ -53,7 +53,7 @@ func (d *Daemon) TriggerPolicyUpdates(force bool, reason string) *sync.WaitGroup
 		log.Debugf("Full policy recalculation triggered")
 	}
 	regenContext := endpoint.NewRegenerationContext(reason)
-	return endpointmanager.RegenerateAllEndpoints(d, regenContext)
+	return endpointmanager.RegenerateAllEndpoints(d, regenContext, false)
 }
 
 type getPolicyResolve struct {

daemon/state.go
@@ -279,7 +279,7 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 			}
 			regenContext := endpoint.NewRegenerationContext(
 				"syncing state to host")
-			if buildSuccess := <-ep.Regenerate(d, regenContext); !buildSuccess {
+			if buildSuccess := <-ep.Regenerate(d, regenContext, false); !buildSuccess {
 				scopedLog.Warn("Failed while regenerating endpoint")
 				epRegenerated <- false
 				return

pkg/endpoint/bpf.go
@@ -504,10 +504,12 @@ func (e *Endpoint) removeOldRedirects(owner Owner, desiredRedirects map[string]b
 
 // regenerateBPF rewrites all headers and updates all BPF maps to reflect the
 // specified endpoint.
+// ReloadDatapath forces the datapath programs to be reloaded. It does
+// not guarantee recompilation of the programs.
 // Must be called with endpoint.Mutex not held and endpoint.BuildMutex held.
 // Returns the policy revision number when the regeneration has called, a
 // boolean if the BPF compilation was executed and an error in case of an error.
-func (e *Endpoint) regenerateBPF(owner Owner, currentDir, nextDir string, regenContext *RegenerationContext) (revnum uint64, compiled bool, reterr error) {
+func (e *Endpoint) regenerateBPF(owner Owner, currentDir, nextDir string, regenContext *RegenerationContext, reloadDatapath bool) (revnum uint64, compiled bool, reterr error) {
 	var (
 		err                 error
 		compilationExecuted bool
@@ -746,7 +748,7 @@ func (e *Endpoint) regenerateBPF(owner Owner, currentDir, nextDir string, regenC
 	e.getLogger().WithField("bpfHeaderfilesChanged", bpfHeaderfilesChanged).Debug("Preparing to compile BPF")
 
 	stats.prepareBuild.End(true)
-	if bpfHeaderfilesChanged || regenContext.ReloadDatapath {
+	if bpfHeaderfilesChanged || reloadDatapath {
 		closeChan := loadinfo.LogPeriodicSystemLoad(log.WithFields(logrus.Fields{logfields.EndpointID: epID}).Debugf, time.Second)
 
 		// Compile and install BPF programs for this endpoint

pkg/endpoint/endpoint.go
@@ -1306,7 +1306,7 @@ func (e *Endpoint) Update(owner Owner, cfg *models.EndpointConfigurationSpec) er
 				stateTransitionSucceeded := e.SetStateLocked(StateWaitingToRegenerate, reason)
 				if stateTransitionSucceeded {
 					e.Unlock()
-					e.Regenerate(owner, NewRegenerationContext(reason))
+					e.Regenerate(owner, NewRegenerationContext(reason), false)
 					return nil
 				}
 				e.Unlock()
@@ -1430,7 +1430,7 @@ func (e *Endpoint) LeaveLocked(owner Owner, proxyWaitGroup *completion.WaitGroup
 // RegenerateWait should only be called when endpoint's state has successfully
 // been changed to "waiting-to-regenerate"
 func (e *Endpoint) RegenerateWait(owner Owner, reason string) error {
-	if !<-e.Regenerate(owner, NewRegenerationContext(reason)) {
+	if !<-e.Regenerate(owner, NewRegenerationContext(reason), false) {
 		return fmt.Errorf("error while regenerating endpoint."+
 			" For more info run: 'cilium endpoint get %d'", e.ID)
 	}
@@ -2060,7 +2060,7 @@ func (e *Endpoint) identityLabelsChanged(owner Owner, myChangeRev int) error {
 	e.Unlock()
 
 	if readyToRegenerate {
-		e.Regenerate(owner, NewRegenerationContext("updated security labels"))
+		e.Regenerate(owner, NewRegenerationContext("updated security labels"), false)
 	}
 
 	return nil

pkg/endpoint/policy.go
@@ -70,10 +70,6 @@ type RegenerationContext struct {
 	// used to generate useful log messages.
 	Reason string
 
-	// ReloadDatapath forces the datapath programs to be reloaded. It does
-	// not guarantee recompilation of the programs.
-	ReloadDatapath bool
-
 	// Stats are collected during the endpoint regeneration and provided
 	// back to the caller
 	Stats regenerationStatistics
@@ -652,7 +648,7 @@ func (e *Endpoint) ComputePolicyEnforcement(repo *policy.Repository) (ingress bo
 }
 
 // Called with e.Mutex UNlocked
-func (e *Endpoint) regenerate(owner Owner, context *RegenerationContext) (retErr error) {
+func (e *Endpoint) regenerate(owner Owner, context *RegenerationContext, reloadDatapath bool) (retErr error) {
 	var revision uint64
 	var compilationExecuted bool
 	var err error
@@ -773,7 +769,7 @@ func (e *Endpoint) regenerate(owner Owner, context *RegenerationContext) (retErr
 		e.Unlock()
 	}()
 
-	revision, compilationExecuted, err = e.regenerateBPF(owner, origDir, tmpDir, context)
+	revision, compilationExecuted, err = e.regenerateBPF(owner, origDir, tmpDir, context, reloadDatapath)
 	if err != nil {
 		failDir := e.FailedDirectoryPath()
 		e.getLogger().WithFields(logrus.Fields{
@@ -822,7 +818,9 @@ func (e *Endpoint) regenerate(owner Owner, context *RegenerationContext) (retErr
 // Regenerate forces the regeneration of endpoint programs & policy
 // Should only be called with e.state == StateWaitingToRegenerate or with
 // e.state == StateWaitingForIdentity
-func (e *Endpoint) Regenerate(owner Owner, context *RegenerationContext) <-chan bool {
+// ReloadDatapath forces the datapath programs to be reloaded. It does
+// not guarantee recompilation of the programs.
+func (e *Endpoint) Regenerate(owner Owner, context *RegenerationContext, reloadDatapath bool) <-chan bool {
 	newReq := &Request{
 		ID:           uint64(e.ID),
 		MyTurn:       make(chan bool),
@@ -859,7 +857,7 @@ func (e *Endpoint) Regenerate(owner Owner, context *RegenerationContext) <-chan 
 		if isMyTurnChanOK && isMyTurn {
 			scopedLog.Debug("Dequeued endpoint from build queue")
 
-			err := e.regenerate(owner, context)
+			err := e.regenerate(owner, context, reloadDatapath)
 			repr, reprerr := monitor.EndpointRegenRepr(e, err)
 			if reprerr != nil {
 				scopedLog.WithError(reprerr).Warn("Notifying monitor about endpoint regeneration failed")

pkg/endpointmanager/manager.go
@@ -278,7 +278,7 @@ func updateReferences(ep *endpoint.Endpoint) {
 // each endpoint to avoid issue on endpoint regenerations statistics.
 // Returns a waiting group that can be used to know when all the endpoints are
 // regenerated.
-func RegenerateAllEndpoints(owner endpoint.Owner, regenContext *endpoint.RegenerationContext) *sync.WaitGroup {
+func RegenerateAllEndpoints(owner endpoint.Owner, regenContext *endpoint.RegenerationContext, reloadDatapath bool) *sync.WaitGroup {
 	var wg sync.WaitGroup
 
 	eps := GetEndpoints()
@@ -297,7 +297,7 @@ func RegenerateAllEndpoints(owner endpoint.Owner, regenContext *endpoint.Regener
 					// Regenerate logs status according to the build success/failure
 					// Create a new regenContext to not overwrite the spanStats
 					// values on the endpoint regeneration.
-					<-ep.Regenerate(owner, endpoint.NewRegenerationContext(regenContext.Reason))
+					<-ep.Regenerate(owner, endpoint.NewRegenerationContext(regenContext.Reason), reloadDatapath)
 				}
 			}
 			wg.Done()

daemon/policy_test.go
@@ -168,7 +168,7 @@ func (ds *DaemonSuite) TestUpdateConsumerMap(c *C) {
 	ready := e.SetStateLocked(endpoint.StateWaitingToRegenerate, "test")
 	e.Unlock()
 	c.Assert(ready, Equals, true)
-	buildSuccess := <-e.Regenerate(ds.d, regenContext)
+	buildSuccess := <-e.Regenerate(ds.d, regenContext, false)
 	c.Assert(buildSuccess, Equals, true)
 	c.Assert(e.Allows(qaBarSecLblsCtx.ID), Equals, false)
 	c.Assert(e.Allows(prodBarSecLblsCtx.ID), Equals, false)
@@ -186,7 +186,7 @@ func (ds *DaemonSuite) TestUpdateConsumerMap(c *C) {
 	ready = e.SetStateLocked(endpoint.StateWaitingToRegenerate, "test")
 	e.Unlock()
 	c.Assert(ready, Equals, true)
-	buildSuccess = <-e.Regenerate(ds.d, regenContext)
+	buildSuccess = <-e.Regenerate(ds.d, regenContext, false)
 	c.Assert(buildSuccess, Equals, true)
 	c.Assert(e.Allows(0), Equals, false)
 	c.Assert(e.Allows(qaBarSecLblsCtx.ID), Equals, false)
@@ -462,7 +462,7 @@ func (ds *DaemonSuite) TestRemovePolicy(c *C) {
 	ready := e.SetStateLocked(endpoint.StateWaitingToRegenerate, "test")
 	e.Unlock()
 	c.Assert(ready, Equals, true)
-	buildSuccess := <-e.Regenerate(ds.d, regenContext)
+	buildSuccess := <-e.Regenerate(ds.d, regenContext, false)
 	c.Assert(buildSuccess, Equals, true)
 
 	// Check that the policy has been updated in the xDS cache for the L7

daemon/state_test.go
@@ -139,7 +139,7 @@ func (ds *DaemonSuite) generateEPs(baseDir string, epsWanted []*e.Endpoint, epsM
 		ready := ep.SetStateLocked(e.StateWaitingToRegenerate, "test")
 		ep.Unlock()
 		if ready {
-			<-ep.Regenerate(ds, regenContext)
+			<-ep.Regenerate(ds, regenContext, false)
 		}
 
 		switch ep.ID {
@@ -162,7 +162,7 @@ func (ds *DaemonSuite) generateEPs(baseDir string, epsWanted []*e.Endpoint, epsM
 				ready := ep.SetStateLocked(e.StateWaitingToRegenerate, "test")
 				ep.Unlock()
 				if ready {
-					<-ep.Regenerate(ds, regenContext)
+					<-ep.Regenerate(ds, regenContext, false)
 				}
 				epsNames = append(epsNames, ep.DirectoryPath())
 			}