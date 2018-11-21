g0odi3z2j/cilium

commit fd6e9648f472de064c20fb913c0112f75faabb06

bpf: Avoid routing loops for former local endpoint IPs
The bpf_netdev BPF program is used for traffic in device snooping mode as well
as to handle packets from the host. When running in the mode to handle traffic
from the host and local delivery is unsuccessful as well as encapsulation is
not an option, packets were routed back to the host so far. This behavior is
required in snooping mode but leads to routing loops until the TTL decreases to
zero when running in host mode.

Add a special case to drop the packet early without requiring the TTL to
decrease to 0 first.

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/bpf_netdev.c
@@ -271,7 +271,16 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 	}
 #endif
 
+#ifdef FROM_HOST
+	/* The destination IP address could not be associated with a local
+	 * endpoint or a tunnel destination. It is likely addressed to a
+	 * destination to a local endpoint IP that has disappeard. Given this
+	 * is coming from the host, we can't route it back as it will create a
+	 * routing loop. */
+	return DROP_NON_LOCAL;
+#else
 	return TC_ACT_OK;
+#endif
 }
 
 #ifdef ENABLE_IPV4
@@ -440,7 +449,16 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 	}
 #endif
 
+#ifdef FROM_HOST
+	/* The destination IP address could not be associated with a local
+	 * endpoint or a tunnel destination. It is likely addressed to a
+	 * destination to a local endpoint IP that has disappeard. Given this
+	 * is coming from the host, we can't route it back as it will create a
+	 * routing loop. */
+	return DROP_NON_LOCAL;
+#else
 	return TC_ACT_OK;
+#endif
 }
 
 #define CB_SRC_IDENTITY 0

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=e588480e66d50a1ed87c475187067e980cf119f0
+GO_BINDATA_SHA1SUM=cec2fa10f9d4626945b531ab3000c000cad1ad01
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

commit a6df7d020757c3b3c631a40834942e5882503091

bpf: Fix node-port access to l7 proxy
Commit fd6e9648f472 ("bpf: Avoid routing loops for former local endpoint
IPs") adjusted the logic for the cilium_host devices to drop traffic
which is destined for somewhere that we don't control, under the
assumption that it is likely something that we previously controlled,
but no longer do.

One hole in this logic was that when external endpoints reach to an
endpoint that has L7 policy applied, the traffic is first routed into
the localhost to terminate the connection in the L7 proxy, which then
sends back a response to establish the connection. This traffic may be
sent back over the cilium_host device, and will then be dropped by the
new logic to avoid routing loops.

Fix this case by only dropping traffic for destinations that lie within
the range of IPs managed by this node, and allowing other traffic to
pass.

Fixes: fd6e9648f472 ("bpf: Avoid routing loops for former local endpoint IPs")
Signed-off-by: Joe Stringer <joe@cilium.io>

bpf/bpf_netdev.c
@@ -273,14 +273,14 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 
 #ifdef FROM_HOST
 	/* The destination IP address could not be associated with a local
-	 * endpoint or a tunnel destination. It is likely addressed to a
-	 * destination to a local endpoint IP that has disappeard. Given this
-	 * is coming from the host, we can't route it back as it will create a
-	 * routing loop. */
-	return DROP_NON_LOCAL;
-#else
-	return TC_ACT_OK;
+	 * endpoint or a tunnel destination. If it is destined to an IP in
+	 * the local range, then we can't route it back to the host as it
+	 * will create a routing loop. Drop it. */
+	dst = (union v6addr *) &ip6->daddr;
+	if (ipv6_match_prefix_96(dst, &node_ip))
+		return DROP_NON_LOCAL;
 #endif
+	return TC_ACT_OK;
 }
 
 #ifdef ENABLE_IPV4
@@ -451,14 +451,13 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 
 #ifdef FROM_HOST
 	/* The destination IP address could not be associated with a local
-	 * endpoint or a tunnel destination. It is likely addressed to a
-	 * destination to a local endpoint IP that has disappeard. Given this
-	 * is coming from the host, we can't route it back as it will create a
-	 * routing loop. */
-	return DROP_NON_LOCAL;
-#else
-	return TC_ACT_OK;
+	 * endpoint or a tunnel destination. If it is destined to an IP in
+	 * the local range, then we can't route it back to the host as it
+	 * will create a routing loop. Drop it. */
+	if ((ip4->daddr & IPV4_MASK) == (IPV4_GATEWAY & IPV4_MASK))
+		return DROP_NON_LOCAL;
 #endif
+	return TC_ACT_OK;
 }
 
 #define CB_SRC_IDENTITY 0

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=cec2fa10f9d4626945b531ab3000c000cad1ad01
+GO_BINDATA_SHA1SUM=98c3e471f4de0ed5a7047014022ed26a4aef0ebe
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 