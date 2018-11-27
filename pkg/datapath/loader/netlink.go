g0odi3z2j/cilium

commit ca79035d2036e92487725c38cf1b00bb4126f65c

loader: replace join endpoint script with loader
We are moving away from invoking scripts to a more programmatic
approach.  This patch still invokes the commands but wraps it all in Go.
This will make it easier to fully replace the init / setup script.

From discussions with Daniel, the next thing is to get the iproute2 code
accessible via cgo or swig. If something like that works we can call tc,
bpf and related functions directly from Go. Then the needed calls are
compiled in and the iproute2 version dependency can be dropped.

Maybe too much work, but it would be nice if we also had a option to use
LLVM apis instead of clang and llc execs for the container case where we
can decide which version is being shipped.

Related: #1711 (evaluate vishvananda/netlink for replacing iproute2 usage)
Related: #1600 (Go native interaction with the kernel)

Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>
Co-authored-by: Joe Stringer <joe@covalent.io>
Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/datapath/loader/netlink.go
@@ -1,0 +1,128 @@
+// Copyright 2017-2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package loader
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/cilium/cilium/pkg/bpf"
+
+	"github.com/vishvananda/netlink"
+)
+
+const (
+	libbpfFixupMsg = "struct bpf_elf_map fixup performed due to size mismatch!"
+)
+
+func replaceQdisc(ifName string) error {
+	link, err := netlink.LinkByName(ifName)
+	if err != nil {
+		return err
+	}
+	attrs := netlink.QdiscAttrs{
+		LinkIndex: link.Attrs().Index,
+		Handle:    netlink.MakeHandle(0xffff, 0),
+		Parent:    netlink.HANDLE_CLSACT,
+	}
+
+	qdisc := &netlink.GenericQdisc{
+		QdiscAttrs: attrs,
+		QdiscType:  "clsact",
+	}
+
+	// Replacing the qdisc after the first creation will always fail with
+	// the current netlink library due to the issue fixed in this PR:
+	// https://github.com/vishvananda/netlink/pull/382
+	//
+	// FIXME GH-5423 rebase against the latest netlink library
+	if err = netlink.QdiscReplace(qdisc); err != nil {
+		log.WithError(err).Debugf("netlink: Replacing qdisc for %s failed", ifName)
+	} else {
+		log.Debugf("netlink: Replacing qdisc for %s succeeded", ifName)
+	}
+
+	return nil
+}
+
+// replaceDatapath the qdisc and BPF program for a endpoint
+func replaceDatapath(ctx context.Context, ifName string, objPath string, progSec string) error {
+	err := replaceQdisc(ifName)
+	if err != nil {
+		return fmt.Errorf("Failed to replace Qdisc for %s: %s", ifName, err)
+	}
+
+	// FIXME: Replace cilium-map-migrate with Golang map migration
+	cmd := exec.CommandContext(ctx, "cilium-map-migrate", "-s", objPath)
+	cmd.Env = bpf.Environment()
+	out, err := cmd.CombinedOutput()
+	if ctx.Err() == context.DeadlineExceeded {
+		err = fmt.Errorf("Command execution failed: Timeout")
+	}
+	if err != nil {
+		scanner := bufio.NewScanner(bytes.NewReader(out))
+		return fmt.Errorf("Failed to migrate endpoint maps: %s: %q", err, scanner.Text())
+	}
+	defer func() {
+		var retCode string
+		if err == nil {
+			retCode = "0"
+		} else {
+			retCode = "1"
+		}
+		cmd := exec.CommandContext(ctx, "cilium-map-migrate", "-e", objPath, "-r", retCode)
+		cmd.Env = bpf.Environment()
+		out2, err2 := cmd.CombinedOutput()
+		if ctx.Err() == context.DeadlineExceeded {
+			err = fmt.Errorf("Command execution failed: Timeout")
+		}
+		if err2 != nil {
+			scanner := bufio.NewScanner(bytes.NewReader(out2))
+			log.Infof("Failed to migrate maps back after unsuccessful prog replace: %s: %q", err2, scanner.Text())
+		}
+	}()
+
+	// FIXME: replace exec with native call
+	args := []string{"filter", "replace", "dev", ifName, "ingress",
+		"prio", "1", "handle", "1", "bpf", "da", "obj", objPath,
+		"sec", progSec,
+	}
+	cmd = exec.CommandContext(ctx, "tc", args...)
+	cmd.Env = bpf.Environment()
+	out, err = cmd.CombinedOutput()
+	if ctx.Err() == context.DeadlineExceeded {
+		err = fmt.Errorf("Command execution failed: Timeout")
+	}
+	if err != nil {
+		filteredOutput := bytes.NewBuffer(make([]byte, 0, len(out)))
+		scanner := bufio.NewScanner(bytes.NewReader(out))
+		for scanner.Scan() {
+			text := scanner.Text()
+			if !strings.Contains(text, libbpfFixupMsg) {
+				_, err2 := filteredOutput.WriteString(text)
+				if err2 != nil {
+					log.WithError(err2).Debugf("Cannot buffer tc failure output: %s: %q", err2, text)
+				}
+			}
+		}
+		return fmt.Errorf("Failed to load tc filter: %s: %q", err, filteredOutput)
+	}
+
+	return nil
+}

bpf/join_ep.sh
@@ -1,69 +1,0 @@
-#!/bin/bash
-#
-# Copyright 2016-2017 Authors of Cilium
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-set -e
-
-LIB=$1
-RUNDIR=$2
-EPDIR=$3
-IFNAME=$4
-DEBUG=$5
-EPID=$6
-
-function bpf_preprocess()
-{
-	SRC=$1
-
-	clang -E -O2 -target bpf -I$RUNDIR/globals -I$EPDIR		\
-		-I$LIB/include -c $LIB/$SRC -o $EPDIR/$SRC
-}
-
-function bpf_compile()
-{
-	IN=$1
-	OUT=$2
-	TYPE=$3
-	EXTRA_CFLAGS=$4
-	LLC_FLAGS=$5
-
-	clang -O2 -target bpf -emit-llvm $EXTRA_CFLAGS			\
-	      -Wno-address-of-packed-member -Wno-unknown-warning-option	\
-	      -I$RUNDIR/globals -I$EPDIR -I$LIB/include			\
-	      -D__NR_CPUS__=$(nproc)					\
-	      -c $LIB/$IN -o - |					\
-	llc -march=bpf -mcpu=probe $LLC_FLAGS -filetype=$TYPE -o $EPDIR/$OUT
-}
-
-echo "Join EP id=$EPDIR ifname=$IFNAME"
-
-# Only generate ASM output if debug is enabled.
-if [[ "${DEBUG}" == "true" ]]; then
-  echo "kernel version: " `uname -a`
-  echo "clang version: " `clang --version`
-  bpf_compile bpf_lxc.c bpf_lxc.dbg.o obj "-g" "-mattr=dwarfris"
-  bpf_compile bpf_lxc.c bpf_lxc.asm asm "-g"
-  bpf_preprocess bpf_lxc.c
-fi
-
-bpf_compile bpf_lxc.c bpf_lxc.o obj
-tc qdisc replace dev $IFNAME clsact || true
-cilium-map-migrate -s $EPDIR/bpf_lxc.o
-set +e
-tc filter replace dev $IFNAME ingress prio 1 handle 1 bpf da obj $EPDIR/bpf_lxc.o sec from-container
-RETCODE=$?
-set -e
-cilium-map-migrate -e $EPDIR/bpf_lxc.o -r $RETCODE
-exit $RETCODE

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=16cae2c6c75c6d91665541389b9ae56fab92b670
-BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 
+GO_BINDATA_SHA1SUM=7a69e06ed046704562d6983fb3807f63eb76332f
+BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

pkg/datapath/loader/compile.go
@@ -1,0 +1,195 @@
+// Copyright 2017-2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package loader
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+// OutputType determines the type to be generated by the compilation steps.
+type OutputType string
+
+const (
+	outputObject   = OutputType("obj")
+	outputAssembly = OutputType("asm")
+	outputSource   = OutputType("c")
+)
+
+// progInfo describes a program to be compiled with the expected output format
+type progInfo struct {
+	// Source is the program source (base) filename to be compiled
+	Source string
+	// Output is the expected (base) filename produced from the source
+	Output string
+	// OutputType to be created by LLVM
+	OutputType OutputType
+}
+
+// directoryInfo includes relevant directories for compilation and linking
+type directoryInfo struct {
+	// Library contains the library code to be used for compilation
+	Library string
+	// Runtime contains headers for compilation
+	Runtime string
+	// Output is the directory where the files will be stored
+	Output string
+}
+
+var (
+	compiler       = "clang"
+	linker         = "llc"
+	standardCFlags = []string{"-O2", "-target", "bpf",
+		fmt.Sprintf("-D__NR_CPUS__=%d", runtime.NumCPU()),
+		"-Wno-address-of-packed-member", "-Wno-unknown-warning-option"}
+	standardLDFlags = []string{"-march=bpf", "-mcpu=probe"}
+
+	endpointPrefix   = "bpf_lxc"
+	endpointProg     = fmt.Sprintf("%s.%s", endpointPrefix, outputSource)
+	endpointObj      = fmt.Sprintf("%s.o", endpointPrefix)
+	endpointObjDebug = fmt.Sprintf("%s.dbg.o", endpointPrefix)
+	endpointAsm      = fmt.Sprintf("%s.%s", endpointPrefix, outputAssembly)
+
+	debugProgs = []*progInfo{
+		{
+			Source:     endpointProg,
+			Output:     endpointObjDebug,
+			OutputType: outputObject,
+		},
+		{
+			Source:     endpointProg,
+			Output:     endpointAsm,
+			OutputType: outputAssembly,
+		},
+		{
+			Source:     endpointProg,
+			Output:     endpointProg,
+			OutputType: outputSource,
+		},
+	}
+	datapathProg = &progInfo{
+		Source:     endpointProg,
+		Output:     endpointObj,
+		OutputType: outputObject,
+	}
+)
+
+// irPath determines a temporary path on the filesystem to store the compiled
+// intermediate representation object for the program.
+//
+// TODO: Remove this and just pipe the 'clang' and 'llc' programs together.
+func irPath(prog *progInfo, dir *directoryInfo) string {
+	return fmt.Sprintf("%s/%s.bc", dir.Output, prog.Output)
+}
+
+// progLDFlags determines the loader flags for the specified prog and paths.
+func progLDFlags(prog *progInfo, dir *directoryInfo) []string {
+	return []string{
+		fmt.Sprintf("-filetype=%s", prog.OutputType),
+		irPath(prog, dir),
+		"-o", path.Join(dir.Output, prog.Output),
+	}
+}
+
+// linkProg links the specified program from the specified path to the
+// intermediate representation, to the output specified in the prog's info.
+func linkProg(ctx context.Context, prog *progInfo, dir *directoryInfo, debug bool) error {
+	args := make([]string, 0, 8)
+	if debug {
+		args = append(args, "-mattr=dwarfris")
+	}
+	args = append(args, standardLDFlags...)
+	args = append(args, progLDFlags(prog, dir)...)
+
+	out, err := exec.CommandContext(ctx, linker, args...).CombinedOutput()
+	if ctx.Err() == context.DeadlineExceeded {
+		err = fmt.Errorf("Command execution failed: Timeout")
+	}
+	if err != nil {
+		scanner := bufio.NewScanner(bytes.NewReader(out))
+		return fmt.Errorf("Failed to link %s: %s: %q", prog.Output, err, scanner.Text())
+	}
+
+	return nil
+}
+
+// progLDFlags determines the compiler flags for the specified prog and paths.
+func progCFlags(prog *progInfo, dir *directoryInfo) []string {
+	var output string
+
+	if prog.OutputType == outputSource {
+		output = path.Join(dir.Output, prog.Output)
+	} else {
+		output = irPath(prog, dir)
+	}
+
+	return []string{
+		fmt.Sprintf("-I%s", path.Join(dir.Runtime, "globals")),
+		fmt.Sprintf("-I%s", dir.Output),
+		fmt.Sprintf("-I%s", path.Join(dir.Library, "include")),
+		"-c", path.Join(dir.Library, prog.Source),
+		"-o", output,
+	}
+}
+
+// compile and link a program.
+func compile(ctx context.Context, prog *progInfo, dir *directoryInfo, debug bool) error {
+	args := make([]string, 0, 16)
+	if prog.OutputType == outputSource {
+		args = append(args, "-E") // Preprocessor
+	} else {
+		args = append(args, "-emit-llvm")
+		if debug {
+			args = append(args, "-g")
+		}
+	}
+	args = append(args, standardCFlags...)
+	args = append(args, progCFlags(prog, dir)...)
+
+	// Compilation is split between two exec calls. First clang generates
+	// LLVM bitcode and then later llc compiles it to byte-code.
+	log.WithFields(logrus.Fields{
+		"target": compiler,
+		"args":   args,
+	}).Debug("Launching CommandContext")
+	out, err := exec.CommandContext(ctx, compiler, args...).CombinedOutput()
+	if ctx.Err() == context.DeadlineExceeded {
+		err = fmt.Errorf("Command execution failed: Timeout")
+	}
+	if err != nil {
+		scanner := bufio.NewScanner(bytes.NewReader(out))
+		return fmt.Errorf("Failed to compile %s: %s: %q", prog.Output, err, scanner.Text())
+	}
+	defer os.Remove(irPath(prog, dir))
+
+	switch prog.OutputType {
+	case outputObject:
+		return linkProg(ctx, prog, dir, debug)
+	case outputAssembly:
+		return linkProg(ctx, prog, dir, false)
+	default:
+		break
+	}
+
+	return nil
+}

pkg/datapath/loader/loader.go
@@ -15,16 +15,9 @@
 package loader
 
 import (
-	"bufio"
-	"bytes"
 	"context"
-	"fmt"
-	"os/exec"
-	"path/filepath"
-	"strconv"
-	"strings"
+	"path"
 
-	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
@@ -36,40 +29,71 @@ import (
 // TODO: Rebase against https://github.com/cilium/cilium/pull/5286.
 var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "datapath-loader")
 
+const (
+	symbolFromEndpoint = "from-container"
+)
+
 // endpoint provides access to endpoint information that is necessary to
 // compile and load the datapath.
 type endpoint interface {
 	InterfaceName() string
 	Logger() *logrus.Entry
 	StateDir() string
-	StringID() string
 }
 
-// joinEP shells out to bpf/join_ep.sh to compile/load the BPF datapath for the
-// specified endpoint, and handles timeouts if the loading takes too long.
-func joinEP(ctx context.Context, ep endpoint, libdir, rundir, epdir, debug string) error {
-	args := []string{libdir, rundir, epdir, debug, ep.InterfaceName(), ep.StringID()}
-	prog := filepath.Join(libdir, "join_ep.sh")
+// compileDatapath invokes the compiler and linker to create all state files for
+// the BPF datapath, with the primary target being the BPF ELF binary.
+//
+// If debug is enabled, create also the following output files:
+// * Preprocessed C
+// * Assembly
+// * Object compiled with debug symbols
+func compileDatapath(ctx context.Context, ep endpoint, dirs *directoryInfo, debug bool) error {
+	// TODO: Consider logging kernel/clang versions here too
+	epLog := ep.Logger()
 
-	scopedLog := ep.Logger()
-
-	joinEpCmd := exec.CommandContext(ctx, prog, args...)
-	joinEpCmd.Env = bpf.Environment()
-	out, err := joinEpCmd.CombinedOutput()
-
-	cmd := fmt.Sprintf("%s %s", prog, strings.Join(args, " "))
-	scopedLog = scopedLog.WithField("cmd", cmd)
-	if ctx.Err() == context.DeadlineExceeded {
-		scopedLog.Error("JoinEP: Command execution failed: Timeout")
-		return ctx.Err()
-	}
-	if err != nil {
-		scopedLog.WithError(err).Warn("JoinEP: Command execution failed")
-		scanner := bufio.NewScanner(bytes.NewReader(out))
-		for scanner.Scan() {
-			log.Warn(scanner.Text())
+	// Write out assembly and preprocessing files for debugging purposes
+	if debug {
+		for _, p := range debugProgs {
+			if err := compile(ctx, p, dirs, debug); err != nil {
+				scopedLog := epLog.WithFields(logrus.Fields{
+					logfields.Params: logfields.Repr(p),
+					logfields.Debug:  debug,
+				})
+				scopedLog.WithError(err).Debug("JoinEP: Failed to compile")
+				return err
+			}
 		}
-		return fmt.Errorf("error: %q command output: %q", err, out)
+	}
+
+	// Compile the new program
+	if err := compile(ctx, datapathProg, dirs, debug); err != nil {
+		scopedLog := epLog.WithFields(logrus.Fields{
+			logfields.Params: logfields.Repr(datapathProg),
+			logfields.Debug:  false,
+		})
+		scopedLog.WithError(err).Warn("JoinEP: Failed to compile")
+		return err
+	}
+
+	return nil
+}
+
+func compileAndLoad(ctx context.Context, ep endpoint, dirs *directoryInfo) error {
+	debug := viper.GetBool(option.BPFCompileDebugName)
+	if err := compileDatapath(ctx, ep, dirs, debug); err != nil {
+		return err
+	}
+
+	// Replace the current program
+	objPath := path.Join(dirs.Output, endpointObj)
+	if err := replaceDatapath(ctx, ep.InterfaceName(), objPath, symbolFromEndpoint); err != nil {
+		scopedLog := ep.Logger().WithFields(logrus.Fields{
+			logfields.Path: objPath,
+			logfields.Veth: ep.InterfaceName(),
+		})
+		scopedLog.WithError(err).Warn("JoinEP: Failed to load program")
+		return err
 	}
 
 	return nil
@@ -84,9 +108,10 @@ func CompileAndLoad(ctx context.Context, ep endpoint) error {
 		log.Fatalf("LoadBPF() doesn't support non-endpoint load")
 	}
 
-	libdir := option.Config.BpfDir
-	rundir := option.Config.StateDir
-	epdir := ep.StateDir()
-	debug := strconv.FormatBool(viper.GetBool(option.BPFCompileDebugName))
-	return joinEP(ctx, ep, libdir, rundir, epdir, debug)
+	dirs := directoryInfo{
+		Library: option.Config.BpfDir,
+		Runtime: option.Config.StateDir,
+		Output:  ep.StateDir(),
+	}
+	return compileAndLoad(ctx, ep, &dirs)
 }

pkg/logging/logfields/logfields.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2017-2018 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -296,4 +296,7 @@ const (
 	// Reason is a human readable string describing why an operation was
 	// performed
 	Reason = "reason"
+
+	// Debug is a boolean value for whether debug is set or not.
+	Debug = "debug"
 )

test/helpers/cons.go
@@ -194,7 +194,7 @@ const (
 	deadLockHeader      = "POTENTIAL DEADLOCK:"               // from github.com/sasha-s/go-deadlock/deadlock.go:header
 	segmentationFault   = "segmentation fault"                // from https://github.com/cilium/cilium/issues/3233
 	NACKreceived        = "NACK received for version"         // from https://github.com/cilium/cilium/issues/4003
-	RunInitFailed       = "JoinEP: Command execution failed"  // from https://github.com/cilium/cilium/pull/5052
+	RunInitFailed       = "JoinEP: "                          // from https://github.com/cilium/cilium/pull/5052
 	selfishThresholdMsg = "Goroutine took lock for more than" // from https://github.com/cilium/cilium/pull/5268
 
 	contextDeadlineExceeded = "context deadline exceeded"

commit 971c1fe8e785b894b8491f8cc2603bcee571495a

loader: Fail replaceDatapath on migrate failure
If the `cilium-map-migrate` binary cannot be found or fails in some way,
this should prevent the replaceDatapath() from returning successfully.

Fixes: ca79035d2036 ("loader: replace join endpoint script with loader")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/datapath/loader/netlink.go
@@ -63,7 +63,9 @@ func replaceDatapath(ctx context.Context, ifName string, objPath string, progSec
 	// FIXME: Replace cilium-map-migrate with Golang map migration
 	cmd := exec.CommandContext(ctx, "cilium-map-migrate", "-s", objPath)
 	cmd.Env = bpf.Environment()
-	_, err = cmd.CombinedOutput(log, true)
+	if _, err = cmd.CombinedOutput(log, true); err != nil {
+		return err
+	}
 	defer func() {
 		var retCode string
 		if err == nil {