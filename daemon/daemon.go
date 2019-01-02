g0odi3z2j/cilium

commit 03c5a6e19bc4e36fb224bf6c9d95d8e7702164da

mtu: Replace global variables with Configuration structure
Fixes: #6197

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/daemon.go
@@ -166,6 +166,8 @@ type Daemon struct {
 
 	// k8sSvcCache is a cache of all Kubernetes services and endpoints
 	k8sSvcCache k8s.ServiceCache
+
+	mtuConfig mtu.Configuration
 }
 
 // UpdateProxyRedirect updates the redirect rules in the proxy for a particular
@@ -456,7 +458,7 @@ func (d *Daemon) compileBase() error {
 		args[initArgIPv6NodeIP] = "<nil>"
 	}
 
-	args[initArgMTU] = fmt.Sprintf("%d", mtu.GetDeviceMTU())
+	args[initArgMTU] = fmt.Sprintf("%d", d.mtuConfig.GetDeviceMTU())
 
 	if option.Config.Device != "undefined" {
 		_, err := netlink.LinkByName(option.Config.Device)
@@ -501,7 +503,7 @@ func (d *Daemon) compileBase() error {
 	}
 
 	ipam.ReserveLocalRoutes()
-	node.InstallHostRoutes()
+	node.InstallHostRoutes(d.mtuConfig)
 
 	if option.Config.EnableIPv4 {
 		// Always remove masquerade rule and then re-add it if required
@@ -531,7 +533,7 @@ func (d *Daemon) init() error {
 		log.WithError(err).WithField(logfields.Path, option.Config.StateDir).Fatal("Could not change to runtime directory")
 	}
 
-	if err = createNodeConfigHeaderfile(); err != nil {
+	if err = d.createNodeConfigHeaderfile(); err != nil {
 		return nil
 	}
 
@@ -643,7 +645,7 @@ func (d *Daemon) init() error {
 	return nil
 }
 
-func createNodeConfigHeaderfile() error {
+func (d *Daemon) createNodeConfigHeaderfile() error {
 	nodeConfigPath := option.Config.GetNodeConfigPath()
 	f, err := os.Create(nodeConfigPath)
 	if err != nil {
@@ -703,7 +705,7 @@ func createNodeConfigHeaderfile() error {
 	fmt.Fprintf(fw, "#define SOCKOPS_MAP_SIZE %d\n", sockmap.MaxEntries)
 
 	fmt.Fprintf(fw, "#define TRACE_PAYLOAD_LEN %dULL\n", tracePayloadLen)
-	fmt.Fprintf(fw, "#define MTU %d\n", mtu.GetDeviceMTU())
+	fmt.Fprintf(fw, "#define MTU %d\n", d.mtuConfig.GetDeviceMTU())
 
 	if option.Config.EnableIPv4 {
 		fmt.Fprintf(fw, "#define ENABLE_IPV4\n")
@@ -864,6 +866,7 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 
 		buildEndpointSem: semaphore.NewWeighted(int64(numWorkerThreads())),
 		compilationMutex: new(lock.RWMutex),
+		mtuConfig:        mtu.NewConfiguration(option.Config.Tunnel != option.TunnelDisabled, viper.GetInt(option.MTUName)),
 	}
 
 	debug.RegisterStatusObject("k8s-service-cache", &d.k8sSvcCache)
@@ -1242,8 +1245,8 @@ func (h *getConfig) Handle(params GetConfigParams) middleware.Responder {
 			Options: kvStoreOpts,
 		},
 		Realized:  spec,
-		DeviceMTU: int64(mtu.GetDeviceMTU()),
-		RouteMTU:  int64(mtu.GetRouteMTU()),
+		DeviceMTU: int64(mtuConfig.GetDeviceMTU()),
+		RouteMTU:  int64(mtuConfig.GetRouteMTU()),
 	}
 
 	cfg := &models.DaemonConfiguration{

daemon/daemon_main.go
@@ -128,9 +128,9 @@ var (
 	validLabels           []string
 	toFQDNsMinTTL         int
 	enableDNSPoller       bool
-)
 
-var (
+	mtuConfig mtu.Configuration
+
 	logOpts                = make(map[string]string)
 	kvStoreOpts            = make(map[string]string)
 	fixedIdentity          = make(map[string]string)
@@ -478,7 +478,7 @@ func init() {
 		"Size of the event queue when reading monitor events")
 	viper.BindEnv(option.MonitorQueueSizeName, option.MonitorQueueSizeNameEnv)
 	flags.IntVar(&option.Config.MTU,
-		option.MTUName, mtu.AutoDetect(), "Overwrite auto-detected MTU of underlying network")
+		option.MTUName, 0, "Overwrite auto-detected MTU of underlying network")
 	flags.Bool(option.PrependIptablesChainsName, true, "Prepend custom iptables chains instead of appending")
 	viper.BindEnv(option.PrependIptablesChainsName, option.PrependIptablesChainsNameEnv)
 	flags.StringVar(&v6Address,
@@ -639,10 +639,6 @@ func initEnv(cmd *cobra.Command) {
 
 	if viper.GetBool("pprof") {
 		pprof.Enable()
-	}
-
-	if configuredMTU := viper.GetInt(option.MTUName); configuredMTU != 0 {
-		mtu.UseMTU(configuredMTU)
 	}
 
 	scopedLog := log.WithFields(logrus.Fields{

daemon/health.go
@@ -106,7 +106,7 @@ func (d *Daemon) runCiliumHealthEndpoint() error {
 	if err := health.PingEndpoint(); err != nil {
 		d.cleanupHealthEndpoint()
 		addressing := node.GetNodeAddressing()
-		return health.LaunchAsEndpoint(d, addressing)
+		return health.LaunchAsEndpoint(d, addressing, mtuConfig)
 	}
 	return nil
 }

cilium-health/launch/endpoint.go
@@ -92,11 +92,11 @@ func logFromCommand(cmd *exec.Cmd, netns string) error {
 	return nil
 }
 
-func configureHealthRouting(netns, dev string, addressing *models.NodeAddressing) error {
+func configureHealthRouting(netns, dev string, addressing *models.NodeAddressing, mtuConfig mtu.Configuration) error {
 	routes := []route.Route{}
 
 	if option.Config.EnableIPv4 {
-		v4Routes, err := connector.IPv4Routes(addressing, mtu.GetRouteMTU())
+		v4Routes, err := connector.IPv4Routes(addressing, mtuConfig.GetRouteMTU())
 		if err == nil {
 			routes = append(routes, v4Routes...)
 		} else {
@@ -105,7 +105,7 @@ func configureHealthRouting(netns, dev string, addressing *models.NodeAddressing
 	}
 
 	if option.Config.EnableIPv6 {
-		v6Routes, err := connector.IPv6Routes(addressing, mtu.GetRouteMTU())
+		v6Routes, err := connector.IPv6Routes(addressing, mtuConfig.GetRouteMTU())
 		if err != nil {
 			return fmt.Errorf("Failed to get IPv6 routes")
 		}
@@ -193,7 +193,7 @@ type Annotator interface {
 //
 // CleanupEndpoint() must be called before calling LaunchAsEndpoint() to ensure
 // cleanup of prior cilium-health endpoint instances.
-func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressing) error {
+func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressing, mtuConfig mtu.Configuration) error {
 	var (
 		cmd  = launcher.Launcher{}
 		info = &models.EndpointChangeRequest{
@@ -219,7 +219,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 		ip6Address = ip6WithMask.String()
 	}
 
-	if _, _, err := connector.SetupVethWithNames(vethName, vethPeerName, mtu.GetDeviceMTU(), info); err != nil {
+	if _, _, err := connector.SetupVethWithNames(vethName, vethPeerName, mtuConfig.GetDeviceMTU(), info); err != nil {
 		return fmt.Errorf("Error while creating veth: %s", err)
 	}
 
@@ -258,7 +258,7 @@ func LaunchAsEndpoint(owner endpoint.Owner, hostAddressing *models.NodeAddressin
 	}
 
 	// Set up the endpoint routes
-	if err = configureHealthRouting(info.ContainerName, vethPeerName, hostAddressing); err != nil {
+	if err = configureHealthRouting(info.ContainerName, vethPeerName, hostAddressing, mtuConfig); err != nil {
 		return fmt.Errorf("Error while configuring routes: %s", err)
 	}
 

pkg/datapath/route/route_darwin.go
@@ -18,6 +18,8 @@ package route
 
 import (
 	"fmt"
+
+	"github.com/cilium/cilium/pkg/mtu"
 )
 
 // ToIPCommand converts the route into a full "ip route ..." command
@@ -39,7 +41,7 @@ func (r *Route) ToIPCommand(dev string) []string {
 
 // ReplaceRoute adds or replaces the specified route if necessary. Does nothing
 // for Darwin-based builds.
-func ReplaceRoute(route Route) error {
+func ReplaceRoute(route Route, mtuConfig mtu.Configuration) error {
 	return nil
 }
 

pkg/datapath/route/route_linux.go
@@ -164,7 +164,7 @@ func deleteNexthopRoute(link netlink.Link, routerNet *net.IPNet) error {
 	return nil
 }
 
-func replaceRoute(route Route) (bool, error) {
+func replaceRoute(route Route, mtuConfig mtu.Configuration) (bool, error) {
 	link, err := netlink.LinkByName(route.Device)
 	if err != nil {
 		return false, fmt.Errorf("unable to lookup interface %s: %s", route.Device, err)
@@ -183,9 +183,9 @@ func replaceRoute(route Route) (bool, error) {
 		// local containers and we can use a high MTU for transmit. Otherwise,
 		// it needs to be able to fit within the MTU of tunnel devices.
 		if route.Prefix.Contains(route.Local) {
-			routeSpec.MTU = mtu.GetDeviceMTU()
+			routeSpec.MTU = mtuConfig.GetDeviceMTU()
 		} else {
-			routeSpec.MTU = mtu.GetRouteMTU()
+			routeSpec.MTU = mtuConfig.GetRouteMTU()
 		}
 	}
 
@@ -201,8 +201,8 @@ func replaceRoute(route Route) (bool, error) {
 }
 
 // ReplaceRoute adds or replaces the specified route if necessary
-func ReplaceRoute(route Route) error {
-	replaced, err := replaceRoute(route)
+func ReplaceRoute(route Route, mtuConfig mtu.Configuration) error {
+	replaced, err := replaceRoute(route, mtuConfig)
 	if err != nil {
 		route.getLogger().WithError(err).Error("Unable to add route")
 		return err

pkg/datapath/route/route_linux_test.go
@@ -20,9 +20,10 @@ import (
 	"net"
 	"testing"
 
-	. "gopkg.in/check.v1"
+	"github.com/cilium/cilium/pkg/mtu"
 
 	"github.com/vishvananda/netlink"
+	. "gopkg.in/check.v1"
 )
 
 // Hook up gocheck into the "go test" runner.
@@ -96,7 +97,7 @@ func testReplaceRoute(c *C, prefixStr, nexthopStr string) {
 		Scope:  netlink.SCOPE_LINK,
 	})
 
-	err = ReplaceRoute(rt)
+	err = ReplaceRoute(rt, mtu.NewConfiguration(false, 0))
 	c.Assert(err, IsNil)
 
 	err = DeleteRoute(rt)

pkg/mtu/detect_darwin.go
@@ -11,12 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// +build darwin
 
 package mtu
 
-import (
-	"github.com/cilium/cilium/pkg/logging"
-	"github.com/cilium/cilium/pkg/logging/logfields"
-)
-
-var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "mtu")
+func autoDetect() (int, error) {
+	return EthernetMTU, nil
+}

commit bee29d4a3c77a6fdc1dacb4d70e532f0d4c6a194

daemon: Fix use of global mtuConfig
Fixes: 03c5a6e19bc ("mtu: Replace global variables with Configuration structure")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/daemon.go
@@ -1245,8 +1245,8 @@ func (h *getConfig) Handle(params GetConfigParams) middleware.Responder {
 			Options: kvStoreOpts,
 		},
 		Realized:  spec,
-		DeviceMTU: int64(mtuConfig.GetDeviceMTU()),
-		RouteMTU:  int64(mtuConfig.GetRouteMTU()),
+		DeviceMTU: int64(d.mtuConfig.GetDeviceMTU()),
+		RouteMTU:  int64(d.mtuConfig.GetRouteMTU()),
 	}
 
 	cfg := &models.DaemonConfiguration{

daemon/daemon_main.go
@@ -50,7 +50,6 @@ import (
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
-	"github.com/cilium/cilium/pkg/mtu"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/pidfile"
@@ -125,8 +124,6 @@ var (
 	validLabels           []string
 	toFQDNsMinTTL         int
 	enableDNSPoller       bool
-
-	mtuConfig mtu.Configuration
 
 	logOpts                = make(map[string]string)
 	kvStoreOpts            = make(map[string]string)

daemon/health.go
@@ -106,7 +106,7 @@ func (d *Daemon) runCiliumHealthEndpoint() error {
 	if err := health.PingEndpoint(); err != nil {
 		d.cleanupHealthEndpoint()
 		addressing := node.GetNodeAddressing()
-		return health.LaunchAsEndpoint(d, addressing, mtuConfig)
+		return health.LaunchAsEndpoint(d, addressing, d.mtuConfig)
 	}
 	return nil
 }