g0odi3z2j/cilium

commit e1b72c4c2e9d61fd7334831ad05873a3efb5d671

restore: Check if Kubernetes pod still exists
It is likely that the network namespace no longer exists and thus the network
device check is already catching dead pods but it makes sense to double check
the pod name with the apiserver as an additional verification step.

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/state.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/ipam"
+	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
@@ -38,6 +39,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type endpointRestoreState struct {
@@ -94,6 +96,13 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 		if ep.HasLabels(labels.LabelHealth) {
 			skipRestore = true
 		} else {
+			if ep.K8sPodName != "" && ep.K8sNamespace != "" {
+				_, err := k8s.Client().CoreV1().Pods(ep.K8sNamespace).Get(ep.K8sPodName, meta_v1.GetOptions{})
+				if err != nil {
+					skipRestore = true
+				}
+			}
+
 			if _, err := netlink.LinkByName(ep.IfName); err != nil {
 				scopedLog.Infof("Interface %s could not be found for endpoint being restored, ignoring", ep.IfName)
 				skipRestore = true

commit 4d5971b32080eeb653978ecb18fda042ed90230d

restore: Improve endpoint restore ability with Kubernetes apiserver
Only perform check with apiserver if Kubernetes is enabled. This fixes a panic
when Kubernetes was enabled before and then gets disabled and endpoints are
being restored.

Instead of treating all errors as a trigger to skip restore, only skip restore
and remove the stale endpoint if the apiserver returns a NotFound error.

Fixes: e1b72c4c2e9 ("restore: Check if Kubernetes pod still exists")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/state.go
@@ -39,6 +39,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -96,9 +97,9 @@ func (d *Daemon) restoreOldEndpoints(dir string, clean bool) (*endpointRestoreSt
 		if ep.HasLabels(labels.LabelHealth) {
 			skipRestore = true
 		} else {
-			if ep.K8sPodName != "" && ep.K8sNamespace != "" {
+			if ep.K8sPodName != "" && ep.K8sNamespace != "" && k8s.IsEnabled() {
 				_, err := k8s.Client().CoreV1().Pods(ep.K8sNamespace).Get(ep.K8sPodName, meta_v1.GetOptions{})
-				if err != nil {
+				if err != nil && k8serrors.IsNotFound(err) {
 					skipRestore = true
 				}
 			}