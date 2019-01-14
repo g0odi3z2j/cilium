g0odi3z2j/cilium

commit 0b317ead3cc50925f898e6ed12ce72213f9b7105

client: Print IPAM range in cilium status
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/client/client.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Authors of Cilium
+// Copyright 2016-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -270,21 +270,23 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, allAddresses, 
 
 	if sr.IPAM != nil {
 		var v4CIDR, v6CIDR string
+		v4AllocRange := localNode.PrimaryAddress.IPV4.AllocRange
+		v6AllocRange := localNode.PrimaryAddress.IPV6.AllocRange
 		if localNode != nil {
-			if nIPs := ip.CountIPsInCIDR(localNode.PrimaryAddress.IPV4.AllocRange); nIPs > 0 {
+			if nIPs := ip.CountIPsInCIDR(v4AllocRange); nIPs > 0 {
 				v4CIDR = fmt.Sprintf("/%d", nIPs)
 			}
-			if nIPs := ip.CountIPsInCIDR(localNode.PrimaryAddress.IPV6.AllocRange); nIPs > 0 {
+			if nIPs := ip.CountIPsInCIDR(v6AllocRange); nIPs > 0 {
 				v6CIDR = fmt.Sprintf("/%d", nIPs)
 			}
 		}
-		fmt.Fprintf(w, "IPv4 address pool:\t%d%s allocated\n", len(sr.IPAM.IPV4), v4CIDR)
+		fmt.Fprintf(w, "IPv4 address pool:\t%d%s allocated from %s\n", len(sr.IPAM.IPV4), v4CIDR, v4AllocRange)
 		if allAddresses {
 			for _, ipv4 := range sr.IPAM.IPV4 {
 				fmt.Fprintf(w, "  %s\n", ipv4)
 			}
 		}
-		fmt.Fprintf(w, "IPv6 address pool:\t%d%s allocated\n", len(sr.IPAM.IPV6), v6CIDR)
+		fmt.Fprintf(w, "IPv6 address pool:\t%d%s allocated from %s\n", len(sr.IPAM.IPV6), v6CIDR, v6AllocRange)
 		if allAddresses {
 			for _, ipv6 := range sr.IPAM.IPV6 {
 				fmt.Fprintf(w, "  %s\n", ipv6)

commit 619aaf98009b4d875ab14572a50a75c7925d8219

client: Fix a possible NULL pointer dereference of localNode
Also, do not print `v{4,6}AllocRange` in a status report if it's missing.

Caught by CI:

```
panic: runtime error: invalid memory address or nil pointer dereference
<..>
github.com/cilium/cilium/pkg/client.FormatStatusResponse(0x2ed1fe0, 0xc0004306e0, 0xc000444fc0, 0x10000)
    /go/src/github.com/cilium/cilium/pkg/client/client.go:273
```
Fixes: 0b317ead3 ("client: Print IPAM range in cilium status")

Signed-off-by: Martynas Pumputis <martynas@covalent.io>

pkg/client/client.go
@@ -269,10 +269,12 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, allAddresses, 
 	}
 
 	if sr.IPAM != nil {
-		var v4CIDR, v6CIDR string
-		v4AllocRange := localNode.PrimaryAddress.IPV4.AllocRange
-		v6AllocRange := localNode.PrimaryAddress.IPV6.AllocRange
+		var v4CIDR, v6CIDR, v4AllocRangeFmt, v6AllocRangeFmt string
 		if localNode != nil {
+			v4AllocRange := localNode.PrimaryAddress.IPV4.AllocRange
+			v6AllocRange := localNode.PrimaryAddress.IPV6.AllocRange
+			v4AllocRangeFmt = fmt.Sprintf(" allocated from %s", v4AllocRange)
+			v6AllocRangeFmt = fmt.Sprintf(" allocated from %s", v6AllocRange)
 			if nIPs := ip.CountIPsInCIDR(v4AllocRange); nIPs > 0 {
 				v4CIDR = fmt.Sprintf("/%d", nIPs)
 			}
@@ -280,13 +282,13 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, allAddresses, 
 				v6CIDR = fmt.Sprintf("/%d", nIPs)
 			}
 		}
-		fmt.Fprintf(w, "IPv4 address pool:\t%d%s allocated from %s\n", len(sr.IPAM.IPV4), v4CIDR, v4AllocRange)
+		fmt.Fprintf(w, "IPv4 address pool:\t%d%s%s\n", len(sr.IPAM.IPV4), v4CIDR, v4AllocRangeFmt)
 		if allAddresses {
 			for _, ipv4 := range sr.IPAM.IPV4 {
 				fmt.Fprintf(w, "  %s\n", ipv4)
 			}
 		}
-		fmt.Fprintf(w, "IPv6 address pool:\t%d%s allocated from %s\n", len(sr.IPAM.IPV6), v6CIDR, v6AllocRange)
+		fmt.Fprintf(w, "IPv6 address pool:\t%d%s%s\n", len(sr.IPAM.IPV6), v6CIDR, v6AllocRangeFmt)
 		if allAddresses {
 			for _, ipv6 := range sr.IPAM.IPV6 {
 				fmt.Fprintf(w, "  %s\n", ipv6)