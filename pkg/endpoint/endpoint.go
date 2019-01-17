g0odi3z2j/cilium

commit 81781c2c29447be93630cd0fecd80b682c03a61b

cilium, ipvlan: add initial endpoint ipvlan support
CNI has been changed to create a randomly named ipvlan slave
device as data path. We move that slave into the target net
namespace, we allocate a BPF tail call map with 1 element,
switch to the target net namespace and rename the device to
the CNI target name, plus we attach a sch_clsact qdisc to it
with a tc filter of cls_bpf on the egress hook. Note that
sch_clsact qdisc on egress is *not* executed under qdisc root
lock, so this is processed lockless. The program we load to
cls_bpf is doing: load BPF map fd into register R2, load 0
into register R3, (R1 has implicit skb already), and do a
tail call into the map. Fall-through is just exiting with
TC_ACT_OK. From the BPF map, we retrieve the (global) map ID.
IPAM has then been slightly changed to first set the IP address
to the ipvlan slave, right after that it brings UP the slave
and then sets the route (as otherwise it cannot be set when
!UP). The API to Cilium daemon has been extended to take a
BPF map ID for the endpoint creation call.

Once the daemon receives the endpoint creation API call, and
adds the newly created endpoint via endpoint manager, we
retrieve an fd from the map ID there and pin the BPF map into
the cilium's pod's BPF fs instance with the endpoint ID in
its name, like:

  # kubectl -n kube-system exec cilium-zgwjd -- find /sys/fs/bpf/
  [...]
  /sys/fs/bpf/tc/globals/cilium_ep_config_42444
  /sys/fs/bpf/tc/globals/lxc_ipve_42444            <--
  /sys/fs/bpf/tc/globals/cilium_ep_config_12857
  /sys/fs/bpf/tc/globals/lxc_ipve_12857            <--
  /sys/fs/bpf/tc/globals/cilium_ep_config_24275
  /sys/fs/bpf/tc/globals/cilium_policy_42285
  /sys/fs/bpf/tc/globals/cilium_policy_8284
  /sys/fs/bpf/tc/globals/cilium_ct_any4_global
  /sys/fs/bpf/tc/globals/cilium_ct4_global
  /sys/fs/bpf/tc/globals/cilium_ct_any6_global
  /sys/fs/bpf/tc/globals/cilium_ct6_global
  /sys/fs/bpf/tc/globals/cilium_policy_23548

pkg/endpoint/endpoint.go
@@ -30,6 +30,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common/addressing"
+	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/defaults"
@@ -53,6 +54,8 @@ import (
 	"github.com/cilium/cilium/pkg/u8proto"
 
 	"github.com/sirupsen/logrus"
+
+	"golang.org/x/sys/unix"
 )
 
 const (
@@ -93,6 +96,8 @@ const (
 	CallsMapName = "cilium_calls_"
 	// PolicyGlobalMapName specifies the global tail call map for EP handle_policy() lookup.
 	PolicyGlobalMapName = "cilium_policy"
+	// IpvlanMapName specifies the tail call map for EP on egress used with ipvlan.
+	IpvlanMapName = "lxc_ipve_"
 
 	// HealthCEPPrefix is the prefix used to name the cilium health endpoints' CEP
 	HealthCEPPrefix = "cilium-health-"
@@ -135,6 +140,9 @@ type Endpoint struct {
 	// DockerEndpointID is the Docker network endpoint ID if managed by
 	// libnetwork
 	DockerEndpointID string
+
+	// Corresponding BPF map identifier for tail call map of ipvlan datapath
+	dataPathMapID int
 
 	// IfName is the name of the host facing interface (veth pair) which
 	// connects into the endpoint
@@ -318,6 +326,14 @@ func (e *Endpoint) HasBPFProgram() bool {
 	}
 }
 
+// HasIpvlanDataPath returns whether the daemon is running in ipvlan mode.
+func (e *Endpoint) HasIpvlanDataPath() bool {
+	if e.dataPathMapID > 0 {
+		return true
+	}
+	return false
+}
+
 // GetIngressPolicyEnabledLocked returns whether ingress policy enforcement is
 // enabled for endpoint or not. The endpoint's mutex must be held.
 func (e *Endpoint) GetIngressPolicyEnabledLocked() bool {
@@ -413,6 +429,7 @@ func NewEndpointFromChangeModel(base *models.EndpointChangeRequest) (*Endpoint, 
 		IfName:           base.InterfaceName,
 		K8sPodName:       base.K8sPodName,
 		K8sNamespace:     base.K8sNamespace,
+		dataPathMapID:    int(base.DataPathMapID),
 		IfIndex:          int(base.InterfaceIndex),
 		OpLabels:         pkgLabels.NewOpLabels(),
 		DNSHistory:       fqdn.NewDNSCache(),
@@ -2200,4 +2217,25 @@ func (e *Endpoint) InsertEvent() {
 // endpoint.mutex must be held in read mode at least
 func (e *Endpoint) IsDisconnecting() bool {
 	return e.state == StateDisconnected || e.state == StateDisconnecting
+}
+
+// MapPin retrieves a file descriptor from the map ID from the API call
+// and pins the corresponding map into the BPF file system.
+func (e *Endpoint) MapPin() error {
+	if e.dataPathMapID == 0 {
+		return nil
+	}
+
+	mapFd, err := bpf.MapFdFromID(e.dataPathMapID)
+	if err != nil {
+		return err
+	}
+
+	err = bpf.ObjPin(mapFd, e.BPFIpvlanMapPath())
+	if err != nil {
+		unix.Close(mapFd)
+		return err
+	}
+
+	return nil
 }

daemon/endpoint.go
@@ -216,6 +216,14 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 		return PutEndpointIDFailedCode, err
 	}
 
+	// Now that we have ep.ID we can pin the map from this point. This
+	// also has to happen before the first build took place.
+	if err = ep.MapPin(); err != nil {
+		d.deleteEndpoint(ep)
+		log.WithError(err).Warn("Aborting endpoint tail call map pin")
+		return PutEndpointIDFailedCode, err
+	}
+
 	ep.UpdateLabels(d, addLabels, infoLabels, true)
 
 	select {

api/v1/models/daemon_configuration_status.go
@@ -23,11 +23,19 @@ type DaemonConfigurationStatus struct {
 	// addressing
 	Addressing *NodeAddressing `json:"addressing,omitempty"`
 
+	// datapath mode
+	DatapathMode DatapathMode `json:"datapathMode,omitempty"`
+
 	// MTU on workload facing devices
 	DeviceMTU int64 `json:"deviceMTU,omitempty"`
 
 	// Immutable configuration (read-only)
 	Immutable ConfigurationMap `json:"immutable,omitempty"`
+
+	// Workload facing ipvlan master device ifindex. Set if the
+	// datapath mode is ipvlan.
+	//
+	IpvlanDeviceIfIndex int64 `json:"ipvlanDeviceIfIndex,omitempty"`
 
 	// k8s configuration
 	K8sConfiguration string `json:"k8s-configuration,omitempty"`
@@ -50,9 +58,13 @@ type DaemonConfigurationStatus struct {
 
 /* polymorph DaemonConfigurationStatus addressing false */
 
+/* polymorph DaemonConfigurationStatus datapathMode false */
+
 /* polymorph DaemonConfigurationStatus deviceMTU false */
 
 /* polymorph DaemonConfigurationStatus immutable false */
+
+/* polymorph DaemonConfigurationStatus ipvlanDeviceIfIndex false */
 
 /* polymorph DaemonConfigurationStatus k8s-configuration false */
 
@@ -71,6 +83,11 @@ func (m *DaemonConfigurationStatus) Validate(formats strfmt.Registry) error {
 	var res []error
 
 	if err := m.validateAddressing(formats); err != nil {
+		// prop
+		res = append(res, err)
+	}
+
+	if err := m.validateDatapathMode(formats); err != nil {
 		// prop
 		res = append(res, err)
 	}
@@ -110,6 +127,22 @@ func (m *DaemonConfigurationStatus) validateAddressing(formats strfmt.Registry) 
 			}
 			return err
 		}
+	}
+
+	return nil
+}
+
+func (m *DaemonConfigurationStatus) validateDatapathMode(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.DatapathMode) { // not required
+		return nil
+	}
+
+	if err := m.DatapathMode.Validate(formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("datapathMode")
+		}
+		return err
 	}
 
 	return nil

api/v1/models/datapath_mode.go
@@ -1,0 +1,62 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package models
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"encoding/json"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/validate"
+)
+
+// DatapathMode Datapath mode
+// swagger:model DatapathMode
+
+type DatapathMode string
+
+const (
+	// DatapathModeVeth captures enum value "veth"
+	DatapathModeVeth DatapathMode = "veth"
+	// DatapathModeIpvlan captures enum value "ipvlan"
+	DatapathModeIpvlan DatapathMode = "ipvlan"
+)
+
+// for schema
+var datapathModeEnum []interface{}
+
+func init() {
+	var res []DatapathMode
+	if err := json.Unmarshal([]byte(`["veth","ipvlan"]`), &res); err != nil {
+		panic(err)
+	}
+	for _, v := range res {
+		datapathModeEnum = append(datapathModeEnum, v)
+	}
+}
+
+func (m DatapathMode) validateDatapathModeEnum(path, location string, value DatapathMode) error {
+	if err := validate.Enum(path, location, value, datapathModeEnum); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Validate validates this datapath mode
+func (m DatapathMode) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	// value enum
+	if err := m.validateDatapathModeEnum("", "body", m); err != nil {
+		return err
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}

api/v1/models/endpoint_change_request.go
@@ -27,6 +27,9 @@ type EndpointChangeRequest struct {
 	// Name assigned to container
 	ContainerName string `json:"container-name,omitempty"`
 
+	// ID of datapath tail call map
+	DataPathMapID int64 `json:"data-path-map-id,omitempty"`
+
 	// Docker endpoint ID
 	DockerEndpointID string `json:"docker-endpoint-id,omitempty"`
 
@@ -74,6 +77,8 @@ type EndpointChangeRequest struct {
 /* polymorph EndpointChangeRequest container-id false */
 
 /* polymorph EndpointChangeRequest container-name false */
+
+/* polymorph EndpointChangeRequest data-path-map-id false */
 
 /* polymorph EndpointChangeRequest docker-endpoint-id false */
 

api/v1/models/message_forwarding_statistics.go
@@ -14,7 +14,7 @@ import (
 
 // MessageForwardingStatistics Statistics of a message forwarding entity
 // swagger:model MessageForwardingStatistics
-// +k8s:deepcopy-gen=true
+
 type MessageForwardingStatistics struct {
 
 	// Number of messages denied

api/v1/models/proxy_statistics.go
@@ -17,7 +17,7 @@ import (
 
 // ProxyStatistics Statistics of a set of proxy redirects for an endpoint
 // swagger:model ProxyStatistics
-// +k8s:deepcopy-gen=true
+
 type ProxyStatistics struct {
 
 	// The port the proxy is listening on

api/v1/models/request_response_statistics.go
@@ -14,7 +14,7 @@ import (
 
 // RequestResponseStatistics Statistics of a proxy redirect
 // swagger:model RequestResponseStatistics
-// +k8s:deepcopy-gen=true
+
 type RequestResponseStatistics struct {
 
 	// requests

commit 7cb18dfe74b447e649e573f86aa22358308e0d19

cilium, ipvlan: fix tail call map fd leakage
After pinning we must close the fd we got before in success *and*
in error case.

Fixes: 81781c2c2944 ("cilium, ipvlan: add initial endpoint ipvlan support")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/endpoint/endpoint.go
@@ -2232,10 +2232,6 @@ func (e *Endpoint) MapPin() error {
 	}
 
 	err = bpf.ObjPin(mapFd, e.BPFIpvlanMapPath())
-	if err != nil {
-		unix.Close(mapFd)
-		return err
-	}
-
-	return nil
+	unix.Close(mapFd)
+	return err
 }