g0odi3z2j/cilium

commit c8072807c09ebeb5170d6b07f117b97f0763f423

daemon: do deep copy of model.StatusResponse
This commit is going to be squashed after all reviews.

Signed-off-by: Martynas Pumputis <martynas@covalent.io>

daemon/status.go
@@ -17,7 +17,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/daemon"
@@ -31,6 +30,7 @@ import (
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/go-openapi/strfmt"
+	"github.com/mohae/deepcopy"
 )
 
 func (d *Daemon) getK8sStatus() *models.K8sStatus {
@@ -97,8 +97,15 @@ func (d *Daemon) getStatus() models.StatusResponse {
 	d.statusCollectMutex.RLock()
 	defer d.statusCollectMutex.RUnlock()
 
-	// TODO(brb) we should do a deep copy instead, as statusResponse contains some references
-	sr := d.statusResponse
+	// d.statusResponse contains references, so we do a deep copy to be able to
+	// safely use sr after the method has returned
+	sr, ok := deepcopy.Copy(d.statusResponse).(models.StatusResponse)
+	if !ok {
+		// Shouldn't happen, but just in case...
+		log.Error("Type assertion failed for models.StatusResponse")
+		return models.StatusResponse{}
+	}
+
 	sr.Stale = stale
 
 	switch {

commit e22fd99560eeff6905488283dc09f60dfb6f9cad

daemon: Use DeepCopy method from StatusResponse
This change replaces usage of mohae/deepcopy with a generated DeepCopy
method of StatusResponse. mohae/deepcopy did not handle copying
strfmt.DateTime value properly.

Fixes: c8072807c09e ("daemon: do deep copy of model.StatusResponse")
Fixes: #6605

Signed-off-by: Michal Rostecki <mrostecki@suse.de>

daemon/status.go
@@ -30,7 +30,6 @@ import (
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/go-openapi/strfmt"
-	"github.com/mohae/deepcopy"
 )
 
 func (d *Daemon) getK8sStatus() *models.K8sStatus {
@@ -99,12 +98,7 @@ func (d *Daemon) getStatus() models.StatusResponse {
 
 	// d.statusResponse contains references, so we do a deep copy to be able to
 	// safely use sr after the method has returned
-	sr, ok := deepcopy.Copy(d.statusResponse).(models.StatusResponse)
-	if !ok {
-		// Shouldn't happen, but just in case...
-		log.Error("Type assertion failed for models.StatusResponse")
-		return models.StatusResponse{}
-	}
+	sr := *d.statusResponse.DeepCopy()
 
 	sr.Stale = stale
 