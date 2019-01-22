g0odi3z2j/cilium

commit bf40a557feda9205fafe6f4e1e97be04c4b53f8b

pkg: bpf: sockops helpers for loading and attaching programs
Add helpers to load and attach sockops and sockmap programs.

This adds necessary code to create a cgroupv2 mount point. Compile and
load sockops programs. Attach sockops programs to cgroupv2. At which
point sock op events will be handled by the program. For our use case
the two relevant events are, BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB and
BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB corresponding to sockets entering
the ESTABLISHED state.

Then helpers to conpile and load the sk_msg program is also included.
Along with code to attach the sk_msg program to the sockmap so that
it will be run on any sockets bound to the sockmap.

Four high-level routines manage the BPF programs for the above,

	SockmapEnable
	SockmapDisable
	SkmsgEnable
	SkmsgDisable

Then  the routine to manage cgroupv2 is,

	CheckOrMountCgrpFS

We default the cgroupv2 mount point to "/var/run/cilium/cgroupv2". Its
OK to have multiple cgroupv2 root mount points so this _should_
not interfere with any existing cgroupv2. I tested this on a
recent Ubuntu distribution that had a "unified" cgroup structure
using cgroupv2 and didn't observer any issues.

Because we do not have golang hooks for libbpf yet, this execs
bpftool and scraps the return. Its not pretty but it works. There
is a larger todo item to convert iproute2/bpftool usage over to
go calls into libbpf. The bigger implication is the system needs
a recent version of bpftool. Minimum version needed is the bpftool
packaged with the 4.20 Linux kernel.

Finally, the Compile hook had to be exposed to compile programs
from pkg.

As a matter of style there is some question about how many of the
strings to define in the top of file var block vs in the actual code.
I struck some balance that I thought was reasonable.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/sockops/sockops.go
@@ -1,0 +1,468 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sockops
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/datapath/loader"
+	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/mountinfo"
+	"github.com/cilium/cilium/pkg/option"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	// Path to where cgroup is mounted
+	cgroupRoot = defaults.DefaultCgroupRoot
+
+	// Only mount a single instance
+	cgrpMountOnce sync.Once
+
+	// Default prefix for map objects
+	mapPrefix = defaults.DefaultMapPrefix
+
+	contextTimeout = 5 * time.Minute
+)
+
+const (
+	cSockops = "bpf_sockops.c"
+	oSockops = "bpf_sockops.o"
+	eSockops = "bpf_sockops"
+
+	cIPC = "bpf_redir.c"
+	oIPC = "bpf_redir.o"
+	eIPC = "bpf_redir"
+
+	sockMap = "sock_ops_map"
+)
+
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "sockops")
+
+// setCgroupRoot will set the path to mount cgroupv2
+func setCgroupRoot(path string) {
+	cgroupRoot = path
+}
+
+// mountCgroup mounts the Cgroup v2 filesystem into the desired cgroupRoot directory.
+func mountCgroup() error {
+	prog := "mount"
+	cgroupRootStat, err := os.Stat(cgroupRoot)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(cgroupRoot, 0755); err != nil {
+				return fmt.Errorf("Unable to create cgroup mount directory: %s", err)
+			}
+		} else {
+			return fmt.Errorf("Failed to stat the mount path %s: %s", cgroupRoot, err)
+		}
+	} else if !cgroupRootStat.IsDir() {
+		return fmt.Errorf("%s is a file which is not a directory", cgroupRoot)
+	}
+
+	mntArgs := []string{"-t", "cgroup2", "none", cgroupRoot}
+	_, err = exec.Command(prog, mntArgs...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Failed to mount %s: %s", cgroupRoot, err)
+	}
+	return nil
+}
+
+// checkOrMountCustomLocation tries to check or mount the BPF filesystem in the
+// given path.
+func cgrpCheckOrMountLocation(cgroupRoot string) error {
+	setCgroupRoot(cgroupRoot)
+
+	// Check whether the custom location has a mount.
+	mounted, cgroupInstance, err := mountinfo.IsMountFS(mountinfo.FilesystemTypeCgroup2, cgroupRoot)
+	if err != nil {
+		return err
+	}
+
+	// If the custom location has no mount, let's mount there.
+	if !mounted {
+		if err := mountCgroup(); err != nil {
+			return err
+		}
+	}
+
+	if !cgroupInstance {
+		return fmt.Errorf("Mount in the custom directory %s has a different filesystem than cgroup2", cgroupRoot)
+	}
+	return nil
+}
+
+// CheckOrMountCgrpFS this checks if the cilium cgroup2 root mount point is
+// mounted and if not mounts it. If mapRoot is "" it will mount the default
+// location. It is harmless to have multiple cgroupv2 root mounts so unlike
+// BPFFS case we simply mount at the cilium default regardless if the system
+// has another mount created by systemd or otherwise.
+func CheckOrMountCgrpFS(mapRoot string) {
+	cgrpMountOnce.Do(func() {
+		if mapRoot == "" {
+			mapRoot = cgroupRoot
+		}
+		err := cgrpCheckOrMountLocation(mapRoot)
+		// Failed cgroup2 mount is not a fatal error, sockmap will be disabled however
+		if err == nil {
+			log.Info("Mounted Cgroup2 filesystem %s", mapRoot)
+		}
+	})
+}
+
+// BPF programs and sockmaps working on cgroups
+func bpftoolMapAttach(progID string, mapID string) error {
+	prog := "bpftool"
+
+	args := []string{"prog", "attach", "id", progID, "msg_verdict", "id", mapID}
+	log.WithFields(logrus.Fields{
+		"bpftool": prog,
+		"args":    args,
+	}).Debug("Map Attach BPF Object:")
+	_, err := exec.Command(prog, args...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Failed to attach prog(%s) to map(%s): %s", progID, mapID, err)
+	}
+	return nil
+}
+
+// #bpftool cgroup attach $cgrp sock_ops /sys/fs/bpf/$bpfObject
+func bpftoolAttach(bpfObject string) error {
+	prog := "bpftool"
+	bpffs := bpf.GetMapRoot() + "/" + bpfObject
+	cgrp := cgroupRoot
+
+	args := []string{"cgroup", "attach", cgrp, "sock_ops", "pinned", bpffs}
+	log.WithFields(logrus.Fields{
+		"bpftool": prog,
+		"args":    args,
+	}).Debug("Attach BPF Object:")
+	_, err := exec.Command(prog, args...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Failed to attach %s: %s", bpfObject, err)
+	}
+	return nil
+}
+
+// #bpftool cgroup detach $cgrp sock_ops /sys/fs/bpf/$bpfObject
+func bpftoolDetach(bpfObject string) error {
+	prog := "bpftool"
+	bpffs := bpf.GetMapRoot() + "/" + bpfObject
+	cgrp := cgroupRoot
+
+	args := []string{"cgroup", "detach", cgrp, "sock_ops", "pinned", bpffs}
+	log.WithFields(logrus.Fields{
+		"bpftool": prog,
+		"args":    args,
+	}).Debug("Detach BPF Object:")
+	_, err := exec.Command(prog, args...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Failed to detach %s: %s", bpfObject, err)
+	}
+	return nil
+
+}
+
+// #bpftool prog load $bpfObject /sys/fs/bpf/sockops
+func bpftoolLoad(bpfObject string, bpfFsFile string) error {
+	sockopsMaps := [...]string{
+		"cilium_lxc",
+		"cilium_ipcache",
+		"cilium_metric",
+		"cilium_events",
+		"sock_ops_map",
+		"cilium_ep_to_policy",
+		"cilium_proxy4", "cilium_proxy6",
+		"cilium_lb6_reverse_nat", "cilium_lb4_reverse_nat",
+		"cilium_lb6_services", "cilium_lb4_services",
+		"cilium_lb6_rr_seq", "cilium_lb4_seq",
+		"cilium_lb6_rr_seq", "cilium_lb4_seq",
+	}
+
+	prog := "bpftool"
+	var mapArgList []string
+	bpffs := bpf.GetMapRoot() + "/" + bpfFsFile
+
+	maps, err := ioutil.ReadDir(bpf.GetMapRoot() + "/tc/globals/")
+	if err != nil {
+		return err
+	}
+
+	for _, f := range maps {
+		// Ignore all backing files
+		if strings.HasPrefix(f.Name(), "..") {
+			continue
+		}
+
+		use := func() bool {
+			for _, n := range sockopsMaps {
+				if f.Name() == n {
+					return true
+				}
+			}
+			return false
+		}()
+
+		if !use {
+			continue
+		}
+
+		mapString := []string{"map", "name", f.Name(), "pinned", bpf.GetMapRoot() + "/tc/globals/" + f.Name()}
+		mapArgList = append(mapArgList, mapString...)
+	}
+
+	args := []string{"-m", "prog", "load", bpfObject, bpffs}
+	args = append(args, mapArgList...)
+	log.WithFields(logrus.Fields{
+		"bpftool": prog,
+		"args":    args,
+	}).Debug("Load BPF Object:")
+	_, err = exec.Command(prog, args...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Failed to load %s: %s", bpfObject, err)
+	}
+	return nil
+}
+
+// #rm $bpfObject
+func bpftoolUnload(bpfObject string) {
+	bpffs := bpf.GetMapRoot() + "/" + bpfObject
+
+	os.Remove(bpffs)
+}
+
+// #bpftool prog show pinned /sys/fs/bpf/
+func bpftoolGetProgID(progName string) (string, error) {
+	bpffs := bpf.GetMapRoot() + "/" + progName
+	prog := "bpftool"
+
+	args := []string{"prog", "show", "pinned", bpffs}
+	log.WithFields(logrus.Fields{
+		"bpftool": prog,
+		"args":    args,
+	}).Debug("GetProgID:")
+	output, err := exec.Command(prog, args...).CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("Failed to load %s: %s", progName, err)
+	}
+
+	// Scrap the prog_id out of the bpftool output after libbpf is dual licensed
+	// we will use programatic API.
+	s := strings.Fields(string(output))
+	if s[0] == "" {
+		return "", fmt.Errorf("Failed to find prog %s: %s", progName, err)
+	}
+	progID := strings.Split(s[0], ":")
+	return progID[0], nil
+}
+
+// #bpftool prog show pinned /sys/fs/bpf/bpf_sockops
+// #bpftool map show id 21
+func bpftoolGetMapID(progName string, mapName string) (int, error) {
+	bpffs := bpf.GetMapRoot() + "/" + progName
+	prog := "bpftool"
+
+	args := []string{"prog", "show", "pinned", bpffs}
+	log.WithFields(logrus.Fields{
+		"bpftool": prog,
+		"args":    args,
+	}).Debug("GetMapID:")
+	output, err := exec.Command(prog, args...).CombinedOutput()
+	if err != nil {
+		return 0, fmt.Errorf("Failed to load %s: %s", progName, err)
+	}
+
+	// Find the mapID out of the bpftool output
+	s := strings.Fields(string(output))
+	for i := range s {
+		if s[i] == "map_ids" {
+			id := strings.Split(s[i+1], ",")
+			for j := range id {
+				args := []string{"map", "show", "id", id[j]}
+				output, err := exec.Command(prog, args...).CombinedOutput()
+				if err != nil {
+					return 0, err
+				}
+
+				if strings.Contains(string(output), mapName) {
+					mapID, _ := strconv.Atoi(id[j])
+					return mapID, nil
+				}
+			}
+			break
+		}
+	}
+	return 0, nil
+}
+
+// #bpftool map pin id map_id /sys/fs/bpf/tc/globals
+func bpftoolPinMapID(mapName string, mapID int) error {
+	bpffs := bpf.GetMapRoot()
+	globals := bpffs + "/" + mapPrefix + "/"
+	mapFile := globals + mapName
+	prog := "bpftool"
+
+	args := []string{"map", "pin", "id", strconv.Itoa(mapID), mapFile}
+	log.WithFields(logrus.Fields{
+		"bpftool": prog,
+		"args":    args,
+	}).Debug("Map pin:")
+	_, err := exec.Command(prog, args...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Failed to pin map %d(%s): %s", mapID, mapName, err)
+	}
+
+	return nil
+}
+
+// #clang ... | llc ...
+func bpfCompileProg(src string, dst string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+
+	srcpath := filepath.Join("sockops", src)
+	outpath := filepath.Join(dst)
+
+	err := loader.Compile(ctx, srcpath, outpath)
+	if err != nil {
+		return fmt.Errorf("failed compile %s: %s", srcpath, err)
+	}
+	return nil
+}
+
+func bpfLoadMapProg(object string, load string) error {
+	sockops := object
+	sockopsObj := option.Config.StateDir + "/" + sockops
+	sockopsLoad := load
+
+	err := bpftoolLoad(sockopsObj, sockopsLoad)
+	if err != nil {
+		return err
+	}
+
+	progID, err := bpftoolGetProgID(load)
+	if err != nil {
+		return err
+	}
+
+	_mapID, err := bpftoolGetMapID("bpf_sockops", sockMap)
+	mapID := strconv.Itoa(_mapID)
+	if err != nil {
+		return err
+	}
+
+	err = bpftoolMapAttach(progID, mapID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// SkmsgEnable will compile and attach the SK_MSG programs to the
+// sockmap. After this all sockets added to the sock_ops_map will
+// have sendmsg/sendfile calls running through BPF program.
+func SkmsgEnable() error {
+	err := bpfCompileProg(cIPC, oIPC)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	err = bpfLoadMapProg(oIPC, eIPC)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	log.Info("Sockmsg Enabled, bpf_redir loaded")
+	return nil
+}
+
+// SkmsgDisable "unloads" the SK_MSG program. This simply deletes
+// the file associated with the program.
+func SkmsgDisable() {
+	bpftoolUnload(eIPC)
+	log.Info("Sockmsg Disabled.")
+}
+
+// First user of sockops root is sockops load programs so we ensure the sockops
+// root path no longer changes.
+func bpfLoadAttachProg(object string, load string, mapName string) (int, int, error) {
+	sockopsObj := option.Config.StateDir + "/" + object
+	mapID := 0
+
+	err := bpftoolLoad(sockopsObj, load)
+	if err != nil {
+		return 0, 0, err
+	}
+	err = bpftoolAttach(load)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if mapName != "" {
+		mapID, err = bpftoolGetMapID(load, mapName)
+		if err != nil {
+			return 0, mapID, err
+		}
+
+		err = bpftoolPinMapID(mapName, mapID)
+		if err != nil {
+			return 0, mapID, err
+		}
+	}
+	return 0, mapID, nil
+}
+
+// SockmapEnable will compile sockops programs and attach the sockops programs
+// to the cgroup. After this all TCP connect events will be filtered by a BPF
+// sockops program.
+func SockmapEnable() error {
+	err := bpfCompileProg(cSockops, oSockops)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	progID, mapID, err := bpfLoadAttachProg(oSockops, eSockops, sockMap)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	log.Info("Sockmap Enabled: bpf_sockops prog_id %d and map_id %d loaded", progID, mapID)
+	return nil
+}
+
+// SockmapDisable will detach any sockmap programs from cgroups then "unload"
+// all the programs and maps associated with it. Here "unload" just means
+// deleting the file associated with the map.
+func SockmapDisable() {
+	mapName := mapPrefix + "/" + sockMap
+	bpftoolDetach(eSockops)
+	bpftoolUnload(eSockops)
+	bpftoolUnload(mapName)
+	log.Info("Sockmap disabled.")
+}

daemon/daemon.go
@@ -76,6 +76,7 @@ import (
 	"github.com/cilium/cilium/pkg/proxy"
 	"github.com/cilium/cilium/pkg/proxy/logger"
 	"github.com/cilium/cilium/pkg/revert"
+	"github.com/cilium/cilium/pkg/sockops"
 	"github.com/cilium/cilium/pkg/u8proto"
 	"github.com/cilium/cilium/pkg/workloads"
 
@@ -818,6 +819,12 @@ func (d *Daemon) init() error {
 		if err := d.compileBase(); err != nil {
 			return err
 		}
+
+		// Remove any old sockops and re-enable with _new_ programs
+		sockops.SockmapDisable()
+		sockops.SkmsgDisable()
+		sockops.SockmapEnable()
+		sockops.SkmsgEnable()
 
 		// Set up the list of IPCache listeners in the daemon, to be
 		// used by syncLXCMap().

daemon/main.go
@@ -56,6 +56,7 @@ import (
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/pprof"
 	"github.com/cilium/cilium/pkg/service"
+	"github.com/cilium/cilium/pkg/sockops"
 	"github.com/cilium/cilium/pkg/version"
 	"github.com/cilium/cilium/pkg/versioncheck"
 	"github.com/cilium/cilium/pkg/workloads"
@@ -96,6 +97,7 @@ var (
 	// Arguments variables keep in alphabetical order
 
 	bpfRoot               string
+	cgroupRoot            string
 	cmdRefDir             string
 	debugVerboseFlags     []string
 	disableConntrack      bool
@@ -355,6 +357,8 @@ func init() {
 		option.AutoIPv6NodeRoutesName, false, "Automatically adds IPv6 L3 routes to reach other nodes for non-overlay mode (--device) (BETA)")
 	flags.StringVar(&bpfRoot,
 		"bpf-root", "", "Path to BPF filesystem")
+	flags.StringVar(&cgroupRoot,
+		"cgroup-root", "", "Path to Cgroup2 filesystem")
 	flags.Bool(option.BPFCompileDebugName, false, "Enable debugging of the BPF compilation process")
 	flags.Int(option.ClusterIDName, 0, "Unique identifier of the cluster")
 	viper.BindEnv(option.ClusterIDName, option.ClusterIDEnv)
@@ -703,6 +707,7 @@ func initEnv(cmd *cobra.Command) {
 	// useful if the daemon is being round inside a namespace and the
 	// BPF filesystem is mapped into the slave namespace.
 	bpf.CheckOrMountFS(bpfRoot)
+	sockops.CheckOrMountCgrpFS(cgroupRoot)
 
 	option.Config.Opts.SetBool(option.Debug, viper.GetBool("debug"))
 

pkg/datapath/loader/loader.go
@@ -130,3 +130,19 @@ func ReloadDatapath(ctx context.Context, ep endpoint) error {
 	}
 	return reloadDatapath(ctx, ep, &dirs)
 }
+
+// Compile compiles a BPF program generating an object file.
+func Compile(ctx context.Context, src string, out string) error {
+	debug := viper.GetBool(option.BPFCompileDebugName)
+	prog := progInfo{
+		Source:     src,
+		Output:     out,
+		OutputType: outputObject,
+	}
+	dirs := directoryInfo{
+		Library: option.Config.BpfDir,
+		Runtime: option.Config.StateDir,
+		Output:  option.Config.StateDir,
+	}
+	return compile(ctx, &prog, &dirs, debug)
+}

pkg/defaults/defaults.go
@@ -72,6 +72,9 @@ const (
 	// DefaultMapRoot is the default path where BPFFS should be mounted
 	DefaultMapRoot = "/sys/fs/bpf"
 
+	// DefaultCgroupRoot is the default path where cilium cgroup2 should be mounted
+	DefaultCgroupRoot = "/var/run/cilium/cgroupv2"
+
 	// DefaultMapRootFallback is the path which is used when /sys/fs/bpf has
 	// a mount, but with the other filesystem than BPFFS.
 	DefaultMapRootFallback = "/run/cilium/bpffs"

pkg/mountinfo/mountinfo.go
@@ -23,9 +23,9 @@ import (
 )
 
 const (
-	// FilesystemTypeBPFFS is a filesystem type name for BPFFS which is used
-	// in /proc/pid/mountinfo
-	FilesystemTypeBPFFS = "bpf"
+	// FilesystemType names for filesystem which are used in /proc/pid/mountinfo
+	FilesystemTypeBPFFS   = "bpf"
+	FilesystemTypeCgroup2 = "cgroup2"
 
 	mountInfoFilepath = "/proc/self/mountinfo"
 )

commit c5868466923d7955e264ee36171ae6528db891b2

sockops: rename sock_ops_map to cilium_sock_ops_map
sock_ops_map was left alone while `cilium cleanup`
`cilium cleanup` use cilium_ prefix for removing
map.

Fixes: bf40a557fe (pkg: bpf: sockops helpers for loading and attaching programs)

Signed-off-by: Nirmoy Das <ndas@suse.de>

bpf/sockops/sockops_config.h
@@ -16,6 +16,6 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifndef SOCK_OPS_MAP
-#define SOCK_OPS_MAP sock_ops_map
+#define SOCK_OPS_MAP test_cilium_sock_ops_map
 #endif
 #define SOCKOPS_MAP_SIZE 65535

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=8ea5cff27ff7138b797ff38dae95ed419c4257b9
+GO_BINDATA_SHA1SUM=13a6474589a65bb6da0c94927a5d94081a53c82a
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

pkg/maps/sockmap/sockmap.go
@@ -89,7 +89,7 @@ func NewSockmapKey(dip, sip net.IP, sport, dport uint32) SockmapKey {
 var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "sockmap")
 
 const (
-	mapName = "sock_ops_map"
+	mapName = "cilium_sock_ops_map"
 
 	// MaxEntries represents the maximum number of endpoints in the map
 	MaxEntries = 65535

pkg/sockops/sockops.go
@@ -60,7 +60,7 @@ const (
 	oIPC = "bpf_redir.o"
 	eIPC = "bpf_redir"
 
-	sockMap = "sock_ops_map"
+	sockMap = "cilium_sock_ops_map"
 )
 
 var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "sockops")
@@ -194,7 +194,7 @@ func bpftoolLoad(bpfObject string, bpfFsFile string) error {
 		"cilium_ipcache",
 		"cilium_metric",
 		"cilium_events",
-		"sock_ops_map",
+		"cilium_sock_ops_map",
 		"cilium_ep_to_policy",
 		"cilium_proxy4", "cilium_proxy6",
 		"cilium_lb6_reverse_nat", "cilium_lb4_reverse_nat",
@@ -383,7 +383,7 @@ func bpfLoadMapProg(object string, load string) error {
 }
 
 // SkmsgEnable will compile and attach the SK_MSG programs to the
-// sockmap. After this all sockets added to the sock_ops_map will
+// sockmap. After this all sockets added to the cilium_sock_ops_map will
 // have sendmsg/sendfile calls running through BPF program.
 func SkmsgEnable() error {
 	err := bpfCompileProg(cIPC, oIPC)