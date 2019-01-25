g0odi3z2j/cilium

commit 06cb733846fe4e129eb0f3aef79a666626ec59e3

endpoint: do not finalize proxy state in DryMode
Signed-off by: Ian Vernon <ian@cilium.io>

pkg/endpoint/bpf.go
@@ -544,7 +544,10 @@ func (e *Endpoint) regenerateBPF(owner Owner, regenContext *regenerationContext)
 	// Also keep track of the regeneration finalization code that can't be
 	// reverted, and execute it in case of regeneration success.
 	defer func() {
-		e.finalizeProxyState(regenContext, reterr)
+		// Ignore finalizing of proxy state in dry mode.
+		if option.Config.DryMode {
+			e.finalizeProxyState(regenContext, reterr)
+		}
 	}()
 
 	if err != nil {

commit 0fbd3ea1f2f061081803b8a1f3ca3e97fa0b659f

endpoint: fix logic to finalize proxy state in dry mode
The logic was inverted for checking whether proxy state should be finalized
or not.

Fixes: 06cb733846fe4e129eb0f3aef79a666626ec59e3

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/endpoint/bpf.go
@@ -542,7 +542,7 @@ func (e *Endpoint) regenerateBPF(owner Owner, regenContext *regenerationContext)
 	// reverted, and execute it in case of regeneration success.
 	defer func() {
 		// Ignore finalizing of proxy state in dry mode.
-		if option.Config.DryMode {
+		if !option.Config.DryMode {
 			e.finalizeProxyState(regenContext, reterr)
 		}
 	}()