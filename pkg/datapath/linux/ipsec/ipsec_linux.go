g0odi3z2j/cilium

commit cbc98e7a471817e74759a1cfdd81a119e3fd7db9

cilium: ipsec, import keys via certs file
Initially, we will support a pre-shared key model where all endpoints
have a pre-shared keys loaded from a file. The file layout is as
follows with a key per line.

authname authkey encname enckey [scope]

This allows keys to be scoped so that it is possible to have a key
per node if desired. If scope is omitted the key is considered a
default key used if a more specific key is not found. My three node
testing file reads as follows,

hmac(sha256) abcdefghijklmnopqrstuvwzyzABCDEF cbc(aes) abcdefghijklmnopqrstuvwzyzABCDEF ubuntu-kvm1
hmac(sha256) abcdefghijklmnopqrstuvwzyzABCDEF cbc(aes) abcdefghijklmnopqrstuvwzyzABCDEF ubuntu-kvm2
hmac(sha256) abcdefghijklmnopqrstuvwzyzABCDEF cbc(aes) abcdefghijklmnopqrstuvwzyzABCDEF

The file is loaded at init time so any changes will have to be consumed
by a restart. In the future we can add watchers for the file and allow
using etcd secrets (files).

Also note the logic is built so that other methods of key exchanges
can be added later if folks want a different model.

Looking forward we can more specific scope types to do per identity
keys if needed.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -17,7 +17,11 @@
 package ipsec
 
 import (
+	"bufio"
+	"fmt"
 	"net"
+	"os"
+	"strings"
 
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -26,24 +30,23 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type ipSecKeys struct {
+type ipSecKey struct {
 	Spi   int
 	ReqID int
 	Auth  *netlink.XfrmStateAlgo
 	Crypt *netlink.XfrmStateAlgo
 }
 
-var ipSecKeysGlobal = &ipSecKeys{
-	Spi:   1,
-	ReqID: 1,
-	Auth: &netlink.XfrmStateAlgo{
-		Name: "hmac(sha256)",
-		Key:  []byte("abcdefghijklmnopqrstuvwzyzABCDEF"),
-	},
-	Crypt: &netlink.XfrmStateAlgo{
-		Name: "cbc(aes)",
-		Key:  []byte("abcdefghijklmnopqrstuvwzyzABCDEF"),
-	},
+// ipSecKeysGlobal is safe to read unlocked because the only writers are from
+// daemon init time before any readers will be online.
+var ipSecKeysGlobal = make(map[string]*ipSecKey)
+
+func getIPSecKeys(ip net.IP) *ipSecKey {
+	key, scoped := ipSecKeysGlobal[ip.String()]
+	if scoped == false {
+		key, _ = ipSecKeysGlobal[""]
+	}
+	return key
 }
 
 func ipSecNewState() *netlink.XfrmState {
@@ -60,7 +63,7 @@ func ipSecNewPolicy() *netlink.XfrmPolicy {
 	return &policy
 }
 
-func ipSecAttachPolicyTempl(policy *netlink.XfrmPolicy, keys *ipSecKeys, srcIP, dstIP net.IP) {
+func ipSecAttachPolicyTempl(policy *netlink.XfrmPolicy, keys *ipSecKey, srcIP, dstIP net.IP) {
 	tmpl := netlink.XfrmPolicyTmpl{
 		Proto: netlink.XFRM_PROTO_ESP,
 		Mode:  netlink.XFRM_MODE_TUNNEL,
@@ -73,7 +76,7 @@ func ipSecAttachPolicyTempl(policy *netlink.XfrmPolicy, keys *ipSecKeys, srcIP, 
 	policy.Tmpls = append(policy.Tmpls, tmpl)
 }
 
-func ipSecJoinState(state *netlink.XfrmState, keys *ipSecKeys) {
+func ipSecJoinState(state *netlink.XfrmState, keys *ipSecKey) {
 	state.Auth = keys.Auth
 	state.Crypt = keys.Crypt
 	state.Spi = keys.Spi
@@ -83,7 +86,7 @@ func ipSecJoinState(state *netlink.XfrmState, keys *ipSecKeys) {
 func ipSecReplaceState(remoteIP, localIP net.IP) error {
 	state := ipSecNewState()
 
-	ipSecJoinState(state, ipSecKeysGlobal)
+	ipSecJoinState(state, getIPSecKeys(localIP))
 	state.Src = localIP
 	state.Dst = remoteIP
 	return netlink.XfrmStateAdd(state)
@@ -107,7 +110,7 @@ func ipSecReplacePolicyInFwd(src, dst *net.IPNet, dir netlink.Dir) error {
 		Value: linux_defaults.RouteMarkDecrypt,
 		Mask:  linux_defaults.RouteMarkMask,
 	}
-	ipSecAttachPolicyTempl(policy, ipSecKeysGlobal, src, dst)
+	ipSecAttachPolicyTempl(policy, getIPSecKeys(dst.IP), src.IP, dst.IP)
 	return netlink.XfrmPolicyUpdate(policy)
 }
 
@@ -120,7 +123,7 @@ func ipSecReplacePolicyOut(src, dst *net.IPNet) error {
 		Value: linux_defaults.RouteMarkEncrypt,
 		Mask:  linux_defaults.RouteMarkMask,
 	}
-	ipSecAttachPolicyTempl(policy, ipSecKeysGlobal, src, dst)
+	ipSecAttachPolicyTempl(policy, getIPSecKeys(dst.IP), src.IP, dst.IP)
 	return netlink.XfrmPolicyUpdate(policy)
 }
 
@@ -240,6 +243,50 @@ func DeleteIPSecEndpoint(src, local net.IP) error {
 	err = ipSecDeletePolicy(src, local)
 	if err != nil {
 		scopedLog.WithError(err).Warning("unable to delete IPSec (policy) context\n")
+	}
+	return nil
+}
+
+// LoadIPSecKeysFile imports IPSec auth and crypt keys from a file. The format
+// is to put a key per line as follows, (auth-algo auth-key enc-algo enc-key)
+func LoadIPSecKeysFile(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		ipSecKey := &ipSecKey{
+			Spi:   1,
+			ReqID: 1,
+		}
+
+		// Scanning IPsec keys formatted as follows,
+		//    auth-algo auth-key enc-algo enc-key
+		s := strings.Split(scanner.Text(), " ")
+		if len(s) < 4 {
+			return fmt.Errorf("missing IPSec keys or invalid format")
+		}
+
+		authname, authkey := s[0], []byte(s[1])
+		encname, enckey := s[2], []byte(s[3])
+
+		ipSecKey.Auth = &netlink.XfrmStateAlgo{
+			Name: authname,
+			Key:  authkey,
+		}
+		ipSecKey.Crypt = &netlink.XfrmStateAlgo{
+			Name: encname,
+			Key:  enckey,
+		}
+		if len(s) == 5 {
+			ipSecKeysGlobal[s[4]] = ipSecKey
+		} else {
+			ipSecKeysGlobal[""] = ipSecKey
+		}
 	}
 	return nil
 }

daemon/daemon.go
@@ -44,6 +44,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath"
 	bpfIPCache "github.com/cilium/cilium/pkg/datapath/ipcache"
 	"github.com/cilium/cilium/pkg/datapath/iptables"
+	"github.com/cilium/cilium/pkg/datapath/linux/ipsec"
 	"github.com/cilium/cilium/pkg/datapath/prefilter"
 	"github.com/cilium/cilium/pkg/debug"
 	"github.com/cilium/cilium/pkg/defaults"
@@ -916,6 +917,12 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 	}
 
 	mtuConfig := mtu.NewConfiguration(option.Config.Tunnel != option.TunnelDisabled, option.Config.MTU)
+
+	if option.Config.EnableIPSec {
+		if err := ipsec.LoadIPSecKeysFile(option.Config.IPSecKeyFile); err != nil {
+			return nil, nil, err
+		}
+	}
 
 	nodeMngr, err := nodemanager.NewManager("all", dp.Node())
 	if err != nil {

daemon/daemon_main.go
@@ -420,6 +420,8 @@ func init() {
 	flags.Bool(option.EnableIPSecName, defaults.EnableIPSec, "Enable IPSec support")
 	option.BindEnv(option.EnableIPSecName)
 
+	flags.StringVar(&option.Config.IPSecKeyFile, "ipsec-key-file", "", "Path to IPSec key file")
+
 	flags.String(option.HTTP403Message, "", "Message returned in proxy L7 403 body")
 	flags.MarkHidden(option.HTTP403Message)
 	option.BindEnv(option.HTTP403Message)

pkg/option/config.go
@@ -652,6 +652,9 @@ type DaemonConfig struct {
 	// EnableIPSec is true when IPSec is enabled
 	EnableIPSec bool
 
+	// IPSec key file for stored keys
+	IPSecKeyFile string
+
 	// MonitorQueueSize is the size of the monitor event queue
 	MonitorQueueSize int
 

commit 00b7555b0058bfe74586f3e5b98077acd3b21693

cilium: return formatted error on ipsec-key-file open failure
before this PR the cilium-agent error was not so clear:
level=fatal msg="Error while creating daemon" error="open : no such file or directory" subsys=daemon

Fixes: cbc98e7a47 (cilium: ipsec, import keys via certs file)

Signed-off-by: Nirmoy Das <ndas@suse.de>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -267,7 +267,7 @@ func DeleteIPSecEndpoint(src, local net.IP) error {
 func LoadIPSecKeysFile(path string) error {
 	file, err := os.Open(path)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to load IPSec Keys File %s: %v", path, err)
 	}
 	defer file.Close()
 