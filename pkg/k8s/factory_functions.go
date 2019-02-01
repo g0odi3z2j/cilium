g0odi3z2j/cilium

commit f0c238d35997747a5d00d628350aa5898eb3a62b

k8s: create Equalness and Missing Functions for CNP and NP
With this commit, Cilium will no longer send unnecessary AddFunc events
and running useless operations for policies that have non-relevant
changes.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/factory_functions.go
@@ -385,18 +385,22 @@ func listV1Namespace(client interface{}) func() (versioned.Map, error) {
 }
 
 func equalV1NetworkPolicy(o1, o2 interface{}) bool {
-	_, ok := o1.(*networkingv1.NetworkPolicy)
+	np1, ok := o1.(*networkingv1.NetworkPolicy)
 	if !ok {
 		log.Panicf("Invalid resource type %q, expecting *networkingv1.NetworkPolicy", reflect.TypeOf(o1))
 		return false
 	}
-	_, ok = o1.(*networkingv1.NetworkPolicy)
+	np2, ok := o2.(*networkingv1.NetworkPolicy)
 	if !ok {
 		log.Panicf("Invalid resource type %q, expecting *networkingv1.NetworkPolicy", reflect.TypeOf(o2))
 		return false
 	}
-	// FIXME write dedicated deep equal function
-	return false
+	// As Cilium uses all of the Spec from a NP it's not probably not worth
+	// it to create a dedicated deep equal	 function to compare both network
+	// policies.
+	return np1.Name == np2.Name &&
+		np1.Namespace == np2.Namespace &&
+		reflect.DeepEqual(np1.Spec, np2.Spec)
 }
 
 func equalV1Services(o1, o2 interface{}) bool {
@@ -445,18 +449,20 @@ func equalV1beta1Ingress(o1, o2 interface{}) bool {
 }
 
 func equalV2CNP(o1, o2 interface{}) bool {
-	_, ok := o1.(*cilium_v2.CiliumNetworkPolicy)
+	cnp1, ok := o1.(*cilium_v2.CiliumNetworkPolicy)
 	if !ok {
 		log.Panicf("Invalid resource type %q, expecting *cilium_v2.CiliumNetworkPolicy", reflect.TypeOf(o1))
 		return false
 	}
-	_, ok = o1.(*cilium_v2.CiliumNetworkPolicy)
+	cnp2, ok := o2.(*cilium_v2.CiliumNetworkPolicy)
 	if !ok {
 		log.Panicf("Invalid resource type %q, expecting *cilium_v2.CiliumNetworkPolicy", reflect.TypeOf(o2))
 		return false
 	}
-	// FIXME write dedicated deep equal function
-	return false
+	return cnp1.Name == cnp2.Name &&
+		cnp1.Namespace == cnp2.Namespace &&
+		reflect.DeepEqual(cnp1.Spec, cnp2.Spec) &&
+		reflect.DeepEqual(cnp1.Specs, cnp2.Specs)
 }
 
 func equalV1Pod(o1, o2 interface{}) bool {

daemon/k8s_watcher.go
@@ -402,9 +402,7 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 						return nil
 					}
 				},
-				func(m versioned.Map) versioned.Map {
-					return m
-				},
+				d.missingK8sNetworkPolicyV1,
 				&networkingv1.NetworkPolicy{},
 				k8s.Client(),
 				reSyncPeriod,
@@ -559,9 +557,7 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 					return nil
 				}
 			},
-			func(m versioned.Map) versioned.Map {
-				return m
-			},
+			d.missingCNPv2,
 			&cilium_v2.CiliumNetworkPolicy{},
 			ciliumNPClient,
 			reSyncPeriod,
@@ -733,6 +729,20 @@ func (d *Daemon) deleteK8sNetworkPolicyV1(k8sNP *networkingv1.NetworkPolicy) {
 	} else {
 		scopedLog.Info("NetworkPolicy successfully removed")
 	}
+}
+
+func (d *Daemon) missingK8sNetworkPolicyV1(m versioned.Map) versioned.Map {
+	missing := versioned.NewMap()
+	d.policy.Mutex.RLock()
+	for k, v := range m {
+		v1NP := v.Data.(*networkingv1.NetworkPolicy)
+		ruleLabels := k8s.GetPolicyLabelsv1(v1NP)
+		if !d.policy.ContainsAllRLocked(labels.LabelArrayList{ruleLabels}) {
+			missing.Add(k, v)
+		}
+	}
+	d.policy.Mutex.RUnlock()
+	return missing
 }
 
 func (d *Daemon) addK8sServiceV1(svc *v1.Service) {
@@ -1728,6 +1738,21 @@ func (d *Daemon) updateCiliumNetworkPolicyV2(ciliumV2Store cache.Store,
 	}
 
 	d.addCiliumNetworkPolicyV2(ciliumV2Store, newRuleCpy)
+}
+
+// missingCNPv2 returns all missing policies from the given map.
+func (d *Daemon) missingCNPv2(m versioned.Map) versioned.Map {
+	missing := versioned.NewMap()
+	d.policy.Mutex.RLock()
+	for k, v := range m {
+		cnp := v.Data.(*cilium_v2.CiliumNetworkPolicy)
+		ruleLabels := cnp.GetRuleLabels()
+		if !d.policy.ContainsAllRLocked(ruleLabels) {
+			missing.Add(k, v)
+		}
+	}
+	d.policy.Mutex.RUnlock()
+	return missing
 }
 
 func (d *Daemon) updatePodHostIP(pod *v1.Pod) (bool, error) {

daemon/k8s_watcher_test.go
@@ -17,7 +17,16 @@ package main
 import (
 	"time"
 
+	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/policy"
+	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/cilium/cilium/pkg/versioned"
+
 	. "gopkg.in/check.v1"
+	"k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (ds *DaemonSuite) TestK8sErrorLogTimeout(c *C) {
@@ -39,4 +48,195 @@ func (ds *DaemonSuite) TestK8sErrorLogTimeout(c *C) {
 	// Returns true because k8sErrLogTimeout has passed
 	shouldLogTime := startTime.Add(k8sErrLogTimeout).Add(time.Nanosecond)
 	c.Assert(k8sErrorUpdateCheckUnmuteTime(errstr, shouldLogTime), Equals, true)
+}
+
+func (ds *DaemonSuite) Test_missingK8sNetworkPolicyV1(c *C) {
+	type args struct {
+		m    versioned.Map
+		repo *policy.Repository
+	}
+	tests := []struct {
+		name        string
+		setupArgs   func() args
+		setupWanted func() versioned.Map
+	}{
+		{
+			name: "both equal",
+			setupArgs: func() args {
+				p1 := policy.NewPolicyRepository()
+				return args{
+					repo: p1,
+					m:    versioned.NewMap(),
+				}
+			},
+			setupWanted: func() versioned.Map {
+				return versioned.NewMap()
+			},
+		},
+		{
+			name: "repository is missing a policy",
+			setupArgs: func() args {
+				p1 := policy.NewPolicyRepository()
+
+				m := versioned.NewMap()
+				m.Add("", versioned.Object{
+					Data: &v1.NetworkPolicy{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "bar",
+							Namespace: "foo",
+						},
+					},
+				})
+
+				return args{
+					m:    m,
+					repo: p1,
+				}
+			},
+			setupWanted: func() versioned.Map {
+				m := versioned.NewMap()
+				m.Add("", versioned.Object{
+					Data: &v1.NetworkPolicy{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "bar",
+							Namespace: "foo",
+						},
+					},
+				})
+				return m
+			},
+		},
+		{
+			name: "repository contains all policies",
+			setupArgs: func() args {
+				p1 := policy.NewPolicyRepository()
+				_, err := p1.Add(api.Rule{
+					EndpointSelector: api.NewESFromLabels(labels.ParseSelectLabel("id=a")),
+					Labels: labels.LabelArray{labels.NewLabel(k8sConst.PolicyLabelName, "bar", labels.LabelSourceK8s),
+						labels.NewLabel(k8sConst.PolicyLabelNamespace, "foo", labels.LabelSourceK8s)},
+				})
+				c.Assert(err, IsNil)
+
+				m := versioned.NewMap()
+				m.Add("", versioned.Object{
+					Data: &v1.NetworkPolicy{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "bar",
+							Namespace: "foo",
+						},
+					},
+				})
+
+				return args{
+					m:    m,
+					repo: p1,
+				}
+			},
+			setupWanted: func() versioned.Map {
+				return versioned.NewMap()
+			},
+		},
+	}
+	for _, tt := range tests {
+		args := tt.setupArgs()
+		want := tt.setupWanted()
+		ds.d.policy = args.repo
+		got := ds.d.missingK8sNetworkPolicyV1(args.m)
+		c.Assert(got, DeepEquals, want, Commentf("Test name: %q", tt.name))
+	}
+}
+
+func (ds *DaemonSuite) Test_missingCNPv2(c *C) {
+	type args struct {
+		m    versioned.Map
+		repo *policy.Repository
+	}
+	tests := []struct {
+		name        string
+		setupArgs   func() args
+		setupWanted func() versioned.Map
+	}{
+		{
+			name: "both equal",
+			setupArgs: func() args {
+				p1 := policy.NewPolicyRepository()
+				return args{
+					repo: p1,
+					m:    versioned.NewMap(),
+				}
+			},
+			setupWanted: func() versioned.Map {
+				return versioned.NewMap()
+			},
+		},
+		{
+			name: "repository is missing a policy",
+			setupArgs: func() args {
+				p1 := policy.NewPolicyRepository()
+
+				m := versioned.NewMap()
+				m.Add("", versioned.Object{
+					Data: &v2.CiliumNetworkPolicy{
+						Spec: &api.Rule{
+							EndpointSelector: api.NewESFromLabels(labels.ParseSelectLabel("id=a")),
+							Labels:           labels.ParseLabelArray("k8s:name=bar", "k8s:namespace=bar"),
+						},
+					},
+				})
+
+				return args{
+					m:    m,
+					repo: p1,
+				}
+			},
+			setupWanted: func() versioned.Map {
+				m := versioned.NewMap()
+				m.Add("", versioned.Object{
+					Data: &v2.CiliumNetworkPolicy{
+						Spec: &api.Rule{
+							EndpointSelector: api.NewESFromLabels(labels.ParseSelectLabel("id=a")),
+							Labels:           labels.ParseLabelArray("k8s:name=bar", "k8s:namespace=bar"),
+						},
+					},
+				})
+				return m
+			},
+		},
+		{
+			name: "repository contains all policies",
+			setupArgs: func() args {
+				p1 := policy.NewPolicyRepository()
+				_, err := p1.Add(api.Rule{
+					EndpointSelector: api.NewESFromLabels(labels.ParseSelectLabel("id=a")),
+					Labels:           labels.ParseLabelArray("k8s:name=bar", "k8s:namespace=bar"),
+				})
+				c.Assert(err, IsNil)
+
+				m := versioned.NewMap()
+				m.Add("", versioned.Object{
+					Data: &v2.CiliumNetworkPolicy{
+						Spec: &api.Rule{
+							EndpointSelector: api.NewESFromLabels(labels.ParseSelectLabel("id=a")),
+							Labels:           labels.ParseLabelArray("k8s:name=bar", "k8s:namespace=bar"),
+						},
+					},
+				})
+
+				return args{
+					m:    m,
+					repo: p1,
+				}
+			},
+			setupWanted: func() versioned.Map {
+				return versioned.NewMap()
+			},
+		},
+	}
+	for _, tt := range tests {
+		args := tt.setupArgs()
+		want := tt.setupWanted()
+		ds.d.policy = args.repo
+		got := ds.d.missingCNPv2(args.m)
+		c.Assert(got, DeepEquals, want, Commentf("Test name: %q", tt.name))
+	}
 }

pkg/k8s/factory_functions_test.go
@@ -1,0 +1,170 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package k8s
+
+import (
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/policy/api"
+
+	. "gopkg.in/check.v1"
+	"k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (s *K8sSuite) Test_missingK8sNetworkPolicyV1(c *C) {
+	type args struct {
+		o1 *v1.NetworkPolicy
+		o2 *v1.NetworkPolicy
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "KNP with the same name",
+			args: args{
+				o1: &v1.NetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+				},
+				o2: &v1.NetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "KNP with the different spec",
+			args: args{
+				o1: &v1.NetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+					Spec: v1.NetworkPolicySpec{
+						PodSelector: metav1.LabelSelector{
+							MatchLabels: map[string]string{
+								"foo": "bar",
+							},
+						},
+					},
+				},
+				o2: &v1.NetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "KNP with the same spec",
+			args: args{
+				o1: &v1.NetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+					Spec: v1.NetworkPolicySpec{},
+				},
+				o2: &v1.NetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+					Spec: v1.NetworkPolicySpec{},
+				},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		got := equalV1NetworkPolicy(tt.args.o1, tt.args.o2)
+		c.Assert(got, Equals, tt.want, Commentf("Test Name: %s", tt.name))
+	}
+}
+
+func (s *K8sSuite) Test_equalV2CNP(c *C) {
+	type args struct {
+		o1 *v2.CiliumNetworkPolicy
+		o2 *v2.CiliumNetworkPolicy
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "CNP with the same name",
+			args: args{
+				o1: &v2.CiliumNetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+				},
+				o2: &v2.CiliumNetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "CNP with the different spec",
+			args: args{
+				o1: &v2.CiliumNetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+					Spec: &api.Rule{
+						EndpointSelector: api.NewESFromLabels(labels.NewLabel("foo", "bar", "k8s")),
+					},
+				},
+				o2: &v2.CiliumNetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+					Spec: nil,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "CNP with the same spec",
+			args: args{
+				o1: &v2.CiliumNetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+					Spec: &api.Rule{},
+				},
+				o2: &v2.CiliumNetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rule1",
+					},
+					Spec: &api.Rule{},
+				},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		got := equalV2CNP(tt.args.o1, tt.args.o2)
+		c.Assert(got, Equals, tt.want, Commentf("Test Name: %s", tt.name))
+	}
+}

commit 8c582bf2e26f377ec06d77e8bce58db5b086f804

pkg/k8s: consider 2 CNPs different if they have different annotations
Fixes: f0c238d35997 ("k8s: create Equalness and Missing Functions for CNP and NP")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/factory_functions.go
@@ -502,6 +502,7 @@ func equalV2CNP(o1, o2 interface{}) bool {
 	}
 	return cnp1.Name == cnp2.Name &&
 		cnp1.Namespace == cnp2.Namespace &&
+		comparator.MapStringEquals(cnp1.GetAnnotations(), cnp2.GetAnnotations()) &&
 		reflect.DeepEqual(cnp1.Spec, cnp2.Spec) &&
 		reflect.DeepEqual(cnp1.Specs, cnp2.Specs)
 }