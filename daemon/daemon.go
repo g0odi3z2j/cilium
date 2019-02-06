g0odi3z2j/cilium

commit e30ea54574e3a62b9cde2092b0d753e5f399d796

ipam: Automatically change the allocation CIDR
When the allocation CIDR changes, the existing code errored out. When that
happened on the first agent restart, all existing endpoints failed to restore
due to failure to allocate the existing endpoint's IP address. The failure to
restore would then in turn get all existing endpoints deleted.

This is bad in two ways:

1. It causes all endpoints to be removed, causing the pods to restart
2. It does not allow the agent to ever come up again without intervention from
   the user.

This commit solves problem #2 to allow nodes to recover automatically. It does
not solve problem #1 yet and requires pods to restart.

Solving problem #2 only makes sense once we have isolated all datapath aspects
such as the DNS proxy and L7 proxies out of the agent as endpoint connetivity
cannot be guaranteed if the agent cannot come up within a reasonable amount of
time.

Fixes: #6215

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/daemon.go
@@ -55,6 +55,7 @@ import (
 	"github.com/cilium/cilium/pkg/fqdn"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/k8s"
@@ -916,6 +917,59 @@ func createPrefixLengthCounter() *counter.PrefixLengthCounter {
 	return counter
 }
 
+func (d *Daemon) prepareAllocationCIDR(family datapath.NodeAddressingFamily) (routerIP net.IP, err error) {
+	// Reserve the IPv4 external node IP within the allocation range if
+	// required.
+	allocRange := family.AllocationCIDR()
+	nodeIP := family.PrimaryExternal()
+	if allocRange.Contains(nodeIP) {
+		err = d.ipam.AllocateIP(nodeIP)
+		if err != nil {
+			err = fmt.Errorf("Unable to allocate external IPv4 node IP %s from allocation range %s: %s",
+				nodeIP, allocRange, err)
+			return
+		}
+	}
+
+	routerIP = family.Router()
+	if routerIP != nil && !allocRange.Contains(routerIP) {
+		log.Warningf("Detected allocation CIDR change to %s, previous router IP %s", allocRange, routerIP)
+
+		// The restored router IP is not part of the allocation range.
+		// This indicates that the allocation range has changed.
+		if !option.Config.IsFlannelMasterDeviceSet() {
+			// Remove the old host device and
+			var link netlink.Link
+			link, err = netlink.LinkByName(option.Config.HostDevice)
+			if err != nil {
+				err = fmt.Errorf("unable to lookup host device %s: %s", option.Config.HostDevice, err)
+				return
+			}
+			if err = netlink.LinkDel(link); err != nil {
+				err = fmt.Errorf("unable to delete host device %s to change allocation CIDR: %s",
+					option.Config.HostDevice, err)
+				return
+			}
+		}
+
+		// force re-allocation of the router IP
+		routerIP = nil
+	}
+
+	if routerIP == nil {
+		routerIP = ip.GetNextIP(family.AllocationCIDR().IP)
+	}
+
+	err = d.ipam.AllocateIP(routerIP)
+	if err != nil {
+		err = fmt.Errorf("Unable to allocate IPv4 router IP %s from allocation range %s: %s",
+			routerIP, allocRange, err)
+		return
+	}
+
+	return
+}
+
 // NewDaemon creates and returns a new Daemon with the parameters set in c.
 func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 	// Validate the daemon-specific global options.
@@ -1095,27 +1149,24 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 		log.WithError(err).Error("Unable to restore existing endpoints")
 	}
 
-	switch err := d.ipam.AllocateInternalIPs(); err.(type) {
-	case ipam.ErrAllocation:
-		if option.Config.IPv4Range == AutoCIDR || option.Config.IPv6ServiceRange == AutoCIDR {
-			log.WithError(err).Fatalf(
-				"The allocation CIDR is different from the previous cilium instance. " +
-					"This error is most likely caused by a temporary network disruption to the kube-apiserver " +
-					"that prevent Cilium from retrieve the node's IPv4/IPv6 allocation range. " +
-					"If you believe the allocation range is supposed to be different you need to clean " +
-					"up all Cilium state with the `cilium cleanup` command on this node. Be aware " +
-					"this will cause network disruption for all existing containers managed by Cilium " +
-					"running on this node and you will have to restart them.")
-		} else {
-			log.WithError(err).Fatalf(
-				"The allocation CIDR is different from the previous cilium instance. " +
-					"If you believe the allocation range is supposed to be different you need to clean " +
-					"up all Cilium state with the `cilium cleanup` command on this node. Be aware " +
-					"this will cause network disruption for all existing containers managed by Cilium " +
-					"running on this node and you will have to restart them.")
+	if option.Config.EnableIPv4 {
+		routerIP, err := d.prepareAllocationCIDR(dp.LocalNodeAddressing().IPv4())
+		if err != nil {
+			return nil, nil, err
 		}
-	case error:
-		log.WithError(err).Fatal("IPAM init failed")
+		if routerIP != nil {
+			node.SetInternalIPv4(routerIP)
+		}
+	}
+
+	if option.Config.EnableIPv6 {
+		routerIP, err := d.prepareAllocationCIDR(dp.LocalNodeAddressing().IPv6())
+		if err != nil {
+			return nil, nil, err
+		}
+		if routerIP != nil {
+			node.SetIPv6Router(routerIP)
+		}
 	}
 
 	log.Info("Addressing information:")

pkg/ipam/allocator_test.go
@@ -19,16 +19,28 @@ package ipam
 import (
 	"net"
 
+	"github.com/cilium/cilium/common/addressing"
 	"github.com/cilium/cilium/pkg/datapath/fake"
 
 	. "gopkg.in/check.v1"
 )
 
 func (s *IPAMSuite) TestAllocatedIPDump(c *C) {
-	ipam := NewIPAM(fake.NewNodeAddressing(), Configuration{EnableIPv4: true, EnableIPv6: true})
+	fakeAddressing := fake.NewNodeAddressing()
+	ipam := NewIPAM(fakeAddressing, Configuration{EnableIPv4: true, EnableIPv6: true})
 
-	err := ipam.AllocateInternalIPs()
-	c.Assert(err, IsNil)
+	ipv4 := fakeAddressing.IPv4().AllocationCIDR().IP
+	ipv6 := fakeAddressing.IPv6().AllocationCIDR().IP
+
+	for i := 0; i < 10; i++ {
+		_, err := addressing.NewCiliumIPv4(ipv4.String())
+		c.Assert(err, IsNil)
+		nextIP(ipv4)
+
+		_, err = addressing.NewCiliumIPv6(ipv6.String())
+		c.Assert(err, IsNil)
+		nextIP(ipv6)
+	}
 
 	allocv4, allocv6 := ipam.Dump()
 	// Test the format of the dumped ip addresses

pkg/ipam/ipam.go
@@ -15,15 +15,12 @@
 package ipam
 
 import (
-	"fmt"
 	"net"
 
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/defaults"
-	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/node"
 
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
@@ -128,75 +125,4 @@ func (ipam *IPAM) ReserveLocalRoutes() {
 	if ipam.IPv4Allocator != nil {
 		ipam.reserveLocalRoutes()
 	}
-}
-
-// AllocateInternalIPs allocates all non endpoint IPs in the CIDR required for
-// operation. This mustbe called *after* endpoints have been restored to avoid
-// allocation conflicts
-func (ipam *IPAM) AllocateInternalIPs() error {
-	if ipam.IPv4Allocator != nil {
-		// Reserve the IPv4 router IP if it is part of the IPv4
-		// allocation range to ensure that we do not hand out the
-		// router IP to a container.
-		allocRange := ipam.nodeAddressing.IPv4().AllocationCIDR()
-		nodeIP := ipam.nodeAddressing.IPv4().PrimaryExternal()
-		if allocRange.Contains(nodeIP) {
-			err := ipam.IPv4Allocator.Allocate(nodeIP)
-			if err != nil {
-				return fmt.Errorf("Unable to allocate external IPv4 node IP %s from allocation range %s: %s", nodeIP, allocRange, err)
-			}
-		}
-
-		internalIP := ipam.nodeAddressing.IPv4().Router()
-		if internalIP == nil {
-			internalIP = ip.GetNextIP(ipam.nodeAddressing.IPv4().AllocationCIDR().IP)
-		}
-		err := ipam.IPv4Allocator.Allocate(internalIP)
-		if err != nil {
-			// If the allocation fails here it is likely that, in a kubernetes
-			// environment, cilium was not able to retrieve the node's pod-cidr
-			// which will cause cilium to start with a default IPv4 allocation range
-			// different from the previous running instance.
-			// Since defaults.HostDevice IP is always automatically derived from the
-			// IPv4 allocation range it is safe to assume defaults.HostDevice IP
-			// will always belong to the IPv4AllocationRange.
-			// Unless of course the user manually specifies a different IPv4range
-			// between restarts which he can only solve by deleting the IPv4
-			// address from defaults.HostDevice as well deleting the node_config.h.
-			return ErrAllocation(fmt.Errorf("Unable to allocate internal IPv4 node IP %s: %s.",
-				internalIP, err))
-		}
-		node.SetInternalIPv4(internalIP)
-	}
-
-	if ipam.IPv6Allocator != nil {
-		// Reserve the IPv6 router and node IP if it is part of the IPv6
-		// allocation range to ensure that we do not hand out the router IP to
-		// a container.
-		allocRange := ipam.nodeAddressing.IPv6().AllocationCIDR()
-		nodeIP := ipam.nodeAddressing.IPv6().PrimaryExternal()
-		for _, ip6 := range []net.IP{nodeIP} {
-			if allocRange.Contains(ip6) {
-				err := ipam.IPv6Allocator.Allocate(ip6)
-				if err != nil {
-					return fmt.Errorf("Unable to allocate external IPv6 node IP %s from allocation range %s: %s", ip6, allocRange, err)
-				}
-			}
-
-			routerIP := ipam.nodeAddressing.IPv6().Router()
-			if routerIP == nil {
-				routerIP = ip.GetNextIP(ipam.nodeAddressing.IPv6().AllocationCIDR().IP)
-			}
-			if !routerIP.Equal(nodeIP) {
-				err := ipam.IPv6Allocator.Allocate(routerIP)
-				if err != nil {
-					return ErrAllocation(fmt.Errorf("Unable to allocate internal IPv6 router IP %s: %s.",
-						routerIP, err))
-				}
-				node.SetIPv6Router(routerIP)
-			}
-		}
-	}
-
-	return nil
 }

pkg/ipam/ipam_test.go
@@ -37,9 +37,6 @@ func (s *IPAMSuite) TestLock(c *C) {
 	fakeAddressing := fake.NewNodeAddressing()
 	ipam := NewIPAM(fakeAddressing, Configuration{EnableIPv4: true, EnableIPv6: true})
 
-	err := ipam.AllocateInternalIPs()
-	c.Assert(err, IsNil)
-
 	// Since the IPs we have allocated to the endpoints might or might not
 	// be in the allocrange specified in cilium, we need to specify them
 	// manually on the endpoint based on the alloc range.

commit 21cc60fd11818203cd66992b422bab47c804d3e5

agent: On CIDR change, do not treat non-existing cilium_host as fatal
It is possible for the router IP to be restored without the presence of a
cilium_host interface. If that happens, we should not abort the agent as it can
result in a non-recoverable situation.

Fixes: e30ea54574e3 ("ipam: Automatically change the allocation CIDR")
Fixes: #6959

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/daemon.go
@@ -924,6 +924,18 @@ func createPrefixLengthCounter() *counter.PrefixLengthCounter {
 	return counter
 }
 
+func deleteHostDevice() {
+	link, err := netlink.LinkByName(option.Config.HostDevice)
+	if err != nil {
+		log.WithError(err).Warningf("Unable to lookup host device %s. No old cilium_host interface exists", option.Config.HostDevice)
+		return
+	}
+
+	if err := netlink.LinkDel(link); err != nil {
+		log.WithError(err).Errorf("Unable to delete host device %s to change allocation CIDR", option.Config.HostDevice)
+	}
+}
+
 func (d *Daemon) prepareAllocationCIDR(family datapath.NodeAddressingFamily) (routerIP net.IP, err error) {
 	// Reserve the IPv4 external node IP within the allocation range if
 	// required.
@@ -945,18 +957,7 @@ func (d *Daemon) prepareAllocationCIDR(family datapath.NodeAddressingFamily) (ro
 		// The restored router IP is not part of the allocation range.
 		// This indicates that the allocation range has changed.
 		if !option.Config.IsFlannelMasterDeviceSet() {
-			// Remove the old host device and
-			var link netlink.Link
-			link, err = netlink.LinkByName(option.Config.HostDevice)
-			if err != nil {
-				err = fmt.Errorf("unable to lookup host device %s: %s", option.Config.HostDevice, err)
-				return
-			}
-			if err = netlink.LinkDel(link); err != nil {
-				err = fmt.Errorf("unable to delete host device %s to change allocation CIDR: %s",
-					option.Config.HostDevice, err)
-				return
-			}
+			deleteHostDevice()
 		}
 
 		// force re-allocation of the router IP