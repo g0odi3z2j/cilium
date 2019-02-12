g0odi3z2j/cilium

commit eda98432fc2e5ddd1bbd2723b3582584fc9d3f2a

cilium: ipsec, insert 'ip rules' for ipsec
IPSec will be isolated from the "normal" routing tables via a set of
marks to direct traffic to an IPSec table. When an endpoint has IPSec
enabled the datapath will first set the mark bit to endicate
MARK_ENCRYPTED or MARK_DECRYPTED to depending on expected direction.
Once the encryption or decryption has occured the mark fields will
be cleared and normal processing will happen.

In this patch add the mark based 'ip rules' to segregate the IPSec
traffic.

Also decided on adding default const to ./pkg/datapath/linux/datapath.go
becaause (a) as defaults they only make sense on linux datapath it
seems and (b) originally I pushed them in via ./pkg/option/ and
./pkg/defaults but this got a bit ugly IMO to translate through to
lower datapath after latest changes to separate fake/linux dp. Sure
we can push them through NewDaemon but do Daemon creators really want
this control? Anyways its easy to go back to pushing at Daemon
create time if anyone cares.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/linux/node.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -393,6 +394,12 @@ func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *node.Node) error {
 		oldIP6 = oldNode.GetNodeIP(true)
 	}
 
+	if n.nodeConfig.EnableIPSec {
+		if newNode.IsLocal() {
+			n.replaceHostRules()
+		}
+	}
+
 	if newNode.IsLocal() {
 		if n.nodeConfig.EnableLocalNodeRoute {
 			n.updateOrRemoveNodeRoutes([]*cidr.CIDR{oldIP4Cidr}, []*cidr.CIDR{newNode.IPv4AllocCIDR})
@@ -493,6 +500,19 @@ func (n *linuxNodeHandler) updateOrRemoveClusterRoute(addressing datapath.NodeAd
 	} else if rt, _ := n.lookupNodeRoute(allocCIDR); rt != nil {
 		n.deleteNodeRoute(allocCIDR)
 	}
+}
+
+func (n *linuxNodeHandler) replaceHostRules() error {
+	err := route.ReplaceRule(linux_defaults.RouteMarkDecrypt, linux_defaults.RouteTableIPSec)
+	if err != nil {
+		log.WithError(err).Error("Replace route rule failed")
+		return err
+	}
+	err = route.ReplaceRule(linux_defaults.RouteMarkEncrypt, linux_defaults.RouteTableIPSec)
+	if err != nil {
+		log.WithError(err).Error("Replace route rule failed")
+	}
+	return err
 }
 
 // NodeConfigurationChanged is called when the LocalNodeConfiguration has changed

pkg/datapath/node.go
@@ -88,6 +88,9 @@ type LocalNodeConfiguration struct {
 	// useful when another component is responsible for the routing of the
 	// allocation CIDR IPs into Cilium endpoints.
 	EnableLocalNodeRoute bool
+
+	// EnableIPSec enables IPSec routes
+	EnableIPSec bool
 }
 
 // NodeHandler handles node related events such as addition, update or deletion

daemon/node_discovery.go
@@ -78,6 +78,7 @@ func newNodeDiscovery(manager *nodemanager.Manager, mtuConfig mtu.Configuration)
 			EnableAutoDirectRouting: option.Config.EnableAutoDirectRouting,
 			EnableLocalNodeRoute:    enableLocalNodeRoute(),
 			AuxiliaryPrefixes:       auxPrefixes,
+			EnableIPSec:             option.Config.EnableIPSec,
 		},
 		localNode: node.Node{
 			Source: node.FromLocalNode,

commit e546ba344e4c87f7a8c734b0fde3389ac1f81bf4

datapath: Fix IPsec with IPv4 or IPv6 disabled
Fixes:
```
panic: runtime error: invalid memory address or nil pointer dereference
[signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0x25e10b2]

goroutine 1 [running]:
github.com/cilium/cilium/pkg/datapath/linux.(*linuxNodeHandler).nodeUpdate(0xc0001a8620, 0x0, 0xc000477830, 0x1, 0x4b5c160, 0x0)
	/go/src/github.com/cilium/cilium/pkg/datapath/linux/node.go:440 +0x1d2
github.com/cilium/cilium/pkg/datapath/linux.(*linuxNodeHandler).NodeConfigurationChanged(0xc0001a8620, 0x5b4, 0x582, 0x1, 0x4b5c160, 0x0, 0x0, 0x1010001000001, 0x0, 0x0)
	/go/src/github.com/cilium/cilium/pkg/datapath/linux/node.go:676 +0x1e6
main.(*Daemon).compileBase(0xc000426540, 0x0, 0x0)
	/go/src/github.com/cilium/cilium/daemon/daemon.go:541 +0x7dc
main.(*Daemon).init(0xc000426540, 0xc0011a7200, 0x1)
	/go/src/github.com/cilium/cilium/daemon/daemon.go:683 +0x126
main.NewDaemon(0x2fa09c0, 0xc00107db60, 0x1, 0x1, 0xc0008f3928, 0x1156212)
	/go/src/github.com/cilium/cilium/daemon/daemon.go:1288 +0x1557
main.runDaemon()
	/go/src/github.com/cilium/cilium/daemon/daemon_main.go:1052 +0x214
main.glob..func1(0x456a520, 0xc0007ec280, 0x0, 0x5)
	/go/src/github.com/cilium/cilium/daemon/daemon_main.go:108 +0x30
github.com/cilium/cilium/vendor/github.com/spf13/cobra.(*Command).execute(0x456a520, 0xc00004c1f0, 0x5, 0x5, 0x456a520, 0xc00004c1f0)
	/go/src/github.com/cilium/cilium/vendor/github.com/spf13/cobra/command.go:766 +0x2cc
github.com/cilium/cilium/vendor/github.com/spf13/cobra.(*Command).ExecuteC(0x456a520, 0x2f83568, 0x2fab440, 0x2f83588)
	/go/src/github.com/cilium/cilium/vendor/github.com/spf13/cobra/command.go:852 +0x2fd
github.com/cilium/cilium/vendor/github.com/spf13/cobra.(*Command).Execute(0x456a520, 0x0, 0x0)
	/go/src/github.com/cilium/cilium/vendor/github.com/spf13/cobra/command.go:800 +0x2b
main.daemonMain()
	/go/src/github.com/cilium/cilium/daemon/daemon_main.go:126 +0x14c
main.main()
	/go/src/github.com/cilium/cilium/daemon/main.go:30 +0xc9
```

Fixes: eda98432fc2 ("cilium: ipsec, insert 'ip rules' for ipsec")

pkg/datapath/linux/node.go
@@ -420,6 +420,45 @@ func (n *linuxNodeHandler) NodeUpdate(oldNode, newNode node.Node) error {
 	return nil
 }
 
+func (n *linuxNodeHandler) enableIPsec(newNode *node.Node) {
+	if newNode.IsLocal() {
+		n.replaceHostRules()
+	}
+
+	if n.nodeConfig.EnableIPv4 {
+		new4Net := &net.IPNet{IP: newNode.IPv4AllocCIDR.IP, Mask: newNode.IPv4AllocCIDR.Mask}
+		if newNode.IsLocal() {
+			n.replaceNodeIPSecInRoute(new4Net)
+		} else {
+			if ciliumInternalIPv4 := newNode.GetCiliumInternalIP(false); ciliumInternalIPv4 != nil {
+				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv4().Router(), Mask: n.nodeAddressing.IPv4().AllocationCIDR().Mask}
+				ipsecRemote := &net.IPNet{IP: ciliumInternalIPv4, Mask: newNode.IPv4AllocCIDR.Mask}
+				n.replaceNodeIPSecOutRoute(new4Net)
+				ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecRemote, linux_defaults.IPSecEndpointSPI)
+			}
+		}
+	}
+
+	if n.nodeConfig.EnableIPv6 {
+		new6Net := &net.IPNet{IP: newNode.IPv6AllocCIDR.IP, Mask: newNode.IPv6AllocCIDR.Mask}
+		if newNode.IsLocal() {
+			n.replaceHostRules()
+			n.replaceNodeIPSecInRoute(new6Net)
+		} else {
+			if ciliumInternalIPv6 := newNode.GetCiliumInternalIP(true); ciliumInternalIPv6 != nil {
+				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv6().Router(), Mask: n.nodeAddressing.IPv6().AllocationCIDR().Mask}
+				ipsecRemote := &net.IPNet{IP: ciliumInternalIPv6, Mask: newNode.IPv6AllocCIDR.Mask}
+				ipsecHost := &net.IPNet{IP: n.nodeAddressing.IPv6().PrimaryExternal(), Mask: n.nodeAddressing.IPv6().AllocationCIDR().Mask}
+				n.replaceNodeIPSecOutRoute(new6Net)
+				ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecRemote, linux_defaults.IPSecEndpointSPI)
+				if !ipsecHost.IP.Equal(ipsecLocal.IP) {
+					ipsec.UpsertIPSecEndpoint(ipsecHost, ipsecRemote, linux_defaults.IPSecNodeSPI)
+				}
+			}
+		}
+	}
+}
+
 func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *node.Node, firstAddition bool) error {
 	var (
 		oldIP4Cidr, oldIP6Cidr *cidr.CIDR
@@ -436,34 +475,7 @@ func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *node.Node, firstAddition
 	}
 
 	if n.nodeConfig.EnableIPSec {
-		new4Net := &net.IPNet{IP: newNode.IPv4AllocCIDR.IP, Mask: newNode.IPv4AllocCIDR.Mask}
-		new6Net := &net.IPNet{IP: newNode.IPv6AllocCIDR.IP, Mask: newNode.IPv6AllocCIDR.Mask}
-		if newNode.IsLocal() {
-			n.replaceHostRules()
-			n.replaceNodeIPSecInRoute(new4Net)
-			n.replaceNodeIPSecInRoute(new6Net)
-		} else {
-			ciliumInternalIPv4 := newNode.GetCiliumInternalIP(false)
-			ciliumInternalIPv6 := newNode.GetCiliumInternalIP(true)
-
-			if ciliumInternalIPv4 != nil {
-				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv4().Router(), Mask: n.nodeAddressing.IPv4().AllocationCIDR().Mask}
-				ipsecRemote := &net.IPNet{IP: ciliumInternalIPv4, Mask: newNode.IPv4AllocCIDR.Mask}
-				n.replaceNodeIPSecOutRoute(new4Net)
-				ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecRemote, linux_defaults.IPSecEndpointSPI)
-			}
-
-			if ciliumInternalIPv6 != nil {
-				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv6().Router(), Mask: n.nodeAddressing.IPv6().AllocationCIDR().Mask}
-				ipsecRemote := &net.IPNet{IP: ciliumInternalIPv6, Mask: newNode.IPv6AllocCIDR.Mask}
-				ipsecHost := &net.IPNet{IP: n.nodeAddressing.IPv6().PrimaryExternal(), Mask: n.nodeAddressing.IPv6().AllocationCIDR().Mask}
-				n.replaceNodeIPSecOutRoute(new6Net)
-				ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecRemote, linux_defaults.IPSecEndpointSPI)
-				if !ipsecHost.IP.Equal(ipsecLocal.IP) {
-					ipsec.UpsertIPSecEndpoint(ipsecHost, ipsecRemote, linux_defaults.IPSecNodeSPI)
-				}
-			}
-		}
+		n.enableIPsec(newNode)
 	}
 
 	if newNode.IsLocal() {
@@ -563,22 +575,28 @@ func (n *linuxNodeHandler) updateOrRemoveClusterRoute(addressing datapath.NodeAd
 }
 
 func (n *linuxNodeHandler) replaceHostRules() error {
-	if err := route.ReplaceRule(linux_defaults.RouteMarkDecrypt, linux_defaults.RouteTableIPSec); err != nil {
-		log.WithError(err).Error("Replace IPv4 route decrypt rule failed")
-		return err
+	if n.nodeConfig.EnableIPv4 {
+		if err := route.ReplaceRule(linux_defaults.RouteMarkDecrypt, linux_defaults.RouteTableIPSec); err != nil {
+			log.WithError(err).Error("Replace IPv4 route decrypt rule failed")
+			return err
+		}
+		if err := route.ReplaceRule(linux_defaults.RouteMarkEncrypt, linux_defaults.RouteTableIPSec); err != nil {
+			log.WithError(err).Error("Replace IPv4 route encrypt rule failed")
+			return err
+		}
 	}
-	if err := route.ReplaceRule(linux_defaults.RouteMarkEncrypt, linux_defaults.RouteTableIPSec); err != nil {
-		log.WithError(err).Error("Replace IPv4 route encrypt rule failed")
-		return err
+
+	if n.nodeConfig.EnableIPv6 {
+		if err := route.ReplaceRuleIPv6(linux_defaults.RouteMarkDecrypt, linux_defaults.RouteTableIPSec); err != nil {
+			log.WithError(err).Error("Replace IPv6 route decrypt rule failed")
+			return err
+		}
+		if err := route.ReplaceRuleIPv6(linux_defaults.RouteMarkEncrypt, linux_defaults.RouteTableIPSec); err != nil {
+			log.WithError(err).Error("Replace IPv6 route ecrypt rule failed")
+			return err
+		}
 	}
-	if err := route.ReplaceRuleIPv6(linux_defaults.RouteMarkDecrypt, linux_defaults.RouteTableIPSec); err != nil {
-		log.WithError(err).Error("Replace IPv6 route decrypt rule failed")
-		return err
-	}
-	if err := route.ReplaceRuleIPv6(linux_defaults.RouteMarkEncrypt, linux_defaults.RouteTableIPSec); err != nil {
-		log.WithError(err).Error("Replace IPv6 route ecrypt rule failed")
-		return err
-	}
+
 	return nil
 }
 