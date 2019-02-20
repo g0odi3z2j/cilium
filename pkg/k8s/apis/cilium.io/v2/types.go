g0odi3z2j/cilium

commit d868e69789ef7ba30fb22713fc039dec33d95e60

Policy: Added ToGroups option into Egress policy.
To be able to add new integrations, a new field ToGroups was added.
These information will be saved in the parent rule, and if the ToGroups
is in place it'll return a new CIDR rule that will get the information
from the provider callbacks.

Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

pkg/k8s/apis/cilium.io/v2/types.go
@@ -67,6 +67,10 @@ type CiliumNetworkPolicy struct {
 type CiliumNetworkPolicyStatus struct {
 	// Nodes is the Cilium policy status for each node
 	Nodes map[string]CiliumNetworkPolicyNodeStatus `json:"nodes,omitempty"`
+
+	// DerivativePolicies is the status of all policies derived from the Cilium
+	// policy
+	DerivativePolicies map[string]CiliumNetworkPolicyNodeStatus `json:"DerivativePolicies,omitempty"`
 }
 
 // CiliumNetworkPolicyNodeStatus is the status of a Cilium policy rule for a
@@ -151,6 +155,15 @@ func (r *CiliumNetworkPolicy) SetPolicyStatus(nodeName string, cnpns CiliumNetwo
 	r.Status.Nodes[nodeName] = cnpns
 }
 
+// SetDerivedPolicyStatus set the derivative policy status for the given
+// derivative policy name.
+func (r *CiliumNetworkPolicy) SetDerivedPolicyStatus(derivativePolicyName string, status CiliumNetworkPolicyNodeStatus) {
+	if r.Status.DerivativePolicies == nil {
+		r.Status.DerivativePolicies = map[string]CiliumNetworkPolicyNodeStatus{}
+	}
+	r.Status.DerivativePolicies[derivativePolicyName] = status
+}
+
 // SpecEquals returns true if the spec and specs metadata is the sa
 func (r *CiliumNetworkPolicy) SpecEquals(o *CiliumNetworkPolicy) bool {
 	if o == nil {
@@ -218,6 +231,24 @@ func (r *CiliumNetworkPolicy) GetIdentityLabels() labels.LabelArray {
 	uid := r.ObjectMeta.UID
 	return k8sCiliumUtils.GetPolicyLabels(namespace, name, uid,
 		k8sCiliumUtils.ResourceTypeCiliumNetworkPolicy)
+}
+
+// RequiresDerivative return true if the CNP has any rule that will create a new
+// derivative rule.
+func (r *CiliumNetworkPolicy) RequiresDerivative() bool {
+	if r.Spec != nil {
+		if r.Spec.RequiresDerivative() {
+			return true
+		}
+	}
+	if r.Specs != nil {
+		for _, rule := range r.Specs {
+			if rule.RequiresDerivative() {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object

daemon/k8s_watcher.go
@@ -561,7 +561,11 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 		rehf := k8sUtils.ResourceEventHandlerFactory(
 			func(i interface{}) func() error {
 				return func() error {
-					err := d.addCiliumNetworkPolicyV2(cnpStore, i.(*cilium_v2.CiliumNetworkPolicy))
+					cnp := i.(*cilium_v2.CiliumNetworkPolicy)
+					if cnp.RequiresDerivative() {
+						return nil
+					}
+					err := d.addCiliumNetworkPolicyV2(cnpStore, cnp)
 					updateK8sEventMetric(metricCNP, metricCreate, err == nil)
 					return nil
 				}
@@ -575,11 +579,14 @@ func (d *Daemon) EnableK8sWatcher(reSyncPeriod time.Duration) error {
 			},
 			func(old, new interface{}) func() error {
 				return func() error {
+					oldCNP := old.(*cilium_v2.CiliumNetworkPolicy)
+					newCNP := new.(*cilium_v2.CiliumNetworkPolicy)
+					if newCNP.RequiresDerivative() {
+						return nil
+					}
+
 					err := d.updateCiliumNetworkPolicyV2(
-						cnpStore,
-						old.(*cilium_v2.CiliumNetworkPolicy),
-						new.(*cilium_v2.CiliumNetworkPolicy),
-					)
+						cnpStore, oldCNP, newCNP)
 					updateK8sEventMetric(metricCNP, metricUpdate, err == nil)
 					return nil
 				}

pkg/k8s/apis/cilium.io/v2/zz_generated.deepcopy.go
@@ -193,6 +193,13 @@ func (in *CiliumNetworkPolicyStatus) DeepCopyInto(out *CiliumNetworkPolicyStatus
 			(*out)[key] = *val.DeepCopy()
 		}
 	}
+	if in.DerivativePolicies != nil {
+		in, out := &in.DerivativePolicies, &out.DerivativePolicies
+		*out = make(map[string]CiliumNetworkPolicyNodeStatus, len(*in))
+		for key, val := range *in {
+			(*out)[key] = *val.DeepCopy()
+		}
+	}
 	return
 }
 

pkg/policy/api/egress.go
@@ -132,6 +132,18 @@ type EgressRule struct {
 	//
 	// +optional
 	ToFQDNs []FQDNSelector `json:"toFQDNs,omitempty"`
+
+	// ToGroups is a directive that allows the integration with multiple outside
+	// providers. Currently, only AWS is supported, and the rule can select by
+	// multiple sub directives:
+	//
+	// Example:
+	// toGroups:
+	// - aws:
+	//     securityGroupsIds:
+	//     - 'sg-XXXXXXXXXXXXX'
+	// +optional
+	ToGroups []ToGroups `json:"toGroups,omitempty"`
 }
 
 // GetDestinationEndpointSelectors returns a slice of endpoints selectors
@@ -147,4 +159,38 @@ func (e *EgressRule) GetDestinationEndpointSelectors() EndpointSelectorSlice {
 // setting any To field.
 func (e *EgressRule) IsLabelBased() bool {
 	return len(e.ToRequires)+len(e.ToCIDR)+len(e.ToCIDRSet)+len(e.ToServices) == 0
+}
+
+// RequiresDerivative returns true when the EgressRule contains sections that
+// need a derivative policy created in order to be enforced (e.g. ToGroups).
+func (e *EgressRule) RequiresDerivative() bool {
+	return len(e.ToGroups) > 0
+}
+
+// CreateDerivative will return a new rule based on the data gathered by the
+// rules that creates a new derivative policy.
+// In the case of ToGroups will call outside using the groups callback and this
+// function can take a bit of time.
+// Important, this call need to happens after call DeepCopy, the egressRule will change
+func (e *EgressRule) CreateDerivative() (*EgressRule, error) {
+	if !e.RequiresDerivative() {
+		return e, nil
+	}
+	blockAll := func() {
+		e = &EgressRule{}
+	}
+	for _, group := range e.ToGroups {
+		cidrSet, err := group.GetCidrSet()
+		if err != nil {
+			blockAll()
+			return nil, err
+		}
+		if len(cidrSet) == 0 {
+			blockAll()
+			return e, nil
+		}
+		e.ToCIDRSet = append(e.ToCIDRSet, cidrSet...)
+	}
+	e.ToGroups = nil
+	return e, nil
 }

pkg/policy/api/groups.go
@@ -1,0 +1,87 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"sort"
+	"sync"
+)
+
+const (
+	AWSProvider = "AWS" // AWS provider key
+)
+
+var (
+	providers = sync.Map{} // map with the list of providers to callback to retrive info from.
+)
+
+// GroupProviderFunc is a func that need to be register to be able to
+// register a new provider in the platform.
+type GroupProviderFunc func(*ToGroups) ([]net.IP, error)
+
+// ToGroups structure to store all kinds of new integrations that needs a new
+// derivative policy.
+type ToGroups struct {
+	AWS *AWSGroup `json:"aws,omitempty"`
+}
+
+// AWSGroup is an structure that can be used to whitelisting information from AWS integration
+type AWSGroup struct {
+	Labels              map[string]string `json:"labels,omitempty"`
+	SecurityGroupsIds   []string          `json:"securityGroupsIds,omitempty"`
+	SecurityGroupsNames []string          `json:"securityGroupsNames,omitempty"`
+	Region              string            `json:"region,omitempty"`
+}
+
+// RegisterToGroupsProvider it will register a new callback that will be used
+// when a new ToGroups rule is added.
+func RegisterToGroupsProvider(providerName string, callback GroupProviderFunc) {
+	providers.Store(providerName, callback)
+}
+
+// GetCidrSet will return the CIDRRule for the rule using the callbacks that
+// are register in the platform.
+func (group *ToGroups) GetCidrSet() ([]CIDRRule, error) {
+
+	var ips []net.IP
+	// Get per  provider CIDRSet
+	if group.AWS != nil {
+		callbackInterface, ok := providers.Load(AWSProvider)
+		if !ok {
+			return nil, fmt.Errorf("Provider %s is not registered", AWSProvider)
+		}
+		callback, ok := callbackInterface.(GroupProviderFunc)
+		if !ok {
+			return nil, fmt.Errorf("Provider callback for %s is not a valid instance", AWSProvider)
+		}
+		awsIPs, err := callback(group)
+		if err != nil {
+			return nil, fmt.Errorf(
+				"Cannot retrieve data from %s provider: %s",
+				AWSProvider, err)
+		}
+		ips = append(ips, awsIPs...)
+	}
+
+	// Sort IPS to have always the same result and do not update policies if it
+	// is not needed.
+	sort.Slice(ips, func(i, j int) bool {
+		return bytes.Compare(ips[i], ips[j]) < 0
+	})
+	return IPsToCIDRRules(ips), nil
+}

pkg/policy/api/rule.go
@@ -61,3 +61,29 @@ type Rule struct {
 	// +optional
 	Description string `json:"description,omitempty"`
 }
+
+// RequiresDerivative it return true if the rule has a derivative rule.
+func (r *Rule) RequiresDerivative() bool {
+	for _, rule := range r.Egress {
+		if rule.RequiresDerivative() {
+			return true
+		}
+	}
+	return false
+}
+
+// CreateDerivative will return a new Rule with the new data based gather
+// by the rules that autogenerated new Rule
+func (r *Rule) CreateDerivative() (*Rule, error) {
+	newRule := r.DeepCopy()
+	newRule.Egress = []EgressRule{}
+
+	for _, egressRule := range r.Egress {
+		derivativeEgressRule, err := egressRule.CreateDerivative()
+		if err != nil {
+			return newRule, err
+		}
+		newRule.Egress = append(newRule.Egress, *derivativeEgressRule)
+	}
+	return newRule, nil
+}

pkg/policy/api/rule_validation.go
@@ -154,6 +154,7 @@ func (e *EgressRule) sanitize() error {
 		"ToEntities":  len(e.ToEntities),
 		"ToServices":  len(e.ToServices),
 		"ToFQDNs":     len(e.ToFQDNs),
+		"ToGroups":    len(e.ToGroups),
 	}
 	l3DependentL4Support := map[interface{}]bool{
 		"ToCIDR":      true,
@@ -162,6 +163,7 @@ func (e *EgressRule) sanitize() error {
 		"ToEntities":  true,
 		"ToServices":  true,
 		"ToFQDNs":     true,
+		"ToGroups":    true,
 	}
 	for m1 := range l3Members {
 		for m2 := range l3Members {

pkg/policy/api/zz_generated.deepcopy.go
@@ -24,6 +24,39 @@ import (
 )
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *AWSGroup) DeepCopyInto(out *AWSGroup) {
+	*out = *in
+	if in.Labels != nil {
+		in, out := &in.Labels, &out.Labels
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	if in.SecurityGroupsIds != nil {
+		in, out := &in.SecurityGroupsIds, &out.SecurityGroupsIds
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.SecurityGroupsNames != nil {
+		in, out := &in.SecurityGroupsNames, &out.SecurityGroupsNames
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new AWSGroup.
+func (in *AWSGroup) DeepCopy() *AWSGroup {
+	if in == nil {
+		return nil
+	}
+	out := new(AWSGroup)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *CIDRRule) DeepCopyInto(out *CIDRRule) {
 	*out = *in
 	if in.ExceptCIDRs != nil {
@@ -138,6 +171,13 @@ func (in *EgressRule) DeepCopyInto(out *EgressRule) {
 		in, out := &in.ToFQDNs, &out.ToFQDNs
 		*out = make([]FQDNSelector, len(*in))
 		copy(*out, *in)
+	}
+	if in.ToGroups != nil {
+		in, out := &in.ToGroups, &out.ToGroups
+		*out = make([]ToGroups, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
 	}
 	return
 }
@@ -636,6 +676,27 @@ func (in *ServiceSelector) DeepCopy() *ServiceSelector {
 		return nil
 	}
 	out := new(ServiceSelector)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ToGroups) DeepCopyInto(out *ToGroups) {
+	*out = *in
+	if in.AWS != nil {
+		in, out := &in.AWS, &out.AWS
+		*out = new(AWSGroup)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ToGroups.
+func (in *ToGroups) DeepCopy() *ToGroups {
+	if in == nil {
+		return nil
+	}
+	out := new(ToGroups)
 	in.DeepCopyInto(out)
 	return out
 }

commit 3dc0da7355498803a2335078bbac423927d49667

cilium.io/v2: set DerivativePolicies json to derivativePolicies
As all fields are used in with small case we should have consistency for
this field as well.

Fixes: d868e69789ef ("Policy: Added ToGroups option into Egress policy.")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/apis/cilium.io/v2/types.go
@@ -72,7 +72,7 @@ type CiliumNetworkPolicyStatus struct {
 
 	// DerivativePolicies is the status of all policies derived from the Cilium
 	// policy
-	DerivativePolicies map[string]CiliumNetworkPolicyNodeStatus `json:"DerivativePolicies,omitempty"`
+	DerivativePolicies map[string]CiliumNetworkPolicyNodeStatus `json:"derivativePolicies,omitempty"`
 }
 
 // CiliumNetworkPolicyNodeStatus is the status of a Cilium policy rule for a