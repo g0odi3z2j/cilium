g0odi3z2j/cilium

commit 36f92d6a6a7574f5f2ea3b771ab74bcd0736c43d

pkg: allocate first IP in IPv4 allocation range
Since kubernetes allocator fixed their randomize function to allocate a
different IP on each new allocation we have to make sure existing cilium
instances keep the previously IPv4 gateway IP.

On a new node however, the IPv4 gateway IP will be the first IP from the
allocation range.

Fixes: cfe05650d (vendor: update k8s dependencies to 1.10.2)

Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon.go
@@ -1057,11 +1057,16 @@ func NewDaemon() (*Daemon, error) {
 	// If the device hasn't been specified, k8s.Init() allocated the
 	// IPv4AllocPrefix and the IPv6AllocPrefix from k8s node annotations.
 	//
+	// If k8s.Init() failed to retrieve the IPv4AllocPrefix we can try to derive
+	// it from an existing node_config.h file or from previous cilium_host
+	// interfaces.
+	//
 	// Then, we will calculate the IPv4 or IPv6 alloc prefix based on the IPv6
 	// or IPv4 alloc prefix, respectively, retrieved by k8s node annotations.
 	log.Info("Initializing node addressing")
-	if option.Config.Device == "undefined" {
-		node.InitDefaultPrefix("")
+
+	if err := node.AutoComplete(); err != nil {
+		log.WithError(err).Fatal("Cannot autocomplete node addresses")
 	}
 
 	node.SetIPv4ClusterCidrMaskSize(v4ClusterCidrMaskSize)
@@ -1103,10 +1108,6 @@ func NewDaemon() (*Daemon, error) {
 		node.AddAuxPrefix(ipnet)
 	}
 
-	if err := node.AutoComplete(); err != nil {
-		log.WithError(err).Fatal("Cannot autocomplete node IPv6 address")
-	}
-
 	if k8s.IsEnabled() {
 		log.Info("Annotating k8s node with CIDR ranges")
 		err := k8s.AnnotateNode(k8s.Client(), node.GetName(),
@@ -1119,7 +1120,26 @@ func NewDaemon() (*Daemon, error) {
 
 	// Set up ipam conf after init() because we might be running d.conf.KVStoreIPv4Registration
 	log.Info("Initializing IPAM")
-	if err = ipam.Init(); err != nil {
+	switch err := ipam.Init(); err.(type) {
+	case ipam.ErrAllocation:
+		if v4Prefix == AutoCIDR || v6Prefix == AutoCIDR {
+			log.WithError(err).Fatalf(
+				"The allocation CIDR is different from the previous cilium instance. " +
+					"This error is most likely caused by a temporary network disruption to the kube-apiserver " +
+					"that prevent Cilium from retrieve the node's IPv4/IPv6 allocation range. " +
+					"If you believe the allocation range is supposed to be different you need to clean " +
+					"up all Cilium state with the `cilium cleanup` command on this node. Be aware " +
+					"this will cause network disruption for all existing containers managed by Cilium " +
+					"running on this node and you will have to restart them.")
+		} else {
+			log.WithError(err).Fatalf(
+				"The allocation CIDR is different from the previous cilium instance. " +
+					"If you believe the allocation range is supposed to be different you need to clean " +
+					"up all Cilium state with the `cilium cleanup` command on this node. Be aware " +
+					"this will cause network disruption for all existing containers managed by Cilium " +
+					"running on this node and you will have to restart them.")
+		}
+	case error:
 		log.WithError(err).Fatal("IPAM init failed")
 	}
 

pkg/ipam/init.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
@@ -33,6 +34,8 @@ var (
 	log      = logging.DefaultLogger
 	ipamConf *Config
 )
+
+type ErrAllocation error
 
 func nextIP(ip net.IP) {
 	for j := len(ip) - 1; j >= 0; j-- {
@@ -144,11 +147,25 @@ func Init() error {
 		}
 	}
 
-	internalIP, err := ipamConf.IPv4Allocator.AllocateNext()
-	if err != nil {
-		return fmt.Errorf("Unable to allocate internal IPv4 node IP: %s", err)
+	internalIP := node.GetInternalIPv4()
+	if internalIP == nil {
+		internalIP = ip.GetNextIP(node.GetIPv4AllocRange().IP)
 	}
-
+	err := ipamConf.IPv4Allocator.Allocate(internalIP)
+	if err != nil {
+		// If the allocation fails here it is likely that, in a kubernetes
+		// environment, cilium was not able to retrieve the node's pod-cidr
+		// which will cause cilium to start with a default IPv4 allocation range
+		// different from the previous running instance.
+		// Since cilium_host IP is always automatically derived from the IPv4
+		// allocation range it is safe to assume cilium_host IP will always
+		// belong to the IPv4AllocationRange.
+		// Unless of course the user manually specifies a different IPv4range
+		// between restarts which he can only solve by deleting the IPv4
+		// address from cilium_host as well deleting the node_config.h.
+		return ErrAllocation(fmt.Errorf("Unable to allocate internal IPv4 node IP %s: %s.",
+			internalIP, err))
+	}
 	node.SetInternalIPv4(internalIP)
 
 	// Reserve the IPv6 router and node IP if it is part of the IPv6
@@ -164,11 +181,17 @@ func Init() error {
 		}
 	}
 
-	routerIP, err := ipamConf.IPv6Allocator.AllocateNext()
-	if err != nil {
-		return fmt.Errorf("Unable to allocate IPv6 router IP: %s", err)
+	routerIP := node.GetIPv6Router()
+	if routerIP == nil {
+		routerIP = ip.GetNextIP(node.GetIPv6AllocRange().IP)
 	}
-
+	if !routerIP.Equal(node.GetIPv6()) {
+		err = ipamConf.IPv6Allocator.Allocate(routerIP)
+		if err != nil {
+			return ErrAllocation(fmt.Errorf("Unable to allocate internal IPv6 router IP %s: %s.",
+				routerIP, err))
+		}
+	}
 	node.SetIPv6Router(routerIP)
 
 	return nil

pkg/node/node_address.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Authors of Cilium
+// Copyright 2016-2018 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,13 +15,19 @@
 package node
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/common"
+	"github.com/cilium/cilium/pkg/byteorder"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-
 	"github.com/cilium/cilium/pkg/option"
+
 	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
 )
@@ -113,8 +119,13 @@ func SetIPv4ClusterCidrMaskSize(size int) {
 // network device of the system in which case the first address with global
 // scope will be regarded as the system's node address.
 func InitDefaultPrefix(device string) {
-	// Find a IPv6 node address first
-	ipv6Address = findIPv6NodeAddr()
+	if ipv6Address == nil {
+		// Find a IPv6 node address first
+		ipv6Address = findIPv6NodeAddr()
+		if ipv6Address == nil {
+			ipv6Address = makeIPv6HostIP()
+		}
+	}
 
 	ip, err := firstGlobalV4Addr(device)
 	if err != nil {
@@ -247,12 +258,18 @@ func SetIPv6NodeRange(net *net.IPNet) error {
 
 // AutoComplete completes the parts of addressing that can be auto derived
 func AutoComplete() error {
-	if ipv6AllocRange == nil {
-		return fmt.Errorf("IPv6 per node allocation prefix is not configured. Please specificy --ipv6-range")
+	// Read the previous cilium host IPs from node_config.h for backward
+	// compatibility
+	ipv4GW, ipv6Router := getCiliumHostIPs()
+	SetInternalIPv4(ipv4GW)
+	SetIPv6Router(ipv6Router)
+
+	if option.Config.Device == "undefined" {
+		InitDefaultPrefix("")
 	}
 
-	if ipv6Address == nil {
-		ipv6Address = makeIPv6HostIP()
+	if ipv6AllocRange == nil {
+		return fmt.Errorf("IPv6 per node allocation prefix is not configured. Please specificy --ipv6-range")
 	}
 
 	return nil
@@ -401,4 +418,82 @@ func GetNodeAddressing(enableIPv4 bool) *models.NodeAddressing {
 			AllocRange: GetIPv4AllocRange().String(),
 		},
 	}
+}
+
+func getCiliumHostIPsFromFile(nodeConfig string) (ipv4GW, ipv6Router net.IP) {
+	f, err := os.Open(nodeConfig)
+	switch {
+	case err != nil:
+	default:
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			txt := scanner.Text()
+			switch {
+			case strings.Contains(txt, "IPV4_GATEWAY"):
+				// #define IPV4_GATEWAY 0xee1c000a
+				defineLine := strings.Split(txt, " ")
+				if len(defineLine) != 3 {
+					continue
+				}
+				ipv4GWHex := strings.TrimPrefix(defineLine[2], "0x")
+				ipv4GWint64, err := strconv.ParseInt(ipv4GWHex, 16, 0)
+				if err != nil {
+					continue
+				}
+				bs := make([]byte, net.IPv4len)
+				byteorder.NetworkToHostPut(bs, uint32(ipv4GWint64))
+				ipv4GW = net.IPv4(bs[0], bs[1], bs[2], bs[3])
+			case strings.Contains(txt, " ROUTER_IP "):
+				// #define ROUTER_IP 0xf0, 0xd, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xa, 0x0, 0x0, 0x0, 0x0, 0x0, 0x8a, 0xd6
+				defineLine := strings.Split(txt, " ROUTER_IP ")
+				if len(defineLine) != 2 {
+					continue
+				}
+				ipv6 := common.C2GoArray(defineLine[1])
+				if len(ipv6) != net.IPv6len {
+					continue
+				}
+				ipv6Router = net.IP(ipv6)
+			}
+		}
+	}
+	return ipv4GW, ipv6Router
+}
+
+// getCiliumHostIPsFromNetDev returns the first IPv4 link local and returns
+// it
+func getCiliumHostIPsFromNetDev(devName string) (ipv4GW, ipv6Router net.IP) {
+	hostDev, err := netlink.LinkByName(devName)
+	if err != nil {
+		return nil, nil
+	}
+	addrs, err := netlink.AddrList(hostDev, netlink.FAMILY_ALL)
+	if err != nil {
+		return nil, nil
+	}
+	for _, addr := range addrs {
+		if addr.IP.To4() != nil {
+			if addr.Scope == int(netlink.SCOPE_LINK) {
+				ipv4GW = addr.IP
+			}
+		} else {
+			if addr.Scope != int(netlink.SCOPE_LINK) {
+				ipv6Router = addr.IP
+			}
+		}
+	}
+	return nil, nil
+}
+
+// getCiliumHostIPs returns the Cilium IPv4 gateway and router IPv6 address from
+// the node_config.h file if is present; or by deriving it from cilium_host
+// interface, on which only the IPv4 is possible to derive.
+func getCiliumHostIPs() (ipv4GW, ipv6Router net.IP) {
+	nodeConfig := option.Config.GetNodeConfigPath()
+	ipv4GW, ipv6Router = getCiliumHostIPsFromFile(nodeConfig)
+	if ipv4GW != nil {
+		return ipv4GW, ipv6Router
+	}
+	return getCiliumHostIPsFromNetDev(HostDevice)
 }

pkg/node/node_address_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Authors of Cilium
+// Copyright 2016-2018 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,7 +15,11 @@
 package node
 
 import (
+	"fmt"
 	"net"
+	"os"
+	"path/filepath"
+	"reflect"
 
 	. "gopkg.in/check.v1"
 )
@@ -43,4 +47,86 @@ func (s *NodeSuite) TestMaskCheck(c *C) {
 	c.Assert(IsHostIPv4(GetInternalIPv4()), Equals, true)
 	c.Assert(IsHostIPv4(GetExternalIPv4()), Equals, true)
 	c.Assert(IsHostIPv6(GetIPv6()), Equals, true)
+}
+
+func (s *NodeSuite) Test_getCiliumHostIPsFromFile(c *C) {
+	tmpDir := c.MkDir()
+	allIPsCorrect := filepath.Join(tmpDir, "node_config.h")
+	f, err := os.Create(allIPsCorrect)
+	c.Assert(err, IsNil)
+	defer f.Close()
+	fmt.Fprintf(f, `/*
+ * Node-IPv6: fd01::b
+ * Router-IPv6: f00d::a00:0:0:a4ad
+ * Host-IPv4: 10.0.0.1
+ */
+
+#define ENABLE_IPV4
+#define ROUTER_IP 0xf0, 0xd, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xa, 0x0, 0x0, 0x0, 0x0, 0x0, 0xa4, 0xad
+#define IPV4_GATEWAY 0x100000a
+#define IPV4_LOOPBACK 0x5dd0000a
+#define IPV4_MASK 0xffff
+#define IPV4_CLUSTER_RANGE 0xa
+#define IPV4_CLUSTER_MASK 0xff
+#define NAT46_PREFIX { .addr = { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xff, 0xff, 0x0, 0x0, 0x0, 0x0 } }
+#define HOST_IP 0xfd, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xb
+#define HOST_ID 1
+#define WORLD_ID 2
+#define CLUSTER_ID 3
+#define LB_RR_MAX_SEQ 31
+#define CILIUM_LB_MAP_MAX_ENTRIES 65536
+#define TUNNEL_ENDPOINT_MAP_SIZE 65536
+#define PROXY_MAP_SIZE 524288
+#define ENDPOINTS_MAP_SIZE 65535
+#define LPM_MAP_SIZE 16384
+#define POLICY_MAP_SIZE 16384
+#define IPCACHE_MAP_SIZE 512000
+#define POLICY_PROG_MAP_SIZE 65535
+#define TRACE_PAYLOAD_LEN 128ULL
+#ifndef CILIUM_NET_MAC
+#define CILIUM_NET_MAC { .addr = {0x26,0x11,0x70,0xcc,0xca,0x0c}}
+#endif /* CILIUM_NET_MAC */
+#define HOST_IFINDEX 356
+#define HOST_IFINDEX_MAC { .addr = {0x3e,0x28,0xb4,0x4b,0x95,0x25}}
+#define ENCAP_VXLAN 1
+#define ENCAP_IFINDEX 358
+`)
+
+	type args struct {
+		nodeConfig string
+	}
+	tests := []struct {
+		name            string
+		args            args
+		wantIpv4GW      net.IP
+		wantIpv6Router  net.IP
+		wantIpv6Address net.IP
+	}{
+		{
+			name: "every-ip-correct",
+			args: args{
+				nodeConfig: allIPsCorrect,
+			},
+			wantIpv4GW:      net.ParseIP("10.0.0.1"),
+			wantIpv6Router:  net.ParseIP("f00d::a00:0:0:a4ad"),
+			wantIpv6Address: net.ParseIP("fd01::b"),
+		},
+		{
+			name: "file-not-present",
+			args: args{
+				nodeConfig: "",
+			},
+			wantIpv4GW:     nil,
+			wantIpv6Router: nil,
+		},
+	}
+	for _, tt := range tests {
+		gotIpv4GW, gotIpv6Router := getCiliumHostIPsFromFile(tt.args.nodeConfig)
+		if !reflect.DeepEqual(gotIpv4GW, tt.wantIpv4GW) {
+			c.Assert(gotIpv4GW, DeepEquals, tt.wantIpv4GW)
+		}
+		if !reflect.DeepEqual(gotIpv6Router, tt.wantIpv6Router) {
+			c.Assert(gotIpv6Router, DeepEquals, tt.wantIpv6Router)
+		}
+	}
 }

commit 6c68471d4160bfb2a91610f9f88e3c7ae0ddc207

cilium: fix bailing out on auto-complete when v4/v6 ranges are specified
The node.AutoComplete() checks for ipv{4,6}AllocRange being set even
though in case of != AutoCIDR we set this after node.AutoComplete(),
meaning --ipv{4,6}-range is ignored and daemon bails out nevertheless.

Fixes: 36f92d6a6a75 ("pkg: allocate first IP in IPv4 allocation range")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

daemon/daemon.go
@@ -1059,10 +1059,6 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 	bootstrapStats.ipam.Start()
 	log.Info("Initializing node addressing")
 
-	if err := node.AutoComplete(); err != nil {
-		log.WithError(err).Fatal("Cannot autocomplete node addresses")
-	}
-
 	node.SetIPv4ClusterCidrMaskSize(option.Config.IPv4ClusterCIDRMaskSize)
 
 	if option.Config.IPv4Range != AutoCIDR {
@@ -1082,6 +1078,10 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 		if err := node.SetIPv6NodeRange(net); err != nil {
 			log.WithError(err).WithField(logfields.V6Prefix, net).Fatal("Invalid per node IPv6 allocation prefix")
 		}
+	}
+
+	if err := node.AutoComplete(); err != nil {
+		log.WithError(err).Fatal("Cannot autocomplete node addresses")
 	}
 
 	// Set up ipam conf after init() because we might be running d.conf.KVStoreIPv4Registration