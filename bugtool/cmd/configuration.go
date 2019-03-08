g0odi3z2j/cilium

commit 9b5046294b16b2f9d589fac2b895b40f1771e466

cilium: bugtool add xfrm details
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bugtool/cmd/configuration.go
@@ -73,6 +73,11 @@ func defaultCommands(confDir string, cmdDir string, k8sPods []string) []string {
 		"ip rule",
 		"ip -4 route show table 2005",
 		"ip -6 route show table 2005",
+		"ip -4 route show table 200",
+		"ip -6 route show table 200",
+		// xfrm
+		"ip xfrm policy",
+		"ip -s xfrm state",
 		// gops
 		fmt.Sprintf("gops memstats $(pidof %s)", components.CiliumAgentName),
 		fmt.Sprintf("gops stack $(pidof %s)", components.CiliumAgentName),
@@ -125,6 +130,7 @@ func loadConfigFile(path string) (*BugtoolConfiguration, error) {
 
 func catCommands() []string {
 	files := []string{
+		"/proc/net/xfrm_stat",
 		"/proc/sys/net/core/bpf_jit_enable",
 		"/proc/kallsyms",
 		"/etc/resolv.conf",

commit 89cca3cb65ab950a692e4be990251cdbc857e58f

cilium: scrub keys from bugtool xfrm
Remove keys from xfrm debug output. We want the state output to
identify any issues with xfrm setup, the output has the stats.
We don't however want to record any keys.

Fixes: 9b5046294b16b ("cilium: bugtool add xfrm details")
Reported-by: Signed-off-by: Martynas Pumputis <m@lambda.lt>
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bugtool/cmd/configuration.go
@@ -77,7 +77,7 @@ func defaultCommands(confDir string, cmdDir string, k8sPods []string) []string {
 		"ip -6 route show table 200",
 		// xfrm
 		"ip xfrm policy",
-		"ip -s xfrm state",
+		"ip -s xfrm state | awk '!/auth|enc/'",
 		// gops
 		fmt.Sprintf("gops memstats $(pidof %s)", components.CiliumAgentName),
 		fmt.Sprintf("gops stack $(pidof %s)", components.CiliumAgentName),