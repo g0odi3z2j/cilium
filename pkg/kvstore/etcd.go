g0odi3z2j/cilium

commit fe58fe1611f1099e0c71dcc870d6f37a8dd6a196

kvstore: forcefully close etcd session on error
If etcd returns an error of lease not found we need to close the session
so it gets renewed automatically.

Signed-off-by: André Martins <andre@cilium.dev>

pkg/kvstore/etcd.go
@@ -243,6 +243,24 @@ func (e *etcdClient) GetLeaseID() client.LeaseID {
 	return l
 }
 
+// checkSession verifies if the lease is still valid from the return error of
+// an etcd API call. If the error explicitly states that a lease was not found
+// we mark the session has an orphan for this etcd client. If we would not mark
+// it as an Orphan() the session would be considered expired after the leaseTTL
+// By make it orphan we guarantee the session will be marked to be renewed.
+func (e *etcdClient) checkSession(err error) {
+	if err == v3rpcErrors.ErrLeaseNotFound {
+		e.closeSession()
+	}
+}
+
+// closeSession closes the current session.
+func (e *etcdClient) closeSession() {
+	e.RWMutex.RLock()
+	e.session.Orphan()
+	e.RWMutex.RUnlock()
+}
+
 func (e *etcdClient) renewSession() error {
 	<-e.firstSession
 	<-e.session.Done()
@@ -701,6 +719,7 @@ func (e *etcdClient) Update(ctx context.Context, key string, value []byte, lease
 	if lease {
 		duration := spanstat.Start()
 		_, err := e.client.Put(ctx, key, string(value), client.WithLease(e.GetLeaseID()))
+		e.checkSession(err)
 		increaseMetric(key, metricSet, "Update", duration.EndError(err).Total(), err)
 		return Hint(err)
 	}
@@ -719,6 +738,7 @@ func (e *etcdClient) CreateOnly(ctx context.Context, key string, value []byte, l
 	txnresp, err := e.client.Txn(ctx).If(cond).Then(*req).Commit()
 	increaseMetric(key, metricSet, "CreateOnly", duration.EndError(err).Total(), err)
 	if err != nil {
+		e.checkSession(err)
 		return Hint(err)
 	}
 
@@ -737,6 +757,7 @@ func (e *etcdClient) CreateIfExists(condKey, key string, value []byte, lease boo
 	txnresp, err := e.client.Txn(ctx.TODO()).If(cond).Then(*req).Commit()
 	increaseMetric(key, metricSet, "CreateIfExists", duration.EndError(err).Total(), err)
 	if err != nil {
+		e.checkSession(err)
 		return Hint(err)
 	}
 

commit d11b03015d14c514d819e2ee8a935d3081246544

kvstore: make session orphan if the leaseID was used on a failed request
If we don't check which lease ID has errored on a request made to
the kvstore, when we marking as session as orphan we can accidentally mark
an already renewed session as orphan so we need to verify if the lease
ID of a session that we want the mark it as orphan belongs to the same
session that should be marked as an orphan.

Fixes: fe58fe1611f1 ("kvstore: forcefully close etcd session on error")
Signed-off-by: André Martins <andre@cilium.dev>

pkg/kvstore/etcd.go
@@ -248,16 +248,20 @@ func (e *etcdClient) GetLeaseID() client.LeaseID {
 // we mark the session has an orphan for this etcd client. If we would not mark
 // it as an Orphan() the session would be considered expired after the leaseTTL
 // By make it orphan we guarantee the session will be marked to be renewed.
-func (e *etcdClient) checkSession(err error) {
+func (e *etcdClient) checkSession(err error, leaseID client.LeaseID) {
 	if err == v3rpcErrors.ErrLeaseNotFound {
-		e.closeSession()
+		e.closeSession(leaseID)
 	}
 }
 
 // closeSession closes the current session.
-func (e *etcdClient) closeSession() {
+func (e *etcdClient) closeSession(leaseID client.LeaseID) {
 	e.RWMutex.RLock()
-	e.session.Orphan()
+	// only mark a session as orphan if the leaseID is the same as the
+	// session ID to avoid making any other sessions as orphan.
+	if e.session.Lease() == leaseID {
+		e.session.Orphan()
+	}
 	e.RWMutex.RUnlock()
 }
 
@@ -698,9 +702,9 @@ func (e *etcdClient) Delete(key string) error {
 	return Hint(err)
 }
 
-func (e *etcdClient) createOpPut(key string, value []byte, lease bool) *client.Op {
-	if lease {
-		op := client.OpPut(key, string(value), client.WithLease(e.GetLeaseID()))
+func (e *etcdClient) createOpPut(key string, value []byte, leaseID client.LeaseID) *client.Op {
+	if leaseID != 0 {
+		op := client.OpPut(key, string(value), client.WithLease(leaseID))
 		return &op
 	}
 
@@ -718,8 +722,9 @@ func (e *etcdClient) Update(ctx context.Context, key string, value []byte, lease
 
 	if lease {
 		duration := spanstat.Start()
-		_, err := e.client.Put(ctx, key, string(value), client.WithLease(e.GetLeaseID()))
-		e.checkSession(err)
+		leaseID := e.GetLeaseID()
+		_, err := e.client.Put(ctx, key, string(value), client.WithLease(leaseID))
+		e.checkSession(err, leaseID)
 		increaseMetric(key, metricSet, "Update", duration.EndError(err).Total(), err)
 		return Hint(err)
 	}
@@ -733,12 +738,16 @@ func (e *etcdClient) Update(ctx context.Context, key string, value []byte, lease
 // CreateOnly creates a key with the value and will fail if the key already exists
 func (e *etcdClient) CreateOnly(ctx context.Context, key string, value []byte, lease bool) error {
 	duration := spanstat.Start()
-	req := e.createOpPut(key, value, lease)
+	var leaseID client.LeaseID
+	if lease {
+		leaseID = e.GetLeaseID()
+	}
+	req := e.createOpPut(key, value, leaseID)
 	cond := client.Compare(client.Version(key), "=", 0)
 	txnresp, err := e.client.Txn(ctx).If(cond).Then(*req).Commit()
 	increaseMetric(key, metricSet, "CreateOnly", duration.EndError(err).Total(), err)
 	if err != nil {
-		e.checkSession(err)
+		e.checkSession(err, leaseID)
 		return Hint(err)
 	}
 
@@ -752,12 +761,16 @@ func (e *etcdClient) CreateOnly(ctx context.Context, key string, value []byte, l
 // CreateIfExists creates a key with the value only if key condKey exists
 func (e *etcdClient) CreateIfExists(condKey, key string, value []byte, lease bool) error {
 	duration := spanstat.Start()
-	req := e.createOpPut(key, value, lease)
+	var leaseID client.LeaseID
+	if lease {
+		leaseID = e.GetLeaseID()
+	}
+	req := e.createOpPut(key, value, leaseID)
 	cond := client.Compare(client.Version(condKey), "!=", 0)
 	txnresp, err := e.client.Txn(ctx.TODO()).If(cond).Then(*req).Commit()
 	increaseMetric(key, metricSet, "CreateIfExists", duration.EndError(err).Total(), err)
 	if err != nil {
-		e.checkSession(err)
+		e.checkSession(err, leaseID)
 		return Hint(err)
 	}
 