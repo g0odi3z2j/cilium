g0odi3z2j/cilium

commit 8d9c282bd928009b1fb3c1782a2af7b6f7cf261b

cilium: ipsec, add BPF datapath encryption direction
This enables encryption on TX path for traffic that is sent over a
vxlan tunnel.

Currently, we use tunneled IPSec but the datapath should work regardless.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bpf/bpf_netdev.c
@@ -255,7 +255,7 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 		key.ip6.p4 = 0;
 		key.family = ENDPOINT_KEY_IPV6;
 
-		ret = encap_and_redirect(skb, &key, secctx, TRACE_PAYLOAD_LEN);
+		ret = encap_and_redirect(skb, &key, secctx, TRACE_PAYLOAD_LEN, true);
 		if (ret != DROP_NO_TUNNEL_ENDPOINT)
 			return ret;
 	}
@@ -442,7 +442,7 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 		key.family = ENDPOINT_KEY_IPV4;
 
 		cilium_dbg(skb, DBG_NETDEV_ENCAP4, key.ip4, secctx);
-		ret = encap_and_redirect(skb, &key, secctx, TRACE_PAYLOAD_LEN);
+		ret = encap_and_redirect(skb, &key, secctx, TRACE_PAYLOAD_LEN, true);
 		if (ret != DROP_NO_TUNNEL_ENDPOINT)
 			return ret;
 	}
@@ -514,6 +514,21 @@ int from_netdev(struct __sk_buff *skb)
 	__u32 identity = 0;
 	int ret;
 
+#ifdef ENABLE_IPSEC
+	if (1) {
+		__u32 magic = skb->mark & MARK_MAGIC_HOST_MASK;
+
+		if (magic == MARK_MAGIC_ENCRYPT) {
+			__u32 seclabel, tunnel_endpoint = 0;
+
+			seclabel = get_identity(skb);
+			tunnel_endpoint = skb->cb[4];
+			skb->mark = 123;
+			bpf_clear_cb(skb);
+			return encap_and_redirect_with_nodeid(skb, tunnel_endpoint, seclabel, TRACE_PAYLOAD_LEN);
+		}
+	}
+#endif
 	bpf_clear_cb(skb);
 
 #ifdef FROM_HOST

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=d2ed0e1989c279c544f1e1c910534b2cf4f31611
+GO_BINDATA_SHA1SUM=5e1b83886d36db25571b146983248afd3c120466
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

bpf/bpf_lxc.c
@@ -351,7 +351,7 @@ skip_service_lookup:
 		key.ip6.p4 = 0;
 		key.family = ENDPOINT_KEY_IPV6;
 
-		ret = encap_and_redirect(skb, &key, SECLABEL, monitor);
+		ret = encap_and_redirect(skb, &key, SECLABEL, monitor, true);
 
 		/* Fall through if remote prefix was not found
 		 * (DROP_NO_TUNNEL_ENDPOINT) */
@@ -635,8 +635,18 @@ skip_service_lookup:
 
 #ifdef ENCAP_IFINDEX
 	if (tunnel_endpoint) {
-		return encap_and_redirect_with_nodeid(skb, tunnel_endpoint,
-						      SECLABEL, monitor);
+		int ret = encap_and_redirect_with_nodeid_from_lxc(skb, tunnel_endpoint,
+								  SECLABEL, monitor);
+		/* If not redirected noteably due to IPSEC then pass up to stack
+		 * for further processing.
+		 */
+		if (ret == IPSEC_ENDPOINT)
+			goto pass_to_stack;
+		/* This is either redirect by encap code or an error has occured
+		 * either way return and stack will consume skb.
+		 */
+		else
+			return ret;
 	} else {
 		/* FIXME GH-1391: Get rid of the initializer */
 		struct endpoint_key key = {};
@@ -651,11 +661,18 @@ skip_service_lookup:
 		key.ip4 = orig_dip & IPV4_MASK;
 		key.family = ENDPOINT_KEY_IPV4;
 
-		ret = encap_and_redirect(skb, &key, SECLABEL, monitor);
-
-		/* Fall through if remote prefix was not found
-		 * (DROP_NO_TUNNEL_ENDPOINT) */
-		if (ret != DROP_NO_TUNNEL_ENDPOINT)
+		/* Three cases exist here either (a) the encap and redirect could
+		 * not find the tunnel so pass to host for further processing, (b)
+		 * the packet needs further stack processing likely due to IPSec so
+		 * pass up to stack or (c) packet was redirected to tunnel device
+		 * so return.
+		 */
+		ret = encap_and_redirect(skb, &key, SECLABEL, monitor, false);
+		if (ret == DROP_NO_TUNNEL_ENDPOINT)
+			goto pass_to_stack;
+		else if (ret == IPSEC_ENDPOINT)
+			goto pass_to_stack;
+		else
 			return ret;
 	}
 #endif

bpf/lib/common.h
@@ -236,6 +236,11 @@ enum {
 
 #define IS_ERR(x) (unlikely((x < 0) || (x == TC_ACT_SHOT)))
 
+/* Cilium IPSec code to indicate packet needs to be handled
+ * by IPSec stack. Maps to TC_ACT_OK.
+ */
+#define IPSEC_ENDPOINT TC_ACT_OK
+
 /* Cilium error codes, must NOT overlap with TC return codes.
  * These also serve as drop reasons for metrics,
  * where reason > 0 corresponds to -(DROP_*)
@@ -301,6 +306,7 @@ enum {
 #define MARK_MAGIC_PROXY_INGRESS	0xA00
 #define MARK_MAGIC_PROXY_EGRESS		0xB00
 #define MARK_MAGIC_HOST			0xC00
+#define MARK_MAGIC_ENCRYPT		0xE00
 
 /**
  * get_identity - returns source identity from the mark field
@@ -308,6 +314,15 @@ enum {
 static inline int __inline__ get_identity(struct __sk_buff *skb)
 {
 	return ((skb->mark & 0xFF) << 16) | skb->mark >> 16;
+}
+
+/**
+ * set_identity - pushes 24 bit identity into skb mark value.
+ */
+static inline void __inline__ set_identity(struct __sk_buff *skb, __u32 identity)
+{
+	skb->mark = skb->mark & MARK_MAGIC_HOST_MASK;
+	skb->mark |= ((identity & 0xFFFF) << 16) | ((identity & 0xFF0000) >> 16);
 }
 
 /*

bpf/lib/encap.h
@@ -22,6 +22,25 @@
 #include "dbg.h"
 
 #ifdef ENCAP_IFINDEX
+#ifdef ENABLE_IPSEC
+static inline int __inline__
+encap_and_redirect_ipsec(struct __sk_buff *skb, __u32 tunnel_endpoint,
+			 __u32 seclabel, __u32 monitor)
+{
+	/* IPSec is performed by the stack on any packets with the
+	 * MARK_MAGIC_ENCRYPT bit set. During the process though we
+	 * lose the lxc context (seclabel and tunnel endpoint). The
+	 * tunnel endpoint can be looked up from daddr but the sec
+	 * label is stashed in the mark and extracted in bpf_netdev
+	 * to send skb onto tunnel for encap.
+	 */
+	skb->mark = MARK_MAGIC_ENCRYPT;
+	set_identity(skb, seclabel);
+	skb->cb[4] = tunnel_endpoint;
+	return IPSEC_ENDPOINT;
+}
+#endif
+
 static inline int __inline__
 encap_and_redirect_with_nodeid(struct __sk_buff *skb, __u32 tunnel_endpoint,
 			       __u32 seclabel, __u32 monitor)
@@ -46,9 +65,36 @@ encap_and_redirect_with_nodeid(struct __sk_buff *skb, __u32 tunnel_endpoint,
 	return redirect(ENCAP_IFINDEX, 0);
 }
 
+/* encap_and_redirect_with_nodeid returns IPSEC_ENDPOINT after skb meta-data is
+ * set when IPSec is enabled. Caller should pass the skb to the stack at this
+ * point. Otherwise returns TC_ACT_REDIRECT on successful redirect to tunnel
+ * device. On error returns TC_ACT_SHOT, DROP_NO_TUNNEL_ENDPOINT or
+ * DROP_WRITE_ERROR.
+ */
+static inline int __inline__
+encap_and_redirect_with_nodeid_from_lxc(struct __sk_buff *skb, __u32 tunnel_endpoint,
+					__u32 seclabel, __u32 monitor)
+{
+#ifdef ENABLE_IPSEC
+	return encap_and_redirect_ipsec(skb, tunnel_endpoint, seclabel, monitor);
+#else
+	return encap_and_redirect_with_nodeid(skb, tunnel_endpoint, seclabel, monitor);
+#endif
+}
+
+/* encap_and_redirect based on ENABLE_IPSEC flag and from_host bool will decide
+ * which version of code to call. With IPSec enabled and from_host set use the
+ * IPSec branch which configures metadata for IPSec kernel stack. Otherwise
+ * packet is redirected to output tunnel device and skb will not be seen by
+ * IP stack.
+ *
+ * Returns IPSEC_ENDPOINT when skb needs to be handed to IP stack for IPSec
+ * handling, TC_ACT_SHOT, DROP_NO_TUNNEL_ENDPOINT or DROP_WRITE_ERROR on error,
+ * and finally on successful redirect returns TC_ACT_REDIRECT.
+ */
 static inline int __inline__
 encap_and_redirect(struct __sk_buff *skb, struct endpoint_key *k,
-		   __u32 seclabel, __u32 monitor)
+		   __u32 seclabel, __u32 monitor, bool from_host)
 {
 	struct endpoint_key *tunnel;
 
@@ -56,6 +102,10 @@ encap_and_redirect(struct __sk_buff *skb, struct endpoint_key *k,
 		return DROP_NO_TUNNEL_ENDPOINT;
 	}
 
+#ifdef ENABLE_IPSEC
+	if (!from_host)
+		return encap_and_redirect_ipsec(skb, tunnel->ip4, seclabel, monitor);
+#endif
 	return encap_and_redirect_with_nodeid(skb, tunnel->ip4, seclabel, monitor);
 }
 #endif /* ENCAP_IFINDEX */

commit 49b909d83ce68864be9ae1c228bd48d22c203650

ipsec, bpf: fix build error when tunneling is disabled
If we don't have encap index defined (ENCAP_IFINDEX), then we also cannot
redirect to it. This compilation error is thrown instead:

  2019-03-16T08:38:33.30268603Z level=warning msg="/var/lib/cilium/bpf/bpf_netdev.c:548:11: warning: implicit declaration of function '__encap_and_redirect_with_nodeid' is invalid in C99 [-Wimplicit-function-declaration]" subsys=daemon
  2019-03-16T08:38:33.302694714Z level=warning msg="                        return __encap_and_redirect_with_nodeid(skb, tunnel_endpoint, seclabel, TRACE_PAYLOAD_LEN);" subsys=daemon
  2019-03-16T08:38:33.302702926Z level=warning msg="                               ^" subsys=daemon
  2019-03-16T08:38:33.302708262Z level=warning msg="1 warning generated." subsys=daemon

Fixes: 8d9c282bd928 ("cilium: ipsec, add BPF datapath encryption direction")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/bpf_netdev.c
@@ -534,7 +534,7 @@ int from_netdev(struct __sk_buff *skb)
 	__u32 identity = 0;
 	int ret;
 
-#ifdef ENABLE_IPSEC
+#if defined ENABLE_IPSEC && defined ENCAP_IFINDEX
 	if (1) {
 		__u32 magic = skb->mark & MARK_MAGIC_HOST_MASK;
 

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=56f522cc2c73bde443cb65fa72b77f2074fffff2
+GO_BINDATA_SHA1SUM=af1ee3c89109c45ca4e8546e542ed854ce4fcc8f
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 