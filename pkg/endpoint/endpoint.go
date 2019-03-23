g0odi3z2j/cilium

commit ad9c773f10a1aa2c4db8acfa8346e96a9a0d9fa3

endpoint: add EventQueue field
This will be used to schedule events for a given Endpoint which cannot run
concurrently, such as policy revision being increased and regeneration. When an
endpoint is deleted, its event queue is closed.

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/endpoint/endpoint.go
@@ -33,6 +33,7 @@ import (
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/eventqueue"
 	"github.com/cilium/cilium/pkg/fqdn"
 	identityPkg "github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
@@ -289,6 +290,8 @@ type Endpoint struct {
 
 	realizedPolicy *policy.EndpointPolicy
 
+	EventQueue *eventqueue.EventQueue
+
 	///////////////////////
 	// DEPRECATED FIELDS //
 	///////////////////////
@@ -411,9 +414,12 @@ func NewEndpointWithState(ID uint16, state string) *Endpoint {
 		state:         state,
 		hasBPFProgram: make(chan struct{}, 0),
 		controllers:   controller.NewManager(),
+		EventQueue:    eventqueue.NewEventQueueBuffered(25),
 	}
 	ep.SetDefaultOpts(option.Config.Opts)
 	ep.UpdateLogger(nil)
+	ep.EventQueue.Run()
+
 	return ep
 }
 
@@ -442,6 +448,7 @@ func NewEndpointFromChangeModel(base *models.EndpointChangeRequest) (*Endpoint, 
 		desiredPolicy:    &policy.EndpointPolicy{},
 		realizedPolicy:   &policy.EndpointPolicy{},
 		controllers:      controller.NewManager(),
+		EventQueue:       eventqueue.NewEventQueueBuffered(25),
 	}
 	ep.UpdateLogger(nil)
 
@@ -481,6 +488,7 @@ func NewEndpointFromChangeModel(base *models.EndpointChangeRequest) (*Endpoint, 
 	}
 
 	ep.SetDefaultOpts(option.Config.Opts)
+	ep.EventQueue.Run()
 
 	return ep, nil
 }
@@ -784,7 +792,7 @@ func (e *Endpoint) policyStatus() models.EndpointPolicyEnabled {
 	return policyEnabled
 }
 
-// GetID returns the endpoint's ID
+// GetID returns the endpoint's ID as a 64-bit unsigned integer.
 func (e *Endpoint) GetID() uint64 {
 	return uint64(e.ID)
 }
@@ -796,6 +804,17 @@ func (e *Endpoint) GetLabels() []string {
 	}
 
 	return e.SecurityIdentity.Labels.GetModel()
+}
+
+// GetSecurityIdentity returns the security identity of the endpoint. It assumes
+// the endpoint's mutex.
+func (e *Endpoint) GetSecurityIdentity() *identityPkg.Identity {
+	return e.SecurityIdentity
+}
+
+// GetID16 returns the endpoint's ID as a 16-bit unsigned integer.
+func (e *Endpoint) GetID16() uint16 {
+	return e.ID
 }
 
 // GetK8sPodLabels returns all labels that exist in the endpoint and were
@@ -1036,6 +1055,7 @@ func ParseEndpoint(strEp string) (*Endpoint, error) {
 	ep.desiredPolicy = &policy.EndpointPolicy{}
 	ep.realizedPolicy = &policy.EndpointPolicy{}
 	ep.controllers = controller.NewManager()
+	ep.EventQueue = eventqueue.NewEventQueueBuffered(25)
 
 	// We need to check for nil in Status, CurrentStatuses and Log, since in
 	// some use cases, status will be not nil and Cilium will eventually
@@ -1048,6 +1068,7 @@ func ParseEndpoint(strEp string) (*Endpoint, error) {
 	ep.UpdateLogger(nil)
 
 	ep.SetStateLocked(StateRestoring, "Endpoint restoring")
+	ep.EventQueue.Run()
 
 	return &ep, nil
 }
@@ -1962,6 +1983,8 @@ func (e *Endpoint) identityLabelsChanged(ctx context.Context, owner Owner, myCha
 
 	elog.WithFields(logrus.Fields{logfields.Identity: identity.StringID()}).
 		Debug("Assigned new identity to endpoint")
+
+	owner.ClearPolicyConsumers(e.ID).Wait()
 
 	e.SetIdentity(identity)
 

daemon/endpoint.go
@@ -585,6 +585,10 @@ func (d *Daemon) deleteEndpointQuiet(ep *endpoint.Endpoint, releaseIP bool) []er
 	}
 	ep.SetStateLocked(endpoint.StateDisconnecting, "Deleting endpoint")
 
+	// Since the endpoint is being deleted, we no longer need to run events
+	// in its event queue.
+	ep.EventQueue.Stop()
+
 	// Remove the endpoint before we clean up. This ensures it is no longer
 	// listed or queued for rebuilds.
 	endpointmanager.Remove(ep)

daemon/daemon.go
@@ -1593,3 +1593,7 @@ func (d *Daemon) GetNodeSuffix() string {
 
 	return ip.String()
 }
+
+func (d *Daemon) ClearPolicyConsumers(id uint16) *sync.WaitGroup {
+	return &sync.WaitGroup{}
+}

daemon/daemon_test.go
@@ -21,6 +21,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"sync"
 	"testing"
 	"time"
 
@@ -71,6 +72,7 @@ type DaemonSuite struct {
 	OnGetCompilationLock      func() *lock.RWMutex
 	OnSendNotification        func(typ monitorAPI.AgentNotification, text string) error
 	OnNewProxyLogRecord       func(l *accesslog.LogRecord) error
+	OnClearPolicyConsumers    func(id uint16) *sync.WaitGroup
 }
 
 func (ds *DaemonSuite) SetUpTest(c *C) {
@@ -125,6 +127,7 @@ func (ds *DaemonSuite) SetUpTest(c *C) {
 	ds.OnGetCompilationLock = nil
 	ds.OnSendNotification = nil
 	ds.OnNewProxyLogRecord = nil
+	ds.OnClearPolicyConsumers = nil
 }
 
 func (ds *DaemonSuite) TearDownTest(c *C) {
@@ -279,4 +282,11 @@ func (ds *DaemonSuite) NewProxyLogRecord(l *accesslog.LogRecord) error {
 
 func (ds *DaemonSuite) Datapath() datapath.Datapath {
 	return ds.d.datapath
+}
+
+func (ds *DaemonSuite) ClearPolicyConsumers(id uint16) *sync.WaitGroup {
+	if ds.OnClearPolicyConsumers != nil {
+		return ds.OnClearPolicyConsumers(id)
+	}
+	panic("ClearPolicyConsumers should not have been called")
 }

pkg/endpoint/events.go
@@ -1,0 +1,104 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package endpoint
+
+import (
+	"sync"
+
+	"github.com/cilium/cilium/pkg/eventqueue"
+)
+
+// EndpointRegenerationEvent contains all fields necessary to regenerate an endpoint.
+type EndpointRegenerationEvent struct {
+	owner        Owner
+	regenContext *regenerationContext
+	ep           *Endpoint
+}
+
+// Handle handles the regeneration event for the endpoint.
+func (ev *EndpointRegenerationEvent) Handle(res chan interface{}) {
+	e := ev.ep
+	owner := ev.owner
+	regenContext := ev.regenContext
+
+	err := e.RLockAlive()
+	if err != nil {
+		e.LogDisconnectedMutexAction(err, "before regeneration")
+		res <- &EndpointRegenerationResult{
+			err: err,
+		}
+
+		return
+	}
+	e.RUnlock()
+
+	// We should only queue the request after we use all the endpoint's
+	// lock/unlock. Otherwise this can get a deadlock if the endpoint is
+	// being deleted at the same time. More info PR-1777.
+	doneFunc := owner.QueueEndpointBuild(uint64(e.ID))
+	if doneFunc != nil {
+		e.getLogger().Debug("Dequeued endpoint from build queue")
+
+		regenContext.DoneFunc = doneFunc
+
+		err = ev.ep.regenerate(ev.owner, ev.regenContext)
+
+		doneFunc()
+		e.notifyEndpointRegeneration(owner, err)
+	} else {
+		e.getLogger().Debug("My request was cancelled because I'm already in line")
+	}
+
+	res <- &EndpointRegenerationResult{
+		err: err,
+	}
+	return
+}
+
+// EndpointRegenerationResult contains the results of an endpoint regeneration.
+type EndpointRegenerationResult struct {
+	err error
+}
+
+// EndpointRevisionBumpEvent contains all fields necessary to bump the policy
+// revision of a given endpoint.
+type EndpointRevisionBumpEvent struct {
+	Rev uint64
+	ep  *Endpoint
+}
+
+// Handle handles the revision bump event for the Endpoint.
+func (ev *EndpointRevisionBumpEvent) Handle(res chan interface{}) {
+	// TODO: if the endpoint is not in a 'ready' state that means that
+	// we cannot set the policy revision, as something else has
+	// changed endpoint state which necessitates regeneration,
+	// *or* the endpoint is in a not-ready state (i.e., a prior
+	// regeneration failed, so there is no way that we can
+	// realize the policy revision yet. Should this be signaled
+	// to the routine waiting for the result of this event?
+	ev.ep.SetPolicyRevision(ev.Rev)
+	res <- struct{}{}
+}
+
+// PolicyRevisionBumpEvent queues an event for the given endpoint to set its
+// realized policy revision to rev. This may block depending on if events have
+// been queued up for the given endpoint. It blocks until the event has
+// succeeded, or if the event has been cancelled.
+func (e *Endpoint) PolicyRevisionBumpEvent(rev uint64, wg *sync.WaitGroup) {
+	epBumpEvent := eventqueue.NewEvent(&EndpointRevisionBumpEvent{Rev: rev, ep: e})
+	// Don't care about policy revision event results - it is best effort.
+	_ = e.EventQueue.Enqueue(epBumpEvent)
+	wg.Done()
+}

pkg/endpoint/owner.go
@@ -15,6 +15,8 @@
 package endpoint
 
 import (
+	"sync"
+
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/identity/cache"
@@ -60,4 +62,8 @@ type Owner interface {
 
 	// Datapath returns a reference to the datapath implementation.
 	Datapath() datapath.Datapath
+
+	// ClearPolicyConsumers removes references to the specified id from the
+	// policy rules managed by Owner.
+	ClearPolicyConsumers(id uint16) *sync.WaitGroup
 }

pkg/endpoint/policy.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
+	"github.com/cilium/cilium/pkg/eventqueue"
 	identityPkg "github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/ipcache"
@@ -422,57 +423,59 @@ func (e *Endpoint) updateRegenerationStatistics(context *regenerationContext, er
 func (e *Endpoint) Regenerate(owner Owner, regenMetadata *ExternalRegenerationMetadata) <-chan bool {
 	done := make(chan bool, 1)
 
+	regenContext := regenMetadata.toRegenerationContext()
+
+	epEvent := eventqueue.NewEvent(&EndpointRegenerationEvent{
+		owner:        owner,
+		regenContext: regenContext,
+		ep:           e,
+	})
+
+	// This may block if the Endpoint's EventQueue is full.
+	resChan := e.EventQueue.Enqueue(epEvent)
+
 	go func() {
+
 		var buildSuccess bool
+		var regenError error
 
-		regenContext := regenMetadata.toRegenerationContext()
+		select {
+		case result, ok := <-resChan:
+			if ok {
+				regenResult := result.(*EndpointRegenerationResult)
+				regenError = regenResult.err
 
-		defer func() {
-			done <- buildSuccess
-			close(done)
-		}()
-
-		err := e.RLockAlive()
-		if err != nil {
-			e.LogDisconnectedMutexAction(err, "before regeneration")
-			return
-		}
-		e.RUnlock()
-
-		// We should only queue the request after we use all the endpoint's
-		// lock/unlock. Otherwise this can get a deadlock if the endpoint is
-		// being deleted at the same time. More info PR-1777.
-		doneFunc := owner.QueueEndpointBuild(uint64(e.ID))
-		if doneFunc != nil {
-			e.getLogger().Debug("Dequeued endpoint from build queue")
-
-			regenContext.DoneFunc = doneFunc
-			err := e.regenerate(owner, regenContext)
-			doneFunc() // in case not called already
-
-			repr, reprerr := monitorAPI.EndpointRegenRepr(e, err)
-			if reprerr != nil {
-				e.getLogger().WithError(reprerr).Warn("Notifying monitor about endpoint regeneration failed")
-			}
-
-			if err != nil {
-				buildSuccess = false
-				if reprerr == nil && !option.Config.DryMode {
-					owner.SendNotification(monitorAPI.AgentNotifyEndpointRegenerateFail, repr)
-				}
+				// Build was successful whether regeneration errored out of not.
+				buildSuccess = regenError == nil
 			} else {
-				buildSuccess = true
-				if reprerr == nil && !option.Config.DryMode {
-					owner.SendNotification(monitorAPI.AgentNotifyEndpointRegenerateSuccess, repr)
-				}
+				// This may be unnecessary(?) since 'closing' of the results
+				// channel means that event has been cancelled?
+				e.getLogger().Debug("regeneration was cancelled")
 			}
-		} else {
-			buildSuccess = false
-			e.getLogger().Debug("My request was cancelled because I'm already in line")
 		}
+
+		done <- buildSuccess
+		close(done)
 	}()
 
 	return done
+}
+
+func (e *Endpoint) notifyEndpointRegeneration(owner Owner, err error) {
+	repr, reprerr := monitorAPI.EndpointRegenRepr(e, err)
+	if reprerr != nil {
+		e.getLogger().WithError(reprerr).Warn("Notifying monitor about endpoint regeneration failed")
+	}
+
+	if err != nil {
+		if reprerr == nil && !option.Config.DryMode {
+			owner.SendNotification(monitorAPI.AgentNotifyEndpointRegenerateFail, repr)
+		}
+	} else {
+		if reprerr == nil && !option.Config.DryMode {
+			owner.SendNotification(monitorAPI.AgentNotifyEndpointRegenerateSuccess, repr)
+		}
+	}
 }
 
 // FormatGlobalEndpointID returns the global ID of endpoint in the format

commit 0e62577ae923def23b59e9dd2483301c5f3ad228

endpoint: only start EventQueue once endpoint is exposed via endpointmanager
This ensures that goroutines are not leaked for endpoints which cannot be
inserted into the endpointmanager / are not inserted into the endpointmanager
due to some error before insertion.

Fixes: ad9c773f10a1aa2c4db8acfa8346e96a9a0d9fa3 ("endpoint: add EventQueue field")

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/endpoint/endpoint.go
@@ -488,7 +488,6 @@ func NewEndpointFromChangeModel(base *models.EndpointChangeRequest) (*Endpoint, 
 	}
 
 	ep.SetDefaultOpts(option.Config.Opts)
-	ep.EventQueue.Run()
 
 	return ep, nil
 }
@@ -1068,7 +1067,6 @@ func ParseEndpoint(strEp string) (*Endpoint, error) {
 	ep.UpdateLogger(nil)
 
 	ep.SetStateLocked(StateRestoring, "Endpoint restoring")
-	ep.EventQueue.Run()
 
 	return &ep, nil
 }

pkg/endpointmanager/manager.go
@@ -92,6 +92,11 @@ func Insert(ep *endpoint.Endpoint) error {
 	ep.UnconditionalRLock()
 	mutex.Lock()
 
+	// The endpoint's eventqueue can be safely started. Ensure that it is only
+	// started once it is exposed to the endpointmanager so that it will be
+	// stopped when the endpoint is removed from the endpointmanager.
+	ep.EventQueue.Run()
+
 	endpoints[ep.ID] = ep
 	updateReferences(ep)
 
@@ -479,7 +484,6 @@ func AddEndpoint(owner endpoint.Owner, ep *endpoint.Endpoint, reason string) (er
 	if ep.ID != 0 {
 		return fmt.Errorf("Endpoint ID is already set to %d", ep.ID)
 	}
-
 	return Insert(ep)
 }
 