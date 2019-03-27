g0odi3z2j/cilium

commit 2634da5df6bb95efa631fefb03ebceb139ab6f05

k8s: set endpoint watcher selector as a CLI option
As certain environments require different tweaks, the endpoint watcher
selector needs to be option so users can set the endpoint watcher
selector accordingly their enviornment.

Signed-off-by: André Martins <andre@cilium.io>

pkg/option/config.go
@@ -155,6 +155,10 @@ const (
 	// status in kube-apiserver.
 	K8sForceJSONPatch = "k8s-force-json-patch"
 
+	// K8sWatcherEndpointSelector specifies the k8s endpoints that Cilium
+	// should watch for.
+	K8sWatcherEndpointSelector = "k8s-watcher-endpoint-selector"
+
 	// K8sAPIServer is the kubernetes api address server (for https use --k8s-kubeconfig-path instead)
 	K8sAPIServer = "k8s-api-server"
 
@@ -744,6 +748,7 @@ type DaemonConfig struct {
 	K8sAPIServer                      string
 	K8sKubeConfigPath                 string
 	K8sLegacyHostAllowsWorld          string
+	K8sWatcherEndpointSelector        string
 	KVStore                           string
 	KVStoreOpt                        map[string]string
 	LabelPrefixFile                   string

daemon/daemon_main.go
@@ -521,6 +521,9 @@ func init() {
 	option.BindEnv(option.K8sForceJSONPatch)
 	flags.MarkHidden(option.K8sForceJSONPatch)
 
+	flags.String(option.K8sWatcherEndpointSelector, defaults.K8sWatcherEndpointSelector, "K8s endpoint watcher will watch for these k8s endpoints")
+	option.BindEnv(option.K8sWatcherEndpointSelector)
+
 	flags.Bool(option.KeepConfig, false, "When restoring state, keeps containers' configuration in place")
 	option.BindEnv(option.KeepConfig)
 

daemon/k8s_watcher.go
@@ -522,8 +522,7 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 	_, endpointController := k8s.NewInformer(
 		cache.NewListWatchFromClient(k8s.Client().CoreV1().RESTClient(),
 			"endpoints", v1.NamespaceAll,
-			// Don't get any events from kubernetes endpoints.
-			fields.ParseSelectorOrDie("metadata.name!=kube-scheduler,metadata.name!=kube-controller-manager"),
+			fields.ParseSelectorOrDie(option.Config.K8sWatcherEndpointSelector),
 		),
 		&v1.Endpoints{},
 		0,

pkg/defaults/defaults.go
@@ -177,4 +177,8 @@ const (
 	// SelectiveRegeneration specifies whether regeneration of endpoints will be
 	// invoked only for endpoints which are selected by policy changes.
 	SelectiveRegeneration = true
+
+	// K8sWatcherEndpointSelector specifies the k8s endpoints that Cilium
+	// should watch for.
+	K8sWatcherEndpointSelector = "metadata.name!=kube-scheduler,metadata.name!=kube-controller-manager"
 )

commit 99998604eaedeaca548ef6e34d9d8f3c349c2425

pkg/option: set option used for k8s endpoint field selector
Fixes: 2634da5df6bb ("k8s: set endpoint watcher selector as a CLI option")
Signed-off-by: André Martins <andre@cilium.io>

pkg/option/config.go
@@ -1120,6 +1120,7 @@ func (c *DaemonConfig) Populate() {
 	c.K8sRequireIPv6PodCIDR = viper.GetBool(K8sRequireIPv6PodCIDRName)
 	c.K8sForceJSONPatch = viper.GetBool(K8sForceJSONPatch)
 	c.K8sWatcherQueueSize = uint(viper.GetInt(K8sWatcherQueueSize))
+	c.K8sWatcherEndpointSelector = viper.GetString(K8sWatcherEndpointSelector)
 	c.KeepTemplates = viper.GetBool(KeepBPFTemplates)
 	c.KeepConfig = viper.GetBool(KeepConfig)
 	c.KVStore = viper.GetString(KVStore)