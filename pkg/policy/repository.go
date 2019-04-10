g0odi3z2j/cilium

commit fd1e19cc7b4c10e05590777551558ac95e5859da

Policy: Increment revision using atomic.
This commit move the BumpPolicyRevision and GetPolicyRevision to use
atomic operations instead of locking the PolicyRepo.

With this change, the policy increment metric will work correctly due to
there is no lock in BumpPolicyRevision.

Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

pkg/policy/repository.go
@@ -17,6 +17,7 @@ package policy
 import (
 	"encoding/json"
 	"sync"
+	"sync/atomic"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/eventqueue"
@@ -187,7 +188,7 @@ func (p *Repository) wildcardL3L4Rules(ctx *SearchContext, ingress bool, l4Polic
 // TODO: Coalesce l7 rules?
 func (p *Repository) ResolveL4IngressPolicy(ctx *SearchContext) (*L4PolicyMap, error) {
 
-	result, err := p.rules.resolveL4IngressPolicy(ctx, p.revision)
+	result, err := p.rules.resolveL4IngressPolicy(ctx, p.GetRevision())
 	if err != nil {
 		return nil, err
 	}
@@ -202,7 +203,7 @@ func (p *Repository) ResolveL4IngressPolicy(ctx *SearchContext) (*L4PolicyMap, e
 // are merged together. If rules contains overlapping port definitions, the first
 // rule found in the repository takes precedence.
 func (p *Repository) ResolveL4EgressPolicy(ctx *SearchContext) (*L4PolicyMap, error) {
-	result, err := p.rules.resolveL4EgressPolicy(ctx, p.revision)
+	result, err := p.rules.resolveL4EgressPolicy(ctx, p.GetRevision())
 
 	if err != nil {
 		return nil, err
@@ -375,7 +376,7 @@ func (p *Repository) Add(r api.Rule, localRuleConsumers []Endpoint) (uint64, map
 	defer p.Mutex.Unlock()
 
 	if err := r.Sanitize(); err != nil {
-		return p.revision, nil, err
+		return p.GetRevision(), nil, err
 	}
 
 	newList := make([]*api.Rule, 1)
@@ -398,11 +399,11 @@ func (p *Repository) AddListLocked(rules api.Rules) (ruleSlice, uint64) {
 	}
 
 	p.rules = append(p.rules, newList...)
-	p.revision++
+	p.BumpRevision()
 	metrics.PolicyCount.Add(float64(len(newList)))
 	metrics.PolicyRevision.Inc()
 
-	return newList, p.revision
+	return newList, p.GetRevision()
 }
 
 // UpdateLocalConsumers updates the cache within each rule in the given repository
@@ -484,13 +485,13 @@ func (p *Repository) DeleteByLabelsLocked(labels labels.LabelArray) (ruleSlice, 
 	}
 
 	if deleted > 0 {
-		p.revision++
+		p.BumpRevision()
 		p.rules = new
 		metrics.PolicyCount.Sub(float64(deleted))
 		metrics.PolicyRevision.Inc()
 	}
 
-	return deletedRules, p.revision, deleted
+	return deletedRules, p.GetRevision(), deleted
 }
 
 // DeleteByLabels deletes all rules in the policy repository which contain the
@@ -586,7 +587,7 @@ func (p *Repository) NumRules() int {
 
 // GetRevision returns the revision of the policy repository
 func (p *Repository) GetRevision() uint64 {
-	return p.revision
+	return atomic.LoadUint64(&p.revision)
 }
 
 // Empty returns 'true' if repository has no rules, 'false' otherwise.
@@ -623,9 +624,7 @@ func (p *Repository) TranslateRules(translator Translator) (*TranslationResult, 
 // BumpRevision allows forcing policy regeneration
 func (p *Repository) BumpRevision() {
 	metrics.PolicyRevision.Inc()
-	p.Mutex.Lock()
-	defer p.Mutex.Unlock()
-	p.revision++
+	atomic.AddUint64(&p.revision, 1)
 }
 
 // GetRulesList returns the current policy
@@ -687,7 +686,7 @@ func (p *Repository) ResolvePolicy(id uint16, securityIdentity *identity.Identit
 	}
 
 	if ingressEnabled {
-		newL4IngressPolicy, err := matchingRules.resolveL4IngressPolicy(&ingressCtx, p.revision)
+		newL4IngressPolicy, err := matchingRules.resolveL4IngressPolicy(&ingressCtx, p.GetRevision())
 		if err != nil {
 			return nil, err
 		}
@@ -720,7 +719,7 @@ func (p *Repository) ResolvePolicy(id uint16, securityIdentity *identity.Identit
 	}
 
 	if egressEnabled {
-		newL4EgressPolicy, err := matchingRules.resolveL4EgressPolicy(&egressCtx, p.revision)
+		newL4EgressPolicy, err := matchingRules.resolveL4EgressPolicy(&egressCtx, p.GetRevision())
 		if err != nil {
 			return nil, err
 		}

commit bce582ee06e09cbc39efd87f8c8fa4d412884ecd

policy: Fix metrics for policy revision
Since commit fd1e19cc7b4c, the metrics policy revision was bumped twice
in add/delete paths, for example once inside `p.BumpRevision()` and once
outside in `AddListLocked()`. Fix this by leaving this responsibility up
to `BumpRevision()`.

Fixes: fd1e19cc7b4c ("Policy: Increment revision using atomic.")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/policy/repository.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Authors of Cilium
+// Copyright 2016-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -401,7 +401,6 @@ func (p *Repository) AddListLocked(rules api.Rules) (ruleSlice, uint64) {
 	p.rules = append(p.rules, newList...)
 	p.BumpRevision()
 	metrics.PolicyCount.Add(float64(len(newList)))
-	metrics.PolicyRevision.Inc()
 
 	return newList, p.GetRevision()
 }
@@ -488,7 +487,6 @@ func (p *Repository) DeleteByLabelsLocked(labels labels.LabelArray) (ruleSlice, 
 		p.BumpRevision()
 		p.rules = new
 		metrics.PolicyCount.Sub(float64(deleted))
-		metrics.PolicyRevision.Inc()
 	}
 
 	return deletedRules, p.GetRevision(), deleted