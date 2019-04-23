g0odi3z2j/cilium

commit 7a37b97feaac930a943f51de42bd0c7cc0c6d95d

datapath/iptables: Check iptables kernel modules
Before this change, ip6tables was called for rules removal even if
ip6tables modules were not loaded.

After this change, iptables module is going to:
- fail if iptables modules are not loaded
- fail if ip6tables modules are not loaded, but IPv6 is enabled
- not use ip6tables if its modules are not loaded and IPv6 is disabled

Fixes: #7453
Fixes: 87eaea1fc94d ("iptables: Add support for IPv6, make IPv4 optional.")
Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

pkg/datapath/iptables/iptables.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/modules"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/proxy"
@@ -214,8 +215,40 @@ var ciliumChains = []customChain{
 	},
 }
 
+// IptablesManager manages the iptables-related configuration for Cilium.
+type IptablesManager struct {
+	ip6tables bool
+}
+
+// Init initializes the iptables manager and checks for iptables kernel modules
+// availability.
+func (m *IptablesManager) Init() {
+	modulesManager := &modules.ModulesManager{}
+	ip6tables := true
+	if err := modulesManager.Init(); err != nil {
+		log.WithError(err).Fatal(
+			"Unable to get information about kernel modules")
+	}
+	if err := modulesManager.FindOrLoadModules(
+		"ip_tables", "iptable_nat", "iptable_mangle", "iptable_raw",
+		"iptable_filter"); err != nil {
+		log.WithError(err).Fatal("iptables modules could not be initialized")
+	}
+	if err := modulesManager.FindOrLoadModules(
+		"ip6_tables", "ip6table_mangle", "ip6table_raw"); err != nil {
+		if option.Config.EnableIPv6 {
+			log.WithError(err).Fatal(
+				"IPv6 is enabled and ip6tables modules could not be initialized")
+		}
+		log.WithError(err).Debug(
+			"ip6tables kernel modules could not be loaded, so IPv6 cannot be used")
+		ip6tables = false
+	}
+	m.ip6tables = ip6tables
+}
+
 // RemoveRules removes iptables rules installed by Cilium.
-func RemoveRules() {
+func (m *IptablesManager) RemoveRules() {
 	// Set of tables that have had iptables rules in any Cilium version
 	tables := []string{"nat", "mangle", "raw", "filter"}
 	for _, t := range tables {
@@ -223,19 +256,21 @@ func RemoveRules() {
 	}
 
 	// Set of tables that have had ip6tables rules in any Cilium version
-	tables6 := []string{"mangle", "raw"}
-	for _, t := range tables6 {
-		removeCiliumRules(t, "ip6tables")
-	}
+	if m.ip6tables {
+		tables6 := []string{"mangle", "raw"}
+		for _, t := range tables6 {
+			removeCiliumRules(t, "ip6tables")
+		}
 
-	for _, c := range ciliumChains {
-		c.remove()
+		for _, c := range ciliumChains {
+			c.remove()
+		}
 	}
 }
 
 // InstallRules installs iptables rules for Cilium in specific use-cases
 // (most specifically, interaction with kube-proxy).
-func InstallRules(ifName string) error {
+func (m *IptablesManager) InstallRules(ifName string) error {
 	for _, c := range ciliumChains {
 		if err := c.add(); err != nil {
 			return fmt.Errorf("cannot add custom chain %s: %s", c.name, err)

daemon/daemon.go
@@ -555,10 +555,12 @@ func (d *Daemon) compileBase() error {
 	}
 
 	if option.Config.EnableIPv4 {
+		iptablesManager := iptables.IptablesManager{}
+		iptablesManager.Init()
 		// Always remove masquerade rule and then re-add it if required
-		iptables.RemoveRules()
+		iptablesManager.RemoveRules()
 		if option.Config.InstallIptRules {
-			if err := iptables.InstallRules(option.Config.HostDevice); err != nil {
+			if err := iptablesManager.InstallRules(option.Config.HostDevice); err != nil {
 				return err
 			}
 		}

commit 21fabe1d4813b926ff0ec0558d9712ec212b7ae4

iptables: Correctly remove Cilium chains when IPv6 is disabled
The Cilium chain removal in RemoveRules() was made dependent on IPv6 being
enabled, this is incorrect and leads to the following error when Ipv6 is
disabled:

```
level=warning msg="iptables: Chain already exists." subsys=iptables
level=error msg="Error while initializing daemon" error="cannot add custom chain CILIUM_OUTPUT: exit status 1" subsys=daemon
```

Fixes: 7a37b97feaa ("datapath/iptables: Check iptables kernel modules")

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/datapath/iptables/iptables.go
@@ -278,10 +278,10 @@ func (m *IptablesManager) RemoveRules() {
 		for _, t := range tables6 {
 			removeCiliumRules(t, "ip6tables")
 		}
+	}
 
-		for _, c := range ciliumChains {
-			c.remove()
-		}
+	for _, c := range ciliumChains {
+		c.remove()
 	}
 }
 