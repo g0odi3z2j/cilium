g0odi3z2j/cilium

commit 7b3ed3074f8ef154844f0a5e0a704492b3de2fd3

k8s: Fix leak of k8s controller on kvstore connect & disconnect
A go channel plus a k8s controller was leaked on each kvstore connect +
disconnect event as the minimal k8s event controller was replaced with a
full-scope controller.

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/k8s_watcher.go
@@ -846,6 +846,7 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 			// Create a new pod controller when we are disconnected with the
 			// kvstore
 			<-kvstore.Client().Disconnected()
+			close(isConnected)
 			log.Info("Disconnected from KVStore, watching for pod events all nodes")
 		}
 	}()
@@ -946,6 +947,7 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 			// Create a new node controller when we are disconnected with the
 			// kvstore
 			<-kvstore.Client().Disconnected()
+			close(isConnected)
 
 			log.Info("Disconnected from KVStore, restarting k8s node watcher")
 		}

commit 43fba39ccbc983a8a87d7d237d0922ac4d1a9186

k8s: fix panic of closed channel
The channel was previously closed and it does not need to be re-closed
again.

Fixes: 7b3ed3074f8e ("k8s: Fix leak of k8s controller on kvstore connect & disconnect")
Signed-off-by: André Martins <andre@cilium.io>

daemon/k8s_watcher.go
@@ -947,7 +947,6 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 			// Create a new node controller when we are disconnected with the
 			// kvstore
 			<-kvstore.Client().Disconnected()
-			close(isConnected)
 
 			log.Info("Disconnected from KVStore, restarting k8s node watcher")
 		}