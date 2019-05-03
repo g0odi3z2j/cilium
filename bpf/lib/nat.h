g0odi3z2j/cilium

commit 6e52f5ea1e3ee394a3ae8276ff0bbcf28aed03a6

bpf: initial bpf-based masquerading support
This work adds a NAT engine in BPF which is working together with
Cilium's BPF-based connection tracker. This gets enabled when the
following is set: --install-iptables-rules=false --masquerade=true
In this case Cilium won't rely on iptables to perform the masquerading
but instead uses its own, more efficient BPF-based implementation.

Right now at the beginning this option is only made available for
direct routing mode with ipvlan, but will be lifted in subsequent
work for veth-based datapath as well. In combination with ipvlan,
this allows to use the more efficient L3 mode instead of relying
on L3S! The faster L3 mode is otherwise not possible to use in
combination with masquerading since it bypasses Netfilter hooks.
For the BPF-based implementation in this work it is not a problem
since tc-based hooks are not bypassed.

The NAT core is mostly stand-alone, and in this work integrated
into the bpf_netdev program, but could in future also be integrated
into others. The NAT code handles connections from containers/pods
but also host local ones. In case of the latter, it will integrate
mappings into the same NAT table to check for potential collisions
and if the case, then it will map them to a different port just like
any other connections from container/pods. For connection originating
from local host, they need to be passed through the BPF-based
connection tracker as well in order to manage lifetime of NAT
mappings. For this purpose, it reuses the global connection tracker.

NAT mappings piggy back on Cilium's CT garbage collector in order
to remove related mappings once connection is terminated. Both
support for IPv4 as well as IPv6 has been implemented and tested
in this work. L3 protos that are supported are: TCP, UDP, ICMP
and ICMPv6. For both ICMP protocols, echo request/reply are supported
and in case of collisions their request/reply identifier translated.

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=7a23cc5cf19a6ae3b638a3e987572881227b97d2
+GO_BINDATA_SHA1SUM=fb7993e0db1910eb8d4df7887581d1dadc9da76b
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

bpf/bpf_alignchecker.c
@@ -61,6 +61,8 @@ int main() {
     DECLARE_STRUCT(ep_config, iter);
     DECLARE_STRUCT(policy_key, iter);
     DECLARE_STRUCT(policy_entry, iter);
+    DECLARE_STRUCT(ipv4_nat_entry, iter);
+    DECLARE_STRUCT(ipv6_nat_entry, iter);
 
     return 0;
 }

bpf/bpf_netdev.c
@@ -47,7 +47,7 @@
 #include "lib/policy.h"
 #include "lib/drop.h"
 #include "lib/encap.h"
-
+#include "lib/nat.h"
 
 #if defined FROM_HOST && (defined ENABLE_IPV4 || defined ENABLE_IPV6)
 static inline int rewrite_dmac_to_host(struct __sk_buff *skb)
@@ -530,8 +530,7 @@ static inline bool __inline__ handle_identity_from_host(struct __sk_buff *skb, _
 }
 #endif
 
-__section("from-netdev")
-int from_netdev(struct __sk_buff *skb)
+static __always_inline int do_netdev(struct __sk_buff *skb)
 {
 	__u32 identity = 0;
 	__u16 proto;
@@ -614,6 +613,36 @@ int from_netdev(struct __sk_buff *skb)
 		ret = TC_ACT_OK;
 	}
 
+	return ret;
+}
+
+__section("from-netdev")
+int from_netdev(struct __sk_buff *skb)
+{
+	return do_netdev(skb);
+}
+
+__section("masq")
+int do_masq(struct __sk_buff *skb)
+{
+	return snat_process(skb, BPF_PKT_DIR);
+}
+
+__section("masq-pre")
+int do_masq_pre(struct __sk_buff *skb)
+{
+	int ret = snat_process(skb, BPF_PKT_DIR);
+	if (!ret)
+		ret = do_netdev(skb);
+	return ret;
+}
+
+__section("masq-post")
+int do_masq_post(struct __sk_buff *skb)
+{
+	int ret = do_netdev(skb);
+	if (!ret)
+		ret = snat_process(skb, BPF_PKT_DIR);
 	return ret;
 }
 

bpf/init.sh
@@ -25,6 +25,7 @@ XDP_DEV=$7
 XDP_MODE=$8
 MTU=$9
 IPSEC=${10}
+MASQ=${11}
 
 ID_HOST=1
 ID_WORLD=2
@@ -251,15 +252,23 @@ function bpf_load()
 	OUT=$5
 	SEC=$6
 	CALLS_MAP=$7
+	SKIP=$8
 
 	NODE_MAC=$(ip link show $DEV | grep ether | awk '{print $2}')
 	NODE_MAC="{.addr=$(mac2array $NODE_MAC)}"
 
-	OPTS="${OPTS} -DNODE_MAC=${NODE_MAC} -DCALLS_MAP=${CALLS_MAP}"
-	bpf_compile $IN $OUT obj "$OPTS"
+	if [ "$WHERE" == "ingress" ]; then
+		OPTS_DIR="-DBPF_PKT_DIR=1"
+	else
+		OPTS_DIR="-DBPF_PKT_DIR=0"
+	fi
 
-	tc qdisc del dev $DEV clsact 2> /dev/null || true
-	tc qdisc add dev $DEV clsact
+	OPTS="${OPTS} ${OPTS_DIR} -DNODE_MAC=${NODE_MAC} -DCALLS_MAP=${CALLS_MAP}"
+	bpf_compile $IN $OUT obj "$OPTS"
+	if [ "$SKIP" != "no_qdisc_reset" ]; then
+		tc qdisc del dev $DEV clsact 2> /dev/null || true
+		tc qdisc add dev $DEV clsact
+	fi
 	cilium-map-migrate -s $OUT
 	set +e
 	tc filter add dev $DEV $WHERE prio 1 handle 1 bpf da obj $OUT sec $SEC
@@ -381,8 +390,8 @@ if [ "$MODE" = "vxlan" -o "$MODE" = "geneve" ]; then
 
 	CALLS_MAP="cilium_calls_overlay_${ID_WORLD}"
 	POLICY_MAP="cilium_policy_reserved_${ID_WORLD}"
-	OPTS="-DSECLABEL=${ID_WORLD} -DPOLICY_MAP=${POLICY_MAP}"
-	bpf_load $ENCAP_DEV "$OPTS" "ingress" bpf_overlay.c bpf_overlay.o from-overlay ${CALLS_MAP}
+	COPTS="-DSECLABEL=${ID_WORLD} -DPOLICY_MAP=${POLICY_MAP}"
+	bpf_load $ENCAP_DEV "$COPTS" "ingress" bpf_overlay.c bpf_overlay.o from-overlay ${CALLS_MAP}
 else
 	# Remove eventual existing encapsulation device from previous run
 	ip link del cilium_vxlan 2> /dev/null || true
@@ -399,8 +408,16 @@ if [ "$MODE" = "direct" ] || [ "$MODE" = "ipvlan" ]; then
 
 		CALLS_MAP=cilium_calls_netdev_${ID_WORLD}
 		POLICY_MAP="cilium_policy_reserved_${ID_WORLD}"
-		OPTS="-DSECLABEL=${ID_WORLD} -DPOLICY_MAP=${POLICY_MAP}"
-		bpf_load $NATIVE_DEV "$OPTS" "ingress" bpf_netdev.c bpf_netdev.o from-netdev $CALLS_MAP
+		COPTS="-DSECLABEL=${ID_WORLD} -DPOLICY_MAP=${POLICY_MAP}"
+		if [ "$MASQ" = "true" ]; then
+			SECTION="masq-pre"
+		else
+			SECTION="from-netdev"
+		fi
+		bpf_load $NATIVE_DEV "$COPTS" "ingress" bpf_netdev.c bpf_netdev.o $SECTION $CALLS_MAP
+		if [ "$MASQ" = "true" ]; then
+			bpf_load $NATIVE_DEV "$COPTS" "egress" bpf_netdev.c bpf_netdev.o masq $CALLS_MAP "no_qdisc_reset"
+		fi
 
 		echo "$NATIVE_DEV" > $RUNDIR/device.state
 	fi
@@ -413,8 +430,8 @@ elif [ "$MODE" = "lb" ]; then
 		fi
 
 		CALLS_MAP="cilium_calls_lb"
-		OPTS="-DLB_L3 -DLB_L4"
-		bpf_load $NATIVE_DEV "$OPTS" "ingress" bpf_lb.c bpf_lb.o from-netdev $CALLS_MAP
+		COPTS="-DLB_L3 -DLB_L4"
+		bpf_load $NATIVE_DEV "$COPTS" "ingress" bpf_lb.c bpf_lb.o from-netdev $CALLS_MAP
 
 		echo "$NATIVE_DEV" > $RUNDIR/device.state
 	fi
@@ -431,11 +448,11 @@ fi
 # bpf_host.o requires to see an updated node_config.h which includes ENCAP_IFINDEX
 CALLS_MAP="cilium_calls_netdev_ns_${ID_HOST}"
 POLICY_MAP="cilium_policy_reserved_${ID_HOST}"
-OPTS="-DFROM_HOST -DFIXED_SRC_SECCTX=${ID_HOST} -DSECLABEL=${ID_HOST} -DPOLICY_MAP=${POLICY_MAP}"
+COPTS="-DFROM_HOST -DFIXED_SRC_SECCTX=${ID_HOST} -DSECLABEL=${ID_HOST} -DPOLICY_MAP=${POLICY_MAP}"
 if [ "$MODE" == "ipvlan" ]; then
-	OPTS+=" -DENABLE_EXTRA_HOST_DEV"
+	COPTS+=" -DENABLE_EXTRA_HOST_DEV"
 fi
-bpf_load $HOST_DEV1 "$OPTS" "egress" bpf_netdev.c bpf_host.o from-netdev $CALLS_MAP
+bpf_load $HOST_DEV1 "$COPTS" "egress" bpf_netdev.c bpf_host.o from-netdev $CALLS_MAP
 
 if [ "$IPSEC" == "true" ]; then
 	bpf_load $HOST_DEV2 "" "ingress" bpf_ipsec.c bpf_ipsec.o from-netdev $CALLS_MAP
@@ -443,8 +460,8 @@ fi
 
 if [ -n "$XDP_DEV" ]; then
 	CIDR_MAP="cilium_cidr_v*"
-	OPTS=""
-	xdp_load $XDP_DEV $XDP_MODE "$OPTS" bpf_xdp.c bpf_xdp.o from-netdev $CIDR_MAP
+	COPTS=""
+	xdp_load $XDP_DEV $XDP_MODE "$COPTS" bpf_xdp.c bpf_xdp.o from-netdev $CIDR_MAP
 fi
 
 # Compile dummy BPF file containing all shared struct definitions used by

bpf/lib/common.h
@@ -304,6 +304,8 @@ enum {
 #define DROP_HOST_UNREACHABLE		-164
 #define DROP_NO_CONFIG		-165
 #define DROP_UNSUPPORTED_L2		-166
+#define DROP_NAT_NO_MAPPING	-167
+#define DROP_NAT_UNSUPP_PROTO	-168
 
 /* Cilium metrics reason for forwarding packet.
  * If reason > 0 then this is a drop reason and value corresponds to -(DROP_*)
@@ -395,6 +397,11 @@ enum {
 	NAT64,
 	NAT46,
 };
+
+#define TUPLE_F_OUT		0	/* Outgoing flow */
+#define TUPLE_F_IN		1	/* Incoming flow */
+#define TUPLE_F_RELATED		2	/* Flow represents related packets */
+#define TUPLE_F_SERVICE		4	/* Flow represents service/slave map */
 
 #define CT_EGRESS 0
 #define CT_INGRESS 1

bpf/lib/conntrack.h
@@ -59,12 +59,6 @@
 #endif
 
 #ifdef CONNTRACK
-
-#define TUPLE_F_OUT		0	/* Outgoing flow */
-#define TUPLE_F_IN		1	/* Incoming flow */
-#define TUPLE_F_RELATED		2	/* Flow represents related packets */
-#define TUPLE_F_SERVICE		4	/* Flow represents service/slave map */
-
 enum {
 	ACTION_UNSPEC,
 	ACTION_CREATE,

bpf/lib/utils.h
@@ -95,6 +95,10 @@ static inline __u32 bpf_ktime_get_sec(void)
 # define __fetch(x) (__u32)(&(x))
 #endif
 
+#ifndef build_bug_on
+# define build_bug_on(e) ((void)sizeof(char[1 - 2*!!(e)]))
+#endif
+
 /* fetch_* macros assist in fetching variously sized static data */
 #define fetch_u32(x) __fetch(x)
 #define fetch_u32_i(x, i) __fetch(x ## _ ## i)

commit 58be3d56d2bd6f78cc67f51a3f8bc09887fa19c8

bpf: Force preallocation for SNAT maps of LRU type
As reported by a user and tgraf, the bpf/hashtab.c in the kernel
has the following precondition when creating maps:

    if (lru && !prealloc)
        return -ENOTSUPP;

Previously, the SNAT related maps set `CONDITIONAL_PREALLOC` regardless
of the maps type. Thus, when `HAVE_LRU_MAP_TYPE` was detected and
`--preallocate-bpf-maps=false` was passed to cilium-agent, the kernel
was failing to load the map with the following error:

    Map object 'cilium_snat_v4_external' rejected: Unknown error 524 (524)!

Fixes: 6e52f5ea1e ("bpf: initial bpf-based masquerading support")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/lib/nat.h
@@ -111,7 +111,9 @@ struct bpf_elf_map __section_maps SNAT_MAPPING_IPV4 = {
 	.size_value	= sizeof(struct ipv4_nat_entry),
 	.pinning	= PIN_GLOBAL_NS,
 	.max_elem	= SNAT_MAPPING_IPV4_SIZE,
+#ifndef HAVE_LRU_MAP_TYPE
 	.flags		= CONDITIONAL_PREALLOC,
+#endif
 };
 
 static __always_inline
@@ -494,7 +496,9 @@ struct bpf_elf_map __section_maps SNAT_MAPPING_IPV6 = {
 	.size_value	= sizeof(struct ipv6_nat_entry),
 	.pinning	= PIN_GLOBAL_NS,
 	.max_elem	= SNAT_MAPPING_IPV6_SIZE,
+#ifndef HAVE_LRU_MAP_TYPE
 	.flags		= CONDITIONAL_PREALLOC,
+#endif
 };
 
 static __always_inline

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=7131896a0dc60a098a17f1bef09cee12d9b033c4
+GO_BINDATA_SHA1SUM=72b36d7b8971729418098eab9fc2f53efbf7cfa4
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 