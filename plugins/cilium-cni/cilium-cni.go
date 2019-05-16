g0odi3z2j/cilium

commit 768cba3ae998c37e5f1757c1427a97d82b268898

cni: Fix unexpected end of JSON input on errors
Due to a defer which overwrite the `err` variable, the cmdAdd signaled success
for various error scenarios. Due to not providing a valid JSON result though,
the CNI layer would complain with `unexpected end of JSON input`

Also provide clearer error messages to allow pinpointing errors immediately.

This will make error messages in `kubectl describe pod` a lot clearer.

Signed-off-by: Thomas Graf <thomas@cilium.io>

plugins/cilium-cni/cilium-cni.go
@@ -275,6 +275,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 
 	n, cniVer, err = types.LoadNetConf(args.StdinData)
 	if err != nil {
+		err = fmt.Errorf("unable to parse CNI configuration \"%s\": %s", args.StdinData, err)
 		return
 	}
 
@@ -336,7 +337,8 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 
 	configResult, err := c.ConfigGet()
 	if err != nil {
-		return fmt.Errorf("unable to retrieve configuration from cilium-agent: %s", err)
+		err = fmt.Errorf("unable to retrieve configuration from cilium-agent: %s", err)
+		return
 	}
 
 	if configResult == nil || configResult.Status == nil {
@@ -364,12 +366,13 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 		)
 		veth, peer, tmpIfName, err = connector.SetupVeth(ep.ContainerID, int(conf.DeviceMTU), ep)
 		if err != nil {
+			err = fmt.Errorf("unable to set up veth on host side: %s", err)
 			return err
 		}
 		defer func() {
 			if err != nil {
-				if err = netlink.LinkDel(veth); err != nil {
-					logger.WithError(err).WithField(logfields.Veth, veth.Name).Warn("failed to clean up and delete veth")
+				if err2 := netlink.LinkDel(veth); err != nil {
+					logger.WithError(err2).WithField(logfields.Veth, veth.Name).Warn("failed to clean up and delete veth")
 				}
 			}
 		}()
@@ -381,6 +384,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 
 		_, _, err = connector.SetupVethRemoteNs(netNs, tmpIfName, args.IfName)
 		if err != nil {
+			err = fmt.Errorf("unable to set up veth on container side: %s", err)
 			return
 		}
 	case option.DatapathModeIpvlan:
@@ -393,6 +397,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 			int(conf.DeviceMTU), index, ipvlanConf.OperationMode, ep,
 		)
 		if err != nil {
+			err = fmt.Errorf("unable to setup ipvlan datapath: %s", err)
 			return
 		}
 		defer unix.Close(mapFD)
@@ -401,6 +406,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 	podName := string(cniArgs.K8S_POD_NAMESPACE) + "/" + string(cniArgs.K8S_POD_NAME)
 	ipam, err = c.IPAMAllocate("", podName)
 	if err != nil {
+		err = fmt.Errorf("unable to allocate IP via local cilium agent: %s", err)
 		return
 	}
 
@@ -418,6 +424,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 	}()
 
 	if err = connector.SufficientAddressing(ipam.HostAddressing); err != nil {
+		err = fmt.Errorf("IP allocation addressing in insufficient: %s", err)
 		return
 	}
 
@@ -439,6 +446,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 
 		ipConfig, routes, err = prepareIP(ep.Addressing.IPV6, true, &state, int(conf.RouteMTU))
 		if err != nil {
+			err = fmt.Errorf("unable to prepare IP addressing for '%s': %s", ep.Addressing.IPV6, err)
 			return
 		}
 		res.IPs = append(res.IPs, ipConfig)
@@ -450,6 +458,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 
 		ipConfig, routes, err = prepareIP(ep.Addressing.IPV4, false, &state, int(conf.RouteMTU))
 		if err != nil {
+			err = fmt.Errorf("unable to prepare IP addressing for '%s': %s", ep.Addressing.IPV4, err)
 			return
 		}
 		res.IPs = append(res.IPs, ipConfig)
@@ -466,6 +475,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 		macAddrStr, err = configureIface(ipam, args.IfName, &state)
 		return err
 	}); err != nil {
+		err = fmt.Errorf("unable to configure interfaces in container namespace: %s", err)
 		return
 	}
 

commit aa55e4132aec2bd831e8a7b62df77d890377ccf5

cni: Fix incorrect logging in failure case
This failure case should only trigger logging when the deletion of the
veth fails during an endpoint add request failure.

Fixes: 768cba3ae998 ("cni: Fix unexpected end of JSON input on errors")
Signed-off-by: Joe Stringer <joe@cilium.io>

plugins/cilium-cni/cilium-cni.go
@@ -371,7 +371,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 		}
 		defer func() {
 			if err != nil {
-				if err2 := netlink.LinkDel(veth); err != nil {
+				if err2 := netlink.LinkDel(veth); err2 != nil {
 					logger.WithError(err2).WithField(logfields.Veth, veth.Name).Warn("failed to clean up and delete veth")
 				}
 			}