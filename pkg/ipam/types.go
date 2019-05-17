g0odi3z2j/cilium

commit 7ac9fdb301bb7fc42f8655883ca21587ed6d8213

ipam: Provide ipam information in debuginfo
Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/ipam/types.go
@@ -19,6 +19,8 @@ import (
 
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/lock"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 // Allocator is the interface for an IP allocator implementation
@@ -53,4 +55,13 @@ type IPAM struct {
 	allocatorMutex lock.RWMutex
 
 	blacklist map[string]string
+}
+
+// DebugStatus implements debug.StatusObject to provide debug status collection
+// ability
+func (ipam *IPAM) DebugStatus() string {
+	ipam.allocatorMutex.RLock()
+	str := spew.Sdump(ipam)
+	ipam.allocatorMutex.RUnlock()
+	return str
 }

daemon/daemon.go
@@ -1021,6 +1021,7 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 	d.policyTrigger = t
 
 	debug.RegisterStatusObject("k8s-service-cache", &d.k8sSvcCache)
+	debug.RegisterStatusObject("ipam", d.ipam)
 
 	bootstrapStats.k8sInit.Start()
 	k8s.Configure(option.Config.K8sAPIServer, option.Config.K8sKubeConfigPath)

commit 7bc3fde97bac96898328f1e5844430f906d888dd

ipam: Fix IPAM debuginfo race on bootstrap
d.ipam can still be nil when debuginfo is called. Fortunately the panic was
captured in the recover() of the API handler.

Fixes: 7ac9fdb301b ("ipam: Provide ipam information in debuginfo")
Fixes: #8074

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/ipam/types.go
@@ -62,6 +62,10 @@ type IPAM struct {
 // DebugStatus implements debug.StatusObject to provide debug status collection
 // ability
 func (ipam *IPAM) DebugStatus() string {
+	if ipam == nil {
+		return "<nil>"
+	}
+
 	ipam.allocatorMutex.RLock()
 	str := spew.Sdump(ipam)
 	ipam.allocatorMutex.RUnlock()