g0odi3z2j/cilium

commit 30bae84876d1919ef17dca3ae175b1bde4689d5e

pkg/{bpf,datapath,maps}: use same MapKey and MapValue in map iterations
As allocating a new MapKey and MapValue is proven to be expensive we can
reuse the same MapKey and MapValue, avoiding re-allocating memory for
each map iteration. The caller only needs to guarantee the Key and
Value will be DeepCopies during the call back to avoid data override.

Signed-off-by: André Martins <andre@cilium.io>

pkg/maps/ctmap/ctmap.go
@@ -119,26 +119,26 @@ func InitMapInfo(tcpMaxEntries, anyMaxEntries int) {
 	mapType := MapTypeIPv4TCPLocal
 	for _, maxEntries := range []int{MapNumEntriesLocal, tcpMaxEntries} {
 		setupMapInfo(MapType(mapType), "CT_MAP_TCP4",
-			&tuple.TupleKey4{},
-			int(unsafe.Sizeof(tuple.TupleKey4{})), maxEntries,
-			ct4DumpParser, natV4)
+			&CtKey4{},
+			int(unsafe.Sizeof(CtKey4{})), maxEntries,
+			bpf.ConvertKeyValue, natV4)
 		mapType++
 		setupMapInfo(MapType(mapType), "CT_MAP_TCP6",
-			&tuple.TupleKey6{},
-			int(unsafe.Sizeof(tuple.TupleKey6{})), maxEntries,
-			ct6DumpParser, natV6)
+			&CtKey6{},
+			int(unsafe.Sizeof(CtKey6{})), maxEntries,
+			bpf.ConvertKeyValue, natV6)
 		mapType++
 	}
 	for _, maxEntries := range []int{MapNumEntriesLocal, anyMaxEntries} {
 		setupMapInfo(MapType(mapType), "CT_MAP_ANY4",
-			&tuple.TupleKey4{},
+			&CtKey4{},
 			int(unsafe.Sizeof(tuple.TupleKey4{})), maxEntries,
-			ct4DumpParser, natV4)
+			bpf.ConvertKeyValue, natV4)
 		mapType++
 		setupMapInfo(MapType(mapType), "CT_MAP_ANY6",
-			&tuple.TupleKey6{},
-			int(unsafe.Sizeof(tuple.TupleKey6{})), maxEntries,
-			ct6DumpParser, natV6)
+			&CtKey6{},
+			int(unsafe.Sizeof(CtKey6{})), maxEntries,
+			bpf.ConvertKeyValue, natV6)
 		mapType++
 	}
 }
@@ -203,30 +203,14 @@ func (m *Map) DumpEntries() (string, error) {
 	return buffer.String(), err
 }
 
-func ct4DumpParser(key []byte, value []byte) (bpf.MapKey, bpf.MapValue, error) {
-	k, v := tuple.TupleKey4Global{}, CtEntry{}
-
-	if err := bpf.ConvertKeyValue(key, value, &k, &v); err != nil {
-		return nil, nil, err
-	}
-	return &k, &v, nil
-}
-
-func ct6DumpParser(key []byte, value []byte) (bpf.MapKey, bpf.MapValue, error) {
-	k, v := tuple.TupleKey6Global{}, CtEntry{}
-
-	if err := bpf.ConvertKeyValue(key, value, &k, &v); err != nil {
-		return nil, nil, err
-	}
-	return &k, &v, nil
-}
-
 // NewMap creates a new CT map of the specified type with the specified name.
 func NewMap(mapName string, mapType MapType) *Map {
 	result := &Map{
 		Map: *bpf.NewMap(mapName,
 			bpf.MapTypeLRUHash,
+			mapInfo[mapType].mapKey,
 			mapInfo[mapType].keySize,
+			&CtEntry{},
 			int(unsafe.Sizeof(CtEntry{})),
 			mapInfo[mapType].maxEntries,
 			0, 0,

pkg/maps/ctmap/lookup.go
@@ -161,6 +161,12 @@ func Lookup(epname string, remoteAddr, localAddr string, proto u8proto.U8proto, 
 		if err != nil {
 			return nil, fmt.Errorf("Can not open CT map %s: %s", mapname, err)
 		}
+		if ipv4 {
+			m.MapKey = &CtKey4{}
+		} else {
+			m.MapKey = &CtKey6{}
+		}
+		m.MapValue = &CtEntry{}
 	}
 
 	v, err := m.Lookup(key)

cilium/cmd/bpf_lb_list.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2017-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -53,7 +53,7 @@ func dumpSVC(serviceList map[string][]string) {
 
 	parseBackendEntry := func(key bpf.MapKey, value bpf.MapValue) {
 		id := key.(lbmap.BackendKey).GetID()
-		backendMap[id] = value.(lbmap.BackendValue)
+		backendMap[id] = value.DeepCopyMapValue().(lbmap.BackendValue)
 	}
 	if err := lbmap.Backend4Map.DumpWithCallbackIfExists(parseBackendEntry); err != nil {
 		Fatalf("Unable to dump IPv4 backends table: %s", err)

pkg/bpf/map_linux.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"reflect"
 	"sync"
 	"syscall"
 	"time"
@@ -66,7 +67,9 @@ type MapValue interface {
 
 type MapInfo struct {
 	MapType       MapType
+	MapKey        MapKey
 	KeySize       uint32
+	MapValue      MapValue
 	ValueSize     uint32
 	MaxEntries    uint32
 	Flags         uint32
@@ -124,11 +127,13 @@ type Map struct {
 }
 
 // NewMap creates a new Map instance - object representing a BPF map
-func NewMap(name string, mapType MapType, keySize int, valueSize int, maxEntries int, flags uint32, innerID uint32, dumpParser DumpParser) *Map {
+func NewMap(name string, mapType MapType, mapKey MapKey, keySize int, mapValue MapValue, valueSize int, maxEntries int, flags uint32, innerID uint32, dumpParser DumpParser) *Map {
 	m := &Map{
 		MapInfo: MapInfo{
 			MapType:       mapType,
+			MapKey:        mapKey,
 			KeySize:       uint32(keySize),
+			MapValue:      mapValue,
 			ValueSize:     uint32(valueSize),
 			MaxEntries:    uint32(maxEntries),
 			Flags:         flags,
@@ -237,10 +242,10 @@ func (m *Map) UnpinIfExists() error {
 // DeepEquals compares the current map against another map to see that the
 // attributes of the two maps are the same.
 func (m *Map) DeepEquals(other *Map) bool {
-	return m.MapInfo == other.MapInfo &&
-		m.name == other.name &&
+	return m.name == other.name &&
 		m.path == other.path &&
-		m.NonPersistent == other.NonPersistent
+		m.NonPersistent == other.NonPersistent &&
+		reflect.DeepEqual(m.MapInfo, other.MapInfo)
 }
 
 func (m *Map) controllerName() string {
@@ -287,6 +292,11 @@ func GetMapInfo(pid int, fd int) (*MapInfo, error) {
 	return info, nil
 }
 
+// OpenMap opens the given bpf map and generates the Map info based in the
+// information stored in the bpf map.
+// *Warning*: Calling this function requires the caller to properly setup
+// the MapInfo.MapKey and MapInfo.MapValues fields as those structures are not
+// stored in the bpf map.
 func OpenMap(name string) (*Map, error) {
 	// Expand path if needed
 	if !path.IsAbs(name) {
@@ -485,13 +495,15 @@ func (m *Map) Reopen() error {
 	return m.Open()
 }
 
-type DumpParser func(key []byte, value []byte) (MapKey, MapValue, error)
+type DumpParser func(key []byte, value []byte, mapKey MapKey, mapValue MapValue) (MapKey, MapValue, error)
 type DumpCallback func(key MapKey, value MapValue)
 type MapValidator func(path string) (bool, error)
 
 // DumpWithCallback iterates over the Map and calls the given callback
 // function on each iteration. That callback function is receiving the
-// actual key and value.
+// actual key and value. The callback function should consider creating a
+// deepcopy of the key and value on between each iterations to avoid memory
+// corruption.
 func (m *Map) DumpWithCallback(cb DumpCallback) error {
 	m.lock.RLock()
 	defer m.lock.RUnlock()
@@ -503,6 +515,9 @@ func (m *Map) DumpWithCallback(cb DumpCallback) error {
 	if err := m.Open(); err != nil {
 		return err
 	}
+
+	mk := m.MapKey.DeepCopyMapKey()
+	mv := m.MapValue.DeepCopyMapValue()
 
 	for {
 		err := GetNextKey(
@@ -525,13 +540,13 @@ func (m *Map) DumpWithCallback(cb DumpCallback) error {
 			return err
 		}
 
-		k, v, err := m.dumpParser(nextKey, value)
+		mk, mv, err = m.dumpParser(nextKey, value, mk, mv)
 		if err != nil {
 			return err
 		}
 
 		if cb != nil {
-			cb(k, v)
+			cb(mk, mv)
 		}
 
 		copy(key, nextKey)
@@ -588,6 +603,9 @@ func (m *Map) DumpReliablyWithCallback(cb DumpCallback, stats *DumpStats) error 
 		return nil
 	}
 
+	mk := m.MapKey.DeepCopyMapKey()
+	mv := m.MapValue.DeepCopyMapValue()
+
 	for stats.Lookup = 1; stats.Lookup <= stats.MaxEntries; stats.Lookup++ {
 		// currentKey was returned by GetNextKey() so we know it existed in the map, but it may have been
 		// deleted by a concurrent map operation. If currentKey is no longer in the map, nextKey will be
@@ -615,14 +633,14 @@ func (m *Map) DumpReliablyWithCallback(cb DumpCallback, stats *DumpStats) error 
 			continue
 		}
 
-		k, v, err := m.dumpParser(currentKey, value)
+		mk, mv, err = m.dumpParser(currentKey, value, mk, mv)
 		if err != nil {
 			stats.Interrupted++
 			return err
 		}
 
 		if cb != nil {
-			cb(k, v)
+			cb(mk, mv)
 		}
 
 		if nextKeyValid != nil {
@@ -643,6 +661,7 @@ func (m *Map) DumpReliablyWithCallback(cb DumpCallback, stats *DumpStats) error 
 // data stored in BPF map.
 func (m *Map) Dump(hash map[string][]string) error {
 	callback := func(key MapKey, value MapValue) {
+		// No need to deep copy since we are creating strings.
 		hash[key.String()] = append(hash[key.String()], value.String())
 	}
 
@@ -812,6 +831,9 @@ func (m *Map) DeleteAll() error {
 		return err
 	}
 
+	mk := m.MapKey.DeepCopyMapKey()
+	mv := m.MapValue.DeepCopyMapValue()
+
 	for {
 		err := GetNextKey(
 			m.fd,
@@ -825,9 +847,9 @@ func (m *Map) DeleteAll() error {
 
 		err = DeleteElement(m.fd, unsafe.Pointer(&nextKey[0]))
 
-		k, _, err2 := m.dumpParser(nextKey, []byte{})
+		mk, _, err2 := m.dumpParser(nextKey, []byte{}, mk, mv)
 		if err2 == nil {
-			m.deleteCacheEntry(k, err)
+			m.deleteCacheEntry(mk, err)
 		} else {
 			log.WithError(err2).Warningf("Unable to correlate iteration key %v with cache entry. Inconsistent cache.", nextKey)
 		}
@@ -856,23 +878,23 @@ func (m *Map) GetNextKey(key MapKey, nextKey MapKey) error {
 }
 
 // ConvertKeyValue converts key and value from bytes to given Golang struct pointers.
-func ConvertKeyValue(bKey []byte, bValue []byte, key interface{}, value interface{}) error {
+func ConvertKeyValue(bKey []byte, bValue []byte, key MapKey, value MapValue) (MapKey, MapValue, error) {
 	keyBuf := bytes.NewBuffer(bKey)
 	valueBuf := bytes.NewBuffer(bValue)
 
 	if len(bKey) > 0 {
 		if err := binary.Read(keyBuf, byteorder.Native, key); err != nil {
-			return fmt.Errorf("Unable to convert key: %s", err)
+			return nil, nil, fmt.Errorf("Unable to convert key: %s", err)
 		}
 	}
 
 	if len(bValue) > 0 {
 		if err := binary.Read(valueBuf, byteorder.Native, value); err != nil {
-			return fmt.Errorf("Unable to convert value: %s", err)
+			return nil, nil, fmt.Errorf("Unable to convert value: %s", err)
 		}
 	}
 
-	return nil
+	return key, value, nil
 }
 
 // GetModel returns a BPF map in the representation served via the API

pkg/bpf/map_linux_test.go
@@ -48,15 +48,6 @@ func (v *TestValue) String() string              { return fmt.Sprintf("value=%d"
 func (v *TestValue) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(v) }
 func (v *TestValue) DeepCopyMapValue() MapValue  { return &TestValue{v.Value} }
 
-func DumpParserFunc(key []byte, value []byte) (MapKey, MapValue, error) {
-	k, v := TestKey{}, TestValue{}
-
-	if err := ConvertKeyValue(key, value, &k, &v); err != nil {
-		return nil, nil, err
-	}
-	return &k, &v, nil
-}
-
 var _ = Suite(&BPFPrivilegedTestSuite{})
 
 var (
@@ -64,12 +55,15 @@ var (
 
 	testMap = NewMap("cilium_test",
 		MapTypeHash,
+		&TestKey{},
 		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
 		int(unsafe.Sizeof(TestValue{})),
 		maxEntries,
 		BPF_F_NO_PREALLOC,
 		0,
-		DumpParserFunc).WithCache()
+		ConvertKeyValue,
+	).WithCache()
 )
 
 func runTests(m *testing.M) (int, error) {
@@ -99,13 +93,21 @@ func TestMain(m *testing.M) {
 func (s *BPFPrivilegedTestSuite) TestGetMapInfo(c *C) {
 	mi, err := GetMapInfo(os.Getpid(), testMap.GetFd())
 	c.Assert(err, IsNil)
+
+	// Check OpenMap warning section
+	testMap.MapKey = nil
+	testMap.MapValue = nil
+	defer func() {
+		testMap.MapKey = &TestKey{}
+		testMap.MapValue = &TestValue{}
+	}()
 	c.Assert(&testMap.MapInfo, checker.DeepEquals, mi)
 }
 
 func (s *BPFPrivilegedTestSuite) TestOpen(c *C) {
 	// Ensure that os.IsNotExist() can be used with Map.Open()
 	noSuchMap := NewMap("cilium_test_no_exist",
-		MapTypeHash, 4, 4, maxEntries, 0, 0, nil)
+		MapTypeHash, &TestKey{}, 4, &TestValue{}, 4, maxEntries, 0, 0, nil)
 	err := noSuchMap.Open()
 	c.Assert(os.IsNotExist(err), Equals, true)
 	c.Assert(err, ErrorMatches, ".*cilium_test_no_exist.*")
@@ -113,12 +115,14 @@ func (s *BPFPrivilegedTestSuite) TestOpen(c *C) {
 	// existingMap is the same as testMap. Opening should succeed.
 	existingMap := NewMap("cilium_test",
 		MapTypeHash,
+		&TestKey{},
 		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
 		int(unsafe.Sizeof(TestValue{})),
 		maxEntries,
 		BPF_F_NO_PREALLOC,
 		0,
-		DumpParserFunc).WithCache()
+		ConvertKeyValue).WithCache()
 	err = existingMap.Open()
 	c.Check(err, IsNil)      // Avoid assert to ensure Close() is called below.
 	err = existingMap.Open() // Reopen should be no-op.
@@ -133,21 +137,31 @@ func (s *BPFPrivilegedTestSuite) TestOpenMap(c *C) {
 	c.Assert(openedMap, IsNil)
 
 	openedMap, err = OpenMap("cilium_test")
+	c.Assert(err, IsNil)
+
+	// Check OpenMap warning section
+	testMap.MapKey = nil
+	testMap.MapValue = nil
+	defer func() {
+		testMap.MapKey = &TestKey{}
+		testMap.MapValue = &TestValue{}
+	}()
 	noDiff := openedMap.DeepEquals(testMap)
 	c.Assert(noDiff, Equals, true)
-	c.Assert(err, IsNil)
 }
 
 func (s *BPFPrivilegedTestSuite) TestOpenOrCreate(c *C) {
 	// existingMap is the same as testMap. OpenOrCreate should skip recreation.
 	existingMap := NewMap("cilium_test",
 		MapTypeHash,
+		&TestKey{},
 		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
 		int(unsafe.Sizeof(TestValue{})),
 		maxEntries,
 		BPF_F_NO_PREALLOC,
 		0,
-		DumpParserFunc).WithCache()
+		ConvertKeyValue).WithCache()
 	isNew, err := existingMap.OpenOrCreate()
 	c.Assert(err, IsNil)
 	c.Assert(isNew, Equals, false)
@@ -156,12 +170,14 @@ func (s *BPFPrivilegedTestSuite) TestOpenOrCreate(c *C) {
 	EnableMapPreAllocation() // prealloc on/off is controllable in HASH map case.
 	preallocMap := NewMap("cilium_test",
 		MapTypeHash,
+		&TestKey{},
 		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
 		int(unsafe.Sizeof(TestValue{})),
 		maxEntries,
 		0,
 		0,
-		DumpParserFunc).WithCache()
+		ConvertKeyValue).WithCache()
 	isNew, err = preallocMap.OpenOrCreate()
 	defer preallocMap.Close()
 	c.Assert(err, IsNil)
@@ -177,12 +193,14 @@ func (s *BPFPrivilegedTestSuite) TestOpenOrCreate(c *C) {
 func (s *BPFPrivilegedTestSuite) TestOpenParallel(c *C) {
 	parallelMap := NewMap("cilium_test",
 		MapTypeHash,
+		&TestKey{},
 		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
 		int(unsafe.Sizeof(TestValue{})),
 		maxEntries,
 		BPF_F_NO_PREALLOC,
 		0,
-		DumpParserFunc).WithCache()
+		ConvertKeyValue).WithCache()
 	isNew, err := parallelMap.OpenParallel()
 	defer parallelMap.Close()
 	c.Assert(err, IsNil)
@@ -192,6 +210,7 @@ func (s *BPFPrivilegedTestSuite) TestOpenParallel(c *C) {
 	c.Assert(isNew, Equals, false)
 	c.Assert(err, Not(IsNil))
 
+	// Check OpenMap warning section
 	noDiff := parallelMap.DeepEquals(testMap)
 	c.Assert(noDiff, Equals, true)
 
@@ -226,12 +245,14 @@ func (s *BPFPrivilegedTestSuite) TestBasicManipulation(c *C) {
 	// existingMap is the same as testMap. Opening should succeed.
 	existingMap := NewMap("cilium_test",
 		MapTypeHash,
+		&TestKey{},
 		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
 		int(unsafe.Sizeof(TestValue{})),
 		maxEntries,
 		BPF_F_NO_PREALLOC,
 		0,
-		DumpParserFunc).WithCache()
+		ConvertKeyValue).WithCache()
 	err := existingMap.Open()
 	defer existingMap.Close()
 	c.Assert(err, IsNil)
@@ -321,7 +342,7 @@ func (s *BPFPrivilegedTestSuite) TestDump(c *C) {
 
 	dump3 := map[string][]string{}
 	noSuchMap := NewMap("cilium_test_no_exist",
-		MapTypeHash, 4, 4, maxEntries, 0, 0, nil)
+		MapTypeHash, &TestKey{}, 4, &TestValue{}, 4, maxEntries, 0, 0, nil)
 	err = noSuchMap.DumpIfExists(dump3)
 	c.Assert(err, IsNil)
 	c.Assert(len(dump3), Equals, 0)
@@ -342,12 +363,14 @@ func (s *BPFPrivilegedTestSuite) TestCheckAndUpgrade(c *C) {
 	// so we setup and use another map.
 	upgradeMap := NewMap("cilium_test_upgrade",
 		MapTypeHash,
+		&TestKey{},
 		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
 		int(unsafe.Sizeof(TestValue{})),
 		maxEntries,
 		BPF_F_NO_PREALLOC,
 		0,
-		DumpParserFunc).WithCache()
+		ConvertKeyValue).WithCache()
 	_, err := upgradeMap.OpenOrCreate()
 	c.Assert(err, IsNil)
 	defer func() {
@@ -364,12 +387,14 @@ func (s *BPFPrivilegedTestSuite) TestCheckAndUpgrade(c *C) {
 	EnableMapPreAllocation()
 	preallocMap := NewMap("cilium_test_upgrade",
 		MapTypeHash,
+		&TestKey{},
 		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
 		int(unsafe.Sizeof(TestValue{})),
 		maxEntries,
 		0,
 		0,
-		DumpParserFunc).WithCache()
+		ConvertKeyValue).WithCache()
 	upgrade = upgradeMap.CheckAndUpgrade(&preallocMap.MapInfo)
 	c.Assert(upgrade, Equals, true)
 	DisableMapPreAllocation()
@@ -379,12 +404,14 @@ func (s *BPFPrivilegedTestSuite) TestUnpin(c *C) {
 	var exist bool
 	unpinMap := NewMap("cilium_test_unpin",
 		MapTypeHash,
+		&TestKey{},
 		int(unsafe.Sizeof(TestKey{})),
+		&TestValue{},
 		int(unsafe.Sizeof(TestValue{})),
 		maxEntries,
 		BPF_F_NO_PREALLOC,
 		0,
-		DumpParserFunc).WithCache()
+		ConvertKeyValue).WithCache()
 	_, err := unpinMap.OpenOrCreate()
 	c.Assert(err, IsNil)
 	exist, err = unpinMap.exist()

commit 3e4edd62971f0e1879abc514eb27c392cd9dff80

pkg/maps: create CtKeyGlobal structures
As Ct{4,6}Key were being passed to the initialization of the BPF Maps
cilium was not correctly dumping the content of the ct global maps as
the Dump method used of for local CT maps.

Fixes: 30bae84876d1 ("pkg/{bpf,datapath,maps}: use same MapKey and MapValue in map iterations")
Signed-off-by: André Martins <andre@cilium.io>

pkg/maps/ctmap/ctmap.go
@@ -94,19 +94,24 @@ const (
 type mapAttributes struct {
 	mapKey     bpf.MapKey
 	keySize    int
+	mapValue   bpf.MapValue
+	valueSize  int
 	maxEntries int
 	parser     bpf.DumpParser
 	bpfDefine  string
 	natMap     *nat.Map
 }
 
-func setupMapInfo(mapType MapType, define string, mapKey bpf.MapKey, keySize, maxEntries int, parser bpf.DumpParser, nat *nat.Map) {
+func setupMapInfo(mapType MapType, define string, mapKey bpf.MapKey, keySize int, maxEntries int, nat *nat.Map) {
 	mapInfo[mapType] = mapAttributes{
-		bpfDefine:  define,
-		mapKey:     mapKey,
-		keySize:    keySize,
+		bpfDefine: define,
+		mapKey:    mapKey,
+		keySize:   keySize,
+		// the value type is CtEntry for all CT maps
+		mapValue:   &CtEntry{},
+		valueSize:  int(unsafe.Sizeof(CtEntry{})),
 		maxEntries: maxEntries,
-		parser:     parser,
+		parser:     bpf.ConvertKeyValue,
 		natMap:     nat,
 	}
 }
@@ -120,31 +125,37 @@ func InitMapInfo(tcpMaxEntries, anyMaxEntries int) {
 	natV4 := natMaps[0]
 	natV6 := natMaps[1]
 
-	mapType := MapTypeIPv4TCPLocal
-	for _, maxEntries := range []int{MapNumEntriesLocal, tcpMaxEntries} {
-		setupMapInfo(MapType(mapType), "CT_MAP_TCP4",
-			&CtKey4{},
-			int(unsafe.Sizeof(CtKey4{})), maxEntries,
-			bpf.ConvertKeyValue, natV4)
-		mapType++
-		setupMapInfo(MapType(mapType), "CT_MAP_TCP6",
-			&CtKey6{},
-			int(unsafe.Sizeof(CtKey6{})), maxEntries,
-			bpf.ConvertKeyValue, natV6)
-		mapType++
-	}
-	for _, maxEntries := range []int{MapNumEntriesLocal, anyMaxEntries} {
-		setupMapInfo(MapType(mapType), "CT_MAP_ANY4",
-			&CtKey4{},
-			int(unsafe.Sizeof(tuple.TupleKey4{})), maxEntries,
-			bpf.ConvertKeyValue, natV4)
-		mapType++
-		setupMapInfo(MapType(mapType), "CT_MAP_ANY6",
-			&CtKey6{},
-			int(unsafe.Sizeof(CtKey6{})), maxEntries,
-			bpf.ConvertKeyValue, natV6)
-		mapType++
-	}
+	setupMapInfo(MapType(MapTypeIPv4TCPLocal), "CT_MAP_TCP4",
+		&CtKey4{}, int(unsafe.Sizeof(CtKey4{})),
+		MapNumEntriesLocal, natV4)
+
+	setupMapInfo(MapType(MapTypeIPv6TCPLocal), "CT_MAP_TCP6",
+		&CtKey6{}, int(unsafe.Sizeof(CtKey6{})),
+		MapNumEntriesLocal, natV6)
+
+	setupMapInfo(MapType(MapTypeIPv4TCPGlobal), "CT_MAP_TCP4",
+		&CtKey4Global{}, int(unsafe.Sizeof(CtKey4Global{})),
+		tcpMaxEntries, natV4)
+
+	setupMapInfo(MapType(MapTypeIPv6TCPGlobal), "CT_MAP_TCP6",
+		&CtKey6Global{}, int(unsafe.Sizeof(CtKey6Global{})),
+		tcpMaxEntries, natV6)
+
+	setupMapInfo(MapType(MapTypeIPv4AnyLocal), "CT_MAP_ANY4",
+		&CtKey4{}, int(unsafe.Sizeof(CtKey4{})),
+		MapNumEntriesLocal, natV4)
+
+	setupMapInfo(MapType(MapTypeIPv6AnyLocal), "CT_MAP_ANY6",
+		&CtKey6{}, int(unsafe.Sizeof(CtKey6{})),
+		MapNumEntriesLocal, natV6)
+
+	setupMapInfo(MapType(MapTypeIPv4AnyGlobal), "CT_MAP_ANY4",
+		&CtKey4Global{}, int(unsafe.Sizeof(CtKey4Global{})),
+		anyMaxEntries, natV4)
+
+	setupMapInfo(MapType(MapTypeIPv6AnyGlobal), "CT_MAP_ANY6",
+		&CtKey6Global{}, int(unsafe.Sizeof(CtKey6Global{})),
+		anyMaxEntries, natV6)
 }
 
 func init() {
@@ -214,8 +225,8 @@ func NewMap(mapName string, mapType MapType) *Map {
 			bpf.MapTypeLRUHash,
 			mapInfo[mapType].mapKey,
 			mapInfo[mapType].keySize,
-			&CtEntry{},
-			int(unsafe.Sizeof(CtEntry{})),
+			mapInfo[mapType].mapValue,
+			mapInfo[mapType].valueSize,
 			mapInfo[mapType].maxEntries,
 			0, 0,
 			mapInfo[mapType].parser,

pkg/maps/ctmap/lookup.go
@@ -26,6 +26,8 @@ import (
 )
 
 // CtKey4 is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
 type CtKey4 struct {
 	tuple.TupleKey4
 }
@@ -41,7 +43,27 @@ func (k *CtKey4) ToNetwork() tuple.TupleKey {
 	return &n
 }
 
+// CtKey4Global is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type CtKey4Global struct {
+	tuple.TupleKey4Global
+}
+
+// NewValue creates a new bpf.MapValue.
+func (k *CtKey4Global) NewValue() bpf.MapValue { return &CtEntry{} }
+
+// ToNetwork converts CtKey4Global ports to network byte order.
+func (k *CtKey4Global) ToNetwork() tuple.TupleKey {
+	n := *k
+	n.SourcePort = byteorder.HostToNetwork(n.SourcePort).(uint16)
+	n.DestPort = byteorder.HostToNetwork(n.DestPort).(uint16)
+	return &n
+}
+
 // CtKey6 is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
 type CtKey6 struct {
 	tuple.TupleKey6
 }
@@ -57,40 +79,78 @@ func (k *CtKey6) ToNetwork() tuple.TupleKey {
 	return &n
 }
 
-func createTupleKey(remoteAddr, localAddr string, proto u8proto.U8proto, ingress bool) (tuple.TupleKey, error) {
+// CtKey6Global is needed to provide CtEntry type to Lookup values
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type CtKey6Global struct {
+	tuple.TupleKey6Global
+}
+
+// NewValue creates a new bpf.MapValue.
+func (k *CtKey6Global) NewValue() bpf.MapValue { return &CtEntry{} }
+
+// ToNetwork converts CtKey6Global ports to network byte order.
+func (k *CtKey6Global) ToNetwork() tuple.TupleKey {
+	n := *k
+	n.SourcePort = byteorder.HostToNetwork(n.SourcePort).(uint16)
+	n.DestPort = byteorder.HostToNetwork(n.DestPort).(uint16)
+	return &n
+}
+
+func createTupleKey(isGlobal bool, remoteAddr, localAddr string, proto u8proto.U8proto, ingress bool) (bpf.MapKey, bool, error) {
 	ip, port, err := net.SplitHostPort(remoteAddr)
 	if err != nil {
-		return nil, fmt.Errorf("invalid remote address '%s': %s", remoteAddr, err)
+		return nil, false, fmt.Errorf("invalid remote address '%s': %s", remoteAddr, err)
 	}
 
 	sIP := net.ParseIP(ip)
 	if sIP == nil {
-		return nil, fmt.Errorf("unable to parse IP %s", ip)
+		return nil, false, fmt.Errorf("unable to parse IP %s", ip)
 	}
 
 	sport, err := strconv.ParseUint(port, 10, 16)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse port string: %s", err)
+		return nil, false, fmt.Errorf("unable to parse port string: %s", err)
 	}
 
 	localIp, localPort, err := net.SplitHostPort(localAddr)
 	if err != nil {
-		return nil, fmt.Errorf("invalid local address '%s': %s", localAddr, err)
+		return nil, false, fmt.Errorf("invalid local address '%s': %s", localAddr, err)
 	}
 
 	dIP := net.ParseIP(localIp)
 	if dIP == nil {
-		return nil, fmt.Errorf("unable to parse IP %s", localIp)
+		return nil, false, fmt.Errorf("unable to parse IP %s", localIp)
 	}
 
 	dport, err := strconv.ParseUint(localPort, 10, 16)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse port string: %s", err)
+		return nil, false, fmt.Errorf("unable to parse port string: %s", err)
 	}
 
 	if sIP.To4() != nil {
+		if isGlobal {
+			key := &CtKey4Global{
+				TupleKey4Global: tuple.TupleKey4Global{
+					TupleKey4: tuple.TupleKey4{
+						SourcePort: uint16(sport),
+						DestPort:   uint16(dport),
+						NextHeader: proto,
+						Flags:      TUPLE_F_OUT,
+					},
+				},
+			}
+			// CTmap has the addresses in the reverse order w.r.t. the original direction
+			copy(key.SourceAddr[:], dIP.To4())
+			copy(key.DestAddr[:], sIP.To4())
+			if ingress {
+				key.Flags = TUPLE_F_IN
+			}
+			return key.ToNetwork(), true, nil
+		}
+
 		key := &CtKey4{
-			tuple.TupleKey4{
+			TupleKey4: tuple.TupleKey4{
 				SourcePort: uint16(sport),
 				DestPort:   uint16(dport),
 				NextHeader: proto,
@@ -103,11 +163,31 @@ func createTupleKey(remoteAddr, localAddr string, proto u8proto.U8proto, ingress
 		if ingress {
 			key.Flags = TUPLE_F_IN
 		}
-		return key.ToNetwork(), nil
+		return key.ToNetwork(), true, nil
+	}
+
+	if isGlobal {
+		key := &CtKey6Global{
+			TupleKey6Global: tuple.TupleKey6Global{
+				TupleKey6: tuple.TupleKey6{
+					SourcePort: uint16(sport),
+					DestPort:   uint16(dport),
+					NextHeader: proto,
+					Flags:      TUPLE_F_OUT,
+				},
+			},
+		}
+		// CTmap has the addresses in the reverse order w.r.t. the original direction
+		copy(key.SourceAddr[:], dIP.To16())
+		copy(key.DestAddr[:], sIP.To16())
+		if ingress {
+			key.Flags = TUPLE_F_IN
+		}
+		return key.ToNetwork(), false, nil
 	}
 
 	key := &CtKey6{
-		tuple.TupleKey6{
+		TupleKey6: tuple.TupleKey6{
 			SourcePort: uint16(sport),
 			DestPort:   uint16(dport),
 			NextHeader: proto,
@@ -120,7 +200,7 @@ func createTupleKey(remoteAddr, localAddr string, proto u8proto.U8proto, ingress
 	if ingress {
 		key.Flags = TUPLE_F_IN
 	}
-	return key.ToNetwork(), nil
+	return key.ToNetwork(), false, nil
 }
 
 func getMapName(mapname string, ipv4 bool, proto u8proto.U8proto) string {
@@ -145,12 +225,12 @@ func getMapName(mapname string, ipv4 bool, proto u8proto.U8proto) string {
 // 'epname' is a 5-digit represenation of the endpoint ID if local maps
 // are to be used, or "global" if global maps should be used.
 func Lookup(epname string, remoteAddr, localAddr string, proto u8proto.U8proto, ingress bool) (*CtEntry, error) {
-	key, err := createTupleKey(remoteAddr, localAddr, proto, ingress)
+	isGlobal := epname == "global"
+
+	key, ipv4, err := createTupleKey(isGlobal, remoteAddr, localAddr, proto, ingress)
 	if err != nil {
 		return nil, err
 	}
-
-	_, ipv4 := key.(*CtKey4)
 
 	mapname := getMapName(epname, ipv4, proto)
 
@@ -161,10 +241,18 @@ func Lookup(epname string, remoteAddr, localAddr string, proto u8proto.U8proto, 
 		if err != nil {
 			return nil, fmt.Errorf("Can not open CT map %s: %s", mapname, err)
 		}
-		if ipv4 {
-			m.MapKey = &CtKey4{}
+		if isGlobal {
+			if ipv4 {
+				m.MapKey = &CtKey4Global{}
+			} else {
+				m.MapKey = &CtKey6Global{}
+			}
 		} else {
-			m.MapKey = &CtKey6{}
+			if ipv4 {
+				m.MapKey = &CtKey4{}
+			} else {
+				m.MapKey = &CtKey6{}
+			}
 		}
 		m.MapValue = &CtEntry{}
 	}

pkg/maps/ctmap/zz_generated.deepcopy.go
@@ -45,3 +45,103 @@ func (in *CtEntry) DeepCopyMapValue() bpf.MapValue {
 	}
 	return nil
 }
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CtKey4) DeepCopyInto(out *CtKey4) {
+	*out = *in
+	in.TupleKey4.DeepCopyInto(&out.TupleKey4)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CtKey4.
+func (in *CtKey4) DeepCopy() *CtKey4 {
+	if in == nil {
+		return nil
+	}
+	out := new(CtKey4)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *CtKey4) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CtKey4Global) DeepCopyInto(out *CtKey4Global) {
+	*out = *in
+	in.TupleKey4Global.DeepCopyInto(&out.TupleKey4Global)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CtKey4Global.
+func (in *CtKey4Global) DeepCopy() *CtKey4Global {
+	if in == nil {
+		return nil
+	}
+	out := new(CtKey4Global)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *CtKey4Global) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CtKey6) DeepCopyInto(out *CtKey6) {
+	*out = *in
+	in.TupleKey6.DeepCopyInto(&out.TupleKey6)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CtKey6.
+func (in *CtKey6) DeepCopy() *CtKey6 {
+	if in == nil {
+		return nil
+	}
+	out := new(CtKey6)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *CtKey6) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *CtKey6Global) DeepCopyInto(out *CtKey6Global) {
+	*out = *in
+	in.TupleKey6Global.DeepCopyInto(&out.TupleKey6Global)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new CtKey6Global.
+func (in *CtKey6Global) DeepCopy() *CtKey6Global {
+	if in == nil {
+		return nil
+	}
+	out := new(CtKey6Global)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *CtKey6Global) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}