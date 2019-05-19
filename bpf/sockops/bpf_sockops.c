g0odi3z2j/cilium

commit cbb99ca927d7238c52c40bacc732d9eb9d7e7ae8

cilium: add bpf cgroup2 sockops program
Add initial sock_ops BPF program which will run on the cgroupv2
BPF mount and monitor socket operations. This will be used to
add sockets to a cilium sockmap.

The following criteria must be met to add a socket to the sockmap
which will later use bpf_redir.c to do redirect.

 - Must not be a service dst. This is just a limitation in
   the code at the moment we can add support after initial
   patches.
 - Must be allowed by the policy, policy_sk_egress()
 - Must be a local dst endpoint either (a) proxy or (b) endpoint

IPv4 supported in this patch set IPv6 will follow shortly.

I put this code into its own directory 'sockops' to make it
clear where sockops/sk_msg code exists and where traditional
skb based BPF code lives.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bpf/sockops/bpf_sockops.c
@@ -1,0 +1,136 @@
+/*
+ *  Copyright (C) 2018 Authors of Cilium
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#define SOCKMAP 1
+#define LB_L3
+
+#include <node_config.h>
+#include <bpf/api.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <sys/socket.h>
+
+#include "../lib/utils.h"
+#include "../lib/common.h"
+#include "../lib/maps.h"
+#include "../lib/lb.h"
+#include "../lib/eps.h"
+#include "../lib/events.h"
+#include "../lib/policy.h"
+
+#include "bpf_sockops.h"
+
+static inline void bpf_sock_ops_ipv4(struct bpf_sock_ops *skops)
+{
+	__u32 dip4, dport, dstID = 0;
+	struct endpoint_info *exists;
+	struct lb4_key lb4_key = {};
+	struct sock_key key = {};
+	struct lb4_service *svc;
+	int verdict;
+
+	sk_extract4_key(skops, &key);
+	sk_lb4_key(&lb4_key, &key);
+
+	/* If endpoint a service use L4/L3 stack for now. These can be
+	 * pulled in as needed.
+	 */
+	svc = __lb4_lookup_service(&lb4_key);
+	if (svc)
+		return;
+
+	/* Policy lookup required to learn proxy port */
+	if (1) {
+		struct remote_endpoint_info *info;
+
+		info = lookup_ip4_remote_endpoint(key.dip4);
+		if (info != NULL && info->sec_label)
+			dstID = info->sec_label;
+		else
+			dstID = WORLD_ID;
+	}
+
+	verdict = policy_sk_egress(dstID, key.sip4, key.dport);
+	if (redirect_to_proxy(verdict)) {
+		__be32 host_ip = IPV4_GATEWAY;
+
+		key.dip4 = key.sip4;
+		key.dport = key.sport;
+		key.sip4 = host_ip;
+		key.sport = verdict;
+
+		sock_hash_update(skops, &SOCK_OPS_MAP, &key, BPF_ANY);
+		return;
+	}
+
+	/* Lookup IPv4 address, this will return a match if:
+	 * - The destination IP address belongs to the local endpoint manage
+	 *   by Cilium.
+	 * - The destination IP address is an IP address associated with the
+	 *   host itself.
+	 * Then because these are local IPs that have passed LB/Policy/NAT
+	 * blocks redirect directly to socket.
+	 */
+	exists = __lookup_ip4_endpoint(key.dip4);
+	if (!exists)
+		return;
+
+	dip4 = key.dip4;
+	dport = key.dport;
+	key.dip4 = key.sip4;
+	key.dport = key.sport;
+	key.sip4 = dip4;
+	key.sport = dport;
+
+	sock_hash_update(skops, &SOCK_OPS_MAP, &key, BPF_ANY);
+}
+
+static inline void bpf_sock_ops_ipv6(struct bpf_sock_ops *skops)
+{
+	if (skops->remote_ip4)
+		bpf_sock_ops_ipv4(skops);
+}
+
+__section("sockops")
+int bpf_sockmap(struct bpf_sock_ops *skops)
+{
+	__u32 family, op;
+
+	family = skops->family;
+	op = skops->op;
+
+	switch (op) {
+	case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
+	case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
+		if (family == AF_INET6)
+			bpf_sock_ops_ipv6(skops);
+		else if (family == AF_INET)
+			bpf_sock_ops_ipv4(skops);
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
+
+BPF_LICENSE("GPL");
+int _version __section("version") = 1;

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=11d88809b61e9a7f6093178c7d9959ebbcc2c721
-BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 
+GO_BINDATA_SHA1SUM=69edf2bfa118910734341226a921090d4023b021
+BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

bpf/Makefile
@@ -1,6 +1,8 @@
 include ../Makefile.defs
 
 FLAGS := -Iinclude -I. -D__NR_CPUS__=$(shell nproc) -O2 -g
+SUBDIRS = sockops
+.PHONY: subdirs $(SUBDIRS)
 
 CLANG_FLAGS :=  ${FLAGS} -target bpf -emit-llvm
 # eBPF verifier enforces unaligned access checks where necessary, so don't
@@ -19,7 +21,7 @@ LLC    ?= llc
 HOSTCC ?= gcc
 
 ifeq ("$(PKG_BUILD)","")
-all: $(BPF) $(TARGET)
+all: $(BPF) $(TARGET) $(SUBDIRS)
 
 %.o: %.c $(LIB)
 	@$(ECHO_CC)
@@ -67,6 +69,10 @@ bpf_lxc.o: bpf_lxc.c $(LIB)
 	@$(ECHO_CC)
 	$(QUIET) ${CLANG} ${CLANG_FLAGS} -c $< -o $(patsubst %.o,%.ll,$@)
 	$(QUIET) ${LLC} ${LLC_FLAGS} -o $@ $(patsubst %.o,%.ll,$@)
+
+subdirs: $(SUBDIRS)
+$(SUBDIRS):
+	$(MAKE) -C $@
 else
 all: $(TARGET)
 endif

bpf/sockops/Makefile
@@ -1,0 +1,44 @@
+
+FLAGS := -I../include -I. -I.. -D__NR_CPUS__=$(shell nproc) -O2 -g
+CLANG_FLAGS :=  ${FLAGS} -target bpf -emit-llvm
+CLANG_FLAGS += -Wall -Werror -Wno-address-of-packed-member -Wno-unknown-warning-option
+LLC_FLAGS   := -march=bpf -mcpu=probe -mattr=dwarfris -filetype=obj
+
+
+SOCKOPS_OPTIONS = \
+	 -DSKIP_DEBUG \
+	 -DHAVE_LPM_MAP_TYPE \
+	 -DHAVE_LRU_MAP_TYPE
+
+BPF = bpf_sockops.o
+LIB := $(shell find ../ -name '*.h')
+
+CLANG  ?= clang
+LLC    ?= llc
+HOSTCC ?= gcc
+
+all: $(BPF)
+
+bpf_sockops.o: bpf_sockops.c $(LIB)
+	@$(ECHO_CC)
+	$(QUIET) ${CLANG} ${CLANG_FLAGS} ${SOCKOPS_OPTIONS} -c $< -o $(patsubst %.o,%.ll,$@)
+	$(QUIET) ${LLC} ${LLC_FLAGS} -o $@ $(patsubst %.o,%.ll,$@)
+
+check:
+	@$(ECHO_CHECK) bpf/*.c
+	$(QUIET) spatch --sp-file ../contrib/coccinelle/null.cocci --dir . --include-headers
+	$(QUIET) sparse -Wsparse-all ${FLAGS} *.c
+	$(QUIET) $(CLANG) ${CLANG_FLAGS} --analyze *.c
+
+preprocess: $(LIB)
+	$(QUIET) $(foreach TARGET,$(shell find ./ -name 'bpf_*.c'), \
+		echo "  GEN   ${TARGET}.generated"; \
+		${CLANG} $(FLAGS) -E -target bpf -c ${TARGET} -o ${TARGET}.generated; )
+
+install:
+	$(INSTALL) -m 0755 $(TARGET) $(DESTDIR)$(BINDIR)
+
+clean:
+	@$(ECHO_CLEAN) $(notdir $(shell pwd))
+	$(QUIET)rm -fr *.o *.ll *.generated
+	$(QUIET)rm -f $(TARGET)

bpf/sockops/bpf_sockops.h
@@ -1,0 +1,89 @@
+/*
+ *  Copyright (C) 2018 Authors of Cilium
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "sockops_config.h"
+
+/* Structure representing an L7 sock */
+struct sock_key {
+	union {
+		struct {
+			__u32		sip4;
+			__u32		pad1;
+			__u32		pad2;
+			__u32		pad3;
+		};
+		union v6addr	sip6;
+	};
+	union {
+		struct {
+			__u32		dip4;
+			__u32		pad4;
+			__u32		pad5;
+			__u32		pad6;
+		};
+		union v6addr	dip6;
+	};
+	__u8 family;
+	__u8 pad7;
+	__u16 pad8;
+	__u32 sport;
+	__u32 dport;
+} __attribute__((packed));
+
+struct bpf_elf_map __section_maps SOCK_OPS_MAP = {
+	.type           = BPF_MAP_TYPE_SOCKHASH,
+	.size_key       = sizeof(struct sock_key),
+	.size_value     = sizeof(int),
+	.pinning        = PIN_GLOBAL_NS,
+	.max_elem       = SOCKOPS_MAP_SIZE,
+};
+
+static inline void sk_extract4_key(struct bpf_sock_ops *ops,
+				   struct sock_key *key)
+{
+	key->dip4 = ops->remote_ip4;
+	key->sip4 = ops->local_ip4;
+	key->family = ENDPOINT_KEY_IPV4;
+
+	key->sport = (bpf_ntohl(ops->local_port) >> 16);
+	key->dport = ops->remote_port >> 16;
+}
+
+static inline void sk_msg_extract4_key(struct sk_msg_md *msg,
+				       struct sock_key *key)
+{
+	key->dip4 = msg->remote_ip4;
+	key->sip4 = msg->local_ip4;
+	key->family = ENDPOINT_KEY_IPV4;
+
+	key->sport = (bpf_ntohl(msg->local_port) >> 16);
+	key->dport = msg->remote_port >> 16;
+}
+
+static inline void sk_lb4_key(struct lb4_key *lb4, struct sock_key *key)
+{
+	/* SK MSG is always egress, so use daddr */
+	lb4->address = key->dip4;
+	lb4->dport = key->dport;
+	lb4->slave = 0;
+}
+
+static inline bool redirect_to_proxy(int verdict)
+{
+	return verdict > 0;
+}

bpf/sockops/sockops_config.h
@@ -1,0 +1,19 @@
+/*
+ *  Copyright (C) 2018 Authors of Cilium
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#define SOCK_OPS_MAP sock_ops_map
+#define SOCKOPS_MAP_SIZE 65535

commit 4b19ba4736f7df71cbbed28f66e0433379f427e3

cilium: bpf sockmap, pull LB define from compile stage
Do not hard code LB enabled instead pull in defines from compilation.

Fixes: cbb99ca927d72 ("cilium: add bpf cgroup2 sockops program")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bpf/sockops/bpf_sockops.c
@@ -16,7 +16,6 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #define SOCKMAP 1
-#define LB_L3
 
 #include <node_config.h>
 #include <bpf/api.h>

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=67eb20f515ef54e0312a9feeed21af92f93a7c9d
+GO_BINDATA_SHA1SUM=77fca59c3c09127ec880f6e257983c42d4e6ca4b
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 