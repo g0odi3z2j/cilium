g0odi3z2j/cilium

commit 82c499d4f09a5866218805bef55003191dc89524

kvstore: Add UpdateIfDifferent()
Add a variant of Update() that performs a Get() and invokes Update if the key
does not exist or if the value is different.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/kvstore/consul.go
@@ -15,6 +15,7 @@
 package kvstore
 
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
 	"errors"
@@ -471,6 +472,19 @@ func (c *consulClient) Update(ctx context.Context, key string, value []byte, lea
 	_, err := c.KV().Put(k, opts.WithContext(ctx))
 	increaseMetric(key, metricSet, "Update", duration.EndError(err).Total(), err)
 	return err
+}
+
+func (c *consulClient) UpdateIfDifferent(ctx context.Context, key string, value []byte, lease bool) (bool, error) {
+	existingValue, err := c.Get(key)
+	// On error, attempt update blindly
+	if err == nil {
+		// Value already exists, do not update
+		if bytes.Equal(existingValue, value) {
+			return false, nil
+		}
+	}
+
+	return true, c.Update(ctx, key, value, lease)
 }
 
 // CreateOnly creates a key with the value and will fail if the key already exists

pkg/kvstore/etcd.go
@@ -15,6 +15,7 @@
 package kvstore
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/url"
@@ -817,6 +818,25 @@ func (e *etcdClient) Update(ctx context.Context, key string, value []byte, lease
 	_, err := e.client.Put(ctx, key, string(value))
 	increaseMetric(key, metricSet, "Update", duration.EndError(err).Total(), err)
 	return Hint(err)
+}
+
+func (e *etcdClient) UpdateIfDifferent(ctx context.Context, key string, value []byte, lease bool) (bool, error) {
+	select {
+	case <-e.firstSession:
+	case <-ctx.Done():
+		return false, fmt.Errorf("update cancelled via context: %s", ctx.Err())
+	}
+
+	existingValue, err := e.Get(key)
+	// On error, attempt update blindly
+	if err == nil {
+		// Value already exists, do not update
+		if bytes.Equal(existingValue, value) {
+			return false, nil
+		}
+	}
+
+	return true, e.Update(ctx, key, value, lease)
 }
 
 // CreateOnly creates a key with the value and will fail if the key already exists

pkg/kvstore/backend.go
@@ -163,6 +163,9 @@ type BackendOperations interface {
 	// Update atomically creates a key or fails if it already exists
 	Update(ctx context.Context, key string, value []byte, lease bool) error
 
+	// UpdateIfDifferent updates a key if the value is different
+	UpdateIfDifferent(ctx context.Context, key string, value []byte, lease bool) (bool, error)
+
 	// CreateOnly atomically creates a key or fails if it already exists
 	CreateOnly(ctx context.Context, key string, value []byte, lease bool) (bool, error)
 

pkg/kvstore/kvstore.go
@@ -76,6 +76,18 @@ func Update(ctx context.Context, key string, value []byte, lease bool) error {
 	return err
 }
 
+// UpdateIfDifferent updates a key if the value is different
+func UpdateIfDifferent(ctx context.Context, key string, value []byte, lease bool) (bool, error) {
+	recreated, err := Client().UpdateIfDifferent(ctx, key, value, lease)
+	Trace("Update", err, logrus.Fields{
+		fieldKey:         key,
+		fieldValue:       string(value),
+		fieldAttachLease: lease,
+		"recreated":      recreated,
+	})
+	return recreated, err
+}
+
 // CreateIfExists creates a key with the value only if key condKey exists
 func CreateIfExists(condKey, key string, value []byte, lease bool) error {
 	err := Client().CreateIfExists(condKey, key, value, lease)

commit 013107c26ad318a323127cad53209ec83d48b64b

pkg/kvstore: perform update if value or lease are different
When UpdateIfDifferent was introduced, the lease information of the key
was not taken into account. When a cilium-agent was restarted, its lease
would also expire as well, making its keys being deleted after the
lease TTL (roughly 15 minutes) expired. As UpdateIfDifferent was not build
accounting for the fact that leases would expire, those leased keys
would never be updated with the new lease as the only attribute being
compared if it was equal or not was the value of the key and it should
have been the value *and* the lease information of that same key.

Fixes: 82c499d4f09a ("kvstore: Add UpdateIfDifferent()")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/consul.go
@@ -496,11 +496,14 @@ func (c *consulClient) Update(ctx context.Context, key string, value []byte, lea
 }
 
 func (c *consulClient) UpdateIfDifferent(ctx context.Context, key string, value []byte, lease bool) (bool, error) {
-	existingValue, err := c.Get(key)
+	duration := spanstat.Start()
+	getR, _, err := c.KV().Get(key, nil)
+	increaseMetric(key, metricRead, "Get", duration.EndError(err).Total(), err)
 	// On error, attempt update blindly
-	if err == nil {
-		// Value already exists, do not update
-		if bytes.Equal(existingValue, value) {
+	if err == nil && getR != nil {
+		// if lease is different and value is not equal then update.
+		if getR.Session == c.lease &&
+			bytes.Equal(getR.Value, value) {
 			return false, nil
 		}
 	}

pkg/kvstore/etcd.go
@@ -873,11 +873,18 @@ func (e *etcdClient) UpdateIfDifferent(ctx context.Context, key string, value []
 		return false, fmt.Errorf("update cancelled via context: %s", ctx.Err())
 	}
 
-	existingValue, err := e.Get(key)
+	duration := spanstat.Start()
+	e.limiter.Wait(ctx)
+	getR, err := e.client.Get(ctx, key)
+	increaseMetric(key, metricRead, "Get", duration.EndError(err).Total(), err)
 	// On error, attempt update blindly
-	if err == nil {
-		// Value already exists, do not update
-		if bytes.Equal(existingValue, value) {
+	if err == nil && getR.Count != 0 {
+		e.RWMutex.RLock()
+		leaseID := e.session.Lease()
+		e.RWMutex.RUnlock()
+		// if lease is different and value is not equal then update.
+		if getR.Kvs[0].Lease == int64(leaseID) &&
+			bytes.Equal(getR.Kvs[0].Value, value) {
 			return false, nil
 		}
 	}