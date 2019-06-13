g0odi3z2j/cilium

commit bb8726467c9f97da5db9c7da8c45f3ab79f19dae

bpf: Get rid of CGO in bpf_linux.go
`C.CString()` can be safely replaced with `&[]byte()[0]`.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/bpf/bpf_linux.go
@@ -16,9 +16,6 @@
 
 package bpf
 
-// #include <stdlib.h>
-import "C"
-
 import (
 	"fmt"
 	"os"
@@ -255,10 +252,9 @@ type bpfAttrObjOp struct {
 
 // ObjPin stores the map's fd in pathname.
 func ObjPin(fd int, pathname string) error {
-	pathStr := C.CString(pathname)
-	defer C.free(unsafe.Pointer(pathStr))
+	pathStr := []byte(pathname)
 	uba := bpfAttrObjOp{
-		pathname: uint64(uintptr(unsafe.Pointer(pathStr))),
+		pathname: uint64(uintptr(unsafe.Pointer(&pathStr[0]))),
 		fd:       uint32(fd),
 	}
 
@@ -285,10 +281,9 @@ func ObjPin(fd int, pathname string) error {
 
 // ObjGet reads the pathname and returns the map's fd read.
 func ObjGet(pathname string) (int, error) {
-	pathStr := C.CString(pathname)
-	defer C.free(unsafe.Pointer(pathStr))
+	pathStr := []byte(pathname)
 	uba := bpfAttrObjOp{
-		pathname: uint64(uintptr(unsafe.Pointer(pathStr))),
+		pathname: uint64(uintptr(unsafe.Pointer(&pathStr[0]))),
 	}
 
 	var duration *spanstat.SpanStat

commit 5e76baef0f78c987176c074ae0a4467b997909ad

bpf: Fix string conversion to byte array
The commit "bpf: Get rid of CGO in bpf_linux.go" missed the fact that
strings in Go are not NUL-terminated, as internally they are represented
as (len, byte array) tuple. So, the conversion introduced in the commit
was unsafe.

Fix the conversion by reusing syscall.StringBytePtr which takes care
of appending "\0" to the end of a byte array.

Fixes: bb8726467c "bpf: Get rid of CGO in bpf_linux.go"
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/bpf/bpf_linux.go
@@ -252,9 +252,9 @@ type bpfAttrObjOp struct {
 
 // ObjPin stores the map's fd in pathname.
 func ObjPin(fd int, pathname string) error {
-	pathStr := []byte(pathname)
+	pathStr := syscall.StringBytePtr(pathname)
 	uba := bpfAttrObjOp{
-		pathname: uint64(uintptr(unsafe.Pointer(&pathStr[0]))),
+		pathname: uint64(uintptr(unsafe.Pointer(pathStr))),
 		fd:       uint32(fd),
 	}
 
@@ -281,9 +281,9 @@ func ObjPin(fd int, pathname string) error {
 
 // ObjGet reads the pathname and returns the map's fd read.
 func ObjGet(pathname string) (int, error) {
-	pathStr := []byte(pathname)
+	pathStr := syscall.StringBytePtr(pathname)
 	uba := bpfAttrObjOp{
-		pathname: uint64(uintptr(unsafe.Pointer(&pathStr[0]))),
+		pathname: uint64(uintptr(unsafe.Pointer(pathStr))),
 	}
 
 	var duration *spanstat.SpanStat