g0odi3z2j/cilium

commit 601ef366755ec218f31fbc1e6c6787d16c49a811

daemon: synchronously add endpoints to endpointmanager in \`regenerateRestoredEndpoints\`
Previously, \`regenerateRestoredEndpoints\` spawned a goroutine for each
endpoint to be restored. In this goroutine, the endpoint to be restored was
inserted into the endpointmanager. This meant that
\`regenerateRestoredEndpoints\` could return before all goroutines were
executed. As such, this meant that there was no guarantee of the state of
endpointmanager after regenerateRestoredEndpoints returned. This could mean that
functions which assume that endpoints are in the endpointmanager (e.g.,
`workloads.EnableEventListener`) would face a situation in which the endpoints
were not always in the endpoint manager. By performing this insertion
synchronously, we guarantee that all endpoints to be restored are inserted into
the endpointmanager before `regenerateRestoredEndpoints` returns.

Signed-off by: Ian Vernon <ian@cilium.io>

daemon/state.go
@@ -131,14 +131,27 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) {
 	epRegenerated := make(chan bool, len(state.restored))
 
 	for _, ep := range state.restored {
+
+		// Insert into endpoint manager so it can be regenerated when calls to
+		// TriggerPolicyUpdates() are made. This must be done synchronously (i.e.,
+		// not in a goroutine) because regenerateRestoredEndpoints must guarantee
+		// upon returning that endpoints are exposed to other subsystems via
+		// endpointmanager.
+		ep.Mutex.RLock()
+		endpointmanager.Insert(ep)
+		ep.Mutex.RUnlock()
+
 		go func(ep *endpoint.Endpoint, epRegenerated chan<- bool) {
 			ep.Mutex.Lock()
 
-			// Insert into endpoint manager so it can be regenerated when calls to
-			// TriggerPolicyUpdates() are made.
-			endpointmanager.Insert(ep)
-
 			scopedLog := log.WithField(logfields.EndpointID, ep.ID)
+
+			state := ep.GetStateLocked()
+			if state == endpoint.StateDisconnected || state == endpoint.StateDisconnecting {
+				scopedLog.Warn("Endpoint to restore has been deleted")
+				ep.Mutex.Unlock()
+				return
+			}
 
 			ep.LogStatusOKLocked(endpoint.Other, "Synchronizing endpoint labels with KVStore")
 			if err := d.syncLabels(ep); err != nil {

commit beea7950773444acd536ac6f38332156ba0235dd

daemon: fix endpoint restore when endpoints are not available
As containers can be removed between cilium-agent restarts, their
endpoints will no longer be valid and they can't be restored. By not
being restored in this case, the channel signalizing they were not
restored was not written making the go routine to be kept running until
the cilium-agent forever.

Fixes: 601ef366755e ("daemon: synchronously add endpoints to endpointmanager in \`regenerateRestoredEndpoints\`")
Fixes: 744312931571 ("Consistently check for liveness of endpoint when re-locking (#5116)")
Fixes: 07aab728e71c ("daemon: on restore, run identity allocation in the background")
Signed-off-by: André Martins <andre@cilium.io>

daemon/state.go
@@ -260,6 +260,7 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) (resto
 		go func(ep *endpoint.Endpoint, epRegenerated chan<- bool) {
 			if err := ep.RLockAlive(); err != nil {
 				ep.LogDisconnectedMutexAction(err, "before filtering labels during regenerating restored endpoint")
+				epRegenerated <- false
 				return
 			}
 			scopedLog := log.WithField(logfields.EndpointID, ep.ID)
@@ -272,6 +273,7 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) (resto
 			if err != nil {
 				scopedLog.WithError(err).Warn("Unable to restore endpoint")
 				epRegenerated <- false
+				return
 			}
 
 			// Wait for initial identities and ipcache from the
@@ -285,6 +287,7 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) (resto
 
 			if err := ep.LockAlive(); err != nil {
 				scopedLog.Warn("Endpoint to restore has been deleted")
+				epRegenerated <- false
 				return
 			}
 