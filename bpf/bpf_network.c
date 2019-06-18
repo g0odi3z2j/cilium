g0odi3z2j/cilium

commit 4f809a315473f4ae912fb7c7d29f91c94a20ff73

cilium: encrypt, use ipcache to lookup IPsec destination IP
In direct routing mode without CNI chaining Cilium defaults to
allocating IPs from a IP/Mask assigned to the node. On the
encryption side we can use this to build an IPsec packet that
will be routed correctly to the node. To do this we build the
IPsec state/policy rules so that the internal IP (IP assigned
to cilium_host) for the node is used for the dip of the tunnel
header. This allows to rely on xfrm layer to correctly set
src and dest IP addresses.

But, with CNI chaining we can no longer infer the destination
node from the original packets destination IP because IPs are
not assigned using a pool per node. To work around this we use
a place holder destination IP during the encryption done by
the xfrm layer then use the ipcache entry to lookup the
required destination IP.

This requires packets to be routed through cilium_host after
the xfrm layer.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=443458c2bab9efd342efdf549a5450c5890369ea
+GO_BINDATA_SHA1SUM=65a93e4917b65586a1c4fb89ff674623dd7be39f
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

bpf/bpf_lxc.c
@@ -325,7 +325,11 @@ pass_to_stack:
 #ifdef ENABLE_IPSEC
 	if (encrypt_key && tunnel_endpoint) {
 		set_encrypt_key(skb, encrypt_key);
+#ifdef IP_POOLS
+		set_encrypt_dip(skb, tunnel_endpoint);
+#else
 		set_identity(skb, SECLABEL);
+#endif
 	}
 #endif
 #endif
@@ -618,7 +622,11 @@ pass_to_stack:
 #ifdef ENABLE_IPSEC
 	if (encrypt_key && tunnel_endpoint) {
 		set_encrypt_key(skb, encrypt_key);
+#ifdef IP_POOLS
+		set_encrypt_dip(skb, tunnel_endpoint);
+#else
 		set_identity(skb, SECLABEL);
+#endif
 	}
 #endif
 #endif

bpf/bpf_netdev.c
@@ -222,7 +222,11 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 src_identity)
 		__u8 key = get_min_encrypt_key(info->key);
 
 		set_encrypt_key_cb(skb, key);
+#ifdef IP_POOLS
+		set_encrypt_dip(skb, info->tunnel_endpoint);
+#else
 		set_identity_cb(skb, secctx);
+#endif
 	}
 #endif
 	return TC_ACT_OK;
@@ -359,7 +363,11 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 src_identity)
 		__u8 key = get_min_encrypt_key(info->key);
 
 		set_encrypt_key_cb(skb, key);
+#ifdef IP_POOLS
+		set_encrypt_dip(skb, info->tunnel_endpoint);
+#else
 		set_identity_cb(skb, secctx);
+#endif
 	}
 #endif
 	return TC_ACT_OK;
@@ -394,15 +402,50 @@ static __always_inline int do_netdev(struct __sk_buff *skb, __u16 proto)
 
 		if (magic == MARK_MAGIC_ENCRYPT) {
 			__u32 seclabel, tunnel_endpoint = 0;
+#ifdef IP_POOLS
+			__u32 tunnel_source = IPV4_ENCRYPT_IFACE;
+			void *data, *data_end;
+			struct iphdr *iphdr;
+			__be32 sum;
+#endif
 
 			seclabel = get_identity(skb);
 			tunnel_endpoint = skb->cb[4];
 			skb->mark = 0;
-			bpf_clear_cb(skb);
-
 #ifdef ENCAP_IFINDEX
+			bpf_clear_cb(skb);
 			return __encap_and_redirect_with_nodeid(skb, tunnel_endpoint, seclabel, TRACE_PAYLOAD_LEN);
-#elif ENCRYPT_NODE
+#endif
+#ifdef IP_POOLS
+			if (!revalidate_data(skb, &data, &data_end, &iphdr))
+				return DROP_INVALID;
+
+			/* When IP_POOLS is enabled ip addresses are not
+			 * assigned on a per node basis so lacking node
+			 * affinity we can not use IP address to assign the
+			 * destination IP. Instead rewrite it here from cb[].
+			 */
+			sum = csum_diff(&iphdr->daddr, 4, &tunnel_endpoint, 4, 0);
+			if (skb_store_bytes(skb, ETH_HLEN + offsetof(struct iphdr, daddr),
+			    &tunnel_endpoint, 4, 0) < 0)
+				return DROP_WRITE_ERROR;
+			if (l3_csum_replace(skb, ETH_HLEN + offsetof(struct iphdr, check),
+			    0, sum, 0) < 0)
+				return DROP_CSUM_L3;
+
+			if (!revalidate_data(skb, &data, &data_end, &iphdr))
+				return DROP_INVALID;
+
+			sum = csum_diff(&iphdr->saddr, 4, &tunnel_source, 4, 0);
+			if (skb_store_bytes(skb, ETH_HLEN + offsetof(struct iphdr, saddr),
+			    &tunnel_source, 4, 0) < 0)
+				return DROP_WRITE_ERROR;
+			if (l3_csum_replace(skb, ETH_HLEN + offsetof(struct iphdr, check),
+			    0, sum, 0) < 0)
+				return DROP_CSUM_L3;
+#endif
+			bpf_clear_cb(skb);
+#if ENCRYPT_NODE
 			return redirect(ENCRYPT_IFACE, 0);
 #else
 			return TC_ACT_OK;

bpf/lib/common.h
@@ -324,6 +324,11 @@ static inline int __inline__ get_identity(struct __sk_buff *skb)
 	return ((skb->mark & 0xFF) << 16) | skb->mark >> 16;
 }
 
+static inline void __inline__ set_encrypt_dip(struct __sk_buff *skb, __u32 ip_endpoint)
+{
+	skb->cb[4] = ip_endpoint;
+}
+
 /**
  * set_identity - pushes 24 bit identity into skb mark value.
  */

commit 6dad9fef92e7ded256ef5191b5748197115e31a3

cilium: add skb_pull_data to bpf_network to avoid revalidate error
Depending on the driver when we have a full stack of headers with
tunneled IPsec its possible the headers are not part of the linear
data. In this case we should pull in the data instead of immediately
aborting. If the pull fails we can then abort.

For this patch we only fix the immediate problem call site. We need
to audit the remaining ones. However, veth devices should not have
this issue in general so bpf_lxc.c should be OK. skbs on bpf_netdev
either passed through bpf_network.c, bpf_lxc.c or were routed from
the host networking.

Fixes: 4f809a315473f ("cilium: encrypt, use ipcache to lookup IPsec destination IP")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bpf/bpf_network.c
@@ -44,8 +44,12 @@ static inline int handle_ipv6(struct __sk_buff *skb)
 	bool decrypted;
 
 	decrypted = ((skb->mark & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_DECRYPT);
-	if (!revalidate_data(skb, &data, &data_end, &ip6))
-		return DROP_INVALID;
+	if (!revalidate_data(skb, &data, &data_end, &ip6)) {
+		int err = skb_pull_data(skb, ETH_HLEN + sizeof(struct ipv6hdr));
+
+		if (err || !revalidate_data(skb, &data, &data_end, &ip6))
+			return DROP_INVALID;
+	}
 
 	if (!decrypted) {
 		/* IPSec is not currently enforce (feature coming soon)
@@ -82,8 +86,12 @@ static inline int handle_ipv4(struct __sk_buff *skb)
 	bool decrypted;
 
 	decrypted = ((skb->mark & MARK_MAGIC_HOST_MASK) == MARK_MAGIC_DECRYPT);
-	if (!revalidate_data(skb, &data, &data_end, &ip4))
-		return DROP_INVALID;
+	if (!revalidate_data(skb, &data, &data_end, &ip4)) {
+		int err = skb_pull_data(skb, ETH_HLEN + sizeof(struct iphdr));
+
+		if (err || !revalidate_data(skb, &data, &data_end, &ip4))
+			return DROP_INVALID;
+	}
 
 	if (!decrypted) {
 		/* IPSec is not currently enforce (feature coming soon)

bpf/include/bpf/api.h
@@ -175,6 +175,7 @@ static int BPF_FUNC(skb_change_proto, struct __sk_buff *skb, uint32_t proto,
 		    uint32_t flags);
 static int BPF_FUNC(skb_change_tail, struct __sk_buff *skb, uint32_t nlen,
 		    uint32_t flags);
+static int BPF_FUNC(skb_pull_data, struct __sk_buff *skb, uint32_t len);
 
 /* Packet vlan encap/decap */
 static int BPF_FUNC(skb_vlan_push, struct __sk_buff *skb, uint16_t proto,

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=a5e3938b52cbf99e5e1131a3d5c15565f96eb523
+GO_BINDATA_SHA1SUM=158ed9e6414958e65438033ddbfc12e31378c9da
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 