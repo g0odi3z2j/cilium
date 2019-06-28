g0odi3z2j/cilium

commit 5319a5b75e8b613be6d37b7a2a2aa5a378ab11be

operator: AWS ENI allocation ability
This adds operator support for ENI allocation. See the previous commits for the
ENI details. The operator will respect the `--ipam=eni` just like the agent. In
general, it makes sense to also enable --enable-metrics to get ENI specific
allocation metrics.

Signed-off-by: Thomas Graf <thomas@cilium.io>

operator/metrics.go
@@ -1,0 +1,37 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const metricNamespace = "operator"
+
+var registry *prometheus.Registry
+
+func registerMetrics() {
+	registry = prometheus.NewPedanticRegistry()
+	registry.MustRegister(prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{Namespace: metricNamespace}))
+	go func() {
+		// The Handler function provides a default handler to expose metrics
+		// via an HTTP server. "/metrics" is the usual endpoint for that.
+		http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+		log.Fatal(http.ListenAndServe(metricsAddress, nil))
+	}()
+}

operator/cilium_node.go
@@ -1,0 +1,107 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"reflect"
+
+	"github.com/cilium/cilium/pkg/k8s"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/informer"
+
+	"k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+)
+
+var ciliumNodeStore cache.Store
+
+func convertToCiliumNode(obj interface{}) interface{} {
+	cnp, _ := obj.(*v2.CiliumNode)
+	return cnp
+}
+
+func startSynchronizingCiliumNodes() {
+	log.Info("Starting to synchronize CiliumNode custom resources...")
+
+	// TODO: The operator is currently storing a full copy of the
+	// CiliumNode resource, as the resource grows, we may want to consider
+	// introducing a slim version of it.
+	ciliumNodeStore = cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
+	ciliumNodeInformer := informer.NewInformerWithStore(
+		cache.NewListWatchFromClient(ciliumK8sClient.CiliumV2().RESTClient(),
+			"ciliumnodes", v1.NamespaceAll, fields.Everything()),
+		&v2.CiliumNode{},
+		0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				if node, ok := obj.(*v2.CiliumNode); ok {
+					_, err := k8s.Client().CoreV1().Nodes().Get(node.Name, metav1.GetOptions{})
+					switch {
+					case k8serrors.IsNotFound(err):
+						log.WithField("name", node.Name).Warning("Discovered CiliumNode without matching Kubernetes node resource")
+						deleteCiliumNode(node.Name)
+						return
+					case err != nil:
+						log.WithField("name", node.Name).WithError(err).Warning("Unable to retrieve Kubernetes node")
+					}
+
+					ciliumNodeUpdated(node.DeepCopy())
+				} else {
+					log.Warningf("Unknown CiliumNode object type %s received: %+v", reflect.TypeOf(obj), obj)
+				}
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				if node, ok := newObj.(*v2.CiliumNode); ok {
+					ciliumNodeUpdated(node.DeepCopy())
+				} else {
+					log.Warningf("Unknown CiliumNode object type %s received: %+v", reflect.TypeOf(newObj), newObj)
+				}
+			},
+			DeleteFunc: func(obj interface{}) {
+				deletedObj, ok := obj.(cache.DeletedFinalStateUnknown)
+				if ok {
+					// Delete was not observed by the
+					// watcher but is removed from
+					// kube-apiserver. This is the last
+					// known state and the object no longer
+					// exists.
+					if node, ok := deletedObj.Obj.(*v2.CiliumNode); ok {
+						ciliumNodeDeleted(node.Name)
+						return
+					}
+				} else if node, ok := obj.(*v2.CiliumNode); ok {
+					ciliumNodeDeleted(node.Name)
+					return
+				}
+				log.Warningf("Unknown CiliumNode object type %s received: %+v", reflect.TypeOf(obj), obj)
+			},
+		},
+		convertToCiliumNode,
+		ciliumNodeStore,
+	)
+
+	go ciliumNodeInformer.Run(wait.NeverStop)
+}
+
+func deleteCiliumNode(name string) {
+	if err := ciliumK8sClient.CiliumV2().CiliumNodes().Delete(name, &metav1.DeleteOptions{}); err == nil {
+		log.WithField("name", name).Info("Removed CiliumNode after receiving node deletion event")
+	}
+	ciliumNodeDeleted(name)
+}

operator/eni.go
@@ -1,0 +1,149 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	ec2shim "github.com/cilium/cilium/pkg/aws/ec2"
+	"github.com/cilium/cilium/pkg/aws/eni"
+	"github.com/cilium/cilium/pkg/aws/eni/metrics"
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	k8sversion "github.com/cilium/cilium/pkg/k8s/version"
+
+	"github.com/aws/aws-sdk-go-v2/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/sirupsen/logrus"
+)
+
+var nodeManager *eni.NodeManager
+
+type k8sAPI struct{}
+
+func (k *k8sAPI) UpdateStatus(node, origNode *v2.CiliumNode) (*v2.CiliumNode, error) {
+	// If k8s supports status as a sub-resource, then we need to update the status separately
+	k8sCapabilities := k8sversion.Capabilities()
+	switch {
+	case k8sCapabilities.UpdateStatus:
+		if !reflect.DeepEqual(origNode.Status, node.Status) {
+			_, err := ciliumK8sClient.CiliumV2().CiliumNodes().UpdateStatus(node)
+			if err != nil {
+				return nil, err
+			}
+		}
+	default:
+		if !reflect.DeepEqual(origNode.Status, node.Status) {
+			return ciliumK8sClient.CiliumV2().CiliumNodes().Update(node)
+		}
+	}
+
+	return nil, nil
+}
+
+func (k *k8sAPI) Update(node, origNode *v2.CiliumNode) (*v2.CiliumNode, error) {
+	// If k8s supports status as a sub-resource, then we need to update the status separately
+	k8sCapabilities := k8sversion.Capabilities()
+	switch {
+	case k8sCapabilities.UpdateStatus:
+		if !reflect.DeepEqual(origNode.Spec, node.Spec) {
+			newNode, err := ciliumK8sClient.CiliumV2().CiliumNodes().Update(node)
+			if err != nil {
+				return newNode, err
+			}
+		}
+	default:
+		if !reflect.DeepEqual(origNode, node) {
+			return ciliumK8sClient.CiliumV2().CiliumNodes().Update(node)
+		}
+	}
+
+	return nil, nil
+}
+
+func ciliumNodeUpdated(resource *v2.CiliumNode) {
+	if nodeManager != nil {
+		nodeManager.Update(resource)
+	}
+}
+
+func ciliumNodeDeleted(nodeName string) {
+	if nodeManager != nil {
+		nodeManager.Delete(nodeName)
+	}
+}
+
+// startENIAllocator kicks of ENI allocation, the initial connection to AWS
+// APIs is done in a blocking manner, given that is successful, a controller is
+// started to manage allocation based on CiliumNode custom resources
+func startENIAllocator() error {
+	log.Info("Starting ENI allocator...")
+
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return fmt.Errorf("unable to load AWS configuration: %s", err)
+	}
+
+	log.Info("Retrieving own metadata from EC2 metadata server...")
+	metadataClient := ec2metadata.New(cfg)
+	instance, err := metadataClient.GetInstanceIdentityDocument()
+	if err != nil {
+		return fmt.Errorf("unable to retrieve instance identity document: %s", err)
+	}
+
+	log.WithFields(logrus.Fields{
+		"instance": instance.InstanceID,
+		"region":   instance.Region,
+	}).Info("Connected to EC2 metadata server")
+
+	cfg.Region = instance.Region
+
+	eniMetrics := metrics.NewPrometheusMetrics(metricNamespace, registry)
+
+	// Rate limit the EC2 API calls to 4/s with a burst of 2 tokens
+	ec2Client := ec2shim.NewClient(ec2.New(cfg), eniMetrics, 4.0, 2)
+	log.Info("Connected to EC2 service API")
+	instances := eni.NewInstancesManager(ec2Client, eniMetrics)
+	nodeManager, err = eni.NewNodeManager(instances, ec2Client, &k8sAPI{}, eniMetrics)
+	if err != nil {
+		return fmt.Errorf("unable to initialize ENI node manager: %s", err)
+	}
+
+	// Initial blocking synchronization of all ENIs and subnets
+	instances.Resync()
+
+	// Start an interval based  background resync for safety, it will
+	// synchronize the state regularly and resolve eventual deficit if the
+	// event driven trigger fails
+	go func() {
+		time.Sleep(time.Minute)
+		mngr := controller.NewManager()
+		mngr.UpdateController("eni-refresh",
+			controller.ControllerParams{
+				RunInterval: time.Minute,
+				DoFunc: func(_ context.Context) error {
+					instances.Resync()
+					nodeManager.Resync()
+					return nil
+				},
+			})
+	}()
+
+	return nil
+}

operator/k8s_node.go
@@ -109,6 +109,7 @@ func runNodeWatcher() error {
 				serNodes.Enqueue(func() error {
 					deletedNode := k8s.ParseNode(n, node.FromKubernetes)
 					ciliumNodeStore.DeleteLocalKey(deletedNode)
+					deleteCiliumNode(n.Name)
 					return nil
 				}, serializer.NoRetry)
 			},

operator/main.go
@@ -59,6 +59,8 @@ var (
 	synchronizeServices bool
 	enableCepGC         bool
 	synchronizeNodes    bool
+	enableMetrics       bool
+	metricsAddress      string
 
 	ciliumK8sClient clientset.Interface
 )
@@ -100,7 +102,10 @@ func init() {
 	flags.StringVar(&kvStore, "kvstore", "", "Key-value store type")
 	flags.Var(option.NewNamedMapOptions("kvstore-opts", &kvStoreOpts, nil), "kvstore-opt", "Key-value store options")
 	flags.Uint16Var(&apiServerPort, "api-server-port", 9234, "Port on which the operator should serve API requests")
-
+	flags.String(option.IPAM, "", "Backend to use for IPAM")
+	option.BindEnv(option.IPAM)
+	flags.BoolVar(&enableMetrics, "enable-metrics", false, "Enable Prometheus metrics")
+	flags.StringVar(&metricsAddress, "metrics-address", ":6942", "Address to serve Prometheus metrics")
 	flags.BoolVar(&synchronizeServices, "synchronize-k8s-services", true, "Synchronize Kubernetes services to kvstore")
 	flags.BoolVar(&synchronizeNodes, "synchronize-k8s-nodes", true, "Synchronize Kubernetes nodes to kvstore and perform CNP GC")
 	flags.BoolVar(&enableCepGC, "cilium-endpoint-gc", true, "Enable CiliumEndpoint garbage collector")
@@ -163,15 +168,8 @@ func runOperator(cmd *cobra.Command) {
 	log.Infof("Cilium Operator %s", version.Version)
 	go startServer(fmt.Sprintf(":%d", apiServerPort), shutdownSignal)
 
-	if requiresKVstore() {
-		scopedLog := log.WithFields(logrus.Fields{
-			"kvstore": kvStore,
-			"address": kvStoreOpts[fmt.Sprintf("%s.address", kvStore)],
-		})
-		scopedLog.Info("Connecting to kvstore...")
-		if err := kvstore.Setup(kvStore, kvStoreOpts, nil); err != nil {
-			scopedLog.WithError(err).Fatal("Unable to setup kvstore")
-		}
+	if enableMetrics {
+		registerMetrics()
 	}
 
 	k8s.Configure(k8sAPIServer, k8sKubeConfigPath)
@@ -184,6 +182,28 @@ func runOperator(cmd *cobra.Command) {
 	if !k8sversion.Capabilities().MinimalVersionMet {
 		log.Fatalf("Minimal kubernetes version not met: %s < %s",
 			k8sversion.Version(), k8sversion.MinimalVersionConstraint)
+	}
+
+	enableENI := viper.GetString(option.IPAM) == option.IPAMENI
+	if enableENI {
+		if err := startENIAllocator(); err != nil {
+			log.WithError(err).Fatal("Unable to start ENI allocator")
+		}
+	}
+
+	if enableENI {
+		startSynchronizingCiliumNodes()
+	}
+
+	if requiresKVstore() {
+		scopedLog := log.WithFields(logrus.Fields{
+			"kvstore": kvStore,
+			"address": kvStoreOpts[fmt.Sprintf("%s.address", kvStore)],
+		})
+		scopedLog.Info("Connecting to kvstore...")
+		if err := kvstore.Setup(kvStore, kvStoreOpts, nil); err != nil {
+			scopedLog.WithError(err).Fatal("Unable to setup kvstore")
+		}
 	}
 
 	if synchronizeServices {

commit 127e644bd27577a490b6a1d9b4bde23d8078eec9

operator: Fix metrics namespace
"operator" is not unique enough, must be "cilium_operator"

Fixes: 5319a5b75e8b ("operator: AWS ENI allocation ability")

Signed-off-by: Thomas Graf <thomas@cilium.io>

operator/metrics.go
@@ -21,7 +21,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-const metricNamespace = "operator"
+const metricNamespace = "cilium_operator"
 
 var registry *prometheus.Registry
 