g0odi3z2j/cilium

commit 6a0bb0b541842f6b6116b0314d44a211ce272c56

daemon,lbmap: Do not update legacy svc if they are disabled
This commit disables updating of the legacy services if they
are disabled (`--enable-legacy-services=false`).

Two other changes:

- Restoring from the legacy services is disabled if the legacy
  services are disabled.
- As we do not support the weighted services, the calculation of
  the service endpoint weights is disabled to simplify the code.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/maps/lbmap/lbmap.go
@@ -249,6 +249,7 @@ func updateMasterService(fe ServiceKey, nbackends int, nonZeroWeights uint16) er
 // legacy and v2).
 func UpdateService(fe ServiceKey, backends []ServiceValue,
 	addRevNAT bool, revNATID int,
+	isLegacySVCEnabled bool,
 	acquireBackendID func(loadbalancer.L3n4Addr) (loadbalancer.BackendID, error),
 	releaseBackendID func(loadbalancer.BackendID)) error {
 
@@ -273,37 +274,42 @@ func UpdateService(fe ServiceKey, backends []ServiceValue,
 		return err
 	}
 
-	besValues := svc.getBackends()
-
-	log.WithFields(logrus.Fields{
-		"frontend": fe,
-		"backends": besValues,
-	}).Debugf("Updating BPF representation of service")
-
-	for _, be := range besValues {
-		weights = append(weights, be.GetWeight())
-		if be.GetWeight() != 0 {
-			nNonZeroWeights++
-		}
-	}
+	// FIXME(brb) Uncomment the following code after we have enabled weights
+	// in the BPF datapath code.
+	//for _, be := range besValues {
+	//	weights = append(weights, be.GetWeight())
+	//	if be.GetWeight() != 0 {
+	//		nNonZeroWeights++
+	//	}
+	//}
 
 	mutex.Lock()
 	defer mutex.Unlock()
+
+	besValuesV2 := svc.getBackendsV2()
+
+	log.WithFields(logrus.Fields{
+		"frontend": fe,
+		"backends": besValuesV2,
+	}).Debugf("Updating BPF representation of service")
 
 	// Add the new backends to the BPF maps
 	if err := updateBackendsLocked(addedBackends); err != nil {
 		return err
 	}
 
-	// Update the legacy service BPF maps
-	if err := updateServiceLegacyLocked(fe, besValues, addRevNAT, revNATID,
-		weights, nNonZeroWeights); err != nil {
-		return err
+	if isLegacySVCEnabled {
+		besValues := svc.getBackends()
+		// Update the legacy service BPF maps
+		if err := updateServiceLegacyLocked(fe, besValues, addRevNAT, revNATID,
+			weights, nNonZeroWeights); err != nil {
+			return err
+		}
 	}
 
 	// Update the v2 service BPF maps
-	if err := updateServiceV2Locked(fe, svc, addRevNAT, revNATID, weights,
-		nNonZeroWeights); err != nil {
+	if err := updateServiceV2Locked(fe, besValuesV2, svc, addRevNAT, revNATID,
+		weights, nNonZeroWeights, isLegacySVCEnabled); err != nil {
 		return err
 	}
 
@@ -417,9 +423,11 @@ func updateServiceLegacyLocked(fe ServiceKey, besValues []ServiceValue,
 	return nil
 }
 
-func updateServiceV2Locked(fe ServiceKey, svc *bpfService,
+func updateServiceV2Locked(fe ServiceKey, backends map[BackendAddrID]ServiceValue,
+	svc *bpfService,
 	addRevNAT bool, revNATID int,
-	weights []uint16, nNonZeroWeights uint16) error {
+	weights []uint16, nNonZeroWeights uint16,
+	isLegacySVCEnabled bool) error {
 
 	var (
 		existingCount int
@@ -434,8 +442,6 @@ func updateServiceV2Locked(fe ServiceKey, svc *bpfService,
 		svcKeyV2 = NewService4KeyV2(svc4Key.Address.IP(), svc4Key.Port, u8proto.All, 0)
 	}
 
-	// Do the corresponding updates to the v2 svc maps
-
 	svcValV2, err := lookupServiceV2(svcKeyV2)
 	if err == nil {
 		existingCount = svcValV2.GetCount()
@@ -443,17 +449,18 @@ func updateServiceV2Locked(fe ServiceKey, svc *bpfService,
 
 	svcValV2 = svcKeyV2.NewValue().(ServiceValueV2)
 	slot := 1
-	for addrID, svcVal := range svc.backendsV2 {
-		legacySlaveSlot, found := svc.getSlaveSlot(addrID)
-		if !found {
-			return fmt.Errorf("Slave slot not found for backend with addrID %s", addrID)
+	for addrID, svcVal := range backends {
+		if isLegacySVCEnabled {
+			legacySlaveSlot, found := svc.getSlaveSlot(addrID)
+			if !found {
+				return fmt.Errorf("Slave slot not found for backend with addrID %s", addrID)
+			}
+			svcValV2.SetCount(legacySlaveSlot) // For the backward-compatibility
 		}
 		backendID := cache.getBackendIDByAddrID(addrID)
-		svcValV2.SetCount(legacySlaveSlot)
 		svcValV2.SetBackendID(backendID)
 		svcValV2.SetRevNat(revNATID)
 		svcValV2.SetWeight(svcVal.GetWeight())
-		svcValV2.SetCount(legacySlaveSlot) // For the backward-compatibility
 		svcKeyV2.SetSlave(slot)
 		if err := updateServiceEndpointV2(svcKeyV2, svcValV2); err != nil {
 			return fmt.Errorf("Unable to update service %+v with the value %+v: %s",
@@ -492,6 +499,7 @@ func updateServiceV2Locked(fe ServiceKey, svc *bpfService,
 	if err != nil {
 		return fmt.Errorf("unable to update service weights for %s with value %+v: %s", svcKeyV2.String(), weights, err)
 	}
+
 	for i := slot; i <= existingCount; i++ {
 		svcKeyV2.SetSlave(i)
 		if err := deleteServiceLockedV2(svcKeyV2); err != nil {

daemon/loadbalancer.go
@@ -40,7 +40,9 @@ func (d *Daemon) addSVC2BPFMap(feCilium loadbalancer.L3n4AddrID, feBPF lbmap.Ser
 
 	revNATID := int(feCilium.ID)
 
-	if err := lbmap.UpdateService(feBPF, besBPF, addRevNAT, revNATID, service.AcquireBackendID, service.DeleteBackendID); err != nil {
+	if err := lbmap.UpdateService(feBPF, besBPF, addRevNAT, revNATID,
+		option.Config.EnableLegacyServices,
+		service.AcquireBackendID, service.DeleteBackendID); err != nil {
 		if addRevNAT {
 			delete(d.loadBalancer.RevNATMap, loadbalancer.ServiceID(feCilium.ID))
 		}
@@ -241,13 +243,22 @@ func (d *Daemon) svcDelete(svc *loadbalancer.LBSVC) error {
 	if err := d.svcDeleteBPF(svc.FE); err != nil {
 		return err
 	}
+
 	d.loadBalancer.DeleteService(svc)
+
 	return nil
 }
 
 func (d *Daemon) svcDeleteBPF(svc loadbalancer.L3n4AddrID) error {
-	errV2 := lbmap.DeleteServiceV2(svc, service.DeleteBackendID)
-	errLegacy := d.svcDeleteBPFLegacy(svc)
+	var (
+		errV2     error
+		errLegacy error
+	)
+
+	errV2 = lbmap.DeleteServiceV2(svc, service.DeleteBackendID)
+	if option.Config.EnableLegacyServices {
+		errLegacy = d.svcDeleteBPFLegacy(svc)
+	}
 
 	if errV2 != nil || errLegacy != nil {
 		return fmt.Errorf("Deleting service from BPF maps failed: %s (v2), %s (legacy)",
@@ -577,7 +588,7 @@ func (d *Daemon) SyncLBMap() error {
 		return nil
 	}
 
-	newSVCMap, newSVCList, lbmapDumpErrors := lbmap.DumpServiceMapsToUserspace()
+	newSVCMap, newSVCList, lbmapDumpErrors := lbmap.DumpServiceMapsToUserspaceV2()
 	for _, err := range lbmapDumpErrors {
 		log.WithError(err).Warn("Unable to list services in services BPF map")
 	}
@@ -676,6 +687,11 @@ func (d *Daemon) SyncLBMap() error {
 // elsewhere it needed. Returns an error if any issues occur dumping BPF maps
 // or deleting entries from BPF maps.
 func (d *Daemon) syncLBMapsWithK8s() error {
+	var (
+		newSVCList      []*loadbalancer.LBSVC
+		lbmapDumpErrors []error
+	)
+
 	k8sDeletedServices := map[string]loadbalancer.L3n4AddrID{}
 	alreadyChecked := map[string]struct{}{}
 
@@ -689,7 +705,12 @@ func (d *Daemon) syncLBMapsWithK8s() error {
 	defer d.loadBalancer.BPFMapMU.Unlock()
 
 	log.Debugf("dumping BPF service maps to userspace")
-	_, newSVCList, lbmapDumpErrors := lbmap.DumpServiceMapsToUserspace()
+	if option.Config.EnableLegacyServices {
+		_, newSVCList, lbmapDumpErrors = lbmap.DumpServiceMapsToUserspace()
+	} else {
+		_, newSVCList, lbmapDumpErrors = lbmap.DumpServiceMapsToUserspaceV2()
+	}
+
 	if len(lbmapDumpErrors) > 0 {
 		errorStrings := ""
 		for _, err := range lbmapDumpErrors {
@@ -809,13 +830,16 @@ func restoreServices() {
 
 	failed, restored, skipped := 0, 0, 0
 
-	svcMap, _, errors := lbmap.DumpServiceMapsToUserspace()
-	for _, err := range errors {
-		log.WithError(err).Warning("Error occured while dumping service table from datapath")
-	}
 	svcMapV2, _, errors := lbmap.DumpServiceMapsToUserspaceV2()
 	for _, err := range errors {
 		log.WithError(err).Warning("Error occured while dumping service v2 table from datapath")
+	}
+	svcMap := svcMapV2
+	if option.Config.EnableLegacyServices {
+		svcMap, _, errors = lbmap.DumpServiceMapsToUserspace()
+		for _, err := range errors {
+			log.WithError(err).Warning("Error occured while dumping service table from datapath")
+		}
 	}
 
 	for feHash, svc := range svcMap {
@@ -845,11 +869,19 @@ func restoreServices() {
 			}
 		}
 
+		v2Exists := true
+		if option.Config.EnableLegacyServices {
+			_, v2Exists = svcMapV2[feHash]
+		}
+
 		// Restore the service cache to guarantee backend ordering
 		// across restarts
-		_, v2Exists := svcMapV2[feHash]
 		if err := lbmap.RestoreService(svc, v2Exists); err != nil {
 			log.WithError(err).Warning("Unable to restore service in cache")
+		}
+
+		if !option.Config.EnableLegacyServices {
+			continue
 		}
 
 		// Create the svc v2 from the legacy one
@@ -864,7 +896,7 @@ func restoreServices() {
 			// We restore only services which has the revNat enabled
 			addRevNAT := true
 			revNATID := int(svc.FE.ID)
-			err = lbmap.UpdateService(fe, besValues, addRevNAT, revNATID,
+			err = lbmap.UpdateService(fe, besValues, addRevNAT, revNATID, true,
 				service.AcquireBackendID, service.DeleteBackendID)
 			if err != nil {
 				failed++

pkg/maps/lbmap/bpfservice.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 type serviceValueMap map[BackendAddrID]ServiceValue
@@ -160,6 +161,20 @@ func (b *bpfService) getSlaveSlot(id BackendAddrID) (int, bool) {
 	return slot, found
 }
 
+// getBackendsV2 makes a copy of backendsV2, so that they are safe to use
+// after the bpfService lock has been released.
+func (b *bpfService) getBackendsV2() map[BackendAddrID]ServiceValue {
+	b.mutex.RLock()
+	defer b.mutex.RUnlock()
+
+	backends := make(map[BackendAddrID]ServiceValue, len(b.backendsV2))
+	for addrID, backend := range b.backendsV2 {
+		backends[addrID] = backend
+	}
+
+	return backends
+}
+
 type lbmapCache struct {
 	mutex             lock.RWMutex
 	entries           map[string]*bpfService
@@ -183,8 +198,7 @@ func createBackendsMap(backends []ServiceValue) serviceValueMap {
 	return m
 }
 
-// restoreService restores service cache of the given legacy svc. If v2Exists
-// is set, the cache for the respective svc v2 is restored as well.
+// restoreService restores service cache of the given legacy and v2 service.
 func (l *lbmapCache) restoreService(svc loadbalancer.LBSVC, v2Exists bool) error {
 	l.mutex.Lock()
 	defer l.mutex.Unlock()
@@ -202,25 +216,30 @@ func (l *lbmapCache) restoreService(svc loadbalancer.LBSVC, v2Exists bool) error
 		l.entries[frontendID] = bpfSvc
 	}
 
-	for index, backend := range serviceValues {
-		b := &bpfBackend{
-			id:       backend.BackendAddrID(),
-			bpfValue: backend,
-		}
-		if _, ok := bpfSvc.uniqueBackends[backend.BackendAddrID()]; ok {
-			b.isHole = true
-		} else {
-			bpfSvc.uniqueBackends[backend.BackendAddrID()] = backend
-			bpfSvc.slaveSlotByBackendAddrID[backend.BackendAddrID()] = index + 1
-		}
+	if option.Config.EnableLegacyServices {
+		for index, backend := range serviceValues {
+			b := &bpfBackend{
+				id:       backend.BackendAddrID(),
+				bpfValue: backend,
+			}
+			if _, ok := bpfSvc.uniqueBackends[backend.BackendAddrID()]; ok {
+				b.isHole = true
+			} else {
+				bpfSvc.uniqueBackends[backend.BackendAddrID()] = backend
+				bpfSvc.slaveSlotByBackendAddrID[backend.BackendAddrID()] = index + 1
+			}
 
-		bpfSvc.backendsByMapIndex[index+1] = b
+			bpfSvc.backendsByMapIndex[index+1] = b
+		}
 	}
 
-	if v2Exists {
-		for addrID, backend := range bpfSvc.uniqueBackends {
+	if !option.Config.EnableLegacyServices || v2Exists {
+		for _, backend := range serviceValues {
+			addrID := backend.BackendAddrID()
+			if _, found := bpfSvc.backendsV2[addrID]; !found {
+				l.addBackendV2Locked(addrID)
+			}
 			bpfSvc.backendsV2[addrID] = backend
-			l.addBackendV2Locked(addrID)
 		}
 	}
 
@@ -251,10 +270,12 @@ func (l *lbmapCache) prepareUpdate(fe ServiceKey, backends []ServiceValue) (
 	// actually remove the backends but overwrite all slave slots that
 	// point to the removed backend with the backend that has the least
 	// duplicated slots.
-	for addrID, b := range bpfSvc.uniqueBackends {
-		if _, ok := newBackendsMap[addrID]; !ok {
-			bpfSvc.deleteBackend(b)
-			delete(bpfSvc.slaveSlotByBackendAddrID, addrID)
+	if option.Config.EnableLegacyServices {
+		for addrID, b := range bpfSvc.uniqueBackends {
+			if _, ok := newBackendsMap[addrID]; !ok {
+				bpfSvc.deleteBackend(b)
+				delete(bpfSvc.slaveSlotByBackendAddrID, addrID)
+			}
 		}
 	}
 	// Step 2: Delete all backends that no longer exist in the service v2.
@@ -273,12 +294,14 @@ func (l *lbmapCache) prepareUpdate(fe ServiceKey, backends []ServiceValue) (
 		}
 	}
 
-	// Step 3: Add all backends that don't exist in the legacy service yet.
-	for _, b := range backends {
-		if _, ok := bpfSvc.uniqueBackends[b.BackendAddrID()]; !ok {
-			addrID := b.BackendAddrID()
-			pos := bpfSvc.addBackend(b)
-			bpfSvc.slaveSlotByBackendAddrID[addrID] = pos
+	if option.Config.EnableLegacyServices {
+		// Step 3: Add all backends that don't exist in the legacy service yet.
+		for _, b := range backends {
+			if _, ok := bpfSvc.uniqueBackends[b.BackendAddrID()]; !ok {
+				addrID := b.BackendAddrID()
+				pos := bpfSvc.addBackend(b)
+				bpfSvc.slaveSlotByBackendAddrID[addrID] = pos
+			}
 		}
 	}
 	// Step 4: Add all backends that don't exist in the service v2 yet.
@@ -356,7 +379,6 @@ func (l *lbmapCache) addBackendIDs(backendIDs map[BackendAddrID]loadbalancer.Bac
 // filterNewBackends filters out backends which already exists from the given
 // map (i.e. keeps only new backends).
 func (l *lbmapCache) filterNewBackends(backends map[BackendAddrID]ServiceValue) map[BackendAddrID]ServiceValue {
-
 	l.mutex.RLock()
 	defer l.mutex.RUnlock()
 

pkg/maps/lbmap/bpfservice_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"github.com/cilium/cilium/pkg/checker"
+	"github.com/cilium/cilium/pkg/option"
 
 	. "gopkg.in/check.v1"
 )
@@ -40,6 +41,10 @@ func createBackend(c *C, ip string, port, revnat uint16) ServiceValue {
 	v := NewService4Value(0, i, port, revnat, 0)
 	c.Assert(v, Not(IsNil))
 	return v
+}
+
+func (b *LBMapTestSuite) SetUpSuite(c *C) {
+	option.Config.EnableLegacyServices = true
 }
 
 func (b *LBMapTestSuite) TestScaleService(c *C) {

commit de3b824b3d6fa54d13f6b0085a7015f68d817030

maps/lbmap: protect service cache refcount with concurrent access
As updating services can modify the lbmapCache the mutex to access the
same bpf map should be held before modifying the cache. Without it we
can risk to have the bpf map out of sync with the lbmap cache.

Fixes: 6a0bb0b54184 ("daemon,lbmap: Do not update legacy svc if they are disabled")
Signed-off-by: André Martins <andre@cilium.io>

pkg/maps/lbmap/lbmap.go
@@ -265,6 +265,9 @@ func UpdateService(fe ServiceKey, backends []ServiceValue,
 		return err
 	}
 
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	// Store mapping of backend addr ID => backend ID in the cache
 	cache.addBackendIDs(newBackendIDs)
 
@@ -282,9 +285,6 @@ func UpdateService(fe ServiceKey, backends []ServiceValue,
 	//		nNonZeroWeights++
 	//	}
 	//}
-
-	mutex.Lock()
-	defer mutex.Unlock()
 
 	besValuesV2 := svc.getBackendsV2()
 