g0odi3z2j/cilium

commit 37e5b3425f29e741fc51e931e3356adb3f82b071

k8s: Add NodePorts field to Service struct
The field is going to store NodePort frontends for a given service
of the NodePort type.

loadbalancer.L3n4AddrID{} was chosen to store a NodePort frontend
because the frontend will get a service ID allocated later by
k8s_watcher.go which will use the ID for reverse NAT xlation and
for deleting the related service.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/k8s/service.go
@@ -24,6 +24,8 @@ import (
 	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/service"
 
 	"github.com/sirupsen/logrus"
@@ -91,12 +93,44 @@ func ParseService(svc *types.Service) (ServiceID, *Service) {
 	svcInfo.IncludeExternal = getAnnotationIncludeExternal(svc)
 	svcInfo.Shared = getAnnotationShared(svc)
 
-	// FIXME: Add support for
-	//  - NodePort
 	for _, port := range svc.Spec.Ports {
 		p := loadbalancer.NewFEPort(loadbalancer.L4Type(port.Protocol), uint16(port.Port))
-		if _, ok := svcInfo.Ports[loadbalancer.FEPortName(port.Name)]; !ok {
-			svcInfo.Ports[loadbalancer.FEPortName(port.Name)] = p
+		portName := loadbalancer.FEPortName(port.Name)
+		if _, ok := svcInfo.Ports[portName]; !ok {
+			svcInfo.Ports[portName] = p
+		}
+		// This is a hack;-( In the case of NodePort service, we need to create
+		// two surrogate frontends per IP protocol - one with a zero IP addr used
+		// by the host-lb, another with a public iface IP addr. For each frontend
+		// we will need to store a service ID used for a reverse NAT translation
+		// and for deleting a service.
+		// Unfortunately, doing this in daemon/{loadbalancer,k8s_watcher}.go
+		// would introduce more complexity in already too complex LB codebase,
+		// so for now (until we have refactored the LB code) keep NodePort
+		// frontends in Service.NodePorts.
+		if svc.Spec.Type == v1.ServiceTypeNodePort {
+			if option.Config.EnableNodePort {
+				if _, ok := svcInfo.NodePorts[portName]; !ok {
+					svcInfo.NodePorts[portName] =
+						make(map[string]*loadbalancer.L3n4AddrID)
+				}
+				proto := loadbalancer.L4Type(port.Protocol)
+				port := uint16(port.NodePort)
+				id := loadbalancer.ID(0) // will be allocated by k8s_watcher
+
+				if option.Config.EnableIPv4 {
+					nodePortFE := loadbalancer.NewL3n4AddrID(proto, net.IPv4(0, 0, 0, 0), port, id)
+					svcInfo.NodePorts[portName][nodePortFE.String()] = nodePortFE
+					nodePortFE = loadbalancer.NewL3n4AddrID(proto, node.GetExternalIPv4(), port, id)
+					svcInfo.NodePorts[portName][nodePortFE.String()] = nodePortFE
+				}
+				if option.Config.EnableIPv6 {
+					nodePortFE := loadbalancer.NewL3n4AddrID(proto, net.IPv6zero, port, id)
+					svcInfo.NodePorts[portName][nodePortFE.String()] = nodePortFE
+					nodePortFE = loadbalancer.NewL3n4AddrID(proto, node.GetIPv6(), port, id)
+					svcInfo.NodePorts[portName][nodePortFE.String()] = nodePortFE
+				}
+			}
 		}
 	}
 
@@ -151,9 +185,13 @@ type Service struct {
 	// Shared is true when the service should be exposed/shared to other clusters
 	Shared bool
 
-	Ports    map[loadbalancer.FEPortName]*loadbalancer.FEPort
-	Labels   map[string]string
-	Selector map[string]string
+	Ports map[loadbalancer.FEPortName]*loadbalancer.FEPort
+	// NodePorts stores mapping for port name => NodePort frontend addr string =>
+	// NodePort fronted addr. The string addr => addr indirection is to avoid
+	// storing duplicates.
+	NodePorts map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID
+	Labels    map[string]string
+	Selector  map[string]string
 }
 
 // String returns the string representation of a service resource
@@ -203,6 +241,30 @@ func (s *Service) DeepEquals(o *Service) bool {
 				return false
 			}
 		}
+
+		if ((s.NodePorts == nil) != (o.NodePorts == nil)) ||
+			len(s.NodePorts) != len(o.NodePorts) {
+			return false
+		}
+		for portName, nodePorts := range s.NodePorts {
+			oNodePorts, ok := o.NodePorts[portName]
+			if !ok {
+				return false
+			}
+			if ((nodePorts == nil) != (oNodePorts == nil)) ||
+				len(nodePorts) != len(oNodePorts) {
+				return false
+			}
+			for nodePortName, nodePort := range nodePorts {
+				oNodePort, ok := oNodePorts[nodePortName]
+				if !ok {
+					return false
+				}
+				if !nodePort.Equals(oNodePort) {
+					return false
+				}
+			}
+		}
 		return true
 	}
 	return false
@@ -214,6 +276,7 @@ func NewService(ip net.IP, headless bool, labels map[string]string, selector map
 		FrontendIP: ip,
 		IsHeadless: headless,
 		Ports:      map[loadbalancer.FEPortName]*loadbalancer.FEPort{},
+		NodePorts:  map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
 		Labels:     labels,
 		Selector:   selector,
 	}

pkg/k8s/ingress_test.go
@@ -80,6 +80,7 @@ func (s *K8sSuite) TestParseIngress(c *check.C) {
 				},
 			},
 		},
+		NodePorts: map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
 	})
 }
 
@@ -145,6 +146,7 @@ func (s *K8sSuite) Test_parsingV1beta1(c *check.C) {
 							},
 						},
 					},
+					NodePorts: map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
 				}, nil
 			},
 		},

pkg/k8s/service_cache.go
@@ -328,6 +328,11 @@ func (s *ServiceCache) UniqueServiceFrontends() FrontendList {
 
 			uniqueFrontends[address.StringWithProtocol()] = struct{}{}
 		}
+		for _, nodePortFEs := range svc.NodePorts {
+			for _, fe := range nodePortFEs {
+				uniqueFrontends[fe.StringWithProtocol()] = struct{}{}
+			}
+		}
 	}
 
 	return uniqueFrontends

pkg/k8s/service_test.go
@@ -93,6 +93,7 @@ func (s *K8sSuite) TestParseService(c *check.C) {
 		Selector:   map[string]string{"foo": "bar"},
 		Labels:     map[string]string{"foo": "bar"},
 		Ports:      map[loadbalancer.FEPortName]*loadbalancer.FEPort{},
+		NodePorts:  map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
 	})
 
 	k8sSvc = &types.Service{
@@ -117,6 +118,7 @@ func (s *K8sSuite) TestParseService(c *check.C) {
 		IsHeadless: true,
 		Labels:     map[string]string{"foo": "bar"},
 		Ports:      map[loadbalancer.FEPortName]*loadbalancer.FEPort{},
+		NodePorts:  map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
 	})
 }
 
@@ -192,6 +194,21 @@ func TestService_Equals(t *testing.T) {
 						ID: 1,
 					},
 				},
+				NodePorts: map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{
+					loadbalancer.FEPortName("foo"): {
+						"0.0.0.0:31000": {
+							L3n4Addr: loadbalancer.L3n4Addr{
+								L4Addr: loadbalancer.L4Addr{
+									Protocol: loadbalancer.NONE,
+									Port:     31000,
+								},
+								IP: net.IPv4(0, 0, 0, 0),
+							},
+							ID: 1,
+						},
+					},
+				},
+
 				Labels: map[string]string{
 					"foo": "bar",
 				},
@@ -212,6 +229,21 @@ func TestService_Equals(t *testing.T) {
 							ID: 1,
 						},
 					},
+					NodePorts: map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{
+						loadbalancer.FEPortName("foo"): {
+							"0.0.0.0:31000": {
+								L3n4Addr: loadbalancer.L3n4Addr{
+									L4Addr: loadbalancer.L4Addr{
+										Protocol: loadbalancer.NONE,
+										Port:     31000,
+									},
+									IP: net.IPv4(0, 0, 0, 0),
+								},
+								ID: 1,
+							},
+						},
+					},
+
 					Labels: map[string]string{
 						"foo": "bar",
 					},
@@ -440,6 +472,80 @@ func TestService_Equals(t *testing.T) {
 					},
 					Labels:   map[string]string{},
 					Selector: map[string]string{},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "nodeports different",
+			fields: &Service{
+				FrontendIP: net.ParseIP("1.1.1.1"),
+				IsHeadless: true,
+				Ports: map[loadbalancer.FEPortName]*loadbalancer.FEPort{
+					loadbalancer.FEPortName("foo"): {
+						L4Addr: &loadbalancer.L4Addr{
+							Protocol: loadbalancer.NONE,
+							Port:     1,
+						},
+						ID: 1,
+					},
+				},
+				NodePorts: map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{
+					loadbalancer.FEPortName("foo"): {
+						"1.1.1.1:31000": {
+							L3n4Addr: loadbalancer.L3n4Addr{
+								L4Addr: loadbalancer.L4Addr{
+									Protocol: loadbalancer.NONE,
+									Port:     31000,
+								},
+								IP: net.IPv4(1, 1, 1, 1),
+							},
+							ID: 1,
+						},
+					},
+				},
+
+				Labels: map[string]string{
+					"foo": "bar",
+				},
+				Selector: map[string]string{
+					"baz": "foz",
+				},
+			},
+			args: args{
+				o: &Service{
+					FrontendIP: net.ParseIP("1.1.1.1"),
+					IsHeadless: true,
+					Ports: map[loadbalancer.FEPortName]*loadbalancer.FEPort{
+						loadbalancer.FEPortName("foo"): {
+							L4Addr: &loadbalancer.L4Addr{
+								Protocol: loadbalancer.NONE,
+								Port:     1,
+							},
+							ID: 1,
+						},
+					},
+					NodePorts: map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{
+						loadbalancer.FEPortName("foo"): {
+							"0.0.0.0:31000": {
+								L3n4Addr: loadbalancer.L3n4Addr{
+									L4Addr: loadbalancer.L4Addr{
+										Protocol: loadbalancer.NONE,
+										Port:     31000,
+									},
+									IP: net.IPv4(0, 0, 0, 0),
+								},
+								ID: 1,
+							},
+						},
+					},
+
+					Labels: map[string]string{
+						"foo": "bar",
+					},
+					Selector: map[string]string{
+						"baz": "foz",
+					},
 				},
 			},
 			want: false,

commit b222e54f6280d329f2ee6242721e5430d35f2416

k8s: Provision NodePort per ClusterIP IP protocol
Previously, we tried to provision a NodePort service for IPv6 depending
on whether `--enable-ipv6` was set to true which was wrong, as enabling
IPv6 does not mean that each Service definition will be in IPv6.

Unfortunately, "net.ParseIP([...]).To{4,16}()" is not reliable enough
to determine whether a clusterIP is in IPv6 or IPv4, thus the hack:
https://github.com/asaskevich/govalidator/pull/100.

Fixes: 37e5b3425 ("k8s: Add NodePorts field to Service struct")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/k8s/service.go
@@ -137,13 +137,18 @@ func ParseService(svc *types.Service) (ServiceID, *Service) {
 				port := uint16(port.NodePort)
 				id := loadbalancer.ID(0) // will be allocated by k8s_watcher
 
-				if option.Config.EnableIPv4 {
+				// TODO(brb) switch to if-clause when dual stack is supported
+				switch {
+				case option.Config.EnableIPv4 &&
+					clusterIP != nil && !strings.Contains(svc.Spec.ClusterIP, ":"):
+
 					nodePortFE := loadbalancer.NewL3n4AddrID(proto, net.IPv4(0, 0, 0, 0), port, id)
 					svcInfo.NodePorts[portName][nodePortFE.String()] = nodePortFE
 					nodePortFE = loadbalancer.NewL3n4AddrID(proto, node.GetExternalIPv4(), port, id)
 					svcInfo.NodePorts[portName][nodePortFE.String()] = nodePortFE
-				}
-				if option.Config.EnableIPv6 {
+				case option.Config.EnableIPv6 &&
+					clusterIP != nil && strings.Contains(svc.Spec.ClusterIP, ":"):
+
 					nodePortFE := loadbalancer.NewL3n4AddrID(proto, net.IPv6zero, port, id)
 					svcInfo.NodePorts[portName][nodePortFE.String()] = nodePortFE
 					nodePortFE = loadbalancer.NewL3n4AddrID(proto, node.GetIPv6(), port, id)