g0odi3z2j/cilium

commit 9aaa599b96c73cdbde73957b4f9e5ae63a6fc572

endpoint: Use accumulated map changes for policy updates
Use the accumulated map changes for bpf policy map updates when the
policy remains the same, but identities have changed.

This change removes the need for the endpoint to track the last
applied identity revision number.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/endpoint/bpf.go
@@ -903,7 +903,7 @@ func (e *Endpoint) GetBPFValue() (*lxcmap.EndpointInfo, error) {
 	return info, nil
 }
 
-func (e *Endpoint) deletePolicyKey(keyToDelete policy.Key) error {
+func (e *Endpoint) deletePolicyKey(keyToDelete policy.Key, incremental bool) bool {
 	// Convert from policy.Key to policymap.Key
 	policymapKey := policymap.PolicyKey{
 		Identity:         keyToDelete.Identity,
@@ -915,16 +915,21 @@ func (e *Endpoint) deletePolicyKey(keyToDelete policy.Key) error {
 	err := e.PolicyMap.DeleteKey(policymapKey)
 	if err != nil {
 		e.getLogger().WithError(err).WithField(logfields.BPFMapKey, policymapKey).Error("Failed to delete PolicyMap key")
-		return err
+		return false
 	}
 
 	// Operation was successful, remove from realized state.
 	delete(e.realizedPolicy.PolicyMapState, keyToDelete)
 
-	return nil
+	// Incremental updates need to update the desired state as well.
+	if incremental && e.desiredPolicy != e.realizedPolicy {
+		delete(e.desiredPolicy.PolicyMapState, keyToDelete)
+	}
+
+	return true
 }
 
-func (e *Endpoint) addPolicyKey(keyToAdd policy.Key, entry policy.MapStateEntry) error {
+func (e *Endpoint) addPolicyKey(keyToAdd policy.Key, entry policy.MapStateEntry, incremental bool) bool {
 	// Convert from policy.Key to policymap.Key
 	policymapKey := policymap.PolicyKey{
 		Identity:         keyToAdd.Identity,
@@ -939,11 +944,55 @@ func (e *Endpoint) addPolicyKey(keyToAdd policy.Key, entry policy.MapStateEntry)
 			logfields.BPFMapKey: policymapKey,
 			logfields.Port:      entry.ProxyPort,
 		}).Error("Failed to add PolicyMap key")
-		return err
+		return false
 	}
 
 	// Operation was successful, add to realized state.
 	e.realizedPolicy.PolicyMapState[keyToAdd] = entry
+
+	// Incremental updates need to update the desired state as well.
+	if incremental && e.desiredPolicy != e.realizedPolicy {
+		e.desiredPolicy.PolicyMapState[keyToAdd] = entry
+	}
+
+	return true
+}
+
+// applyPolicyMapChanges applies any incremental policy map changes
+// collected on the desired policy.
+func (e *Endpoint) applyPolicyMapChanges() error {
+	errors := 0
+
+	//  Note that after successful endpoint regeneration the
+	//  desired and realized policies are the same pointer. During
+	//  the bpf regeneration possible incremental updates are
+	//  collected on the newly computed desired policy, which is
+	//  not fully realized yet. This is why we get the map changes
+	//  from the desired policy here.
+	adds, deletes := e.desiredPolicy.PolicyMapChanges.ConsumeMapChanges()
+
+	for keyToAdd, entry := range adds {
+		// Keep the existing proxy port, if any
+		entry.ProxyPort = e.realizedRedirects[policy.ProxyIDFromKey(e.ID, keyToAdd)]
+		if !e.addPolicyKey(keyToAdd, entry, true) {
+			errors++
+		}
+	}
+
+	for keyToDelete := range deletes {
+		if !e.deletePolicyKey(keyToDelete, true) {
+			errors++
+		}
+	}
+
+	if errors > 0 {
+		return fmt.Errorf("updating desired PolicyMap state failed")
+	} else if len(adds)+len(deletes) > 0 {
+		e.getLogger().WithFields(logrus.Fields{
+			logfields.AddedPolicyID:   adds,
+			logfields.DeletedPolicyID: deletes,
+		}).Debug("Applied policy map updates due identity changes")
+	}
 
 	return nil
 }
@@ -953,30 +1002,32 @@ func (e *Endpoint) addPolicyKey(keyToAdd policy.Key, entry policy.MapStateEntry)
 // dumping the bpf policy map.
 func (e *Endpoint) syncPolicyMap() error {
 	// Nothing to do if the desired policy is already fully realized.
-	if e.realizedPolicy == e.desiredPolicy {
-		return nil
-	}
+	if e.realizedPolicy != e.desiredPolicy {
+		errors := 0
 
-	errors := []error{}
-
-	// Delete policy keys present in the realized state, but not present in the desired state
-	for keyToDelete := range e.realizedPolicy.PolicyMapState {
-		// If key that is in realized state is not in desired state, just remove it.
-		if _, ok := e.desiredPolicy.PolicyMapState[keyToDelete]; !ok {
-			err := e.deletePolicyKey(keyToDelete)
-			if err != nil {
-				errors = append(errors, err)
+		// Delete policy keys present in the realized state, but not present in the desired state
+		for keyToDelete := range e.realizedPolicy.PolicyMapState {
+			// If key that is in realized state is not in desired state, just remove it.
+			if _, ok := e.desiredPolicy.PolicyMapState[keyToDelete]; !ok {
+				if !e.deletePolicyKey(keyToDelete, false) {
+					errors++
+				}
 			}
+		}
+
+		err := e.addPolicyMapDelta()
+		if err != nil {
+			errors++
+		}
+
+		if errors > 0 {
+			return fmt.Errorf("syncPolicyMapDelta failed")
 		}
 	}
 
-	err := e.addPolicyMapDelta()
-
-	if len(errors) > 0 {
-		return fmt.Errorf("deleting stale PolicyMap state failed: %s", errors)
-	}
-
-	return err
+	// Still may have changes due to identities added and/or
+	// deleted after the desired policy was computed.
+	return e.applyPolicyMapChanges()
 }
 
 // addPolicyMapDelta adds new or updates existing bpf policy map state based
@@ -988,19 +1039,18 @@ func (e *Endpoint) addPolicyMapDelta() error {
 		return nil
 	}
 
-	errors := []error{}
+	errors := 0
 
 	for keyToAdd, entry := range e.desiredPolicy.PolicyMapState {
 		if oldEntry, ok := e.realizedPolicy.PolicyMapState[keyToAdd]; !ok || oldEntry != entry {
-			err := e.addPolicyKey(keyToAdd, entry)
-			if err != nil {
-				errors = append(errors, err)
+			if !e.addPolicyKey(keyToAdd, entry, false) {
+				errors++
 			}
 		}
 	}
 
-	if len(errors) > 0 {
-		return fmt.Errorf("updating desired PolicyMap state failed: %s", errors)
+	if errors > 0 {
+		return fmt.Errorf("updating desired PolicyMap state failed")
 	}
 
 	return nil
@@ -1056,7 +1106,7 @@ func (e *Endpoint) syncPolicyMapWithDump() error {
 		}
 	}
 
-	errors := []error{}
+	errors := 0
 
 	for _, entry := range currentMapContents {
 		// Convert key to host-byte order for lookup in the desiredMapState.
@@ -1073,17 +1123,16 @@ func (e *Endpoint) syncPolicyMapWithDump() error {
 		// If key that is in policy map is not in desired state, just remove it.
 		if _, ok := e.desiredPolicy.PolicyMapState[keyToDelete]; !ok {
 			e.getLogger().WithField(logfields.BPFMapKey, entry.Key.String()).Debug("syncPolicyMapWithDump removing a bpf policy entry not in the desired state")
-			err := e.deletePolicyKey(keyToDelete)
-			if err != nil {
-				errors = append(errors, err)
+			if !e.deletePolicyKey(keyToDelete, false) {
+				errors++
 			}
 		}
 	}
 
 	err = e.addPolicyMapDelta()
 
-	if len(errors) > 0 {
-		return fmt.Errorf("synchronizing desired PolicyMap state failed: %s", errors)
+	if errors > 0 {
+		return fmt.Errorf("synchronizing desired PolicyMap state failed")
 	}
 
 	return err

pkg/endpoint/endpoint.go
@@ -187,10 +187,6 @@ type Endpoint struct {
 	// TODO: Currently this applies only to HTTP L7 rules. Kafka L7 rules are still enforced by Cilium's node-wide Kafka proxy.
 	hasSidecarProxy bool
 
-	// prevIdentityCacheRevision is the revision of the identity cache used in the
-	// previous policy computation
-	prevIdentityCacheRevision uint64
-
 	// PolicyMap is the policy related state of the datapath including
 	// reference to all policy related BPF
 	PolicyMap *policymap.PolicyMap `json:"-"`
@@ -2031,7 +2027,7 @@ func (e *Endpoint) identityLabelsChanged(ctx context.Context, owner Owner, myCha
 
 	readyToRegenerate := false
 
-	// Regeneration is olny triggered once the endpoint ID has been
+	// Regeneration is only triggered once the endpoint ID has been
 	// assigned. This ensures that on the initial creation, the endpoint is
 	// not generated until the endpoint ID has been assigned. If the
 	// identity is resolved before the endpoint ID is assigned, the

pkg/endpoint/policy.go
@@ -45,7 +45,7 @@ import (
 
 // ProxyID returns a unique string to identify a proxy mapping.
 func (e *Endpoint) ProxyID(l4 *policy.L4Filter) string {
-	return policy.ProxyID(e.ID, l4.Ingress, string(l4.Protocol), uint16(l4.Port))
+	return policy.ProxyIDFromFilter(e.ID, l4)
 }
 
 // lookupRedirectPort returns the redirect L4 proxy port for the given L4
@@ -125,19 +125,8 @@ func (e *Endpoint) regeneratePolicy(owner Owner) (retErr error) {
 	defer repo.Mutex.RUnlock()
 	stats.waitingForPolicyRepository.End(true)
 
-	// Collect label arrays before policy computation, as this can fail.
-	// GH-1128 should allow optimizing this away, but currently we can't
-	// reliably know if the KV-store has changed or not, so we must scan
-	// through it each time.
-	stats.waitingForIdentityCache.Start()
-	prevIdentityCacheRevision := repo.GetSelectorCache().GetIDCacheRevision()
-	stats.waitingForIdentityCache.End(true)
-
 	// Recompute policy for this endpoint only if not already done for this revision.
-	// Must recompute if labels have changed.
-	if !e.forcePolicyCompute && e.nextPolicyRevision >= revision &&
-		e.prevIdentityCacheRevision == prevIdentityCacheRevision {
-
+	if !e.forcePolicyCompute && e.nextPolicyRevision >= revision {
 		e.getLogger().WithFields(logrus.Fields{
 			"policyRevision.next": e.nextPolicyRevision,
 			"policyRevision.repo": revision,
@@ -170,11 +159,8 @@ func (e *Endpoint) regeneratePolicy(owner Owner) (retErr error) {
 	calculatedPolicy := e.selectorPolicy.Consume(e)
 	stats.policyCalculation.End(true)
 
+	// This marks the e.desiredPolicy different from the previously realized policy
 	e.desiredPolicy = calculatedPolicy
-
-	// Set the revision of the identity cache at which the policy computation started
-	// now that we know the policy computation was successful.
-	e.prevIdentityCacheRevision = prevIdentityCacheRevision
 
 	if e.forcePolicyCompute {
 		forceRegeneration = true     // Options were changed by the caller.
@@ -380,7 +366,7 @@ func (e *Endpoint) updateRealizedState(stats *regenerationStatistics, origDir st
 
 	e.realizedBPFConfig = e.desiredBPFConfig
 
-	// Set realized state to desired state fields.
+	// Set realized state to desired state.
 	e.realizedPolicy = e.desiredPolicy
 
 	// Mark the endpoint to be running the policy revision it was

pkg/policy/proxyid.go
@@ -18,15 +18,27 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+
+	"github.com/cilium/cilium/pkg/policy/trafficdirection"
+	"github.com/cilium/cilium/pkg/u8proto"
 )
 
-// ProxyID returns a unique string to identify a proxy mapping.
-func ProxyID(endpointID uint16, ingress bool, protocol string, port uint16) string {
+func proxyID(endpointID uint16, ingress bool, protocol string, port uint16) string {
 	direction := "egress"
 	if ingress {
 		direction = "ingress"
 	}
 	return fmt.Sprintf("%d:%s:%s:%d", endpointID, direction, protocol, port)
+}
+
+// ProxyIDFromKey returns a unique string to identify a proxy mapping.
+func ProxyIDFromKey(endpointID uint16, key Key) string {
+	return proxyID(endpointID, key.TrafficDirection == trafficdirection.Ingress.Uint8(), u8proto.U8proto(key.Nexthdr).String(), key.DestPort)
+}
+
+// ProxyIDFromFilter returns a unique string to identify a proxy mapping.
+func ProxyIDFromFilter(endpointID uint16, l4 *L4Filter) string {
+	return proxyID(endpointID, l4.Ingress, string(l4.Protocol), uint16(l4.Port))
 }
 
 // ParseProxyID parses a proxy ID returned by ProxyID and returns its components.

pkg/policy/proxyid_test.go
@@ -21,7 +21,7 @@ import (
 )
 
 func (s *PolicyTestSuite) TestProxyID(c *C) {
-	id := ProxyID(123, true, "TCP", uint16(8080))
+	id := proxyID(123, true, "TCP", uint16(8080))
 	endpointID, ingress, protocol, port, err := ParseProxyID(id)
 	c.Assert(endpointID, Equals, uint16(123))
 	c.Assert(ingress, Equals, true)

pkg/policy/selectorcache.go
@@ -167,8 +167,7 @@ type SelectorCache struct {
 	// idCache contains all known identities as informed by the
 	// kv-store and the local identity facility via our
 	// UpdateIdentities() function.
-	idCache         scIdentityCache
-	idCacheRevision uint64
+	idCache scIdentityCache
 
 	// map key is the string representation of the selector being cached.
 	selectors map[string]identitySelector
@@ -705,8 +704,6 @@ func (sc *SelectorCache) UpdateIdentities(added, deleted cache.IdentityCache) {
 	}
 
 	if len(deleted)+len(added) > 0 {
-		sc.idCacheRevision++
-
 		// Iterate through all locally used identity selectors and
 		// update the cached numeric identities as required.
 		for _, sel := range sc.selectors {
@@ -737,16 +734,6 @@ func (sc *SelectorCache) UpdateIdentities(added, deleted cache.IdentityCache) {
 			}
 		}
 	}
-}
-
-// GetIDCacheRevision can be used to figure our if the selections
-// may have changed. Should not be used when identity updates are
-// properly propagated to the policy realization.
-func (sc *SelectorCache) GetIDCacheRevision() uint64 {
-	sc.mutex.Lock()
-	rev := sc.idCacheRevision
-	sc.mutex.Unlock()
-	return rev
 }
 
 // RemoveIdentitiesFQDNSelectors removes all identities from being mapped to the

commit 30f4f0fc0396761fd76f792a3f4b6c6ba4840325

endpoint: Do not error out when bpf map entry is already deleted.
Do not error out if the map entry was already deleted from the bpf
map.  Incremental updates depend on this being OK in cases where
identity change events overlap with full policy computation.  In other
cases we only delete entries that exist, but even in that case it is
better to not error out if the map entry has been deleted in the
meanwhile.

Fixes: 9aaa599b96c ("endpoint: Use accumulated map changes for policy updates")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/endpoint/bpf.go
@@ -21,6 +21,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"syscall"
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
@@ -909,8 +910,14 @@ func (e *Endpoint) deletePolicyKey(keyToDelete policy.Key, incremental bool) boo
 		TrafficDirection: keyToDelete.TrafficDirection,
 	}
 
-	err := e.PolicyMap.DeleteKey(policymapKey)
-	if err != nil {
+	// Do not error out if the map entry was already deleted from the bpf map.
+	// Incremental updates depend on this being OK in cases where identity change
+	// events overlap with full policy computation.
+	// In other cases we only delete entries that exist, but even in that case it
+	// is better to not error out if somebody else has deleted the map entry in the
+	// meanwhile.
+	err, errno := e.PolicyMap.DeleteKeyWithErrno(policymapKey)
+	if err != nil && errno != syscall.ENOENT {
 		e.getLogger().WithError(err).WithField(logfields.BPFMapKey, policymapKey).Error("Failed to delete PolicyMap key")
 		return false
 	}

pkg/maps/policymap/policymap.go
@@ -17,6 +17,7 @@ package policymap
 import (
 	"bytes"
 	"fmt"
+	"syscall"
 	"unsafe"
 
 	"github.com/cilium/cilium/pkg/bpf"
@@ -198,9 +199,9 @@ func (pm *PolicyMap) Exists(id uint32, dport uint16, proto u8proto.U8proto, traf
 
 // DeleteKey deletes the key-value pair from the given PolicyMap with PolicyKey
 // k. Returns an error if deletion from the PolicyMap fails.
-func (pm *PolicyMap) DeleteKey(key PolicyKey) error {
+func (pm *PolicyMap) DeleteKeyWithErrno(key PolicyKey) (error, syscall.Errno) {
 	k := key.ToNetwork()
-	return pm.Map.Delete(&k)
+	return pm.Map.DeleteWithErrno(&k)
 }
 
 // Delete removes an entry from the PolicyMap for identity `id`

pkg/maps/policymap/policymap_privileged_test.go
@@ -19,10 +19,13 @@ package policymap
 import (
 	"fmt"
 	"os"
+	"syscall"
 	"testing"
 
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/checker"
+	"github.com/cilium/cilium/pkg/policy/trafficdirection"
+	"github.com/cilium/cilium/pkg/u8proto"
 
 	. "gopkg.in/check.v1"
 )
@@ -91,4 +94,11 @@ func (pm *PolicyMapTestSuite) TestPolicyMapDumpToSlice(c *C) {
 	dump, err = testMap.DumpToSlice()
 	c.Assert(err, IsNil)
 	c.Assert(len(dump), Equals, 2)
+}
+
+func (pm *PolicyMapTestSuite) TestDeleteNonexistentKey(c *C) {
+	key := newKey(27, 80, u8proto.ANY, trafficdirection.Ingress)
+	err, errno := testMap.Map.DeleteWithErrno(&key)
+	c.Assert(err, Not(IsNil))
+	c.Assert(errno, Equals, syscall.ENOENT)
 }