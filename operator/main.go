g0odi3z2j/cilium

commit a23dac30290956951761b83a0e975583b351aa1c

operator: Don't attempt to connect to kvstore if disabled
Signed-off-by: Thomas Graf <thomas@cilium.io>

operator/main.go
@@ -170,7 +170,11 @@ func initConfig() {
 	viper.SetConfigName("cilium-operator")
 }
 
-func requiresKVstore() bool {
+func kvstoreEnabled() bool {
+	if option.Config.KVStore == "" {
+		return false
+	}
+
 	return identityAllocationMode == option.IdentityAllocationModeKVstore ||
 		synchronizeServices ||
 		synchronizeNodes
@@ -223,11 +227,11 @@ func runOperator(cmd *cobra.Command) {
 		startSynchronizingCiliumNodes()
 	}
 
-	if synchronizeServices {
-		startSynchronizingServices()
-	}
+	if kvstoreEnabled() {
+		if synchronizeServices {
+			startSynchronizingServices()
+		}
 
-	if requiresKVstore() {
 		var goopts *kvstore.ExtraOptions
 		scopedLog := log.WithFields(logrus.Fields{
 			"kvstore": kvStore,
@@ -277,6 +281,12 @@ func runOperator(cmd *cobra.Command) {
 		if err := kvstore.Setup(kvStore, kvStoreOpts, goopts); err != nil {
 			scopedLog.WithError(err).Fatal("Unable to setup kvstore")
 		}
+
+		if synchronizeNodes {
+			if err := runNodeWatcher(); err != nil {
+				log.WithError(err).Error("Unable to setup node watcher")
+			}
+		}
 	}
 
 	if identityAllocationMode == option.IdentityAllocationModeCRD {
@@ -289,12 +299,6 @@ func runOperator(cmd *cobra.Command) {
 
 	if enableCepGC {
 		enableCiliumEndpointSyncGC()
-	}
-
-	if synchronizeNodes {
-		if err := runNodeWatcher(); err != nil {
-			log.WithError(err).Error("Unable to setup node watcher")
-		}
 	}
 
 	if identityGCInterval != time.Duration(0) {

operator/api.go
@@ -60,7 +60,7 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 // checkStatus checks the connection status to the kvstore and
 // k8s apiserver and returns an error if any of them is unhealthy
 func checkStatus() error {
-	if requiresKVstore() {
+	if kvstoreEnabled() {
 		if client := kvstore.Client(); client == nil {
 			return fmt.Errorf("kvstore client not configured")
 		} else if _, err := client.Status(); err != nil {

commit 08b9bfae9852572310f03989037f7a12b7a69dc7

operator: Fix kvstore configuration inheritance from ConfigMap
The kvstore settings were not correctly derived from the ConfigMap which
resulted in all kvstore functionality to always be disabled.

Fixes: a23dac302909 ("operator: Don't attempt to connect to kvstore if disabled")
Fixes: #8849

Reported-by: @juan-daishogroup
Reported-by: Ara Zarifian
Signed-off-by: Thomas Graf <thomas@cilium.io>

install/kubernetes/cilium/charts/operator/templates/deployment.yaml
@@ -77,6 +77,18 @@ spec:
               key: disable-endpoint-crd
               name: cilium-config
               optional: true
+        - name: CILIUM_KVSTORE
+          valueFrom:
+            configMapKeyRef:
+              key: kvstore
+              name: cilium-config
+              optional: true
+        - name: CILIUM_KVSTORE_OPT
+          valueFrom:
+            configMapKeyRef:
+              key: kvstore-opt
+              name: cilium-config
+              optional: true
         - name: AWS_ACCESS_KEY_ID
           valueFrom:
             secretKeyRef:

install/kubernetes/quick-install.yaml
@@ -128,6 +128,7 @@ data:
   # - auto (automatically detect the container runtime)
   #
   container-runtime: none
+
   masquerade: "true"
 
   install-iptables-rules: "true"
@@ -441,6 +442,8 @@ spec:
         - mountPath: /lib/modules
           name: lib-modules
           readOnly: true
+        - mountPath: /run/xtables.lock
+          name: xtables-lock
       hostNetwork: true
       initContainers:
       - command:
@@ -508,6 +511,11 @@ spec:
       - hostPath:
           path: /lib/modules
         name: lib-modules
+        # To access iptables concurrently with other processes (e.g. kube-proxy)
+      - hostPath:
+          path: /run/xtables.lock
+          type: FileOrCreate
+        name: xtables-lock
         # To read the clustermesh configuration
       - name: clustermesh-secrets
         secret:
@@ -597,6 +605,18 @@ spec:
               key: disable-endpoint-crd
               name: cilium-config
               optional: true
+        - name: CILIUM_KVSTORE
+          valueFrom:
+            configMapKeyRef:
+              key: kvstore
+              name: cilium-config
+              optional: true
+        - name: CILIUM_KVSTORE_OPT
+          valueFrom:
+            configMapKeyRef:
+              key: kvstore-opt
+              name: cilium-config
+              optional: true
         - name: AWS_ACCESS_KEY_ID
           valueFrom:
             secretKeyRef:
@@ -630,4 +650,16 @@ spec:
       restartPolicy: Always
       serviceAccount: cilium-operator
       serviceAccountName: cilium-operator
+
+---
+# Source: cilium/charts/agent/templates/servicemonitor.yaml
+
+---
+# Source: cilium/charts/agent/templates/svc.yaml
+
+---
+# Source: cilium/charts/operator/templates/servicemonitor.yaml
+
+---
+# Source: cilium/charts/operator/templates/svc.yaml
 

operator/main.go
@@ -105,8 +105,10 @@ func init() {
 	flags.BoolP("debug", "D", false, "Enable debugging mode")
 	flags.StringVar(&k8sAPIServer, "k8s-api-server", "", "Kubernetes api address server (for https use --k8s-kubeconfig-path instead)")
 	flags.StringVar(&k8sKubeConfigPath, "k8s-kubeconfig-path", "", "Absolute path of the kubernetes kubeconfig file")
-	flags.StringVar(&kvStore, "kvstore", "", "Key-value store type")
-	flags.Var(option.NewNamedMapOptions("kvstore-opts", &kvStoreOpts, nil), "kvstore-opt", "Key-value store options")
+	flags.String(option.KVStore, "", "Key-value store type")
+	option.BindEnv(option.KVStore)
+	flags.Var(option.NewNamedMapOptions(option.KVStoreOpt, &kvStoreOpts, nil), option.KVStoreOpt, "Key-value store options")
+	option.BindEnv(option.KVStoreOpt)
 	flags.Uint16Var(&apiServerPort, "api-server-port", 9234, "Port on which the operator should serve API requests")
 	flags.String(option.IPAM, "", "Backend to use for IPAM")
 	option.BindEnv(option.IPAM)
@@ -171,7 +173,7 @@ func initConfig() {
 }
 
 func kvstoreEnabled() bool {
-	if option.Config.KVStore == "" {
+	if kvStore == "" {
 		return false
 	}
 
@@ -193,6 +195,8 @@ func runOperator(cmd *cobra.Command) {
 
 	k8sClientQPSLimit := viper.GetFloat64(option.K8sClientQPSLimit)
 	k8sClientBurst := viper.GetInt(option.K8sClientBurst)
+	kvStore = viper.GetString(option.KVStore)
+	kvStoreOpts = viper.GetStringMapString(option.KVStoreOpt)
 
 	k8s.Configure(k8sAPIServer, k8sKubeConfigPath, float32(k8sClientQPSLimit), k8sClientBurst)
 	if err := k8s.Init(); err != nil {