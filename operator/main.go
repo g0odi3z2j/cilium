g0odi3z2j/cilium

commit 08b9bfae9852572310f03989037f7a12b7a69dc7

operator: Fix kvstore configuration inheritance from ConfigMap
The kvstore settings were not correctly derived from the ConfigMap which
resulted in all kvstore functionality to always be disabled.

Fixes: a23dac302909 ("operator: Don't attempt to connect to kvstore if disabled")
Fixes: #8849

Reported-by: @juan-daishogroup
Reported-by: Ara Zarifian
Signed-off-by: Thomas Graf <thomas@cilium.io>

operator/main.go
@@ -105,8 +105,10 @@ func init() {
 	flags.BoolP("debug", "D", false, "Enable debugging mode")
 	flags.StringVar(&k8sAPIServer, "k8s-api-server", "", "Kubernetes api address server (for https use --k8s-kubeconfig-path instead)")
 	flags.StringVar(&k8sKubeConfigPath, "k8s-kubeconfig-path", "", "Absolute path of the kubernetes kubeconfig file")
-	flags.StringVar(&kvStore, "kvstore", "", "Key-value store type")
-	flags.Var(option.NewNamedMapOptions("kvstore-opts", &kvStoreOpts, nil), "kvstore-opt", "Key-value store options")
+	flags.String(option.KVStore, "", "Key-value store type")
+	option.BindEnv(option.KVStore)
+	flags.Var(option.NewNamedMapOptions(option.KVStoreOpt, &kvStoreOpts, nil), option.KVStoreOpt, "Key-value store options")
+	option.BindEnv(option.KVStoreOpt)
 	flags.Uint16Var(&apiServerPort, "api-server-port", 9234, "Port on which the operator should serve API requests")
 	flags.String(option.IPAM, "", "Backend to use for IPAM")
 	option.BindEnv(option.IPAM)
@@ -171,7 +173,7 @@ func initConfig() {
 }
 
 func kvstoreEnabled() bool {
-	if option.Config.KVStore == "" {
+	if kvStore == "" {
 		return false
 	}
 
@@ -193,6 +195,8 @@ func runOperator(cmd *cobra.Command) {
 
 	k8sClientQPSLimit := viper.GetFloat64(option.K8sClientQPSLimit)
 	k8sClientBurst := viper.GetInt(option.K8sClientBurst)
+	kvStore = viper.GetString(option.KVStore)
+	kvStoreOpts = viper.GetStringMapString(option.KVStoreOpt)
 
 	k8s.Configure(k8sAPIServer, k8sKubeConfigPath, float32(k8sClientQPSLimit), k8sClientBurst)
 	if err := k8s.Init(); err != nil {

install/kubernetes/cilium/charts/operator/templates/deployment.yaml
@@ -77,6 +77,18 @@ spec:
               key: disable-endpoint-crd
               name: cilium-config
               optional: true
+        - name: CILIUM_KVSTORE
+          valueFrom:
+            configMapKeyRef:
+              key: kvstore
+              name: cilium-config
+              optional: true
+        - name: CILIUM_KVSTORE_OPT
+          valueFrom:
+            configMapKeyRef:
+              key: kvstore-opt
+              name: cilium-config
+              optional: true
         - name: AWS_ACCESS_KEY_ID
           valueFrom:
             secretKeyRef:

install/kubernetes/quick-install.yaml
@@ -128,6 +128,7 @@ data:
   # - auto (automatically detect the container runtime)
   #
   container-runtime: none
+
   masquerade: "true"
 
   install-iptables-rules: "true"
@@ -441,6 +442,8 @@ spec:
         - mountPath: /lib/modules
           name: lib-modules
           readOnly: true
+        - mountPath: /run/xtables.lock
+          name: xtables-lock
       hostNetwork: true
       initContainers:
       - command:
@@ -508,6 +511,11 @@ spec:
       - hostPath:
           path: /lib/modules
         name: lib-modules
+        # To access iptables concurrently with other processes (e.g. kube-proxy)
+      - hostPath:
+          path: /run/xtables.lock
+          type: FileOrCreate
+        name: xtables-lock
         # To read the clustermesh configuration
       - name: clustermesh-secrets
         secret:
@@ -597,6 +605,18 @@ spec:
               key: disable-endpoint-crd
               name: cilium-config
               optional: true
+        - name: CILIUM_KVSTORE
+          valueFrom:
+            configMapKeyRef:
+              key: kvstore
+              name: cilium-config
+              optional: true
+        - name: CILIUM_KVSTORE_OPT
+          valueFrom:
+            configMapKeyRef:
+              key: kvstore-opt
+              name: cilium-config
+              optional: true
         - name: AWS_ACCESS_KEY_ID
           valueFrom:
             secretKeyRef:
@@ -630,4 +650,16 @@ spec:
       restartPolicy: Always
       serviceAccount: cilium-operator
       serviceAccountName: cilium-operator
+
+---
+# Source: cilium/charts/agent/templates/servicemonitor.yaml
+
+---
+# Source: cilium/charts/agent/templates/svc.yaml
+
+---
+# Source: cilium/charts/operator/templates/servicemonitor.yaml
+
+---
+# Source: cilium/charts/operator/templates/svc.yaml
 

commit 549a874abeb753caa9e1fdd7a11cb2b668da324e

operator: Fix passing kvstore options via arguments
The kvstore option variable was overwritten to be an empty array unless passed
in as environment variable.

Fixes: 08b9bfae985 ("operator: Fix kvstore configuration inheritance from ConfigMap")

Signed-off-by: Thomas Graf <thomas@cilium.io>

operator/main.go
@@ -213,7 +213,9 @@ func runOperator(cmd *cobra.Command) {
 	k8sClientQPSLimit := viper.GetFloat64(option.K8sClientQPSLimit)
 	k8sClientBurst := viper.GetInt(option.K8sClientBurst)
 	kvStore = viper.GetString(option.KVStore)
-	kvStoreOpts = viper.GetStringMapString(option.KVStoreOpt)
+	if m := viper.GetStringMapString(option.KVStoreOpt); len(m) > 0 {
+		kvStoreOpts = m
+	}
 
 	k8s.Configure(k8sAPIServer, k8sKubeConfigPath, float32(k8sClientQPSLimit), k8sClientBurst)
 	if err := k8s.Init(); err != nil {