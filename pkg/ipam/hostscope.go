g0odi3z2j/cilium

commit 2be2963298c01b07be3ac9c966c5aa9d92552af2

ipam: Define interface for allocator
This prepares the IPAM layer to support multiple implementations. No functional
changes are made in this commit.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/ipam/hostscope.go
@@ -1,0 +1,65 @@
+// Copyright 2017-2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ipam
+
+import (
+	"math/big"
+	"net"
+
+	k8sAPI "k8s.io/kubernetes/pkg/apis/core"
+	"k8s.io/kubernetes/pkg/registry/core/service/ipallocator"
+)
+
+type hostScopeAllocator struct {
+	allocCIDR *net.IPNet
+	allocator *ipallocator.Range
+}
+
+func newHostScopeAllocator(n *net.IPNet) Allocator {
+	a := &hostScopeAllocator{
+		allocCIDR: n,
+		allocator: ipallocator.NewCIDRRange(n),
+	}
+
+	return a
+}
+
+func (h *hostScopeAllocator) Allocate(ip net.IP, owner string) error {
+	return h.allocator.Allocate(ip)
+}
+
+func (h *hostScopeAllocator) Release(ip net.IP) error {
+	return h.allocator.Release(ip)
+}
+
+func (h *hostScopeAllocator) AllocateNext(owner string) (net.IP, error) {
+	return h.allocator.AllocateNext()
+}
+
+func (h *hostScopeAllocator) Dump() map[string]string {
+	alloc := map[string]string{}
+	ral := k8sAPI.RangeAllocation{}
+	h.allocator.Snapshot(&ral)
+	origIP := big.NewInt(0).SetBytes(h.allocCIDR.IP.To4())
+	bits := big.NewInt(0).SetBytes(ral.Data)
+	for i := 0; i < bits.BitLen(); i++ {
+		if bits.Bit(i) != 0 {
+			ip := net.IP(big.NewInt(0).Add(origIP, big.NewInt(int64(uint(i+1)))).Bytes()).String()
+			alloc[ip] = ""
+		}
+	}
+
+	return alloc
+}

daemon/daemon.go
@@ -1098,10 +1098,6 @@ func NewDaemon(dp datapath.Datapath) (*Daemon, *endpointRestoreState, error) {
 	}
 
 	// Set up ipam conf after init() because we might be running d.conf.KVStoreIPv4Registration
-	log.WithFields(logrus.Fields{
-		logfields.V4Prefix: dp.LocalNodeAddressing().IPv4().AllocationCIDR(),
-		logfields.V6Prefix: dp.LocalNodeAddressing().IPv6().AllocationCIDR(),
-	}).Info("Initializing IPAM")
 	d.ipam = ipam.NewIPAM(dp.LocalNodeAddressing(), ipam.Configuration{
 		EnableIPv4: option.Config.EnableIPv4,
 		EnableIPv6: option.Config.EnableIPv6,

pkg/ipam/allocator.go
@@ -17,14 +17,11 @@ package ipam
 import (
 	"errors"
 	"fmt"
-	"math/big"
 	"net"
 
 	"github.com/cilium/cilium/pkg/metrics"
 
 	"github.com/sirupsen/logrus"
-	k8sAPI "k8s.io/kubernetes/pkg/apis/core"
-	"k8s.io/kubernetes/pkg/registry/core/service/ipallocator"
 )
 
 const (
@@ -60,7 +57,7 @@ func (ipam *IPAM) AllocateIP(ip net.IP, owner string) error {
 			return ErrIPv4Disabled
 		}
 
-		if err := ipam.IPv4Allocator.Allocate(ip); err != nil {
+		if err := ipam.IPv4Allocator.Allocate(ip, owner); err != nil {
 			return err
 		}
 	} else {
@@ -69,7 +66,7 @@ func (ipam *IPAM) AllocateIP(ip net.IP, owner string) error {
 			return ErrIPv6Disabled
 		}
 
-		if err := ipam.IPv6Allocator.Allocate(ip); err != nil {
+		if err := ipam.IPv6Allocator.Allocate(ip, owner); err != nil {
 			return err
 		}
 	}
@@ -94,13 +91,13 @@ func (ipam *IPAM) AllocateIPString(ipAddr, owner string) error {
 	return ipam.AllocateIP(ip, owner)
 }
 
-func (ipam *IPAM) allocateNextFamily(family Family, allocator *ipallocator.Range, owner string) (ip net.IP, err error) {
+func (ipam *IPAM) allocateNextFamily(family Family, allocator Allocator, owner string) (ip net.IP, err error) {
 	if allocator == nil {
 		return nil, fmt.Errorf("%s allocator not available", family)
 	}
 
 	for {
-		ip, err = allocator.AllocateNext()
+		ip, err = allocator.AllocateNext(owner)
 		if err != nil {
 			return
 		}
@@ -208,41 +205,27 @@ func (ipam *IPAM) ReleaseIPString(ipAddr string) error {
 }
 
 // Dump dumps the list of allocated IP addresses
-func (ipam *IPAM) Dump() (map[string]string, map[string]string) {
+func (ipam *IPAM) Dump() (allocv4 map[string]string, allocv6 map[string]string) {
 	ipam.allocatorMutex.RLock()
 	defer ipam.allocatorMutex.RUnlock()
 
-	allocv4 := map[string]string{}
-	ralv4 := k8sAPI.RangeAllocation{}
 	if ipam.IPv4Allocator != nil {
-		ipam.IPv4Allocator.Snapshot(&ralv4)
-		origIP := big.NewInt(0).SetBytes(ipam.nodeAddressing.IPv4().AllocationCIDR().IP.To4())
-		v4Bits := big.NewInt(0).SetBytes(ralv4.Data)
-		for i := 0; i < v4Bits.BitLen(); i++ {
-			if v4Bits.Bit(i) != 0 {
-				ip := net.IP(big.NewInt(0).Add(origIP, big.NewInt(int64(uint(i+1)))).Bytes()).String()
-				owner, _ := ipam.owner[ip]
-				// If owner is not available, report IP but leave owner empty
-				allocv4[ip] = owner
-			}
+		allocv4 = ipam.IPv4Allocator.Dump()
+		for ip := range allocv4 {
+			owner, _ := ipam.owner[ip]
+			// If owner is not available, report IP but leave owner empty
+			allocv4[ip] = owner
 		}
 	}
 
-	allocv6 := map[string]string{}
-	ralv6 := k8sAPI.RangeAllocation{}
 	if ipam.IPv6Allocator != nil {
-		ipam.IPv6Allocator.Snapshot(&ralv6)
-		origIP := big.NewInt(0).SetBytes(ipam.nodeAddressing.IPv6().AllocationCIDR().IP)
-		v6Bits := big.NewInt(0).SetBytes(ralv6.Data)
-		for i := 0; i < v6Bits.BitLen(); i++ {
-			if v6Bits.Bit(i) != 0 {
-				ip := net.IP(big.NewInt(0).Add(origIP, big.NewInt(int64(uint(i+1)))).Bytes()).String()
-				owner, _ := ipam.owner[ip]
-				// If owner is not available, report IP but leave owner empty
-				allocv6[ip] = owner
-			}
+		allocv6 = ipam.IPv6Allocator.Dump()
+		for ip := range allocv6 {
+			owner, _ := ipam.owner[ip]
+			// If owner is not available, report IP but leave owner empty
+			allocv6[ip] = owner
 		}
 	}
 
-	return allocv4, allocv6
+	return
 }

pkg/ipam/ipam.go
@@ -23,8 +23,8 @@ import (
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 
+	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
-	"k8s.io/kubernetes/pkg/registry/core/service/ipallocator"
 )
 
 var (
@@ -65,12 +65,17 @@ func NewIPAM(nodeAddressing datapath.NodeAddressing, c Configuration) *IPAM {
 		blacklist:      map[string]string{},
 	}
 
+	log.WithFields(logrus.Fields{
+		logfields.V4Prefix: nodeAddressing.IPv4().AllocationCIDR(),
+		logfields.V6Prefix: nodeAddressing.IPv6().AllocationCIDR(),
+	}).Info("Initializing hostscope IPAM")
+
 	if c.EnableIPv6 {
-		ipam.IPv6Allocator = ipallocator.NewCIDRRange(nodeAddressing.IPv6().AllocationCIDR().IPNet)
+		ipam.IPv6Allocator = newHostScopeAllocator(nodeAddressing.IPv6().AllocationCIDR().IPNet)
 	}
 
 	if c.EnableIPv4 {
-		ipam.IPv4Allocator = ipallocator.NewCIDRRange(nodeAddressing.IPv4().AllocationCIDR().IPNet)
+		ipam.IPv4Allocator = newHostScopeAllocator(nodeAddressing.IPv4().AllocationCIDR().IPNet)
 	}
 
 	return ipam

pkg/ipam/ipam_test.go
@@ -58,7 +58,7 @@ func (s *IPAMSuite) TestLock(c *C) {
 	c.Assert(err, IsNil)
 
 	// Let's allocate the IP first so we can see the tests failing
-	err = ipam.IPv4Allocator.Allocate(epipv4.IP())
+	err = ipam.IPv4Allocator.Allocate(epipv4.IP(), "test")
 	c.Assert(err, IsNil)
 
 	err = ipam.IPv4Allocator.Release(epipv4.IP())

pkg/ipam/types.go
@@ -15,19 +15,36 @@
 package ipam
 
 import (
+	"net"
+
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/lock"
-
-	"k8s.io/kubernetes/pkg/registry/core/service/ipallocator"
 )
+
+// Allocator is the interface for an IP allocator implementation
+type Allocator interface {
+	// Allocate allocates a specific IP or fails
+	Allocate(ip net.IP, owner string) error
+
+	// Release releases a previously allocated IP or fails
+	Release(ip net.IP) error
+
+	// AllocateNext allocates the next available IP or fails if no more IPs
+	// are available
+	AllocateNext(owner string) (net.IP, error)
+
+	// Dump returns a map of all allocated IPs with the IP represented as
+	// key in the map
+	Dump() map[string]string
+}
 
 // Config is the IPAM configuration used for a particular IPAM type.
 type IPAM struct {
 	nodeAddressing datapath.NodeAddressing
 	config         Configuration
 
-	IPv6Allocator *ipallocator.Range
-	IPv4Allocator *ipallocator.Range
+	IPv6Allocator Allocator
+	IPv4Allocator Allocator
 
 	// owner maps an IP to the owner
 	owner map[string]string

commit f9491c83c812291f0349a4486290072680c2394d

ipam: fix v6 address corruption in cilium status dump
After both fixes, result now looks proper:

  # ./cilium/cilium status --verbose -o json
  [...]
  "ipam": {
    "allocations": {
      "192.168.1.229": "health",
      "192.168.211.128": "router",
      "192.168.247.238": "/ [restored]",
      "2001:db8:5::213c": "router",
      "2001:db8:5::9761": "health",
      "2001:db8:5::d965": "/ [restored]"
    },
    "ipv4": [
      "192.168.247.238",
      "192.168.1.229",
      "192.168.211.128"
    ],
    "ipv6": [
      "2001:db8:5::213c",
      "2001:db8:5::9761",
      "2001:db8:5::d965"
    ],
    "status": "IPv4: 3/65535 allocated from 192.168.0.0/16, IPv6: 3/65535 allocated from 2001:db8:5::/112"
  },
  [...]

Fixes: #9099
Fixes: 2be2963298c0 ("ipam: Define interface for allocator")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/ipam/hostscope.go
@@ -61,10 +61,15 @@ func (h *hostScopeAllocator) AllocateNext(owner string) (*AllocationResult, erro
 }
 
 func (h *hostScopeAllocator) Dump() (map[string]string, string) {
+	var origIP *big.Int
 	alloc := map[string]string{}
 	ral := k8sAPI.RangeAllocation{}
 	h.allocator.Snapshot(&ral)
-	origIP := big.NewInt(0).SetBytes(h.allocCIDR.IP.To4())
+	if h.allocCIDR.IP.To4() != nil {
+		origIP = big.NewInt(0).SetBytes(h.allocCIDR.IP.To4())
+	} else {
+		origIP = big.NewInt(0).SetBytes(h.allocCIDR.IP.To16())
+	}
 	bits := big.NewInt(0).SetBytes(ral.Data)
 	for i := 0; i < bits.BitLen(); i++ {
 		if bits.Bit(i) != 0 {