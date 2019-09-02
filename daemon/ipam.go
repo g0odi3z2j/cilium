g0odi3z2j/cilium

commit e772091657202044cb091ef5e819a4915807802b

ipam: Provide ownership information of IP allocations
Keeps track of who owns an allocated IP and provides it as part of cilium
status:
```
IPv4 address pool:      13/65535 allocated from 10.16.0.0/16
IPv6 address pool:      13/65535 allocated from f00d::a10:0:0:0/112
Allocated addresses:
  10.16.0.1 (router)
  10.16.113.196 (default/echo-79b5fcd4d9-2qlrn [restored])
  10.16.126.206 (default/probe-7b4494bd59-m5222 [restored])
  10.16.139.230 (default/echo-79b5fcd4d9-vwc6l [restored])
  10.16.163.105 (loopback)
  10.16.174.44 (default/echo-79b5fcd4d9-cfrgn [restored])
  10.16.183.123 (default/echo-79b5fcd4d9-nk9ln [restored])
  10.16.23.213 (health)
  10.16.5.111 (default/probe-7b4494bd59-8rrs2 [restored])
  10.16.71.7 (default/probe-7b4494bd59-whqv8 [restored])
  10.16.76.95 (default/probe-7b4494bd59-pqvx4 [restored])
  10.16.88.220 (default/probe-7b4494bd59-lcd9s [restored])
  10.16.92.82 (default/echo-79b5fcd4d9-bxgw5 [restored])
  f00d::a10:0:0:1 (router)
  f00d::a10:0:0:13ba (default/probe-7b4494bd59-8rrs2 [restored])
  f00d::a10:0:0:19f5 (default/probe-7b4494bd59-m5222 [restored])
  f00d::a10:0:0:2601 (health)
  f00d::a10:0:0:69d6 (default/probe-7b4494bd59-whqv8 [restored])
  f00d::a10:0:0:71 (default/echo-79b5fcd4d9-bxgw5 [restored])
  f00d::a10:0:0:722c (default/probe-7b4494bd59-lcd9s [restored])
  f00d::a10:0:0:8e65 (default/echo-79b5fcd4d9-2qlrn [restored])
  f00d::a10:0:0:ab5e (default/echo-79b5fcd4d9-nk9ln [restored])
  f00d::a10:0:0:b3ec (default/probe-7b4494bd59-pqvx4 [restored])
  f00d::a10:0:0:ca90 (default/echo-79b5fcd4d9-cfrgn [restored])
  f00d::a10:0:0:d8d4 (default/echo-79b5fcd4d9-vwc6l [restored])
```

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/ipam.go
@@ -43,7 +43,9 @@ func (h *postIPAM) Handle(params ipamapi.PostIPAMParams) middleware.Responder {
 		Address:        &models.AddressPair{},
 	}
 
-	ipv4, ipv6, err := h.daemon.ipam.AllocateNext(strings.ToLower(swag.StringValue(params.Family)))
+	family := strings.ToLower(swag.StringValue(params.Family))
+	owner := swag.StringValue(params.Owner)
+	ipv4, ipv6, err := h.daemon.ipam.AllocateNext(family, owner)
 	if err != nil {
 		return api.Error(ipamapi.PostIPAMFailureCode, err)
 	}
@@ -72,7 +74,8 @@ func NewPostIPAMIPHandler(d *Daemon) ipamapi.PostIPAMIPHandler {
 
 // Handle incoming requests address allocation requests for the daemon.
 func (h *postIPAMIP) Handle(params ipamapi.PostIPAMIPParams) middleware.Responder {
-	if err := h.daemon.ipam.AllocateIPString(params.IP); err != nil {
+	owner := swag.StringValue(params.Owner)
+	if err := h.daemon.ipam.AllocateIPString(params.IP, owner); err != nil {
 		return api.Error(ipamapi.PostIPAMIPFailureCode, err)
 	}
 
@@ -102,13 +105,27 @@ func (d *Daemon) DumpIPAM() *models.IPAMStatus {
 	allocv4, allocv6 := d.ipam.Dump()
 	status := &models.IPAMStatus{}
 
+	v4 := []string{}
+	for ip := range allocv4 {
+		v4 = append(v4, ip)
+	}
+
+	v6 := []string{}
+	for ip, owner := range allocv6 {
+		v6 = append(v6, ip)
+		// merge allocv6 into allocv4
+		allocv4[ip] = owner
+	}
+
 	if option.Config.EnableIPv4 {
-		status.IPV4 = allocv4
+		status.IPV4 = v4
 	}
 
 	if option.Config.EnableIPv6 {
-		status.IPV6 = allocv6
+		status.IPV6 = v4
 	}
+
+	status.Allocations = allocv4
 
 	return status
 }

pkg/client/ipam.go
@@ -26,11 +26,15 @@ const (
 )
 
 // IPAMAllocate allocates an IP address out of address family specific pool.
-func (c *Client) IPAMAllocate(family string) (*models.IPAMResponse, error) {
+func (c *Client) IPAMAllocate(family, owner string) (*models.IPAMResponse, error) {
 	params := ipam.NewPostIPAMParams().WithTimeout(api.ClientTimeout)
 
 	if family != "" {
 		params.SetFamily(&family)
+	}
+
+	if owner != "" {
+		params.SetOwner(&owner)
 	}
 
 	resp, err := c.IPAM.PostIPAM(params)
@@ -41,8 +45,8 @@ func (c *Client) IPAMAllocate(family string) (*models.IPAMResponse, error) {
 }
 
 // IPAMAllocateIP tries to allocate a particular IP address.
-func (c *Client) IPAMAllocateIP(ip string) error {
-	params := ipam.NewPostIPAMIPParams().WithIP(ip).WithTimeout(api.ClientTimeout)
+func (c *Client) IPAMAllocateIP(ip, owner string) error {
+	params := ipam.NewPostIPAMIPParams().WithIP(ip).WithOwner(&owner).WithTimeout(api.ClientTimeout)
 	_, err := c.IPAM.PostIPAMIP(params)
 	return Hint(err)
 }

pkg/ipam/ipam.go
@@ -53,6 +53,7 @@ func NewIPAM(nodeAddressing datapath.NodeAddressing, c Configuration) *IPAM {
 	ipam := &IPAM{
 		nodeAddressing: nodeAddressing,
 		config:         c,
+		owner:          map[string]string{},
 	}
 
 	if c.EnableIPv6 {
@@ -119,7 +120,7 @@ func (ipam *IPAM) reserveLocalRoutes() {
 			}).Info("Marking local route as no-alloc in node allocation prefix")
 
 			for ip := r.Dst.IP.Mask(r.Dst.Mask); r.Dst.Contains(ip); nextIP(ip) {
-				ipam.IPv4Allocator.Allocate(ip)
+				ipam.AllocateIP(ip, r.Dst.String())
 			}
 		}
 	}

plugins/cilium-docker/driver/ipam.go
@@ -116,7 +116,7 @@ func (driver *driver) requestAddress(w http.ResponseWriter, r *http.Request) {
 		family = client.AddressFamilyIPv6
 	}
 
-	ipam, err := driver.client.IPAMAllocate(family)
+	ipam, err := driver.client.IPAMAllocate(family, "docker-ipam")
 	if err != nil {
 		sendError(w, fmt.Sprintf("Could not allocate IP address: %s", err), http.StatusBadRequest)
 		return

api/v1/client/ipam/post_ip_a_m_ip_parameters.go
@@ -66,6 +66,8 @@ type PostIPAMIPParams struct {
 
 	*/
 	IP string
+	/*Owner*/
+	Owner *string
 
 	timeout    time.Duration
 	Context    context.Context
@@ -116,6 +118,17 @@ func (o *PostIPAMIPParams) SetIP(ip string) {
 	o.IP = ip
 }
 
+// WithOwner adds the owner to the post IP a m IP params
+func (o *PostIPAMIPParams) WithOwner(owner *string) *PostIPAMIPParams {
+	o.SetOwner(owner)
+	return o
+}
+
+// SetOwner adds the owner to the post IP a m IP params
+func (o *PostIPAMIPParams) SetOwner(owner *string) {
+	o.Owner = owner
+}
+
 // WriteToRequest writes these params to a swagger request
 func (o *PostIPAMIPParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Registry) error {
 
@@ -127,6 +140,22 @@ func (o *PostIPAMIPParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Re
 	// path param ip
 	if err := r.SetPathParam("ip", o.IP); err != nil {
 		return err
+	}
+
+	if o.Owner != nil {
+
+		// query param owner
+		var qrOwner string
+		if o.Owner != nil {
+			qrOwner = *o.Owner
+		}
+		qOwner := qrOwner
+		if qOwner != "" {
+			if err := r.SetQueryParam("owner", qOwner); err != nil {
+				return err
+			}
+		}
+
 	}
 
 	if len(res) > 0 {

api/v1/client/ipam/post_ip_a_m_parameters.go
@@ -63,6 +63,8 @@ type PostIPAMParams struct {
 
 	/*Family*/
 	Family *string
+	/*Owner*/
+	Owner *string
 
 	timeout    time.Duration
 	Context    context.Context
@@ -113,6 +115,17 @@ func (o *PostIPAMParams) SetFamily(family *string) {
 	o.Family = family
 }
 
+// WithOwner adds the owner to the post IP a m params
+func (o *PostIPAMParams) WithOwner(owner *string) *PostIPAMParams {
+	o.SetOwner(owner)
+	return o
+}
+
+// SetOwner adds the owner to the post IP a m params
+func (o *PostIPAMParams) SetOwner(owner *string) {
+	o.Owner = owner
+}
+
 // WriteToRequest writes these params to a swagger request
 func (o *PostIPAMParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Registry) error {
 
@@ -131,6 +144,22 @@ func (o *PostIPAMParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Regi
 		qFamily := qrFamily
 		if qFamily != "" {
 			if err := r.SetQueryParam("family", qFamily); err != nil {
+				return err
+			}
+		}
+
+	}
+
+	if o.Owner != nil {
+
+		// query param owner
+		var qrOwner string
+		if o.Owner != nil {
+			qrOwner = *o.Owner
+		}
+		qOwner := qrOwner
+		if qOwner != "" {
+			if err := r.SetQueryParam("owner", qOwner); err != nil {
 				return err
 			}
 		}

api/v1/models/allocation_map.go
@@ -1,0 +1,20 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package models
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	strfmt "github.com/go-openapi/strfmt"
+)
+
+// AllocationMap Map of allocated IPs
+//
+// swagger:model AllocationMap
+type AllocationMap map[string]string
+
+// Validate validates this allocation map
+func (m AllocationMap) Validate(formats strfmt.Registry) error {
+	return nil
+}

api/v1/models/ip_a_m_status.go
@@ -8,6 +8,7 @@ package models
 import (
 	strfmt "github.com/go-openapi/strfmt"
 
+	"github.com/go-openapi/errors"
 	"github.com/go-openapi/swag"
 )
 
@@ -15,6 +16,9 @@ import (
 // swagger:model IPAMStatus
 // +k8s:deepcopy-gen=true
 type IPAMStatus struct {
+
+	// allocations
+	Allocations AllocationMap `json:"allocations,omitempty"`
 
 	// ipv4
 	IPV4 []string `json:"ipv4"`
@@ -25,6 +29,31 @@ type IPAMStatus struct {
 
 // Validate validates this IP a m status
 func (m *IPAMStatus) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.validateAllocations(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
+
+func (m *IPAMStatus) validateAllocations(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Allocations) { // not required
+		return nil
+	}
+
+	if err := m.Allocations.Validate(formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("allocations")
+		}
+		return err
+	}
+
 	return nil
 }
 

commit 1b25147beca9702932ef1c860a8415aff2d2948e

ipam: do not assign v4 addresses for status.IPV6
Spotted by inspection. status.IPV6 should only hold v6 addresses
that we prepped earlier. Only the allocv4 is merged and holds both
but not v4.

Fixes: e77209165720 ("ipam: Provide ownership information of IP allocations")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

daemon/ipam.go
@@ -145,7 +145,7 @@ func (d *Daemon) DumpIPAM() *models.IPAMStatus {
 	}
 
 	if option.Config.EnableIPv6 {
-		status.IPV6 = v4
+		status.IPV6 = v6
 	}
 
 	status.Allocations = allocv4