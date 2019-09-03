g0odi3z2j/cilium

commit ae8ab3682c377683e95776e857e530eba032a353

node: Update ipcache entries independent of node update source
Update ipache entries in the generic node manager instead of assuming that
ipcache entries only need to be updated when node discovery updates come in via
the kvstore.

This enables to carry ipcache relevant node information in state sources other
than the kvstore.

Also simplify the node deletion logic and remove all ipcache entries of all
node IPs based on last state.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/node/manager/manager.go
@@ -16,12 +16,17 @@ package manager
 
 import (
 	"math"
+	"net"
 	"time"
 
 	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/node/addressing"
+	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/source"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -266,24 +271,41 @@ func (m *Manager) backgroundSync() {
 	}
 }
 
-// NodeSoftUpdated is called after the information of a node has be upated but
-// unlike a NodeUpdated does not require the datapath to be updated.
-func (m *Manager) NodeSoftUpdated(n node.Node) {
-	log.Debugf("Received soft node update event from %s: %#v", n.Source, n)
-	m.nodeUpdated(n, false)
-}
-
 // NodeUpdated is called after the information of a node has been updated. The
 // node in the manager is added or updated if the source is allowed to update
 // the node. If an update or addition has occurred, NodeUpdate() of the datapath
 // interface is invoked.
 func (m *Manager) NodeUpdated(n node.Node) {
 	log.Debugf("Received node update event from %s: %#v", n.Source, n)
-	m.nodeUpdated(n, true)
-}
-
-func (m *Manager) nodeUpdated(n node.Node, dpUpdate bool) {
 	nodeIdentity := n.Identity()
+	dpUpdate := true
+
+	for _, address := range n.IPAddresses {
+		var nodeIP net.IP
+
+		// Map the Cilium internal IP to the reachable node IP so it
+		// can be routed via the overlay
+		if address.Type == addressing.NodeCiliumInternalIP {
+			nodeIP = n.GetNodeIP(address.IP.To4() == nil)
+		} else if !option.Config.EncryptNode {
+			// When node encryption is disabled, only the internal
+			// IP of the cilium device has to be added.
+			continue
+		}
+
+		isOwning := ipcache.IPIdentityCache.Upsert(address.IP.String(), nodeIP, n.EncryptionKey, ipcache.Identity{
+			ID:     identity.ReservedIdentityHost,
+			Source: n.Source,
+		})
+
+		// Upsert() will return true if the ipcache entry is owned by
+		// the source of the node update that triggered this node
+		// update (kvstore, k8s, ...) The datapath is only updated if
+		// that source of truth is updated.
+		if !isOwning {
+			dpUpdate = false
+		}
+	}
 
 	m.mutex.Lock()
 	entry, oldNodeExists := m.nodes[nodeIdentity]
@@ -353,6 +375,10 @@ func (m *Manager) NodeDeleted(n node.Node) {
 				n.Name, n.Source, entry.node.Source)
 		}
 		return
+	}
+
+	for _, address := range entry.node.IPAddresses {
+		ipcache.IPIdentityCache.Delete(address.IP.String(), n.Source)
 	}
 
 	m.metricNumNodes.Dec()

daemon/k8s_watcher.go
@@ -857,7 +857,10 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 						if Node := k8s.CopyObjToV1Node(obj); Node != nil {
 							valid = true
 							serNodes.Enqueue(func() error {
-								err := d.addK8sNodeV1(Node)
+								err := d.updateK8sNode(nil, Node)
+								if err != nil {
+									log.WithError(err).Warning("Unable to process node addition")
+								}
 								d.K8sEventProcessed(metricNode, metricCreate, err == nil)
 								return nil
 							}, serializer.NoRetry)
@@ -875,7 +878,10 @@ func (d *Daemon) EnableK8sWatcher(queueSize uint) error {
 								}
 
 								serNodes.Enqueue(func() error {
-									err := d.updateK8sNodeV1(oldNode, newNode)
+									err := d.updateK8sNode(oldNode, newNode)
+									if err != nil {
+										log.WithError(err).Warning("Unable to process node update")
+									}
 									d.K8sEventProcessed(metricNode, metricUpdate, err == nil)
 									return nil
 								}, serializer.NoRetry)
@@ -1788,7 +1794,7 @@ func (d *Daemon) updateK8sV1Namespace(oldNS, newNS *types.Namespace) error {
 	return nil
 }
 
-func (d *Daemon) updateK8sNodeTunneling(k8sNodeOld, k8sNodeNew *types.Node) error {
+func (d *Daemon) updateK8sNode(k8sNodeOld, k8sNodeNew *types.Node) error {
 	nodeNew := k8s.ParseNode(k8sNodeNew, source.Kubernetes)
 	// Ignore own node
 	if nodeNew.Name == node.GetName() {
@@ -1849,35 +1855,11 @@ func (d *Daemon) updateK8sNodeTunneling(k8sNodeOld, k8sNodeNew *types.Node) erro
 		}
 	}
 
-	hostKey := node.GetIPsecKeyIdentity()
-	selfOwned := ipcache.IPIdentityCache.Upsert(ciliumIPStrNew, hostIPNew, hostKey, ipcache.Identity{
-		ID:     identity.ReservedIdentityHost,
-		Source: source.Kubernetes,
-	})
-	if !selfOwned {
-		d.nodeDiscovery.Manager.NodeSoftUpdated(*nodeNew)
-		return errIPCacheOwnedByNonK8s
-	}
+	// Kubernetes node resource does not carry the encryption, refer to the
+	// local key configured.
+	nodeNew.EncryptionKey = node.GetIPsecKeyIdentity()
 
-	nodeNew.EncryptionKey = hostKey
 	d.nodeDiscovery.Manager.NodeUpdated(*nodeNew)
-
-	return nil
-}
-
-func (d *Daemon) addK8sNodeV1(k8sNode *types.Node) error {
-	if err := d.updateK8sNodeTunneling(nil, k8sNode); err != nil {
-		log.WithError(err).Warning("Unable to add ipcache entry of Kubernetes node")
-		return err
-	}
-	return nil
-}
-
-func (d *Daemon) updateK8sNodeV1(k8sNodeOld, k8sNodeNew *types.Node) error {
-	if err := d.updateK8sNodeTunneling(k8sNodeOld, k8sNodeNew); err != nil {
-		log.WithError(err).Warning("Unable to update ipcache entry of Kubernetes node")
-		return err
-	}
 	return nil
 }
 
@@ -1888,36 +1870,8 @@ func (d *Daemon) deleteK8sNodeV1(k8sNode *types.Node) error {
 		return nil
 	}
 
-	ip := k8sNode.GetAnnotations()[annotation.CiliumHostIP]
-
-	logger := log.WithFields(logrus.Fields{
-		"K8sNodeName":    k8sNode.ObjectMeta.Name,
-		logfields.IPAddr: ip,
-	})
-
 	d.nodeDiscovery.Manager.NodeDeleted(*oldNode)
 
-	id, exists := ipcache.IPIdentityCache.LookupByIP(ip)
-	if !exists {
-		logger.Debug("identity for Cilium IP not found")
-		return nil
-	}
-
-	// The ipcache entry ownership may have been taken over by a kvstore
-	// based entry in which case we should ignore the delete event and wait
-	// for the kvstore delete event.
-	if id.Source != source.Kubernetes {
-		logger.Debug("ipcache entry for Cilium IP no longer owned by Kubernetes")
-		return nil
-	}
-
-	ipcache.IPIdentityCache.Delete(ip, source.Kubernetes)
-
-	ciliumIP := net.ParseIP(ip)
-	if ciliumIP == nil {
-		logger.Warning("Unable to parse Cilium IP")
-		return nil
-	}
 	return nil
 }
 

pkg/node/store/store.go
@@ -17,14 +17,11 @@ package store
 import (
 	"path"
 
-	"github.com/cilium/cilium/pkg/identity"
-	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
-	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/source"
 )
 
@@ -61,37 +58,6 @@ func (o *NodeObserver) OnUpdate(k store.Key) {
 		nodeCopy := n.DeepCopy()
 		nodeCopy.Source = source.KVStore
 		o.manager.NodeUpdated(*nodeCopy)
-
-		ciliumIPv4 := nodeCopy.GetCiliumInternalIP(false)
-		if ciliumIPv4 != nil {
-			hostIP := nodeCopy.GetNodeIP(false)
-			hostKey := node.GetIPsecKeyIdentity()
-			ipcache.IPIdentityCache.Upsert(ciliumIPv4.String(), hostIP, hostKey, ipcache.Identity{
-				ID:     identity.ReservedIdentityHost,
-				Source: source.KVStore,
-			})
-		}
-
-		if option.Config.EncryptNode {
-			hostIP := nodeCopy.GetNodeIP(false)
-			if hostIP != nil {
-				hostKey := node.GetIPsecKeyIdentity()
-				ipcache.IPIdentityCache.Upsert(hostIP.String(), hostIP, hostKey, ipcache.Identity{
-					ID:     identity.ReservedIdentityHost,
-					Source: source.KVStore,
-				})
-			}
-		}
-
-		ciliumIPv6 := nodeCopy.GetCiliumInternalIP(true)
-		if ciliumIPv6 != nil {
-			hostIP := nodeCopy.GetNodeIP(true)
-			hostKey := node.GetIPsecKeyIdentity()
-			ipcache.IPIdentityCache.Upsert(ciliumIPv6.String(), hostIP, hostKey, ipcache.Identity{
-				ID:     identity.ReservedIdentityHost,
-				Source: source.KVStore,
-			})
-		}
 	}
 }
 
@@ -99,17 +65,7 @@ func (o *NodeObserver) OnDelete(k store.NamedKey) {
 	if n, ok := k.(*node.Node); ok {
 		nodeCopy := n.DeepCopy()
 		nodeCopy.Source = source.KVStore
-
 		o.manager.NodeDeleted(*nodeCopy)
-
-		ciliumIPv4 := nodeCopy.GetCiliumInternalIP(false)
-		if ciliumIPv4 != nil {
-			ipcache.IPIdentityCache.Delete(ciliumIPv4.String(), source.KVStore)
-		}
-		ciliumIPv6 := nodeCopy.GetCiliumInternalIP(true)
-		if ciliumIPv6 != nil {
-			ipcache.IPIdentityCache.Delete(ciliumIPv6.String(), source.KVStore)
-		}
 	}
 }
 
@@ -120,10 +76,6 @@ type NodeRegistrar struct {
 
 // NodeManager is the interface that the manager of nodes has to implement
 type NodeManager interface {
-	// NodeSoftUpdated is called when the store detects a change in the
-	// node that does not require datapath updates.
-	NodeSoftUpdated(n node.Node)
-
 	// NodeUpdated is called when the store detects a change in node
 	// information
 	NodeUpdated(n node.Node)

commit 3d8fe9477bcfee1f63f73b9ecd50a93fbab017ed

bpf: fix routing of cilium_host router ip and health in v6 tunnel mode
Even though the v4 cilium_host as well as health IPs are properly set up
with 192.168.1.120 as tunnel endpoint as well as remote v4+v6 pod addresses,
the cilium_host router IP is not and therefore attempted to be routed
publicly which is incorrect. Same for health by the way. Noticed as BPF
based nodeport replies in v6 via tunnel to cilium_host were correctly
NATed but routed via public interface instead of cilium_vxlan:

Tunnel map:

  # ./cilium/cilium bpf tunnel list
  TUNNEL           VALUE
  10.21.0.0:0      192.168.1.120:0
  2001:db1:3:::0   192.168.1.120:0

Before:

  # ./cilium/cilium bpf ipcache list
  IP PREFIX/ADDRESS                            IDENTITY
  2001:db1:3::c8a4/128                         4 0 0.0.0.0                <-- Remote node's health IPv6 (BUG: no tunnel entry)
  fe80::3090:86ff:fe32:2e11/128                1 0 0.0.0.0
  10.11.39.69/32                               1 0 0.0.0.0
  10.11.118.73/32                              4 0 0.0.0.0
  10.21.251.113/32                             1 0 192.168.1.120          <-- Remote node's cilium_host IPv4
  192.168.1.125/32                             1 0 0.0.0.0
  2001:db1:3::f9a/128                          16777217 0 192.168.1.120   <-- Remote pod IPv6 address
  2001:db1:3::6e04/128                         1 0 0.0.0.0                <-- Remote node's cilium_host [router] IPv6 (BUG: no tunnel entry)
  ::/0                                         2 0 0.0.0.0
  2001:db8:5::9c23/128                         4 0 0.0.0.0
  2a02:120b:2c12:c120:ec4:7aff:fe03:f925/128   1 0 0.0.0.0
  10.21.157.26/32                              16777217 0 192.168.1.120   <-- Remote pod IPv4 address
  10.21.184.75/32                              4 0 192.168.1.120          <-- Remote node's health IPv4
  0.0.0.0/0                                    2 0 0.0.0.0
  2001:db8:5::a36a/128                         1 0 0.0.0.0

After:

  # ./cilium/cilium bpf ipcache list
  IP PREFIX/ADDRESS                            IDENTITY
  10.11.39.69/32                               1 0 0.0.0.0
  10.21.184.75/32                              4 0 192.168.1.120
  192.168.1.125/32                             1 0 0.0.0.0
  2001:db1:3::6e04/128                         1 0 192.168.1.120          <-- Remote node's cilium_host [router] IPv6 via tunnel now
  2001:db1:3::c8a4/128                         4 0 192.168.1.120          <-- Remote node's health IPv6 via tunnel now
  2001:db8:5::9240/128                         4 0 0.0.0.0
  2a02:120b:2c12:c120:ec4:7aff:fe03:f925/128   1 0 0.0.0.0
  fe80::3090:86ff:fe32:2e11/128                1 0 0.0.0.0
  10.11.172.94/32                              4 0 0.0.0.0
  0.0.0.0/0                                    2 0 0.0.0.0
  2001:db1:3::f9a/128                          16777217 0 192.168.1.120
  2001:db8:5::a377/128                         4 0 0.0.0.0
  10.21.251.113/32                             1 0 192.168.1.120
  10.21.157.26/32                              16777217 0 192.168.1.120
  2001:db8:5::a36a/128                         1 0 0.0.0.0
  ::/0                                         2 0 0.0.0.0
  10.11.115.69/32                              4 0 0.0.0.0

BPF NodePort request via cilium_vxlan where backend is local working now:

  23:27:40.224225 IP6 2001:db1:3::6e04.46593 > 2001:db8:5::a36a.30000: Flags [S], seq 1928315317, win 27800, options [mss 1390,sackOK,TS val 1737072991 ecr 0,nop,wscale 7], length 0
  23:27:40.224263 IP6 2001:db8:5::a36a.30000 > 2001:db1:3::6e04.46593: Flags [S.], seq 1813566207, ack 1928315318, win 64260, options [mss 1440,sackOK,TS val 1976342860 ecr 1737072991,nop,wscale 7], length 0
  23:27:40.224501 IP6 2001:db1:3::6e04.46593 > 2001:db8:5::a36a.30000: Flags [.], ack 1, win 218, options [nop,nop,TS val 1737072992 ecr 1976342860], length 0
  [...]

Fixes: ae8ab3682c37 ("node: Update ipcache entries independent of node update source")

pkg/node/manager/manager.go
@@ -283,9 +283,10 @@ func (m *Manager) NodeUpdated(n node.Node) {
 
 	for _, address := range n.IPAddresses {
 		// Map the Cilium internal IP to the reachable node IP so it
-		// can be routed via the overlay
+		// can be routed via the overlay. Routing via overlay is always
+		// done via public v4 address hence n.GetNodeIP(false).
 		if address.Type == addressing.NodeCiliumInternalIP {
-			nodeIP = n.GetNodeIP(address.IP.To4() == nil)
+			nodeIP = n.GetNodeIP(false)
 			if address.IP.To4() != nil {
 				nodeIP4 = nodeIP
 			}
@@ -326,8 +327,7 @@ func (m *Manager) NodeUpdated(n node.Node) {
 		if address == nil {
 			continue
 		}
-		isIPv6 := address.To4() == nil
-		isOwning := ipcache.IPIdentityCache.Upsert(address.String(), n.GetNodeIP(isIPv6), n.EncryptionKey, ipcache.Identity{
+		isOwning := ipcache.IPIdentityCache.Upsert(address.String(), n.GetNodeIP(false), n.EncryptionKey, ipcache.Identity{
 			ID:     identity.ReservedIdentityHealth,
 			Source: n.Source,
 		})