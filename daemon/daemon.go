g0odi3z2j/cilium

commit de3522cdccd54e0026c49b1965cf99c46f30ba88

cilium: only start daemon's monitoring agent after base datapath setup
The latter is supposed to set up cilium_events, only then we can
fetch the pinned map from BPF fs.

The NewAgent() basically sets up the unix domain socket file under
unix:///var/run/cilium/monitor1_2.sock, and once connection got
accepted, it's reading out BPF perf event buffer (logs "Beginning
to read perf buffer" to the agent). Then it tries to fetch cilium_events
map from BPF fs, but if this happens at a time before init.sh was
run, agent will bail out with an error:

  level=fatal msg="Cannot initialise BPF perf ring buffer sockets" error="Unable to get object /sys/fs/bpf/tc/globals/cilium_events: no such file or directory" startTime="2019-07-24 18:13:00.920286144 +0000 UTC m=+1.662936719" subsys=monitor-agent

Move this to after d.init() such that it is able to open cilium_events.

Fixes: #8671
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

daemon/daemon.go
@@ -772,14 +772,6 @@ func NewDaemon(dp datapath.Datapath, iptablesManager rulesManager) (*Daemon, *en
 		iptablesManager:   iptablesManager,
 		endpointManager:   epMgr,
 	}
-
-	if option.Config.RunMonitorAgent {
-		monitorAgent, err := monitoragent.NewAgent(context.TODO(), defaults.MonitorBufferPages)
-		if err != nil {
-			return nil, nil, err
-		}
-		d.monitorAgent = monitorAgent
-	}
 	bootstrapStats.daemonInit.End(true)
 
 	// Open or create BPF maps.
@@ -914,6 +906,14 @@ func NewDaemon(dp datapath.Datapath, iptablesManager rulesManager) (*Daemon, *en
 
 	bootstrapStats.bpfBase.Start()
 	err = d.init()
+	// We can only start monitor agent once cilium_event has been set up.
+	if option.Config.RunMonitorAgent {
+		monitorAgent, err := monitoragent.NewAgent(context.TODO(), defaults.MonitorBufferPages)
+		if err != nil {
+			return nil, nil, err
+		}
+		d.monitorAgent = monitorAgent
+	}
 	bootstrapStats.bpfBase.EndError(err)
 	if err != nil {
 		log.WithError(err).Error("Error while initializing daemon")

commit 9832af88f2550bc51782f14c6d47c7e5418f6dfd

daemon: check error from `d.init()`
Commit de3522cdccd54e0026c49b1965cf99c46f30ba88 shuffled code around such that
the `err` variable from `d.init()` was not checked until after the call to
`monitoragent.NewAgent()`. While the error itself is not overwritten ( see
https://play.golang.org/p/Q-HzFMwhTAy for an example ), we still need to check
the error before starting the monitor agent, as it assumes that the base
programs were successfully installed, which we were not checking.

Fixes: de3522cdccd54e0026c49b1965cf99c46f30ba88

Signed-off by: Ian Vernon <ian@cilium.io>

daemon/daemon.go
@@ -491,6 +491,12 @@ func NewDaemon(dp datapath.Datapath, iptablesManager rulesManager) (*Daemon, *en
 
 	bootstrapStats.bpfBase.Start()
 	err = d.init()
+	bootstrapStats.bpfBase.EndError(err)
+	if err != nil {
+		log.WithError(err).Error("Error while initializing daemon")
+		return nil, restoredEndpoints, err
+	}
+
 	// We can only start monitor agent once cilium_event has been set up.
 	if option.Config.RunMonitorAgent {
 		monitorAgent, err := monitoragent.NewAgent(context.TODO(), defaults.MonitorBufferPages)
@@ -498,11 +504,6 @@ func NewDaemon(dp datapath.Datapath, iptablesManager rulesManager) (*Daemon, *en
 			return nil, nil, err
 		}
 		d.monitorAgent = monitorAgent
-	}
-	bootstrapStats.bpfBase.EndError(err)
-	if err != nil {
-		log.WithError(err).Error("Error while initializing daemon")
-		return nil, restoredEndpoints, err
 	}
 	if err := loader.RestoreTemplates(option.Config.StateDir); err != nil {
 		log.WithError(err).Error("Unable to restore previous BPF templates")