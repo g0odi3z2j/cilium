g0odi3z2j/cilium

commit 2cc52deea37f9dd35e7302e402046f7e0ce67481

cilium: populate wildcard src->dst policy for ipsec
Policy/State rules are populated when we receive kvstore or k8s
events. On egress we must match the (dst_addr, spi) tuple via the xfrm
policy and then link this to a state using the tmpl including the
same (dst_addr, spi). This results in a pair of policy/state rules
per node on the host. The above rules also specify a src address
so that we only match Cilium managed traffic. This includes the
IP address on cilium_host and the endpoint allocation range.

On the ingress side we have been doing the same but swapped. The
tuple (src, dst, spi) is used in the policy to match traffic and
then linked to the state via the tmpl. These are also added on
update events. However, this creates more rule than are actually
needed. By wildcarding the source on ingress traffic we can
match all traffic for the node with a single rule. Further the
existing scheme is buggy and dst-ip/spi pairs can collide.

The fix is to insert a wildcard decrypt rule when the local node
update event is received. Then on node update events only
add the egress rule.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/linux/node.go
@@ -32,6 +32,11 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
+var (
+	wildcardIPv4 = "0.0.0.0"
+	wildcardIPv6 = "0::0"
+)
+
 type linuxNodeHandler struct {
 	mutex          lock.Mutex
 	isInitialized  bool
@@ -429,12 +434,18 @@ func (n *linuxNodeHandler) enableIPsec(newNode *node.Node) {
 		new4Net := &net.IPNet{IP: newNode.IPv4AllocCIDR.IP, Mask: newNode.IPv4AllocCIDR.Mask}
 		if newNode.IsLocal() {
 			n.replaceNodeIPSecInRoute(new4Net)
+			ciliumInternalIPv4 := newNode.GetCiliumInternalIP(false)
+			if ciliumInternalIPv4 != nil {
+				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv4().Router(), Mask: n.nodeAddressing.IPv4().AllocationCIDR().Mask}
+				ipsecIPv4Wildcard := &net.IPNet{IP: net.ParseIP(wildcardIPv4), Mask: net.IPv4Mask(0, 0, 0, 0)}
+				ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecIPv4Wildcard, linux_defaults.IPSecEndpointSPI, ipsec.IPSecDirIn)
+			}
 		} else {
 			if ciliumInternalIPv4 := newNode.GetCiliumInternalIP(false); ciliumInternalIPv4 != nil {
 				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv4().Router(), Mask: n.nodeAddressing.IPv4().AllocationCIDR().Mask}
 				ipsecRemote := &net.IPNet{IP: ciliumInternalIPv4, Mask: newNode.IPv4AllocCIDR.Mask}
 				n.replaceNodeIPSecOutRoute(new4Net)
-				err := ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecRemote, linux_defaults.IPSecEndpointSPI)
+				err := ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecRemote, linux_defaults.IPSecEndpointSPI, ipsec.IPSecDirOut)
 				if err != nil {
 					log.WithError(err).Errorf("Upsert local %s remote %s", ipsecLocal, ipsecRemote)
 				} else {
@@ -449,15 +460,21 @@ func (n *linuxNodeHandler) enableIPsec(newNode *node.Node) {
 		if newNode.IsLocal() {
 			n.replaceHostRules()
 			n.replaceNodeIPSecInRoute(new6Net)
+			ciliumInternalIPv6 := newNode.GetCiliumInternalIP(true)
+			if ciliumInternalIPv6 != nil {
+				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv6().Router(), Mask: n.nodeAddressing.IPv6().AllocationCIDR().Mask}
+				ipsecIPv6Wildcard := &net.IPNet{IP: net.ParseIP(wildcardIPv6), Mask: net.CIDRMask(0, 0)}
+				ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecIPv6Wildcard, linux_defaults.IPSecEndpointSPI, ipsec.IPSecDirIn)
+			}
 		} else {
 			if ciliumInternalIPv6 := newNode.GetCiliumInternalIP(true); ciliumInternalIPv6 != nil {
 				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv6().Router(), Mask: n.nodeAddressing.IPv6().AllocationCIDR().Mask}
 				ipsecRemote := &net.IPNet{IP: ciliumInternalIPv6, Mask: newNode.IPv6AllocCIDR.Mask}
 				ipsecHost := &net.IPNet{IP: n.nodeAddressing.IPv6().PrimaryExternal(), Mask: n.nodeAddressing.IPv6().AllocationCIDR().Mask}
 				n.replaceNodeIPSecOutRoute(new6Net)
-				ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecRemote, linux_defaults.IPSecEndpointSPI)
+				ipsec.UpsertIPSecEndpoint(ipsecLocal, ipsecRemote, linux_defaults.IPSecEndpointSPI, ipsec.IPSecDirOut)
 				if !ipsecHost.IP.Equal(ipsecLocal.IP) {
-					ipsec.UpsertIPSecEndpoint(ipsecHost, ipsecRemote, linux_defaults.IPSecNodeSPI)
+					ipsec.UpsertIPSecEndpoint(ipsecHost, ipsecRemote, linux_defaults.IPSecNodeSPI, ipsec.IPSecDirOut)
 				}
 			}
 		}

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -33,6 +33,14 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type IPSecDir string
+
+const (
+	IPSecDirIn   IPSecDir = "IPSEC_IN"
+	IPSecDirOut  IPSecDir = "IPSEC_OUT"
+	IPSecDirBoth IPSecDir = "IPSEC_BOTH"
+)
+
 type ipSecKey struct {
 	Spi   int
 	ReqID int
@@ -206,7 +214,7 @@ func ipSecDeletePolicy(src, local net.IP) error {
  * state space. Basic idea would be to reference a state using any key generated
  * from BPF program allowing for a single state per security ctx.
  */
-func UpsertIPSecEndpoint(local, remote *net.IPNet, spi int) error {
+func UpsertIPSecEndpoint(local, remote *net.IPNet, spi int, dir IPSecDir) error {
 	/* TODO: state reference ID is (dip,spi) which can be duplicated in the current global
 	 * mode. The duplication is on _all_ ingress states because dst_ip == host_ip in this
 	 * case and only a single spi entry is in use. Currently no check is done to avoid
@@ -219,24 +227,30 @@ func UpsertIPSecEndpoint(local, remote *net.IPNet, spi int) error {
 	 * transparent mode ciliumIP == nil case must also be handled.
 	 */
 	if !local.IP.Equal(remote.IP) {
-		if err := ipSecReplaceState(local.IP, remote.IP, spi); err != nil {
-			if !os.IsExist(err) {
-				return fmt.Errorf("unable to replace local state: %s", err)
+		if dir == IPSecDirIn || dir == IPSecDirBoth {
+			if err := ipSecReplaceState(local.IP, remote.IP, spi); err != nil {
+				if !os.IsExist(err) {
+					return fmt.Errorf("unable to replace local state: %s", err)
+				}
+			}
+			if err := ipSecReplacePolicyIn(remote, local); err != nil {
+				if !os.IsExist(err) {
+					return fmt.Errorf("unable to replace policy in: %s", err)
+				}
 			}
 		}
-		if err := ipSecReplaceState(remote.IP, local.IP, spi); err != nil {
-			if !os.IsExist(err) {
-				return fmt.Errorf("unable to replace remote state: %s", err)
+
+		if dir == IPSecDirOut || dir == IPSecDirBoth {
+			if err := ipSecReplaceState(remote.IP, local.IP, spi); err != nil {
+				if !os.IsExist(err) {
+					return fmt.Errorf("unable to replace remote state: %s", err)
+				}
 			}
-		}
-		if err := ipSecReplacePolicyOut(local, remote); err != nil {
-			if !os.IsExist(err) {
-				return fmt.Errorf("unable to replace policy out: %s", err)
-			}
-		}
-		if err := ipSecReplacePolicyIn(remote, local); err != nil {
-			if !os.IsExist(err) {
-				return fmt.Errorf("unable to replace policy in: %s", err)
+
+			if err := ipSecReplacePolicyOut(local, remote); err != nil {
+				if !os.IsExist(err) {
+					return fmt.Errorf("unable to replace policy out: %s", err)
+				}
 			}
 		}
 	}

commit 555d5bf708fc4ab36440b8cc79ab2b0007085048

cilium: encryption, replace Router() IP with CiliumInternal
Apparently I thought the RouterIP _should_ be the same as the CiliumInternalIP
for a local Node. And this is usually the case, but it is not required. We
have had a couple reports of cases where encryption was not able to connect
because of this.

Fix by using the CiliumInternalIP in all cases.

Fixes: 2cc52deea37f9 ("cilium: populate wildcard src->dst policy for ipsec")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/linux/node.go
@@ -624,7 +624,7 @@ func (n *linuxNodeHandler) enableIPsec(newNode *node.Node) {
 			n.replaceNodeIPSecInRoute(new4Net)
 			ciliumInternalIPv4 := newNode.GetCiliumInternalIP(false)
 			if ciliumInternalIPv4 != nil {
-				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv4().Router(), Mask: n.nodeAddressing.IPv4().AllocationCIDR().Mask}
+				ipsecLocal := &net.IPNet{IP: ciliumInternalIPv4, Mask: n.nodeAddressing.IPv4().AllocationCIDR().Mask}
 				ipsecIPv4Wildcard := &net.IPNet{IP: net.ParseIP(wildcardIPv4), Mask: net.IPv4Mask(0, 0, 0, 0)}
 				spi, err = ipsec.UpsertIPsecEndpoint(ipsecLocal, ipsecIPv4Wildcard, ipsec.IPSecDirIn)
 				upsertIPsecLog(err, "local IPv4", ipsecLocal, ipsecIPv4Wildcard, spi)
@@ -646,7 +646,7 @@ func (n *linuxNodeHandler) enableIPsec(newNode *node.Node) {
 			n.replaceNodeIPSecInRoute(new6Net)
 			ciliumInternalIPv6 := newNode.GetCiliumInternalIP(true)
 			if ciliumInternalIPv6 != nil {
-				ipsecLocal := &net.IPNet{IP: n.nodeAddressing.IPv6().Router(), Mask: n.nodeAddressing.IPv6().AllocationCIDR().Mask}
+				ipsecLocal := &net.IPNet{IP: ciliumInternalIPv6, Mask: n.nodeAddressing.IPv6().AllocationCIDR().Mask}
 				ipsecIPv6Wildcard := &net.IPNet{IP: net.ParseIP(wildcardIPv6), Mask: net.CIDRMask(0, 0)}
 				spi, err = ipsec.UpsertIPsecEndpoint(ipsecLocal, ipsecIPv6Wildcard, ipsec.IPSecDirIn)
 				upsertIPsecLog(err, "local IPv6", ipsecLocal, ipsecIPv6Wildcard, spi)