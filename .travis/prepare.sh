g0odi3z2j/cilium

commit f414a944687504cb94ad4037ba73f8170ee0489d

vendor: add go-mod support
As a consequence of doing updates for all dependencies some changes had
to be performed:
- pkg/aws/ec2/ec2.go add context to function calls
- pkg/kvstore change import path from go.etcd.io/etcd to github.com/coreos/etcd
- add tools package to import non-go dependencies into vendor.
- add documentation to update dependencies

Signed-off-by: André Martins <andre@cilium.io>

.travis/prepare.sh
@@ -14,7 +14,6 @@ rm $CLANG_FILE
 NEWPATH="/usr/local/clang/bin"
 export PATH="$NEWPATH:$PATH"
 
-go get -u github.com/jteeuwen/go-bindata/...
+go get github.com/cilium/go-bindata/go-bindata
 go get golang.org/x/tools/cmd/cover
 go get github.com/mattn/goveralls
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh

Makefile
@@ -6,9 +6,9 @@ ifdef LIBNETWORK_PLUGIN
 SUBDIRS_CILIUM_CONTAINER += plugins/cilium-docker
 endif
 SUBDIRS := $(SUBDIRS_CILIUM_CONTAINER) operator plugins tools
-GOFILES_EVAL := $(subst _$(ROOT_DIR)/,,$(shell $(CGO_DISABLED) $(GO) list ./... | grep -v -e /vendor/ -e /contrib/))
+GOFILES_EVAL := $(subst _$(ROOT_DIR)/,,$(shell $(CGO_DISABLED) $(GOLIST) $(GO) list -e ./...))
 GOFILES ?= $(GOFILES_EVAL)
-TESTPKGS_EVAL := $(subst github.com/cilium/cilium/,,$(shell $(CGO_DISABLED) $(GO) list ./... | grep -v '/api/v1\|/vendor\|/contrib' | grep -v -P 'test(?!/helpers/logutils)'))
+TESTPKGS_EVAL := $(subst github.com/cilium/cilium/,,$(shell $(CGO_DISABLED) $(GOLIST) $(GO) list -e ./... | grep -v '/api/v1\|/vendor\|/contrib' | grep -v -P 'test(?!/helpers/logutils)'))
 TESTPKGS ?= $(TESTPKGS_EVAL)
 GOLANGVERSION := $(shell $(GO) version 2>/dev/null | grep -Eo '(go[0-9].[0-9])')
 GOLANG_SRCFILES := $(shell for pkg in $(subst github.com/cilium/cilium/,,$(GOFILES)); do find $$pkg -name *.go -print; done | grep -v vendor | sort | uniq)
@@ -97,7 +97,7 @@ tests-privileged:
 	# cilium-map-migrate is a dependency of some unit tests.
 	$(MAKE) -C bpf cilium-map-migrate
 	$(QUIET)$(foreach pkg,$(PRIV_TEST_PKGS),\
-		$(GO) test $(TEST_LDFLAGS) github.com/cilium/cilium/$(pkg) $(GOTEST_PRIV_OPTS) || exit 1;)
+		$(GO) test $(GOFLAGS) $(TEST_LDFLAGS) github.com/cilium/cilium/$(pkg) $(GOTEST_PRIV_OPTS) || exit 1;)
 
 start-kvstores:
 ifeq ($(SKIP_KVSTORES),"false")
@@ -155,7 +155,7 @@ endif
 	$(QUIET) echo "mode: count" > coverage-all-tmp.out
 	$(QUIET) echo "mode: count" > coverage.out
 	$(QUIET)$(foreach pkg,$(patsubst %,github.com/cilium/cilium/%,$(TESTPKGS)),\
-		$(GO) test $(TEST_UNITTEST_LDFLAGS) $(pkg) $(GOTEST_BASE) $(GOTEST_COVER_OPTS) || exit 1; \
+		$(GO) test $(GOFLAGS) $(TEST_UNITTEST_LDFLAGS) $(pkg) $(GOTEST_BASE) $(GOTEST_COVER_OPTS) || exit 1; \
 		tail -n +2 coverage.out >> coverage-all-tmp.out;)
 	$(MAKE) generate-cov
 	@rmdir ./daemon/1 ./daemon/1_backup 2> /dev/null || true
@@ -163,14 +163,14 @@ endif
 
 bench: start-kvstores
 	$(QUIET)$(foreach pkg,$(patsubst %,github.com/cilium/cilium/%,$(TESTPKGS)),\
-		$(GO) test $(TEST_UNITTEST_LDFLAGS) $(GOTEST_BASE) $(BENCHFLAGS) \
+		$(GO) test $(GOFLAGS) $(TEST_UNITTEST_LDFLAGS) $(GOTEST_BASE) $(BENCHFLAGS) \
 			$(pkg) \
 		|| exit 1;)
 	$(MAKE) stop-kvstores
 
 bench-privileged:
 	$(QUIET)$(foreach pkg,$(patsubst %,github.com/cilium/cilium/%,$(TESTPKGS)),\
-		$(GO) test $(TEST_UNITTEST_LDFLAGS) $(GOTEST_BASE) $(BENCHFLAGS) \
+		$(GO) test $(GOFLAGS) $(TEST_UNITTEST_LDFLAGS) $(GOTEST_BASE) $(BENCHFLAGS) \
 			-tags=privileged_tests $(pkg) \
 		|| exit 1;)
 
@@ -268,7 +268,15 @@ generate-health-api: api/v1/health/openapi.yaml
 	-$(SWAGGER) generate client -a restapi \
 		-t api/v1 -t api/v1/health/ -f api/v1/health/openapi.yaml
 
-generate-k8s-api:
+revert-k8s-generator-patch:
+	patch -p1 -R < hack/82410.patch
+
+apply-k8s-generator-patch:
+	patch -p1 < hack/82410.patch
+
+generate-k8s-api: apply-k8s-generator-patch generate-k8s-api-patch revert-k8s-generator-patch
+
+generate-k8s-api-patch:
 	$(call generate_k8s_api_all,github.com/cilium/cilium/pkg/k8s/apis,"cilium.io:v2")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium/pkg,"policy:api")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium,"pkg:node")
@@ -335,7 +343,7 @@ govet:
     ./tools/...
 
 ineffassign:
-	@$(ECHO_CHECK) ineffassign
+	@$(ECHO_CHECK) $(GOFILES)
 	$(QUIET) ineffassign .
 
 logging-subsys-field:

bugtool/Makefile
@@ -25,7 +25,7 @@ all: $(TARGET)
 clean:
 	@$(ECHO_CLEAN)
 	-$(QUIET)rm -f .cilium-bugtool.config $(TARGET)
-	$(QUIET)$(GO) clean
+	$(QUIET)$(GO) clean $(GOCLEAN)
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)

cilium-health/Makefile
@@ -20,7 +20,7 @@ clean:
 	@$(ECHO_CLEAN)
 	$(QUIET)for i in $(SUBDIRS); do $(MAKE) -C $$i clean; done
 	-$(QUIET)rm -f $(TARGET)
-	$(QUIET)$(GO) clean
+	$(QUIET)$(GO) clean $(GOCLEAN)
 
 install:
 	$(QUIET)for i in $(SUBDIRS); do $(MAKE) -C $$i install; done

cilium-health/responder/Makefile
@@ -11,7 +11,7 @@ all: $(TARGET)
 clean:
 	@$(ECHO_CLEAN)
 	-$(QUIET)rm -f $(TARGET)
-	$(QUIET)$(GO) clean
+	$(QUIET)$(GO) clean $(GOCLEAN)
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)

cilium/Makefile
@@ -14,7 +14,7 @@ all: $(TARGET)
 clean:
 	@$(ECHO_CLEAN) $(notdir $(shell pwd))
 	-$(QUIET)rm -f $(TARGET)
-	$(QUIET)$(GO) clean
+	$(QUIET)$(GO) clean $(GOCLEAN)
 
 install:
 	$(INSTALL) -m 0755 -d $(DESTDIR)$(BINDIR)

daemon/Makefile
@@ -28,7 +28,7 @@ links:
 clean:
 	@$(ECHO_CLEAN)
 	$(QUIET)rm -f $(TARGET)
-	$(QUIET)$(GO) clean
+	$(QUIET)$(GO) clean $(GOCLEAN)
 	$(foreach link,$(LINKS), rm -f $(link);)
 
 ifeq ("$(PKG_BUILD)","")

operator/Makefile
@@ -12,7 +12,7 @@ all: $(TARGET)
 clean:
 	@$(ECHO_CLEAN)
 	$(QUIET)rm -f $(TARGET)
-	$(GO) clean
+	$(GO) clean $(GOCLEAN)
 
 install:
 	groupadd -f cilium

commit 9e7d61f8cfaeb238fb53f4b655cae412ef5fa4dd

.travis: disable go modules download when installing dependencies
Also use the GOLIST environment variable which is used for all go
sub-commands that make go module to download all dependencies.

Fixes: f414a9446875 ("vendor: add go-mod support")
Signed-off-by: André Martins <andre@cilium.io>

.travis/prepare.sh
@@ -14,6 +14,7 @@ rm $CLANG_FILE
 NEWPATH="/usr/local/clang/bin"
 export PATH="$NEWPATH:$PATH"
 
-go get github.com/cilium/go-bindata/go-bindata
-go get golang.org/x/tools/cmd/cover
-go get github.com/mattn/goveralls
+# disable go modules to avoid downloading all dependencies when doing go get
+GO111MODULE=off go get github.com/cilium/go-bindata/go-bindata
+GO111MODULE=off go get golang.org/x/tools/cmd/cover
+GO111MODULE=off go get github.com/mattn/goveralls

Makefile
@@ -323,7 +323,7 @@ gofmt:
 
 govet:
 	@$(ECHO_CHECK) vetting all GOFILES...
-	$(QUIET)$(GO) vet \
+	$(QUIET) $(GOLIST) $(GO) vet \
     ./api/... \
     ./bugtool/... \
     ./cilium/... \