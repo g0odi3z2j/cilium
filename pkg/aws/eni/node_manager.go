g0odi3z2j/cilium

commit b2b95dd1028aa2685bce524dfcdc74c400027864

eni: Support for parallel workers
The latency of the EC2 and Kubernetes API calls can quickly become the
bottleneck as it can serialize required operations longer than what the rate
limiter would allow. Allow configuring a number of parallel workers for both
deficit resolution and custom resource updates.

```
PASS: node_manager_test.go:401: ENISuite.BenchmarkAllocDelay20Worker1	      50	  61034990 ns/op
PASS: node_manager_test.go:404: ENISuite.BenchmarkAllocDelay20Worker10	      50	  41070433 ns/op
PASS: node_manager_test.go:407: ENISuite.BenchmarkAllocDelay20Worker50	      20	  52750255 ns/op
PASS: node_manager_test.go:410: ENISuite.BenchmarkAllocDelay50Worker1	      20	 105488723 ns/op
PASS: node_manager_test.go:413: ENISuite.BenchmarkAllocDelay50Worker10	      20	  55290044 ns/op
PASS: node_manager_test.go:416: ENISuite.BenchmarkAllocDelay50Worker50	      20	  55462961 ns/op
```

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/aws/eni/node_manager.go
@@ -16,6 +16,7 @@
 package eni
 
 import (
+	"context"
 	"fmt"
 	"sort"
 	"time"
@@ -26,6 +27,7 @@ import (
 	"github.com/cilium/cilium/pkg/trigger"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/semaphore"
 )
 
 type k8sAPI interface {
@@ -71,31 +73,46 @@ type NodeManager struct {
 	metricsAPI      metricsAPI
 	resyncTrigger   *trigger.Trigger
 	deficitResolver *trigger.Trigger
+	parallelWorkers int64
 }
 
 // NewNodeManager returns a new NodeManager
-func NewNodeManager(instancesAPI nodeManagerAPI, ec2API ec2API, k8sAPI k8sAPI, metrics metricsAPI) (*NodeManager, error) {
+func NewNodeManager(instancesAPI nodeManagerAPI, ec2API ec2API, k8sAPI k8sAPI, metrics metricsAPI, parallelWorkers int64) (*NodeManager, error) {
+	if parallelWorkers < 1 {
+		parallelWorkers = 1
+	}
+
 	mngr := &NodeManager{
-		nodes:        nodeMap{},
-		instancesAPI: instancesAPI,
-		ec2API:       ec2API,
-		k8sAPI:       k8sAPI,
-		metricsAPI:   metrics,
+		nodes:           nodeMap{},
+		instancesAPI:    instancesAPI,
+		ec2API:          ec2API,
+		k8sAPI:          k8sAPI,
+		metricsAPI:      metrics,
+		parallelWorkers: parallelWorkers,
 	}
 
 	deficitResolver, err := trigger.NewTrigger(trigger.Parameters{
 		Name:        "eni-node-manager-deficit-resolver",
 		MinInterval: time.Second,
 		TriggerFunc: func(reasons []string) {
+			sem := semaphore.NewWeighted(parallelWorkers)
+
 			for _, name := range reasons {
-				if node := mngr.Get(name); node != nil {
-					if err := node.ResolveIPDeficit(); err != nil {
-						node.logger().WithError(err).Warning("Unable to resolve IP deficit of node")
+				sem.Acquire(context.TODO(), 1)
+				go func(name string) {
+					if node := mngr.Get(name); node != nil {
+						if err := node.ResolveIPDeficit(); err != nil {
+							node.logger().WithError(err).Warning("Unable to resolve IP deficit of node")
+						}
+					} else {
+						log.WithField(fieldName, name).Warning("Node has disappeared while allocation request was queued")
 					}
-				} else {
-					log.WithField(fieldName, name).Warning("Node has disappeared while allocation request was queued")
-				}
+					sem.Release(1)
+				}(name)
 			}
+			// Acquire the full semaphore, this requires all go routines to
+			// complete and thus blocks until all nodes are synced
+			sem.Acquire(context.TODO(), parallelWorkers)
 		},
 	})
 	if err != nil {
@@ -189,44 +206,71 @@ func (n *NodeManager) GetNodesByNeededAddresses() []*Node {
 	return list
 }
 
+type resyncStats struct {
+	mutex               lock.Mutex
+	totalUsed           int
+	totalAvailable      int
+	totalNeeded         int
+	remainingInterfaces int
+	nodesAtCapacity     int
+}
+
+func (n *NodeManager) resyncNode(node *Node, stats *resyncStats) {
+	node.mutex.Lock()
+
+	// Resync() is always called after resync of the instance data,
+	// mark node as resynced
+	node.resyncNeeded = false
+	allocationNeeded := node.recalculateLocked()
+	node.loggerLocked().WithFields(logrus.Fields{
+		fieldName:   node.name,
+		"available": node.stats.availableIPs,
+		"used":      node.stats.usedIPs,
+	}).Debug("Recalculated allocation requirements")
+
+	stats.mutex.Lock()
+	stats.totalUsed += node.stats.usedIPs
+	availableOnNode := node.stats.availableIPs - node.stats.usedIPs
+	stats.totalAvailable += availableOnNode
+	stats.totalNeeded += node.stats.neededIPs
+	stats.remainingInterfaces += node.stats.remainingInterfaces
+
+	if node.stats.remainingInterfaces == 0 && availableOnNode == 0 {
+		stats.nodesAtCapacity++
+	}
+	stats.mutex.Unlock()
+
+	if allocationNeeded && node.stats.remainingInterfaces > 0 {
+		n.deficitResolver.TriggerWithReason(node.name)
+	}
+	node.mutex.Unlock()
+
+	node.SyncToAPIServer()
+}
+
 // Resync will attend all nodes and resolves IP deficits. The order of
 // attendance is defined by the number of IPs needed to reach the configured
 // watermarks. Any updates to the node resource are synchronized to the
 // Kubernetes apiserver.
 func (n *NodeManager) Resync() {
-	var totalUsed, totalAvailable, totalNeeded, remainingInterfaces, nodesAtCapacity int
+	stats := resyncStats{}
+	sem := semaphore.NewWeighted(n.parallelWorkers)
 
 	for _, node := range n.GetNodesByNeededAddresses() {
-		node.mutex.Lock()
-		// Resync() is always called after resync of the instance data,
-		// mark node as resynced
-		node.resyncNeeded = false
-		allocationNeeded := node.recalculateLocked()
-		node.loggerLocked().WithFields(logrus.Fields{
-			fieldName:   node.name,
-			"available": node.stats.availableIPs,
-			"used":      node.stats.usedIPs,
-		}).Debug("Recalculated allocation requirements")
-		totalUsed += node.stats.usedIPs
-		availableOnNode := node.stats.availableIPs - node.stats.usedIPs
-		totalAvailable += availableOnNode
-		totalNeeded += node.stats.neededIPs
-		remainingInterfaces += node.stats.remainingInterfaces
-
-		if node.stats.remainingInterfaces == 0 && availableOnNode == 0 {
-			nodesAtCapacity++
-		}
-		if allocationNeeded && node.stats.remainingInterfaces > 0 {
-			n.deficitResolver.TriggerWithReason(node.name)
-		}
-		node.mutex.Unlock()
-
-		node.SyncToAPIServer()
+		sem.Acquire(context.TODO(), 1)
+		go func(node *Node, stats *resyncStats) {
+			n.resyncNode(node, stats)
+			sem.Release(1)
+		}(node, &stats)
 	}
 
-	n.metricsAPI.SetAllocatedIPs("used", totalUsed)
-	n.metricsAPI.SetAllocatedIPs("available", totalAvailable)
-	n.metricsAPI.SetAllocatedIPs("needed", totalNeeded)
-	n.metricsAPI.SetAvailableENIs(remainingInterfaces)
-	n.metricsAPI.SetNodesAtCapacity(nodesAtCapacity)
+	// Acquire the full semaphore, this requires all go routines to
+	// complete and thus blocks until all nodes are synced
+	sem.Acquire(context.TODO(), n.parallelWorkers)
+
+	n.metricsAPI.SetAllocatedIPs("used", stats.totalUsed)
+	n.metricsAPI.SetAllocatedIPs("available", stats.totalAvailable)
+	n.metricsAPI.SetAllocatedIPs("needed", stats.totalNeeded)
+	n.metricsAPI.SetAvailableENIs(stats.remainingInterfaces)
+	n.metricsAPI.SetNodesAtCapacity(stats.nodesAtCapacity)
 }

operator/eni.go
@@ -114,7 +114,7 @@ func startENIAllocator() error {
 	ec2Client := ec2shim.NewClient(ec2.New(cfg), eniMetrics, 4.0, 2)
 	log.Info("Connected to EC2 service API")
 	instances := eni.NewInstancesManager(ec2Client, eniMetrics)
-	nodeManager, err = eni.NewNodeManager(instances, ec2Client, &k8sAPI{}, eniMetrics)
+	nodeManager, err = eni.NewNodeManager(instances, ec2Client, &k8sAPI{}, eniMetrics, eniParallelWorkers)
 	if err != nil {
 		return fmt.Errorf("unable to initialize ENI node manager: %s", err)
 	}

operator/main.go
@@ -63,6 +63,7 @@ var (
 	synchronizeNodes    bool
 	enableMetrics       bool
 	metricsAddress      string
+	eniParallelWorkers  int64
 
 	ciliumK8sClient clientset.Interface
 )
@@ -113,6 +114,7 @@ func init() {
 	flags.BoolVar(&enableCepGC, "cilium-endpoint-gc", true, "Enable CiliumEndpoint garbage collector")
 	flags.DurationVar(&identityGCInterval, "identity-gc-interval", time.Minute*10, "GC interval for security identities")
 	flags.DurationVar(&kvNodeGCInterval, "nodes-gc-interval", time.Minute*2, "GC interval for nodes store in the kvstore")
+	flags.Int64Var(&eniParallelWorkers, "eni-parallel-workers", 50, "Maximum number of parallel workers used by ENI allocator")
 
 	flags.IntVar(&unmanagedKubeDnsWatcherInterval, "unmanaged-pod-watcher-interval", 15, "Interval to check for unmanaged kube-dns pods (0 to disable)")
 

pkg/aws/eni/node_manager_test.go
@@ -45,7 +45,7 @@ var (
 
 func (e *ENISuite) TestGetNodeNames(c *check.C) {
 	ec2api := ec2mock.NewAPI([]*types.Subnet{testSubnet})
-	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi)
+	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi, 10)
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
 
@@ -69,7 +69,7 @@ func (e *ENISuite) TestGetNodeNames(c *check.C) {
 
 func (e *ENISuite) TestNodeManagerGet(c *check.C) {
 	ec2api := ec2mock.NewAPI([]*types.Subnet{testSubnet})
-	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi)
+	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi, 10)
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
 
@@ -153,7 +153,7 @@ func reachedAddressesNeeded(mngr *NodeManager, nodeName string, needed int) (suc
 // - PreAllocate 0 (default: 8)
 func (e *ENISuite) TestNodeManagerDefaultAllocation(c *check.C) {
 	ec2api := ec2mock.NewAPI([]*types.Subnet{testSubnet})
-	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi)
+	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi, 10)
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
 
@@ -184,7 +184,7 @@ func (e *ENISuite) TestNodeManagerDefaultAllocation(c *check.C) {
 // - PreAllocate -1
 func (e *ENISuite) TestNodeManagerMinAllocate20(c *check.C) {
 	ec2api := ec2mock.NewAPI([]*types.Subnet{testSubnet})
-	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi)
+	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi, 10)
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
 
@@ -224,7 +224,7 @@ func (e *ENISuite) TestNodeManagerMinAllocate20(c *check.C) {
 // - PreAllocate 1
 func (e *ENISuite) TestNodeManagerMinAllocateAndPreallocate(c *check.C) {
 	ec2api := ec2mock.NewAPI([]*types.Subnet{testSubnet})
-	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi)
+	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi, 10)
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
 
@@ -270,7 +270,7 @@ func (e *ENISuite) TestNodeManagerMinAllocateAndPreallocate(c *check.C) {
 // - PreAllocate 8
 func (e *ENISuite) TestNodeManagerExceedENICapacity(c *check.C) {
 	ec2api := ec2mock.NewAPI([]*types.Subnet{testSubnet})
-	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi)
+	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi, 10)
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
 
@@ -295,6 +295,12 @@ func (e *ENISuite) TestNodeManagerExceedENICapacity(c *check.C) {
 	c.Assert(node.stats.usedIPs, check.Equals, 16)
 }
 
+type nodeState struct {
+	cn           *v2.CiliumNode
+	name         string
+	instanceName string
+}
+
 // TestNodeManagerManyNodes tests IP allocation of 100 nodes across 3 subnets
 //
 // - m4.large (2x ENIs, 2x10 IPs)
@@ -306,15 +312,9 @@ func (e *ENISuite) TestNodeManagerManyNodes(c *check.C) {
 	testSubnet3 := &types.Subnet{ID: "s-3", AvailabilityZone: "us-west-1", VpcID: "vpc-1", AvailableAddresses: 400}
 
 	ec2api := ec2mock.NewAPI([]*types.Subnet{testSubnet1, testSubnet2, testSubnet3})
-	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi)
+	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi, 10)
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
-
-	type nodeState struct {
-		cn           *v2.CiliumNode
-		name         string
-		instanceName string
-	}
 
 	state := make([]*nodeState, 100)
 
@@ -341,7 +341,7 @@ func (e *ENISuite) TestNodeManagerInstanceNotRunning(c *check.C) {
 	ec2api := ec2mock.NewAPI([]*types.Subnet{testSubnet})
 	ec2api.SetMockError(ec2mock.AttachNetworkInterface, errors.New("foo is not 'running' foo"))
 	metricsMock := metricsmock.NewMockMetrics()
-	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsMock)
+	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsMock, 10)
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
 
@@ -364,4 +364,55 @@ func (e *ENISuite) TestNodeManagerInstanceNotRunning(c *check.C) {
 	c.Assert(node, check.Not(check.IsNil))
 	c.Assert(node.stats.availableIPs, check.Equals, 0)
 	c.Assert(node.stats.usedIPs, check.Equals, 0)
+}
+
+func benchmarkAllocWorker(c *check.C, workers int64, delay time.Duration) {
+	testSubnet1 := &types.Subnet{ID: "s-1", AvailabilityZone: "us-west-1", VpcID: "vpc-1", AvailableAddresses: 1000000}
+	testSubnet2 := &types.Subnet{ID: "s-2", AvailabilityZone: "us-west-1", VpcID: "vpc-1", AvailableAddresses: 1000000}
+	testSubnet3 := &types.Subnet{ID: "s-3", AvailabilityZone: "us-west-1", VpcID: "vpc-1", AvailableAddresses: 1000000}
+
+	ec2api := ec2mock.NewAPI([]*types.Subnet{testSubnet1, testSubnet2, testSubnet3})
+	ec2api.SetDelay(ec2mock.AllOperations, delay)
+	mngr, err := NewNodeManager(ec2api, ec2api, k8sapi, metricsapi, workers)
+	c.Assert(err, check.IsNil)
+	c.Assert(mngr, check.Not(check.IsNil))
+
+	state := make([]*nodeState, c.N)
+
+	c.ResetTimer()
+	for i := range state {
+		s := &nodeState{name: fmt.Sprintf("node%d", i), instanceName: fmt.Sprintf("i-%d", i)}
+		s.cn = newCiliumNode(s.name, s.instanceName, "m4.large", "us-west-1", "vpc-1", 1, 10, 0, 0)
+		state[i] = s
+		mngr.Update(s.cn)
+	}
+
+restart:
+	for _, s := range state {
+		if !reachedAddressesNeeded(mngr, s.name, 0) {
+			time.Sleep(5 * time.Millisecond)
+			goto restart
+		}
+	}
+	c.StopTimer()
+
+}
+
+func (e *ENISuite) BenchmarkAllocDelay20Worker1(c *check.C) {
+	benchmarkAllocWorker(c, 1, 20*time.Millisecond)
+}
+func (e *ENISuite) BenchmarkAllocDelay20Worker10(c *check.C) {
+	benchmarkAllocWorker(c, 10, 20*time.Millisecond)
+}
+func (e *ENISuite) BenchmarkAllocDelay20Worker50(c *check.C) {
+	benchmarkAllocWorker(c, 50, 20*time.Millisecond)
+}
+func (e *ENISuite) BenchmarkAllocDelay50Worker1(c *check.C) {
+	benchmarkAllocWorker(c, 1, 50*time.Millisecond)
+}
+func (e *ENISuite) BenchmarkAllocDelay50Worker10(c *check.C) {
+	benchmarkAllocWorker(c, 10, 50*time.Millisecond)
+}
+func (e *ENISuite) BenchmarkAllocDelay50Worker50(c *check.C) {
+	benchmarkAllocWorker(c, 50, 50*time.Millisecond)
 }

commit 6c1eb079a759430a0b133a62992d4d4705399496

aws/eni: do not resync node if semaphore Acquire fails
In case the semaphore fails to be acquired we should not continue
resyncing the node and eventually release that same semaphore as it
might cause the semaphore to panic due the number of 'Release' calls
is superior than the number of previous acquires.

Fixes: b2b95dd1028a ("eni: Support for parallel workers")
Signed-off-by: André Martins <andre@cilium.io>

pkg/aws/eni/node_manager.go
@@ -299,7 +299,10 @@ func (n *NodeManager) Resync(ctx context.Context, syncTime time.Time) {
 	sem := semaphore.NewWeighted(n.parallelWorkers)
 
 	for _, node := range n.GetNodesByIPWatermark() {
-		sem.Acquire(ctx, 1)
+		err := sem.Acquire(ctx, 1)
+		if err != nil {
+			continue
+		}
 		go func(node *Node, stats *resyncStats) {
 			n.resyncNode(ctx, node, stats, syncTime)
 			sem.Release(1)