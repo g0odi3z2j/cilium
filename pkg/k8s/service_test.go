g0odi3z2j/cilium

commit c4aa2ee581e9904a8f167e82848937f095d9d670

add externalIPs implementation in Cilium
This commit adds the implementation for k8s externalIPs into the control
plane. The user will be able to observe the externalIPs services
implemented into Cilium by typing `cilium service list`:

```
$ cilium service list
ID   Frontend              Service Type   Backend
...
4    192.0.2.233:31389     ExternalIPs    1 => 10.16.48.207:80
                                          2 => 10.16.123.29:80
...
```

Contrary to what is currently implemented in kube-proxy, Cilium will not
perform any service translation for traffic made to an externalIP where
its origin comes from either the host or any pod running in locally in
that host. In kube-proxy the service translation for externalIPs will be
made for traffic originated from the pod but it will not be made for
traffic originated from the host.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/service.go
@@ -75,15 +75,15 @@ func ParseService(svc *types.Service) (ServiceID, *Service) {
 
 	case v1.ServiceTypeExternalName:
 		// External-name services must be ignored
-		return svcID, nil
+		return ServiceID{}, nil
 
 	default:
 		scopedLog.Warn("Ignoring k8s service: unsupported type")
-		return svcID, nil
+		return ServiceID{}, nil
 	}
 
-	if svc.Spec.ClusterIP == "" {
-		return svcID, nil
+	if svc.Spec.ClusterIP == "" && (!option.Config.EnableNodePort || len(svc.Spec.ExternalIPs) == 0) {
+		return ServiceID{}, nil
 	}
 
 	clusterIP := net.ParseIP(svc.Spec.ClusterIP)
@@ -91,7 +91,7 @@ func ParseService(svc *types.Service) (ServiceID, *Service) {
 	if strings.ToLower(svc.Spec.ClusterIP) == "none" {
 		headless = true
 	}
-	svcInfo := NewService(clusterIP, headless, svc.Labels, svc.Spec.Selector)
+	svcInfo := NewService(clusterIP, svc.Spec.ExternalIPs, headless, svc.Labels, svc.Spec.Selector)
 	svcInfo.IncludeExternal = getAnnotationIncludeExternal(svc)
 	svcInfo.Shared = getAnnotationShared(svc)
 
@@ -199,8 +199,11 @@ type Service struct {
 	// NodePort fronted addr. The string addr => addr indirection is to avoid
 	// storing duplicates.
 	NodePorts map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID
-	Labels    map[string]string
-	Selector  map[string]string
+	// K8sExternalIPs stores mapping of the endpoint in a string format to the
+	// externalIP in net.IP format.
+	K8sExternalIPs map[string]net.IP
+	Labels         map[string]string
+	Selector       map[string]string
 }
 
 // String returns the string representation of a service resource
@@ -274,20 +277,43 @@ func (s *Service) DeepEquals(o *Service) bool {
 				}
 			}
 		}
+
+		for k, v := range s.K8sExternalIPs {
+			vOther, ok := o.K8sExternalIPs[k]
+			if !ok || !v.Equal(vOther) {
+				return false
+			}
+		}
 		return true
 	}
 	return false
 }
 
+func parseExternalIPs(externalIPs []string) map[string]net.IP {
+	m := map[string]net.IP{}
+	for _, externalIP := range externalIPs {
+		ip := net.ParseIP(externalIP)
+		if ip != nil {
+			m[externalIP] = ip
+		}
+	}
+	return m
+}
+
 // NewService returns a new Service with the Ports map initialized.
-func NewService(ip net.IP, headless bool, labels map[string]string, selector map[string]string) *Service {
+func NewService(ip net.IP, externalIPs []string, headless bool, labels map[string]string, selector map[string]string) *Service {
+	var k8sExternalIPs map[string]net.IP
+	if option.Config.EnableNodePort {
+		k8sExternalIPs = parseExternalIPs(externalIPs)
+	}
 	return &Service{
-		FrontendIP: ip,
-		IsHeadless: headless,
-		Ports:      map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
-		NodePorts:  map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
-		Labels:     labels,
-		Selector:   selector,
+		FrontendIP:     ip,
+		IsHeadless:     headless,
+		Ports:          map[loadbalancer.FEPortName]*loadbalancer.L4Addr{},
+		NodePorts:      map[loadbalancer.FEPortName]map[string]*loadbalancer.L3n4AddrID{},
+		K8sExternalIPs: k8sExternalIPs,
+		Labels:         labels,
+		Selector:       selector,
 	}
 }
 

pkg/service/service.go
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package service
 
 import (
@@ -37,7 +38,7 @@ var (
 
 // LBMap is the interface describing methods for manipulating service maps.
 type LBMap interface {
-	UpsertService(uint16, net.IP, uint16, []uint16, int, bool) error
+	UpsertService(uint16, net.IP, uint16, []uint16, int, bool, lb.SVCType) error
 	DeleteService(lb.L3n4AddrID, int) error
 	AddBackend(uint16, net.IP, uint16, bool) error
 	DeleteBackendByID(uint16, bool) error
@@ -387,7 +388,7 @@ func (s *Service) upsertServiceIntoLBMaps(svc *svcInfo, prevBackendCount int,
 		uint16(svc.frontend.ID), svc.frontend.L3n4Addr.IP,
 		svc.frontend.L3n4Addr.L4Addr.Port,
 		backendIDs, prevBackendCount,
-		ipv6)
+		ipv6, svc.svcType)
 	if err != nil {
 		return err
 	}

daemon/loadbalancer.go
@@ -62,6 +62,8 @@ func (h *putServiceID) Handle(params PutServiceIDParams) middleware.Responder {
 
 	var svcType loadbalancer.SVCType
 	switch params.Config.Flags.Type {
+	case models.ServiceSpecFlagsTypeExternalIPs:
+		svcType = loadbalancer.SVCTypeExternalIPs
 	case models.ServiceSpecFlagsTypeNodePort:
 		svcType = loadbalancer.SVCTypeNodePort
 	default:

pkg/datapath/linux/config/config.go
@@ -246,6 +246,7 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 
 	if option.Config.EnableNodePort {
 		cDefinesMap["ENABLE_NODEPORT"] = "1"
+		cDefinesMap["ENABLE_K8S_EXTERNAL_IP"] = "1"
 		cDefinesMap["NODEPORT_PORT_MIN"] = fmt.Sprintf("%d", option.Config.NodePortMin)
 		cDefinesMap["NODEPORT_PORT_MAX"] = fmt.Sprintf("%d", option.Config.NodePortMax)
 		cDefinesMap["NODEPORT_PORT_MIN_NAT"] = fmt.Sprintf("%d", option.Config.NodePortMax+1)

pkg/k8s/factory_functions.go
@@ -131,14 +131,14 @@ func EqualV1Services(svc1, svc2 *types.Service) bool {
 	if strings.ToLower(svc1.Spec.ClusterIP) == "none" {
 		headless = true
 	}
-	si1 := NewService(clusterIP, headless, svc1.Labels, svc1.Spec.Selector)
+	si1 := NewService(clusterIP, svc1.Spec.ExternalIPs, headless, svc1.Labels, svc1.Spec.Selector)
 
 	clusterIP = net.ParseIP(svc2.Spec.ClusterIP)
 	headless = false
 	if strings.ToLower(svc2.Spec.ClusterIP) == "none" {
 		headless = true
 	}
-	si2 := NewService(clusterIP, headless, svc2.Labels, svc2.Spec.Selector)
+	si2 := NewService(clusterIP, svc2.Spec.ExternalIPs, headless, svc2.Labels, svc2.Spec.Selector)
 
 	// Please write all the equalness logic inside the K8sServiceInfo.Equals()
 	// method.

pkg/k8s/watchers/watcher.go
@@ -496,6 +496,10 @@ func (k *K8sWatcher) delK8sSVCs(svc k8s.ServiceID, svcInfo *k8s.Service, se *k8s
 		for _, nodePortFE := range svcInfo.NodePorts[portName] {
 			frontends = append(frontends, &nodePortFE.L3n4Addr)
 		}
+
+		for _, k8sExternalIP := range svcInfo.K8sExternalIPs {
+			frontends = append(frontends, loadbalancer.NewL3n4Addr(svcPort.Protocol, k8sExternalIP, svcPort.Port))
+		}
 	}
 
 	for _, fe := range frontends {
@@ -565,6 +569,11 @@ func datapathSVCs(svc *k8s.Service, endpoints *k8s.Endpoints) (svcs []loadbalanc
 	}
 	if svc.FrontendIP != nil {
 		dpSVC := genCartesianProduct(svc.FrontendIP, loadbalancer.SVCTypeClusterIP, clusterIPPorts, endpoints)
+		svcs = append(svcs, dpSVC...)
+	}
+
+	for _, k8sExternalIP := range svc.K8sExternalIPs {
+		dpSVC := genCartesianProduct(k8sExternalIP, loadbalancer.SVCTypeExternalIPs, clusterIPPorts, endpoints)
 		svcs = append(svcs, dpSVC...)
 	}
 

pkg/k8s/zz_generated.deepcopy.go
@@ -112,6 +112,21 @@ func (in *Service) DeepCopyInto(out *Service) {
 			(*out)[key] = outVal
 		}
 	}
+	if in.K8sExternalIPs != nil {
+		in, out := &in.K8sExternalIPs, &out.K8sExternalIPs
+		*out = make(map[string]net.IP, len(*in))
+		for key, val := range *in {
+			var outVal []byte
+			if val == nil {
+				(*out)[key] = nil
+			} else {
+				in, out := &val, &outVal
+				*out = make(net.IP, len(*in))
+				copy(*out, *in)
+			}
+			(*out)[key] = outVal
+		}
+	}
 	if in.Labels != nil {
 		in, out := &in.Labels, &out.Labels
 		*out = make(map[string]string, len(*in))

commit 4dc77c78150fdd6580d36142a120326bd5a88f2f

k8s: Fix Service.DeepEquals for ExternalIP
Previously, the DeepEquals() method did not consider a change when
an external IP had been added to a service which didn't have any
externalIP assigned to it.

This prevented from updating such service, therefore no externalIP
services were provisioned for the service.

Fixes: c4aa2ee581 ("add externalIPs implementation in Cilium")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/k8s/service.go
@@ -278,6 +278,10 @@ func (s *Service) DeepEquals(o *Service) bool {
 			}
 		}
 
+		if ((s.K8sExternalIPs == nil) != (o.K8sExternalIPs == nil)) ||
+			len(s.K8sExternalIPs) != len(o.K8sExternalIPs) {
+			return false
+		}
 		for k, v := range s.K8sExternalIPs {
 			vOther, ok := o.K8sExternalIPs[k]
 			if !ok || !v.Equal(vOther) {

pkg/k8s/service_test.go
@@ -520,6 +520,48 @@ func TestService_Equals(t *testing.T) {
 			want: false,
 		},
 		{
+			name: "external-ip was added",
+			fields: &Service{
+				FrontendIP: net.ParseIP("1.1.1.1"),
+				IsHeadless: false,
+				Ports: map[loadbalancer.FEPortName]*loadbalancer.L4Addr{
+					loadbalancer.FEPortName("foo"): {
+						Protocol: loadbalancer.NONE,
+						Port:     1,
+					},
+				},
+				K8sExternalIPs: nil,
+				Labels: map[string]string{
+					"foo": "bar",
+				},
+				Selector: map[string]string{
+					"baz": "foz",
+				},
+			},
+			args: args{
+				o: &Service{
+					FrontendIP: net.ParseIP("1.1.1.1"),
+					IsHeadless: false,
+					Ports: map[loadbalancer.FEPortName]*loadbalancer.L4Addr{
+						loadbalancer.FEPortName("foo"): {
+							Protocol: loadbalancer.NONE,
+							Port:     1,
+						},
+					},
+					K8sExternalIPs: map[string]net.IP{
+						"2.2.2.2": net.ParseIP("2.2.2.2"),
+					},
+					Labels: map[string]string{
+						"foo": "bar",
+					},
+					Selector: map[string]string{
+						"baz": "foz",
+					},
+				},
+			},
+			want: false,
+		},
+		{
 			name: "both nil",
 			args: args{},
 			want: true,