g0odi3z2j/cilium

commit 44bedb871056a428ffcdcf99e4155ffb864967d9

cilium, daemon: fix device detection for default route
I noticed on one of my test machines that the agent refuses to start up
when IPv6 and NodePort gets enabled:

  [...]
  level=info msg=" - !:pod-template-generation" subsys=labels-filter
  level=info msg=" - !:pod-template-hash" subsys=labels-filter
  level=info msg=" - !:controller-revision-hash" subsys=labels-filter
  level=info msg=" - !:annotation.*" subsys=labels-filter
  level=info msg=" - !:etcd_node" subsys=labels-filter
  level=info msg="Auto-enabling host reachable services for UDP and TCP as required by BPF NodePort." subsys=daemon
  level=fatal msg="BPF NodePort's external facing device could not be determined. Use --device to specify." error="Found (2) default routes" subsys=daemon
  #

Turns out in case of IPv6 the machine had the same route but different
MTU metric:

  # ip -6 r | grep default
  default via fe80::9a9b:cbff:fe05:1eae dev eno1 proto ra metric 100 mtu 1492 pref medium
  default via fe80::9a9b:cbff:fe05:1eae dev eno1 proto ra metric 1024 pref medium
  # ip -4 r | grep default
  default via 192.168.178.1 dev eno1 proto dhcp src 192.168.178.28 metric 100

Fix the detection to walk and check all ifindices wrt whether they are
equal, and if that is the case then proceed normally.

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/datapath/linux/route/route.go
@@ -86,13 +86,19 @@ func (r *Route) ToIPCommand(dev string) []string {
 }
 
 func lookupDefaultRoute(family int) (netlink.Route, error) {
+	linkIndex := 0
+
 	routes, err := netlink.RouteListFiltered(family, &netlink.Route{Dst: nil}, netlink.RT_FILTER_DST)
 	if err != nil {
 		return netlink.Route{}, fmt.Errorf("Unable to list direct routes: %s", err)
 	}
 
-	if len(routes) != 1 {
-		return netlink.Route{}, fmt.Errorf("Found (%d) default routes", len(routes))
+	for _, route := range routes {
+		if linkIndex != 0 && linkIndex != route.LinkIndex {
+			return netlink.Route{}, fmt.Errorf("Found default routes with different netdev ifindices: %v vs %v",
+				linkIndex, route.LinkIndex)
+		}
+		linkIndex = route.LinkIndex
 	}
 
 	log.Debugf("Found default route on node %v", routes[0])

commit 0a6d15177e7d183e011ffa86ce27e4280a8b1bf7

datapath: Return err if default route is not found
Previously, cilium-agent was panicking if no default route was found:

panic: runtime error: index out of range [0] with length 0

    goroutine 1 [running]:
    github.com/cilium/cilium/pkg/datapath/linux/route.lookupDefaultRoute(0xa,
    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, ...)
            /go/src/github.com/cilium/cilium/pkg/datapath/linux/route/route.go:104
            +0x445

To fix this, return an error instead.

Fixes: 44bedb8710 ("cilium, daemon: fix device detection for default route")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/datapath/linux/route/route.go
@@ -93,6 +93,10 @@ func lookupDefaultRoute(family int) (netlink.Route, error) {
 		return netlink.Route{}, fmt.Errorf("Unable to list direct routes: %s", err)
 	}
 
+	if len(routes) == 0 {
+		return netlink.Route{}, fmt.Errorf("Default route not found for family %d", family)
+	}
+
 	for _, route := range routes {
 		if linkIndex != 0 && linkIndex != route.LinkIndex {
 			return netlink.Route{}, fmt.Errorf("Found default routes with different netdev ifindices: %v vs %v",