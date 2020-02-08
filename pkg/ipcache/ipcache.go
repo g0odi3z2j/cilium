g0odi3z2j/cilium

commit 454929f6229ddcfe2aba126b07659253b3d4e803

ipcache: Associate IPs with K8s namespace and pod name
This extends the ipcache with the Kubernetes namespace and pod name of
each address if available. This will enable reverse lookups of pod names
by IP address.

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/ipcache/ipcache.go
@@ -48,6 +48,14 @@ type IPKeyPair struct {
 	Key uint8
 }
 
+// K8sMetadata contains Kubernetes pod information of the IP
+type K8sMetadata struct {
+	// Namespace is the Kubernetes namespace of the pod behind the IP
+	Namespace string
+	// PodName is the Kubernetes pod name behind the IP
+	PodName string
+}
+
 // IPCache is a collection of mappings:
 // - mapping of endpoint IP or CIDR to security identities of all endpoints
 //   which are part of the same cluster, and vice-versa
@@ -57,6 +65,7 @@ type IPCache struct {
 	ipToIdentityCache map[string]Identity
 	identityToIPCache map[identity.NumericIdentity]map[string]struct{}
 	ipToHostIPCache   map[string]IPKeyPair
+	ipToK8sMetadata   map[string]K8sMetadata
 
 	// prefixLengths reference-count the number of CIDRs that use
 	// particular prefix lengths for the mask.
@@ -74,6 +83,7 @@ func NewIPCache() *IPCache {
 		ipToIdentityCache: map[string]Identity{},
 		identityToIPCache: map[identity.NumericIdentity]map[string]struct{}{},
 		ipToHostIPCache:   map[string]IPKeyPair{},
+		ipToK8sMetadata:   map[string]K8sMetadata{},
 		v4PrefixLengths:   map[int]int{},
 		v6PrefixLengths:   map[int]int{},
 	}
@@ -139,6 +149,13 @@ func (ipc *IPCache) getHostIPCache(ip string) (net.IP, uint8) {
 	return ipKeyPair.IP, ipKeyPair.Key
 }
 
+func (ipc *IPCache) getK8sMetadata(ip string) *K8sMetadata {
+	if k8sMeta, ok := ipc.ipToK8sMetadata[ip]; ok {
+		return &k8sMeta
+	}
+	return nil
+}
+
 // Upsert adds / updates the provided IP (endpoint or CIDR prefix) and identity
 // into the IPCache.
 //
@@ -146,8 +163,9 @@ func (ipc *IPCache) getHostIPCache(ip string) (net.IP, uint8) {
 // returns false if the kubernetes layer is trying to upsert an entry now
 // managed by the kvstore layer. See source.AllowOverwrite() for rules on
 // ownership. hostIP is the location of the given IP. It is optional (may be
-// nil) and is propagated to the listeners.
-func (ipc *IPCache) Upsert(ip string, hostIP net.IP, hostKey uint8, newIdentity Identity) bool {
+// nil) and is propagated to the listeners. k8sMeta contains Kubernetes-specific
+// metadata such as pod namespace and pod name belonging to the IP (may be nil).
+func (ipc *IPCache) Upsert(ip string, hostIP net.IP, hostKey uint8, k8sMeta *K8sMetadata, newIdentity Identity) bool {
 	scopedLog := log
 	if option.Config.Debug {
 		scopedLog = log.WithFields(logrus.Fields{
@@ -165,6 +183,7 @@ func (ipc *IPCache) Upsert(ip string, hostIP net.IP, hostKey uint8, newIdentity 
 	callbackListeners := true
 
 	oldHostIP, oldHostKey := ipc.getHostIPCache(ip)
+	oldK8sMeta := ipc.ipToK8sMetadata[ip]
 
 	cachedIdentity, found := ipc.ipToIdentityCache[ip]
 	if found {
@@ -174,7 +193,8 @@ func (ipc *IPCache) Upsert(ip string, hostIP net.IP, hostKey uint8, newIdentity 
 
 		// Skip update if IP is already mapped to the given identity
 		// and the host IP hasn't changed.
-		if cachedIdentity == newIdentity && oldHostIP.Equal(hostIP) && hostKey == oldHostKey {
+		if cachedIdentity == newIdentity && oldHostIP.Equal(hostIP) &&
+			hostKey == oldHostKey && oldK8sMeta.Equal(k8sMeta) {
 			return true
 		}
 
@@ -246,9 +266,15 @@ func (ipc *IPCache) Upsert(ip string, hostIP net.IP, hostKey uint8, newIdentity 
 		ipc.ipToHostIPCache[ip] = IPKeyPair{IP: hostIP, Key: hostKey}
 	}
 
+	if k8sMeta == nil {
+		delete(ipc.ipToK8sMetadata, ip)
+	} else {
+		ipc.ipToK8sMetadata[ip] = *k8sMeta
+	}
+
 	if callbackListeners {
 		for _, listener := range ipc.listeners {
-			listener.OnIPIdentityCacheChange(Upsert, *cidr, oldHostIP, hostIP, oldIdentity, newIdentity.ID, hostKey)
+			listener.OnIPIdentityCacheChange(Upsert, *cidr, oldHostIP, hostIP, oldIdentity, newIdentity.ID, hostKey, k8sMeta)
 		}
 	}
 
@@ -260,12 +286,13 @@ func (ipc *IPCache) Upsert(ip string, hostIP net.IP, hostKey uint8, newIdentity 
 func (ipc *IPCache) DumpToListenerLocked(listener IPIdentityMappingListener) {
 	for ip, identity := range ipc.ipToIdentityCache {
 		hostIP, encryptKey := ipc.getHostIPCache(ip)
+		k8sMeta := ipc.getK8sMetadata(ip)
 		_, cidr, err := net.ParseCIDR(ip)
 		if err != nil {
 			endpointIP := net.ParseIP(ip)
 			cidr = endpointIPToCIDR(endpointIP)
 		}
-		listener.OnIPIdentityCacheChange(Upsert, *cidr, nil, hostIP, nil, identity.ID, encryptKey)
+		listener.OnIPIdentityCacheChange(Upsert, *cidr, nil, hostIP, nil, identity.ID, encryptKey, k8sMeta)
 	}
 }
 
@@ -291,6 +318,7 @@ func (ipc *IPCache) deleteLocked(ip string, source source.Source) {
 	var cidr *net.IPNet
 	cacheModification := Delete
 	oldHostIP, encryptKey := ipc.getHostIPCache(ip)
+	oldK8sMeta := ipc.getK8sMetadata(ip)
 	var newHostIP net.IP
 	var oldIdentity *identity.NumericIdentity
 	newIdentity := cachedIdentity
@@ -338,11 +366,12 @@ func (ipc *IPCache) deleteLocked(ip string, source source.Source) {
 		delete(ipc.identityToIPCache, cachedIdentity.ID)
 	}
 	delete(ipc.ipToHostIPCache, ip)
+	delete(ipc.ipToK8sMetadata, ip)
 
 	if callbackListeners {
 		for _, listener := range ipc.listeners {
 			listener.OnIPIdentityCacheChange(cacheModification, *cidr, oldHostIP, newHostIP,
-				oldIdentity, newIdentity.ID, encryptKey)
+				oldIdentity, newIdentity.ID, encryptKey, oldK8sMeta)
 		}
 	}
 }
@@ -415,4 +444,16 @@ func (ipc *IPCache) LookupByIdentity(id identity.NumericIdentity) (map[string]st
 func GetIPIdentityMapModel() {
 	// TODO (ianvernon) return model of ip to identity mapping. For use in CLI.
 	// see GH-2555
+}
+
+// Equal returns true if two K8sMetadata pointers contain the same data or are
+// both nil.
+func (m *K8sMetadata) Equal(o *K8sMetadata) bool {
+	if m == o {
+		return true
+	} else if m == nil || o == nil {
+		return false
+	}
+
+	return m.Namespace == o.Namespace && m.PodName == m.PodName
 }

daemon/datapath.go
@@ -504,7 +504,7 @@ func (d *Daemon) syncEndpointsAndHostIPs() error {
 
 		// Upsert will not propagate (reserved:foo->ID) mappings across the cluster,
 		// and we specifically don't want to do so.
-		ipcache.IPIdentityCache.Upsert(ipIDPair.PrefixString(), nil, hostKey, ipcache.Identity{
+		ipcache.IPIdentityCache.Upsert(ipIDPair.PrefixString(), nil, hostKey, nil, ipcache.Identity{
 			ID:     ipIDPair.ID,
 			Source: source.Local,
 		})

daemon/k8s_watcher.go
@@ -1483,10 +1483,15 @@ func (d *Daemon) updatePodHostIP(pod *types.Pod) (bool, error) {
 
 	hostKey := node.GetIPsecKeyIdentity()
 
+	k8sMeta := &ipcache.K8sMetadata{
+		Namespace: pod.Namespace,
+		PodName:   pod.Name,
+	}
+
 	// Initial mapping of podIP <-> hostIP <-> identity. The mapping is
 	// later updated once the allocator has determined the real identity.
 	// If the endpoint remains unmanaged, the identity remains untouched.
-	selfOwned := ipcache.IPIdentityCache.Upsert(pod.StatusPodIP, hostIP, hostKey, ipcache.Identity{
+	selfOwned := ipcache.IPIdentityCache.Upsert(pod.StatusPodIP, hostIP, hostKey, k8sMeta, ipcache.Identity{
 		ID:     identity.ReservedIdentityUnmanaged,
 		Source: source.Kubernetes,
 	})
@@ -1695,14 +1700,19 @@ func endpointUpdated(endpoint *types.CiliumEndpoint) {
 			return
 		}
 
+		k8sMeta := &ipcache.K8sMetadata{
+			Namespace: endpoint.Namespace,
+			PodName:   endpoint.Name,
+		}
+
 		for _, pair := range endpoint.Networking.Addressing {
 			if pair.IPV4 != "" {
-				ipcache.IPIdentityCache.Upsert(pair.IPV4, nodeIP, encryptionKey,
+				ipcache.IPIdentityCache.Upsert(pair.IPV4, nodeIP, encryptionKey, k8sMeta,
 					ipcache.Identity{ID: id, Source: source.CustomResource})
 			}
 
 			if pair.IPV6 != "" {
-				ipcache.IPIdentityCache.Upsert(pair.IPV6, nodeIP, encryptionKey,
+				ipcache.IPIdentityCache.Upsert(pair.IPV6, nodeIP, encryptionKey, k8sMeta,
 					ipcache.Identity{ID: id, Source: source.CustomResource})
 			}
 		}

pkg/datapath/ipcache/listener.go
@@ -79,7 +79,8 @@ func NewListener(d datapath) *BPFListener {
 // IP->ID mapping will replace any existing contents; knowledge of the old pair
 // is not required to upsert the new pair.
 func (l *BPFListener) OnIPIdentityCacheChange(modType ipcache.CacheModification, cidr net.IPNet,
-	oldHostIP, newHostIP net.IP, oldID *identity.NumericIdentity, newID identity.NumericIdentity, encryptKey uint8) {
+	oldHostIP, newHostIP net.IP, oldID *identity.NumericIdentity, newID identity.NumericIdentity,
+	encryptKey uint8, k8sMeta *ipcache.K8sMetadata) {
 
 	scopedLog := log
 	if option.Config.Debug {

pkg/endpoint/policy.go
@@ -624,12 +624,14 @@ func (e *Endpoint) runIPIdentitySync(endpointIP addressing.CiliumIP) {
 				hostIP := node.GetExternalIPv4()
 				key := node.GetIPsecKeyIdentity()
 				metadata := e.FormatGlobalEndpointID()
+				k8sNamespace := e.K8sNamespace
+				k8sPodName := e.K8sPodName
 
 				// Release lock as we do not want to have long-lasting key-value
 				// store operations resulting in lock being held for a long time.
 				e.runlock()
 
-				if err := ipcache.UpsertIPToKVStore(ctx, IP, hostIP, ID, key, metadata); err != nil {
+				if err := ipcache.UpsertIPToKVStore(ctx, IP, hostIP, ID, key, metadata, k8sNamespace, k8sPodName); err != nil {
 					return fmt.Errorf("unable to add endpoint IP mapping '%s'->'%d': %s", IP.String(), ID, err)
 				}
 				return nil

pkg/envoy/resources.go
@@ -75,7 +75,8 @@ func (cache *NPHDSCache) OnIPIdentityCacheGC() {
 // OnIPIdentityCacheChange pushes modifications to the IP<->Identity mapping
 // into the Network Policy Host Discovery Service (NPHDS).
 func (cache *NPHDSCache) OnIPIdentityCacheChange(modType ipcache.CacheModification, cidr net.IPNet,
-	oldHostIP, newHostIP net.IP, oldID *identity.NumericIdentity, newID identity.NumericIdentity, encryptKey uint8) {
+	oldHostIP, newHostIP net.IP, oldID *identity.NumericIdentity, newID identity.NumericIdentity,
+	encryptKey uint8, k8sMeta *ipcache.K8sMetadata) {
 	// An upsert where an existing pair exists should translate into a
 	// delete (for the old Identity) followed by an upsert (for the new).
 	if oldID != nil && modType == ipcache.Upsert {
@@ -84,7 +85,7 @@ func (cache *NPHDSCache) OnIPIdentityCacheChange(modType ipcache.CacheModificati
 			return
 		}
 
-		cache.OnIPIdentityCacheChange(ipcache.Delete, cidr, nil, nil, nil, *oldID, encryptKey)
+		cache.OnIPIdentityCacheChange(ipcache.Delete, cidr, nil, nil, nil, *oldID, encryptKey, k8sMeta)
 	}
 
 	cidrStr := cidr.String()

pkg/identity/identity.go
@@ -60,12 +60,14 @@ type Identity struct {
 // This structure is written as JSON to the key-value store. Do NOT modify this
 // structure in ways which are not JSON forward compatible.
 type IPIdentityPair struct {
-	IP       net.IP          `json:"IP"`
-	Mask     net.IPMask      `json:"Mask"`
-	HostIP   net.IP          `json:"HostIP"`
-	ID       NumericIdentity `json:"ID"`
-	Key      uint8           `json:"Key"`
-	Metadata string          `json:"Metadata"`
+	IP           net.IP          `json:"IP"`
+	Mask         net.IPMask      `json:"Mask"`
+	HostIP       net.IP          `json:"HostIP"`
+	ID           NumericIdentity `json:"ID"`
+	Key          uint8           `json:"Key"`
+	Metadata     string          `json:"Metadata"`
+	K8sNamespace string          `json:"K8sNamespace,omitempty"`
+	K8sPodName   string          `json:"K8sPodName,omitempty"`
 }
 
 func NewIdentityFromModel(base *models.Identity) *Identity {

pkg/ipcache/cidr.go
@@ -83,7 +83,7 @@ func allocateCIDRs(prefixes []*net.IPNet) ([]*identity.Identity, error) {
 
 	// Only upsert into ipcache if identity wasn't allocated before.
 	for prefixString, id := range newlyAllocatedIdentities {
-		IPIdentityCache.Upsert(prefixString, nil, 0, Identity{
+		IPIdentityCache.Upsert(prefixString, nil, 0, nil, Identity{
 			ID:     id.ID,
 			Source: source.Generated,
 		})

commit 75010c9d21fdb52205af76a8be30c6cc72e7ce40

ipcache: Fix ipcache pod IP update
If an IP was reused by another pod, then a bug in the pod/namespace
comparison logic would prevent the ipcache being updated for the pod.
Fix it.

Reported-at: https://lgtm.com/projects/g/cilium/cilium/alerts
Fixes: 454929f6229d ("ipcache: Associate IPs with K8s namespace and pod name")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/ipcache/ipcache.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Authors of Cilium
+// Copyright 2018-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -455,5 +455,5 @@ func (m *K8sMetadata) Equal(o *K8sMetadata) bool {
 		return false
 	}
 
-	return m.Namespace == o.Namespace && m.PodName == m.PodName
+	return m.Namespace == o.Namespace && m.PodName == o.PodName
 }