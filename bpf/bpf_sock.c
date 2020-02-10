g0odi3z2j/cilium

commit 245bd348c3e9067e3463e737b2915c4d06e569e5

bpf: Add post_bind{4,6} programs to block NodePorts
This attaches new programs to be called after bind{4,6} to block user
processes from binding ports that overlap with installed NodePort or
ExternalIP services. This ensures that BPF NodePort never accidentally
hijacks traffic to such processes. Sockets which were bound before the
BPF program is installed are not affected - this behavior is consistent
with kube-proxy.

Fixes: #9206

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

bpf/bpf_sock.c
@@ -206,36 +206,37 @@ static __always_inline bool sock4_skip_xlate(struct lb4_service *svc,
 	return false;
 }
 
-static __always_inline void sock4_handle_node_port(struct bpf_sock_addr *ctx,
-						   struct lb4_key *key)
+static __always_inline
+struct lb4_service *sock4_nodeport_wildcard_lookup(struct lb4_key *key,
+						   const bool include_remote_hosts)
 {
 #ifdef ENABLE_NODEPORT
 	struct remote_endpoint_info *info;
-	__be32 daddr = ctx->user_ip4;
 	__u16 service_port;
 
 	service_port = bpf_ntohs(key->dport);
 	if (service_port < NODEPORT_PORT_MIN ||
 	    service_port > NODEPORT_PORT_MAX)
-		goto out_fill_addr;
+		return NULL;
 
 	/* When connecting to node port services in our cluster that
 	 * have either {REMOTE_NODE,HOST}_ID or loopback address, we
 	 * do a wild-card lookup with IP of 0.
 	 */
-	if (is_v4_loopback(daddr))
-		return;
+	if (is_v4_loopback(key->address))
+		goto wildcard_lookup;
 
-	info = ipcache_lookup4(&IPCACHE_MAP, daddr, V4_CACHE_KEY_LEN);
+	info = ipcache_lookup4(&IPCACHE_MAP, key->address, V4_CACHE_KEY_LEN);
 	if (info != NULL && (info->sec_label == HOST_ID ||
-			     info->sec_label == REMOTE_NODE_ID))
-		return;
+	    (include_remote_hosts && info->sec_label == REMOTE_NODE_ID)))
+		goto wildcard_lookup;
 
-	/* For everything else in terms of node port, do a direct lookup. */
-out_fill_addr:
-	key->address = daddr;
+	return NULL;
+wildcard_lookup:
+	key->address = 0;
+	return __lb4_lookup_service(key);
 #else
-	key->address = ctx->user_ip4;
+	return NULL;
 #endif /* ENABLE_NODEPORT */
 }
 
@@ -260,16 +261,9 @@ static __always_inline int __sock4_xlate(struct bpf_sock_addr *ctx,
 	 */
 	svc = __lb4_lookup_service(&key);
 	if (!svc) {
-		key.address = 0;
 		key.dport = ctx_get_port(ctx);
 
-		/* We already performed a lookup where key.address
-		 * was ctx->user_ip4. If it was not found then, it
-		 * is not going to be found again.
-		 */
-		sock4_handle_node_port(ctx, &key);
-		if (key.address != ctx->user_ip4)
-			svc = __lb4_lookup_service(&key);
+		svc = sock4_nodeport_wildcard_lookup(&key, true);
 		if (svc && !lb4_svc_is_nodeport(svc))
 			svc = NULL;
 	}
@@ -319,6 +313,48 @@ int sock4_xlate(struct bpf_sock_addr *ctx)
 	return SYS_PROCEED;
 }
 
+#if defined(ENABLE_NODEPORT) || defined(ENABLE_EXTERNAL_IP)
+static __always_inline int __sock4_post_bind(struct bpf_sock *ctx)
+{
+	struct lb4_service *svc;
+	struct lb4_key key = {
+		.address	= ctx->src_ip4,
+		.dport		= bpf_htons(ctx->src_port),
+	};
+
+	if (!sock_proto_enabled(ctx->protocol))
+		return 0;
+
+	svc = __lb4_lookup_service(&key);
+	if (!svc) {
+		/* Perform a wildcard lookup for the case where the caller tries
+		 * to bind to loopback or an address with host identity
+		 * (without remote hosts).
+		 */
+		key.dport = bpf_htons(ctx->src_port);
+		svc = sock4_nodeport_wildcard_lookup(&key, false);
+	}
+
+	/* If the sockaddr of this socket overlaps with a NodePort
+	 * or ExternalIP service. We must reject this bind() call
+	 * to avoid accidentally hijacking its traffic.
+	 */
+	if (svc && (lb4_svc_is_nodeport(svc) || lb4_svc_is_external_ip(svc)))
+		return -EADDRINUSE;
+
+	return 0;
+}
+
+__section("post-bind-sock4")
+int sock4_post_bind(struct bpf_sock *ctx)
+{
+	if (__sock4_post_bind(ctx) < 0)
+		return SYS_REJECT;
+
+	return SYS_PROCEED;
+}
+#endif /* ENABLE_NODEPORT || ENABLE_EXTERNAL_IP */
+
 #ifdef ENABLE_HOST_SERVICES_UDP
 static __always_inline int __sock4_xlate_snd(struct bpf_sock_addr *ctx,
 					     struct bpf_sock_addr *ctx_full)
@@ -333,12 +369,8 @@ static __always_inline int __sock4_xlate_snd(struct bpf_sock_addr *ctx,
 
 	svc = __lb4_lookup_service(&lkey);
 	if (!svc) {
-		lkey.address = 0;
 		lkey.dport = ctx_get_port(ctx);
-
-		sock4_handle_node_port(ctx, &lkey);
-		if (lkey.address != ctx->user_ip4)
-			svc = __lb4_lookup_service(&lkey);
+		svc = sock4_nodeport_wildcard_lookup(&lkey, true);
 		if (svc && !lb4_svc_is_nodeport(svc))
 			svc = NULL;
 	}
@@ -484,6 +516,17 @@ static __always_inline void ctx_get_v6_address(struct bpf_sock_addr *ctx,
 	addr->p4 = ctx->user_ip6[3];
 }
 
+#ifdef ENABLE_NODEPORT
+static __always_inline void ctx_get_v6_src_address(struct bpf_sock *ctx,
+					       union v6addr *addr)
+{
+	addr->p1 = ctx->src_ip6[0];
+	addr->p2 = ctx->src_ip6[1];
+	addr->p3 = ctx->src_ip6[2];
+	addr->p4 = ctx->src_ip6[3];
+}
+#endif /* ENABLE_NODEPORT */
+
 static __always_inline void ctx_set_v6_address(struct bpf_sock_addr *ctx,
 					       union v6addr *addr)
 {
@@ -513,38 +556,37 @@ static __always_inline bool sock6_skip_xlate(struct lb6_service *svc,
 	return false;
 }
 
-static __always_inline void sock6_handle_node_port(struct bpf_sock_addr *ctx,
-						   struct lb6_key *key)
+static __always_inline
+struct lb6_service *sock6_nodeport_wildcard_lookup(struct lb6_key *key,
+						   bool include_remote_hosts)
 {
 #ifdef ENABLE_NODEPORT
 	struct remote_endpoint_info *info;
-	union v6addr daddr;
 	__u16 service_port;
-
-	ctx_get_v6_address(ctx, &daddr);
 
 	service_port = bpf_ntohs(key->dport);
 	if (service_port < NODEPORT_PORT_MIN ||
 	    service_port > NODEPORT_PORT_MAX)
-		goto out_fill_addr;
+		return NULL;
 
 	/* When connecting to node port services in our cluster that
 	 * have either {REMOTE_NODE,HOST}_ID or loopback address, we
 	 * do a wild-card lookup with IP of 0.
 	 */
-	if (is_v6_loopback(&daddr))
-		return;
+	if (is_v6_loopback(&key->address))
+		goto wildcard_lookup;
 
-	info = ipcache_lookup6(&IPCACHE_MAP, &daddr, V6_CACHE_KEY_LEN);
+	info = ipcache_lookup6(&IPCACHE_MAP, &key->address, V6_CACHE_KEY_LEN);
 	if (info != NULL && (info->sec_label == HOST_ID ||
-			     info->sec_label == REMOTE_NODE_ID))
-		return;
+	    (include_remote_hosts && info->sec_label == REMOTE_NODE_ID)))
+		goto wildcard_lookup;
 
-	/* For everything else in terms of node port, do a direct lookup. */
-out_fill_addr:
-	key->address = daddr;
+	return NULL;
+wildcard_lookup:
+	__builtin_memset(&key->address, 0, sizeof(key->address));
+	return __lb6_lookup_service(key);
 #else
-	ctx_get_v6_address(ctx, &key->address);
+	return NULL;
 #endif /* ENABLE_NODEPORT */
 }
 
@@ -577,6 +619,66 @@ static __always_inline int sock6_xlate_v4_in_v6(struct bpf_sock_addr *ctx)
 	return -ENXIO;
 }
 
+#if defined(ENABLE_NODEPORT) || defined(ENABLE_EXTERNAL_IP)
+static __always_inline int sock6_post_bind_v4_in_v6(struct bpf_sock *ctx)
+{
+#ifdef ENABLE_IPV4
+	struct bpf_sock fake_ctx;
+	union v6addr addr6;
+
+	ctx_get_v6_src_address(ctx, &addr6);
+	if (!is_v4_in_v6(&addr6))
+		return 0;
+
+	__builtin_memset(&fake_ctx, 0, sizeof(fake_ctx));
+	fake_ctx.protocol = ctx->protocol;
+	fake_ctx.src_ip4  = addr6.p4;
+	fake_ctx.src_port = ctx->src_port;
+
+	return __sock4_post_bind(&fake_ctx);
+#endif /* ENABLE_IPV4 */
+	return 0;
+}
+
+static __always_inline int __sock6_post_bind(struct bpf_sock *ctx)
+{
+	struct lb6_service *svc;
+	struct lb6_key key = {
+		.dport		= bpf_htons(ctx->src_port),
+	};
+
+	if (!sock_proto_enabled(ctx->protocol))
+		return 0;
+
+	ctx_get_v6_src_address(ctx, &key.address);
+
+	svc = __lb6_lookup_service(&key);
+	if (!svc) {
+		key.dport = bpf_htons(ctx->src_port);
+		svc = sock6_nodeport_wildcard_lookup(&key, false);
+		if (!svc)
+			return sock6_post_bind_v4_in_v6(ctx);
+	}
+
+	if (svc && (lb6_svc_is_nodeport(svc) || lb6_svc_is_external_ip(svc))) {
+		return -EADDRINUSE;
+	}
+
+	return 0;
+}
+
+
+__section("post-bind-sock6")
+int sock6_post_bind(struct bpf_sock *ctx)
+{
+	if (__sock6_post_bind(ctx) < 0)
+		return SYS_REJECT;
+
+	return SYS_PROCEED;
+}
+
+#endif /* ENABLE_NODEPORT || ENABLE_EXTERNAL_IP */
+
 static __always_inline int __sock6_xlate(struct bpf_sock_addr *ctx)
 {
 	struct lb6_backend *backend;
@@ -595,12 +697,9 @@ static __always_inline int __sock6_xlate(struct bpf_sock_addr *ctx)
 
 	svc = __lb6_lookup_service(&key);
 	if (!svc) {
-		__builtin_memset(&key.address, 0, sizeof(key.address));
 		key.dport = ctx_get_port(ctx);
 
-		sock6_handle_node_port(ctx, &key);
-		if (ipv6_addrcmp(&key.address, &v6_orig))
-			svc = __lb6_lookup_service(&key);
+		svc = sock6_nodeport_wildcard_lookup(&key, true);
 		if (svc && !lb6_svc_is_nodeport(svc))
 			svc = NULL;
 		else if (!svc)
@@ -691,12 +790,9 @@ static __always_inline int __sock6_xlate_snd(struct bpf_sock_addr *ctx)
 
 	svc = __lb6_lookup_service(&lkey);
 	if (!svc) {
-		__builtin_memset(&lkey.address, 0, sizeof(lkey.address));
 		lkey.dport = ctx_get_port(ctx);
 
-		sock6_handle_node_port(ctx, &lkey);
-		if (ipv6_addrcmp(&lkey.address, &v6_orig))
-			svc = __lb6_lookup_service(&lkey);
+		svc = sock6_nodeport_wildcard_lookup(&lkey, true);
 		if (svc && !lb6_svc_is_nodeport(svc))
 			svc = NULL;
 		else if (!svc)

bpf/init.sh
@@ -569,6 +569,7 @@ if [ "$HOSTLB" = "true" ]; then
 	CALLS_MAP="cilium_calls_lb"
 	COPTS="-DLB_L3 -DLB_L4"
 	if [ "$IP6_HOST" != "<nil>" ]; then
+		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sock post_bind6 post-bind-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr connect6 from-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 		if [ "$HOSTLB_UDP" = "true" ]; then
 			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr sendmsg6 snd-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
@@ -579,6 +580,7 @@ if [ "$HOSTLB" = "true" ]; then
 		fi
 	fi
 	if [ "$IP4_HOST" != "<nil>" ]; then
+		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sock post_bind4 post-bind-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr connect4 from-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 		if [ "$HOSTLB_UDP" = "true" ]; then
 			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr sendmsg4 snd-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
@@ -589,6 +591,8 @@ if [ "$HOSTLB" = "true" ]; then
 		fi
 	fi
 else
+	bpf_clear_cgroups $CGROUP_ROOT post_bind4
+	bpf_clear_cgroups $CGROUP_ROOT post_bind6
 	bpf_clear_cgroups $CGROUP_ROOT connect4
 	bpf_clear_cgroups $CGROUP_ROOT connect6
 	bpf_clear_cgroups $CGROUP_ROOT sendmsg4

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=9d1794ed0d6432dd6992787180501669910541f1
+GO_BINDATA_SHA1SUM=efeffc4accd73ad58f2c53879203ddc77abe76c8
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/conntrack_test.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/identity.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/ipv6_test.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/signal.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_max_insn.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

bpf/lib/common.h
@@ -721,5 +721,8 @@ static inline int redirect_peer(int ifindex, uint32_t flags)
 #ifndef ENOMEM
 # define ENOMEM		12
 #endif
+#ifndef EADDRINUSE
+# define EADDRINUSE		98
+#endif
 
 #endif /* __LIB_COMMON_H_ */

commit c5630359915b84e85d8a65a4a3cdc81ef52411b7

bpf, sock: fix post-bind-sock{4,6} not found in ELF file
If kube-proxy replacement is in partial mode and only host-reachable services
are enabled, the agent will bail out with the following error:

  [...]
  level=warning msg="+ tc exec bpf pin /sys/fs/bpf/tc/globals/cilium_cgroups_post_bind4 obj bpf_sock.o type sock attach_type post_bind4 sec post-bind-sock4" subsys=datapath-loader
  level=warning msg="Program section 'post-bind-sock4' not found in ELF file!" subsys=datapath-loader
  [...]

Given externalIPs depends on NodePort, we can reuse $NODE_PORT in init.sh.

Also fix up some small code nits in bpf_sock in bind sections.

Fixes: #10120
Fixes: 245bd348c3e9 ("bpf: Add post_bind{4,6} programs to block NodePorts")
Reported-by: Paul Chaignon <paul@isovalent.com>
Reported-by: Quentin Monnet <quentin@isovalent.com>
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/bpf_sock.c
@@ -667,13 +667,11 @@ static __always_inline int __sock6_post_bind(struct bpf_sock *ctx)
 			return sock6_post_bind_v4_in_v6(ctx);
 	}
 
-	if (svc && (lb6_svc_is_nodeport(svc) || lb6_svc_is_external_ip(svc))) {
+	if (svc && (lb6_svc_is_nodeport(svc) || lb6_svc_is_external_ip(svc)))
 		return -EADDRINUSE;
-	}
 
 	return 0;
 }
-
 
 __section("post-bind-sock6")
 int sock6_post_bind(struct bpf_sock *ctx)
@@ -683,7 +681,6 @@ int sock6_post_bind(struct bpf_sock *ctx)
 
 	return SYS_PROCEED;
 }
-
 #endif /* ENABLE_NODEPORT || ENABLE_EXTERNAL_IP */
 
 static __always_inline int __sock6_xlate(struct bpf_sock_addr *ctx)

bpf/init.sh
@@ -569,8 +569,12 @@ if [ "$HOSTLB" = "true" ]; then
 	CALLS_MAP="cilium_calls_lb"
 	COPTS="-DLB_L3 -DLB_L4"
 	if [ "$IP6_HOST" != "<nil>" ] || [ "$IP4_HOST" != "<nil>" ] && [ -f /proc/sys/net/ipv6/conf/all/forwarding ]; then
-		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sock post_bind6 post-bind-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr connect6 from-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		if [ "$NODE_PORT" = "true" ]; then
+			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sock post_bind6 post-bind-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		else
+			bpf_clear_cgroups $CGROUP_ROOT post_bind6
+		fi
 		if [ "$HOSTLB_UDP" = "true" ]; then
 			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr sendmsg6 snd-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr recvmsg6 rcv-sock6 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
@@ -580,8 +584,12 @@ if [ "$HOSTLB" = "true" ]; then
 		fi
 	fi
 	if [ "$IP4_HOST" != "<nil>" ]; then
-		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sock post_bind4 post-bind-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 		bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr connect4 from-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		if [ "$NODE_PORT" = "true" ]; then
+			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sock post_bind4 post-bind-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
+		else
+			bpf_clear_cgroups $CGROUP_ROOT post_bind4
+		fi
 		if [ "$HOSTLB_UDP" = "true" ]; then
 			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr sendmsg4 snd-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT
 			bpf_load_cgroups "$COPTS" bpf_sock.c bpf_sock.o sockaddr recvmsg4 rcv-sock4 $CALLS_MAP $CGROUP_ROOT $BPFFS_ROOT

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=2187397449d5dfb385f994d126aba468e8af59ce
+GO_BINDATA_SHA1SUM=e1803132450ba29cc22d740d0085bace4ea2dfe3
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/conntrack_test.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/identity.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/ipv6_test.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/signal.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_max_insn.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 