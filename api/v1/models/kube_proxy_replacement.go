g0odi3z2j/cilium

commit 16bc511c5b9e3b9f0fe80298b7011484b12f2ae9

api: Extend proxy redirect status
Extend the proxy redirect status to include more useful debugging
information including a detailed breakdown of each redirect.

Signed-off-by: Joe Stringer <joe@cilium.io>

api/v1/models/proxy_status.go
@@ -6,8 +6,11 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"strconv"
+
 	strfmt "github.com/go-openapi/strfmt"
 
+	"github.com/go-openapi/errors"
 	"github.com/go-openapi/swag"
 )
 
@@ -20,10 +23,53 @@ type ProxyStatus struct {
 
 	// Port range used for proxying
 	PortRange string `json:"port-range,omitempty"`
+
+	// Detailed description of configured redirects
+	Redirects []*ProxyRedirect `json:"redirects"`
+
+	// Total number of listening proxy ports
+	TotalPorts int64 `json:"total-ports,omitempty"`
+
+	// Total number of ports configured to redirect to proxies
+	TotalRedirects int64 `json:"total-redirects,omitempty"`
 }
 
 // Validate validates this proxy status
 func (m *ProxyStatus) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.validateRedirects(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
+
+func (m *ProxyStatus) validateRedirects(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Redirects) { // not required
+		return nil
+	}
+
+	for i := 0; i < len(m.Redirects); i++ {
+		if swag.IsZero(m.Redirects[i]) { // not required
+			continue
+		}
+
+		if m.Redirects[i] != nil {
+			if err := m.Redirects[i].Validate(formats); err != nil {
+				if ve, ok := err.(*errors.Validation); ok {
+					return ve.ValidateName("redirects" + "." + strconv.Itoa(i))
+				}
+				return err
+			}
+		}
+
+	}
+
 	return nil
 }
 

api/v1/models/proxy_redirect.go
@@ -1,0 +1,49 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package models
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/go-openapi/swag"
+)
+
+// ProxyRedirect Configured proxy redirection state
+// swagger:model ProxyRedirect
+type ProxyRedirect struct {
+
+	// Name of the proxy redirect
+	Name string `json:"name,omitempty"`
+
+	// Name of the proxy this redirect points to
+	Proxy string `json:"proxy,omitempty"`
+
+	// Host port that this redirect points to
+	ProxyPort int64 `json:"proxy-port,omitempty"`
+}
+
+// Validate validates this proxy redirect
+func (m *ProxyRedirect) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *ProxyRedirect) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *ProxyRedirect) UnmarshalBinary(b []byte) error {
+	var res ProxyRedirect
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}

api/v1/openapi.yaml
@@ -2065,6 +2065,30 @@ definitions:
       ip:
         description: IP address that the proxy listens on
         type: string
+      total-redirects:
+        description: Total number of ports configured to redirect to proxies
+        type: integer
+      total-ports:
+        description: Total number of listening proxy ports
+        type: integer
+      redirects:
+        description: Detailed description of configured redirects
+        type: array
+        items:
+          "$ref": "#/definitions/ProxyRedirect"
+  ProxyRedirect:
+    description: Configured proxy redirection state
+    type: object
+    properties:
+      name:
+        description: Name of the proxy redirect
+        type: string
+      proxy:
+        description: Name of the proxy this redirect points to
+        type: string
+      proxy-port:
+        description: Host port that this redirect points to
+        type: integer
   ProxyStatistics:
     description: Statistics of a set of proxy redirects for an endpoint
     type: object

api/v1/server/embedded_spec.go
@@ -2787,6 +2787,24 @@ func init() {
         }
       }
     },
+    "ProxyRedirect": {
+      "description": "Configured proxy redirection state",
+      "type": "object",
+      "properties": {
+        "name": {
+          "description": "Name of the proxy redirect",
+          "type": "string"
+        },
+        "proxy": {
+          "description": "Name of the proxy this redirect points to",
+          "type": "string"
+        },
+        "proxy-port": {
+          "description": "Host port that this redirect points to",
+          "type": "integer"
+        }
+      }
+    },
     "ProxyStatistics": {
       "description": "Statistics of a set of proxy redirects for an endpoint",
       "type": "object",
@@ -2828,6 +2846,21 @@ func init() {
         "port-range": {
           "description": "Port range used for proxying",
           "type": "string"
+        },
+        "redirects": {
+          "description": "Detailed description of configured redirects",
+          "type": "array",
+          "items": {
+            "$ref": "#/definitions/ProxyRedirect"
+          }
+        },
+        "total-ports": {
+          "description": "Total number of listening proxy ports",
+          "type": "integer"
+        },
+        "total-redirects": {
+          "description": "Total number of ports configured to redirect to proxies",
+          "type": "integer"
         }
       }
     },
@@ -6144,6 +6177,24 @@ func init() {
         }
       }
     },
+    "ProxyRedirect": {
+      "description": "Configured proxy redirection state",
+      "type": "object",
+      "properties": {
+        "name": {
+          "description": "Name of the proxy redirect",
+          "type": "string"
+        },
+        "proxy": {
+          "description": "Name of the proxy this redirect points to",
+          "type": "string"
+        },
+        "proxy-port": {
+          "description": "Host port that this redirect points to",
+          "type": "integer"
+        }
+      }
+    },
     "ProxyStatistics": {
       "description": "Statistics of a set of proxy redirects for an endpoint",
       "type": "object",
@@ -6185,6 +6236,21 @@ func init() {
         "port-range": {
           "description": "Port range used for proxying",
           "type": "string"
+        },
+        "redirects": {
+          "description": "Detailed description of configured redirects",
+          "type": "array",
+          "items": {
+            "$ref": "#/definitions/ProxyRedirect"
+          }
+        },
+        "total-ports": {
+          "description": "Total number of listening proxy ports",
+          "type": "integer"
+        },
+        "total-redirects": {
+          "description": "Total number of ports configured to redirect to proxies",
+          "type": "integer"
         }
       }
     },

commit 1d2df2e8a79b2f0f889dadb4b517363df35ed8e8

api: Add missing annotations to generate DeepCopy for new status fields
The referenced commits below did not add the annotations required to generate
the DeepCopy() code. Add the annotations and autogenerate the code.

Fixes: 16bc511c5b9 ("api: Extend proxy redirect status")
Fixes: fd6c5887c01 ("api: Add KubeProxyReplacement field to StatusResponse")

Signed-off-by: Thomas Graf <thomas@cilium.io>

api/v1/models/kube_proxy_replacement.go
@@ -17,6 +17,7 @@ import (
 
 // KubeProxyReplacement Status of kube-proxy replacement
 // swagger:model KubeProxyReplacement
+// +k8s:deepcopy-gen=true
 type KubeProxyReplacement struct {
 
 	// features
@@ -132,6 +133,7 @@ func (m *KubeProxyReplacement) UnmarshalBinary(b []byte) error {
 
 // KubeProxyReplacementFeatures kube proxy replacement features
 // swagger:model KubeProxyReplacementFeatures
+// +k8s:deepcopy-gen=true
 type KubeProxyReplacementFeatures struct {
 
 	// external i ps
@@ -240,6 +242,7 @@ func (m *KubeProxyReplacementFeatures) UnmarshalBinary(b []byte) error {
 
 // KubeProxyReplacementFeaturesExternalIPs kube proxy replacement features external i ps
 // swagger:model KubeProxyReplacementFeaturesExternalIPs
+// +k8s:deepcopy-gen=true
 type KubeProxyReplacementFeaturesExternalIPs struct {
 
 	// enabled
@@ -271,6 +274,7 @@ func (m *KubeProxyReplacementFeaturesExternalIPs) UnmarshalBinary(b []byte) erro
 
 // KubeProxyReplacementFeaturesHostReachableServices kube proxy replacement features host reachable services
 // swagger:model KubeProxyReplacementFeaturesHostReachableServices
+// +k8s:deepcopy-gen=true
 type KubeProxyReplacementFeaturesHostReachableServices struct {
 
 	// enabled
@@ -305,6 +309,7 @@ func (m *KubeProxyReplacementFeaturesHostReachableServices) UnmarshalBinary(b []
 
 // KubeProxyReplacementFeaturesNodePort kube proxy replacement features node port
 // swagger:model KubeProxyReplacementFeaturesNodePort
+// +k8s:deepcopy-gen=true
 type KubeProxyReplacementFeaturesNodePort struct {
 
 	// enabled

api/v1/models/proxy_status.go
@@ -16,6 +16,7 @@ import (
 
 // ProxyStatus Status of proxy
 // swagger:model ProxyStatus
+// +k8s:deepcopy-gen=true
 type ProxyStatus struct {
 
 	// IP address that the proxy listens on

api/v1/models/zz_generated.deepcopy.go
@@ -154,6 +154,111 @@ func (in *K8sStatus) DeepCopy() *K8sStatus {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KubeProxyReplacement) DeepCopyInto(out *KubeProxyReplacement) {
+	*out = *in
+	if in.Features != nil {
+		in, out := &in.Features, &out.Features
+		*out = new(KubeProxyReplacementFeatures)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KubeProxyReplacement.
+func (in *KubeProxyReplacement) DeepCopy() *KubeProxyReplacement {
+	if in == nil {
+		return nil
+	}
+	out := new(KubeProxyReplacement)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KubeProxyReplacementFeatures) DeepCopyInto(out *KubeProxyReplacementFeatures) {
+	*out = *in
+	if in.ExternalIPs != nil {
+		in, out := &in.ExternalIPs, &out.ExternalIPs
+		*out = new(KubeProxyReplacementFeaturesExternalIPs)
+		**out = **in
+	}
+	if in.HostReachableServices != nil {
+		in, out := &in.HostReachableServices, &out.HostReachableServices
+		*out = new(KubeProxyReplacementFeaturesHostReachableServices)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.NodePort != nil {
+		in, out := &in.NodePort, &out.NodePort
+		*out = new(KubeProxyReplacementFeaturesNodePort)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KubeProxyReplacementFeatures.
+func (in *KubeProxyReplacementFeatures) DeepCopy() *KubeProxyReplacementFeatures {
+	if in == nil {
+		return nil
+	}
+	out := new(KubeProxyReplacementFeatures)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KubeProxyReplacementFeaturesExternalIPs) DeepCopyInto(out *KubeProxyReplacementFeaturesExternalIPs) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KubeProxyReplacementFeaturesExternalIPs.
+func (in *KubeProxyReplacementFeaturesExternalIPs) DeepCopy() *KubeProxyReplacementFeaturesExternalIPs {
+	if in == nil {
+		return nil
+	}
+	out := new(KubeProxyReplacementFeaturesExternalIPs)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KubeProxyReplacementFeaturesHostReachableServices) DeepCopyInto(out *KubeProxyReplacementFeaturesHostReachableServices) {
+	*out = *in
+	if in.Protocols != nil {
+		in, out := &in.Protocols, &out.Protocols
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KubeProxyReplacementFeaturesHostReachableServices.
+func (in *KubeProxyReplacementFeaturesHostReachableServices) DeepCopy() *KubeProxyReplacementFeaturesHostReachableServices {
+	if in == nil {
+		return nil
+	}
+	out := new(KubeProxyReplacementFeaturesHostReachableServices)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *KubeProxyReplacementFeaturesNodePort) DeepCopyInto(out *KubeProxyReplacementFeaturesNodePort) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new KubeProxyReplacementFeaturesNodePort.
+func (in *KubeProxyReplacementFeaturesNodePort) DeepCopy() *KubeProxyReplacementFeaturesNodePort {
+	if in == nil {
+		return nil
+	}
+	out := new(KubeProxyReplacementFeaturesNodePort)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *NodeAddressing) DeepCopyInto(out *NodeAddressing) {
 	*out = *in
 	if in.IPV4 != nil {
@@ -238,6 +343,33 @@ func (in *ProxyStatistics) DeepCopy() *ProxyStatistics {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ProxyStatus) DeepCopyInto(out *ProxyStatus) {
+	*out = *in
+	if in.Redirects != nil {
+		in, out := &in.Redirects, &out.Redirects
+		*out = make([]*ProxyRedirect, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(ProxyRedirect)
+				**out = **in
+			}
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ProxyStatus.
+func (in *ProxyStatus) DeepCopy() *ProxyStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(ProxyStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *RequestResponseStatistics) DeepCopyInto(out *RequestResponseStatistics) {
 	*out = *in
 	if in.Requests != nil {
@@ -297,6 +429,11 @@ func (in *StatusResponse) DeepCopyInto(out *StatusResponse) {
 		*out = new(IPAMStatus)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.KubeProxyReplacement != nil {
+		in, out := &in.KubeProxyReplacement, &out.KubeProxyReplacement
+		*out = new(KubeProxyReplacement)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.Kubernetes != nil {
 		in, out := &in.Kubernetes, &out.Kubernetes
 		*out = new(K8sStatus)
@@ -315,7 +452,7 @@ func (in *StatusResponse) DeepCopyInto(out *StatusResponse) {
 	if in.Proxy != nil {
 		in, out := &in.Proxy, &out.Proxy
 		*out = new(ProxyStatus)
-		**out = **in
+		(*in).DeepCopyInto(*out)
 	}
 	if in.Stale != nil {
 		in, out := &in.Stale, &out.Stale