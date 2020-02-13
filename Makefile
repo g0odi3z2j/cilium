g0odi3z2j/cilium

commit 7052a26b961461ca6094e954bfadfada53903dd2

Makefile: Fix duplicates in cscope output
For some reason, some files would show up multiple times in the
GOLANG_SRCFILES variable, which would cause cscope to create duplicate
references to symbols across the codebase. Pass the list through sort
and uniq to ensure each file is only scanned once.

Signed-off-by: Joe Stringer <joe@cilium.io>

Makefile
@@ -9,7 +9,7 @@ SUBDIRS = $(SUBDIRS_CILIUM_CONTAINER) operator plugins tools
 GOFILES ?= $(subst _$(ROOT_DIR)/,,$(shell $(GO) list ./... | grep -v -e /vendor/ -e /contrib/))
 TESTPKGS ?= $(subst github.com/cilium/cilium/,,$(shell $(GO) list ./... | grep -v -e /api/v1 -e /vendor/ -e /contrib/ -e test))
 GOLANGVERSION = $(shell $(GO) version 2>/dev/null | grep -Eo '(go[0-9].[0-9])')
-GOLANG_SRCFILES=$(shell for pkg in $(subst github.com/cilium/cilium/,,$(GOFILES)); do find $$pkg -name *.go -print; done | grep -v vendor)
+GOLANG_SRCFILES=$(shell for pkg in $(subst github.com/cilium/cilium/,,$(GOFILES)); do find $$pkg -name *.go -print; done | grep -v vendor | sort | uniq)
 BPF_FILES ?= $(shell git ls-files ../bpf/ | grep -v .gitignore | tr "\n" ' ')
 BPF_SRCFILES=$(subst ../,,$(BPF_FILES))
 
@@ -135,7 +135,7 @@ clean-tags:
 	@$(ECHO_CLEAN) tags
 	@-rm -f cscope.out cscope.in.out cscope.po.out cscope.files tags
 
-tags: $(GOLANG_SRCFILES) $(BPF_SRCFILES)
+tags: $(GOLANG_SRCFILES) $(BPF_SRCFILES) cscope.files
 	ctags $(GOLANG_SRCFILES) $(BPF_SRCFILES)
 	@ echo $(GOLANG_SRCFILES) $(BPF_SRCFILES) | sed 's/ /\n/g' | sort > cscope.files
 	cscope -R -b -q

commit ca7ce68de869c1f2f1427f0962941e76f17e3256

Makefile: move cscope.files generation to its own target
Commit 7052a26b9614 ("Makefile: Fix duplicates in cscope output") added
cscope.files as a dependency to the "tags" target. However, this is the
very target supposed to be used to build cscope.files in the first
place, so running "make tags" for the first time (when cscope.files is
not present) fails.

Let's keep cscope.files as a dependency for "tags", but move the
generation of that file to its own target.

Also silence the "ctags" command when creating tags.

Fixes: 7052a26b9614 ("Makefile: Fix duplicates in cscope output")

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

Makefile
@@ -178,9 +178,11 @@ clean-tags:
 	@$(ECHO_CLEAN) tags
 	@-rm -f cscope.out cscope.in.out cscope.po.out cscope.files tags
 
+cscope.files: $(GOLANG_SRCFILES) $(BPF_SRCFILES)
+	@echo $(GOLANG_SRCFILES) $(BPF_SRCFILES) | sed 's/ /\n/g' | sort > cscope.files
+
 tags: $(GOLANG_SRCFILES) $(BPF_SRCFILES) cscope.files
-	ctags $(GOLANG_SRCFILES) $(BPF_SRCFILES)
-	@ echo $(GOLANG_SRCFILES) $(BPF_SRCFILES) | sed 's/ /\n/g' | sort > cscope.files
+	@ctags $(GOLANG_SRCFILES) $(BPF_SRCFILES)
 	cscope -R -b -q
 
 clean-container: