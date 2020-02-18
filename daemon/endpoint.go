g0odi3z2j/cilium

commit d4751e16f5cbcca1fc607d4af17ed20158ce1219

cni: Release IP even when endpoint deletion fails
When the endpoint create API call takes a long time, the CNI ADD caller can
forcefully stop the cilium-cni binary. While the endpoint create API call will
eventually time out or is forcefully interrupted due to a broken pipe, the
error handler to release the previously allocated IP is never invoked. This
will result in leaked IP addresses.

In order to resolve this, release all IPs via the pod name on CNI DEL in case
the endpoint delete API call does not succeed. In the case of a successful
endpoint delete call, the IPs were already released by deleting the endpoint.

Fixes: #9854

Signed-off-by: Thomas Graf <thomas@cilium.io>

plugins/cilium-cni/cilium-cni.go
@@ -600,6 +600,20 @@ func cmdDel(args *skel.CmdArgs) error {
 		//                         the endpoint is always deleted though, no
 		//                         need to retry
 		log.WithError(err).Warning("Errors encountered while deleting endpoint")
+
+		// Endpoint deletion has failed. We can't be sure whether the
+		// CNI ADD was interrupted in between the IP allocation and the
+		// endpoint creation. In order to guarantee to never leak IP
+		// addresses, delete any IP addresses associated with the pod
+		// being deleted. This task is only done manually on endpoint
+		// delete failure as all IPs of an endpoint are released when
+		// the endpoint is deleted.
+		log.Info("Releasing IPs manually due to inability to delete endpoint")
+		podName := string(cniArgs.K8S_POD_NAMESPACE) + "/" + string(cniArgs.K8S_POD_NAME)
+		err = c.IPAMReleaseIP(podName)
+		if err != nil {
+			log.WithError(err).WithField("pod", podName).Warning("Unable to manually release IPs of pod")
+		}
 	}
 
 	netNs, err := ns.GetNS(args.Netns)

api/v1/client/ipam/delete_ipam_ip_parameters.go
@@ -62,7 +62,7 @@ for the delete ipam IP operation typically these are written to a http.Request
 type DeleteIpamIPParams struct {
 
 	/*IP
-	  IP address
+	  IP address or owner name
 
 	*/
 	IP string

api/v1/openapi.yaml
@@ -444,7 +444,7 @@ paths:
       tags:
       - ipam
       parameters:
-      - "$ref": "#/parameters/ipam-ip"
+      - "$ref": "#/parameters/ipam-release-arg"
       responses:
         '200':
           description: Success
@@ -934,6 +934,12 @@ parameters:
   ipam-ip:
     name: ip
     description: IP address
+    in: path
+    required: true
+    type: string
+  ipam-release-arg:
+    name: ip
+    description: IP address or owner name
     in: path
     required: true
     type: string

api/v1/server/embedded_spec.go
@@ -844,7 +844,7 @@ func init() {
         "summary": "Release an allocated IP address",
         "parameters": [
           {
-            "$ref": "#/parameters/ipam-ip"
+            "$ref": "#/parameters/ipam-release-arg"
           }
         ],
         "responses": {
@@ -3063,6 +3063,13 @@ func init() {
       "name": "owner",
       "in": "query"
     },
+    "ipam-release-arg": {
+      "type": "string",
+      "description": "IP address or owner name",
+      "name": "ip",
+      "in": "path",
+      "required": true
+    },
     "labels": {
       "description": "List of labels\n",
       "name": "labels",
@@ -4074,7 +4081,7 @@ func init() {
         "parameters": [
           {
             "type": "string",
-            "description": "IP address",
+            "description": "IP address or owner name",
             "name": "ip",
             "in": "path",
             "required": true
@@ -6340,6 +6347,13 @@ func init() {
       "type": "string",
       "name": "owner",
       "in": "query"
+    },
+    "ipam-release-arg": {
+      "type": "string",
+      "description": "IP address or owner name",
+      "name": "ip",
+      "in": "path",
+      "required": true
     },
     "labels": {
       "description": "List of labels\n",

api/v1/server/restapi/ipam/delete_ipam_ip_parameters.go
@@ -30,7 +30,7 @@ type DeleteIpamIPParams struct {
 	// HTTP Request Object
 	HTTPRequest *http.Request `json:"-"`
 
-	/*IP address
+	/*IP address or owner name
 	  Required: true
 	  In: path
 	*/

commit 75f4ff825ca0fab8765f1004bac56ca658b3f35f

plugins: Enable address allocation expiration
This is a replacement fix for d4751e16f5 which turned out to be racy, leading
to alive PodIPs to be freed. Instead of attempting to reconstruct the context
for potential release in CNI DEL for any failure scenarios, introduce an
expiration timer that is enabled when any IP is allocated via the CNI plugin.
The expiration timer must be explicitly stopped by the endpoint creation
following the allocation to avoid releasing the IP again after a timeout. This
ensures that IPs are always either used or released without requiring any
cleanup from the CNI plugin itself.

In the event that an IP is released due to expiration but then the endpoint
still succeeds to be created, the endpoint creation will fail, triggering a
re-creation of the endpoint.

Fixes: #10065
Fixes: d4751e16f5c ("cni: Release IP even when endpoint deletion fails")
Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/endpoint.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
 	"runtime"
 	"sync"
 
@@ -321,6 +322,26 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 	if err := ep.RegenerateAfterCreation(ctx, epTemplate.SyncBuildEndpoint); err != nil {
 		return d.errorDuringCreation(ep, err)
 	}
+
+	// The endpoint has been successfully created, stop the expiration
+	// timers of all attached IPs
+	if addressing := epTemplate.Addressing; addressing != nil {
+		if uuid := addressing.IPV4ExpirationUUID; uuid != "" {
+			if ip := net.ParseIP(addressing.IPV4); ip != nil {
+				if err := d.ipam.StopExpirationTimer(ip, uuid); err != nil {
+					return d.errorDuringCreation(ep, err)
+				}
+			}
+		}
+		if uuid := addressing.IPV6ExpirationUUID; uuid != "" {
+			if ip := net.ParseIP(addressing.IPV6); ip != nil {
+				if err := d.ipam.StopExpirationTimer(ip, uuid); err != nil {
+					return d.errorDuringCreation(ep, err)
+				}
+			}
+		}
+	}
+
 	return ep, 0, nil
 }
 

daemon/ipam.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,12 +18,14 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	ipamapi "github.com/cilium/cilium/api/v1/server/restapi/ipam"
 	"github.com/cilium/cilium/pkg/api"
 	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
@@ -46,7 +48,11 @@ func NewPostIPAMHandler(d *Daemon) ipamapi.PostIpamHandler {
 func (h *postIPAM) Handle(params ipamapi.PostIpamParams) middleware.Responder {
 	family := strings.ToLower(swag.StringValue(params.Family))
 	owner := swag.StringValue(params.Owner)
-	ipv4Result, ipv6Result, err := h.daemon.ipam.AllocateNext(family, owner)
+	var expirationTimeout time.Duration
+	if swag.BoolValue(params.Expiration) {
+		expirationTimeout = defaults.IPAMExpiration
+	}
+	ipv4Result, ipv6Result, err := h.daemon.ipam.AllocateNextWithExpiration(family, owner, expirationTimeout)
 	if err != nil {
 		return api.Error(ipamapi.PostIpamFailureCode, err)
 	}
@@ -59,20 +65,22 @@ func (h *postIPAM) Handle(params ipamapi.PostIpamParams) middleware.Responder {
 	if ipv4Result != nil {
 		resp.Address.IPV4 = ipv4Result.IP.String()
 		resp.IPV4 = &models.IPAMAddressResponse{
-			Cidrs:     ipv4Result.CIDRs,
-			IP:        ipv4Result.IP.String(),
-			MasterMac: ipv4Result.Master,
-			Gateway:   ipv4Result.GatewayIP,
+			Cidrs:          ipv4Result.CIDRs,
+			IP:             ipv4Result.IP.String(),
+			MasterMac:      ipv4Result.Master,
+			Gateway:        ipv4Result.GatewayIP,
+			ExpirationUUID: ipv4Result.ExpirationUUID,
 		}
 	}
 
 	if ipv6Result != nil {
 		resp.Address.IPV6 = ipv6Result.IP.String()
 		resp.IPV6 = &models.IPAMAddressResponse{
-			Cidrs:     ipv6Result.CIDRs,
-			IP:        ipv6Result.IP.String(),
-			MasterMac: ipv6Result.Master,
-			Gateway:   ipv6Result.GatewayIP,
+			Cidrs:          ipv6Result.CIDRs,
+			IP:             ipv6Result.IP.String(),
+			MasterMac:      ipv6Result.Master,
+			Gateway:        ipv6Result.GatewayIP,
+			ExpirationUUID: ipv6Result.ExpirationUUID,
 		}
 	}
 

pkg/client/ipam.go
@@ -26,7 +26,7 @@ const (
 )
 
 // IPAMAllocate allocates an IP address out of address family specific pool.
-func (c *Client) IPAMAllocate(family, owner string) (*models.IPAMResponse, error) {
+func (c *Client) IPAMAllocate(family, owner string, expiration bool) (*models.IPAMResponse, error) {
 	params := ipam.NewPostIpamParams().WithTimeout(api.ClientTimeout)
 
 	if family != "" {
@@ -36,6 +36,8 @@ func (c *Client) IPAMAllocate(family, owner string) (*models.IPAMResponse, error
 	if owner != "" {
 		params.SetOwner(&owner)
 	}
+
+	params.SetExpiration(&expiration)
 
 	resp, err := c.Ipam.PostIpam(params)
 	if err != nil {

pkg/defaults/defaults.go
@@ -346,4 +346,8 @@ const (
 
 	// NodePortMode is the default value for option.NodePortMode
 	NodePortMode = "snat"
+
+	// IPAMExpiration is the timeout after which an IP subject to expiratio
+	// is being released again if no endpoint is being created in time.
+	IPAMExpiration = 3 * time.Minute
 )

plugins/cilium-cni/cilium-cni.go
@@ -432,7 +432,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 	}
 
 	podName := string(cniArgs.K8S_POD_NAMESPACE) + "/" + string(cniArgs.K8S_POD_NAME)
-	ipam, err = c.IPAMAllocate("", podName)
+	ipam, err = c.IPAMAllocate("", podName, true)
 	if err != nil {
 		err = fmt.Errorf("unable to allocate IP via local cilium agent: %s", err)
 		return
@@ -471,6 +471,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 
 	if ipv6IsEnabled(ipam) {
 		ep.Addressing.IPV6 = ipam.Address.IPV6
+		ep.Addressing.IPV6ExpirationUUID = ipam.IPV6.ExpirationUUID
 
 		ipConfig, routes, err = prepareIP(ep.Addressing.IPV6, true, &state, int(conf.RouteMTU))
 		if err != nil {
@@ -483,6 +484,7 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 
 	if ipv4IsEnabled(ipam) {
 		ep.Addressing.IPV4 = ipam.Address.IPV4
+		ep.Addressing.IPV4ExpirationUUID = ipam.IPV4.ExpirationUUID
 
 		ipConfig, routes, err = prepareIP(ep.Addressing.IPV4, false, &state, int(conf.RouteMTU))
 		if err != nil {
@@ -602,20 +604,6 @@ func cmdDel(args *skel.CmdArgs) error {
 		//                         the endpoint is always deleted though, no
 		//                         need to retry
 		log.WithError(err).Warning("Errors encountered while deleting endpoint")
-
-		// Endpoint deletion has failed. We can't be sure whether the
-		// CNI ADD was interrupted in between the IP allocation and the
-		// endpoint creation. In order to guarantee to never leak IP
-		// addresses, delete any IP addresses associated with the pod
-		// being deleted. This task is only done manually on endpoint
-		// delete failure as all IPs of an endpoint are released when
-		// the endpoint is deleted.
-		log.Info("Releasing IPs manually due to inability to delete endpoint")
-		podName := string(cniArgs.K8S_POD_NAMESPACE) + "/" + string(cniArgs.K8S_POD_NAME)
-		err = c.IPAMReleaseIP(podName)
-		if err != nil {
-			log.WithError(err).WithField("pod", podName).Warning("Unable to manually release IPs of pod")
-		}
 	}
 
 	netNs, err := ns.GetNS(args.Netns)

plugins/cilium-docker/driver/ipam.go
@@ -116,7 +116,7 @@ func (driver *driver) requestAddress(w http.ResponseWriter, r *http.Request) {
 		family = client.AddressFamilyIPv6
 	}
 
-	ipam, err := driver.client.IPAMAllocate(family, "docker-ipam")
+	ipam, err := driver.client.IPAMAllocate(family, "docker-ipam", false)
 	if err != nil {
 		sendError(w, fmt.Sprintf("Could not allocate IP address: %s", err), http.StatusBadRequest)
 		return