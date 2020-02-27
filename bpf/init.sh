g0odi3z2j/cilium

commit fbe207eaeb982df0fbde760d1d6d3a292d30894b

bpf: Remove bpf_netdev.o from previously used devices
This commit makes cilium-agent to remove bpf_netdev.o from devices which
no longer suppose to have the program attached. This can happen when
e.g. a user has specified a different device for NodePort via `--device`
or they switched from the direct routing mode to the tunnel mode.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/init.sh
@@ -561,6 +561,17 @@ else
 	fi
 fi
 
+# Remove bpf_netdev.o from previously used devices
+for iface in $(ip -o -a l | awk '{print $2}' | cut -d: -f1 | cut -d@ -f1 | grep -v cilium); do
+    [ "$iface" == "$NATIVE_DEV" ] && continue
+    for where in ingress egress; do
+        if tc tc filter show dev "$iface" "$where" | grep -q "bpf_netdev.o"; then
+            echo "Removing bpf_netdev.o from $where of $iface"
+            tc filter del dev "$iface" "$where" || true
+        fi
+    done
+done
+
 if [ "$HOSTLB" = "true" ]; then
 	if [ "$IP6_HOST" != "<nil>" ]; then
 		echo 1 > /proc/sys/net/ipv6/conf/all/forwarding

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=e1803132450ba29cc22d740d0085bace4ea2dfe3
+GO_BINDATA_SHA1SUM=b2a1b7f878323fce6afa178a3bc347aa457a923c
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/conntrack_test.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/identity.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/ipv6_test.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/signal.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_max_insn.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

commit 2083be9b12b65da01a418529a89c979b00c0a990

bpf: Fix native dev cleanup
Fixes: fbe207eaeb ("bpf: Remove bpf_netdev.o from previously used devices")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/init.sh
@@ -568,7 +568,7 @@ fi
 for iface in $(ip -o -a l | awk '{print $2}' | cut -d: -f1 | cut -d@ -f1 | grep -v cilium); do
     [ "$iface" == "$NATIVE_DEV" ] && continue
     for where in ingress egress; do
-        if tc tc filter show dev "$iface" "$where" | grep -q "bpf_netdev.o"; then
+        if tc filter show dev "$iface" "$where" | grep -q "bpf_netdev.o"; then
             echo "Removing bpf_netdev.o from $where of $iface"
             tc filter del dev "$iface" "$where" || true
         fi