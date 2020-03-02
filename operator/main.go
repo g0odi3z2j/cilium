g0odi3z2j/cilium

commit 69babe82c8a84a1ccab4144ab108859d4ee5d995

operator: add ability to ready options from a config directory
Similar to what we have in cilium-agent we can now have the same in
cilium-operator where the options can be specified in a directory for
better UX when deploying cilium-operator in a cluster.

Signed-off-by: André Martins <andre@cilium.io>

operator/main.go
@@ -88,12 +88,10 @@ func main() {
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if viper.GetBool("version") {
-		fmt.Printf("Cilium %s\n", version.Version)
+		fmt.Printf("Cilium-Operator %s\n", version.Version)
 		os.Exit(0)
 	}
-
-	viper.SetEnvPrefix("cilium")
-	viper.SetConfigName("cilium-operator")
+	cobra.OnInitialize(option.InitConfig("cilium-operator"))
 }
 
 func kvstoreEnabled() bool {

daemon/daemon_main.go
@@ -155,7 +155,11 @@ func init() {
 		return
 	}
 
-	cobra.OnInitialize(initConfig)
+	if viper.GetBool("version") {
+		fmt.Printf("Cilium %s\n", version.Version)
+		os.Exit(0)
+	}
+	cobra.OnInitialize(option.InitConfig("ciliumd"))
 
 	// Reset the help function to also exit, as we block elsewhere in interrupts
 	// and would not exit when called with -h.
@@ -746,57 +750,6 @@ func restoreExecPermissions(searchDir string, patterns ...string) error {
 		}
 	}
 	return err
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if viper.GetBool("version") {
-		fmt.Printf("Cilium %s\n", version.Version)
-		os.Exit(0)
-	}
-
-	if viper.GetString(option.CMDRef) != "" {
-		return
-	}
-
-	option.Config.ConfigFile = viper.GetString(option.ConfigFile) // enable ability to specify config file via flag
-	option.Config.ConfigDir = viper.GetString(option.ConfigDir)
-	viper.SetEnvPrefix("cilium")
-
-	if option.Config.ConfigDir != "" {
-		if _, err := os.Stat(option.Config.ConfigDir); os.IsNotExist(err) {
-			log.Fatalf("Non-existent configuration directory %s", option.Config.ConfigDir)
-		}
-
-		if m, err := option.ReadDirConfig(option.Config.ConfigDir); err != nil {
-			log.Fatalf("Unable to read configuration directory %s: %s", option.Config.ConfigDir, err)
-		} else {
-			// replace deprecated fields with new fields
-			option.ReplaceDeprecatedFields(m)
-			err := option.MergeConfig(m)
-			if err != nil {
-				log.Fatalf("Unable to merge configuration: %s", err)
-			}
-		}
-	}
-
-	if option.Config.ConfigFile != "" {
-		viper.SetConfigFile(option.Config.ConfigFile)
-	} else {
-		viper.SetConfigName("ciliumd") // name of config file (without extension)
-		viper.AddConfigPath("$HOME")   // adding home directory as first search path
-	}
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		log.WithField(logfields.Path, viper.ConfigFileUsed()).
-			Info("Using config from file")
-	} else if option.Config.ConfigFile != "" {
-		log.WithField(logfields.Path, option.Config.ConfigFile).
-			Fatal("Error reading config file")
-	} else {
-		log.WithField(logfields.Reason, err).Info("Skipped reading configuration file")
-	}
 }
 
 func initEnv(cmd *cobra.Command) {

operator/flags.go
@@ -70,6 +70,12 @@ func init() {
 	option.BindEnv(option.ClusterName)
 
 	// Operator-specific flags
+	flags.String(option.ConfigFile, "", `Configuration file (default "$HOME/ciliumd.yaml")`)
+	option.BindEnv(option.ConfigFile)
+
+	flags.String(option.ConfigDir, "", `Configuration directory that contains a file for each option`)
+	option.BindEnv(option.ConfigDir)
+
 	// Deprecated, remove in 1.9
 	flags.Bool(option.EnableCCNPNodeStatusGC, true, "Enable CiliumClusterwideNetworkPolicy Status garbage collection for nodes which have been removed from the cluster")
 	option.BindEnv(option.EnableCCNPNodeStatusGC)

pkg/option/config.go
@@ -2181,3 +2181,51 @@ func getHostDevice() string {
 	}
 	return hostDevice
 }
+
+// InitConfig reads in config file and ENV variables if set.
+func InitConfig(configName string) func() {
+	return func() {
+		if viper.GetString(CMDRef) != "" {
+			return
+		}
+
+		Config.ConfigFile = viper.GetString(ConfigFile) // enable ability to specify config file via flag
+		Config.ConfigDir = viper.GetString(ConfigDir)
+		viper.SetEnvPrefix("cilium")
+
+		if Config.ConfigDir != "" {
+			if _, err := os.Stat(Config.ConfigDir); os.IsNotExist(err) {
+				log.Fatalf("Non-existent configuration directory %s", Config.ConfigDir)
+			}
+
+			if m, err := ReadDirConfig(Config.ConfigDir); err != nil {
+				log.Fatalf("Unable to read configuration directory %s: %s", Config.ConfigDir, err)
+			} else {
+				// replace deprecated fields with new fields
+				ReplaceDeprecatedFields(m)
+				err := MergeConfig(m)
+				if err != nil {
+					log.Fatalf("Unable to merge configuration: %s", err)
+				}
+			}
+		}
+
+		if Config.ConfigFile != "" {
+			viper.SetConfigFile(Config.ConfigFile)
+		} else {
+			viper.SetConfigName(configName) // name of config file (without extension)
+			viper.AddConfigPath("$HOME")    // adding home directory as first search path
+		}
+
+		// If a config file is found, read it in.
+		if err := viper.ReadInConfig(); err == nil {
+			log.WithField(logfields.Path, viper.ConfigFileUsed()).
+				Info("Using config from file")
+		} else if Config.ConfigFile != "" {
+			log.WithField(logfields.Path, Config.ConfigFile).
+				Fatal("Error reading config file")
+		} else {
+			log.WithField(logfields.Reason, err).Info("Skipped reading configuration file")
+		}
+	}
+}

commit a3d3605b808d0f7a293ba4d41b166bb2145f54c9

operator: populate CLI flags from cilium-operator
This commit fixes the ability to read the CLI flags by the operator.

Fixes: 69babe82c8a8 ("operator: add ability to ready options from a config directory")
Signed-off-by: André Martins <andre@cilium.io>

operator/main.go
@@ -21,6 +21,7 @@ import (
 	"os/signal"
 
 	"github.com/cilium/cilium/pkg/aws/eni"
+	"github.com/cilium/cilium/pkg/components"
 	"github.com/cilium/cilium/pkg/k8s"
 	clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
 	"github.com/cilium/cilium/pkg/k8s/types"
@@ -28,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/version"
 
@@ -53,6 +55,7 @@ var (
 				genMarkdown(cmd, cmdRefDir)
 				os.Exit(0)
 			}
+			initEnv()
 			runOperator(cmd)
 		},
 	}
@@ -63,6 +66,19 @@ var (
 
 	ciliumK8sClient clientset.Interface
 )
+
+func initEnv() {
+	// Prepopulate option.Config with options from CLI.
+	option.Config.Populate()
+
+	// add hooks after setting up metrics in the option.Confog
+	logging.DefaultLogger.Hooks.Add(metrics.NewLoggingHook(components.CiliumOperatortName))
+
+	// Logging should always be bootstrapped first. Do not add any code above this!
+	logging.SetupLogging(option.Config.LogDriver, option.Config.LogOpt, "cilium-operator", option.Config.Debug)
+
+	option.LogRegisteredOptions(log)
+}
 
 func main() {
 	signals := make(chan os.Signal, 1)
@@ -113,7 +129,6 @@ func getAPIServerAddr() []string {
 }
 
 func runOperator(cmd *cobra.Command) {
-	logging.SetupLogging([]string{}, map[string]string{}, "cilium-operator", viper.GetBool("debug"))
 
 	log.Infof("Cilium Operator %s", version.Version)
 	k8sInitDone := make(chan struct{})

pkg/components/components.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Authors of Cilium
+// Copyright 2018-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,6 +22,8 @@ import (
 const (
 	// CiliumAgentName is the name of cilium-agent (daemon) process name.
 	CiliumAgentName = "cilium-agent"
+	// CiliumOperatortName is the name of cilium-operator process name.
+	CiliumOperatortName = "cilium-operator"
 	// CiliumDaemonTestName is the name of test binary for daemon package.
 	CiliumDaemonTestName = "daemon.test"
 )

pkg/metrics/logging_hook.go
@@ -42,6 +42,8 @@ func NewLoggingHook(component string) *LoggingHook {
 	switch component {
 	case components.CiliumAgentName:
 		metric = ErrorsWarnings
+	case components.CiliumOperatortName:
+		metric = ErrorsWarnings
 	default:
 		panic(fmt.Sprintf("component %s is unsupported by LoggingHook", component))
 	}