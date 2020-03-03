g0odi3z2j/cilium

commit 6dc451f0611b02823be464a9f1d4b1cd3ad83a91

add capability to disable CNP NodeStatus updates
For `cilium-agent`, add the following option:

*  `disable-cnp-status-updates` will disable CNP NodeStatus updates if set to
true. By default, it is false.

For `cilium-operator`, add the following options:

* `cnp-node-status-gc`: default set to `true`. When `false`, no garbage
collection is performed for noes which have been removed from the cluster in the
Status section of CNPs.

* `cnp-node-status-gc-interval`: default set to `2 mimutes` - the interval of
garbage collection for nodes which have been removed from the cluster for CNP
Node Status

Signed-off by: Ian Vernon <ian@cilium.io>

daemon/daemon_main.go
@@ -829,6 +829,9 @@ func init() {
 	flags.MarkHidden(option.PolicyTriggerInterval)
 	option.BindEnv(option.PolicyTriggerInterval)
 
+	flags.Bool(option.DisableCNPStatusUpdates, false, "Do not send CNP NodeStatus updates to the Kubernetes api-server (recommended to run with `cnp-node-status-gc=false` in cilium-operator)")
+	option.BindEnv(option.DisableCNPStatusUpdates)
+
 	viper.BindPFlags(flags)
 }
 

daemon/k8s_watcher.go
@@ -1533,6 +1533,7 @@ func (d *Daemon) deleteIngressV1beta1(ingress *types.Ingress) error {
 }
 
 func (d *Daemon) updateCiliumNetworkPolicyV2AnnotationsOnly(ciliumNPClient clientset.Interface, ciliumV2Store cache.Store, cnp *types.SlimCNP) {
+
 	scopedLog := log.WithFields(logrus.Fields{
 		logfields.CiliumNetworkPolicyName: cnp.ObjectMeta.Name,
 		logfields.K8sAPIVersion:           cnp.TypeMeta.APIVersion,
@@ -1598,23 +1599,26 @@ func (d *Daemon) addCiliumNetworkPolicyV2(ciliumNPClient clientset.Interface, ci
 	// is populated by the time updateCiliumNetworkPolicyV2 is invoked.
 	importMetadataCache.upsert(cnp, rev, policyImportErr)
 
-	updateContext := &k8s.CNPStatusUpdateContext{
-		CiliumNPClient:              ciliumNPClient,
-		CiliumV2Store:               ciliumV2Store,
-		NodeName:                    node.GetName(),
-		NodeManager:                 d.nodeDiscovery.Manager,
-		UpdateDuration:              spanstat.Start(),
-		WaitForEndpointsAtPolicyRev: endpointmanager.WaitForEndpointsAtPolicyRev,
+	if !option.Config.DisableCNPStatusUpdates {
+		updateContext := &k8s.CNPStatusUpdateContext{
+			CiliumNPClient:              ciliumNPClient,
+			CiliumV2Store:               ciliumV2Store,
+			NodeName:                    node.GetName(),
+			NodeManager:                 d.nodeDiscovery.Manager,
+			UpdateDuration:              spanstat.Start(),
+			WaitForEndpointsAtPolicyRev: endpointmanager.WaitForEndpointsAtPolicyRev,
+		}
+
+		ctrlName := cnp.GetControllerName()
+		k8sCM.UpdateController(ctrlName,
+			controller.ControllerParams{
+				DoFunc: func(ctx context.Context) error {
+					return updateContext.UpdateStatus(ctx, cnp, rev, policyImportErr)
+				},
+			},
+		)
 	}
 
-	ctrlName := cnp.GetControllerName()
-	k8sCM.UpdateController(ctrlName,
-		controller.ControllerParams{
-			DoFunc: func(ctx context.Context) error {
-				return updateContext.UpdateStatus(ctx, cnp, rev, policyImportErr)
-			},
-		},
-	)
 	return policyImportErr
 }
 
@@ -1671,27 +1675,29 @@ func (d *Daemon) updateCiliumNetworkPolicyV2(ciliumNPClient clientset.Interface,
 	}).Debug("Modified CiliumNetworkPolicy")
 
 	// Do not add rule into policy repository if the spec remains unchanged.
-	if oldRuleCpy.SpecEquals(newRuleCpy.CiliumNetworkPolicy) {
-		if !oldRuleCpy.AnnotationsEquals(newRuleCpy.CiliumNetworkPolicy) {
+	if !option.Config.DisableCNPStatusUpdates {
+		if oldRuleCpy.SpecEquals(newRuleCpy.CiliumNetworkPolicy) {
+			if !oldRuleCpy.AnnotationsEquals(newRuleCpy.CiliumNetworkPolicy) {
 
-			// Update annotations within a controller so the status of the update
-			// is trackable from the list of running controllers, and so we do
-			// not block subsequent policy lifecycle operations from Kubernetes
-			// until the update is complete.
-			oldCtrlName := oldRuleCpy.GetControllerName()
-			newCtrlName := newRuleCpy.GetControllerName()
+				// Update annotations within a controller so the status of the update
+				// is trackable from the list of running controllers, and so we do
+				// not block subsequent policy lifecycle operations from Kubernetes
+				// until the update is complete.
+				oldCtrlName := oldRuleCpy.GetControllerName()
+				newCtrlName := newRuleCpy.GetControllerName()
 
-			// In case the controller name changes between copies of rules,
-			// remove old controller so we do not leak goroutines.
-			if oldCtrlName != newCtrlName {
-				err := k8sCM.RemoveController(oldCtrlName)
-				if err != nil {
-					log.Debugf("Unable to remove controller %s: %s", oldCtrlName, err)
+				// In case the controller name changes between copies of rules,
+				// remove old controller so we do not leak goroutines.
+				if oldCtrlName != newCtrlName {
+					err := k8sCM.RemoveController(oldCtrlName)
+					if err != nil {
+						log.Debugf("Unable to remove controller %s: %s", oldCtrlName, err)
+					}
 				}
+				d.updateCiliumNetworkPolicyV2AnnotationsOnly(ciliumNPClient, ciliumV2Store, newRuleCpy)
 			}
-			d.updateCiliumNetworkPolicyV2AnnotationsOnly(ciliumNPClient, ciliumV2Store, newRuleCpy)
+			return nil
 		}
-		return nil
 	}
 
 	return d.addCiliumNetworkPolicyV2(ciliumNPClient, ciliumV2Store, newRuleCpy)

operator/k8s_node.go
@@ -46,7 +46,9 @@ import (
 
 var (
 	// kvNodeGCInterval duration for which the nodes are GC in the KVStore.
-	kvNodeGCInterval time.Duration
+	kvNodeGCInterval              time.Duration
+	enableCNPNodeStatusGC         bool
+	ciliumCNPNodeStatusGCInterval time.Duration
 )
 
 func runNodeWatcher() error {
@@ -161,6 +163,9 @@ func runNodeWatcher() error {
 	}()
 
 	go func() {
+		if !enableCNPNodeStatusGC {
+			return
+		}
 		parallelRequests := 4
 		removeNodeFromCNP := make(chan func(), 50)
 		for i := 0; i < parallelRequests; i++ {
@@ -172,7 +177,7 @@ func runNodeWatcher() error {
 		}
 		controller.NewManager().UpdateController("cnp-node-gc",
 			controller.ControllerParams{
-				RunInterval: kvNodeGCInterval,
+				RunInterval: ciliumCNPNodeStatusGCInterval,
 				DoFunc: func(ctx context.Context) error {
 					lastRun := time.Now().Add(-kvNodeGCInterval)
 					k8sCapabilities := k8sversion.Capabilities()

operator/main.go
@@ -144,6 +144,9 @@ func init() {
 	flags.MarkHidden(option.DisableCiliumEndpointCRDName)
 	option.BindEnv(option.DisableCiliumEndpointCRDName)
 
+	flags.BoolVar(&enableCNPNodeStatusGC, "cnp-node-status-gc", true, "Enable CiliumNetworkPolicy Status garbage collection for nodes which have been removed from the cluster")
+	flags.DurationVar(&ciliumCNPNodeStatusGCInterval, "cnp-node-status-gc-interval", time.Minute*2, "GC interval for nodes which have been removed from the cluster in CiliumNetworkPolicy Status")
+
 	viper.BindPFlags(flags)
 
 	// Make sure that klog logging variables are initialized so that we can

pkg/option/config.go
@@ -588,6 +588,10 @@ const (
 	// IdentityAllocationModeCRD enables use of Kubernetes CRDs for
 	// identity allocation
 	IdentityAllocationModeCRD = "crd"
+
+	// DisableCNPStatusUpdates disables updating of CNP NodeStatus in the CNP
+	// CRD.
+	DisableCNPStatusUpdates = "disable-cnp-status-updates"
 )
 
 // Default string arguments
@@ -1170,6 +1174,10 @@ type DaemonConfig struct {
 	// IdentityAllocationMode specifies what mode to use for identity
 	// allocation
 	IdentityAllocationMode string
+
+	// DisableCNPStatusUpdates disables updating of CNP NodeStatus in the CNP
+	// CRD.
+	DisableCNPStatusUpdates bool
 }
 
 var (
@@ -1764,6 +1772,7 @@ func (c *DaemonConfig) Populate() {
 	c.EndpointQueueSize = sanitizeIntParam(EndpointQueueSize, defaults.EndpointQueueSize)
 	c.SelectiveRegeneration = viper.GetBool(SelectiveRegeneration)
 	c.SkipCRDCreation = viper.GetBool(SkipCRDCreation)
+	c.DisableCNPStatusUpdates = viper.GetBool(DisableCNPStatusUpdates)
 }
 
 func sanitizeIntParam(paramName string, paramDefault int) int {

commit 8a08998ad729785e44cf2ca954ce301645118ece

daemon: fix cilium-agent helper message for disable-cnp-status-updates
The helper message seems to print a string in front of it that should
not have been printed.

Fixes: 6dc451f0611b ("add capability to disable CNP NodeStatus updates")
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon_main.go
@@ -725,7 +725,7 @@ func init() {
 	flags.MarkHidden(option.PolicyTriggerInterval)
 	option.BindEnv(option.PolicyTriggerInterval)
 
-	flags.Bool(option.DisableCNPStatusUpdates, false, "Do not send CNP NodeStatus updates to the Kubernetes api-server (recommended to run with `cnp-node-status-gc=false` in cilium-operator)")
+	flags.Bool(option.DisableCNPStatusUpdates, false, `Do not send CNP NodeStatus updates to the Kubernetes api-server (recommended to run with "cnp-node-status-gc=false" in cilium-operator)`)
 	option.BindEnv(option.DisableCNPStatusUpdates)
 
 	viper.BindPFlags(flags)