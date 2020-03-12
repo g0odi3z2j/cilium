g0odi3z2j/cilium

commit 69babe82c8a84a1ccab4144ab108859d4ee5d995

operator: add ability to ready options from a config directory
Similar to what we have in cilium-agent we can now have the same in
cilium-operator where the options can be specified in a directory for
better UX when deploying cilium-operator in a cluster.

Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon_main.go
@@ -155,7 +155,11 @@ func init() {
 		return
 	}
 
-	cobra.OnInitialize(initConfig)
+	if viper.GetBool("version") {
+		fmt.Printf("Cilium %s\n", version.Version)
+		os.Exit(0)
+	}
+	cobra.OnInitialize(option.InitConfig("ciliumd"))
 
 	// Reset the help function to also exit, as we block elsewhere in interrupts
 	// and would not exit when called with -h.
@@ -746,57 +750,6 @@ func restoreExecPermissions(searchDir string, patterns ...string) error {
 		}
 	}
 	return err
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if viper.GetBool("version") {
-		fmt.Printf("Cilium %s\n", version.Version)
-		os.Exit(0)
-	}
-
-	if viper.GetString(option.CMDRef) != "" {
-		return
-	}
-
-	option.Config.ConfigFile = viper.GetString(option.ConfigFile) // enable ability to specify config file via flag
-	option.Config.ConfigDir = viper.GetString(option.ConfigDir)
-	viper.SetEnvPrefix("cilium")
-
-	if option.Config.ConfigDir != "" {
-		if _, err := os.Stat(option.Config.ConfigDir); os.IsNotExist(err) {
-			log.Fatalf("Non-existent configuration directory %s", option.Config.ConfigDir)
-		}
-
-		if m, err := option.ReadDirConfig(option.Config.ConfigDir); err != nil {
-			log.Fatalf("Unable to read configuration directory %s: %s", option.Config.ConfigDir, err)
-		} else {
-			// replace deprecated fields with new fields
-			option.ReplaceDeprecatedFields(m)
-			err := option.MergeConfig(m)
-			if err != nil {
-				log.Fatalf("Unable to merge configuration: %s", err)
-			}
-		}
-	}
-
-	if option.Config.ConfigFile != "" {
-		viper.SetConfigFile(option.Config.ConfigFile)
-	} else {
-		viper.SetConfigName("ciliumd") // name of config file (without extension)
-		viper.AddConfigPath("$HOME")   // adding home directory as first search path
-	}
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		log.WithField(logfields.Path, viper.ConfigFileUsed()).
-			Info("Using config from file")
-	} else if option.Config.ConfigFile != "" {
-		log.WithField(logfields.Path, option.Config.ConfigFile).
-			Fatal("Error reading config file")
-	} else {
-		log.WithField(logfields.Reason, err).Info("Skipped reading configuration file")
-	}
 }
 
 func initEnv(cmd *cobra.Command) {

operator/flags.go
@@ -70,6 +70,12 @@ func init() {
 	option.BindEnv(option.ClusterName)
 
 	// Operator-specific flags
+	flags.String(option.ConfigFile, "", `Configuration file (default "$HOME/ciliumd.yaml")`)
+	option.BindEnv(option.ConfigFile)
+
+	flags.String(option.ConfigDir, "", `Configuration directory that contains a file for each option`)
+	option.BindEnv(option.ConfigDir)
+
 	// Deprecated, remove in 1.9
 	flags.Bool(option.EnableCCNPNodeStatusGC, true, "Enable CiliumClusterwideNetworkPolicy Status garbage collection for nodes which have been removed from the cluster")
 	option.BindEnv(option.EnableCCNPNodeStatusGC)

operator/main.go
@@ -88,12 +88,10 @@ func main() {
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if viper.GetBool("version") {
-		fmt.Printf("Cilium %s\n", version.Version)
+		fmt.Printf("Cilium-Operator %s\n", version.Version)
 		os.Exit(0)
 	}
-
-	viper.SetEnvPrefix("cilium")
-	viper.SetConfigName("cilium-operator")
+	cobra.OnInitialize(option.InitConfig("cilium-operator"))
 }
 
 func kvstoreEnabled() bool {

pkg/option/config.go
@@ -2181,3 +2181,51 @@ func getHostDevice() string {
 	}
 	return hostDevice
 }
+
+// InitConfig reads in config file and ENV variables if set.
+func InitConfig(configName string) func() {
+	return func() {
+		if viper.GetString(CMDRef) != "" {
+			return
+		}
+
+		Config.ConfigFile = viper.GetString(ConfigFile) // enable ability to specify config file via flag
+		Config.ConfigDir = viper.GetString(ConfigDir)
+		viper.SetEnvPrefix("cilium")
+
+		if Config.ConfigDir != "" {
+			if _, err := os.Stat(Config.ConfigDir); os.IsNotExist(err) {
+				log.Fatalf("Non-existent configuration directory %s", Config.ConfigDir)
+			}
+
+			if m, err := ReadDirConfig(Config.ConfigDir); err != nil {
+				log.Fatalf("Unable to read configuration directory %s: %s", Config.ConfigDir, err)
+			} else {
+				// replace deprecated fields with new fields
+				ReplaceDeprecatedFields(m)
+				err := MergeConfig(m)
+				if err != nil {
+					log.Fatalf("Unable to merge configuration: %s", err)
+				}
+			}
+		}
+
+		if Config.ConfigFile != "" {
+			viper.SetConfigFile(Config.ConfigFile)
+		} else {
+			viper.SetConfigName(configName) // name of config file (without extension)
+			viper.AddConfigPath("$HOME")    // adding home directory as first search path
+		}
+
+		// If a config file is found, read it in.
+		if err := viper.ReadInConfig(); err == nil {
+			log.WithField(logfields.Path, viper.ConfigFileUsed()).
+				Info("Using config from file")
+		} else if Config.ConfigFile != "" {
+			log.WithField(logfields.Path, Config.ConfigFile).
+				Fatal("Error reading config file")
+		} else {
+			log.WithField(logfields.Reason, err).Info("Skipped reading configuration file")
+		}
+	}
+}

commit b7956202e88dade0ce6d8fff0ac8ae3d96f1c780

operator/cilium: only print version with --version
The programs should be executed when running with `--version` and should
only print the version and terminate immediately.

Fixes: 69babe82c8a8 ("operator: add ability to ready options from a config directory")
Signed-off-by: André Martins <andre@cilium.io>

daemon/daemon_main.go
@@ -160,11 +160,7 @@ func init() {
 		return
 	}
 
-	if viper.GetBool("version") {
-		fmt.Printf("Cilium %s\n", version.Version)
-		os.Exit(0)
-	}
-	cobra.OnInitialize(option.InitConfig("ciliumd"))
+	cobra.OnInitialize(option.InitConfig("Cilium", "ciliumd"))
 
 	// Reset the help function to also exit, as we block elsewhere in interrupts
 	// and would not exit when called with -h.

operator/flags.go
@@ -19,14 +19,17 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/spf13/viper"
-	"k8s.io/klog"
-
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/option"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"k8s.io/klog"
 )
 
 func init() {
+	cobra.OnInitialize(option.InitConfig("Cilium-Operator", "cilium-operator"))
+
 	flags := rootCmd.Flags()
 
 	flags.Int(option.AWSClientBurstDeprecated, defaults.IPAMAPIBurst, "")

operator/main.go
@@ -102,15 +102,6 @@ func main() {
 	}
 }
 
-// init reads in config file and ENV variables if set.
-func init() {
-	if viper.GetBool("version") {
-		fmt.Printf("Cilium-Operator %s\n", version.Version)
-		os.Exit(0)
-	}
-	cobra.OnInitialize(option.InitConfig("cilium-operator"))
-}
-
 func kvstoreEnabled() bool {
 	if option.Config.KVStore == "" {
 		return false

pkg/option/config.go
@@ -37,6 +37,7 @@ import (
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/version"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
@@ -2347,8 +2348,13 @@ func getHostDevice() string {
 }
 
 // InitConfig reads in config file and ENV variables if set.
-func InitConfig(configName string) func() {
+func InitConfig(programName, configName string) func() {
 	return func() {
+		if viper.GetBool("version") {
+			fmt.Printf("%s %s\n", programName, version.Version)
+			os.Exit(0)
+		}
+
 		if viper.GetString(CMDRef) != "" {
 			return
 		}