g0odi3z2j/cilium

commit 553203daba1897a3a227f7734cd1ef95103c7ae4

bpf: Split connection tracking for TCP and non-TCP
Split the connection tracking of TCP connections into a separate table
from non-TCP, to improve the isolation between the flows. This means
that if there is a bursty set of UDP traffic, it will not prevent new
TCP connections from establishing.

Related: #5048

Signed-off-by: Joe Stringer <joe@covalent.io>

bpf/bpf_lxc.c
@@ -50,29 +50,61 @@
 
 #define POLICY_ID ((LXC_ID << 16) | SECLABEL)
 
-struct bpf_elf_map __section_maps CT_MAP6 = {
 #ifdef HAVE_LRU_MAP_TYPE
-	.type		= BPF_MAP_TYPE_LRU_HASH,
+#define CT_MAP_TYPE BPF_MAP_TYPE_LRU_HASH
 #else
-	.type		= BPF_MAP_TYPE_HASH,
+#define CT_MAP_TYPE BPF_MAP_TYPE_HASH
 #endif
+
+struct bpf_elf_map __section_maps CT_MAP_TCP6 = {
+	.type		= CT_MAP_TYPE,
 	.size_key	= sizeof(struct ipv6_ct_tuple),
 	.size_value	= sizeof(struct ct_entry),
 	.pinning	= PIN_GLOBAL_NS,
 	.max_elem	= CT_MAP_SIZE,
 };
 
-struct bpf_elf_map __section_maps CT_MAP4 = {
-#ifdef HAVE_LRU_MAP_TYPE
-	.type		= BPF_MAP_TYPE_LRU_HASH,
-#else
-	.type		= BPF_MAP_TYPE_HASH,
-#endif
+struct bpf_elf_map __section_maps CT_MAP_ANY6 = {
+	.type		= CT_MAP_TYPE,
+	.size_key	= sizeof(struct ipv6_ct_tuple),
+	.size_value	= sizeof(struct ct_entry),
+	.pinning	= PIN_GLOBAL_NS,
+	.max_elem	= CT_MAP_SIZE,
+};
+
+struct bpf_elf_map __section_maps CT_MAP_TCP4 = {
+	.type		= CT_MAP_TYPE,
 	.size_key	= sizeof(struct ipv4_ct_tuple),
 	.size_value	= sizeof(struct ct_entry),
 	.pinning	= PIN_GLOBAL_NS,
 	.max_elem	= CT_MAP_SIZE,
 };
+
+struct bpf_elf_map __section_maps CT_MAP_ANY4 = {
+	.type		= CT_MAP_TYPE,
+	.size_key	= sizeof(struct ipv4_ct_tuple),
+	.size_value	= sizeof(struct ct_entry),
+	.pinning	= PIN_GLOBAL_NS,
+	.max_elem	= CT_MAP_SIZE,
+};
+
+static inline struct bpf_elf_map *
+get_ct_map6(struct ipv6_ct_tuple *tuple)
+{
+	if (tuple->nexthdr == IPPROTO_TCP) {
+		return &CT_MAP_TCP6;
+	}
+	return &CT_MAP_ANY6;
+}
+
+static inline struct bpf_elf_map *
+get_ct_map4(struct ipv4_ct_tuple *tuple)
+{
+	if (tuple->nexthdr == IPPROTO_TCP) {
+		return &CT_MAP_TCP4;
+	}
+	return &CT_MAP_ANY4;
+}
 
 static inline bool redirect_to_proxy(int verdict)
 {
@@ -132,7 +164,7 @@ static inline int ipv6_l3_from_lxc(struct __sk_buff *skb,
 	 * address.
 	 */
 	if ((svc = lb6_lookup_service(skb, &key)) != NULL) {
-		ret = lb6_local(&CT_MAP6, skb, l3_off, l4_off,
+		ret = lb6_local(get_ct_map6(tuple), skb, l3_off, l4_off,
 				&csum_off, &key, tuple, svc, &ct_state_new);
 		if (IS_ERR(ret))
 			return ret;
@@ -155,7 +187,7 @@ skip_service_lookup:
 	 * entry to allow reverse packets and return set cb[CB_POLICY] to
 	 * POLICY_SKIP if the packet is a reply packet to an existing
 	 * incoming connection. */
-	ret = ct_lookup6(&CT_MAP6, tuple, skb, l4_off, CT_EGRESS,
+	ret = ct_lookup6(get_ct_map6(tuple), tuple, skb, l4_off, CT_EGRESS,
 			 &ct_state, &monitor);
 	if (ret < 0) {
 		relax_verifier();
@@ -197,7 +229,7 @@ skip_service_lookup:
 		/* If the connection was previously known and packet is now
 		 * denied, remove the connection tracking entry */
 		if (ret == CT_ESTABLISHED)
-			ct_delete6(&CT_MAP6, tuple, skb);
+			ct_delete6(get_ct_map6(tuple), tuple, skb);
 
 		return verdict;
 	}
@@ -210,7 +242,7 @@ skip_service_lookup:
 		 * reverse NAT.
 		 */
 		ct_state_new.src_sec_id = SECLABEL;
-		ret = ct_create6(&CT_MAP6, tuple, skb, CT_EGRESS, &ct_state_new);
+		ret = ct_create6(get_ct_map6(tuple), tuple, skb, CT_EGRESS, &ct_state_new);
 		if (IS_ERR(ret))
 			return ret;
 		monitor = TRACE_PAYLOAD_LEN;
@@ -452,7 +484,7 @@ static inline int handle_ipv4_from_lxc(struct __sk_buff *skb, __u32 *dstID)
 	ct_state_new.orig_dport = key.dport;
 #ifdef ENABLE_IPV4
 	if ((svc = lb4_lookup_service(skb, &key)) != NULL) {
-		ret = lb4_local(&CT_MAP4, skb, l3_off, l4_off, &csum_off,
+		ret = lb4_local(get_ct_map4(&tuple), skb, l3_off, l4_off, &csum_off,
 				&key, &tuple, svc, &ct_state_new, ip4->saddr);
 		if (IS_ERR(ret))
 			return ret;
@@ -474,7 +506,7 @@ skip_service_lookup:
 	 * entry to allow reverse packets and return set cb[CB_POLICY] to
 	 * POLICY_SKIP if the packet is a reply packet to an existing
 	 * incoming connection. */
-	ret = ct_lookup4(&CT_MAP4, &tuple, skb, l4_off, CT_EGRESS,
+	ret = ct_lookup4(get_ct_map4(&tuple), &tuple, skb, l4_off, CT_EGRESS,
 			 &ct_state, &monitor);
 	if (ret < 0)
 		return ret;
@@ -507,7 +539,7 @@ skip_service_lookup:
 		/* If the connection was previously known and packet is now
 		 * denied, remove the connection tracking entry */
 		if (ret == CT_ESTABLISHED)
-			ct_delete4(&CT_MAP4, &tuple, skb);
+			ct_delete4(get_ct_map4(&tuple), &tuple, skb);
 
 		return verdict;
 	}
@@ -520,7 +552,8 @@ skip_service_lookup:
 		 * reverse NAT.
 		 */
 		ct_state_new.src_sec_id = SECLABEL;
-		ret = ct_create4(&CT_MAP4, &tuple, skb, CT_EGRESS, &ct_state_new);
+		ret = ct_create4(get_ct_map4(&tuple), &tuple, skb, CT_EGRESS,
+				 &ct_state_new);
 		if (IS_ERR(ret))
 			return ret;
 		break;
@@ -771,7 +804,7 @@ ipv6_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 		}
 	}
 
-	ret = ct_lookup6(&CT_MAP6, &tuple, skb, l4_off, CT_INGRESS,
+	ret = ct_lookup6(get_ct_map6(&tuple), &tuple, skb, l4_off, CT_INGRESS,
 			 &ct_state, &monitor);
 	if (ret < 0)
 		return ret;
@@ -797,7 +830,7 @@ ipv6_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 		/* If the connection was previously known and packet is now
 		 * denied, remove the connection tracking entry */
 		if (ret == CT_ESTABLISHED)
-			ct_delete6(&CT_MAP6, &tuple, skb);
+			ct_delete6(get_ct_map6(&tuple), &tuple, skb);
 
 		return DROP_POLICY;
 	}
@@ -808,7 +841,7 @@ ipv6_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 	if (ret == CT_NEW) {
 		ct_state_new.orig_dport = tuple.dport;
 		ct_state_new.src_sec_id = src_label;
-		ret = ct_create6(&CT_MAP6, &tuple, skb, CT_INGRESS, &ct_state_new);
+		ret = ct_create6(get_ct_map6(&tuple), &tuple, skb, CT_INGRESS, &ct_state_new);
 		if (IS_ERR(ret))
 			return ret;
 
@@ -896,7 +929,7 @@ ipv4_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 	csum_l4_offset_and_flags(tuple.nexthdr, &csum_off);
 	is_fragment = ipv4_is_fragment(ip4);
 
-	ret = ct_lookup4(&CT_MAP4, &tuple, skb, l4_off, CT_INGRESS, &ct_state,
+	ret = ct_lookup4(get_ct_map4(&tuple), &tuple, skb, l4_off, CT_INGRESS, &ct_state,
 			 &monitor);
 	if (ret < 0)
 		return ret;
@@ -931,7 +964,7 @@ ipv4_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 		/* If the connection was previously known and packet is now
 		 * denied, remove the connection tracking entry */
 		if (ret == CT_ESTABLISHED)
-			ct_delete4(&CT_MAP4, &tuple, skb);
+			ct_delete4(get_ct_map4(&tuple), &tuple, skb);
 
 		return DROP_POLICY;
 	}
@@ -942,7 +975,7 @@ ipv4_policy(struct __sk_buff *skb, int ifindex, __u32 src_label, int *forwarding
 	if (ret == CT_NEW) {
 		ct_state_new.orig_dport = tuple.dport;
 		ct_state_new.src_sec_id = src_label;
-		ret = ct_create4(&CT_MAP4, &tuple, skb, CT_INGRESS, &ct_state_new);
+		ret = ct_create4(get_ct_map4(&tuple), &tuple, skb, CT_INGRESS, &ct_state_new);
 		if (IS_ERR(ret))
 			return ret;
 

bpf/lxc_config.h
@@ -37,8 +37,10 @@
 #endif
 #define DROP_NOTIFY
 #define TRACE_NOTIFY
-#define CT_MAP6 cilium_ct6_111
-#define CT_MAP4 cilium_ct4_111
+#define CT_MAP_TCP6 cilium_ct_tcp6_111
+#define CT_MAP_ANY6 cilium_ct_any6_111
+#define CT_MAP_TCP4 cilium_ct_tcp4_111
+#define CT_MAP_ANY4 cilium_ct_any4_111
 #define CT_MAP_SIZE 4096
 #define CALLS_MAP cilium_calls_111
 #define LB_L3

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=034c8207c4d8bf10b01cecd6c7add6e16eedb149
+GO_BINDATA_SHA1SUM=4f3bc614b4c4456be0d038f88cc0d0846ec23be0
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

daemon/daemon.go
@@ -1351,9 +1351,7 @@ func (d *Daemon) checkStaleMap(path string, filename string, id string) {
 func (d *Daemon) checkStaleGlobalMap(path string, filename string) {
 	globalCTinUse := endpointmanager.HasGlobalCT()
 
-	if !globalCTinUse &&
-		(filename == ctmap.MapName6Global ||
-			filename == ctmap.MapName4Global) {
+	if !globalCTinUse && ctmap.NameIsGlobal(filename) {
 		d.removeStaleMap(path)
 	}
 }
@@ -1363,8 +1361,7 @@ func (d *Daemon) staleMapWalker(path string) error {
 
 	mapPrefix := []string{
 		policymap.MapName,
-		ctmap.MapName6,
-		ctmap.MapName4,
+		ctmap.MapNamePrefix,
 		endpoint.CallsMapName,
 	}
 

pkg/maps/ctmap/ctmap.go
@@ -45,48 +45,27 @@ var (
 		metrics.LabelDatapathFamily: "ipv4",
 	}
 
-	mapInfo = map[MapType]struct {
-		keySize    int
-		maxEntries int
-		parser     bpf.DumpParser
-		bpfDefine  string
-	}{
-		MapTypeIPv4Local: {
-			keySize:    int(unsafe.Sizeof(CtKey4{})),
-			maxEntries: MapNumEntriesLocal,
-			parser:     ct4DumpParser,
-			bpfDefine:  "CT_MAP4",
-		},
-		MapTypeIPv6Local: {
-			keySize:    int(unsafe.Sizeof(CtKey6{})),
-			maxEntries: MapNumEntriesLocal,
-			parser:     ct6DumpParser,
-			bpfDefine:  "CT_MAP6",
-		},
-		MapTypeIPv4Global: {
-			keySize:    int(unsafe.Sizeof(CtKey4{})),
-			maxEntries: MapNumEntriesGlobal,
-			parser:     ct4DumpParser,
-			bpfDefine:  "CT_MAP4",
-		},
-		MapTypeIPv6Global: {
-			keySize:    int(unsafe.Sizeof(CtKey6{})),
-			maxEntries: MapNumEntriesGlobal,
-			parser:     ct6DumpParser,
-			bpfDefine:  "CT_MAP6",
-		},
-	}
+	mapInfo = make(map[MapType]mapAttributes)
 )
 
 const (
 	// mapCount counts the maximum number of CT maps that one endpoint may
 	// access at once.
-	mapCount = 2
+	mapCount = 4
 
-	MapName6       = "cilium_ct6_"
-	MapName4       = "cilium_ct4_"
-	MapName6Global = MapName6 + "global"
-	MapName4Global = MapName4 + "global"
+	// Map names for TCP CT tables are retained from Cilium 1.0 naming
+	// scheme to minimize disruption of ongoing connections during upgrade.
+	MapNamePrefix     = "cilium_ct"
+	MapNameTCP6       = MapNamePrefix + "6_"
+	MapNameTCP4       = MapNamePrefix + "4_"
+	MapNameTCP6Global = MapNameTCP6 + "global"
+	MapNameTCP4Global = MapNameTCP4 + "global"
+
+	// Map names for "any" protocols indicate CT for non-TCP protocols.
+	MapNameAny6       = MapNamePrefix + "_any6_"
+	MapNameAny4       = MapNamePrefix + "_any4_"
+	MapNameAny6Global = MapNameAny6 + "global"
+	MapNameAny4Global = MapNameAny4 + "global"
 
 	MapNumEntriesLocal  = 64000
 	MapNumEntriesGlobal = 1000000
@@ -105,6 +84,41 @@ const (
 	metricsAlive   = "alive"
 	metricsDeleted = "deleted"
 )
+
+type mapAttributes struct {
+	keySize    int
+	maxEntries int
+	parser     bpf.DumpParser
+	bpfDefine  string
+}
+
+func setupMapInfo(mapType MapType, define string, keySize, maxEntries int, parser bpf.DumpParser) {
+	mapInfo[mapType] = mapAttributes{
+		bpfDefine:  define,
+		keySize:    keySize,
+		maxEntries: maxEntries,
+		parser:     parser,
+	}
+}
+
+func initMapInfo() {
+	mapType := MapTypeIPv4TCPLocal
+	for _, maxEntries := range []int{MapNumEntriesLocal, MapNumEntriesGlobal} {
+		// CT_MAP_ANY4, CT_MAP_ANY6, CT_MAP_TCP4, CT_MAP_TCP6
+		for _, proto := range []string{"TCP", "ANY"} {
+			setupMapInfo(MapType(mapType), fmt.Sprintf("CT_MAP_%s4", proto),
+				int(unsafe.Sizeof(CtKey4{})), maxEntries, ct4DumpParser)
+			mapType++
+			setupMapInfo(MapType(mapType), fmt.Sprintf("CT_MAP_%s6", proto),
+				int(unsafe.Sizeof(CtKey6{})), maxEntries, ct6DumpParser)
+			mapType++
+		}
+	}
+}
+
+func init() {
+	initMapInfo()
+}
 
 // CtEndpoint represents an endpoint for the functions required to manage
 // conntrack maps for the endpoint.
@@ -380,17 +394,21 @@ func maps(e CtEndpoint, ipv4, ipv6 bool) []*Map {
 	result := make([]*Map, 0, mapCount)
 	if e == nil {
 		if ipv4 {
-			result = append(result, NewMap(MapName4Global, MapTypeIPv4Global))
+			result = append(result, NewMap(MapNameTCP4Global, MapTypeIPv4TCPGlobal))
+			result = append(result, NewMap(MapNameAny4Global, MapTypeIPv4AnyGlobal))
 		}
 		if ipv6 {
-			result = append(result, NewMap(MapName6Global, MapTypeIPv6Global))
+			result = append(result, NewMap(MapNameTCP6Global, MapTypeIPv6TCPGlobal))
+			result = append(result, NewMap(MapNameAny6Global, MapTypeIPv6AnyGlobal))
 		}
 	} else {
 		if ipv4 {
-			result = append(result, NewMap(MapName4+e.StringID(), MapTypeIPv4Local))
+			result = append(result, NewMap(MapNameTCP4+e.StringID(), MapTypeIPv4TCPLocal))
+			result = append(result, NewMap(MapNameAny4+e.StringID(), MapTypeIPv4AnyLocal))
 		}
 		if ipv6 {
-			result = append(result, NewMap(MapName6+e.StringID(), MapTypeIPv6Local))
+			result = append(result, NewMap(MapNameTCP6+e.StringID(), MapTypeIPv6TCPLocal))
+			result = append(result, NewMap(MapNameAny6+e.StringID(), MapTypeIPv6AnyLocal))
 		}
 	}
 	return result
@@ -412,6 +430,16 @@ func LocalMaps(e CtEndpoint, ipv4, ipv6 bool) []*Map {
 // The returned maps are not yet opened.
 func GlobalMaps(ipv4, ipv6 bool) []*Map {
 	return maps(nil, ipv4, ipv6)
+}
+
+// NameIsGlobal returns true if the specified filename (basename) denotes a
+// global conntrack map.
+func NameIsGlobal(filename string) bool {
+	switch filename {
+	case MapNameTCP4Global, MapNameAny4Global, MapNameTCP6Global, MapNameAny6Global:
+		return true
+	}
+	return false
 }
 
 // WriteBPFMacros writes the map names for conntrack maps into the specified

pkg/maps/ctmap/ctmap_test.go
@@ -42,6 +42,11 @@ func (t *CTMapTestSuite) TestInit(c *C) {
 			c.Assert(info.keySize, Equals, int(unsafe.Sizeof(CtKey4{})))
 			c.Assert(strings.Contains(info.bpfDefine, "4"), Equals, true)
 		}
+		if mapType.isTCP() {
+			c.Assert(strings.Contains(info.bpfDefine, "TCP"), Equals, true)
+		} else {
+			c.Assert(strings.Contains(info.bpfDefine, "ANY"), Equals, true)
+		}
 		if mapType.isLocal() {
 			c.Assert(info.maxEntries, Equals, MapNumEntriesLocal)
 		}

pkg/maps/ctmap/metrics.go
@@ -65,12 +65,15 @@ type gcProtocol int
 
 const (
 	gcProtocolAny = iota
+	gcProtocolTCP
 )
 
 func (g gcProtocol) String() string {
 	switch g {
 	case gcProtocolAny:
-		return "any"
+		return "non-TCP"
+	case gcProtocolTCP:
+		return "TCP"
 	default:
 		return fmt.Sprintf("unknown (%d)", int(g))
 	}
@@ -85,7 +88,11 @@ func statStartGc(m *Map) gcStats {
 	} else {
 		result.family = gcFamilyIPv4
 	}
-	result.proto = gcProtocolAny
+	if m.mapType.isTCP() {
+		result.proto = gcProtocolTCP
+	} else {
+		result.proto = gcProtocolAny
+	}
 	return result
 }
 

commit 55ead3729df082fed2395b4c2a1349b47801f46f

bpf: significantly improve capacity of TCP CT tables
ct_create{4,6}() inserts related entries into the TCP CT tables given
the map is usually in the form of ct_create4(get_ct_map4(&tuple)) or
ct_create6(get_ct_map6(&tuple)). Similarly, the lookup parts are in
form of ct_lookup4(get_ct_map4(&tuple)) or ct_lookup6(get_ct_map6(&tuple)).

However, the tuples' nexthdr usually points to the one in the packet.
This means, we can /never/ find a related entry since it sits in the TCP
CT tables, but their lookup is always in the ANY table instead.

Fix the insertions by adding to the CT_MAP_ANY{4,6} tables and by that
implicityly double the capacity of TCP CT tables.

Go even beyond that by not creating related entries for CT_SERVICE entries.

It does not make sense to create CT_SERVICE entries with related flag
since we don't translate ICMP there anyway. Save overhead and don't add
them to the maps (same for NodePort/NAT related ones).

Fixes: 553203daba18 ("bpf: Split connection tracking for TCP and non-TCP")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/bpf_lxc.c
@@ -188,7 +188,8 @@ ct_recreate6:
 		 * reverse NAT.
 		 */
 		ct_state_new.src_sec_id = SECLABEL;
-		ret = ct_create6(get_ct_map6(tuple), tuple, ctx, CT_EGRESS, &ct_state_new, verdict > 0);
+		ret = ct_create6(get_ct_map6(tuple), &CT_MAP_ANY6, tuple, ctx,
+				 CT_EGRESS, &ct_state_new, verdict > 0);
 		if (IS_ERR(ret))
 			return ret;
 		monitor = TRACE_PAYLOAD_LEN;
@@ -537,8 +538,11 @@ ct_recreate4:
 		 * reverse NAT.
 		 */
 		ct_state_new.src_sec_id = SECLABEL;
-		ret = ct_create4(get_ct_map4(&tuple), &tuple, ctx, CT_EGRESS,
-				 &ct_state_new, verdict > 0);
+		/* We could avoid creating related entries for legacy ClusterIP
+		 * handling here, but turns out that verifier cannot handle it.
+		 */
+		ret = ct_create4(get_ct_map4(&tuple), &CT_MAP_ANY4, &tuple, ctx,
+				 CT_EGRESS, &ct_state_new, verdict > 0);
 		if (IS_ERR(ret))
 			return ret;
 		break;
@@ -884,7 +888,8 @@ ipv6_policy(struct __ctx_buff *ctx, int ifindex, __u32 src_label, __u8 *reason,
 
 		ct_state_new.src_sec_id = src_label;
 		ct_state_new.node_port = ct_state.node_port;
-		ret = ct_create6(get_ct_map6(&tuple), &tuple, ctx, CT_INGRESS, &ct_state_new, verdict > 0);
+		ret = ct_create6(get_ct_map6(&tuple), &CT_MAP_ANY6, &tuple, ctx, CT_INGRESS,
+				 &ct_state_new, verdict > 0);
 		if (IS_ERR(ret))
 			return ret;
 
@@ -1091,7 +1096,8 @@ ipv4_policy(struct __ctx_buff *ctx, int ifindex, __u32 src_label, __u8 *reason,
 
 		ct_state_new.src_sec_id = src_label;
 		ct_state_new.node_port = ct_state.node_port;
-		ret = ct_create4(get_ct_map4(&tuple), &tuple, ctx, CT_INGRESS, &ct_state_new, verdict > 0);
+		ret = ct_create4(get_ct_map4(&tuple), &CT_MAP_ANY4, &tuple, ctx, CT_INGRESS,
+				 &ct_state_new, verdict > 0);
 		if (IS_ERR(ret))
 			return ret;
 

bpf/include/bpf/helpers.h
@@ -31,10 +31,10 @@
 #endif
 
 /* Map access/manipulation */
-static void *BPF_FUNC(map_lookup_elem, void *map, const void *key);
-static int BPF_FUNC(map_update_elem, void *map, const void *key,
+static void *BPF_FUNC(map_lookup_elem, const void *map, const void *key);
+static int BPF_FUNC(map_update_elem, const void *map, const void *key,
 		    const void *value, __u32 flags);
-static int BPF_FUNC(map_delete_elem, void *map, const void *key);
+static int BPF_FUNC(map_delete_elem, const void *map, const void *key);
 
 /* Time access */
 static __u64 BPF_FUNC(ktime_get_ns);

bpf/lib/conntrack.h
@@ -631,8 +631,9 @@ ct_update6_rev_nat_index(void *map, struct ipv6_ct_tuple *tuple,
 }
 
 /* Offset must point to IPv6 */
-static __always_inline int ct_create6(void *map, struct ipv6_ct_tuple *tuple,
-				      struct __ctx_buff *ctx, int dir,
+static __always_inline int ct_create6(const void *map_main, const void *map_related,
+				      struct ipv6_ct_tuple *tuple,
+				      struct __ctx_buff *ctx, const int dir,
 				      struct ct_state *ct_state,
 				      bool proxy_redirect)
 {
@@ -669,30 +670,26 @@ static __always_inline int ct_create6(void *map, struct ipv6_ct_tuple *tuple,
 	cilium_dbg3(ctx, DBG_CT_CREATED6, entry.rev_nat_index, ct_state->src_sec_id, 0);
 
 	entry.src_sec_id = ct_state->src_sec_id;
-	if (map_update_elem(map, tuple, &entry, 0) < 0)
+	if (map_update_elem(map_main, tuple, &entry, 0) < 0)
 		return DROP_CT_CREATE_FAILED;
 
-	/* Create an ICMPv6 entry to relate errors */
-	struct ipv6_ct_tuple icmp_tuple = {
-		.nexthdr = IPPROTO_ICMPV6,
-		.sport = 0,
-		.dport = 0,
-		.flags = tuple->flags | TUPLE_F_RELATED,
-	};
+	if (map_related != NULL) {
+		/* Create an ICMPv6 entry to relate errors */
+		struct ipv6_ct_tuple icmp_tuple = {
+			.nexthdr = IPPROTO_ICMPV6,
+			.sport = 0,
+			.dport = 0,
+			.flags = tuple->flags | TUPLE_F_RELATED,
+		};
 
-	entry.seen_non_syn = true; /* For ICMP, there is no SYN. */
+		entry.seen_non_syn = true; /* For ICMP, there is no SYN. */
 
-	ipv6_addr_copy(&icmp_tuple.daddr, &tuple->daddr);
-	ipv6_addr_copy(&icmp_tuple.saddr, &tuple->saddr);
+		ipv6_addr_copy(&icmp_tuple.daddr, &tuple->daddr);
+		ipv6_addr_copy(&icmp_tuple.saddr, &tuple->saddr);
 
-	/* FIXME: We could do a lookup and check if an L3 entry already exists */
-	if (map_update_elem(map, &icmp_tuple, &entry, 0) < 0) {
-		/* Previous map update succeeded, we could delete it
-		 * but we might as well just let it time out.
-		 */
-		return DROP_CT_CREATE_FAILED;
+		if (map_update_elem(map_related, &icmp_tuple, &entry, 0) < 0)
+			return DROP_CT_CREATE_FAILED;
 	}
-
 	return 0;
 }
 
@@ -726,8 +723,9 @@ ct_update4_rev_nat_index(void *map, struct ipv4_ct_tuple *tuple,
 	return;
 }
 
-static __always_inline int ct_create4(void *map, struct ipv4_ct_tuple *tuple,
-				      struct __ctx_buff *ctx, int dir,
+static __always_inline int ct_create4(const void *map_main, const void *map_related,
+				      struct ipv4_ct_tuple *tuple,
+				      struct __ctx_buff *ctx, const int dir,
 				      struct ct_state *ct_state,
 				      bool proxy_redirect)
 {
@@ -777,7 +775,7 @@ static __always_inline int ct_create4(void *map, struct ipv4_ct_tuple *tuple,
 	cilium_dbg3(ctx, DBG_CT_CREATED4, entry.rev_nat_index, ct_state->src_sec_id, ct_state->addr);
 
 	entry.src_sec_id = ct_state->src_sec_id;
-	if (map_update_elem(map, tuple, &entry, 0) < 0)
+	if (map_update_elem(map_main, tuple, &entry, 0) < 0)
 		return DROP_CT_CREATE_FAILED;
 
 	if (ct_state->addr && ct_state->loopback) {
@@ -800,29 +798,32 @@ static __always_inline int ct_create4(void *map, struct ipv4_ct_tuple *tuple,
 			tuple->daddr = ct_state->addr;
 		}
 
-		if (map_update_elem(map, tuple, &entry, 0) < 0)
+		if (map_update_elem(map_main, tuple, &entry, 0) < 0)
 			return DROP_CT_CREATE_FAILED;
 		tuple->saddr = saddr;
 		tuple->daddr = daddr;
 		tuple->flags = flags;
 	}
 
-	/* Create an ICMP entry to relate errors */
-	struct ipv4_ct_tuple icmp_tuple = {
-		.daddr = tuple->daddr,
-		.saddr = tuple->saddr,
-		.nexthdr = IPPROTO_ICMP,
-		.sport = 0,
-		.dport = 0,
-		.flags = tuple->flags | TUPLE_F_RELATED,
-	};
+	if (map_related != NULL) {
+		/* Create an ICMP entry to relate errors */
+		struct ipv4_ct_tuple icmp_tuple = {
+			.daddr = tuple->daddr,
+			.saddr = tuple->saddr,
+			.nexthdr = IPPROTO_ICMP,
+			.sport = 0,
+			.dport = 0,
+			.flags = tuple->flags | TUPLE_F_RELATED,
+		};
 
-	entry.seen_non_syn = true; /* For ICMP, there is no SYN. */
-
-	/* FIXME: We could do a lookup and check if an L3 entry already exists */
-	if (map_update_elem(map, &icmp_tuple, &entry, 0) < 0)
-		return DROP_CT_CREATE_FAILED;
-
+		entry.seen_non_syn = true; /* For ICMP, there is no SYN. */
+		/* Previous map update succeeded, we could delete it in case
+		 * the below throws an error, but we might as well just let
+		 * it time out.
+		 */
+		if (map_update_elem(map_related, &icmp_tuple, &entry, 0) < 0)
+			return DROP_CT_CREATE_FAILED;
+	}
 	return 0;
 }
 #else /* !CONNTRACK */
@@ -852,8 +853,9 @@ ct_update6_rev_nat_index(void *map, struct ipv6_ct_tuple *tuple,
 {
 }
 
-static __always_inline int ct_create6(void *map, struct ipv6_ct_tuple *tuple,
-				      struct __ctx_buff *ctx, int dir,
+static __always_inline int ct_create6(const void *map_main, const void *map_related,
+				      struct ipv6_ct_tuple *tuple,
+				      struct __ctx_buff *ctx, const int dir,
 				      struct ct_state *ct_state,
 				      bool from_proxy)
 {
@@ -872,8 +874,9 @@ ct_update4_rev_nat_index(void *map, struct ipv4_ct_tuple *tuple,
 {
 }
 
-static __always_inline int ct_create4(void *map, struct ipv4_ct_tuple *tuple,
-				      struct __ctx_buff *ctx, int dir,
+static __always_inline int ct_create4(const void *map_main, const void *map_related,
+				      struct ipv4_ct_tuple *tuple,
+				      struct __ctx_buff *ctx, const int dir,
 				      struct ct_state *ct_state, bool from_proxy)
 {
 	return 0;

bpf/lib/lb.h
@@ -459,7 +459,7 @@ static __always_inline int lb6_local(void *map, struct __ctx_buff *ctx,
 		}
 		state->backend_id = slave_svc->backend_id;
 		state->rev_nat_index = svc->rev_nat_index;
-		ret = ct_create6(map, tuple, ctx, CT_SERVICE, state, false);
+		ret = ct_create6(map, NULL, tuple, ctx, CT_SERVICE, state, false);
 		/* Fail closed, if the conntrack entry create fails drop
 		 * service lookup.
 		 */
@@ -830,7 +830,7 @@ static __always_inline int lb4_local(void *map, struct __ctx_buff *ctx,
 		}
 		state->backend_id = slave_svc->backend_id;
 		state->rev_nat_index = svc->rev_nat_index;
-		ret = ct_create4(map, tuple, ctx, CT_SERVICE, state, false);
+		ret = ct_create4(map, NULL, tuple, ctx, CT_SERVICE, state, false);
 		/* Fail closed, if the conntrack entry create fails drop
 		 * service lookup.
 		 */

bpf/lib/nat.h
@@ -280,8 +280,8 @@ static __always_inline int snat_v4_track_local(struct __ctx_buff *ctx,
 	if (ret < 0) {
 		return ret;
 	} else if (ret == CT_NEW) {
-		ret = ct_create4(get_ct_map4(&tmp), &tmp, ctx, where,
-				 &ct_state, false);
+		ret = ct_create4(get_ct_map4(&tmp), NULL, &tmp, ctx,
+				 where, &ct_state, false);
 		if (IS_ERR(ret))
 			return ret;
 	}
@@ -748,7 +748,7 @@ static __always_inline int snat_v6_track_local(struct __ctx_buff *ctx,
 	if (ret < 0) {
 		return ret;
 	} else if (ret == CT_NEW) {
-		ret = ct_create6(get_ct_map6(&tmp), &tmp, ctx, where,
+		ret = ct_create6(get_ct_map6(&tmp), NULL, &tmp, ctx, where,
 				 &ct_state, false);
 		if (IS_ERR(ret))
 			return ret;

bpf/lib/nodeport.h
@@ -508,15 +508,15 @@ static __always_inline int nodeport_lb6(struct __ctx_buff *ctx,
 	case CT_NEW:
 		ct_state_new.src_sec_id = SECLABEL;
 		ct_state_new.node_port = 1;
-		ret = ct_create6(get_ct_map6(&tuple), &tuple, ctx, CT_EGRESS,
-				 &ct_state_new, false);
+		ret = ct_create6(get_ct_map6(&tuple), NULL, &tuple, ctx,
+				 CT_EGRESS, &ct_state_new, false);
 		if (IS_ERR(ret))
 			return ret;
 		if (backend_local) {
 			ct_flip_tuple_dir6(&tuple);
 redo:
 			ct_state_new.rev_nat_index = 0;
-			ret = ct_create6(get_ct_map6(&tuple), &tuple, ctx,
+			ret = ct_create6(get_ct_map6(&tuple), NULL, &tuple, ctx,
 					 CT_INGRESS, &ct_state_new, false);
 			if (IS_ERR(ret))
 				return ret;
@@ -1066,15 +1066,15 @@ static __always_inline int nodeport_lb4(struct __ctx_buff *ctx,
 	case CT_NEW:
 		ct_state_new.src_sec_id = SECLABEL;
 		ct_state_new.node_port = 1;
-		ret = ct_create4(get_ct_map4(&tuple), &tuple, ctx, CT_EGRESS,
-				 &ct_state_new, false);
+		ret = ct_create4(get_ct_map4(&tuple), NULL, &tuple, ctx,
+				 CT_EGRESS, &ct_state_new, false);
 		if (IS_ERR(ret))
 			return ret;
 		if (backend_local) {
 			ct_flip_tuple_dir4(&tuple);
 redo:
 			ct_state_new.rev_nat_index = 0;
-			ret = ct_create4(get_ct_map4(&tuple), &tuple, ctx,
+			ret = ct_create4(get_ct_map4(&tuple), NULL, &tuple, ctx,
 					 CT_INGRESS, &ct_state_new, false);
 			if (IS_ERR(ret))
 				return ret;