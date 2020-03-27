g0odi3z2j/cilium

commit 9eb7c82c9043afb5702c747f2c0f52cb1df5d73d

maps/neighborsmap, daemon: Create map from daemon
Create neighbors maps used to store IP-MAC associations when NodePort
services are implemented in BPF datapath.

To generate the deepcopy file for the new maps, I used:

    make generate-k8s-api

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/maps/neighborsmap/neighborsmap.go
@@ -1,0 +1,115 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package neighborsmap
+
+import (
+	"fmt"
+	"unsafe"
+
+	"github.com/cilium/cilium/common/types"
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/option"
+)
+
+const (
+	// Map4Name is the BPF map name.
+	Map4Name = "cilium_nodeport_neigh4"
+	// Map6Name is the BPF map name.
+	Map6Name = "cilium_nodeport_neigh6"
+)
+
+// Key4 is the IPv4 for the IP-to-MAC address mappings.
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type Key4 struct {
+	ipv4 types.IPv4
+}
+
+// Key6 is the IPv6 for the IP-to-MAC address mappings.
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type Key6 struct {
+	ipv6 types.IPv6
+}
+
+// Value is the MAC address for the IP-to-MAC address mappings.
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapValue
+type Value struct {
+	macaddr types.MACAddr
+}
+
+// GetKeyPtr returns the unsafe pointer to the BPF key
+func (k *Key4) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+
+// GetKeyPtr returns the unsafe pointer to the BPF key
+func (k *Key6) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+
+// GetValuePtr returns the unsafe pointer to the BPF value
+func (v *Value) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(v) }
+
+// String converts the key into a human readable string format.
+func (k *Key4) String() string { return fmt.Sprintf("%s", k.ipv4) }
+
+// String converts the key into a human readable string format.
+func (k *Key6) String() string { return fmt.Sprintf("%s", k.ipv6) }
+
+// String converts the value into a human readable string format.
+func (v *Value) String() string { return fmt.Sprintf("%s", v.macaddr) }
+
+// NewValue returns a new empty instance of the structure representing the BPF
+// map value.
+func (k Key4) NewValue() bpf.MapValue { return &Value{} }
+
+// NewValue returns a new empty instance of the structure representing the BPF
+// map value.
+func (k Key6) NewValue() bpf.MapValue { return &Value{} }
+
+// InitMaps creates the nodeport neighbors maps in the kernel.
+func InitMaps(ipv4, ipv6 bool) error {
+	if ipv4 {
+		neighbors4Map := bpf.NewMap(Map4Name,
+			bpf.MapTypeLRUHash,
+			&Key4{},
+			int(unsafe.Sizeof(Key4{})),
+			&Value{},
+			int(unsafe.Sizeof(Value{})),
+			option.Config.NATMapEntriesGlobal,
+			0,
+			0,
+			bpf.ConvertKeyValue,
+		)
+		if _, err := neighbors4Map.Create(); err != nil {
+			return err
+		}
+	}
+	if ipv6 {
+		neighbors6Map := bpf.NewMap(Map6Name,
+			bpf.MapTypeLRUHash,
+			&Key6{},
+			int(unsafe.Sizeof(Key6{})),
+			&Value{},
+			int(unsafe.Sizeof(Value{})),
+			option.Config.NATMapEntriesGlobal,
+			0,
+			0,
+			bpf.ConvertKeyValue,
+		)
+		if _, err := neighbors6Map.Create(); err != nil {
+			return err
+		}
+	}
+	return nil
+}

Makefile
@@ -334,6 +334,7 @@ generate-k8s-api:
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium/pkg,"maps:ctmap")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium/pkg,"maps:eventsmap")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium/pkg,"maps:signalmap")
+	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium/pkg,"maps:neighborsmap")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium,"pkg:tuple")
 	$(call generate_k8s_api_deepcopy,github.com/cilium/cilium,"pkg:bpf")
 

common/types/macaddr.go
@@ -1,0 +1,36 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	"net"
+)
+
+// MACAddr is the binary representation for encoding in binary structs.
+type MACAddr [6]byte
+
+func (addr MACAddr) hardwareAddr() net.HardwareAddr {
+	return addr[:]
+}
+
+func (addr MACAddr) String() string {
+	return addr.hardwareAddr().String()
+}
+
+// DeepCopyInto is a deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (addr *MACAddr) DeepCopyInto(out *MACAddr) {
+	copy(out[:], addr[:])
+	return
+}

common/types/macaddr_test.go
@@ -1,0 +1,46 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package types
+
+import (
+	"net"
+
+	"github.com/cilium/cilium/pkg/checker"
+
+	"gopkg.in/check.v1"
+)
+
+var testMACAddress MACAddr = [6]byte{1, 2, 3, 4, 5, 6}
+
+type MACAddrSuite struct{}
+
+var _ = check.Suite(&MACAddrSuite{})
+
+func (s *MACAddrSuite) TestHardwareAddr(c *check.C) {
+	var expectedAddress net.HardwareAddr
+	expectedAddress = []byte{1, 2, 3, 4, 5, 6}
+	result := testMACAddress.hardwareAddr()
+
+	c.Assert(result, checker.DeepEquals, expectedAddress)
+}
+
+func (s *MACAddrSuite) TestString(c *check.C) {
+	expectedStr := "01:02:03:04:05:06"
+	result := testMACAddress.String()
+
+	c.Assert(result, check.Equals, expectedStr)
+}

daemon/cmd/datapath.go
@@ -38,6 +38,7 @@ import (
 	"github.com/cilium/cilium/pkg/maps/lxcmap"
 	"github.com/cilium/cilium/pkg/maps/metricsmap"
 	"github.com/cilium/cilium/pkg/maps/nat"
+	"github.com/cilium/cilium/pkg/maps/neighborsmap"
 	"github.com/cilium/cilium/pkg/maps/policymap"
 	"github.com/cilium/cilium/pkg/maps/signalmap"
 	"github.com/cilium/cilium/pkg/maps/tunnel"
@@ -390,6 +391,13 @@ func (d *Daemon) initMaps() error {
 	}
 	if option.Config.EnableIPv6 {
 		if _, err := ipv6Nat.Create(); err != nil {
+			return err
+		}
+	}
+
+	if option.Config.EnableNodePort {
+		if err := neighborsmap.InitMaps(option.Config.EnableIPv4,
+			option.Config.EnableIPv6); err != nil {
 			return err
 		}
 	}

pkg/datapath/linux/config/config.go
@@ -39,6 +39,7 @@ import (
 	"github.com/cilium/cilium/pkg/maps/lxcmap"
 	"github.com/cilium/cilium/pkg/maps/metricsmap"
 	"github.com/cilium/cilium/pkg/maps/nat"
+	"github.com/cilium/cilium/pkg/maps/neighborsmap"
 	"github.com/cilium/cilium/pkg/maps/policymap"
 	"github.com/cilium/cilium/pkg/maps/signalmap"
 	"github.com/cilium/cilium/pkg/maps/sockmap"
@@ -207,10 +208,10 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		cDefinesMap["ENABLE_NODEPORT"] = "1"
 
 		if option.Config.EnableIPv4 {
-			cDefinesMap["NODEPORT_NEIGH4"] = "cilium_nodeport_neigh4"
+			cDefinesMap["NODEPORT_NEIGH4"] = neighborsmap.Map4Name
 		}
 		if option.Config.EnableIPv6 {
-			cDefinesMap["NODEPORT_NEIGH6"] = "cilium_nodeport_neigh6"
+			cDefinesMap["NODEPORT_NEIGH6"] = neighborsmap.Map6Name
 		}
 		if option.Config.NodePortMode == option.NodePortModeDSR ||
 			option.Config.NodePortMode == option.NodePortModeHybrid {

pkg/maps/neighborsmap/doc.go
@@ -1,0 +1,19 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package neighborsmap represents the map that stores IP to mac address
+// mappings for NodePort clients. It is primarily managed from the
+// datapath; Cilium side is used to create the map only.
+// +groupName=maps
+package neighborsmap

pkg/maps/neighborsmap/zz_generated.deepcopy.go
@@ -1,0 +1,98 @@
+// +build !ignore_autogenerated
+
+// Copyright 2017-2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Code generated by deepcopy-gen. DO NOT EDIT.
+
+package neighborsmap
+
+import (
+	bpf "github.com/cilium/cilium/pkg/bpf"
+)
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Key4) DeepCopyInto(out *Key4) {
+	*out = *in
+	in.ipv4.DeepCopyInto(&out.ipv4)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Key4.
+func (in *Key4) DeepCopy() *Key4 {
+	if in == nil {
+		return nil
+	}
+	out := new(Key4)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *Key4) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Key6) DeepCopyInto(out *Key6) {
+	*out = *in
+	in.ipv6.DeepCopyInto(&out.ipv6)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Key6.
+func (in *Key6) DeepCopy() *Key6 {
+	if in == nil {
+		return nil
+	}
+	out := new(Key6)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *Key6) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Value) DeepCopyInto(out *Value) {
+	*out = *in
+	in.macaddr.DeepCopyInto(&out.macaddr)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Value.
+func (in *Value) DeepCopy() *Value {
+	if in == nil {
+		return nil
+	}
+	out := new(Value)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapValue is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapValue.
+func (in *Value) DeepCopyMapValue() bpf.MapValue {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}

commit 3031fd7aee023c5720ea8345343dafba532356c5

pkg/neighborsmap: Missing padding for map values
Commit 9eb7c82 introduces a Go definition of the NodePort neighbor maps,
to enable their creation during the daemon's initialization. The value
structure is however missing some padding to match its C counterpart,
resulting in the following warnings:

    2020-03-25T23:07:55.245972761Z level=warning msg="Value-size mismatch for BPF map" file-path=/sys/fs/bpf/tc/globals/cilium_nodeport_neigh4 new=6 old=8 subsys=bpf
    2020-03-25T23:07:55.245975572Z level=warning msg="Removing map to allow for property upgrade (expect map data loss)" file-path=/sys/fs/bpf/tc/globals/cilium_nodeport_neigh4 subsys=bpf
    2020-03-25T23:07:55.256431723Z level=debug msg="Registered BPF map" path=/sys/fs/bpf/tc/globals/cilium_nodeport_neigh4 subsys=bpf
    2020-03-25T23:07:55.256461108Z level=debug msg="Unregistered BPF map" path=/sys/fs/bpf/tc/globals/cilium_nodeport_neigh4 subsys=bpf

Fixes: 9eb7c82 ("maps/neighborsmap, daemon: Create map from daemon")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/maps/neighborsmap/neighborsmap.go
@@ -49,6 +49,7 @@ type Key6 struct {
 // +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapValue
 type Value struct {
 	macaddr types.MACAddr
+	pad     uint16
 }
 
 // GetKeyPtr returns the unsafe pointer to the BPF key