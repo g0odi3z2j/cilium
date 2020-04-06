g0odi3z2j/cilium

commit 906d4b459a56ef8d84d27d6a5c1819a2839472a3

monitor: Extend trace notification version
Extend pkg/monitor to provide a generic DecodeTraceNotify() function
which receives a TraceNotify type (which supports both v0 and v1), and
reads into it the data from the monitor regardless of whether the
incoming messages are v0 or v1.

The version 0 is reused for existing messages to avoid breaking the
format for those messages; version 1 includes the original source
address.

Note that this may cause unnecessary dissection when using an older
monitor client with a newer version of Cilium, but in practice the
library that Cilium provides handled this in a reasonable manner in past
versions, and also it's more likely for users to run newer clients with
older Cilium, so this problem is largely ignored for this commit.

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/monitor/datapath_trace.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018 Authors of Cilium
+// Copyright 2016-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,17 +15,25 @@
 package monitor
 
 import (
+	"bytes"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"net"
+	"unsafe"
 
 	"github.com/cilium/cilium/common/types"
+	"github.com/cilium/cilium/pkg/byteorder"
 	"github.com/cilium/cilium/pkg/monitor/api"
 )
 
 const (
-	// TraceNotifyLen is the amount of packet data provided in a trace notification
-	TraceNotifyLen = 48
+	// traceNotifyCommonLen is the minimum length required to determine the version of the TN event.
+	traceNotifyCommonLen = 16
+	// traceNotifyV1Len is the amount of packet data provided in a trace notification v1
+	traceNotifyV1Len = 32
+	// traceNotifyV2Len is the amount of packet data provided in a trace notification v2
+	traceNotifyV2Len = 48
 	// TraceReasonEncryptMask is the bit used to indicate encryption or not
 	TraceReasonEncryptMask uint8 = 0x80
 )
@@ -36,23 +44,45 @@ const (
 	TraceNotifyFlagIsIPv6 uint8 = 1
 )
 
-// TraceNotify is the message format of a trace notification in the BPF ring buffer
-type TraceNotify struct {
+const (
+	TraceNotifyVersion0 = iota
+	TraceNotifyVersion1
+)
+
+// TraceNotifyV0 is the common message format for versions 0 and 1.
+type TraceNotifyV0 struct {
 	Type     uint8
 	ObsPoint uint8
 	Source   uint16
 	Hash     uint32
 	OrigLen  uint32
-	CapLen   uint32
+	CapLen   uint16
+	Version  uint16
 	SrcLabel uint32
 	DstLabel uint32
 	DstID    uint16
 	Reason   uint8
 	Flags    uint8
 	Ifindex  uint32
-	OrigIP   types.IPv6
 	// data
 }
+
+// TraceNotifyV1 is the version 1 message format.
+type TraceNotifyV1 struct {
+	TraceNotifyV0
+	OrigIP types.IPv6
+	// data
+}
+
+// TraceNotify is the message format of a trace notification in the BPF ring buffer
+type TraceNotify TraceNotifyV1
+
+var (
+	traceNotifyLength = map[uint16]uint{
+		TraceNotifyVersion0: 32, // unsafe.Sizeof(&TraceNotifyV0{})
+		TraceNotifyVersion1: 48, // unsafe.Sizeof(&TraceNotifyV1{})
+	}
+)
 
 // Reasons for forwarding a packet.
 const (
@@ -75,6 +105,35 @@ func connState(reason uint8) string {
 		return str
 	}
 	return fmt.Sprintf("%d", reason)
+}
+
+func fetchVersion(data []byte, tn *TraceNotify) (version uint16, err error) {
+	offset := unsafe.Offsetof(tn.Version)
+	length := unsafe.Sizeof(tn.Version)
+	reader := bytes.NewReader(data[offset : offset+length])
+	err = binary.Read(reader, byteorder.Native, &version)
+	return version, err
+}
+
+// DecodeTraceNotify will decode 'data' into the provided TraceNotify structure
+func DecodeTraceNotify(data []byte, tn *TraceNotify) error {
+	if len(data) < traceNotifyCommonLen {
+		return fmt.Errorf("Unknown trace event")
+	}
+
+	version, err := fetchVersion(data, tn)
+	if err != nil {
+		return err
+	}
+	switch version {
+	case TraceNotifyVersion0:
+		err = binary.Read(bytes.NewReader(data), byteorder.Native, &tn.TraceNotifyV0)
+	case TraceNotifyVersion1:
+		err = binary.Read(bytes.NewReader(data), byteorder.Native, tn)
+	default:
+		err = fmt.Errorf("Unrecognized trace event (version %d)", version)
+	}
+	return err
 }
 
 func (n *TraceNotify) encryptReason() string {
@@ -128,14 +187,15 @@ func (n *TraceNotify) OriginalIP() net.IP {
 
 // DumpInfo prints a summary of the trace messages.
 func (n *TraceNotify) DumpInfo(data []byte) {
+	hdrLen := traceNotifyLength[n.Version]
 	if n.encryptReason() != "" {
 		fmt.Printf("%s %s flow %#x identity %d->%d state %s ifindex %s orig-ip %s: %s\n",
 			n.traceSummary(), n.encryptReason(), n.Hash, n.SrcLabel, n.DstLabel,
-			n.traceReason(), ifname(int(n.Ifindex)), n.OriginalIP().String(), GetConnectionSummary(data[TraceNotifyLen:]))
+			n.traceReason(), ifname(int(n.Ifindex)), n.OriginalIP().String(), GetConnectionSummary(data[hdrLen:]))
 	} else {
 		fmt.Printf("%s flow %#x identity %d->%d state %s ifindex %s orig-ip %s: %s\n",
 			n.traceSummary(), n.Hash, n.SrcLabel, n.DstLabel,
-			n.traceReason(), ifname(int(n.Ifindex)), n.OriginalIP().String(), GetConnectionSummary(data[TraceNotifyLen:]))
+			n.traceReason(), ifname(int(n.Ifindex)), n.OriginalIP().String(), GetConnectionSummary(data[hdrLen:]))
 	}
 }
 
@@ -160,16 +220,18 @@ func (n *TraceNotify) DumpVerbose(dissect bool, data []byte, prefix string) {
 		fmt.Printf("\n")
 	}
 
-	if n.CapLen > 0 && len(data) > TraceNotifyLen {
-		Dissect(dissect, data[TraceNotifyLen:])
+	hdrLen := traceNotifyLength[n.Version]
+	if n.CapLen > 0 && len(data) > int(hdrLen) {
+		Dissect(dissect, data[hdrLen:])
 	}
 }
 
 func (n *TraceNotify) getJSON(data []byte, cpuPrefix string) (string, error) {
 	v := TraceNotifyToVerbose(n)
 	v.CPUPrefix = cpuPrefix
-	if n.CapLen > 0 && len(data) > TraceNotifyLen {
-		v.Summary = GetDissectSummary(data[TraceNotifyLen:])
+	hdrLen := traceNotifyLength[n.Version]
+	if n.CapLen > 0 && len(data) > int(hdrLen) {
+		v.Summary = GetDissectSummary(data[hdrLen:])
 	}
 
 	ret, err := json.Marshal(v)

bpf/lib/common.h
@@ -268,7 +268,7 @@ enum {
 	__u16		version;	/* Capture header version */
 
 /* Capture notifications version. Must be incremented when format changes. */
-#define NOTIFY_CAPTURE_VER 0
+#define NOTIFY_CAPTURE_VER 1
 
 #ifndef TRACE_PAYLOAD_LEN
 #define TRACE_PAYLOAD_LEN 128ULL

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=a8b3b35755d84ddccdfd17f4cb5151acf188c3f9
+GO_BINDATA_SHA1SUM=e809122a2f5d56b927801b7538b5996849a16660
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/conntrack_test.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/ipv6_test.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/signal.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_max_insn.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

pkg/monitor/format/format.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Authors of Cilium
+// Copyright 2018-2019 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -117,7 +117,7 @@ func (m *MonitorFormatter) dropEvents(prefix string, data []byte) {
 func (m *MonitorFormatter) traceEvents(prefix string, data []byte) {
 	tn := monitor.TraceNotify{}
 
-	if err := binary.Read(bytes.NewReader(data), byteorder.Native, &tn); err != nil {
+	if err := monitor.DecodeTraceNotify(data, &tn); err != nil {
 		fmt.Printf("Error while parsing trace notification message: %s\n", err)
 	}
 	if m.match(monitorAPI.MessageTypeTrace, tn.Source, tn.DstID) {

commit c6695f63a315f6fc457a310343774007de9fb1e0

monitor: rename and use traceNotifyV[12]Len consts
Rename the consts so they match the corresponding type and use them in
the definition of the traceNotifyLength map.

Fixes: 906d4b459a56 ("monitor: Extend trace notification version")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

pkg/monitor/datapath_trace.go
@@ -30,10 +30,10 @@ import (
 const (
 	// traceNotifyCommonLen is the minimum length required to determine the version of the TN event.
 	traceNotifyCommonLen = 16
-	// traceNotifyV1Len is the amount of packet data provided in a trace notification v1
-	traceNotifyV1Len = 32
-	// traceNotifyV2Len is the amount of packet data provided in a trace notification v2
-	traceNotifyV2Len = 48
+	// traceNotifyV0Len is the amount of packet data provided in a trace notification v0.
+	traceNotifyV0Len = 32
+	// traceNotifyV1Len is the amount of packet data provided in a trace notification v1.
+	traceNotifyV1Len = 48
 	// TraceReasonEncryptMask is the bit used to indicate encryption or not
 	TraceReasonEncryptMask uint8 = 0x80
 )
@@ -79,8 +79,8 @@ type TraceNotify TraceNotifyV1
 
 var (
 	traceNotifyLength = map[uint16]uint{
-		TraceNotifyVersion0: 32, // unsafe.Sizeof(&TraceNotifyV0{})
-		TraceNotifyVersion1: 48, // unsafe.Sizeof(&TraceNotifyV1{})
+		TraceNotifyVersion0: traceNotifyV0Len,
+		TraceNotifyVersion1: traceNotifyV1Len,
 	}
 )
 