g0odi3z2j/cilium

commit 0eca58908c7fe61118d6b43db7d33eaa8623ebe8

ipam: Support for Azure IPAM
Following the refactoring of the AWS ENI IPAM logic to be generic. This commit
adds support for Azure IPAM.

The function is similar to how AWS ENI mode operates with a couple of key
differences:
 - A single interface provides enough IPs to host all local pods so support to
   create additional interfaces is not required.
 - The Azure API does not support allocating IP itself. It can only assign IPs
   to interfaces. The cilium-operator thus maintains a small allocator to keep
   track of available IPs in all subnets.

In order to access the Azure API, a service principal is required. The
credentials for the service principal is passed to the operator as environment
variables or command line arguments.

Signed-off-by: Thomas Graf <thomas@cilium.io>

go.mod
@@ -4,6 +4,10 @@ go 1.14
 
 // direct dependencies
 require (
+	github.com/Azure/azure-sdk-for-go v38.0.0+incompatible
+	github.com/Azure/go-autorest/autorest/azure/auth v0.4.2
+	github.com/Azure/go-autorest/autorest/to v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.2.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496
 	github.com/aws/aws-sdk-go-v2 v0.18.0
 	github.com/blang/semver v3.5.0+incompatible
@@ -62,7 +66,7 @@ require (
 	github.com/spf13/viper v1.6.1
 	github.com/vishvananda/netlink v1.1.1-0.20200210222539-bfba8e4149db
 	go.etcd.io/etcd v0.0.0-20191023171146-3cf2f69b5738
-	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586
+	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413
 	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5

commit 0ffef3966e85a404a4e109ae895005e1748bc7ef

azure: Fix address allocation
The CreateOrUpdate API did not support adding additional addresses. IPs
must be requested via the VMScaleSet.

Fixes: 0eca58908c ("ipam: Support for Azure IPAM")

Signed-off-by: Vlad Ungureanu <vladu@palantir.com>
Signed-off-by: Thomas Graf <thomas@cilium.io>

go.mod
@@ -6,6 +6,7 @@ go 1.14
 require (
 	github.com/Azure/azure-sdk-for-go v38.0.0+incompatible
 	github.com/Azure/go-autorest/autorest/azure/auth v0.4.2
+	github.com/Azure/go-autorest/autorest/to v0.3.0
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496
 	github.com/aws/aws-sdk-go-v2 v0.18.0
 	github.com/blang/semver v3.5.0+incompatible

pkg/azure/api/api.go
@@ -16,7 +16,7 @@ package api
 
 import (
 	"context"
-	"net"
+	"fmt"
 	"strings"
 	"time"
 
@@ -26,9 +26,12 @@ import (
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/rand"
 	"github.com/cilium/cilium/pkg/spanstat"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
+	"github.com/Azure/go-autorest/autorest/to"
+
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-09-01/network"
 	"github.com/Azure/go-autorest/autorest/azure/auth"
 )
@@ -44,6 +47,7 @@ type Client struct {
 	resourceGroup   string
 	interfaces      network.InterfacesClient
 	virtualnetworks network.VirtualNetworksClient
+	vmss            compute.VirtualMachineScaleSetVMsClient
 	vmscalesets     compute.VirtualMachineScaleSetsClient
 	limiter         *helpers.ApiLimiter
 	metricsAPI      MetricsAPI
@@ -61,6 +65,7 @@ func NewClient(subscriptionID, resourceGroup string, metrics MetricsAPI, rateLim
 		resourceGroup:   resourceGroup,
 		interfaces:      network.NewInterfacesClient(subscriptionID),
 		virtualnetworks: network.NewVirtualNetworksClient(subscriptionID),
+		vmss:            compute.NewVirtualMachineScaleSetVMsClient(subscriptionID),
 		vmscalesets:     compute.NewVirtualMachineScaleSetsClient(subscriptionID),
 		metricsAPI:      metrics,
 		limiter:         helpers.NewApiLimiter(metrics, rateLimit, burst),
@@ -76,6 +81,8 @@ func NewClient(subscriptionID, resourceGroup string, metrics MetricsAPI, rateLim
 	c.interfaces.AddToUserAgent(userAgent)
 	c.virtualnetworks.Authorizer = authorizer
 	c.virtualnetworks.AddToUserAgent(userAgent)
+	c.vmss.Authorizer = authorizer
+	c.vmss.AddToUserAgent(userAgent)
 	c.vmscalesets.Authorizer = authorizer
 	c.vmscalesets.AddToUserAgent(userAgent)
 
@@ -152,6 +159,10 @@ func parseInterface(iface *network.Interface) (instanceID string, i *types.Azure
 
 	if iface.ID != nil {
 		i.ID = *iface.ID
+	}
+
+	if iface.Name != nil {
+		i.Name = *iface.Name
 	}
 
 	if iface.NetworkSecurityGroup != nil {
@@ -274,32 +285,50 @@ func (c *Client) GetVpcsAndSubnets(ctx context.Context) (ipamTypes.VirtualNetwor
 	return vpcs, subnets, nil
 }
 
-// AssignPrivateIpAddresses assigns the IPs to the interface as specified by
-// the interfaceID. The provided IPs must belong to the subnet as specified by
-// the subnet ID.
-func (c *Client) AssignPrivateIpAddresses(ctx context.Context, subnetID, interfaceID string, ips []net.IP) error {
-	ipConfigurations := make([]network.InterfaceIPConfiguration, 0, len(ips))
+func generateIpConfigName() string {
+	return "Cilium-" + rand.RandomRuneWithLen(8)
+}
 
-	for _, ip := range ips {
-		ipString := ip.String()
+// AssignPrivateIpAddresses assigns the IPs to the interface as specified by
+// the interfaceName.
+func (c *Client) AssignPrivateIpAddresses(ctx context.Context, instanceID, vmssName, subnetID, interfaceName string, addresses int) error {
+	var netIfConfig *compute.VirtualMachineScaleSetNetworkConfiguration
+
+	result, err := c.vmss.Get(ctx, c.resourceGroup, vmssName, instanceID, compute.InstanceView)
+	if err != nil {
+		return fmt.Errorf("failed to get VM %s from VMSS %s: %s", instanceID, vmssName, err)
+	}
+
+	// Search for the existing network interface configuration
+	if result.NetworkProfileConfiguration != nil {
+		for _, networkInterfaceConfiguration := range *result.NetworkProfileConfiguration.NetworkInterfaceConfigurations {
+			if to.String(networkInterfaceConfiguration.Name) == interfaceName {
+				netIfConfig = &networkInterfaceConfiguration
+				break
+			}
+		}
+	}
+
+	if netIfConfig == nil {
+		return fmt.Errorf("interface %s does not exist in VM %s", interfaceName, instanceID)
+	}
+
+	ipConfigurations := make([]compute.VirtualMachineScaleSetIPConfiguration, 0, addresses)
+	for i := 0; i <= addresses; i++ {
 		ipConfigurations = append(ipConfigurations,
-			network.InterfaceIPConfiguration{
-				InterfaceIPConfigurationPropertiesFormat: &network.InterfaceIPConfigurationPropertiesFormat{
-					PrivateIPAddress: &ipString,
-					Subnet:           &network.Subnet{ID: &subnetID},
+			compute.VirtualMachineScaleSetIPConfiguration{
+				Name: to.StringPtr(generateIpConfigName()),
+				VirtualMachineScaleSetIPConfigurationProperties: &compute.VirtualMachineScaleSetIPConfigurationProperties{
+					PrivateIPAddressVersion: compute.IPv4,
+					Subnet:                  &compute.APIEntityReference{ID: to.StringPtr(subnetID)},
 				},
 			},
 		)
-
 	}
 
-	ifaceParams := network.Interface{
-		ID: &interfaceID,
-		InterfacePropertiesFormat: &network.InterfacePropertiesFormat{
-			IPConfigurations: &ipConfigurations,
-		},
-	}
+	ipConfigurations = append(*netIfConfig.IPConfigurations, ipConfigurations...)
+	netIfConfig.IPConfigurations = &ipConfigurations
 
-	_, err := c.interfaces.CreateOrUpdate(ctx, c.resourceGroup, interfaceID, ifaceParams)
+	_, err = c.vmss.Update(ctx, c.resourceGroup, vmssName, instanceID, result)
 	return err
 }

pkg/azure/api/mock/mock.go
@@ -25,6 +25,7 @@ import (
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	"github.com/cilium/cilium/pkg/lock"
 
+	"github.com/cilium/ipam/service/ipallocator"
 	"golang.org/x/time/rate"
 )
 
@@ -39,9 +40,14 @@ const (
 	MaxOperation
 )
 
+type subnet struct {
+	subnet    *ipamTypes.Subnet
+	allocator *ipallocator.Range
+}
+
 type API struct {
 	mutex     lock.RWMutex
-	subnets   map[string]*ipamTypes.Subnet
+	subnets   map[string]*subnet
 	instances *ipamTypes.InstanceMap
 	vnets     map[string]*ipamTypes.VirtualNetwork
 	errors    map[Operation]error
@@ -52,7 +58,7 @@ type API struct {
 func NewAPI(subnets []*ipamTypes.Subnet, vnets []*ipamTypes.VirtualNetwork) *API {
 	api := &API{
 		instances: ipamTypes.NewInstanceMap(),
-		subnets:   map[string]*ipamTypes.Subnet{},
+		subnets:   map[string]*subnet{},
 		vnets:     map[string]*ipamTypes.VirtualNetwork{},
 		errors:    map[Operation]error{},
 		delaySim:  helpers.NewDelaySimulator(),
@@ -69,9 +75,18 @@ func NewAPI(subnets []*ipamTypes.Subnet, vnets []*ipamTypes.VirtualNetwork) *API
 
 func (a *API) UpdateSubnets(subnets []*ipamTypes.Subnet) {
 	a.mutex.Lock()
-	a.subnets = map[string]*ipamTypes.Subnet{}
+	a.subnets = map[string]*subnet{}
 	for _, s := range subnets {
-		a.subnets[s.ID] = s.DeepCopy()
+		_, cidr, _ := net.ParseCIDR(s.CIDR.String())
+		cidrRange, err := ipallocator.NewCIDRRange(cidr)
+		if err != nil {
+			panic(err)
+		}
+
+		a.subnets[s.ID] = &subnet{
+			subnet:    s.DeepCopy(),
+			allocator: cidrRange,
+		}
 	}
 	a.mutex.Unlock()
 }
@@ -150,7 +165,7 @@ func (a *API) GetVpcsAndSubnets(ctx context.Context) (ipamTypes.VirtualNetworkMa
 	subnets := ipamTypes.SubnetMap{}
 
 	for _, s := range a.subnets {
-		subnets[s.ID] = s.DeepCopy()
+		subnets[s.subnet.ID] = s.subnet.DeepCopy()
 	}
 
 	for _, v := range a.vnets {
@@ -160,7 +175,7 @@ func (a *API) GetVpcsAndSubnets(ctx context.Context) (ipamTypes.VirtualNetworkMa
 	return vnets, subnets, nil
 }
 
-func (a *API) AssignPrivateIpAddresses(ctx context.Context, subnetID, interfaceID string, ips []net.IP) error {
+func (a *API) AssignPrivateIpAddresses(ctx context.Context, vmName, vmssName, subnetID, interfaceName string, addresses int) error {
 	a.rateLimit()
 	a.delaySim.Delay(AssignPrivateIpAddresses)
 
@@ -173,21 +188,30 @@ func (a *API) AssignPrivateIpAddresses(ctx context.Context, subnetID, interfaceI
 
 	foundInterface := false
 
-	err := a.instances.ForeachInterface("", func(instanceID, id string, iface ipamTypes.InterfaceRevision) error {
+	err := a.instances.ForeachInterface("", func(id, _ string, iface ipamTypes.InterfaceRevision) error {
 		intf, ok := iface.Resource.(*types.AzureInterface)
 		if !ok {
 			return fmt.Errorf("invalid interface object")
 		}
 
-		if id != interfaceID {
+		if intf.Name != interfaceName || intf.VMID() != vmName {
 			return nil
 		}
 
-		if len(intf.Addresses)+len(ips) > types.InterfaceAddressLimit {
+		if len(intf.Addresses)+addresses > types.InterfaceAddressLimit {
 			return fmt.Errorf("exceeded interface limit")
 		}
 
-		for _, ip := range ips {
+		s, ok := a.subnets[subnetID]
+		if !ok {
+			return fmt.Errorf("subnet %s does not exist", subnetID)
+		}
+
+		for i := 0; i < addresses; i++ {
+			ip, err := s.allocator.AllocateNext()
+			if err != nil {
+				panic("Unable to allocate IP from allocator")
+			}
 			intf.Addresses = append(intf.Addresses, types.AzureAddress{
 				IP:     ip.String(),
 				Subnet: subnetID,
@@ -203,7 +227,7 @@ func (a *API) AssignPrivateIpAddresses(ctx context.Context, subnetID, interfaceI
 	}
 
 	if !foundInterface {
-		return fmt.Errorf("interface %s not found", interfaceID)
+		return fmt.Errorf("interface %s not found", interfaceName)
 	}
 
 	return nil

pkg/azure/ipam/instances.go
@@ -16,7 +16,6 @@ package ipam
 
 import (
 	"context"
-	"net"
 	"time"
 
 	"github.com/cilium/cilium/pkg/ipam"
@@ -32,7 +31,7 @@ import (
 type AzureAPI interface {
 	GetInstances(ctx context.Context) (*ipamTypes.InstanceMap, error)
 	GetVpcsAndSubnets(ctx context.Context) (ipamTypes.VirtualNetworkMap, ipamTypes.SubnetMap, error)
-	AssignPrivateIpAddresses(ctx context.Context, subnetID, interfaceID string, ips []net.IP) error
+	AssignPrivateIpAddresses(ctx context.Context, instanceID, vmssName, subnetID, interfaceName string, addresses int) error
 }
 
 // InstancesManager maintains the list of instances. It must be kept up to date

pkg/azure/ipam/node.go
@@ -105,6 +105,7 @@ func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (a *ipam.AllocationA
 				}).Debug("Subnet has IPs available")
 
 				a.InterfaceID = iface.ID
+				a.Interface = interfaceObj
 				a.PoolID = poolID
 				a.AvailableForAllocation = math.IntMin(available, availableOnInterface)
 			}
@@ -117,18 +118,12 @@ func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (a *ipam.AllocationA
 
 // AllocateIPs performs the Azure IP allocation operation
 func (n *Node) AllocateIPs(ctx context.Context, a *ipam.AllocationAction) error {
-	ips, err := n.manager.getAllocator().AllocateMany(a.PoolID, a.AvailableForAllocation)
-	if err != nil {
-		return err
+	iface, ok := a.Interface.Resource.(*types.AzureInterface)
+	if !ok {
+		return fmt.Errorf("invalid interface object")
 	}
 
-	err = n.manager.api.AssignPrivateIpAddresses(ctx, string(a.PoolID), a.InterfaceID, ips)
-	if err != nil {
-		n.manager.getAllocator().ReleaseMany(a.PoolID, ips)
-		return err
-	}
-
-	return nil
+	return n.manager.api.AssignPrivateIpAddresses(ctx, iface.VMID(), iface.VMScaleSetName(), string(a.PoolID), iface.Name, a.AvailableForAllocation)
 }
 
 // CreateInterface is called to create a new interface. This operation is

pkg/azure/types/types.go
@@ -15,6 +15,8 @@
 package types
 
 import (
+	"strings"
+
 	"github.com/cilium/cilium/pkg/ipam/types"
 )
 
@@ -67,6 +69,11 @@ type AzureInterface struct {
 	// +optional
 	ID string `json:"id,omitempty"`
 
+	// Name is the name of the interface
+	//
+	// +optional
+	Name string `json:"name,omitempty"`
+
 	// MAC is the mac address
 	//
 	// +optional
@@ -85,11 +92,62 @@ type AzureInterface struct {
 
 	// SecurityGroup is the security group associated with the interface
 	SecurityGroup string `json:"security-group,omitempty"`
+
+	// vmssName is the name of the virtual machine scale set. This field is
+	// set by extractIDs()
+	vmssName string
+
+	// vmID is the ID of the virtual machine
+	vmID string
+
+	// resourceGroup is the resource group the interface belongs to
+	resourceGroup string
 }
 
 // InterfaceID returns the identifier of the interface
 func (a *AzureInterface) InterfaceID() string {
 	return a.ID
+}
+
+func (a *AzureInterface) extractIDs() {
+	switch {
+	// //subscriptions/xxx/resourceGroups/yyy/providers/Microsoft.Compute/virtualMachineScaleSets/ssss/virtualMachines/vvv/networkInterfaces/iii
+	case strings.Contains(a.ID, "virtualMachineScaleSets"):
+		segs := strings.Split(a.ID, "/")
+		if len(segs) >= 5 {
+			a.resourceGroup = segs[4]
+		}
+		if len(segs) >= 9 {
+			a.vmssName = segs[8]
+		}
+		if len(segs) >= 11 {
+			a.vmID = segs[10]
+		}
+	}
+}
+
+// ResourceGroup returns the resource group the interface belongs to
+func (a *AzureInterface) ResourceGroup() string {
+	if a.resourceGroup == "" {
+		a.extractIDs()
+	}
+	return a.resourceGroup
+}
+
+// VMScaleSetName returns the VM scale set name the interface belongs to
+func (a *AzureInterface) VMScaleSetName() string {
+	if a.vmssName == "" {
+		a.extractIDs()
+	}
+	return a.vmssName
+}
+
+// VMID returns the VM ID the interface belongs to
+func (a *AzureInterface) VMID() string {
+	if a.vmID == "" {
+		a.extractIDs()
+	}
+	return a.vmID
 }
 
 // ForeachAddress iterates over all addresses and calls fn

pkg/ipam/node.go
@@ -429,6 +429,9 @@ type AllocationAction struct {
 	// context is needed or a new interface must be created.
 	InterfaceID string
 
+	// Interface is the interface to allocate IPs on
+	Interface ipamTypes.InterfaceRevision
+
 	// PoolID is the IPAM pool identifier to allocate the IPs from. This
 	// can correspond to a subnet ID or it can also left blank or set to a
 	// value such as "global" to indicate a single address pool.

pkg/azure/api/api_interaction_test.go
@@ -1,0 +1,63 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build azure
+
+package api
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cilium/cilium/pkg/api/metrics/mock"
+
+	"gopkg.in/check.v1"
+)
+
+// How to run these tests:
+//
+// 1. Modify testSubscription and testResourceGroup
+// 2. Create a service principal
+//      az ad sp create-for-rbac -n cilium-unit-test
+//      {
+//        "appId": "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+//        "displayName": "cilium-unit-test",
+//        "name": "http://cilium-unit-test",
+//        "password": "bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb",
+//        "tenant": "cccccccc-cccc-cccc-cccc-cccccccccccc"
+//      }
+// 3. Set
+//      AZURE_SUBSCRIPTION_ID=$(az account show --query id | tr -d \")
+//      AZURE_CLIENT_ID=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa
+//      AZURE_CLIENT_SECRET=bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb
+//      AZURE_TENANT_ID=cccccccc-cccc-cccc-cccc-cccccccccccc
+//      AZURE_NODE_RESOURCE_GROUP=$(az aks show -n $CLUSTER_NAME -g $RESOURCE_GROUP | jq -r .nodeResourceGroup)
+
+type ApiInteractionSuite struct{}
+
+var (
+	_ = check.Suite(&ApiInteractionSuite{})
+
+	testSubscription  = "fd182b79-36cb-4a4e-b567-e568d63f9f62"
+	testResourceGroup = "MC_aks-test_aks-test_westeurope"
+)
+
+func (a *ApiInteractionSuite) TestRateLimit(c *check.C) {
+	client, err := NewClient(testSubscription, testResourceGroup, mock.NewMockMetrics(), 10.0, 4)
+	c.Assert(err, check.IsNil)
+
+	instances, err := client.GetInstances(context.Background())
+	c.Assert(err, check.IsNil)
+	fmt.Printf("%+v\n", instances)
+}

pkg/azure/api/mock/mock_test.go
@@ -19,11 +19,11 @@ package mock
 import (
 	"context"
 	"errors"
-	"net"
 	"testing"
 
 	"github.com/cilium/cilium/pkg/azure/types"
 	"github.com/cilium/cilium/pkg/checker"
+	"github.com/cilium/cilium/pkg/cidr"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 
 	"gopkg.in/check.v1"
@@ -38,7 +38,8 @@ type MockSuite struct{}
 var _ = check.Suite(&MockSuite{})
 
 func (e *MockSuite) TestMock(c *check.C) {
-	api := NewAPI([]*ipamTypes.Subnet{{ID: "s-1", AvailableAddresses: 100}}, []*ipamTypes.VirtualNetwork{{ID: "v-1"}})
+	subnet := &ipamTypes.Subnet{ID: "s-1", CIDR: cidr.MustParseCIDR("10.0.0.0/16"), AvailableAddresses: 100}
+	api := NewAPI([]*ipamTypes.Subnet{subnet}, []*ipamTypes.VirtualNetwork{{ID: "v-1"}})
 	c.Assert(api, check.Not(check.IsNil))
 
 	instances, err := api.GetInstances(context.Background())
@@ -50,37 +51,35 @@ func (e *MockSuite) TestMock(c *check.C) {
 	c.Assert(len(vnets), check.Equals, 1)
 	c.Assert(vnets["v-1"], checker.DeepEquals, &ipamTypes.VirtualNetwork{ID: "v-1"})
 	c.Assert(len(subnets), check.Equals, 1)
-	c.Assert(subnets["s-1"], checker.DeepEquals, &ipamTypes.Subnet{ID: "s-1", AvailableAddresses: 100})
+	c.Assert(subnets["s-1"], checker.DeepEquals, subnet)
 
+	ifaceID := "/subscriptions/xxx/resourceGroups/g1/providers/Microsoft.Compute/virtualMachineScaleSets/vmss11/virtualMachines/vm1/networkInterfaces/vmss11"
 	instances = ipamTypes.NewInstanceMap()
-	instances.Update("i-1", ipamTypes.InterfaceRevision{Resource: &types.AzureInterface{ID: "intf-1"}})
+	instances.Update("vm1", ipamTypes.InterfaceRevision{
+		Resource: &types.AzureInterface{ID: ifaceID, Name: "eth0"},
+	})
 	api.UpdateInstances(instances)
 	instances, err = api.GetInstances(context.Background())
 	c.Assert(err, check.IsNil)
 	c.Assert(instances.NumInstances(), check.Equals, 1)
 	instances.ForeachInterface("", func(instanceID, interfaceID string, iface ipamTypes.InterfaceRevision) error {
-		c.Assert(instanceID, check.Equals, "i-1")
-		c.Assert(interfaceID, check.Equals, "intf-1")
+		c.Assert(instanceID, check.Equals, "vm1")
+		c.Assert(interfaceID, check.Equals, ifaceID)
 		return nil
 	})
 
-	err = api.AssignPrivateIpAddresses(context.Background(), "s-1", "intf-1", []net.IP{net.ParseIP("1.1.1.1"), net.ParseIP("2.2.2.2")})
+	err = api.AssignPrivateIpAddresses(context.Background(), "vm1", "vmss1", "s-1", "eth0", 2)
 	c.Assert(err, check.IsNil)
 	instances, err = api.GetInstances(context.Background())
 	c.Assert(err, check.IsNil)
 	c.Assert(instances.NumInstances(), check.Equals, 1)
 	instances.ForeachInterface("", func(instanceID, interfaceID string, revision ipamTypes.InterfaceRevision) error {
-		c.Assert(instanceID, check.Equals, "i-1")
-		c.Assert(interfaceID, check.Equals, "intf-1")
+		c.Assert(instanceID, check.Equals, "vm1")
+		c.Assert(interfaceID, check.Equals, ifaceID)
 
 		iface, ok := revision.Resource.(*types.AzureInterface)
 		c.Assert(ok, check.Equals, true)
-		c.Assert(iface, checker.DeepEquals, &types.AzureInterface{
-			ID: "intf-1",
-			Addresses: []types.AzureAddress{
-				{IP: "1.1.1.1", Subnet: "s-1", State: types.StateSucceeded},
-				{IP: "2.2.2.2", Subnet: "s-1", State: types.StateSucceeded},
-			}})
+		c.Assert(len(iface.Addresses), check.Equals, 2)
 		return nil
 	})
 }
@@ -100,12 +99,13 @@ func (e *MockSuite) TestSetMockError(c *check.C) {
 	c.Assert(err, check.Equals, mockError)
 
 	api.SetMockError(AssignPrivateIpAddresses, mockError)
-	err = api.AssignPrivateIpAddresses(context.Background(), "s-1", "i-1", []net.IP{})
+	err = api.AssignPrivateIpAddresses(context.Background(), "vmss1", "i-1", "s-1", "eth0", 0)
 	c.Assert(err, check.Equals, mockError)
 }
 
 func (e *MockSuite) TestSetLimiter(c *check.C) {
-	api := NewAPI([]*ipamTypes.Subnet{{ID: "s-1", AvailableAddresses: 100}}, []*ipamTypes.VirtualNetwork{{ID: "v-1"}})
+	subnet := &ipamTypes.Subnet{ID: "s-1", CIDR: cidr.MustParseCIDR("10.0.0.0/16"), AvailableAddresses: 100}
+	api := NewAPI([]*ipamTypes.Subnet{subnet}, []*ipamTypes.VirtualNetwork{{ID: "v-1"}})
 	c.Assert(api, check.Not(check.IsNil))
 
 	api.SetLimiter(10.0, 2)

pkg/azure/ipam/ipam_test.go
@@ -168,9 +168,10 @@ func (e *IPAMSuite) TestIpamPreAllocate8(c *check.C) {
 	c.Assert(instances, check.Not(check.IsNil))
 
 	m := ipamTypes.NewInstanceMap()
-	m.Update("i-1", ipamTypes.InterfaceRevision{
+	m.Update("vm1", ipamTypes.InterfaceRevision{
 		Resource: &types.AzureInterface{
-			ID:            "intf-1",
+			ID:            "/subscriptions/xxx/resourceGroups/g1/providers/Microsoft.Compute/virtualMachineScaleSets/vmss11/virtualMachines/vm1/networkInterfaces/vmss11",
+			Name:          "eth0",
 			SecurityGroup: "sg1",
 			Addresses: []types.AzureAddress{
 				{
@@ -191,7 +192,7 @@ func (e *IPAMSuite) TestIpamPreAllocate8(c *check.C) {
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
 
-	cn := newCiliumNode("node1", "i-1", preAllocate, minAllocate)
+	cn := newCiliumNode("node1", "vm1", preAllocate, minAllocate)
 	statusRevision := k8sapi.statusRevision()
 	mngr.Update(cn)
 	c.Assert(testutils.WaitUntil(func() bool { return reachedAddressesNeeded(mngr, "node1", 0) }, 5*time.Second), check.IsNil)
@@ -227,9 +228,10 @@ func (e *IPAMSuite) TestIpamMinAllocate10(c *check.C) {
 	c.Assert(instances, check.Not(check.IsNil))
 
 	m := ipamTypes.NewInstanceMap()
-	m.Update("i-1", ipamTypes.InterfaceRevision{
+	m.Update("vm1", ipamTypes.InterfaceRevision{
 		Resource: &types.AzureInterface{
-			ID:            "intf-1",
+			ID:            "/subscriptions/xxx/resourceGroups/g1/providers/Microsoft.Compute/virtualMachineScaleSets/vmss11/virtualMachines/vm1/networkInterfaces/vmss11",
+			Name:          "eth0",
 			SecurityGroup: "sg1",
 			Addresses: []types.AzureAddress{
 				{
@@ -250,7 +252,7 @@ func (e *IPAMSuite) TestIpamMinAllocate10(c *check.C) {
 	c.Assert(err, check.IsNil)
 	c.Assert(mngr, check.Not(check.IsNil))
 
-	cn := newCiliumNode("node1", "i-1", preAllocate, minAllocate)
+	cn := newCiliumNode("node1", "vm1", preAllocate, minAllocate)
 	statusRevision := k8sapi.statusRevision()
 	mngr.Update(cn)
 	c.Assert(testutils.WaitUntil(func() bool { return reachedAddressesNeeded(mngr, "node1", 0) }, 5*time.Second), check.IsNil)
@@ -302,9 +304,10 @@ func (e *IPAMSuite) TestIpamManyNodes(c *check.C) {
 	allInstances := ipamTypes.NewInstanceMap()
 
 	for i := range state {
-		allInstances.Update(fmt.Sprintf("i-%d", i), ipamTypes.InterfaceRevision{
+		allInstances.Update(fmt.Sprintf("vm%d", i), ipamTypes.InterfaceRevision{
 			Resource: &types.AzureInterface{
-				ID:            fmt.Sprintf("intf-%d", i),
+				ID:            fmt.Sprintf("/subscriptions/xxx/resourceGroups/g1/providers/Microsoft.Compute/virtualMachineScaleSets/vmss11/virtualMachines/vm%d/networkInterfaces/vmss11", i),
+				Name:          "eth0",
 				SecurityGroup: "sg1",
 				Addresses:     []types.AzureAddress{},
 				State:         types.StateSucceeded,
@@ -317,7 +320,7 @@ func (e *IPAMSuite) TestIpamManyNodes(c *check.C) {
 	instances.Resync(context.TODO())
 
 	for i := range state {
-		state[i] = &nodeState{name: fmt.Sprintf("node%d", i), instanceName: fmt.Sprintf("i-%d", i)}
+		state[i] = &nodeState{name: fmt.Sprintf("node%d", i), instanceName: fmt.Sprintf("vm%d", i)}
 		state[i].cn = newCiliumNode(state[i].name, state[i].instanceName, 1, minAllocate)
 		mngr.Update(state[i].cn)
 	}
@@ -377,9 +380,10 @@ func benchmarkAllocWorker(c *check.C, workers int64, delay time.Duration, rateLi
 	c.ResetTimer()
 
 	for i := range state {
-		allInstances.Update(fmt.Sprintf("i-%d", i), ipamTypes.InterfaceRevision{
+		allInstances.Update(fmt.Sprintf("vm%d", i), ipamTypes.InterfaceRevision{
 			Resource: &types.AzureInterface{
-				ID:            fmt.Sprintf("intf-%d", i),
+				ID:            fmt.Sprintf("/subscriptions/xxx/resourceGroups/g1/providers/Microsoft.Compute/virtualMachineScaleSets/vmss11/virtualMachines/vm%d/networkInterfaces/vmss11", i),
+				Name:          "eth0",
 				SecurityGroup: "sg1",
 				Addresses:     []types.AzureAddress{},
 				State:         types.StateSucceeded,
@@ -391,7 +395,7 @@ func benchmarkAllocWorker(c *check.C, workers int64, delay time.Duration, rateLi
 	instances.Resync(context.Background())
 
 	for i := range state {
-		state[i] = &nodeState{name: fmt.Sprintf("node%d", i), instanceName: fmt.Sprintf("i-%d", i)}
+		state[i] = &nodeState{name: fmt.Sprintf("node%d", i), instanceName: fmt.Sprintf("vm%d", i)}
 		state[i].cn = newCiliumNode(state[i].name, state[i].instanceName, 1, 10)
 		mngr.Update(state[i].cn)
 	}

pkg/azure/types/types_test.go
@@ -71,3 +71,13 @@ func (e *TypesSuite) TestForeachAddresses(c *check.C) {
 	})
 	c.Assert(interfaces, check.Equals, 2)
 }
+
+func (e *TypesSuite) TestExtractIDs(c *check.C) {
+	intf := AzureInterface{
+		ID: "/subscriptions/xxx/resourceGroups/MC_aks-test_aks-test_westeurope/providers/Microsoft.Compute/virtualMachineScaleSets/aks-nodepool1-10706209-vmss/virtualMachines/3/networkInterfaces/aks-nodepool1-10706209-vmss",
+	}
+
+	c.Assert(intf.ResourceGroup(), check.Equals, "MC_aks-test_aks-test_westeurope")
+	c.Assert(intf.VMID(), check.Equals, "3")
+	c.Assert(intf.VMScaleSetName(), check.Equals, "aks-nodepool1-10706209-vmss")
+}