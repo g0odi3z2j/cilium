g0odi3z2j/cilium

commit 097d155578c6b8869f1a6c9fe763a72efd30e8af

bpf: support IPv4 fragments for L4 load balancer key extraction
Add support for IPv4 fragments for L4 load balancer.

Define and use a LRU table for storing information about datagram
fragments. Add the relevant processing functions to ipv4.h. When a
TCP/UDP IPv4 fragment is found:

- If this is the first (logical) fragment of the datagram, add an entry
  to that map using (dst_ip, src_ip, proto, datagram_id) as a key and
  (dst_port, src_port) as value.

- If this is another fragment in the datagram, attempt to retrieve L4
  ports from that map.

Fragment processing is added to "extract_l4_port()", where L4 ports
extraction is performed for load balancing, to avoid adding a new
switch/case on ip4->protocol elsewhere. However, it is worth noting that
"extract_l4_port()", called by "lb(4|6)_extract_key()", is also part of
the "nodeport_lb(4|6)()" path, and I am unsure whether this is
desirable.

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

bpf/lib/ipv4.h
@@ -8,6 +8,30 @@
 
 #include "dbg.h"
 
+struct ipv4_frag_id {
+	__be32	daddr;
+	__be32	saddr;
+	__be16	id;		/* L4 datagram identifier */
+	__u8	proto;
+	__u8	pad;
+} __attribute__((packed));
+
+struct ipv4_frag_l4ports {
+	__be16	sport;
+	__be16	dport;
+} __attribute__((packed));
+
+#if defined IPV4_FRAGMENTS
+struct bpf_elf_map __section_maps IPV4_FRAG_DATAGRAMS_MAP = {
+	.type           = BPF_MAP_TYPE_LRU_HASH,
+	.size_key	= sizeof(struct ipv4_frag_id),
+	.size_value	= sizeof(struct ipv4_frag_l4ports),
+	.pinning	= PIN_GLOBAL_NS,
+	.max_elem	= CILIUM_IPV4_FRAG_MAP_MAX_ENTRIES,
+	.flags		= CONDITIONAL_PREALLOC,
+};
+#endif
+
 static __always_inline int ipv4_load_daddr(struct __ctx_buff *ctx, int off,
 					   __u32 *dst)
 {
@@ -47,5 +71,68 @@ static __always_inline bool ipv4_is_fragment(struct iphdr *ip4)
 	// fragment (RFC791).
 	return ip4->frag_off & bpf_htons(0x3FFF);
 }
+
+#if defined IPV4_FRAGMENTS
+static __always_inline bool ipv4_is_not_first_fragment(const struct iphdr *ip4)
+{
+	/* Ignore "More fragments" bit to catch all fragments but the first */
+	return ip4->frag_off & bpf_htons(0x1FFF);
+}
+
+static __always_inline int
+ipv4_frag_get_l4ports(const struct ipv4_frag_id *frag_id,
+		      struct ipv4_frag_l4ports *ports)
+{
+	struct ipv4_frag_l4ports *tmp;
+
+	tmp = map_lookup_elem(&IPV4_FRAG_DATAGRAMS_MAP, frag_id);
+	if (!tmp)
+		return DROP_FRAG_NOSUPPORT;
+
+	/* Do not make ports a pointer to map data, copy from map */
+	__builtin_memcpy(ports, tmp, sizeof(*ports));
+	return 0;
+}
+
+static __always_inline int
+ipv4_frag_register_datagram(struct __ctx_buff *ctx, int l4_off,
+			    const struct ipv4_frag_id *frag_id,
+			    struct ipv4_frag_l4ports *ports)
+{
+	int ret;
+
+	ret = ctx_load_bytes(ctx, l4_off, ports, 4);
+	if (ret < 0)
+		return ret;
+
+	map_update_elem(&IPV4_FRAG_DATAGRAMS_MAP, frag_id, ports, BPF_ANY);
+	/* Do not return an error if map update failed, as nothing prevents us
+	 * to process the current packet normally */
+	return 0;
+}
+
+static __always_inline int
+ipv4_handle_fragment(struct __ctx_buff *ctx,
+		     const struct iphdr *ip4, int l4_off,
+		     struct ipv4_frag_l4ports *ports)
+{
+	struct ipv4_frag_id frag_id = {
+		.daddr = ip4->daddr,
+		.saddr = ip4->saddr,
+		.id = ip4->id,
+		.proto = ip4->protocol,
+		.pad = 0,
+	};
+
+	if (likely(ipv4_is_not_first_fragment(ip4)))
+		return ipv4_frag_get_l4ports(&frag_id, ports);
+	else
+		/* First logical fragment for this datagram (not necessarily the
+		 * first we receive). Fragment has L4 header, we can retrieve L4
+		 * ports and create an entry in datagrams map. */
+		return ipv4_frag_register_datagram(ctx, l4_off, &frag_id,
+						   ports);
+}
+#endif
 
 #endif /* __LIB_IPV4__ */

bpf/bpf_lxc.c
@@ -451,7 +451,7 @@ static __always_inline int handle_ipv4_from_lxc(struct __ctx_buff *ctx,
 		struct lb4_service *svc;
 		struct lb4_key key = {};
 
-		ret = lb4_extract_key(ctx, &tuple, l4_off, &key, &csum_off,
+		ret = lb4_extract_key(ctx, ip4, l4_off, &key, &csum_off,
 				      CT_EGRESS);
 		if (IS_ERR(ret)) {
 			if (ret == DROP_UNKNOWN_L4)

bpf/lib/lb.h
@@ -15,6 +15,7 @@
 
 #include "csum.h"
 #include "conntrack.h"
+#include "ipv4.h"
 
 #define CILIUM_LB_MAP_MAX_FE		256
 
@@ -159,13 +160,29 @@ static __always_inline int lb4_select_slave(__u16 count)
 }
 
 static __always_inline int extract_l4_port(struct __ctx_buff *ctx, __u8 nexthdr,
-					   int l4_off, __be16 *port)
+					   int l4_off, __be16 *port,
+					   __maybe_unused struct iphdr *ip4)
 {
 	int ret;
 
 	switch (nexthdr) {
 	case IPPROTO_TCP:
 	case IPPROTO_UDP:
+#ifdef IPV4_FRAGMENTS
+		if (ip4) {
+			struct ipv4_frag_l4ports ports = { };
+			int ret;
+
+			if (unlikely(ipv4_is_fragment(ip4))) {
+				ret = ipv4_handle_fragment(ctx, ip4, l4_off,
+							   &ports);
+				if (IS_ERR(ret))
+					return ret;
+				*port = ports.dport;
+				break;
+			}
+		}
+#endif
 		/* Port offsets for UDP and TCP are the same */
 		ret = l4_load_port(ctx, l4_off + TCP_DPORT_OFF, port);
 		if (IS_ERR(ret))
@@ -317,7 +334,7 @@ static __always_inline int lb6_extract_key(struct __ctx_buff *ctx __maybe_unused
 	csum_l4_offset_and_flags(tuple->nexthdr, csum_off);
 
 #ifdef LB_L4
-	return extract_l4_port(ctx, tuple->nexthdr, l4_off, &key->dport);
+	return extract_l4_port(ctx, tuple->nexthdr, l4_off, &key->dport, NULL);
 #else
 	return 0;
 #endif
@@ -649,7 +666,7 @@ static __always_inline int lb4_rev_nat(struct __ctx_buff *ctx, int l3_off, int l
 
 /** Extract IPv4 LB key from packet
  * @arg ctx		Packet
- * @arg tuple		Tuple
+ * @arg ip4		Pointer to L3 header
  * @arg l4_off		Offset to L4 header
  * @arg key		Pointer to store LB key in
  * @arg csum_off	Pointer to store L4 checksum field offset  in
@@ -661,7 +678,7 @@ static __always_inline int lb4_rev_nat(struct __ctx_buff *ctx, int l3_off, int l
  *   - Negative error code
  */
 static __always_inline int lb4_extract_key(struct __ctx_buff *ctx __maybe_unused,
-					   struct ipv4_ct_tuple *tuple,
+					   struct iphdr *ip4,
 					   int l4_off __maybe_unused,
 					   struct lb4_key *key,
 					   struct csum_offset *csum_off,
@@ -669,11 +686,11 @@ static __always_inline int lb4_extract_key(struct __ctx_buff *ctx __maybe_unused
 {
 	// FIXME: set after adding support for different L4 protocols in LB
 	key->proto = 0;
-	key->address = (dir == CT_INGRESS) ? tuple->saddr : tuple->daddr;
-	csum_l4_offset_and_flags(tuple->nexthdr, csum_off);
+	key->address = (dir == CT_INGRESS) ? ip4->saddr : ip4->daddr;
+	csum_l4_offset_and_flags(ip4->protocol, csum_off);
 
 #ifdef LB_L4
-	return extract_l4_port(ctx, tuple->nexthdr, l4_off, &key->dport);
+	return extract_l4_port(ctx, ip4->protocol, l4_off, &key->dport, ip4);
 #else
 	return 0;
 #endif

bpf/lib/nodeport.h
@@ -1036,7 +1036,7 @@ static __always_inline int nodeport_lb4(struct __ctx_buff *ctx,
 
 	l4_off = l3_off + ipv4_hdrlen(ip4);
 
-	ret = lb4_extract_key(ctx, &tuple, l4_off, &key, &csum_off, CT_EGRESS);
+	ret = lb4_extract_key(ctx, ip4, l4_off, &key, &csum_off, CT_EGRESS);
 	if (IS_ERR(ret)) {
 		if (ret == DROP_UNKNOWN_L4)
 			return CTX_ACT_OK;

bpf/node_config.h
@@ -102,6 +102,8 @@ DEFINE_IPV6(SNAT_IPV6_EXTERNAL, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0
 #define POLICY_MAP_SIZE 16384
 #define IPCACHE_MAP_SIZE 512000
 #define POLICY_PROG_MAP_SIZE ENDPOINTS_MAP_SIZE
+#define IPV4_FRAG_DATAGRAMS_MAP test_cilium_ipv4_frag_datagrams
+#define CILIUM_IPV4_FRAG_MAP_MAX_ENTRIES 8192
 #ifndef SKIP_DEBUG
 #define LB_DEBUG
 #endif

commit 37da8da2cf5820cbfd48e0dac63b423135f9c51a

bpf: remove CONDITIONAL_PREALLOC flag for fragment tracking LRU map
Remove the CONDITIONAL_PREALLOC flag for the fragment tracking map: This
is a LRU map which will not work without pre-allocating the space.

Fixes: 097d155578c6 ("bpf: support IPv4 fragments for L4 load balancer key extraction")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

bpf/lib/ipv4.h
@@ -28,7 +28,6 @@ struct bpf_elf_map __section_maps IPV4_FRAG_DATAGRAMS_MAP = {
 	.size_value	= sizeof(struct ipv4_frag_l4ports),
 	.pinning	= PIN_GLOBAL_NS,
 	.max_elem	= CILIUM_IPV4_FRAG_MAP_MAX_ENTRIES,
-	.flags		= CONDITIONAL_PREALLOC,
 };
 #endif
 