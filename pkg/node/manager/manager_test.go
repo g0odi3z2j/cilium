g0odi3z2j/cilium

commit 032532b388f04522c3840c1f074d345c62993b00

node: Assign remote node identity to all node IPs
Node IPs other than the Cilium internal IPs where only added to the ipcache so
far when encryption was enabled. This commit adds all node IPs to the ipcache
and assign the correct node identity.

The code is cleaned up while doing so. `n.GetNodeIP(false)` always returns the
IPv4 node address and is not dependant on the current address while iterating
over all node addresses. Calling it inside the loop is therefore unnecessary as
the result is always the same. Similarly, the `nodeIP4` variable is redundant
as the nodeIP is always an IPv4 address anyway.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/node/manager/manager.go
@@ -280,8 +280,8 @@ func (m *Manager) backgroundSync() {
 func (m *Manager) NodeUpdated(n node.Node) {
 	log.Debugf("Received node update event from %s: %#v", n.Source, n)
 	nodeIdentity := n.Identity()
-	var nodeIP, nodeIP4 net.IP
 	dpUpdate := true
+	nodeIP := n.GetNodeIP(false)
 
 	remoteHostIdentity := identity.ReservedIdentityHost
 	if option.Config.EnableRemoteNodeIdentity && n.Source != source.Local {
@@ -289,19 +289,12 @@ func (m *Manager) NodeUpdated(n node.Node) {
 	}
 
 	for _, address := range n.IPAddresses {
-		// Map the Cilium internal IP to the reachable node IP so it
-		// can be routed via the overlay. Routing via overlay is always
-		// done via public v4 address hence n.GetNodeIP(false).
-		if address.Type == addressing.NodeCiliumInternalIP {
-			nodeIP = n.GetNodeIP(false)
-			if address.IP.To4() != nil {
-				nodeIP4 = nodeIP
-			}
-		} else {
-			continue
+		var tunnelIP net.IP
+		if address.Type == addressing.NodeCiliumInternalIP || option.Config.EncryptNode {
+			tunnelIP = nodeIP
 		}
 
-		isOwning := ipcache.IPIdentityCache.Upsert(address.IP.String(), nodeIP, n.EncryptionKey, nil, ipcache.Identity{
+		isOwning := ipcache.IPIdentityCache.Upsert(address.IP.String(), tunnelIP, n.EncryptionKey, nil, ipcache.Identity{
 			ID:     remoteHostIdentity,
 			Source: n.Source,
 		})
@@ -314,27 +307,12 @@ func (m *Manager) NodeUpdated(n node.Node) {
 			dpUpdate = false
 		}
 	}
-	if option.Config.EncryptNode {
-		for _, address := range n.IPAddresses {
-			if address.Type == addressing.NodeCiliumInternalIP {
-				continue
-			}
-
-			isOwning := ipcache.IPIdentityCache.Upsert(address.IP.String(), nodeIP4, n.EncryptionKey, nil, ipcache.Identity{
-				ID:     remoteHostIdentity,
-				Source: n.Source,
-			})
-			if !isOwning {
-				dpUpdate = false
-			}
-		}
-	}
 
 	for _, address := range []net.IP{n.IPv4HealthIP, n.IPv6HealthIP} {
 		if address == nil {
 			continue
 		}
-		isOwning := ipcache.IPIdentityCache.Upsert(address.String(), n.GetNodeIP(false), n.EncryptionKey, nil, ipcache.Identity{
+		isOwning := ipcache.IPIdentityCache.Upsert(address.String(), nodeIP, n.EncryptionKey, nil, ipcache.Identity{
 			ID:     identity.ReservedIdentityHealth,
 			Source: n.Source,
 		})

commit 8a8e3bc29b518b62c003730360a1a838b4e3b2e8

node/manager: Restore node IP ipcache behavior
Commit 032532b38 added all IPs of a node to the ipcache to unify the
behavior of the identity representing a node. While the fix is correct,
it results in unexpected behavior change for some users. Revert the
behavior and only do so if remote-node identities are enabled which is
the new opt-in behavior starting with Cilium 1.7.

Fixes: 032532b38 ("node: Assign remote node identity to all node IPs")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/cmd/daemon.go
@@ -291,7 +291,7 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 
 	mtuConfig := mtu.NewConfiguration(authKeySize, option.Config.EnableIPSec, option.Config.Tunnel != option.TunnelDisabled, configuredMTU)
 
-	nodeMngr, err := nodemanager.NewManager("all", dp.Node())
+	nodeMngr, err := nodemanager.NewManager("all", dp.Node(), ipcache.IPIdentityCache, option.Config)
 	if err != nil {
 		return nil, nil, err
 	}

pkg/ipcache/fake/ipcache.go
@@ -1,0 +1,55 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package fake
+
+import (
+	"net"
+
+	"github.com/cilium/cilium/pkg/ipcache"
+	"github.com/cilium/cilium/pkg/source"
+)
+
+const (
+	EventUpsert = "upsert"
+	EventDelete = "delete"
+)
+
+type NodeEvent struct {
+	event string
+	ip    net.IP
+}
+
+type IPCache struct {
+	eventsEnabled bool
+	Events        chan NodeEvent
+}
+
+func NewIPCache(events bool) *IPCache {
+	return &IPCache{
+		eventsEnabled: events,
+		Events:        make(chan NodeEvent, 1024),
+	}
+}
+
+func (i *IPCache) Upsert(ip string, hostIP net.IP, hostKey uint8, k8sMeta *ipcache.K8sMetadata, newIdentity ipcache.Identity) bool {
+	i.Events <- NodeEvent{EventUpsert, net.ParseIP(ip)}
+	return true
+}
+
+func (i *IPCache) Delete(IP string, source source.Source) {
+	i.Events <- NodeEvent{EventDelete, net.ParseIP(IP)}
+}

pkg/node/manager/manager.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -26,7 +26,6 @@ import (
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/node/addressing"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
-	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/source"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -47,6 +46,19 @@ type nodeEntry struct {
 	// Manager.mutex must *always* be acquired first.
 	mutex lock.Mutex
 	node  nodeTypes.Node
+}
+
+// IPCache is the set of interactions the node manager performs with the ipcache
+type IPCache interface {
+	Upsert(ip string, hostIP net.IP, hostKey uint8, k8sMeta *ipcache.K8sMetadata, newIdentity ipcache.Identity) bool
+	Delete(IP string, source source.Source)
+}
+
+// Configuration is the set of configuration options the node manager depends
+// on
+type Configuration interface {
+	RemoteNodeIdentitiesEnabled() bool
+	NodeEncryptionEnabled() bool
 }
 
 // Manager is the entity that manages a collection of nodes
@@ -97,6 +109,13 @@ type Manager struct {
 	// metricDatapathValidations is the prometheus metric to track the
 	// number of datapath node validation calls
 	metricDatapathValidations prometheus.Counter
+
+	// conf is the configuration of the caller passed in via NewManager.
+	// This field is immutable after NewManager()
+	conf Configuration
+
+	// ipcache is the set operations performed against the ipcache
+	ipcache IPCache
 }
 
 // Subscribe subscribes the given node handler to node events.
@@ -132,10 +151,12 @@ func (m *Manager) Iter(f func(nh datapath.NodeHandler)) {
 }
 
 // NewManager returns a new node manager
-func NewManager(name string, dp datapath.NodeHandler) (*Manager, error) {
+func NewManager(name string, dp datapath.NodeHandler, ipcache IPCache, c Configuration) (*Manager, error) {
 	m := &Manager{
 		name:         name,
 		nodes:        map[nodeTypes.Identity]*nodeEntry{},
+		conf:         c,
+		ipcache:      ipcache,
 		nodeHandlers: map[datapath.NodeHandler]struct{}{},
 		closeChan:    make(chan struct{}),
 	}
@@ -273,6 +294,19 @@ func (m *Manager) backgroundSync() {
 	}
 }
 
+// legacyNodeIpBehavior returns true if the agent is still running in legacy
+// mode regarding node IPs
+func (m *Manager) legacyNodeIpBehavior() bool {
+	// Cilium < 1.7 only exposed the Cilium internalIP to the ipcache
+	// unless encryption was enabled. This meant that for the majority of
+	// node IPs, CIDR policy rules would apply. With the introduction of
+	// remote-node identities, all node IPs were suddenly added to the
+	// ipcache. This resulted in a behavioral change. New deployments will
+	// provide this behavior out of the gate, existing deployments will
+	// have to opt into this by enabling remote-node identities.
+	return !m.conf.NodeEncryptionEnabled() && !m.conf.RemoteNodeIdentitiesEnabled()
+}
+
 // NodeUpdated is called after the information of a node has been updated. The
 // node in the manager is added or updated if the source is allowed to update
 // the node. If an update or addition has occurred, NodeUpdate() of the datapath
@@ -284,17 +318,21 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 	nodeIP := n.GetNodeIP(false)
 
 	remoteHostIdentity := identity.ReservedIdentityHost
-	if option.Config.EnableRemoteNodeIdentity && n.Source != source.Local {
+	if m.conf.RemoteNodeIdentitiesEnabled() && n.Source != source.Local {
 		remoteHostIdentity = identity.ReservedIdentityRemoteNode
 	}
 
 	for _, address := range n.IPAddresses {
 		var tunnelIP net.IP
-		if address.Type == addressing.NodeCiliumInternalIP || option.Config.EncryptNode {
+		if address.Type == addressing.NodeCiliumInternalIP || m.conf.NodeEncryptionEnabled() {
 			tunnelIP = nodeIP
 		}
 
-		isOwning := ipcache.IPIdentityCache.Upsert(address.IP.String(), tunnelIP, n.EncryptionKey, nil, ipcache.Identity{
+		if m.legacyNodeIpBehavior() && address.Type != addressing.NodeCiliumInternalIP {
+			continue
+		}
+
+		isOwning := m.ipcache.Upsert(address.IP.String(), tunnelIP, n.EncryptionKey, nil, ipcache.Identity{
 			ID:     remoteHostIdentity,
 			Source: n.Source,
 		})
@@ -312,7 +350,7 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 		if address == nil {
 			continue
 		}
-		isOwning := ipcache.IPIdentityCache.Upsert(address.String(), nodeIP, n.EncryptionKey, nil, ipcache.Identity{
+		isOwning := m.ipcache.Upsert(address.String(), nodeIP, n.EncryptionKey, nil, ipcache.Identity{
 			ID:     identity.ReservedIdentityHealth,
 			Source: n.Source,
 		})
@@ -392,7 +430,7 @@ func (m *Manager) NodeDeleted(n nodeTypes.Node) {
 	}
 
 	for _, address := range entry.node.IPAddresses {
-		ipcache.IPIdentityCache.Delete(address.IP.String(), n.Source)
+		m.ipcache.Delete(address.IP.String(), n.Source)
 	}
 
 	m.metricNumNodes.Dec()

pkg/option/config.go
@@ -2030,6 +2030,17 @@ func (c *DaemonConfig) AlwaysAllowLocalhost() bool {
 	}
 }
 
+// RemoteNodeIdentitiesEnabled returns true if the remote-node identity feature
+// is enabled
+func (c *DaemonConfig) RemoteNodeIdentitiesEnabled() bool {
+	return c.EnableRemoteNodeIdentity
+}
+
+// NodeEncryptionEnabled returns true if node encryption is enabled
+func (c *DaemonConfig) NodeEncryptionEnabled() bool {
+	return c.EncryptNode
+}
+
 // IPv4Enabled returns true if IPv4 is enabled
 func (c *DaemonConfig) IPv4Enabled() bool {
 	return c.EnableIPv4

pkg/option/fake/config.go
@@ -26,3 +26,14 @@ func (f *Config) LocalClusterName() string {
 func (f *Config) CiliumNamespaceName() string {
 	return "kube-system"
 }
+
+// RemoteNodeIdentitiesEnabled returns true if the remote-node identity feature
+// is enabled
+func (f *Config) RemoteNodeIdentitiesEnabled() bool {
+	return true
+}
+
+// NodeEncryptionEnabled returns true if node encryption is enabled
+func (f *Config) NodeEncryptionEnabled() bool {
+	return true
+}

daemon/cmd/status_test.go
@@ -23,10 +23,12 @@ import (
 	. "github.com/cilium/cilium/api/v1/server/restapi/daemon"
 	"github.com/cilium/cilium/pkg/checker"
 	"github.com/cilium/cilium/pkg/datapath/fake"
+	fakeIPCache "github.com/cilium/cilium/pkg/ipcache/fake"
 	"github.com/cilium/cilium/pkg/mtu"
 	"github.com/cilium/cilium/pkg/node/manager"
 	"github.com/cilium/cilium/pkg/nodediscovery"
 	"github.com/cilium/cilium/pkg/option"
+	fakeConfig "github.com/cilium/cilium/pkg/option/fake"
 	cnitypes "github.com/cilium/cilium/plugins/cilium-cni/types"
 
 	"github.com/go-openapi/runtime/middleware"
@@ -47,7 +49,7 @@ func (g *GetNodesSuite) SetUpTest(c *C) {
 
 func (g *GetNodesSuite) SetUpSuite(c *C) {
 	var err error
-	nm, err = manager.NewManager("", fake.NewNodeHandler())
+	nm, err = manager.NewManager("", fake.NewNodeHandler(), fakeIPCache.NewIPCache(false), &fakeConfig.Config{})
 	c.Assert(err, IsNil)
 }
 

pkg/ipcache/fake/ipcache_test.go
@@ -1,0 +1,58 @@
+// Copyright 2018-2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package fake
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/cilium/cilium/pkg/checker"
+	"github.com/cilium/cilium/pkg/ipcache"
+	"github.com/cilium/cilium/pkg/source"
+
+	"gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+type fakeIPCacheSuite struct{}
+
+var _ = check.Suite(&fakeIPCacheSuite{})
+
+func (s *fakeIPCacheSuite) TestFakeIPCache(c *check.C) {
+	ipcacheMock := NewIPCache(true)
+
+	ipcacheMock.Upsert("1.1.1.1", net.ParseIP("2.2.2.2"), 0, nil, ipcache.Identity{ID: 1, Source: source.Local})
+	select {
+	case event := <-ipcacheMock.Events:
+		c.Assert(event, checker.DeepEquals, NodeEvent{EventUpsert, net.ParseIP("1.1.1.1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache upsert for IP 1.1.1.1")
+	}
+
+	ipcacheMock.Delete("1.1.1.1", source.Local)
+
+	select {
+	case event := <-ipcacheMock.Events:
+		c.Assert(event, checker.DeepEquals, NodeEvent{EventDelete, net.ParseIP("1.1.1.1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache delete for IP 1.1.1.1")
+	}
+}

pkg/node/manager/manager_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Authors of Cilium
+// Copyright 2018-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package manager
 
 import (
 	"fmt"
+	"net"
 	"sync"
 	"testing"
 	"time"
@@ -25,6 +26,8 @@ import (
 	"github.com/cilium/cilium/pkg/checker"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/datapath/fake"
+	"github.com/cilium/cilium/pkg/ipcache"
+	"github.com/cilium/cilium/pkg/node/addressing"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/source"
 
@@ -38,6 +41,43 @@ func Test(t *testing.T) {
 type managerTestSuite struct{}
 
 var _ = check.Suite(&managerTestSuite{})
+
+type configMock struct {
+	RemoteNodeIdentity bool
+	NodeEncryption     bool
+}
+
+func (c *configMock) RemoteNodeIdentitiesEnabled() bool {
+	return c.RemoteNodeIdentity
+}
+
+func (c *configMock) NodeEncryptionEnabled() bool {
+	return c.NodeEncryption
+}
+
+type nodeEvent struct {
+	event string
+	ip    net.IP
+}
+
+type ipcacheMock struct {
+	events chan nodeEvent
+}
+
+func newIPcacheMock() *ipcacheMock {
+	return &ipcacheMock{
+		events: make(chan nodeEvent, 1024),
+	}
+}
+
+func (i *ipcacheMock) Upsert(ip string, hostIP net.IP, hostKey uint8, k8sMeta *ipcache.K8sMetadata, newIdentity ipcache.Identity) bool {
+	i.events <- nodeEvent{"upsert", net.ParseIP(ip)}
+	return true
+}
+
+func (i *ipcacheMock) Delete(IP string, source source.Source) {
+	i.events <- nodeEvent{"delete", net.ParseIP(IP)}
+}
 
 type signalNodeHandler struct {
 	EnableNodeAddEvent                    bool
@@ -96,7 +136,7 @@ func (s *managerTestSuite) TestNodeLifecycle(c *check.C) {
 	dp.EnableNodeAddEvent = true
 	dp.EnableNodeUpdateEvent = true
 	dp.EnableNodeDeleteEvent = true
-	mngr, err := NewManager("test", dp)
+	mngr, err := NewManager("test", dp, newIPcacheMock(), &configMock{})
 	c.Assert(err, check.IsNil)
 
 	n1 := nodeTypes.Node{Name: "node1", Cluster: "c1"}
@@ -165,7 +205,7 @@ func (s *managerTestSuite) TestMultipleSources(c *check.C) {
 	dp.EnableNodeAddEvent = true
 	dp.EnableNodeUpdateEvent = true
 	dp.EnableNodeDeleteEvent = true
-	mngr, err := NewManager("test", dp)
+	mngr, err := NewManager("test", dp, newIPcacheMock(), &configMock{})
 	c.Assert(err, check.IsNil)
 	defer mngr.Close()
 
@@ -234,7 +274,7 @@ func (s *managerTestSuite) TestMultipleSources(c *check.C) {
 }
 
 func (s *managerTestSuite) BenchmarkUpdateAndDeleteCycle(c *check.C) {
-	mngr, err := NewManager("test", fake.NewNodeHandler())
+	mngr, err := NewManager("test", fake.NewNodeHandler(), newIPcacheMock(), &configMock{})
 	c.Assert(err, check.IsNil)
 	defer mngr.Close()
 
@@ -252,7 +292,7 @@ func (s *managerTestSuite) BenchmarkUpdateAndDeleteCycle(c *check.C) {
 }
 
 func (s *managerTestSuite) TestClusterSizeDependantInterval(c *check.C) {
-	mngr, err := NewManager("test", fake.NewNodeHandler())
+	mngr, err := NewManager("test", fake.NewNodeHandler(), newIPcacheMock(), &configMock{})
 	c.Assert(err, check.IsNil)
 	defer mngr.Close()
 
@@ -277,7 +317,7 @@ func (s *managerTestSuite) TestBackgroundSync(c *check.C) {
 
 	signalNodeHandler := newSignalNodeHandler()
 	signalNodeHandler.EnableNodeValidateImplementationEvent = true
-	mngr, err := NewManager("test", signalNodeHandler)
+	mngr, err := NewManager("test", signalNodeHandler, newIPcacheMock(), &configMock{})
 	c.Assert(err, check.IsNil)
 	defer mngr.Close()
 
@@ -308,4 +348,180 @@ func (s *managerTestSuite) TestBackgroundSync(c *check.C) {
 	}
 
 	allNodeValidateCallsReceived.Wait()
+}
+
+func (s *managerTestSuite) TestIpcache(c *check.C) {
+	ipcacheMock := newIPcacheMock()
+	mngr, err := NewManager("test", newSignalNodeHandler(), ipcacheMock, &configMock{})
+	c.Assert(err, check.IsNil)
+	defer mngr.Close()
+
+	n1 := nodeTypes.Node{
+		Name:    "node1",
+		Cluster: "c1",
+		IPAddresses: []nodeTypes.Address{
+			{Type: addressing.NodeCiliumInternalIP, IP: net.ParseIP("1.1.1.1")},
+			{Type: addressing.NodeInternalIP, IP: net.ParseIP("2.2.2.2")},
+			{Type: addressing.NodeExternalIP, IP: net.ParseIP("f00d::1")},
+		},
+	}
+	mngr.NodeUpdated(n1)
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "upsert", ip: net.ParseIP("1.1.1.1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache upsert for IP 1.1.1.1")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Errorf("unexected ipcache interaction %+v", event)
+	default:
+	}
+
+	mngr.NodeDeleted(n1)
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "delete", ip: net.ParseIP("1.1.1.1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache delete for IP 1.1.1.1")
+	}
+}
+
+func (s *managerTestSuite) TestRemoteNodeIdentities(c *check.C) {
+	ipcacheMock := newIPcacheMock()
+	mngr, err := NewManager("test", newSignalNodeHandler(), ipcacheMock, &configMock{RemoteNodeIdentity: true})
+	c.Assert(err, check.IsNil)
+	defer mngr.Close()
+
+	n1 := nodeTypes.Node{
+		Name:    "node1",
+		Cluster: "c1",
+		IPAddresses: []nodeTypes.Address{
+			{Type: addressing.NodeCiliumInternalIP, IP: net.ParseIP("1.1.1.1")},
+			{Type: addressing.NodeInternalIP, IP: net.ParseIP("2.2.2.2")},
+			{Type: addressing.NodeExternalIP, IP: net.ParseIP("f00d::1")},
+		},
+	}
+	mngr.NodeUpdated(n1)
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "upsert", ip: net.ParseIP("1.1.1.1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache upsert for IP 1.1.1.1")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "upsert", ip: net.ParseIP("2.2.2.2")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache upsert for IP 2.2.2.2")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "upsert", ip: net.ParseIP("f00d::1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache upsert for IP f00d::1")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Errorf("unexected ipcache interaction %+v", event)
+	default:
+	}
+
+	mngr.NodeDeleted(n1)
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "delete", ip: net.ParseIP("1.1.1.1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache delete for IP 1.1.1.1")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "delete", ip: net.ParseIP("2.2.2.2")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache delete for IP 2.2.2.2")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "delete", ip: net.ParseIP("f00d::1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache delete for IP f00d::1")
+	}
+}
+
+func (s *managerTestSuite) TestNodeEncryption(c *check.C) {
+	ipcacheMock := newIPcacheMock()
+	mngr, err := NewManager("test", newSignalNodeHandler(), ipcacheMock, &configMock{NodeEncryption: true})
+	c.Assert(err, check.IsNil)
+	defer mngr.Close()
+
+	n1 := nodeTypes.Node{
+		Name:    "node1",
+		Cluster: "c1",
+		IPAddresses: []nodeTypes.Address{
+			{Type: addressing.NodeCiliumInternalIP, IP: net.ParseIP("1.1.1.1")},
+			{Type: addressing.NodeInternalIP, IP: net.ParseIP("2.2.2.2")},
+			{Type: addressing.NodeExternalIP, IP: net.ParseIP("f00d::1")},
+		},
+	}
+	mngr.NodeUpdated(n1)
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "upsert", ip: net.ParseIP("1.1.1.1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache upsert for IP 1.1.1.1")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "upsert", ip: net.ParseIP("2.2.2.2")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache upsert for IP 2.2.2.2")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "upsert", ip: net.ParseIP("f00d::1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache upsert for IP f00d::1")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Errorf("unexected ipcache interaction %+v", event)
+	default:
+	}
+
+	mngr.NodeDeleted(n1)
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "delete", ip: net.ParseIP("1.1.1.1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache delete for IP 1.1.1.1")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "delete", ip: net.ParseIP("2.2.2.2")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache delete for IP 2.2.2.2")
+	}
+
+	select {
+	case event := <-ipcacheMock.events:
+		c.Assert(event, checker.DeepEquals, nodeEvent{event: "delete", ip: net.ParseIP("f00d::1")})
+	case <-time.After(5 * time.Second):
+		c.Errorf("timeout while waiting for ipcache delete for IP f00d::1")
+	}
 }