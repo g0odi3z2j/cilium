g0odi3z2j/cilium

commit 1717ededdb43969174ffd2c53f145459b026f15e

operator: split operator-only options into separate package
Split out all options which are specific to cilium-operator into the
separate github.com/cilium/cilium/operator/option package.

In the long term, the goal is to separate options which are common to
all Cilium binaries from options applicable to only a specific binary
(or a subset of them). This should also help to reduce unnecessary
dependencies.

Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

operator/main.go
@@ -21,6 +21,7 @@ import (
 	"os/signal"
 
 	operatorMetrics "github.com/cilium/cilium/operator/metrics"
+	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/components"
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/k8s"
@@ -109,24 +110,23 @@ func kvstoreEnabled() bool {
 	}
 
 	return option.Config.IdentityAllocationMode == option.IdentityAllocationModeKVstore ||
-		option.Config.SyncK8sServices ||
-		option.Config.SyncK8sNodes
+		operatorOption.Config.SyncK8sServices ||
+		operatorOption.Config.SyncK8sNodes
 }
 
 func getAPIServerAddr() []string {
-	if option.Config.OperatorAPIServeAddr == "" {
+	if operatorOption.Config.OperatorAPIServeAddr == "" {
 		return []string{fmt.Sprintf("127.0.0.1:%d", apiServerPort), fmt.Sprintf("[::1]:%d", apiServerPort)}
 	}
-	return []string{option.Config.OperatorAPIServeAddr}
+	return []string{operatorOption.Config.OperatorAPIServeAddr}
 }
 
 func runOperator(cmd *cobra.Command) {
-
 	log.Infof("Cilium Operator %s", version.Version)
 	k8sInitDone := make(chan struct{})
 	go startServer(shutdownSignal, k8sInitDone, getAPIServerAddr()...)
 
-	if option.Config.EnableMetrics {
+	if operatorOption.Config.EnableMetrics {
 		operatorMetrics.Register()
 	}
 
@@ -153,7 +153,7 @@ func runOperator(cmd *cobra.Command) {
 	// etcd from reaching out kube-dns in EKS.
 	if option.Config.DisableCiliumEndpointCRD {
 		log.Infof("KubeDNS unmanaged pods controller disabled as %q option is set to 'disabled' in Cilium ConfigMap", option.DisableCiliumEndpointCRDName)
-	} else if option.Config.UnmanagedPodWatcherInterval != 0 {
+	} else if operatorOption.Config.UnmanagedPodWatcherInterval != 0 {
 		enableUnmanagedKubeDNSController()
 	}
 
@@ -198,7 +198,7 @@ func runOperator(cmd *cobra.Command) {
 	}
 
 	if kvstoreEnabled() {
-		if option.Config.SyncK8sServices {
+		if operatorOption.Config.SyncK8sServices {
 			startSynchronizingServices()
 		}
 
@@ -207,7 +207,7 @@ func runOperator(cmd *cobra.Command) {
 			"kvstore": option.Config.KVStore,
 			"address": option.Config.KVStoreOpt[fmt.Sprintf("%s.address", option.Config.KVStore)],
 		})
-		if option.Config.SyncK8sServices {
+		if operatorOption.Config.SyncK8sServices {
 			// If K8s is enabled we can do the service translation automagically by
 			// looking at services from k8s and retrieve the service IP from that.
 			// This makes cilium to not depend on kube dns to interact with etcd
@@ -269,7 +269,7 @@ func runOperator(cmd *cobra.Command) {
 			scopedLog.WithError(err).Fatal("Unable to setup kvstore")
 		}
 
-		if option.Config.SyncK8sNodes {
+		if operatorOption.Config.SyncK8sNodes {
 			if err := runNodeWatcher(nodeManager); err != nil {
 				log.WithError(err).Error("Unable to setup node watcher")
 			}
@@ -286,16 +286,16 @@ func runOperator(cmd *cobra.Command) {
 
 		startManagingK8sIdentities()
 
-		if option.Config.IdentityGCInterval != 0 {
+		if operatorOption.Config.IdentityGCInterval != 0 {
 			go startCRDIdentityGC()
 		}
 	case option.IdentityAllocationModeKVstore:
-		if option.Config.IdentityGCInterval != 0 {
+		if operatorOption.Config.IdentityGCInterval != 0 {
 			startKvstoreIdentityGC()
 		}
 	}
 
-	if option.Config.EnableCEPGC && option.Config.EndpointGCInterval != 0 {
+	if operatorOption.Config.EnableCEPGC && operatorOption.Config.EndpointGCInterval != 0 {
 		enableCiliumEndpointSyncGC()
 	}
 

operator/ccnp_event.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"time"
 
+	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
@@ -25,7 +26,6 @@ import (
 	k8sversion "github.com/cilium/cilium/pkg/k8s/version"
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	"github.com/cilium/cilium/pkg/metrics"
-	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy/groups"
 
 	"k8s.io/api/core/v1"
@@ -67,7 +67,7 @@ func enableCCNPWatcher() error {
 			return err
 		}
 
-		ccnpStatusMgr = k8s.NewCCNPStatusEventHandler(ccnpSharedStore, ccnpStore, option.Config.CNPStatusUpdateInterval)
+		ccnpStatusMgr = k8s.NewCCNPStatusEventHandler(ccnpSharedStore, ccnpStore, operatorOption.Config.CNPStatusUpdateInterval)
 
 		go ccnpStatusMgr.WatchForCCNPStatusEvents()
 	}

operator/flags.go
@@ -19,6 +19,7 @@ import (
 	"time"
 
 	operatorMetrics "github.com/cilium/cilium/operator/metrics"
+	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/option"
 
@@ -31,17 +32,17 @@ func init() {
 
 	flags := rootCmd.Flags()
 
-	flags.Int(option.AWSClientBurstDeprecated, defaults.IPAMAPIBurst, "")
-	flags.MarkDeprecated(option.AWSClientBurstDeprecated, fmt.Sprintf("please use --%s", option.IPAMAPIBurst))
+	flags.Int(operatorOption.AWSClientBurstDeprecated, defaults.IPAMAPIBurst, "")
+	flags.MarkDeprecated(operatorOption.AWSClientBurstDeprecated, fmt.Sprintf("please use --%s", operatorOption.IPAMAPIBurst))
 
-	flags.Int(option.IPAMAPIBurst, defaults.IPAMAPIBurst, "Upper burst limit when accessing external APIs")
-	option.BindEnv(option.IPAMAPIBurst)
+	flags.Int(operatorOption.IPAMAPIBurst, defaults.IPAMAPIBurst, "Upper burst limit when accessing external APIs")
+	option.BindEnv(operatorOption.IPAMAPIBurst)
 
-	flags.Float64(option.AWSClientQPSLimitDeprecated, defaults.IPAMAPIQPSLimit, "")
-	flags.MarkDeprecated(option.AWSClientQPSLimitDeprecated, fmt.Sprintf("please use --%s", option.IPAMAPIQPSLimit))
+	flags.Float64(operatorOption.AWSClientQPSLimitDeprecated, defaults.IPAMAPIQPSLimit, "")
+	flags.MarkDeprecated(operatorOption.AWSClientQPSLimitDeprecated, fmt.Sprintf("please use --%s", operatorOption.IPAMAPIQPSLimit))
 
-	flags.Float64(option.IPAMAPIQPSLimit, defaults.IPAMAPIQPSLimit, "Queries per second limit when accessing external IPAM APIs")
-	option.BindEnv(option.IPAMAPIQPSLimit)
+	flags.Float64(operatorOption.IPAMAPIQPSLimit, defaults.IPAMAPIQPSLimit, "Queries per second limit when accessing external IPAM APIs")
+	option.BindEnv(operatorOption.IPAMAPIQPSLimit)
 
 	flags.String(option.AzureSubscriptionID, "", "Subscription ID to access Azure API")
 	option.BindEnvWithLegacyEnvFallback(option.AzureSubscriptionID, "AZURE_SUBSCRIPTION_ID")
@@ -66,11 +67,13 @@ func init() {
 		option.ENITags, "ENI tags in the form of k1=v1 (multiple k/v pairs can be passed by repeating the CLI flag)")
 	option.BindEnv(option.ENITags)
 
-	flags.StringToStringVar(&option.Config.IPAMSubnetsTags, option.IPAMSubnetsTags, option.Config.IPAMSubnetsTags,
+	flags.StringToStringVar(&operatorOption.Config.IPAMSubnetsTags, operatorOption.IPAMSubnetsTags, operatorOption.Config.IPAMSubnetsTags,
 		"Subnets tags in the form of k1=v1,k2=v2 (multiple k/v pairs can also be passed by repeating the CLI flag")
-	option.BindEnv(option.IPAMSubnetsTags)
-	flags.StringSliceVar(&option.Config.IPAMSubnetsIDs, option.IPAMSubnetsIDs, option.Config.IPAMSubnetsIDs, "Subnets IDs (separated by commas)")
-	option.BindEnv(option.IPAMSubnetsIDs)
+	option.BindEnv(operatorOption.IPAMSubnetsTags)
+
+	flags.StringSliceVar(&operatorOption.Config.IPAMSubnetsIDs, operatorOption.IPAMSubnetsIDs, operatorOption.Config.IPAMSubnetsIDs,
+		"Subnets IDs (separated by commas)")
+	option.BindEnv(operatorOption.IPAMSubnetsIDs)
 
 	flags.Int64(option.ENIParallelWorkersDeprecated, defaults.ParallelAllocWorkers, "")
 	flags.MarkDeprecated(option.ENIParallelWorkersDeprecated, fmt.Sprintf("please use --%s", option.ParallelAllocWorkers))
@@ -97,20 +100,20 @@ func init() {
 	option.BindEnv(option.ConfigDir)
 
 	// Deprecated, remove in 1.9
-	flags.Bool(option.EnableCCNPNodeStatusGC, true, "Enable CiliumClusterwideNetworkPolicy Status garbage collection for nodes which have been removed from the cluster")
-	option.BindEnv(option.EnableCCNPNodeStatusGC)
-	flags.MarkDeprecated(option.EnableCCNPNodeStatusGC, fmt.Sprintf("Please use %s=0 to disable CCNP Status GC", option.CNPNodeStatusGCInterval))
+	flags.Bool(operatorOption.EnableCCNPNodeStatusGC, true, "Enable CiliumClusterwideNetworkPolicy Status garbage collection for nodes which have been removed from the cluster")
+	option.BindEnv(operatorOption.EnableCCNPNodeStatusGC)
+	flags.MarkDeprecated(operatorOption.EnableCCNPNodeStatusGC, fmt.Sprintf("Please use %s=0 to disable CCNP Status GC", operatorOption.CNPNodeStatusGCInterval))
 
 	// Deprecated, remove in 1.9
-	flags.Bool(option.EnableCNPNodeStatusGC, true, "Enable CiliumNetworkPolicy Status garbage collection for nodes which have been removed from the cluster")
-	option.BindEnv(option.EnableCNPNodeStatusGC)
-	flags.MarkDeprecated(option.EnableCNPNodeStatusGC, fmt.Sprintf("Please use %s=0 to disable CNP Status GC", option.CNPNodeStatusGCInterval))
+	flags.Bool(operatorOption.EnableCNPNodeStatusGC, true, "Enable CiliumNetworkPolicy Status garbage collection for nodes which have been removed from the cluster")
+	option.BindEnv(operatorOption.EnableCNPNodeStatusGC)
+	flags.MarkDeprecated(operatorOption.EnableCNPNodeStatusGC, fmt.Sprintf("Please use %s=0 to disable CNP Status GC", operatorOption.CNPNodeStatusGCInterval))
 
-	flags.Duration(option.CNPNodeStatusGCInterval, 2*time.Minute, "GC interval for nodes which have been removed from the cluster in CiliumNetworkPolicy Status")
-	option.BindEnv(option.CNPNodeStatusGCInterval)
+	flags.Duration(operatorOption.CNPNodeStatusGCInterval, 2*time.Minute, "GC interval for nodes which have been removed from the cluster in CiliumNetworkPolicy Status")
+	option.BindEnv(operatorOption.CNPNodeStatusGCInterval)
 
-	flags.Duration(option.CNPStatusUpdateInterval, 1*time.Second, "Interval between CNP status updates sent to the k8s-apiserver per-CNP")
-	option.BindEnv(option.CNPStatusUpdateInterval)
+	flags.Duration(operatorOption.CNPStatusUpdateInterval, 1*time.Second, "Interval between CNP status updates sent to the k8s-apiserver per-CNP")
+	option.BindEnv(operatorOption.CNPStatusUpdateInterval)
 
 	flags.BoolP(option.DebugArg, "D", false, "Enable debugging mode")
 	option.BindEnv(option.DebugArg)
@@ -124,27 +127,27 @@ func init() {
 	option.BindEnv(option.DisableCiliumEndpointCRDName)
 
 	// Deprecated, remove in 1.9
-	flags.Bool(option.EnableCEPGC, true, "Enable CiliumEndpoint garbage collector")
-	option.BindEnv(option.EnableCEPGC)
-	flags.MarkDeprecated(option.EnableCEPGC, fmt.Sprintf("Please use %s=0 to disable CEP GC", option.EndpointGCInterval))
+	flags.Bool(operatorOption.EnableCEPGC, true, "Enable CiliumEndpoint garbage collector")
+	option.BindEnv(operatorOption.EnableCEPGC)
+	flags.MarkDeprecated(operatorOption.EnableCEPGC, fmt.Sprintf("Please use %s=0 to disable CEP GC", operatorOption.EndpointGCInterval))
 
-	flags.Duration(option.EndpointGCInterval, 30*time.Minute, "GC interval for cilium endpoints")
-	option.BindEnv(option.EndpointGCInterval)
+	flags.Duration(operatorOption.EndpointGCInterval, 30*time.Minute, "GC interval for cilium endpoints")
+	option.BindEnv(operatorOption.EndpointGCInterval)
 
-	flags.Bool(option.EnableMetrics, false, "Enable Prometheus metrics")
-	option.BindEnv(option.EnableMetrics)
+	flags.Bool(operatorOption.EnableMetrics, false, "Enable Prometheus metrics")
+	option.BindEnv(operatorOption.EnableMetrics)
 
 	flags.String(option.IPAM, option.IPAMHostScopeLegacy, "Backend to use for IPAM")
 	option.BindEnv(option.IPAM)
 
-	flags.Duration(option.IdentityHeartbeatTimeout, 15*time.Minute, "Timeout after which identity expires on lack of heartbeat")
-	option.BindEnv(option.IdentityHeartbeatTimeout)
+	flags.Duration(operatorOption.IdentityHeartbeatTimeout, 15*time.Minute, "Timeout after which identity expires on lack of heartbeat")
+	option.BindEnv(operatorOption.IdentityHeartbeatTimeout)
 
 	flags.String(option.IdentityAllocationMode, option.IdentityAllocationModeKVstore, "Method to use for identity allocation")
 	option.BindEnv(option.IdentityAllocationMode)
 
-	flags.Duration(option.IdentityGCInterval, defaults.KVstoreLeaseTTL, "GC interval for security identities")
-	option.BindEnv(option.IdentityGCInterval)
+	flags.Duration(operatorOption.IdentityGCInterval, defaults.KVstoreLeaseTTL, "GC interval for security identities")
+	option.BindEnv(operatorOption.IdentityGCInterval)
 
 	flags.String(option.KVStore, "", "Key-value store type")
 	option.BindEnv(option.KVStore)
@@ -168,34 +171,34 @@ func init() {
 	flags.String(option.K8sKubeConfigPath, "", "Absolute path of the kubernetes kubeconfig file")
 	option.BindEnv(option.K8sKubeConfigPath)
 
-	flags.Duration(option.NodesGCInterval, 2*time.Minute, "GC interval for nodes store in the kvstore")
-	option.BindEnv(option.NodesGCInterval)
+	flags.Duration(operatorOption.NodesGCInterval, 2*time.Minute, "GC interval for nodes store in the kvstore")
+	option.BindEnv(operatorOption.NodesGCInterval)
 
-	flags.String(option.OperatorPrometheusServeAddr, ":6942", "Address to serve Prometheus metrics")
-	option.BindEnv(option.OperatorPrometheusServeAddr)
+	flags.String(operatorOption.OperatorPrometheusServeAddr, ":6942", "Address to serve Prometheus metrics")
+	option.BindEnv(operatorOption.OperatorPrometheusServeAddr)
 
-	flags.String(option.OperatorAPIServeAddr, "localhost:9234", "Address to serve API requests")
-	option.BindEnv(option.OperatorAPIServeAddr)
+	flags.String(operatorOption.OperatorAPIServeAddr, "localhost:9234", "Address to serve API requests")
+	option.BindEnv(operatorOption.OperatorAPIServeAddr)
 
-	flags.Bool(option.SyncK8sServices, true, "Synchronize Kubernetes services to kvstore")
-	option.BindEnv(option.SyncK8sServices)
+	flags.Bool(operatorOption.SyncK8sServices, true, "Synchronize Kubernetes services to kvstore")
+	option.BindEnv(operatorOption.SyncK8sServices)
 
-	flags.Bool(option.SyncK8sNodes, true, "Synchronize Kubernetes nodes to kvstore and perform CNP GC")
-	option.BindEnv(option.SyncK8sNodes)
+	flags.Bool(operatorOption.SyncK8sNodes, true, "Synchronize Kubernetes nodes to kvstore and perform CNP GC")
+	option.BindEnv(operatorOption.SyncK8sNodes)
 
-	flags.Int(option.UnmanagedPodWatcherInterval, 15, "Interval to check for unmanaged kube-dns pods (0 to disable)")
-	option.BindEnv(option.UnmanagedPodWatcherInterval)
+	flags.Int(operatorOption.UnmanagedPodWatcherInterval, 15, "Interval to check for unmanaged kube-dns pods (0 to disable)")
+	option.BindEnv(operatorOption.UnmanagedPodWatcherInterval)
 
 	flags.Bool(option.Version, false, "Print version information")
 	option.BindEnv(option.Version)
 
 	// Deprecated, remove in 1.9
 	flags.Uint16Var(&apiServerPort, "api-server-port", 9234, "Port on which the operator should serve API requests")
-	flags.MarkDeprecated("api-server-port", fmt.Sprintf("Please use %s instead", option.OperatorAPIServeAddr))
+	flags.MarkDeprecated("api-server-port", fmt.Sprintf("Please use %s instead", operatorOption.OperatorAPIServeAddr))
 
 	// Deprecated, remove in 1.9
 	flags.StringVar(&operatorMetrics.Address, "metrics-address", ":6942", "Address to serve Prometheus metrics")
-	flags.MarkDeprecated("metrics-address", fmt.Sprintf("Please use %s instead", option.OperatorPrometheusServeAddr))
+	flags.MarkDeprecated("metrics-address", fmt.Sprintf("Please use %s instead", operatorOption.OperatorPrometheusServeAddr))
 
 	flags.String(option.CMDRef, "", "Path to cmdref output directory")
 	flags.MarkHidden(option.CMDRef)

operator/identity_gc.go
@@ -17,15 +17,15 @@ package main
 import (
 	"time"
 
+	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/allocator"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/kvstore"
 	kvstoreallocator "github.com/cilium/cilium/pkg/kvstore/allocator"
-	"github.com/cilium/cilium/pkg/option"
 )
 
 func startKvstoreIdentityGC() {
-	log.Infof("Starting kvstore identity garbage collector with %s interval...", option.Config.IdentityGCInterval)
+	log.Infof("Starting kvstore identity garbage collector with %s interval...", operatorOption.Config.IdentityGCInterval)
 	backend, err := kvstoreallocator.NewKVStoreBackend(cache.IdentitiesPath, "", nil, kvstore.Client())
 	if err != nil {
 		log.WithError(err).Fatal("Unable to initialize kvstore backend for identity allocation")
@@ -42,7 +42,7 @@ func startKvstoreIdentityGC() {
 				keysToDelete = keysToDelete2
 			}
 
-			<-time.After(option.Config.IdentityGCInterval)
+			<-time.After(operatorOption.Config.IdentityGCInterval)
 		}
 	}()
 }

operator/k8s_cep_gc.go
@@ -18,10 +18,10 @@ import (
 	"context"
 	"time"
 
+	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/option"
 
 	"github.com/sirupsen/logrus"
 	core_v1 "k8s.io/api/core/v1"
@@ -53,11 +53,11 @@ func enableCiliumEndpointSyncGC() {
 	// this dummy manager is needed only to add this controller to the global list
 	controller.NewManager().UpdateController(controllerName,
 		controller.ControllerParams{
-			RunInterval: option.Config.EndpointGCInterval,
+			RunInterval: operatorOption.Config.EndpointGCInterval,
 			DoFunc: func(ctx context.Context) error {
 				var (
 					listOpts = meta_v1.ListOptions{Limit: 10}
-					loopStop = time.Now().Add(option.Config.EndpointGCInterval)
+					loopStop = time.Now().Add(operatorOption.Config.EndpointGCInterval)
 				)
 
 				pods, err := k8s.Client().CoreV1().Pods("").List(ctx, meta_v1.ListOptions{})

operator/k8s_identity.go
@@ -18,12 +18,12 @@ import (
 	"context"
 	"time"
 
+	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/option"
 
 	"github.com/sirupsen/logrus"
 	"k8s.io/api/core/v1"
@@ -80,7 +80,7 @@ nextIdentity:
 		}
 
 		for _, heartbeat := range identity.Status.Nodes {
-			if time.Since(heartbeat.Time) < option.Config.IdentityHeartbeatTimeout {
+			if time.Since(heartbeat.Time) < operatorOption.Config.IdentityHeartbeatTimeout {
 				continue nextIdentity
 			}
 		}
@@ -94,11 +94,11 @@ nextIdentity:
 }
 
 func startCRDIdentityGC() {
-	log.Infof("Starting CRD identity garbage collector with %s interval...", option.Config.IdentityGCInterval)
+	log.Infof("Starting CRD identity garbage collector with %s interval...", operatorOption.Config.IdentityGCInterval)
 
 	controller.NewManager().UpdateController("crd-identity-gc",
 		controller.ControllerParams{
-			RunInterval: option.Config.IdentityGCInterval,
+			RunInterval: operatorOption.Config.IdentityGCInterval,
 			DoFunc: func(ctx context.Context) error {
 				identityGCIteration()
 				return nil

operator/k8s_node.go
@@ -21,6 +21,7 @@ import (
 	"sync"
 	"time"
 
+	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/ipam"
 	"github.com/cilium/cilium/pkg/k8s"
@@ -141,11 +142,11 @@ func runNodeWatcher(nodeManager *ipam.NodeManager) error {
 
 	}()
 
-	if option.Config.EnableCNPNodeStatusGC && option.Config.CNPNodeStatusGCInterval != 0 {
+	if operatorOption.Config.EnableCNPNodeStatusGC && operatorOption.Config.CNPNodeStatusGCInterval != 0 {
 		go runCNPNodeStatusGC("cnp-node-gc", false, ciliumNodeStore)
 	}
 
-	if option.Config.EnableCCNPNodeStatusGC && option.Config.CNPNodeStatusGCInterval != 0 {
+	if operatorOption.Config.EnableCCNPNodeStatusGC && operatorOption.Config.CNPNodeStatusGCInterval != 0 {
 		go runCNPNodeStatusGC("ccnp-node-gc", true, ciliumNodeStore)
 	}
 
@@ -168,9 +169,9 @@ func runCNPNodeStatusGC(name string, clusterwide bool, ciliumNodeStore *store.Sh
 
 	controller.NewManager().UpdateController(name,
 		controller.ControllerParams{
-			RunInterval: option.Config.CNPNodeStatusGCInterval,
+			RunInterval: operatorOption.Config.CNPNodeStatusGCInterval,
 			DoFunc: func(ctx context.Context) error {
-				lastRun := time.Now().Add(-option.Config.NodesGCInterval)
+				lastRun := time.Now().Add(-operatorOption.Config.NodesGCInterval)
 				k8sCapabilities := k8sversion.Capabilities()
 				continueID := ""
 				wg := sync.WaitGroup{}

operator/k8s_pod_controller.go
@@ -19,9 +19,9 @@ import (
 	"fmt"
 	"time"
 
+	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
-	"github.com/cilium/cilium/pkg/option"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -39,7 +39,7 @@ var (
 func enableUnmanagedKubeDNSController() {
 	controller.NewManager().UpdateController("restart-unmanaged-kube-dns",
 		controller.ControllerParams{
-			RunInterval: time.Duration(option.Config.UnmanagedPodWatcherInterval) * time.Second,
+			RunInterval: time.Duration(operatorOption.Config.UnmanagedPodWatcherInterval) * time.Second,
 			DoFunc: func(ctx context.Context) error {
 				for podName, lastRestart := range lastPodRestart {
 					if time.Since(lastRestart) > 2*minimalPodRestartInterval {

commit d23d92e3b5b95b1afd24148ec4db3d082dbdec46

operator: Fix operator flags
The operator specific flags were not populated

Fixes: 1717ededd ("operator: split operator-only options into separate package")
Signed-off-by: Thomas Graf <thomas@cilium.io>

operator/main.go
@@ -72,6 +72,7 @@ var (
 func initEnv() {
 	// Prepopulate option.Config with options from CLI.
 	option.Config.Populate()
+	operatorOption.Config.Populate()
 
 	// add hooks after setting up metrics in the option.Confog
 	logging.DefaultLogger.Hooks.Add(metrics.NewLoggingHook(components.CiliumOperatortName))