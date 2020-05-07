g0odi3z2j/cilium

commit 18eba2dd11b432db72abe141c5fc0bd3bc2ed741

coccinelle: Use Docker image to patch issues
Developers likely won't have Coccinelle installed on their host so we
should allow them to use the Coccinelle Docker image to fix any issue
found by the GitHub action.

Signed-off-by: Paul Chaignon <paul@cilium.io>

contrib/coccinelle/aligned.cocci
@@ -58,5 +58,8 @@ cnt += 1
 @@
 
 if cnt > 0:
-  print "Use the following command to fix the above issues:"
-  print "spatch --sp-file contrib/coccinelle/aligned.cocci --include-headers --very-quiet --in-place bpf/\n"
+  print """Use the following command to fix the above issues:
+docker run --rm --user 1000 --workdir /workspace -v `pwd`:/workspace \\
+    -it coccicheck spatch --sp-file contrib/coccinelle/aligned.cocci \\
+    --include-headers --very-quiet --in-place bpf/\n
+"""

contrib/coccinelle/const.cocci
@@ -69,5 +69,8 @@ cnt += 1
 @@
 
 if cnt > 0:
-  print "Use the following command to fix the above issues:"
-  print "spatch --sp-file contrib/coccinelle/const.cocci --include-headers --very-quiet --in-place bpf/\n"
+  print """Use the following command to fix the above issues:
+docker run --rm --user 1000 --workdir /workspace -v `pwd`:/workspace \\
+    -it coccicheck spatch --sp-file contrib/coccinelle/const.cocci   \\
+    --include-headers --very-quiet --in-place bpf/\n
+"""

.github/workflows/coccicheck.yaml
@@ -10,3 +10,5 @@ jobs:
         steps:
             - uses: actions/checkout@v2
             - uses: docker://cilium/coccicheck:latest
+              with:
+                entrypoint: ./contrib/coccinelle/check-cocci.sh

contrib/coccinelle/Dockerfile
@@ -15,5 +15,3 @@ RUN apk add -t .build_apks curl autoconf automake gcc libc-dev ocaml ocaml-dev o
     cd .. && rm -r coccinelle-$COCCINELLE_VERSION && \
     strip `which spatch` && \
     apk del .build_apks
-
-ENTRYPOINT ["./contrib/coccinelle/check-cocci.sh"]

commit 670f076574c8b30f20cf20a8d692876f6e6b5a50

coccinelle: Fix Docker image name printed on errors
When Coccinelle scripts find code that needs to be changed, they print an
error message that includes the command to fix the code. That command
relies on a Docker image to avoid having to install Coccinelle. The name
of the Docker image was however incorrect. This commit fixes it.

Fixes: 18eba2d ("coccinelle: Use Docker image to patch issues")
Signed-off-by: Paul Chaignon <paul@cilium.io>

contrib/coccinelle/aligned.cocci
@@ -59,7 +59,7 @@ cnt += 1
 
 if cnt > 0:
   print """Use the following command to fix the above issues:
-docker run --rm --user 1000 --workdir /workspace -v `pwd`:/workspace \\
-    -it coccicheck spatch --sp-file contrib/coccinelle/aligned.cocci \\
+docker run --rm --user 1000 --workdir /workspace -v `pwd`:/workspace                  \\
+    -it docker.io/cilium/coccicheck spatch --sp-file contrib/coccinelle/aligned.cocci \\
     --include-headers --very-quiet --in-place bpf/\n
 """

contrib/coccinelle/const.cocci
@@ -65,7 +65,7 @@ cnt += 1
 
 if cnt > 0:
   print """Use the following command to fix the above issues:
-docker run --rm --user 1000 --workdir /workspace -v `pwd`:/workspace \\
-    -it coccicheck spatch --sp-file contrib/coccinelle/const.cocci   \\
+docker run --rm --user 1000 --workdir /workspace -v `pwd`:/workspace                \\
+    -it docker.io/cilium/coccicheck spatch --sp-file contrib/coccinelle/const.cocci \\
     --include-headers --very-quiet --in-place bpf/\n
 """