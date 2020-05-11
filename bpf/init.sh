g0odi3z2j/cilium

commit 6d69a62b877582db929c6e310cfce2f79e54f57c

bpf: probe and emit kernel hz to node_config
In order to use and work with jiffies64() BPF helper, we need to know
the HZ value that the kernel is operating in. Doing something like a
sysconf(_SC_CLK_TCK) from user space to get the tick frequency won't
work since this is the user space HZ value whereas BPF programs run
in kernel space, so we need the kernel HZ. It is not exposed directly
and we cannot assume that the kernel config is always exposed by
distros, so as an alternative /proc/timer_list can be used. This work
adds a smal tool cilium-probe-kernel-hz which probes and sanity checks
the HZ value and emits a define KERNEL_HZ which can be used by the
BPF prog.

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/init.sh
@@ -437,6 +437,8 @@ case "${MODE}" in
         ;;
 esac
 
+cilium-probe-kernel-hz >> $RUNDIR/globals/node_config.h
+
 # node_config.h header generation
 case "${MODE}" in
 	*)

bpf/Makefile
@@ -10,7 +10,7 @@ SUBDIRS = sockops
 BPF_SIMPLE = bpf_network.o bpf_alignchecker.o bpf_hostdev_ingress.o
 BPF = bpf_lxc.o bpf_overlay.o bpf_sock.o bpf_netdev.o bpf_xdp.o $(BPF_SIMPLE)
 
-TARGET=cilium-map-migrate
+TARGET=cilium-map-migrate cilium-probe-kernel-hz
 
 include ./Makefile.bpf
 

bpf/cilium-probe-kernel-hz.c
@@ -1,0 +1,175 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (C) 2020 Authors of Cilium */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sched.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <sys/resource.h>
+
+struct cpu_jiffies {
+	uint64_t *jiffies;
+	uint32_t cpus;
+};
+
+static const uint64_t kernel_hz[] = { 100, 250, 300, 1000 };
+
+#define abs(x)	({ x < 0 ? -x : x; })
+
+static int pin_to_cpu(int cpu)
+{
+	cpu_set_t set;
+
+	CPU_ZERO(&set);
+	CPU_SET(cpu, &set);
+
+	return sched_setaffinity(0, sizeof(set), &set);
+}
+
+static int fix_priority(void)
+{
+	struct sched_param sp = {
+		.sched_priority = sched_get_priority_max(SCHED_FIFO),
+	};
+
+	return sched_setscheduler(0, SCHED_FIFO, &sp);
+}
+
+static FILE *timer_list_open(void)
+{
+	return fopen("/proc/timer_list", "r");
+}
+
+static void timer_list_close(FILE *fp)
+{
+	fclose(fp);
+}
+
+static int prep_kern_jiffies(struct cpu_jiffies *before,
+			     struct cpu_jiffies *after)
+{
+	uint64_t jiffies = 0;
+	char buff[512];
+	int cpus = 0;
+	FILE *fp;
+
+	fp = timer_list_open();
+	while (fp && fgets(buff, sizeof(buff), fp)) {
+		if (sscanf(buff, "jiffies: %lu\n", &jiffies) == 1)
+			cpus++;
+	}
+	timer_list_close(fp);
+
+	if (!cpus) {
+		fprintf(stderr, "No procfs support?\n");
+		return -EIO;
+	}
+
+	before->cpus = after->cpus = cpus;
+	before->jiffies = calloc(cpus, sizeof(*before->jiffies));
+	after->jiffies  = calloc(cpus, sizeof(*before->jiffies));
+
+	if (!before->jiffies || !after->jiffies) {
+		free(before->jiffies);
+		free(after->jiffies);
+		fprintf(stderr, "Error allocating per CPU jiffies: %s\n",
+			strerror(errno));
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int fetch_kern_jiffies(const struct cpu_jiffies *curr)
+{
+	char buff[512];
+	int cpus = 0;
+	FILE *fp;
+
+	fp = timer_list_open();
+	while (fp && fgets(buff, sizeof(buff), fp) && cpus < curr->cpus) {
+		if (sscanf(buff, "jiffies: %lu\n", &curr->jiffies[cpus]) == 1)
+			cpus++;
+	}
+	timer_list_close(fp);
+
+	if (cpus != curr->cpus) {
+		fprintf(stderr, "CPU mismatch when fetching jiffies\n");
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int dump_kern_jiffies(const struct cpu_jiffies *fixed,
+			     const struct cpu_jiffies *result)
+{
+	uint64_t delta, warp = 0;
+	int i, j, ret = 0;
+	int64_t x;
+
+	for (i = 0; i < result->cpus; i++) {
+		result->jiffies[i] -= fixed->jiffies[i];
+		for (j = 0, delta = ~0;
+		     j < sizeof(kernel_hz)/sizeof(kernel_hz[0]); j++) {
+			x = abs((int64_t)(kernel_hz[j] - result->jiffies[i]));
+			if (x < delta) {
+				delta = x;
+				fixed->jiffies[i] = kernel_hz[j];
+			}
+		}
+		if (delta > warp)
+			warp = delta;
+		if (fixed->jiffies[i] != fixed->jiffies[0]) {
+			fprintf(stderr, "Probed jiffies mismatch: %lu vs %lu HZ\n",
+				fixed->jiffies[i], fixed->jiffies[0]);
+			goto out;
+		}
+	}
+
+	printf("#define KERNEL_HZ %lu\t/* warp: %lu jiffies */\n", fixed->jiffies[0], warp);
+out:
+	free(fixed->jiffies);
+	free(result->jiffies);
+	return ret;
+}
+
+int main(void)
+{
+	struct cpu_jiffies before, after;
+	struct timespec tv = {
+		.tv_sec  = 1,
+		.tv_nsec = 0,
+	};
+	int sig, ret, tries = 4;
+
+	if (pin_to_cpu(0)) {
+		fprintf(stderr, "Cannot pin to CPU 0: %s\n", strerror(errno));
+		return -1;
+	}
+	if (fix_priority()) {
+		fprintf(stderr, "Cannot set priority: %s\n", strerror(errno));
+		return -1;
+	}
+	if (prep_kern_jiffies(&before, &after)) {
+		fprintf(stderr, "Cannot prep jiffies: %s\n", strerror(errno));
+		return -1;
+	}
+
+	do {
+		ret  = fetch_kern_jiffies(&before);
+		sig  = nanosleep(&tv, NULL);
+		ret += fetch_kern_jiffies(&after);
+	} while (!ret && sig && errno == EINTR && --tries >= 0);
+
+	if (!ret && !sig)
+		dump_kern_jiffies(&before, &after);
+	return ret;
+}

bpf/node_config.h
@@ -45,6 +45,8 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 #define CT_REPORT_INTERVAL		5
 #define CT_REPORT_FLAGS			0xff
 
+#define KERNEL_HZ 250   /* warp: 0 jiffies */
+
 #ifdef ENABLE_IPV4
 #define IPV4_MASK 0xffff
 #define IPV4_GATEWAY 0xfffff50a

commit 22fd0f8182e395876bdeedaf90ab5a3bf860b181

cilium, bpf: improve and fix probing of kernel hz
This work removes probing of kernel hz from init.sh and and moves it into
the agent. I'll upstream a small bpftool change which reads CONFIG_HZ from
the underlying kernel config which Cilium agent can then pick up via probe
manager automatically:

  diff --git a/tools/bpf/bpftool/feature.c b/tools/bpf/bpftool/feature.c
  index 88718ee6a438..946e9b89467d 100644
  --- a/tools/bpf/bpftool/feature.c
  +++ b/tools/bpf/bpftool/feature.c
  @@ -378,6 +378,9 @@ static void probe_kernel_image_config(void)

                  /* test_bpf module for BPF tests */
                  "CONFIG_TEST_BPF",
  +
  +               /* Miscellaneous, e.g. for jiffies conversion */
  +               "CONFIG_HZ",
          };
          char *values[ARRAY_SIZE(options)] = { };
          struct utsname utsn;

If it is not present on the system, we then fall-back to run the probe
(cilium-probe-kernel-hz) and hold the result in the agent config. Either
way, we then emit the KERNEL_HZ when generating the node_config.h from
the agent. If kernel config or probing failed, we disable checking for
jiffie helper since there is no point due to missing KERNEL_HZ value.

Fixes: 6d69a62b8775 ("bpf: probe and emit kernel hz to node_config")
Reported-by: Zang Li <zangli@google.com>
Reported-by: Benjamin Pineau <benjamin.pineau@datadoghq.com>
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

bpf/init.sh
@@ -437,8 +437,6 @@ case "${MODE}" in
         ;;
 esac
 
-cilium-probe-kernel-hz -m >> $RUNDIR/globals/node_config.h
-
 # node_config.h header generation
 case "${MODE}" in
 	*)

bpf/lib/mono.h
@@ -5,7 +5,7 @@
 #define __LIB_MONO_H_
 
 /* Moved out from time.h to avoid circular header dependency. */
-#if defined(BPF_HAVE_JIFFIES) && defined(ENABLE_JIFFIES)
+#if defined(BPF_HAVE_JIFFIES) && defined(ENABLE_JIFFIES) && KERNEL_HZ != 1
 # define BPF_MONO_SCALER	8
 # define bpf_mono_now()		(jiffies >> BPF_MONO_SCALER)
 # define bpf_sec_to_mono(s)	(bpf_sec_to_jiffies(s) >> BPF_MONO_SCALER)

daemon/cmd/daemon_main.go
@@ -1513,12 +1513,24 @@ func initSockmapOption() {
 
 func initClockSourceOption() {
 	option.Config.ClockSource = option.ClockSourceKtime
-	if !option.Config.DryMode && option.Config.EnableBPFClockProbe {
-		if h := probes.NewProbeManager().GetHelpers("xdp"); h != nil {
-			if _, ok := h["bpf_jiffies64"]; ok {
-				t, err := bpf.GetJtime()
-				if err == nil && t > 0 {
-					option.Config.ClockSource = option.ClockSourceJiffies
+	option.Config.KernelHz = 1 // Known invalid non-zero to avoid div by zero.
+	if !option.Config.DryMode {
+		hz, err := probes.NewProbeManager().SystemKernelHz()
+		if err != nil {
+			log.Warnf("Auto-disabling %q feature since KERNEL_HZ cannot be determined",
+				option.EnableBPFClockProbe)
+			option.Config.EnableBPFClockProbe = false
+		} else {
+			option.Config.KernelHz = hz
+		}
+
+		if option.Config.EnableBPFClockProbe {
+			if h := probes.NewProbeManager().GetHelpers("xdp"); h != nil {
+				if _, ok := h["bpf_jiffies64"]; ok {
+					t, err := bpf.GetJtime()
+					if err == nil && t > 0 {
+						option.Config.ClockSource = option.ClockSourceJiffies
+					}
 				}
 			}
 		}

pkg/datapath/linux/config/config.go
@@ -89,6 +89,8 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 	fw.WriteString(dumpRaw(defaults.RestoreV4Addr, node.GetInternalIPv4()))
 	fmt.Fprintf(fw, " */\n\n")
 
+	cDefinesMap["KERNEL_HZ"] = fmt.Sprintf("%d", option.Config.KernelHz)
+
 	if option.Config.EnableIPv6 {
 		extraMacrosMap["ROUTER_IP"] = routerIP.String()
 		fw.WriteString(defineIPv6("ROUTER_IP", routerIP))

pkg/datapath/linux/probes/probes.go
@@ -22,6 +22,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"sync"
 
 	"github.com/cilium/cilium/pkg/command/exec"
@@ -93,6 +94,7 @@ type SystemConfig struct {
 	ConfigBpfilter               KernelParam `json:"CONFIG_BPFILTER"`
 	ConfigBpfilterUmh            KernelParam `json:"CONFIG_BPFILTER_UMH"`
 	ConfigTestBpf                KernelParam `json:"CONFIG_TEST_BPF"`
+	ConfigKernelHz               KernelParam `json:"CONFIG_HZ"`
 }
 
 // MapTypes contains bools indicating which types of BPF maps the currently
@@ -154,6 +156,39 @@ func NewProbeManager() *ProbeManager {
 	}
 	once.Do(newProbeManager)
 	return probeManager
+}
+
+func (p *ProbeManager) probeSystemKernelHz() (int, error) {
+	out, err := exec.WithTimeout(
+		defaults.ExecTimeout,
+		"cilium-probe-kernel-hz",
+	).Output(log, false)
+	if err != nil {
+		return 0, fmt.Errorf("Cannot probe CONFIG_HZ")
+	}
+	hz := 0
+	warp := 0
+	n, _ := fmt.Sscanf(string(out), "%d, %d\n", &hz, &warp)
+	if n == 2 && hz > 0 && hz < 100000 {
+		return hz, nil
+	}
+	return 0, fmt.Errorf("Invalid probed CONFIG_HZ value")
+}
+
+// SystemKernelHz returns the HZ value that the kernel has been configured with.
+func (p *ProbeManager) SystemKernelHz() (int, error) {
+	config := p.features.SystemConfig
+	if config.ConfigKernelHz == "" {
+		return p.probeSystemKernelHz()
+	}
+	hz, err := strconv.Atoi(string(config.ConfigKernelHz))
+	if err != nil {
+		return 0, err
+	}
+	if hz > 0 && hz < 100000 {
+		return hz, nil
+	}
+	return 0, fmt.Errorf("Invalid CONFIG_HZ value")
 }
 
 // SystemConfigProbes performs a check of kernel configuration parameters. It

pkg/option/config.go
@@ -1675,6 +1675,9 @@ type DaemonConfig struct {
 	// Selection of BPF main clock source (ktime vs jiffies)
 	ClockSource BPFClockSource
 
+	// KernelHz is the HZ rate the kernel is operating in
+	KernelHz int
+
 	// excludeLocalAddresses excludes certain addresses to be recognized as
 	// a local address
 	excludeLocalAddresses []*net.IPNet