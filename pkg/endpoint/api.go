g0odi3z2j/cilium

commit 783cb839853f603caba24d8e46d48e8fb90350f9

api: Add named ports
Add named ports to endpoint status.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/endpoint/api.go
@@ -37,6 +37,7 @@ import (
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/policy/trafficdirection"
+	"github.com/cilium/cilium/pkg/u8proto"
 )
 
 // GetLabelsModel returns the labels of the endpoint in their representation
@@ -227,6 +228,7 @@ func (e *Endpoint) GetModelRLocked() *models.Endpoint {
 			Controllers: controllerMdl,
 			State:       currentState, // TODO: Validate
 			Health:      e.getHealthModel(),
+			NamedPorts:  e.getNamedPortsModel(),
 		},
 	}
 
@@ -296,6 +298,31 @@ func (e *Endpoint) GetHealthModel() *models.EndpointHealth {
 	e.mutex.RLock()
 	defer e.mutex.RUnlock()
 	return e.getHealthModel()
+}
+
+// getNamedPortsModel returns the endpoint's NamedPorts object.
+//
+// Must be called with e.Mutex locked.
+func (e *Endpoint) getNamedPortsModel() (np models.NamedPorts) {
+	k8sPorts := e.k8sPorts
+	np = make(models.NamedPorts, 0, len(k8sPorts))
+	for name, value := range k8sPorts {
+		np = append(np, &models.Port{
+			Name:     name,
+			Port:     value.Port,
+			Protocol: u8proto.U8proto(value.Proto).String(),
+		})
+	}
+	return np
+}
+
+// GetNamedPortsModel returns the endpoint's NamedPorts object.
+func (e *Endpoint) GetNamedPortsModel() models.NamedPorts {
+	if err := e.rlockAlive(); err != nil {
+		return nil
+	}
+	defer e.runlock()
+	return e.getNamedPortsModel()
 }
 
 // GetModel returns the API model of endpoint e.

api/v1/models/endpoint_status.go
@@ -34,6 +34,9 @@ type EndpointStatus struct {
 	// Most recent status log. See endpoint/{id}/log for the complete log.
 	Log EndpointStatusLog `json:"log,omitempty"`
 
+	// List of named ports that can be used in Network Policy
+	NamedPorts NamedPorts `json:"namedPorts,omitempty"`
+
 	// Networking properties of the endpoint
 	Networking *EndpointNetworking `json:"networking,omitempty"`
 
@@ -73,6 +76,10 @@ func (m *EndpointStatus) Validate(formats strfmt.Registry) error {
 	}
 
 	if err := m.validateLog(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateNamedPorts(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -195,6 +202,22 @@ func (m *EndpointStatus) validateLog(formats strfmt.Registry) error {
 	if err := m.Log.Validate(formats); err != nil {
 		if ve, ok := err.(*errors.Validation); ok {
 			return ve.ValidateName("log")
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (m *EndpointStatus) validateNamedPorts(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.NamedPorts) { // not required
+		return nil
+	}
+
+	if err := m.NamedPorts.Validate(formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("namedPorts")
 		}
 		return err
 	}

api/v1/models/named_ports.go
@@ -1,0 +1,47 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package models
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"strconv"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/swag"
+)
+
+// NamedPorts List of named Layer 4 port and protocol pairs which will be used in Network
+// Policy specs.
+//
+// swagger:model NamedPorts
+type NamedPorts []*Port
+
+// Validate validates this named ports
+func (m NamedPorts) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	for i := 0; i < len(m); i++ {
+		if swag.IsZero(m[i]) { // not required
+			continue
+		}
+
+		if m[i] != nil {
+			if err := m[i].Validate(formats); err != nil {
+				if ve, ok := err.(*errors.Validation); ok {
+					return ve.ValidateName(strconv.Itoa(i))
+				}
+				return err
+			}
+		}
+
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}

api/v1/models/port.go
@@ -19,6 +19,9 @@ import (
 // swagger:model Port
 type Port struct {
 
+	// Optional layer 4 port name
+	Name string `json:"name,omitempty"`
+
 	// Layer 4 port number
 	Port uint16 `json:"port,omitempty"`
 

api/v1/openapi.yaml
@@ -1121,6 +1121,9 @@ definitions:
       health:
         description: Summary overall endpoint & subcomponent health
         "$ref": "#/definitions/EndpointHealth"
+      namedPorts:
+        description: List of named ports that can be used in Network Policy
+        "$ref": "#/definitions/NamedPorts"
   EndpointState:
     description: State of endpoint
     type: string
@@ -2024,6 +2027,9 @@ definitions:
         description: Layer 4 port number
         type: integer
         format: uint16
+      name:
+        description: Optional layer 4 port name
+        type: string
   SelectorCache:
     description: cache of which identities match selectors in the policy repository
     type: array
@@ -2451,3 +2457,10 @@ definitions:
       name:
         description: Name assigned to the IP (e.g. Kubernetes pod name)
         type: string
+  NamedPorts:
+    description: |
+      List of named Layer 4 port and protocol pairs which will be used in Network
+      Policy specs.
+    type: array
+    items:
+      "$ref": "#/definitions/Port"

api/v1/server/embedded_spec.go
@@ -2151,6 +2151,10 @@ func init() {
           "description": "Most recent status log. See endpoint/{id}/log for the complete log.",
           "$ref": "#/definitions/EndpointStatusLog"
         },
+        "namedPorts": {
+          "description": "List of named ports that can be used in Network Policy",
+          "$ref": "#/definitions/NamedPorts"
+        },
         "networking": {
           "description": "Networking properties of the endpoint",
           "$ref": "#/definitions/EndpointNetworking"
@@ -2742,6 +2746,13 @@ func init() {
         }
       }
     },
+    "NamedPorts": {
+      "description": "List of named Layer 4 port and protocol pairs which will be used in Network\nPolicy specs.\n",
+      "type": "array",
+      "items": {
+        "$ref": "#/definitions/Port"
+      }
+    },
     "NodeAddressing": {
       "description": "Addressing information of a node for all address families",
       "type": "object",
@@ -2849,6 +2860,10 @@ func init() {
       "description": "Layer 4 port / protocol pair",
       "type": "object",
       "properties": {
+        "name": {
+          "description": "Optional layer 4 port name",
+          "type": "string"
+        },
         "port": {
           "description": "Layer 4 port number",
           "type": "integer",
@@ -5700,6 +5715,10 @@ func init() {
           "description": "Most recent status log. See endpoint/{id}/log for the complete log.",
           "$ref": "#/definitions/EndpointStatusLog"
         },
+        "namedPorts": {
+          "description": "List of named ports that can be used in Network Policy",
+          "$ref": "#/definitions/NamedPorts"
+        },
         "networking": {
           "description": "Networking properties of the endpoint",
           "$ref": "#/definitions/EndpointNetworking"
@@ -6291,6 +6310,13 @@ func init() {
         }
       }
     },
+    "NamedPorts": {
+      "description": "List of named Layer 4 port and protocol pairs which will be used in Network\nPolicy specs.\n",
+      "type": "array",
+      "items": {
+        "$ref": "#/definitions/Port"
+      }
+    },
     "NodeAddressing": {
       "description": "Addressing information of a node for all address families",
       "type": "object",
@@ -6398,6 +6424,10 @@ func init() {
       "description": "Layer 4 port / protocol pair",
       "type": "object",
       "properties": {
+        "name": {
+          "description": "Optional layer 4 port name",
+          "type": "string"
+        },
         "port": {
           "description": "Layer 4 port number",
           "type": "integer",

pkg/endpoint/endpoint_status.go
@@ -327,6 +327,7 @@ func (e *Endpoint) GetCiliumEndpointStatus(conf EndpointStatusConfiguration) *ci
 		Networking:          getEndpointNetworking(modelStatus),
 		State:               compressEndpointState(modelStatus.State),
 		Encryption:          cilium_v2.EncryptionSpec{Key: int(node.GetIPsecKeyIdentity())},
+		NamedPorts:          modelStatus.NamedPorts,
 	}
 
 	if conf.EndpointStatusIsEnabled(option.EndpointStatusControllers) {

pkg/k8s/apis/cilium.io/v2/client/register.go
@@ -54,7 +54,7 @@ const (
 
 	// CustomResourceDefinitionSchemaVersion is semver-conformant version of CRD schema
 	// Used to determine if CRD needs to be updated in cluster
-	CustomResourceDefinitionSchemaVersion = "1.17"
+	CustomResourceDefinitionSchemaVersion = "1.18"
 
 	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
 	CustomResourceDefinitionSchemaVersionKey = "io.cilium.k8s.crd.schema.version"
@@ -1132,13 +1132,16 @@ var (
 		},
 		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
 			"port": {
-				Description: "Port is an L4 port number. For now the string will be strictly " +
-					"parsed as a single uint16. In the future, this field may support ranges " +
+				Description: "Port is an L4 port number or a port name. For now the string will be strictly " +
+					"parsed as a single uint16 or a valid port name, which consists of at most 15 alphanumeric " +
+					"characters of which at least one is not a number, as well as non-consecutive dashes " +
+					"('-') except for in the beginning or the end. In the future, this field may support ranges " +
 					"in the form \"1024-2048",
 				Type: "string",
-				// uint16 string regex
+				// uint16 or port name string regex
 				Pattern: `^(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|` +
-					`[1-5][0-9]{4}|[0-9]{1,4})$`,
+					`[1-5][0-9]{4}|[0-9]{1,4}|` +
+					`([a-zA-Z0-9]-?)*[a-zA-Z](-?[a-zA-Z0-9])*)$`,
 			},
 			"protocol": {
 				Description: `Protocol is the L4 protocol. If omitted or empty, any protocol ` +

commit 5a77edc7f6752316af16235216aa50562da2a219

pkg/endpoint: returned NamedPorts model consistently
We need to return the NamedPorts consistently across calls of
getNamedPortsModel function. This avoids unnecessary CEP updates to
kube-apiserver.

Fixes: 783cb839853f ("api: Add named ports")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/api.go
@@ -305,8 +305,17 @@ func (e *Endpoint) GetHealthModel() *models.EndpointHealth {
 // Must be called with e.Mutex locked.
 func (e *Endpoint) getNamedPortsModel() (np models.NamedPorts) {
 	k8sPorts := e.k8sPorts
+	// keep named ports ordered to avoid the unnecessary updates to
+	// kube-apiserver
+	names := make([]string, 0, len(k8sPorts))
+	for name := range k8sPorts {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+
 	np = make(models.NamedPorts, 0, len(k8sPorts))
-	for name, value := range k8sPorts {
+	for _, name := range names {
+		value := k8sPorts[name]
 		np = append(np, &models.Port{
 			Name:     name,
 			Port:     value.Port,