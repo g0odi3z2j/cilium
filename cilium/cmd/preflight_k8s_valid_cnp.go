g0odi3z2j/cilium

commit d0530980210de3eb1e1dee1c7f947fdfe5b4b947

add validation checker for installed CNPs in preflight
As Cilium might require to update its CRD validation schema it is
important for the users to make sure all policies installed in their
cluster are valid in the point of view the new CRD validation schema
before performing an upgrade.

Avoiding doing this validation might cause Cilium from updating its
NodeStatus in those invalid Network Policies as well as in the worst
case scenario it might give a false sense of security to the user if
a policy is badly formatted and Cilium is not inforcing that policy
due a bad validation.

Signed-off-by: André Martins <andre@cilium.io>

cilium/cmd/preflight_k8s_valid_cnp.go
@@ -1,0 +1,140 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/cilium/cilium/pkg/k8s"
+	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/scheme"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/option"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	apiextensionsinternal "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+var validateCNP = &cobra.Command{
+	Use:   "validate-cnp",
+	Short: "Validate Cilium Network Policies deployed in the cluster",
+	Long: `Before upgrading Cilium it is recommended to run this validation checker
+to make sure the policies deployed are valid. The validator will verify if all policies
+deployed in the cluster are valid, in case they are not, an error is printed and the
+has an exit code -1 is returned.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := validateCNPs()
+		if err != nil {
+			log.Error(err)
+			os.Exit(-1)
+		}
+	},
+}
+
+const validateK8sPoliciesTimeout = 5 * time.Minute
+
+func validateCNPs() error {
+	// The internal packages log things. Make sure they follow the setup of of
+	// the CLI tool.
+	logging.DefaultLogger.SetFormatter(log.Formatter)
+
+	log.Info("Setting up Kubernetes client")
+
+	k8sClientQPSLimit := viper.GetFloat64(option.K8sClientQPSLimit)
+	k8sClientBurst := viper.GetInt(option.K8sClientBurst)
+
+	k8s.Configure(k8sAPIServer, k8sKubeConfigPath, float32(k8sClientQPSLimit), k8sClientBurst)
+
+	if err := k8s.Init(); err != nil {
+		log.WithError(err).Fatal("Unable to connect to Kubernetes apiserver")
+	}
+
+	ctx, initCancel := context.WithTimeout(context.Background(), validateK8sPoliciesTimeout)
+	defer initCancel()
+	cnpErr := validateNPResources(ctx, "ciliumnetworkpolicies", "CiliumNetworkPolicy")
+
+	ctx, initCancel2 := context.WithTimeout(context.Background(), validateK8sPoliciesTimeout)
+	defer initCancel2()
+	ccnpErr := validateNPResources(ctx, "ciliumclusterwidenetworkpolicies", "CiliumClusterwideNetworkPolicy")
+
+	if cnpErr != nil {
+		return cnpErr
+	}
+	if ccnpErr != nil {
+		return ccnpErr
+	}
+	log.Info("All CCNPs and CNPs valid!")
+	return nil
+}
+
+func validateNPResources(ctx context.Context, name, shortName string) error {
+	var internal apiextensionsinternal.CustomResourceValidation
+	err := v1beta1.Convert_v1beta1_CustomResourceValidation_To_apiextensions_CustomResourceValidation(
+		&v2.CNPCRV,
+		&internal,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+	validator, _, err := validation.NewSchemaValidator(&internal)
+	if err != nil {
+		return err
+	}
+
+	var policyErr error
+	var cnps unstructured.UnstructuredList
+	for {
+		opts := metav1.ListOptions{
+			Limit:    25,
+			Continue: cnps.GetContinue(),
+		}
+		err = k8s.CiliumClient().
+			Interface.
+			CiliumV2().
+			RESTClient().
+			Get().
+			VersionedParams(&opts, scheme.ParameterCodec).
+			Resource(name).
+			Do(ctx).
+			Into(&cnps)
+		if err != nil {
+			return err
+		}
+
+		for _, cnp := range cnps.Items {
+			cnpName := fmt.Sprintf("%s/%s", cnp.GetNamespace(), cnp.GetName())
+			if errs := validation.ValidateCustomResource(nil, &cnp, validator); len(errs) > 0 {
+				log.Errorf("Validating %s '%s': unexpected validation error: %s",
+					shortName, cnpName, errs.ToAggregate())
+				policyErr = fmt.Errorf("Found invalid %s", shortName)
+			} else {
+				log.Infof("Validating %s '%s': OK!", shortName, cnpName)
+			}
+		}
+		if cnps.GetContinue() == "" {
+			break
+		}
+	}
+	return policyErr
+}

CODEOWNERS
@@ -32,6 +32,7 @@ api/ @cilium/api
 bpf/ @cilium/bpf
 bugtool/cmd/ @cilium/cli
 cilium/ @cilium/cli
+cilium/cmd/preflight_k8s_valid_cnp.go @cilium/kubernetes
 cilium-health/cmd/ @cilium/cli
 cilium-health/launch/ @cilium/health
 contrib/packaging/deb/ @eloycoto

cilium/cmd/preflight.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Authors of Cilium
+// Copyright 2019-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -76,6 +76,10 @@ func init() {
 	migrateIdentityCmd.Flags().StringVar(&kvStore, "kvstore", "", "Key-value store type")
 	migrateIdentityCmd.Flags().Var(option.NewNamedMapOptions("kvstore-opts", &kvStoreOpts, nil), "kvstore-opt", "Key-value store options")
 	preflightCmd.AddCommand(migrateIdentityCmd)
+
+	validateCNP.Flags().StringVar(&k8sAPIServer, "k8s-api-server", "", "Kubernetes api address server (for https use --k8s-kubeconfig-path instead)")
+	validateCNP.Flags().StringVar(&k8sKubeConfigPath, "k8s-kubeconfig-path", "", "Absolute path of the kubernetes kubeconfig file")
+	preflightCmd.AddCommand(validateCNP)
 
 	rootCmd.AddCommand(preflightCmd)
 }

install/kubernetes/cilium/charts/preflight/templates/daemonset.yaml
@@ -101,6 +101,15 @@ spec:
               - /tmp/read-tofqdns-precachey
             initialDelaySeconds: 5
             periodSeconds: 5
+          env:
+{{- if .Values.global.k8sServiceHost }}
+          - name: KUBERNETES_SERVICE_HOST
+            value: {{ .Values.global.k8sServiceHost | quote }}
+{{- end }}
+{{- if .Values.global.k8sServicePort }}
+          - name: KUBERNETES_SERVICE_PORT
+            value: {{ .Values.global.k8sServicePort | quote }}
+{{- end }}
           volumeMounts:
           - mountPath: /var/run/cilium
             name: cilium-run

install/kubernetes/cilium/charts/preflight/templates/deployment.yaml
@@ -1,0 +1,80 @@
+{{- if .Values.validateCNPs }}
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: cilium-pre-flight-check
+  namespace: {{ .Release.Namespace }}
+spec:
+  selector:
+    matchLabels:
+      k8s-app: cilium-pre-flight-check-deployment
+      kubernetes.io/cluster-service: "true"
+  template:
+    metadata:
+      labels:
+        k8s-app: cilium-pre-flight-check-deployment
+        kubernetes.io/cluster-service: "true"
+    spec:
+      affinity:
+        podAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+          - labelSelector:
+              matchExpressions:
+              - key: "k8s-app"
+                operator: In
+                values:
+                - cilium
+            topologyKey: "kubernetes.io/hostname"
+      containers:
+{{- if .Values.validateCNPs }}
+{{- if contains "/" .Values.image }}
+        - image: "{{ .Values.image }}"
+{{- else }}
+        - image: "{{ .Values.global.registry }}/{{ .Values.image }}:{{ .Values.global.tag }}"
+{{- end }}
+          imagePullPolicy: {{ .Values.global.pullPolicy }}
+          name: cnp-validator
+          command: ["/bin/sh"]
+          args:
+          - -c
+          - "cilium preflight validate-cnp && touch /tmp/ready-validate-cnp && sleep 1h"
+          livenessProbe:
+            exec:
+              command:
+              - cat
+              - /tmp/ready-validate-cnp
+            initialDelaySeconds: 5
+            periodSeconds: 5
+          readinessProbe:
+            exec:
+              command:
+              - cat
+              - /tmp/ready-validate-cnp
+            initialDelaySeconds: 5
+            periodSeconds: 5
+          env:
+{{- if .Values.global.k8sServiceHost }}
+          - name: KUBERNETES_SERVICE_HOST
+            value: {{ .Values.global.k8sServiceHost | quote }}
+{{- end }}
+{{- if .Values.global.k8sServicePort }}
+          - name: KUBERNETES_SERVICE_PORT
+            value: {{ .Values.global.k8sServicePort | quote }}
+{{- end }}
+{{- end }}
+      hostNetwork: true
+      restartPolicy: Always
+      serviceAccount: cilium
+      serviceAccountName: cilium
+      terminationGracePeriodSeconds: 1
+      tolerations:
+        - effect: NoSchedule
+          key: node.kubernetes.io/not-ready
+        - effect: NoSchedule
+          key: node-role.kubernetes.io/master
+        - effect: NoSchedule
+          key: node.cloudprovider.kubernetes.io/uninitialized
+          value: "true"
+        - key: CriticalAddonsOnly
+          operator: "Exists"
+{{- end }}

install/kubernetes/cilium/values.yaml
@@ -22,6 +22,10 @@ preflight:
   # Path to write the --tofqdns-pre-cache file to. When empty no file is
   # generated.
   tofqdnsPreCache: ""
+  # By default we should always validate the installed CNPs before upgrading
+  # Cilium. This will make sure the user will have the policies deployed in the
+  # cluster with the right schema.
+  validateCNPs: true
 
 # global groups all configuration options that have effect on all sub-charts
 global:

pkg/k8s/apis/cilium.io/v2/register.go
@@ -191,7 +191,7 @@ func createCNPCRD(clientset apiextensionsclient.Interface) error {
 				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
 			},
 			Scope:      apiextensionsv1beta1.NamespaceScoped,
-			Validation: &cnpCRV,
+			Validation: &CNPCRV,
 		},
 	}
 	// Kubernetes < 1.12 does not support having the field Type set in the root
@@ -243,7 +243,7 @@ func createCCNPCRD(clientset apiextensionsclient.Interface) error {
 				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
 			},
 			Scope:      apiextensionsv1beta1.ClusterScoped,
-			Validation: &cnpCRV,
+			Validation: &CNPCRV,
 		},
 	}
 	// Kubernetes < 1.12 does not support having the field Type set in the root
@@ -655,7 +655,7 @@ var (
 		OpenAPIV3Schema: &apiextensionsv1beta1.JSONSchemaProps{},
 	}
 
-	cnpCRV = apiextensionsv1beta1.CustomResourceValidation{
+	CNPCRV = apiextensionsv1beta1.CustomResourceValidation{
 		OpenAPIV3Schema: &apiextensionsv1beta1.JSONSchemaProps{
 			// TODO: remove the following comment when we add checker
 			// to detect if we should install the CNP validation for k8s > 1.11

pkg/k8s/apis/cilium.io/v2/register_test.go
@@ -36,7 +36,7 @@ func (s *CiliumV2RegisterSuite) getTestUpToDateDefinition() *apiextensionsv1beta
 			},
 		},
 		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-			Validation: &cnpCRV,
+			Validation: &CNPCRV,
 		},
 	}
 }

commit 5cabc9499c3ac79d7bc0749769aedbb5b7fdf720

cilium/cmd: do not fail if CCNPs are not found
In a cluster being upgraded from < 1.7, it is expected that CCNPs are
not available. For this reason, the CNP validation tool should not fail
has it is expected for CCNPs to not be available.

Fixes: d0530980210d ("add validation checker for installed CNPs in preflight")
Signed-off-by: André Martins <andre@cilium.io>

cilium/cmd/preflight_k8s_valid_cnp.go
@@ -32,6 +32,8 @@ import (
 	apiextensionsinternal "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -52,7 +54,10 @@ has an exit code -1 is returned.`,
 	},
 }
 
-const validateK8sPoliciesTimeout = 5 * time.Minute
+const (
+	validateK8sPoliciesTimeout = 5 * time.Minute
+	ciliumGroup                = "cilium.io"
+)
 
 func validateCNPs() error {
 	// The internal packages log things. Make sure they follow the setup of of
@@ -70,13 +75,22 @@ func validateCNPs() error {
 		log.WithError(err).Fatal("Unable to connect to Kubernetes apiserver")
 	}
 
+	restConfig, err := k8s.CreateConfig()
+	if err != nil {
+		return fmt.Errorf("Unable to create rest configuration for k8s CRD: %w", err)
+	}
+	apiExtensionsClient, err := apiextensionsclient.NewForConfig(restConfig)
+	if err != nil {
+		return fmt.Errorf("Unable to create API extensions clientset for k8s CRD: %w", err)
+	}
+
 	ctx, initCancel := context.WithTimeout(context.Background(), validateK8sPoliciesTimeout)
 	defer initCancel()
-	cnpErr := validateNPResources(ctx, "ciliumnetworkpolicies", "CiliumNetworkPolicy")
+	cnpErr := validateNPResources(ctx, apiExtensionsClient, "ciliumnetworkpolicies", "CiliumNetworkPolicy")
 
 	ctx, initCancel2 := context.WithTimeout(context.Background(), validateK8sPoliciesTimeout)
 	defer initCancel2()
-	ccnpErr := validateNPResources(ctx, "ciliumclusterwidenetworkpolicies", "CiliumClusterwideNetworkPolicy")
+	ccnpErr := validateNPResources(ctx, apiExtensionsClient, "ciliumclusterwidenetworkpolicies", "CiliumClusterwideNetworkPolicy")
 
 	if cnpErr != nil {
 		return cnpErr
@@ -88,9 +102,19 @@ func validateCNPs() error {
 	return nil
 }
 
-func validateNPResources(ctx context.Context, name, shortName string) error {
+func validateNPResources(ctx context.Context, apiExtensionsClient *apiextensionsclient.Clientset, name, shortName string) error {
+	// check if the crd is installed at all
+	_, err := apiExtensionsClient.ApiextensionsV1beta1().CustomResourceDefinitions().Get(ctx, name+"."+ciliumGroup, metav1.GetOptions{})
+	switch {
+	case err == nil:
+	case k8sErrors.IsNotFound(err):
+		return nil
+	default:
+		return err
+	}
+
 	var internal apiextensionsinternal.CustomResourceValidation
-	err := v1beta1.Convert_v1beta1_CustomResourceValidation_To_apiextensions_CustomResourceValidation(
+	err = v1beta1.Convert_v1beta1_CustomResourceValidation_To_apiextensions_CustomResourceValidation(
 		&v2_client.CNPCRV,
 		&internal,
 		nil,