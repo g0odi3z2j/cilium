g0odi3z2j/cilium

commit 6e4727af8a85ab3afdbace6f008dab7aa0f11ef4

proxy: Add RedirectImplementation.UpdateRules
Envoy maintains the desired state  in the xDS cache, and kafka uses the
rules in the redirect object directly during processing. The DNS proxy
needs an event when rules are updated, and UpdateRules is the callback
for that. It is a no-op in this commit to simplify it.

Signed-off-by: Ray Bejjani <ray@covalent.io>

pkg/proxy/proxy.go
@@ -183,6 +183,13 @@ func (p *Proxy) CreateOrUpdateRedirect(l4 *policy.L4Filter, id string, localEndp
 		if redir.parserType == l4.L7Parser {
 			updateRevertFunc := redir.updateRules(l4)
 			revertStack.Push(updateRevertFunc)
+			var implUpdateRevertFunc revert.RevertFunc
+			implUpdateRevertFunc, err = redir.implementation.UpdateRules(wg, l4)
+			if err != nil {
+				err = fmt.Errorf("unable to update existing redirect: %s", err)
+				return
+			}
+			revertStack.Push(implUpdateRevertFunc)
 
 			redir.lastUpdated = time.Now()
 
@@ -210,6 +217,7 @@ func (p *Proxy) CreateOrUpdateRedirect(l4 *policy.L4Filter, id string, localEndp
 	redir.ingress = l4.Ingress
 	redir.parserType = l4.L7Parser
 	redir.updateRules(l4)
+	// Rely on create*Redirect to update rules, unlike the update case above.
 
 	for nRetry := 0; ; nRetry++ {
 		var to uint16

pkg/proxy/dns.go
@@ -52,8 +52,8 @@ type dnsRedirect struct {
 type dnsConfiguration struct {
 }
 
-// UpdateRules replaces old l7 rules of a redirect with new ones.
-func (dr *dnsRedirect) UpdateRules(wg *completion.WaitGroup) error {
+// setRules replaces old l7 rules of a redirect with new ones.
+func (dr *dnsRedirect) setRules(wg *completion.WaitGroup, newRules policy.L7DataMap) error {
 	var toRemove, toAdd []string
 
 	for _, rule := range dr.currentRules {
@@ -97,6 +97,12 @@ func (dr *dnsRedirect) UpdateRules(wg *completion.WaitGroup) error {
 	return nil
 }
 
+// UpdateRules is a no-op for the DNS proxy as it currently doesn't support
+// updating rules.
+func (dr *dnsRedirect) UpdateRules(wg *completion.WaitGroup, l4 *policy.L4Filter) (revert.RevertFunc, error) {
+	return func() error { return nil }, nil
+}
+
 // Close the redirect.
 func (dr *dnsRedirect) Close(wg *completion.WaitGroup) (revert.FinalizeFunc, revert.RevertFunc) {
 	for _, rule := range dr.currentRules {
@@ -134,7 +140,7 @@ func createDNSRedirect(r *Redirect, conf dnsConfiguration, endpointInfoRegistry 
 		"conf":        conf,
 	}).Debug("Creating DNS Proxy redirect")
 
-	return dr, dr.UpdateRules(nil)
+	return dr, dr.setRules(nil, r.rules)
 }
 
 func copyRules(rules policy.L7DataMap) policy.L7DataMap {

pkg/proxy/envoyproxy.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/envoy"
 	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/policy"
 	"github.com/cilium/cilium/pkg/revert"
 )
 
@@ -62,6 +63,12 @@ func createEnvoyRedirect(r *Redirect, stateDir string, xdsServer *envoy.XDSServe
 	}
 
 	return nil, fmt.Errorf("%s: Envoy proxy process failed to start, cannot add redirect", r.id)
+}
+
+// UpdateRules is a no-op for envoy, as redirect data is synchronized via the
+// xDS cache.
+func (k *envoyRedirect) UpdateRules(wg *completion.WaitGroup, l4 *policy.L4Filter) (revert.RevertFunc, error) {
+	return func() error { return nil }, nil
 }
 
 // Close the redirect.

pkg/proxy/kafka.go
@@ -441,6 +441,12 @@ func (k *kafkaRedirect) handleResponseConnection(pair *connectionPair, correlati
 		}, remoteAddr, remoteIdentity, origDstAddr)
 }
 
+// UpdateRules is a no-op for kafka redirects, as rules are read directly
+// during request processing.
+func (k *kafkaRedirect) UpdateRules(wg *completion.WaitGroup, l4 *policy.L4Filter) (revert.RevertFunc, error) {
+	return func() error { return nil }, nil
+}
+
 // Close the redirect.
 func (k *kafkaRedirect) Close(wg *completion.WaitGroup) (revert.FinalizeFunc, revert.RevertFunc) {
 	return k.socket.Close, nil

pkg/proxy/redirect.go
@@ -30,6 +30,18 @@ import (
 // RedirectImplementation is the generic proxy redirect interface that each
 // proxy redirect type must implement
 type RedirectImplementation interface {
+	// UpdateRules notifies the proxy implementation that the new rules in
+	// parameter l4 are to be applied. The implementation should .Add to the
+	// WaitGroup if the update is asynchronous and the update should not return
+	// until it is complete.
+	// The returned RevertFunc must be non-nil.
+	// Note: UpdateRules is not called when a redirect is created.
+	UpdateRules(wg *completion.WaitGroup, l4 *policy.L4Filter) (revert.RevertFunc, error)
+
+	// Close closes and cleans up resources associated with the redirect
+	// implementation. The implementation should .Add to the WaitGroup if the
+	// update is asynchronous and the update should not return until it is
+	// complete.
 	Close(wg *completion.WaitGroup) (revert.FinalizeFunc, revert.RevertFunc)
 }
 

commit 7bfe01fdb68e52a74dc40f76da5ebdc17bd57566

proxy: release redir.mutex on early exit from CreateOrUpdateRedirect()
In CreateOrUpdateRedirect(), the redir.mutex is not release in the case
where redir.implementation.UpdateRules() fails. Although failure in that
function looks unlikely, let's release the mutex before we leave to
avoid any bad surprise.

Fixes: 6e4727af8a85 ("proxy: Add RedirectImplementation.UpdateRules")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

pkg/proxy/proxy.go
@@ -412,6 +412,7 @@ func (p *Proxy) CreateOrUpdateRedirect(l4 policy.ProxyPolicy, id string, localEn
 			var implUpdateRevertFunc revert.RevertFunc
 			implUpdateRevertFunc, err = redir.implementation.UpdateRules(wg)
 			if err != nil {
+				redir.mutex.Unlock()
 				err = fmt.Errorf("unable to update existing redirect: %s", err)
 				return 0, err, nil, nil
 			}