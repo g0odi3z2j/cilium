g0odi3z2j/cilium

commit 807b146d41087fdcbc6f7e89fcb96d9b4297abb0

Add gops to cilium-cni
Failure to start gops is considered non-fatal here since this feature
should be mostly useful to debug hangs or weird condition and trying to
avoid breaking container creation if gops init fails seems reasonable.

Signed-off by: Jean Raby <jean@raby.sh>

plugins/cilium-cni/cilium-cni.go
@@ -51,6 +51,7 @@ import (
 	cniTypesVer "github.com/containernetworking/cni/pkg/types/current"
 	cniVersion "github.com/containernetworking/cni/pkg/version"
 	"github.com/containernetworking/plugins/pkg/ns"
+	gops "github.com/google/gops/agent"
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
 
@@ -77,6 +78,10 @@ type CmdState struct {
 }
 
 func main() {
+	if err := gops.Listen(gops.Options{}); err != nil {
+		log.WithError(err).Warn("Unable to start gops")
+	}
+
 	skel.PluginMain(cmdAdd,
 		nil,
 		cmdDel,

commit 5ae3c9a348574641e22371ef1af29bc9b46d2f35

plugins/cilium-cni: start gops in debug mode
Fixes: 807b146d4108 ("Add gops to cilium-cni")
Signed-off-by: André Martins <andre@cilium.io>

plugins/cilium-cni/cilium-cni.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -77,17 +77,11 @@ type CmdState struct {
 }
 
 func main() {
-	if err := gops.Listen(gops.Options{}); err != nil {
-		log.WithError(err).Warn("Unable to start gops")
-	}
-
 	skel.PluginMain(cmdAdd,
 		nil,
 		cmdDel,
 		cniVersion.PluginSupports("0.1.0", "0.2.0", "0.3.0", "0.3.1"),
 		"Cilium CNI plugin "+version.Version)
-
-	gops.Close()
 }
 
 func ipv6IsEnabled(ipam *models.IPAMResponse) bool {
@@ -288,6 +282,12 @@ func cmdAdd(args *skel.CmdArgs) (err error) {
 	}
 	if !n.EnableDebug {
 		logger.Logger.SetLevel(logrus.InfoLevel)
+	} else {
+		if err := gops.Listen(gops.Options{}); err != nil {
+			log.WithError(err).Warn("Unable to start gops")
+		} else {
+			defer gops.Close()
+		}
 	}
 	logger.Debugf("Processing CNI ADD request %#v", args)
 
@@ -550,6 +550,12 @@ func cmdDel(args *skel.CmdArgs) error {
 	}
 	if !n.EnableDebug {
 		logger.Logger.SetLevel(logrus.InfoLevel)
+	} else {
+		if err := gops.Listen(gops.Options{}); err != nil {
+			log.WithError(err).Warn("Unable to start gops")
+		} else {
+			defer gops.Close()
+		}
 	}
 	logger.Debugf("Processing CNI DEL request %#v", args)
 