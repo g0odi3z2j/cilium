g0odi3z2j/cilium

commit f069507eb90de8c6745f5f3bfb0d29657f63d79a

pkg/policy: Host network policies
This commit adds network policies for nodes. Node (or host) network
policies are loaded with a new nodeSelector field. The nodeSelector and
endpointSelector fields cannot co-exist for now. Rules with a nodeSelector
field apply only to the special endpoint representing the host.

Network policies for the host cannot contain L7 policies yet as this
isn't supported in the datapath.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/k8s/apis/cilium.io/v2/client/register.go
@@ -54,7 +54,7 @@ const (
 
 	// CustomResourceDefinitionSchemaVersion is semver-conformant version of CRD schema
 	// Used to determine if CRD needs to be updated in cluster
-	CustomResourceDefinitionSchemaVersion = "1.18"
+	CustomResourceDefinitionSchemaVersion = "1.19"
 
 	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
 	CustomResourceDefinitionSchemaVersionKey = "io.cilium.k8s.crd.schema.version"
@@ -1442,15 +1442,20 @@ var (
 	Rule = apiextensionsv1beta1.JSONSchemaProps{
 		Type: "object",
 		Description: "Rule is a policy rule which must be applied to all endpoints which match " +
-			"the labels contained in the endpointSelector\n\nEach rule is split into an " +
-			"ingress section which contains all rules applicable at ingress, and an egress " +
-			"section applicable at egress. For rule types such as `L4Rule` and `CIDR` which " +
-			"can be applied at both ingress and egress, both ingress and egress side have to " +
-			"either specifically allow the connection or one side has to be omitted.\n\n" +
-			"Either ingress, egress, or both can be provided. If both ingress and egress are " +
-			"omitted, the rule has no effect.",
-		Required: []string{
-			"endpointSelector",
+			"the labels contained in the endpointSelector or all nodes which match the labels " +
+			"contained in the nodeSelector.\n\nEach rule is split into an ingress section " +
+			"which contains all rules applicable at ingress, and an egress section applicable " +
+			"at egress. For rule types such as `L4Rule` and `CIDR` which can be applied at both " +
+			"ingress and egress, both ingress and egress side have to either specifically allow " +
+			"the connection or one side has to be omitted.\n\nEither ingress, egress, or both " +
+			"can be provided. If both ingress and egress are omitted, the rule has no effect.",
+		OneOf: []apiextensionsv1beta1.JSONSchemaProps{
+			{
+				Required: []string{"endpointSelector"},
+			},
+			{
+				Required: []string{"nodeSelector"},
+			},
 		},
 		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
 			"Description": {
@@ -1468,6 +1473,7 @@ var (
 				},
 			},
 			"endpointSelector": EndpointSelector,
+			"nodeSelector":     EndpointSelector,
 			"ingress": {
 				Description: "Ingress is a list of IngressRule which are enforced at ingress. " +
 					"If omitted or empty, this rule does not apply at ingress.",
@@ -1534,8 +1540,13 @@ func init() {
 
 	ruleProps := Rule.Properties["endpointSelector"]
 	ruleProps.Description = "EndpointSelector selects all endpoints which should be subject " +
-		"to this rule. Cannot be empty."
+		"to this rule. Cannot be empty if nodeSelector is empty."
 	Rule.Properties["endpointSelector"] = ruleProps
+
+	ruleProps = Rule.Properties["nodeSelector"]
+	ruleProps.Description = "NodeSelector selects all nodes which should be subject " +
+		"to this rule. Cannot be empty if endpointSelector is empty."
+	Rule.Properties["nodeSelector"] = ruleProps
 
 	serviceProps := Service.Properties["k8sServiceSelector"]
 	serviceProps.Description = "K8sServiceSelector selects services by k8s labels. " +

pkg/policy/api/rule.go
@@ -15,7 +15,9 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 
 	"github.com/cilium/cilium/pkg/labels"
 )
@@ -34,8 +36,18 @@ import (
 // +deepequal-gen:private-method=true
 type Rule struct {
 	// EndpointSelector selects all endpoints which should be subject to
-	// this rule. Cannot be empty.
-	EndpointSelector EndpointSelector `json:"endpointSelector"`
+	// this rule. EndpointSelector and NodeSelector cannot be both empty and
+	// are mutually exclusive.
+	//
+	// +optional
+	EndpointSelector EndpointSelector `json:"endpointSelector,omitempty"`
+
+	// NodeSelector selects all nodes which should be subject to this rule.
+	// EndpointSelector and NodeSelector cannot be both empty and are mutually
+	// exclusive.
+	//
+	// +optional
+	NodeSelector EndpointSelector `json:"nodeSelector,omitempty"`
 
 	// Ingress is a list of IngressRule which are enforced at ingress.
 	// If omitted or empty, this rule does not apply at ingress.
@@ -63,6 +75,70 @@ type Rule struct {
 	//
 	// +optional
 	Description string `json:"description,omitempty"`
+}
+
+// MarshalJSON returns the JSON encoding of Rule r. We need to overwrite it to
+// enforce omitempty on the EndpointSelector nested structures.
+func (r *Rule) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+
+	if r.EndpointSelector.LabelSelector != nil {
+		jsonValue, err := json.Marshal(r.EndpointSelector)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write([]byte(`"endpointSelector":`))
+		buf.Write(jsonValue)
+	}
+
+	if r.NodeSelector.LabelSelector != nil {
+		jsonValue, err := json.Marshal(r.NodeSelector)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write([]byte(`"nodeSelector":`))
+		buf.Write(jsonValue)
+	}
+
+	if r.Ingress != nil {
+		jsonValue, err := json.Marshal(r.Ingress)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write([]byte(`,"ingress":`))
+		buf.Write(jsonValue)
+	}
+
+	if r.Egress != nil {
+		jsonValue, err := json.Marshal(r.Egress)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write([]byte(`,"egress":`))
+		buf.Write(jsonValue)
+	}
+
+	if r.Labels != nil {
+		jsonValue, err := json.Marshal(r.Labels)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write([]byte(`,"labels":`))
+		buf.Write(jsonValue)
+	}
+
+	if r.Description != "" {
+		jsonValue, err := json.Marshal(r.Description)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write([]byte(`,"description":`))
+		buf.Write(jsonValue)
+	}
+
+	buf.WriteByte('}')
+	return buf.Bytes(), nil
 }
 
 func (r *Rule) DeepEqual(o *Rule) bool {

pkg/policy/api/rule_test.go
@@ -1,0 +1,49 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package api
+
+import (
+	"encoding/json"
+
+	. "gopkg.in/check.v1"
+)
+
+func checkMarshalUnmarshal(c *C, r *Rule) {
+	jsonData, err := json.Marshal(r)
+	c.Assert(err, IsNil)
+
+	newRule := Rule{}
+	err = json.Unmarshal(jsonData, &newRule)
+	c.Assert(err, IsNil)
+
+	c.Check(newRule.EndpointSelector.LabelSelector == nil, Equals, r.EndpointSelector.LabelSelector == nil)
+	c.Check(newRule.NodeSelector.LabelSelector == nil, Equals, r.NodeSelector.LabelSelector == nil)
+}
+
+// This test ensures that the NodeSelector and EndpointSelector fields are kept
+// empty when the rule is marshalled/unmarshalled.
+func (s *PolicyAPITestSuite) TestJSONMarshalling(c *C) {
+	validEndpointRule := Rule{
+		EndpointSelector: WildcardEndpointSelector,
+	}
+	checkMarshalUnmarshal(c, &validEndpointRule)
+
+	validNodeRule := Rule{
+		NodeSelector: WildcardEndpointSelector,
+	}
+	checkMarshalUnmarshal(c, &validNodeRule)
+}

pkg/policy/api/rule_validation.go
@@ -46,23 +46,46 @@ func (r Rule) Sanitize() error {
 		}
 	}
 
-	if r.EndpointSelector.LabelSelector == nil {
-		return fmt.Errorf("rule cannot have nil EndpointSelector")
+	if r.EndpointSelector.LabelSelector == nil && r.NodeSelector.LabelSelector == nil {
+		return fmt.Errorf("rule must have one of EndpointSelector or NodeSelector")
+	}
+	if r.EndpointSelector.LabelSelector != nil && r.NodeSelector.LabelSelector != nil {
+		return fmt.Errorf("rule cannot have both EndpointSelector and NodeSelector")
 	}
 
-	if err := r.EndpointSelector.sanitize(); err != nil {
-		return err
+	if r.EndpointSelector.LabelSelector != nil {
+		if err := r.EndpointSelector.sanitize(); err != nil {
+			return err
+		}
+	}
+
+	var hostPolicy bool
+	if r.NodeSelector.LabelSelector != nil {
+		if err := r.NodeSelector.sanitize(); err != nil {
+			return err
+		}
+		hostPolicy = true
 	}
 
 	for i := range r.Ingress {
 		if err := r.Ingress[i].sanitize(); err != nil {
 			return err
 		}
+		if hostPolicy {
+			if len(countL7Rules(r.Ingress[i].ToPorts)) > 0 {
+				return fmt.Errorf("host policies do not support L7 rules yet")
+			}
+		}
 	}
 
 	for i := range r.Egress {
 		if err := r.Egress[i].sanitize(); err != nil {
 			return err
+		}
+		if hostPolicy {
+			if len(countL7Rules(r.Egress[i].ToPorts)) > 0 {
+				return fmt.Errorf("host policies do not support L7 rules yet")
+			}
 		}
 	}
 

pkg/policy/api/rule_validation_test.go
@@ -421,6 +421,62 @@ func (s *PolicyAPITestSuite) TestL7Rules(c *C) {
 	c.Assert(err, Not(IsNil))
 }
 
+// This test ensures that host policies with L7 rules are rejected.
+func (s *PolicyAPITestSuite) TestL7RulesWithNodeSelector(c *C) {
+	invalidL7RuleIngress := Rule{
+		NodeSelector: WildcardEndpointSelector,
+		Ingress: []IngressRule{
+			{
+				FromEndpoints: []EndpointSelector{WildcardEndpointSelector},
+				ToPorts: []PortRule{{
+					Ports: []PortProtocol{
+						{Port: "80", Protocol: ProtoTCP},
+					},
+					Rules: &L7Rules{
+						HTTP: []PortRuleHTTP{
+							{Method: "PUT", Path: "/"},
+						},
+					},
+				}},
+			},
+		},
+	}
+	err := invalidL7RuleIngress.Sanitize()
+	c.Assert(err.Error(), Equals, "host policies do not support L7 rules yet")
+
+	invalidL7RuleEgress := Rule{
+		NodeSelector: WildcardEndpointSelector,
+		Egress: []EgressRule{
+			{
+				ToEndpoints: []EndpointSelector{WildcardEndpointSelector},
+				ToPorts: []PortRule{{
+					Ports: []PortProtocol{
+						{Port: "53", Protocol: ProtoUDP},
+					},
+					Rules: &L7Rules{
+						DNS: []PortRuleDNS{
+							{MatchName: "domain.com"},
+						},
+					},
+				}},
+			},
+		},
+	}
+	err = invalidL7RuleEgress.Sanitize()
+	c.Assert(err.Error(), Equals, "host policies do not support L7 rules yet")
+
+	validL7RuleIngress := Rule{
+		NodeSelector: WildcardEndpointSelector,
+		Ingress: []IngressRule{
+			{
+				FromEndpoints: []EndpointSelector{WildcardEndpointSelector},
+			},
+		},
+	}
+	err = validL7RuleIngress.Sanitize()
+	c.Assert(err, IsNil)
+}
+
 func (s *PolicyAPITestSuite) TestInvalidEndpointSelectors(c *C) {
 
 	// Operator in MatchExpressions is invalid, so sanitization should fail.
@@ -495,6 +551,41 @@ func (s *PolicyAPITestSuite) TestInvalidEndpointSelectors(c *C) {
 	err = invalidEpSelectorEgressToReq.Sanitize()
 	c.Assert(err, Not(IsNil))
 
+}
+
+func (s *PolicyAPITestSuite) TestNodeSelector(c *C) {
+	// Operator in MatchExpressions is invalid, so sanitization should fail.
+	labelSel := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]string{
+			"any.foo": "bar",
+			"k8s.baz": "alice",
+		},
+		MatchExpressions: []slim_metav1.LabelSelectorRequirement{
+			{
+				Key:      "any.foo",
+				Operator: "asdfasdfasdf",
+				Values:   []string{"default"},
+			},
+		},
+	}
+	invalidSel := NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, labelSel)
+	invalidNodeSelectorRule := Rule{
+		NodeSelector: invalidSel,
+	}
+	err := invalidNodeSelectorRule.Sanitize()
+	c.Assert(err.Error(), Equals,
+		"invalid label selector: matchExpressions[0].operator: Invalid value: \"asdfasdfasdf\": not a valid selector operator")
+
+	invalidRuleBothSelectors := Rule{
+		EndpointSelector: WildcardEndpointSelector,
+		NodeSelector:     WildcardEndpointSelector,
+	}
+	err = invalidRuleBothSelectors.Sanitize()
+	c.Assert(err.Error(), Equals, "rule cannot have both EndpointSelector and NodeSelector")
+
+	invalidRuleNoSelector := Rule{}
+	err = invalidRuleNoSelector.Sanitize()
+	c.Assert(err.Error(), Equals, "rule must have one of EndpointSelector or NodeSelector")
 }
 
 func (s *PolicyAPITestSuite) TestTooManyPortsRule(c *C) {

pkg/policy/api/zz_generated.deepcopy.go
@@ -644,6 +644,7 @@ func (in PortRuleL7) DeepCopy() PortRuleL7 {
 func (in *Rule) DeepCopyInto(out *Rule) {
 	*out = *in
 	in.EndpointSelector.DeepCopyInto(&out.EndpointSelector)
+	in.NodeSelector.DeepCopyInto(&out.NodeSelector)
 	if in.Ingress != nil {
 		in, out := &in.Ingress, &out.Ingress
 		*out = make([]IngressRule, len(*in))

pkg/policy/api/zz_generated.deepequal.go
@@ -894,6 +894,10 @@ func (in *Rule) deepEqual(other *Rule) bool {
 		return false
 	}
 
+	if !in.NodeSelector.DeepEqual(&other.NodeSelector) {
+		return false
+	}
+
 	if ((in.Ingress != nil) && (other.Ingress != nil)) || ((in.Ingress == nil) != (other.Ingress == nil)) {
 		in, other := &in.Ingress, &other.Ingress
 		if other == nil {

pkg/policy/identifier.go
@@ -25,10 +25,12 @@ import (
 // * a node-local ID stored as a uint16
 // * a security identity
 // * a means of incrementing its policy revision
+// * a means of checking if it represents a node or a pod.
 type Endpoint interface {
 	GetID16() uint16
 	GetSecurityIdentity() (*identity.Identity, error)
 	PolicyRevisionBumpEvent(rev uint64)
+	IsHost() bool
 }
 
 // EndpointSet is used to be able to group together a given set of Endpoints

commit 266023fb574f9890f0c9b2747fee0ab3e96cde3f

k8s: Fix CCNP for host policies
This commit fixes the CCNP parsing to accept NodeSelectors, used to
define host policies. NodeSelectors are only accepted in CCNP resources.
Because host policies are never limited to a namespace, it would be
misleading to accept NodeSelectors in CNP resources.

This commit also adds a new CCNP example of host policies in the same
directory as the existing JSON example.

Fixes: f069507 ("pkg/policy: Host network policies")
Signed-off-by: Paul Chaignon <paul@cilium.io>

examples/policies/l3/host/host-policy.yaml
@@ -1,0 +1,29 @@
+apiVersion: "cilium.io/v2"
+kind: CiliumClusterwideNetworkPolicy
+description: "Allow ports for kube-dns, kube-api, Cilium, and SSH, as well as queries to Google's DNS on UDP/53 only"
+metadata:
+  name: "allow-google-dns-on-udp-53-only"
+spec:
+  nodeSelector:
+    matchLabels:
+      {}
+  ingress:
+  - toPorts:
+    - ports:
+      - port: "6443"
+        protocol: TCP
+      - port: "22"
+        protocol: TCP
+  egress:
+  - toPorts:
+    - ports:
+      - port: "4240"
+        protocol: TCP
+      - port: "8080"
+        protocol: TCP
+  - toCIDR:
+    - "8.8.0.0/16"
+    toPorts:
+    - ports:
+      - port: "53"
+        protocol: UDP

pkg/k8s/apis/cilium.io/utils/utils.go
@@ -101,7 +101,7 @@ func getEndpointSelector(namespace string, labelSelector *slim_metav1.LabelSelec
 }
 
 func parseToCiliumIngressRule(namespace string, inRule, retRule *api.Rule) {
-	matchesInit := retRule.EndpointSelector.HasKey(podInitLbl)
+	matchesInit := matchesPodInit(retRule.EndpointSelector)
 
 	if inRule.Ingress != nil {
 		retRule.Ingress = make([]api.IngressRule, len(inRule.Ingress))
@@ -145,7 +145,7 @@ func parseToCiliumIngressRule(namespace string, inRule, retRule *api.Rule) {
 }
 
 func parseToCiliumEgressRule(namespace string, inRule, retRule *api.Rule) {
-	matchesInit := retRule.EndpointSelector.HasKey(podInitLbl)
+	matchesInit := matchesPodInit(retRule.EndpointSelector)
 
 	if inRule.Egress != nil {
 		retRule.Egress = make([]api.EgressRule, len(inRule.Egress))
@@ -204,6 +204,13 @@ func parseToCiliumEgressRule(namespace string, inRule, retRule *api.Rule) {
 	}
 }
 
+func matchesPodInit(epSelector api.EndpointSelector) bool {
+	if epSelector.LabelSelector == nil {
+		return false
+	}
+	return epSelector.HasKey(podInitLbl)
+}
+
 // namespacesAreValid checks the set of namespaces from a rule returns true if
 // they are not specified, or if they are specified and match the namespace
 // where the rule is being inserted.
@@ -240,6 +247,8 @@ func ParseToCiliumRule(namespace, name string, uid types.UID, r *api.Rule) *api.
 			}
 			retRule.EndpointSelector.AddMatch(podPrefixLbl, namespace)
 		}
+	} else if r.NodeSelector.LabelSelector != nil {
+		retRule.NodeSelector = api.NewESFromK8sLabelSelector("", r.NodeSelector.LabelSelector)
 	}
 
 	parseToCiliumIngressRule(namespace, r, retRule)

pkg/k8s/apis/cilium.io/v2/client/register.go
@@ -54,7 +54,7 @@ const (
 
 	// CustomResourceDefinitionSchemaVersion is semver-conformant version of CRD schema
 	// Used to determine if CRD needs to be updated in cluster
-	CustomResourceDefinitionSchemaVersion = "1.19"
+	CustomResourceDefinitionSchemaVersion = "1.20"
 
 	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
 	CustomResourceDefinitionSchemaVersionKey = "io.cilium.k8s.crd.schema.version"
@@ -182,6 +182,27 @@ func createCCNPCRD(clientset apiextensionsclient.Interface) error {
 		CRDName = CustomResourceDefinitionPluralName + "." + SchemeGroupVersion.Group
 	)
 
+	CCNPCRV := CNPCRV.DeepCopy()
+	clusterwideSpec := CCNPCRV.OpenAPIV3Schema.Properties["spec"]
+	clusterwideSpec.Required = nil
+	clusterwideSpec.OneOf = []apiextensionsv1beta1.JSONSchemaProps{
+		{
+			Required: []string{"endpointSelector"},
+		},
+		{
+			Required: []string{"nodeSelector"},
+		},
+	}
+	CCNPCRV.OpenAPIV3Schema.Properties["spec"] = clusterwideSpec
+	CCNPCRV.OpenAPIV3Schema.Properties["specs"] = apiextensionsv1beta1.JSONSchemaProps{
+		Description: "Specs is a list of desired Cilium specific rule specification.",
+		Type:        "array",
+		Items: &apiextensionsv1beta1.JSONSchemaPropsOrArray{
+			Schema: &clusterwideSpec,
+		},
+	}
+	CCNPCRV.OpenAPIV3Schema.Properties["NodeSelector"] = NodeSelector
+
 	res := &apiextensionsv1beta1.CustomResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: CRDName,
@@ -202,7 +223,7 @@ func createCCNPCRD(clientset apiextensionsclient.Interface) error {
 				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
 			},
 			Scope:      apiextensionsv1beta1.ClusterScoped,
-			Validation: &CNPCRV,
+			Validation: CCNPCRV,
 		},
 	}
 	// Kubernetes < 1.12 does not support having the field Type set in the root
@@ -877,7 +898,9 @@ var (
 			},
 		},
 	}
+
 	EndpointSelector = *LabelSelector.DeepCopy()
+	NodeSelector     = *LabelSelector.DeepCopy()
 
 	IngressRule = apiextensionsv1beta1.JSONSchemaProps{
 		Type: "object",
@@ -1449,14 +1472,7 @@ var (
 			"ingress and egress, both ingress and egress side have to either specifically allow " +
 			"the connection or one side has to be omitted.\n\nEither ingress, egress, or both " +
 			"can be provided. If both ingress and egress are omitted, the rule has no effect.",
-		OneOf: []apiextensionsv1beta1.JSONSchemaProps{
-			{
-				Required: []string{"endpointSelector"},
-			},
-			{
-				Required: []string{"nodeSelector"},
-			},
-		},
+		Required: []string{"endpointSelector"},
 		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
 			"Description": {
 				Description: "Description is a free form string, it can be used by the creator " +
@@ -1473,7 +1489,6 @@ var (
 				},
 			},
 			"endpointSelector": EndpointSelector,
-			"nodeSelector":     EndpointSelector,
 			"ingress": {
 				Description: "Ingress is a list of IngressRule which are enforced at ingress. " +
 					"If omitted or empty, this rule does not apply at ingress.",
@@ -1542,11 +1557,6 @@ func init() {
 	ruleProps.Description = "EndpointSelector selects all endpoints which should be subject " +
 		"to this rule. Cannot be empty if nodeSelector is empty."
 	Rule.Properties["endpointSelector"] = ruleProps
-
-	ruleProps = Rule.Properties["nodeSelector"]
-	ruleProps.Description = "NodeSelector selects all nodes which should be subject " +
-		"to this rule. Cannot be empty if endpointSelector is empty."
-	Rule.Properties["nodeSelector"] = ruleProps
 
 	serviceProps := Service.Properties["k8sServiceSelector"]
 	serviceProps.Description = "K8sServiceSelector selects services by k8s labels. " +

pkg/k8s/apis/cilium.io/v2/types.go
@@ -260,7 +260,6 @@ func (r *CiliumNetworkPolicy) Parse() (api.Rules, error) {
 	if r.Spec != nil {
 		if err := r.Spec.Sanitize(); err != nil {
 			return nil, fmt.Errorf("Invalid CiliumNetworkPolicy spec: %s", err)
-
 		}
 		cr := k8sCiliumUtils.ParseToCiliumRule(namespace, name, uid, r.Spec)
 		retRules = append(retRules, cr)