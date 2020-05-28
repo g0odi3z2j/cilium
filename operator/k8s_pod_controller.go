g0odi3z2j/cilium

commit 0afebab326fa789a926deeb35cf12ee70d8719ea

operator: use watcher to track unmanaged pods
Listing all pods, even if with a filter will load the etcd kvstore as
the filtering is done in kube-apiserver. To avoid doing this operation
every 10 seconds we can have a single watcher that keeps track of all
unmanaged kube-dns pods locally.

Signed-off-by: André Martins <andre@cilium.io>

operator/k8s_pod_controller.go
@@ -20,10 +20,11 @@ import (
 	"time"
 
 	operatorOption "github.com/cilium/cilium/operator/option"
+	"github.com/cilium/cilium/operator/watchers"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/core/v1"
 
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -37,6 +38,10 @@ var (
 )
 
 func enableUnmanagedKubeDNSController() {
+	// These functions will block until the resources are synced with k8s.
+	watchers.CiliumEndpointsInit(k8s.CiliumClient().CiliumV2())
+	watchers.UnmanagedPodsInit(k8s.WatcherCli())
+
 	controller.NewManager().UpdateController("restart-unmanaged-kube-dns",
 		controller.ControllerParams{
 			RunInterval: time.Duration(operatorOption.Config.UnmanagedPodWatcherInterval) * time.Second,
@@ -46,27 +51,21 @@ func enableUnmanagedKubeDNSController() {
 						delete(lastPodRestart, podName)
 					}
 				}
-
-				pods, err := k8s.Client().CoreV1().Pods("").List(ctx,
-					metav1.ListOptions{
-						LabelSelector: "k8s-app=kube-dns",
-						FieldSelector: "status.phase=Running",
-					})
-				if err != nil {
-					return err
-				}
-
-				for _, pod := range pods.Items {
+				for _, podItem := range watchers.UnmanagedPodStore.List() {
+					pod, ok := podItem.(*slim_corev1.Pod)
+					if !ok {
+						log.Errorf("unexpected type mapping: found %T, expected %T", pod, &slim_corev1.Pod{})
+						continue
+					}
 					if pod.Spec.HostNetwork {
 						continue
 					}
-					cep, err := ciliumK8sClient.CiliumV2().CiliumEndpoints(pod.Namespace).Get(ctx,
-						pod.Name, metav1.GetOptions{})
+					cep, exists, err := watchers.HasCE(pod.Namespace, pod.Name)
 					podID := fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
 					switch {
 					case err == nil:
 						log.Debugf("Found kube-dns pod %s with identity %d", podID, cep.Status.ID)
-					case errors.IsNotFound(err):
+					case !exists:
 						log.Debugf("Found unmanaged kube-dns pod %s", podID)
 						if startTime := pod.Status.StartTime; startTime != nil {
 							if age := time.Since((*startTime).Time); age > unmanagedKubeDnsMinimalAge {

operator/main.go
@@ -157,7 +157,7 @@ func runOperator(cmd *cobra.Command) {
 	if option.Config.DisableCiliumEndpointCRD {
 		log.Infof("KubeDNS unmanaged pods controller disabled as %q option is set to 'disabled' in Cilium ConfigMap", option.DisableCiliumEndpointCRDName)
 	} else if operatorOption.Config.UnmanagedPodWatcherInterval != 0 {
-		enableUnmanagedKubeDNSController()
+		go enableUnmanagedKubeDNSController()
 	}
 
 	var (

operator/watchers/cilium_endpoint.go
@@ -18,6 +18,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"sync"
 
 	cilium_api_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	cilium_cli "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/typed/cilium.io/v2"
@@ -48,6 +49,8 @@ var (
 	// CiliumEndpointsSynced is closed once the CiliumEndpointStore is synced
 	// with k8s.
 	CiliumEndpointsSynced = make(chan struct{})
+	// once is used to make sure CiliumEndpointsInit is only setup once.
+	once sync.Once
 )
 
 // identityIndexFunc index identities by ID.
@@ -65,21 +68,23 @@ func identityIndexFunc(obj interface{}) ([]string, error) {
 
 // CiliumEndpointsInit starts a CiliumEndpointWatcher
 func CiliumEndpointsInit(ciliumNPClient cilium_cli.CiliumV2Interface) {
-	CiliumEndpointStore = cache.NewIndexer(cache.DeletionHandlingMetaNamespaceKeyFunc, indexers)
+	once.Do(func() {
+		CiliumEndpointStore = cache.NewIndexer(cache.DeletionHandlingMetaNamespaceKeyFunc, indexers)
 
-	ciliumEndpointInformer := informer.NewInformerWithStore(
-		cache.NewListWatchFromClient(ciliumNPClient.RESTClient(),
-			"ciliumendpoints", v1.NamespaceAll, fields.Everything()),
-		&cilium_api_v2.CiliumEndpoint{},
-		0,
-		cache.ResourceEventHandlerFuncs{},
-		convertToCiliumEndpoint,
-		CiliumEndpointStore,
-	)
-	go ciliumEndpointInformer.Run(wait.NeverStop)
+		ciliumEndpointInformer := informer.NewInformerWithStore(
+			cache.NewListWatchFromClient(ciliumNPClient.RESTClient(),
+				"ciliumendpoints", v1.NamespaceAll, fields.Everything()),
+			&cilium_api_v2.CiliumEndpoint{},
+			0,
+			cache.ResourceEventHandlerFuncs{},
+			convertToCiliumEndpoint,
+			CiliumEndpointStore,
+		)
+		go ciliumEndpointInformer.Run(wait.NeverStop)
 
-	cache.WaitForCacheSync(wait.NeverStop, ciliumEndpointInformer.HasSynced)
-	close(CiliumEndpointsSynced)
+		cache.WaitForCacheSync(wait.NeverStop, ciliumEndpointInformer.HasSynced)
+		close(CiliumEndpointsSynced)
+	})
 }
 
 // convertToCiliumEndpoint converts a CiliumEndpoint to a minimal CiliumEndpoint
@@ -138,4 +143,22 @@ func HasCEWithIdentity(identity string) bool {
 	ces, _ := CiliumEndpointStore.IndexKeys(identityIndex, identity)
 
 	return len(ces) != 0
+}
+
+// HasCE returns true or false if the Cilium Endpoint store has the endpoint
+// with the given name.
+func HasCE(ns, name string) (*cilium_api_v2.CiliumEndpoint, bool, error) {
+	if CiliumEndpointStore == nil {
+		return nil, false, nil
+	}
+	cepKey := fmt.Sprintf("%s/%s", ns, name)
+	item, exists, err := CiliumEndpointStore.GetByKey(cepKey)
+	if err != nil {
+		return nil, false, err
+	}
+	if !exists {
+		return nil, false, nil
+	}
+	cep := item.(*cilium_api_v2.CiliumEndpoint)
+	return cep, exists, nil
 }

operator/watchers/pod.go
@@ -20,6 +20,7 @@ import (
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
@@ -34,6 +35,16 @@ var (
 
 	// PodStoreSynced is closed once the PodStore is synced with k8s.
 	PodStoreSynced = make(chan struct{})
+
+	// UnmanagedPodStore has a minimal copy of the unmanaged kube-dns pods running
+	// in the cluster.
+	// Warning: The pods stored in the cache are not intended to be used for Update
+	// operations in k8s as some of its fields were are not populated.
+	UnmanagedPodStore cache.Store
+
+	// UnmanagedPodStoreSynced is closed once the UnmanagedPodStore is synced
+	// with k8s.
+	UnmanagedPodStoreSynced = make(chan struct{})
 )
 
 func PodsInit(k8sClient kubernetes.Interface) {
@@ -81,6 +92,74 @@ func convertToPod(obj interface{}) interface{} {
 					Name:            pod.Name,
 					Namespace:       pod.Namespace,
 					ResourceVersion: pod.ResourceVersion,
+				},
+			},
+		}
+		// Small GC optimization
+		*pod = slim_corev1.Pod{}
+		return dfsu
+	default:
+		return obj
+	}
+}
+
+func UnmanagedPodsInit(k8sClient kubernetes.Interface) {
+	var unmanagedPodInformer cache.Controller
+	UnmanagedPodStore, unmanagedPodInformer = informer.NewInformer(
+		cache.NewFilteredListWatchFromClient(k8sClient.CoreV1().RESTClient(),
+			"pods", v1.NamespaceAll, func(options *metav1.ListOptions) {
+				options.LabelSelector = "k8s-app=kube-dns"
+				options.FieldSelector = "status.phase=Running"
+			}),
+		&slim_corev1.Pod{},
+		0,
+		cache.ResourceEventHandlerFuncs{},
+		convertToUnmanagedPod,
+	)
+	go unmanagedPodInformer.Run(wait.NeverStop)
+
+	cache.WaitForCacheSync(wait.NeverStop, unmanagedPodInformer.HasSynced)
+	close(UnmanagedPodStoreSynced)
+}
+
+func convertToUnmanagedPod(obj interface{}) interface{} {
+	switch concreteObj := obj.(type) {
+	case *slim_corev1.Pod:
+		p := &slim_corev1.Pod{
+			TypeMeta: concreteObj.TypeMeta,
+			ObjectMeta: slim_metav1.ObjectMeta{
+				Name:            concreteObj.Name,
+				Namespace:       concreteObj.Namespace,
+				ResourceVersion: concreteObj.ResourceVersion,
+			},
+			Spec: slim_corev1.PodSpec{
+				HostNetwork: concreteObj.Spec.HostNetwork,
+			},
+			Status: slim_corev1.PodStatus{
+				StartTime: concreteObj.Status.StartTime,
+			},
+		}
+		*concreteObj = slim_corev1.Pod{}
+		return p
+	case cache.DeletedFinalStateUnknown:
+		pod, ok := concreteObj.Obj.(*slim_corev1.Pod)
+		if !ok {
+			return obj
+		}
+		dfsu := cache.DeletedFinalStateUnknown{
+			Key: concreteObj.Key,
+			Obj: &slim_corev1.Pod{
+				TypeMeta: pod.TypeMeta,
+				ObjectMeta: slim_metav1.ObjectMeta{
+					Name:            pod.Name,
+					Namespace:       pod.Namespace,
+					ResourceVersion: pod.ResourceVersion,
+				},
+				Spec: slim_corev1.PodSpec{
+					HostNetwork: pod.Spec.HostNetwork,
+				},
+				Status: slim_corev1.PodStatus{
+					StartTime: pod.Status.StartTime,
 				},
 			},
 		}

pkg/k8s/slim/k8s/apis/core/v1/generated.proto
@@ -415,6 +415,11 @@ message PodStatus {
   // +patchStrategy=merge
   // +patchMergeKey=ip
   repeated PodIP podIPs = 12;
+
+  // RFC 3339 date and time at which the object was acknowledged by the Kubelet.
+  // This is before the Kubelet pulled the container image(s) for the pod.
+  // +optional
+  optional github.com.cilium.cilium.pkg.k8s.slim.k8s.apis.meta.v1.Time startTime = 7;
 }
 
 // Service is a named abstraction of software service (for example, mysql) consisting of local port

pkg/k8s/slim/k8s/apis/core/v1/types.go
@@ -213,6 +213,11 @@ type PodStatus struct {
 	// +patchStrategy=merge
 	// +patchMergeKey=ip
 	PodIPs []PodIP `json:"podIPs,omitempty" protobuf:"bytes,12,rep,name=podIPs" patchStrategy:"merge" patchMergeKey:"ip"`
+
+	// RFC 3339 date and time at which the object was acknowledged by the Kubelet.
+	// This is before the Kubelet pulled the container image(s) for the pod.
+	// +optional
+	StartTime *slim_metav1.Time `json:"startTime,omitempty" protobuf:"bytes,7,opt,name=startTime"`
 }
 
 // +genclient

pkg/k8s/slim/k8s/apis/core/v1/zz_generated.deepcopy.go
@@ -579,6 +579,10 @@ func (in *PodStatus) DeepCopyInto(out *PodStatus) {
 		*out = make([]PodIP, len(*in))
 		copy(*out, *in)
 	}
+	if in.StartTime != nil {
+		in, out := &in.StartTime, &out.StartTime
+		*out = (*in).DeepCopy()
+	}
 	return
 }
 

commit 30b6f750a2df5aeb13326afde3a4392449ad4521

operator: fix panic for non existing CEPs
```
panic: runtime error: invalid memory address or nil pointer dereference
[signal SIGSEGV: segmentation violation code=0x1 addr=0x118 pc=0x1dcb219]
goroutine 161 [running]:
main.enableUnmanagedKubeDNSController.func1(0x28aa1a0, 0xc00069e880, 0x3d3d0a0, 0xd9fc43ebeec50347)
	/go/src/github.com/cilium/cilium/operator/k8s_pod_controller.go:67 +0xa69
github.com/cilium/cilium/pkg/controller.(*Controller).runController(0xc000789a00)
	/go/src/github.com/cilium/cilium/pkg/controller/controller.go:205 +0xa2a
created by github.com/cilium/cilium/pkg/controller.(*Manager).updateController
	/go/src/github.com/cilium/cilium/pkg/controller/manager.go:120 +0xb09
```

Fixes: 0afebab326fa ("operator: use watcher to track unmanaged pods")
Reported-by: Tobias Klauser <tklauser@distanz.ch>
Signed-off-by: André Martins <andre@cilium.io>

operator/k8s_pod_controller.go
@@ -61,11 +61,14 @@ func enableUnmanagedKubeDNSController() {
 						continue
 					}
 					cep, exists, err := watchers.HasCE(pod.Namespace, pod.Name)
+					if err != nil {
+						log.WithError(err).Errorf("unexpected error when getting CiliumEndpoint %s/%s", pod.Namespace, pod.Name)
+						continue
+					}
 					podID := fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
-					switch {
-					case err == nil:
+					if exists {
 						log.Debugf("Found kube-dns pod %s with identity %d", podID, cep.Status.ID)
-					case !exists:
+					} else {
 						log.Debugf("Found unmanaged kube-dns pod %s", podID)
 						if startTime := pod.Status.StartTime; startTime != nil {
 							if age := time.Since((*startTime).Time); age > unmanagedKubeDnsMinimalAge {
@@ -88,8 +91,6 @@ func enableUnmanagedKubeDNSController() {
 
 							}
 						}
-					default:
-						return err
 					}
 				}
 