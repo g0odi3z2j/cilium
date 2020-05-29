g0odi3z2j/cilium

commit b180709396089fd2dcbf80ccc7bfa5edcba067f5

pkg/endpoint: fetch pod and namespace labels from local stores
This commit reduces the number of k8s apiserver calls by switching to a
local store fetch logic for Pods an Namespaces.

Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/endpoint.go
@@ -1487,7 +1487,7 @@ func (e *Endpoint) getIDandLabels() string {
 
 // MetadataResolverCB provides an implementation for resolving the endpoint
 // metadata for an endpoint such as the associated labels and annotations.
-type MetadataResolverCB func(*Endpoint) (identityLabels labels.Labels, infoLabels labels.Labels, annotations map[string]string, err error)
+type MetadataResolverCB func(ns, podName string) (identityLabels labels.Labels, infoLabels labels.Labels, annotations map[string]string, err error)
 
 // RunMetadataResolver starts a controller associated with the received
 // endpoint which will periodically attempt to resolve the metadata for the
@@ -1508,13 +1508,14 @@ func (e *Endpoint) RunMetadataResolver(resolveMetadata MetadataResolverCB) {
 	e.controllers.UpdateController(controllerName,
 		controller.ControllerParams{
 			DoFunc: func(ctx context.Context) error {
-				identityLabels, info, _, err := resolveMetadata(e)
+				ns, podName := e.GetK8sNamespace(), e.GetK8sPodName()
+				identityLabels, info, _, err := resolveMetadata(ns, podName)
 				if err != nil {
 					e.Logger(controllerName).WithError(err).Warning("Unable to fetch kubernetes labels")
 					return err
 				}
 				e.UpdateVisibilityPolicy(func(_, _ string) (proxyVisibility string, err error) {
-					_, _, annotations, err := resolveMetadata(e)
+					_, _, annotations, err := resolveMetadata(ns, podName)
 					if err != nil {
 						return "", err
 					}

daemon/endpoint.go
@@ -148,8 +148,17 @@ func NewPutEndpointIDHandler(d *Daemon) PutEndpointIDHandler {
 
 // fetchK8sLabelsAndAnnotations wraps the k8s package to fetch and provide
 // endpoint metadata. It implements endpoint.MetadataResolverCB.
-func fetchK8sLabelsAndAnnotations(ep *endpoint.Endpoint) (labels.Labels, labels.Labels, map[string]string, error) {
-	lbls, annotations, err := k8s.GetPodMetadata(ep.GetK8sNamespace(), ep.GetK8sPodName())
+func (d *Daemon) fetchK8sLabelsAndAnnotations(nsName, podName string) (labels.Labels, labels.Labels, map[string]string, error) {
+	p, err := d.k8sWatcher.GetCachedPod(nsName, podName)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	ns, err := d.k8sWatcher.GetCachedNamespace(nsName)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	lbls, annotations, err := k8s.GetPodMetadata(ns, p)
 	if err != nil {
 		return nil, nil, nil, err
 	}
@@ -250,7 +259,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 	}
 
 	if ep.K8sNamespaceAndPodNameIsSet() && k8s.IsEnabled() {
-		identityLabels, info, _, err := fetchK8sLabelsAndAnnotations(ep)
+		identityLabels, info, _, err := d.fetchK8sLabelsAndAnnotations(ep.K8sNamespace, ep.K8sPodName)
 		if err != nil {
 			ep.Logger("api").WithError(err).Warning("Unable to fetch kubernetes labels")
 		} else {
@@ -284,7 +293,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 		// If there are labels, but no pod namespace, then it's
 		// likely that there are no k8s labels at all. Resolve.
 		if _, k8sLabelsConfigured := addLabels[k8sConst.PodNamespaceLabel]; !k8sLabelsConfigured {
-			ep.RunMetadataResolver(fetchK8sLabelsAndAnnotations)
+			ep.RunMetadataResolver(d.fetchK8sLabelsAndAnnotations)
 		}
 	}
 

daemon/state.go
@@ -35,7 +35,6 @@ import (
 
 	"github.com/sirupsen/logrus"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type endpointRestoreState struct {
@@ -69,19 +68,38 @@ func (d *Daemon) validateEndpoint(ep *endpoint.Endpoint) (valid bool, err error)
 	}
 
 	if ep.K8sPodName != "" && ep.K8sNamespace != "" && k8s.IsEnabled() {
-		p, err := k8s.Client().CoreV1().Pods(ep.K8sNamespace).Get(ep.K8sPodName, meta_v1.GetOptions{})
-		if err != nil && k8serrors.IsNotFound(err) {
-			return false, fmt.Errorf("kubernetes pod not found")
-		}
-		if err == nil {
-			// We were able to get the pod. Given that we get the entire pod
-			// object, including annotations in the above API call, update
-			// the Endpoint's visibility policy accordingly so we don't have
-			// to make another API call later.
+		go func() {
+			err := ep.WaitUntilExposed(context.Background())
+			if err != nil {
+				// If an error has happen it's because the endpoint is not alive
+				return
+			}
+			_, err = d.k8sWatcher.GetCachedPod(ep.K8sNamespace, ep.K8sPodName)
+			if err != nil && k8serrors.IsNotFound(err) {
+				scopedLog := log.WithFields(logrus.Fields{
+					logfields.EndpointID:   ep.ID,
+					logfields.K8sNamespace: ep.K8sNamespace,
+					logfields.K8sPodName:   ep.K8sPodName,
+				})
+				scopedLog.Warningf("kubernetes pod not found, deleting endpoint from endpoint manager")
+				errs := d.deleteEndpointQuiet(ep, endpoint.DeleteConfig{
+					// If the IP is managed by an external IPAM, it does not need to be released
+					NoIPRelease: ep.DatapathConfiguration.ExternalIpam,
+				})
+				for _, err := range errs {
+					scopedLog.WithError(err).Warningf("Error deleting endpoint")
+				}
+				return
+			}
+
 			ep.UpdateVisibilityPolicy(func(ns, podName string) (proxyVisibility string, err error) {
-				return p.Annotations[annotation.ProxyVisibility], nil
+				_, _, annotations, err := d.fetchK8sLabelsAndAnnotations(ns, podName)
+				if err != nil {
+					return "", err
+				}
+				return annotations[annotation.ProxyVisibility], nil
 			})
-		}
+		}()
 	}
 
 	if err := ep.ValidateConnectorPlumbing(connector.CheckLink); err != nil {

pkg/k8s/labels.go
@@ -18,13 +18,12 @@ import (
 	"regexp"
 
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 
 	"github.com/sirupsen/logrus"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -56,7 +55,7 @@ var (
 
 // isInjectedWithIstioSidecarProxy returns whether the given pod has been
 // injected by Istio with a sidecar proxy that is compatible with Cilium.
-func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *corev1.Pod) bool {
+func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *types.Pod) bool {
 	istioStatusString, ok := pod.GetAnnotations()[AnnotationIstioSidecarStatus]
 	if !ok {
 		// Istio's injection annotation was not found.
@@ -69,7 +68,7 @@ func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *corev1.Pod) b
 
 	// Check that there's an "istio-proxy" container that uses an image
 	// compatible with Cilium.
-	for _, container := range pod.Spec.Containers {
+	for _, container := range pod.SpecContainers {
 		if container.Name != "istio-proxy" {
 			continue
 		}
@@ -80,8 +79,8 @@ func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *corev1.Pod) b
 		}
 		scopedLog.Debugf("istio-proxy container runs Cilium-compatible image: %s", container.Image)
 
-		for _, mount := range container.VolumeMounts {
-			if mount.MountPath != "/var/run/cilium" {
+		for _, mountPath := range container.VolumeMountsPaths {
+			if mountPath != "/var/run/cilium" {
 				continue
 			}
 			scopedLog.Debug("istio-proxy container has volume mounted into /var/run/cilium")
@@ -96,26 +95,16 @@ func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *corev1.Pod) b
 
 // GetPodMetadata returns the labels and annotations of the pod with the given
 // namespace / name.
-func GetPodMetadata(namespace, podName string) (lbls map[string]string, retAnno map[string]string, retErr error) {
+func GetPodMetadata(k8sNs *types.Namespace, pod *types.Pod) (lbls map[string]string, retAnno map[string]string, retErr error) {
+	namespace := pod.Namespace
 	scopedLog := log.WithFields(logrus.Fields{
 		logfields.K8sNamespace: namespace,
-		logfields.K8sPodName:   podName,
+		logfields.K8sPodName:   pod.Name,
 	})
-	scopedLog.Debug("Connecting to k8s apiserver to retrieve labels for pod")
+	scopedLog.Debug("Connecting to k8s local stores to retrieve labels for pod")
 
-	result, err := Client().CoreV1().Pods(namespace).Get(podName, metav1.GetOptions{})
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Also get all labels from the namespace where the pod is running
-	k8sNs, err := Client().CoreV1().Namespaces().Get(namespace, metav1.GetOptions{})
-	if err != nil {
-		return nil, nil, err
-	}
-
-	annotations := result.GetAnnotations()
-	k8sLabels := result.GetLabels()
+	annotations := pod.GetAnnotations()
+	k8sLabels := pod.GetLabels()
 	if k8sLabels == nil {
 		k8sLabels = map[string]string{}
 	}
@@ -124,8 +113,8 @@ func GetPodMetadata(namespace, podName string) (lbls map[string]string, retAnno 
 	}
 	k8sLabels[k8sConst.PodNamespaceLabel] = namespace
 
-	if result.Spec.ServiceAccountName != "" {
-		k8sLabels[k8sConst.PolicyLabelServiceAccount] = result.Spec.ServiceAccountName
+	if pod.SpecServiceAccountName != "" {
+		k8sLabels[k8sConst.PolicyLabelServiceAccount] = pod.SpecServiceAccountName
 	} else {
 		delete(k8sLabels, k8sConst.PolicyLabelServiceAccount)
 	}
@@ -135,7 +124,7 @@ func GetPodMetadata(namespace, podName string) (lbls map[string]string, retAnno 
 	// If the pod already contains that label to explicitly enable or disable
 	// the sidecar proxy mode, keep it as is.
 	if _, ok := k8sLabels[k8sConst.PolicyLabelIstioSidecarProxy]; !ok &&
-		isInjectedWithIstioSidecarProxy(scopedLog, result) {
+		isInjectedWithIstioSidecarProxy(scopedLog, pod) {
 		k8sLabels[k8sConst.PolicyLabelIstioSidecarProxy] = "true"
 	}
 

commit b4306265c530587603690397311b60c50d95d7bf

endpoint: Ensure restored endpoint is validated before regenerating it
Due to a performance optimization, validateEndpoint() was changed to use
the pod store instad of contacting the apiserver directly. This required
to make the validation async to wait until the pod cache is available.

This change however caused the endpoint to be exposed and regenerated
before the endpoint was fully validated. This in turn caused the
validation to occur while the regeneration was being performed. When the
validation then failed, the endpoint was deleted while the endpoint was
being regenerated, causing the endpoint to fail with a "JoinEP" message
in the logs.

This commit requires the endpoint to be validated before exposure and
initial regeneration.

Fixes: #11645
Fixes: #11076
Fixes: #10856
Fixes: #10667
Fixes: #10278
Fixes: b1807093960 ("pkg/endpoint: fetch pod and namespace labels from local stores")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/cmd/daemon.go
@@ -146,6 +146,10 @@ type Daemon struct {
 	healthEndpointRouting *linuxrouting.RoutingInfo
 
 	hubbleObserver *observer.LocalObserverServer
+
+	// k8sCachesSynced is closed when all essential Kubernetes caches have
+	// been fully synchronized
+	k8sCachesSynced <-chan struct{}
 }
 
 // GetPolicyRepository returns the policy repository of the daemon
@@ -392,6 +396,8 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 	treatRemoteNodeAsHost := option.Config.AlwaysAllowLocalhost() && !option.Config.EnableRemoteNodeIdentity
 	policyApi.InitEntities(option.Config.ClusterName, treatRemoteNodeAsHost)
 
+	d.k8sCachesSynced = d.k8sWatcher.InitK8sSubsystem()
+
 	bootstrapStats.cleanup.Start()
 	err = clearCiliumVeths()
 	bootstrapStats.cleanup.EndError(err)
@@ -400,11 +406,6 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 	}
 
 	if k8s.IsEnabled() {
-		bootstrapStats.k8sInit.Start()
-		if err := k8s.Init(option.Config); err != nil {
-			log.WithError(err).Fatal("Unable to initialize Kubernetes subsystem")
-		}
-
 		if err := k8s.RegisterCRDs(); err != nil {
 			log.WithError(err).Fatal("Unable to register CRDs")
 		}

daemon/cmd/daemon_main.go
@@ -1267,6 +1267,14 @@ func runDaemon() {
 	iptablesManager := &iptables.IptablesManager{}
 	iptablesManager.Init()
 
+	if k8s.IsEnabled() {
+		bootstrapStats.k8sInit.Start()
+		if err := k8s.Init(option.Config); err != nil {
+			log.WithError(err).Fatal("Unable to initialize Kubernetes subsystem")
+		}
+		bootstrapStats.k8sInit.End(true)
+	}
+
 	d, restoredEndpoints, err := NewDaemon(server.ServerCtx, linuxdatapath.NewDatapath(datapathConfig, iptablesManager))
 	if err != nil {
 		log.WithError(err).Fatal("Error while creating daemon")
@@ -1296,10 +1304,6 @@ func runDaemon() {
 
 	bootstrapStats.k8sInit.Start()
 
-	// We need to set up etcd in parallel so we will initialize the k8s
-	// subsystem as well in parallel so caches will start to be synchronized
-	// with k8s.
-	k8sCachesSynced := d.k8sWatcher.InitK8sSubsystem()
 	if option.Config.KVStore == "" {
 		log.Info("Skipping kvstore configuration")
 	} else {
@@ -1308,7 +1312,7 @@ func runDaemon() {
 
 	// Wait only for certain caches, but not all!
 	// (Check Daemon.initK8sSubsystem() for more info)
-	<-k8sCachesSynced
+	<-d.k8sCachesSynced
 	bootstrapStats.k8sInit.End(true)
 	restoreComplete := d.initRestore(restoredEndpoints)
 

daemon/cmd/state.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/connector"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/k8s"
+	"github.com/cilium/cilium/pkg/k8s/watchers"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
@@ -67,32 +68,13 @@ func (d *Daemon) validateEndpoint(ep *endpoint.Endpoint) (valid bool, err error)
 	}
 
 	if ep.K8sPodName != "" && ep.K8sNamespace != "" && k8s.IsEnabled() {
-		go func() {
-			err := ep.WaitUntilExposed(context.Background())
-			if err != nil {
-				// If an error has happen it's because the endpoint is not alive
-				return
-			}
-			_, err = d.k8sWatcher.GetCachedPod(ep.K8sNamespace, ep.K8sPodName)
-			if err != nil && k8serrors.IsNotFound(err) {
-				scopedLog := log.WithFields(logrus.Fields{
-					logfields.EndpointID:   ep.ID,
-					logfields.K8sNamespace: ep.K8sNamespace,
-					logfields.K8sPodName:   ep.K8sPodName,
-				})
-				scopedLog.Warningf("kubernetes pod not found, deleting endpoint from endpoint manager")
-				errs := d.deleteEndpointQuiet(ep, endpoint.DeleteConfig{
-					// If the IP is managed by an external IPAM, it does not need to be released
-					NoIPRelease: ep.DatapathConfiguration.ExternalIpam,
-				})
-				for _, err := range errs {
-					scopedLog.WithError(err).Warningf("Error deleting endpoint")
-				}
-				return
-			}
+		d.k8sWatcher.WaitForCacheSync(watchers.K8sAPIGroupPodV1Core)
+		_, err = d.k8sWatcher.GetCachedPod(ep.K8sNamespace, ep.K8sPodName)
+		if err != nil && k8serrors.IsNotFound(err) {
+			return false, fmt.Errorf("Kubernetes pod %s/%s does not exist", ep.K8sNamespace, ep.K8sPodName)
+		}
 
-			ep.RunMetadataResolver(d.fetchK8sLabelsAndAnnotations)
-		}()
+		ep.RunMetadataResolver(d.fetchK8sLabelsAndAnnotations)
 	}
 
 	if err := ep.ValidateConnectorPlumbing(connector.CheckLink); err != nil {
@@ -264,6 +246,7 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) (resto
 	}
 
 	for _, ep := range state.restored {
+		log.WithField(logfields.EndpointID, ep.ID).Info("Successfully restored endpoint. Scheduling regeneration")
 		go func(ep *endpoint.Endpoint, epRegenerated chan<- bool) {
 			if err := ep.RegenerateAfterRestore(); err != nil {
 				epRegenerated <- false

pkg/endpoint/endpoint.go
@@ -308,9 +308,6 @@ type Endpoint struct {
 	aliveCtx        context.Context
 	aliveCancel     context.CancelFunc
 	regenFailedChan chan struct{}
-	// exposed is a channel that is closed when the endpoint is exposed in the
-	// endpoint manager.
-	exposed chan struct{}
 
 	allocator cache.IdentityAllocator
 
@@ -440,7 +437,6 @@ func createEndpoint(owner regeneration.Owner, proxy EndpointProxy, allocator cac
 		controllers:     controller.NewManager(),
 		regenFailedChan: make(chan struct{}, 1),
 		allocator:       allocator,
-		exposed:         make(chan struct{}),
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -780,7 +776,6 @@ func parseEndpoint(ctx context.Context, owner regeneration.Owner, strEp string) 
 	ep.realizedPolicy = ep.desiredPolicy
 	ep.controllers = controller.NewManager()
 	ep.regenFailedChan = make(chan struct{}, 1)
-	ep.exposed = make(chan struct{})
 
 	ctx, cancel := context.WithCancel(ctx)
 	ep.aliveCancel = cancel
@@ -2312,19 +2307,4 @@ func (e *Endpoint) setDefaultPolicyConfig() {
 	alwaysEnforce := policy.GetPolicyEnabled() == option.AlwaysEnforce
 	e.desiredPolicy.IngressPolicyEnabled = alwaysEnforce
 	e.desiredPolicy.EgressPolicyEnabled = alwaysEnforce
-}
-
-// WaitUntilExposed will return once the endpoint is exposed in the endpoint
-// manager. It returns an error in case the given context expires of if the
-// endpoint is deleted in the meanwhile, i.e., the endpoint's aliveCtx is
-// canceled.
-func (e *Endpoint) WaitUntilExposed(ctx context.Context) error {
-	select {
-	case <-e.exposed:
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-e.aliveCtx.Done():
-		return e.aliveCtx.Err()
-	}
 }

pkg/endpoint/manager.go
@@ -46,7 +46,6 @@ func (e *Endpoint) Expose(mgr endpointManager) error {
 		e.unlock()
 		return err
 	}
-	defer close(e.exposed)
 
 	e.ID = newID
 	e.UpdateLogger(map[string]interface{}{