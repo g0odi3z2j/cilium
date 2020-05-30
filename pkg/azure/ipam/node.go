g0odi3z2j/cilium

commit 313769b639ff845fdf70ee9d0fac47480e5e8538

ipam: Move iterator logic into generic InstanceMap
Move code to iterate over interfaces and addresses into the InstanceMap.
This allows to defer deep copying until it is really necessary.

This also allows to move the logic to reserve IPs of all interfaces of
all instances into the generic GroupPoolAllocator for reuse in future
IPAM code.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/azure/ipam/node.go
@@ -52,9 +52,13 @@ func (n *Node) UpdatedNode(obj *v2.CiliumNode) {
 func (n *Node) PopulateStatusFields(k8sObj *v2.CiliumNode) {
 	n.mutex.RLock()
 	k8sObj.Status.Azure.Interfaces = []types.AzureInterface{}
-	for _, iface := range n.manager.GetInterfaces(n.k8sObj.InstanceID()) {
-		k8sObj.Status.Azure.Interfaces = append(k8sObj.Status.Azure.Interfaces, *iface)
-	}
+	n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error {
+		iface, ok := interfaceObj.Resource.(*types.AzureInterface)
+		if ok {
+			k8sObj.Status.Azure.Interfaces = append(k8sObj.Status.Azure.Interfaces, *(iface.DeepCopy()))
+		}
+		return nil
+	})
 	n.mutex.RUnlock()
 }
 
@@ -71,8 +75,12 @@ func (n *Node) ReleaseIPs(ctx context.Context, r *ipam.ReleaseAction) error {
 // PrepareIPAllocation returns the number of IPs that can be allocated/created.
 func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (a *ipam.AllocationAction, err error) {
 	a = &ipam.AllocationAction{}
+	err = n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error {
+		iface, ok := interfaceObj.Resource.(*types.AzureInterface)
+		if !ok {
+			return fmt.Errorf("invalid interface object")
+		}
 
-	for _, iface := range n.manager.GetInterfaces(n.k8sObj.InstanceID()) {
 		scopedLog.WithFields(logrus.Fields{
 			"id":           iface.ID,
 			"numAddresses": len(iface.Addresses),
@@ -80,10 +88,10 @@ func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (a *ipam.AllocationA
 
 		availableOnInterface := math.IntMax(types.InterfaceAddressLimit-len(iface.Addresses), 0)
 		if availableOnInterface <= 0 {
-			continue
-		} else {
-			a.AvailableInterfaces++
+			return nil
 		}
+
+		a.AvailableInterfaces++
 
 		if a.InterfaceID == "" {
 			scopedLog.WithFields(logrus.Fields{
@@ -110,7 +118,8 @@ func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (a *ipam.AllocationA
 				a.AvailableForAllocation = math.IntMin(available, availableOnInterface)
 			}
 		}
-	}
+		return nil
+	})
 
 	return
 }
@@ -148,19 +157,23 @@ func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Ent
 	}
 
 	available := ipamTypes.AllocationMap{}
-	interfaces := n.manager.GetInterfaces(n.k8sObj.InstanceID())
-	for _, iface := range interfaces {
-		for _, address := range iface.Addresses {
-			if address.State == types.StateSucceeded {
-				available[address.IP] = ipamTypes.AllocationIP{Resource: iface.ID}
-			} else {
-				log.WithFields(logrus.Fields{
-					"ip":    address.IP,
-					"state": address.State,
-				}).Warning("Ignoring potentially available IP due to non-successful state")
-			}
+	n.manager.instances.ForeachAddress(n.k8sObj.InstanceID(), func(instanceID, interfaceID, ip, poolID string, addressObj ipamTypes.Address) error {
+		address, ok := addressObj.(types.AzureAddress)
+		if !ok {
+			log.WithField("ip", ip).Warning("Not an Azure address object, ignoring IP")
+			return nil
 		}
-	}
+
+		if address.State == types.StateSucceeded {
+			available[address.IP] = ipamTypes.AllocationIP{Resource: interfaceID}
+		} else {
+			log.WithFields(logrus.Fields{
+				"ip":    address.IP,
+				"state": address.State,
+			}).Warning("Ignoring potentially available IP due to non-successful state")
+		}
+		return nil
+	})
 
 	return available, nil
 }

pkg/aws/eni/node.go
@@ -77,7 +77,16 @@ func (n *Node) loggerLocked() *logrus.Entry {
 // PopulateStatusFields fills in the status field of the CiliumNode custom
 // resource with ENI specific information
 func (n *Node) PopulateStatusFields(k8sObj *v2.CiliumNode) {
-	k8sObj.Status.ENI.ENIs = n.getENIs()
+	k8sObj.Status.ENI.ENIs = map[string]eniTypes.ENI{}
+	n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(),
+		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
+			e, ok := rev.Resource.(*eniTypes.ENI)
+			if ok {
+				k8sObj.Status.ENI.ENIs[interfaceID] = *e.DeepCopy()
+			}
+			return nil
+		})
+	return
 }
 
 // getLimits returns the interface and IP limits of this node
@@ -202,17 +211,6 @@ func (n *Node) AllocateIPs(ctx context.Context, a *ipam.AllocationAction) error 
 	return n.manager.api.AssignPrivateIpAddresses(ctx, a.InterfaceID, int64(a.AvailableForAllocation))
 }
 
-// getENIs returns a copy of all ENIs attached to the node
-func (n *Node) getENIs() (enis map[string]eniTypes.ENI) {
-	enis = map[string]eniTypes.ENI{}
-	n.mutex.RLock()
-	for _, e := range n.enis {
-		enis[e.ID] = e
-	}
-	n.mutex.RUnlock()
-	return
-}
-
 func (n *Node) getSecurityGroupIDs(ctx context.Context) ([]string, error) {
 	// 1. check explicit security groups associations via checking Spec.ENI.SecurityGroups
 	// 2. check if Spec.ENI.SecurityGroupTags is passed and if so filter by those
@@ -239,11 +237,22 @@ func (n *Node) getSecurityGroupIDs(ctx context.Context) ([]string, error) {
 		}
 	}
 
-	if eni := n.manager.GetENI(n.k8sObj.InstanceID(), 0); eni != nil {
-		return eni.SecurityGroups, nil
+	var securityGroups []string
+	n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(),
+		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
+			e, ok := rev.Resource.(*eniTypes.ENI)
+			if ok && e.Number == 0 {
+				securityGroups = make([]string, len(e.SecurityGroups))
+				copy(securityGroups, e.SecurityGroups)
+			}
+			return nil
+		})
+
+	if securityGroups == nil {
+		return nil, fmt.Errorf("failed to get security group ids")
 	}
 
-	return nil, fmt.Errorf("failed to get security group ids")
+	return securityGroups, nil
 }
 
 func (n *Node) errorInstanceNotRunning(err error) (notRunning bool) {
@@ -416,23 +425,29 @@ func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Ent
 
 	available := ipamTypes.AllocationMap{}
 	n.enis = map[string]eniTypes.ENI{}
-	enis := n.manager.GetENIs(n.k8sObj.InstanceID())
+
+	n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(),
+		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
+			e, ok := rev.Resource.(*eniTypes.ENI)
+			if !ok {
+				return nil
+			}
+			n.enis[e.ID] = *e
+
+			if e.Number < *n.k8sObj.Spec.ENI.FirstInterfaceIndex {
+				return nil
+			}
+
+			for _, ip := range e.Addresses {
+				available[ip] = ipamTypes.AllocationIP{Resource: e.ID}
+			}
+			return nil
+		})
+
 	// An ec2 instance has at least one ENI attached, no ENI found implies instance not found.
-	if len(enis) == 0 {
+	if len(n.enis) == 0 {
 		scopedLog.Warning("Instance not found! Please delete corresponding ciliumnode if instance has already been deleted.")
 		return nil, fmt.Errorf("unable to retrieve ENIs")
-	}
-
-	for _, e := range enis {
-		n.enis[e.ID] = *e
-
-		if e.Number < *n.k8sObj.Spec.ENI.FirstInterfaceIndex {
-			continue
-		}
-
-		for _, ip := range e.Addresses {
-			available[ip] = ipamTypes.AllocationIP{Resource: e.ID}
-		}
 	}
 
 	return available, nil

pkg/aws/ec2/ec2.go
@@ -172,8 +172,8 @@ func parseENI(iface *ec2.NetworkInterface, vpcs ipamTypes.VirtualNetworkMap, sub
 
 // GetInstances returns the list of all instances including their ENIs as
 // instanceMap
-func (c *Client) GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap, subnets ipamTypes.SubnetMap) (types.InstanceMap, error) {
-	instances := types.InstanceMap{}
+func (c *Client) GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap, subnets ipamTypes.SubnetMap) (*ipamTypes.InstanceMap, error) {
+	instances := ipamTypes.NewInstanceMap()
 
 	networkInterfaces, err := c.describeNetworkInterfaces(ctx)
 	if err != nil {
@@ -187,7 +187,7 @@ func (c *Client) GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetwork
 		}
 
 		if id != "" {
-			instances.Add(id, eni)
+			instances.Update(id, ipamTypes.InterfaceRevision{Resource: eni})
 		}
 	}
 

pkg/aws/ec2/mock/mock.go
@@ -359,8 +359,8 @@ func (e *API) UnassignPrivateIpAddresses(ctx context.Context, eniID string, addr
 	return fmt.Errorf("Unable to find ENI with ID %s", eniID)
 }
 
-func (e *API) GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap, subnets ipamTypes.SubnetMap) (types.InstanceMap, error) {
-	instances := types.InstanceMap{}
+func (e *API) GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap, subnets ipamTypes.SubnetMap) (*ipamTypes.InstanceMap, error) {
+	instances := ipamTypes.NewInstanceMap()
 
 	e.mutex.RLock()
 	defer e.mutex.RUnlock()
@@ -379,7 +379,8 @@ func (e *API) GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap
 				}
 			}
 
-			instances.Add(instanceID, eni.DeepCopy())
+			eniRevision := ipamTypes.InterfaceRevision{Resource: eni.DeepCopy()}
+			instances.Update(instanceID, eniRevision)
 		}
 	}
 

pkg/aws/eni/instances.go
@@ -31,7 +31,7 @@ import (
 
 // EC2API is the API surface used of the EC2 API
 type EC2API interface {
-	GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap, subnets ipamTypes.SubnetMap) (types.InstanceMap, error)
+	GetInstances(ctx context.Context, vpcs ipamTypes.VirtualNetworkMap, subnets ipamTypes.SubnetMap) (*ipamTypes.InstanceMap, error)
 	GetSubnets(ctx context.Context) (ipamTypes.SubnetMap, error)
 	GetVpcs(ctx context.Context) (ipamTypes.VirtualNetworkMap, error)
 	GetSecurityGroups(ctx context.Context) (types.SecurityGroupMap, error)
@@ -48,7 +48,7 @@ type EC2API interface {
 // by calling resync() regularly.
 type InstancesManager struct {
 	mutex          lock.RWMutex
-	instances      types.InstanceMap
+	instances      *ipamTypes.InstanceMap
 	subnets        ipamTypes.SubnetMap
 	vpcs           ipamTypes.VirtualNetworkMap
 	securityGroups types.SecurityGroupMap
@@ -59,7 +59,7 @@ type InstancesManager struct {
 // NewInstancesManager returns a new instances manager
 func NewInstancesManager(api EC2API, eniTags map[string]string) *InstancesManager {
 	return &InstancesManager{
-		instances: types.InstanceMap{},
+		instances: ipamTypes.NewInstanceMap(),
 		api:       api,
 		eniTags:   eniTags,
 	}
@@ -175,7 +175,7 @@ func (m *InstancesManager) Resync(ctx context.Context) time.Time {
 	}
 
 	log.WithFields(logrus.Fields{
-		"numENIs":           len(instances),
+		"numInstances":      instances.NumInstances(),
 		"numVPCs":           len(vpcs),
 		"numSubnets":        len(subnets),
 		"numSecurityGroups": len(securityGroups),
@@ -191,34 +191,12 @@ func (m *InstancesManager) Resync(ctx context.Context) time.Time {
 	return resyncStart
 }
 
-// GetENI returns the ENI of an instance at a particular interface index
-func (m *InstancesManager) GetENI(instanceID string, index int) *eniTypes.ENI {
-	for _, eni := range m.getENIs(instanceID) {
-		if eni.Number == index {
-			return eni
-		}
-	}
-
-	return nil
-}
-
-// GetENIs returns the list of ENIs associated with a particular instance
-func (m *InstancesManager) GetENIs(instanceID string) []*eniTypes.ENI {
-	return m.getENIs(instanceID)
-}
-
-// getENIs returns the list of ENIs associated with a particular instance
-func (m *InstancesManager) getENIs(instanceID string) []*eniTypes.ENI {
-	m.mutex.RLock()
-	defer m.mutex.RUnlock()
-	return m.instances.Get(instanceID)
-}
-
 // UpdateENI updates the ENI definition of an ENI for a particular instance. If
 // the ENI is already known, the definition is updated, otherwise the ENI is
 // added to the instance.
 func (m *InstancesManager) UpdateENI(instanceID string, eni *eniTypes.ENI) {
 	m.mutex.Lock()
-	m.instances.Update(instanceID, eni)
+	eniRevision := ipamTypes.InterfaceRevision{Resource: eni}
+	m.instances.Update(instanceID, eniRevision)
 	m.mutex.Unlock()
 }

pkg/aws/eni/instances_test.go
@@ -311,39 +311,3 @@ func (e *ENISuite) TestGetSecurityGroupByTags(c *check.C) {
 	c.Assert(sgGroups[0].Tags, checker.DeepEquals, reqTags)
 	c.Assert(sgGroups[1].Tags, checker.DeepEquals, reqTags)
 }
-
-func (e *ENISuite) TestGetENIs(c *check.C) {
-	api := ec2mock.NewAPI(subnets, vpcs, securityGroups)
-	c.Assert(api, check.Not(check.IsNil))
-
-	mngr := NewInstancesManager(api, nil)
-	c.Assert(mngr, check.Not(check.IsNil))
-
-	iteration1(api, mngr)
-	c.Assert(len(mngr.GetENIs("i-1")), check.Equals, 1)
-	c.Assert(len(mngr.GetENIs("i-2")), check.Equals, 1)
-	c.Assert(len(mngr.GetENIs("i-unknown")), check.Equals, 0)
-
-	iteration2(api, mngr)
-	c.Assert(len(mngr.GetENIs("i-1")), check.Equals, 2)
-	c.Assert(len(mngr.GetENIs("i-2")), check.Equals, 1)
-	c.Assert(len(mngr.GetENIs("i-unknown")), check.Equals, 0)
-}
-
-func (e *ENISuite) TestGetENI(c *check.C) {
-	api := ec2mock.NewAPI(subnets, vpcs, securityGroups)
-	c.Assert(api, check.Not(check.IsNil))
-
-	mngr := NewInstancesManager(api, nil)
-	c.Assert(mngr, check.Not(check.IsNil))
-
-	iteration1(api, mngr)
-	c.Assert(mngr.GetENI("i-1", 0), check.Not(check.IsNil))
-	c.Assert(mngr.GetENI("i-1", 1), check.IsNil)
-	c.Assert(mngr.GetENI("i-2", 0), check.Not(check.IsNil))
-
-	iteration2(api, mngr)
-	c.Assert(mngr.GetENI("i-1", 0), check.Not(check.IsNil))
-	c.Assert(mngr.GetENI("i-1", 1), check.Not(check.IsNil))
-	c.Assert(mngr.GetENI("i-2", 0), check.Not(check.IsNil))
-}

pkg/aws/eni/node_manager_test.go
@@ -302,11 +302,13 @@ func (e *ENISuite) TestNodeManagerENIWithSGTags(c *check.C) {
 	// and remove eth0 from the map
 	eniNode, castOK := node.Ops().(*Node)
 	c.Assert(castOK, check.Equals, true)
-	enis := eniNode.getENIs()
-	delete(enis, eniID1)
-	for _, eni := range enis {
-		c.Assert(eni.SecurityGroups, checker.DeepEquals, []string{"sg-1"})
+	eniNode.mutex.RLock()
+	for id, eni := range eniNode.enis {
+		if id != eniID1 {
+			c.Assert(eni.SecurityGroups, checker.DeepEquals, []string{"sg-1"})
+		}
 	}
+	eniNode.mutex.RUnlock()
 }
 
 // TestNodeManagerMinAllocate20 tests MinAllocate without PreAllocate

pkg/aws/eni/types/types.go
@@ -14,6 +14,10 @@
 
 package types
 
+import (
+	"github.com/cilium/cilium/pkg/ipam/types"
+)
+
 // ENISpec is the ENI specification of a node. This specification is considered
 // by the cilium-operator to act as an IPAM operator and makes ENI IPs available
 // via the IPAMSpec section.
@@ -160,6 +164,22 @@ type ENI struct {
 
 	// SecurityGroups are the security groups associated with the ENI
 	SecurityGroups []string `json:"security-groups,omitempty"`
+}
+
+// InterfaceID returns the identifier of the interface
+func (e *ENI) InterfaceID() string {
+	return e.ID
+}
+
+// ForeachAddress iterates over all addresses and calls fn
+func (e *ENI) ForeachAddress(id string, fn types.AddressIterator) error {
+	for _, address := range e.Addresses {
+		if err := fn(id, e.ID, address, "", address); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // ENIStatus is the status of ENI addressing of the node

commit 9024d5c2df8552159fcea6f33e0c7913d02dfda4

azure/ipam: Fix nil dereference with logger
The code erroneously used `log` which is a global variable, that's
unprotected by a mutex. The correct usage is to use `scopedLog` which is
passed into ResyncInterfacesAndIPs(), which already has been locked by
the caller of ResyncInterfacesAndIPs(). The caller is n.recalculate().

Fixes: https://github.com/cilium/cilium/issues/11785
Fixes: 313769b639 ("ipam: Move iterator logic into generic InstanceMap")
Fixes: 0eca58908c ("ipam: Support for Azure IPAM")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/azure/ipam/node.go
@@ -158,14 +158,14 @@ func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Ent
 	n.manager.instances.ForeachAddress(n.node.InstanceID(), func(instanceID, interfaceID, ip, poolID string, addressObj ipamTypes.Address) error {
 		address, ok := addressObj.(types.AzureAddress)
 		if !ok {
-			log.WithField("ip", ip).Warning("Not an Azure address object, ignoring IP")
+			scopedLog.WithField("ip", ip).Warning("Not an Azure address object, ignoring IP")
 			return nil
 		}
 
 		if address.State == types.StateSucceeded {
 			available[address.IP] = ipamTypes.AllocationIP{Resource: interfaceID}
 		} else {
-			log.WithFields(logrus.Fields{
+			scopedLog.WithFields(logrus.Fields{
 				"ip":    address.IP,
 				"state": address.State,
 			}).Warning("Ignoring potentially available IP due to non-successful state")