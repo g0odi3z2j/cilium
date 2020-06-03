g0odi3z2j/cilium

commit b4306265c530587603690397311b60c50d95d7bf

endpoint: Ensure restored endpoint is validated before regenerating it
Due to a performance optimization, validateEndpoint() was changed to use
the pod store instad of contacting the apiserver directly. This required
to make the validation async to wait until the pod cache is available.

This change however caused the endpoint to be exposed and regenerated
before the endpoint was fully validated. This in turn caused the
validation to occur while the regeneration was being performed. When the
validation then failed, the endpoint was deleted while the endpoint was
being regenerated, causing the endpoint to fail with a "JoinEP" message
in the logs.

This commit requires the endpoint to be validated before exposure and
initial regeneration.

Fixes: #11645
Fixes: #11076
Fixes: #10856
Fixes: #10667
Fixes: #10278
Fixes: b1807093960 ("pkg/endpoint: fetch pod and namespace labels from local stores")

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/cmd/daemon.go
@@ -146,6 +146,10 @@ type Daemon struct {
 	healthEndpointRouting *linuxrouting.RoutingInfo
 
 	hubbleObserver *observer.LocalObserverServer
+
+	// k8sCachesSynced is closed when all essential Kubernetes caches have
+	// been fully synchronized
+	k8sCachesSynced <-chan struct{}
 }
 
 // GetPolicyRepository returns the policy repository of the daemon
@@ -392,6 +396,8 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 	treatRemoteNodeAsHost := option.Config.AlwaysAllowLocalhost() && !option.Config.EnableRemoteNodeIdentity
 	policyApi.InitEntities(option.Config.ClusterName, treatRemoteNodeAsHost)
 
+	d.k8sCachesSynced = d.k8sWatcher.InitK8sSubsystem()
+
 	bootstrapStats.cleanup.Start()
 	err = clearCiliumVeths()
 	bootstrapStats.cleanup.EndError(err)
@@ -400,11 +406,6 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 	}
 
 	if k8s.IsEnabled() {
-		bootstrapStats.k8sInit.Start()
-		if err := k8s.Init(option.Config); err != nil {
-			log.WithError(err).Fatal("Unable to initialize Kubernetes subsystem")
-		}
-
 		if err := k8s.RegisterCRDs(); err != nil {
 			log.WithError(err).Fatal("Unable to register CRDs")
 		}

daemon/cmd/daemon_main.go
@@ -1267,6 +1267,14 @@ func runDaemon() {
 	iptablesManager := &iptables.IptablesManager{}
 	iptablesManager.Init()
 
+	if k8s.IsEnabled() {
+		bootstrapStats.k8sInit.Start()
+		if err := k8s.Init(option.Config); err != nil {
+			log.WithError(err).Fatal("Unable to initialize Kubernetes subsystem")
+		}
+		bootstrapStats.k8sInit.End(true)
+	}
+
 	d, restoredEndpoints, err := NewDaemon(server.ServerCtx, linuxdatapath.NewDatapath(datapathConfig, iptablesManager))
 	if err != nil {
 		log.WithError(err).Fatal("Error while creating daemon")
@@ -1296,10 +1304,6 @@ func runDaemon() {
 
 	bootstrapStats.k8sInit.Start()
 
-	// We need to set up etcd in parallel so we will initialize the k8s
-	// subsystem as well in parallel so caches will start to be synchronized
-	// with k8s.
-	k8sCachesSynced := d.k8sWatcher.InitK8sSubsystem()
 	if option.Config.KVStore == "" {
 		log.Info("Skipping kvstore configuration")
 	} else {
@@ -1308,7 +1312,7 @@ func runDaemon() {
 
 	// Wait only for certain caches, but not all!
 	// (Check Daemon.initK8sSubsystem() for more info)
-	<-k8sCachesSynced
+	<-d.k8sCachesSynced
 	bootstrapStats.k8sInit.End(true)
 	restoreComplete := d.initRestore(restoredEndpoints)
 

daemon/cmd/state.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/connector"
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/k8s"
+	"github.com/cilium/cilium/pkg/k8s/watchers"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
@@ -67,32 +68,13 @@ func (d *Daemon) validateEndpoint(ep *endpoint.Endpoint) (valid bool, err error)
 	}
 
 	if ep.K8sPodName != "" && ep.K8sNamespace != "" && k8s.IsEnabled() {
-		go func() {
-			err := ep.WaitUntilExposed(context.Background())
-			if err != nil {
-				// If an error has happen it's because the endpoint is not alive
-				return
-			}
-			_, err = d.k8sWatcher.GetCachedPod(ep.K8sNamespace, ep.K8sPodName)
-			if err != nil && k8serrors.IsNotFound(err) {
-				scopedLog := log.WithFields(logrus.Fields{
-					logfields.EndpointID:   ep.ID,
-					logfields.K8sNamespace: ep.K8sNamespace,
-					logfields.K8sPodName:   ep.K8sPodName,
-				})
-				scopedLog.Warningf("kubernetes pod not found, deleting endpoint from endpoint manager")
-				errs := d.deleteEndpointQuiet(ep, endpoint.DeleteConfig{
-					// If the IP is managed by an external IPAM, it does not need to be released
-					NoIPRelease: ep.DatapathConfiguration.ExternalIpam,
-				})
-				for _, err := range errs {
-					scopedLog.WithError(err).Warningf("Error deleting endpoint")
-				}
-				return
-			}
+		d.k8sWatcher.WaitForCacheSync(watchers.K8sAPIGroupPodV1Core)
+		_, err = d.k8sWatcher.GetCachedPod(ep.K8sNamespace, ep.K8sPodName)
+		if err != nil && k8serrors.IsNotFound(err) {
+			return false, fmt.Errorf("Kubernetes pod %s/%s does not exist", ep.K8sNamespace, ep.K8sPodName)
+		}
 
-			ep.RunMetadataResolver(d.fetchK8sLabelsAndAnnotations)
-		}()
+		ep.RunMetadataResolver(d.fetchK8sLabelsAndAnnotations)
 	}
 
 	if err := ep.ValidateConnectorPlumbing(connector.CheckLink); err != nil {
@@ -264,6 +246,7 @@ func (d *Daemon) regenerateRestoredEndpoints(state *endpointRestoreState) (resto
 	}
 
 	for _, ep := range state.restored {
+		log.WithField(logfields.EndpointID, ep.ID).Info("Successfully restored endpoint. Scheduling regeneration")
 		go func(ep *endpoint.Endpoint, epRegenerated chan<- bool) {
 			if err := ep.RegenerateAfterRestore(); err != nil {
 				epRegenerated <- false

pkg/endpoint/endpoint.go
@@ -308,9 +308,6 @@ type Endpoint struct {
 	aliveCtx        context.Context
 	aliveCancel     context.CancelFunc
 	regenFailedChan chan struct{}
-	// exposed is a channel that is closed when the endpoint is exposed in the
-	// endpoint manager.
-	exposed chan struct{}
 
 	allocator cache.IdentityAllocator
 
@@ -440,7 +437,6 @@ func createEndpoint(owner regeneration.Owner, proxy EndpointProxy, allocator cac
 		controllers:     controller.NewManager(),
 		regenFailedChan: make(chan struct{}, 1),
 		allocator:       allocator,
-		exposed:         make(chan struct{}),
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -780,7 +776,6 @@ func parseEndpoint(ctx context.Context, owner regeneration.Owner, strEp string) 
 	ep.realizedPolicy = ep.desiredPolicy
 	ep.controllers = controller.NewManager()
 	ep.regenFailedChan = make(chan struct{}, 1)
-	ep.exposed = make(chan struct{})
 
 	ctx, cancel := context.WithCancel(ctx)
 	ep.aliveCancel = cancel
@@ -2312,19 +2307,4 @@ func (e *Endpoint) setDefaultPolicyConfig() {
 	alwaysEnforce := policy.GetPolicyEnabled() == option.AlwaysEnforce
 	e.desiredPolicy.IngressPolicyEnabled = alwaysEnforce
 	e.desiredPolicy.EgressPolicyEnabled = alwaysEnforce
-}
-
-// WaitUntilExposed will return once the endpoint is exposed in the endpoint
-// manager. It returns an error in case the given context expires of if the
-// endpoint is deleted in the meanwhile, i.e., the endpoint's aliveCtx is
-// canceled.
-func (e *Endpoint) WaitUntilExposed(ctx context.Context) error {
-	select {
-	case <-e.exposed:
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-e.aliveCtx.Done():
-		return e.aliveCtx.Err()
-	}
 }

pkg/endpoint/manager.go
@@ -46,7 +46,6 @@ func (e *Endpoint) Expose(mgr endpointManager) error {
 		e.unlock()
 		return err
 	}
-	defer close(e.exposed)
 
 	e.ID = newID
 	e.UpdateLogger(map[string]interface{}{

commit 8d233bc328050e93b0c96482b68412a5fbbf59ea

k8s: Fix data race when setting node address
The k8s cache handlers will depend on node addressing information so the
retrieval of own node information must complete before caches are being
synchronized.

Also re-add a bootstrap start sequence that was removed accidentally by
b4306265c53.

Fixes: b4306265c53 ("endpoint: Ensure restored endpoint is validated before regenerating it")
Fixes: #11836

Signed-off-by: Thomas Graf <thomas@cilium.io>

daemon/cmd/daemon.go
@@ -379,16 +379,8 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 	treatRemoteNodeAsHost := option.Config.AlwaysAllowLocalhost() && !option.Config.EnableRemoteNodeIdentity
 	policyApi.InitEntities(option.Config.ClusterName, treatRemoteNodeAsHost)
 
-	d.k8sCachesSynced = d.k8sWatcher.InitK8sSubsystem()
-
-	bootstrapStats.cleanup.Start()
-	err = clearCiliumVeths()
-	bootstrapStats.cleanup.EndError(err)
-	if err != nil {
-		log.WithError(err).Warning("Unable to clean stale endpoint interfaces")
-	}
-
 	if k8s.IsEnabled() {
+		bootstrapStats.k8sInit.Start()
 		if err := k8s.RegisterCRDs(); err != nil {
 			log.WithError(err).Fatal("Unable to register CRDs")
 		}
@@ -413,6 +405,15 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 		}
 
 		bootstrapStats.k8sInit.End(true)
+	}
+
+	d.k8sCachesSynced = d.k8sWatcher.InitK8sSubsystem()
+
+	bootstrapStats.cleanup.Start()
+	err = clearCiliumVeths()
+	bootstrapStats.cleanup.EndError(err)
+	if err != nil {
+		log.WithError(err).Warning("Unable to clean stale endpoint interfaces")
 	}
 
 	d.bootstrapIPAM()