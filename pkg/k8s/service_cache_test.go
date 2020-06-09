g0odi3z2j/cilium

commit 11832af01931bc4cb855102eae848f6917568a05

add support for k8s endpoint slice
This PR adds support for k8s endpoint slice. Since kubernetes does not
enable this feature by default, Cilium will detect if the feature is
enabled by checking if there is at least a single endpoint slice
available in the whole cluster. The user can always fallback to
v1.Endpoints by setting the option
`enable-k8s-endpoint-slice-auto-detection` to false which makes Cilium
to only watch for v1.Endpoint k8s events.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/endpoints.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Authors of Cilium
+// Copyright 2018-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,10 +21,18 @@ import (
 	"strconv"
 	"strings"
 
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+
 	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/k8s/types"
+	"github.com/cilium/cilium/pkg/k8s/version"
 	"github.com/cilium/cilium/pkg/loadbalancer"
+	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/service"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/discovery/v1beta1"
 )
 
 // Endpoints is an abstraction for the Kubernetes endpoints object. Endpoints
@@ -162,6 +170,75 @@ func ParseEndpoints(ep *types.Endpoints) (ServiceID, *Endpoints) {
 	return ParseEndpointsID(ep), endpoints
 }
 
+// ParseEndpointSliceID parses a Kubernetes endpoints slice and returns the
+// ServiceID
+func ParseEndpointSliceID(svc *types.EndpointSlice) ServiceID {
+	return ServiceID{
+		Name:      svc.ObjectMeta.GetLabels()[v1beta1.LabelServiceName],
+		Namespace: svc.ObjectMeta.Namespace,
+	}
+}
+
+// ParseEndpointSlice parses a Kubernetes Endpoints resource
+func ParseEndpointSlice(ep *types.EndpointSlice) (ServiceID, *Endpoints) {
+	endpoints := newEndpoints()
+
+	for _, sub := range ep.Endpoints {
+		// ready indicates that this endpoint is prepared to receive traffic,
+		// according to whatever system is managing the endpoint. A nil value
+		// indicates an unknown state. In most cases consumers should interpret this
+		// unknown state as ready.
+		// More info: vendor/k8s.io/api/discovery/v1beta1/types.go:114
+		if sub.Conditions.Ready != nil && !*sub.Conditions.Ready {
+			continue
+		}
+		for _, addr := range sub.Addresses {
+			backend, ok := endpoints.Backends[addr]
+			if !ok {
+				backend = &Backend{Ports: service.PortConfiguration{}}
+				endpoints.Backends[addr] = backend
+				if nodeName, ok := sub.Topology["kubernetes.io/hostname"]; ok {
+					backend.NodeName = nodeName
+				}
+			}
+
+			for _, port := range ep.Ports {
+				name, lbPort := parseEndpointPort(port)
+				if lbPort != nil {
+					backend.Ports[name] = lbPort
+				}
+			}
+		}
+	}
+
+	return ParseEndpointSliceID(ep), endpoints
+}
+
+// parseEndpointPort returns the port name and the port parsed as a L4Addr from
+// the given port.
+func parseEndpointPort(port v1beta1.EndpointPort) (string, *loadbalancer.L4Addr) {
+	proto := loadbalancer.TCP
+	if port.Protocol != nil {
+		switch *port.Protocol {
+		case v1.ProtocolTCP:
+			proto = loadbalancer.TCP
+		case v1.ProtocolUDP:
+			proto = loadbalancer.UDP
+		default:
+			return "", nil
+		}
+	}
+	if port.Port == nil {
+		return "", nil
+	}
+	var name string
+	if port.Name != nil {
+		name = *port.Name
+	}
+	lbPort := loadbalancer.NewL4Addr(proto, uint16(*port.Port))
+	return name, lbPort
+}
+
 // externalEndpoints is the collection of external endpoints in all remote
 // clusters. The map key is the name of the remote cluster.
 type externalEndpoints struct {
@@ -173,4 +250,38 @@ func newExternalEndpoints() externalEndpoints {
 	return externalEndpoints{
 		endpoints: map[string]*Endpoints{},
 	}
+}
+
+// SupportsEndpointSlice returns true if cilium-operator or cilium-agent should
+// watch and process endpoint slices.
+func SupportsEndpointSlice() bool {
+	return version.Capabilities().EndpointSlice && option.Config.K8sEnableK8sEndpointSlice
+}
+
+// HasEndpointSlice returns true if the hasEndpointSlices is closed before the
+// controller has been synchronized with k8s.
+func HasEndpointSlice(hasEndpointSlices chan struct{}, controller cache.Controller) bool {
+	endpointSliceSynced := make(chan struct{})
+	go func() {
+		cache.WaitForCacheSync(wait.NeverStop, controller.HasSynced)
+		close(endpointSliceSynced)
+	}()
+
+	// Check if K8s has a single endpointslice endpoint. By default, k8s has
+	// always the kubernetes-apiserver endpoint. If the endpointSlice are synced
+	// but we haven't received any endpoint slice then it means k8s is not
+	// running with k8s endpoint slice enabled.
+	select {
+	case <-endpointSliceSynced:
+		select {
+		// In case both select cases are ready to be selected we will recheck if
+		// hasEndpointSlices was closed.
+		case <-hasEndpointSlices:
+			return true
+		default:
+		}
+	case <-hasEndpointSlices:
+		return true
+	}
+	return false
 }

commit 11111c1aeae2ce5b1c6baae38365271618b4a88e

pkg/k8s: fix endpoint slice support
The endpoint slice implementation did not account that a service could
map to multiple endpoint slices. Wrongly assuming that only a single
endpoint slice existed for a single service can cause Cilium to fail
the translation of a service to a backend in case 2 or more endpoint
slices existed. In some rare occurrences where a single endpoint slice
was deleted and another one was recreated, for the same service, it
could cause Cilium stop doing service translation entirely.

The unit test added easily replicates the issue as we can see the test
fails in the current master where the 2nd endpoint slice added overwrote
the 1st endpoint slice:

```
c.Assert(endpoints.String(), check.Equals, "2.2.2.2:8080/TCP,2.2.2.3:8080/TCP")
Expected :string = "2.2.2.2:8080/TCP,2.2.2.3:8080/TCP"
Actual   :string = "2.2.2.3:8080/TCP"
```

Fixes: 11832af01931 ("add support for k8s endpoint slice")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/endpoints.go
@@ -21,9 +21,6 @@ import (
 	"strconv"
 	"strings"
 
-	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/tools/cache"
-
 	"github.com/cilium/cilium/pkg/ip"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/core/v1"
 	slim_discover_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/discovery/v1beta1"
@@ -33,6 +30,8 @@ import (
 	serviceStore "github.com/cilium/cilium/pkg/service/store"
 
 	"k8s.io/api/discovery/v1beta1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
 )
 
 // Endpoints is an abstraction for the Kubernetes endpoints object. Endpoints
@@ -170,17 +169,20 @@ func ParseEndpoints(ep *slim_corev1.Endpoints) (ServiceID, *Endpoints) {
 	return ParseEndpointsID(ep), endpoints
 }
 
-// ParseEndpointSliceID parses a Kubernetes endpoints slice and returns the
-// ServiceID
-func ParseEndpointSliceID(svc *slim_discover_v1beta1.EndpointSlice) ServiceID {
-	return ServiceID{
-		Name:      svc.ObjectMeta.GetLabels()[v1beta1.LabelServiceName],
-		Namespace: svc.ObjectMeta.Namespace,
+// ParseEndpointSliceID parses a Kubernetes endpoints slice and returns a
+// EndpointSliceID
+func ParseEndpointSliceID(es *slim_discover_v1beta1.EndpointSlice) EndpointSliceID {
+	return EndpointSliceID{
+		ServiceID: ServiceID{
+			Name:      es.ObjectMeta.GetLabels()[v1beta1.LabelServiceName],
+			Namespace: es.ObjectMeta.Namespace,
+		},
+		EndpointSliceName: es.ObjectMeta.GetName(),
 	}
 }
 
 // ParseEndpointSlice parses a Kubernetes Endpoints resource
-func ParseEndpointSlice(ep *slim_discover_v1beta1.EndpointSlice) (ServiceID, *Endpoints) {
+func ParseEndpointSlice(ep *slim_discover_v1beta1.EndpointSlice) (EndpointSliceID, *Endpoints) {
 	endpoints := newEndpoints()
 
 	for _, sub := range ep.Endpoints {
@@ -237,6 +239,57 @@ func parseEndpointPort(port slim_discover_v1beta1.EndpointPort) (string, *loadba
 	}
 	lbPort := loadbalancer.NewL4Addr(proto, uint16(*port.Port))
 	return name, lbPort
+}
+
+// endpointSlices is the collection of all endpoint slices of a service.
+// The map key is the name of the endpoint slice or the name of the legacy
+// v1.Endpoint. The endpoints stored here are not namespaced since this
+// structure is only used as a value of another map that is already namespaced.
+// (see ServiceCache.endpoints).
+type endpointSlices struct {
+	epSlices map[string]*Endpoints
+}
+
+// newEndpointsSlices returns a new endpointSlices
+func newEndpointsSlices() *endpointSlices {
+	return &endpointSlices{
+		epSlices: map[string]*Endpoints{},
+	}
+}
+
+// GetEndpoints returns a read only a single *Endpoints structure with all
+// Endpoints' backends joined.
+func (es *endpointSlices) GetEndpoints() *Endpoints {
+	if es == nil || len(es.epSlices) == 0 {
+		return nil
+	}
+	allEps := newEndpoints()
+	for _, eps := range es.epSlices {
+		for backend, ep := range eps.Backends {
+			allEps.Backends[backend] = ep
+		}
+	}
+	return allEps
+}
+
+// Upsert maps the 'esname' to 'e'.
+// - 'esName': Name of the Endpoint Slice
+// - 'e': Endpoints to store in the map
+func (es *endpointSlices) Upsert(esName string, e *Endpoints) {
+	if es == nil {
+		panic("BUG: endpointSlices is nil")
+	}
+	es.epSlices[esName] = e
+}
+
+// Delete deletes the endpoint slice in the internal map. Returns true if there
+// are not any more endpoints available in the map.
+func (es *endpointSlices) Delete(esName string) bool {
+	if es == nil || len(es.epSlices) == 0 {
+		return true
+	}
+	delete(es.epSlices, esName)
+	return len(es.epSlices) == 0
 }
 
 // externalEndpoints is the collection of external endpoints in all remote

pkg/k8s/rule_translate.go
@@ -242,10 +242,13 @@ func PreprocessRules(r api.Rules, cache *ServiceCache) error {
 		for ns, ep := range cache.endpoints {
 			svc, ok := cache.services[ns]
 			if ok && svc.IsExternal() {
-				t := NewK8sTranslator(ns, *ep, false, svc.Labels, false)
-				err := t.Translate(rule, &policy.TranslationResult{})
-				if err != nil {
-					return err
+				eps := ep.GetEndpoints()
+				if eps != nil {
+					t := NewK8sTranslator(ns, *eps, false, svc.Labels, false)
+					err := t.Translate(rule, &policy.TranslationResult{})
+					if err != nil {
+						return err
+					}
 				}
 			}
 		}

pkg/k8s/service.go
@@ -167,7 +167,7 @@ func ParseService(svc *slim_corev1.Service, nodeAddressing datapath.NodeAddressi
 	return svcID, svcInfo
 }
 
-// ServiceID identities the Kubernetes service
+// ServiceID identifies the Kubernetes service
 type ServiceID struct {
 	Name      string `json:"serviceName,omitempty"`
 	Namespace string `json:"namespace,omitempty"`
@@ -176,6 +176,13 @@ type ServiceID struct {
 // String returns the string representation of a service ID
 func (s ServiceID) String() string {
 	return fmt.Sprintf("%s/%s", s.Namespace, s.Name)
+}
+
+// EndpointSliceID identifies a Kubernetes EndpointSlice as well as the legacy
+// v1.Endpoints.
+type EndpointSliceID struct {
+	ServiceID
+	EndpointSliceName string
 }
 
 // ParseServiceIDFrom returns a ServiceID derived from the given kubernetes

pkg/k8s/service_cache.go
@@ -81,9 +81,12 @@ type ServiceCache struct {
 
 	// mutex protects the maps below including the concurrent access of each
 	// value.
-	mutex     lock.RWMutex
-	services  map[ServiceID]*Service
-	endpoints map[ServiceID]*Endpoints
+	mutex    lock.RWMutex
+	services map[ServiceID]*Service
+	// endpoints maps a service to a map of endpointSlices. In case the cluster
+	// is still using the v1.Endpoints, the key used in the internal map of
+	// endpointSlices is the v1.Endpoint name.
+	endpoints map[ServiceID]*endpointSlices
 
 	// externalEndpoints is a list of additional service backends derived from source other than the local cluster
 	externalEndpoints map[ServiceID]externalEndpoints
@@ -95,7 +98,7 @@ type ServiceCache struct {
 func NewServiceCache(nodeAddressing datapath.NodeAddressing) ServiceCache {
 	return ServiceCache{
 		services:          map[ServiceID]*Service{},
-		endpoints:         map[ServiceID]*Endpoints{},
+		endpoints:         map[ServiceID]*endpointSlices{},
 		externalEndpoints: map[ServiceID]externalEndpoints{},
 		Events:            make(chan ServiceEvent, option.Config.K8sServiceCacheSize),
 		nodeAddressing:    nodeAddressing,
@@ -179,33 +182,37 @@ func (s *ServiceCache) DeleteService(k8sSvc *slim_corev1.Service, swg *lock.Stop
 	}
 }
 
-func (s *ServiceCache) updateEndpoints(svcID ServiceID, newEndpoints *Endpoints, swg *lock.StoppableWaitGroup) (ServiceID, *Endpoints) {
+func (s *ServiceCache) updateEndpoints(esID EndpointSliceID, newEndpoints *Endpoints, swg *lock.StoppableWaitGroup) (ServiceID, *Endpoints) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	if oldEndpoints, ok := s.endpoints[svcID]; ok {
-		if oldEndpoints.DeepEquals(newEndpoints) {
-			return svcID, newEndpoints
+	eps, ok := s.endpoints[esID.ServiceID]
+	if ok {
+		if eps.epSlices[esID.EndpointSliceName].DeepEquals(newEndpoints) {
+			return esID.ServiceID, newEndpoints
 		}
+	} else {
+		eps = newEndpointsSlices()
+		s.endpoints[esID.ServiceID] = eps
 	}
 
-	s.endpoints[svcID] = newEndpoints
+	eps.Upsert(esID.EndpointSliceName, newEndpoints)
 
 	// Check if the corresponding Endpoints resource is already available
-	svc, ok := s.services[svcID]
-	endpoints, serviceReady := s.correlateEndpoints(svcID)
+	svc, ok := s.services[esID.ServiceID]
+	endpoints, serviceReady := s.correlateEndpoints(esID.ServiceID)
 	if ok && serviceReady {
 		swg.Add()
 		s.Events <- ServiceEvent{
 			Action:    UpdateService,
-			ID:        svcID,
+			ID:        esID.ServiceID,
 			Service:   svc,
 			Endpoints: endpoints,
 			SWG:       swg,
 		}
 	}
 
-	return svcID, newEndpoints
+	return esID.ServiceID, newEndpoints
 }
 
 // UpdateEndpoints parses a Kubernetes endpoints and adds or updates it in the
@@ -214,8 +221,11 @@ func (s *ServiceCache) updateEndpoints(svcID ServiceID, newEndpoints *Endpoints,
 // cache or not.
 func (s *ServiceCache) UpdateEndpoints(k8sEndpoints *slim_corev1.Endpoints, swg *lock.StoppableWaitGroup) (ServiceID, *Endpoints) {
 	svcID, newEndpoints := ParseEndpoints(k8sEndpoints)
-
-	return s.updateEndpoints(svcID, newEndpoints, swg)
+	epSliceID := EndpointSliceID{
+		ServiceID:         svcID,
+		EndpointSliceName: k8sEndpoints.GetName(),
+	}
+	return s.updateEndpoints(epSliceID, newEndpoints, swg)
 }
 
 func (s *ServiceCache) UpdateEndpointSlices(epSlice *slim_discovery_v1beta1.EndpointSlice, swg *lock.StoppableWaitGroup) (ServiceID, *Endpoints) {
@@ -224,19 +234,22 @@ func (s *ServiceCache) UpdateEndpointSlices(epSlice *slim_discovery_v1beta1.Endp
 	return s.updateEndpoints(svcID, newEndpoints, swg)
 }
 
-func (s *ServiceCache) deleteEndpoints(svcID ServiceID, swg *lock.StoppableWaitGroup) ServiceID {
+func (s *ServiceCache) deleteEndpoints(svcID EndpointSliceID, swg *lock.StoppableWaitGroup) ServiceID {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	svc, serviceOK := s.services[svcID]
-	delete(s.endpoints, svcID)
-	endpoints, _ := s.correlateEndpoints(svcID)
+	svc, serviceOK := s.services[svcID.ServiceID]
+	isEmpty := s.endpoints[svcID.ServiceID].Delete(svcID.EndpointSliceName)
+	if isEmpty {
+		delete(s.endpoints, svcID.ServiceID)
+	}
+	endpoints, _ := s.correlateEndpoints(svcID.ServiceID)
 
 	if serviceOK {
 		swg.Add()
 		event := ServiceEvent{
 			Action:    UpdateService,
-			ID:        svcID,
+			ID:        svcID.ServiceID,
 			Service:   svc,
 			Endpoints: endpoints,
 			SWG:       swg,
@@ -245,15 +258,18 @@ func (s *ServiceCache) deleteEndpoints(svcID ServiceID, swg *lock.StoppableWaitG
 		s.Events <- event
 	}
 
-	return svcID
+	return svcID.ServiceID
 }
 
 // DeleteEndpoints parses a Kubernetes endpoints and removes it from the
 // ServiceCache
 func (s *ServiceCache) DeleteEndpoints(k8sEndpoints *slim_corev1.Endpoints, swg *lock.StoppableWaitGroup) ServiceID {
 	svcID := ParseEndpointsID(k8sEndpoints)
-
-	return s.deleteEndpoints(svcID, swg)
+	epSliceID := EndpointSliceID{
+		ServiceID:         svcID,
+		EndpointSliceName: k8sEndpoints.GetName(),
+	}
+	return s.deleteEndpoints(epSliceID, swg)
 }
 
 func (s *ServiceCache) DeleteEndpointSlices(epSlice *slim_discovery_v1beta1.EndpointSlice, swg *lock.StoppableWaitGroup) ServiceID {
@@ -324,7 +340,8 @@ func (s *ServiceCache) UniqueServiceFrontends() FrontendList {
 func (s *ServiceCache) correlateEndpoints(id ServiceID) (*Endpoints, bool) {
 	endpoints := newEndpoints()
 
-	localEndpoints, hasLocalEndpoints := s.endpoints[id]
+	localEndpoints := s.endpoints[id].GetEndpoints()
+	hasLocalEndpoints := localEndpoints != nil
 	if hasLocalEndpoints {
 		for ip, e := range localEndpoints.Backends {
 			endpoints.Backends[ip] = e
@@ -335,6 +352,9 @@ func (s *ServiceCache) correlateEndpoints(id ServiceID) (*Endpoints, bool) {
 	if hasExternalService && svc.IncludeExternal {
 		externalEndpoints, hasExternalEndpoints := s.externalEndpoints[id]
 		if hasExternalEndpoints {
+			// remote cluster endpoints already contain all Endpoints from all
+			// EndpointSlices so no need to search the endpoints of a particular
+			// EndpointSlice.
 			for clusterName, remoteClusterEndpoints := range externalEndpoints.endpoints {
 				if clusterName == option.Config.ClusterName {
 					continue

pkg/k8s/rule_translate_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -310,8 +310,12 @@ func (s *K8sSuite) TestPreprocessRules(c *C) {
 		Labels: tag1,
 	}
 
-	cache.endpoints = map[ServiceID]*Endpoints{
-		serviceInfo: &endpointInfo,
+	cache.endpoints = map[ServiceID]*endpointSlices{
+		serviceInfo: {
+			epSlices: map[string]*Endpoints{
+				"": &endpointInfo,
+			},
+		},
 	}
 
 	cache.services = map[ServiceID]*Service{

pkg/k8s/service_cache_test.go
@@ -77,9 +77,17 @@ func (s *K8sSuite) TestGetUniqueServiceFrontends(c *check.C) {
 			},
 		},
 	}
-	cache.endpoints = map[ServiceID]*Endpoints{
-		svcID1: &endpoints,
-		svcID2: &endpoints,
+	cache.endpoints = map[ServiceID]*endpointSlices{
+		svcID1: {
+			epSlices: map[string]*Endpoints{
+				"": &endpoints,
+			},
+		},
+		svcID2: {
+			epSlices: map[string]*Endpoints{
+				"": &endpoints,
+			},
+		},
 	}
 
 	frontends := cache.UniqueServiceFrontends()
@@ -626,6 +634,249 @@ func (s *K8sSuite) TestNonSharedServie(c *check.C) {
 	swgSvcs.Stop()
 	c.Assert(testutils.WaitUntil(func() bool {
 		swgSvcs.Wait()
+		return true
+	}, 2*time.Second), IsNil)
+}
+
+func (s *K8sSuite) TestServiceCacheWith2EndpointSlice(c *check.C) {
+	k8sEndpointSlice1 := &slim_discovery_v1beta1.EndpointSlice{
+		AddressType: slim_discovery_v1beta1.AddressTypeIPv4,
+		ObjectMeta: slim_metav1.ObjectMeta{
+			Name:      "foo-yyyyy",
+			Namespace: "bar",
+			Labels: map[string]string{
+				slim_discovery_v1beta1.LabelServiceName: "foo",
+			},
+		},
+		Endpoints: []slim_discovery_v1beta1.Endpoint{
+			{
+				Addresses: []string{
+					"2.2.2.2",
+				},
+			},
+		},
+		Ports: []slim_discovery_v1beta1.EndpointPort{
+			{
+				Name:     func() *string { a := "http-test-svc"; return &a }(),
+				Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+				Port:     func() *int32 { a := int32(8080); return &a }(),
+			},
+		},
+	}
+
+	k8sEndpointSlice2 := &slim_discovery_v1beta1.EndpointSlice{
+		AddressType: slim_discovery_v1beta1.AddressTypeIPv4,
+		ObjectMeta: slim_metav1.ObjectMeta{
+			Name:      "foo-xxxxx",
+			Namespace: "bar",
+			Labels: map[string]string{
+				slim_discovery_v1beta1.LabelServiceName: "foo",
+			},
+		},
+		Endpoints: []slim_discovery_v1beta1.Endpoint{
+			{
+				Addresses: []string{
+					"2.2.2.3",
+				},
+			},
+		},
+		Ports: []slim_discovery_v1beta1.EndpointPort{
+			{
+				Name:     func() *string { a := "http-test-svc"; return &a }(),
+				Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+				Port:     func() *int32 { a := int32(8080); return &a }(),
+			},
+		},
+	}
+
+	k8sEndpointSlice3 := &slim_discovery_v1beta1.EndpointSlice{
+		AddressType: slim_discovery_v1beta1.AddressTypeIPv4,
+		ObjectMeta: slim_metav1.ObjectMeta{
+			Name:      "foo-xxxxx",
+			Namespace: "baz",
+			Labels: map[string]string{
+				slim_discovery_v1beta1.LabelServiceName: "foo",
+			},
+		},
+		Endpoints: []slim_discovery_v1beta1.Endpoint{
+			{
+				Addresses: []string{
+					"2.2.2.4",
+				},
+			},
+		},
+		Ports: []slim_discovery_v1beta1.EndpointPort{
+			{
+				Name:     func() *string { a := "http-test-svc"; return &a }(),
+				Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+				Port:     func() *int32 { a := int32(8080); return &a }(),
+			},
+		},
+	}
+
+	svcCache := NewServiceCache(fakeDatapath.NewNodeAddressing())
+
+	k8sSvc := &slim_corev1.Service{
+		ObjectMeta: slim_metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "bar",
+			Labels: map[string]string{
+				"foo": "bar",
+			},
+		},
+		Spec: slim_corev1.ServiceSpec{
+			ClusterIP: "127.0.0.1",
+			Selector: map[string]string{
+				"foo": "bar",
+			},
+			Type: slim_corev1.ServiceTypeClusterIP,
+		},
+	}
+
+	swgSvcs := lock.NewStoppableWaitGroup()
+	svcID := svcCache.UpdateService(k8sSvc, swgSvcs)
+
+	time.Sleep(100 * time.Millisecond)
+
+	select {
+	case <-svcCache.Events:
+		c.Error("Unexpected service event received before endpoints have been imported")
+	default:
+	}
+
+	swgEps := lock.NewStoppableWaitGroup()
+	svcCache.UpdateEndpointSlices(k8sEndpointSlice1, swgEps)
+	svcCache.UpdateEndpointSlices(k8sEndpointSlice2, swgEps)
+	svcCache.UpdateEndpointSlices(k8sEndpointSlice3, swgEps)
+
+	// The service should be ready as both service and endpoints have been
+	// imported for k8sEndpointSlice1
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	// The service should be ready as both service and endpoints have been
+	// imported for k8sEndpointSlice2
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	select {
+	case <-svcCache.Events:
+		c.Error("Unexpected service event received when endpoints not selected by a service have been imported")
+	default:
+	}
+	endpoints, ready := svcCache.correlateEndpoints(svcID)
+	c.Assert(ready, check.Equals, true)
+	c.Assert(endpoints.String(), check.Equals, "2.2.2.2:8080/TCP,2.2.2.3:8080/TCP")
+
+	// Updating the service without changing it should not result in an event
+	svcCache.UpdateService(k8sSvc, swgSvcs)
+	time.Sleep(100 * time.Millisecond)
+	select {
+	case <-svcCache.Events:
+		c.Error("Unexpected service event received for unchanged service object")
+	default:
+	}
+
+	// Deleting the service will result in a service delete event
+	svcCache.DeleteService(k8sSvc, swgSvcs)
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, DeleteService)
+		c.Assert(event.ID, check.Equals, svcID)
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	// Reinserting the service should re-match with the still existing endpoints
+	svcCache.UpdateService(k8sSvc, swgSvcs)
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	// Deleting the k8sEndpointSlice2 will result in a service update event
+	svcCache.DeleteEndpointSlices(k8sEndpointSlice2, swgEps)
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	endpoints, ready = svcCache.correlateEndpoints(svcID)
+	c.Assert(ready, check.Equals, true)
+	c.Assert(endpoints.String(), check.Equals, "2.2.2.2:8080/TCP")
+
+	svcCache.DeleteEndpointSlices(k8sEndpointSlice1, swgEps)
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	endpoints, serviceReady := svcCache.correlateEndpoints(svcID)
+	c.Assert(serviceReady, check.Equals, false)
+	c.Assert(endpoints.String(), check.Equals, "")
+
+	// Reinserting the endpoints should re-match with the still existing service
+	svcCache.UpdateEndpointSlices(k8sEndpointSlice1, swgEps)
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, UpdateService)
+		c.Assert(event.ID, check.Equals, svcID)
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	endpoints, serviceReady = svcCache.correlateEndpoints(svcID)
+	c.Assert(serviceReady, check.Equals, true)
+	c.Assert(endpoints.String(), check.Equals, "2.2.2.2:8080/TCP")
+
+	// Deleting the service will result in a service delete event
+	svcCache.DeleteService(k8sSvc, swgSvcs)
+	c.Assert(testutils.WaitUntil(func() bool {
+		event := <-svcCache.Events
+		defer event.SWG.Done()
+		c.Assert(event.Action, check.Equals, DeleteService)
+		c.Assert(event.ID, check.Equals, svcID)
+		return true
+	}, 2*time.Second), check.IsNil)
+
+	// Deleting the endpoints will not emit an event as the notification
+	// was sent out when the service was deleted.
+	svcCache.DeleteEndpointSlices(k8sEndpointSlice1, swgEps)
+	time.Sleep(100 * time.Millisecond)
+	select {
+	case <-svcCache.Events:
+		c.Error("Unexpected service delete event received")
+	default:
+	}
+
+	swgSvcs.Stop()
+	c.Assert(testutils.WaitUntil(func() bool {
+		swgSvcs.Wait()
+		return true
+	}, 2*time.Second), IsNil)
+
+	swgEps.Stop()
+	c.Assert(testutils.WaitUntil(func() bool {
+		swgEps.Wait()
 		return true
 	}, 2*time.Second), IsNil)
 }