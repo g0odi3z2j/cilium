g0odi3z2j/cilium

commit d187e7edccd996a626443a1555cf35d550307d35

cocci: Detect unlogged missed tail calls
This new Coccinelle script will detect tail calls (ep_tail_call() calls)
that are not followed by either a call to send_drop_notify_error(arg1,
arg2, DROP_MISSED_TAIL_CALL, ...) or return DROP_MISSED_TAIL_CALL.

Tail calls in macros are not analyzed.

The script cannot patch such unlogged tail calls as the correct fix
depends on the context. It is up to the developer to fix any error found.

Signed-off-by: Paul Chaignon <paul@cilium.io>

contrib/coccinelle/tail_calls.cocci
@@ -1,0 +1,97 @@
+// SPDX-License-Identifier: GPL-2.0
+/// Find tail calls not followed by a call to send_drop_notify_error with
+/// DROP_MISSED_TAIL_CALL. Such code patterns may lead to missed tail calls not
+/// being logged.
+// Confidence: Medium
+// Copyright (C) 2020 Authors of Cilium.
+// Comments:
+// Options: --include-headers
+
+@initialize:python@
+@@
+
+cnt = 0
+
+// We whitelist two ep_tail_call cases:
+// - One used in invoke_tailcall_if as we'll check invoke_tailcall_if itself.
+// - One used in send_drop_notify() as we're already in the code that's
+//   supposed to be called after tail calls.
+def whitelist_tailcalls(p):
+    return p.current_element != "send_drop_notify" and \
+           not p.file.endswith("lib/tailcall.h")
+
+
+@rule@
+position p : script:python() { whitelist_tailcalls(p[0]) };
+expression e1, e2, e3, e4, x;
+symbol ret;
+@@
+
+(
+  // Classic cases of send_drop_notify_error with DROP_MISSED_TAIL_CALL.
+  ep_tail_call(...);
+  ... when forall
+  return send_drop_notify_error(e1, e2, DROP_MISSED_TAIL_CALL, ...);
+|
+  ep_tail_call(...);
+  <+... when forall
+  x = DROP_MISSED_TAIL_CALL;
+  ...+>
+  return send_drop_notify_error(e1, e2, x, ...);
+|
+  // We also whitelist any function returning DROP_MISSED_TAIL_CALL, assuming
+  // this will be catch afterwards and transformed in call to
+  // send_drop_notify_error().
+  ep_tail_call(...);
+  ... when forall
+  return DROP_MISSED_TAIL_CALL;
+|
+  ep_tail_call(...);
+  <+... when forall
+  x = DROP_MISSED_TAIL_CALL;
+  ...+>
+  return x;
+|
+  ep_tail_call@p(...);
+|
+  // invoke_tailcall_if sets variable ret which should be used in subsequent
+  // call to send_drop_notify{,_error}.
+  invoke_tailcall_if(...);
+  ... when != ret = ...;
+      when forall
+  \(
+    return send_drop_notify(e1, e2, e3, e4, ret, CTX_ACT_DROP, ...);
+  \|
+    return send_drop_notify_error(e1, e2, ret, CTX_ACT_DROP, ...);
+  \)
+|
+  invoke_tailcall_if(...);
+  ... when != ret = ...;
+      when forall
+  if (IS_ERR(ret))
+    \(
+      return send_drop_notify(e1, e2, e3, e4, ret, CTX_ACT_DROP, ...);
+    \|
+      return send_drop_notify_error(e1, e2, ret, CTX_ACT_DROP, ...);
+    \)
+|
+  invoke_tailcall_if@p(...);
+)
+
+
+@script:python@
+p << rule.p;
+@@
+
+print "* file %s: DROP_MISSED_TAIL_CALL missing after tail call on line %s" % (p[0].file, p[0].line)
+cnt += 1
+
+
+@finalize:python@
+@@
+
+if cnt > 0:
+  print """Unlogged tail calls found. Please fix and use the following command to check:
+docker run --rm --user 1000 --workdir /workspace -v `pwd`:/workspace \\
+    -it docker.io/cilium/coccicheck make -C bpf coccicheck\n
+"""

commit 63ec0ee21258f6cebc11eae5b0fbeac190ee3f87

cocci: Simplify and extend tail_call.cocci
This commit extends tail_call.cocci to:
- recognize invoke_tailcall_if() calls followed by 'return ret;', as
  used in nodeport.h;
- refactor the code a bit using disjunctions;
- ensure the return statement we are trying to match (e.g., return
  DROP_MISSED_TAIL_CALL or return send_drop_notify()) is not preceeded by
  another return statement.

Fixes: d187e7e ("cocci: Detect unlogged missed tail calls")
Signed-off-by: Paul Chaignon <paul@cilium.io>

contrib/coccinelle/tail_calls.cocci
@@ -21,7 +21,7 @@ def whitelist_tailcalls(p):
            not p.file.endswith("lib/tailcall.h")
 
 
-@rule@
+@rule forall@
 position p : script:python() { whitelist_tailcalls(p[0]) };
 expression e1, e2, e3, e4, x;
 symbol ret;
@@ -30,50 +30,58 @@ symbol ret;
 (
   // Classic cases of send_drop_notify_error with DROP_MISSED_TAIL_CALL.
   ep_tail_call(...);
-  ... when forall
+  ... when != return ...;
   return send_drop_notify_error(e1, e2, DROP_MISSED_TAIL_CALL, ...);
 |
   ep_tail_call(...);
-  <+... when forall
+  <+... when != return ...;
   x = DROP_MISSED_TAIL_CALL;
   ...+>
   return send_drop_notify_error(e1, e2, x, ...);
 |
   // We also whitelist any function returning DROP_MISSED_TAIL_CALL, assuming
-  // this will be catch afterwards and transformed in call to
+  // this will be caught afterwards and transformed in call to
   // send_drop_notify_error().
-  ep_tail_call(...);
-  ... when forall
+  \(ep_tail_call\|invoke_tailcall_if\)(...);
+  ... when != return ...;
   return DROP_MISSED_TAIL_CALL;
 |
   ep_tail_call(...);
-  <+... when forall
+  <+... when != return ...;
   x = DROP_MISSED_TAIL_CALL;
   ...+>
   return x;
-|
-  ep_tail_call@p(...);
 |
   // invoke_tailcall_if sets variable ret which should be used in subsequent
   // call to send_drop_notify{,_error}.
   invoke_tailcall_if(...);
   ... when != ret = ...;
-      when forall
+      when != return ...;
   \(
     return send_drop_notify(e1, e2, e3, e4, ret, CTX_ACT_DROP, ...);
   \|
     return send_drop_notify_error(e1, e2, ret, CTX_ACT_DROP, ...);
+  \|
+    // For invoke_tailcall_if(), it sets variable 'ret', so we need to check
+    // that variable specifically.
+    return ret;
   \)
 |
   invoke_tailcall_if(...);
   ... when != ret = ...;
-      when forall
+      when != return ...;
   if (IS_ERR(ret))
     \(
       return send_drop_notify(e1, e2, e3, e4, ret, CTX_ACT_DROP, ...);
     \|
       return send_drop_notify_error(e1, e2, ret, CTX_ACT_DROP, ...);
+    \|
+      // For invoke_tailcall_if(), it sets variable 'ret', so we need to check
+      // that variable specifically.
+      return ret;
     \)
+|
+  ep_tail_call@p(...);
 |
   invoke_tailcall_if@p(...);
 )