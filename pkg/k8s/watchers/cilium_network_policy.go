g0odi3z2j/cilium

commit 7a896780fef5a19fca509cf95eea4b443925c2ac

use deepequal generated code
This commit uses the deepqual code generated in the previous code.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/cilium_network_policy.go
@@ -138,7 +138,7 @@ func (k *K8sWatcher) ciliumNetworkPoliciesInit(ciliumNPClient *k8s.K8sCiliumClie
 				if oldCNP := k8s.ObjToSlimCNP(oldObj); oldCNP != nil {
 					if newCNP := k8s.ObjToSlimCNP(newObj); newCNP != nil {
 						valid = true
-						if k8s.EqualV2CNP(oldCNP, newCNP) {
+						if oldCNP.DeepEqual(newCNP) {
 							equal = true
 							return
 						}
@@ -288,7 +288,7 @@ func (k *K8sWatcher) updateCiliumNetworkPolicyV2(ciliumNPClient clientset.Interf
 
 	// Do not add rule into policy repository if the spec remains unchanged.
 	if !option.Config.DisableCNPStatusUpdates {
-		if oldRuleCpy.SpecEquals(newRuleCpy.CiliumNetworkPolicy) {
+		if oldRuleCpy.Spec.DeepEqual(newRuleCpy.CiliumNetworkPolicy.Spec) {
 			if !oldRuleCpy.AnnotationsEquals(newRuleCpy.CiliumNetworkPolicy) {
 
 				// Update annotations within a controller so the status of the update

operator/ccnp_event.go
@@ -97,7 +97,7 @@ func enableCCNPWatcher() error {
 				metrics.EventTSK8s.SetToCurrentTime()
 				if oldCNP := k8s.ObjToSlimCNP(oldObj); oldCNP != nil {
 					if newCNP := k8s.ObjToSlimCNP(newObj); newCNP != nil {
-						if k8s.EqualV2CNP(oldCNP, newCNP) {
+						if oldCNP.DeepEqual(newCNP) {
 							return
 						}
 

operator/cilium_node.go
@@ -16,7 +16,6 @@ package main
 
 import (
 	"context"
-	"reflect"
 
 	"github.com/cilium/cilium/pkg/ipam/allocator"
 	"github.com/cilium/cilium/pkg/k8s"
@@ -100,11 +99,11 @@ func (c *ciliumNodeUpdateImplementation) UpdateStatus(origNode, node *v2.CiliumN
 	k8sCapabilities := k8sversion.Capabilities()
 	switch {
 	case k8sCapabilities.UpdateStatus:
-		if origNode == nil || !reflect.DeepEqual(origNode.Status, node.Status) {
+		if origNode == nil || !origNode.Status.DeepEqual(&node.Status) {
 			return ciliumK8sClient.CiliumV2().CiliumNodes().UpdateStatus(context.TODO(), node, metav1.UpdateOptions{})
 		}
 	default:
-		if origNode == nil || !reflect.DeepEqual(origNode.Status, node.Status) {
+		if origNode == nil || !origNode.Status.DeepEqual(&node.Status) {
 			return ciliumK8sClient.CiliumV2().CiliumNodes().Update(context.TODO(), node, metav1.UpdateOptions{})
 		}
 	}
@@ -117,11 +116,11 @@ func (c *ciliumNodeUpdateImplementation) Update(origNode, node *v2.CiliumNode) (
 	k8sCapabilities := k8sversion.Capabilities()
 	switch {
 	case k8sCapabilities.UpdateStatus:
-		if origNode == nil || !reflect.DeepEqual(origNode.Spec, node.Spec) {
+		if origNode == nil || !origNode.Spec.DeepEqual(&node.Spec) {
 			return ciliumK8sClient.CiliumV2().CiliumNodes().Update(context.TODO(), node, metav1.UpdateOptions{})
 		}
 	default:
-		if origNode == nil || !reflect.DeepEqual(origNode, node) {
+		if origNode == nil || !origNode.DeepEqual(node) {
 			return ciliumK8sClient.CiliumV2().CiliumNodes().Update(context.TODO(), node, metav1.UpdateOptions{})
 		}
 	}

operator/cnp_event.go
@@ -105,7 +105,7 @@ func enableCNPWatcher() error {
 				metrics.EventTSK8s.SetToCurrentTime()
 				if oldCNP := k8s.ObjToSlimCNP(oldObj); oldCNP != nil {
 					if newCNP := k8s.ObjToSlimCNP(newObj); newCNP != nil {
-						if k8s.EqualV2CNP(oldCNP, newCNP) {
+						if oldCNP.DeepEqual(newCNP) {
 							return
 						}
 

operator/k8s_node.go
@@ -71,7 +71,7 @@ func runNodeWatcher(nodeManager *allocator.NodeEventHandler) error {
 			UpdateFunc: func(oldObj, newObj interface{}) {
 				if oldNode := k8s.ObjToV1Node(oldObj); oldNode != nil {
 					if newNode := k8s.ObjToV1Node(newObj); newNode != nil {
-						if k8s.EqualV1Node(oldNode, newNode) {
+						if oldNode.DeepEqual(newNode) {
 							return
 						}
 

operator/k8s_service_sync.go
@@ -132,7 +132,7 @@ func startSynchronizingServices() {
 				metrics.EventTSK8s.SetToCurrentTime()
 				if oldk8sSvc := k8s.ObjToV1Services(oldObj); oldk8sSvc != nil {
 					if newk8sSvc := k8s.ObjToV1Services(newObj); newk8sSvc != nil {
-						if k8s.EqualV1Services(oldk8sSvc, newk8sSvc, nil) {
+						if oldk8sSvc.DeepEqual(newk8sSvc) {
 							return
 						}
 
@@ -212,7 +212,7 @@ func endpointsInit(k8sClient kubernetes.Interface, swgEps *lock.StoppableWaitGro
 				metrics.EventTSK8s.SetToCurrentTime()
 				if oldk8sEP := k8s.ObjToV1Endpoints(oldObj); oldk8sEP != nil {
 					if newk8sEP := k8s.ObjToV1Endpoints(newObj); newk8sEP != nil {
-						if k8s.EqualV1Endpoints(oldk8sEP, newk8sEP) {
+						if oldk8sEP.DeepEqual(newk8sEP) {
 							return
 						}
 						k8sSvcCache.UpdateEndpoints(newk8sEP, swgEps)
@@ -260,7 +260,7 @@ func endpointSlicesInit(k8sClient kubernetes.Interface, swgEps *lock.StoppableWa
 				metrics.EventTSK8s.SetToCurrentTime()
 				if oldk8sEP := k8s.ObjToV1EndpointSlice(oldObj); oldk8sEP != nil {
 					if newk8sEP := k8s.ObjToV1EndpointSlice(newObj); newk8sEP != nil {
-						if k8s.EqualV1EndpointSlice(oldk8sEP, newk8sEP) {
+						if oldk8sEP.DeepEqual(newk8sEP) {
 							return
 						}
 						k8sSvcCache.UpdateEndpointSlices(newk8sEP, swgEps)

pkg/k8s/apis/cilium.io/v2/types.go
@@ -234,14 +234,6 @@ func (r *CiliumNetworkPolicy) SetDerivedPolicyStatus(derivativePolicyName string
 	r.Status.DerivativePolicies[derivativePolicyName] = status
 }
 
-// SpecEquals returns true if the spec and specs metadata is the sa
-func (r *CiliumNetworkPolicy) SpecEquals(o *CiliumNetworkPolicy) bool {
-	if o == nil {
-		return r == nil
-	}
-	return r.Spec.DeepEquals(o.Spec) && r.Specs.DeepEquals(o.Specs)
-}
-
 // AnnotationsEquals returns true if ObjectMeta.Annotations of each
 // CiliumNetworkPolicy are equivalent (i.e., they contain equivalent key-value
 // pairs).

pkg/k8s/factory_functions.go
@@ -15,9 +15,6 @@
 package k8s
 
 import (
-	"reflect"
-
-	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/comparator"
 	"github.com/cilium/cilium/pkg/datapath"
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
@@ -192,15 +189,6 @@ func ObjToV1Namespace(obj interface{}) *slim_corev1.Namespace {
 	return nil
 }
 
-func EqualV1NetworkPolicy(np1, np2 *slim_networkingv1.NetworkPolicy) bool {
-	// As Cilium uses all of the Spec from a NP it's not probably not worth
-	// it to create a dedicated deep equal	 function to compare both network
-	// policies.
-	return np1.Name == np2.Name &&
-		np1.Namespace == np2.Namespace &&
-		reflect.DeepEqual(np1.Spec, np2.Spec)
-}
-
 func EqualV1Services(k8sSVC1, k8sSVC2 *slim_corev1.Service, nodeAddressing datapath.NodeAddressing) bool {
 	// Service annotations are used to mark services as global, shared, etc.
 	if !comparator.MapStringEquals(k8sSVC1.GetAnnotations(), k8sSVC2.GetAnnotations()) {
@@ -219,49 +207,6 @@ func EqualV1Services(k8sSVC1, k8sSVC2 *slim_corev1.Service, nodeAddressing datap
 	return svc1.DeepEquals(svc2)
 }
 
-func EqualV1Endpoints(ep1, ep2 *slim_corev1.Endpoints) bool {
-	// We only care about the Name, Namespace and Subsets of a particular
-	// endpoint.
-	return ep1.Name == ep2.Name &&
-		ep1.Namespace == ep2.Namespace &&
-		reflect.DeepEqual(ep1.Subsets, ep2.Subsets)
-}
-
-func EqualV1EndpointSlice(ep1, ep2 *slim_discover_v1beta1.EndpointSlice) bool {
-	// We only care about the Name, Namespace and Subsets of a particular
-	// endpoint.
-	// AddressType is omitted because it's immutable after EndpointSlice's
-	// creation.
-	return ep1.Name == ep2.Name &&
-		ep1.Namespace == ep2.Namespace &&
-		reflect.DeepEqual(ep1.Endpoints, ep2.Endpoints) &&
-		reflect.DeepEqual(ep1.Ports, ep2.Ports)
-}
-
-func EqualV2CNP(cnp1, cnp2 *types.SlimCNP) bool {
-	if !(cnp1.Name == cnp2.Name && cnp1.Namespace == cnp2.Namespace) {
-		return false
-	}
-
-	// Ignore v1.LastAppliedConfigAnnotation annotation
-	lastAppliedCfgAnnotation1, ok1 := cnp1.GetAnnotations()[v1.LastAppliedConfigAnnotation]
-	lastAppliedCfgAnnotation2, ok2 := cnp2.GetAnnotations()[v1.LastAppliedConfigAnnotation]
-	defer func() {
-		if ok1 && cnp1.GetAnnotations() != nil {
-			cnp1.GetAnnotations()[v1.LastAppliedConfigAnnotation] = lastAppliedCfgAnnotation1
-		}
-		if ok2 && cnp2.GetAnnotations() != nil {
-			cnp2.GetAnnotations()[v1.LastAppliedConfigAnnotation] = lastAppliedCfgAnnotation2
-		}
-	}()
-	delete(cnp1.GetAnnotations(), v1.LastAppliedConfigAnnotation)
-	delete(cnp2.GetAnnotations(), v1.LastAppliedConfigAnnotation)
-
-	return comparator.MapStringEquals(cnp1.GetAnnotations(), cnp2.GetAnnotations()) &&
-		reflect.DeepEqual(cnp1.Spec, cnp2.Spec) &&
-		reflect.DeepEqual(cnp1.Specs, cnp2.Specs)
-}
-
 // AnnotationsEqual returns whether the annotation with any key in
 // relevantAnnotations is equal in anno1 and anno2.
 func AnnotationsEqual(relevantAnnotations []string, anno1, anno2 map[string]string) bool {
@@ -271,119 +216,6 @@ func AnnotationsEqual(relevantAnnotations []string, anno1, anno2 map[string]stri
 		}
 	}
 	return true
-}
-
-func EqualV1PodContainers(c1, c2 slim_corev1.Container) bool {
-	if c1.Name != c2.Name ||
-		c1.Image != c2.Image {
-		return false
-	}
-
-	if len(c1.VolumeMounts) != len(c2.VolumeMounts) {
-		return false
-	}
-	for i, vlmMount1 := range c1.VolumeMounts {
-		if vlmMount1.MountPath != c2.VolumeMounts[i].MountPath {
-			return false
-		}
-	}
-	return true
-}
-
-// EqualPodIPs returns true if both slices are equal.
-func EqualPodIPs(a, b []slim_corev1.PodIP) bool {
-	// If one is nil, the other must also be nil.
-	if (a == nil) != (b == nil) {
-		return false
-	}
-
-	if len(a) != len(b) {
-		return false
-	}
-
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
-func EqualV1Pod(pod1, pod2 *slim_corev1.Pod) bool {
-	// We only care about the HostIP, the PodIP and the labels of the pods.
-	if pod1.Status.PodIP != pod2.Status.PodIP ||
-		pod1.Status.HostIP != pod2.Status.HostIP ||
-		pod1.Spec.ServiceAccountName != pod2.Spec.ServiceAccountName ||
-		pod1.Spec.HostNetwork != pod2.Spec.HostNetwork {
-		return false
-	}
-
-	if !EqualPodIPs(pod1.Status.PodIPs, pod2.Status.PodIPs) {
-		return false
-	}
-
-	if !AnnotationsEqual([]string{annotation.ProxyVisibility}, pod1.Annotations, pod2.Annotations) {
-		return false
-	}
-
-	oldPodLabels := pod1.Labels
-	newPodLabels := pod2.Labels
-	if !comparator.MapStringEquals(oldPodLabels, newPodLabels) {
-		return false
-	}
-
-	if len(pod1.Spec.Containers) != len(pod2.Spec.Containers) {
-		return false
-	}
-	for i, c1 := range pod1.Spec.Containers {
-		if !EqualV1PodContainers(c1, pod2.Spec.Containers[i]) {
-			return false
-		}
-	}
-	return true
-}
-
-func EqualV1Node(node1, node2 *slim_corev1.Node) bool {
-	if node1.GetObjectMeta().GetName() != node2.GetObjectMeta().GetName() {
-		return false
-	}
-
-	anno1 := node1.GetAnnotations()
-	anno2 := node2.GetAnnotations()
-	annotationsWeCareAbout := []string{
-		annotation.CiliumHostIP,
-		annotation.CiliumHostIPv6,
-		annotation.V4HealthName,
-		annotation.V6HealthName,
-	}
-	for _, an := range annotationsWeCareAbout {
-		if anno1[an] != anno2[an] {
-			return false
-		}
-	}
-	if len(node1.Spec.Taints) != len(node2.Spec.Taints) {
-		return false
-	}
-	for i, taint2 := range node2.Spec.Taints {
-		taint1 := node1.Spec.Taints[i]
-		if !taint1.MatchTaint(&taint2) {
-			return false
-		}
-		if taint1.Value != taint2.Value {
-			return false
-		}
-		if !taint1.TimeAdded.Equal(taint2.TimeAdded) {
-			return false
-		}
-	}
-	return true
-}
-
-func EqualV1Namespace(ns1, ns2 *slim_corev1.Namespace) bool {
-	// we only care about namespace labels.
-	return ns1.Name == ns2.Name &&
-		comparator.MapStringEquals(ns1.GetLabels(), ns2.GetLabels())
 }
 
 func convertToK8sServicePorts(ports []v1.ServicePort) []slim_corev1.ServicePort {

commit d7211e9d7a1cf150aaa5f58aa862f5b52c3ace1c

k8s/watchers: fix missing missing CNP/CCNP updates
Since we were only comparing the spec of a CNP / CCNP, whenever the
cilium-agent received a CNP or CCNP containing a 'specs', any following
updates to that policy wouldn't be enforced in Cilium since the network
policy would be considered equal as the one already being enforced.

As Cilium is already comparing the network policies, before processing
any policy update, we can remove this Spec.Equals.

Fixes: 7a896780fef5 ("use deepequal generated code")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/cilium_network_policy.go
@@ -288,27 +288,24 @@ func (k *K8sWatcher) updateCiliumNetworkPolicyV2(ciliumNPClient clientset.Interf
 
 	// Do not add rule into policy repository if the spec remains unchanged.
 	if !option.Config.DisableCNPStatusUpdates {
-		if oldRuleCpy.Spec.DeepEqual(newRuleCpy.CiliumNetworkPolicy.Spec) {
-			if !oldRuleCpy.AnnotationsEquals(newRuleCpy.CiliumNetworkPolicy) {
+		if !oldRuleCpy.AnnotationsEquals(newRuleCpy.CiliumNetworkPolicy) {
 
-				// Update annotations within a controller so the status of the update
-				// is trackable from the list of running controllers, and so we do
-				// not block subsequent policy lifecycle operations from Kubernetes
-				// until the update is complete.
-				oldCtrlName := oldRuleCpy.GetControllerName()
-				newCtrlName := newRuleCpy.GetControllerName()
+			// Update annotations within a controller so the status of the update
+			// is trackable from the list of running controllers, and so we do
+			// not block subsequent policy lifecycle operations from Kubernetes
+			// until the update is complete.
+			oldCtrlName := oldRuleCpy.GetControllerName()
+			newCtrlName := newRuleCpy.GetControllerName()
 
-				// In case the controller name changes between copies of rules,
-				// remove old controller so we do not leak goroutines.
-				if oldCtrlName != newCtrlName {
-					err := k8sCM.RemoveController(oldCtrlName)
-					if err != nil {
-						log.Debugf("Unable to remove controller %s: %s", oldCtrlName, err)
-					}
+			// In case the controller name changes between copies of rules,
+			// remove old controller so we do not leak goroutines.
+			if oldCtrlName != newCtrlName {
+				err := k8sCM.RemoveController(oldCtrlName)
+				if err != nil {
+					log.Debugf("Unable to remove controller %s: %s", oldCtrlName, err)
 				}
-				k.updateCiliumNetworkPolicyV2AnnotationsOnly(ciliumNPClient, ciliumV2Store, newRuleCpy)
 			}
-			return nil
+			k.updateCiliumNetworkPolicyV2AnnotationsOnly(ciliumNPClient, ciliumV2Store, newRuleCpy)
 		}
 	}
 