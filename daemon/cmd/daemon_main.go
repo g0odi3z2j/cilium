g0odi3z2j/cilium

commit 27653e34653294e54f324f577ff51ec27a1f8fb8

daemon: Introduce enable-host-firewall option
BPF-based egress load balancing isn't supported from the host namespace,
so the host policies will fail (due to wrong destination endpoint) if
that was enabled. Most users likely use bpf_sock (newer kernels) or
kube-proxy for egress load balancing so we just disable BPF-based egress
load balancing with a warning if the host firewall is enabled.

Signed-off-by: Paul Chaignon <paul@cilium.io>

daemon/cmd/daemon_main.go
@@ -526,6 +526,9 @@ func init() {
 	flags.Bool(option.EnableSessionAffinity, false, "Enable support for service session affinity")
 	option.BindEnv(option.EnableSessionAffinity)
 
+	flags.Bool(option.EnableHostFirewall, false, "Enable host network policies")
+	option.BindEnv(option.EnableHostFirewall)
+
 	flags.String(option.LibDir, defaults.LibraryPath, "Directory path to store runtime build environment")
 	option.BindEnv(option.LibDir)
 
@@ -1099,6 +1102,7 @@ func initEnv(cmd *cobra.Command) {
 		option.Config.EncryptInterface = link
 	}
 
+	checkHostFirewallWithEgressLB()
 	initKubeProxyReplacementOptions()
 	if option.Config.EnableNodePort {
 		if err := node.InitNodePortAddrs(option.Config.Devices); err != nil {
@@ -1130,6 +1134,17 @@ func initEnv(cmd *cobra.Command) {
 		if !probe.HaveFullLPM() {
 			log.Fatal("BPF ip-masq-agent needs kernel 4.16 or newer")
 		}
+	}
+
+	if option.Config.EnableHostFirewall && len(option.Config.Devices) == 0 {
+		device, err := linuxdatapath.NodeDeviceNameWithDefaultRoute()
+		if err != nil {
+			msg := "Host firewall's external facing device could not be determined. Use --%s to specify."
+			log.WithError(err).Fatalf(msg, option.Device)
+		}
+		log.WithField(logfields.Interface, device).
+			Info("Using auto-derived device for host firewall")
+		option.Config.Devices = []string{device}
 	}
 
 	// If there is one device specified, use it to derive better default
@@ -1550,6 +1565,30 @@ func initClockSourceOption() {
 				}
 			}
 		}
+	}
+}
+
+func checkHostFirewallWithEgressLB() {
+	// Egress LB is enabled in datapath under condition:
+	// ENABLE_SERVICES && (!ENABLE_HOST_SERVICES_FULL || \
+	//                    (ENABLE_EXTERNAL_IP && !BPF_HAVE_NETNS_COOKIE))
+	// We can't enable both egress LB and host firewall on kernels <4.14 due to
+	// the verifier complexity limit, at 96k instructions.
+	var netnsCookieSupport bool
+	pm := probes.NewProbeManager()
+	h1, h2 := pm.GetHelpers("cgroup_sock_addr"), pm.GetHelpers("cgroup_sock")
+	if _, ok := h1["bpf_get_netns_cookie"]; ok {
+		if _, ok := h2["bpf_get_netns_cookie"]; ok {
+			netnsCookieSupport = true
+		}
+	}
+	egressLBEnabled := !option.Config.DisableK8sServices &&
+		(!option.Config.EnableHostServicesTCP || !option.Config.EnableHostServicesUDP ||
+			(option.Config.EnableExternalIPs && !netnsCookieSupport))
+	if option.Config.EnableHostFirewall && egressLBEnabled {
+		log.Warn("Enabling both BPF-based east-west load balancing and the host firewall isn't supported yet. Disabling east-west load balancing.")
+		option.Config.DisableK8sServices = true
+		option.Config.KubeProxyReplacement = option.KubeProxyReplacementDisabled
 	}
 }
 

pkg/datapath/linux/config/config.go
@@ -261,6 +261,10 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		cDefinesMap["NODEPORT_PORT_MAX_NAT"] = "65535"
 	}
 
+	if option.Config.EnableHostFirewall {
+		cDefinesMap["ENABLE_HOST_FIREWALL"] = "1"
+	}
+
 	if option.Config.EncryptInterface != "" {
 		link, err := netlink.LinkByName(option.Config.EncryptInterface)
 		if err == nil {

pkg/datapath/loader/loader.go
@@ -234,7 +234,7 @@ func (l *Loader) reloadHostDatapath(ctx context.Context, ep datapath.Endpoint, o
 		interfaceNames = append(interfaceNames, device)
 		symbols = append(symbols, symbolFromHostNetdevEp)
 		directions = append(directions, dirIngress)
-		if option.Config.EnableNodePort {
+		if option.Config.EnableNodePort || option.Config.EnableHostFirewall {
 			interfaceNames = append(interfaceNames, device)
 			symbols = append(symbols, symbolToHostNetdevEp)
 			directions = append(directions, dirEgress)

pkg/option/config.go
@@ -221,6 +221,9 @@ const (
 	// LabelPrefixFile is the valid label prefixes file path
 	LabelPrefixFile = "label-prefix-file"
 
+	// EnableHostFirewall enables network policies for the host
+	EnableHostFirewall = "enable-host-firewall"
+
 	// EnableHostPort enables HostPort forwarding implemented by Cilium in BPF
 	EnableHostPort = "enable-host-port"
 
@@ -1627,6 +1630,9 @@ type DaemonConfig struct {
 	// EnableExternalIPs enables implementation of k8s services with externalIPs in datapath
 	EnableExternalIPs bool
 
+	// EnableHostFirewall enables network policies for the host
+	EnableHostFirewall bool
+
 	// K8sEnableEndpointSlice enables k8s endpoint slice feature that is used
 	// in kubernetes.
 	K8sEnableK8sEndpointSlice bool
@@ -2156,6 +2162,7 @@ func (c *DaemonConfig) Populate() {
 	c.EnableAutoProtectNodePortRange = viper.GetBool(EnableAutoProtectNodePortRange)
 	c.KubeProxyReplacement = viper.GetString(KubeProxyReplacement)
 	c.EnableSessionAffinity = viper.GetBool(EnableSessionAffinity)
+	c.EnableHostFirewall = viper.GetBool(EnableHostFirewall)
 	c.EncryptInterface = viper.GetString(EncryptInterface)
 	c.EncryptNode = viper.GetBool(EncryptNode)
 	c.EnvoyLogPath = viper.GetString(EnvoyLog)

commit 8e713ef1a40f2494ba9843f618a7bcf44ebce67a

daemon: Error if host firewall and IPSec enabled
Both features cannot be used together currently since they will both try
to attach different programs, bpf_host and bpf_network, to the
encryption interface.

Fixes: 27653e3 ("daemon: Introduce enable-host-firewall option")
Signed-off-by: Paul Chaignon <paul@cilium.io>

daemon/cmd/daemon_main.go
@@ -1122,6 +1122,10 @@ func initEnv(cmd *cobra.Command) {
 	initClockSourceOption()
 	initSockmapOption()
 
+	if option.Config.EnableHostFirewall && option.Config.EnableIPSec {
+		log.Fatal("IPSec cannot be used with the host firewall.")
+	}
+
 	// If there is one device specified, use it to derive better default
 	// allocation prefixes
 	node.InitDefaultPrefix(option.Config.DirectRoutingDevice)