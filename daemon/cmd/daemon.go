g0odi3z2j/cilium

commit 67d1ebd7592b0870101bfe42a7db243fe3613b3d

daemon: Move kubeProxyReplacement init after connect to k8s
This is required because the NodePort BPF device detection will need
to know self k8s Node IP addrs.

Also, move BPF-masq and host-fw checks too. The both depend on settings
configured by initKubeProxyReplacementOptions().

Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/cmd/daemon.go
@@ -31,8 +31,10 @@ import (
 	"github.com/cilium/cilium/pkg/counter"
 	"github.com/cilium/cilium/pkg/crypto/certificatemanager"
 	"github.com/cilium/cilium/pkg/datapath"
+	linuxdatapath "github.com/cilium/cilium/pkg/datapath/linux"
 	linuxrouting "github.com/cilium/cilium/pkg/datapath/linux/routing"
 	"github.com/cilium/cilium/pkg/datapath/loader"
+	datapathOption "github.com/cilium/cilium/pkg/datapath/option"
 	"github.com/cilium/cilium/pkg/debug"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
@@ -69,6 +71,7 @@ import (
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 	policyApi "github.com/cilium/cilium/pkg/policy/api"
+	"github.com/cilium/cilium/pkg/probe"
 	"github.com/cilium/cilium/pkg/proxy"
 	"github.com/cilium/cilium/pkg/service"
 	"github.com/cilium/cilium/pkg/sockops"
@@ -419,6 +422,52 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 		}
 
 		bootstrapStats.k8sInit.End(true)
+	}
+
+	// The kube-proxy replacement should be initialized after establishing
+	// connection to kube-apiserver, but before starting a k8s watcher, as
+	// retrieving Node object for self is needed by BPF NodePort device selection,
+	// and the k8s watcher depends on option.Config.EnableNodePort flag which
+	// can be modified by the initialization routine.
+	initKubeProxyReplacementOptions()
+	if option.Config.EnableNodePort {
+		if err := node.InitNodePortAddrs(option.Config.Devices); err != nil {
+			log.WithError(err).Fatal("Failed to initialize NodePort addrs")
+		}
+	}
+	// BPF masquerade depends on BPF NodePort, so the following checks should
+	// happen after invoking initKubeProxyReplacementOptions().
+	if option.Config.Masquerade && option.Config.EnableBPFMasquerade {
+		// TODO(brb) nodeport + ipvlan constraints will be lifted once the SNAT BPF code has been refactored
+		if !option.Config.EnableNodePort {
+			log.Fatalf("BPF masquerade requires NodePort (--%s=\"true\")", option.EnableNodePort)
+		}
+		if option.Config.DatapathMode == datapathOption.DatapathModeIpvlan {
+			log.Fatalf("BPF masquerade works only in veth mode (--%s=\"%s\"", option.DatapathMode, datapathOption.DatapathModeVeth)
+		}
+		if option.Config.EgressMasqueradeInterfaces != "" {
+			log.Fatalf("BPF masquerade does not allow to specify devices via --%s. Use --%s instead.", option.EgressMasqueradeInterfaces, option.Device)
+		}
+	} else if option.Config.EnableIPMasqAgent {
+		log.Fatalf("BPF ip-masq-agent requires --%s=\"true\" and --%s=\"true\"", option.Masquerade, option.EnableBPFMasquerade)
+	}
+	if option.Config.EnableIPMasqAgent {
+		if !option.Config.EnableIPv4 {
+			log.Fatalf("BPF ip-masq-agent requires IPv4 support (--%s=\"true\")", option.EnableIPv4Name)
+		}
+		if !probe.HaveFullLPM() {
+			log.Fatal("BPF ip-masq-agent needs kernel 4.16 or newer")
+		}
+	}
+	if option.Config.EnableHostFirewall && len(option.Config.Devices) == 0 {
+		device, err := linuxdatapath.NodeDeviceNameWithDefaultRoute()
+		if err != nil {
+			msg := "Host firewall's external facing device could not be determined. Use --%s to specify."
+			log.WithError(err).Fatalf(msg, option.Device)
+		}
+		log.WithField(logfields.Interface, device).
+			Info("Using auto-derived device for host firewall")
+		option.Config.Devices = []string{device}
 	}
 
 	d.k8sCachesSynced = d.k8sWatcher.InitK8sSubsystem()

daemon/cmd/daemon_main.go
@@ -1110,49 +1110,8 @@ func initEnv(cmd *cobra.Command) {
 		option.Config.EncryptInterface = link
 	}
 
-	initKubeProxyReplacementOptions()
-	if option.Config.EnableNodePort {
-		if err := node.InitNodePortAddrs(option.Config.Devices); err != nil {
-			log.WithError(err).Fatal("Failed to initialize NodePort addrs")
-		}
-	}
 	initClockSourceOption()
 	initSockmapOption()
-
-	if option.Config.Masquerade && option.Config.EnableBPFMasquerade {
-		// TODO(brb) nodeport + ipvlan constraints will be lifted once the SNAT BPF code has been refactored
-		if !option.Config.EnableNodePort {
-			log.Fatalf("BPF masquerade requires NodePort (--%s=\"true\")", option.EnableNodePort)
-		}
-		if option.Config.DatapathMode == datapathOption.DatapathModeIpvlan {
-			log.Fatalf("BPF masquerade works only in veth mode (--%s=\"%s\"", option.DatapathMode, datapathOption.DatapathModeVeth)
-		}
-		if option.Config.EgressMasqueradeInterfaces != "" {
-			log.Fatalf("BPF masquerade does not allow to specify devices via --%s. Use --%s instead.", option.EgressMasqueradeInterfaces, option.Device)
-		}
-	} else if option.Config.EnableIPMasqAgent {
-		log.Fatalf("BPF ip-masq-agent requires --%s=\"true\" and --%s=\"true\"", option.Masquerade, option.EnableBPFMasquerade)
-	}
-
-	if option.Config.EnableIPMasqAgent {
-		if !option.Config.EnableIPv4 {
-			log.Fatalf("BPF ip-masq-agent requires IPv4 support (--%s=\"true\")", option.EnableIPv4Name)
-		}
-		if !probe.HaveFullLPM() {
-			log.Fatal("BPF ip-masq-agent needs kernel 4.16 or newer")
-		}
-	}
-
-	if option.Config.EnableHostFirewall && len(option.Config.Devices) == 0 {
-		device, err := linuxdatapath.NodeDeviceNameWithDefaultRoute()
-		if err != nil {
-			msg := "Host firewall's external facing device could not be determined. Use --%s to specify."
-			log.WithError(err).Fatalf(msg, option.Device)
-		}
-		log.WithField(logfields.Interface, device).
-			Info("Using auto-derived device for host firewall")
-		option.Config.Devices = []string{device}
-	}
 
 	// If there is one device specified, use it to derive better default
 	// allocation prefixes

commit 11e355fecdc020d134275ea3c711e05c0e333422

daemon: Fix session affinity maps creation
Previously, initKubeProxyReplacementOptions() was called after
initMaps() which when running with --kube-proxy-replacement=probe and
--enable-session-affinity=false resulted in the session affinity -related
BPF maps not being created.

An example error log message which illustrated that:

    level=warning msg="Unable to add entry to
    affinity match map" backendID=5 error="Unable to get object
    /sys/fs/bpf/tc/globals/cilium_lb_affinity_match: no such file or
    directory" serviceID=11 subsys=service

Fixes: 67d1ebd759 ("daemon: Move kubeProxyReplacement init after connect to k8s")
Reported-by: Jarno Rajahalme <jarno@covalent.io>
Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/cmd/daemon.go
@@ -356,6 +356,14 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 		})
 	}
 
+	// Do the partial kube-proxy replacement initialization before creating BPF
+	// maps. Otherwise, some maps might not be created (e.g. session affinity).
+	// finishKubeProxyReplacementInit(), which is called later after the device
+	// detection, might disable BPF NodePort and friends. But this is fine, as
+	// the feature does not influence the decision which BPF maps should be
+	// created.
+	isKubeProxyReplacementStrict := initKubeProxyReplacementOptions()
+
 	// Open or create BPF maps.
 	bootstrapStats.mapsInit.Start()
 	err = d.initMaps()
@@ -424,13 +432,12 @@ func NewDaemon(ctx context.Context, dp datapath.Datapath) (*Daemon, *endpointRes
 		bootstrapStats.k8sInit.End(true)
 	}
 
-	// The kube-proxy replacement should be initialized after establishing
-	// connection to kube-apiserver, but before starting a k8s watcher, as
-	// retrieving Node object for self is needed by BPF NodePort device selection,
-	// and the k8s watcher depends on option.Config.EnableNodePort flag which
-	// can be modified by the initialization routine.
-	strict := initKubeProxyReplacementOptions()
-	detectDevicesForNodePortAndHostFirewall(strict)
+	// The kube-proxy replacement and host-fw devices detection should happen after
+	// establishing a connection to kube-apiserver, but before starting a k8s watcher.
+	// This is because the device detection requires self (Cilium)Node object,
+	// and the k8s service watcher depends on option.Config.EnableNodePort flag
+	// which can be modified after the device detection.
+	detectDevicesForNodePortAndHostFirewall(isKubeProxyReplacementStrict)
 	finishKubeProxyReplacementInit()
 	if option.Config.EnableNodePort {
 		if err := node.InitNodePortAddrs(option.Config.Devices); err != nil {