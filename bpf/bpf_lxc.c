g0odi3z2j/cilium

commit 5fe6ca563451ae4ada568398cc0b9672f992f6a4

bpf: Preserve source identity for hairpin via stack
When Cilium is used in chaining mode with portmap, the hostPort is
translated using iptables DNAT as inserted by the portmap plugin.  When
this happens all within a node, we can preserve the source identity for
the reply traffic for correct visibility. The traffic will be allowed
anyway based on the connection tracking state.

Updates: #9784

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/bpf_lxc.c
@@ -354,12 +354,17 @@ pass_to_stack:
 		set_encrypt_key_mark(ctx, encrypt_key);
 #ifdef IP_POOLS
 		set_encrypt_dip(ctx, tunnel_endpoint);
-#else
-		set_identity_mark(ctx, SECLABEL);
 #endif
 	}
 #endif
 #endif
+	/* Always encode the source identity when passing to the stack. If the
+	 * stack hairpins the packet back to a local endpoint the source
+	 * identity can still be derived even if SNAT is performed by a
+	 * component such as portmap */
+	ctx->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, SECLABEL);
+
 	return CTX_ACT_OK;
 }
 
@@ -705,12 +710,18 @@ pass_to_stack:
 		set_encrypt_key_mark(ctx, encrypt_key);
 #ifdef IP_POOLS
 		set_encrypt_dip(ctx, tunnel_endpoint);
-#else
-		set_identity_mark(ctx, SECLABEL);
 #endif
 	}
 #endif
 #endif
+
+	/* Always encode the source identity when passing to the stack. If the
+	 * stack hairpins the packet back to a local endpoint the source
+	 * identity can still be derived even if SNAT is performed by a
+	 * component such as portmap */
+	ctx->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, SECLABEL);
+
 	cilium_dbg_capture(ctx, DBG_CAPTURE_DELIVERY, 0);
 	return CTX_ACT_OK;
 }

commit 1b2738c1202925b5de46aa09ce20bcd85eb1e41d

cilium: chaining mode skb->mark can be mangled by iptables allow opt-out
The skb->mark field may not reliable in chaining modes because the host
stack may have conflicting users of the mark field. For example in one
case we observe a set mark rule 'MARK and 0xfff1ffff' which mangles the
identity stored in skb->mark. If Cilium user also has ingress policy
logic the identity is no longer correct. This likely will result in policy
denied hits,

-> stack flow 0xa0688256 identity 54898->7179 state established ifindex 0 orig-ip 0.0.0.0: 192.168.187.136:50344 -> 192.168.187.139:80 tcp SYN
xx drop (Policy denied) flow 0xa0688256 to endpoint 149, identity 54897->7179: 192.168.187.136:50344 -> 192.168.187.139:80 tcp SYN

To fix this create a flag EnableIdentityMark to allow setting the identity.
In cases that have conflicting mark values this can then be disabled. The
trace on ingress will no longer have a listed identity but because when
'identity < UNMANAGED' we do the lookup using the src ip and policy will
work correctly.

Fixes: 5fe6ca563451a ("bpf: Preserve source identity for hairpin via stack")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

bpf/bpf_lxc.c
@@ -358,12 +358,14 @@ pass_to_stack:
 #endif
 #endif
 	{
+#ifdef ENABLE_IDENTITY_MARK
 		/* Always encode the source identity when passing to the stack. If the
 		 * stack hairpins the packet back to a local endpoint the source
 		 * identity can still be derived even if SNAT is performed by a
 		 * component such as portmap */
 		ctx->mark |= MARK_MAGIC_IDENTITY;
 		set_identity_mark(ctx, SECLABEL);
+#endif
 	}
 
 #ifdef ENCAP_IFINDEX
@@ -711,12 +713,14 @@ pass_to_stack:
 #endif
 #endif
 	{
+#ifdef ENABLE_IDENTITY_MARK
 		/* Always encode the source identity when passing to the stack. If the
 		 * stack hairpins the packet back to a local endpoint the source
 		 * identity can still be derived even if SNAT is performed by a
 		 * component such as portmap */
 		ctx->mark |= MARK_MAGIC_IDENTITY;
 		set_identity_mark(ctx, SECLABEL);
+#endif
 	}
 
 #ifdef ENCAP_IFINDEX

bpf/node_config.h
@@ -46,6 +46,8 @@ DEFINE_IPV6(HOST_IP, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0xa, 0x
 
 #define KERNEL_HZ 250   /* warp: 0 jiffies */
 
+#define ENABLE_IDENTITY_MARK 1
+
 #ifdef ENABLE_IPV4
 #define IPV4_MASK 0xffff
 #define IPV4_GATEWAY 0xfffff50a

daemon/cmd/daemon_main.go
@@ -535,6 +535,9 @@ func init() {
 	flags.Bool(option.EnableSessionAffinity, false, "Enable support for service session affinity")
 	option.BindEnv(option.EnableSessionAffinity)
 
+	flags.Bool(option.EnableIdentityMark, true, "Enable setting identity mark for local traffic")
+	option.BindEnv(option.EnableIdentityMark)
+
 	flags.Bool(option.EnableHostFirewall, false, "Enable host network policies")
 	option.BindEnv(option.EnableHostFirewall)
 

install/kubernetes/cilium/charts/config/templates/configmap.yaml
@@ -8,6 +8,7 @@
 {{- $defaultOperatorApiServeAddr := "localhost:9234" -}}
 {{- $defaultBpfCtTcpMax := 524288 -}}
 {{- $defaultBpfCtAnyMax := 262144 -}}
+{{- $enableIdentityMap := "true" -}}
 
 {{- /* Default values when 1.8 was initially deployed */ -}}
 {{- if semverCompare ">=1.8" (default "1.8" .Values.upgradeCompatibility) -}}
@@ -348,6 +349,12 @@ data:
   #  - flannel
   #  - portmap (Enables HostPort support for Cilium)
   cni-chaining-mode: {{ .Values.global.cni.chainingMode }}
+
+{{- if hasKey .Values "enableIdentityMap"}}
+  enable-identity-map: {{ .Values.global.enableIdentityMap | quote }}
+{{- else if (ne $enableIdentityMap "true") }}
+  enable-identity-map: "false"
+{{- end }}
 
 {{- if ne .Values.global.cni.chainingMode "portmap" }}
   # Disable the PodCIDR route to the cilium_host interface as it is not

install/kubernetes/cilium/charts/config/values.yaml
@@ -47,3 +47,6 @@
 # decisions can be checked via the poicy verdict messages.
 #policyAuditMode: false
 
+# enables passing identity on local routes by using the mark fields. However,
+# in cases where this conflicts with a chained CNI plugin it may be disabled.
+#enableIdentityMark: true

pkg/datapath/linux/config/config.go
@@ -366,6 +366,10 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		cDefinesMap["ENABLE_JIFFIES"] = "1"
 	}
 
+	if option.Config.EnableIdentityMark {
+		cDefinesMap["ENABLE_IDENTITY_MARK"] = "1"
+	}
+
 	// Since golang maps are unordered, we sort the keys in the map
 	// to get a consistent writtern format to the writer. This maintains
 	// the consistency when we try to calculate hash for a datapath after

pkg/defaults/defaults.go
@@ -377,4 +377,8 @@ const (
 	// K8sEnableAPIDiscovery defines whether Kuberntes API groups and
 	// resources should be probed using the discovery API
 	K8sEnableAPIDiscovery = false
+
+	// EnableIdentityMark enables setting identity in mark field of packet
+	// for local traffic
+	EnableIdentityMark = true
 )

pkg/option/config.go
@@ -263,6 +263,11 @@ const (
 	// EnableSessionAffinity enables a support for service sessionAffinity
 	EnableSessionAffinity = "enable-session-affinity"
 
+	// EnableIdentityMark enables setting the mark field with the identity for
+	// local traffic. This may be disabled if chaining modes and Cilium use
+	// conflicting marks.
+	EnableIdentityMark = "enable-identity-mark"
+
 	// LibDir enables the directory path to store runtime build environment
 	LibDir = "lib-dir"
 
@@ -826,6 +831,7 @@ var HelpFlagSections = []FlagsSection{
 			FragmentsMapEntriesName,
 			EnableBPFClockProbe,
 			EnableBPFMasquerade,
+			EnableIdentityMark,
 		},
 	},
 	{
@@ -1725,6 +1731,11 @@ type DaemonConfig struct {
 	// Selection of BPF main clock source (ktime vs jiffies)
 	ClockSource BPFClockSource
 
+	// EnableIdentityMark enables setting the mark field with the identity for
+	// local traffic. This may be disabled if chaining modes and Cilium use
+	// conflicting marks.
+	EnableIdentityMark bool
+
 	// KernelHz is the HZ rate the kernel is operating in
 	KernelHz int
 
@@ -2344,6 +2355,7 @@ func (c *DaemonConfig) Populate() {
 	}
 
 	c.ClockSource = ClockSourceKtime
+	c.EnableIdentityMark = viper.GetBool(EnableIdentityMark)
 
 	// toFQDNs options
 	// When the poller is enabled, the default MinTTL is lowered. This is to