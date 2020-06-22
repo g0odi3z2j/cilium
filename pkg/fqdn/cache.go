g0odi3z2j/cilium

commit e67dc21e37fa5baa31f51cd46f233da696ba2aa0

fqdn: Add and use DNSZombieMappings in Endpoint
We need a way to delay expiring entries that are removed from DNSCache
instances in order to avoid blocking connections that exist longer than
the DNS TTLs.
DNSZombieMappings holds these expirations and allows each to be marked as
alive and in-use. This functionality should be held within DNSCache but
the serialization scheme precludes changing the structure (this would
work for upgrades but fail for downgrades).

Signed-off-by: Ray Bejjani <ray@isovalent.com>

pkg/fqdn/cache.go
@@ -521,8 +521,12 @@ func (c *DNSCache) removeReverse(ip string, entry *cacheEntry) {
 // ForceExpire is used to clear entries from the cache before their TTL is
 // over. This operation does not keep previous guarantees that, for each IP,
 // the most recent lookup to provide that IP is used.
-// Note that all parameters must match, if provided. `time.Time{}` is
-// considered not-provided for time parameters.
+// Note that all parameters must match, if provided. `time.Time{}` is the
+// match-all time parameter.
+// For example:
+//   ForceExpire(time.Time{}, 'cilium.io') expires all entries for cilium.io.
+//   ForceExpire(time.Now(), 'cilium.io') expires all entries for cilium.io
+//   that expired before the current time.
 // expireLookupsBefore requires a lookup to have a LookupTime before it in
 // order to remove it.
 // nameMatch will remove any DNS names that match.
@@ -640,5 +644,252 @@ func (c *DNSCache) UnmarshalJSON(raw []byte) error {
 		c.updateWithEntry(newLookup)
 	}
 
+	return nil
+}
+
+// DNSZombieMapping is an IP that has expired or been evicted from a DNS cache.
+// It records the DNS name and IP, along with other bookkeeping timestamps that
+// help determine when it can be finally deleted. Zombies are dead when
+// they are not marked alive by CT GC.
+type DNSZombieMapping struct {
+	// Names is the list of names that had DNS lookups with this IP. These may
+	// derive from unrelated DNS lookups. The list is maintained de-duplicated.
+	Names []string `json:"names,omitempty"`
+
+	// IP is an address that is pending for delete but may be in-use by a
+	// connection.
+	IP net.IP `json:"ip,omitempty"`
+
+	// AliveAt is the last time this IP was marked alive via
+	// DNSZombieMappings.MarkAlive.
+	// When AliveAt is later than DNSZombieMappings.lastCTGCUpdate the zombie is
+	// considered alive.
+	AliveAt time.Time `json:"alive-at,omitempty"`
+
+	// DeletePendingAt is the time at which this IP was most-recently scheduled
+	// for deletion. This can be updated if an IP expires from the DNS caches
+	// multiple times.
+	// When DNSZombieMappings.lastCTGCUpdate is earlier than DeletePendingAt a
+	// zombie is alive.
+	DeletePendingAt time.Time `json:"delete-pending-at,omitempty"`
+}
+
+// DeepCopy returns a copy of zombie that does not share any internal pointers
+// or fields
+func (zombie *DNSZombieMapping) DeepCopy() *DNSZombieMapping {
+	return &DNSZombieMapping{
+		Names:           append([]string{}, zombie.Names...),
+		IP:              zombie.IP,
+		DeletePendingAt: zombie.DeletePendingAt,
+		AliveAt:         zombie.AliveAt,
+	}
+}
+
+// DNSZombieMappings collects DNS Name->IP mappings that may be inactive and
+// evicted, and so may be deleted. They are periodically marked alive by the CT
+// GC goroutine. When .GC is called, alive and dead zombies are returned,
+// allowing us to skip deleting an IP from the global DNS cache to avoid
+// breaking connections that outlast the DNS TTL.
+type DNSZombieMappings struct {
+	lock.Mutex
+	deletes        map[string]*DNSZombieMapping // map[ip]toDelete
+	lastCTGCUpdate time.Time
+}
+
+// NewDNSZombieMappings constructs a DNSZombieMappings that is read to use
+func NewDNSZombieMappings() *DNSZombieMappings {
+	return &DNSZombieMappings{
+		deletes: make(map[string]*DNSZombieMapping),
+	}
+}
+
+// Upsert enqueues the ip -> qname as a possible deletion
+// updatedExisting is true when an earlier enqueue existed and was updated
+func (zombies *DNSZombieMappings) Upsert(now time.Time, ipStr string, qname ...string) (updatedExisting bool) {
+	zombies.Lock()
+	defer zombies.Unlock()
+
+	zombie, updatedExisting := zombies.deletes[ipStr]
+	if !updatedExisting {
+		zombie = &DNSZombieMapping{}
+		zombies.deletes[ipStr] = zombie
+	}
+
+	zombie.Names = KeepUniqueNames(append(zombie.Names, qname...))
+	zombie.IP = net.ParseIP(ipStr)
+	zombie.DeletePendingAt = now
+
+	return updatedExisting
+}
+
+// isAlive returns true when a CT GC has completed without marking this zombie
+// alive. This occurs when:
+//  DeletePendingAt <= lastCTGCUpdate (i.e. CT GC has not happened yet), or
+//  AliveAt is not 0 and AliveAt >= lastCTGCUpdate (i.e it is marked by the CT GC run)
+func (zombies *DNSZombieMappings) isAlive(zombie *DNSZombieMapping) bool {
+	// These are opposite because there is no BeforeEquals with time.Time :/
+	return !(zombies.lastCTGCUpdate.After(zombie.DeletePendingAt) && zombies.lastCTGCUpdate.After(zombie.AliveAt))
+}
+
+// GC returns alive and dead DNSZombieMapping entries. This removes dead
+// zombies interally, and repeated calls will return different data.
+// Zombies are alive if they have been marked alive (with MarkAlive). When
+// SetCTGCTime is called and an zombie not marked alive, it becomes dead.
+// Calling Upsert on a dead zombie will make it alive again.
+func (zombies *DNSZombieMappings) GC() (alive, dead []*DNSZombieMapping) {
+	zombies.Lock()
+	defer zombies.Unlock()
+
+	// Collect zombies we can delete
+	for _, zombie := range zombies.deletes {
+		if zombies.isAlive(zombie) {
+			alive = append(alive, zombie.DeepCopy())
+		} else {
+			// Emit the actual object here since we will no longer update it
+			dead = append(dead, zombie)
+		}
+	}
+
+	// Delete the zombies we collected above from the internal map
+	for _, zombie := range dead {
+		delete(zombies.deletes, zombie.IP.String())
+	}
+
+	return alive, dead
+}
+
+// MarkAlive makes an zombie alive and not dead. When now is later than the
+// time set with SetCTGCTime the zombie remains alive.
+func (zombies *DNSZombieMappings) MarkAlive(now time.Time, ip net.IP) {
+	zombies.Lock()
+	defer zombies.Unlock()
+
+	if zombie, exists := zombies.deletes[ip.String()]; exists {
+		zombie.AliveAt = now
+	}
+}
+
+// SetCTGCTime marks the start of the most recent CT GC. This must be set after
+// all MarkAlive calls complete to avoid a race between the DNS garbage
+// collector and the CT GC. This would occur when a DNS zombie that has not
+// been visited by the CT GC run is seen by a concurrent DNS garbage collector
+// run, and then deleted.
+// When now is later than an alive timestamp, set with MarkAlive, the zombie is
+// no longer alive. Thus, this call acts as a gating function for what data is
+// returned by GC.
+func (zombies *DNSZombieMappings) SetCTGCTime(now time.Time) {
+	zombies.lastCTGCUpdate = now
+}
+
+// ForceExpire is used to clear zombies irrespective of their alive status.
+// Only zombies with DeletePendingAt times before expireLookupBefore are
+// considered for deletion. Each name in an zombie is matched against
+// nameMatcher (nil is match all) and when an zombie no longer has any valid
+// names will it be removed outright.
+// Note that all parameters must match, if provided. `time.Time{}` is the
+// match-all time parameter.
+// expireLookupsBefore requires an zombie to have been enqueued before the
+// specified time in order to remove it.
+// For example:
+//   ForceExpire(time.Time{}, 'cilium.io') expires all entries for cilium.io.
+//   ForceExpire(time.Now(), 'cilium.io') expires all entries for cilium.io
+//   that expired before the current time.
+// nameMatch will remove that specific DNS name from zombies that include it,
+// deleting it when no DNS names remain.
+func (zombies *DNSZombieMappings) ForceExpire(expireLookupsBefore time.Time, nameMatch *regexp.Regexp) (namesAffected []string) {
+	zombies.Lock()
+	defer zombies.Unlock()
+
+	var toDelete []*DNSZombieMapping
+
+	for _, zombie := range zombies.deletes {
+		// Do not expire zombies that were enqueued after expireLookupsBefore, but
+		// only if the value is non-zero
+		if !expireLookupsBefore.IsZero() && zombie.DeletePendingAt.After(expireLookupsBefore) {
+			continue
+		}
+
+		// A zombie has multiple names, collect the ones that should remain (i.e.
+		// do not match nameMatch)
+		var newNames []string
+		for _, name := range zombie.Names {
+			if nameMatch != nil && !nameMatch.MatchString(name) {
+				newNames = append(newNames, name)
+			} else {
+				namesAffected = append(namesAffected, name)
+			}
+		}
+		zombie.Names = newNames
+
+		// Delete the zombie outright if no names remain
+		if len(zombie.Names) == 0 {
+			toDelete = append(toDelete, zombie)
+		}
+	}
+
+	// Delete the zombies that are now empty
+	for _, zombie := range toDelete {
+		delete(zombies.deletes, zombie.IP.String())
+	}
+
+	return namesAffected
+}
+
+// DumpAlive returns copies of still-alive zombies
+func (zombies *DNSZombieMappings) DumpAlive() (alive []*DNSZombieMapping) {
+	zombies.Lock()
+	defer zombies.Unlock()
+
+	for _, zombie := range zombies.deletes {
+		if !zombies.isAlive(zombie) {
+			continue
+		}
+
+		alive = append(alive, zombie.DeepCopy())
+	}
+
+	return alive
+}
+
+// MarshalJSON encodes DNSZombieMappings into JSON. Only the DNSZombieMapping
+// entries are encoded.
+func (zombies *DNSZombieMappings) MarshalJSON() ([]byte, error) {
+	// This hackery avoids exposing DNSZombieMappings.deletes as a public field.
+	// The JSON package cannot serialize private fields so we have to make a
+	// proxy type here.
+	aux := struct {
+		Deletes map[string]*DNSZombieMapping `json:"deletes,omitempty"`
+	}{
+		Deletes: zombies.deletes,
+	}
+
+	return json.Marshal(aux)
+}
+
+// UnmarshalJSON rebuilds a DNSZombieMappings from serialized JSON. It resets
+// the AliveAt timestamps, requiring a CT GC cycle to occur before any zombies
+// are deleted (by not being marked alive).
+// Note: This is destructive to any currect data
+func (zombies *DNSZombieMappings) UnmarshalJSON(raw []byte) error {
+	zombies.Lock()
+	defer zombies.Unlock()
+
+	// This hackery avoids exposing DNSZombieMappings.deletes as a public field.
+	// The JSON package cannot deserialize private fields so we have to make a
+	// proxy type here.
+	aux := struct {
+		Deletes map[string]*DNSZombieMapping `json:"deletes,omitempty"`
+	}{
+		Deletes: zombies.deletes,
+	}
+	if err := json.Unmarshal(raw, &aux); err != nil {
+		return err
+	}
+	zombies.deletes = aux.Deletes
+
+	// Reset the alive time to ensure no deletes happen until we run CT GC again
+	for _, zombie := range zombies.deletes {
+		zombie.AliveAt = time.Time{}
+	}
 	return nil
 }

daemon/fqdn.go
@@ -697,6 +697,28 @@ func extractDNSLookups(endpoints []*endpoint.Endpoint, CIDRStr, matchPatternStr 
 				EndpointID:     int64(ep.ID),
 			})
 		}
+
+		for _, delete := range ep.DNSZombies.DumpAlive() {
+			// only proceed if any IP matches the cidr selector
+			if !cidrMatcher(delete.IP) {
+				continue
+			}
+
+			for _, name := range delete.Names {
+				if !nameMatcher(name) {
+					continue
+				}
+
+				lookups = append(lookups, &models.DNSLookup{
+					Fqdn:           name,
+					Ips:            []string{delete.IP.String()},
+					LookupTime:     strfmt.DateTime(delete.AliveAt),
+					TTL:            0,
+					ExpirationTime: strfmt.DateTime(delete.AliveAt),
+					EndpointID:     int64(ep.ID),
+				})
+			}
+		}
 	}
 
 	return lookups, nil
@@ -721,6 +743,18 @@ func deleteDNSLookups(globalCache *fqdn.DNSCache, pollerCache *fqdn.DNSCache, en
 	for _, ep := range endpoints {
 		namesToRegen = append(namesToRegen, ep.DNSHistory.ForceExpire(expireLookupsBefore, nameMatcher)...)
 		globalCache.UpdateFromCache(ep.DNSHistory, nil)
+
+		namesToRegen = append(namesToRegen, ep.DNSZombies.ForceExpire(expireLookupsBefore, nameMatcher)...)
+		activeConnections := fqdn.NewDNSCache(0)
+		zombies, _ := ep.DNSZombies.GC()
+		lookupTime := time.Now()
+		for _, zombie := range zombies {
+			namesToRegen = append(namesToRegen, zombie.Names...)
+			for _, name := range zombie.Names {
+				activeConnections.Update(lookupTime, name, []net.IP{zombie.IP}, 0)
+			}
+		}
+		globalCache.UpdateFromCache(activeConnections, nil)
 	}
 
 	return namesToRegen, nil

pkg/endpoint/api.go
@@ -81,6 +81,7 @@ func NewEndpointFromChangeModel(ctx context.Context, owner regeneration.Owner, p
 		ifIndex:          int(base.InterfaceIndex),
 		OpLabels:         labels.NewOpLabels(),
 		DNSHistory:       fqdn.NewDNSCacheWithLimit(option.Config.ToFQDNsMinTTL, option.Config.ToFQDNsMaxIPsPerHost),
+		DNSZombies:       fqdn.NewDNSZombieMappings(),
 		state:            "",
 		status:           NewEndpointStatus(),
 		hasBPFProgram:    make(chan struct{}, 0),

pkg/endpoint/endpoint.go
@@ -192,6 +192,11 @@ type Endpoint struct {
 	// this endpoint.
 	DNSHistory *fqdn.DNSCache
 
+	// DNSZombies is the collection of DNS IPs that have expired in or been
+	// evicted from DNSHistory. They are held back from deletion until we can
+	// confirm that no existing connection is using them.
+	DNSZombies *fqdn.DNSZombieMappings
+
 	// dnsHistoryTrigger is the trigger to write down the lxc_config.h to make
 	// sure that restores when DNS policy is in there are correct
 	dnsHistoryTrigger *trigger.Trigger
@@ -389,6 +394,7 @@ func NewEndpointWithState(owner regeneration.Owner, proxy EndpointProxy, allocat
 		OpLabels:        pkgLabels.NewOpLabels(),
 		status:          NewEndpointStatus(),
 		DNSHistory:      fqdn.NewDNSCacheWithLimit(option.Config.ToFQDNsMinTTL, option.Config.ToFQDNsMaxIPsPerHost),
+		DNSZombies:      fqdn.NewDNSZombieMappings(),
 		state:           state,
 		hasBPFProgram:   make(chan struct{}, 0),
 		controllers:     controller.NewManager(),

pkg/endpoint/fqdn.go
@@ -1,0 +1,55 @@
+// Copyright 2016-2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package endpoint
+
+import (
+	"net"
+	"time"
+)
+
+const logSubsys = "fqdn"
+
+// MarkDNSCTEntry records that dstIP is in use by a connection that is allowed
+// by toFQDNs policy. The reverse lookup is attempted in both DNSHistory and
+// DNSCTHistory, allowing short DNS TTLs but long-lived connections to
+// persist there.DNSCTHistory is used to suppress delete handling of expired DNS
+// lookups (in DNSHistory) and it relies on pkg/maps/ctmap/gc to call this
+// function.
+// Internally, the lookupTime is used to checkpoint this update so that
+// dns-garbage-collector-job can correctly clear older connection data.
+func (e *Endpoint) MarkDNSCTEntry(dstIP net.IP, now time.Time) {
+	if dstIP == nil {
+		e.Logger(logSubsys).Error("MarkDNSCTEntry called with nil IP")
+		return
+	}
+
+	e.DNSZombies.MarkAlive(now, dstIP)
+	e.SyncEndpointHeaderFile() // This is behind a rate-limit trigger
+}
+
+// MarkCTGCTime is the START time of a GC run. It is used by the DNS garbage
+// collector to determine whether a DNS zombie can be deleted. This is done by
+// comparing the timestamp of the start CT GC run with the ailve timestamps of
+// specific DNS zombies IPs marked with MarkDNSCTEntry.
+// NOTE: While the timestamp is ths start of the run, it should be set AFTER
+// the run completes. This avoids a race between the DNS garbage collector and
+// the CT GC. This would occur when a DNS zombie that has not been visited by
+// the CT GC run is seen by a concurrent DNS garbage collector run, and then
+// deleted.
+// The DNS garbage collector is in daemon/fqdn.go and the CT GC is in
+// pkg/maps/ctmap/gc/gc.go
+func (e *Endpoint) MarkCTGCTime(now time.Time) {
+	e.DNSZombies.SetCTGCTime(now)
+}

pkg/endpoint/restore.go
@@ -252,6 +252,7 @@ func (e *Endpoint) toSerializedEndpoint() *serializableEndpoint {
 		SecurityIdentity:      e.SecurityIdentity,
 		Options:               e.Options,
 		DNSHistory:            e.DNSHistory,
+		DNSZombies:            e.DNSZombies,
 		K8sPodName:            e.K8sPodName,
 		K8sNamespace:          e.K8sNamespace,
 		DatapathConfiguration: e.DatapathConfiguration,
@@ -323,9 +324,14 @@ type serializableEndpoint struct {
 
 	// Options determine the datapath configuration of the endpoint.
 	Options *option.IntOptions
+
 	// DNSHistory is the collection of still-valid DNS responses intercepted for
 	// this endpoint.
 	DNSHistory *fqdn.DNSCache
+
+	// DNSZombies is the collection of DNS entries that have been expired or
+	// evicted from DNSHistory.
+	DNSZombies *fqdn.DNSZombieMappings
 
 	// K8sPodName is the Kubernetes pod name of the endpoint
 	K8sPodName string
@@ -348,6 +354,7 @@ func (ep *Endpoint) UnmarshalJSON(raw []byte) error {
 	restoredEp := &serializableEndpoint{
 		OpLabels:   labels.NewOpLabels(),
 		DNSHistory: fqdn.NewDNSCacheWithLimit(option.Config.ToFQDNsMinTTL, option.Config.ToFQDNsMaxIPsPerHost),
+		DNSZombies: fqdn.NewDNSZombieMappings(),
 	}
 	if err := json.Unmarshal(raw, restoredEp); err != nil {
 		return fmt.Errorf("error unmarshaling serializableEndpoint from base64 representation: %s", err)
@@ -378,6 +385,7 @@ func (ep *Endpoint) fromSerializedEndpoint(r *serializableEndpoint) {
 	ep.nodeMAC = r.NodeMAC
 	ep.SecurityIdentity = r.SecurityIdentity
 	ep.DNSHistory = r.DNSHistory
+	ep.DNSZombies = r.DNSZombies
 	ep.K8sPodName = r.K8sPodName
 	ep.K8sNamespace = r.K8sNamespace
 	ep.DatapathConfiguration = r.DatapathConfiguration

pkg/fqdn/cache_test.go
@@ -599,3 +599,125 @@ func (ds *DNSCacheTestSuite) TestOverlimitAfterDeleteForwardEntry(c *C) {
 	dnsCache.overLimit["test.com"] = true
 	c.Assert(dnsCache.cleanupOverLimitEntries(), checker.DeepEquals, []string{})
 }
+
+func assertZombiesContain(c *C, zombies []*DNSZombieMapping, mappings map[string][]string) {
+	c.Assert(zombies, HasLen, len(mappings), Commentf("Different number of zombies than expected: %+v", zombies))
+
+	for _, zombie := range zombies {
+		names, exists := mappings[zombie.IP.String()]
+		c.Assert(exists, Equals, true, Commentf("Missing expected zombie"))
+
+		sort.Strings(zombie.Names)
+		sort.Strings(names)
+
+		c.Assert(zombie.Names, HasLen, len(names))
+		for i := range zombie.Names {
+			c.Assert(zombie.Names[i], Equals, names[i], Commentf("Unexpected name in zombie names list"))
+		}
+	}
+}
+
+func (ds *DNSCacheTestSuite) TestZombiesGC(c *C) {
+	now := time.Now()
+	zombies := NewDNSZombieMappings()
+
+	zombies.Upsert(now, "1.1.1.1", "test.com")
+	zombies.Upsert(now, "2.2.2.2", "somethingelse.com")
+
+	// Without any MarkAlive or SetCTGCTime, all entries remain alive
+	alive, dead := zombies.GC()
+	c.Assert(dead, HasLen, 0)
+	assertZombiesContain(c, alive, map[string][]string{
+		"1.1.1.1": {"test.com"},
+		"2.2.2.2": {"somethingelse.com"},
+	})
+
+	// Adding another name to 1.1.1.1 keeps it alive and adds the name to the
+	// zombie
+	zombies.Upsert(now, "1.1.1.1", "anotherthing.com")
+	alive, dead = zombies.GC()
+	c.Assert(dead, HasLen, 0)
+	assertZombiesContain(c, alive, map[string][]string{
+		"1.1.1.1": {"test.com", "anotherthing.com"},
+		"2.2.2.2": {"somethingelse.com"},
+	})
+
+	// Cause 1.1.1.1 to die by not marking it alive before the second GC
+	//zombies.MarkAlive(now, net.ParseIP("1.1.1.1"))
+	now = now.Add(time.Second)
+	zombies.MarkAlive(now, net.ParseIP("2.2.2.2"))
+	zombies.SetCTGCTime(now)
+
+	// alive should contain 2.2.2.2 -> somethingelse.com
+	// dead should contain 1.1.1.1 -> anotherthing.com, test.com
+	alive, dead = zombies.GC()
+	assertZombiesContain(c, alive, map[string][]string{
+		"2.2.2.2": {"somethingelse.com"},
+	})
+	assertZombiesContain(c, dead, map[string][]string{
+		"1.1.1.1": {"test.com", "anotherthing.com"},
+	})
+
+	// A second GC call only returns alive entries
+	alive, dead = zombies.GC()
+	c.Assert(dead, HasLen, 0)
+	c.Assert(alive, HasLen, 1)
+
+	// Update 2.2.2.2 with a new DNS name. It remains alive.
+	// Add 1.1.1.1 again. It is alive.
+	zombies.Upsert(now, "2.2.2.2", "thelastthing.com")
+	zombies.Upsert(now, "1.1.1.1", "onemorething.com")
+
+	alive, dead = zombies.GC()
+	c.Assert(dead, HasLen, 0)
+	assertZombiesContain(c, alive, map[string][]string{
+		"1.1.1.1": {"onemorething.com"},
+		"2.2.2.2": {"somethingelse.com", "thelastthing.com"},
+	})
+
+	// Cause all zombies to die
+	now = now.Add(time.Second)
+	zombies.SetCTGCTime(now)
+	alive, dead = zombies.GC()
+	c.Assert(alive, HasLen, 0)
+	assertZombiesContain(c, dead, map[string][]string{
+		"1.1.1.1": {"onemorething.com"},
+		"2.2.2.2": {"somethingelse.com", "thelastthing.com"},
+	})
+}
+
+func (ds *DNSCacheTestSuite) TestZombiesForceExpire(c *C) {
+	now := time.Now()
+	zombies := NewDNSZombieMappings()
+
+	zombies.Upsert(now, "1.1.1.1", "test.com", "anothertest.com")
+	zombies.Upsert(now, "2.2.2.2", "somethingelse.com")
+
+	// Without any MarkAlive or SetCTGCTime, all entries remain alive
+	alive, dead := zombies.GC()
+	c.Assert(dead, HasLen, 0)
+	c.Assert(alive, HasLen, 2)
+
+	// Expire only 1 name on 1 zombie
+	nameMatch, err := regexp.Compile("^test.com$")
+	c.Assert(err, IsNil)
+	zombies.ForceExpire(time.Time{}, nameMatch)
+
+	alive, dead = zombies.GC()
+	c.Assert(dead, HasLen, 0)
+	assertZombiesContain(c, alive, map[string][]string{
+		"1.1.1.1": {"anothertest.com"},
+		"2.2.2.2": {"somethingelse.com"},
+	})
+
+	// Expire the last name on a zombie. It will be deleted and not returned in a
+	// GC
+	nameMatch, err = regexp.Compile("^anothertest.com$")
+	c.Assert(err, IsNil)
+	zombies.ForceExpire(time.Time{}, nameMatch)
+	alive, dead = zombies.GC()
+	c.Assert(dead, HasLen, 0)
+	assertZombiesContain(c, alive, map[string][]string{
+		"2.2.2.2": {"somethingelse.com"},
+	})
+}

commit 86c7e6eedaaa61b35ad6402c150fba47e66483f8

fqdn: Fix panic on MarshalJSON
We need to hold a lock on DNSZombieMappings when calling json.Marshal(),
otherwise we risk hitting panics on concurrent writes.

  panic: reflect: call of reflect.Value.IsNil on zero Value [recovered]
   goroutine 2807 [running]:
   ...
   panic(...)
   	/usr/local/go/src/runtime/panic.go:679 +0x1b2
   reflect.Value.IsNil(...)
   	/usr/local/go/src/reflect/value.go:1073
   ...
   github.com/cilium/cilium/pkg/fqdn.(*DNSZombieMappings).MarshalJSON(...)
   	/go/src/github.com/cilium/cilium/pkg/fqdn/cache.go:949 +0x3b

Fixes: e67dc21 ("fqdn: Add and use DNSZombieMappings in Endpoint")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/fqdn/cache.go
@@ -831,7 +831,9 @@ func (zombies *DNSZombieMappings) MarkAlive(now time.Time, ip net.IP) {
 // no longer alive. Thus, this call acts as a gating function for what data is
 // returned by GC.
 func (zombies *DNSZombieMappings) SetCTGCTime(now time.Time) {
+	zombies.Lock()
 	zombies.lastCTGCUpdate = now
+	zombies.Unlock()
 }
 
 // ForceExpire is used to clear zombies irrespective of their alive status.
@@ -937,6 +939,9 @@ func (zombies *DNSZombieMappings) DumpAlive() (alive []*DNSZombieMapping) {
 // MarshalJSON encodes DNSZombieMappings into JSON. Only the DNSZombieMapping
 // entries are encoded.
 func (zombies *DNSZombieMappings) MarshalJSON() ([]byte, error) {
+	zombies.Lock()
+	defer zombies.Unlock()
+
 	// This hackery avoids exposing DNSZombieMappings.deletes as a public field.
 	// The JSON package cannot serialize private fields so we have to make a
 	// proxy type here.