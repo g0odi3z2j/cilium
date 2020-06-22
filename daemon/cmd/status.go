g0odi3z2j/cilium

commit fc6131ccb1d6b6ae30bcd321a968693bbbe01367

api,daemon,cli: Add SNAT exclusion dst to cilium status
This commits extends "cilium status" to show dst cidr of SNAT exclusion.
E.g.:

    $ cilium status | grep Masquerading
    Masquerading:           BPF   [eth0, eth1]  10.0.0.0/16

Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/cmd/status.go
@@ -117,6 +117,8 @@ func (d *Daemon) getMasqueradingStatus() *models.Masquerading {
 		return s
 	}
 
+	s.SnatExclusionCidr = datapath.RemoteSNATDstAddrExclusionCIDR().String()
+
 	if option.Config.EnableBPFMasquerade {
 		s.Mode = models.MasqueradingModeBPF
 		s.IPMasqAgent = option.Config.EnableIPMasqAgent

api/v1/models/masquerading.go
@@ -29,6 +29,9 @@ type Masquerading struct {
 	// mode
 	// Enum: [BPF iptables]
 	Mode string `json:"mode,omitempty"`
+
+	// Any packet sent to IP addr belonging to CIDR will not be SNAT'd
+	SnatExclusionCidr string `json:"snat-exclusion-cidr,omitempty"`
 }
 
 // Validate validates this masquerading

api/v1/openapi.yaml
@@ -2286,6 +2286,9 @@ definitions:
       ip-masq-agent:
         description: Is BPF ip-masq-agent enabled
         type: boolean
+      snat-exclusion-cidr:
+        description: Any packet sent to IP addr belonging to CIDR will not be SNAT'd
+        type: string
   ControllerStatuses:
     description: Collection of controller statuses
     type: array

api/v1/server/embedded_spec.go
@@ -2694,6 +2694,10 @@ func init() {
             "BPF",
             "iptables"
           ]
+        },
+        "snat-exclusion-cidr": {
+          "description": "Any packet sent to IP addr belonging to CIDR will not be SNAT'd",
+          "type": "string"
         }
       }
     },
@@ -6301,6 +6305,10 @@ func init() {
             "BPF",
             "iptables"
           ]
+        },
+        "snat-exclusion-cidr": {
+          "description": "Any packet sent to IP addr belonging to CIDR will not be SNAT'd",
+          "type": "string"
         }
       }
     },

pkg/client/client.go
@@ -382,8 +382,9 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, allAddresses, 
 				status = "BPF"
 			}
 			if sr.KubeProxyReplacement != nil {
-				status += fmt.Sprintf("\t[%s]",
-					strings.Join(sr.KubeProxyReplacement.Devices, ", "))
+				status += fmt.Sprintf("\t[%s]\t%s",
+					strings.Join(sr.KubeProxyReplacement.Devices, ", "),
+					sr.Masquerading.SnatExclusionCidr)
 			}
 
 		} else if sr.Masquerading.Mode == models.MasqueradingModeIptables {

commit 85411aca4a7da3f3917a41d1315e1663c3b1d9e8

daemon: fix panic for cilium status in IPv6 only cluster
This commit fixes a panic that occurs when calling
`getMasqueradingStatus()` in IPv6 only clusters. It fixes a regression
that was introduced in `fc6131c` as
`datapath.RemoteSNATDstAddrExclusionCIDR()` returns `<nil>` on IPv6 only
clusters.

Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

daemon/cmd/status.go
@@ -117,7 +117,9 @@ func (d *Daemon) getMasqueradingStatus() *models.Masquerading {
 		return s
 	}
 
-	s.SnatExclusionCidr = datapath.RemoteSNATDstAddrExclusionCIDR().String()
+	if option.Config.EnableIPv4 {
+		s.SnatExclusionCidr = datapath.RemoteSNATDstAddrExclusionCIDR().String()
+	}
 
 	if option.Config.EnableBPFMasquerade {
 		s.Mode = models.MasqueradingModeBPF