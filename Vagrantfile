g0odi3z2j/cilium

commit 47ab30aa66adcedfc8adc9fd585d752c0bd1aff6

examples/kubernetes-ingress: fixing scripts to run k8s 1.8.1
Add new mode `ETCD_CLEAN=1` that allows the developer to delete the etcd
state running.

Disabled IPV4=0 mode until k8s 1.9 is released

Signed-off-by: André Martins <andre@cilium.io>

Vagrantfile
@@ -11,6 +11,12 @@ Calling 'vagrant up' directly is not supported.  Instead, please run the followi
 END
 end
 
+if ENV['IPV4'] == '0'
+    raise Vagrant::Errors::VagrantError.new, <<END
+Disabling IPv4 is currently not allowed until k8s 1.9 is released
+END
+end
+
 $bootstrap = <<SCRIPT
 sudo service docker restart
 sudo apt-get -y update || true
@@ -123,6 +129,7 @@ Vagrant.configure(2) do |config|
 
     master_vm_name = "cilium#{$k8stag}-master#{$build_id_name}"
     config.vm.define master_vm_name, primary: true do |cm|
+        node_ip = "#{$master_ip}"
         cm.vm.network "private_network", ip: "#{$master_ip}",
             virtualbox__intnet: "cilium-test-#{$build_id}",
             :libvirt__guest_ipv6 => "yes",
@@ -137,12 +144,21 @@ Vagrant.configure(2) do |config|
                 type: "shell",
                 run: "always",
                 inline: "ip -6 a a #{$master_ipv6}/16 dev enp0s9"
+            node_ip = "#{$nfs_ipv4_master_addr}"
+            if ENV["IPV6_EXT"] then
+                node_ip = "#{$master_ipv6}"
+            end
         end
         cm.vm.hostname = "cilium#{$k8stag}-master"
         if ENV['CILIUM_TEMP'] then
            if ENV["K8S"] then
                k8sinstall = "#{ENV['CILIUM_TEMP']}/cilium-k8s-install-1st-part.sh"
-               cm.vm.provision "shell", privileged: true, path: k8sinstall
+               cm.vm.provision "k8s-install-master-part-1",
+                   type: "shell",
+                   run: "always",
+                   env: {"node_ip" => node_ip},
+                   privileged: true,
+                   path: k8sinstall
            end
            script = "#{ENV['CILIUM_TEMP']}/cilium-master.sh"
            cm.vm.provision "config-install", type: "shell", privileged: true, run: "always", path: script
@@ -151,7 +167,12 @@ Vagrant.configure(2) do |config|
            # policies into kubernetes and cilium.
            if ENV["K8S"] then
                k8sinstall = "#{ENV['CILIUM_TEMP']}/cilium-k8s-install-2nd-part.sh"
-               cm.vm.provision "shell", privileged: true, path: k8sinstall
+               cm.vm.provision "k8s-install-master-part-2",
+                   type: "shell",
+                   run: "always",
+                   env: {"node_ip" => node_ip},
+                   privileged: true,
+                   path: k8sinstall
            end
         end
         if ENV['RUN_TEST_SUITE'] then
@@ -171,6 +192,7 @@ Vagrant.configure(2) do |config|
                 :libvirt__dhcp_enabled => false
             if ENV["NFS"] || ENV["IPV6_EXT"] then
                 nfs_ipv4_addr = $workers_ipv4_addrs_nfs[n]
+                node_ip = "#{nfs_ipv4_addr}"
                 ipv6_addr = $workers_ipv6_addrs[n]
                 node.vm.network "private_network", ip: "#{nfs_ipv4_addr}", bridge: "enp0s9"
                 # Add IPv6 address this way or we get hit by a virtualbox bug
@@ -178,18 +200,31 @@ Vagrant.configure(2) do |config|
                     type: "shell",
                     run: "always",
                     inline: "ip -6 a a #{ipv6_addr}/16 dev enp0s9"
+                if ENV["IPV6_EXT"] then
+                    node_ip = "#{ipv6_addr}"
+                end
             end
             node.vm.hostname = "cilium#{$k8stag}-node-#{n+2}"
             if ENV['CILIUM_TEMP'] then
                 if ENV["K8S"] then
                     k8sinstall = "#{ENV['CILIUM_TEMP']}/cilium-k8s-install-1st-part.sh"
-                    node.vm.provision "shell", privileged: true, path: k8sinstall
+                    node.vm.provision "k8s-install-node-part-1",
+                        type: "shell",
+                        run: "always",
+                        env: {"node_ip" => node_ip},
+                        privileged: true,
+                        path: k8sinstall
                 end
                 script = "#{ENV['CILIUM_TEMP']}/node-start-#{n+2}.sh"
                 node.vm.provision "config-install", type: "shell", privileged: true, run: "always", path: script
                 if ENV["K8S"] then
                     k8sinstall = "#{ENV['CILIUM_TEMP']}/cilium-k8s-install-2nd-part.sh"
-                    node.vm.provision "shell", privileged: true, path: k8sinstall
+                    node.vm.provision "k8s-install-node-part-2",
+                        type: "shell",
+                        run: "always",
+                        env: {"node_ip" => node_ip},
+                        privileged: true,
+                        path: k8sinstall
                 end
             end
         end

contrib/vagrant/start.sh
@@ -29,8 +29,8 @@ export 'IPV6_INTERNAL_CIDR'=${IPV4+"FD01::"}
 # Cilium IPv6 node CIDR. Each node will be setup with IPv6 network of
 # $CILIUM_IPV6_NODE_CIDR + 6to4($MASTER_IPV4). For IPv4 "192.168.33.11" we will
 # have for example:
-#   master  : FD02::C0A8:210B:0:0/96
-#   worker 1: FD02::C0A8:210C:0:0/96
+#   master  : FD02::0:0:0/96
+#   worker 1: FD02::1:0:0/96
 export 'CILIUM_IPV6_NODE_CIDR'=${CILIUM_IPV6_NODE_CIDR:-"FD02::"}
 # VM memory
 export 'VM_MEMORY'=${MEMORY:-3072}
@@ -78,6 +78,11 @@ function write_netcfg_header(){
     filename="${3}"
     cat <<EOF > "${filename}"
 #!/usr/bin/env bash
+
+if [ -n "${K8S}" ]; then
+    export K8S="1"
+fi
+
 # Use of IPv6 'documentation block' to provide example
 if [ -n "\$(grep DISTRIB_RELEASE=14.04 /etc/lsb-release)" ]; then
     ip -6 a a ${vm_ipv6}/16 dev eth1
@@ -86,7 +91,6 @@ else
 fi
 
 echo '${master_ipv6} cilium${K8STAG}-master' >> /etc/hosts
-export 'TUNNEL_MODE_STRING'="--tunnel vxlan"
 sysctl -w net.ipv6.conf.all.forwarding=1
 EOF
 }
@@ -102,7 +106,8 @@ function write_master_route(){
     node_index="${4}"
     worker_ipv6="${5}"
     filename="${6}"
-    cat <<EOF >> "${filename}"
+    if [ -z "${K8S}" ]; then
+        cat <<EOF >> "${filename}"
 # Master route
 if [ -n "\$(grep DISTRIB_RELEASE=14.04 /etc/lsb-release)" ]; then
     ip r a 10.${master_ipv4_suffix}.0.1/32 dev eth1
@@ -111,8 +116,10 @@ else
     ip r a 10.${master_ipv4_suffix}.0.1/32 dev enp0s8
     ip r a 10.${master_ipv4_suffix}.0.0/16 via 10.${master_ipv4_suffix}.0.1
 fi
+EOF
+    fi
 
-ip -6 r a ${master_cilium_ipv6}/96 via ${master_ipv6}
+    cat <<EOF >> "${filename}"
 echo "${worker_ipv6} cilium${K8STAG}-node-${node_index}" >> /etc/hosts
 
 EOF
@@ -138,8 +145,8 @@ EOF
             continue
         fi
         worker_internal_ipv6=${IPV6_INTERNAL_CIDR}$(printf "%02X" "${i}")
-
-        cat <<EOF >> "${filename}"
+        if [ -z "${K8S}" ]; then
+            cat <<EOF >> "${filename}"
 if [ -n "\$(grep DISTRIB_RELEASE=14.04 /etc/lsb-release)" ]; then
     ip r a 10.${i}.0.1/32 dev eth1
 else
@@ -147,8 +154,10 @@ else
 fi
 
 ip r a 10.${i}.0.0/16 via 10.${i}.0.1
+EOF
+        fi
 
-ip -6 r a ${CILIUM_IPV6_NODE_CIDR}${hexIPv4}:0:0/96 via ${worker_internal_ipv6}
+        cat <<EOF >> "${filename}"
 echo "${worker_internal_ipv6} cilium${K8STAG}-node-${index}" >> /etc/hosts
 EOF
     done
@@ -181,17 +190,20 @@ EOF
 function write_install_nsenter(){
     filename="${1}"
     cat <<EOF >> "${filename}"
-#Install nsenter
-sudo apt-get -y install bison libncurses5-dev libslang2-dev gettext \
-zlib1g-dev libselinux1-dev debhelper lsb-release pkg-config \
-po-debconf autoconf automake autopoint libtool
-wget -nv https://www.kernel.org/pub/linux/utils/util-linux/v2.30/util-linux-2.30.1.tar.gz
-tar -xvzf util-linux-2.30.1.tar.gz
-cd util-linux-2.30.1
-./autogen.sh
-./configure --without-python --disable-all-programs --enable-nsenter
-make nsenter
-sudo cp nsenter /usr/bin
+
+if [ -n "\${INSTALL}" ]; then
+    #Install nsenter
+    sudo apt-get -y install bison libncurses5-dev libslang2-dev gettext \
+    zlib1g-dev libselinux1-dev debhelper lsb-release pkg-config \
+    po-debconf autoconf automake autopoint libtool
+    wget -nv https://www.kernel.org/pub/linux/utils/util-linux/v2.30/util-linux-2.30.1.tar.gz
+    tar -xvzf util-linux-2.30.1.tar.gz
+    cd util-linux-2.30.1
+    ./autogen.sh
+    ./configure --without-python --disable-all-programs --enable-nsenter
+    make nsenter
+    sudo cp nsenter /usr/bin
+fi
 
 EOF
 }
@@ -204,17 +216,22 @@ function write_k8s_install() {
     filename="${2}"
     filename_2nd_half="${3}"
     if [[ -n "${IPV6_EXT}" ]]; then
-        split_ipv4 ipv4_array "${MASTER_IPV4}"
-        hexIPv4=$(printf "%d.%d.0.0" "${ipv4_array[0]}" "${ipv4_array[1]}")
-        get_cilium_node_addr k8s_cluster_cidr "${hexIPv4}"
-        k8s_cluster_cidr+="/96"
-        k8s_node_cidr_mask_size="112"
+        # The k8s cluster cidr will be /80
+        # it can be any value as long it's lower than /96
+        # k8s will assign each node a cidr for example:
+        #   master  : FD02::0:0:0/96
+        #   worker 1: FD02::1:0:0/96
+        #   worker 1: FD02::2:0:0/96
+        k8s_cluster_cidr+="FD02::/80"
+        k8s_node_cidr_mask_size="96"
         k8s_service_cluster_ip_range="FD03::/112"
+        k8s_cluster_api_server_ip="FD03::1"
         k8s_cluster_dns_ip="FD03::A"
     fi
     k8s_cluster_cidr=${k8s_cluster_cidr:-"10.0.0.0/10"}
     k8s_node_cidr_mask_size=${k8s_node_cidr_mask_size:-"16"}
     k8s_service_cluster_ip_range=${k8s_service_cluster_ip_range:-"172.20.0.0/24"}
+    k8s_cluster_api_server_ip=${k8s_cluster_api_server_ip:-"172.20.0.1"}
     k8s_cluster_dns_ip=${k8s_cluster_dns_ip:-"172.20.0.10"}
 
     cat <<EOF >> "${filename}"
@@ -222,16 +239,30 @@ function write_k8s_install() {
 k8s_path="/home/vagrant/go/src/github.com/cilium/cilium/examples/kubernetes-ingress/scripts"
 export IPV6_EXT="${IPV6_EXT}"
 export K8S_CLUSTER_CIDR="${k8s_cluster_cidr}"
-export K8S_NODE_CDIR_MASK_SIZE="${k8s_node_cidr_mask_size}"
+export K8S_NODE_CIDR_MASK_SIZE="${k8s_node_cidr_mask_size}"
 export K8S_SERVICE_CLUSTER_IP_RANGE="${k8s_service_cluster_ip_range}"
+export K8S_CLUSTER_API_SERVER_IP="${k8s_cluster_api_server_ip}"
 export K8S_CLUSTER_DNS_IP="${k8s_cluster_dns_ip}"
+# Only do installation if RELOAD is not set
+if [ -z "${RELOAD}" ]; then
+    export INSTALL="1"
+fi
+export ETCD_CLEAN="${ETCD_CLEAN}"
+
+# Stop cilium before until we install kubelet. This prevents cilium from
+# allocating its own podCIDR without using the kubernetes allocated podCIDR.
+sudo service cilium stop
+EOF
+    write_install_nsenter "${filename}"
+    cat <<EOF >> "${filename}"
 if [[ "\$(hostname)" -eq "cilium${K8STAG}-master" ]]; then
-    "\${k8s_path}/03-2-run-inside-vms-etcd.sh"
-    "\${k8s_path}/04-2-run-inside-vms-kubernetes-controller.sh"
+    "\${k8s_path}/00-create-certs.sh"
+    "\${k8s_path}/01-install-etcd.sh"
+    "\${k8s_path}/02-install-kubernetes-master.sh"
 fi
 # All nodes are a kubernetes worker
-"\${k8s_path}/05-2-run-inside-vms-kubernetes-worker.sh"
-INSTALL=1 "\${k8s_path}/06-kubectl.sh"
+"\${k8s_path}/03-install-kubernetes-worker.sh"
+"\${k8s_path}/04-install-kubectl.sh"
 chown vagrant.vagrant -R "${k8s_dir}"
 
 EOF
@@ -242,18 +273,24 @@ EOF
 k8s_path="/home/vagrant/go/src/github.com/cilium/cilium/examples/kubernetes-ingress/scripts"
 export IPV6_EXT="${IPV6_EXT}"
 export K8S_CLUSTER_CIDR="${k8s_cluster_cidr}"
-export K8S_NODE_CDIR_MASK_SIZE="${k8s_node_cidr_mask_size}"
+export K8S_NODE_CIDR_MASK_SIZE="${k8s_node_cidr_mask_size}"
 export K8S_SERVICE_CLUSTER_IP_RANGE="${k8s_service_cluster_ip_range}"
+export K8S_CLUSTER_API_SERVER_IP="${k8s_cluster_api_server_ip}"
 export K8S_CLUSTER_DNS_IP="${k8s_cluster_dns_ip}"
 export K8STAG="${K8STAG}"
 export NWORKERS="${NWORKERS}"
+# Only do installation if RELOAD is not set
+if [ -z "${RELOAD}" ]; then
+    export INSTALL="1"
+fi
+export ETCD_CLEAN="${ETCD_CLEAN}"
 
 cd "${k8s_dir}"
-"\${k8s_path}/08-cilium.sh"
+"\${k8s_path}/05-install-cilium.sh"
 if [[ "\$(hostname)" -eq "cilium${K8STAG}-master" ]]; then
-    "\${k8s_path}/09-dns-addon.sh"
+    "\${k8s_path}/06-install-kubedns.sh"
 else
-    "\${k8s_path}/06-kubectl.sh"
+    "\${k8s_path}/04-install-kubectl.sh"
 fi
 EOF
 }
@@ -264,18 +301,20 @@ function write_cilium_cfg() {
     ipv6_addr="${3}"
     filename="${4}"
 
-    cilium_options="--ipv6-range ${ipv6_addr}/96"
+    cilium_options="--auto-ipv6-node-routes"
 
     if [[ "${IPV4}" -eq "1" ]]; then
-        cilium_options+=" --ipv4-range 10.${master_ipv4_suffix}.0.0/16"
+        if [[ -z "${K8S}" ]]; then
+            cilium_options+=" --ipv4-range 10.${master_ipv4_suffix}.0.0/16"
+        fi
     else
         cilium_options+=" --disable-ipv4"
     fi
 
     if [ -n "${K8S}" ]; then
-        cilium_options+=" --k8s-api-server http://${MASTER_IPV4}:8080"
-        cilium_options+=" --kvstore-opt etcd.config=/var/lib/cilium/etcd-config.yml"
+        cilium_options+=" --k8s-kubeconfig-path /var/lib/cilium/cilium.kubeconfig"
         cilium_options+=" --kvstore etcd"
+        cilium_options+=" --kvstore-opt etcd.config=/var/lib/cilium/etcd-config.yml"
     else
         if [[ "${IPV4}" -eq "1" ]]; then
             cilium_options+=" --kvstore-opt consul.address=${MASTER_IPV4}:8500"
@@ -288,17 +327,18 @@ function write_cilium_cfg() {
     if [ "$LB" = 1 ]; then
         # The LB interface needs to be the "exposed" to the host
         # interface only for master node.
-        if [ $((node_index)) -eq 1 ]; then
+        # FIXME GH-1054
+#        if [ $((node_index)) -eq 1 ]; then
+#            ubuntu_1404_interface="-d eth2"
+#            ubuntu_1604_interface="-d enp0s9"
+#            ubuntu_1404_cilium_lb="--lb eth2"
+#            ubuntu_1604_cilium_lb="--lb enp0s9"
+#        else
             ubuntu_1404_interface="-d eth2"
             ubuntu_1604_interface="-d enp0s9"
-            ubuntu_1404_cilium_lb="--lb eth2"
-            ubuntu_1604_cilium_lb="--lb enp0s9"
-        else
-            ubuntu_1404_interface="-d eth1"
-            ubuntu_1604_interface="-d enp0s8"
             ubuntu_1404_cilium_lb=""
             ubuntu_1604_cilium_lb=""
-        fi
+#        fi
     else
         cilium_options+=" ${TUNNEL_MODE_STRING}"
     fi
@@ -312,9 +352,23 @@ if [ -n "\$(grep DISTRIB_RELEASE=14.04 /etc/lsb-release)" ]; then
     echo 'exec cilium-agent --debug ${ubuntu_1404_cilium_lb} ${ubuntu_1404_interface} ${cilium_options}' >> /etc/init/cilium.conf
 else
     sed -i '9s+.*+ExecStart=/usr/bin/cilium-agent --debug \$CILIUM_OPTS+' /lib/systemd/system/cilium.service
+    echo "K8S_NODE_NAME=\$(hostname)" >> /etc/sysconfig/cilium
     echo 'CILIUM_OPTS="${ubuntu_1604_cilium_lb} ${ubuntu_1604_interface} ${cilium_options}"' >> /etc/sysconfig/cilium
     echo 'PATH=/usr/local/clang/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin' >> /etc/sysconfig/cilium
     chmod 644 /etc/sysconfig/cilium
+fi
+
+# Wait for the node to have a podCIDR so that cilium can use the podCIDR
+# allocated by k8s
+if [ -n "\${K8S}" ]; then
+    for ((i = 0 ; i < 24; i++)); do
+        if kubectl get nodes -o json | grep -i podCIDR > /dev/null 2>&1; then
+            podCIDR=true
+            break
+        fi
+        sleep 5s
+        echo "Waiting for kubernetes node \$(hostname) to have a podCIDR"
+    done
 fi
 
 service cilium restart
@@ -356,6 +410,7 @@ function create_master(){
 
 function create_workers(){
     split_ipv4 ipv4_array "${MASTER_IPV4}"
+    master_prefix_ip="${ipv4_array[3]}"
     get_cilium_node_addr master_cilium_ipv6 "${MASTER_IPV4}"
     base_workers_ip=$(printf "%d.%d.%d." "${ipv4_array[0]}" "${ipv4_array[1]}" "${ipv4_array[2]}")
     if [ -n "${NWORKERS}" ]; then
@@ -368,9 +423,8 @@ function create_workers(){
 
             write_netcfg_header "${worker_ipv6}" "${MASTER_IPV6}" "${output_file}"
 
-            write_master_route "${ipv4_array[3]}" "${master_cilium_ipv6}" \
+            write_master_route "${master_prefix_ip}" "${master_cilium_ipv6}" \
                 "${MASTER_IPV6}" "${i}" "${worker_ipv6}" "${output_file}"
-
             write_nodes_routes "${i}" ${MASTER_IPV4} "${output_file}"
 
             worker_cilium_ipv4="${base_workers_ip}${worker_ip_suffix}"
@@ -387,7 +441,6 @@ function create_k8s_config(){
         output_file="${dir}/cilium-k8s-install-1st-part.sh"
         output_2nd_file="${dir}/cilium-k8s-install-2nd-part.sh"
         write_k8s_header "${k8s_temp_dir}" "${output_file}"
-        write_install_nsenter "${output_file}"
         write_k8s_install "${k8s_temp_dir}" "${output_file}" "${output_2nd_file}"
     fi
 }
@@ -495,17 +548,17 @@ function vboxnet_addr_finder(){
         if [ -z "${vboxnet_interface}" ]; then
             exit 1
         fi
-    elif [[ "${net_mask}" -ne 16 ]]; then
+    elif [[ "${net_mask}" -ne 64 ]]; then
         echo "WARN: VirtualBox interface with \"${IPV6_PUBLIC_CIDR}\" found in ${vboxnetname}"
-        echo "but set wrong network mask (${net_mask} instead of 16)"
+        echo "but set wrong network mask (${net_mask} instead of 64)"
         if [ ${YES_TO_ALL} -eq "0" ]; then
-            read -r -p "Change network mask of '${vboxnetname}' to 16? [y/N] " response
+            read -r -p "Change network mask of '${vboxnetname}' to 64? [y/N] " response
         else
             response="Y"
         fi
         case "${response}" in
             [yY])
-                echo "Changing network mask to 16..."
+                echo "Changing network mask to 64..."
             ;;
             *)
                 exit
@@ -514,7 +567,7 @@ function vboxnet_addr_finder(){
     fi
     split_ipv4 ipv4_array_nfs "${MASTER_IPV4_NFS}"
     IPV4_BASE_ADDR_NFS="$(printf "%d.%d.%d.1" "${ipv4_array_nfs[0]}" "${ipv4_array_nfs[1]}" "${ipv4_array_nfs[2]}")"
-    vboxnet_add_ipv6 "${vboxnetname}" "${IPV6_PUBLIC_CIDR}1" 16
+    vboxnet_add_ipv6 "${vboxnetname}" "${IPV6_PUBLIC_CIDR}1" 64
     vboxnet_add_ipv4 "${vboxnetname}" "${IPV4_BASE_ADDR_NFS}" "255.255.255.0"
 }
 

examples/kubernetes-ingress/deployments/.gitignore
@@ -1,2 +1,3 @@
 kubedns-rc.yaml
 kubedns-svc.yaml
+kubedns-controller.yaml

examples/kubernetes-ingress/deployments/guestbook/.gitignore
@@ -1,3 +1,0 @@
-1-redis-master-controller.json
-3-redis-slave-controller.json
-5-guestbook-controller.json

examples/kubernetes-ingress/deployments/guestbook/ingress/1-nginx-ingress-controller-rbac.yml
@@ -1,0 +1,132 @@
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: nginx-ingress
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: nginx-ingress-serviceaccount
+  namespace: nginx-ingress
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRole
+metadata:
+  name: nginx-ingress-clusterrole
+rules:
+  - apiGroups:
+      - ""
+    resources:
+      - configmaps
+      - endpoints
+      - nodes
+      - pods
+      - secrets
+    verbs:
+      - list
+      - watch
+  - apiGroups:
+      - ""
+    resources:
+      - nodes
+    verbs:
+      - get
+  - apiGroups:
+      - ""
+    resources:
+      - services
+    verbs:
+      - get
+      - list
+      - watch
+  - apiGroups:
+      - "extensions"
+    resources:
+      - ingresses
+    verbs:
+      - get
+      - list
+      - watch
+  - apiGroups:
+      - ""
+    resources:
+        - events
+    verbs:
+        - create
+        - patch
+  - apiGroups:
+      - "extensions"
+    resources:
+      - ingresses/status
+    verbs:
+      - update
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: Role
+metadata:
+  name: nginx-ingress-role
+  namespace: nginx-ingress
+rules:
+  - apiGroups:
+      - ""
+    resources:
+      - configmaps
+      - pods
+      - secrets
+      - namespaces
+    verbs:
+      - get
+  - apiGroups:
+      - ""
+    resources:
+      - configmaps
+    resourceNames:
+      # Defaults to "<election-id>-<ingress-class>"
+      # Here: "<ingress-controller-leader>-<nginx>"
+      # This has to be adapted if you change either parameter
+      # when launching the nginx-ingress-controller.
+      - "ingress-controller-leader-nginx"
+    verbs:
+      - get
+      - update
+  - apiGroups:
+      - ""
+    resources:
+      - configmaps
+    verbs:
+      - create
+  - apiGroups:
+      - ""
+    resources:
+      - endpoints
+    verbs:
+      - get
+      - create
+      - update
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: RoleBinding
+metadata:
+  name: nginx-ingress-role-nisa-binding
+  namespace: nginx-ingress
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: nginx-ingress-role
+subjects:
+  - kind: ServiceAccount
+    name: nginx-ingress-serviceaccount
+    namespace: nginx-ingress
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: nginx-ingress-clusterrole-nisa-binding
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: nginx-ingress-clusterrole
+subjects:
+  - kind: ServiceAccount
+    name: nginx-ingress-serviceaccount
+    namespace: nginx-ingress

examples/kubernetes-ingress/deployments/guestbook/ingress/default-server-secret.yaml
@@ -1,0 +1,9 @@
+apiVersion: v1
+kind: Secret
+metadata:
+  name: default-server-secret
+  namespace: nginx-ingress
+type: Opaque
+data:
+  tls.crt: LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURTVENDQWpHZ0F3SUJBZ0lKQUs5L2NDNWZocDJHTUEwR0NTcUdTSWIzRFFFQkJRVUFNQ0V4SHpBZEJnTlYKQkFNVEZrNUhTVTVZU1c1bmNtVnpjME52Ym5SeWIyeHNaWEl3SGhjTk1UY3dPRE14TVRBeE16UTRXaGNOTVRndwpPRE14TVRBeE16UTRXakFoTVI4d0hRWURWUVFERXhaT1IwbE9XRWx1WjNKbGMzTkRiMjUwY205c2JHVnlNSUlCCklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUF0bXhhMDhadExIaWxleWhOUWN5OUl4ankKWTBYdy9CRmZvM3duMDRsSXRoaGRxbkZ3NTZIVG1RVjIvbnEyRUxMdTNoejNjc3Urc3M5WFEzL3BrbXVwTEE5TApuaVVRZFVNcER4VlE1VFFKRW5CanJ5aXc4RWFlcEp4NUNCYVB5V3ZSZkpPb0pFSW56ZmNaYnE4OEVmQklYOHdtClFCa0xlcnFTVmRYWjBXR3FINVVQVlVZMVBqZXBqSXAyZ0NvbDRMUjM1aHRlSk9OMmZVTEF6cmRGMDBDT092WGsKUzgwRGw5eHdoUkVwVWVySGNuNXZod3BJazNkY3FNS3BxWTY2elF3dStMcFJEM3ZVWjR0eC9VYnlUdStkMkdhVwpWaG1RLy85RmtzUzVBS1d2ZXkrK3pPUTFDZTAxNzhDU0hRYXRDaWFuU2lTT3lwakZtTUZ0N1Mra25pbm9Xd0lECkFRQUJvNEdETUlHQU1CMEdBMVVkRGdRV0JCUlFUODVHRzV6a0QxV3FNSzZvOW8xWWFqUVBXVEJSQmdOVkhTTUUKU2pCSWdCUlFUODVHRzV6a0QxV3FNSzZvOW8xWWFqUVBXYUVscENNd0lURWZNQjBHQTFVRUF4TVdUa2RKVGxoSgpibWR5WlhOelEyOXVkSEp2Ykd4bGNvSUpBSzkvY0M1ZmhwMkdNQXdHQTFVZEV3UUZNQU1CQWY4d0RRWUpLb1pJCmh2Y05BUUVGQlFBRGdnRUJBSTIxcXpDN0lIYTEzblNvRkMxVFdtSUZydjQ2L2hRSFRjSFhxazRXZW16Z3VwVW8Kdmp0R05DVFlaR1VtL3RZY1FobDZvOXVJZlV5N3NlVS9OeWVCWHpOdGFiQUczQUIzanREVUJySy9xeVJ5cDZjRApIL0MzNmd5VFh3OGJxYVdOSzg0VGhYOVg2MFVFNVE2NzFUQUJMbk9paEhKUVVxTHdRc1VkdEkxRHBQb1BOOFlWCm5YQVl1RXJKWTVRckhzdHZoOFNZM2xoV3BSOWJ0eTVySldweUhIM3NDL1lHN2lFam5TUXp2LzdhK3cxTW1RQ0EKTk1wQnFvdzJKZkdveklyV2JvcFBVR2lmZ2szSjBKT24rcnA4RDRVc1lvNEo4Y3RvVk5qUFdmeU9zczB6ZWZ2aQpyUmVEUDdJOXc5THF1eERIRUhzeUpMUXN0MzNlQWlna1FBQU9zMUU9Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K
+  tls.key: LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlFb3dJQkFBS0NBUUVBdG14YTA4WnRMSGlsZXloTlFjeTlJeGp5WTBYdy9CRmZvM3duMDRsSXRoaGRxbkZ3CjU2SFRtUVYyL25xMkVMTHUzaHozY3N1K3NzOVhRMy9wa211cExBOUxuaVVRZFVNcER4VlE1VFFKRW5CanJ5aXcKOEVhZXBKeDVDQmFQeVd2UmZKT29KRUluemZjWmJxODhFZkJJWDh3bVFCa0xlcnFTVmRYWjBXR3FINVVQVlVZMQpQamVwaklwMmdDb2w0TFIzNWh0ZUpPTjJmVUxBenJkRjAwQ09PdlhrUzgwRGw5eHdoUkVwVWVySGNuNXZod3BJCmszZGNxTUtwcVk2NnpRd3UrTHBSRDN2VVo0dHgvVWJ5VHUrZDJHYVdWaG1RLy85RmtzUzVBS1d2ZXkrK3pPUTEKQ2UwMTc4Q1NIUWF0Q2lhblNpU095cGpGbU1GdDdTK2tuaW5vV3dJREFRQUJBb0lCQVFDQ002UkFNd2dKRGJOTwp5OTBZY2NFdEk4a2RBZmFXY3ZBSUI3MkZSaDhYbVJ5QllxWnJMUjJSd2t6RUpXRjlXYmtUM3lqZVRuMjFzamRlCmZoVi81RWZDb3NnZC8rWlhTN0FxaTlSSlEzS1dMcEYzbTF0dW8zam5sS2J1RnV4Wm54TE9EN1dhNjN6dGpNZ2kKTUFCMzdVQTYzOE1OVE5MY3JmMTBOa1paSTVRQkpYWWNPRk1ueDJ4MXVLRkU5RHQzWUEzbE9nOWNGdmFJTFpEQQo3WTVHVDlmUXdJQS92OGRWRU1DTkNiSzI1b1dnRG90WUdZaUhiYm1hUk9DTkRpNzVQZFpkM2daQ3IxUHFPWEZHCkJaVEh1L3Q4OXMwV1QyUkpNV2ljVW5XV0oyVHhmRWU1YUQ4R0JjRzEyN0pkamxLSitWZCtHWmxvODVYYVBvdnUKTVFxek1nbUJBb0dCQU9IS1pGbzVnSVkzL0J3aElCZ2RGUytnOG1GK21JTWpxSGVMN1NFSTNYL0UzWjhJd0syUgpmTTVFRUpTZnlETFpDVkNlSS8veWhBOUF6dG9Dam12TzdjMUxJT3kwR3k5dFlJVHlYY0xQNWNBWitBTkJCRExFCitYZkx5SE9KVXBDM2o4RFRZWDF0RENiUGJ5UFZTZENUNHNKT2JrNDVZVXQ3a3pEYTVHSFpsL3hqQW9HQkFNN1UKayt6TE5zbFQ2azJaakJaZW81YUdoMUNCSVV4bzNFNVpGYUZWR2lyMSs4NVlkVDdXVEpublJ6K0l6QXBMMmRqZApPZjVlQS9wa3JVNExMeGMzVVNEYjJwczJuT1hQd1p1OWdqRTM3aml0SUFRd3BHL3FiamQ3Y1ZaR2hlUkQyK3l4ClptTWU3c1BCZEVmcldmK1REYU9lT3B4L2RRcnFyTEc2UXo1ZHlQbXBBb0dBVmsyZ0VnU01wY0RjY253TzRtaXIKWW1zb2VpK0RhQXpISmZxc0JzWjJzNUd5REVteUxDWENDSzFua1FlSjVEV2xJOVZ1ZVRSZldkMHhzNDdxbFRhaApHcWt1eW9zRklSbXpuTjF2RFRtZDNkR1BSTjhqRmF6SWxndWtjTlQ2WkNwbG5oU3QzTjFEbWNvTDl5eGRiSVk2ClZIN2FGcmhFQWpBWDBNSzZMTlNaRFhVQ2dZQlRYc3JWeTBBbFBTY1g2b25XUm9Xb1drZlhBb1lhbDdZZCtyakcKVkZoODhyUnlnNk9YRmFqQTdNSUNjVERXQWFjcFRGdGhGaUtDWHV5Z3BjOXdpMEt2ZlErTU95SlpYRHBOZmNFcAo5OEtWbyt0ZzVQNlRnaXExUUpQNTArbUtqblBxMzhOR3R5UkZVZ2grS1BjWkZ2eUxkRzlwdjlLOCtNVnR5b2ZxCmJzRmhLUUtCZ0NvcEg5Wm95MjJBNStLcnJYZmQ0VXRBcndjN0dVanFUT1hhTzgyd3FpU0hZMndPTGdkWWw0L3kKSDJEYy9EMWxmWS9GL09sckNMZDNpL0lLc0wxNG13R2dxODZRdDhxeTIwcWw4RFNyWG91TmhsQTJmL1ZUTk1SMAp2OXAwU1JrQjI2UVYyUitndnNVYk9xb1lhMlVQVkNuQW9QeTYwTXlBaVJUR3cyeTExbm9lCi0tLS0tRU5EIFJTQSBQUklWQVRFIEtFWS0tLS0tCg==

examples/kubernetes-ingress/deployments/guestbook/ingress/guestbook-ingress.yaml
@@ -2,7 +2,15 @@ apiVersion: extensions/v1beta1
 kind: Ingress
 metadata:
   name: guestbook-ingress
+  annotations:
+    ingress.kubernetes.io/paths-as-regex: "true"
+    ingress.kubernetes.io/rewrite-target: "/"
 spec:
-  backend:
-    serviceName: guestbook
-    servicePort: 80 
+  rules:
+  - host: foo.bar.com
+    http:
+      paths:
+      - path: /*
+        backend:
+          serviceName: guestbook
+          servicePort: 80

examples/kubernetes-ingress/deployments/guestbook/ingress/nginx-ingress-controller-service.yml
@@ -1,0 +1,16 @@
+apiVersion: v1
+kind: Service
+metadata:
+  name: nginx-ingress
+  namespace: nginx-ingress
+spec:
+# Can also use LoadBalancer type
+  type: NodePort
+  ports:
+  - name: http
+    port: 8080
+    nodePort: 30080
+    targetPort: 80
+    protocol: TCP
+  selector:
+    k8s-app: nginx-ingress-lb

commit b52971181f7173d3fc1afbc7fed473967b327600

vagrant: Don't use the NFS device's IP as node IP
The K8s node IP is the IP address propagated to other nodes and mapped to
the REMOTE_NODE_ID in the ipcache. We therefore don't want to use the IP
address of the NFS interface (enp0s9) for that. When we use that IP
address, any policy using the remote-node identity (or host in case the
two aren't dissociated) will fail to resolve properly.

In general, I don't think K8s even needs to know about the NFS interface
or its IP addresses.

Fixes: 47ab30a ("examples/kubernetes-ingress: fixing scripts to run k8s 1.8.1")
Signed-off-by: Paul Chaignon <paul@cilium.io>

Vagrantfile
@@ -177,7 +177,6 @@ Vagrant.configure(2) do |config|
                 type: "shell",
                 run: "always",
                 inline: "ip -6 a a #{$master_ipv6}/16 dev enp0s9"
-            node_ip = "#{$nfs_ipv4_master_addr}"
             if ENV["IPV6_EXT"] then
                 node_ip = "#{$master_ipv6}"
             end
@@ -225,7 +224,6 @@ Vagrant.configure(2) do |config|
                 :libvirt__dhcp_enabled => false
             if ENV["NFS"] || ENV["IPV6_EXT"] then
                 nfs_ipv4_addr = $workers_ipv4_addrs_nfs[n]
-                node_ip = "#{nfs_ipv4_addr}"
                 ipv6_addr = $workers_ipv6_addrs[n]
                 node.vm.network "private_network", ip: "#{nfs_ipv4_addr}", bridge: "enp0s9"
                 # Add IPv6 address this way or we get hit by a virtualbox bug