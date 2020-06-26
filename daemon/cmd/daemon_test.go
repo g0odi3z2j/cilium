g0odi3z2j/cilium

commit 36e6564b16413478ed2c7f54286cec2d11078b65

daemon: Add assertions for endpoint state metrics
This commit adds new assertions on endpoint state metrics to the
existing test cases. This commit helps validate that when an endpoint
fails during creation, it will be deleted, and the bookkeeping of the
endpoint state metrics is correct.

Additionally, this commits increases the code coverage around the
creation of endpoints when they fail.

Signed-off-by: Chris Tarazi <chris@isovalent.com>

daemon/cmd/daemon_test.go
@@ -25,6 +25,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/datapath"
 	fakedatapath "github.com/cilium/cilium/pkg/datapath/fake"
@@ -35,10 +36,12 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labelsfilter"
 	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/metrics"
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 
+	"github.com/prometheus/client_golang/prometheus"
 	. "gopkg.in/check.v1"
 )
 
@@ -59,6 +62,9 @@ type DaemonSuite struct {
 	OnQueueEndpointBuild  func(ctx context.Context, epID uint64) (func(), error)
 	OnGetCompilationLock  func() *lock.RWMutex
 	OnSendNotification    func(typ monitorAPI.AgentNotification, text string) error
+
+	// Metrics
+	collectors []prometheus.Collector
 }
 
 func setupTestDirectories() {
@@ -105,6 +111,19 @@ type dummyEpSyncher struct{}
 func (epSync *dummyEpSyncher) RunK8sCiliumEndpointSync(e *endpoint.Endpoint, conf endpoint.EndpointStatusConfiguration) {
 }
 
+func (ds *DaemonSuite) SetUpSuite(c *C) {
+	// Register metrics once before running the suite
+	_, ds.collectors = metrics.CreateConfiguration([]string{"cilium_endpoint_state"})
+	metrics.MustRegister(ds.collectors...)
+}
+
+func (ds *DaemonSuite) TearDownSuite(c *C) {
+	// Unregister the metrics after the suite has finished
+	for _, c := range ds.collectors {
+		metrics.Unregister(c)
+	}
+}
+
 func (ds *DaemonSuite) SetUpTest(c *C) {
 
 	setupTestDirectories()
@@ -124,6 +143,14 @@ func (ds *DaemonSuite) SetUpTest(c *C) {
 	ds.OnGetCompilationLock = nil
 	ds.OnSendNotification = nil
 	ds.d.endpointManager = endpointmanager.NewEndpointManager(&dummyEpSyncher{})
+
+	// Reset the most common endpoint states before each test.
+	for _, s := range []string{
+		string(models.EndpointStateReady),
+		string(models.EndpointStateWaitingForIdentity),
+		string(models.EndpointStateWaitingToRegenerate)} {
+		metrics.EndpointStateCount.WithLabelValues(s).Set(0.0)
+	}
 }
 
 func (ds *DaemonSuite) TearDownTest(c *C) {

daemon/cmd/endpoint_test.go
@@ -19,6 +19,7 @@ package cmd
 import (
 	"context"
 	"net"
+	"runtime"
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
@@ -27,6 +28,8 @@ import (
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/metrics"
+
 	. "gopkg.in/check.v1"
 )
 
@@ -47,26 +50,58 @@ func getEPTemplate(c *C, d *Daemon) *models.EndpointChangeRequest {
 }
 
 func (ds *DaemonSuite) TestEndpointAddReservedLabel(c *C) {
+	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
+
 	epTemplate := getEPTemplate(c, ds.d)
 	epTemplate.Labels = []string{"reserved:world"}
 	_, code, err := ds.d.createEndpoint(context.TODO(), epTemplate)
 	c.Assert(err, Not(IsNil))
 	c.Assert(code, Equals, apiEndpoint.PutEndpointIDInvalidCode)
+
+	// Endpoint was created with invalid data; should transition from
+	// WaitForIdentity -> Invalid.
+	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
+	assertOnMetric(c, string(models.EndpointStateInvalid), 0)
+
+	// Endpoint is created with inital label as well as disallowed
+	// reserved:world label.
+	epTemplate.Labels = append(epTemplate.Labels, "reserved:init")
+	_, code, err = ds.d.createEndpoint(context.TODO(), epTemplate)
+	c.Assert(err, ErrorMatches, "not allowed to add reserved labels:.+")
+	c.Assert(code, Equals, apiEndpoint.PutEndpointIDInvalidCode)
+
+	// Endpoint was created with invalid data; should transition from
+	// WaitForIdentity -> Invalid.
+	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
+	assertOnMetric(c, string(models.EndpointStateInvalid), 0)
 }
 
 func (ds *DaemonSuite) TestEndpointAddInvalidLabel(c *C) {
+	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
+
 	epTemplate := getEPTemplate(c, ds.d)
 	epTemplate.Labels = []string{"reserved:foo"}
 	_, code, err := ds.d.createEndpoint(context.TODO(), epTemplate)
 	c.Assert(err, Not(IsNil))
 	c.Assert(code, Equals, apiEndpoint.PutEndpointIDInvalidCode)
+
+	// Endpoint was created with invalid data; should transition from
+	// WaitForIdentity -> Invalid.
+	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
+	assertOnMetric(c, string(models.EndpointStateInvalid), 0)
 }
 
 func (ds *DaemonSuite) TestEndpointAddNoLabels(c *C) {
+	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
+
 	// Create the endpoint without any labels.
 	epTemplate := getEPTemplate(c, ds.d)
 	_, _, err := ds.d.createEndpoint(context.TODO(), epTemplate)
 	c.Assert(err, IsNil)
+
+	// Endpoint enters WaitingToRegenerate as it has its labels updated during
+	// creation.
+	assertOnMetric(c, string(models.EndpointStateWaitingToRegenerate), 1)
 
 	expectedLabels := labels.Labels{
 		labels.IDNameInit: labels.NewLabel(labels.IDNameInit, "", labels.LabelSourceReserved),
@@ -79,6 +114,10 @@ func (ds *DaemonSuite) TestEndpointAddNoLabels(c *C) {
 	secID := ep.WaitForIdentity(3 * time.Second)
 	c.Assert(secID, Not(IsNil))
 	c.Assert(secID.ID, Equals, identity.ReservedIdentityInit)
+
+	// Endpoint should transition from WaitingToRegenerate -> Ready.
+	assertOnMetric(c, string(models.EndpointStateWaitingToRegenerate), 0)
+	assertOnMetric(c, string(models.EndpointStateReady), 1)
 }
 
 func (ds *DaemonSuite) TestUpdateSecLabels(c *C) {
@@ -86,4 +125,29 @@ func (ds *DaemonSuite) TestUpdateSecLabels(c *C) {
 	code, err := ds.d.modifyEndpointIdentityLabelsFromAPI("1", lbls, nil)
 	c.Assert(err, Not(IsNil))
 	c.Assert(code, Equals, apiEndpoint.PatchEndpointIDLabelsUpdateFailedCode)
+}
+
+func (ds *DaemonSuite) TestUpdateLabelsFailed(c *C) {
+	cancelledContext, cancelFunc := context.WithTimeout(context.Background(), 1*time.Second)
+	cancelFunc() // Cancel immediatly to trigger the codepath to test.
+
+	// Create the endpoint without any labels.
+	epTemplate := getEPTemplate(c, ds.d)
+	_, _, err := ds.d.createEndpoint(cancelledContext, epTemplate)
+	c.Assert(err, ErrorMatches, "request cancelled while resolving identity")
+
+	assertOnMetric(c, string(models.EndpointStateReady), 0)
+}
+
+func getMetricValue(state string) int64 {
+	return int64(metrics.GetGaugeValue(metrics.EndpointStateCount.WithLabelValues(state)))
+}
+
+func assertOnMetric(c *C, state string, expected int64) {
+	obtained := getMetricValue(state)
+	if obtained != expected {
+		_, _, line, _ := runtime.Caller(1)
+		c.Errorf("Metrics assertion failed on line %d for Endpoint state %s: obtained %d, expected %d",
+			line, state, obtained, expected)
+	}
 }

commit dec9ac9fc26067a3876e710bebbbf3babea0df2d

daemon: Pass regen. owner to endpoint creation
For testing purposes, it's useful to pass the owner of the endpoint to
`(*Daemon).createEndpoint`. Since an owner must implement the
`regeneration.Owner` interface, we can utilize this fact to mock out the
enqueuing of endpoint regeneration. In other words, we avoid the state
transitions caused by endpoint regeneration because the enqueuing has
been "blocked" by mocking it out. This allows us to assert on specific
state transitions without worrying about endpoint regeneration kicking
in and transitioning endpoint state, unintentionally.

We also are utilizing the fact that this test suite (DaemonSuite)
already implemented the `regeneration.Owner` interface for mocking
purposes, but wasn't used in such a fashion.

This commit also fixes two incorrect assertions in the test:

1) Remove premature assertion on metrics because the
   `TestEndpointAddNoLabels` test is about creating an endpoint without
   any initial labels set. The endpoint's initial label is assigned
   (`reserved:init`) within the call of `(*Daemon).createEndpoint`, so
   it is very unlikely that we catch the endpoint in any other state
   besides "ready". Therefore, we remove this premature assertion as it
   doesn't test anything useful.
2) The final state transition in `TestEndpointAddNoLabels` should be
   "regenerating" -> "ready" because now
   `(*Daemon).QueueEndpointBuild()` is using the implementation (not
   mocked out, like in the rest of the suite).

Fixes: 36e6564b16 ("daemon: Add assertions for endpoint state metrics")
Fixes: 5c391ea52d ("daemon: Fix waiting on metrics in endpoint_test.go")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

daemon/cmd/endpoint.go
@@ -282,7 +282,7 @@ func (m *endpointCreationManager) DebugStatus() (output string) {
 
 // createEndpoint attempts to create the endpoint corresponding to the change
 // request that was specified.
-func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.EndpointChangeRequest) (*endpoint.Endpoint, int, error) {
+func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, epTemplate *models.EndpointChangeRequest) (*endpoint.Endpoint, int, error) {
 	if option.Config.EnableEndpointRoutes {
 		if epTemplate.DatapathConfiguration == nil {
 			epTemplate.DatapathConfiguration = &models.EndpointDatapathConfiguration{}
@@ -313,7 +313,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 		"sync-build":            epTemplate.SyncBuildEndpoint,
 	}).Info("Create endpoint request")
 
-	ep, err := endpoint.NewEndpointFromChangeModel(d.ctx, d, d.l7Proxy, d.identityAllocator, epTemplate)
+	ep, err := endpoint.NewEndpointFromChangeModel(d.ctx, owner, d.l7Proxy, d.identityAllocator, epTemplate)
 	if err != nil {
 		return invalidDataError(ep, fmt.Errorf("unable to parse endpoint parameters: %s", err))
 	}
@@ -409,7 +409,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 	}
 
 	// e.ID assigned here
-	err = d.endpointManager.AddEndpoint(d, ep, "Create endpoint from API PUT")
+	err = d.endpointManager.AddEndpoint(owner, ep, "Create endpoint from API PUT")
 	if err != nil {
 		return d.errorDuringCreation(ep, fmt.Errorf("unable to insert endpoint into manager: %s", err))
 	}
@@ -494,7 +494,7 @@ func (h *putEndpointID) Handle(params PutEndpointIDParams) middleware.Responder 
 	}
 	epTemplate := params.Endpoint
 
-	ep, code, err := h.d.createEndpoint(params.HTTPRequest.Context(), epTemplate)
+	ep, code, err := h.d.createEndpoint(params.HTTPRequest.Context(), h.d, epTemplate)
 	if err != nil {
 		return api.Error(code, err)
 	}

daemon/cmd/daemon_test.go
@@ -58,10 +58,11 @@ type DaemonSuite struct {
 	kvstoreInit bool
 
 	// Owners interface mock
-	OnGetPolicyRepository func() *policy.Repository
-	OnQueueEndpointBuild  func(ctx context.Context, epID uint64) (func(), error)
-	OnGetCompilationLock  func() *lock.RWMutex
-	OnSendNotification    func(typ monitorAPI.AgentNotification, text string) error
+	OnGetPolicyRepository  func() *policy.Repository
+	OnQueueEndpointBuild   func(ctx context.Context, epID uint64) (func(), error)
+	OnGetCompilationLock   func() *lock.RWMutex
+	OnSendNotification     func(typ monitorAPI.AgentNotification, text string) error
+	OnGetCIDRPrefixLengths func() ([]int, []int)
 
 	// Metrics
 	collectors []prometheus.Collector
@@ -143,10 +144,12 @@ func (ds *DaemonSuite) SetUpTest(c *C) {
 	kvstore.Client().DeletePrefix(context.TODO(), common.OperationalPath)
 	kvstore.Client().DeletePrefix(context.TODO(), kvstore.BaseKeyPrefix)
 
-	ds.OnGetPolicyRepository = nil
+	ds.OnGetPolicyRepository = d.GetPolicyRepository
 	ds.OnQueueEndpointBuild = nil
-	ds.OnGetCompilationLock = nil
-	ds.OnSendNotification = nil
+	ds.OnGetCompilationLock = d.GetCompilationLock
+	ds.OnSendNotification = d.SendNotification
+	ds.OnGetCIDRPrefixLengths = nil
+
 	ds.d.endpointManager = endpointmanager.NewEndpointManager(&dummyEpSyncher{})
 
 	// Reset the most common endpoint states before each test.
@@ -239,7 +242,8 @@ func (ds *DaemonSuite) QueueEndpointBuild(ctx context.Context, epID uint64) (fun
 	if ds.OnQueueEndpointBuild != nil {
 		return ds.OnQueueEndpointBuild(ctx, epID)
 	}
-	panic("QueueEndpointBuild should not have been called")
+
+	return nil, nil
 }
 
 func (ds *DaemonSuite) GetCompilationLock() *lock.RWMutex {
@@ -254,6 +258,13 @@ func (ds *DaemonSuite) SendNotification(typ monitorAPI.AgentNotification, text s
 		return ds.OnSendNotification(typ, text)
 	}
 	panic("SendNotification should not have been called")
+}
+
+func (ds *DaemonSuite) GetCIDRPrefixLengths() ([]int, []int) {
+	if ds.OnGetCIDRPrefixLengths != nil {
+		return ds.OnGetCIDRPrefixLengths()
+	}
+	panic("GetCIDRPrefixLengths should not have been called")
 }
 
 func (ds *DaemonSuite) Datapath() datapath.Datapath {

daemon/cmd/endpoint_test.go
@@ -55,24 +55,24 @@ func (ds *DaemonSuite) TestEndpointAddReservedLabel(c *C) {
 
 	epTemplate := getEPTemplate(c, ds.d)
 	epTemplate.Labels = []string{"reserved:world"}
-	_, code, err := ds.d.createEndpoint(context.TODO(), epTemplate)
+	_, code, err := ds.d.createEndpoint(context.TODO(), ds, epTemplate)
 	c.Assert(err, Not(IsNil))
 	c.Assert(code, Equals, apiEndpoint.PutEndpointIDInvalidCode)
 
 	// Endpoint was created with invalid data; should transition from
-	// WaitForIdentity -> Invalid.
+	// WaitingForIdentity -> Invalid.
 	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
 	assertOnMetric(c, string(models.EndpointStateInvalid), 0)
 
 	// Endpoint is created with inital label as well as disallowed
 	// reserved:world label.
 	epTemplate.Labels = append(epTemplate.Labels, "reserved:init")
-	_, code, err = ds.d.createEndpoint(context.TODO(), epTemplate)
+	_, code, err = ds.d.createEndpoint(context.TODO(), ds, epTemplate)
 	c.Assert(err, ErrorMatches, "not allowed to add reserved labels:.+")
 	c.Assert(code, Equals, apiEndpoint.PutEndpointIDInvalidCode)
 
 	// Endpoint was created with invalid data; should transition from
-	// WaitForIdentity -> Invalid.
+	// WaitingForIdentity -> Invalid.
 	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
 	assertOnMetric(c, string(models.EndpointStateInvalid), 0)
 }
@@ -82,12 +82,12 @@ func (ds *DaemonSuite) TestEndpointAddInvalidLabel(c *C) {
 
 	epTemplate := getEPTemplate(c, ds.d)
 	epTemplate.Labels = []string{"reserved:foo"}
-	_, code, err := ds.d.createEndpoint(context.TODO(), epTemplate)
+	_, code, err := ds.d.createEndpoint(context.TODO(), ds, epTemplate)
 	c.Assert(err, Not(IsNil))
 	c.Assert(code, Equals, apiEndpoint.PutEndpointIDInvalidCode)
 
 	// Endpoint was created with invalid data; should transition from
-	// WaitForIdentity -> Invalid.
+	// WaitingForIdentity -> Invalid.
 	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
 	assertOnMetric(c, string(models.EndpointStateInvalid), 0)
 }
@@ -95,14 +95,14 @@ func (ds *DaemonSuite) TestEndpointAddInvalidLabel(c *C) {
 func (ds *DaemonSuite) TestEndpointAddNoLabels(c *C) {
 	assertOnMetric(c, string(models.EndpointStateWaitingForIdentity), 0)
 
+	// For this test case, we want to allow the endpoint controllers to rebuild
+	// the endpoint after getting new labels.
+	ds.OnQueueEndpointBuild = ds.d.QueueEndpointBuild
+
 	// Create the endpoint without any labels.
 	epTemplate := getEPTemplate(c, ds.d)
-	_, _, err := ds.d.createEndpoint(context.TODO(), epTemplate)
+	_, _, err := ds.d.createEndpoint(context.TODO(), ds, epTemplate)
 	c.Assert(err, IsNil)
-
-	// Endpoint enters WaitingToRegenerate as it has its labels updated during
-	// creation.
-	assertOnMetric(c, string(models.EndpointStateWaitingToRegenerate), 1)
 
 	expectedLabels := labels.Labels{
 		labels.IDNameInit: labels.NewLabel(labels.IDNameInit, "", labels.LabelSourceReserved),
@@ -116,8 +116,10 @@ func (ds *DaemonSuite) TestEndpointAddNoLabels(c *C) {
 	c.Assert(secID, Not(IsNil))
 	c.Assert(secID.ID, Equals, identity.ReservedIdentityInit)
 
-	// Endpoint should transition from WaitingToRegenerate -> Ready.
-	assertOnMetric(c, string(models.EndpointStateWaitingToRegenerate), 0)
+	// Endpoint should transition from Regenerating -> Ready after we've
+	// waitied for its new identity. The presence of new labels triggers a
+	// regeneration.
+	assertOnMetric(c, string(models.EndpointStateRegenerating), 0)
 	assertOnMetric(c, string(models.EndpointStateReady), 1)
 }
 
@@ -134,7 +136,7 @@ func (ds *DaemonSuite) TestUpdateLabelsFailed(c *C) {
 
 	// Create the endpoint without any labels.
 	epTemplate := getEPTemplate(c, ds.d)
-	_, _, err := ds.d.createEndpoint(cancelledContext, epTemplate)
+	_, _, err := ds.d.createEndpoint(cancelledContext, ds, epTemplate)
 	c.Assert(err, ErrorMatches, "request cancelled while resolving identity")
 
 	assertOnMetric(c, string(models.EndpointStateReady), 0)