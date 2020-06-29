g0odi3z2j/cilium

commit 63bdd3fc716dcd9877d12bcb62b4704ca8c40210

bpf: Use `nproc --all` for __NR_CPUS__
This uses `-D__NR_CPUS__=$(nproc --all)` (or `GetNumPossibleCPUs` when
invoked from Go) to compile the datapath.

This fixes an issue where cilium monitor fails to report any events
on AKS, due to the `perf_event_array` map duplicates being created
with different max_entries sizes, presumably causing the datapath
to write to the first one, while the agent is reading from the second
one.

This bug occurs for example on AKS due to the present/possible cpuset on
the VMs. The default Standard_D2s_v3 node size has 2 present CPUs, but
128 possible CPUs in /sys/devices/system/cpu.

Fixes: #12070

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

bpf/init.sh
@@ -278,7 +278,7 @@ function bpf_compile()
 	      -Wno-gnu-variable-sized-type-not-at-end			\
 	      -Wdeclaration-after-statement				\
 	      -I. -I$DIR -I$LIB -I$LIB/include				\
-	      -D__NR_CPUS__=$(nproc)					\
+	      -D__NR_CPUS__=$(nproc --all)					\
 	      -DENABLE_ARP_RESPONDER=1					\
 	      -DHANDLE_NS=1						\
 	      $EXTRA_OPTS						\

bpf/Makefile.bpf
@@ -1,7 +1,7 @@
 # Copyright 2017-2020 Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
-FLAGS := -I$(ROOT_DIR)/bpf/include -I$(ROOT_DIR)/bpf -D__NR_CPUS__=$(shell nproc) -O2 -g
+FLAGS := -I$(ROOT_DIR)/bpf/include -I$(ROOT_DIR)/bpf -D__NR_CPUS__=$(shell nproc --all) -O2 -g
 
 CLANG_FLAGS := ${FLAGS} -target bpf -std=gnu89 -nostdinc -emit-llvm
 # eBPF verifier enforces unaligned access checks where necessary, so don't

pkg/datapath/loader/compile.go
@@ -23,10 +23,10 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
-	"runtime"
 	"sync"
 
 	"github.com/cilium/cilium/pkg/command/exec"
+	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/datapath/linux/probes"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
@@ -90,7 +90,7 @@ type directoryInfo struct {
 
 var (
 	standardCFlags = []string{"-O2", "-target", "bpf", "-std=gnu89",
-		"-nostdinc", fmt.Sprintf("-D__NR_CPUS__=%d", runtime.NumCPU()),
+		"-nostdinc", fmt.Sprintf("-D__NR_CPUS__=%d", common.GetNumPossibleCPUs(log)),
 		"-Wall", "-Wextra", "-Werror", "-Wshadow",
 		"-Wno-address-of-packed-member",
 		"-Wno-unknown-warning-option",

test/bpf/Makefile
@@ -3,7 +3,7 @@
 
 include ../../Makefile.defs
 
-FLAGS := -I../../bpf/ -I../../bpf/include -I. -D__NR_CPUS__=$(shell nproc) -O2
+FLAGS := -I../../bpf/ -I../../bpf/include -I. -D__NR_CPUS__=$(shell nproc --all) -O2
 FLAGS_CLANG := -Wall -Wextra -Werror -Wshadow -Wno-unused-parameter
 FLAGS_CLANG += -Wno-address-of-packed-member
 FLAGS_CLANG += -Wno-unknown-warning-option

commit 680107ffa318b0909d8f82e598910c4a83ed1f9a

bpf: Use same file as Golang side instead of nproc
On Golang side we get the number of CPUs from
/sys/devices/system/cpu/possible. On BPF side, we use $(nproc -all).
nproc calls num_processors() from the gnulib. That function, however, may
not always return the value from the /sys file above.

Instead, we should use the exact same source as Golang side to ensure
both sides have the same value and avoid issues later on. See #12070
for details.

The __NR_CPUS__ values in test/bpf/Makefile and bpf/Makefile.bpf do not
need to be in sync. with Golang values because these files are only used
for unit tests, sparse, and compile testing.

Fixes: 63bdd3f ("bpf: Use `nproc --all` for __NR_CPUS__")
Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/init.sh
@@ -36,6 +36,7 @@ NODE_PORT_BIND=${18}
 MCPU=${19}
 NODE_PORT_IPV4_ADDRS=${20}
 NODE_PORT_IPV6_ADDRS=${21}
+NR_CPUS=${22}
 
 ID_HOST=1
 ID_WORLD=2
@@ -271,18 +272,18 @@ function bpf_compile()
 	TYPE=$3
 	EXTRA_OPTS=$4
 
-	clang -O2 -target bpf -std=gnu89 -nostdinc -emit-llvm		\
-	      -Wall -Wextra -Werror -Wshadow				\
-	      -Wno-address-of-packed-member				\
-	      -Wno-unknown-warning-option				\
-	      -Wno-gnu-variable-sized-type-not-at-end			\
-	      -Wdeclaration-after-statement				\
-	      -I. -I$DIR -I$LIB -I$LIB/include				\
-	      -D__NR_CPUS__=$(nproc --all)					\
-	      -DENABLE_ARP_RESPONDER=1					\
-	      -DHANDLE_NS=1						\
-	      $EXTRA_OPTS						\
-	      -c $LIB/$IN -o - |					\
+	clang -O2 -target bpf -std=gnu89 -nostdinc -emit-llvm	\
+	      -Wall -Wextra -Werror -Wshadow			\
+	      -Wno-address-of-packed-member			\
+	      -Wno-unknown-warning-option			\
+	      -Wno-gnu-variable-sized-type-not-at-end		\
+	      -Wdeclaration-after-statement			\
+	      -I. -I$DIR -I$LIB -I$LIB/include			\
+	      -D__NR_CPUS__=$NR_CPUS				\
+	      -DENABLE_ARP_RESPONDER=1				\
+	      -DHANDLE_NS=1					\
+	      $EXTRA_OPTS					\
+	      -c $LIB/$IN -o - |				\
 	llc -march=bpf -mcpu=$MCPU -mattr=dwarfris -filetype=$TYPE -o $OUT
 }
 

pkg/datapath/loader/base.go
@@ -63,6 +63,7 @@ const (
 	initBPFCPU
 	initArgNodePortIPv4Addrs
 	initArgNodePortIPv6Addrs
+	initArgNrCPUs
 	initArgMax
 )
 
@@ -286,6 +287,7 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 	}
 
 	args[initBPFCPU] = GetBPFCPU()
+	args[initArgNrCPUs] = fmt.Sprintf("%d", common.GetNumPossibleCPUs(log))
 
 	clockSource := []string{"ktime", "jiffies"}
 	log.Infof("Setting up base BPF datapath (BPF %s instruction set, %s clock source)",