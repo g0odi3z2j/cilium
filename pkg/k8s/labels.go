g0odi3z2j/cilium

commit b180709396089fd2dcbf80ccc7bfa5edcba067f5

pkg/endpoint: fetch pod and namespace labels from local stores
This commit reduces the number of k8s apiserver calls by switching to a
local store fetch logic for Pods an Namespaces.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/labels.go
@@ -18,13 +18,12 @@ import (
 	"regexp"
 
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 
 	"github.com/sirupsen/logrus"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -56,7 +55,7 @@ var (
 
 // isInjectedWithIstioSidecarProxy returns whether the given pod has been
 // injected by Istio with a sidecar proxy that is compatible with Cilium.
-func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *corev1.Pod) bool {
+func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *types.Pod) bool {
 	istioStatusString, ok := pod.GetAnnotations()[AnnotationIstioSidecarStatus]
 	if !ok {
 		// Istio's injection annotation was not found.
@@ -69,7 +68,7 @@ func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *corev1.Pod) b
 
 	// Check that there's an "istio-proxy" container that uses an image
 	// compatible with Cilium.
-	for _, container := range pod.Spec.Containers {
+	for _, container := range pod.SpecContainers {
 		if container.Name != "istio-proxy" {
 			continue
 		}
@@ -80,8 +79,8 @@ func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *corev1.Pod) b
 		}
 		scopedLog.Debugf("istio-proxy container runs Cilium-compatible image: %s", container.Image)
 
-		for _, mount := range container.VolumeMounts {
-			if mount.MountPath != "/var/run/cilium" {
+		for _, mountPath := range container.VolumeMountsPaths {
+			if mountPath != "/var/run/cilium" {
 				continue
 			}
 			scopedLog.Debug("istio-proxy container has volume mounted into /var/run/cilium")
@@ -96,26 +95,16 @@ func isInjectedWithIstioSidecarProxy(scopedLog *logrus.Entry, pod *corev1.Pod) b
 
 // GetPodMetadata returns the labels and annotations of the pod with the given
 // namespace / name.
-func GetPodMetadata(namespace, podName string) (lbls map[string]string, retAnno map[string]string, retErr error) {
+func GetPodMetadata(k8sNs *types.Namespace, pod *types.Pod) (lbls map[string]string, retAnno map[string]string, retErr error) {
+	namespace := pod.Namespace
 	scopedLog := log.WithFields(logrus.Fields{
 		logfields.K8sNamespace: namespace,
-		logfields.K8sPodName:   podName,
+		logfields.K8sPodName:   pod.Name,
 	})
-	scopedLog.Debug("Connecting to k8s apiserver to retrieve labels for pod")
+	scopedLog.Debug("Connecting to k8s local stores to retrieve labels for pod")
 
-	result, err := Client().CoreV1().Pods(namespace).Get(podName, metav1.GetOptions{})
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Also get all labels from the namespace where the pod is running
-	k8sNs, err := Client().CoreV1().Namespaces().Get(namespace, metav1.GetOptions{})
-	if err != nil {
-		return nil, nil, err
-	}
-
-	annotations := result.GetAnnotations()
-	k8sLabels := result.GetLabels()
+	annotations := pod.GetAnnotations()
+	k8sLabels := pod.GetLabels()
 	if k8sLabels == nil {
 		k8sLabels = map[string]string{}
 	}
@@ -124,8 +113,8 @@ func GetPodMetadata(namespace, podName string) (lbls map[string]string, retAnno 
 	}
 	k8sLabels[k8sConst.PodNamespaceLabel] = namespace
 
-	if result.Spec.ServiceAccountName != "" {
-		k8sLabels[k8sConst.PolicyLabelServiceAccount] = result.Spec.ServiceAccountName
+	if pod.SpecServiceAccountName != "" {
+		k8sLabels[k8sConst.PolicyLabelServiceAccount] = pod.SpecServiceAccountName
 	} else {
 		delete(k8sLabels, k8sConst.PolicyLabelServiceAccount)
 	}
@@ -135,7 +124,7 @@ func GetPodMetadata(namespace, podName string) (lbls map[string]string, retAnno 
 	// If the pod already contains that label to explicitly enable or disable
 	// the sidecar proxy mode, keep it as is.
 	if _, ok := k8sLabels[k8sConst.PolicyLabelIstioSidecarProxy]; !ok &&
-		isInjectedWithIstioSidecarProxy(scopedLog, result) {
+		isInjectedWithIstioSidecarProxy(scopedLog, pod) {
 		k8sLabels[k8sConst.PolicyLabelIstioSidecarProxy] = "true"
 	}
 

daemon/endpoint.go
@@ -148,8 +148,17 @@ func NewPutEndpointIDHandler(d *Daemon) PutEndpointIDHandler {
 
 // fetchK8sLabelsAndAnnotations wraps the k8s package to fetch and provide
 // endpoint metadata. It implements endpoint.MetadataResolverCB.
-func fetchK8sLabelsAndAnnotations(ep *endpoint.Endpoint) (labels.Labels, labels.Labels, map[string]string, error) {
-	lbls, annotations, err := k8s.GetPodMetadata(ep.GetK8sNamespace(), ep.GetK8sPodName())
+func (d *Daemon) fetchK8sLabelsAndAnnotations(nsName, podName string) (labels.Labels, labels.Labels, map[string]string, error) {
+	p, err := d.k8sWatcher.GetCachedPod(nsName, podName)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	ns, err := d.k8sWatcher.GetCachedNamespace(nsName)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	lbls, annotations, err := k8s.GetPodMetadata(ns, p)
 	if err != nil {
 		return nil, nil, nil, err
 	}
@@ -250,7 +259,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 	}
 
 	if ep.K8sNamespaceAndPodNameIsSet() && k8s.IsEnabled() {
-		identityLabels, info, _, err := fetchK8sLabelsAndAnnotations(ep)
+		identityLabels, info, _, err := d.fetchK8sLabelsAndAnnotations(ep.K8sNamespace, ep.K8sPodName)
 		if err != nil {
 			ep.Logger("api").WithError(err).Warning("Unable to fetch kubernetes labels")
 		} else {
@@ -284,7 +293,7 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 		// If there are labels, but no pod namespace, then it's
 		// likely that there are no k8s labels at all. Resolve.
 		if _, k8sLabelsConfigured := addLabels[k8sConst.PodNamespaceLabel]; !k8sLabelsConfigured {
-			ep.RunMetadataResolver(fetchK8sLabelsAndAnnotations)
+			ep.RunMetadataResolver(d.fetchK8sLabelsAndAnnotations)
 		}
 	}
 

daemon/state.go
@@ -35,7 +35,6 @@ import (
 
 	"github.com/sirupsen/logrus"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type endpointRestoreState struct {
@@ -69,19 +68,38 @@ func (d *Daemon) validateEndpoint(ep *endpoint.Endpoint) (valid bool, err error)
 	}
 
 	if ep.K8sPodName != "" && ep.K8sNamespace != "" && k8s.IsEnabled() {
-		p, err := k8s.Client().CoreV1().Pods(ep.K8sNamespace).Get(ep.K8sPodName, meta_v1.GetOptions{})
-		if err != nil && k8serrors.IsNotFound(err) {
-			return false, fmt.Errorf("kubernetes pod not found")
-		}
-		if err == nil {
-			// We were able to get the pod. Given that we get the entire pod
-			// object, including annotations in the above API call, update
-			// the Endpoint's visibility policy accordingly so we don't have
-			// to make another API call later.
+		go func() {
+			err := ep.WaitUntilExposed(context.Background())
+			if err != nil {
+				// If an error has happen it's because the endpoint is not alive
+				return
+			}
+			_, err = d.k8sWatcher.GetCachedPod(ep.K8sNamespace, ep.K8sPodName)
+			if err != nil && k8serrors.IsNotFound(err) {
+				scopedLog := log.WithFields(logrus.Fields{
+					logfields.EndpointID:   ep.ID,
+					logfields.K8sNamespace: ep.K8sNamespace,
+					logfields.K8sPodName:   ep.K8sPodName,
+				})
+				scopedLog.Warningf("kubernetes pod not found, deleting endpoint from endpoint manager")
+				errs := d.deleteEndpointQuiet(ep, endpoint.DeleteConfig{
+					// If the IP is managed by an external IPAM, it does not need to be released
+					NoIPRelease: ep.DatapathConfiguration.ExternalIpam,
+				})
+				for _, err := range errs {
+					scopedLog.WithError(err).Warningf("Error deleting endpoint")
+				}
+				return
+			}
+
 			ep.UpdateVisibilityPolicy(func(ns, podName string) (proxyVisibility string, err error) {
-				return p.Annotations[annotation.ProxyVisibility], nil
+				_, _, annotations, err := d.fetchK8sLabelsAndAnnotations(ns, podName)
+				if err != nil {
+					return "", err
+				}
+				return annotations[annotation.ProxyVisibility], nil
 			})
-		}
+		}()
 	}
 
 	if err := ep.ValidateConnectorPlumbing(connector.CheckLink); err != nil {

pkg/endpoint/endpoint.go
@@ -1487,7 +1487,7 @@ func (e *Endpoint) getIDandLabels() string {
 
 // MetadataResolverCB provides an implementation for resolving the endpoint
 // metadata for an endpoint such as the associated labels and annotations.
-type MetadataResolverCB func(*Endpoint) (identityLabels labels.Labels, infoLabels labels.Labels, annotations map[string]string, err error)
+type MetadataResolverCB func(ns, podName string) (identityLabels labels.Labels, infoLabels labels.Labels, annotations map[string]string, err error)
 
 // RunMetadataResolver starts a controller associated with the received
 // endpoint which will periodically attempt to resolve the metadata for the
@@ -1508,13 +1508,14 @@ func (e *Endpoint) RunMetadataResolver(resolveMetadata MetadataResolverCB) {
 	e.controllers.UpdateController(controllerName,
 		controller.ControllerParams{
 			DoFunc: func(ctx context.Context) error {
-				identityLabels, info, _, err := resolveMetadata(e)
+				ns, podName := e.GetK8sNamespace(), e.GetK8sPodName()
+				identityLabels, info, _, err := resolveMetadata(ns, podName)
 				if err != nil {
 					e.Logger(controllerName).WithError(err).Warning("Unable to fetch kubernetes labels")
 					return err
 				}
 				e.UpdateVisibilityPolicy(func(_, _ string) (proxyVisibility string, err error) {
-					_, _, annotations, err := resolveMetadata(e)
+					_, _, annotations, err := resolveMetadata(ns, podName)
 					if err != nil {
 						return "", err
 					}

commit fac0b0cedccafa10596f00fcfc6775aabff1c9db

pkg/k8s: use copy of objectmeta when fetching from local stores
This commit fixes a bug introduced by b18070939608 where we were
accidentally writing into the object metadata of the local pod store,
which should never happen. Doing that could cause k8s update pod
events to give to the Update function handlers an old pod structure with
such fields modified.

Fixes: b18070939608 ("pkg/endpoint: fetch pod and namespace labels from local stores")

Reported-by: Deepesh Pathak <deepshpathak@gmail.com>
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/labels.go
@@ -103,8 +103,9 @@ func GetPodMetadata(k8sNs *slim_corev1.Namespace, pod *slim_corev1.Pod) (contain
 	})
 	scopedLog.Debug("Connecting to k8s local stores to retrieve labels for pod")
 
-	annotations := pod.Annotations
-	k8sLabels := pod.Labels
+	objMetaCpy := pod.ObjectMeta.DeepCopy()
+	annotations := objMetaCpy.Annotations
+	k8sLabels := objMetaCpy.Labels
 	if k8sLabels == nil {
 		k8sLabels = map[string]string{}
 	}