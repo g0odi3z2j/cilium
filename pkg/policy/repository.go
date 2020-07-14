g0odi3z2j/cilium

commit 6eadc30e92dddc2f7239e9e9e92c667108d13f09

policy: Fix enforcement status for host endpoint
Before this commit, host policy enforcement was reported as enabled as
soon as policies were loaded for the host, even if the host firewall was
disabled:

    ENDPOINT   POLICY (ingress)   POLICY (egress)   IDENTITY   LABELS (source:key[=value])                       IPv6                 IPv4          STATUS
               ENFORCEMENT        ENFORCEMENT
    318        Enabled            Enabled           1          reserved:host                                                                        ready
    3423       Disabled           Disabled          4          reserved:health                                   f00d::a0f:0:0:7ba4   10.16.0.148   ready

With this commit, enforcement will remain as disabled as long as the
host firewall is disabled:

    ENDPOINT   POLICY (ingress)   POLICY (egress)   IDENTITY   LABELS (source:key[=value])                       IPv6                 IPv4          STATUS
               ENFORCEMENT        ENFORCEMENT
    318        Disabled           Disabled          1          reserved:host                                                                        ready
    3423       Disabled           Disabled          4          reserved:health                                   f00d::a0f:0:0:7ba4   10.16.0.148   ready

Fixes: f069507 ("pkg/policy: Host network policies")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/policy/repository.go
@@ -703,6 +703,10 @@ func (p *Repository) computePolicyEnforcementAndRules(securityIdentity *identity
 		// enabled for the endpoint.
 		return true, true, matchingRules
 	case option.DefaultEnforcement:
+		if lbls.Has(labels.IDNameHost) && !option.Config.EnableHostFirewall {
+			return false, false, nil
+		}
+
 		ingress, egress, matchingRules = p.getMatchingRules(securityIdentity)
 		// If the endpoint has the reserved:init label, i.e. if it has not yet
 		// received any labels, always enforce policy (default deny).

commit e71394dfe9f455411d375cd6b4a0eb0389bd9e6b

policy: Fix enforcement status of host when PolicyEnforcement=always
6eadc30 updated computePolicyEnforcementAndRules() to display the host
endpoint has having no policies enforced as long as the host firewall is
disabled. It however did so only when PolicyEnforcement=default. This
commit fixes it to mark the host policies are not enforced regardless of
PolicyEnforcement's value.

Fixes: 6eadc30 ("policy: Fix enforcement status for host endpoint")
Fixes: f069507 ("pkg/policy: Host network policies")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/policy/repository.go
@@ -693,9 +693,12 @@ func (p *Repository) resolvePolicyLocked(securityIdentity *identity.Identity) (*
 //
 // Must be called with repo mutex held for reading.
 func (p *Repository) computePolicyEnforcementAndRules(securityIdentity *identity.Identity) (ingress bool, egress bool, matchingRules ruleSlice) {
-
 	lbls := securityIdentity.LabelArray
+
 	// Check if policy enforcement should be enabled at the daemon level.
+	if lbls.Has(labels.IDNameHost) && !option.Config.EnableHostFirewall {
+		return false, false, nil
+	}
 	switch GetPolicyEnabled() {
 	case option.AlwaysEnforce:
 		_, _, matchingRules = p.getMatchingRules(securityIdentity)
@@ -703,10 +706,6 @@ func (p *Repository) computePolicyEnforcementAndRules(securityIdentity *identity
 		// enabled for the endpoint.
 		return true, true, matchingRules
 	case option.DefaultEnforcement:
-		if lbls.Has(labels.IDNameHost) && !option.Config.EnableHostFirewall {
-			return false, false, nil
-		}
-
 		ingress, egress, matchingRules = p.getMatchingRules(securityIdentity)
 		// If the endpoint has the reserved:init label, i.e. if it has not yet
 		// received any labels, always enforce policy (default deny).