g0odi3z2j/cilium

commit d85873495e34cc08278b18f1a21494831f1fef5f

clustermesh: Use remote kvstore backend
Clustermesh remote-cluster watching accidentally used the local caching
identity allocator in cilium-agent. This meant that while ipcache and
nodes were sycned correctly, identities were not. This was because the
watch was called on the main identity allocator, adding itself to itself
as a remote cluster. This resulted in two connections to the local etcd,
and double entries when listing identities.

A new allocator instance is created to use the remote cluster etcd
backend. This is then added to the main allocators remote clusters list.

Signed-off-by: Ray Bejjani <ray@isovalent.com>

pkg/clustermesh/remote_cluster.go
@@ -194,10 +194,15 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 				}
 				rc.swg.Stop()
 
+				remoteIdentityCache, err := allocator.WatchRemoteIdentities(backend)
+				if err != nil {
+					remoteNodes.Close(context.TODO())
+					backend.Close()
+					return err
+				}
+
 				ipCacheWatcher := ipcache.NewIPIdentityWatcher(backend)
 				go ipCacheWatcher.Watch(ctx)
-
-				remoteIdentityCache := allocator.WatchRemoteIdentities(backend)
 
 				rc.mutex.Lock()
 				rc.remoteNodes = remoteNodes

pkg/allocator/allocator.go
@@ -305,7 +305,7 @@ func NewAllocator(typ AllocatorKey, backend Backend, opts ...AllocatorOption) (*
 
 	a.idPool = idpool.NewIDPool(a.min, a.max)
 
-	a.initialListDone = a.mainCache.start(a)
+	a.initialListDone = a.mainCache.start()
 	if !a.disableGC {
 		go func() {
 			select {
@@ -786,17 +786,17 @@ type RemoteCache struct {
 // kvstore will be maintained in the RemoteCache structure returned and will
 // start being reported in the identities returned by the ForeachCache()
 // function.
-func (a *Allocator) WatchRemoteKVStore(backend kvstore.BackendOperations, prefix string) *RemoteCache {
+func (a *Allocator) WatchRemoteKVStore(remoteAlloc *Allocator) *RemoteCache {
 	rc := &RemoteCache{
-		cache:     newCache(a),
-		allocator: a,
+		cache:     newCache(remoteAlloc),
+		allocator: remoteAlloc,
 	}
 
 	a.remoteCachesMutex.Lock()
 	a.remoteCaches[rc] = struct{}{}
 	a.remoteCachesMutex.Unlock()
 
-	rc.cache.start(a)
+	rc.cache.start()
 
 	return rc
 }

pkg/allocator/cache.go
@@ -176,7 +176,7 @@ func (c *cache) OnDelete(id idpool.ID, key AllocatorKey) {
 
 // start requests a LIST operation from the kvstore and starts watching the
 // prefix in a go subroutine.
-func (c *cache) start(a *Allocator) waitChan {
+func (c *cache) start() waitChan {
 	c.listDone = make(waitChan)
 
 	c.mutex.Lock()

pkg/clustermesh/clustermesh.go
@@ -70,7 +70,7 @@ type Configuration struct {
 type RemoteIdentityWatcher interface {
 	// WatchRemoteIdentities starts watching for identities in another kvstore and
 	// syncs all identities to the local identity cache.
-	WatchRemoteIdentities(backend kvstore.BackendOperations) *allocator.RemoteCache
+	WatchRemoteIdentities(backend kvstore.BackendOperations) (*allocator.RemoteCache, error)
 
 	// Close stops the watcher.
 	Close()

pkg/identity/cache/allocator.go
@@ -435,7 +435,18 @@ func (m *CachingIdentityAllocator) ReleaseSlice(ctx context.Context, owner Ident
 
 // WatchRemoteIdentities starts watching for identities in another kvstore and
 // syncs all identities to the local identity cache.
-func (m *CachingIdentityAllocator) WatchRemoteIdentities(backend kvstore.BackendOperations) *allocator.RemoteCache {
+func (m *CachingIdentityAllocator) WatchRemoteIdentities(backend kvstore.BackendOperations) (*allocator.RemoteCache, error) {
 	<-m.globalIdentityAllocatorInitialized
-	return m.IdentityAllocator.WatchRemoteKVStore(backend, m.identitiesPath)
+
+	remoteAllocatorBackend, err := kvstoreallocator.NewKVStoreBackend(m.identitiesPath, m.owner.GetNodeSuffix(), GlobalIdentity{}, backend)
+	if err != nil {
+		return nil, fmt.Errorf("Error setting up remote allocator backend: %s", err)
+	}
+
+	remoteAlloc, err := allocator.NewAllocator(GlobalIdentity{}, remoteAllocatorBackend)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to initialize remote Identity Allocator: %s", err)
+	}
+
+	return m.IdentityAllocator.WatchRemoteKVStore(remoteAlloc), nil
 }

pkg/kvstore/allocator/allocator_test.go
@@ -519,7 +519,11 @@ func (s *AllocatorSuite) TestRemoteCache(c *C) {
 	}
 
 	// watch the prefix in the same kvstore via a 2nd watcher
-	rc := a.WatchRemoteKVStore(kvstore.Client(), testName)
+	backend2, err := NewKVStoreBackend(testName, "a", TestAllocatorKey(""), kvstore.Client())
+	c.Assert(err, IsNil)
+	a2, err := allocator.NewAllocator(TestAllocatorKey(""), backend2, allocator.WithMax(idpool.ID(256)))
+	c.Assert(err, IsNil)
+	rc := a.WatchRemoteKVStore(a2)
 	c.Assert(rc, Not(IsNil))
 
 	// wait for remote cache to be populated

commit f4c2d7803dd158edc68159189cd341a3d628d6da

clustermesh: Tidy up services connection on failure
In the case where there is a failure to watch remote identities from the
peer cluster, we would not close out the connection to the services
store properly. Fix it up.

The context usage in this code should also be investigated more closely
but we can fix that up in a subsequent commit.

Fixes: d85873495e34 ("clustermesh: Use remote kvstore backend")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/clustermesh/remote_cluster.go
@@ -196,6 +196,7 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 
 				remoteIdentityCache, err := allocator.WatchRemoteIdentities(backend)
 				if err != nil {
+					remoteServices.Close(context.TODO())
 					remoteNodes.Close(context.TODO())
 					backend.Close()
 					return err