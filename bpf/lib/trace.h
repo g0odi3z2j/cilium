g0odi3z2j/cilium

commit 9c6f420709ff6e776e1023de4f17104bfed60218

bpf: Add MonitorAggregation option
Add the first level of monitor aggregation, which reduces the tracing of
packets to once per packet by deferring trace notifications until the
packet is transmitted from Cilium to something outside of Cilium. In
other words, the trace points for when a packet is initially received
from Cilium are disabled.

Signed-off-by: Joe Stringer <joe@covalent.io>

bpf/lib/trace.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016-2017 Authors of Cilium
+ *  Copyright (C) 2016-2018 Authors of Cilium
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -55,6 +55,16 @@ enum {
 	TRACE_REASON_CT_RELATED = CT_RELATED,
 };
 
+/* Trace aggregation levels. */
+enum {
+	TRACE_AGGREGATE_NONE = 0,      /* Trace every packet on rx & tx */
+	TRACE_AGGREGATE_RX = 1,        /* Hide trace on packet receive */
+};
+
+#ifndef MONITOR_AGGREGATION
+#define MONITOR_AGGREGATION TRACE_AGGREGATE_NONE
+#endif
+
 #ifdef TRACE_NOTIFY
 
 struct trace_notify {
@@ -84,23 +94,6 @@ struct trace_notify {
 static inline void send_trace_notify(struct __sk_buff *skb, __u8 obs_point, __u32 src, __u32 dst,
 				     __u16 dst_id, __u32 ifindex, __u8 reason)
 {
-	uint64_t skb_len = (uint64_t)skb->len, cap_len = min((uint64_t)TRACE_PAYLOAD_LEN, (uint64_t)skb_len);
-	uint32_t hash = get_hash_recalc(skb);
-	struct trace_notify msg = {
-		.type = CILIUM_NOTIFY_TRACE,
-		.subtype = obs_point,
-		.source = EVENT_SOURCE,
-		.hash = hash,
-		.len_orig = skb_len,
-		.len_cap = cap_len,
-		.src_label = src,
-		.dst_label = dst,
-		.dst_id = dst_id,
-		.reason = reason,
-		.pad = 0,
-		.ifindex = ifindex,
-	};
-
 	switch (obs_point) {
 		case TRACE_TO_LXC:
 			update_metrics(skb->len, METRIC_INGRESS, REASON_FORWARDED);
@@ -120,6 +113,35 @@ static inline void send_trace_notify(struct __sk_buff *skb, __u8 obs_point, __u3
 		case TRACE_TO_OVERLAY:
 			update_metrics(skb->len, METRIC_EGRESS, REASON_FORWARDED);
 	}
+	if (MONITOR_AGGREGATION >= TRACE_AGGREGATE_RX) {
+		switch (obs_point) {
+		case TRACE_FROM_LXC:
+		case TRACE_FROM_PROXY:
+		case TRACE_FROM_HOST:
+		case TRACE_FROM_STACK:
+		case TRACE_FROM_OVERLAY:
+			return;
+		default:
+			break;
+		}
+	}
+
+	uint64_t skb_len = (uint64_t)skb->len, cap_len = min((uint64_t)TRACE_PAYLOAD_LEN, (uint64_t)skb_len);
+	uint32_t hash = get_hash_recalc(skb);
+	struct trace_notify msg = {
+		.type = CILIUM_NOTIFY_TRACE,
+		.subtype = obs_point,
+		.source = EVENT_SOURCE,
+		.hash = hash,
+		.len_orig = skb_len,
+		.len_cap = cap_len,
+		.src_label = src,
+		.dst_label = dst,
+		.dst_id = dst_id,
+		.reason = reason,
+		.pad = 0,
+		.ifindex = ifindex,
+	};
 	skb_event_output(skb, &cilium_events,
 			 (cap_len << 32) | BPF_F_CURRENT_CPU,
 			 &msg, sizeof(msg));

bpf/node_config.h
@@ -63,3 +63,4 @@
 #ifndef SKIP_DEBUG
 #define LB_DEBUG
 #endif
+#define MONITOR_AGGREGATION 5

cilium/cmd/config.go
@@ -107,10 +107,14 @@ func configDaemon(cmd *cobra.Command, opts []string) {
 			os.Exit(1)
 		}
 
-		if value == option.OptionDisabled {
-			dOpts[name] = "Disabled"
+		if opt, ok := option.DaemonMutableOptionLibrary[name]; !ok || opt.Parse == nil {
+			if value == option.OptionDisabled {
+				dOpts[name] = "Disabled"
+			} else {
+				dOpts[name] = "Enabled"
+			}
 		} else {
-			dOpts[name] = "Enabled"
+			dOpts[name] = optionSplit[1]
 		}
 	}
 

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=ccbeb6cfbc24a55d1f65d9b8944926974e41d48a
+GO_BINDATA_SHA1SUM=587e52e31bc83c2392decf148899363ff062c69c
 BPF_FILES=../bpf/.gitignore ../bpf/COPYING ../bpf/Makefile ../bpf/bpf_features.h ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_overlay.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/join_ep.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/conntrack.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/run_probes.sh ../bpf/spawn_netns.sh 

daemon/main.go
@@ -103,6 +103,7 @@ var (
 	logstashAddr          string
 	logstashProbeTimer    uint32
 	masquerade            bool
+	monitorAggregation    string
 	nat46prefix           string
 	prometheusServeAddr   string
 	socketPath            string
@@ -424,6 +425,9 @@ func init() {
 		"nat46-range", node.DefaultNAT46Prefix, "IPv6 prefix to map IPv4 addresses to")
 	flags.BoolVar(&masquerade,
 		"masquerade", true, "Masquerade packets from endpoints leaving the host")
+	flags.StringVar(&monitorAggregation, option.MonitorAggregationName,
+		fmt.Sprintf("None"), "Level of monitor aggregation for traces from the datapath")
+	viper.BindEnv(option.MonitorAggregationName, "MONITOR_AGGREGATION_LEVEL")
 	flags.IntVar(&option.Config.MTU,
 		option.MTUName, mtu.AutoDetect(), "Overwrite auto-detected MTU of underlying network")
 	flags.StringVar(&v6Address,
@@ -663,6 +667,13 @@ func initEnv(cmd *cobra.Command) {
 	option.Config.Opts.SetBool(option.Conntrack, !disableConntrack)
 	option.Config.Opts.SetBool(option.ConntrackAccounting, !disableConntrack)
 	option.Config.Opts.SetBool(option.ConntrackLocal, false)
+
+	monitorAggregationLevel, err := option.ParseMonitorAggregationLevel(monitorAggregation)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to parse %s: %s",
+			option.MonitorAggregationName, err)
+	}
+	option.Config.Opts.SetValidated(option.MonitorAggregation, monitorAggregationLevel)
 
 	policy.SetPolicyEnabled(strings.ToLower(viper.GetString("enable-policy")))
 

pkg/option/config.go
@@ -77,6 +77,10 @@ const (
 	// a separate route for each cluster node CIDR. This option is not
 	// compatible with Tunnel=TunnelDisabled
 	SingleClusterRouteName = "single-cluster-route"
+
+	// MonitorAggregationName specifies the MonitorAggregationLevel on the
+	// comandline.
+	MonitorAggregationName = "monitor-aggregation"
 )
 
 // Available option for daemonConfig.Tunnel

pkg/option/daemon.go
@@ -31,6 +31,7 @@ var (
 		DebugLB:             &specDebugLB,
 		DropNotify:          &specDropNotify,
 		TraceNotify:         &specTraceNotify,
+		MonitorAggregation:  &specMonitorAggregation,
 		NAT46:               &specNAT46,
 	}
 )

pkg/option/endpoint.go
@@ -23,6 +23,7 @@ var (
 		DebugLB:             &specDebugLB,
 		DropNotify:          &specDropNotify,
 		TraceNotify:         &specTraceNotify,
+		MonitorAggregation:  &specMonitorAggregation,
 		NAT46:               &specNAT46,
 		IngressPolicy:       &IngressSpecPolicy,
 		EgressPolicy:        &EgressSpecPolicy,

commit b9afc0a234e7e27adbc45bc22da5265c7e75e6f6

bpf: Fix monitor aggregation for 'from-network'
Previously, we did not take into account 'from-network' sources in the
monitor aggregation logic check in `send_trace_notify()`, which was fine
because we rarely ever sent such events (limited to ipsec for instance).
However, since commit 018723961211 we also use this in bpf_host which
suddenly means that any and all traffic from the network will trigger
monitor events, flooding the monitor output.

Fixes: 9c6f420709ff ("bpf: Add MonitorAggregation option")
Fixes: 018723961211 ("Adds TRACE_TO_NETWORK obs label and trace pkts in to-netdev prog.")
Fixes: https://github.com/cilium/cilium/issues/12555

Signed-off-by: Joe Stringer <joe@cilium.io>

bpf/lib/trace.h
@@ -133,6 +133,7 @@ static __always_inline bool emit_trace_notify(__u8 obs_point, __u32 monitor)
 		case TRACE_FROM_HOST:
 		case TRACE_FROM_STACK:
 		case TRACE_FROM_OVERLAY:
+		case TRACE_FROM_NETWORK:
 			return false;
 		default:
 			break;