g0odi3z2j/cilium

commit 06f3b74425c1cb6252002ae5ddcf7391f7cc7f86

k8s,node: Reuse retrieveNodeInformation to retrieve node labels
When creating the host endpoint, we need to retrieve existing node
labels. Instead of making a new API call, we can reuse
retrieveNodeInformation() to get those labels.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/k8s/init.go
@@ -226,6 +226,8 @@ func GetNodeSpec(nodeName string) error {
 		if nodeIP6 != nil {
 			node.SetIPv6(nodeIP6)
 		}
+
+		node.SetLabels(n.Labels)
 	} else {
 		// if node resource could not be received, fail if
 		// PodCIDR requirement has been requested

pkg/node/types/node.go
@@ -166,6 +166,9 @@ type Node struct {
 
 	// Key index used for transparent encryption or 0 for no encryption
 	EncryptionKey uint8
+
+	// Node labels
+	Labels map[string]string
 }
 
 // Fullname returns the node's full name including the cluster name if a

pkg/k8s/node.go
@@ -204,6 +204,8 @@ func ParseNode(k8sNode *slim_corev1.Node, source source.Source) *nodeTypes.Node 
 		}
 	}
 
+	newNode.Labels = k8sNode.GetLabels()
+
 	return newNode
 }
 

pkg/endpointmanager/manager.go
@@ -27,7 +27,6 @@ import (
 	"github.com/cilium/cilium/pkg/endpoint/regeneration"
 	"github.com/cilium/cilium/pkg/endpointmanager/idallocator"
 	"github.com/cilium/cilium/pkg/identity/cache"
-	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/labelsfilter"
 	"github.com/cilium/cilium/pkg/lock"
@@ -35,6 +34,7 @@ import (
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/policy"
 	"github.com/prometheus/client_golang/prometheus"
@@ -479,16 +479,10 @@ func (mgr *EndpointManager) AddHostEndpoint(ctx context.Context, owner regenerat
 	epLabels := labels.Labels{}
 	epLabels.MergeLabels(labels.LabelHost)
 
-	if k8s.IsEnabled() {
-		// Retrieve k8s labels.
-		if k8sNode, err := k8s.GetNode(k8s.Client(), nodeName); err != nil {
-			log.WithError(err).Warning("Kubernetes node resource representing own node is not available, cannot set Labels")
-		} else {
-			newLabels := labels.Map2Labels(k8sNode.GetLabels(), labels.LabelSourceK8s)
-			newIdtyLabels, _ := labelsfilter.Filter(newLabels)
-			epLabels.MergeLabels(newIdtyLabels)
-		}
-	}
+	// Initialize with known node labels.
+	newLabels := labels.Map2Labels(node.GetLabels(), labels.LabelSourceK8s)
+	newIdtyLabels, _ := labelsfilter.Filter(newLabels)
+	epLabels.MergeLabels(newIdtyLabels)
 
 	// Give the endpoint a security identity
 	newCtx, cancel := context.WithTimeout(ctx, launchTime)

pkg/k8s/node_test.go
@@ -38,6 +38,9 @@ func (s *K8sSuite) TestParseNode(c *C) {
 				annotation.V4CIDRName: "10.254.0.0/16",
 				annotation.V6CIDRName: "f00d:aaaa:bbbb:cccc:dddd:eeee::/112",
 			},
+			Labels: map[string]string{
+				"type": "m5.xlarge",
+			},
 		},
 		Spec: slim_corev1.NodeSpec{
 			PodCIDR: "10.1.0.0/16",
@@ -50,6 +53,7 @@ func (s *K8sSuite) TestParseNode(c *C) {
 	c.Assert(n.IPv4AllocCIDR.String(), Equals, "10.1.0.0/16")
 	c.Assert(n.IPv6AllocCIDR, NotNil)
 	c.Assert(n.IPv6AllocCIDR.String(), Equals, "f00d:aaaa:bbbb:cccc:dddd:eeee::/112")
+	c.Assert(n.Labels["type"], Equals, "m5.xlarge")
 
 	// No IPv6 annotation
 	k8sNode = &slim_corev1.Node{

pkg/node/labels.go
@@ -1,0 +1,29 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package node
+
+var (
+	labels map[string]string
+)
+
+// GetLabels returns the labels of this node.
+func GetLabels() map[string]string {
+	return labels
+}
+
+// SetLabels sets the labels of this node.
+func SetLabels(l map[string]string) {
+	labels = l
+}

pkg/node/types/zz_generated.deepcopy.go
@@ -71,6 +71,13 @@ func (in *Node) DeepCopyInto(out *Node) {
 		*out = make(net.IP, len(*in))
 		copy(*out, *in)
 	}
+	if in.Labels != nil {
+		in, out := &in.Labels, &out.Labels
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
 	return
 }
 

commit 11d2ccaaeb0c2b02d004ec83802e4c559db0ee7b

k8s,node: Fix init of labels with Operator IPAM
When using the Operator IPAM, the node labels are not properly
initialized simply because the labels are not copied from K8s nodes to
CiliumNode objects and vice-versa. This commit fixes it.

Fixes: 06f3b74 ("k8s,node: Reuse retrieveNodeInformation to retrieve node labels")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/k8s/init.go
@@ -215,6 +215,7 @@ func GetNodeSpec() error {
 
 		log.WithFields(logrus.Fields{
 			logfields.NodeName:         n.Name,
+			logfields.Labels:           logfields.Repr(n.Labels),
 			logfields.IPAddr + ".ipv4": nodeIP4,
 			logfields.IPAddr + ".ipv6": nodeIP6,
 			logfields.V4Prefix:         n.IPv4AllocCIDR,

pkg/node/types/node.go
@@ -52,6 +52,7 @@ func ParseCiliumNode(n *ciliumv2.CiliumNode) (node Node) {
 		Cluster:       option.Config.ClusterName,
 		ClusterID:     option.Config.ClusterID,
 		Source:        source.CustomResource,
+		Labels:        n.ObjectMeta.Labels,
 	}
 
 	for _, cidrString := range n.Spec.IPAM.PodCIDRs {
@@ -112,7 +113,8 @@ func (n *Node) ToCiliumNode() *ciliumv2.CiliumNode {
 
 	return &ciliumv2.CiliumNode{
 		ObjectMeta: v1.ObjectMeta{
-			Name: n.Name,
+			Name:   n.Name,
+			Labels: n.Labels,
 		},
 		Spec: ciliumv2.NodeSpec{
 			Addresses: ipAddrs,

pkg/nodediscovery/nodediscovery.go
@@ -132,6 +132,7 @@ func (n *NodeDiscovery) StartDiscovery(nodeName string) {
 	n.LocalNode.IPv6AllocCIDR = node.GetIPv6AllocRange()
 	n.LocalNode.ClusterID = option.Config.ClusterID
 	n.LocalNode.EncryptionKey = node.GetIPsecKeyIdentity()
+	n.LocalNode.Labels = node.GetLabels()
 
 	if node.GetExternalIPv4() != nil {
 		n.LocalNode.IPAddresses = append(n.LocalNode.IPAddresses, nodeTypes.Address{
@@ -291,6 +292,8 @@ func (n *NodeDiscovery) mutateNodeResource(nodeResource *ciliumv2.CiliumNode) {
 		typesNode := nodeInterface.(*k8sTypes.Node)
 		k8sNodeParsed := k8s.ParseNode(typesNode, source.Unspec)
 		k8sNodeAddresses = k8sNodeParsed.IPAddresses
+
+		nodeResource.ObjectMeta.Labels = k8sNodeParsed.Labels
 
 		for _, k8sAddress := range k8sNodeAddresses {
 			k8sAddressStr := k8sAddress.IP.String()