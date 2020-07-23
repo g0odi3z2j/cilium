g0odi3z2j/cilium

commit 98ec0bb084cb49bac8c683bad7c0dcd44b2c3453

kvstore: Fix Watch() to return when client is closed or context is cancelled
Connected() is being used for two use cases:

1. To wait for a particular client to be connected and reach quorum.
2. To wait for the overall agent to connect to a kvstore and reach quorum.

The first use case is tied to a particular kvstore client. The second use case
is not.

The current implementation will block Connected() forever until
isConnectedAndHasQuorum() returns success. If the context is cancelled or if
the client is closed, this never happens as an error will always be returned.

Change etcd's Connected() implementation to check the context and the etcd
client context and return an error on the channel. This allows Watch() to
return if this condition is met to properly return the Watch() call which is
made against a particular client. This fixes a Watch() call never returning in
case the etcd client is closed before connectivity was ever achieved.

To account for the second use case, introduce an overall Connected() function
which will block closing the channel until kvstore connectivity has been
achieved.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/k8s/cnp.go
@@ -350,10 +350,8 @@ func (c *CNPStatusUpdateContext) updateViaKVStore(ctx context.Context, cnp *type
 		annotations map[string]string
 	)
 
-	select {
-	case <-kvstore.Client().Connected(ctx):
-	case <-ctx.Done():
-		return ctx.Err()
+	if err := <-kvstore.Client().Connected(ctx); err != nil {
+		return nil
 	}
 
 	capabilities := k8sversion.Capabilities()

pkg/k8s/watchers/cilium_endpoint.go
@@ -15,7 +15,6 @@
 package watchers
 
 import (
-	"context"
 	"net"
 	"sync"
 
@@ -97,7 +96,7 @@ func (k *K8sWatcher) ciliumEndpointsInit(ciliumNPClient *k8s.K8sCiliumClient, as
 		k.k8sAPIGroups.addAPI(k8sAPIGroupCiliumEndpointV2)
 		go ciliumEndpointInformer.Run(isConnected)
 
-		<-kvstore.Client().Connected(context.TODO())
+		<-kvstore.Connected()
 		close(isConnected)
 
 		log.Info("Connected to key-value store, stopping CiliumEndpoint watcher")

pkg/k8s/watchers/cilium_node.go
@@ -15,7 +15,6 @@
 package watchers
 
 import (
-	"context"
 	"sync"
 
 	"github.com/cilium/cilium/pkg/k8s"
@@ -102,7 +101,7 @@ func (k *K8sWatcher) ciliumNodeInit(ciliumNPClient *k8s.K8sCiliumClient, asyncCo
 		k.k8sAPIGroups.addAPI(k8sAPIGroupCiliumNodeV2)
 		go ciliumNodeInformer.Run(isConnected)
 
-		<-kvstore.Client().Connected(context.TODO())
+		<-kvstore.Connected()
 		close(isConnected)
 
 		log.Info("Connected to key-value store, stopping CiliumNode watcher")

pkg/k8s/watchers/pod.go
@@ -15,7 +15,6 @@
 package watchers
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"net"
@@ -164,8 +163,7 @@ func (k *K8sWatcher) podsInit(k8sClient kubernetes.Interface, asyncControllers *
 
 		// Replace pod controller by only receiving events from our own
 		// node once we are connected to the kvstore.
-
-		<-kvstore.Client().Connected(context.TODO())
+		<-kvstore.Connected()
 		close(isConnected)
 
 		log.WithField(logfields.Node, nodeTypes.GetName()).Info("Connected to KVStore, watching for pod events on node")

pkg/kvstore/backend.go
@@ -130,8 +130,8 @@ func getBackend(name string) backendModule {
 // tracing layer.
 type BackendOperations interface {
 	// Connected returns a channel which is closed whenever the kvstore client
-	// is connected to the kvstore server. (Only implemented for etcd)
-	Connected(ctx context.Context) <-chan struct{}
+	// is connected to the kvstore server.
+	Connected(ctx context.Context) <-chan error
 
 	// Disconnected returns a channel which is closed whenever the kvstore
 	// client is not connected to the kvstore server. (Only implemented for etcd)

pkg/kvstore/client.go
@@ -17,6 +17,7 @@ package kvstore
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 var (
@@ -84,4 +85,27 @@ func NewClient(ctx context.Context, selectedBackend string, opts map[string]stri
 	}
 
 	return module.newClient(ctx, options)
+}
+
+// Connected returns a channel which is closed when the following conditions
+// are being met at the same time:
+// * The kvstore client is configured
+// * Connectivity to the kvstore has been established
+// * The kvstore has quorum
+//
+// The channel will *not* be closed if the kvstore client is closed before
+// connectivity or quorum has been achieved. It will wait until a new kvstore
+// client is configured to again wait for connectivity and quorum.
+func Connected() <-chan struct{} {
+	c := make(chan struct{})
+	go func(c chan struct{}) {
+		for {
+			if err := <-Client().Connected(context.Background()); err == nil {
+				close(c)
+				return
+			}
+			time.Sleep(100 * time.Millisecond)
+		}
+	}(c)
+	return c
 }

pkg/kvstore/consul.go
@@ -421,8 +421,8 @@ func (c *consulClient) waitForInitLock(ctx context.Context) <-chan struct{} {
 }
 
 // Connected closes the returned channel when the consul client is connected.
-func (c *consulClient) Connected(ctx context.Context) <-chan struct{} {
-	ch := make(chan struct{})
+func (c *consulClient) Connected(ctx context.Context) <-chan error {
+	ch := make(chan error)
 	go func() {
 		for {
 			qo := &consulAPI.QueryOptions{}

pkg/kvstore/etcd.go
@@ -451,14 +451,28 @@ func (e *etcdClient) isConnectedAndHasQuorum(ctx context.Context) error {
 	}
 }
 
-// Connected closes the returned channel when the etcd client is connected.
-func (e *etcdClient) Connected(ctx context.Context) <-chan struct{} {
-	out := make(chan struct{})
+// Connected closes the returned channel when the etcd client is connected. If
+// the context is cancelled or if the etcd client is closed, an error is
+// returned on the channel.
+func (e *etcdClient) Connected(ctx context.Context) <-chan error {
+	out := make(chan error)
 	go func() {
-		for e.isConnectedAndHasQuorum(ctx) != nil {
+		defer close(out)
+		for {
+			select {
+			case <-e.client.Ctx().Done():
+				out <- fmt.Errorf("etcd client context ended")
+				return
+			case <-ctx.Done():
+				out <- ctx.Err()
+				return
+			default:
+			}
+			if e.isConnectedAndHasQuorum(ctx) == nil {
+				return
+			}
 			time.Sleep(100 * time.Millisecond)
 		}
-		close(out)
 	}()
 	return out
 }
@@ -791,7 +805,13 @@ func (e *etcdClient) Watch(ctx context.Context, w *Watcher) {
 		fieldWatcher: w,
 		fieldPrefix:  w.prefix,
 	})
-	<-e.Connected(ctx)
+
+	err := <-e.Connected(ctx)
+	if err != nil {
+		// The context ended or the etcd client was closed
+		// before connectivity was achieved
+		return
+	}
 
 reList:
 	for {

commit cce1ab8cda0dfa5e2d558a6c54afa0c8cb76f247

k8s: Exit CNP status update controller if kvstore is unavailable
Commit 98ec0bb084 incorrectly changed the return value to nil instead of
returning an error.

Fixes: 98ec0bb084c ("kvstore: Fix Watch() to return when client is closed or context is cancelled")
Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/k8s/cnp.go
@@ -351,7 +351,7 @@ func (c *CNPStatusUpdateContext) updateViaKVStore(ctx context.Context, cnp *type
 	)
 
 	if err := <-kvstore.Client().Connected(ctx); err != nil {
-		return nil
+		return fmt.Errorf("kvstore is unavailable: %w", err)
 	}
 
 	capabilities := k8sversion.Capabilities()