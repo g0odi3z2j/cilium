g0odi3z2j/cilium

commit 5f9bdf46deba2c4d2e2527fddd8dc070876953d0

bpf: Add session affinity maps
This commit introduces the BPF maps used by the sessionAffinity feature:

- lb{4,6}_affinity:     used to store current affinity sessions;
                        key = svc id + client_id (either netns cookie or
                        client ip addr), val = last_used and backend_id.

- lb_affinity_match:    used to check whether a backend still belongs
                        to a svc; key = svc id + backend id; the map
                        is going to be shared between ipv4 and ipv6, as
                        svc and backend id pools are the same for ipv4
                        and ipv6.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/maps/lbmap/affinity.go
@@ -1,0 +1,183 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lbmap
+
+import (
+	"fmt"
+	"unsafe"
+
+	"github.com/cilium/cilium/common/types"
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/byteorder"
+)
+
+const (
+	AffinityMatchMapName = "cilium_lb_affinity_match"
+	Affinity4MapName     = "cilium_lb4_affinity"
+	Affinity6MapName     = "cilium_lb6_affinity"
+)
+
+var (
+	AffinityMatchMap = bpf.NewMap(
+		AffinityMatchMapName,
+		bpf.MapTypeHash,
+		&AffinityMatchKey{},
+		int(unsafe.Sizeof(AffinityMatchKey{})),
+		&AffinityMatchValue{},
+		int(unsafe.Sizeof(AffinityMatchValue{})),
+		MaxEntries,
+		0, 0,
+		func(key []byte, value []byte, mapKey bpf.MapKey, mapValue bpf.MapValue) (bpf.MapKey, bpf.MapValue, error) {
+			aKey, aVal := mapKey.(*AffinityMatchKey), mapValue.(*AffinityMatchValue)
+
+			if _, _, err := bpf.ConvertKeyValue(key, value, aKey, aVal); err != nil {
+				return nil, nil, err
+			}
+
+			return aKey.ToNetwork(), aVal, nil
+		}).WithCache()
+	Affinity4Map = bpf.NewMap(
+		Affinity4MapName,
+		bpf.MapTypeLRUHash,
+		&Affinity4Key{},
+		int(unsafe.Sizeof(Affinity4Key{})),
+		&AffinityValue{},
+		int(unsafe.Sizeof(AffinityValue{})),
+		MaxEntries,
+		0,
+		0,
+		bpf.ConvertKeyValue,
+	)
+	Affinity6Map = bpf.NewMap(
+		Affinity4MapName,
+		bpf.MapTypeLRUHash,
+		&Affinity6Key{},
+		int(unsafe.Sizeof(Affinity4Key{})),
+		&AffinityValue{},
+		int(unsafe.Sizeof(AffinityValue{})),
+		MaxEntries,
+		0,
+		0,
+		bpf.ConvertKeyValue,
+	)
+)
+
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type AffinityMatchKey struct {
+	BackendID uint32 `align:"backend_id"`
+	RevNATID  uint16 `align:"rev_nat_id"`
+	Pad       uint16 `align:"pad"`
+}
+
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapValue
+type AffinityMatchValue struct {
+	Pad uint8 `align:"pad"`
+}
+
+// NewAffinityMatchKey creates the AffinityMatch key
+func NewAffinityMatchKey(revNATID uint16, backendID uint32) *AffinityMatchKey {
+	return &AffinityMatchKey{
+		BackendID: backendID,
+		RevNATID:  revNATID,
+	}
+}
+
+// GetKeyPtr returns the unsafe pointer to the BPF key
+func (k *AffinityMatchKey) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+
+// GetValuePtr returns the unsafe pointer to the BPF value
+func (v *AffinityMatchValue) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(v) }
+
+// String converts the key into a human readable string format
+func (k *AffinityMatchKey) String() string { return fmt.Sprintf("%d %d", k.BackendID, k.RevNATID) }
+
+// String converts the value into a human readable string format
+func (v *AffinityMatchValue) String() string { return "" }
+
+// NewValue returns a new empty instance of the structure representing the BPF
+// map value
+func (k *AffinityMatchKey) NewValue() bpf.MapValue { return &AffinityMatchValue{} }
+
+// ToNetwork returns the key in the network byte order
+func (k *AffinityMatchKey) ToNetwork() *AffinityMatchKey {
+	n := *k
+	// For some reasons rev_nat_index is stored in network byte order in
+	// the SVC BPF maps
+	n.RevNATID = byteorder.HostToNetwork(n.RevNATID).(uint16)
+	return &n
+}
+
+// Affinity4Key is the Go representation of lb4_affinity_key
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type Affinity4Key struct {
+	ClientID    uint64 `align:"client_id"`
+	RevNATID    uint16 `align:"rev_nat_id"`
+	NetNSCookie uint8  `align:"netns_cookie"`
+	Pad1        uint8  `align:"pad1"`
+	Pad2        uint32 `align:"pad2"`
+}
+
+// Affinity6Key is the Go representation of lb6_affinity_key
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type Affinity6Key struct {
+	ClientID    types.IPv6 `align:"client_id"`
+	RevNATID    uint16     `align:"rev_nat_id"`
+	NetNSCookie uint8      `align:"netns_cookie"`
+	Pad1        uint8      `align:"pad1"`
+	Pad2        uint32     `align:"pad2"`
+}
+
+// AffinityValue is the Go representing of lb_affinity_value
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapValue
+type AffinityValue struct {
+	LastUsed  uint64 `align:"last_used"`
+	BackendID uint32 `align:"backend_id"`
+	Pad       uint32 `align:"pad"`
+}
+
+// GetKeyPtr returns the unsafe pointer to the BPF key
+func (k *Affinity4Key) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+
+// GetKeyPtr returns the unsafe pointer to the BPF key
+func (k *Affinity6Key) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+
+// GetValuePtr returns the unsafe pointer to the BPF value
+func (v *AffinityValue) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(v) }
+
+// String converts the key into a human readable string format.
+func (k *Affinity4Key) String() string {
+	return fmt.Sprintf("%d %d %d", k.ClientID, k.NetNSCookie, k.RevNATID)
+}
+
+// String converts the key into a human readable string format.
+func (k *Affinity6Key) String() string {
+	return fmt.Sprintf("%d %d %d", k.ClientID, k.NetNSCookie, k.RevNATID)
+}
+
+// String converts the value into a human readable string format.
+func (v *AffinityValue) String() string { return fmt.Sprintf("%d %d", v.BackendID, v.LastUsed) }
+
+// NewValue returns a new empty instance of the structure representing the BPF
+// map value.
+func (k Affinity4Key) NewValue() bpf.MapValue { return &AffinityValue{} }
+
+// NewValue returns a new empty instance of the structure representing the BPF
+// map value.
+func (k Affinity6Key) NewValue() bpf.MapValue { return &AffinityValue{} }

bpf/bpf_alignchecker.c
@@ -72,6 +72,10 @@ int main() {
     DECLARE(struct, ipv4_frag_id, iter);
     DECLARE(struct, ipv4_frag_l4ports, iter);
     DECLARE(union, macaddr, iter);
+    DECLARE(struct, lb4_affinity_key, iter);
+    DECLARE(struct, lb6_affinity_key, iter);
+    DECLARE(struct, lb_affinity_val, iter);
+    DECLARE(struct, lb_affinity_match, iter);
 
     return 0;
 }

bpf/lib/common.h
@@ -643,6 +643,46 @@ struct ipv4_revnat_entry {
 	__u16 rev_nat_index;
 };
 
+union lb4_affinity_client_id {
+	__u32 client_ip;
+	__u64 client_cookie; /* netns cookie */
+} __packed;
+
+struct lb4_affinity_key {
+	union lb4_affinity_client_id client_id;
+	__u16 rev_nat_id;
+	__u8 netns_cookie:1,
+	     reserved:7;
+	__u8 pad1;
+	__u32 pad2;
+} __packed;
+
+union lb6_affinity_client_id {
+	union v6addr client_ip;
+	__u64 client_cookie; /* netns cookie */
+} __packed;
+
+struct lb6_affinity_key {
+	union lb6_affinity_client_id client_id;
+	__u16 rev_nat_id;
+	__u8 netns_cookie:1,
+	     reserved:7;
+	__u8 pad1;
+	__u32 pad2;
+} __packed;
+
+struct lb_affinity_val {
+	__u64 last_used;
+	__u32 backend_id;
+	__u32 pad;
+} __packed;
+
+struct lb_affinity_match {
+	__u32 backend_id;
+	__u16 rev_nat_id;
+	__u16 pad;
+} __packed;
+
 struct ct_state {
 	__u16 rev_nat_index;
 	__u16 loopback:1,

bpf/lib/lb.h
@@ -47,6 +47,16 @@ struct bpf_elf_map __section_maps LB6_BACKEND_MAP = {
 	.flags          = CONDITIONAL_PREALLOC,
 };
 
+#ifdef ENABLE_SESSION_AFFINITY
+struct bpf_elf_map __section_maps LB6_AFFINITY_MAP = {
+	.type		= BPF_MAP_TYPE_LRU_HASH,
+	.size_key	= sizeof(struct lb6_affinity_key),
+	.size_value	= sizeof(struct lb_affinity_val),
+	.pinning	= PIN_GLOBAL_NS,
+	.max_elem	= CILIUM_LB_MAP_MAX_ENTRIES,
+};
+#endif
+
 #endif /* ENABLE_IPV6 */
 
 #ifdef ENABLE_IPV4
@@ -77,8 +87,28 @@ struct bpf_elf_map __section_maps LB4_BACKEND_MAP = {
 	.flags          = CONDITIONAL_PREALLOC,
 };
 
+#ifdef ENABLE_SESSION_AFFINITY
+struct bpf_elf_map __section_maps LB4_AFFINITY_MAP = {
+	.type		= BPF_MAP_TYPE_LRU_HASH,
+	.size_key	= sizeof(struct lb4_affinity_key),
+	.size_value	= sizeof(struct lb_affinity_val),
+	.pinning	= PIN_GLOBAL_NS,
+	.max_elem	= CILIUM_LB_MAP_MAX_ENTRIES,
+};
+#endif
+
 #endif /* ENABLE_IPV4 */
 
+#ifdef ENABLE_SESSION_AFFINITY
+struct bpf_elf_map __section_maps LB_AFFINITY_MATCH_MAP = {
+	.type		= BPF_MAP_TYPE_HASH,
+	.size_key	= sizeof(struct lb_affinity_match),
+	.size_value	= sizeof(__u8), /* dummy value, map is used as a set */
+	.pinning	= PIN_GLOBAL_NS,
+	.max_elem	= CILIUM_LB_MAP_MAX_ENTRIES,
+	.flags		= CONDITIONAL_PREALLOC,
+};
+#endif
 
 #define REV_NAT_F_TUPLE_SADDR 1
 #ifdef LB_DEBUG

bpf/node_config.h
@@ -91,6 +91,9 @@ DEFINE_IPV6(SNAT_IPV6_EXTERNAL, 0xbe, 0xef, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0
 #define LB4_BACKEND_MAP test_cilium_lb4_backends
 #define LB4_REVERSE_NAT_SK_MAP test_cilium_lb4_reverse_sk
 #define LB4_REVERSE_NAT_SK_MAP_SIZE 262144
+#define LB4_AFFINITY_MAP test_cilium_lb4_affinity
+#define LB6_AFFINITY_MAP test_cilium_lb6_affinity
+#define LB_AFFINITY_MATCH_MAP test_cilium_lb_affinity_match
 #define ENABLE_ARP_RESPONDER
 #define TUNNEL_ENDPOINT_MAP_SIZE 65536
 #define ENDPOINTS_MAP_SIZE 65536

bugtool/cmd/configuration.go
@@ -72,6 +72,9 @@ func defaultCommands(confDir string, cmdDir string, k8sPods []string) []string {
 		"bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb4_reverse_nat",
 		"bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_ct4_global",
 		"bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_ct_any4_global",
+		"bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb4_affinity",
+		"bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb6_affinity",
+		"bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb_affinity_match",
 		"bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb6_services_v2",
 		"bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb6_services",
 		"bpftool map dump pinned /sys/fs/bpf/tc/globals/cilium_lb6_backends",

daemon/cmd/datapath.go
@@ -36,6 +36,7 @@ import (
 	"github.com/cilium/cilium/pkg/maps/eventsmap"
 	"github.com/cilium/cilium/pkg/maps/fragmap"
 	ipcachemap "github.com/cilium/cilium/pkg/maps/ipcache"
+	"github.com/cilium/cilium/pkg/maps/lbmap"
 	"github.com/cilium/cilium/pkg/maps/lxcmap"
 	"github.com/cilium/cilium/pkg/maps/metricsmap"
 	"github.com/cilium/cilium/pkg/maps/nat"
@@ -430,6 +431,22 @@ func (d *Daemon) initMaps() error {
 		// If we are not restoring state, all endpoints can be
 		// deleted. Entries will be re-populated.
 		lxcmap.LXCMap.DeleteAll()
+	}
+
+	if option.Config.EnableSessionAffinity {
+		if _, err := lbmap.AffinityMatchMap.OpenOrCreate(); err != nil {
+			return err
+		}
+		if option.Config.EnableIPv4 {
+			if _, err := lbmap.Affinity4Map.OpenOrCreate(); err != nil {
+				return err
+			}
+		}
+		if option.Config.EnableIPv6 {
+			if _, err := lbmap.Affinity6Map.OpenOrCreate(); err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil

pkg/datapath/alignchecker/alignchecker.go
@@ -80,6 +80,10 @@ func CheckStructAlignments(path string) error {
 			reflect.TypeOf(eppolicymap.EndpointKey{}),
 			reflect.TypeOf(tunnel.TunnelEndpoint{}),
 		},
+		"lb4_affinity_key":  {reflect.TypeOf(lbmap.Affinity4Key{})},
+		"lb6_affinity_key":  {reflect.TypeOf(lbmap.Affinity6Key{})},
+		"lb_affinity_match": {reflect.TypeOf(lbmap.AffinityMatchKey{})},
+		"lb_affinity_val":   {reflect.TypeOf(lbmap.AffinityValue{})},
 	}
 	if err := check.CheckStructAlignments(path, toCheck, true); err != nil {
 		return err

commit 4243be87e97c806623c57c7df87a0f09d0b6b1cc

lbmap: Fix Affinity6Map key size and name
Previously, the affinity map for IPv6 was using the corresponding IPv4
map's name and key size. Luckily, the LB6_AFFINITY_MAP name didn't match
the name of Affinity6Map, so the former map was not re-created each time
cilium-agent was restarted. Therefore, there was no user impact.

Fixes: 5f9bdf46de ("bpf: Add session affinity maps")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/maps/lbmap/affinity.go
@@ -61,10 +61,10 @@ var (
 		bpf.ConvertKeyValue,
 	)
 	Affinity6Map = bpf.NewMap(
-		Affinity4MapName,
+		Affinity6MapName,
 		bpf.MapTypeLRUHash,
 		&Affinity6Key{},
-		int(unsafe.Sizeof(Affinity4Key{})),
+		int(unsafe.Sizeof(Affinity6Key{})),
 		&AffinityValue{},
 		int(unsafe.Sizeof(AffinityValue{})),
 		MaxEntries,