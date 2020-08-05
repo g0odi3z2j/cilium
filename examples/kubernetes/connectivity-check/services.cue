g0odi3z2j/cilium

commit a3f50aff568f9d53308d1e95e6baa2149f699ba8

Extend connectivity-check for HTTP policy validation via CUE (#12599)
* connectivity-check: Add 'make clean' support

Factor out the targets for all YAMLs so it can be reused by a new phony
target, 'clean'.

Signed-off-by: Joe Stringer <joe@cilium.io>

* connectivity-check: Introduce cuelang framework

CUE (https://cuelang.org/) is a data constraint language built defined
as a superset of JSON which aims to "simplify tasks involving defining
and using data".

In context of the connectivity check YAMLs, CUE is useful to allow us to
"evaporate" the boilerplate necessary to define Kubernetes YAMLs for
Deployments, Services and CiliumNetworkPolicies and allow developers to
specify various permutations for connectivity checks concisely.

Why should we use it?

* It's more concise: One template definition, multiple reuse. This is
  useful for introducing new connectivity checks as upcoming commits
  will demonstrate as the developer doesn't need to perform the tedious
  and error-prone process of copying and modifying the YAMLs to
  implement various permutations of a check. Furthermore this helps
  reviewers as they will not have to read through swathes of YAMLs but
  can instead focus on the diffs in the templating that are introduced
  and compare to existing data definitions.

* Consolidate constant declaration. When a core change needs to be made
  to something like the readinessProbe for probes that expect a
  success or failure, we can update one definition in the main CUE file
  and all YAMLs will subsequently be generated with this change in mind.
  During the process of preparing these changes, I noticed
  inconsistencies between various existing YAMLs which appear to just be
  unintentional, where some YAMLs were improved with better timeoute
  behaviour or error rendering, but other YAMLs were left out.

* The data is more structured. Upcoming commits will introduce simple

examples/kubernetes/connectivity-check/services.cue
@@ -1,0 +1,38 @@
+package connectivity_check
+
+deployment: [ID=_]: {
+	if ID =~ "^[-_a-zA-Z0-9]*-headless$" {
+		_probeTarget: "echo-b-headless"
+	}
+}
+
+// deployment (defaults.cue) implicitly configures the deployments below such
+// that deployments with names matching 'pod-to-<X>', '*-[intra|multi]-node'
+// and '*-headless' will contact the related echo server via the related
+// service and will be scheduled with affinity / anti-affinity to that server.
+_serviceDeployment: {
+	metadata: labels: component: "services-check"
+}
+
+// Service checks
+deployment: "pod-to-b-multi-node-clusterip": _serviceDeployment
+deployment: "pod-to-b-multi-node-headless":  _serviceDeployment
+//deployment: "pod-to-b-intra-node-clusterip": _serviceDeployment
+//deployment: "pod-to-b-intra-node-headless":  _serviceDeployment
+
+_hostnetDeployment: _serviceDeployment & {
+	spec: template: spec: {
+		hostNetwork: true
+		dnsPolicy:   "ClusterFirstWithHostNet"
+	}
+}
+deployment: "host-to-b-multi-node-clusterip": _hostnetDeployment
+deployment: "host-to-b-multi-node-headless":  _hostnetDeployment
+
+// Hostport checks
+_hostPortDeployment: {
+	metadata: labels: component: "hostport-check"
+	_probeTarget: "echo-b-host-headless:40000"
+}
+deployment: "pod-to-b-multi-node-hostport": _hostPortDeployment
+deployment: "pod-to-b-intra-node-hostport": _hostPortDeployment

.github/workflows/smoke-test.yaml
@@ -85,14 +85,7 @@ jobs:
         run: |
           kubectl get pods -o wide
           kubectl get deploy -o wide
-          kubectl describe service echo-a
-          kubectl logs service/echo-a --all-containers --since=$LOG_TIME
-          kubectl describe service echo-b
-          kubectl logs service/echo-b --all-containers --since=$LOG_TIME
-          kubectl describe service echo-b-headless
-          kubectl logs service/echo-b-headless --all-containers --since=$LOG_TIME
-          kubectl describe service echo-b-host-headless
-          kubectl logs service/echo-b-host-headless --all-containers --since=$LOG_TIME
+          for svc in $(make -C examples/kubernetes/connectivity-check/ list | grep Service | awk '{ print $4 }'); do kubectl describe service $svc; kubectl logs service/$svc --all-containers --since=$LOG_TIME; done
 
       - name: Dump hubble related logs and events
         if: ${{ failure() &&  matrix.target.name == 'experimental-install' }}
@@ -139,6 +132,7 @@ jobs:
              --set global.externalIPs.enabled=true \
              --set global.nodePort.enabled=true \
              --set global.hostPort.enabled=true \
+             --set config.bpfMasquerade=false \
              --set config.ipam=kubernetes \
              --set global.pullPolicy=Never
 
@@ -165,11 +159,6 @@ jobs:
         env:
           LOG_TIME: 30m
         run: |
-          kubectl describe service echo-a
-          kubectl logs service/echo-a --all-containers --since=$LOG_TIME
-          kubectl describe service echo-b
-          kubectl logs service/echo-b --all-containers --since=$LOG_TIME
-          kubectl describe service echo-b-headless
-          kubectl logs service/echo-b-headless --all-containers --since=$LOG_TIME
-          kubectl describe service echo-b-host-headless
-          kubectl logs service/echo-b-host-headless --all-containers --since=$LOG_TIME
+          kubectl get pods -o wide
+          kubectl get deploy -o wide
+          for svc in $(make -C examples/kubernetes/connectivity-check/ list | grep Service | awk '{ print $4 }'); do kubectl describe service $svc; kubectl logs service/$svc --all-containers --since=$LOG_TIME; done

examples/kubernetes/connectivity-check/Makefile
@@ -1,35 +1,90 @@
 # Copyright 2017-2020 Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
-#
+
+include ../../../Makefile.defs
+include ../../../Makefile.quiet
+
 DEFAULT_OUT := connectivity-check.yaml
 HOSTPORT_OUT := connectivity-check-hostport.yaml
 SINGLE_OUT := connectivity-check-single-node.yaml
+PROXY_OUT := connectivity-check-proxy.yaml
+QUARANTINE_OUT := connectivity-check-quarantine.yaml
 
-SRC := $(wildcard *.yaml)
-HOSTPORT_SRC := $(filter-out $(DEFAULT_OUT) $(HOSTPORT_OUT) $(SINGLE_OUT), $(SRC))
-DEFAULT_SRC := $(filter-out $(wildcard *-hostport.yaml),$(HOSTPORT_SRC))
-SINGLE_SRC := $(filter-out $(wildcard *-multi-node*.yaml),$(DEFAULT_SRC))
+SERVERS_NAME := echo-a echo-b echo-b-host echo-b-host-headless echo-c echo-c-host echo-c-host-headless
+SERVERS_OUT := servers.yaml
 
+SRC := $(wildcard *.cue)
+ALL_TARGETS := $(DEFAULT_OUT) $(HOSTPORT_OUT) $(SINGLE_OUT) $(PROXY_OUT) $(QUARANTINE_OUT)
 
-all: $(DEFAULT_OUT) $(HOSTPORT_OUT) $(SINGLE_OUT)
+DOCKER_RUN := $(CONTAINER_ENGINE) container run --rm \
+	--workdir /src/examples/kubernetes/connectivity-check \
+	--volume $(CURDIR)/../../..:/src \
+	--user "$(shell id -u):$(shell id -g)"
 
-$(DEFAULT_OUT): $(DEFAULT_SRC)
+CUE_IMAGE := "docker.io/cuelang/cue:v0.2.2@sha256:2ea932c771212db140c9996c6fff1d236f3f84ae82add914374cee553b6fc60c"
+CUE := $(DOCKER_RUN) $(CUE_IMAGE)
+
+all: $(ALL_TARGETS)
+
+$(DEFAULT_OUT): $(SRC)
 	@echo '# Automatically generated by Makefile. DO NOT EDIT' > $(DEFAULT_OUT)
-	for FILE in $(DEFAULT_SRC); do \
-		cat $$FILE >> $(DEFAULT_OUT); \
-		echo "---" >> $(DEFAULT_OUT); \
-	done
+	$(QUIET)$(CUE) dump > $(DEFAULT_OUT)
 
-$(HOSTPORT_OUT): $(HOSTPORT_SRC)
+$(HOSTPORT_OUT): $(SRC)
 	@echo '# Automatically generated by Makefile. DO NOT EDIT' > $(HOSTPORT_OUT)
-	for FILE in $(HOSTPORT_SRC); do \
-		cat $$FILE >> $(HOSTPORT_OUT); \
-		echo "---" >> $(HOSTPORT_OUT); \
+	$(QUIET)$(CUE) -t component=all dump > $(HOSTPORT_OUT)
+
+$(SINGLE_OUT): $(SRC)
+	@echo '# Automatically generated by Makefile. DO NOT EDIT' > $(SINGLE_OUT)
+	$(QUIET)$(CUE) -t topology=single-node dump > $(SINGLE_OUT)
+
+$(PROXY_OUT): $(SRC) $(SERVERS_OUT)
+	@echo '# Automatically generated by Makefile. DO NOT EDIT' > $(PROXY_OUT)
+	$(QUIET)$(CUE) -t component=proxy dump > $@
+	@cat $(SERVERS_OUT) >> $@
+
+$(QUARANTINE_OUT): $(SRC) $(SERVERS_OUT)
+	@echo '# Automatically generated by Makefile. DO NOT EDIT' > $(QUARANTINE_OUT)
+	$(QUIET)$(CUE) -t component=all -t quarantine=true dump > $@
+	@cat $(SERVERS_OUT) >> $@
+
+clean:
+	@rm -f $(ALL_TARGETS)
+	@rm -f *.new
+	@rm -f *.diff
+	@rm -f *.yaml
+
+deploy:
+	$(QUIET)kubectl apply -f connectivity-check-hostport.yaml
+
+eval:
+	$(QUIET)$(CUE) eval -c ./...
+
+# To easier inspect individual yamls for specific checks, generate all YAMLs from *.cue
+generate_all: $(SRC)
+	# TODO: Don't run docker for every command
+	@for deployment in $(shell $(CUE) -t component=all ls | tail -n+2 | awk '{ print $$4 }' | sort | uniq); do \
+		$(CUE) -t component=all -t name=$$deployment dump > $$deployment.yaml; \
 	done
 
-$(SINGLE_OUT): $(SINGLE_SRC)
-	@echo '# Automatically generated by Makefile. DO NOT EDIT' > $(SINGLE_OUT)
-	for FILE in $(SINGLE_SRC); do \
-		cat $$FILE >> $(SINGLE_OUT); \
-		echo "---" >> $(SINGLE_OUT); \
+inspect:
+	@echo "Comparing latest cue declarations against cluster deployment from "make deploy"..."
+	$(QUIET)$(CUE) -t component=all dump > connectivity-check-hostport.yaml.new
+	-@kubectl diff -f connectivity-check-hostport.yaml.new > connectivity-check-hostport.yaml.diff
+	@cat connectivity-check-hostport.yaml.diff
+	@echo
+	@echo "The full diff is available in connectivity-check-hostport.yaml.diff."
+
+help:
+	$(QUIET)$(CUE) cmd help
+
+list:
+	$(QUIET)$(CUE) cmd ls
+
+$(SERVERS_OUT): $(SRC)
+	@echo > $(SERVERS_OUT)
+	@for name in $(SERVERS_NAME); do \
+		$(CUE) cmd -t name=$$name dump >> $@; \
 	done
+
+.PHONY: all clean deploy eval generate_all help inspect list

commit f4ce8c640a97713e7b5aba4f0651dd4e55df1e2b

connectivity-check: re-introduce port-to-b NodePort checks
Commit a3f50aff568f ("Extend connectivity-check for HTTP policy
validation via CUE (#12599)") introduced the usage of the CUE language
to make the management of the connectivity checks easier and more
consistent. But two deployments, "pod-to-b-intra-node-nodeport" and
"pod-to-b-multi-node-nodeport", have accidentally been left out of the
process.

This commit reintroduces them. Only the cue file was hand-edited, the
changes in the yaml files where generated by running "make".

Fixes: a3f50aff568f ("Extend connectivity-check for HTTP policy validation via CUE (#12599)")
Fixes: #12599
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

examples/kubernetes/connectivity-check/connectivity-check-hostport.yaml
@@ -1381,6 +1381,132 @@ apiVersion: apps/v1
 kind: Deployment
 ---
 metadata:
+  name: pod-to-b-multi-node-nodeport
+  labels:
+    name: pod-to-b-multi-node-nodeport
+    topology: multi-node
+    component: nodeport-check
+    quarantine: "false"
+    type: autocheck
+spec:
+  template:
+    metadata:
+      labels:
+        name: pod-to-b-multi-node-nodeport
+    spec:
+      hostNetwork: false
+      containers:
+      - name: pod-to-b-multi-node-nodeport-container
+        ports: []
+        image: docker.io/byrnedo/alpine-curl:0.1.8
+        imagePullPolicy: IfNotPresent
+        command:
+        - /bin/ash
+        - -c
+        - sleep 1000000000
+        readinessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+        livenessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+      affinity:
+        podAntiAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+          - labelSelector:
+              matchExpressions:
+              - key: name
+                operator: In
+                values:
+                - echo-b
+            topologyKey: kubernetes.io/hostname
+  selector:
+    matchLabels:
+      name: pod-to-b-multi-node-nodeport
+  replicas: 1
+apiVersion: apps/v1
+kind: Deployment
+---
+metadata:
+  name: pod-to-b-intra-node-nodeport
+  labels:
+    name: pod-to-b-intra-node-nodeport
+    topology: intra-node
+    component: nodeport-check
+    quarantine: "false"
+    type: autocheck
+spec:
+  template:
+    metadata:
+      labels:
+        name: pod-to-b-intra-node-nodeport
+    spec:
+      hostNetwork: false
+      containers:
+      - name: pod-to-b-intra-node-nodeport-container
+        ports: []
+        image: docker.io/byrnedo/alpine-curl:0.1.8
+        imagePullPolicy: IfNotPresent
+        command:
+        - /bin/ash
+        - -c
+        - sleep 1000000000
+        readinessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+        livenessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+      affinity:
+        podAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+          - labelSelector:
+              matchExpressions:
+              - key: name
+                operator: In
+                values:
+                - echo-b
+            topologyKey: kubernetes.io/hostname
+  selector:
+    matchLabels:
+      name: pod-to-b-intra-node-nodeport
+  replicas: 1
+apiVersion: apps/v1
+kind: Deployment
+---
+metadata:
   name: echo-a
   labels:
     name: echo-a

examples/kubernetes/connectivity-check/connectivity-check-single-node.yaml
@@ -420,6 +420,69 @@ apiVersion: apps/v1
 kind: Deployment
 ---
 metadata:
+  name: pod-to-b-intra-node-nodeport
+  labels:
+    name: pod-to-b-intra-node-nodeport
+    topology: intra-node
+    component: nodeport-check
+    quarantine: "false"
+    type: autocheck
+spec:
+  template:
+    metadata:
+      labels:
+        name: pod-to-b-intra-node-nodeport
+    spec:
+      hostNetwork: false
+      containers:
+      - name: pod-to-b-intra-node-nodeport-container
+        ports: []
+        image: docker.io/byrnedo/alpine-curl:0.1.8
+        imagePullPolicy: IfNotPresent
+        command:
+        - /bin/ash
+        - -c
+        - sleep 1000000000
+        readinessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+        livenessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+      affinity:
+        podAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+          - labelSelector:
+              matchExpressions:
+              - key: name
+                operator: In
+                values:
+                - echo-b
+            topologyKey: kubernetes.io/hostname
+  selector:
+    matchLabels:
+      name: pod-to-b-intra-node-nodeport
+  replicas: 1
+apiVersion: apps/v1
+kind: Deployment
+---
+metadata:
   name: echo-a
   labels:
     name: echo-a

examples/kubernetes/connectivity-check/connectivity-check.yaml
@@ -674,6 +674,132 @@ apiVersion: apps/v1
 kind: Deployment
 ---
 metadata:
+  name: pod-to-b-multi-node-nodeport
+  labels:
+    name: pod-to-b-multi-node-nodeport
+    topology: multi-node
+    component: nodeport-check
+    quarantine: "false"
+    type: autocheck
+spec:
+  template:
+    metadata:
+      labels:
+        name: pod-to-b-multi-node-nodeport
+    spec:
+      hostNetwork: false
+      containers:
+      - name: pod-to-b-multi-node-nodeport-container
+        ports: []
+        image: docker.io/byrnedo/alpine-curl:0.1.8
+        imagePullPolicy: IfNotPresent
+        command:
+        - /bin/ash
+        - -c
+        - sleep 1000000000
+        readinessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+        livenessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+      affinity:
+        podAntiAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+          - labelSelector:
+              matchExpressions:
+              - key: name
+                operator: In
+                values:
+                - echo-b
+            topologyKey: kubernetes.io/hostname
+  selector:
+    matchLabels:
+      name: pod-to-b-multi-node-nodeport
+  replicas: 1
+apiVersion: apps/v1
+kind: Deployment
+---
+metadata:
+  name: pod-to-b-intra-node-nodeport
+  labels:
+    name: pod-to-b-intra-node-nodeport
+    topology: intra-node
+    component: nodeport-check
+    quarantine: "false"
+    type: autocheck
+spec:
+  template:
+    metadata:
+      labels:
+        name: pod-to-b-intra-node-nodeport
+    spec:
+      hostNetwork: false
+      containers:
+      - name: pod-to-b-intra-node-nodeport-container
+        ports: []
+        image: docker.io/byrnedo/alpine-curl:0.1.8
+        imagePullPolicy: IfNotPresent
+        command:
+        - /bin/ash
+        - -c
+        - sleep 1000000000
+        readinessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+        livenessProbe:
+          exec:
+            command:
+            - curl
+            - -sS
+            - --fail
+            - --connect-timeout
+            - "5"
+            - -o
+            - /dev/null
+            - echo-b-host-headless:31313/public
+      affinity:
+        podAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+          - labelSelector:
+              matchExpressions:
+              - key: name
+                operator: In
+                values:
+                - echo-b
+            topologyKey: kubernetes.io/hostname
+  selector:
+    matchLabels:
+      name: pod-to-b-intra-node-nodeport
+  replicas: 1
+apiVersion: apps/v1
+kind: Deployment
+---
+metadata:
   name: echo-a
   labels:
     name: echo-a

examples/kubernetes/connectivity-check/services.cue
@@ -36,3 +36,11 @@ _hostPortDeployment: {
 }
 deployment: "pod-to-b-multi-node-hostport": _hostPortDeployment
 deployment: "pod-to-b-intra-node-hostport": _hostPortDeployment
+
+// NodePort checks
+_nodePortDeployment: {
+	metadata: labels: component: "nodeport-check"
+	_probeTarget: "echo-b-host-headless:31313"
+}
+deployment: "pod-to-b-multi-node-nodeport": _nodePortDeployment
+deployment: "pod-to-b-intra-node-nodeport": _nodePortDeployment