g0odi3z2j/cilium

commit 8f1deb9769322f9f1980c750c3d21575e2e14f9e

vendor: Add go-bindata
This is used for embedding the generated CRD YAMLs into binary form.

Signed-off-by: Chris Tarazi <chris@isovalent.com>

tools/tools.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Authors of Cilium
+// Copyright 2019-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package tools
 
 import (
 	_ "github.com/cilium/deepequal-gen"
+	_ "github.com/go-bindata/go-bindata/v3"
 	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"

vendor/github.com/go-bindata/go-bindata/v3/.gitignore
@@ -1,0 +1,16 @@
+# Binaries for programs and plugins
+*.exe
+*.exe~
+*.dll
+*.so
+*.dylib
+
+# Test binary, build with `go test -c`
+*.test
+
+# Output of the go coverage tool, specifically when used with LiteIDE
+*.out
+
+# Goland project files
+.idea/
+*.iml

vendor/github.com/go-bindata/go-bindata/v3/tools.go
@@ -1,0 +1,8 @@
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/kisielk/errcheck"
+	_ "golang.org/x/lint/golint"
+)

vendor/github.com/kisielk/errcheck/main.go
@@ -1,0 +1,194 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strings"
+
+	"github.com/kisielk/errcheck/internal/errcheck"
+)
+
+const (
+	exitCodeOk int = iota
+	exitUncheckedError
+	exitFatalError
+)
+
+var abspath bool
+
+type ignoreFlag map[string]*regexp.Regexp
+
+func (f ignoreFlag) String() string {
+	pairs := make([]string, 0, len(f))
+	for pkg, re := range f {
+		prefix := ""
+		if pkg != "" {
+			prefix = pkg + ":"
+		}
+		pairs = append(pairs, prefix+re.String())
+	}
+	return fmt.Sprintf("%q", strings.Join(pairs, ","))
+}
+
+func (f ignoreFlag) Set(s string) error {
+	if s == "" {
+		return nil
+	}
+	for _, pair := range strings.Split(s, ",") {
+		colonIndex := strings.Index(pair, ":")
+		var pkg, re string
+		if colonIndex == -1 {
+			pkg = ""
+			re = pair
+		} else {
+			pkg = pair[:colonIndex]
+			re = pair[colonIndex+1:]
+		}
+		regex, err := regexp.Compile(re)
+		if err != nil {
+			return err
+		}
+		f[pkg] = regex
+	}
+	return nil
+}
+
+type tagsFlag []string
+
+func (f *tagsFlag) String() string {
+	return fmt.Sprintf("%q", strings.Join(*f, " "))
+}
+
+func (f *tagsFlag) Set(s string) error {
+	if s == "" {
+		return nil
+	}
+	tags := strings.Split(s, " ")
+	if tags == nil {
+		return nil
+	}
+	for _, tag := range tags {
+		if tag != "" {
+			*f = append(*f, tag)
+		}
+	}
+	return nil
+}
+
+var dotStar = regexp.MustCompile(".*")
+
+func reportUncheckedErrors(e *errcheck.UncheckedErrors, verbose bool) {
+	wd, err := os.Getwd()
+	if err != nil {
+		wd = ""
+	}
+	for _, uncheckedError := range e.Errors {
+		pos := uncheckedError.Pos.String()
+		if !abspath {
+			newPos, err := filepath.Rel(wd, pos)
+			if err == nil {
+				pos = newPos
+			}
+		}
+
+		if verbose && uncheckedError.FuncName != "" {
+			fmt.Printf("%s:\t%s\t%s\n", pos, uncheckedError.FuncName, uncheckedError.Line)
+		} else {
+			fmt.Printf("%s:\t%s\n", pos, uncheckedError.Line)
+		}
+	}
+}
+
+func mainCmd(args []string) int {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	checker := errcheck.NewChecker()
+	paths, err := parseFlags(checker, args)
+	if err != exitCodeOk {
+		return err
+	}
+
+	if err := checker.CheckPackages(paths...); err != nil {
+		if e, ok := err.(*errcheck.UncheckedErrors); ok {
+			reportUncheckedErrors(e, checker.Verbose)
+			return exitUncheckedError
+		} else if err == errcheck.ErrNoGoFiles {
+			fmt.Fprintln(os.Stderr, err)
+			return exitCodeOk
+		}
+		fmt.Fprintf(os.Stderr, "error: failed to check packages: %s\n", err)
+		return exitFatalError
+	}
+	return exitCodeOk
+}
+
+func parseFlags(checker *errcheck.Checker, args []string) ([]string, int) {
+	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	flags.BoolVar(&checker.Blank, "blank", false, "if true, check for errors assigned to blank identifier")
+	flags.BoolVar(&checker.Asserts, "asserts", false, "if true, check for ignored type assertion results")
+	flags.BoolVar(&checker.WithoutTests, "ignoretests", false, "if true, checking of _test.go files is disabled")
+	flags.BoolVar(&checker.WithoutGeneratedCode, "ignoregenerated", false, "if true, checking of files with generated code is disabled")
+	flags.BoolVar(&checker.Verbose, "verbose", false, "produce more verbose logging")
+
+	flags.BoolVar(&abspath, "abspath", false, "print absolute paths to files")
+
+	tags := tagsFlag{}
+	flags.Var(&tags, "tags", "space-separated list of build tags to include")
+	ignorePkg := flags.String("ignorepkg", "", "comma-separated list of package paths to ignore")
+	ignore := ignoreFlag(map[string]*regexp.Regexp{})
+	flags.Var(ignore, "ignore", "[deprecated] comma-separated list of pairs of the form pkg:regex\n"+
+		"            the regex is used to ignore names within pkg.")
+
+	var excludeFile string
+	flags.StringVar(&excludeFile, "exclude", "", "Path to a file containing a list of functions to exclude from checking")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return nil, exitFatalError
+	}
+
+	if excludeFile != "" {
+		exclude := make(map[string]bool)
+		fh, err := os.Open(excludeFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not read exclude file: %s\n", err)
+			return nil, exitFatalError
+		}
+		scanner := bufio.NewScanner(fh)
+		for scanner.Scan() {
+			name := scanner.Text()
+			exclude[name] = true
+
+			if checker.Verbose {
+				fmt.Printf("Excluding %s\n", name)
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "Could not read exclude file: %s\n", err)
+			return nil, exitFatalError
+		}
+		checker.SetExclude(exclude)
+	}
+
+	checker.Tags = tags
+	for _, pkg := range strings.Split(*ignorePkg, ",") {
+		if pkg != "" {
+			ignore[pkg] = dotStar
+		}
+	}
+	checker.Ignore = ignore
+
+	paths := flags.Args()
+	if len(paths) == 0 {
+		paths = []string{"."}
+	}
+	return paths, exitCodeOk
+}
+
+func main() {
+	os.Exit(mainCmd(os.Args))
+}

go.mod
@@ -26,6 +26,7 @@ require (
 	github.com/docker/libnetwork v0.0.0-20190128195551-d8d4c8cf03d7
 	github.com/envoyproxy/protoc-gen-validate v0.4.0
 	github.com/fsnotify/fsnotify v1.4.10-0.20200417215612-7f4cf4dd2b52
+	github.com/go-bindata/go-bindata/v3 v3.1.3
 	github.com/go-openapi/errors v0.19.6
 	github.com/go-openapi/loads v0.19.5
 	github.com/go-openapi/runtime v0.19.20

go.sum
@@ -216,6 +216,8 @@ github.com/ghodss/yaml v1.0.0 h1:wQHKEahhL6wmXdzwWG11gIVCkOv05bNOh+Rxn0yngAk=
 github.com/ghodss/yaml v1.0.0/go.mod h1:4dBDuWmgqj2HViK6kFavaiC9ZROes6MMH2rRYeMEF04=
 github.com/globalsign/mgo v0.0.0-20180905125535-1ca0a4f7cbcb/go.mod h1:xkRDCp4j0OGD1HRkm4kmhM+pmpv3AKq5SU7GMg4oO/Q=
 github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8/go.mod h1:xkRDCp4j0OGD1HRkm4kmhM+pmpv3AKq5SU7GMg4oO/Q=
+github.com/go-bindata/go-bindata/v3 v3.1.3 h1:F0nVttLC3ws0ojc7p60veTurcOm//D4QBODNM7EGrCI=
+github.com/go-bindata/go-bindata/v3 v3.1.3/go.mod h1:1/zrpXsLD8YDIbhZRqXzm1Ghc7NhEvIN9+Z6R5/xH4I=
 github.com/go-gl/glfw/v3.3/glfw v0.0.0-20191125211704-12ad95a8df72/go.mod h1:tQ2UAYgL5IevRw8kRxooKSPJfGvJ9fJQFa0TUsXzTg8=
 github.com/go-kit/kit v0.8.0/go.mod h1:xBxKIO96dXMWWy0MnWVtmwkA9/13aqxPnvrjFYMA2as=
 github.com/go-kit/kit v0.9.0/go.mod h1:xBxKIO96dXMWWy0MnWVtmwkA9/13aqxPnvrjFYMA2as=
@@ -481,6 +483,7 @@ github.com/kevinburke/ssh_config v0.0.0-20190725054713-01f96b0aa0cd h1:Coekwdh0v
 github.com/kevinburke/ssh_config v0.0.0-20190725054713-01f96b0aa0cd/go.mod h1:CT57kijsi8u/K/BOFA39wgDQJ9CxiF4nAY/ojJ6r6mM=
 github.com/keybase/go-ps v0.0.0-20190827175125-91aafc93ba19/go.mod h1:hY+WOq6m2FpbvyrI93sMaypsttvaIL5nhVR92dTMUcQ=
 github.com/kisielk/errcheck v1.1.0/go.mod h1:EZBBE59ingxPouuu3KfxchcWSUPOHkagtvWXihfKN4Q=
+github.com/kisielk/errcheck v1.2.0 h1:reN85Pxc5larApoH1keMBiu2GWtPqXQ1nc9gx+jOU+E=
 github.com/kisielk/errcheck v1.2.0/go.mod h1:/BMXB+zMLi60iA8Vv6Ksmxu/1UDYcXs4uQLJ+jE2L00=
 github.com/kisielk/gotool v1.0.0/go.mod h1:XhKaO+MFFWcvkIS/tQcRk01m1F5IRFswLeQ+oQHNcck=
 github.com/klauspost/compress v1.9.5/go.mod h1:RyIbtBH6LamlWaDj8nUwkbUhJ87Yi3uG0guNDohfE1A=
@@ -522,7 +525,6 @@ github.com/mattn/go-runewidth v0.0.2/go.mod h1:LwmH8dsx7+W8Uxz3IHJYH5QSwggIsqBzp
 github.com/mattn/go-shellwords v1.0.3/go.mod h1:3xCvwCdWdlDJUrvuMn7Wuy9eWs4pE8vqg+NOMyg4B2o=
 github.com/mattn/go-shellwords v1.0.5 h1:JhhFTIOslh5ZsPrpa3Wdg8bF0WI3b44EMblmU9wIsXc=
 github.com/mattn/go-shellwords v1.0.5/go.mod h1:3xCvwCdWdlDJUrvuMn7Wuy9eWs4pE8vqg+NOMyg4B2o=
-github.com/matttproud/golang_protobuf_extensions v1.0.1 h1:4hp9jkHxhMHkqkrB3Ix0jegS5sx/RkqARlsWZ6pIwiU=
 github.com/matttproud/golang_protobuf_extensions v1.0.1/go.mod h1:D8He9yQNgCq6Z5Ld7szi9bcBfOoFv/3dc6xSMkL2PC0=
 github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 h1:I0XW9+e1XWDxdcEniV4rQAIOPUGDq67JSCiRCgGCZLI=
 github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369/go.mod h1:BSXmuO+STAnVfrANrmjBb36TMTDstsz7MSK+HVaYKv4=
@@ -702,7 +704,6 @@ github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77/go.mod h1:
 github.com/yuin/goldmark v1.1.25/go.mod h1:3hX8gzYuyVAZsxl0MRgGTJEmQBFcNTphYh9decYSb74=
 github.com/yuin/goldmark v1.1.27/go.mod h1:3hX8gzYuyVAZsxl0MRgGTJEmQBFcNTphYh9decYSb74=
 go.etcd.io/bbolt v1.3.2/go.mod h1:IbVyRI1SCnLcuJnV2u8VeU0CEYM7e686BmAb1XKL+uU=
-go.etcd.io/bbolt v1.3.3 h1:MUGmc65QhB3pIlaQ5bB4LwqSj6GIonVJXpZiaKNyaKk=
 go.etcd.io/bbolt v1.3.3/go.mod h1:IbVyRI1SCnLcuJnV2u8VeU0CEYM7e686BmAb1XKL+uU=
 go.etcd.io/bbolt v1.3.5 h1:XAzx9gjCb0Rxj7EoqcClPD1d5ZBxZJk0jbuoPHenBt0=
 go.etcd.io/bbolt v1.3.5/go.mod h1:G5EMThwa9y8QZGBClrRx5EY+Yw9kAhnjy3bSjsnlVTQ=

vendor/github.com/go-bindata/go-bindata/v3/.drone.yml
@@ -1,0 +1,13 @@
+kind: pipeline
+name: default
+
+workspace:
+  base: /go
+  path: github.com/go-bindata/go-bindata
+steps:
+- name: build
+  image: golang:1.12
+  commands:
+    - go get -u honnef.co/go/tools/cmd/staticcheck
+    - staticcheck ./...
+    - make

vendor/github.com/go-bindata/go-bindata/v3/LICENSE
@@ -1,0 +1,3 @@
+This work is subject to the CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+license. Its contents can be found at:
+http://creativecommons.org/publicdomain/zero/1.0

commit 5eb63178b7ce5fb02219a3227f47c78f1d3b8977

vendor: Add go-bindata CLI
This commit allows us to remove the need for developers to install the
tool itself externally. We can instead run it from the vendor/ path.

Fixes: 8f1deb976 ("vendor: Add go-bindata")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

tools/tools.go
@@ -19,6 +19,7 @@ package tools
 import (
 	_ "github.com/cilium/deepequal-gen"
 	_ "github.com/go-bindata/go-bindata/v3"
+	_ "github.com/go-bindata/go-bindata/v3/go-bindata"
 	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"

vendor/github.com/go-bindata/go-bindata/v3/go-bindata/.gitignore
@@ -1,0 +1,1 @@
+go-bindata

vendor/github.com/go-bindata/go-bindata/v3/go-bindata/AppendSliceValue.go
@@ -1,0 +1,22 @@
+package main
+
+import "strings"
+
+// borrowed from https://github.com/hashicorp/serf/blob/master/command/agent/flag_slice_value.go
+
+// AppendSliceValue implements the flag.Value interface and allows multiple
+// calls to the same variable to append a list.
+type AppendSliceValue []string
+
+func (s *AppendSliceValue) String() string {
+	return strings.Join(*s, ",")
+}
+
+func (s *AppendSliceValue) Set(value string) error {
+	if *s == nil {
+		*s = make([]string, 0, 1)
+	}
+
+	*s = append(*s, value)
+	return nil
+}

vendor/github.com/go-bindata/go-bindata/v3/go-bindata/main.go
@@ -1,0 +1,108 @@
+// This work is subject to the CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+// license. Its contents can be found at:
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/go-bindata/go-bindata/v3"
+)
+
+func main() {
+	cfg := parseArgs()
+	err := bindata.Translate(cfg)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "bindata: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+// parseArgs create s a new, filled configuration instance
+// by reading and parsing command line options.
+//
+// This function exits the program with an error, if
+// any of the command line options are incorrect.
+func parseArgs() *bindata.Config {
+	var version bool
+
+	c := bindata.NewConfig()
+
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [options] <input directories>\n\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.BoolVar(&c.Debug, "debug", c.Debug, "Do not embed the assets, but provide the embedding API. Contents will still be loaded from disk.")
+	flag.BoolVar(&c.Dev, "dev", c.Dev, "Similar to debug, but does not emit absolute paths. Expects a rootDir variable to already exist in the generated code's package.")
+	flag.StringVar(&c.Tags, "tags", c.Tags, "Optional set of build tags to include.")
+	flag.StringVar(&c.Prefix, "prefix", c.Prefix, "Optional path prefix to strip off asset names.")
+	flag.StringVar(&c.Package, "pkg", c.Package, "Package name to use in the generated code.")
+	flag.BoolVar(&c.NoMemCopy, "nomemcopy", c.NoMemCopy, "Use a .rodata hack to get rid of unnecessary memcopies. Refer to the documentation to see what implications this carries.")
+	flag.BoolVar(&c.NoCompress, "nocompress", c.NoCompress, "Assets will *not* be GZIP compressed when this flag is specified.")
+	flag.BoolVar(&c.NoMetadata, "nometadata", c.NoMetadata, "Assets will not preserve size, mode, and modtime info.")
+	flag.BoolVar(&c.HttpFileSystem, "fs", c.HttpFileSystem, "Whether generate instance http.FileSystem interface code.")
+	flag.UintVar(&c.Mode, "mode", c.Mode, "Optional file mode override for all files.")
+	flag.Int64Var(&c.ModTime, "modtime", c.ModTime, "Optional modification unix timestamp override for all files.")
+	flag.StringVar(&c.Output, "o", c.Output, "Optional name of the output file to be generated.")
+	flag.BoolVar(&version, "version", false, "Displays version information.")
+
+	ignore := make([]string, 0)
+	flag.Var((*AppendSliceValue)(&ignore), "ignore", "Regex pattern to ignore")
+
+	flag.Parse()
+
+	patterns := make([]*regexp.Regexp, 0)
+	for _, pattern := range ignore {
+		patterns = append(patterns, regexp.MustCompile(pattern))
+	}
+	c.Ignore = patterns
+
+	if version {
+		fmt.Printf("%s\n", Version())
+		os.Exit(0)
+	}
+
+	// Make sure we have input paths.
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "Missing <input dir>\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Create input configurations.
+	c.Input = make([]bindata.InputConfig, flag.NArg())
+	for i := range c.Input {
+		c.Input[i] = parseInput(flag.Arg(i))
+	}
+
+	return c
+}
+
+// parseRecursive determines whether the given path has a recrusive indicator and
+// returns a new path with the recursive indicator chopped off if it does.
+//
+//  ex:
+//      /path/to/foo/...    -> (/path/to/foo, true)
+//      /path/to/bar        -> (/path/to/bar, false)
+func parseInput(path string) bindata.InputConfig {
+	if strings.HasSuffix(path, "/...") {
+		return bindata.InputConfig{
+			Path:      filepath.Clean(path[:len(path)-4]),
+			Recursive: true,
+		}
+	} else {
+		return bindata.InputConfig{
+			Path:      filepath.Clean(path),
+			Recursive: false,
+		}
+	}
+
+}

vendor/github.com/go-bindata/go-bindata/v3/go-bindata/version.go
@@ -1,0 +1,31 @@
+// This work is subject to the CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+// license. Its contents can be found at:
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+const (
+	AppName         = "go-bindata"
+	AppVersionMajor = 3
+	AppVersionMinor = 1
+)
+
+// revision part of the program version.
+// This will be set automatically at build time like so:
+//
+//     go build -ldflags "-X main.AppVersionRev `date -u +%s`"
+var AppVersionRev string
+
+func Version() string {
+	if len(AppVersionRev) == 0 {
+		AppVersionRev = "3"
+	}
+
+	return fmt.Sprintf("%s %d.%d.%s (Go runtime %s).\nCopyright (c) 2010-2013, Jim Teeuwen.",
+		AppName, AppVersionMajor, AppVersionMinor, AppVersionRev, runtime.Version())
+}