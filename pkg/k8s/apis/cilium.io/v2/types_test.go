g0odi3z2j/cilium

commit bc64728f0e5d9a682e9caa30db11c688f1624efb

k8s: Disallow nodeSelector for CNP at agent level
Previously, this validation was done at the apiserver level with
handwritten schema validation. Now that we have automated CRD schema
validation, we must implement this check at the agent level as a
stopgap.

The reason for this is that given the relationship of CCNP and CNP (CCNP
embeds CNP), it was impossible to represent the following in
controller-gen marker comments to generate validation schema:

- CNP only supports EndpointSelector.
- CCNP supports both EndpointSelector and NodeSelector, but only one at
  a time in a policy.

This commit is meant to be reverted once an approach for seperating the
CNP and CCNP CRD types has been implemented.

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/k8s/apis/cilium.io/v2/cnp_types.go
@@ -227,6 +227,9 @@ func (r *CiliumNetworkPolicy) Parse() (api.Rules, error) {
 		if err := r.Spec.Sanitize(); err != nil {
 			return nil, fmt.Errorf("Invalid CiliumNetworkPolicy spec: %s", err)
 		}
+		if r.Spec.NodeSelector.LabelSelector != nil {
+			return nil, fmt.Errorf("Invalid CiliumNetworkPolicy spec: rule cannot have NodeSelector")
+		}
 		cr := k8sCiliumUtils.ParseToCiliumRule(namespace, name, uid, r.Spec)
 		retRules = append(retRules, cr)
 	}

pkg/k8s/apis/cilium.io/v2/types_test.go
@@ -405,6 +405,89 @@ func (s *CiliumV2Suite) TestParseRules(c *C) {
 	c.Assert(cnpl, checker.DeepEquals, *expectedPolicyRuleListWithLabel)
 }
 
+func (s *CiliumV2Suite) TestParseWithNodeSelector(c *C) {
+	// A rule without any L7 rules so that we can validate both CNP and CCNP.
+	// CCNP doesn't support L7 rules just yet.
+	rule := api.Rule{
+		EndpointSelector: api.NewESFromLabels(),
+		Ingress: []api.IngressRule{
+			{
+				FromEndpoints: []api.EndpointSelector{
+					api.NewESFromLabels(
+						labels.ParseSelectLabel("role=frontend"),
+					),
+					api.NewESFromLabels(
+						labels.ParseSelectLabel("reserved:world"),
+					),
+				},
+				ToPorts: []api.PortRule{
+					{
+						Ports: []api.PortProtocol{{Port: "80", Protocol: "TCP"}},
+					},
+				},
+			},
+		},
+		Egress: []api.EgressRule{
+			{
+				ToPorts: []api.PortRule{
+					{
+						Ports: []api.PortProtocol{{Port: "80", Protocol: "TCP"}},
+					},
+				},
+			}, {
+				ToCIDR: []api.CIDR{"10.0.0.1"},
+			}, {
+				ToCIDRSet: []api.CIDRRule{{Cidr: api.CIDR("10.0.0.0/8"), ExceptCIDRs: []api.CIDR{"10.96.0.0/12"}}},
+			},
+		},
+	}
+
+	emptySelector := api.EndpointSelector{LabelSelector: nil}
+	prevEPSelector := rule.EndpointSelector
+
+	// A NodeSelector is an EndpointSelector. We can reuse the previous value
+	// that was set as an EndpointSelector.
+	rule.EndpointSelector = emptySelector
+	rule.NodeSelector = prevEPSelector
+
+	// Expect CNP parse error because it's not allowed to have a NodeSelector.
+	cnpl := CiliumNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "rule",
+			UID:       uuidRule,
+		},
+		Spec: &rule,
+	}
+	_, err := cnpl.Parse()
+	c.Assert(err, ErrorMatches,
+		"Invalid CiliumNetworkPolicy spec: rule cannot have NodeSelector")
+
+	// CCNP parse is allowed to have a NodeSelector.
+	ccnpl := CiliumClusterwideNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "rule",
+			UID:       uuidRule,
+		},
+		CiliumNetworkPolicy: &cnpl,
+	}
+	_, err = ccnpl.Parse()
+	c.Assert(err, IsNil)
+
+	// Now test a CNP and CCNP with an EndpointSelector only.
+	rule.EndpointSelector = prevEPSelector
+	rule.NodeSelector = emptySelector
+
+	// CNP and CCNP parse is allowed to have an EndpointSelector.
+	_, err = cnpl.Parse()
+	c.Assert(err, IsNil)
+
+	// CNP and CCNP parse is allowed to have an EndpointSelector.
+	_, err = ccnpl.Parse()
+	c.Assert(err, IsNil)
+}
+
 func (s *CiliumV2Suite) TestCiliumNodeInstanceID(c *C) {
 	c.Assert((*CiliumNode)(nil).InstanceID(), Equals, "")
 	c.Assert((&CiliumNode{}).InstanceID(), Equals, "")

pkg/k8s/apis/cilium.io/v2/ccnp_types.go
@@ -15,6 +15,10 @@
 package v2
 
 import (
+	"fmt"
+
+	k8sCiliumUtils "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/utils"
+	"github.com/cilium/cilium/pkg/policy/api"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -74,4 +78,37 @@ type CiliumClusterwideNetworkPolicyList struct {
 
 	// Items is a list of CiliumClusterwideNetworkPolicies.
 	Items []CiliumClusterwideNetworkPolicy `json:"items"`
+}
+
+// Parse parses a CiliumClusterwideNetworkPolicy and returns a list of cilium
+// policy rules.
+func (r *CiliumClusterwideNetworkPolicy) Parse() (api.Rules, error) {
+	if r.ObjectMeta.Name == "" {
+		return nil, fmt.Errorf("CiliumClusterwideNetworkPolicy must have name")
+	}
+
+	name := r.ObjectMeta.Name
+	uid := r.ObjectMeta.UID
+
+	retRules := api.Rules{}
+
+	if r.Spec != nil {
+		if err := r.Spec.Sanitize(); err != nil {
+			return nil, fmt.Errorf("Invalid CiliumClusterwideNetworkPolicy spec: %s", err)
+		}
+		cr := k8sCiliumUtils.ParseToCiliumRule("", name, uid, r.Spec)
+		retRules = append(retRules, cr)
+	}
+	if r.Specs != nil {
+		for _, rule := range r.Specs {
+			if err := rule.Sanitize(); err != nil {
+				return nil, fmt.Errorf("Invalid CiliumClusterwideNetworkPolicy specs: %s", err)
+
+			}
+			cr := k8sCiliumUtils.ParseToCiliumRule("", name, uid, rule)
+			retRules = append(retRules, cr)
+		}
+	}
+
+	return retRules, nil
 }

commit bb723c994e1439174fffe435797bf2b83827d2e9

k8s: Fix parsing of CCNPs
Host policies are rejected on master with the following error:

    $ kubectl describe ccnp | tail -n9
    Status:
      Nodes:
        k8s1:
          Error:         Invalid CiliumNetworkPolicy spec: rule cannot have NodeSelector
          Last Updated:  2020-08-10T12:52:51Z
        k8s2:
          Error:         Invalid CiliumNetworkPolicy spec: rule cannot have NodeSelector
          Last Updated:  2020-08-10T12:52:48Z
    Events:              <none>

This error is printed because CiliumNetworkPolicy.Parse() has some new
checks to prevent using NodeSelector in CNPs. It assumes
CiliumClusterwideNetworkPolicy.Parse() will be called in the case of CCNPs
with NodeSelectors. However, the k8s watcher for CCNPs calls
addCiliumNetworkPolicyV2 which takes a types.SlimCNP and therefore calls
CiliumNetworkPolicy.Parse().

This commit implements a temporary fix for this regression by manually
construction a CCNP object from CiliumNetworkPolicy.Parse() when the
namespace is empty (CNP is actually a CCNP) and parsing that instead of
the CNP object. It also adds a new unit test to check this behavior.

Related: #12834
Fixes: bc64728 ("k8s: Disallow nodeSelector for CNP at agent level")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/k8s/apis/cilium.io/v2/cnp_types.go
@@ -218,6 +218,18 @@ func (r *CiliumNetworkPolicy) Parse() (api.Rules, error) {
 	}
 
 	namespace := k8sUtils.ExtractNamespace(&r.ObjectMeta)
+	// Temporary fix for CCNPs. See #12834.
+	// TL;DR. CCNPs are converted into SlimCNPs and end up here so we need to
+	// convert them back to CCNPs to allow proper parsing.
+	if namespace == "" {
+		ccnp := CiliumClusterwideNetworkPolicy{
+			TypeMeta:            r.TypeMeta,
+			ObjectMeta:          r.ObjectMeta,
+			CiliumNetworkPolicy: r,
+			Status:              r.Status,
+		}
+		return ccnp.Parse()
+	}
 	name := r.ObjectMeta.Name
 	uid := r.ObjectMeta.UID
 

pkg/k8s/apis/cilium.io/v2/types_test.go
@@ -466,13 +466,26 @@ func (s *CiliumV2Suite) TestParseWithNodeSelector(c *C) {
 	// CCNP parse is allowed to have a NodeSelector.
 	ccnpl := CiliumClusterwideNetworkPolicy{
 		ObjectMeta: metav1.ObjectMeta{
-			Namespace: "default",
+			Namespace: "",
 			Name:      "rule",
 			UID:       uuidRule,
 		},
 		CiliumNetworkPolicy: &cnpl,
 	}
 	_, err = ccnpl.Parse()
+	c.Assert(err, IsNil)
+
+	// CCNPs are received as CNP and initially parsed as CNP. Create a CNP with
+	// an empty namespace to test this case. See #12834 for details.
+	ccnplAsCNP := CiliumNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "",
+			Name:      "rule",
+			UID:       uuidRule,
+		},
+		Spec: &rule,
+	}
+	_, err = ccnplAsCNP.Parse()
 	c.Assert(err, IsNil)
 
 	// Now test a CNP and CCNP with an EndpointSelector only.
@@ -482,9 +495,9 @@ func (s *CiliumV2Suite) TestParseWithNodeSelector(c *C) {
 	// CNP and CCNP parse is allowed to have an EndpointSelector.
 	_, err = cnpl.Parse()
 	c.Assert(err, IsNil)
-
-	// CNP and CCNP parse is allowed to have an EndpointSelector.
 	_, err = ccnpl.Parse()
+	c.Assert(err, IsNil)
+	_, err = ccnplAsCNP.Parse()
 	c.Assert(err, IsNil)
 }
 