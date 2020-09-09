g0odi3z2j/cilium

commit 8267885b071e363f1d1763bad7bf0144884b26ee

bpf: Skip ingress policy at egress of source if egress prog is in use
When a dedicated BPF prog is in use implementing ingress policy for an
endpoint, there is no need to enforce the ingress policy of the local
destination at the source. The policy will be enforced twice.

In this new mode, the identity of the source is carried in the skb mark to the
BPF program on the destination endpoint side. For this purpose, a new mark flag
is defined.

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/lib/l3.h
@@ -88,8 +88,6 @@ static inline int ipv6_local_delivery(struct __sk_buff *skb, int l3_off, int l4_
 		return ret;
 
 	cilium_dbg(skb, DBG_LXC_FOUND, ep->ifindex, 0);
-	skb->cb[CB_SRC_LABEL] = seclabel;
-	skb->cb[CB_IFINDEX] = ep->ifindex;
 
 #if defined LOCAL_DELIVERY_METRICS
 	/*
@@ -99,8 +97,16 @@ static inline int ipv6_local_delivery(struct __sk_buff *skb, int l3_off, int l4_
 	 */
 	update_metrics(skb->len, direction, REASON_FORWARDED);
 #endif
+
+#ifdef USE_BPF_PROG_FOR_INGRESS_POLICY
+	skb->mark = (seclabel << 16) | MARK_MAGIC_IDENTITY;
+	return redirect_peer(ep->ifindex, 0);
+#else
+	skb->cb[CB_SRC_LABEL] = seclabel;
+	skb->cb[CB_IFINDEX] = ep->ifindex;
 	tail_call(skb, &POLICY_CALL_MAP, ep->lxc_id);
 	return DROP_MISSED_TAIL_CALL;
+#endif
 }
 #endif /* ENABLE_IPV6 */
 
@@ -120,8 +126,6 @@ static inline int __inline__ ipv4_local_delivery(struct __sk_buff *skb, int l3_o
 		return ret;
 
 	cilium_dbg(skb, DBG_LXC_FOUND, ep->ifindex, 0);
-	skb->cb[CB_SRC_LABEL] = seclabel;
-	skb->cb[CB_IFINDEX] = ep->ifindex;
 
 #if defined LOCAL_DELIVERY_METRICS
 	/*
@@ -131,8 +135,16 @@ static inline int __inline__ ipv4_local_delivery(struct __sk_buff *skb, int l3_o
 	 */
 	update_metrics(skb->len, direction, REASON_FORWARDED);
 #endif
+
+#ifdef USE_BPF_PROG_FOR_INGRESS_POLICY
+	skb->mark = (seclabel << 16) | MARK_MAGIC_IDENTITY;
+	return redirect_peer(ep->ifindex, 0);
+#else
+	skb->cb[CB_SRC_LABEL] = seclabel;
+	skb->cb[CB_IFINDEX] = ep->ifindex;
 	tail_call(skb, &POLICY_CALL_MAP, ep->lxc_id);
 	return DROP_MISSED_TAIL_CALL;
+#endif
 }
 
 static inline __u8 __inline__ get_encrypt_key(__u32 ctx)

bpf/lib/common.h
@@ -307,6 +307,7 @@ enum {
 #define MARK_MAGIC_HOST			0x0C00
 #define MARK_MAGIC_DECRYPT		0x0D00
 #define MARK_MAGIC_ENCRYPT		0x0E00
+#define MARK_MAGIC_IDENTITY		0x0F00 /* mark carries identity */
 #define MARK_MAGIC_TO_PROXY		0x0200
 
 #define MARK_MAGIC_KEY_ID		0xF000

bpf/lib/policy.h
@@ -37,6 +37,8 @@ static inline bool __inline__ inherit_identity_from_host(struct __sk_buff *skb, 
 	} else if (magic == MARK_MAGIC_PROXY_EGRESS) {
 		*identity = get_identity(skb);
 		from_proxy = true;
+	} else if (magic == MARK_MAGIC_IDENTITY) {
+		*identity = get_identity(skb);
 	} else if (magic == MARK_MAGIC_HOST) {
 		*identity = HOST_ID;
 	} else {

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=9dc2aa6d3b45c06ea551a7c030735aa9f2c5fca6
+GO_BINDATA_SHA1SUM=224445dc494d3c31e1791413383bf26112bcd426
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h ../bpf/spawn_netns.sh 

pkg/datapath/linux/config.go
@@ -232,6 +232,10 @@ func (l *linuxDatapath) WriteEndpointConfig(w io.Writer, e datapath.EndpointConf
 }
 
 func (l *linuxDatapath) writeTemplateConfig(fw *bufio.Writer, e datapath.EndpointConfiguration) error {
+	if e.RequireEgressProg() {
+		fmt.Fprintf(fw, "#define USE_BPF_PROG_FOR_INGRESS_POLICY 1\n")
+	}
+
 	if !e.HasIpvlanDataPath() {
 		if e.RequireARPPassthrough() {
 			fmt.Fprint(fw, "#define ENABLE_ARP_PASSTHROUGH 1\n")

commit 2c5ab74f895a81e4f45cffca14394ac63f184fce

bpf: Fix clustermesh policy with endpoint-routes
In endpoint-routes mode, we encode the source identity in the ctx->mark
when locally routing the packet to the destination device for ingress
policy assessment. Previously we only encoded the local cluster
identity in the mark, thereby omitting the original cluster portion of
the identity.

Found by code inspection.

Fixes: 8267885b071e ("bpf: Skip ingress policy at egress of source if egress prog is in use")
Signed-off-by: Joe Stringer <joe@cilium.io>

bpf/lib/l3.h
@@ -86,7 +86,8 @@ static __always_inline int ipv6_local_delivery(struct __ctx_buff *ctx, int l3_of
 
 #if defined(USE_BPF_PROG_FOR_INGRESS_POLICY) && \
 	!defined(FORCE_LOCAL_POLICY_EVAL_AT_SOURCE)
-	ctx->mark = (seclabel << 16) | MARK_MAGIC_IDENTITY;
+	ctk->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, seclabel)
 	return redirect_peer(ep->ifindex, 0);
 #else
 	ctx_store_meta(ctx, CB_SRC_LABEL, seclabel);
@@ -126,7 +127,8 @@ static __always_inline int ipv4_local_delivery(struct __ctx_buff *ctx, int l3_of
 
 #if defined(USE_BPF_PROG_FOR_INGRESS_POLICY) && \
 	!defined(FORCE_LOCAL_POLICY_EVAL_AT_SOURCE)
-	ctx->mark = (seclabel << 16) | MARK_MAGIC_IDENTITY;
+	ctk->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, seclabel)
 	return redirect_peer(ep->ifindex, 0);
 #else
 	ctx_store_meta(ctx, CB_SRC_LABEL, seclabel);