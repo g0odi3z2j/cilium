g0odi3z2j/cilium

commit cef5634f3703d2953cb3e0785712afd75d9a9662

daemon: expose HTTP endpoint on localhost for health checks
This will be used to replace liveness/readiness probes currently
invoking `cilium status --brief`.

HTTP 200 corresponds to `cilium status --brief` printing `OK` and
exiting with status 0.

HTTP 500 corresponds to `cilium status --brief` printing an error and
exiting with status 1.

Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

daemon/cmd/agenthealth.go
@@ -1,0 +1,89 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"syscall"
+
+	"github.com/cilium/cilium/api/v1/models"
+
+	"golang.org/x/sys/unix"
+)
+
+func setsockoptReuseAddrAndPort(network, address string, c syscall.RawConn) error {
+	var soerr error
+	if err := c.Control(func(su uintptr) {
+		s := int(su)
+		// Allow reuse of recently-used addresses. This socket option is
+		// set by default on listeners in Go's net package, see
+		// net setDefaultListenerSockopts
+		soerr = unix.SetsockoptInt(s, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+		if soerr != nil {
+			return
+		}
+		// Allow reuse of recently-used ports. This gives the agent a
+		// better change to re-bind upon restarts.
+		soerr = unix.SetsockoptInt(s, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+	}); err != nil {
+		return err
+	}
+	return soerr
+}
+
+// startAgentHealthHTTPService registers a handler function for the /healthz
+// status HTTP endpoint exposed on addr. This endpoint reports the agent health
+// status and is equivalent to what the `cilium status --brief` CLI tool reports.
+func (d *Daemon) startAgentHealthHTTPService(addr string) {
+	lc := net.ListenConfig{Control: setsockoptReuseAddrAndPort}
+	ln, err := lc.Listen(context.Background(), "tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		isUnhealthy := func(sr *models.StatusResponse) bool {
+			if sr.Cilium != nil {
+				state := sr.Cilium.State
+				return state != models.StatusStateOk && state != models.StatusStateDisabled
+			}
+			return false
+		}
+
+		statusCode := http.StatusOK
+		sr := d.getStatus(true)
+		if isUnhealthy(&sr) {
+			statusCode = http.StatusInternalServerError
+		}
+		w.WriteHeader(statusCode)
+	}))
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	go func() {
+		err := srv.Serve(ln)
+		if err == http.ErrServerClosed {
+			log.Info("healthz status API server shutdown")
+		} else if err != nil {
+			log.WithError(err).Fatal("Unable to start status healthz status API server")
+		}
+	}()
+	log.Infof("Started healthz status API server on address %s", addr)
+}

daemon/cmd/daemon_main.go
@@ -190,6 +190,9 @@ func init() {
 	})
 
 	// Env bindings
+	flags.Int(option.AgentHealthPort, defaults.AgentHealthPort, "TCP port for agent health status API")
+	option.BindEnv(option.AgentHealthPort)
+
 	flags.StringSlice(option.AgentLabels, []string{}, "Additional labels to identify this agent")
 	option.BindEnv(option.AgentLabels)
 
@@ -1332,6 +1335,8 @@ func runDaemon() {
 	d.startStatusCollector()
 
 	metricsErrs := initMetrics()
+
+	d.startAgentHealthHTTPService(fmt.Sprintf("localhost:%d", option.Config.AgentHealthPort))
 
 	bootstrapStats.initAPI.Start()
 	srv := server.NewServer(d.instantiateAPI())

install/kubernetes/cilium/charts/config/templates/configmap.yaml
@@ -69,6 +69,12 @@ data:
   debug-verbose: "{{ .Values.global.debug.verbose }}"
 {{- end }}
 
+{{- if .Values.global.agent.healthPort }}
+  # Set the TCP port for the agent health status API. This is not the port used
+  # for cilium-health.
+  agent-health-port: "{{ .Values.global.agent.healthPort }}"
+{{- end }}
+
 {{- if .Values.global.prometheus.enabled }}
   # If you want metrics enabled in all of your Cilium agents, set the port for
   # which the Cilium agents will have their metrics exposed.

install/kubernetes/cilium/values.yaml
@@ -94,6 +94,10 @@ global:
   # verbose allows additional levels of debug/trace messaging
   # verbose: flow
 
+  agent:
+    # TCP port for the agent health API. This is not the port for cilium-health.
+    healthPort: 9876
+
   # prometheus enables
   prometheus:
     enabled: false

install/kubernetes/quick-install.yaml
@@ -42,6 +42,9 @@ data:
 
   # If you want to run cilium in debug mode change this value to true
   debug: "false"
+  # Set the TCP port for the agent health status API. This is not the port used
+  # for cilium-health.
+  agent-health-port: "9876"
 
   # Enable IPv4 addressing. If enabled, all endpoints are allocated an IPv4
   # address.

pkg/defaults/defaults.go
@@ -19,6 +19,9 @@ import (
 )
 
 const (
+	// AgentHealthPort is the default value for option.AgentHealthPort
+	AgentHealthPort = 9876
+
 	// IPv6ClusterAllocCIDR is the default value for option.IPv6ClusterAllocCIDR
 	IPv6ClusterAllocCIDR = IPv6ClusterAllocCIDRBase + "/64"
 

pkg/option/config.go
@@ -57,6 +57,9 @@ type FlagsSection struct {
 }
 
 const (
+	// AgentHealthPort is the TCP port for the agent health status API.
+	AgentHealthPort = "agent-health-port"
+
 	// AgentLabels are additional labels to identify this agent
 	AgentLabels = "agent-labels"
 
@@ -1253,6 +1256,9 @@ type DaemonConfig struct {
 	// Monitor contains the configuration for the node monitor.
 	Monitor *models.MonitorStatus
 
+	// AgentHealthPort is the TCP port for the agent health status API.
+	AgentHealthPort int
+
 	// AgentLabels contains additional labels to identify this agent in monitor events.
 	AgentLabels []string
 
@@ -2140,6 +2146,7 @@ func (c *DaemonConfig) parseExcludedLocalAddresses(s []string) error {
 func (c *DaemonConfig) Populate() {
 	var err error
 
+	c.AgentHealthPort = viper.GetInt(AgentHealthPort)
 	c.AgentLabels = viper.GetStringSlice(AgentLabels)
 	c.AllowICMPFragNeeded = viper.GetBool(AllowICMPFragNeeded)
 	c.AllowLocalhost = viper.GetString(AllowLocalhost)

commit df26949733b9bff66b64d32d06d3814dd68a8e13

daemon: report HTTP service unavailable (503) in unhealthy state
Report HTTP status "service unavailable" (503) instead of "internal
server error" (500) on liveness/readiness probs in case the agent is
unhealthy.

Fixes: cef5634f3703 ("daemon: expose HTTP endpoint on localhost for health checks")
Reported-by: André Martins <andre@cilium.io>
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

daemon/cmd/agenthealth.go
@@ -69,7 +69,7 @@ func (d *Daemon) startAgentHealthHTTPService(addr string) {
 		statusCode := http.StatusOK
 		sr := d.getStatus(true)
 		if isUnhealthy(&sr) {
-			statusCode = http.StatusInternalServerError
+			statusCode = http.StatusServiceUnavailable
 		}
 		w.WriteHeader(statusCode)
 	}))