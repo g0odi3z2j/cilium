g0odi3z2j/cilium

commit 8941a1339da189b74c0a1f2b4bb3df923ac3cc58

agent: Add an option to cilium-agent for disabling 'HealthCheckNodePort'
This PR adds in an option 'enable-health-check-nodeport' to the
cilium-agent for disabling 'HealthCheckNodePort' based on the
KubeProxyReplacement configuration.
It also adds in an opton 'enableHealthCheckNodePort' to the helm
charts and documents the new config options with impacts when
kubeProxyReplacement is set to 'partial'.

Fixes: #11168
Signed-off-by: Swaminathan Vasudevan <svasudevan@suse.com>

pkg/service/service.go
@@ -129,13 +129,18 @@ type Service struct {
 
 // NewService creates a new instance of the service handler.
 func NewService(monitorNotify monitorNotify) *Service {
+
+	var localHealthServer healthServer
+	if option.Config.EnableHealthCheckNodePort {
+		localHealthServer = healthserver.New()
+	}
 	return &Service{
 		svcByHash:       map[string]*svcInfo{},
 		svcByID:         map[lb.ID]*svcInfo{},
 		backendRefCount: counter.StringCounter{},
 		backendByHash:   map[string]*lb.Backend{},
 		monitorNotify:   monitorNotify,
-		healthServer:    healthserver.New(),
+		healthServer:    localHealthServer,
 		lbmap:           &lbmap.LBBPFMap{},
 	}
 }
@@ -266,8 +271,10 @@ func (s *Service) UpsertService(
 	// only contain local backends (i.e. it has externalTrafficPolicy=Local)
 	if onlyLocalBackends && filterBackends {
 		localBackendCount := len(backendsCopy)
-		s.healthServer.UpsertService(lb.ID(svc.frontend.ID), svc.svcNamespace, svc.svcName,
-			localBackendCount, svc.svcHealthCheckNodePort)
+		if option.Config.EnableHealthCheckNodePort {
+			s.healthServer.UpsertService(lb.ID(svc.frontend.ID), svc.svcNamespace, svc.svcName,
+				localBackendCount, svc.svcHealthCheckNodePort)
+		}
 	} else if svc.svcHealthCheckNodePort == 0 {
 		// Remove the health check server in case this service used to have
 		// externalTrafficPolicy=Local with HealthCheckNodePort in the previous
@@ -761,7 +768,9 @@ func (s *Service) deleteServiceLocked(svc *svcInfo) error {
 		return fmt.Errorf("Unable to release service ID %d: %s", svc.frontend.ID, err)
 	}
 
-	s.healthServer.DeleteService(lb.ID(svc.frontend.ID))
+	if option.Config.EnableHealthCheckNodePort {
+		s.healthServer.DeleteService(lb.ID(svc.frontend.ID))
+	}
 
 	deleteMetric.Inc()
 	s.notifyMonitorServiceDelete(svc.frontend.ID)

daemon/cmd/daemon_main.go
@@ -282,6 +282,9 @@ func init() {
 	flags.Bool(option.EnableHealthChecking, defaults.EnableHealthChecking, "Enable connectivity health checking")
 	option.BindEnv(option.EnableHealthChecking)
 
+	flags.Bool(option.EnableHealthCheckNodePort, defaults.EnableHealthCheckNodePort, "Enables a healthcheck nodePort server for NodePort services with 'healthCheckNodePort' being set")
+	option.BindEnv(option.EnableHealthCheckNodePort)
+
 	flags.StringSlice(option.EndpointStatus, []string{},
 		"Enable additional CiliumEndpoint status features ("+strings.Join(option.EndpointStatusValues(), ",")+")")
 	option.BindEnv(option.EndpointStatus)

install/kubernetes/cilium/charts/config/templates/configmap.yaml
@@ -423,7 +423,7 @@ data:
 
 {{- if .Values.global.kubeProxyReplacement }}
   kube-proxy-replacement:  {{ .Values.global.kubeProxyReplacement | quote }}
-{{- end}}
+{{- end }}
 {{- if .Values.global.hostServices }}
 {{- if .Values.global.hostServices.enabled }}
   enable-host-reachable-services: {{ .Values.global.hostServices.enabled | quote }}
@@ -454,6 +454,9 @@ data:
 {{- end }}
 {{- if .Values.global.nodePort.mode }}
   node-port-mode: {{ .Values.global.nodePort.mode | quote }}
+{{- end }}
+{{- if .Values.global.nodePort.enableHealthCheck }}
+  enable-health-check-nodeport: {{ .Values.global.nodePort.enableHealthCheck | quote}}
 {{- end }}
 {{- if .Values.global.nodePort.acceleration }}
   node-port-acceleration: {{ .Values.global.nodePort.acceleration | quote }}

install/kubernetes/cilium/values.yaml
@@ -356,6 +356,9 @@ global:
     # ports is detected
     autoProtectPortRange: true
 
+    # enableHealthCheck enables healthcheck nodePort server for NodePort services
+    enableHealthCheck: true
+
   # hostPort is the configuration for container hostPort mapping
   hostPort:
     # enabled enables the hostPort functionality

install/kubernetes/experimental-install.yaml
@@ -137,6 +137,7 @@ data:
   install-iptables-rules: "true"
   auto-direct-node-routes: "false"
   kube-proxy-replacement:  "probe"
+  enable-health-check-nodeport: "true"
   node-port-bind-protection: "true"
   enable-auto-protect-node-port-range: "true"
   enable-session-affinity: "true"

install/kubernetes/quick-install.yaml
@@ -130,6 +130,7 @@ data:
   install-iptables-rules: "true"
   auto-direct-node-routes: "false"
   kube-proxy-replacement:  "probe"
+  enable-health-check-nodeport: "true"
   node-port-bind-protection: "true"
   enable-auto-protect-node-port-range: "true"
   enable-session-affinity: "true"

pkg/defaults/defaults.go
@@ -198,6 +198,10 @@ const (
 	// EnableEndpointHealthChecking
 	EnableEndpointHealthChecking = true
 
+	// EnableHealthCheckNodePort is the default value for
+	// EnableHealthCheckNodePort
+	EnableHealthCheckNodePort = true
+
 	// AlignCheckerName is the BPF object name for the alignchecker.
 	AlignCheckerName = "bpf_alignchecker.o"
 

pkg/option/config.go
@@ -618,6 +618,9 @@ const (
 	// EnableEndpointHealthChecking is the name of the EnableEndpointHealthChecking option
 	EnableEndpointHealthChecking = "enable-endpoint-health-checking"
 
+	// EnableHealthCheckNodePort is the name of the EnableHealthCheckNodePort option
+	EnableHealthCheckNodePort = "enable-health-check-nodeport"
+
 	// PolicyQueueSize is the size of the queues utilized by the policy
 	// repository.
 	PolicyQueueSize = "policy-queue-size"
@@ -1586,6 +1589,10 @@ type DaemonConfig struct {
 	// health endpoints
 	EnableEndpointHealthChecking bool
 
+	// EnableHealthCheckNodePort enables health checking of NodePort by
+	// cilium
+	EnableHealthCheckNodePort bool
+
 	// KVstoreKeepAliveInterval is the interval in which the lease is being
 	// renewed. This must be set to a value lesser than the LeaseTTL ideally
 	// by a factor of 3.
@@ -1866,6 +1873,7 @@ var (
 		EnableHostIPRestore:          defaults.EnableHostIPRestore,
 		EnableHealthChecking:         defaults.EnableHealthChecking,
 		EnableEndpointHealthChecking: defaults.EnableEndpointHealthChecking,
+		EnableHealthCheckNodePort:    defaults.EnableHealthCheckNodePort,
 		EnableIPv4:                   defaults.EnableIPv4,
 		EnableIPv6:                   defaults.EnableIPv6,
 		EnableL7Proxy:                defaults.EnableL7Proxy,
@@ -2240,6 +2248,7 @@ func (c *DaemonConfig) Populate() {
 	c.EnableEndpointRoutes = viper.GetBool(EnableEndpointRoutes)
 	c.EnableHealthChecking = viper.GetBool(EnableHealthChecking)
 	c.EnableEndpointHealthChecking = viper.GetBool(EnableEndpointHealthChecking)
+	c.EnableHealthCheckNodePort = viper.GetBool(EnableHealthCheckNodePort)
 	c.EnableLocalNodeRoute = viper.GetBool(EnableLocalNodeRoute)
 	c.EnablePolicy = strings.ToLower(viper.GetString(EnablePolicy))
 	c.EnableExternalIPs = viper.GetBool(EnableExternalIPs)

commit d66a82869b7d3a3b142e628109acab1ff23feaa1

service: Fix panic when restoring services with enable-health-check-nodeport: false
We do not instanciate the service health server if HealthCheckNodePort
support is disabled in our kube-proxy replacement. This means that we
need to check the `EnableHealthCheckNodePort` flag before accessing the
service health server to avoid a nil pointer panic.

Fixes: 8941a1339da1 ("agent: Add an option to cilium-agent for disabling 'HealthCheckNodePort'")
Fixes: #13178

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/service/service.go
@@ -278,17 +278,17 @@ func (s *Service) UpsertService(params *lb.SVC) (bool, lb.ID, error) {
 
 	// Only add a HealthCheckNodePort server if this is a service which may
 	// only contain local backends (i.e. it has externalTrafficPolicy=Local)
-	if onlyLocalBackends && filterBackends {
-		localBackendCount := len(backendsCopy)
-		if option.Config.EnableHealthCheckNodePort {
+	if option.Config.EnableHealthCheckNodePort {
+		if onlyLocalBackends && filterBackends {
+			localBackendCount := len(backendsCopy)
 			s.healthServer.UpsertService(lb.ID(svc.frontend.ID), svc.svcNamespace, svc.svcName,
 				localBackendCount, svc.svcHealthCheckNodePort)
+		} else if svc.svcHealthCheckNodePort == 0 {
+			// Remove the health check server in case this service used to have
+			// externalTrafficPolicy=Local with HealthCheckNodePort in the previous
+			// version, but not anymore.
+			s.healthServer.DeleteService(lb.ID(svc.frontend.ID))
 		}
-	} else if svc.svcHealthCheckNodePort == 0 {
-		// Remove the health check server in case this service used to have
-		// externalTrafficPolicy=Local with HealthCheckNodePort in the previous
-		// version, but not anymore.
-		s.healthServer.DeleteService(lb.ID(svc.frontend.ID))
 	}
 
 	if new {