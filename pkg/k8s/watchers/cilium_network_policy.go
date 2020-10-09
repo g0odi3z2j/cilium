g0odi3z2j/cilium

commit d7211e9d7a1cf150aaa5f58aa862f5b52c3ace1c

k8s/watchers: fix missing missing CNP/CCNP updates
Since we were only comparing the spec of a CNP / CCNP, whenever the
cilium-agent received a CNP or CCNP containing a 'specs', any following
updates to that policy wouldn't be enforced in Cilium since the network
policy would be considered equal as the one already being enforced.

As Cilium is already comparing the network policies, before processing
any policy update, we can remove this Spec.Equals.

Fixes: 7a896780fef5 ("use deepequal generated code")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/cilium_network_policy.go
@@ -288,27 +288,24 @@ func (k *K8sWatcher) updateCiliumNetworkPolicyV2(ciliumNPClient clientset.Interf
 
 	// Do not add rule into policy repository if the spec remains unchanged.
 	if !option.Config.DisableCNPStatusUpdates {
-		if oldRuleCpy.Spec.DeepEqual(newRuleCpy.CiliumNetworkPolicy.Spec) {
-			if !oldRuleCpy.AnnotationsEquals(newRuleCpy.CiliumNetworkPolicy) {
+		if !oldRuleCpy.AnnotationsEquals(newRuleCpy.CiliumNetworkPolicy) {
 
-				// Update annotations within a controller so the status of the update
-				// is trackable from the list of running controllers, and so we do
-				// not block subsequent policy lifecycle operations from Kubernetes
-				// until the update is complete.
-				oldCtrlName := oldRuleCpy.GetControllerName()
-				newCtrlName := newRuleCpy.GetControllerName()
+			// Update annotations within a controller so the status of the update
+			// is trackable from the list of running controllers, and so we do
+			// not block subsequent policy lifecycle operations from Kubernetes
+			// until the update is complete.
+			oldCtrlName := oldRuleCpy.GetControllerName()
+			newCtrlName := newRuleCpy.GetControllerName()
 
-				// In case the controller name changes between copies of rules,
-				// remove old controller so we do not leak goroutines.
-				if oldCtrlName != newCtrlName {
-					err := k8sCM.RemoveController(oldCtrlName)
-					if err != nil {
-						log.Debugf("Unable to remove controller %s: %s", oldCtrlName, err)
-					}
+			// In case the controller name changes between copies of rules,
+			// remove old controller so we do not leak goroutines.
+			if oldCtrlName != newCtrlName {
+				err := k8sCM.RemoveController(oldCtrlName)
+				if err != nil {
+					log.Debugf("Unable to remove controller %s: %s", oldCtrlName, err)
 				}
-				k.updateCiliumNetworkPolicyV2AnnotationsOnly(ciliumNPClient, ciliumV2Store, newRuleCpy)
 			}
-			return nil
+			k.updateCiliumNetworkPolicyV2AnnotationsOnly(ciliumNPClient, ciliumV2Store, newRuleCpy)
 		}
 	}
 

commit a0f19404fd6d3566a1ac9bfcdc511f111ed67452

k8s/watcher: check if spec and specs are equal
If both Spec and Specs are considered the same we don't need to re-add
the CNP into the policy repository. The commit d7211e9d7a1c should have
added the Specs.DeepEqual and not remove the Spec.DeepEqual.

Fixes: d7211e9d7a1c ("k8s/watchers: fix missing missing CNP/CCNP updates")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/cilium_network_policy.go
@@ -301,24 +301,28 @@ func (k *K8sWatcher) updateCiliumNetworkPolicyV2(ciliumNPClient clientset.Interf
 
 	// Do not add rule into policy repository if the spec remains unchanged.
 	if !option.Config.DisableCNPStatusUpdates {
-		if !oldRuleCpy.AnnotationsEquals(newRuleCpy.CiliumNetworkPolicy) {
+		if oldRuleCpy.Spec.DeepEqual(newRuleCpy.CiliumNetworkPolicy.Spec) &&
+			oldRuleCpy.Specs.DeepEqual(&newRuleCpy.CiliumNetworkPolicy.Specs) {
+			if !oldRuleCpy.AnnotationsEquals(newRuleCpy.CiliumNetworkPolicy) {
 
-			// Update annotations within a controller so the status of the update
-			// is trackable from the list of running controllers, and so we do
-			// not block subsequent policy lifecycle operations from Kubernetes
-			// until the update is complete.
-			oldCtrlName := oldRuleCpy.GetControllerName()
-			newCtrlName := newRuleCpy.GetControllerName()
+				// Update annotations within a controller so the status of the update
+				// is trackable from the list of running controllers, and so we do
+				// not block subsequent policy lifecycle operations from Kubernetes
+				// until the update is complete.
+				oldCtrlName := oldRuleCpy.GetControllerName()
+				newCtrlName := newRuleCpy.GetControllerName()
 
-			// In case the controller name changes between copies of rules,
-			// remove old controller so we do not leak goroutines.
-			if oldCtrlName != newCtrlName {
-				err := k8sCM.RemoveController(oldCtrlName)
-				if err != nil {
-					log.Debugf("Unable to remove controller %s: %s", oldCtrlName, err)
+				// In case the controller name changes between copies of rules,
+				// remove old controller so we do not leak goroutines.
+				if oldCtrlName != newCtrlName {
+					err := k8sCM.RemoveController(oldCtrlName)
+					if err != nil {
+						log.Debugf("Unable to remove controller %s: %s", oldCtrlName, err)
+					}
 				}
+				k.updateCiliumNetworkPolicyV2AnnotationsOnly(ciliumNPClient, ciliumV2Store, newRuleCpy)
 			}
-			k.updateCiliumNetworkPolicyV2AnnotationsOnly(ciliumNPClient, ciliumV2Store, newRuleCpy)
+			return nil
 		}
 	}
 