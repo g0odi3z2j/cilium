g0odi3z2j/cilium

commit 0203941c0def54f042cb33da65243a11cfabbcb2

azure: Calculate available addresses based on subnet resource
Using the subnet resource allows to avoid using an allocator to scan all
used addresses.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/azure/ipam/instances.go
@@ -19,7 +19,6 @@ import (
 	"time"
 
 	"github.com/cilium/cilium/pkg/ipam"
-	"github.com/cilium/cilium/pkg/ipam/allocator"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/lock"
@@ -40,8 +39,8 @@ type InstancesManager struct {
 	mutex     lock.RWMutex
 	instances *ipamTypes.InstanceMap
 	vnets     ipamTypes.VirtualNetworkMap
+	subnets   ipamTypes.SubnetMap
 	api       AzureAPI
-	allocator allocator.Allocator
 }
 
 // NewInstancesManager returns a new instances manager
@@ -49,15 +48,7 @@ func NewInstancesManager(api AzureAPI) *InstancesManager {
 	return &InstancesManager{
 		instances: ipamTypes.NewInstanceMap(),
 		api:       api,
-		allocator: &allocator.NoOpAllocator{},
 	}
-}
-
-func (m *InstancesManager) getAllocator() (allocator allocator.Allocator) {
-	m.mutex.RLock()
-	allocator = m.allocator
-	m.mutex.RUnlock()
-	return
 }
 
 // CreateNode is called on discovery of a new node
@@ -67,7 +58,22 @@ func (m *InstancesManager) CreateNode(obj *v2.CiliumNode, n *ipam.Node) ipam.Nod
 
 // GetPoolQuota returns the number of available IPs in all IP pools
 func (m *InstancesManager) GetPoolQuota() (quota ipamTypes.PoolQuotaMap) {
-	return m.getAllocator().GetPoolQuota()
+	m.mutex.RLock()
+	pool := ipamTypes.PoolQuotaMap{}
+	for subnetID, subnet := range m.subnets {
+		pool[ipamTypes.PoolID(subnetID)] = ipamTypes.PoolQuota{
+			AvailableIPs: subnet.AvailableAddresses,
+		}
+	}
+	m.mutex.RUnlock()
+	return pool
+}
+
+// GetPoolQuota returns the number of available IPs in all IP pools
+func (m *InstancesManager) FindSubnetForAllocation(preferredPoolIDs []ipamTypes.PoolID) (ipamTypes.PoolID, int) {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	return m.subnets.FirstSubnetWithAvailableAddresses(preferredPoolIDs)
 }
 
 // Resync fetches the list of EC2 instances and subnets and updates the local
@@ -94,20 +100,10 @@ func (m *InstancesManager) Resync(ctx context.Context) time.Time {
 		"numSubnets":         len(subnets),
 	}).Info("Synchronized Azure IPAM information")
 
-	groupAllocator, err := allocator.NewPoolGroupAllocator(subnets)
-	if err != nil {
-		log.WithError(err).Warning("Unable to create allocator")
-		return time.Time{}
-	}
-
-	// Iterate over all addresses of all instances and reserve them in the
-	// allocator
-	groupAllocator.ReserveAddresses(instances)
-
 	m.mutex.Lock()
 	m.instances = instances
 	m.vnets = vnets
-	m.allocator = groupAllocator
+	m.subnets = subnets
 	m.mutex.Unlock()
 
 	return resyncStart

pkg/azure/ipam/node.go
@@ -97,7 +97,7 @@ func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (a *ipam.AllocationA
 				}
 			}
 
-			poolID, available := n.manager.getAllocator().FirstPoolWithAvailableQuota(preferredPoolIDs)
+			poolID, available := n.manager.FindSubnetForAllocation(preferredPoolIDs)
 			if poolID != ipamTypes.PoolNotExists {
 				scopedLog.WithFields(logrus.Fields{
 					"subnetID":           poolID,

pkg/azure/api/api.go
@@ -247,6 +247,9 @@ func parseSubnet(subnet *network.Subnet) (s *ipamTypes.Subnet) {
 			return nil
 		}
 		s.CIDR = c
+		if subnet.IPConfigurations != nil {
+			s.AvailableAddresses = c.AvailableIPs() - len(*subnet.IPConfigurations)
+		}
 	}
 
 	return

pkg/azure/api/mock/mock.go
@@ -165,7 +165,9 @@ func (a *API) GetVpcsAndSubnets(ctx context.Context) (ipamTypes.VirtualNetworkMa
 	subnets := ipamTypes.SubnetMap{}
 
 	for _, s := range a.subnets {
-		subnets[s.subnet.ID] = s.subnet.DeepCopy()
+		sd := s.subnet.DeepCopy()
+		sd.AvailableAddresses = s.allocator.Free()
+		subnets[sd.ID] = sd
 	}
 
 	for _, v := range a.vnets {

pkg/azure/api/mock/mock_test.go
@@ -38,7 +38,7 @@ type MockSuite struct{}
 var _ = check.Suite(&MockSuite{})
 
 func (e *MockSuite) TestMock(c *check.C) {
-	subnet := &ipamTypes.Subnet{ID: "s-1", CIDR: cidr.MustParseCIDR("10.0.0.0/16"), AvailableAddresses: 100}
+	subnet := &ipamTypes.Subnet{ID: "s-1", CIDR: cidr.MustParseCIDR("10.0.0.0/16"), AvailableAddresses: 65534}
 	api := NewAPI([]*ipamTypes.Subnet{subnet}, []*ipamTypes.VirtualNetwork{{ID: "v-1"}})
 	c.Assert(api, check.Not(check.IsNil))
 

pkg/azure/ipam/instances_test.go
@@ -18,7 +18,6 @@ package ipam
 
 import (
 	"context"
-	"math"
 
 	apimock "github.com/cilium/cilium/pkg/azure/api/mock"
 	"github.com/cilium/cilium/pkg/azure/types"
@@ -175,48 +174,19 @@ func (e *IPAMSuite) TestGetVpcsAndSubnets(c *check.C) {
 	mngr := NewInstancesManager(api)
 	c.Assert(mngr, check.Not(check.IsNil))
 
-	c.Assert(mngr.getAllocator().PoolExists("subnet-1"), check.Equals, false)
-	c.Assert(mngr.getAllocator().PoolExists("subnet-2"), check.Equals, false)
-	c.Assert(mngr.getAllocator().PoolExists("subnet-3"), check.Equals, false)
+	c.Assert(mngr.subnets["subnet-1"], check.IsNil)
+	c.Assert(mngr.subnets["subnet-2"], check.IsNil)
+	c.Assert(mngr.subnets["subnet-3"], check.IsNil)
 
 	iteration1(api, mngr)
 
-	c.Assert(mngr.getAllocator().PoolExists("subnet-1"), check.Equals, true)
-	c.Assert(mngr.getAllocator().PoolExists("subnet-2"), check.Equals, true)
-	c.Assert(mngr.getAllocator().PoolExists("subnet-3"), check.Equals, false)
+	c.Assert(mngr.subnets["subnet-1"], check.Not(check.IsNil))
+	c.Assert(mngr.subnets["subnet-2"], check.Not(check.IsNil))
+	c.Assert(mngr.subnets["subnet-3"], check.IsNil)
 
 	iteration2(api, mngr)
 
-	c.Assert(mngr.getAllocator().PoolExists("subnet-1"), check.Equals, true)
-	c.Assert(mngr.getAllocator().PoolExists("subnet-2"), check.Equals, true)
-	c.Assert(mngr.getAllocator().PoolExists("subnet-3"), check.Equals, true)
-}
-
-func (e *IPAMSuite) TestPoolQuota(c *check.C) {
-	api := apimock.NewAPI(subnets, vnets)
-	c.Assert(api, check.Not(check.IsNil))
-
-	mngr := NewInstancesManager(api)
-	c.Assert(mngr, check.Not(check.IsNil))
-
-	quota := mngr.GetPoolQuota()
-	c.Assert(len(quota), check.Equals, 0)
-
-	iteration1(api, mngr)
-	quota = mngr.GetPoolQuota()
-	c.Assert(len(quota), check.Equals, 2)
-	// 2 IPs should be allocated
-	c.Assert(quota["subnet-1"].AvailableIPs, check.Equals, int(math.Pow(2.0, 16.0)-4))
-	// No IPs should be allocated
-	c.Assert(quota["subnet-2"].AvailableIPs, check.Equals, int(math.Pow(2.0, 16.0)-2))
-
-	iteration2(api, mngr)
-	quota = mngr.GetPoolQuota()
-	c.Assert(len(quota), check.Equals, 3)
-	// 2 IPs should be allocated
-	c.Assert(quota["subnet-1"].AvailableIPs, check.Equals, int(math.Pow(2.0, 16.0)-4))
-	// No IP should be allocated
-	c.Assert(quota["subnet-2"].AvailableIPs, check.Equals, int(math.Pow(2.0, 16.0)-2))
-	// 1 IP should be allocated
-	c.Assert(quota["subnet-3"].AvailableIPs, check.Equals, int(math.Pow(2.0, 16.0)-3))
+	c.Assert(mngr.subnets["subnet-1"], check.Not(check.IsNil))
+	c.Assert(mngr.subnets["subnet-2"], check.Not(check.IsNil))
+	c.Assert(mngr.subnets["subnet-3"], check.Not(check.IsNil))
 }

pkg/azure/ipam/ipam_test.go
@@ -19,6 +19,7 @@ package ipam
 import (
 	"context"
 	"fmt"
+	"math"
 	"testing"
 	"time"
 
@@ -275,6 +276,10 @@ func (e *IPAMSuite) TestIpamMinAllocate10(c *check.C) {
 	c.Assert(node, check.Not(check.IsNil))
 	c.Assert(node.Stats().AvailableIPs, check.Equals, toUse+preAllocate)
 	c.Assert(node.Stats().UsedIPs, check.Equals, toUse)
+
+	quota := instances.GetPoolQuota()
+	c.Assert(len(quota), check.Equals, 1)
+	c.Assert(quota["subnet-1"].AvailableIPs, check.Equals, int(math.Pow(2.0, 16.0)-16))
 }
 
 type nodeState struct {

commit b0fde52d96169cfdb4de88a01f3003c8d1f8fe3a

pkg/azure: protect against potential deadlock
Fixes the following potential deadlock

```
POTENTIAL DEADLOCK: Inconsistent locking. saw this ordering in one goroutine:
happened before
pkg/lock/lock_debug.go:78 lock.(*internalRWMutex).RLock { i.RWMutex.RLock() } <<<<<
pkg/azure/ipam/node.go:162 ipam.(*Node).ResyncInterfacesAndIPs { n.manager.mutex.RLock() }
pkg/ipam/node.go:358 ipam.(*Node).recalculate { a, err := n.ops.ResyncInterfacesAndIPs(context.TODO(), scopedLog) }
pkg/ipam/node.go:334 ipam.(*Node).UpdatedResource { n.recalculate() }
pkg/ipam/node_manager.go:315 ipam.(*NodeManager).Update { return node.UpdatedResource(resource) }
pkg/azure/ipam/ipam_test.go:345 ipam.(*IPAMSuite).TestIpamManyNodes { mngr.Update(state[i].cn) }
~/.gimme/versions/go1.15.2.linux.amd64/src/reflect/value.go:475 reflect.Value.call { call(frametype, fn, args, uint32(frametype.size), uint32(retOffset)) }
~/.gimme/versions/go1.15.2.linux.amd64/src/reflect/value.go:336 reflect.Value.Call { return v.call("Call", in) }
pkg/../vendor/gopkg.in/check.v1/check.go:781 check%2ev1.(*suiteRunner).forkTest.func1 { c.method.Call([]reflect.Value{reflect.ValueOf(c)}) }
pkg/../vendor/gopkg.in/check.v1/check.go:675 check%2ev1.(*suiteRunner).forkCall.func1 { dispatcher(c) }
happened after
pkg/lock/lock_debug.go:78 lock.(*internalRWMutex).RLock { i.RWMutex.RLock() } <<<<<
pkg/ipam/types/types.go:340 types.(*InstanceMap).ForeachAddress { m.mutex.RLock() }
pkg/azure/ipam/node.go:164 ipam.(*Node).ResyncInterfacesAndIPs { n.manager.instances.ForeachAddress(n.node.InstanceID(), func(instanceID, interfaceID, ip, poolID string, addressObj ipamTypes.Address) error { }
pkg/ipam/node.go:358 ipam.(*Node).recalculate { a, err := n.ops.ResyncInterfacesAndIPs(context.TODO(), scopedLog) }
pkg/ipam/node.go:334 ipam.(*Node).UpdatedResource { n.recalculate() }
pkg/ipam/node_manager.go:315 ipam.(*NodeManager).Update { return node.UpdatedResource(resource) }
pkg/azure/ipam/ipam_test.go:345 ipam.(*IPAMSuite).TestIpamManyNodes { mngr.Update(state[i].cn) }
~/.gimme/versions/go1.15.2.linux.amd64/src/reflect/value.go:475 reflect.Value.call { call(frametype, fn, args, uint32(frametype.size), uint32(retOffset)) }
~/.gimme/versions/go1.15.2.linux.amd64/src/reflect/value.go:336 reflect.Value.Call { return v.call("Call", in) }
pkg/../vendor/gopkg.in/check.v1/check.go:781 check%2ev1.(*suiteRunner).forkTest.func1 { c.method.Call([]reflect.Value{reflect.ValueOf(c)}) }
pkg/../vendor/gopkg.in/check.v1/check.go:675 check%2ev1.(*suiteRunner).forkCall.func1 { dispatcher(c) }
in another goroutine: happened before
pkg/lock/lock_debug.go:78 lock.(*internalRWMutex).RLock { i.RWMutex.RLock() } <<<<<
pkg/ipam/types/types.go:381 types.(*InstanceMap).ForeachInterface { m.mutex.RLock() }
pkg/azure/ipam/node.go:74 ipam.(*Node).PrepareIPAllocation { err = n.manager.instances.ForeachInterface(n.node.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error { }
pkg/ipam/node.go:548 ipam.(*Node).determineMaintenanceAction { a.allocation, err = n.ops.PrepareIPAllocation(scopedLog) }
pkg/ipam/node.go:584 ipam.(*Node).maintainIPPool { a, err := n.determineMaintenanceAction() }
pkg/ipam/node.go:678 ipam.(*Node).MaintainIPPool { err := n.maintainIPPool(ctx) }
pkg/ipam/node_manager.go:272 ipam.(*NodeManager).Update.func1 { if err := node.MaintainIPPool(context.TODO()); err != nil { }
pkg/trigger/trigger.go:206 trigger.(*Trigger).waiter { t.params.TriggerFunc(reasons) }
happened after
pkg/lock/lock_debug.go:78 lock.(*internalRWMutex).RLock { i.RWMutex.RLock() } <<<<<
pkg/azure/ipam/instances.go:75 ipam.(*InstancesManager).FindSubnetForAllocation { m.mutex.RLock() }
pkg/azure/ipam/node.go:115 ipam.(*Node).PrepareIPAllocation.func1 { poolID, available := n.manager.FindSubnetForAllocation(preferredPoolIDs) }
pkg/ipam/types/types.go:364 types.foreachInterface { if err := fn(instanceID, rev.Resource.InterfaceID(), rev); err != nil { }
pkg/ipam/types/types.go:386 types.(*InstanceMap).ForeachInterface { return foreachInterface(instanceID, instance, fn) }
pkg/azure/ipam/node.go:74 ipam.(*Node).PrepareIPAllocation { err = n.manager.instances.ForeachInterface(n.node.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error { }
pkg/ipam/node.go:548 ipam.(*Node).determineMaintenanceAction { a.allocation, err = n.ops.PrepareIPAllocation(scopedLog) }
pkg/ipam/node.go:584 ipam.(*Node).maintainIPPool { a, err := n.determineMaintenanceAction() }
pkg/ipam/node.go:678 ipam.(*Node).MaintainIPPool { err := n.maintainIPPool(ctx) }
pkg/ipam/node_manager.go:272 ipam.(*NodeManager).Update.func1 { if err := node.MaintainIPPool(context.TODO()); err != nil { }
pkg/trigger/trigger.go:206 trigger.(*Trigger).waiter { t.params.TriggerFunc(reasons) }
```

Fixes: 0203941c0def ("azure: Calculate available addresses based on subnet resource")

pkg/azure/ipam/instances.go
@@ -70,13 +70,6 @@ func (m *InstancesManager) GetPoolQuota() (quota ipamTypes.PoolQuotaMap) {
 	return pool
 }
 
-// GetPoolQuota returns the number of available IPs in all IP pools
-func (m *InstancesManager) FindSubnetForAllocation(preferredPoolIDs []ipamTypes.PoolID) (ipamTypes.PoolID, int) {
-	m.mutex.RLock()
-	defer m.mutex.RUnlock()
-	return m.subnets.FirstSubnetWithAvailableAddresses(preferredPoolIDs)
-}
-
 // Resync fetches the list of EC2 instances and subnets and updates the local
 // cache in the instanceManager. It returns the time when the resync has
 // started or time.Time{} if it did not complete.

pkg/azure/ipam/node.go
@@ -71,6 +71,8 @@ func (n *Node) ReleaseIPs(ctx context.Context, r *ipam.ReleaseAction) error {
 func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (a *ipam.AllocationAction, err error) {
 	a = &ipam.AllocationAction{}
 	requiredIfaceName := n.k8sObj.Spec.Azure.InterfaceName
+	n.manager.mutex.RLock()
+	defer n.manager.mutex.RUnlock()
 	err = n.manager.instances.ForeachInterface(n.node.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error {
 		iface, ok := interfaceObj.Resource.(*types.AzureInterface)
 		if !ok {
@@ -112,7 +114,7 @@ func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (a *ipam.AllocationA
 				}
 			}
 
-			poolID, available := n.manager.FindSubnetForAllocation(preferredPoolIDs)
+			poolID, available := n.manager.subnets.FirstSubnetWithAvailableAddresses(preferredPoolIDs)
 			if poolID != ipamTypes.PoolNotExists {
 				scopedLog.WithFields(logrus.Fields{
 					"subnetID":           poolID,