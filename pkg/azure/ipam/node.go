g0odi3z2j/cilium

commit f5f08842c367e6d8612c24f1c8b5a4c5355c34b9

ipam: Move instance ID retrieval into generic code
This removes the need to track the CiliumNode resource in the Azure
specific code entirely.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/azure/ipam/node.go
@@ -22,7 +22,6 @@ import (
 	"github.com/cilium/cilium/pkg/ipam"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
-	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/math"
 
 	"github.com/sirupsen/logrus"
@@ -30,13 +29,8 @@ import (
 
 // Node represents a node representing an Azure instance
 type Node struct {
-	mutex lock.RWMutex
-
 	// node contains the general purpose fields of a node
 	node *ipam.Node
-
-	// k8sObj is the CiliumNode custom resource representing the node
-	k8sObj *v2.CiliumNode
 
 	// manager is the Azure node manager responsible for this node
 	manager *InstancesManager
@@ -44,22 +38,19 @@ type Node struct {
 
 // UpdatedNode is called when an update to the CiliumNode is received.
 func (n *Node) UpdatedNode(obj *v2.CiliumNode) {
-	n.k8sObj = obj
 }
 
 // PopulateStatusFields fills in the status field of the CiliumNode custom
 // resource with Azure specific information
 func (n *Node) PopulateStatusFields(k8sObj *v2.CiliumNode) {
-	n.mutex.RLock()
 	k8sObj.Status.Azure.Interfaces = []types.AzureInterface{}
-	n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error {
+	n.manager.instances.ForeachInterface(n.node.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error {
 		iface, ok := interfaceObj.Resource.(*types.AzureInterface)
 		if ok {
 			k8sObj.Status.Azure.Interfaces = append(k8sObj.Status.Azure.Interfaces, *(iface.DeepCopy()))
 		}
 		return nil
 	})
-	n.mutex.RUnlock()
 }
 
 // PrepareIPRelease prepares the release of IPs
@@ -75,7 +66,7 @@ func (n *Node) ReleaseIPs(ctx context.Context, r *ipam.ReleaseAction) error {
 // PrepareIPAllocation returns the number of IPs that can be allocated/created.
 func (n *Node) PrepareIPAllocation(scopedLog *logrus.Entry) (a *ipam.AllocationAction, err error) {
 	a = &ipam.AllocationAction{}
-	err = n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error {
+	err = n.manager.instances.ForeachInterface(n.node.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error {
 		iface, ok := interfaceObj.Resource.(*types.AzureInterface)
 		if !ok {
 			return fmt.Errorf("invalid interface object")
@@ -149,15 +140,12 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 // ResyncInterfacesAndIPs is called to retrieve and interfaces and IPs as known
 // to the Azure API and return them
 func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Entry) (ipamTypes.AllocationMap, error) {
-	n.mutex.Lock()
-	defer n.mutex.Unlock()
-
-	if n.k8sObj.InstanceID() == "" {
+	if n.node.InstanceID() == "" {
 		return nil, nil
 	}
 
 	available := ipamTypes.AllocationMap{}
-	n.manager.instances.ForeachAddress(n.k8sObj.InstanceID(), func(instanceID, interfaceID, ip, poolID string, addressObj ipamTypes.Address) error {
+	n.manager.instances.ForeachAddress(n.node.InstanceID(), func(instanceID, interfaceID, ip, poolID string, addressObj ipamTypes.Address) error {
 		address, ok := addressObj.(types.AzureAddress)
 		if !ok {
 			log.WithField("ip", ip).Warning("Not an Azure address object, ignoring IP")

pkg/aws/eni/node.go
@@ -71,14 +71,14 @@ func (n *Node) loggerLocked() *logrus.Entry {
 		return log
 	}
 
-	return log.WithField("instanceID", n.k8sObj.InstanceID())
+	return log.WithField("instanceID", n.node.InstanceID())
 }
 
 // PopulateStatusFields fills in the status field of the CiliumNode custom
 // resource with ENI specific information
 func (n *Node) PopulateStatusFields(k8sObj *v2.CiliumNode) {
 	k8sObj.Status.ENI.ENIs = map[string]eniTypes.ENI{}
-	n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(),
+	n.manager.instances.ForeachInterface(n.node.InstanceID(),
 		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
 			e, ok := rev.Resource.(*eniTypes.ENI)
 			if ok {
@@ -238,7 +238,7 @@ func (n *Node) getSecurityGroupIDs(ctx context.Context) ([]string, error) {
 	}
 
 	var securityGroups []string
-	n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(),
+	n.manager.instances.ForeachInterface(n.node.InstanceID(),
 		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
 			e, ok := rev.Resource.(*eniTypes.ENI)
 			if ok && e.Number == 0 {
@@ -325,7 +325,7 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 		return 0, errUnableToGetSecurityGroups, fmt.Errorf("%s %s", errUnableToGetSecurityGroups, err)
 	}
 
-	desc := "Cilium-CNI (" + n.k8sObj.InstanceID() + ")"
+	desc := "Cilium-CNI (" + n.node.InstanceID() + ")"
 	// Must allocate secondary ENI IPs as needed, up to ENI instance limit - 1 (reserve 1 for primary IP)
 	toAllocate := math.IntMin(allocation.MaxIPsToAllocate, limits.IPv4-1)
 	// Validate whether request has already been fulfilled in the meantime
@@ -352,7 +352,7 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 
 	var attachmentID string
 	for attachRetries := 0; attachRetries < maxAttachRetries; attachRetries++ {
-		attachmentID, err = n.manager.api.AttachNetworkInterface(ctx, index, n.k8sObj.InstanceID(), eniID)
+		attachmentID, err = n.manager.api.AttachNetworkInterface(ctx, index, n.node.InstanceID(), eniID)
 
 		// The index is already in use, this can happen if the local
 		// list of ENIs is oudated.  Retry the attachment to avoid
@@ -413,7 +413,7 @@ func (n *Node) CreateInterface(ctx context.Context, allocation *ipam.AllocationA
 	}
 
 	// Add the information of the created ENI to the instances manager
-	n.manager.UpdateENI(n.k8sObj.InstanceID(), eni)
+	n.manager.UpdateENI(n.node.InstanceID(), eni)
 	return toAllocate, "", nil
 }
 
@@ -426,7 +426,7 @@ func (n *Node) ResyncInterfacesAndIPs(ctx context.Context, scopedLog *logrus.Ent
 	available := ipamTypes.AllocationMap{}
 	n.enis = map[string]eniTypes.ENI{}
 
-	n.manager.instances.ForeachInterface(n.k8sObj.InstanceID(),
+	n.manager.instances.ForeachInterface(n.node.InstanceID(),
 		func(instanceID, interfaceID string, rev ipamTypes.InterfaceRevision) error {
 			e, ok := rev.Resource.(*eniTypes.ENI)
 			if !ok {

pkg/ipam/node.go
@@ -270,6 +270,16 @@ func (n *Node) poolMaintenanceComplete() {
 	n.mutex.Unlock()
 }
 
+// InstanceID returns the instance ID of the node
+func (n *Node) InstanceID() (id string) {
+	n.mutex.RLock()
+	if n.resource != nil {
+		id = n.resource.InstanceID()
+	}
+	n.mutex.RUnlock()
+	return
+}
+
 // UpdatedResource is called when an update to the CiliumNode has been
 // received. The IPAM layer will attempt to immediately resolve any IP deficits
 // and also trigger the background sync to continue working in the background

pkg/azure/ipam/instances.go
@@ -63,7 +63,7 @@ func (m *InstancesManager) getAllocator() (allocator allocator.Allocator) {
 
 // CreateNode is called on discovery of a new node
 func (m *InstancesManager) CreateNode(obj *v2.CiliumNode, n *ipam.Node) ipam.NodeOperations {
-	return &Node{k8sObj: obj, manager: m, node: n}
+	return &Node{manager: m, node: n}
 }
 
 // GetPoolQuota returns the number of available IPs in all IP pools

commit 8eb072f83efeb55c0a1f916f8e344a555fc3c7b0

pkg/azure/ipam: fix data race in (*Node).PopulateStatusFields
Fixes the following data race:

```
WARNING: DATA RACE
Write at 0x00c000358270 by goroutine 57:
  github.com/cilium/cilium/pkg/azure/ipam.(*InstancesManager).Resync()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/azure/ipam/instances.go:98 +0x933
  github.com/cilium/cilium/pkg/ipam.(*NodeManager).instancesAPIResync()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/ipam/node_manager.go:186 +0x8b
  github.com/cilium/cilium/pkg/ipam.NewNodeManager.func1()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/ipam/node_manager.go:168 +0x8f
  github.com/cilium/cilium/pkg/trigger.(*Trigger).waiter()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/trigger/trigger.go:206 +0x5cf

Previous read at 0x00c000358270 by goroutine 250:
  github.com/cilium/cilium/pkg/azure/ipam.(*Node).PopulateStatusFields()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/azure/ipam/node.go:51 +0x11a
  github.com/cilium/cilium/pkg/ipam.(*Node).syncToAPIServer()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/ipam/node.go:716 +0x232
  github.com/cilium/cilium/pkg/ipam.(*NodeManager).Update.func4()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/ipam/node_manager.go:305 +0x75
  github.com/cilium/cilium/pkg/trigger.(*Trigger).waiter()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/trigger/trigger.go:206 +0x5cf

Goroutine 57 (running) created at:
  github.com/cilium/cilium/pkg/trigger.NewTrigger()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/trigger/trigger.go:129 +0x2c4
  github.com/cilium/cilium/pkg/ipam.NewNodeManager()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/ipam/node_manager.go:163 +0x356
  github.com/cilium/cilium/pkg/azure/ipam.(*IPAMSuite).TestIpamManyNodes()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/azure/ipam/ipam_test.go:319 +0x52e
  runtime.call32()
      /home/travis/.gimme/versions/go1.15.2.linux.amd64/src/runtime/asm_amd64.s:540 +0x3d
  reflect.Value.Call()
      /home/travis/.gimme/versions/go1.15.2.linux.amd64/src/reflect/value.go:336 +0xd8
  gopkg.in/check%2ev1.(*suiteRunner).forkTest.func1()
      /home/travis/gopath/src/github.com/cilium/cilium/vendor/gopkg.in/check.v1/check.go:781 +0xabb
  gopkg.in/check%2ev1.(*suiteRunner).forkCall.func1()
      /home/travis/gopath/src/github.com/cilium/cilium/vendor/gopkg.in/check.v1/check.go:675 +0xe1

Goroutine 250 (running) created at:
  github.com/cilium/cilium/pkg/trigger.NewTrigger()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/trigger/trigger.go:129 +0x2c4
  github.com/cilium/cilium/pkg/ipam.(*NodeManager).Update()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/ipam/node_manager.go:300 +0xfe5
  github.com/cilium/cilium/pkg/azure/ipam.(*IPAMSuite).TestIpamManyNodes()
      /home/travis/gopath/src/github.com/cilium/cilium/pkg/azure/ipam/ipam_test.go:345 +0xbcd
  runtime.call32()
      /home/travis/.gimme/versions/go1.15.2.linux.amd64/src/runtime/asm_amd64.s:540 +0x3d
  reflect.Value.Call()
      /home/travis/.gimme/versions/go1.15.2.linux.amd64/src/reflect/value.go:336 +0xd8
  gopkg.in/check%2ev1.(*suiteRunner).forkTest.func1()
      /home/travis/gopath/src/github.com/cilium/cilium/vendor/gopkg.in/check.v1/check.go:781 +0xabb
  gopkg.in/check%2ev1.(*suiteRunner).forkCall.func1()
      /home/travis/gopath/src/github.com/cilium/cilium/vendor/gopkg.in/check.v1/check.go:675 +0xe1
```

Closes #13580

Fixes: f5f08842c367 ("ipam: Move instance ID retrieval into generic code")

pkg/azure/ipam/node.go
@@ -48,6 +48,9 @@ func (n *Node) UpdatedNode(obj *v2.CiliumNode) {
 // resource with Azure specific information
 func (n *Node) PopulateStatusFields(k8sObj *v2.CiliumNode) {
 	k8sObj.Status.Azure.Interfaces = []types.AzureInterface{}
+
+	n.manager.mutex.RLock()
+	defer n.manager.mutex.RUnlock()
 	n.manager.instances.ForeachInterface(n.node.InstanceID(), func(instanceID, interfaceID string, interfaceObj ipamTypes.InterfaceRevision) error {
 		iface, ok := interfaceObj.Resource.(*types.AzureInterface)
 		if ok {