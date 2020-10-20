g0odi3z2j/cilium

commit 33439da346183cf058a11abe03f3decc65c64a21

k8s/cilium Event handlers and processing logic for LRPs
- Define internal representation of LRP
- Add event handlers for various resources (LRP CRs, pods, services)
  that update LRPs
- Plumb datapath with policy configs

TODO: Add unit tests

Testing:
Manually tested various cases and verified that loadbalancer
service entries were getting created.
 - LRPs with address/service matchers
 - Delete service/backend pods
 - Update pod labels

 cilium service list
 ID   Frontend           Service Type    Backend
 1    172.20.0.20:5001   ClusterIP       1 => 10.16.189.7:5001
                                         2 => 10.16.45.6:5001
 2    172.20.0.1:443     ClusterIP       1 => 192.168.33.11:6443
 3    172.20.0.194:80    LocalRedirect   1 => 10.16.86.228:80
 6    172.20.0.10:53     ClusterIP       1 => 10.16.14.226:53
 7    172.20.0.10:9153   ClusterIP       1 => 10.16.14.226:9153

 kubectl exec nginx-client -- curl -s -I http://172.20.0.194/index.html
 HTTP/1.1 200 OK
 Server: nginx/1.19.2
 Date: Fri, 21 Aug 2020 16:50:06 GMT
 Content-Type: text/html
 Content-Length: 612
 Last-Modified: Tue, 11 Aug 2020 14:50:35 GMT
 Connection: keep-alive
 ETag: "5f32b03b-264"
 Accept-Ranges: bytes

 cilium -D monitor --related-to 2280
 Listening for events on 6 CPUs with 64x4096 of shared memory
 Press Ctrl-C to quit
 level=info msg="Initializing dissection cache..." subsys=monitor
 -> endpoint 2280 flow 0x52c1fcd0 identity 7086->59395 state new ifindex lxca7d000aa6ecc orig-ip 10.16.224.211: 10.16.224.211:34262 -> 10.16.86.228:80 tcp SYN

 LRP with addressMatcher -

  cilium service list
 ID   Frontend               Service Type    Backend
 1    172.20.0.20:5001       ClusterIP       1 => 10.16.189.7:5001
                                             2 => 10.16.45.6:5001
 2    172.20.0.1:443         ClusterIP       1 => 192.168.33.11:6443
 3    169.254.169.254:8080   LocalRedirect   1 => 10.16.86.228:80
 6    172.20.0.10:53         ClusterIP       1 => 10.16.14.226:53

pkg/k8s/watchers/service.go
@@ -80,7 +80,12 @@ func (k *K8sWatcher) servicesInit(k8sClient kubernetes.Interface, swgSvcs *lock.
 }
 
 func (k *K8sWatcher) addK8sServiceV1(svc *slim_corev1.Service, swg *lock.StoppableWaitGroup) error {
-	k.K8sSvcCache.UpdateService(svc, swg)
+	svcID := k.K8sSvcCache.UpdateService(svc, swg)
+	if svc.Spec.Type == slim_corev1.ServiceTypeClusterIP {
+		// The local redirect policies currently support services of type
+		// clusterIP only.
+		k.redirectPolicyManager.OnAddService(svcID, &k.K8sSvcCache, k.podStore)
+	}
 	return nil
 }
 
@@ -90,5 +95,9 @@ func (k *K8sWatcher) updateK8sServiceV1(oldSvc, newSvc *slim_corev1.Service, swg
 
 func (k *K8sWatcher) deleteK8sServiceV1(svc *slim_corev1.Service, swg *lock.StoppableWaitGroup) error {
 	k.K8sSvcCache.DeleteService(svc, swg)
+	svcID := k8s.ParseServiceID(svc)
+	if svc.Spec.Type == slim_corev1.ServiceTypeClusterIP {
+		k.redirectPolicyManager.OnDeleteService(svcID)
+	}
 	return nil
 }

pkg/service/service.go
@@ -102,7 +102,7 @@ func (svc *svcInfo) deepCopyToLBSVC() *lb.SVC {
 	}
 }
 
-// requireNodeLocalBackends returns if the frontend service traffic policy
+// requireNodeLocalBackends returns true if the frontend service traffic policy
 // is lb.SVCTrafficPolicyLocal and whether only local backends need to be filtered for the
 // given frontend.
 func (svc *svcInfo) requireNodeLocalBackends(frontend lb.L3n4AddrID) (bool, bool) {

CODEOWNERS
@@ -143,6 +143,7 @@ Jenkinsfile.nightly @cilium/ci
 /pkg/policy/groups/aws/ @cilium/policy @cilium/aws
 /pkg/proxy/ @cilium/proxy
 /pkg/proxy/accesslog @cilium/api
+/pkg/redirectpolicy @cilium/loadbalancer
 /pkg/serializer @cilium/agent
 /pkg/service @cilium/loadbalancer
 /pkg/sysctl @cilium/bpf

daemon/cmd/daemon.go
@@ -74,6 +74,7 @@ import (
 	policyApi "github.com/cilium/cilium/pkg/policy/api"
 	"github.com/cilium/cilium/pkg/probe"
 	"github.com/cilium/cilium/pkg/proxy"
+	"github.com/cilium/cilium/pkg/redirectpolicy"
 	"github.com/cilium/cilium/pkg/service"
 	"github.com/cilium/cilium/pkg/sockops"
 	"github.com/cilium/cilium/pkg/status"
@@ -155,6 +156,8 @@ type Daemon struct {
 	// endpointCreations is a map of all currently ongoing endpoint
 	// creation events
 	endpointCreations *endpointCreationManager
+
+	redirectPolicyManager *redirectpolicy.Manager
 }
 
 // GetPolicyRepository returns the policy repository of the daemon
@@ -338,6 +341,8 @@ func NewDaemon(ctx context.Context, epMgr *endpointmanager.EndpointManager, dp d
 	d.endpointManager = epMgr
 	d.endpointManager.InitMetrics()
 
+	d.redirectPolicyManager = redirectpolicy.NewRedirectPolicyManager(d.svc)
+
 	d.k8sWatcher = watchers.NewK8sWatcher(
 		d.endpointManager,
 		d.nodeDiscovery.Manager,
@@ -345,6 +350,7 @@ func NewDaemon(ctx context.Context, epMgr *endpointmanager.EndpointManager, dp d
 		d.policy,
 		d.svc,
 		d.datapath,
+		d.redirectPolicyManager,
 	)
 
 	bootstrapStats.daemonInit.End(true)

pkg/k8s/apis/cilium.io/v2/clrp_types.go
@@ -15,7 +15,13 @@
 package v2
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cilium/cilium/pkg/iana"
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	lb "github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/policy/api"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -197,4 +203,42 @@ type CiliumLocalRedirectPolicyList struct {
 
 	// Items is a list of CiliumLocalRedirectPolicy
 	Items []CiliumLocalRedirectPolicy `json:"items"`
+}
+
+// SanitizePortInfo sanitizes all the fields in the PortInfo.
+// It returns port number, name, and protocol derived from the given input  and error (failure cases).
+func (pInfo *PortInfo) SanitizePortInfo(checkNamedPort bool) (uint16, string, lb.L4Type, error) {
+	var (
+		pInt     uint16
+		pName    string
+		protocol lb.L4Type
+	)
+	// Sanitize port
+	if pInfo.Port == "" {
+		return pInt, pName, protocol, fmt.Errorf("port must be specified")
+	} else {
+		p, err := strconv.ParseUint(pInfo.Port, 0, 16)
+		if err != nil {
+			return pInt, pName, protocol, fmt.Errorf("unable to parse port: %v", err)
+		}
+		if p == 0 {
+			return pInt, pName, protocol, fmt.Errorf("port cannot be 0")
+		}
+		pInt = uint16(p)
+	}
+	// Sanitize name
+	if checkNamedPort {
+		if !iana.IsSvcName(pInfo.Name) {
+			return pInt, pName, protocol, fmt.Errorf("valid port name is not present")
+		}
+	}
+	pName = strings.ToLower(pInfo.Name) // Normalize for case insensitive comparison
+
+	// Sanitize protocol
+	var err error
+	protocol, err = lb.NewL4Type(string(pInfo.Protocol))
+	if err != nil {
+		return pInt, pName, protocol, err
+	}
+	return pInt, pName, protocol, nil
 }

pkg/k8s/factory_functions.go
@@ -655,6 +655,30 @@ func ConvertToCiliumNode(obj interface{}) interface{} {
 	}
 }
 
+// ConvertToCiliumLocalRedirectPolicy converts a *cilium_v2.CiliumLocalRedirectPolicy into a
+// *cilium_v2.CiliumLocalRedirectPolicy or a cache.DeletedFinalStateUnknown into
+// a cache.DeletedFinalStateUnknown with a *cilium_v2.CiliumLocalRedirectPolicy in its Obj.
+// If the given obj can't be cast into either *cilium_v2.CiliumLocalRedirectPolicy
+// nor cache.DeletedFinalStateUnknown, the original obj is returned.
+func ConvertToCiliumLocalRedirectPolicy(obj interface{}) interface{} {
+	// TODO create a slim type of the CiliumLocalRedirectPolicy
+	switch concreteObj := obj.(type) {
+	case *cilium_v2.CiliumLocalRedirectPolicy:
+		return concreteObj
+	case cache.DeletedFinalStateUnknown:
+		ciliumLocalRedirectPolicy, ok := concreteObj.Obj.(*cilium_v2.CiliumLocalRedirectPolicy)
+		if !ok {
+			return obj
+		}
+		return cache.DeletedFinalStateUnknown{
+			Key: concreteObj.Key,
+			Obj: ciliumLocalRedirectPolicy,
+		}
+	default:
+		return obj
+	}
+}
+
 // ObjToCiliumNode attempts to cast object to a CiliumNode object and
 // returns a deep copy if the castin succeeds. Otherwise, nil is returned.
 func ObjToCiliumNode(obj interface{}) *cilium_v2.CiliumNode {
@@ -767,5 +791,27 @@ func ObjToCiliumEndpoint(obj interface{}) *types.CiliumEndpoint {
 	}
 	log.WithField(logfields.Object, logfields.Repr(obj)).
 		Warn("Ignoring invalid v2 CiliumEndpoint")
+	return nil
+}
+
+// ObjToCLRP attempts to cast object to a CLRP object and
+// returns a deep copy if the castin succeeds. Otherwise, nil is returned.
+func ObjToCLRP(obj interface{}) *cilium_v2.CiliumLocalRedirectPolicy {
+	cLRP, ok := obj.(*cilium_v2.CiliumLocalRedirectPolicy)
+	if ok {
+		return cLRP
+	}
+	deletedObj, ok := obj.(cache.DeletedFinalStateUnknown)
+	if ok {
+		// Delete was not observed by the watcher but is
+		// removed from kube-apiserver. This is the last
+		// known state and the object no longer exists.
+		cn, ok := deletedObj.Obj.(*cilium_v2.CiliumLocalRedirectPolicy)
+		if ok {
+			return cn
+		}
+	}
+	log.WithField(logfields.Object, logfields.Repr(obj)).
+		Warn("Ignoring invalid v2 Cilium Local Redirect Policy")
 	return nil
 }

pkg/k8s/service_cache.go
@@ -15,6 +15,8 @@
 package k8s
 
 import (
+	"net"
+
 	"github.com/cilium/cilium/pkg/datapath"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/core/v1"
 	slim_discovery_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/discovery/v1beta1"
@@ -120,6 +122,36 @@ func (s *ServiceCache) GetServiceIP(svcID ServiceID) *loadbalancer.L3n4Addr {
 			loadbalancer.ScopeExternal)
 	}
 	return nil
+}
+
+// GetServiceFrontendIP returns the frontend IP (aka clusterIP) for the given service with type.
+func (s *ServiceCache) GetServiceFrontendIP(svcID ServiceID, svcType loadbalancer.SVCType) net.IP {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	svc := s.services[svcID]
+	if svc == nil || svc.Type != svcType {
+		return nil
+	}
+
+	return svc.FrontendIP
+}
+
+// GetServiceAddrWithPortsAndType returns a slice of all the L3n4Addr that are backing the
+// given Service ID with given type.
+// Note: The returned IPs are with External scope.
+func (s *ServiceCache) GetServiceAddrsWithType(svcID ServiceID, svcType loadbalancer.SVCType) map[loadbalancer.FEPortName]*loadbalancer.L3n4Addr {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	svc := s.services[svcID]
+	if svc == nil || svc.Type != svcType {
+		return nil
+	}
+	addrsByPort := make(map[loadbalancer.FEPortName]*loadbalancer.L3n4Addr)
+	for pName, l4Addr := range svc.Ports {
+		addrsByPort[pName] = loadbalancer.NewL3n4Addr(l4Addr.Protocol, svc.FrontendIP,
+			l4Addr.Port, loadbalancer.ScopeExternal)
+	}
+	return addrsByPort
 }
 
 // GetNodeAddressing returns the registered node addresses to this service cache.

pkg/k8s/utils/utils.go
@@ -15,9 +15,14 @@
 package utils
 
 import (
+	"fmt"
+	"sort"
+
+	slimcorev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/core/v1"
 	"github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/labels"
 	"github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/selection"
 	"github.com/cilium/cilium/pkg/option"
+
 	v1 "k8s.io/api/core/v1"
 	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -102,4 +107,30 @@ func GetServiceListOptionsModifier() (func(options *v1meta.ListOptions), error) 
 	return func(options *v1meta.ListOptions) {
 		options.LabelSelector = labelSelector.String()
 	}, nil
+}
+
+// ValidIPs return a sorted slice of unique IP addresses retrieved from the given PodStatus.
+// Returns an error when no IPs are found.
+func ValidIPs(podStatus slimcorev1.PodStatus) ([]string, error) {
+	if len(podStatus.PodIPs) == 0 && len(podStatus.PodIP) == 0 {
+		return nil, fmt.Errorf("empty PodIPs")
+	}
+
+	// make it a set first to avoid repeated IP addresses
+	ipsMap := make(map[string]struct{}, 1+len(podStatus.PodIPs))
+	if podStatus.PodIP != "" {
+		ipsMap[podStatus.PodIP] = struct{}{}
+	}
+	for _, podIP := range podStatus.PodIPs {
+		if podIP.IP != "" {
+			ipsMap[podIP.IP] = struct{}{}
+		}
+	}
+
+	ips := make([]string, 0, len(ipsMap))
+	for ipStr := range ipsMap {
+		ips = append(ips, ipStr)
+	}
+	sort.Strings(ips)
+	return ips, nil
 }

commit c7d78ad16a73db6dbec74ae0898310d0b26d27c1

k8s/watchers: fix data race in (*K8sWatcher).addK8sServiceV1
Access to k.podStore must be protected by k.podStoreMU.

Closes #13603

Fixes: 33439da34618 ("k8s/cilium Event handlers and processing logic for LRPs")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

pkg/k8s/watchers/service.go
@@ -84,7 +84,9 @@ func (k *K8sWatcher) addK8sServiceV1(svc *slim_corev1.Service, swg *lock.Stoppab
 	if svc.Spec.Type == slim_corev1.ServiceTypeClusterIP {
 		// The local redirect policies currently support services of type
 		// clusterIP only.
+		k.podStoreMU.RLock()
 		k.redirectPolicyManager.OnAddService(svcID, k.podStore)
+		k.podStoreMU.RUnlock()
 	}
 	return nil
 }