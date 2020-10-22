g0odi3z2j/cilium

commit db4a6f90f13a4234a4c6c512671c9deaa2627e47

endpoint: Run labels controller under ep manager
Move the labels resolution controller for an endpoint into the endpoint
package and run it under the manager for the endpoint. This way, when
the endpoint is removed, the controller will also be stopped.

The other minor change is that the context for the controller is now the
endpoint's aliveCtx rather than the daemon's context directly.

Fixes: #9540
Fixes: 2e8eb33b5b5a ("daemon: Start controller when pod labels resolution fails")

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/endpoint/endpoint.go
@@ -29,6 +29,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/common/addressing"
+	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/completion"
 	"github.com/cilium/cilium/pkg/controller"
@@ -39,6 +40,7 @@ import (
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/identity/identitymanager"
 	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	"github.com/cilium/cilium/pkg/labels"
 	pkgLabels "github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -1473,6 +1475,45 @@ func (e *Endpoint) getIDandLabels() string {
 	}
 
 	return fmt.Sprintf("%d (%s)", e.ID, labels)
+}
+
+// MetadataResolverCB provides an implementation for resolving the endpoint
+// metadata for an endpoint such as the associated labels and annotations.
+type MetadataResolverCB func(*Endpoint) (identityLabels labels.Labels, infoLabels labels.Labels, annotations map[string]string, err error)
+
+// RunMetadataResolver starts a controller associated with the received
+// endpoint which will periodically attempt to resolve the metadata for the
+// endpoint and update the endpoint with the related. It stops resolving after
+// either the first successful metadata resolution or when the endpoint is
+// removed.
+//
+// This assumes that after the initial successful resolution, other mechanisms
+// will handle updates (such as pkg/k8s/watchers informers).
+func (e *Endpoint) RunMetadataResolver(resolveMetadata MetadataResolverCB) {
+	done := make(chan struct{})
+	controllerName := fmt.Sprintf("resolve-labels-%s", e.GetK8sNamespaceAndPodName())
+	go func() {
+		<-done
+		e.controllers.RemoveController(controllerName)
+	}()
+
+	e.controllers.UpdateController(controllerName,
+		controller.ControllerParams{
+			DoFunc: func(ctx context.Context) error {
+				identityLabels, info, annotations, err := resolveMetadata(e)
+				if err != nil {
+					e.Logger(controllerName).WithError(err).Warning("Unable to fetch kubernetes labels")
+					return err
+				}
+				e.UpdateVisibilityPolicy(annotations[annotation.ProxyVisibility])
+				e.UpdateLabels(ctx, identityLabels, info, true)
+				close(done)
+				return nil
+			},
+			RunInterval: 30 * time.Second,
+			Context:     e.aliveCtx,
+		},
+	)
 }
 
 // ModifyIdentityLabels changes the custom and orchestration identity labels of an endpoint.

daemon/endpoint.go
@@ -19,13 +19,11 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
-	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
 	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/api"
-	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/endpoint"
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/k8s"
@@ -149,6 +147,8 @@ func NewPutEndpointIDHandler(d *Daemon) PutEndpointIDHandler {
 	return &putEndpointID{d: d}
 }
 
+// fetchK8sLabelsAndAnnotations wraps the k8s package to fetch and provide
+// endpoint metadata. It implements endpoint.MetadataResolverCB.
 func fetchK8sLabelsAndAnnotations(ep *endpoint.Endpoint) (labels.Labels, labels.Labels, map[string]string, error) {
 	lbls, annotations, err := k8s.GetPodMetadata(ep.GetK8sNamespace(), ep.GetK8sPodName())
 	if err != nil {
@@ -275,34 +275,10 @@ func (d *Daemon) createEndpoint(ctx context.Context, epTemplate *models.Endpoint
 	// static pod's labels. In this case, start a controller to attempt to
 	// resolve the labels.
 	if ep.K8sNamespaceAndPodNameIsSet() && k8s.IsEnabled() {
-		// If there are labels, but no pod namespace then it's
+		// If there are labels, but no pod namespace, then it's
 		// likely that there are no k8s labels at all. Resolve.
 		if _, k8sLabelsConfigured := addLabels[k8sConst.PodNamespaceLabel]; !k8sLabelsConfigured {
-			done := make(chan struct{})
-
-			controllerName := fmt.Sprintf("resolve-labels-%s", ep.GetK8sNamespaceAndPodName())
-			mgr := controller.NewManager()
-			mgr.UpdateController(controllerName,
-				controller.ControllerParams{
-					DoFunc: func(ctx context.Context) error {
-						identityLabels, info, annotations, err := fetchK8sLabelsAndAnnotations(ep)
-						if err != nil {
-							ep.Logger(controllerName).WithError(err).Warning("Unable to fetch kubernetes labels")
-							return err
-						}
-						ep.UpdateVisibilityPolicy(annotations[annotation.ProxyVisibility])
-						ep.UpdateLabels(ctx, identityLabels, info, true)
-						close(done)
-						return nil
-					},
-					RunInterval: 30 * time.Second,
-					Context:     d.ctx,
-				},
-			)
-			go func() {
-				<-done
-				mgr.RemoveController(controllerName)
-			}()
+			ep.RunMetadataResolver(fetchK8sLabelsAndAnnotations)
 		}
 	}
 

commit 11319b033286bec73fd5d790d10fa4e768bd8eb0

pkg/endpoint: reduce cardinality of prometheus labels
If the controller that is used for label resolution fails, the
prometheus metrics will increase its cardinality since the uniquely
controller name was being used as a prometheus label. To avoid this, we
will reference these warnings with a common subsystem name,
'resolve-labels'.

Fixes: db4a6f90f13a ("endpoint: Run labels controller under ep manager")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/endpoint.go
@@ -1645,7 +1645,8 @@ type MetadataResolverCB func(ns, podName string) (pod *slim_corev1.Pod, _ []slim
 // will handle updates (such as pkg/k8s/watchers informers).
 func (e *Endpoint) RunMetadataResolver(resolveMetadata MetadataResolverCB) {
 	done := make(chan struct{})
-	controllerName := fmt.Sprintf("resolve-labels-%s", e.GetK8sNamespaceAndPodName())
+	const controllerPrefix = "resolve-labels"
+	controllerName := fmt.Sprintf("%s-%s", controllerPrefix, e.GetK8sNamespaceAndPodName())
 	go func() {
 		select {
 		case <-done:
@@ -1661,7 +1662,7 @@ func (e *Endpoint) RunMetadataResolver(resolveMetadata MetadataResolverCB) {
 				ns, podName := e.GetK8sNamespace(), e.GetK8sPodName()
 				pod, cp, identityLabels, info, _, err := resolveMetadata(ns, podName)
 				if err != nil {
-					e.Logger(controllerName).WithError(err).Warning("Unable to fetch kubernetes labels")
+					e.Logger(controllerPrefix).WithError(err).Warning("Unable to fetch kubernetes labels")
 					return err
 				}
 				e.SetPod(pod)