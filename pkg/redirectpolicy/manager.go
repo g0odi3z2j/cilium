g0odi3z2j/cilium

commit 4478c70862d869aa665a8225f7a521b659f48bb9

Enables LRP to react on pod readiness.
Currently local redirect service won't react if backend pod becomes
unhealty.

This adds logic that checks the pod status within watcher and update LRP
configs accordingly.

To achieve the above goal, PodConditions are added into the slim apis to
allow for watching the "ready" condition of pods.

This PR also adds logic in LRP manager to restore service when no
backend is healthy.

Signed-off-by: Weilong Cui <cuiwl@google.com>

pkg/redirectpolicy/manager.go
@@ -237,10 +237,10 @@ func (rpm *Manager) OnAddPod(pod *slimcorev1.Pod) {
 	if _, ok := rpm.policyPods[id]; ok {
 		return
 	}
-	rpm.onUpdatePodLocked(pod)
+	rpm.OnUpdatePodLocked(pod, false, true)
 }
 
-func (rpm *Manager) onUpdatePodLocked(pod *slimcorev1.Pod) {
+func (rpm *Manager) OnUpdatePodLocked(pod *slimcorev1.Pod, removeOld bool, upsertNew bool) {
 	if len(rpm.policyConfigs) == 0 {
 		return
 	}
@@ -258,26 +258,31 @@ func (rpm *Manager) onUpdatePodLocked(pod *slimcorev1.Pod) {
 		return
 	}
 
-	// Check if the pod was previously selected by any of the policies.
-	if policies, ok := rpm.policyPods[podData.id]; ok {
-		for _, policy := range policies {
-			config := rpm.policyConfigs[policy]
-			rpm.deletePolicyBackends(config, podData.id)
+	if removeOld {
+		// Check if the pod was previously selected by any of the policies.
+		if policies, ok := rpm.policyPods[podData.id]; ok {
+			for _, policy := range policies {
+				config := rpm.policyConfigs[policy]
+				rpm.deletePolicyBackends(config, podData.id)
+			}
 		}
 	}
-	// Check if any of the current redirect policies select this pod.
-	for _, config := range rpm.policyConfigs {
-		if config.policyConfigSelectsPod(podData) {
-			rpm.processConfig(config, podData)
+
+	if upsertNew {
+		// Check if any of the current redirect policies select this pod.
+		for _, config := range rpm.policyConfigs {
+			if config.policyConfigSelectsPod(podData) {
+				rpm.processConfig(config, podData)
+			}
 		}
 	}
 }
 
-func (rpm *Manager) OnUpdatePod(pod *slimcorev1.Pod) {
+func (rpm *Manager) OnUpdatePod(pod *slimcorev1.Pod, needsReassign bool, ready bool) {
 	rpm.mutex.Lock()
 	defer rpm.mutex.Unlock()
 	// TODO add unit test to validate that we get callbacks only for relevant events
-	rpm.onUpdatePodLocked(pod)
+	rpm.OnUpdatePodLocked(pod, needsReassign || !ready, ready)
 }
 
 func (rpm *Manager) OnDeletePod(pod *slimcorev1.Pod) {
@@ -434,6 +439,11 @@ func (rpm *Manager) notifyPolicyBackendDelete(config *LRPConfig, frontendMapping
 			log.WithError(err).Errorf("Local redirect service for policy (%v)"+
 				" with frontend (%v) not deleted", config.id, frontendMapping.feAddr)
 		}
+		if restored := rpm.svcCache.EnsureService(*config.serviceID, lock.NewStoppableWaitGroup()); restored {
+			log.WithFields(logrus.Fields{
+				logfields.K8sSvcID: *config.serviceID,
+			}).Info("Restored service")
+		}
 	}
 }
 
@@ -513,6 +523,9 @@ func (rpm *Manager) getLocalPodsForPolicy(config *LRPConfig, podStore cache.Stor
 				logfields.K8sPodName:   pod.Name,
 				logfields.K8sNamespace: pod.Namespace,
 			}).Error("failed to get valid pod metadata")
+			continue
+		}
+		if k8sUtils.GetLatestPodReadiness(pod.Status) != slimcorev1.ConditionTrue {
 			continue
 		}
 		if !config.policyConfigSelectsPod(podData) {

pkg/k8s/slim/k8s/apis/core/v1/generated.proto
@@ -335,6 +335,34 @@ message Pod {
   optional PodStatus status = 3;
 }
 
+// PodCondition contains details for the current condition of this pod.
+message PodCondition {
+  // Type is the type of the condition.
+  // More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#pod-conditions
+  optional string type = 1;
+
+  // Status is the status of the condition.
+  // Can be True, False, Unknown.
+  // More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#pod-conditions
+  optional string status = 2;
+
+  // Last time we probed the condition.
+  // +optional
+  optional github.com.cilium.cilium.pkg.k8s.slim.k8s.apis.meta.v1.Time lastProbeTime = 3;
+
+  // Last time the condition transitioned from one status to another.
+  // +optional
+  optional github.com.cilium.cilium.pkg.k8s.slim.k8s.apis.meta.v1.Time lastTransitionTime = 4;
+
+  // Unique, one-word, CamelCase reason for the condition's last transition.
+  // +optional
+  optional string reason = 5;
+
+  // Human-readable message indicating details about last transition.
+  // +optional
+  optional string message = 6;
+}
+
 // IP address information for entries in the (plural) PodIPs field.
 // Each entry includes:
 //    IP: An IP address allocated to the pod. Routable at least within the cluster.
@@ -419,6 +447,13 @@ message PodStatus {
   // More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#pod-phase
   // +optional
   optional string phase = 1;
+
+  // Current service state of pod.
+  // More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#pod-conditions
+  // +optional
+  // +patchMergeKey=type
+  // +patchStrategy=merge
+  repeated PodCondition conditions = 2;
 
   // IP address of the host to which the pod is assigned. Empty if not yet scheduled.
   // +optional

pkg/k8s/slim/k8s/apis/core/v1/types.go
@@ -97,6 +97,18 @@ type Container struct {
 	VolumeMounts []VolumeMount `json:"volumeMounts,omitempty" patchStrategy:"merge" patchMergeKey:"mountPath" protobuf:"bytes,9,rep,name=volumeMounts"`
 }
 
+type ConditionStatus string
+
+// These are valid condition statuses. "ConditionTrue" means a resource is in the condition.
+// "ConditionFalse" means a resource is not in the condition. "ConditionUnknown" means kubernetes
+// can't decide if a resource is in the condition or not. In the future, we could add other
+// intermediate conditions, e.g. ConditionDegraded.
+const (
+	ConditionTrue    ConditionStatus = "True"
+	ConditionFalse   ConditionStatus = "False"
+	ConditionUnknown ConditionStatus = "Unknown"
+)
+
 // PodPhase is a label for the condition of a pod at the current time.
 type PodPhase string
 
@@ -119,6 +131,52 @@ const (
 	// to an error in communicating with the host of the pod.
 	PodUnknown PodPhase = "Unknown"
 )
+
+// PodConditionType is a valid value for PodCondition.Type
+type PodConditionType string
+
+// These are valid conditions of pod.
+const (
+	// ContainersReady indicates whether all containers in the pod are ready.
+	ContainersReady PodConditionType = "ContainersReady"
+	// PodInitialized means that all init containers in the pod have started successfully.
+	PodInitialized PodConditionType = "Initialized"
+	// PodReady means the pod is able to service requests and should be added to the
+	// load balancing pools of all matching services.
+	PodReady PodConditionType = "Ready"
+	// PodScheduled represents status of the scheduling process for this pod.
+	PodScheduled PodConditionType = "PodScheduled"
+)
+
+// These are reasons for a pod's transition to a condition.
+const (
+	// PodReasonUnschedulable reason in PodScheduled PodCondition means that the scheduler
+	// can't schedule the pod right now, for example due to insufficient resources in the cluster.
+	PodReasonUnschedulable = "Unschedulable"
+)
+
+// PodCondition contains details for the current condition of this pod.
+type PodCondition struct {
+	// Type is the type of the condition.
+	// More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#pod-conditions
+	Type PodConditionType `json:"type" protobuf:"bytes,1,opt,name=type,casttype=PodConditionType"`
+	// Status is the status of the condition.
+	// Can be True, False, Unknown.
+	// More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#pod-conditions
+	Status ConditionStatus `json:"status" protobuf:"bytes,2,opt,name=status,casttype=ConditionStatus"`
+	// Last time we probed the condition.
+	// +optional
+	LastProbeTime slim_metav1.Time `json:"lastProbeTime,omitempty" protobuf:"bytes,3,opt,name=lastProbeTime"`
+	// Last time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime slim_metav1.Time `json:"lastTransitionTime,omitempty" protobuf:"bytes,4,opt,name=lastTransitionTime"`
+	// Unique, one-word, CamelCase reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty" protobuf:"bytes,5,opt,name=reason"`
+	// Human-readable message indicating details about last transition.
+	// +optional
+	Message string `json:"message,omitempty" protobuf:"bytes,6,opt,name=message"`
+}
 
 // The node this Taint is attached to has the "effect" on
 // any pod that does not tolerate the Taint.
@@ -230,6 +288,12 @@ type PodStatus struct {
 	// More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#pod-phase
 	// +optional
 	Phase PodPhase `json:"phase,omitempty" protobuf:"bytes,1,opt,name=phase,casttype=PodPhase"`
+	// Current service state of pod.
+	// More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#pod-conditions
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions []PodCondition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,2,rep,name=conditions"`
 	// IP address of the host to which the pod is assigned. Empty if not yet scheduled.
 	// +optional
 	HostIP string `json:"hostIP,omitempty" protobuf:"bytes,5,opt,name=hostIP"`

pkg/k8s/slim/k8s/apis/core/v1/zz_generated.deepcopy.go
@@ -493,6 +493,24 @@ func (in *Pod) DeepCopyObject() runtime.Object {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PodCondition) DeepCopyInto(out *PodCondition) {
+	*out = *in
+	in.LastProbeTime.DeepCopyInto(&out.LastProbeTime)
+	in.LastTransitionTime.DeepCopyInto(&out.LastTransitionTime)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PodCondition.
+func (in *PodCondition) DeepCopy() *PodCondition {
+	if in == nil {
+		return nil
+	}
+	out := new(PodCondition)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *PodIP) DeepCopyInto(out *PodIP) {
 	*out = *in
 	return
@@ -574,6 +592,13 @@ func (in *PodSpec) DeepCopy() *PodSpec {
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *PodStatus) DeepCopyInto(out *PodStatus) {
 	*out = *in
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]PodCondition, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
 	if in.PodIPs != nil {
 		in, out := &in.PodIPs, &out.PodIPs
 		*out = make([]PodIP, len(*in))

pkg/k8s/slim/k8s/apis/core/v1/zz_generated.deepequal.go
@@ -559,6 +559,37 @@ func (in *Pod) DeepEqual(other *Pod) bool {
 
 // DeepEqual is an autogenerated deepequal function, deeply comparing the
 // receiver with other. in must be non-nil.
+func (in *PodCondition) DeepEqual(other *PodCondition) bool {
+	if other == nil {
+		return false
+	}
+
+	if in.Type != other.Type {
+		return false
+	}
+	if in.Status != other.Status {
+		return false
+	}
+	if !in.LastProbeTime.DeepEqual(&other.LastProbeTime) {
+		return false
+	}
+
+	if !in.LastTransitionTime.DeepEqual(&other.LastTransitionTime) {
+		return false
+	}
+
+	if in.Reason != other.Reason {
+		return false
+	}
+	if in.Message != other.Message {
+		return false
+	}
+
+	return true
+}
+
+// DeepEqual is an autogenerated deepequal function, deeply comparing the
+// receiver with other. in must be non-nil.
 func (in *PodIP) DeepEqual(other *PodIP) bool {
 	if other == nil {
 		return false
@@ -667,6 +698,23 @@ func (in *PodStatus) DeepEqual(other *PodStatus) bool {
 	if in.Phase != other.Phase {
 		return false
 	}
+	if ((in.Conditions != nil) && (other.Conditions != nil)) || ((in.Conditions == nil) != (other.Conditions == nil)) {
+		in, other := &in.Conditions, &other.Conditions
+		if other == nil {
+			return false
+		}
+
+		if len(*in) != len(*other) {
+			return false
+		} else {
+			for i, inElement := range *in {
+				if !inElement.DeepEqual(&(*other)[i]) {
+					return false
+				}
+			}
+		}
+	}
+
 	if in.HostIP != other.HostIP {
 		return false
 	}

pkg/k8s/utils/utils.go
@@ -109,6 +109,16 @@ func GetServiceListOptionsModifier() (func(options *v1meta.ListOptions), error) 
 	}, nil
 }
 
+// GetLatestPodReadiness returns the lastest podReady condition on a given pod.
+func GetLatestPodReadiness(podStatus slim_corev1.PodStatus) slim_corev1.ConditionStatus {
+	for _, cond := range podStatus.Conditions {
+		if cond.Type == slim_corev1.PodReady {
+			return cond.Status
+		}
+	}
+	return slim_corev1.ConditionUnknown
+}
+
 // ValidIPs return a sorted slice of unique IP addresses retrieved from the given PodStatus.
 // Returns an error when no IPs are found.
 func ValidIPs(podStatus slim_corev1.PodStatus) ([]string, error) {

pkg/k8s/watchers/pod.go
@@ -265,6 +265,7 @@ func (k *K8sWatcher) updateK8sPodV1(oldK8sPod, newK8sPod *slim_corev1.Pod) error
 	newPodLabels := newK8sPod.ObjectMeta.Labels
 	labelsChanged := !comparator.MapStringEquals(oldPodLabels, newPodLabels)
 
+	lrpNeedsReassign := false
 	// The relevant updates are : podIPs and label updates.
 	oldPodIPLen := len(oldK8sPod.Status.PodIP)
 	newPodIPLen := len(newK8sPod.Status.PodIP)
@@ -276,7 +277,14 @@ func (k *K8sWatcher) updateK8sPodV1(oldK8sPod, newK8sPod *slim_corev1.Pod) error
 		// PodIPs update
 		fallthrough
 	case labelsChanged:
-		k.redirectPolicyManager.OnUpdatePod(newK8sPod)
+		lrpNeedsReassign = true
+	}
+
+	oldPodReady := k8sUtils.GetLatestPodReadiness(oldK8sPod.Status)
+	newPodReady := k8sUtils.GetLatestPodReadiness(newK8sPod.Status)
+
+	if lrpNeedsReassign || (oldPodReady != newPodReady) {
+		k.redirectPolicyManager.OnUpdatePod(newK8sPod, lrpNeedsReassign, newPodReady == slim_corev1.ConditionTrue)
 	}
 
 	// Nothing changed.

commit 528fa45fb3d9da75104bd9a500316daeec4d0a15

redirectpolicy: Check lrp type before restoring lrp service
This fixes the regression caused by commit 4478c70, whereby not
checking the lrp type before restoring clusterIP service would
lead to nil pointer access for addressMatcher lrps that don't
have the serviceID field set in the lrp config. As there is no
backing k8s service for such lrps so they don't need restore.

Signed-off-by: Aditi Ghag <aditi@cilium.io>

pkg/redirectpolicy/manager.go
@@ -449,10 +449,12 @@ func (rpm *Manager) notifyPolicyBackendDelete(config *LRPConfig, frontendMapping
 			log.WithError(err).Errorf("Local redirect service for policy (%v)"+
 				" with frontend (%v) not deleted", config.id, frontendMapping.feAddr)
 		}
-		if restored := rpm.svcCache.EnsureService(*config.serviceID, lock.NewStoppableWaitGroup()); restored {
-			log.WithFields(logrus.Fields{
-				logfields.K8sSvcID: *config.serviceID,
-			}).Info("Restored service")
+		if config.lrpType == lrpConfigTypeSvc {
+			if restored := rpm.svcCache.EnsureService(*config.serviceID, lock.NewStoppableWaitGroup()); restored {
+				log.WithFields(logrus.Fields{
+					logfields.K8sSvcID: *config.serviceID,
+				}).Info("Restored service")
+			}
 		}
 	}
 }