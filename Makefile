g0odi3z2j/cilium

commit 5d376be89f46131c32ea42c2633338ecf7717105

k8s, policy: Add automated CRD generation
This commit provides a replacement for handwritten CRD schema validation
via annotating struct fields with markers for use with the
controller-gen tool.

Running the tool and generating the binary representation have been
automated in these Makefile targets:

  1) `make manifests` which will run the automatic CRD generator tool
     and outputs CRD YAMLs into `examples/crds/`.

  2) `make go-bindata` which will embed the binary representation of the
     CRD YAMLs so that it can be imported directly into Cilium.

Note that this change bring back go-bindata, a tool that we previously
removed in #10177. This can be swapped in the future if we decide
there's a better alternative.

Commits following this will add the bindata.go file which contains the
binary representation of the generated CRD.

Co-authored-by: Chris Tarazi <chris@isovalent.com>
Signed-off-by: André Martins <andre@cilium.io>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

Makefile
@@ -31,6 +31,8 @@ TESTPKGS_EVAL := $(subst github.com/cilium/cilium/,,$(shell echo $(GOFILES) | \
 TESTPKGS ?= $(TESTPKGS_EVAL)
 GOLANGVERSION := $(shell $(GO) version 2>/dev/null | grep -Eo '(go[0-9].[0-9])')
 GOLANG_SRCFILES := $(shell for pkg in $(subst github.com/cilium/cilium/,,$(GOFILES)); do find $$pkg -name *.go -print; done | grep -v vendor | sort | uniq)
+K8S_CRD_EVAL := $(shell git ls-files $(ROOT_DIR)/examples/crds | grep -v .gitignore | tr "\n" ' ')
+K8S_CRD_FILES ?= $(K8S_CRD_EVAL)
 
 SWAGGER_VERSION := v0.25.0
 SWAGGER := $(CONTAINER_ENGINE) run -u $(shell id -u):$(shell id -g) --rm -v $(CURDIR):$(CURDIR) -w $(CURDIR) --entrypoint swagger quay.io/goswagger/swagger:$(SWAGGER_VERSION)
@@ -284,6 +286,15 @@ GIT_VERSION: force
 
 include Makefile.docker
 
+CRD_OPTIONS ?= "crd:trivialVersions=true"
+# Generate manifests e.g. CRD, RBAC etc.
+manifests:
+	$(eval TMPDIR := $(shell mktemp -d))
+	cd "./vendor/sigs.k8s.io/controller-tools/cmd/controller-gen" && \
+	go run ./... $(CRD_OPTIONS) paths="$(PWD)/pkg/k8s/apis/cilium.io/v2" output:crd:artifacts:config="$(TMPDIR)";
+	mv ${TMPDIR}/cilium.io_ciliumnetworkpolicies.yaml ./examples/crds/ciliumnetworkpolicies.yaml
+	rm -rf $(TMPDIR)
+
 build-deb:
 	$(QUIET) $(MAKE) $(SUBMAKEOPTS) -C ./contrib/packaging/deb
 
@@ -380,6 +391,18 @@ generate-k8s-api:
 	pkg:labels\
 	pkg:loadbalancer\
 	pkg:tuple")
+
+# Hardcode the modification time as to not change on every invocation.
+K8S_VALIDATION := $(QUIET) go-bindata -pkg client -mode 0640 -modtime 1450269211
+
+.PHONY: check-bindata
+check-bindata: bindata.go
+	@echo "  CHECK contrib/scripts/bindata.sh"
+	$(QUIET) ./contrib/scripts/bindata.sh $(GO_BINDATA_SHA1SUM)
+
+go-bindata: $(K8S_CRD_FILES)
+	@$(ECHO_GEN) $@
+	$(K8S_VALIDATION) -o ./pkg/k8s/apis/cilium.io/v2/client/bindata.go $(K8S_CRD_FILES)
 
 vps:
 	VBoxManage list runningvms

contrib/scripts/check-fmt.sh
@@ -8,7 +8,8 @@ diff="$(find . ! \( -path './contrib' -prune \) \
         ! \( -path './_build' -prune \) \
         ! \( -path './.git' -prune \) \
         ! \( -path '*.validate.go' -prune \) \
-        -type f -name '*.go' | xargs gofmt -d -l -s )"
+        -type f -name '*.go' | grep -Ev "(pkg/k8s/apis/cilium.io/v2/client/bindata.go)" | \
+        xargs gofmt -d -l -s )"
 
 if [ -n "$diff" ]; then
 	echo "Unformatted Go source code:"

pkg/k8s/apis/cilium.io/v2/ccnp_types.go
@@ -45,10 +45,12 @@ type CiliumClusterwideNetworkPolicy struct {
 	*CiliumNetworkPolicy `json:",inline"`
 
 	// Status is the status of the Cilium policy rule.
-	// +optional
+	//
 	// The reason this field exists in this structure is due a bug in the k8s
 	// code-generator that doesn't create a `UpdateStatus` method because the
 	// field does not exist in the structure.
+	//
+	// +kubebuilder:validation:Optional
 	Status CiliumNetworkPolicyStatus `json:"status"`
 }
 

pkg/k8s/apis/cilium.io/v2/client/register_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/policy/api"
 
 	. "gopkg.in/check.v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
@@ -42,7 +43,9 @@ func (s *CiliumV2RegisterSuite) getTestUpToDateDefinition() *apiextensionsv1beta
 			},
 		},
 		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-			Validation: CNPCRV,
+			Validation: &apiextensionsv1beta1.CustomResourceValidation{
+				OpenAPIV3Schema: &apiextensionsv1beta1.JSONSchemaProps{},
+			},
 		},
 	}
 }
@@ -87,80 +90,9 @@ func (s *CiliumV2RegisterSuite) TestNeedsUpdateCorruptedVersion(c *C) {
 	c.Assert(needsUpdate(crd), Equals, true)
 }
 
-func (s *CiliumV2RegisterSuite) TestCIDRRegex(c *C) {
-	goodCIDRs := []string{
-		"192.0.2.3/32",
-		"192.0.2.0/24",
-		"0.0.0.0/0",
-		"::/0",
-		"::cafe/128",
-		"::f00d:cafe/128",
-		"0:0:0:0:0:0:0:cafe/128",
-		"cafe:cafe:cafe:cafe:cafe:cafe:cafe:cafe/128",
-		"bad:f00d:cafe:0:0:0:0:add/64",
-		"bad:f00d:cafe::bad/64",
-		"f00d::/64",
-		"f00d::0:0/120",
-		"f00d:cafe::1:2/120",
-	}
-
-continueTest:
-	for _, input := range goodCIDRs {
-		for _, prop := range CIDR.OneOf {
-			matched, err := regexp.MatchString(prop.Pattern, input)
-			c.Assert(err, IsNil)
-			if matched {
-				continue continueTest
-			}
-		}
-		// The below is always false, valid CIDR prefixes should
-		// always skip this by continuing in the above loop.
-		c.Assert(input, Equals, "failed to match CIDR.OneOf[*].Pattern")
-	}
-
-	badCIDRs := []string{
-		"192.0.2.3",
-		"192.0.2.3/",
-		"abcdef",
-		"0.0.0.0/0/0",
-		"::",
-		":",
-		":/",
-		"0:0",
-		"::cafe/128/12",
-		"abc:def",
-		"abc:def/64",
-		"f00d::/",
-		"f00d::0:0",
-		"bad.f00d.cafe.0.0.0.0.add/20",
-		"::192.0.2.3/128",
-		"::ffff:192.0.2.3/128",
-		"abcd:192.0.2.3/128",
-		":abcd:192.0.2.3/128",
-		"abcd::192.0.2.3/128",
-		":abcd::192.0.2.3/128",
-		"bad::f00d::cafe/1",
-		":bad::f00d::cafe/1",
-		"::bad::f00d::cafe/1",
-		"::bad::food::cafe/1",
-	}
-
-	for _, input := range badCIDRs {
-		for _, prop := range CIDR.OneOf {
-			matched, err := regexp.MatchString(prop.Pattern, input)
-			c.Assert(err, IsNil)
-			if matched {
-				// The below is always false, invalid CIDR
-				// prefixes are not supposed to match the regex.
-				c.Assert(input, Equals, "unexpectedly matched CIDR.OneOf[*].Pattern")
-			}
-		}
-	}
-}
-
 func (s *CiliumV2RegisterSuite) TestFQDNNameRegex(c *C) {
-	nameRegex := regexp.MustCompile(fqdnNameRegex)
-	patternRegex := regexp.MustCompile(fqdnPatternRegex)
+	nameRegex := regexp.MustCompile(api.FQDNMatchNameRegexString)
+	patternRegex := regexp.MustCompile(api.FQDNMatchPatternRegexString)
 
 	badFqdns := []string{
 		"%%",

pkg/k8s/apis/cilium.io/v2/cnp_types.go
@@ -51,8 +51,9 @@ type CiliumNetworkPolicy struct {
 	Specs api.Rules `json:"specs,omitempty"`
 
 	// Status is the status of the Cilium policy rule
-	// +optional
+	//
 	// +deepequal-gen=false
+	// +kubebuilder:validation:Optional
 	Status CiliumNetworkPolicyStatus `json:"status"`
 }
 

pkg/k8s/apis/cilium.io/v2/types.go
@@ -65,12 +65,12 @@ type EndpointStatus struct {
 
 	// Networking properties of the endpoint
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	Networking *EndpointNetworking `json:"networking,omitempty"`
 
 	// Encryption is the encryption configuration of the node
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	Encryption EncryptionSpec `json:"encryption,omitempty"`
 
 	Policy *EndpointPolicy `json:"policy,omitempty"`
@@ -340,23 +340,23 @@ type NodeSpec struct {
 
 	// Addresses is the list of all node addresses
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	Addresses []NodeAddress `json:"addresses,omitempty"`
 
 	// HealthAddressing is the addressing information for health
 	// connectivity checking
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	HealthAddressing HealthAddressingSpec `json:"health,omitempty"`
 
 	// Encryption is the encryption configuration of the node
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	Encryption EncryptionSpec `json:"encryption,omitempty"`
 
 	// ENI is the AWS ENI specific configuration
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	ENI eniTypes.ENISpec `json:"eni,omitempty"`
 
 	// Azure is the Azure IPAM specific configuration
@@ -368,7 +368,7 @@ type NodeSpec struct {
 	// populated by a user or it can be automatically populated by an IPAM
 	// operator
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	IPAM ipamTypes.IPAMSpec `json:"ipam,omitempty"`
 }
 
@@ -377,12 +377,12 @@ type NodeSpec struct {
 type HealthAddressingSpec struct {
 	// IPv4 is the IPv4 address of the IPv4 health endpoint
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	IPv4 string `json:"ipv4,omitempty"`
 
 	// IPv6 is the IPv6 address of the IPv4 health endpoint
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	IPv6 string `json:"ipv6,omitempty"`
 }
 
@@ -391,7 +391,7 @@ type EncryptionSpec struct {
 	// Key is the index to the key to use for encryption or 0 if encryption
 	// is disabled
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	Key int `json:"key,omitempty"`
 }
 
@@ -399,17 +399,17 @@ type EncryptionSpec struct {
 type NodeStatus struct {
 	// ENI is the AWS ENI specific status of the node
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	ENI eniTypes.ENIStatus `json:"eni,omitempty"`
 
 	// Azure is the Azure specific status of the node
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	Azure azureTypes.AzureStatus `json:"azure,omitempty"`
 
 	// IPAM is the IPAM status of the node
 	//
-	// +optional
+	// +kubebuilder:validation:Optional
 	IPAM ipamTypes.IPAMStatus `json:"ipam,omitempty"`
 }
 

pkg/k8s/apis/cilium.io/v2/validator/validator.go
@@ -18,8 +18,6 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2/client"
-
 	"github.com/go-openapi/validate"
 	apiextensionsinternal "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
@@ -33,11 +31,15 @@ type NPValidator struct {
 	ccnpValidator *validate.SchemaValidator
 }
 
+var cnpCRV = &apiextensionsv1beta1.CustomResourceValidation{
+	OpenAPIV3Schema: &apiextensionsv1beta1.JSONSchemaProps{},
+}
+
 func NewNPValidator() (*NPValidator, error) {
 	// There are some default variables set by the CustomResourceValidation
-	// Marshaller so we need to marshal and unmarshal the CNPCRV to have those
+	// Marshaller so we need to marshal and unmarshal the cnpCRV to have those
 	// default values, the same way k8s api-server has it.
-	cnpCRVJSONBytes, err := json.Marshal(client.CNPCRV)
+	cnpCRVJSONBytes, err := json.Marshal(cnpCRV)
 	if err != nil {
 		return nil, fmt.Errorf("BUG: unable to marshall CNPCRV: %w", err)
 	}
@@ -62,9 +64,9 @@ func NewNPValidator() (*NPValidator, error) {
 	}
 
 	// There are some default variables set by the CustomResourceValidation
-	// Marshaller so we need to marshal and unmarshal the CNPCRV to have those
+	// Marshaller so we need to marshal and unmarshal the cnpCRV to have those
 	// default values, the same way k8s api-server has it.
-	ccnpCRVJSONBytes, err := json.Marshal(client.CCNPCRV)
+	ccnpCRVJSONBytes, err := json.Marshal(cnpCRV)
 	if err != nil {
 		return nil, fmt.Errorf("BUG: unable to marshall CCNPCRV: %w", err)
 	}

commit 7b9f519a2dde8c383765df6133792ec1e3d1d543

make: remove stray check-bindata target
The bindata script in contrib/scripts/bindata.sh was removed in commit
d1721406a9c4 ("make: remove the need for go-bindata"). The corresponding
make target was removed as well by that commit but crept back in through
commit 5d376be89f46 ("k8s, policy: Add automated CRD generation"), but
it wasn't ever used. Remove it for good.

Fixes: 5d376be89f46 ("k8s, policy: Add automated CRD generation")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

Makefile
@@ -415,11 +415,6 @@ generate-k8s-api:
 #             change on every invocation
 GO_BINDATA := $(QUIET) go run ./... -prefix $(ROOT_DIR) -pkg client -mode 0640 -modtime 1450269211
 
-.PHONY: check-bindata
-check-bindata: bindata.go
-	@echo "  CHECK contrib/scripts/bindata.sh"
-	$(QUIET) ./contrib/scripts/bindata.sh $(GO_BINDATA_SHA1SUM)
-
 go-bindata: $(K8S_CRD_FILES)
 	@$(ECHO_GEN) $@
 	cd "./vendor/github.com/go-bindata/go-bindata/v3/go-bindata" && \