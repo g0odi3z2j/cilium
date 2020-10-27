g0odi3z2j/cilium

commit 2c5ab74f895a81e4f45cffca14394ac63f184fce

bpf: Fix clustermesh policy with endpoint-routes
In endpoint-routes mode, we encode the source identity in the ctx->mark
when locally routing the packet to the destination device for ingress
policy assessment. Previously we only encoded the local cluster
identity in the mark, thereby omitting the original cluster portion of
the identity.

Found by code inspection.

Fixes: 8267885b071e ("bpf: Skip ingress policy at egress of source if egress prog is in use")
Signed-off-by: Joe Stringer <joe@cilium.io>

bpf/lib/l3.h
@@ -86,7 +86,8 @@ static __always_inline int ipv6_local_delivery(struct __ctx_buff *ctx, int l3_of
 
 #if defined(USE_BPF_PROG_FOR_INGRESS_POLICY) && \
 	!defined(FORCE_LOCAL_POLICY_EVAL_AT_SOURCE)
-	ctx->mark = (seclabel << 16) | MARK_MAGIC_IDENTITY;
+	ctk->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, seclabel)
 	return redirect_peer(ep->ifindex, 0);
 #else
 	ctx_store_meta(ctx, CB_SRC_LABEL, seclabel);
@@ -126,7 +127,8 @@ static __always_inline int ipv4_local_delivery(struct __ctx_buff *ctx, int l3_of
 
 #if defined(USE_BPF_PROG_FOR_INGRESS_POLICY) && \
 	!defined(FORCE_LOCAL_POLICY_EVAL_AT_SOURCE)
-	ctx->mark = (seclabel << 16) | MARK_MAGIC_IDENTITY;
+	ctk->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, seclabel)
 	return redirect_peer(ep->ifindex, 0);
 #else
 	ctx_store_meta(ctx, CB_SRC_LABEL, seclabel);

commit 3c80f99b599df2f8fdd00c6331022eb87f201527

bpf: Fix --force-local-policy-eval-at-source=false
When this flag was set to false (not the default), it failed to compile
due to the typo. Fix it.

The bpf build_all target also did not pick up on this, add these options
to the permutations for building.

Fixes: 2c5ab74f895a ("bpf: Fix clustermesh policy with endpoint-routes")
Fixes: #12694
Signed-off-by: Joe Stringer <joe@cilium.io>

bpf/Makefile
@@ -177,7 +177,8 @@ LXC_OPTIONS = \
 	 -DENABLE_HOST_REDIRECT:-DENABLE_IPV4:-DENABLE_IPV6 \
 	 -DENABLE_HOST_REDIRECT:-DENABLE_IPV4:-DENABLE_IPV6:-DENABLE_NAT46 \
 	 -DENABLE_NODEPORT:-DENABLE_IPV4:-DENABLE_IPV6 \
-	 -DENABLE_NODEPORT:-DENABLE_DSR:-DENABLE_IPV4:-DENABLE_IPV6
+	 -DENABLE_NODEPORT:-DENABLE_DSR:-DENABLE_IPV4:-DENABLE_IPV6 \
+	 -DENABLE_IPV4:-DENABLE_IPV4:-DUSE_BPF_PROG_FOR_INGRESS_POLICY
 
 # These options are intended to max out the BPF program complexity. it is load
 # tested as well.

bpf/lib/l3.h
@@ -85,8 +85,8 @@ static __always_inline int ipv6_local_delivery(struct __ctx_buff *ctx, int l3_of
 
 #if defined(USE_BPF_PROG_FOR_INGRESS_POLICY) && \
 	!defined(FORCE_LOCAL_POLICY_EVAL_AT_SOURCE)
-	ctk->mark |= MARK_MAGIC_IDENTITY;
-	set_identity_mark(ctx, seclabel)
+	ctx->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, seclabel);
 
 	return redirect_ep(ep->ifindex, from_host);
 #else
@@ -127,8 +127,8 @@ static __always_inline int ipv4_local_delivery(struct __ctx_buff *ctx, int l3_of
 
 #if defined(USE_BPF_PROG_FOR_INGRESS_POLICY) && \
 	!defined(FORCE_LOCAL_POLICY_EVAL_AT_SOURCE)
-	ctk->mark |= MARK_MAGIC_IDENTITY;
-	set_identity_mark(ctx, seclabel)
+	ctx->mark |= MARK_MAGIC_IDENTITY;
+	set_identity_mark(ctx, seclabel);
 
 	return redirect_ep(ep->ifindex, from_host);
 #else