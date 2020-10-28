g0odi3z2j/cilium

commit 2aa01eb2cec5675cd9a6e3ba3796b056c291f3b1

pkg/kvstore: add etcd lease information into cilium status
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/etcd.go
@@ -832,8 +832,14 @@ func (e *etcdClient) statusChecker() {
 
 		allConnected := len(endpoints) == ok
 
+		e.RWMutex.RLock()
+		sessionLeaseID := e.session.Lease()
+		lockSessionLeaseID := e.lockSession.Lease()
+		e.RWMutex.RUnlock()
+
 		e.statusLock.Lock()
-		e.latestStatusSnapshot = fmt.Sprintf("etcd: %d/%d connected, has-quorum=%t: %s", ok, len(endpoints), hasQuorum, strings.Join(newStatus, "; "))
+		e.latestStatusSnapshot = fmt.Sprintf("etcd: %d/%d connected, lease-ID=%x, lock lease-ID=%x, has-quorum=%t: %s",
+			ok, len(endpoints), sessionLeaseID, lockSessionLeaseID, hasQuorum, strings.Join(newStatus, "; "))
 
 		// Only mark the etcd health as unstable if no etcd endpoints can be reached
 		if len(endpoints) > 0 && ok == 0 {

commit 6d1708067240f9dede91a9f727c07795bf98f4e1

pkg/kvstore: fix race in etcd initialization
Fixes: 2aa01eb2cec5 ("pkg/kvstore: add etcd lease information into cilium status")
Signed-off-by: André Martins <andre@cilium.io>

pkg/kvstore/etcd.go
@@ -698,28 +698,6 @@ func connectEtcdClient(ctx context.Context, config *client.Config, cfgPath strin
 	var s, ls concurrency.Session
 	errorChan := make(chan error)
 
-	// create session in parallel as this is a blocking operation
-	go func() {
-		session, err := concurrency.NewSession(c, concurrency.WithTTL(int(option.Config.KVstoreLeaseTTL.Seconds())))
-		if err != nil {
-			errorChan <- err
-			close(errorChan)
-			return
-		}
-		lockSession, err := concurrency.NewSession(c, concurrency.WithTTL(int(defaults.LockLeaseTTL.Seconds())))
-		if err != nil {
-			errorChan <- err
-			close(errorChan)
-			return
-		}
-		s = *session
-		ls = *lockSession
-
-		log.Infof("Got lease ID %x", s.Lease())
-		log.Infof("Got lock lease ID %x", ls.Lease())
-		close(errorChan)
-	}()
-
 	ec := &etcdClient{
 		client:               c,
 		config:               config,
@@ -734,6 +712,31 @@ func connectEtcdClient(ctx context.Context, config *client.Config, cfgPath strin
 		limiter:              rate.NewLimiter(rate.Limit(clientOptions.RateLimit), clientOptions.RateLimit),
 		statusCheckErrors:    make(chan error, 128),
 	}
+
+	// create session in parallel as this is a blocking operation
+	go func() {
+		session, err := concurrency.NewSession(c, concurrency.WithTTL(int(option.Config.KVstoreLeaseTTL.Seconds())))
+		if err != nil {
+			errorChan <- err
+			close(errorChan)
+			return
+		}
+		lockSession, err := concurrency.NewSession(c, concurrency.WithTTL(int(defaults.LockLeaseTTL.Seconds())))
+		if err != nil {
+			errorChan <- err
+			close(errorChan)
+			return
+		}
+
+		ec.RWMutex.Lock()
+		s = *session
+		ls = *lockSession
+		ec.RWMutex.Unlock()
+
+		log.Infof("Got lease ID %x", s.Lease())
+		log.Infof("Got lock lease ID %x", ls.Lease())
+		close(errorChan)
+	}()
 
 	handleSessionError := func(err error) {
 		ec.RWMutex.Lock()