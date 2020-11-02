g0odi3z2j/cilium

commit 8f78334947e05f9d90cc00167e8f3e8ff9019ce8

lbmap: Add SourceRange maps
The maps are going to be used by the LB source range check. The check is
going to be enabled for those services which type is LoadBlancer AND
.spec.loadBalancerSourceRanges is not empty. Such service flags have the
check source range bit being set. This allows the datapath to avoid
doing a lookup in the maps for each request sent to a service of the
LoadBalancer type.

The maps are of the LPM type, and a key is a tuple containing LPM
prefix, SVC rev_nat_id (aka svc ID), some padding and source IPv{4,6}
addr.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/maps/lbmap/source_range.go
@@ -1,0 +1,159 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lbmap
+
+import (
+	"fmt"
+	"net"
+	"unsafe"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/cidr"
+	"github.com/cilium/cilium/pkg/types"
+)
+
+const (
+	SourceRange4MapName = "cilium_lb4_source_range"
+	SourceRange6MapName = "cilium_lb6_source_range"
+	lpmPrefixLen4       = 16 + 16 // sizeof(SourceRangeKey4.RevNATID)+sizeof(SourceRangeKey4.Pad)
+	lpmPrefixLen6       = 16 + 16 // sizeof(SourceRangeKey6.RevNATID)+sizeof(SourceRangeKey6.Pad)
+)
+
+type SourceRangeKey interface {
+	GetCIDR() *cidr.CIDR
+	GetRevNATID() uint16
+}
+
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type SourceRangeKey4 struct {
+	PrefixLen uint32     `align:"lpm_key"`
+	RevNATID  uint16     `align:"rev_nat_id"`
+	Pad       uint16     `align:"pad"`
+	Address   types.IPv4 `align:"addr"`
+}
+
+func (k *SourceRangeKey4) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+func (k *SourceRangeKey4) NewValue() bpf.MapValue    { return &SourceRangeValue{} }
+func (k *SourceRangeKey4) String() string            { return fmt.Sprintf("%s", k.Address) }
+func (k *SourceRangeKey4) ToNetwork() *SourceRangeKey4 {
+	n := *k
+	// For some reasons rev_nat_index is stored in network byte order in
+	// the SVC BPF maps
+	n.RevNATID = byteorder.HostToNetwork(n.RevNATID).(uint16)
+	return &n
+}
+func (k *SourceRangeKey4) GetCIDR() *cidr.CIDR {
+	var (
+		c  net.IPNet
+		ip types.IPv4
+	)
+	c.Mask = net.CIDRMask(int(k.PrefixLen)-lpmPrefixLen4, 32)
+	k.Address.DeepCopyInto(&ip)
+	c.IP = ip.IP()
+	return cidr.NewCIDR(&c)
+}
+func (k *SourceRangeKey4) GetRevNATID() uint16 {
+	return k.RevNATID
+}
+
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
+type SourceRangeKey6 struct {
+	PrefixLen uint32     `align:"lpm_key"`
+	RevNATID  uint16     `align:"rev_nat_id"`
+	Pad       uint16     `align:"pad"`
+	Address   types.IPv6 `align:"addr"`
+}
+
+func (k *SourceRangeKey6) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
+func (k *SourceRangeKey6) NewValue() bpf.MapValue    { return &SourceRangeValue{} }
+func (k *SourceRangeKey6) String() string            { return fmt.Sprintf("%s", k.Address) }
+func (k *SourceRangeKey6) ToNetwork() *SourceRangeKey6 {
+	n := *k
+	// For some reasons rev_nat_index is stored in network byte order in
+	// the SVC BPF maps
+	n.RevNATID = byteorder.HostToNetwork(n.RevNATID).(uint16)
+	return &n
+}
+func (k *SourceRangeKey6) GetCIDR() *cidr.CIDR {
+	var (
+		c  net.IPNet
+		ip types.IPv6
+	)
+	c.Mask = net.CIDRMask(int(k.PrefixLen)-lpmPrefixLen6, 128)
+	k.Address.DeepCopyInto(&ip)
+	c.IP = ip.IP()
+	return cidr.NewCIDR(&c)
+}
+func (k *SourceRangeKey6) GetRevNATID() uint16 {
+	return k.RevNATID
+}
+
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapValue
+type SourceRangeValue struct {
+	Pad uint8 // not used
+}
+
+func (v *SourceRangeValue) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(v) }
+func (v *SourceRangeValue) String() string              { return "" }
+
+var SourceRange4Map = bpf.NewMap(
+	SourceRange4MapName,
+	bpf.MapTypeLPMTrie,
+	&SourceRangeKey4{}, int(unsafe.Sizeof(SourceRangeKey4{})),
+	&SourceRangeValue{}, int(unsafe.Sizeof(SourceRangeValue{})),
+	MaxEntries,
+	bpf.BPF_F_NO_PREALLOC, 0,
+	func(key []byte, value []byte, mapKey bpf.MapKey, mapValue bpf.MapValue) (bpf.MapKey, bpf.MapValue, error) {
+		sKey, sVal := mapKey.(*SourceRangeKey4), mapValue.(*SourceRangeValue)
+		if _, _, err := bpf.ConvertKeyValue(key, value, sKey, sVal); err != nil {
+			return nil, nil, err
+		}
+		return sKey.ToNetwork(), sVal, nil
+	},
+).WithCache()
+
+var SourceRange6Map = bpf.NewMap(
+	SourceRange6MapName,
+	bpf.MapTypeLPMTrie,
+	&SourceRangeKey6{}, int(unsafe.Sizeof(SourceRangeKey6{})),
+	&SourceRangeValue{}, int(unsafe.Sizeof(SourceRangeValue{})),
+	MaxEntries,
+	bpf.BPF_F_NO_PREALLOC, 0,
+	func(key []byte, value []byte, mapKey bpf.MapKey, mapValue bpf.MapValue) (bpf.MapKey, bpf.MapValue, error) {
+		sKey, sVal := mapKey.(*SourceRangeKey6), mapValue.(*SourceRangeValue)
+		if _, _, err := bpf.ConvertKeyValue(key, value, sKey, sVal); err != nil {
+			return nil, nil, err
+		}
+		return sKey.ToNetwork(), sVal, nil
+	},
+).WithCache()
+
+func srcRangeKey(cidr *cidr.CIDR, revNATID uint16, ipv6 bool) bpf.MapKey {
+	ones, _ := cidr.Mask.Size()
+	id := byteorder.HostToNetwork(revNATID).(uint16)
+	if ipv6 {
+		key := &SourceRangeKey6{PrefixLen: uint32(ones) + lpmPrefixLen6, RevNATID: id}
+		copy(key.Address[:], cidr.IP.To16())
+		return key
+	} else {
+		key := &SourceRangeKey4{PrefixLen: uint32(ones) + lpmPrefixLen4, RevNATID: id}
+		copy(key.Address[:], cidr.IP.To4())
+		return key
+	}
+}

pkg/maps/lbmap/lbmap.go
@@ -20,6 +20,7 @@ import (
 	"strconv"
 
 	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -51,8 +52,8 @@ func (*LBBPFMap) UpsertService(
 	svcID uint16, svcIP net.IP, svcPort uint16,
 	backendIDs []uint16, prevBackendCount int,
 	ipv6 bool, svcType loadbalancer.SVCType, svcLocal bool,
-	svcScope uint8, sessionAffinity bool,
-	sessionAffinityTimeoutSec uint32) error {
+	svcScope uint8, sessionAffinity bool, sessionAffinityTimeoutSec uint32,
+	checkSourceRange bool) error {
 	var svcKey ServiceKey
 
 	if svcID == 0 {
@@ -90,7 +91,7 @@ func (*LBBPFMap) UpsertService(
 	}
 
 	if err := updateMasterService(svcKey, len(backendIDs), int(svcID), svcType, svcLocal,
-		sessionAffinity, sessionAffinityTimeoutSec); err != nil {
+		sessionAffinity, sessionAffinityTimeoutSec, checkSourceRange); err != nil {
 
 		deleteRevNatLocked(revNATKey)
 		return fmt.Errorf("Unable to update service %+v: %s", svcKey, err)
@@ -229,6 +230,28 @@ func (*LBBPFMap) DumpAffinityMatches() (BackendIDByServiceIDSet, error) {
 	return matches, nil
 }
 
+func (*LBBPFMap) DumpSourceRanges(ipv6 bool) (SourceRangeSetByServiceID, error) {
+	ret := SourceRangeSetByServiceID{}
+	parser := func(key bpf.MapKey, value bpf.MapValue) {
+		k := key.(SourceRangeKey)
+		revNATID := k.GetRevNATID()
+		if _, found := ret[revNATID]; !found {
+			ret[revNATID] = []*cidr.CIDR{}
+		}
+		ret[revNATID] = append(ret[revNATID], k.GetCIDR())
+	}
+
+	m := SourceRange4Map
+	if ipv6 {
+		m = SourceRange6Map
+	}
+	if err := m.DumpWithCallback(parser); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
 func updateRevNatLocked(key RevNatKey, value RevNatValue) error {
 	if key.GetKey() == 0 {
 		return fmt.Errorf("invalid RevNat ID (0)")
@@ -242,6 +265,38 @@ func updateRevNatLocked(key RevNatKey, value RevNatValue) error {
 
 func deleteRevNatLocked(key RevNatKey) error {
 	return key.Map().Delete(key.ToNetwork())
+}
+
+func (*LBBPFMap) UpdateSourceRanges(revNATID uint16, prevSourceRanges []*cidr.CIDR,
+	sourceRanges []*cidr.CIDR, ipv6 bool) error {
+
+	m := SourceRange4Map
+	if ipv6 {
+		m = SourceRange6Map
+	}
+
+	srcRangeMap := map[string]*cidr.CIDR{}
+	for _, cidr := range sourceRanges {
+		srcRangeMap[cidr.String()] = cidr
+	}
+
+	for _, prevCIDR := range prevSourceRanges {
+		if _, found := srcRangeMap[prevCIDR.String()]; !found {
+			if err := m.Delete(srcRangeKey(prevCIDR, revNATID, ipv6)); err != nil {
+				return err
+			}
+		} else {
+			delete(srcRangeMap, prevCIDR.String())
+		}
+	}
+
+	for _, cidr := range srcRangeMap {
+		if err := m.Update(srcRangeKey(cidr, revNATID, ipv6), &SourceRangeValue{}); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // DumpServiceMaps dumps the services from the BPF maps.
@@ -365,17 +420,19 @@ func (*LBBPFMap) DumpBackendMaps() ([]*loadbalancer.Backend, error) {
 }
 
 func updateMasterService(fe ServiceKey, nbackends int, revNATID int, svcType loadbalancer.SVCType,
-	svcLocal bool, sessionAffinity bool, sessionAffinityTimeoutSec uint32) error {
+	svcLocal bool, sessionAffinity bool, sessionAffinityTimeoutSec uint32,
+	checkSourceRange bool) error {
 
 	fe.SetBackendSlot(0)
 	zeroValue := fe.NewValue().(ServiceValue)
 	zeroValue.SetCount(nbackends)
 	zeroValue.SetRevNat(revNATID)
 	flag := loadbalancer.NewSvcFlag(&loadbalancer.SvcFlagParam{
-		SvcType:         svcType,
-		SvcLocal:        svcLocal,
-		SessionAffinity: sessionAffinity,
-		IsRoutable:      !fe.IsSurrogate(),
+		SvcType:          svcType,
+		SvcLocal:         svcLocal,
+		SessionAffinity:  sessionAffinity,
+		IsRoutable:       !fe.IsSurrogate(),
+		CheckSourceRange: checkSourceRange,
 	})
 	zeroValue.SetFlags(flag.UInt8())
 	if sessionAffinity {

pkg/maps/lbmap/lbmap_mock.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/cilium/cilium/pkg/cidr"
 	lb "github.com/cilium/cilium/pkg/loadbalancer"
 )
 
@@ -25,6 +26,7 @@ type LBMockMap struct {
 	BackendByID   map[uint16]*lb.Backend
 	ServiceByID   map[uint16]*lb.SVC
 	AffinityMatch BackendIDByServiceIDSet
+	SourceRanges  SourceRangeSetByServiceID
 }
 
 func NewLBMockMap() *LBMockMap {
@@ -32,12 +34,13 @@ func NewLBMockMap() *LBMockMap {
 		BackendByID:   map[uint16]*lb.Backend{},
 		ServiceByID:   map[uint16]*lb.SVC{},
 		AffinityMatch: BackendIDByServiceIDSet{},
+		SourceRanges:  SourceRangeSetByServiceID{},
 	}
 }
 
 func (m *LBMockMap) UpsertService(id uint16, ip net.IP, port uint16,
 	backendIDs []uint16, prevCount int, ipv6 bool, svcType lb.SVCType, svcLocal bool,
-	svcScope uint8, sessionAffinity bool, sessionAffinityTimeoutSec uint32) error {
+	svcScope uint8, sessionAffinity bool, sessionAffinityTimeoutSec uint32, checkLBSrcRange bool) error {
 
 	backends := make([]lb.Backend, len(backendIDs))
 	for i, backendID := range backendIDs {
@@ -146,4 +149,22 @@ func (m *LBMockMap) DeleteAffinityMatch(revNATID uint16, backendID uint16) error
 
 func (m *LBMockMap) DumpAffinityMatches() (BackendIDByServiceIDSet, error) {
 	return m.AffinityMatch, nil
+}
+
+func (m *LBMockMap) UpdateSourceRanges(revNATID uint16, prevRanges []*cidr.CIDR,
+	ranges []*cidr.CIDR, ipv6 bool) error {
+
+	if len(prevRanges) == 0 {
+		m.SourceRanges[revNATID] = []*cidr.CIDR{}
+	}
+	if len(prevRanges) != len(m.SourceRanges[revNATID]) {
+		return fmt.Errorf("Inconsistent view of source ranges")
+	}
+	m.SourceRanges[revNATID] = ranges
+
+	return nil
+}
+
+func (m *LBMockMap) DumpSourceRanges(ipv6 bool) (SourceRangeSetByServiceID, error) {
+	return m.SourceRanges, nil
 }

pkg/maps/lbmap/types.go
@@ -18,6 +18,7 @@ import (
 	"net"
 
 	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 )
 
@@ -163,6 +164,8 @@ type RevNatValue interface {
 // BackendIDByServiceIDSet is the type of a set for checking whether a backend
 // belongs to a given service
 type BackendIDByServiceIDSet map[uint16]map[uint16]struct{} // svc ID => backend ID
+
+type SourceRangeSetByServiceID map[uint16][]*cidr.CIDR // svc ID => src range CIDRs
 
 func svcFrontend(svcKey ServiceKey, svcValue ServiceValue) *loadbalancer.L3n4AddrID {
 	feL3n4Addr := loadbalancer.NewL3n4Addr(loadbalancer.NONE, svcKey.GetAddress(), svcKey.GetPort(), svcKey.GetScope())

pkg/maps/lbmap/zz_generated.deepcopy.go
@@ -540,3 +540,77 @@ func (in *SockRevNat6Value) DeepCopyMapValue() bpf.MapValue {
 	}
 	return nil
 }
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *SourceRangeKey4) DeepCopyInto(out *SourceRangeKey4) {
+	*out = *in
+	in.Address.DeepCopyInto(&out.Address)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new SourceRangeKey4.
+func (in *SourceRangeKey4) DeepCopy() *SourceRangeKey4 {
+	if in == nil {
+		return nil
+	}
+	out := new(SourceRangeKey4)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *SourceRangeKey4) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *SourceRangeKey6) DeepCopyInto(out *SourceRangeKey6) {
+	*out = *in
+	in.Address.DeepCopyInto(&out.Address)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new SourceRangeKey6.
+func (in *SourceRangeKey6) DeepCopy() *SourceRangeKey6 {
+	if in == nil {
+		return nil
+	}
+	out := new(SourceRangeKey6)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapKey is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapKey.
+func (in *SourceRangeKey6) DeepCopyMapKey() bpf.MapKey {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *SourceRangeValue) DeepCopyInto(out *SourceRangeValue) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new SourceRangeValue.
+func (in *SourceRangeValue) DeepCopy() *SourceRangeValue {
+	if in == nil {
+		return nil
+	}
+	out := new(SourceRangeValue)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyMapValue is an autogenerated deepcopy function, copying the receiver, creating a new bpf.MapValue.
+func (in *SourceRangeValue) DeepCopyMapValue() bpf.MapValue {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}

commit 2f3347d43d77064dc16ae1c423aa7dcc900b6869

pkg/maps: fix interface implementation for SourceRangeKey{4,6}
The interface SourceRangeKey should implement the ToNetwork() and
ToHost() methods by returning a SourceRangeKey and not a pointer for the
implementation structure.

Fixes: 8f78334947e0 ("lbmap: Add SourceRange maps")
Fixes: 973e279b2d20 ("lbmap: Optimize lbmap byte order related code")
Signed-off-by: André Martins <andre@cilium.io>

pkg/maps/lbmap/source_range.go
@@ -55,7 +55,7 @@ type SourceRangeKey4 struct {
 func (k *SourceRangeKey4) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
 func (k *SourceRangeKey4) NewValue() bpf.MapValue    { return &SourceRangeValue{} }
 func (k *SourceRangeKey4) String() string            { return fmt.Sprintf("%s", k.Address) }
-func (k *SourceRangeKey4) ToNetwork() *SourceRangeKey4 {
+func (k *SourceRangeKey4) ToNetwork() SourceRangeKey {
 	n := *k
 	// For some reasons rev_nat_index is stored in network byte order in
 	// the SVC BPF maps
@@ -64,7 +64,7 @@ func (k *SourceRangeKey4) ToNetwork() *SourceRangeKey4 {
 }
 
 // ToHost returns the key in the host byte order
-func (k *SourceRangeKey4) ToHost() *SourceRangeKey4 {
+func (k *SourceRangeKey4) ToHost() SourceRangeKey {
 	h := *k
 	h.RevNATID = byteorder.NetworkToHost(h.RevNATID).(uint16)
 	return &h
@@ -96,7 +96,7 @@ type SourceRangeKey6 struct {
 func (k *SourceRangeKey6) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
 func (k *SourceRangeKey6) NewValue() bpf.MapValue    { return &SourceRangeValue{} }
 func (k *SourceRangeKey6) String() string            { return fmt.Sprintf("%s", k.Address) }
-func (k *SourceRangeKey6) ToNetwork() *SourceRangeKey6 {
+func (k *SourceRangeKey6) ToNetwork() SourceRangeKey {
 	n := *k
 	// For some reasons rev_nat_index is stored in network byte order in
 	// the SVC BPF maps
@@ -105,7 +105,7 @@ func (k *SourceRangeKey6) ToNetwork() *SourceRangeKey6 {
 }
 
 // ToHost returns the key in the host byte order
-func (k *SourceRangeKey6) ToHost() *SourceRangeKey6 {
+func (k *SourceRangeKey6) ToHost() SourceRangeKey {
 	h := *k
 	h.RevNATID = byteorder.NetworkToHost(h.RevNATID).(uint16)
 	return &h