g0odi3z2j/cilium

commit 7b497d92576790bb754cae580e7f6f5e4427a089

add error log When ipam allocate nodecidr failure
Signed-off-by: hui.kong <konghui@live.cn>

pkg/ipam/allocator/podcidr/podcidr.go
@@ -15,6 +15,7 @@
 package podcidr
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net"
@@ -171,6 +172,7 @@ type NodesPodCIDRManager struct {
 }
 
 type CIDRAllocator interface {
+	fmt.Stringer
 	Occupy(cidr *net.IPNet) error
 	AllocateNext() (*net.IPNet, error)
 	Release(cidr *net.IPNet) error
@@ -849,10 +851,35 @@ func (n *NodesPodCIDRManager) allocateNext(nodeName string) (*nodeCIDRs, bool, e
 		nCIDRs.v6PodCIDRs = []*net.IPNet{v6CIDR}
 	}
 
+	if nCIDRs == nil {
+		err = fmt.Errorf("Unable to allocate node CIDR for node %s. IPAMInfo: {IPv4: %s, IPv6: %s}. Please check that your configuration is correct",
+			nodeName,
+			getCIDRAllocatorsInfo(n.v4CIDRAllocators, v4AllocatorType),
+			getCIDRAllocatorsInfo(n.v6CIDRAllocators, v6AllocatorType))
+		return nil, false, err
+	}
+
 	n.nodes[nodeName] = nCIDRs
 
 	return nCIDRs, true, nil
 
+}
+
+func getCIDRAllocatorsInfo(cidrAllocators []CIDRAllocator, netTypes string) string {
+	var cidrAllocatorsInfo bytes.Buffer
+	cidrAllocatorsLength := len(cidrAllocators)
+	if cidrAllocatorsLength == 0 {
+		return "[]"
+	}
+
+	for index, cidrAllocator := range cidrAllocators {
+		cidrAllocatorsInfo.WriteString(fmt.Sprintf("%s", cidrAllocator.String()))
+		if index < cidrAllocatorsLength-1 {
+			cidrAllocatorsInfo.WriteString(fmt.Sprintf(", "))
+		}
+	}
+
+	return fmt.Sprintf("[%s]", cidrAllocatorsInfo.String())
 }
 
 // allocateFirstFreeCIDR allocates the first CIDR available from the slice of

pkg/ipam/allocator/podcidr/podcidr_test.go
@@ -79,6 +79,10 @@ type mockCIDRAllocator struct {
 	OnInRange      func(cidr *net.IPNet) bool
 }
 
+func (d *mockCIDRAllocator) String() string {
+	return "clusterCIDR: 10.0.0.0/24, nodeMask: 24"
+}
+
 func (d *mockCIDRAllocator) Occupy(cidr *net.IPNet) error {
 	if d.OnOccupy != nil {
 		return d.OnOccupy(cidr)

commit a50103a1d6efd32c73581e5f644e15cf024da39f

ipam/allocator: Fix nil check on node CIDR
Previously, the nil check was useless because the variable is never
going to be nil, as it was initialized to an empty `nodeCIDRs` instance.
This made the "Unable to allocate node CIDR" error impossible to
surface.

This commit fixes this by making the variable not a pointer, but rather
a value, and reworking the further checks to ensure that the error in
allocating a node CIDR can be surfaced.

Fixes: 7b497d9257 ("add error log When ipam allocate nodecidr failure")
Fixes: https://github.com/cilium/cilium/pull/13299

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipam/allocator/podcidr/podcidr.go
@@ -849,9 +849,10 @@ func (n *NodesPodCIDRManager) allocateNext(nodeName string) (*nodeCIDRs, bool, e
 		}
 	}()
 
-	var v4CIDR, v6CIDR *net.IPNet
-
-	nCIDRs := &nodeCIDRs{}
+	var (
+		cidrs          nodeCIDRs
+		v4CIDR, v6CIDR *net.IPNet
+	)
 
 	// Only allocate a v4 CIDR if the v4CIDR allocator is available
 	if len(n.v4CIDRAllocators) != 0 {
@@ -859,21 +860,25 @@ func (n *NodesPodCIDRManager) allocateNext(nodeName string) (*nodeCIDRs, bool, e
 		if err != nil {
 			return nil, false, err
 		}
-		revertStack.Push(revertFunc)
+
 		log.WithField("CIDR", v4CIDR).Debug("v4 allocated CIDR")
-		nCIDRs.v4PodCIDRs = []*net.IPNet{v4CIDR}
+		cidrs.v4PodCIDRs = []*net.IPNet{v4CIDR}
+
+		revertStack.Push(revertFunc)
 	}
 	if len(n.v6CIDRAllocators) != 0 {
 		revertFunc, v6CIDR, err = allocateFirstFreeCIDR(n.v6CIDRAllocators)
 		if err != nil {
 			return nil, false, err
 		}
-		revertStack.Push(revertFunc)
+
 		log.WithField("CIDR", v6CIDR).Debug("v6 allocated CIDR")
-		nCIDRs.v6PodCIDRs = []*net.IPNet{v6CIDR}
+		cidrs.v6PodCIDRs = []*net.IPNet{v6CIDR}
+
+		revertStack.Push(revertFunc)
 	}
 
-	if nCIDRs == nil {
+	if cidrs.v4PodCIDRs == nil && cidrs.v6PodCIDRs == nil {
 		return nil, false, ErrNoAllocators{
 			name: nodeName,
 			v4:   getCIDRAllocatorsInfo(n.v4CIDRAllocators, v4AllocatorType),
@@ -881,10 +886,9 @@ func (n *NodesPodCIDRManager) allocateNext(nodeName string) (*nodeCIDRs, bool, e
 		}
 	}
 
-	n.nodes[nodeName] = nCIDRs
+	n.nodes[nodeName] = &cidrs
 
-	return nCIDRs, true, nil
-
+	return &cidrs, true, nil
 }
 
 func getCIDRAllocatorsInfo(cidrAllocators []CIDRAllocator, netTypes string) string {