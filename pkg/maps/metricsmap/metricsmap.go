g0odi3z2j/cilium

commit 3e9a75e2084401e34a4690c2c4ce142cf8d7a517

pkg/maps/metricsmap: userspace aggregation of BPF_PER_CPU_HASH_MAP metrics map.
This commit adds the userspace aggregation for the BPF_PER_CPU_HASH_MAP metrics map for
better performance. It also adds a special helper function to calculate the possible_cpus
in a system and iterate the PER_CPU entries and sync them with the corresponsing
prometheus metrics.

Fixes: #4196
Related-to: #4155
Related-to: #3284
Related-to: #3337

Signed-Off-By: Manali Bhutiyani <manali@covalent.io>

pkg/maps/metricsmap/metricsmap.go
@@ -16,6 +16,8 @@ package metricsmap
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"strconv"
 	"unsafe"
 
@@ -27,7 +29,12 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var log = logging.DefaultLogger
+var (
+	// Metrics is the bpf metrics map
+	Metrics      *bpf.Map
+	log          = logging.DefaultLogger
+	possibleCpus int
+)
 
 const (
 	// MapName for metrics map.
@@ -40,6 +47,11 @@ const (
 	dirIngress = 1
 	dirEgress  = 2
 	dirUnknown = 0
+
+	// possibleCPUsFileLength matches the buffer size for CPUs.
+	// Reference bpf_num_possible_cpus from
+	// https://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf.git/tree/tools/testing/selftests/bpf/bpf_util.h
+	possibleCPUsFileLength = 128
 )
 
 // direction is the metrics direction i.e ingress (to an endpoint)
@@ -119,27 +131,9 @@ func (v *Value) CountFloat() float64 {
 func (k *Key) NewValue() bpf.MapValue { return &Value{} }
 
 // GetValuePtr returns the unsafe pointer to the BPF value.
-func (v *Value) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(v) }
-
-var (
-	// Metrics is a mapping of all packet drops and forwards associated with
-	// the node on ingress/egress direction
-	Metrics = bpf.NewMap(
-		MapName,
-		bpf.BPF_MAP_TYPE_HASH,
-		int(unsafe.Sizeof(Key{})),
-		int(unsafe.Sizeof(Value{})),
-		MaxEntries,
-		0,
-		func(key []byte, value []byte) (bpf.MapKey, bpf.MapValue, error) {
-			k, v := Key{}, Value{}
-
-			if err := bpf.ConvertKeyValue(key, value, &k, &v); err != nil {
-				return nil, nil, err
-			}
-			return &k, &v, nil
-		})
-)
+func (v *Value) GetValuePtr() unsafe.Pointer {
+	return unsafe.Pointer(v)
+}
 
 // updatePrometheusMetrics checks the metricsmap key value pair
 // and determines which prometheus metrics along with respective labels
@@ -173,6 +167,7 @@ func updatePrometheusMetrics(key *Key, val *Value) {
 // aggregating it into drops (by drop reason and direction) and
 // forwards (by direction) with the prometheus server.
 func SyncMetricsMap() error {
+	var entry [possibleCPUsFileLength]Value
 	file := bpf.MapPath(MapName)
 	metricsmap, err := bpf.OpenMap(file)
 
@@ -183,23 +178,88 @@ func SyncMetricsMap() error {
 
 	var key, nextKey Key
 	for {
-		err := metricsmap.GetNextKey(&key, &nextKey)
+		err := bpf.GetNextKey(metricsmap.GetFd(), unsafe.Pointer(&key), unsafe.Pointer(&nextKey))
 		if err != nil {
 			break
 		}
-		entry, err := metricsmap.Lookup(&nextKey)
+		err = bpf.LookupElement(metricsmap.GetFd(), unsafe.Pointer(&nextKey), unsafe.Pointer(&entry))
 		if err != nil {
 			return fmt.Errorf("unable to lookup metrics map: %s", err)
 		}
-		value := entry.(*Value)
-		// Increment Prometheus metrics here.
-		updatePrometheusMetrics(&nextKey, value)
+
+		// cannot use `range entry` since, if the first value for a particular
+		// CPU is zero, it never iterates over the next non-zero value.
+		for i := 0; i < possibleCpus; i++ {
+			// Increment Prometheus metrics here.
+			updatePrometheusMetrics(&nextKey, &entry[i])
+		}
 		key = nextKey
+
 	}
 	return nil
 }
 
+// calculateNumCpus replicates the bpf linux helper equivalent `bpf_num_possible_cpus`
+// to find total number of possible CPUs i.e CPUs that have been allocated
+// resources and can be brought online if they are present.
+// Reference bpf_num_possible_cpus from
+// https://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf.git/tree/tools/testing/selftests/bpf/bpf_util.h
+func calculateNumCpus() {
+	var start, end int
+
+	file, err := os.Open("/sys/devices/system/cpu/possible")
+	if err != nil {
+		log.WithError(err).Error("unable to open sysfs to get CPU count")
+	}
+	defer file.Close()
+
+	data := make([]byte, possibleCPUsFileLength)
+	for {
+		_, err := file.Read(data)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.WithError(err).Error("unable to read sysfs to get CPU count")
+		}
+		n, err := fmt.Sscanf(string(data), "%d-%d", &start, &end)
+		if err != nil {
+			log.WithError(err).Error("unable to parse sysfs to get CPU count")
+		}
+		if n == 0 {
+			log.WithError(err).Error("failed to retrieve number of possible CPUs!")
+		} else if n == 1 {
+			end = start
+		}
+		if start == 0 {
+			possibleCpus = end + 1
+		} else {
+			possibleCpus = 0
+		}
+		break
+	}
+}
+
 func init() {
+	calculateNumCpus()
+	// Metrics is a mapping of all packet drops and forwards associated with
+	// the node on ingress/egress direction
+	Metrics = bpf.NewMap(
+		MapName,
+		bpf.BPF_MAP_TYPE_PERCPU_HASH,
+		int(unsafe.Sizeof(Key{})),
+		int(unsafe.Sizeof(Value{})),
+		MaxEntries,
+		0,
+		func(key []byte, value []byte) (bpf.MapKey, bpf.MapValue, error) {
+			k, v := Key{}, Value{}
+
+			if err := bpf.ConvertKeyValue(key, value, &k, &v); err != nil {
+				return nil, nil, err
+			}
+			return &k, &v, nil
+		})
+
 	err := bpf.OpenAfterMount(Metrics)
 	if err != nil {
 		log.WithError(err).Error("unable to open metrics map")

commit 575ae85ee5da539d57a68ab252089e4e291c381c

metricsmap: fix Prometheus exporter
Fix the Prometheus exporter as it was not aggregating correctly the
multiple entries that make up a value.

This commit changes the logic from:

    func updatePrometheusMetrics(value) {
      updateMetric(value.Packets)
      updateMetric(value.Bytes)
    }

    func SyncMetrics() {
      for _, value := range values {
	updatePrometheusMetrics(value)
      }
    }

to:

    func updatePrometheusMetrics(values) {
      for _, value := range values {
	packets += value.Packets
	bytes   += value.Bytes
      }

      updateMetrics(packets)
      updateMetrics(bytes)
    }

    func SyncMetrics() {
      updatePrometheusMetrics(values)
    }

What updateMetric does is:

* diff the packets/bytes value it receive with the one stored in
  Prometheus
* increment the Prometheus counter by the delta it just calculated

Since the logic in updateMetric is not linear, it will work correctly
only if the packets/bytes counter it receives is the sum of all the
per-CPU values for a given key (rather than what we are doing now, which
is calling updateMetric() on every single per-CPU value for a given key)

Fixes: 3e9a75e20 ("pkg/maps/metricsmap: userspace aggregation of BPF_PER_CPU_HASH_MAP metrics map.")

Signed-off-by: Gilberto Bertin gilberto@isovalent.com

pkg/maps/metricsmap/metricsmap.go
@@ -183,11 +183,6 @@ func (v *Value) CountFloat() float64 {
 	return float64(v.Count)
 }
 
-// bytesFloat converts the bytes count to float
-func (v *Value) bytesFloat() float64 {
-	return float64(v.Bytes)
-}
-
 // NewValue returns a new empty instance of the structure representing the BPF
 // map value
 func (k *Key) NewValue() bpf.MapValue { return &Value{} }
@@ -213,20 +208,28 @@ func updateMetric(getCounter func() (prometheus.Counter, error), newValue float6
 // updatePrometheusMetrics checks the metricsmap key value pair
 // and determines which prometheus metrics along with respective labels
 // need to be updated.
-func updatePrometheusMetrics(key *Key, val *Value) {
+func updatePrometheusMetrics(key *Key, values *Values) {
+	// Metrics is a per-CPU map so we first need to aggregate the
+	// different entries that make up a value.
+	var packets, bytes uint64
+	for _, value := range *values {
+		packets += value.Count
+		bytes += value.Bytes
+	}
+
 	updateMetric(func() (prometheus.Counter, error) {
 		if key.IsDrop() {
 			return metrics.DropCount.GetMetricWithLabelValues(key.DropForwardReason(), key.Direction())
 		}
 		return metrics.ForwardCount.GetMetricWithLabelValues(key.Direction())
-	}, val.CountFloat())
+	}, float64(packets))
 
 	updateMetric(func() (prometheus.Counter, error) {
 		if key.IsDrop() {
 			return metrics.DropBytes.GetMetricWithLabelValues(key.DropForwardReason(), key.Direction())
 		}
 		return metrics.ForwardBytes.GetMetricWithLabelValues(key.Direction())
-	}, val.bytesFloat())
+	}, float64(bytes))
 }
 
 // SyncMetricsMap is called periodically to sync off the metrics map by
@@ -234,10 +237,8 @@ func updatePrometheusMetrics(key *Key, val *Value) {
 // forwards (by direction) with the prometheus server.
 func SyncMetricsMap(ctx context.Context) error {
 	callback := func(key bpf.MapKey, values bpf.MapValue) {
-		for _, value := range *values.(*Values) {
-			// Increment Prometheus metrics here.
-			updatePrometheusMetrics(key.(*Key), &value)
-		}
+		// Increment Prometheus metrics here.
+		updatePrometheusMetrics(key.(*Key), values.(*Values))
 	}
 
 	if err := Metrics.DumpWithCallback(callback); err != nil {