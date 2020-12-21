g0odi3z2j/cilium

commit 93443da0eaef127d5cd19d0c7419d815e34c30e3

bpf: datapath: Rewite base devices setup in Go
This change removes the part of init.sh responsible for base devices
setup and remplements it in Go.

Ref: #920

Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

pkg/datapath/loader/base.go
@@ -51,6 +51,8 @@ const (
 	initArgIPv6NodeIP
 	initArgMode
 	initArgDevices
+	initArgHostDev1
+	initArgHostDev2
 	initArgXDPDevice
 	initArgXDPMode
 	initArgMTU
@@ -272,6 +274,7 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		args[initArgEncryptInterface] = "<nil>"
 	}
 
+	devices := make([]netlink.Link, 0, len(option.Config.Devices))
 	if len(option.Config.Devices) != 0 {
 		for _, device := range option.Config.Devices {
 			_, err := netlink.LinkByName(device)
@@ -287,16 +290,18 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		args[initArgDevices] = "<nil>"
 	}
 
+	var mode baseDeviceMode
 	switch {
 	case option.Config.IsFlannelMasterDeviceSet():
-		args[initArgMode] = "flannel"
+		mode = flannelMode
 	case option.Config.Tunnel != option.TunnelDisabled:
-		args[initArgMode] = option.Config.Tunnel
+		mode = baseDeviceMode(option.Config.Tunnel)
 	case option.Config.DatapathMode == datapathOption.DatapathModeIpvlan:
-		args[initArgMode] = "ipvlan"
+		mode = ipvlanMode
 	default:
-		args[initArgMode] = "direct"
+		mode = directMode
 	}
+	args[initArgMode] = string(mode)
 
 	if option.Config.EnableNodePort {
 		args[initArgNodePort] = "true"
@@ -358,6 +363,16 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 
 	sysctl.ApplySettings(sysSettings)
 
+	// Datapath initialization
+	hostDev1, hostDev2, err := setupBaseDevice(devices, mode, deviceMTU)
+	if err != nil {
+		return err
+	}
+	args[initArgHostDev1] = hostDev1.Attrs().Name
+	args[initArgHostDev2] = hostDev2.Attrs().Name
+
+	// "Legacy" datapath inizialization with the init.sh script
+	// TODO(mrostecki): Rewrite the whole init.sh in Go, step by step.
 	for i, arg := range args {
 		if arg == "" {
 			log.Warningf("empty argument passed to bpf/init.sh at position %d", i)

bpf/init.sh
@@ -21,23 +21,25 @@ IP6_HOST=$4
 MODE=$5
 # Only set if MODE = "direct", "ipvlan", "flannel"
 NATIVE_DEVS=$6
-XDP_DEV=$7
-XDP_MODE=$8
-MTU=$9
-IPSEC=${10}
-ENCRYPT_DEV=${11}
-HOSTLB=${12}
-HOSTLB_UDP=${13}
-HOSTLB_PEER=${14}
-CGROUP_ROOT=${15}
-BPFFS_ROOT=${16}
-NODE_PORT=${17}
-NODE_PORT_BIND=${18}
-MCPU=${19}
-NODE_PORT_IPV4_ADDRS=${20}
-NODE_PORT_IPV6_ADDRS=${21}
-NR_CPUS=${22}
-ENDPOINT_ROUTES=${23}
+HOST_DEV1=$7
+HOST_DEV2=$8
+XDP_DEV=$9
+XDP_MODE=${10}
+MTU=${11}
+IPSEC=${12}
+ENCRYPT_DEV=${13}
+HOSTLB=${14}
+HOSTLB_UDP=${15}
+HOSTLB_PEER=${16}
+CGROUP_ROOT=${17}
+BPFFS_ROOT=${18}
+NODE_PORT=${19}
+NODE_PORT_BIND=${20}
+MCPU=${21}
+NODE_PORT_IPV4_ADDRS=${22}
+NODE_PORT_IPV6_ADDRS=${23}
+NR_CPUS=${24}
+ENDPOINT_ROUTES=${25}
 
 ID_HOST=1
 ID_WORLD=2
@@ -78,37 +80,6 @@ function setup_dev()
 		echo 1 > /proc/sys/net/ipv4/conf/${NAME}/accept_local
 		echo 0 > /proc/sys/net/ipv4/conf/${NAME}/send_redirects
 	fi
-}
-
-function setup_veth_pair()
-{
-	local -r NAME1=$1
-	local -r NAME2=$2
-
-	# Only recreate the veth pair if it does not exist already.
-	# This avoids problems with changing MAC addresses.
- 	if [ "$(ip link show $NAME1 type veth | cut -d ' ' -f 2)" != "${NAME1}@${NAME2}:" ] ; then
-		ip link del $NAME1 2> /dev/null || true
-		ip link add name $NAME1 address $(rnd_mac_addr) type veth \
-            peer name $NAME2 address $(rnd_mac_addr)
-	fi
-
-	setup_dev $NAME1
-	setup_dev $NAME2
-}
-
-function setup_ipvlan_slave()
-{
-	local -r NATIVE_DEV=$1
-	local -r HOST_DEV=$2
-
-	# No issues with changing MAC addresses since all ipvlan
-	# slaves always inherits MAC from native device.
-	ip link del $HOST_DEV 2> /dev/null || true
-
-	ip link add link $NATIVE_DEV name $HOST_DEV type ipvlan mode l3
-
-	setup_dev $HOST_DEV
 }
 
 function move_local_rules_af()
@@ -395,36 +366,6 @@ function encap_fail()
 	(>&2 ip link show type $MODE)
 	exit 1
 }
-
-# Base device setup
-case "${MODE}" in
-	"flannel")
-		HOST_DEV1="${NATIVE_DEVS}"
-		HOST_DEV2="${NATIVE_DEVS}"
-
-		setup_dev "${NATIVE_DEVS}"
-		;;
-	"ipvlan")
-		HOST_DEV1="cilium_host"
-		HOST_DEV2="${HOST_DEV1}"
-
-		setup_ipvlan_slave $NATIVE_DEVS $HOST_DEV1
-
-		ip link set $HOST_DEV1 mtu $MTU
-		;;
-	*)
-		HOST_DEV1="cilium_host"
-		HOST_DEV2="cilium_net"
-
-		setup_veth_pair $HOST_DEV1 $HOST_DEV2
-
-		ip link set $HOST_DEV1 arp off
-		ip link set $HOST_DEV2 arp off
-
-		ip link set $HOST_DEV1 mtu $MTU
-		ip link set $HOST_DEV2 mtu $MTU
-        ;;
-esac
 
 # node_config.h header generation
 case "${MODE}" in

pkg/datapath/loader/netlink.go
@@ -17,14 +17,25 @@ package loader
 import (
 	"context"
 	"fmt"
+	"net"
 
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/command/exec"
+	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/mac"
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/sysctl"
 
 	"github.com/vishvananda/netlink"
 )
 
+type baseDeviceMode string
+
 const (
+	flannelMode = baseDeviceMode("flannel")
+	ipvlanMode  = baseDeviceMode("ipvlan")
+	directMode  = baseDeviceMode("direct")
+
 	libbpfFixupMsg = "struct bpf_elf_map fixup performed due to size mismatch!"
 )
 
@@ -150,4 +161,165 @@ func RemoveTCFilters(ifName string, tcDir uint32) error {
 	}
 
 	return nil
+}
+
+func setupDev(link netlink.Link) error {
+	ifName := link.Attrs().Name
+
+	if err := netlink.LinkSetUp(link); err != nil {
+		log.WithError(err).WithField("device", ifName).Warn("Could not set up the link")
+		return err
+	}
+
+	sysSettings := make([]sysctl.Setting, 0, 5)
+	if option.Config.EnableIPv6 {
+		sysSettings = append(sysSettings, sysctl.Setting{
+			Name: fmt.Sprintf("net.ipv6.conf.%s.forwarding", ifName), Val: "1", IgnoreErr: false})
+	}
+	if option.Config.EnableIPv4 {
+		sysSettings = append(sysSettings, []sysctl.Setting{
+			{Name: fmt.Sprintf("net.ipv4.conf.%s.forwarding", ifName), Val: "1", IgnoreErr: false},
+			{Name: fmt.Sprintf("net.ipv4.conf.%s.rp_filter", ifName), Val: "0", IgnoreErr: false},
+			{Name: fmt.Sprintf("net.ipv4.conf.%s.accept_local", ifName), Val: "1", IgnoreErr: false},
+			{Name: fmt.Sprintf("net.ipv4.conf.%s.send_redirects", ifName), Val: "0", IgnoreErr: false},
+		}...)
+	}
+	if err := sysctl.ApplySettings(sysSettings); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setupDevs(links ...netlink.Link) error {
+	for _, link := range links {
+		if err := setupDev(link); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func setupVethPair(name, peerName string) error {
+	// Create the veth pair if it doesn't exist.
+	if _, err := netlink.LinkByName(name); err != nil {
+		hostMac, err := mac.GenerateRandMAC()
+		if err != nil {
+			return err
+		}
+		peerMac, err := mac.GenerateRandMAC()
+		if err != nil {
+			return err
+		}
+
+		veth := &netlink.Veth{
+			LinkAttrs: netlink.LinkAttrs{
+				Name:         name,
+				HardwareAddr: net.HardwareAddr(hostMac),
+			},
+			PeerName:         peerName,
+			PeerHardwareAddr: net.HardwareAddr(peerMac),
+		}
+		if err := netlink.LinkAdd(veth); err != nil {
+			return err
+		}
+	}
+
+	veth, err := netlink.LinkByName(name)
+	if err != nil {
+		return err
+	}
+	if err := setupDev(veth); err != nil {
+		return err
+	}
+	peer, err := netlink.LinkByName(peerName)
+	if err != nil {
+		return err
+	}
+	if err := setupDev(peer); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setupIpvlan(name string, nativeLink netlink.Link) (*netlink.IPVlan, error) {
+	hostLink, err := netlink.LinkByName(name)
+	if err == nil {
+		// Ignore the error.
+		netlink.LinkDel(hostLink)
+	}
+
+	ipvlan := &netlink.IPVlan{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:        name,
+			ParentIndex: nativeLink.Attrs().Index,
+		},
+		Mode: netlink.IPVLAN_MODE_L3,
+	}
+	if err := netlink.LinkAdd(ipvlan); err != nil {
+		return nil, err
+	}
+
+	if err := setupDev(ipvlan); err != nil {
+		return nil, err
+	}
+
+	return ipvlan, nil
+}
+
+// setupBaseDevice decides which and what kind of interfaces should be set up as
+// the first step of datapath initialization, then performs the setup (and
+// creation, if needed) of those interfaces. It returns two links and an error.
+// By default, it sets up the veth pair - cilium_host and cilium_net.
+// In flannel mode, it sets up the native interface used by flannel.
+// In ipvlan mode, it creates the cilium_host ipvlan with the native device as a
+// parent.
+func setupBaseDevice(nativeDevs []netlink.Link, mode baseDeviceMode, mtu int) (netlink.Link, netlink.Link, error) {
+	switch mode {
+	case flannelMode:
+		if err := setupDevs(nativeDevs...); err != nil {
+			return nil, nil, err
+		}
+		return nativeDevs[0], nativeDevs[0], nil
+	case ipvlanMode:
+		ipvlan, err := setupIpvlan(defaults.HostDevice, nativeDevs[0])
+		if err != nil {
+			return nil, nil, err
+		}
+		if err := netlink.LinkSetMTU(ipvlan, mtu); err != nil {
+			return nil, nil, err
+		}
+
+		return ipvlan, ipvlan, nil
+	default:
+		if err := setupVethPair(defaults.HostDevice, defaults.SecondHostDevice); err != nil {
+			return nil, nil, err
+		}
+
+		linkHost, err := netlink.LinkByName(defaults.HostDevice)
+		if err != nil {
+			return nil, nil, err
+		}
+		linkNet, err := netlink.LinkByName(defaults.SecondHostDevice)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if err := netlink.LinkSetARPOff(linkHost); err != nil {
+			return nil, nil, err
+		}
+		if err := netlink.LinkSetARPOff(linkNet); err != nil {
+			return nil, nil, err
+		}
+
+		if err := netlink.LinkSetMTU(linkHost, mtu); err != nil {
+			return nil, nil, err
+		}
+		if err := netlink.LinkSetMTU(linkNet, mtu); err != nil {
+			return nil, nil, err
+		}
+
+		return linkHost, linkNet, nil
+	}
 }

pkg/datapath/loader/netlink_test.go
@@ -1,0 +1,85 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build linux,privileged_tests
+
+package loader
+
+import (
+	"fmt"
+
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/sysctl"
+
+	"github.com/vishvananda/netlink"
+	. "gopkg.in/check.v1"
+)
+
+type NetlinkTestSuite struct {
+	prevConfigEnableIPv4 bool
+	prevConfigEnableIPv6 bool
+}
+
+var _ = Suite(&NetlinkTestSuite{})
+
+func (s *NetlinkTestSuite) SetUpSuite(c *C) {
+	s.prevConfigEnableIPv4 = option.Config.EnableIPv4
+	s.prevConfigEnableIPv6 = option.Config.EnableIPv6
+
+	option.Config.EnableIPv4 = true
+	option.Config.EnableIPv6 = true
+}
+
+func (s *NetlinkTestSuite) TearDownSuite(c *C) {
+	option.Config.EnableIPv4 = s.prevConfigEnableIPv4
+	option.Config.EnableIPv6 = s.prevConfigEnableIPv6
+}
+
+func (s *NetlinkTestSuite) TestSetupDev(c *C) {
+	ifName := "dummy9"
+
+	dummy := &netlink.Dummy{
+		LinkAttrs: netlink.LinkAttrs{
+			Name: ifName,
+		},
+	}
+	err := netlink.LinkAdd(dummy)
+	c.Assert(err, IsNil)
+
+	err = setupDev(dummy)
+	c.Assert(err, IsNil)
+
+	enabledSettings := []string{
+		fmt.Sprintf("net.ipv6.conf.%s.forwarding", ifName),
+		fmt.Sprintf("net.ipv4.conf.%s.forwarding", ifName),
+		fmt.Sprintf("net.ipv4.conf.%s.accept_local", ifName),
+	}
+	disabledSettings := []string{
+		fmt.Sprintf("net.ipv4.conf.%s.rp_filter", ifName),
+		fmt.Sprintf("net.ipv4.conf.%s.send_redirects", ifName),
+	}
+	for _, setting := range enabledSettings {
+		s, err := sysctl.Read(setting)
+		c.Assert(err, IsNil)
+		c.Assert(s, Equals, "1")
+	}
+	for _, setting := range disabledSettings {
+		s, err := sysctl.Read(setting)
+		c.Assert(err, IsNil)
+		c.Assert(s, Equals, "0")
+	}
+
+	err = netlink.LinkDel(dummy)
+	c.Assert(err, IsNil)
+}

commit 7c64d99594270e5b9fee973069f40071439dbf9a

bpf: datapath: Fix fetching configured base devices
Before this change, the base loader logic was not creating the slice of
base devices properly.

Fixes: 93443da0eaef ("bpf: datapath: Rewite base devices setup in Go")
Reported-by: Martynas Pumputis <m@lambda.lt>
Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

pkg/datapath/loader/base.go
@@ -273,11 +273,12 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 	devices := make([]netlink.Link, 0, len(option.Config.Devices))
 	if len(option.Config.Devices) != 0 {
 		for _, device := range option.Config.Devices {
-			_, err := netlink.LinkByName(device)
+			link, err := netlink.LinkByName(device)
 			if err != nil {
 				log.WithError(err).WithField("device", device).Warn("Link does not exist")
 				return err
 			}
+			devices = append(devices, link)
 		}
 		args[initArgDevices] = strings.Join(option.Config.Devices, ";")
 	} else if option.Config.IsFlannelMasterDeviceSet() {