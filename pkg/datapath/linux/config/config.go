g0odi3z2j/cilium

commit e71b718e445d1e0f5d35c2324b1823f660454383

datapath: Enable BPF MASQ for veth mode in IPv4
This commit enables MASQUERADE'ing packets sent from local endpoints to
outside (WORLD_ID).

The MASQ is enabled only when --enable-bpf-masquerade is set.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/datapath/linux/config/config.go
@@ -290,6 +290,10 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 			cDefinesMap["SNAT_MAPPING_IPV6_SIZE"] = fmt.Sprintf("%d", option.Config.NATMapEntriesGlobal)
 		}
 
+		if option.Config.EnableBPFMasquerade {
+			cDefinesMap["ENABLE_MASQUERADE"] = "1"
+		}
+
 		ctmap.WriteBPFMacros(fw, nil)
 	}
 

bpf/lib/nat.h
@@ -422,14 +422,17 @@ static __always_inline int snat_v4_rewrite_ingress(struct __ctx_buff *ctx,
 }
 
 static __always_inline bool snat_v4_can_skip(const struct ipv4_nat_target *target,
-					     const struct ipv4_ct_tuple *tuple, int dir)
+					     const struct ipv4_ct_tuple *tuple, int dir,
+					     bool from_endpoint)
 {
 	__u16 dport = bpf_ntohs(tuple->dport), sport = bpf_ntohs(tuple->sport);
 
-	if (dir == NAT_DIR_EGRESS && !target->src_from_world && sport < NAT_MIN_EGRESS)
+	if (dir == NAT_DIR_EGRESS && !from_endpoint && !target->src_from_world
+	    && sport < NAT_MIN_EGRESS)
 		return true;
 	if (dir == NAT_DIR_INGRESS && (dport < target->min_port || dport > target->max_port))
 		return true;
+
 	return false;
 }
 
@@ -483,7 +486,8 @@ static __always_inline __maybe_unused int snat_v4_create_dsr(struct __ctx_buff *
 }
 
 static __always_inline __maybe_unused int snat_v4_process(struct __ctx_buff *ctx, int dir,
-					   const struct ipv4_nat_target *target)
+						const struct ipv4_nat_target *target,
+						bool from_endpoint)
 {
 	struct icmphdr icmphdr __align_stack_8;
 	struct ipv4_nat_entry *state, tmp;
@@ -533,7 +537,7 @@ static __always_inline __maybe_unused int snat_v4_process(struct __ctx_buff *ctx
 		return NAT_PUNT_TO_STACK;
 	};
 
-	if (snat_v4_can_skip(target, &tuple, dir))
+	if (snat_v4_can_skip(target, &tuple, dir, from_endpoint))
 		return NAT_PUNT_TO_STACK;
 	ret = snat_v4_handle_mapping(ctx, &tuple, &state, &tmp, dir, off, target);
 	if (ret > 0)
@@ -549,7 +553,8 @@ static __always_inline __maybe_unused int snat_v4_process(struct __ctx_buff *ctx
 static __always_inline __maybe_unused
 int snat_v4_process(struct __ctx_buff *ctx __maybe_unused,
 		    int dir __maybe_unused,
-		    const struct ipv4_nat_target *target __maybe_unused)
+		    const struct ipv4_nat_target *target __maybe_unused,
+		    bool from_endpoint __maybe_unused)
 {
 	return CTX_ACT_OK;
 }

bpf/lib/nodeport.h
@@ -698,7 +698,9 @@ static __always_inline bool nodeport_uses_dsr4(const struct ipv4_ct_tuple *tuple
 }
 
 static __always_inline bool nodeport_nat_ipv4_needed(struct __ctx_buff *ctx,
-						     __be32 addr, int dir)
+						     __be32 addr, int dir,
+						     bool *from_endpoint __maybe_unused,
+						     const bool encap __maybe_unused)
 {
 	void *data, *data_end;
 	struct iphdr *ip4;
@@ -713,24 +715,69 @@ static __always_inline bool nodeport_nat_ipv4_needed(struct __ctx_buff *ctx,
 	 * overlapping tuples, e.g. applications in hostns reusing
 	 * source IPs we SNAT in node-port.
 	 */
-	if (dir == NAT_DIR_EGRESS)
-		return ip4->saddr == addr;
-	else
+	if (dir == NAT_DIR_EGRESS) {
+		if (ip4->saddr == addr) {
+			return true;
+		}
+#ifdef ENABLE_MASQUERADE
+		struct endpoint_info *ep;
+
+		/* Do not MASQ when this function is executed from bpf_overlay
+		 * (encap=true denotes this fact). Otherwise, a packet will be
+		 * SNAT'd to cilium_host IP addr. */
+		if (encap)
+			return false;
+
+		if ((ep = __lookup_ip4_endpoint(ip4->saddr)) != NULL &&
+		    !(ep->flags & ENDPOINT_F_HOST)) {
+			struct remote_endpoint_info *info;
+			*from_endpoint = true;
+			info = ipcache_lookup4(&IPCACHE_MAP, ip4->daddr,
+					       V4_CACHE_KEY_LEN);
+			if (info != NULL) {
+				if (info->sec_label == WORLD_ID)
+					return true;
+#ifdef ENCAP_IFINDEX
+				/* In the tunnel mode, a packet from a local ep
+				 * to a remote node is not encap'd, and is sent
+				 * via a native dev. Therefore, such packet has
+				 * to be MASQ'd. Otherwise, it might be dropped
+				 * either by underlying network (e.g. AWS drops
+				 * packets by default from unknown subnets) or
+				 * by the remote node if its native dev's
+				 * rp_filter=1. */
+				if (info->sec_label == REMOTE_NODE_ID)
+					return true;
+#endif
+			}
+		}
+#endif /*ENABLE_MASQUERADE */
+
+		return false;
+	} else {
 		return ip4->daddr == addr;
+	}
 }
 
 static __always_inline int nodeport_nat_ipv4_fwd(struct __ctx_buff *ctx,
-						 const __be32 addr)
+						 const __be32 addr,
+						 const bool encap)
 {
+	bool from_endpoint = false;
 	struct ipv4_nat_target target = {
 		.min_port = NODEPORT_PORT_MIN_NAT,
 		.max_port = 65535,
 		.addr = addr,
 	};
-	int ret = nodeport_nat_ipv4_needed(ctx, addr, NAT_DIR_EGRESS) ?
-		snat_v4_process(ctx, NAT_DIR_EGRESS, &target) : CTX_ACT_OK;
+	int ret = CTX_ACT_OK;
+
+	if (nodeport_nat_ipv4_needed(ctx, addr, NAT_DIR_EGRESS,
+				     &from_endpoint, encap))
+		ret = snat_v4_process(ctx, NAT_DIR_EGRESS, &target,
+				      from_endpoint);
 	if (ret == NAT_PUNT_TO_STACK)
 		ret = CTX_ACT_OK;
+
 	return ret;
 }
 
@@ -926,7 +973,7 @@ int tail_nodeport_nat_ipv4(struct __ctx_buff *ctx)
 		}
 	}
 #endif
-	ret = snat_v4_process(ctx, dir, &target);
+	ret = snat_v4_process(ctx, dir, &target, false);
 	if (IS_ERR(ret)) {
 		/* In case of no mapping, recircle back to main path. SNAT is very
 		 * expensive in terms of instructions (since we don't have BPF to
@@ -1266,7 +1313,7 @@ static __always_inline int nodeport_nat_fwd(struct __ctx_buff *ctx,
 		else
 #endif
 			addr = IPV4_NODEPORT;
-		return nodeport_nat_ipv4_fwd(ctx, addr);
+		return nodeport_nat_ipv4_fwd(ctx, addr, encap);
 	}
 #endif /* ENABLE_IPV4 */
 #ifdef ENABLE_IPV6

commit a05d0e359d7f9a849354f1e9b3d75d3b543ad350

bpf: fix misconfigured nat to 0.0.0.0 on !masquerade config
Thomas reported:

  When I do Pod to Pod I see this on the interface of the VM:

  21:30:11.022617 IP 0.0.0.0.53336 > 10.240.0.22.8080: Flags [S], seq 2523807568, win 64240,
    options [mss 1460,sackOK,TS val 207396274 ecr 0,nop,wscale 7], length 0

  When I do Host to Pod I see this on the interface of the same VM:

  21:30:02.275063 IP 10.240.0.35.46236 > 10.240.0.22.8080: Flags [S], seq 1718112282, win 64240,
    options [mss 1460,sackOK,TS val 2667439682 ecr 0,nop,wscale 7], length 0

The NAT table indeed had a bunch of entries that would cause the 0.0.0.0 translation:

  [...]
  TCP OUT 10.240.0.16:60250 -> 10.0.222.20:443 XLATE_SRC 0.0.0.0:60250 Created=17376sec HostLocal=0
  TCP OUT 10.240.0.16:46342 -> 10.240.0.49:9153 XLATE_SRC 0.0.0.0:46342 Created=17376sec HostLocal=0
  [...]

From the config, it turns out that the following was set:

  [...]
  enable-bpf-masquerade: "true"
  masquerade: "false"
  [...]

And looking at the headers, ENABLE_MASQUERADE was enabled. This in fact meant that
in snat_v4_needed() we'd look up the endpoint based on the src IPv4 and given neither
ENABLE_IP_MASQ_AGENT nor ENCAP_IFINDEX was set, IPV4_MASQUERADE is set for the NAT
address. However, the latter will be 0.0.0.0 in this case since IPV4_MASQUERADE is
not defined from template side given it depends on ...

  if option.Config.EnableIPv4Masquerade && option.Config.EnableBPFMasquerade && bpfMasqIPv4Addrs != nil {
      if option.Config.EnableIPv4 {
         ipv4 := bpfMasqIPv4Addrs[ifName]
         opts["IPV4_MASQUERADE"] = byteorder.HostSliceToNetwork(ipv4, reflect.Uint32).(uint32)
      }
  }

... of which option.Config.EnableIPv4Masquerade is false. Yet ENABLE_MASQUERADE otoh
is defined in a more relaxed manner, that is option.Config.EnableBPFMasquerade &&
option.Config.EnableIPv4, both of which were true in this case. Fix it by requiring
option.Config.EnableIPv4Masquerade instead of option.Config.EnableIPv4 for the latter.

The commit got uncovered by b533e19d6841 ("datapath: Decouple IPV4_MASQUERADE from
IPV4_NODEPORT"), but the original commit introducing the issue is e71b718e445d
("datapath: Enable BPF MASQ for veth mode in IPv4").

Fixes: e71b718e445d ("datapath: Enable BPF MASQ for veth mode in IPv4")

pkg/datapath/linux/config/config.go
@@ -422,7 +422,7 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 			cDefinesMap["SNAT_MAPPING_IPV6_SIZE"] = fmt.Sprintf("%d", option.Config.NATMapEntriesGlobal)
 		}
 
-		if option.Config.EnableBPFMasquerade && option.Config.EnableIPv4 {
+		if option.Config.EnableIPv4Masquerade && option.Config.EnableBPFMasquerade {
 			cDefinesMap["ENABLE_MASQUERADE"] = "1"
 			cidr := datapath.RemoteSNATDstAddrExclusionCIDRv4()
 			cDefinesMap["IPV4_SNAT_EXCLUSION_DST_CIDR"] =