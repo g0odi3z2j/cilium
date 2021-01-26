g0odi3z2j/cilium

commit 9046485fcf10250cc7caf4f953d6b1264570cc8b

bpf: optimize datapath through host netns
Optimize the direct routing host data path via redirect_neigh() helper based
on our recent kernel work [0]. As outlined at Plumbers [1] (the first out
of the two new helpers), the current routing scheme is suboptimal in that we
push skbs up the host stack.

This has a huge cost in itself and additionally it also orphans the skb
from the socket so that TCP stack does not get right backpressure signal
for TSO. With the redirect_neigh() helper we can perform the forwarding in
tc layer _only_ while we let kernel handle neighboring subsystem in reply
path. This also means we bypass things like netfilter in host netns thus
if masquerading is enabled it must be done via BPF.

This preserves also skb->sk all the way till qdisc layer which also helps
BPF bandwidth manager for FQ.

Used agent config on apoc/tank with bpf-next tree each:

  ./daemon/cilium-agent --identity-allocation-mode=crd --enable-ipv6=true --enable-ipv4=true \
    --disable-envoy-version-check=true --tunnel=disabled --k8s-kubeconfig-path=$HOME/.kube/config \
    --kube-proxy-replacement=strict --enable-l7-proxy=false --auto-direct-node-routes=true \
    --native-routing-cidr=10.217.0.0/16 --enable-bandwidth-manager=true --enable-bpf-masquerade=true

Benchmark via direct routing, pod to pod, over wire gives a nice stable
improvement that way:

 * Before:

  root@apoc:~# kubectl exec -it netperf-5fdd54b4cc-bcqzg -- netperf -H 10.217.1.93 -t TCP_STREAM
  MIGRATED TCP STREAM TEST from 0.0.0.0 (0.0.0.0) port 0 AF_INET to 10.217.1.93 (10.217.) port 0 AF_INET
  Recv   Send    Send
  Socket Socket  Message  Elapsed
  Size   Size    Size     Time     Throughput
  bytes  bytes   bytes    secs.    10^6bits/sec

  131072  16384  16384    10.00    9460.80

 * After:

  root@apoc:~# kubectl exec -it netperf-5fdd54b4cc-bcqzg -- netperf -H 10.217.1.93 -t TCP_STREAM
  MIGRATED TCP STREAM TEST from 0.0.0.0 (0.0.0.0) port 0 AF_INET to 10.217.1.93 (10.217.) port 0 AF_INET

daemon/cmd/kube_proxy_replacement.go
@@ -238,6 +238,30 @@ func initKubeProxyReplacementOptions() (strict bool) {
 		}
 	}
 
+	if !option.Config.EnableHostLegacyRouting {
+		switch {
+		case !option.Config.EnableNodePort:
+			fallthrough
+		case option.Config.Tunnel != option.TunnelDisabled:
+			fallthrough
+		// TODO: add multi-dev support
+		case len(option.Config.Devices) > 1:
+			fallthrough
+		// Non-BPF masquerade requires netfilter and hence CT.
+		case option.Config.Masquerade && !option.Config.EnableBPFMasquerade:
+			option.Config.EnableHostLegacyRouting = true
+		default:
+			found := false
+			if h := probesManager.GetHelpers("sched_cls"); h != nil {
+				_, found = h["bpf_redirect_neigh"]
+			}
+			if !found {
+				log.Warnf("Re-enabling host legacy routing as bypassing needs kernel 5.10.0 or newer.")
+				option.Config.EnableHostLegacyRouting = true
+			}
+		}
+	}
+
 	if option.Config.EnableNodePort {
 		if option.Config.Tunnel != option.TunnelDisabled &&
 			option.Config.NodePortMode != option.NodePortModeSNAT {

bpf/bpf_host.c
@@ -193,10 +193,10 @@ handle_ipv6(struct __ctx_buff *ctx, __u32 secctx, const bool from_host)
 			if (ret < 0)
 				return ret;
 		}
-#if defined(ENCAP_IFINDEX) || defined(NO_REDIRECT)
-		/* See IPv4 case for NO_REDIRECT comments */
+#if defined(ENCAP_IFINDEX) || (defined(NO_REDIRECT) && !defined(ENABLE_REDIRECT_NEIGH))
+		/* See IPv4 case for NO_REDIRECT/ENABLE_REDIRECT_NEIGH comments */
 		skip_redirect = true;
-#endif /* ENCAP_IFINDEX || NO_REDIRECT */
+#endif /* ENCAP_IFINDEX || (NO_REDIRECT && !ENABLE_REDIRECT_NEIGH) */
 		/* Verifier workaround: modified ctx access. */
 		if (!revalidate_data(ctx, &data, &data_end, &ip6))
 			return DROP_INVALID;
@@ -443,15 +443,18 @@ handle_ipv4(struct __ctx_buff *ctx, __u32 secctx,
 			if (ret < 0)
 				return ret;
 		}
-#if defined(ENCAP_IFINDEX) || defined(NO_REDIRECT)
-		/* We cannot redirect a packet to a local endpoint in the direct
-		 * routing mode, as the redirect bypasses nf_conntrack table.
-		 * This makes a second reply from the endpoint to be MASQUERADEd
-		 * or to be DROP-ed by k8s's "--ctstate INVALID -j DROP"
-		 * depending via which interface it was inputed.
+#if defined(ENCAP_IFINDEX) || (defined(NO_REDIRECT) && !defined(ENABLE_REDIRECT_NEIGH))
+		/* Without bpf_redirect_neigh() helper, we cannot redirect a
+		 * packet to a local endpoint in the direct routing mode, as
+		 * the redirect bypasses nf_conntrack table. This makes a
+		 * second reply from the endpoint to be MASQUERADEd or to be
+		 * DROP-ed by k8s's "--ctstate INVALID -j DROP" depending via
+		 * which interface it was inputed. With bpf_redirect_neigh()
+		 * we bypass request and reply path in the host namespace and
+		 * do not run into this issue.
 		 */
 		skip_redirect = true;
-#endif /* ENCAP_IFINDEX || NO_REDIRECT */
+#endif /* ENCAP_IFINDEX || (NO_REDIRECT && !ENABLE_REDIRECT_NEIGH) */
 		/* Verifier workaround: modified ctx access. */
 		if (!revalidate_data(ctx, &data, &data_end, &ip4))
 			return DROP_INVALID;

bpf/bpf_lxc.c
@@ -315,13 +315,18 @@ ct_recreate6:
 			return ret;
 	}
 #endif
-
 #ifdef ENABLE_NAT46
 	if (unlikely(ipv6_addr_is_mapped(daddr))) {
 		ep_tail_call(ctx, CILIUM_CALL_NAT64);
 		return DROP_MISSED_TAIL_CALL;
 	}
 #endif
+	if (is_defined(ENABLE_REDIRECT_NEIGH)) {
+		ret = ipv6_l3(ctx, l3_off, NULL, NULL, METRIC_EGRESS);
+		if (unlikely(ret != CTX_ACT_OK))
+			return ret;
+		return redirect_neigh(DIRECT_ROUTING_DEV_IFINDEX, 0);
+	}
 	goto pass_to_stack;
 
 #ifdef ENABLE_ROUTING
@@ -685,9 +690,14 @@ ct_recreate4:
 		else
 			return ret;
 	}
-#else
-	goto pass_to_stack;
 #endif
+	if (is_defined(ENABLE_REDIRECT_NEIGH)) {
+		ret = ipv4_l3(ctx, l3_off, NULL, NULL, ip4);
+		if (unlikely(ret != CTX_ACT_OK))
+			return ret;
+		return redirect_neigh(DIRECT_ROUTING_DEV_IFINDEX, 0);
+	}
+	goto pass_to_stack;
 
 #ifdef ENABLE_ROUTING
 to_host:

bpf/ep_config.h
@@ -44,4 +44,4 @@ DEFINE_U32(POLICY_VERDICT_LOG_FILTER, 0xffff);
 #define LOCAL_DELIVERY_METRICS
 #define CONNTRACK
 #define CONNTRACK_ACCOUNTING
-
+#define DIRECT_ROUTING_DEV_IFINDEX 0

bpf/include/bpf/helpers_skb.h
@@ -14,6 +14,7 @@
 
 /* Packet redirection */
 static int BPF_FUNC(redirect, int ifindex, __u32 flags);
+static int BPF_FUNC(redirect_neigh, int ifindex, __u32 flags);
 
 /* Packet manipulation */
 static int BPF_FUNC(skb_load_bytes, struct __sk_buff *skb, __u32 off,

bpf/lib/eth.h
@@ -54,13 +54,13 @@ static __always_inline int eth_load_saddr(struct __ctx_buff *ctx, __u8 *mac,
 }
 
 static __always_inline int eth_store_saddr_aligned(struct __ctx_buff *ctx,
-						   __u8 *mac, int off)
+						   const __u8 *mac, int off)
 {
 	return ctx_store_bytes(ctx, off + ETH_ALEN, mac, ETH_ALEN, 0);
 }
 
-static __always_inline int eth_store_saddr(struct __ctx_buff *ctx, __u8 *mac,
-					   int off)
+static __always_inline int eth_store_saddr(struct __ctx_buff *ctx,
+					   const __u8 *mac, int off)
 {
 #if !CTX_DIRECT_WRITE_OK
 	return eth_store_saddr_aligned(ctx, mac, off);
@@ -85,13 +85,13 @@ static __always_inline int eth_load_daddr(struct __ctx_buff *ctx, __u8 *mac,
 }
 
 static __always_inline int eth_store_daddr_aligned(struct __ctx_buff *ctx,
-						   __u8 *mac, int off)
+						   const __u8 *mac, int off)
 {
 	return ctx_store_bytes(ctx, off, mac, ETH_ALEN, 0);
 }
 
-static __always_inline int eth_store_daddr(struct __ctx_buff *ctx, __u8 *mac,
-					   int off)
+static __always_inline int eth_store_daddr(struct __ctx_buff *ctx,
+					   const __u8 *mac, int off)
 {
 #if !CTX_DIRECT_WRITE_OK
 	return eth_store_daddr_aligned(ctx, mac, off);

bpf/lib/l3.h
@@ -16,14 +16,14 @@
 
 #ifdef ENABLE_IPV6
 static __always_inline int ipv6_l3(struct __ctx_buff *ctx, int l3_off,
-				   __u8 *smac, __u8 *dmac, __u8 direction)
+				   const __u8 *smac, const __u8 *dmac,
+				   __u8 direction)
 {
 	int ret;
 
 	ret = ipv6_dec_hoplimit(ctx, l3_off);
 	if (IS_ERR(ret))
 		return ret;
-
 	if (ret > 0) {
 		/* Hoplimit was reached */
 		return icmp6_send_time_exceeded(ctx, l3_off, direction);
@@ -31,8 +31,7 @@ static __always_inline int ipv6_l3(struct __ctx_buff *ctx, int l3_off,
 
 	if (smac && eth_store_saddr(ctx, smac, 0) < 0)
 		return DROP_WRITE_ERROR;
-
-	if (eth_store_daddr(ctx, dmac, 0) < 0)
+	if (dmac && eth_store_daddr(ctx, dmac, 0) < 0)
 		return DROP_WRITE_ERROR;
 
 	return CTX_ACT_OK;
@@ -40,7 +39,8 @@ static __always_inline int ipv6_l3(struct __ctx_buff *ctx, int l3_off,
 #endif /* ENABLE_IPV6 */
 
 static __always_inline int ipv4_l3(struct __ctx_buff *ctx, int l3_off,
-				   __u8 *smac, __u8 *dmac, struct iphdr *ip4)
+				   const __u8 *smac, const __u8 *dmac,
+				   struct iphdr *ip4)
 {
 	if (ipv4_dec_ttl(ctx, l3_off, ip4)) {
 		/* FIXME: Send ICMP TTL */
@@ -49,8 +49,7 @@ static __always_inline int ipv4_l3(struct __ctx_buff *ctx, int l3_off,
 
 	if (smac && eth_store_saddr(ctx, smac, 0) < 0)
 		return DROP_WRITE_ERROR;
-
-	if (eth_store_daddr(ctx, dmac, 0) < 0)
+	if (dmac && eth_store_daddr(ctx, dmac, 0) < 0)
 		return DROP_WRITE_ERROR;
 
 	return CTX_ACT_OK;

daemon/cmd/daemon_main.go
@@ -350,6 +350,9 @@ func init() {
 	flags.Bool(option.EnableBPFTProxy, defaults.EnableBPFTProxy, "Enable BPF-based proxy redirection, if support available")
 	option.BindEnv(option.EnableBPFTProxy)
 
+	flags.Bool(option.EnableHostLegacyRouting, defaults.EnableHostLegacyRouting, "Enable the legacy host forwarding model which does not bypass upper stack in host namespace")
+	option.BindEnv(option.EnableHostLegacyRouting)
+
 	flags.Bool(option.EnableXTSocketFallbackName, defaults.EnableXTSocketFallback, "Enable fallback for missing xt_socket module")
 	option.BindEnv(option.EnableXTSocketFallbackName)
 

commit 8259b3ca8d7e76f732a37a2f99c5e83afaa1d7a2

bpf: do not enable host routing when kpr is disabled
Given we rely on this for the case where kpr is enabled, we must
strictly disable host routing for the case when not.

Fixes: 9046485fcf10 ("bpf: optimize datapath through host netns")
Reported-by: Martynas Pumputis <m@lambda.lt>
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

daemon/cmd/kube_proxy_replacement.go
@@ -47,16 +47,17 @@ func initKubeProxyReplacementOptions() (strict bool) {
 	}
 
 	if option.Config.KubeProxyReplacement == option.KubeProxyReplacementDisabled {
-		log.Infof("Auto-disabling %q, %q, %q, %q, %q features",
+		log.Infof("Auto-disabling %q, %q, %q, %q, %q features and falling back to %q",
 			option.EnableNodePort, option.EnableExternalIPs,
 			option.EnableHostReachableServices, option.EnableHostPort,
-			option.EnableSessionAffinity)
+			option.EnableSessionAffinity, option.EnableHostLegacyRouting)
 
 		disableNodePort()
 		option.Config.EnableHostReachableServices = false
 		option.Config.EnableHostServicesTCP = false
 		option.Config.EnableHostServicesUDP = false
 		option.Config.EnableSessionAffinity = false
+		option.Config.EnableHostLegacyRouting = true
 
 		return
 	}