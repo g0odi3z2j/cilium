g0odi3z2j/cilium

commit 04afeb1a9c0500e7cf63cd85390ab849edcb8d70

bpf: Set BPF_F_NO_PREALLOC before comparing maps
Previously, the `bpf.Map.CheckAndUpgrade` method didn't take into
consideration the `--preallocate-bpf-maps` flag of the cilium-agent.
Therefore, in the case of the map with `type=BPF_MAP_TYPE_HASH` and
`flags=BPF_F_NO_PREALLOC(0x1)`, it was wrongly detecting that
the map needs to be updated (= removed and recreated).

This led to the CT maps w/o prealloc on older kernels to be destroyed
each time cilium-agent has restarted.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/bpf/map_linux.go
@@ -979,6 +979,8 @@ func (m *Map) resolveErrors(ctx context.Context) error {
 // Returns true if the map was upgraded.
 func (m *Map) CheckAndUpgrade(desired *MapInfo) bool {
 	desiredMapType := GetMapType(desired.MapType)
+	desired.Flags |= GetPreAllocateMapFlags(desired.MapType)
+
 	return objCheck(
 		m.fd,
 		m.path,

commit 082247498d6480137d95dd82be132fe0a2fce9a2

bpf: Fix wrong map type when fetching preallocate map flags
Previously, the wrong map type was passed into GetPreAllocateMapFlags(),
causing the wrong flags to be returned. This caused maps such as the CT
maps to be recreated, for example, but this was only observable on older
kernels such as <= 4.9:

```
2021-02-04T13:40:49.490752344Z level=debug msg="Registered BPF map" path=/sys/fs/bpf/tc/globals/cilium_ct4_global subsys=bpf
...
2021-02-04T13:40:49.494128705Z level=info msg="Regenerating restored endpoints" numRestored=2 subsys=daemon
2021-02-04T13:40:49.494245539Z level=debug msg="Registered BPF map" path=/sys/fs/bpf/tc/globals/cilium_ct4_global subsys=bpf
2021-02-04T13:40:49.494377306Z level=warning msg="Flags mismatch for BPF map" file-path=/sys/fs/bpf/tc/globals/cilium_ct4_global new=0 old=1 subsys=bpf
2021-02-04T13:40:49.494387620Z level=warning msg="Removing map to allow for property upgrade (expect map data loss)" file-path=/sys/fs/bpf/tc/globals/cilium_ct4_global subsys=bpf
2021-02-04T13:40:49.494392369Z level=warning msg="CT Map upgraded, expect brief disruption of ongoing connections" file-path=/sys/fs/bpf/tc/globals/cilium_ct4_global subsys=map-ct
```

The reason it was only observed on such kernels is because the CT maps
are first created with MapTypeLRUHash. This type is not supported in 4.9
kernels so the call to GetMapType() will normalize the map type to
another that is supported on the kernel. In this case, it would be
MapTypeHash. As previously mentioned, the flags returned depend on the
map type. So in the CT maps case, they're first created with
MapTypeLRUHash and then normalized to MapTypeHash, which if
`--preallocate-bpf-maps=false`, then the map will be created with
BPF_F_NO_PREALLOC.

However, in the map recreate path which occurs after endpoint
restoration, bpf.(*Map).CheckAndUpgrade()'s logic was asymmetric in how
it fetches the preallocate flags via GetPreAllocateMapFlags() compared
to bpf.(*Map).openOrCreate() since the wrong map type was passed in.
Concretely, it passed MapTypeLRUHash instead of MapTypeHash to
GetPreAllocateMapFlags() which requires preallocation (meaning not
BPF_F_NO_PREALLOC), hence returning 0 for the flags. Therefore, since
the wrong map type was being passed, the wrong flags were returned.

Fixes: 04afeb1a9c ("bpf: Set BPF_F_NO_PREALLOC before comparing maps")

pkg/bpf/map_linux.go
@@ -1124,7 +1124,7 @@ func (m *Map) resolveErrors(ctx context.Context) error {
 // Returns true if the map was upgraded.
 func (m *Map) CheckAndUpgrade(desired *MapInfo) bool {
 	desiredMapType := GetMapType(desired.MapType)
-	desired.Flags |= GetPreAllocateMapFlags(desired.MapType)
+	desired.Flags |= GetPreAllocateMapFlags(desiredMapType)
 
 	return objCheck(
 		m.fd,