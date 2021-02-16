g0odi3z2j/cilium

commit 9faeadd73b078acb10c86fe027589e6bf7bc0a43

eni: No longer set Spec.ENI.PreAllocate
The field has been obsoleted in favour of Spec.IPAM.PreAllocate. It no
longer makes sense to initialize this to the default if unset.

This change is backwards compatible because even the operator is
upgraded after a respective Cilium node, it would still fall back to
this default value. The value in the Spec field is primiarly for
documentation purposes when set to the default value.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/nodediscovery/nodediscovery.go
@@ -282,7 +282,6 @@ func (n *NodeDiscovery) UpdateCiliumNodeResource() {
 
 		nodeResource.Spec.ENI.VpcID = vpcID
 		nodeResource.Spec.ENI.FirstInterfaceIndex = getInt(defaults.ENIFirstInterfaceIndex)
-		nodeResource.Spec.ENI.PreAllocate = defaults.IPAMPreAllocation
 
 		if c := n.NetConf; c != nil {
 			if c.IPAM.MinAllocate != 0 {

commit 6e6b2a4ae986a4ef9a511020c0aaa156d64fc5f3

eni: Fix compatibility when operator <= 1.7 is running
In 1.8, some ENI specifics fields have been moved from spec.ENI to
spec.IPAM to make them available for Azure IPAM. The old fields still
need to be set to account for:

 * Downgrade of operator from 1.8 to 1.7 while agents remain on 1.8.

 * Race condition when the agents get upgraded from 1.7 to 1.8 before
   the operator gets upgraded to 1.8.

While this version mismatch exists, the operator will assume a value of
MinAllocate of 0 and a value of InstanceID of "". This leads to the
following incorrect behavior:

 * The operator will not allocate up to MinAllocate

 * The InstanceID "" value will result in incorrect attachments of ENIs
   to the Status.ENI field, i.e., ENIs of other nodes will get reported
   on a node. This in turn will result in IPs being used on nodes for
   ENIs which are not attached to that node.

Fixes: 9faeadd73b0 ("eni: No longer set Spec.ENI.PreAllocate")
Fixes: 8602b82d7e6 ("k8s: CiliumNode: Move generic IPAM parmeters into Spec.IPAM")

Spotted-by: André Martins <andre@cilium.io>
Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/nodediscovery/nodediscovery.go
@@ -437,14 +437,22 @@ func (n *NodeDiscovery) mutateNodeResource(nodeResource *ciliumv2.CiliumNode) {
 		if c := n.NetConf; c != nil {
 			if c.IPAM.MinAllocate != 0 {
 				nodeResource.Spec.IPAM.MinAllocate = c.IPAM.MinAllocate
+				// OBSOLETE: Left for backwards compatibility with <=1.7. Remove in >=1.11
+				nodeResource.Spec.ENI.MinAllocate = c.IPAM.MinAllocate
 			} else if c.ENI.MinAllocate != 0 {
 				nodeResource.Spec.IPAM.MinAllocate = c.ENI.MinAllocate
+				// OBSOLETE: Left for backwards compatibility with <=1.7. Remove in >=1.11
+				nodeResource.Spec.ENI.MinAllocate = c.ENI.MinAllocate
 			}
 
 			if c.IPAM.PreAllocate != 0 {
 				nodeResource.Spec.IPAM.PreAllocate = c.IPAM.PreAllocate
+				// OBSOLETE: Left for backwards compatibility with <=1.7. Remove in >=1.11
+				nodeResource.Spec.ENI.PreAllocate = c.IPAM.PreAllocate
 			} else if c.ENI.PreAllocate != 0 {
 				nodeResource.Spec.IPAM.PreAllocate = c.ENI.PreAllocate
+				// OBSOLETE: Left for backwards compatibility with <=1.7. Remove in >=1.11
+				nodeResource.Spec.ENI.PreAllocate = c.ENI.PreAllocate
 			}
 
 			if c.ENI.FirstInterfaceIndex != nil {
@@ -467,6 +475,8 @@ func (n *NodeDiscovery) mutateNodeResource(nodeResource *ciliumv2.CiliumNode) {
 		}
 
 		nodeResource.Spec.InstanceID = instanceID
+		// OBSOLETE: Left for backwards compatibility with <=1.7. Remove in >=1.11
+		nodeResource.Spec.ENI.InstanceID = instanceID
 		nodeResource.Spec.ENI.InstanceType = instanceType
 		nodeResource.Spec.ENI.AvailabilityZone = availabilityZone
 