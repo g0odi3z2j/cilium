g0odi3z2j/cilium

commit e35cea426a9785431cf8e597fc30feebb12aa99b

iptables: Populate CILIUM_FORWARD chain for IPv6
Kube-proxy installs the following rule in the iptables FORWARD chain:

    -A KUBE-FORWARD -m conntrack --ctstate INVALID -j DROP

That rule causes iptables to drop packets whenever we have a path
asymmetry with for example a SYN packet going through iptables and a
SYN+ACK packet skipping iptables via BPF's redirect(). In that example,
the next ACK packet would be dropped by that rule because the connection
is invalid (SYN+ACK is missing) from iptables' point of view.

To avoid such drops, in the IPv4 case, Cilium installs rules in the
FORWARD chain (via its own CILIUM_FORWARD chain) to bypass the
subsequent -j DROP rule from kube-proxy. For example, with per-endpoint
routes enabled:

    -A FORWARD -m comment --comment "cilium-feeder: CILIUM_FORWARD" -j CILIUM_FORWARD
    -A CILIUM_FORWARD -o cilium_host -m comment --comment "cilium: any->cluster on cilium_host forward accept" -j ACCEPT
    -A CILIUM_FORWARD -i cilium_host -m comment --comment "cilium: cluster->any on cilium_host forward accept (nodeport)" -j ACCEPT
    -A CILIUM_FORWARD -i lxc+ -m comment --comment "cilium: cluster->any on lxc+ forward accept" -j ACCEPT
    -A CILIUM_FORWARD -i cilium_net -m comment --comment "cilium: cluster->any on cilium_net forward accept (nodeport)" -j ACCEPT
    -A CILIUM_FORWARD -o lxc+ -m comment --comment "cilium: any->cluster on lxc+ forward accept" -j ACCEPT
    -A CILIUM_FORWARD -i lxc+ -m comment --comment "cilium: cluster->any on lxc+ forward accept (nodeport)" -j ACCEPT

The same rules are however missing from the IPv6 tables. This commit
fixes this inconsistency.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/iptables/iptables.go
@@ -353,6 +353,7 @@ var ciliumChains = []customChain{
 		table:      "filter",
 		hook:       "FORWARD",
 		feederArgs: []string{""},
+		ipv6:       true,
 	},
 }
 
@@ -839,6 +840,19 @@ func getDeliveryInterface(ifName string) string {
 }
 
 func (m *IptablesManager) installForwardChainRules(ifName, localDeliveryInterface, forwardChain string) error {
+	if option.Config.EnableIPv4 {
+		err := m.installForwardChainRulesIpX("iptables", ifName, localDeliveryInterface, forwardChain)
+		if err != nil {
+			return err
+		}
+	}
+	if option.Config.EnableIPv6 {
+		return m.installForwardChainRulesIpX("ip6tables", ifName, localDeliveryInterface, forwardChain)
+	}
+	return nil
+}
+
+func (m *IptablesManager) installForwardChainRulesIpX(prog, ifName, localDeliveryInterface, forwardChain string) error {
 	transient := ""
 	if forwardChain == ciliumTransientForwardChain {
 		transient = " (transient)"
@@ -865,7 +879,7 @@ func (m *IptablesManager) installForwardChainRules(ifName, localDeliveryInterfac
 	//  - Node running backend:
 	//       IN=eno1 OUT=cilium_host
 	//       IN=lxc... OUT=eno1
-	if err := runProg("iptables", append(
+	if err := runProg(prog, append(
 		m.waitArgs,
 		"-A", forwardChain,
 		"-o", ifName,
@@ -873,7 +887,7 @@ func (m *IptablesManager) installForwardChainRules(ifName, localDeliveryInterfac
 		"-j", "ACCEPT"), false); err != nil {
 		return err
 	}
-	if err := runProg("iptables", append(
+	if err := runProg(prog, append(
 		m.waitArgs,
 		"-A", forwardChain,
 		"-i", ifName,
@@ -881,7 +895,7 @@ func (m *IptablesManager) installForwardChainRules(ifName, localDeliveryInterfac
 		"-j", "ACCEPT"), false); err != nil {
 		return err
 	}
-	if err := runProg("iptables", append(
+	if err := runProg(prog, append(
 		m.waitArgs,
 		"-A", forwardChain,
 		"-i", "lxc+",
@@ -893,7 +907,7 @@ func (m *IptablesManager) installForwardChainRules(ifName, localDeliveryInterfac
 	// TODO: Make 'cilium_net' configurable if we ever support other than "cilium_host" as the Cilium host device.
 	if ifName == "cilium_host" {
 		ifPeerName := "cilium_net"
-		if err := runProg("iptables", append(
+		if err := runProg(prog, append(
 			m.waitArgs,
 			"-A", forwardChain,
 			"-i", ifPeerName,
@@ -906,7 +920,7 @@ func (m *IptablesManager) installForwardChainRules(ifName, localDeliveryInterfac
 	// same (enable-endpoint-routes), a separate set of rules to allow
 	// from/to delivery interface is required.
 	if localDeliveryInterface != ifName {
-		if err := runProg("iptables", append(
+		if err := runProg(prog, append(
 			m.waitArgs,
 			"-A", forwardChain,
 			"-o", localDeliveryInterface,
@@ -914,7 +928,7 @@ func (m *IptablesManager) installForwardChainRules(ifName, localDeliveryInterfac
 			"-j", "ACCEPT"), false); err != nil {
 			return err
 		}
-		if err := runProg("iptables", append(
+		if err := runProg(prog, append(
 			m.waitArgs,
 			"-A", forwardChain,
 			"-i", localDeliveryInterface,
@@ -1163,11 +1177,11 @@ func (m *IptablesManager) InstallRules(ifName string) error {
 
 	localDeliveryInterface := getDeliveryInterface(ifName)
 
-	if option.Config.EnableIPv4 {
-		if err := m.installForwardChainRules(ifName, localDeliveryInterface, ciliumForwardChain); err != nil {
-			return fmt.Errorf("cannot install forward chain rules to %s: %s", transientChain.name, err)
-		}
+	if err := m.installForwardChainRules(ifName, localDeliveryInterface, ciliumForwardChain); err != nil {
+		return fmt.Errorf("cannot install forward chain rules to %s: %s", transientChain.name, err)
+	}
 
+	if option.Config.EnableIPv4 {
 		if err := m.installHostTrafficMarkRule("iptables"); err != nil {
 			return err
 		}

commit 37c2939a79bcbd3e5c3a43a8c77cb9690a2a743c

iptables: Skip CILIUM_TRANSIENT_FORWARD for IPv6
Commit e35cea4 ("iptables: Populate CILIUM_FORWARD chain for IPv6")
fixed the agent to add IPv6 counterparts to the Cilium IPv4 rules in
CILIUM_FORWARD. The same method is however used to install rules in
CILIUM_FORWARD and CILIUM_TRANSIENT_FORWARD. The IPv6 chain for
CILIUM_TRANSIENT_FORWARD doesn't exist, resulting in the following
error message:

    level=error msg="Command execution failed" cmd="[ip6tables -w 5 -A CILIUM_TRANSIENT_FORWARD -o cilium_host -m comment --comment cilium (transient): any->cluster on cilium_host forward accept -j ACCEPT]" error="exit status 1" subsys=iptables
    level=warning msg="ip6tables: No chain/target/match by that name." subsys=iptables
    level=warning msg="failed to install transient iptables rules" error="cannot install forward chain rules to CILIUM_TRANSIENT_FORWARD: exit status 1" subsys=datapath-loader

This commit fixes it by skipping the installation of IPv6 rules in
CILIUM_TRANSIENT_FORWARD. If we want those rules, more work is required.

Fixes: e35cea4 ("iptables: Populate CILIUM_FORWARD chain for IPv6")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/iptables/iptables.go
@@ -1124,7 +1124,7 @@ func (m *IptablesManager) TransientRulesStart(ifName string) error {
 		if err := transientChain.add(m.waitArgs); err != nil {
 			return fmt.Errorf("cannot add custom chain %s: %s", transientChain.name, err)
 		}
-		if err := m.installForwardChainRules(ifName, localDeliveryInterface, transientChain.name); err != nil {
+		if err := m.installForwardChainRulesIpX("iptables", ifName, localDeliveryInterface, transientChain.name); err != nil {
 			return fmt.Errorf("cannot install forward chain rules to %s: %s", transientChain.name, err)
 		}
 		if err := transientChain.installFeeder(m.waitArgs); err != nil {