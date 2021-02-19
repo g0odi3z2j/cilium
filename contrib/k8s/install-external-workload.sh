g0odi3z2j/cilium

commit 7758a47f41de0a56f1afece84e2bfaa80ae7be03

contrib/k8s: Add scripts for provisioning VMs
Add script 'extract-external-workload-certs.sh' to extract external
workload certs from k8s cluster to be used by a VM. Certs are placed
in the current directory, which can be overridden by defining
TLS_PATH.

Add script 'install-external-workload.sh' to run Cilium in a docker
container in a VM. CLUSTER_ADDR must be defined as the IP in the k8s
cluster at which 'clustermesh-apiserver' is available. TLS certs need
to exist in the current directory, or in dir TLS_PATH. Default Cilium
image can be overridden with CILIUM_IMAGE. VM's IP address can be
selected by defining 'NODE_IP'. Script waits for Cilium and kube-dns
service to become available and overrides the VM DNS config to use
kube-dns as the nameserver.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

contrib/k8s/install-external-workload.sh
@@ -1,0 +1,132 @@
+#!/bin/bash
+set -e
+set -x
+shopt -s extglob
+
+if [ -z "$CLUSTER_ADDR" ] ; then
+    echo "CLUSTER_ADDR must be defined to the IP:PORT at which the clustermesh-apiserver is reachable."
+    exit 1
+fi
+
+CILIUM_IMAGE=${CILIUM_IMAGE:-cilium/cilium:latest}
+
+# TLS certs path defaults to the current directory
+TLS_PATH=${TLS_PATH:-.}
+
+port='@(6553[0-5]|655[0-2][0-9]|65[0-4][0-9][0-9]|6[0-4][0-9][0-9][0-9]|[1-5][0-9][0-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9]|[1-9])'
+byte='@(25[0-5]|2[0-4][0-9]|[1][0-9][0-9]|[1-9][0-9]|[0-9])'
+ipv4="$byte\.$byte\.$byte\.$byte"
+
+# Default port is for a HostPort service
+case "$CLUSTER_ADDR" in
+    \[+([0-9a-fA-F:])\]:$port)
+	CLUSTER_PORT=${CLUSTER_ADDR##\[*\]:}
+	CLUSTER_IP=${CLUSTER_ADDR#\[}
+	CLUSTER_IP=${CLUSTER_IP%\]:*}
+	;;
+    [^[]$ipv4:$port)
+	CLUSTER_PORT=${CLUSTER_ADDR##*:}
+	CLUSTER_IP=${CLUSTER_ADDR%:*}
+	;;
+    *:*)
+	echo "Malformed CLUSTER_ADDR: $CLUSTER_ADDR"
+	exit 1
+	;;
+    *)
+	CLUSTER_PORT=32379
+	CLUSTER_IP=$CLUSTER_ADDR
+	;;
+esac
+
+CA_CRT="${TLS_PATH}/external-workload-ca.crt"
+TLS_CRT="${TLS_PATH}/external-workload-tls.crt"
+TLS_KEY="${TLS_PATH}/external-workload-tls.key"
+
+if [[ ! ( -f $CA_CRT && -f $TLS_CRT && -f $TLS_KEY ) ]]; then
+    echo "Client certificates not found. These can be extracted from your Kubernetes"
+    echo "cluster with 'contrib/k8s/extract-external-workload-certs.sh'"
+    exit 1
+fi
+
+sudo mkdir -p /var/lib/cilium/etcd
+sudo cp "$CA_CRT" /var/lib/cilium/etcd/ca.crt
+sudo cp "$TLS_CRT" /var/lib/cilium/etcd/tls.crt
+sudo cp "$TLS_KEY" /var/lib/cilium/etcd/tls.key
+sudo tee /var/lib/cilium/etcd/config.yaml <<EOF >/dev/null
+---
+trusted-ca-file: /var/lib/cilium/etcd/ca.crt
+cert-file: /var/lib/cilium/etcd/tls.crt
+key-file: /var/lib/cilium/etcd/tls.key
+endpoints:
+- https://clustermesh-apiserver.cilium.io:$CLUSTER_PORT
+EOF
+
+CILIUM_OPTS=" --join-cluster --enable-host-reachable-services"
+CILIUM_OPTS+=" --kvstore etcd --kvstore-opt etcd.config=/var/lib/cilium/etcd/config.yaml"
+if [ -n "$HOST_IP" ] ; then
+    CILIUM_OPTS+=" --ipv4-node $HOST_IP"
+fi
+if [ -n "$DEBUG" ] ; then
+    CILIUM_OPTS+=" --debug"
+fi
+
+DOCKER_OPTS=" -d --log-driver syslog --restart always"
+DOCKER_OPTS+=" --privileged --network host --cap-add NET_ADMIN --cap-add SYS_MODULE"
+DOCKER_OPTS+=" --volume /var/lib/cilium/etcd:/var/lib/cilium/etcd"
+DOCKER_OPTS+=" --volume /var/run/cilium:/var/run/cilium"
+DOCKER_OPTS+=" --volume /boot:/boot"
+DOCKER_OPTS+=" --volume /lib/modules:/lib/modules"
+DOCKER_OPTS+=" --volume /sys/fs/bpf:/sys/fs/bpf"
+DOCKER_OPTS+=" --volume /run/xtables.lock:/run/xtables.lock"
+DOCKER_OPTS+=" --add-host clustermesh-apiserver.cilium.io:$CLUSTER_IP"
+
+if [ -n "$(docker ps -a -q -f name=cilium)" ]; then
+    echo "Shutting down running Cilium agent"
+    sudo docker rm -f cilium || true
+fi
+
+echo "Launching Cilium agent..."
+sudo docker run --name cilium $DOCKER_OPTS cilium/cilium:latest cilium-agent $CILIUM_OPTS
+
+# Copy Cilium CLI
+sudo docker cp cilium:/usr/bin/cilium /usr/bin/cilium
+
+# Wait for cilium agent to become available
+cilium_started=false
+for ((i = 0 ; i < 24; i++)); do
+    if cilium status --brief > /dev/null 2>&1; then
+        cilium_started=true
+        break
+    fi
+    sleep 5s
+    echo "Waiting for Cilium daemon to come up..."
+done
+
+if [ "$cilium_started" = true ] ; then
+    echo 'Cilium successfully started!'
+else
+    >&2 echo 'Timeout waiting for Cilium to start.'
+    exit 1
+fi
+
+# Wait for kube-dns service to become available
+kubedns=""
+for ((i = 0 ; i < 24; i++)); do
+    kubedns=$(cilium service list get -o jsonpath='{[?(@.spec.frontend-address.port==53)].spec.frontend-address.ip}')
+    if [ -n "$kubedns" ] ; then
+        break
+    fi
+    sleep 5s
+    echo "Waiting for kube-dns service to come available..."
+done
+
+if [ -n "$kubedns" ] ; then
+    echo "updating /etc/resolv.conf with kube-dns IP $kubedns"
+
+    sudo tee /etc/resolv.conf <<EOF >/dev/null
+nameserver $kubedns
+EOF
+else
+    >&2 echo "kube-dns not found."
+    exit 1
+fi

.gitignore
@@ -68,9 +68,9 @@ test/cilium-istioctl
 # generated test files
 test/k8sT/manifests/cnp-second-namespaces.yaml
 test/cilium.conf.ginkgo
-test/provision/externalworkload-client-ca.crt
-test/provision/externalworkload-client-tls.crt
-test/provision/externalworkload-client-tls.key
+external-workload-ca.crt
+external-workload-tls.crt
+external-workload-tls.key
 
 # GKE temporary files
 test/gke/cluster-name

contrib/k8s/extract-external-workload-certs.sh
@@ -1,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# TLS certs path defaults to the current directory
+TLS_PATH=${TLS_PATH:-.}
+
+echo "Extracting external workload TLS config from Kubernetes secrets in to ${TLS_PATH}"
+kubectl -n kube-system get secret clustermesh-apiserver-ca-cert -o jsonpath="{.data['ca\.crt']}" | base64 --decode >"${TLS_PATH}"/external-workload-ca.crt
+kubectl -n kube-system get secret clustermesh-apiserver-client-cert -o jsonpath="{.data['tls\.crt']}" | base64 --decode >"${TLS_PATH}"/external-workload-tls.crt
+kubectl -n kube-system get secret clustermesh-apiserver-client-cert -o jsonpath="{.data['tls\.key']}" | base64 --decode >"${TLS_PATH}"/external-workload-tls.key

test/provision/externalworkload_extract_k8s_certs.sh
@@ -1,9 +1,0 @@
-#!/bin/bash
-set -e
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-echo "Extracting VM TLS config from Kubernetes secrets"
-kubectl -n kube-system get secret clustermesh-apiserver-ca-cert -o jsonpath="{.data['ca\.crt']}" | base64 --decode >${DIR}/externalworkload-client-ca.crt
-kubectl -n kube-system get secret clustermesh-apiserver-client-cert -o jsonpath="{.data['tls\.crt']}" | base64 --decode >${DIR}/externalworkload-client-tls.crt
-kubectl -n kube-system get secret clustermesh-apiserver-client-cert -o jsonpath="{.data['tls\.key']}" | base64 --decode >${DIR}/externalworkload-client-tls.key

test/provision/externalworkload_install.sh
@@ -1,61 +1,9 @@
 #!/bin/bash
 set -e
-set -x
 
-# IP address at which the clustermesh-apiserver service is reachable at
-# Default to the address of k8s1
-CLUSTER_IP=${CLUSTER_IP:-"192.168.36.11"}
-
-# IP address of the VM itself. This is needed due to avoid Cilium selecting the "10.0.2.15"
-# each vagrant VM has as the first address. Default to the address of the runtime VM
-VM_IP=${VM_IP:-"192.168.36.10"}
-
-PROVISIONSRC="/tmp/provision"
-
-CA_CRT="${PROVISIONSRC}/externalworkload-client-ca.crt"
-TLS_CRT="${PROVISIONSRC}/externalworkload-client-tls.crt"
-TLS_KEY="${PROVISIONSRC}/externalworkload-client-tls.key"
-
-if [[ ! ( -f $CA_CRT && -f $TLS_CRT && -f $TLS_KEY ) ]]; then
-    echo "Client certificates not found. These can be extracted from your Kubernetes"
-    echo "cluster with 'test/provision/externalworkload_extract_k8s_certs.sh'"
-    exit 1
-fi
-
-sudo mkdir -p /var/lib/cilium/etcd
-sudo cp $CA_CRT /var/lib/cilium/etcd/ca.crt
-sudo cp $TLS_CRT /var/lib/cilium/etcd/tls.crt
-sudo cp $TLS_KEY /var/lib/cilium/etcd/tls.key
-sudo tee /var/lib/cilium/etcd/config.yaml <<EOF
----
-trusted-ca-file: /var/lib/cilium/etcd/ca.crt
-cert-file: /var/lib/cilium/etcd/tls.crt
-key-file: /var/lib/cilium/etcd/tls.key
-endpoints:
-- https://clustermesh-apiserver.cilium.io:32379
-EOF
-
-CILIUM_OPTS=" --debug --ipv4-node $VM_IP"
-CILIUM_OPTS+=" --join-cluster"
-CILIUM_OPTS+=" --kvstore etcd --kvstore-opt etcd.config=/var/lib/cilium/etcd/config.yaml"
+cd /home/vagrant/go/src/github.com/cilium/cilium
 
 # Build docker image
-DOCKER_BUILDKIT=1 make -C /home/vagrant/go/src/github.com/cilium/cilium dev-docker-image
+DOCKER_BUILDKIT=1 make docker-cilium-image
 
-# Etcd TLS config needs hostname IP mapping
-CLUSTER_HOST="clustermesh-apiserver.cilium.io:$CLUSTER_IP"
-
-DOCKER_OPTS=" -d --log-driver syslog --restart always"
-DOCKER_OPTS+=" --privileged --network host --cap-add NET_ADMIN --cap-add SYS_MODULE"
-DOCKER_OPTS+=" --volume /var/lib/cilium/etcd:/var/lib/cilium/etcd"
-DOCKER_OPTS+=" --volume /var/run/cilium:/var/run/cilium"
-DOCKER_OPTS+=" --volume /boot:/boot"
-DOCKER_OPTS+=" --volume /lib/modules:/lib/modules"
-DOCKER_OPTS+=" --volume /sys/fs/bpf:/sys/fs/bpf"
-DOCKER_OPTS+=" --volume /run/xtables.lock:/run/xtables.lock"
-DOCKER_OPTS+=" --add-host $CLUSTER_HOST"
-
-sudo docker run --name cilium $DOCKER_OPTS cilium/cilium-dev:latest cilium-agent $CILIUM_OPTS
-
-# Copy Cilium CLI
-sudo docker cp cilium:/usr/bin/cilium /usr/bin/cilium
+CLUSTER_ADDR=192.168.36.11:32379 HOST_IP=192.168.36.10 CILIUM_IMAGE=cilium/cilium:latest contrib/k8s/install-external-workload.sh

test/provision/runtime_install.sh
@@ -15,8 +15,7 @@ sudo systemctl restart ssh
 
 if [[ "${PROVISION_EXTERNAL_WORKLOAD}" == "false" ]]; then
     "${PROVISIONSRC}"/compile.sh
+    "${PROVISIONSRC}"/wait-cilium.sh
 else
     "${PROVISIONSRC}"/externalworkload_install.sh
 fi
-
-"${PROVISIONSRC}"/wait-cilium.sh

commit 46d1707bc33c288309b3325158105096208d81b1

contrib/k8s: use provided image when installing external workload
When installing external workloads, launch Cilium agent from the
provided, versioned image instead of hardcoding cilium/cilium:latest.
The following command will now use Cilium 1.9.4 instead of latest:

    CLUSTER_ADDR=<ip> CILIUM_IMAGE=cilium/cilium:v1.9.4 ./install-external-workload.sh`

Fixes: 7758a47f41de ("contrib/k8s: Add scripts for provisioning VMs")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

contrib/k8s/install-external-workload.sh
@@ -86,7 +86,7 @@ if [ -n "$(docker ps -a -q -f name=cilium)" ]; then
 fi
 
 echo "Launching Cilium agent..."
-sudo docker run --name cilium $DOCKER_OPTS cilium/cilium:latest cilium-agent $CILIUM_OPTS
+sudo docker run --name cilium $DOCKER_OPTS $CILIUM_IMAGE cilium-agent $CILIUM_OPTS
 
 # Copy Cilium CLI
 sudo docker cp cilium:/usr/bin/cilium /usr/bin/cilium