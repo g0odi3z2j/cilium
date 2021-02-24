g0odi3z2j/cilium

commit 3620edaa055b7c95f2d8fca43d28e4414ee576b1

Makefile: build Cilium with madvdontneed=1
Setting this option makes Cilium to run in a mode that will allow
it to reduce its process' RSS even if the system is not under
memory pressure.

By default, i.e. without 'madvdontneed' being set, Cilium will only
release its process' RSS when the system is under pressure. This
may be confusing for users since they will assume Cilium needs the
memory reported in RSS, which is not true.

Signed-off-by: André Martins <andre@cilium.io>

contrib/vagrant/start.sh
@@ -348,6 +348,8 @@ sleep 2s
 if [ -n "\${K8S}" ]; then
     echo "K8S_NODE_NAME=\$(hostname)" >> /etc/sysconfig/cilium
 fi
+# FIXME Remove me once we add support for Go 1.16
+echo 'GODEBUG="madvdontneed=1"' >> /etc/sysconfig/cilium
 echo 'CILIUM_OPTS="${cilium_options}"' >> /etc/sysconfig/cilium
 echo 'CILIUM_OPERATOR_OPTS="${cilium_operator_options}"' >> /etc/sysconfig/cilium
 echo 'PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin' >> /etc/sysconfig/cilium

Dockerfile
@@ -81,4 +81,6 @@ RUN groupadd -f cilium \
     && echo ". /etc/profile.d/bash_completion.sh" >> /etc/bash.bashrc
 
 ENV INITSYSTEM="SYSTEMD"
+# FIXME Remove me once we add support for Go 1.16
+ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium"]

cilium-operator-aws.Dockerfile
@@ -53,4 +53,6 @@ COPY --from=certs /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certifica
 COPY --from=gops /go/bin/gops /bin/gops
 COPY --from=builder /go/src/github.com/cilium/cilium/LICENSE.all /LICENSE.all
 WORKDIR /
+# FIXME Remove me once we add support for Go 1.16
+ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium-operator-aws"]

cilium-operator-azure.Dockerfile
@@ -53,4 +53,6 @@ COPY --from=certs /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certifica
 COPY --from=gops /go/bin/gops /bin/gops
 COPY --from=builder /go/src/github.com/cilium/cilium/LICENSE.all /LICENSE.all
 WORKDIR /
+# FIXME Remove me once we add support for Go 1.16
+ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium-operator-azure"]

cilium-operator-generic.Dockerfile
@@ -53,4 +53,6 @@ COPY --from=certs /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certifica
 COPY --from=gops /go/bin/gops /bin/gops
 COPY --from=builder /go/src/github.com/cilium/cilium/LICENSE.all /LICENSE.all
 WORKDIR /
+# FIXME Remove me once we add support for Go 1.16
+ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium-operator-generic"]

cilium-operator.Dockerfile
@@ -53,4 +53,6 @@ COPY --from=certs /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certifica
 COPY --from=gops /go/bin/gops /bin/gops
 COPY --from=builder /go/src/github.com/cilium/cilium/LICENSE.all /LICENSE.all
 WORKDIR /
+# FIXME Remove me once we add support for Go 1.16
+ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium-operator"]

clustermesh-apiserver.Dockerfile
@@ -56,4 +56,6 @@ COPY --from=builder /go/src/github.com/cilium/cilium/clustermesh-apiserver/clust
 COPY --from=certs /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
 COPY --from=gops /go/bin/gops /bin/gops
 COPY --from=builder /go/src/github.com/cilium/cilium/LICENSE.all /LICENSE.all
+# FIXME Remove me once we add support for Go 1.16
+ENV GODEBUG="madvdontneed=1"
 ENTRYPOINT ["/usr/bin/clustermesh-apiserver"]

hubble-relay.Dockerfile
@@ -48,4 +48,6 @@ COPY --from=certs /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certifica
 COPY --from=gops /go/bin/gops /bin/gops
 COPY --from=builder /go/src/github.com/cilium/cilium/LICENSE.all /LICENSE.all
 ENTRYPOINT ["/usr/bin/hubble-relay"]
+# FIXME Remove me once we add support for Go 1.16
+ENV GODEBUG="madvdontneed=1"
 CMD ["serve"]

commit 46bdbe1d080daacfe26f20fdb5162222ffa677cd

Drop GODEBUG='madvdontneed=1' setting with Go 1.16
Go 1.16 switched back to madvdontneed=1 by default, see
https://golang.org/doc/go1.16#runtime. Thus we no longer need to set
this explicitly at runtime after switching to Go 1.16.

This reverts the GODEBUG setting introduced by commit 3620edaa055b
("Makefile: build Cilium with madvdontneed=1")

Fixes: 1b40c43022a2 ("Update Go to 1.16")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

contrib/vagrant/start.sh
@@ -348,8 +348,6 @@ sleep 2s
 if [ -n "\${K8S}" ]; then
     echo "K8S_NODE_NAME=\$(hostname)" >> /etc/sysconfig/cilium
 fi
-# FIXME Remove me once we add support for Go 1.16
-echo 'GODEBUG="madvdontneed=1"' >> /etc/sysconfig/cilium
 echo 'CILIUM_OPTS="${cilium_options}"' >> /etc/sysconfig/cilium
 echo 'CILIUM_OPERATOR_OPTS="${cilium_operator_options}"' >> /etc/sysconfig/cilium
 echo 'PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin' >> /etc/sysconfig/cilium

images/cilium-docker-plugin/Dockerfile
@@ -32,6 +32,4 @@ ARG TARGETARCH
 LABEL maintainer="maintainer@cilium.io"
 COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/usr/bin/cilium-docker /usr/bin/cilium-docker
 WORKDIR /
-# FIXME Remove me once we add support for Go 1.16
-ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium-docker"]

images/cilium/Dockerfile
@@ -88,6 +88,4 @@ COPY --from=builder /tmp/install/${TARGETOS}/${TARGETARCH} /
 WORKDIR /home/cilium
 
 ENV INITSYSTEM="SYSTEMD"
-# FIXME Remove me once we add support for Go 1.16
-ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium"]

images/clustermesh-apiserver/Dockerfile
@@ -63,6 +63,4 @@ COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/usr/bin/clustermesh-apiserver
 COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/LICENSE.all /LICENSE.all
 WORKDIR /
 ENV GOPS_CONFIG_DIR=/
-# FIXME Remove me once we add support for Go 1.16
-ENV GODEBUG="madvdontneed=1"
 ENTRYPOINT ["/usr/bin/clustermesh-apiserver"]

images/hubble-relay/Dockerfile
@@ -61,6 +61,4 @@ COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/LICENSE.all /LICENSE.all
 WORKDIR /
 ENV GOPS_CONFIG_DIR=/
 ENTRYPOINT ["/usr/bin/hubble-relay"]
-# FIXME Remove me once we add support for Go 1.16
-ENV GODEBUG="madvdontneed=1"
 CMD ["serve"]

images/operator-aws/Dockerfile
@@ -60,6 +60,4 @@ COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/usr/bin/cilium-operator-aws /
 COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/LICENSE.all /LICENSE.all
 WORKDIR /
 ENV GOPS_CONFIG_DIR=/
-# FIXME Remove me once we add support for Go 1.16
-ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium-operator-aws"]

images/operator-azure/Dockerfile
@@ -60,6 +60,4 @@ COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/usr/bin/cilium-operator-azure
 COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/LICENSE.all /LICENSE.all
 WORKDIR /
 ENV GOPS_CONFIG_DIR=/
-# FIXME Remove me once we add support for Go 1.16
-ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium-operator-azure"]

images/operator-generic/Dockerfile
@@ -60,6 +60,4 @@ COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/usr/bin/cilium-operator-gener
 COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/LICENSE.all /LICENSE.all
 WORKDIR /
 ENV GOPS_CONFIG_DIR=/
-# FIXME Remove me once we add support for Go 1.16
-ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium-operator-generic"]

images/operator/Dockerfile
@@ -60,6 +60,4 @@ COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/usr/bin/cilium-operator /usr/
 COPY --from=builder /out/${TARGETOS}/${TARGETARCH}/LICENSE.all /LICENSE.all
 WORKDIR /
 ENV GOPS_CONFIG_DIR=/
-# FIXME Remove me once we add support for Go 1.16
-ENV GODEBUG="madvdontneed=1"
 CMD ["/usr/bin/cilium-operator"]