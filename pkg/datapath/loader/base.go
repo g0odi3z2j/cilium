g0odi3z2j/cilium

commit dcf1f5321e9defaa2ba5c2e7b66624e6921cd298

encryption/loader: Attach bpf_network program to correct interface for ENI mode
Problem: Auto detection of encryption.interface leads to bpf_network program
that does decryption to get attached to a wrong network interface.
As a result, packets were not decrypted on the destination remote node.

If users don't specify the cilium `encryption.interface`
configuration, the interface can be auto detected based on main routing
table. This breaks in ENI IPAM mode, where cilium_host is allocated an IP
address from the pool of secondary IP addresses of a primary ENI interface.
The IP allocation is influenced by the specified `first-interface-index` configuration.
Hence, retrieve the correct primary interface for cilium_host for bpf_network
program to be attached.

Fix is verified based on the xfrm stats -

Packet stats on source node :

src 192.168.117.26 dst 192.168.142.218
	proto esp spi 0x00000003(3) reqid 1(0x00000001) mode tunnel
	replay-window 0 seq 0x00000000 flag  (0x00000000)
	mark 0x3e00/0xff00 output-mark 0xe00
	aead rfc4106(gcm(aes)) 0xea5799388f441ba52b8c60c4f11c084bcdf5373e (160 bits) 128
	anti-replay context: seq 0x0, oseq 0x9, bitmap 0x00000000
	sel src 0.0.0.0/0 dst 0.0.0.0/0 uid 0
	lifetime config:
	  limit: soft (INF)(bytes), hard (INF)(bytes)
	  limit: soft (INF)(packets), hard (INF)(packets)
	  expire add: soft 0(sec), hard 0(sec)
	  expire use: soft 0(sec), hard 0(sec)
	lifetime current:
	  540(bytes), 9(packets)
	  add 2021-02-05 18:12:08 use 2021-02-05 18:13:01
	stats:
	  replay-window 0 replay 0 failed 0

Packet stats on dest node :

ip -s xfrm s
src 192.168.142.218 dst 192.168.117.26
	proto esp spi 0x00000003(3) reqid 1(0x00000001) mode tunnel
	replay-window 0 seq 0x00000000 flag  (0x00000000)
	mark 0x3e00/0xff00 output-mark 0xe00
	aead rfc4106(gcm(aes)) 0xea5799388f441ba52b8c60c4f11c084bcdf5373e (160 bits) 128
	anti-replay context: seq 0x0, oseq 0x6, bitmap 0x00000000
	sel src 0.0.0.0/0 dst 0.0.0.0/0 uid 0
	lifetime config:

pkg/datapath/loader/base.go
@@ -346,6 +346,16 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 	}).Info("Setting up BPF datapath")
 
 	if option.Config.IPAM == ipamOption.IPAMENI {
+		// For the ENI ipam mode on EKS, this will be the interface that
+		// the router (cilium_host) IP is associated to.
+		if info := node.GetRouterInfo(); info != nil {
+			mac := info.GetMac()
+			iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
+			if err != nil {
+				log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
+			}
+			args[initArgEncryptInterface] = iface
+		}
 		var err error
 		if sysSettings, err = addENIRules(sysSettings, o.Datapath().LocalNodeAddressing()); err != nil {
 			return fmt.Errorf("unable to install ip rule for ENI multi-node NodePort: %w", err)

pkg/datapath/linux/routing/routing.go
@@ -223,6 +223,17 @@ func SetupRules(from, to *net.IPNet, mac string, ifaceNum int) error {
 	})
 }
 
+// RetrieveIfaceNameFromMAC finds the corresponding device name for a
+// given MAC address.
+func RetrieveIfaceNameFromMAC(mac string) (string, error) {
+	iface, err := retrieveIfaceFromMAC(mac)
+	if err != nil {
+		err = fmt.Errorf("failed to get iface name with MAC %w", err)
+		return "", err
+	}
+	return iface.Attrs().Name, nil
+}
+
 func deleteRule(r route.Rule) error {
 	rules, err := route.ListRules(netlink.FAMILY_V4, &r)
 	if err != nil {

commit 0d6a729a3288765ece317608a4ca4ca4e31217ae

cilium: encryption, do not try to auto-discover if user provides iface
We currently ignore the user provided interface in ENI modes. Lets not
overwrite user input if they specify it.

Fixes: dcf1f5321e ("encryption/loader: Attach bpf_network program to correct interface for ENI mode")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/loader/base.go
@@ -357,13 +357,15 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 	if option.Config.IPAM == ipamOption.IPAMENI {
 		// For the ENI ipam mode on EKS, this will be the interface that
 		// the router (cilium_host) IP is associated to.
-		if info := node.GetRouterInfo(); info != nil {
-			mac := info.GetMac()
-			iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
-			if err != nil {
-				log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
+		if option.Config.EncryptInterface == "" {
+			if info := node.GetRouterInfo(); info != nil {
+				mac := info.GetMac()
+				iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
+				if err != nil {
+					log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
+				}
+				args[initArgEncryptInterface] = iface
 			}
-			args[initArgEncryptInterface] = iface
 		}
 		var err error
 		if sysSettings, err = addENIRules(sysSettings, o.Datapath().LocalNodeAddressing()); err != nil {