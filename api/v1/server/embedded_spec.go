g0odi3z2j/cilium

commit 0ce3683ec99e65a641c6f24c577fd9cb641d5174

datapath: masquerade IPv6 traffic when using iptables masquerading mode
* This commits enables masquerading for IPv6 traffic using iptables when
  running in iptables masquerading mode.
* This resolves asymmetric traffic flows for NodePort services when
  running Cilium with IPv6 enabled. Earlier for IPv6 if a client on
  Node1 sends a request to Node2 NodePort service then the traffic is
  asymmetrically routed. The outgoing packets from Node1 are sent via host
  interface but the reply packets are sent back from the tunnel since
  there is no masquerading for IPv6 traffic.

Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

api/v1/models/masquerading.go
@@ -34,8 +34,14 @@ type Masquerading struct {
 	// Enum: [BPF iptables]
 	Mode string `json:"mode,omitempty"`
 
-	// Any packet sent to IP addr belonging to CIDR will not be SNAT'd
-	SnatExclusionCidr string `json:"snat-exclusion-cidr,omitempty"`
+	// SnatExclusionCIDRv4 exempts SNAT from being performed on any packet sent to
+	// an IPv4 address that belongs to this CIDR.
+	SnatExclusionCidrV4 string `json:"snat-exclusion-cidr-v4,omitempty"`
+
+	// SnatExclusionCIDRv6 exempts SNAT from being performed on any packet sent to
+	// an IPv6 address that belongs to this CIDR.
+	// For IPv6 we only do masquerading in iptables mode.
+	SnatExclusionCidrV6 string `json:"snat-exclusion-cidr-v6,omitempty"`
 }
 
 // Validate validates this masquerading

api/v1/openapi.yaml
@@ -2500,8 +2500,16 @@ definitions:
       ip-masq-agent:
         description: Is BPF ip-masq-agent enabled
         type: boolean
-      snat-exclusion-cidr:
-        description: Any packet sent to IP addr belonging to CIDR will not be SNAT'd
+      snat-exclusion-cidr-v4:
+        description: |-
+          SnatExclusionCIDRv4 exempts SNAT from being performed on any packet sent to
+          an IPv4 address that belongs to this CIDR.
+        type: string
+      snat-exclusion-cidr-v6:
+        description: |-
+          SnatExclusionCIDRv6 exempts SNAT from being performed on any packet sent to
+          an IPv6 address that belongs to this CIDR.
+          For IPv6 we only do masquerading in iptables mode.
         type: string
   ClockSource:
     description: |-

daemon/cmd/status.go
@@ -131,7 +131,11 @@ func (d *Daemon) getMasqueradingStatus() *models.Masquerading {
 	}
 
 	if option.Config.EnableIPv4 {
-		s.SnatExclusionCidr = datapath.RemoteSNATDstAddrExclusionCIDR().String()
+		s.SnatExclusionCidrV4 = datapath.RemoteSNATDstAddrExclusionCIDRv4().String()
+	}
+
+	if option.Config.EnableIPv6 {
+		s.SnatExclusionCidrV6 = datapath.RemoteSNATDstAddrExclusionCIDRv6().String()
 	}
 
 	if option.Config.EnableBPFMasquerade {

api/v1/server/embedded_spec.go
@@ -2903,8 +2903,12 @@ func init() {
             "iptables"
           ]
         },
-        "snat-exclusion-cidr": {
-          "description": "Any packet sent to IP addr belonging to CIDR will not be SNAT'd",
+        "snat-exclusion-cidr-v4": {
+          "description": "SnatExclusionCIDRv4 exempts SNAT from being performed on any packet sent to\nan IPv4 address that belongs to this CIDR.",
+          "type": "string"
+        },
+        "snat-exclusion-cidr-v6": {
+          "description": "SnatExclusionCIDRv6 exempts SNAT from being performed on any packet sent to\nan IPv6 address that belongs to this CIDR.\nFor IPv6 we only do masquerading in iptables mode.",
           "type": "string"
         }
       }
@@ -7009,8 +7013,12 @@ func init() {
             "iptables"
           ]
         },
-        "snat-exclusion-cidr": {
-          "description": "Any packet sent to IP addr belonging to CIDR will not be SNAT'd",
+        "snat-exclusion-cidr-v4": {
+          "description": "SnatExclusionCIDRv4 exempts SNAT from being performed on any packet sent to\nan IPv4 address that belongs to this CIDR.",
+          "type": "string"
+        },
+        "snat-exclusion-cidr-v6": {
+          "description": "SnatExclusionCIDRv6 exempts SNAT from being performed on any packet sent to\nan IPv6 address that belongs to this CIDR.\nFor IPv6 we only do masquerading in iptables mode.",
           "type": "string"
         }
       }

pkg/client/client.go
@@ -409,9 +409,11 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 				status = "BPF"
 			}
 			if sr.KubeProxyReplacement != nil {
+				// When BPF Masquerading is enabled we don't do any masquerading for IPv6
+				// traffic so no SNAT Exclusion IPv6 CIDR is listed in status output.
 				status += fmt.Sprintf("\t[%s]\t%s",
 					strings.Join(sr.KubeProxyReplacement.Devices, ", "),
-					sr.Masquerading.SnatExclusionCidr)
+					sr.Masquerading.SnatExclusionCidrV4)
 			}
 
 		} else if sr.Masquerading.Mode == models.MasqueradingModeIptables {

pkg/datapath/config.go
@@ -123,14 +123,21 @@ type ConfigWriter interface {
 	WriteEndpointConfig(w io.Writer, cfg EndpointConfiguration) error
 }
 
-// RemoteSNATDstAddrExclusionCIDR returns a CIDR for SNAT exclusion. Any
+// RemoteSNATDstAddrExclusionCIDRv4 returns a CIDR for SNAT exclusion. Any
 // packet sent from a local endpoint to an IP address belonging to the CIDR
 // should not be SNAT'd.
-func RemoteSNATDstAddrExclusionCIDR() *cidr.CIDR {
+func RemoteSNATDstAddrExclusionCIDRv4() *cidr.CIDR {
 	if c := option.Config.IPv4NativeRoutingCIDR(); c != nil {
 		// native-routing-cidr is set, so use it
 		return c
 	}
 
 	return node.GetIPv4AllocRange()
+}
+
+// RemoteSNATDstAddrExclusionCIDRv6 returns a IPv6 CIDR for SNAT exclusion. Any
+// packet sent from a local endpoint to an IP address belonging to the CIDR
+// should not be SNAT'd.
+func RemoteSNATDstAddrExclusionCIDRv6() *cidr.CIDR {
+	return node.GetIPv6AllocRange()
 }

pkg/datapath/iptables/iptables.go
@@ -314,6 +314,7 @@ var ciliumChains = []customChain{
 		table:      "nat",
 		hook:       "POSTROUTING",
 		feederArgs: []string{""},
+		ipv6:       true,
 	},
 	{
 		name:       ciliumOutputNatChain,
@@ -469,7 +470,7 @@ func (m *IptablesManager) RemoveRules(quiet bool) {
 
 	// Set of tables that have had ip6tables rules in any Cilium version
 	if m.haveIp6tables {
-		tables6 := []string{"mangle", "raw", "filter"}
+		tables6 := []string{"nat", "mangle", "raw", "filter"}
 		for _, t := range tables6 {
 			m.removeCiliumRules(t, "ip6tables", ciliumPrefix)
 		}
@@ -1024,6 +1025,11 @@ func (m *IptablesManager) installMasqueradeRules(prog, ifName, localDeliveryInte
 		}
 	}
 
+	loopbackAddr := "127.0.0.1"
+	if prog == "ip6tables" {
+		loopbackAddr = "::1"
+	}
+
 	// Masquerade all traffic from the host into local
 	// endpoints if the source is 127.0.0.1. This is
 	// required to force replies out of the endpoint's
@@ -1036,9 +1042,9 @@ func (m *IptablesManager) installMasqueradeRules(prog, ifName, localDeliveryInte
 		m.waitArgs,
 		"-t", "nat",
 		"-A", ciliumPostNatChain,
-		"-s", "127.0.0.1",
+		"-s", loopbackAddr,
 		"-o", localDeliveryInterface,
-		"-m", "comment", "--comment", "cilium host->cluster from 127.0.0.1 masquerade",
+		"-m", "comment", "--comment", "cilium host->cluster from "+loopbackAddr+" masquerade",
 		"-j", "SNAT", "--to-source", hostMasqueradeIP), false); err != nil {
 		return err
 	}
@@ -1183,6 +1189,32 @@ func (m *IptablesManager) InstallRules(ifName string) error {
 		); err != nil {
 			return err
 		}
+	}
+
+	if option.Config.EnableIPv6 {
+		// See comment above.
+		matchFromProxy := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkIsProxy, linux_defaults.MagicMarkProxyMask)
+		markAsFromHost := fmt.Sprintf("%#08x/%#08x", linux_defaults.MagicMarkHost, linux_defaults.MagicMarkHostMask)
+		if err := runProg("ip6tables", append(
+			m.waitArgs,
+			"-t", "filter",
+			"-A", ciliumOutputChain,
+			"-m", "mark", "!", "--mark", matchFromIPSecDecrypt, // Don't match ipsec traffic
+			"-m", "mark", "!", "--mark", matchFromIPSecEncrypt, // Don't match ipsec traffic
+			"-m", "mark", "!", "--mark", matchFromProxy, // Don't match proxy traffic
+			"-m", "comment", "--comment", "cilium: host->any mark as from host",
+			"-j", "MARK", "--set-xmark", markAsFromHost), false); err != nil {
+			return err
+		}
+
+		if err := m.installMasqueradeRules("ip6tables", ifName, localDeliveryInterface,
+			datapath.RemoteSNATDstAddrExclusionCIDRv6().String(),
+			node.GetIPv6AllocRange().String(),
+			node.GetHostMasqueradeIPv6().String(),
+		); err != nil {
+			return err
+		}
+
 	}
 
 	// AWS ENI requires to mark packets ingressing on the primary interface

pkg/datapath/linux/config/config.go
@@ -424,7 +424,7 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 
 		if option.Config.EnableBPFMasquerade && option.Config.EnableIPv4 {
 			cDefinesMap["ENABLE_MASQUERADE"] = "1"
-			cidr := datapath.RemoteSNATDstAddrExclusionCIDR()
+			cidr := datapath.RemoteSNATDstAddrExclusionCIDRv4()
 			cDefinesMap["IPV4_SNAT_EXCLUSION_DST_CIDR"] =
 				fmt.Sprintf("%#x", byteorder.HostSliceToNetwork(cidr.IP, reflect.Uint32).(uint32))
 			ones, _ := cidr.Mask.Size()

commit d626baafa2c6920f122f222ee9e1ffe78830fe1a

api: Fix status compatibility: SNAT exclusion CIDR
This partially reverts 0ce3683ec99e ("datapath: masquerade IPv6 traffic
when using iptables masquerading mode") to re-estasblish API
compatibility of the status call so it will work correctly when a <= 1.9
client queries the status API.

Fixes: 0ce3683ec99e ("datapath: masquerade IPv6 traffic when using iptables masquerading mode")
Signed-off-by: Thomas Graf <thomas@cilium.io>

api/v1/models/masquerading.go
@@ -37,6 +37,9 @@ type Masquerading struct {
 	// Enum: [BPF iptables]
 	Mode string `json:"mode,omitempty"`
 
+	// This field is obsolete, please use snat-exclusion-cidr-v4 or snat-exclusion-cidr-v6.
+	SnatExclusionCidr string `json:"snat-exclusion-cidr,omitempty"`
+
 	// SnatExclusionCIDRv4 exempts SNAT from being performed on any packet sent to
 	// an IPv4 address that belongs to this CIDR.
 	SnatExclusionCidrV4 string `json:"snat-exclusion-cidr-v4,omitempty"`

api/v1/openapi.yaml
@@ -2548,6 +2548,10 @@ definitions:
       ip-masq-agent:
         description: Is BPF ip-masq-agent enabled
         type: boolean
+      snat-exclusion-cidr:
+        description: |-
+          This field is obsolete, please use snat-exclusion-cidr-v4 or snat-exclusion-cidr-v6.
+        type: string
       snat-exclusion-cidr-v4:
         description: |-
           SnatExclusionCIDRv4 exempts SNAT from being performed on any packet sent to

daemon/cmd/status.go
@@ -137,6 +137,9 @@ func (d *Daemon) getMasqueradingStatus() *models.Masquerading {
 	}
 
 	if option.Config.EnableIPv4 {
+		// SnatExclusionCidr is the legacy field, continue to provide
+		// it for the time being
+		s.SnatExclusionCidr = datapath.RemoteSNATDstAddrExclusionCIDRv4().String()
 		s.SnatExclusionCidrV4 = datapath.RemoteSNATDstAddrExclusionCIDRv4().String()
 	}
 

api/v1/server/embedded_spec.go
@@ -2957,6 +2957,10 @@ func init() {
             "iptables"
           ]
         },
+        "snat-exclusion-cidr": {
+          "description": "This field is obsolete, please use snat-exclusion-cidr-v4 or snat-exclusion-cidr-v6.",
+          "type": "string"
+        },
         "snat-exclusion-cidr-v4": {
           "description": "SnatExclusionCIDRv4 exempts SNAT from being performed on any packet sent to\nan IPv4 address that belongs to this CIDR.",
           "type": "string"
@@ -7137,6 +7141,10 @@ func init() {
             "BPF",
             "iptables"
           ]
+        },
+        "snat-exclusion-cidr": {
+          "description": "This field is obsolete, please use snat-exclusion-cidr-v4 or snat-exclusion-cidr-v6.",
+          "type": "string"
         },
         "snat-exclusion-cidr-v4": {
           "description": "SnatExclusionCIDRv4 exempts SNAT from being performed on any packet sent to\nan IPv4 address that belongs to this CIDR.",