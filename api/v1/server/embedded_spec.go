g0odi3z2j/cilium

commit 4576e0f1f492cec790761e0e1cca3047bd897684

daemon: add separate flags to enable masquerading for ipv4 and ipv6 traffic
* This commit adds a two new flag `enable-ipv6-masquerade` and
  `enable-ipv4-masquerade` to enable masquerading for ipv6 and ipv4
  traffic respectivily.

* Deamon flag `--masquerade` is now deprecated in favour of the new
  `--enable-ipv4-masquerade` flag. The flag will be removed in 1.11
  cycle.

* Enabling IPv6 masquerading and BPF masquerading simulatneously will
  result in no masquerading as BPF masq is currently not supported for
  IPv6 traffic.

Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

api/v1/models/daemon_configuration_status.go
@@ -49,8 +49,8 @@ type DaemonConfigurationStatus struct {
 	// kvstore configuration
 	KvstoreConfiguration *KVstoreConfiguration `json:"kvstoreConfiguration,omitempty"`
 
-	// Status of masquerading feature
-	Masquerade bool `json:"masquerade,omitempty"`
+	// masquerade
+	Masquerade *DaemonConfigurationStatusMasquerade `json:"masquerade,omitempty"`
 
 	// Status of the node monitor
 	NodeMonitor *MonitorStatus `json:"nodeMonitor,omitempty"`
@@ -83,6 +83,10 @@ func (m *DaemonConfigurationStatus) Validate(formats strfmt.Registry) error {
 	}
 
 	if err := m.validateKvstoreConfiguration(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateMasquerade(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -186,6 +190,24 @@ func (m *DaemonConfigurationStatus) validateKvstoreConfiguration(formats strfmt.
 	return nil
 }
 
+func (m *DaemonConfigurationStatus) validateMasquerade(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Masquerade) { // not required
+		return nil
+	}
+
+	if m.Masquerade != nil {
+		if err := m.Masquerade.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("masquerade")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (m *DaemonConfigurationStatus) validateNodeMonitor(formats strfmt.Registry) error {
 
 	if swag.IsZero(m.NodeMonitor) { // not required
@@ -233,6 +255,41 @@ func (m *DaemonConfigurationStatus) MarshalBinary() ([]byte, error) {
 // UnmarshalBinary interface implementation
 func (m *DaemonConfigurationStatus) UnmarshalBinary(b []byte) error {
 	var res DaemonConfigurationStatus
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}
+
+// DaemonConfigurationStatusMasquerade Status of masquerading feature
+//
+// swagger:model DaemonConfigurationStatusMasquerade
+type DaemonConfigurationStatusMasquerade struct {
+
+	// Status of masquerading for IPv4 traffic
+	IPV4 bool `json:"ipv4,omitempty"`
+
+	// Status of masquerading for IPv6 traffic
+	IPV6 bool `json:"ipv6,omitempty"`
+}
+
+// Validate validates this daemon configuration status masquerade
+func (m *DaemonConfigurationStatusMasquerade) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *DaemonConfigurationStatusMasquerade) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *DaemonConfigurationStatusMasquerade) UnmarshalBinary(b []byte) error {
+	var res DaemonConfigurationStatusMasquerade
 	if err := swag.ReadJSON(b, &res); err != nil {
 		return err
 	}

api/v1/models/masquerading.go
@@ -24,8 +24,8 @@ import (
 // swagger:model Masquerading
 type Masquerading struct {
 
-	// Is masquerading enabled
-	Enabled bool `json:"enabled,omitempty"`
+	// enabled
+	Enabled *MasqueradingEnabled `json:"enabled,omitempty"`
 
 	// Is BPF ip-masq-agent enabled
 	IPMasqAgent bool `json:"ip-masq-agent,omitempty"`
@@ -48,6 +48,10 @@ type Masquerading struct {
 func (m *Masquerading) Validate(formats strfmt.Registry) error {
 	var res []error
 
+	if err := m.validateEnabled(formats); err != nil {
+		res = append(res, err)
+	}
+
 	if err := m.validateMode(formats); err != nil {
 		res = append(res, err)
 	}
@@ -55,6 +59,24 @@ func (m *Masquerading) Validate(formats strfmt.Registry) error {
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
+	return nil
+}
+
+func (m *Masquerading) validateEnabled(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Enabled) { // not required
+		return nil
+	}
+
+	if m.Enabled != nil {
+		if err := m.Enabled.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("enabled")
+			}
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -112,6 +134,41 @@ func (m *Masquerading) MarshalBinary() ([]byte, error) {
 // UnmarshalBinary interface implementation
 func (m *Masquerading) UnmarshalBinary(b []byte) error {
 	var res Masquerading
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}
+
+// MasqueradingEnabled Is masquerading enabled
+//
+// swagger:model MasqueradingEnabled
+type MasqueradingEnabled struct {
+
+	// Is masquerading enabled for IPv4 traffic
+	IPV4 bool `json:"ipv4,omitempty"`
+
+	// Is masquerading enabled for IPv6 traffic
+	IPV6 bool `json:"ipv6,omitempty"`
+}
+
+// Validate validates this masquerading enabled
+func (m *MasqueradingEnabled) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *MasqueradingEnabled) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *MasqueradingEnabled) UnmarshalBinary(b []byte) error {
+	var res MasqueradingEnabled
 	if err := swag.ReadJSON(b, &res); err != nil {
 		return err
 	}

api/v1/models/zz_generated.deepcopy.go
@@ -598,6 +598,11 @@ func (in *L4Policy) DeepCopy() *L4Policy {
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *Masquerading) DeepCopyInto(out *Masquerading) {
 	*out = *in
+	if in.Enabled != nil {
+		in, out := &in.Enabled, &out.Enabled
+		*out = new(MasqueradingEnabled)
+		**out = **in
+	}
 	return
 }
 
@@ -874,7 +879,7 @@ func (in *StatusResponse) DeepCopyInto(out *StatusResponse) {
 	if in.Masquerading != nil {
 		in, out := &in.Masquerading, &out.Masquerading
 		*out = new(Masquerading)
-		**out = **in
+		(*in).DeepCopyInto(*out)
 	}
 	if in.NodeMonitor != nil {
 		in, out := &in.NodeMonitor, &out.NodeMonitor

api/v1/openapi.yaml
@@ -2039,7 +2039,14 @@ definitions:
         type: string
       masquerade:
         description: Status of masquerading feature
-        type: boolean
+        type: object
+        properties:
+          ipv4:
+            description: Status of masquerading for IPv4 traffic
+            type: boolean
+          ipv6:
+            description: Status of masquerading for IPv6 traffic
+            type: boolean
   DatapathMode:
     description: Datapath mode
     type: string
@@ -2491,7 +2498,14 @@ definitions:
     properties:
       enabled:
         description: Is masquerading enabled
-        type: boolean
+        type: object
+        properties:
+          ipv4:
+            description: Is masquerading enabled for IPv4 traffic
+            type: boolean
+          ipv6:
+            description: Is masquerading enabled for IPv6 traffic
+            type: boolean
       mode:
         type: string
         enum:

daemon/cmd/config.go
@@ -132,8 +132,11 @@ func (h *getConfig) Handle(params GetConfigParams) middleware.Responder {
 			MasterDeviceIndex: int64(option.Config.Ipvlan.MasterDeviceIndex),
 			OperationMode:     option.Config.Ipvlan.OperationMode,
 		},
-		IpamMode:   option.Config.IPAM,
-		Masquerade: option.Config.Masquerade,
+		IpamMode: option.Config.IPAM,
+		Masquerade: &models.DaemonConfigurationStatusMasquerade{
+			IPV4: option.Config.EnableIPv4Masquerade,
+			IPV6: option.Config.EnableIPv6Masquerade,
+		},
 	}
 
 	cfg := &models.DaemonConfiguration{

daemon/cmd/status.go
@@ -123,10 +123,13 @@ func (d *Daemon) getK8sStatus() *models.K8sStatus {
 
 func (d *Daemon) getMasqueradingStatus() *models.Masquerading {
 	s := &models.Masquerading{
-		Enabled: option.Config.Masquerade,
+		Enabled: &models.MasqueradingEnabled{
+			IPV4: option.Config.EnableIPv4Masquerade,
+			IPV6: option.Config.EnableIPv6Masquerade,
+		},
 	}
 
-	if !option.Config.Masquerade {
+	if !option.Config.EnableIPv4Masquerade && !option.Config.EnableIPv6Masquerade {
 		return s
 	}
 

pkg/client/client.go
@@ -400,24 +400,37 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 
 	if sr.Masquerading != nil {
 		var status string
-		if !sr.Masquerading.Enabled {
-			status = "Disabled"
-		} else if sr.Masquerading.Mode == models.MasqueradingModeBPF {
-			if sr.Masquerading.IPMasqAgent {
-				status = "BPF (ip-masq-agent)"
-			} else {
-				status = "BPF"
+
+		enabled := func(enabled bool) string {
+			if enabled {
+				return "Enabled"
 			}
-			if sr.KubeProxyReplacement != nil {
-				// When BPF Masquerading is enabled we don't do any masquerading for IPv6
-				// traffic so no SNAT Exclusion IPv6 CIDR is listed in status output.
-				status += fmt.Sprintf("\t[%s]\t%s",
-					strings.Join(sr.KubeProxyReplacement.Devices, ", "),
-					sr.Masquerading.SnatExclusionCidrV4)
+			return "Disabled"
+		}
+
+		if !sr.Masquerading.Enabled.IPV4 && !sr.Masquerading.Enabled.IPV6 {
+			status = enabled(false)
+		} else {
+			if sr.Masquerading.Mode == models.MasqueradingModeBPF {
+				if sr.Masquerading.IPMasqAgent {
+					status = "BPF (ip-masq-agent)"
+				} else {
+					status = "BPF"
+				}
+				if sr.KubeProxyReplacement != nil {
+					// When BPF Masquerading is enabled we don't do any masquerading for IPv6
+					// traffic so no SNAT Exclusion IPv6 CIDR is listed in status output.
+					status += fmt.Sprintf("\t[%s]\t%s",
+						strings.Join(sr.KubeProxyReplacement.Devices, ", "),
+						sr.Masquerading.SnatExclusionCidrV4)
+				}
+
+			} else if sr.Masquerading.Mode == models.MasqueradingModeIptables {
+				status = "IPTables"
 			}
 
-		} else if sr.Masquerading.Mode == models.MasqueradingModeIptables {
-			status = "IPTables"
+			status = fmt.Sprintf("%s [IPv4: %s, IPv6: %s]", status,
+				enabled(sr.Masquerading.Enabled.IPV4), enabled(sr.Masquerading.Enabled.IPV6))
 		}
 		fmt.Fprintf(w, "Masquerading:\t%s\n", status)
 	}

plugins/cilium-cni/interface.go
@@ -31,6 +31,15 @@ func interfaceAdd(ipConfig *current.IPConfig, ipam *models.IPAMAddressResponse, 
 		return nil
 	}
 
+	var masq bool
+	if ipConfig.Version == "4" {
+		masq = conf.Masquerade.IPV4
+	} else if ipConfig.Version == "6" {
+		masq = conf.Masquerade.IPV6
+	} else {
+		return fmt.Errorf("Invalid IPConfig version: %s", ipConfig.Version)
+	}
+
 	allCIDRs := make([]*net.IPNet, 0, len(ipam.Cidrs))
 	for _, cidrString := range ipam.Cidrs {
 		_, cidr, err := net.ParseCIDR(cidrString)
@@ -46,14 +55,13 @@ func interfaceAdd(ipConfig *current.IPConfig, ipam *models.IPAMAddressResponse, 
 		cidrs = append(cidrs, cidr.String())
 	}
 
-	routingInfo, err := linuxrouting.NewRoutingInfo(ipam.Gateway, cidrs, ipam.MasterMac, conf.Masquerade)
+	routingInfo, err := linuxrouting.NewRoutingInfo(ipam.Gateway, cidrs, ipam.MasterMac, masq)
 	if err != nil {
 		return fmt.Errorf("unable to parse routing info: %v", err)
 	}
 
 	if err := routingInfo.Configure(ipConfig.Address.IP,
-		int(conf.DeviceMTU),
-		conf.Masquerade); err != nil {
+		int(conf.DeviceMTU), masq); err != nil {
 		return fmt.Errorf("unable to install ip rules and routes: %s", err)
 	}
 

commit 64023cae4a1ef340a1e64d2f561cfd392f6c0da6

api: Fix status compatibility: masquerading
This partially reverts 4576e0f1f4 ("daemon: add separate flags to enable
masquerading for ipv4 and ipv6 traffic") to re-estasblish API
compatibility of the status call so it will work correctly when a <= 1.9
client queries the status API.

Fixes: 4576e0f1f4 ("daemon: add separate flags to enable masquerading for ipv4 and ipv6 traffic")
Signed-off-by: Thomas Graf <thomas@cilium.io>

api/v1/models/daemon_configuration_status.go
@@ -53,7 +53,10 @@ type DaemonConfigurationStatus struct {
 	KvstoreConfiguration *KVstoreConfiguration `json:"kvstoreConfiguration,omitempty"`
 
 	// masquerade
-	Masquerade *DaemonConfigurationStatusMasquerade `json:"masquerade,omitempty"`
+	Masquerade bool `json:"masquerade,omitempty"`
+
+	// masquerade protocols
+	MasqueradeProtocols *DaemonConfigurationStatusMasqueradeProtocols `json:"masqueradeProtocols,omitempty"`
 
 	// Status of the node monitor
 	NodeMonitor *MonitorStatus `json:"nodeMonitor,omitempty"`
@@ -89,7 +92,7 @@ func (m *DaemonConfigurationStatus) Validate(formats strfmt.Registry) error {
 		res = append(res, err)
 	}
 
-	if err := m.validateMasquerade(formats); err != nil {
+	if err := m.validateMasqueradeProtocols(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -193,16 +196,16 @@ func (m *DaemonConfigurationStatus) validateKvstoreConfiguration(formats strfmt.
 	return nil
 }
 
-func (m *DaemonConfigurationStatus) validateMasquerade(formats strfmt.Registry) error {
+func (m *DaemonConfigurationStatus) validateMasqueradeProtocols(formats strfmt.Registry) error {
 
-	if swag.IsZero(m.Masquerade) { // not required
+	if swag.IsZero(m.MasqueradeProtocols) { // not required
 		return nil
 	}
 
-	if m.Masquerade != nil {
-		if err := m.Masquerade.Validate(formats); err != nil {
+	if m.MasqueradeProtocols != nil {
+		if err := m.MasqueradeProtocols.Validate(formats); err != nil {
 			if ve, ok := err.(*errors.Validation); ok {
-				return ve.ValidateName("masquerade")
+				return ve.ValidateName("masqueradeProtocols")
 			}
 			return err
 		}
@@ -265,10 +268,10 @@ func (m *DaemonConfigurationStatus) UnmarshalBinary(b []byte) error {
 	return nil
 }
 
-// DaemonConfigurationStatusMasquerade Status of masquerading feature
+// DaemonConfigurationStatusMasqueradeProtocols Status of masquerading feature
 //
-// swagger:model DaemonConfigurationStatusMasquerade
-type DaemonConfigurationStatusMasquerade struct {
+// swagger:model DaemonConfigurationStatusMasqueradeProtocols
+type DaemonConfigurationStatusMasqueradeProtocols struct {
 
 	// Status of masquerading for IPv4 traffic
 	IPV4 bool `json:"ipv4,omitempty"`
@@ -277,13 +280,13 @@ type DaemonConfigurationStatusMasquerade struct {
 	IPV6 bool `json:"ipv6,omitempty"`
 }
 
-// Validate validates this daemon configuration status masquerade
-func (m *DaemonConfigurationStatusMasquerade) Validate(formats strfmt.Registry) error {
+// Validate validates this daemon configuration status masquerade protocols
+func (m *DaemonConfigurationStatusMasqueradeProtocols) Validate(formats strfmt.Registry) error {
 	return nil
 }
 
 // MarshalBinary interface implementation
-func (m *DaemonConfigurationStatusMasquerade) MarshalBinary() ([]byte, error) {
+func (m *DaemonConfigurationStatusMasqueradeProtocols) MarshalBinary() ([]byte, error) {
 	if m == nil {
 		return nil, nil
 	}
@@ -291,8 +294,8 @@ func (m *DaemonConfigurationStatusMasquerade) MarshalBinary() ([]byte, error) {
 }
 
 // UnmarshalBinary interface implementation
-func (m *DaemonConfigurationStatusMasquerade) UnmarshalBinary(b []byte) error {
-	var res DaemonConfigurationStatusMasquerade
+func (m *DaemonConfigurationStatusMasqueradeProtocols) UnmarshalBinary(b []byte) error {
+	var res DaemonConfigurationStatusMasqueradeProtocols
 	if err := swag.ReadJSON(b, &res); err != nil {
 		return err
 	}

api/v1/models/masquerading.go
@@ -25,7 +25,10 @@ import (
 type Masquerading struct {
 
 	// enabled
-	Enabled *MasqueradingEnabled `json:"enabled,omitempty"`
+	Enabled bool `json:"enabled,omitempty"`
+
+	// enabled protocols
+	EnabledProtocols *MasqueradingEnabledProtocols `json:"enabledProtocols,omitempty"`
 
 	// Is BPF ip-masq-agent enabled
 	IPMasqAgent bool `json:"ip-masq-agent,omitempty"`
@@ -48,7 +51,7 @@ type Masquerading struct {
 func (m *Masquerading) Validate(formats strfmt.Registry) error {
 	var res []error
 
-	if err := m.validateEnabled(formats); err != nil {
+	if err := m.validateEnabledProtocols(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -62,16 +65,16 @@ func (m *Masquerading) Validate(formats strfmt.Registry) error {
 	return nil
 }
 
-func (m *Masquerading) validateEnabled(formats strfmt.Registry) error {
+func (m *Masquerading) validateEnabledProtocols(formats strfmt.Registry) error {
 
-	if swag.IsZero(m.Enabled) { // not required
+	if swag.IsZero(m.EnabledProtocols) { // not required
 		return nil
 	}
 
-	if m.Enabled != nil {
-		if err := m.Enabled.Validate(formats); err != nil {
+	if m.EnabledProtocols != nil {
+		if err := m.EnabledProtocols.Validate(formats); err != nil {
 			if ve, ok := err.(*errors.Validation); ok {
-				return ve.ValidateName("enabled")
+				return ve.ValidateName("enabledProtocols")
 			}
 			return err
 		}
@@ -141,10 +144,10 @@ func (m *Masquerading) UnmarshalBinary(b []byte) error {
 	return nil
 }
 
-// MasqueradingEnabled Is masquerading enabled
+// MasqueradingEnabledProtocols Is masquerading enabled
 //
-// swagger:model MasqueradingEnabled
-type MasqueradingEnabled struct {
+// swagger:model MasqueradingEnabledProtocols
+type MasqueradingEnabledProtocols struct {
 
 	// Is masquerading enabled for IPv4 traffic
 	IPV4 bool `json:"ipv4,omitempty"`
@@ -153,13 +156,13 @@ type MasqueradingEnabled struct {
 	IPV6 bool `json:"ipv6,omitempty"`
 }
 
-// Validate validates this masquerading enabled
-func (m *MasqueradingEnabled) Validate(formats strfmt.Registry) error {
+// Validate validates this masquerading enabled protocols
+func (m *MasqueradingEnabledProtocols) Validate(formats strfmt.Registry) error {
 	return nil
 }
 
 // MarshalBinary interface implementation
-func (m *MasqueradingEnabled) MarshalBinary() ([]byte, error) {
+func (m *MasqueradingEnabledProtocols) MarshalBinary() ([]byte, error) {
 	if m == nil {
 		return nil, nil
 	}
@@ -167,8 +170,8 @@ func (m *MasqueradingEnabled) MarshalBinary() ([]byte, error) {
 }
 
 // UnmarshalBinary interface implementation
-func (m *MasqueradingEnabled) UnmarshalBinary(b []byte) error {
-	var res MasqueradingEnabled
+func (m *MasqueradingEnabledProtocols) UnmarshalBinary(b []byte) error {
+	var res MasqueradingEnabledProtocols
 	if err := swag.ReadJSON(b, &res); err != nil {
 		return err
 	}

api/v1/models/zz_generated.deepcopy.go
@@ -630,9 +630,9 @@ func (in *L4Policy) DeepCopy() *L4Policy {
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *Masquerading) DeepCopyInto(out *Masquerading) {
 	*out = *in
-	if in.Enabled != nil {
-		in, out := &in.Enabled, &out.Enabled
-		*out = new(MasqueradingEnabled)
+	if in.EnabledProtocols != nil {
+		in, out := &in.EnabledProtocols, &out.EnabledProtocols
+		*out = new(MasqueradingEnabledProtocols)
 		**out = **in
 	}
 	return

api/v1/openapi.yaml
@@ -2065,6 +2065,8 @@ definitions:
         description: Configured IPAM mode
         type: string
       masquerade:
+        type: boolean
+      masqueradeProtocols:
         description: Status of masquerading feature
         type: object
         properties:
@@ -2527,6 +2529,8 @@ definitions:
     type: object
     properties:
       enabled:
+        type: boolean
+      enabledProtocols:
         description: Is masquerading enabled
         type: object
         properties:

daemon/cmd/config.go
@@ -188,8 +188,9 @@ func (h *getConfig) Handle(params GetConfigParams) middleware.Responder {
 			MasterDeviceIndex: int64(option.Config.Ipvlan.MasterDeviceIndex),
 			OperationMode:     option.Config.Ipvlan.OperationMode,
 		},
-		IpamMode: option.Config.IPAM,
-		Masquerade: &models.DaemonConfigurationStatusMasquerade{
+		IpamMode:   option.Config.IPAM,
+		Masquerade: option.Config.EnableIPv4Masquerade || option.Config.EnableIPv6Masquerade,
+		MasqueradeProtocols: &models.DaemonConfigurationStatusMasqueradeProtocols{
 			IPV4: option.Config.EnableIPv4Masquerade,
 			IPV6: option.Config.EnableIPv6Masquerade,
 		},

daemon/cmd/status.go
@@ -125,7 +125,8 @@ func (d *Daemon) getK8sStatus() *models.K8sStatus {
 
 func (d *Daemon) getMasqueradingStatus() *models.Masquerading {
 	s := &models.Masquerading{
-		Enabled: &models.MasqueradingEnabled{
+		Enabled: option.Config.EnableIPv4Masquerade || option.Config.EnableIPv6Masquerade,
+		EnabledProtocols: &models.MasqueradingEnabledProtocols{
 			IPV4: option.Config.EnableIPv4Masquerade,
 			IPV6: option.Config.EnableIPv6Masquerade,
 		},

pkg/client/client.go
@@ -405,7 +405,7 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 			return "Disabled"
 		}
 
-		if !sr.Masquerading.Enabled.IPV4 && !sr.Masquerading.Enabled.IPV6 {
+		if !sr.Masquerading.EnabledProtocols.IPV4 && !sr.Masquerading.EnabledProtocols.IPV6 {
 			status = enabled(false)
 		} else {
 			if sr.Masquerading.Mode == models.MasqueradingModeBPF {
@@ -434,7 +434,7 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 			}
 
 			status = fmt.Sprintf("%s [IPv4: %s, IPv6: %s]", status,
-				enabled(sr.Masquerading.Enabled.IPV4), enabled(sr.Masquerading.Enabled.IPV6))
+				enabled(sr.Masquerading.EnabledProtocols.IPV4), enabled(sr.Masquerading.EnabledProtocols.IPV6))
 		}
 		fmt.Fprintf(w, "Masquerading:\t%s\n", status)
 	}

plugins/cilium-cni/interface.go
@@ -33,9 +33,9 @@ func interfaceAdd(ipConfig *current.IPConfig, ipam *models.IPAMAddressResponse, 
 
 	var masq bool
 	if ipConfig.Version == "4" {
-		masq = conf.Masquerade.IPV4
+		masq = conf.MasqueradeProtocols.IPV4
 	} else if ipConfig.Version == "6" {
-		masq = conf.Masquerade.IPV6
+		masq = conf.MasqueradeProtocols.IPV6
 	} else {
 		return fmt.Errorf("Invalid IPConfig version: %s", ipConfig.Version)
 	}

api/v1/server/embedded_spec.go
@@ -1790,6 +1790,9 @@ func init() {
           "$ref": "#/definitions/KVstoreConfiguration"
         },
         "masquerade": {
+          "type": "boolean"
+        },
+        "masqueradeProtocols": {
           "description": "Status of masquerading feature",
           "type": "object",
           "properties": {
@@ -2927,6 +2930,9 @@ func init() {
       "type": "object",
       "properties": {
         "enabled": {
+          "type": "boolean"
+        },
+        "enabledProtocols": {
           "description": "Is masquerading enabled",
           "type": "object",
           "properties": {
@@ -5737,6 +5743,9 @@ func init() {
           "$ref": "#/definitions/KVstoreConfiguration"
         },
         "masquerade": {
+          "type": "boolean"
+        },
+        "masqueradeProtocols": {
           "description": "Status of masquerading feature",
           "type": "object",
           "properties": {
@@ -5764,7 +5773,7 @@ func init() {
         }
       }
     },
-    "DaemonConfigurationStatusMasquerade": {
+    "DaemonConfigurationStatusMasqueradeProtocols": {
       "description": "Status of masquerading feature",
       "type": "object",
       "properties": {
@@ -7102,6 +7111,9 @@ func init() {
       "type": "object",
       "properties": {
         "enabled": {
+          "type": "boolean"
+        },
+        "enabledProtocols": {
           "description": "Is masquerading enabled",
           "type": "object",
           "properties": {
@@ -7136,7 +7148,7 @@ func init() {
         }
       }
     },
-    "MasqueradingEnabled": {
+    "MasqueradingEnabledProtocols": {
       "description": "Is masquerading enabled",
       "type": "object",
       "properties": {