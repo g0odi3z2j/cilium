g0odi3z2j/cilium

commit 8259b3ca8d7e76f732a37a2f99c5e83afaa1d7a2

bpf: do not enable host routing when kpr is disabled
Given we rely on this for the case where kpr is enabled, we must
strictly disable host routing for the case when not.

Fixes: 9046485fcf10 ("bpf: optimize datapath through host netns")
Reported-by: Martynas Pumputis <m@lambda.lt>
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

daemon/cmd/kube_proxy_replacement.go
@@ -47,16 +47,17 @@ func initKubeProxyReplacementOptions() (strict bool) {
 	}
 
 	if option.Config.KubeProxyReplacement == option.KubeProxyReplacementDisabled {
-		log.Infof("Auto-disabling %q, %q, %q, %q, %q features",
+		log.Infof("Auto-disabling %q, %q, %q, %q, %q features and falling back to %q",
 			option.EnableNodePort, option.EnableExternalIPs,
 			option.EnableHostReachableServices, option.EnableHostPort,
-			option.EnableSessionAffinity)
+			option.EnableSessionAffinity, option.EnableHostLegacyRouting)
 
 		disableNodePort()
 		option.Config.EnableHostReachableServices = false
 		option.Config.EnableHostServicesTCP = false
 		option.Config.EnableHostServicesUDP = false
 		option.Config.EnableSessionAffinity = false
+		option.Config.EnableHostLegacyRouting = true
 
 		return
 	}

commit 054c6393ba67ec1d019517361e978bf00fdc54f6

daemon: Fix ordering of BPF host routing detection
Fix a bug when NodePort BPF is disabled after the device
detection, and BPF host routing is kept enabled.

Fixes: 8259b3ca ("bpf: do not enable host routing when kpr is disabled")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/cmd/kube_proxy_replacement.go
@@ -290,40 +290,6 @@ func initKubeProxyReplacementOptions() (strict bool) {
 		}
 	}
 
-	if !option.Config.EnableHostLegacyRouting {
-		msg := ""
-		switch {
-		case !option.Config.EnableNodePort:
-			msg = fmt.Sprintf("BPF host routing requires %s.", option.EnableNodePort)
-		// If we chain CNIs then all bets are off.
-		case option.Config.IsFlannelMasterDeviceSet():
-			msg = fmt.Sprintf("BPF host routing is incompatible with %s.", option.FlannelMasterDevice)
-		// Needs host stack for packet handling.
-		case option.Config.EnableEndpointRoutes:
-			msg = fmt.Sprintf("BPF host routing is incompatible with %s.", option.EnableEndpointRoutes)
-		case option.Config.EnableIPSec:
-			msg = fmt.Sprintf("BPF host routing is incompatible with %s.", option.EnableIPSecName)
-		// Non-BPF masquerade requires netfilter and hence CT.
-		case (option.Config.EnableIPv4Masquerade || option.Config.EnableIPv6Masquerade) &&
-			!option.Config.EnableBPFMasquerade:
-			msg = fmt.Sprintf("BPF host routing requires %s.", option.EnableBPFMasquerade)
-		default:
-			foundNeigh := false
-			foundPeer := false
-			if h := probesManager.GetHelpers("sched_cls"); h != nil {
-				_, foundNeigh = h["bpf_redirect_neigh"]
-				_, foundPeer = h["bpf_redirect_peer"]
-			}
-			if !foundNeigh || !foundPeer {
-				msg = fmt.Sprintf("BPF host routing requires kernel 5.10 or newer.")
-			}
-		}
-		if msg != "" {
-			option.Config.EnableHostLegacyRouting = true
-			log.Infof("%s Falling back to legacy host routing (%s=true).", msg, option.EnableHostLegacyRouting)
-		}
-	}
-
 	if option.Config.EnableNodePort {
 		if option.Config.Tunnel != option.TunnelDisabled &&
 			option.Config.NodePortMode != option.NodePortModeSNAT {
@@ -448,6 +414,43 @@ func finishKubeProxyReplacementInit(isKubeProxyReplacementStrict bool) {
 	}
 
 	// After this point, BPF NodePort should not be disabled
+
+	if !option.Config.EnableHostLegacyRouting {
+		msg := ""
+		switch {
+		case !option.Config.EnableNodePort:
+			msg = fmt.Sprintf("BPF host routing requires %s.", option.EnableNodePort)
+		// If we chain CNIs then all bets are off.
+		case option.Config.IsFlannelMasterDeviceSet():
+			msg = fmt.Sprintf("BPF host routing is incompatible with %s.", option.FlannelMasterDevice)
+		case option.Config.Tunnel != option.TunnelDisabled:
+			msg = fmt.Sprintf("BPF host routing is only available in native routing mode.")
+		// Needs host stack for packet handling.
+		case option.Config.EnableEndpointRoutes:
+			msg = fmt.Sprintf("BPF host routing is incompatible with %s.", option.EnableEndpointRoutes)
+		case option.Config.EnableIPSec:
+			msg = fmt.Sprintf("BPF host routing is incompatible with %s.", option.EnableIPSecName)
+		// Non-BPF masquerade requires netfilter and hence CT.
+		case (option.Config.EnableIPv4Masquerade || option.Config.EnableIPv6Masquerade) &&
+			!option.Config.EnableBPFMasquerade:
+			msg = fmt.Sprintf("BPF host routing requires %s.", option.EnableBPFMasquerade)
+		default:
+			probesManager := probes.NewProbeManager()
+			foundNeigh := false
+			foundPeer := false
+			if h := probesManager.GetHelpers("sched_cls"); h != nil {
+				_, foundNeigh = h["bpf_redirect_neigh"]
+				_, foundPeer = h["bpf_redirect_peer"]
+			}
+			if !foundNeigh || !foundPeer {
+				msg = fmt.Sprintf("BPF host routing requires kernel 5.10 or newer.")
+			}
+		}
+		if msg != "" {
+			option.Config.EnableHostLegacyRouting = true
+			log.Infof("%s Falling back to legacy host routing (%s=true).", msg, option.EnableHostLegacyRouting)
+		}
+	}
 
 	if option.Config.NodePortAcceleration != option.NodePortAccelerationDisabled {
 		if option.Config.XDPDevice != "undefined" &&