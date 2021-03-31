g0odi3z2j/cilium

commit ce36353153f2c6d18a3e4af3e01f9899887fef7f

ipam: Fix ENI IPAM on smaller instance types
This commit fixes the ENI IPAM mode when the instance types are very
limited [1].

The problem was that Cilium by default was attempting to allocate 8 IPs
(PreAllocate). However, on smaller instance types such as "t3a.micro" or
similar, the maximum number of IPs that can be attached to one ENI
device is 4. On top of that, Cilium by default does not use eth0 as an
ENI device (default interface index is 1), which could further restrict
itself from having enough ENIs for pods in the cluster.

This commit fixes this by checking the instance type limits and
adjusting the PreAllocate value. Additionally, we likely also need to
include eth0 as an ENI device to give more buffer for Cilium to allocate
IPs by setting the first interface index to 0.

[1]:
https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-eni.html?shortFooter=true#AvailableIpPerENI

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipam/node.go
@@ -20,12 +20,15 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/cilium/cilium/pkg/aws/eni/limits"
 	"github.com/cilium/cilium/pkg/defaults"
+	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/math"
+	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/trigger"
 
 	"github.com/sirupsen/logrus"
@@ -741,7 +744,7 @@ func (n *Node) syncToAPIServer() (err error) {
 		scopedLog.WithField("poolSize", len(node.Spec.IPAM.Pool)).Debug("Updating node in apiserver")
 
 		if node.Spec.IPAM.PreAllocate == 0 {
-			node.Spec.IPAM.PreAllocate = defaults.IPAMPreAllocation
+			adjustPreAllocateIfNeeded(node)
 		}
 
 		origNode, node, err = n.update(origNode, node, retry, false)
@@ -815,4 +818,32 @@ func (n *Node) update(
 	}
 
 	return origNode, node, err
+}
+
+// adjustPreAllocateIfNeeded adjusts IPAM values depending on the instance
+// type. This is needed when the instance type is on the smaller side which
+// requires us to lower the PreAllocate value and include eth0 as an ENI device
+// because the instance type does not allow for additional ENIs to be attached
+// (hence limited).
+//
+// For now, this function is only needed in ENI IPAM mode. In the future, this
+// may also be needed for Azure IPAM as well.
+func adjustPreAllocateIfNeeded(node *v2.CiliumNode) {
+	if option.Config.IPAM != ipamOption.IPAMENI {
+		return
+	}
+
+	// Auto set the PreAllocate to the default value and we'll determine if we
+	// need to adjust it below.
+	node.Spec.IPAM.PreAllocate = defaults.IPAMPreAllocation
+
+	if lim, ok := limits.Get(node.Spec.ENI.InstanceType); ok {
+		max := lim.Adapters * lim.IPv4
+		if node.Spec.IPAM.PreAllocate > max {
+			node.Spec.IPAM.PreAllocate = max
+
+			var i int = 0
+			node.Spec.ENI.FirstInterfaceIndex = &i // Include eth0
+		}
+	}
 }

pkg/nodediscovery/nodediscovery.go
@@ -19,6 +19,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/cilium/cilium/pkg/aws/eni/limits"
 	eniTypes "github.com/cilium/cilium/pkg/aws/eni/types"
 	"github.com/cilium/cilium/pkg/aws/metadata"
 	azureTypes "github.com/cilium/cilium/pkg/azure/types"
@@ -410,8 +411,15 @@ func (n *NodeDiscovery) mutateNodeResource(nodeResource *ciliumv2.CiliumNode) {
 			log.WithError(err).Fatal("Unable to retrieve InstanceID of own EC2 instance")
 		}
 
+		// It is important to determine the interface index here because this
+		// function (mutateNodeResource) will be called when the agent is first
+		// coming up and is initializing the IPAM layer (CRD allocator in this
+		// case). Later on, the Operator will adjust this value based on the
+		// PreAllocate value, so to ensure that the agent and the Operator are
+		// not conflicting with each other, we must have similar logic to
+		// determine the appropriate value to place inside the resource.
 		nodeResource.Spec.ENI.VpcID = vpcID
-		nodeResource.Spec.ENI.FirstInterfaceIndex = getInt(defaults.ENIFirstInterfaceIndex)
+		nodeResource.Spec.ENI.FirstInterfaceIndex = determineFirstInterfaceIndex(instanceType)
 
 		if c := n.NetConf; c != nil {
 			if c.IPAM.MinAllocate != 0 {
@@ -475,4 +483,36 @@ func (n *NodeDiscovery) mutateNodeResource(nodeResource *ciliumv2.CiliumNode) {
 			}
 		}
 	}
+}
+
+// determineFirstInterfaceIndex determines the appropriate default interface
+// index for the ENI IPAM mode. The interface index is stored inside the
+// CiliumNode resource. It specifies which device offset (ENI) to start
+// assigning IPs to. It is important to seed the CiliumNode resource with the
+// appropriate value, otherwise pods will fail to come up because they won't
+// have an IP assigned, because the instance limits (depending on the instance
+// type) have a maximum threshold. See
+// Documentation/concepts/networking/ipam/eni.rst for more details on this
+// value.
+//
+// This value is also ensured to stay in place using similar logic in
+// adjustPreAllocateIfNeeded(), inside
+// github.com/cilium/cilium/pkg/ipam.(*Node).syncToAPIServer().
+func determineFirstInterfaceIndex(instanceType string) *int {
+	if option.Config.IPAM != ipamOption.IPAMENI {
+		return nil
+	}
+
+	// Fallback to default value if we determine below that the instance limits
+	// do not require us to adjust the interface index.
+	idx := defaults.ENIFirstInterfaceIndex
+
+	if l, ok := limits.Get(instanceType); ok {
+		max := l.Adapters * l.IPv4
+		if defaults.IPAMPreAllocation > max {
+			idx = 0 // Include eth0
+		}
+	}
+
+	return &idx
 }

commit 6a783da13211fb7bf86b267ce65725a39e17f879

ipam, nodediscovery: Correct logic for PreAllocate
This commit effectively reverts ce36353153 ("ipam: Fix ENI IPAM on
smaller instance types") as the majority of the logic it implemented is
no longer needed, since the default for FirstInterfaceIndex has been
changed to 0. While we're at it, correct the logic for computing the
PreAllocate value which is vital for ENI (and Azure) mode.

This should allow all instance types to be used with Cilium, including
the smallest ones.

Fixes: ce36353153 ("ipam: Fix ENI IPAM on smaller instance types")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/aws/eni/node.go
@@ -20,10 +20,13 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/aws/smithy-go"
+	"github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/aws/eni/limits"
 	eniTypes "github.com/cilium/cilium/pkg/aws/eni/types"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/ipam"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
@@ -550,4 +553,61 @@ func (n *Node) GetMaximumAllocatableIPv4() int {
 
 	// Return the maximum amount of IP addresses allocatable on the instance
 	return (limits.Adapters - firstInterfaceIndex) * limits.IPv4
+}
+
+var adviseOperatorFlagOnce sync.Once
+
+// GetMinimumAllocatableIPv4 returns the minimum amount of IPv4 addresses that
+// must be allocated to the instance.
+func (n *Node) GetMinimumAllocatableIPv4() int {
+	n.mutex.RLock()
+	defer n.mutex.RUnlock()
+
+	minimum := defaults.IPAMPreAllocation
+
+	if n.k8sObj == nil || n.k8sObj.Spec.ENI.FirstInterfaceIndex == nil {
+		n.loggerLocked().WithFields(logrus.Fields{
+			"adaptors-limit": "unknown",
+			"pre-allocate":   minimum,
+		}).Warning("Could not determine first-interface-index, falling back to default pre-allocate value")
+		return minimum
+	}
+
+	index := *n.k8sObj.Spec.ENI.FirstInterfaceIndex
+
+	// In ENI mode, we must adjust the PreAllocate value based on the instance
+	// type. An adjustment is necessary when the number of possible IPs
+	// corresponding to the instance type limit is smaller than the default
+	// PreAllocate value. Otherwise, we fallback to the default PreAllocate.
+	//
+	// If we don't adjust the PreAllocate value, then it would be impossible to
+	// allocate IPs for smaller instance types because the PreAllocate would
+	// exceed the maximum possible number of IPs per instance.
+
+	limits, limitsAvailable := n.getLimitsLocked()
+	if !limitsAvailable {
+		adviseOperatorFlagOnce.Do(func() {
+			n.loggerLocked().WithFields(logrus.Fields{
+				"instance-type": n.k8sObj.Spec.ENI.InstanceType,
+			}).Warningf(
+				"Unable to find limits for instance type, consider setting --%s=true on the Operator",
+				option.UpdateEC2AdapterLimitViaAPI,
+			)
+		})
+
+		n.loggerLocked().WithFields(logrus.Fields{
+			"adaptors-limit":        "unknown",
+			"first-interface-index": index,
+			"pre-allocate":          minimum,
+		}).Warning("Could not determine instance limits, falling back to default pre-allocate value")
+		return minimum
+	}
+
+	// We cannot allocate any IPs if this is the case because all the ENIs will
+	// be skipped.
+	if index >= limits.Adapters {
+		return 0
+	}
+
+	return math.IntMin(minimum, (limits.Adapters-index)*limits.IPv4)
 }

pkg/azure/ipam/node.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 
 	"github.com/cilium/cilium/pkg/azure/types"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/ipam"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
@@ -193,4 +194,10 @@ func (n *Node) GetMaximumAllocatableIPv4() int {
 	// An Azure node can allocate up to 256 private IP addresses
 	// source: https://github.com/MicrosoftDocs/azure-docs/blob/master/includes/azure-virtual-network-limits.md#networking-limits---azure-resource-manager
 	return types.InterfaceAddressLimit
+}
+
+// GetMinimumAllocatableIPv4 returns the minimum amount of IPv4 addresses that
+// must be allocated to the instance.
+func (n *Node) GetMinimumAllocatableIPv4() int {
+	return defaults.IPAMPreAllocation
 }

pkg/ipam/node.go
@@ -20,15 +20,12 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/cilium/cilium/pkg/aws/eni/limits"
 	"github.com/cilium/cilium/pkg/defaults"
-	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/math"
-	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/trigger"
 
 	"github.com/sirupsen/logrus"
@@ -693,9 +690,9 @@ func (n *Node) MaintainIPPool(ctx context.Context) error {
 // [(*Node).resource)] with the K8s apiserver. This operation occurs on an
 // interval to refresh the CiliumNode resource.
 //
-// For Azure and ENI IPAM modes, this function serves two purposes: (1) as the
-// entry point to initialize the CiliumNode resource and (2) to keep the
-// resource up-to-date with K8s.
+// For Azure and ENI IPAM modes, this function serves two purposes: (1)
+// finalizes the initialization of the CiliumNode resource (setting
+// PreAllocate) and (2) to keep the resource up-to-date with K8s.
 //
 // To initialize, or seed, the CiliumNode resource, the PreAllocate field is
 // populated with a default value and then is adjusted as necessary.
@@ -743,8 +740,13 @@ func (n *Node) syncToAPIServer() (err error) {
 		node.Spec.IPAM.Pool = n.Pool()
 		scopedLog.WithField("poolSize", len(node.Spec.IPAM.Pool)).Debug("Updating node in apiserver")
 
+		// The PreAllocate value is added here rather than where the CiliumNode
+		// resource is created ((*NodeDiscovery).mutateNodeResource() inside
+		// pkg/nodediscovery), because mutateNodeResource() does not have
+		// access to the ipam.Node object. Since we are in the CiliumNode
+		// update sync loop, we can compute the value.
 		if node.Spec.IPAM.PreAllocate == 0 {
-			adjustPreAllocateIfNeeded(node)
+			node.Spec.IPAM.PreAllocate = n.ops.GetMinimumAllocatableIPv4()
 		}
 
 		err = n.update(origNode, node, retry, false)
@@ -819,32 +821,4 @@ func (n *Node) update(origNode, node *v2.CiliumNode, attempts int, status bool) 
 	}
 
 	return err
-}
-
-// adjustPreAllocateIfNeeded adjusts IPAM values depending on the instance
-// type. This is needed when the instance type is on the smaller side which
-// requires us to lower the PreAllocate value and include eth0 as an ENI device
-// because the instance type does not allow for additional ENIs to be attached
-// (hence limited).
-//
-// For now, this function is only needed in ENI IPAM mode. In the future, this
-// may also be needed for Azure IPAM as well.
-func adjustPreAllocateIfNeeded(node *v2.CiliumNode) {
-	if option.Config.IPAM != ipamOption.IPAMENI {
-		return
-	}
-
-	// Auto set the PreAllocate to the default value and we'll determine if we
-	// need to adjust it below.
-	node.Spec.IPAM.PreAllocate = defaults.IPAMPreAllocation
-
-	if lim, ok := limits.Get(node.Spec.ENI.InstanceType); ok {
-		max := lim.Adapters * lim.IPv4
-		if node.Spec.IPAM.PreAllocate > max {
-			node.Spec.IPAM.PreAllocate = max
-
-			var i int = 0
-			node.Spec.ENI.FirstInterfaceIndex = &i // Include eth0
-		}
-	}
 }

pkg/ipam/node_manager.go
@@ -92,6 +92,10 @@ type NodeOperations interface {
 	// GetMaximumAllocatableIPv4 returns the maximum amount of IPv4 addresses
 	// that can be allocated to the instance
 	GetMaximumAllocatableIPv4() int
+
+	// GetMinimumAllocatableIPv4 returns the minimum amount of IPv4 addresses that
+	// must be allocated to the instance.
+	GetMinimumAllocatableIPv4() int
 }
 
 // AllocationImplementation is the interface an implementation must provide.

pkg/nodediscovery/nodediscovery.go
@@ -20,9 +20,7 @@ import (
 	"strings"
 	"time"
 
-	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/annotation"
-	"github.com/cilium/cilium/pkg/aws/eni/limits"
 	eniTypes "github.com/cilium/cilium/pkg/aws/eni/types"
 	"github.com/cilium/cilium/pkg/aws/metadata"
 	azureTypes "github.com/cilium/cilium/pkg/azure/types"
@@ -478,14 +476,14 @@ func (n *NodeDiscovery) mutateNodeResource(nodeResource *ciliumv2.CiliumNode) er
 		}
 
 		// It is important to determine the interface index here because this
-		// function (mutateNodeResource) will be called when the agent is first
-		// coming up and is initializing the IPAM layer (CRD allocator in this
-		// case). Later on, the Operator will adjust this value based on the
-		// PreAllocate value, so to ensure that the agent and the Operator are
-		// not conflicting with each other, we must have similar logic to
+		// function (mutateNodeResource()) will be called when the agent is
+		// first coming up and is initializing the IPAM layer (CRD allocator in
+		// this case). Later on, the Operator will adjust this value based on
+		// the PreAllocate value, so to ensure that the agent and the Operator
+		// are not conflicting with each other, we must have similar logic to
 		// determine the appropriate value to place inside the resource.
 		nodeResource.Spec.ENI.VpcID = vpcID
-		nodeResource.Spec.ENI.FirstInterfaceIndex = determineFirstInterfaceIndex(instanceType)
+		nodeResource.Spec.ENI.FirstInterfaceIndex = getInt(defaults.ENIFirstInterfaceIndex)
 
 		if c := n.NetConf; c != nil {
 			if c.IPAM.MinAllocate != 0 {
@@ -563,41 +561,6 @@ func (n *NodeDiscovery) mutateNodeResource(nodeResource *ciliumv2.CiliumNode) er
 	return nil
 }
 
-// determineFirstInterfaceIndex determines the appropriate default interface
-// index for the ENI IPAM mode. The interface index is stored inside the
-// CiliumNode resource. It specifies which device offset (ENI) to start
-// assigning IPs to. It is important to seed the CiliumNode resource with the
-// appropriate value, otherwise pods will fail to come up because they won't
-// have an IP assigned, because the instance limits (depending on the instance
-// type) have a maximum threshold. See
-// Documentation/concepts/networking/ipam/eni.rst for more details on this
-// value.
-//
-// This value is also ensured to stay in place using similar logic in
-// adjustPreAllocateIfNeeded(), inside
-// github.com/cilium/cilium/pkg/ipam.(*Node).syncToAPIServer().
-func determineFirstInterfaceIndex(instanceType string) *int {
-	if option.Config.IPAM != ipamOption.IPAMENI {
-		return nil
-	}
-
-	// Fallback to default value if we determine below that the instance limits
-	// do not require us to adjust the interface index.
-	idx := defaults.ENIFirstInterfaceIndex
-
-	if l, ok := limits.Get(instanceType); ok {
-		max := l.Adapters * l.IPv4
-		if defaults.IPAMPreAllocation > max {
-			idx = 0 // Include eth0
-		}
-	} else {
-		log.WithFields(logrus.Fields{
-			"instance-type": instanceType,
-		}).Warningf(
-			"Unable to find limits for instance type, consider setting --%s=true on the Operator",
-			operatorOption.UpdateEC2AdapterLimitViaAPI,
-		)
-	}
-
-	return &idx
+func getInt(i int) *int {
+	return &i
 }

pkg/ipam/node_manager_test.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/cilium/cilium/pkg/defaults"
 	metricsmock "github.com/cilium/cilium/pkg/ipam/metrics/mock"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
@@ -154,6 +155,10 @@ func (n *nodeOperationsMock) ReleaseIPs(ctx context.Context, release *ReleaseAct
 
 func (n *nodeOperationsMock) GetMaximumAllocatableIPv4() int {
 	return 0
+}
+
+func (n *nodeOperationsMock) GetMinimumAllocatableIPv4() int {
+	return defaults.IPAMPreAllocation
 }
 
 func (e *IPAMSuite) TestGetNodeNames(c *check.C) {