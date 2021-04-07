g0odi3z2j/cilium

commit 2557278c9195806653045549cf9b1c234aaee9ad

add support for EndpointSlice V1
With this commit Cilium will have support for EndpointSlice v1 which is
being enabled by default in Kubernetes Clusters running with v1.21.

To have backward compatibility, Cilium will still continue to support
EndpointSlice v1beta1 for clusters below v1.21.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/endpoints.go
@@ -23,13 +23,13 @@ import (
 
 	"github.com/cilium/cilium/pkg/ip"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
-	slim_discover_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1beta1"
+	slim_discovery_v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1"
+	slim_discovery_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1beta1"
 	"github.com/cilium/cilium/pkg/k8s/version"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/option"
 	serviceStore "github.com/cilium/cilium/pkg/service/store"
 
-	"k8s.io/api/discovery/v1beta1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/cache"
 )
@@ -146,20 +146,26 @@ func ParseEndpoints(ep *slim_corev1.Endpoints) (ServiceID, *Endpoints) {
 	return ParseEndpointsID(ep), endpoints
 }
 
+type endpointSlice interface {
+	GetNamespace() string
+	GetName() string
+	GetLabels() map[string]string
+}
+
 // ParseEndpointSliceID parses a Kubernetes endpoints slice and returns a
 // EndpointSliceID
-func ParseEndpointSliceID(es *slim_discover_v1beta1.EndpointSlice) EndpointSliceID {
+func ParseEndpointSliceID(es endpointSlice) EndpointSliceID {
 	return EndpointSliceID{
 		ServiceID: ServiceID{
-			Name:      es.ObjectMeta.GetLabels()[v1beta1.LabelServiceName],
-			Namespace: es.ObjectMeta.Namespace,
+			Name:      es.GetLabels()[slim_discovery_v1.LabelServiceName],
+			Namespace: es.GetNamespace(),
 		},
-		EndpointSliceName: es.ObjectMeta.GetName(),
+		EndpointSliceName: es.GetName(),
 	}
 }
 
-// ParseEndpointSlice parses a Kubernetes Endpoints resource
-func ParseEndpointSlice(ep *slim_discover_v1beta1.EndpointSlice) (EndpointSliceID, *Endpoints) {
+// ParseEndpointSliceV1Beta1 parses a Kubernetes EndpointsSlice v1beta1 resource
+func ParseEndpointSliceV1Beta1(ep *slim_discovery_v1beta1.EndpointSlice) (EndpointSliceID, *Endpoints) {
 	endpoints := newEndpoints()
 
 	for _, sub := range ep.Endpoints {
@@ -182,7 +188,7 @@ func ParseEndpointSlice(ep *slim_discover_v1beta1.EndpointSlice) (EndpointSliceI
 			}
 
 			for _, port := range ep.Ports {
-				name, lbPort := parseEndpointPort(port)
+				name, lbPort := parseEndpointPortV1Beta1(port)
 				if lbPort != nil {
 					backend.Ports[name] = lbPort
 				}
@@ -193,9 +199,73 @@ func ParseEndpointSlice(ep *slim_discover_v1beta1.EndpointSlice) (EndpointSliceI
 	return ParseEndpointSliceID(ep), endpoints
 }
 
-// parseEndpointPort returns the port name and the port parsed as a L4Addr from
+// parseEndpointPortV1Beta1 returns the port name and the port parsed as a
+// L4Addr from the given port.
+func parseEndpointPortV1Beta1(port slim_discovery_v1beta1.EndpointPort) (string, *loadbalancer.L4Addr) {
+	proto := loadbalancer.TCP
+	if port.Protocol != nil {
+		switch *port.Protocol {
+		case slim_corev1.ProtocolTCP:
+			proto = loadbalancer.TCP
+		case slim_corev1.ProtocolUDP:
+			proto = loadbalancer.UDP
+		default:
+			return "", nil
+		}
+	}
+	if port.Port == nil {
+		return "", nil
+	}
+	var name string
+	if port.Name != nil {
+		name = *port.Name
+	}
+	lbPort := loadbalancer.NewL4Addr(proto, uint16(*port.Port))
+	return name, lbPort
+}
+
+// ParseEndpointSliceV1 parses a Kubernetes Endpoints resource
+func ParseEndpointSliceV1(ep *slim_discovery_v1.EndpointSlice) (EndpointSliceID, *Endpoints) {
+	endpoints := newEndpoints()
+
+	for _, sub := range ep.Endpoints {
+		// ready indicates that this endpoint is prepared to receive traffic,
+		// according to whatever system is managing the endpoint. A nil value
+		// indicates an unknown state. In most cases consumers should interpret this
+		// unknown state as ready.
+		// More info: vendor/k8s.io/api/discovery/v1/types.go:117
+		if sub.Conditions.Ready != nil && !*sub.Conditions.Ready {
+			continue
+		}
+		for _, addr := range sub.Addresses {
+			backend, ok := endpoints.Backends[addr]
+			if !ok {
+				backend = &Backend{Ports: serviceStore.PortConfiguration{}}
+				endpoints.Backends[addr] = backend
+				if sub.NodeName != nil {
+					backend.NodeName = *sub.NodeName
+				} else {
+					if nodeName, ok := sub.DeprecatedTopology["kubernetes.io/hostname"]; ok {
+						backend.NodeName = nodeName
+					}
+				}
+			}
+
+			for _, port := range ep.Ports {
+				name, lbPort := parseEndpointPortV1(port)
+				if lbPort != nil {
+					backend.Ports[name] = lbPort
+				}
+			}
+		}
+	}
+
+	return ParseEndpointSliceID(ep), endpoints
+}
+
+// parseEndpointPortV1 returns the port name and the port parsed as a L4Addr from
 // the given port.
-func parseEndpointPort(port slim_discover_v1beta1.EndpointPort) (string, *loadbalancer.L4Addr) {
+func parseEndpointPortV1(port slim_discovery_v1.EndpointPort) (string, *loadbalancer.L4Addr) {
 	proto := loadbalancer.TCP
 	if port.Protocol != nil {
 		switch *port.Protocol {
@@ -288,6 +358,12 @@ func newExternalEndpoints() externalEndpoints {
 // watch and process endpoint slices.
 func SupportsEndpointSlice() bool {
 	return version.Capabilities().EndpointSlice && option.Config.K8sEnableK8sEndpointSlice
+}
+
+// SupportsEndpointSliceV1 returns true if cilium-operator or cilium-agent should
+// watch and process endpoint slices V1.
+func SupportsEndpointSliceV1() bool {
+	return version.Capabilities().EndpointSliceV1 && option.Config.K8sEnableK8sEndpointSlice
 }
 
 // HasEndpointSlice returns true if the hasEndpointSlices is closed before the

operator/watchers/k8s_service_sync.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Authors of Cilium
+// Copyright 2018-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import (
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	slim_discover_v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1"
 	slim_discover_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1beta1"
 	"github.com/cilium/cilium/pkg/k8s/utils"
 	"github.com/cilium/cilium/pkg/k8s/watchers/subscriber"
@@ -36,8 +37,10 @@ import (
 	v1 "k8s.io/api/core/v1"
 	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -262,47 +265,93 @@ func endpointSlicesInit(k8sClient kubernetes.Interface, swgEps *lock.StoppableWa
 	var (
 		hasEndpointSlices = make(chan struct{})
 		once              sync.Once
+		esClient          rest.Interface
+		objType           runtime.Object
+		addFunc, delFunc  func(obj interface{})
+		updateFunc        func(oldObj, newObj interface{})
 	)
 
+	if k8s.SupportsEndpointSliceV1() {
+		esClient = k8sClient.DiscoveryV1().RESTClient()
+		objType = &slim_discover_v1.EndpointSlice{}
+		addFunc = func(obj interface{}) {
+			once.Do(func() {
+				// signalize that we have received an endpoint slice
+				// so it means the cluster has endpoint slices enabled.
+				close(hasEndpointSlices)
+			})
+			metrics.EventTSK8s.SetToCurrentTime()
+			if k8sEP := k8s.ObjToV1EndpointSlice(obj); k8sEP != nil {
+				K8sSvcCache.UpdateEndpointSlicesV1(k8sEP, swgEps)
+			}
+		}
+		updateFunc = func(oldObj, newObj interface{}) {
+			metrics.EventTSK8s.SetToCurrentTime()
+			if oldk8sEP := k8s.ObjToV1EndpointSlice(oldObj); oldk8sEP != nil {
+				if newk8sEP := k8s.ObjToV1EndpointSlice(newObj); newk8sEP != nil {
+					if oldk8sEP.DeepEqual(newk8sEP) {
+						return
+					}
+					K8sSvcCache.UpdateEndpointSlicesV1(newk8sEP, swgEps)
+				}
+			}
+		}
+		delFunc = func(obj interface{}) {
+			metrics.EventTSK8s.SetToCurrentTime()
+			k8sEP := k8s.ObjToV1EndpointSlice(obj)
+			if k8sEP == nil {
+				return
+			}
+			K8sSvcCache.DeleteEndpointSlices(k8sEP, swgEps)
+		}
+	} else {
+		esClient = k8sClient.DiscoveryV1beta1().RESTClient()
+		objType = &slim_discover_v1beta1.EndpointSlice{}
+		addFunc = func(obj interface{}) {
+			once.Do(func() {
+				// signalize that we have received an endpoint slice
+				// so it means the cluster has endpoint slices enabled.
+				close(hasEndpointSlices)
+			})
+			metrics.EventTSK8s.SetToCurrentTime()
+			if k8sEP := k8s.ObjToV1Beta1EndpointSlice(obj); k8sEP != nil {
+				K8sSvcCache.UpdateEndpointSlicesV1Beta1(k8sEP, swgEps)
+			}
+		}
+		updateFunc = func(oldObj, newObj interface{}) {
+			metrics.EventTSK8s.SetToCurrentTime()
+			if oldk8sEP := k8s.ObjToV1Beta1EndpointSlice(oldObj); oldk8sEP != nil {
+				if newk8sEP := k8s.ObjToV1Beta1EndpointSlice(newObj); newk8sEP != nil {
+					if oldk8sEP.DeepEqual(newk8sEP) {
+						return
+					}
+					K8sSvcCache.UpdateEndpointSlicesV1Beta1(newk8sEP, swgEps)
+				}
+			}
+		}
+		delFunc = func(obj interface{}) {
+			metrics.EventTSK8s.SetToCurrentTime()
+			k8sEP := k8s.ObjToV1Beta1EndpointSlice(obj)
+			if k8sEP == nil {
+				return
+			}
+			K8sSvcCache.DeleteEndpointSlices(k8sEP, swgEps)
+		}
+	}
+
 	_, endpointController := informer.NewInformer(
-		cache.NewListWatchFromClient(k8sClient.DiscoveryV1beta1().RESTClient(),
+		cache.NewListWatchFromClient(esClient,
 			"endpointslices", v1.NamespaceAll, fields.Everything()),
-		&slim_discover_v1beta1.EndpointSlice{},
+		objType,
 		0,
 		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				once.Do(func() {
-					// signalize that we have received an endpoint slice
-					// so it means the cluster has endpoint slices enabled.
-					close(hasEndpointSlices)
-				})
-				metrics.EventTSK8s.SetToCurrentTime()
-				if k8sEP := k8s.ObjToV1EndpointSlice(obj); k8sEP != nil {
-					K8sSvcCache.UpdateEndpointSlices(k8sEP, swgEps)
-				}
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
-				if oldk8sEP := k8s.ObjToV1EndpointSlice(oldObj); oldk8sEP != nil {
-					if newk8sEP := k8s.ObjToV1EndpointSlice(newObj); newk8sEP != nil {
-						if oldk8sEP.DeepEqual(newk8sEP) {
-							return
-						}
-						K8sSvcCache.UpdateEndpointSlices(newk8sEP, swgEps)
-					}
-				}
-			},
-			DeleteFunc: func(obj interface{}) {
-				metrics.EventTSK8s.SetToCurrentTime()
-				k8sEP := k8s.ObjToV1EndpointSlice(obj)
-				if k8sEP == nil {
-					return
-				}
-				K8sSvcCache.DeleteEndpointSlices(k8sEP, swgEps)
-			},
+			AddFunc:    addFunc,
+			UpdateFunc: updateFunc,
+			DeleteFunc: delFunc,
 		},
 		nil,
 	)
+
 	ecr := make(chan struct{})
 	go endpointController.Run(ecr)
 

pkg/k8s/endpoints_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Authors of Cilium
+// Copyright 2018-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/checker"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	slim_discovery_v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1"
 	slim_discovery_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1beta1"
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	"github.com/cilium/cilium/pkg/loadbalancer"
@@ -775,12 +776,12 @@ func (s *K8sSuite) Test_parseK8sEPSlicev1Beta1(c *check.C) {
 	for _, tt := range tests {
 		args := tt.setupArgs()
 		want := tt.setupWanted()
-		_, got := ParseEndpointSlice(args.eps)
+		_, got := ParseEndpointSliceV1Beta1(args.eps)
 		c.Assert(got, checker.DeepEquals, want, check.Commentf("Test name: %q", tt.name))
 	}
 }
 
-func Test_parseEndpointPort(t *testing.T) {
+func Test_parseEndpointPortV1Beta1(t *testing.T) {
 	type args struct {
 		port slim_discovery_v1beta1.EndpointPort
 	}
@@ -845,12 +846,397 @@ func Test_parseEndpointPort(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotPortName, gotL4Addr := parseEndpointPort(tt.args.port)
+			gotPortName, gotL4Addr := parseEndpointPortV1Beta1(tt.args.port)
 			if gotPortName != tt.portName {
-				t.Errorf("parseEndpointPort() got = %v, want %v", gotPortName, tt.portName)
+				t.Errorf("parseEndpointPortV1Beta1() got = %v, want %v", gotPortName, tt.portName)
 			}
 			if !reflect.DeepEqual(gotL4Addr, tt.l4Addr) {
-				t.Errorf("parseEndpointPort() got1 = %v, want %v", gotL4Addr, tt.l4Addr)
+				t.Errorf("parseEndpointPortV1Beta1() got1 = %v, want %v", gotL4Addr, tt.l4Addr)
+			}
+		})
+	}
+}
+
+func (s *K8sSuite) Test_parseK8sEPSlicev1(c *check.C) {
+	nodeName := "k8s1"
+
+	type args struct {
+		eps *slim_discovery_v1.EndpointSlice
+	}
+	tests := []struct {
+		name        string
+		setupArgs   func() args
+		setupWanted func() *Endpoints
+	}{
+		{
+			name: "empty endpoint",
+			setupArgs: func() args {
+				return args{
+					eps: &slim_discovery_v1.EndpointSlice{
+						ObjectMeta: slim_metav1.ObjectMeta{
+							Name:      "foo",
+							Namespace: "bar",
+						},
+					},
+				}
+			},
+			setupWanted: func() *Endpoints {
+				return newEndpoints()
+			},
+		},
+		{
+			name: "endpoint with an address and port",
+			setupArgs: func() args {
+				return args{
+					eps: &slim_discovery_v1.EndpointSlice{
+						AddressType: slim_discovery_v1.AddressTypeIPv4,
+						ObjectMeta: slim_metav1.ObjectMeta{
+							Name:      "foo",
+							Namespace: "bar",
+						},
+						Endpoints: []slim_discovery_v1.Endpoint{
+							{
+								Addresses: []string{
+									"172.0.0.1",
+								},
+								DeprecatedTopology: map[string]string{
+									"kubernetes.io/hostname": nodeName,
+								},
+							},
+						},
+						Ports: []slim_discovery_v1.EndpointPort{
+							{
+								Name:     func() *string { a := "http-test-svc"; return &a }(),
+								Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+								Port:     func() *int32 { a := int32(8080); return &a }(),
+							},
+						},
+					},
+				}
+			},
+			setupWanted: func() *Endpoints {
+				svcEP := newEndpoints()
+				svcEP.Backends["172.0.0.1"] = &Backend{
+					Ports: serviceStore.PortConfiguration{
+						"http-test-svc": loadbalancer.NewL4Addr(loadbalancer.TCP, 8080),
+					},
+					NodeName: nodeName,
+				}
+				return svcEP
+			},
+		},
+		{
+			name: "endpoint with an address and 2 ports",
+			setupArgs: func() args {
+				return args{
+					eps: &slim_discovery_v1.EndpointSlice{
+						AddressType: slim_discovery_v1.AddressTypeIPv4,
+						ObjectMeta: slim_metav1.ObjectMeta{
+							Name:      "foo",
+							Namespace: "bar",
+						},
+						Endpoints: []slim_discovery_v1.Endpoint{
+							{
+								Addresses: []string{
+									"172.0.0.1",
+								},
+								DeprecatedTopology: map[string]string{
+									"kubernetes.io/hostname": nodeName,
+								},
+							},
+						},
+						Ports: []slim_discovery_v1.EndpointPort{
+							{
+								Name:     func() *string { a := "http-test-svc"; return &a }(),
+								Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+								Port:     func() *int32 { a := int32(8080); return &a }(),
+							},
+							{
+								Name:     func() *string { a := "http-test-svc-2"; return &a }(),
+								Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+								Port:     func() *int32 { a := int32(8081); return &a }(),
+							},
+						},
+					},
+				}
+			},
+			setupWanted: func() *Endpoints {
+				svcEP := newEndpoints()
+				svcEP.Backends["172.0.0.1"] = &Backend{
+					Ports: serviceStore.PortConfiguration{
+						"http-test-svc":   loadbalancer.NewL4Addr(loadbalancer.TCP, 8080),
+						"http-test-svc-2": loadbalancer.NewL4Addr(loadbalancer.TCP, 8081),
+					},
+					NodeName: nodeName,
+				}
+				return svcEP
+			},
+		},
+		{
+			name: "endpoint with 2 addresses and 2 ports",
+			setupArgs: func() args {
+				return args{
+					eps: &slim_discovery_v1.EndpointSlice{
+						AddressType: slim_discovery_v1.AddressTypeIPv4,
+						ObjectMeta: slim_metav1.ObjectMeta{
+							Name:      "foo",
+							Namespace: "bar",
+						},
+						Endpoints: []slim_discovery_v1.Endpoint{
+							{
+								Addresses: []string{
+									"172.0.0.1",
+								},
+								DeprecatedTopology: map[string]string{
+									"kubernetes.io/hostname": nodeName,
+								},
+							},
+							{
+								Addresses: []string{
+									"172.0.0.2",
+								},
+							},
+						},
+						Ports: []slim_discovery_v1.EndpointPort{
+							{
+								Name:     func() *string { a := "http-test-svc"; return &a }(),
+								Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+								Port:     func() *int32 { a := int32(8080); return &a }(),
+							},
+							{
+								Name:     func() *string { a := "http-test-svc-2"; return &a }(),
+								Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+								Port:     func() *int32 { a := int32(8081); return &a }(),
+							},
+						},
+					},
+				}
+			},
+			setupWanted: func() *Endpoints {
+				svcEP := newEndpoints()
+				svcEP.Backends["172.0.0.1"] = &Backend{
+					Ports: serviceStore.PortConfiguration{
+						"http-test-svc":   loadbalancer.NewL4Addr(loadbalancer.TCP, 8080),
+						"http-test-svc-2": loadbalancer.NewL4Addr(loadbalancer.TCP, 8081),
+					},
+					NodeName: nodeName,
+				}
+				svcEP.Backends["172.0.0.2"] = &Backend{
+					Ports: serviceStore.PortConfiguration{
+						"http-test-svc":   loadbalancer.NewL4Addr(loadbalancer.TCP, 8080),
+						"http-test-svc-2": loadbalancer.NewL4Addr(loadbalancer.TCP, 8081),
+					},
+				}
+				return svcEP
+			},
+		},
+		{
+			name: "endpoint with 2 addresses, 1 address not ready and 2 ports",
+			setupArgs: func() args {
+				return args{
+					eps: &slim_discovery_v1.EndpointSlice{
+						ObjectMeta: slim_metav1.ObjectMeta{
+							Name:      "foo",
+							Namespace: "bar",
+						},
+						Endpoints: []slim_discovery_v1.Endpoint{
+							{
+								Addresses: []string{
+									"172.0.0.1",
+								},
+								DeprecatedTopology: map[string]string{
+									"kubernetes.io/hostname": nodeName,
+								},
+							},
+							{
+								Addresses: []string{
+									"172.0.0.2",
+								},
+							},
+							{
+								Conditions: slim_discovery_v1.EndpointConditions{
+									Ready: func() *bool { a := false; return &a }(),
+								},
+								Addresses: []string{
+									"172.0.0.3",
+								},
+							},
+						},
+						Ports: []slim_discovery_v1.EndpointPort{
+							{
+								Name:     func() *string { a := "http-test-svc"; return &a }(),
+								Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+								Port:     func() *int32 { a := int32(8080); return &a }(),
+							},
+							{
+								Name:     func() *string { a := "http-test-svc-2"; return &a }(),
+								Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+								Port:     func() *int32 { a := int32(8081); return &a }(),
+							},
+						},
+					},
+				}
+			},
+			setupWanted: func() *Endpoints {
+				svcEP := newEndpoints()
+				svcEP.Backends["172.0.0.1"] = &Backend{
+					Ports: serviceStore.PortConfiguration{
+						"http-test-svc":   loadbalancer.NewL4Addr(loadbalancer.TCP, 8080),
+						"http-test-svc-2": loadbalancer.NewL4Addr(loadbalancer.TCP, 8081),
+					},
+					NodeName: nodeName,
+				}
+				svcEP.Backends["172.0.0.2"] = &Backend{
+					Ports: serviceStore.PortConfiguration{
+						"http-test-svc":   loadbalancer.NewL4Addr(loadbalancer.TCP, 8080),
+						"http-test-svc-2": loadbalancer.NewL4Addr(loadbalancer.TCP, 8081),
+					},
+				}
+				return svcEP
+			},
+		}, {
+			name: "endpoint with 2 addresses, 1 address not ready and 2 ports",
+			setupArgs: func() args {
+				return args{
+					eps: &slim_discovery_v1.EndpointSlice{
+						ObjectMeta: slim_metav1.ObjectMeta{
+							Name:      "foo",
+							Namespace: "bar",
+						},
+						Endpoints: []slim_discovery_v1.Endpoint{
+							{
+								Addresses: []string{
+									"172.0.0.1",
+								},
+								NodeName: func() *string { return &nodeName }(),
+							},
+							{
+								Addresses: []string{
+									"172.0.0.2",
+								},
+							},
+							{
+								Conditions: slim_discovery_v1.EndpointConditions{
+									Ready: func() *bool { a := false; return &a }(),
+								},
+								Addresses: []string{
+									"172.0.0.3",
+								},
+							},
+						},
+						Ports: []slim_discovery_v1.EndpointPort{
+							{
+								Name:     func() *string { a := "http-test-svc"; return &a }(),
+								Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+								Port:     func() *int32 { a := int32(8080); return &a }(),
+							},
+							{
+								Name:     func() *string { a := "http-test-svc-2"; return &a }(),
+								Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+								Port:     func() *int32 { a := int32(8081); return &a }(),
+							},
+						},
+					},
+				}
+			},
+			setupWanted: func() *Endpoints {
+				svcEP := newEndpoints()
+				svcEP.Backends["172.0.0.1"] = &Backend{
+					Ports: serviceStore.PortConfiguration{
+						"http-test-svc":   loadbalancer.NewL4Addr(loadbalancer.TCP, 8080),
+						"http-test-svc-2": loadbalancer.NewL4Addr(loadbalancer.TCP, 8081),
+					},
+					NodeName: nodeName,
+				}
+				svcEP.Backends["172.0.0.2"] = &Backend{
+					Ports: serviceStore.PortConfiguration{
+						"http-test-svc":   loadbalancer.NewL4Addr(loadbalancer.TCP, 8080),
+						"http-test-svc-2": loadbalancer.NewL4Addr(loadbalancer.TCP, 8081),
+					},
+				}
+				return svcEP
+			},
+		},
+	}
+	for _, tt := range tests {
+		args := tt.setupArgs()
+		want := tt.setupWanted()
+		_, got := ParseEndpointSliceV1(args.eps)
+		c.Assert(got, checker.DeepEquals, want, check.Commentf("Test name: %q", tt.name))
+	}
+}
+
+func Test_parseEndpointPortV1(t *testing.T) {
+	type args struct {
+		port slim_discovery_v1.EndpointPort
+	}
+	tests := []struct {
+		name     string
+		args     args
+		portName string
+		l4Addr   *loadbalancer.L4Addr
+	}{
+		{
+			name: "tcp-port",
+			args: args{
+				port: slim_discovery_v1.EndpointPort{
+					Name:     func() *string { a := "http-test-svc"; return &a }(),
+					Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolTCP; return &a }(),
+					Port:     func() *int32 { a := int32(8080); return &a }(),
+				},
+			},
+			portName: "http-test-svc",
+			l4Addr: &loadbalancer.L4Addr{
+				Protocol: loadbalancer.TCP,
+				Port:     8080,
+			},
+		},
+		{
+			name: "udp-port",
+			args: args{
+				port: slim_discovery_v1.EndpointPort{
+					Name:     func() *string { a := "http-test-svc"; return &a }(),
+					Protocol: func() *slim_corev1.Protocol { a := slim_corev1.ProtocolUDP; return &a }(),
+					Port:     func() *int32 { a := int32(8080); return &a }(),
+				},
+			},
+			portName: "http-test-svc",
+			l4Addr: &loadbalancer.L4Addr{
+				Protocol: loadbalancer.UDP,
+				Port:     8080,
+			},
+		},
+		{
+			name: "unset-protocol-should-have-tcp-port",
+			args: args{
+				port: slim_discovery_v1.EndpointPort{
+					Name: func() *string { a := "http-test-svc"; return &a }(),
+					Port: func() *int32 { a := int32(8080); return &a }(),
+				},
+			},
+			portName: "http-test-svc",
+			l4Addr: &loadbalancer.L4Addr{
+				Protocol: loadbalancer.TCP,
+				Port:     8080,
+			},
+		},
+		{
+			name: "unset-port-number-should-fail",
+			args: args{
+				port: slim_discovery_v1.EndpointPort{
+					Name: func() *string { a := "http-test-svc"; return &a }(),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotPortName, gotL4Addr := parseEndpointPortV1(tt.args.port)
+			if gotPortName != tt.portName {
+				t.Errorf("parseEndpointPortV1() got = %v, want %v", gotPortName, tt.portName)
+			}
+			if !reflect.DeepEqual(gotL4Addr, tt.l4Addr) {
+				t.Errorf("parseEndpointPortV1() got1 = %v, want %v", gotL4Addr, tt.l4Addr)
 			}
 		})
 	}

pkg/k8s/factory_functions.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Authors of Cilium
+// Copyright 2018-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import (
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	cilium_v2alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	slim_discover_v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1"
 	slim_discover_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1beta1"
 	slim_networkingv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/networking/v1"
 	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
@@ -90,7 +91,7 @@ func ObjToV1Endpoints(obj interface{}) *slim_corev1.Endpoints {
 	return nil
 }
 
-func ObjToV1EndpointSlice(obj interface{}) *slim_discover_v1beta1.EndpointSlice {
+func ObjToV1Beta1EndpointSlice(obj interface{}) *slim_discover_v1beta1.EndpointSlice {
 	ep, ok := obj.(*slim_discover_v1beta1.EndpointSlice)
 	if ok {
 		return ep
@@ -107,6 +108,26 @@ func ObjToV1EndpointSlice(obj interface{}) *slim_discover_v1beta1.EndpointSlice 
 	}
 	log.WithField(logfields.Object, logfields.Repr(obj)).
 		Warn("Ignoring invalid k8s v1beta1 EndpointSlice")
+	return nil
+}
+
+func ObjToV1EndpointSlice(obj interface{}) *slim_discover_v1.EndpointSlice {
+	ep, ok := obj.(*slim_discover_v1.EndpointSlice)
+	if ok {
+		return ep
+	}
+	deletedObj, ok := obj.(cache.DeletedFinalStateUnknown)
+	if ok {
+		// Delete was not observed by the watcher but is
+		// removed from kube-apiserver. This is the last
+		// known state and the object no longer exists.
+		ep, ok := deletedObj.Obj.(*slim_discover_v1.EndpointSlice)
+		if ok {
+			return ep
+		}
+	}
+	log.WithField(logfields.Object, logfields.Repr(obj)).
+		Warn("Ignoring invalid k8s v1 EndpointSlice")
 	return nil
 }
 

pkg/k8s/service_cache.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Authors of Cilium
+// Copyright 2018-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/ip"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	slim_discovery_v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1"
 	slim_discovery_v1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/discovery/v1beta1"
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/lock"
@@ -304,8 +305,14 @@ func (s *ServiceCache) UpdateEndpoints(k8sEndpoints *slim_corev1.Endpoints, swg 
 	return s.updateEndpoints(epSliceID, newEndpoints, swg)
 }
 
-func (s *ServiceCache) UpdateEndpointSlices(epSlice *slim_discovery_v1beta1.EndpointSlice, swg *lock.StoppableWaitGroup) (ServiceID, *Endpoints) {
-	svcID, newEndpoints := ParseEndpointSlice(epSlice)
+func (s *ServiceCache) UpdateEndpointSlicesV1(epSlice *slim_discovery_v1.EndpointSlice, swg *lock.StoppableWaitGroup) (ServiceID, *Endpoints) {
+	svcID, newEndpoints := ParseEndpointSliceV1(epSlice)
+
+	return s.updateEndpoints(svcID, newEndpoints, swg)
+}
+
+func (s *ServiceCache) UpdateEndpointSlicesV1Beta1(epSlice *slim_discovery_v1beta1.EndpointSlice, swg *lock.StoppableWaitGroup) (ServiceID, *Endpoints) {
+	svcID, newEndpoints := ParseEndpointSliceV1Beta1(epSlice)
 
 	return s.updateEndpoints(svcID, newEndpoints, swg)
 }
@@ -348,7 +355,7 @@ func (s *ServiceCache) DeleteEndpoints(k8sEndpoints *slim_corev1.Endpoints, swg 
 	return s.deleteEndpoints(epSliceID, swg)
 }
 
-func (s *ServiceCache) DeleteEndpointSlices(epSlice *slim_discovery_v1beta1.EndpointSlice, swg *lock.StoppableWaitGroup) ServiceID {
+func (s *ServiceCache) DeleteEndpointSlices(epSlice endpointSlice, swg *lock.StoppableWaitGroup) ServiceID {
 	svcID := ParseEndpointSliceID(epSlice)
 
 	return s.deleteEndpoints(svcID, swg)

commit 8179bb681c6d709639ec81017c04ae185fde043e

k8s: Consolidate check for EndpointSlice support
When v1 support for EndpointSlice is checked, the capability for the
base EndpointSlice support is also toggled, therefore we can consolidate
the logic inside the support helpers. It's not possible for one variant
to return true and the other false, and vice versa.

Fixes: 2557278c91 ("add support for EndpointSlice V1")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/k8s/endpoints.go
@@ -363,7 +363,7 @@ func SupportsEndpointSlice() bool {
 // SupportsEndpointSliceV1 returns true if cilium-operator or cilium-agent should
 // watch and process endpoint slices V1.
 func SupportsEndpointSliceV1() bool {
-	return version.Capabilities().EndpointSliceV1 && option.Config.K8sEnableK8sEndpointSlice
+	return SupportsEndpointSlice() && version.Capabilities().EndpointSliceV1
 }
 
 // HasEndpointSlice returns true if the hasEndpointSlices is closed before the