g0odi3z2j/cilium

commit c90849124018ba8728d39febe6057c6025ff60b4

cilium: encryptInterface requires multiple interfaces in multi-dev env
When a node is using multiple nodes and traffic is being source routed,
or routed some other way, across multiple netdevs we need to ensure
our bpf_network programs are attached to all interfaces that can receive
pod traffic.

This patch adds support for users to specify multiple interfaces. In
order to do this lift bpf_network (encryption program) compile and load
out of init.sh and do it from go side.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

daemon/cmd/daemon_main.go
@@ -1287,12 +1287,12 @@ func initEnv(cmd *cobra.Command) {
 		}
 	}
 
-	if option.Config.EnableIPSec && option.Config.Tunnel == option.TunnelDisabled && option.Config.EncryptInterface == "" {
+	if option.Config.EnableIPSec && option.Config.Tunnel == option.TunnelDisabled && len(option.Config.EncryptInterface) == 0 {
 		link, err := linuxdatapath.NodeDeviceNameWithDefaultRoute()
 		if err != nil {
 			log.WithError(err).Fatal("Ipsec default interface lookup failed, consider \"encrypt-interface\" to manually configure interface.")
 		}
-		option.Config.EncryptInterface = link
+		option.Config.EncryptInterface = append(option.Config.EncryptInterface, link)
 	}
 
 	if option.Config.Tunnel != option.TunnelDisabled && option.Config.EnableAutoDirectRouting {
@@ -1459,8 +1459,8 @@ func (d *Daemon) initKVStore() {
 
 func runDaemon() {
 	datapathConfig := linuxdatapath.DatapathConfiguration{
-		HostDevice:       option.Config.HostDevice,
-		EncryptInterface: option.Config.EncryptInterface,
+		HostDevice:        option.Config.HostDevice,
+		EncryptInterfaces: option.Config.EncryptInterface,
 	}
 
 	log.Info("Initializing daemon")

pkg/datapath/linux/datapath.go
@@ -27,8 +27,8 @@ import (
 type DatapathConfiguration struct {
 	// HostDevice is the name of the device to be used to access the host.
 	HostDevice string
-	// EncryptInterface is the name of the device to be used for direct ruoting encryption
-	EncryptInterface string
+	// EncryptInterfaces is the list of devices used for direct ruoting encryption
+	EncryptInterfaces []string
 }
 
 type linuxDatapath struct {
@@ -54,9 +54,9 @@ func NewDatapath(cfg DatapathConfiguration, ruleManager datapath.IptablesManager
 
 	dp.node = NewNodeHandler(cfg, dp.nodeAddressing, wgAgent)
 
-	if cfg.EncryptInterface != "" {
-		if err := connector.DisableRpFilter(cfg.EncryptInterface); err != nil {
-			log.WithField(logfields.Interface, cfg.EncryptInterface).Warn("Rpfilter could not be disabled, node to node encryption may fail")
+	for _, iface := range cfg.EncryptInterfaces {
+		if err := connector.DisableRpFilter(iface); err != nil {
+			log.WithError(err).WithField(logfields.Interface, iface).Warn("Rpfilter could not be disabled, node to node encryption may fail")
 		}
 	}
 

pkg/datapath/linux/node.go
@@ -463,8 +463,22 @@ func upsertIPsecLog(err error, spec string, loc, rem *net.IPNet, spi uint8) {
 	}
 }
 
+// getDefaultEncryptionInterface() is needed to find the interface used when
+// populating neighbor table and doing arpRequest. For most configurations
+// there is only a single interface so choosing [0] works by choosing the only
+// interface. However EKS, uses multiple interfaces, but fortunately for us
+// in EKS any interface would work so pick the [0] index here as well.
+func getDefaultEncryptionInterface() string {
+	iface := ""
+	if len(option.Config.EncryptInterface) > 0 {
+		iface = option.Config.EncryptInterface[0]
+	}
+	return iface
+}
+
 func getLinkLocalIp(family int) (*net.IPNet, error) {
-	link, err := netlink.LinkByName(option.Config.EncryptInterface)
+	iface := getDefaultEncryptionInterface()
+	link, err := netlink.LinkByName(iface)
 	if err != nil {
 		return nil, err
 	}
@@ -1105,7 +1119,7 @@ func (n *linuxNodeHandler) createNodeIPSecInRoute(ip *net.IPNet) route.Route {
 	var device string
 
 	if option.Config.Tunnel == option.TunnelDisabled {
-		device = n.datapathConfig.EncryptInterface
+		device = n.datapathConfig.EncryptInterfaces[0]
 	} else {
 		device = linux_defaults.TunnelDeviceName
 	}
@@ -1320,8 +1334,14 @@ func (n *linuxNodeHandler) NodeConfigurationChanged(newConfig datapath.LocalNode
 			}
 			ifaceName = option.Config.DirectRoutingDevice
 			n.enableNeighDiscovery = mac != nil // No need to arping for L2-less devices
-		case n.nodeConfig.EnableIPSec && option.Config.Tunnel == option.TunnelDisabled:
-			ifaceName = option.Config.EncryptInterface
+		case n.nodeConfig.EnableIPSec &&
+			option.Config.Tunnel == option.TunnelDisabled &&
+			len(option.Config.EncryptInterface) != 0:
+			// When FIB lookup is not supported we need to pick an
+			// interface so pick first interface in the list. On
+			// kernels with FIB lookup helpers we do a lookup from
+			// the datapath side and ignore this value.
+			ifaceName = option.Config.EncryptInterface[0]
 			n.enableNeighDiscovery = true
 		}
 

pkg/datapath/loader/base.go
@@ -57,8 +57,6 @@ const (
 	initArgXDPDevice
 	initArgXDPMode
 	initArgMTU
-	initArgIPSec
-	initArgEncryptInterface
 	initArgHostReachableServices
 	initArgHostReachableServicesUDP
 	initArgHostReachableServicesPeer
@@ -257,12 +255,6 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 
 	args[initArgMTU] = fmt.Sprintf("%d", deviceMTU)
 
-	if option.Config.EnableIPSec {
-		args[initArgIPSec] = "true"
-	} else {
-		args[initArgIPSec] = "false"
-	}
-
 	if option.Config.EnableHostReachableServices {
 		args[initArgHostReachableServices] = "true"
 		if option.Config.EnableHostServicesUDP {
@@ -279,12 +271,6 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		args[initArgHostReachableServices] = "false"
 		args[initArgHostReachableServicesUDP] = "false"
 		args[initArgHostReachableServicesPeer] = "false"
-	}
-
-	if option.Config.EncryptInterface != "" {
-		args[initArgEncryptInterface] = option.Config.EncryptInterface
-	} else {
-		args[initArgEncryptInterface] = "<nil>"
 	}
 
 	devices := make([]netlink.Link, 0, len(option.Config.Devices))
@@ -358,18 +344,6 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 	}).Info("Setting up BPF datapath")
 
 	if option.Config.IPAM == ipamOption.IPAMENI {
-		// For the ENI ipam mode on EKS, this will be the interface that
-		// the router (cilium_host) IP is associated to.
-		if option.Config.EncryptInterface == "" {
-			if info := node.GetRouterInfo(); info != nil {
-				mac := info.GetMac()
-				iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
-				if err != nil {
-					log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
-				}
-				args[initArgEncryptInterface] = iface
-			}
-		}
 		var err error
 		if sysSettings, err = addENIRules(sysSettings, o.Datapath().LocalNodeAddressing()); err != nil {
 			return fmt.Errorf("unable to install ip rule for ENI multi-node NodePort: %w", err)
@@ -416,6 +390,27 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 		}
 	} else {
 		log.Warning("Cannot check matching of C and Go common struct alignments due to old LLVM/clang version")
+	}
+
+	// Compile and load network programs, currently used for encryption.
+	if option.Config.EnableIPSec {
+		interfaces := option.Config.EncryptInterface
+
+		// For the ENI ipam mode on EKS, this will be the interface that
+		// the router (cilium_host) IP is associated to.
+		if len(interfaces) == 0 && option.Config.IPAM == ipamOption.IPAMENI {
+			if info := node.GetRouterInfo(); info != nil {
+				mac := info.GetMac()
+				iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
+				if err != nil {
+					log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
+				}
+				interfaces = append(interfaces, iface)
+			}
+		}
+		if err := l.replaceNetworkDatapath(ctx, interfaces); err != nil {
+			log.WithError(err).Fatal("failed to load encryption program")
+		}
 	}
 
 	if err := o.Datapath().Node().NodeConfigurationChanged(*o.LocalConfig()); err != nil {

bpf/init.sh
@@ -27,19 +27,17 @@ HOST_DEV2=$9
 XDP_DEV=${10}
 XDP_MODE=${11}
 MTU=${12}
-IPSEC=${13}
-ENCRYPT_DEV=${14}
-HOSTLB=${15}
-HOSTLB_UDP=${16}
-HOSTLB_PEER=${17}
-CGROUP_ROOT=${18}
-BPFFS_ROOT=${19}
-NODE_PORT=${20}
-NODE_PORT_BIND=${21}
-MCPU=${22}
-NR_CPUS=${23}
-ENDPOINT_ROUTES=${24}
-PROXY_RULE=${25}
+HOSTLB=${13}
+HOSTLB_UDP=${14}
+HOSTLB_PEER=${15}
+CGROUP_ROOT=${16}
+BPFFS_ROOT=${17}
+NODE_PORT=${18}
+NODE_PORT_BIND=${19}
+MCPU=${20}
+NR_CPUS=${21}
+ENDPOINT_ROUTES=${22}
+PROXY_RULE=${23}
 
 ID_HOST=1
 ID_WORLD=2
@@ -608,12 +606,6 @@ else
 	bpf_clear_cgroups $CGROUP_ROOT getpeername6
 fi
 
-if [ "$IPSEC" == "true" ]; then
-	if [ "$ENCRYPT_DEV" != "<nil>" ]; then
-		CALLS_MAP="cilium_calls_netdev_ns_${ID_HOST}"
-		bpf_load $ENCRYPT_DEV "" "ingress" bpf_network.c bpf_network.o from-network $CALLS_MAP
-	fi
-fi
 if [ "$HOST_DEV1" != "$HOST_DEV2" ]; then
 	bpf_unload $HOST_DEV2 "egress"
 fi

pkg/datapath/linux/config/config.go
@@ -440,8 +440,13 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 		cDefinesMap["ENABLE_HOST_FIREWALL"] = "1"
 	}
 
-	if iface := option.Config.EncryptInterface; iface != "" {
-		link, err := netlink.LinkByName(iface)
+	if iface := option.Config.EncryptInterface; len(iface) != 0 {
+		// When FIB lookup is not supported (older kernels)  we need to
+		// pick an interface so pick first interface in list. Then we pick
+		// an IPv4 address to use by selecting link IPAddr. In case with
+		// kernel support, the kernel datapath will use the FIB lookup helper
+		// and this define is ignored.
+		link, err := netlink.LinkByName(iface[0])
 		if err == nil {
 			cDefinesMap["ENCRYPT_IFACE"] = fmt.Sprintf("%d", link.Attrs().Index)
 

pkg/datapath/loader/compile.go
@@ -56,6 +56,10 @@ const (
 	hostEndpointObj          = hostEndpointPrefix + ".o"
 	hostEndpointObjDebug     = hostEndpointPrefix + ".dbg.o"
 	hostEndpointAsm          = hostEndpointPrefix + "." + string(outputAssembly)
+
+	networkPrefix = "bpf_network"
+	networkProg   = networkPrefix + "." + string(outputSource)
+	networkObj    = networkPrefix + ".o"
 )
 
 var (
@@ -143,6 +147,11 @@ var (
 	hostEpProg = &progInfo{
 		Source:     hostEndpointProg,
 		Output:     hostEndpointObj,
+		OutputType: outputObject,
+	}
+	networkTcProg = &progInfo{
+		Source:     networkProg,
+		Output:     networkObj,
 		OutputType: outputObject,
 	}
 )
@@ -395,4 +404,38 @@ func compileTemplate(ctx context.Context, out string, isHost bool) error {
 		State:   out,
 	}
 	return compileDatapath(ctx, &dirs, isHost, log)
+}
+
+// compileNetwork compiles a BPF program attached to network
+func compileNetwork(ctx context.Context) error {
+	dirs := directoryInfo{
+		Library: option.Config.BpfDir,
+		Runtime: option.Config.StateDir,
+		Output:  option.Config.StateDir,
+		State:   option.Config.StateDir,
+	}
+	scopedLog := log.WithField(logfields.Debug, true)
+
+	versionCmd := exec.CommandContext(ctx, compiler, "--version")
+	compilerVersion, err := versionCmd.CombinedOutput(scopedLog, true)
+	if err != nil {
+		return err
+	}
+	versionCmd = exec.CommandContext(ctx, linker, "--version")
+	linkerVersion, err := versionCmd.CombinedOutput(scopedLog, true)
+	if err != nil {
+		return err
+	}
+	scopedLog.WithFields(logrus.Fields{
+		compiler: string(compilerVersion),
+		linker:   string(linkerVersion),
+	}).Debug("Compiling network programs")
+
+	// Write out assembly and preprocessing files for debugging purposes
+	if err := compile(ctx, networkTcProg, &dirs); err != nil {
+		scopedLog.WithField(logfields.Params, logfields.Repr(networkTcProg)).
+			WithError(err).Warn("Failed to compile")
+		return err
+	}
+	return nil
 }

pkg/datapath/loader/loader.go
@@ -47,6 +47,7 @@ const (
 
 	symbolFromEndpoint = "from-container"
 	symbolToEndpoint   = "to-container"
+	symbolFromNetwork  = "from-network"
 
 	symbolFromHostNetdevEp = "from-netdev"
 	symbolToHostNetdevEp   = "to-netdev"
@@ -366,6 +367,18 @@ func (l *Loader) reloadDatapath(ctx context.Context, ep datapath.Endpoint, dirs 
 		}
 	}
 
+	return nil
+}
+
+func (l *Loader) replaceNetworkDatapath(ctx context.Context, interfaces []string) error {
+	if err := compileNetwork(ctx); err != nil {
+		log.WithError(err).Fatal("failed to compile encryption programs")
+	}
+	for _, iface := range option.Config.EncryptInterface {
+		if err := l.replaceDatapath(ctx, iface, networkObj, symbolFromNetwork, dirIngress); err != nil {
+			log.WithField(logfields.Interface, iface).Fatal("Load encryption network failed")
+		}
+	}
 	return nil
 }
 

commit 4d6dcddcb96fe8573233fa665a1fbe6c00a42ad6

cilium: fix panic when encryptInterfaces is empty
Currently, we use encryptInterfaces[0] in EKS configuration, but this
may not be populated before its reference. So we get a panic. To fix
this case use the encryptInterface instead. But, after this change
the only remaining user of encryptInterfaces is for setting rpfilters.
This can also use the config variable though and then we can revert
use of encryptInterfaces completely.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>
Fixes: c90849124018b ("cilium: encryptInterface requires multiple interfaces in multi-dev env")

daemon/cmd/daemon_main.go
@@ -1505,8 +1505,7 @@ func (d *Daemon) initKVStore() {
 
 func runDaemon() {
 	datapathConfig := linuxdatapath.DatapathConfiguration{
-		HostDevice:        option.Config.HostDevice,
-		EncryptInterfaces: option.Config.EncryptInterface,
+		HostDevice: option.Config.HostDevice,
 	}
 
 	log.Info("Initializing daemon")

pkg/datapath/linux/datapath.go
@@ -16,10 +16,8 @@ package linux
 
 import (
 	"github.com/cilium/cilium/pkg/datapath"
-	"github.com/cilium/cilium/pkg/datapath/connector"
 	"github.com/cilium/cilium/pkg/datapath/linux/config"
 	"github.com/cilium/cilium/pkg/datapath/loader"
-	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
 // DatapathConfiguration is the static configuration of the datapath. The
@@ -27,8 +25,6 @@ import (
 type DatapathConfiguration struct {
 	// HostDevice is the name of the device to be used to access the host.
 	HostDevice string
-	// EncryptInterfaces is the list of devices used for direct ruoting encryption
-	EncryptInterfaces []string
 }
 
 type linuxDatapath struct {
@@ -53,13 +49,6 @@ func NewDatapath(cfg DatapathConfiguration, ruleManager datapath.IptablesManager
 	}
 
 	dp.node = NewNodeHandler(cfg, dp.nodeAddressing, wgAgent)
-
-	for _, iface := range cfg.EncryptInterfaces {
-		if err := connector.DisableRpFilter(iface); err != nil {
-			log.WithError(err).WithField(logfields.Interface, iface).Warn("Rpfilter could not be disabled, node to node encryption may fail")
-		}
-	}
-
 	return dp
 }
 

pkg/datapath/linux/node.go
@@ -1125,7 +1125,7 @@ func (n *linuxNodeHandler) createNodeIPSecInRoute(ip *net.IPNet) route.Route {
 	var device string
 
 	if option.Config.Tunnel == option.TunnelDisabled {
-		device = n.datapathConfig.EncryptInterfaces[0]
+		device = option.Config.EncryptInterface[0]
 	} else {
 		device = linux_defaults.TunnelDeviceName
 	}

pkg/datapath/loader/base.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/datapath/alignchecker"
+	"github.com/cilium/cilium/pkg/datapath/connector"
 	"github.com/cilium/cilium/pkg/datapath/linux/ethtool"
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
@@ -195,6 +196,7 @@ func (l *Loader) reinitializeIPSec(ctx context.Context) error {
 					}
 				}
 			}
+			option.Config.EncryptInterface = interfaces
 		}
 
 		// For the ENI ipam mode on EKS, this will be the interface that
@@ -210,6 +212,12 @@ func (l *Loader) reinitializeIPSec(ctx context.Context) error {
 
 	// No interfaces is valid in tunnel disabled case
 	if len(interfaces) != 0 {
+		for _, iface := range interfaces {
+			if err := connector.DisableRpFilter(iface); err != nil {
+				log.WithError(err).WithField(logfields.Interface, iface).Warn("Rpfilter could not be disabled, node to node encryption may fail")
+			}
+		}
+
 		if err := l.replaceNetworkDatapath(ctx, interfaces); err != nil {
 			return fmt.Errorf("failed to load encryption program: %w", err)
 		}