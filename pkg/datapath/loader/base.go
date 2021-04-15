g0odi3z2j/cilium

commit c5492b5430f04a48f28216b547b78d4e902c1340

cilium: auto-discovery pod subnets for ENI IPAM
When encryption is enabled with an IPAM using a single IP pool for the
entire cluster instead of per node we need to know those CIDRs. This is
required so we can place a decrypt/encrypt rules that covers the subnet.

In this patch lookup the CIDRs from the routeInfo and use those. Otherwise
users would need to manually modify helm charts, yamls, etc.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/loader/base.go
@@ -398,14 +398,24 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 
 		// For the ENI ipam mode on EKS, this will be the interface that
 		// the router (cilium_host) IP is associated to.
-		if len(interfaces) == 0 && option.Config.IPAM == ipamOption.IPAMENI {
-			if info := node.GetRouterInfo(); info != nil {
-				mac := info.GetMac()
-				iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
-				if err != nil {
-					log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
+		if option.Config.IPAM == ipamOption.IPAMENI {
+			if len(interfaces) == 0 {
+				if info := node.GetRouterInfo(); info != nil {
+					mac := info.GetMac()
+					iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
+					if err != nil {
+						log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
+					}
+					interfaces = append(interfaces, iface)
 				}
-				interfaces = append(interfaces, iface)
+			}
+			if len(option.Config.IPv4PodSubnets) == 0 {
+				if info := node.GetRouterInfo(); info != nil {
+					for _, c := range info.GetIPv4CIDRs() {
+						option.Config.IPv4PodSubnets = append(option.Config.IPv4PodSubnets, &c)
+					}
+				}
+				log.Info("Encryption IPv4PodSubnets in-use")
 			}
 		}
 		if err := l.replaceNetworkDatapath(ctx, interfaces); err != nil {

commit 97d82daa3f5192c5f750bcb572e1c83c8024e19a

ipsec: Fix routing CIDR iteration on EKS
When taking a reference of the iterated value in Golang, one needs to
take care to copy the value before taking a reference.

In the case of EKS (where we can have multiple VPC CIDRs, c.f. #15303)
this meant that we only the last routing CIDR was appended to
`IPv4PodSubnets`.

Fixes: c5492b5430f0 ("cilium: auto-discovery pod subnets for ENI IPAM")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/datapath/loader/base.go
@@ -204,7 +204,8 @@ func (l *Loader) reinitializeIPSec(ctx context.Context) error {
 		if len(option.Config.IPv4PodSubnets) == 0 {
 			if info := node.GetRouterInfo(); info != nil {
 				for _, c := range info.GetIPv4CIDRs() {
-					option.Config.IPv4PodSubnets = append(option.Config.IPv4PodSubnets, &c)
+					cidr := c // create a copy to be able to take a reference
+					option.Config.IPv4PodSubnets = append(option.Config.IPv4PodSubnets, &cidr)
 				}
 			}
 		}