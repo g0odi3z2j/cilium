g0odi3z2j/cilium

commit 2160dbfd969eb721621a80d9bc67b561d0fea97e

endpoint: Refactor init of EndpointDatapathConfiguration
EndpointDatapathConfiguration is initialized for every new endpoint
based on whether per-endpoint routes are enabled. As commit 1ed8adc
("health: Disable routing in BPF when per-endpoint routes are enabled")
illustrates, this can lead to error where some initializations are
updated and not others.

This commit consolidates all initializations under a new helper
function.

Signed-off-by: Paul Chaignon <paul@cilium.io>

daemon/cmd/endpoint.go
@@ -306,24 +306,20 @@ func (m *endpointCreationManager) DebugStatus() (output string) {
 // createEndpoint attempts to create the endpoint corresponding to the change
 // request that was specified.
 func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, epTemplate *models.EndpointChangeRequest) (*endpoint.Endpoint, int, error) {
-	if option.Config.EnableEndpointRoutes {
-		if epTemplate.DatapathConfiguration == nil {
-			epTemplate.DatapathConfiguration = &models.EndpointDatapathConfiguration{}
+	if epTemplate.DatapathConfiguration == nil {
+		dpConfig := endpoint.NewDatapathConfiguration()
+		epTemplate.DatapathConfiguration = &dpConfig
+	} else {
+		if option.Config.EnableEndpointRoutes {
+			epTemplate.DatapathConfiguration.InstallEndpointRoute = true
+			epTemplate.DatapathConfiguration.RequireEgressProg = true
+			disabled := false
+			epTemplate.DatapathConfiguration.RequireRouting = &disabled
+		} else {
+			epTemplate.DatapathConfiguration.InstallEndpointRoute = false
+			epTemplate.DatapathConfiguration.RequireEgressProg = false
+			epTemplate.DatapathConfiguration.RequireRouting = nil
 		}
-
-		// Indicate to insert a per endpoint route instead of routing
-		// via cilium_host interface
-		epTemplate.DatapathConfiguration.InstallEndpointRoute = true
-
-		// Since routing occurs via endpoint interface directly, BPF
-		// program is needed on that device at egress as BPF program on
-		// cilium_host interface is bypassed
-		epTemplate.DatapathConfiguration.RequireEgressProg = true
-
-		// Delegate routing to the Linux stack rather than tail-calling
-		// between BPF programs.
-		disabled := false
-		epTemplate.DatapathConfiguration.RequireRouting = &disabled
 	}
 
 	log.WithFields(logrus.Fields{

cilium-health/launch/endpoint.go
@@ -266,15 +266,8 @@ func LaunchAsEndpoint(baseCtx context.Context,
 		ip4Address = &net.IPNet{IP: healthIP, Mask: defaults.ContainerIPv4Mask}
 	}
 
-	if option.Config.EnableEndpointRoutes {
-		disabled := false
-		dpConfig := &models.EndpointDatapathConfiguration{
-			InstallEndpointRoute: true,
-			RequireEgressProg:    true,
-			RequireRouting:       &disabled,
-		}
-		info.DatapathConfiguration = dpConfig
-	}
+	dpConfig := endpoint.NewDatapathConfiguration()
+	info.DatapathConfiguration = &dpConfig
 
 	netNS, err := netns.ReplaceNetNSWithName(netNSName)
 	if err != nil {

pkg/endpoint/endpoint.go
@@ -870,6 +870,28 @@ func parseEndpoint(ctx context.Context, owner regeneration.Owner, bEp []byte) (*
 	return &ep, nil
 }
 
+// NewDatapathConfiguration return the default endpoint datapath configuration
+// based on whether per-endpoint routes are enabled.
+func NewDatapathConfiguration() models.EndpointDatapathConfiguration {
+	config := models.EndpointDatapathConfiguration{}
+	if option.Config.EnableEndpointRoutes {
+		// Indicate to insert a per endpoint route instead of routing
+		// via cilium_host interface
+		config.InstallEndpointRoute = true
+
+		// Since routing occurs via endpoint interface directly, BPF
+		// program is needed on that device at egress as BPF program on
+		// cilium_host interface is bypassed
+		config.RequireEgressProg = true
+
+		// Delegate routing to the Linux stack rather than tail-calling
+		// between BPF programs.
+		disabled := false
+		config.RequireRouting = &disabled
+	}
+	return config
+}
+
 func (e *Endpoint) LogStatus(typ StatusType, code StatusCode, msg string) {
 	e.unconditionalLock()
 	defer e.unlock()

commit e2483c6b36a69d08084a8987fb873dd891855e67

daemon: Fix the init of the endpoints' datapath config
An incorrect refactor, 2160dbf ("endpoint: Refactor init of
EndpointDatapathConfiguration"), changed the behavior of that function.
This commit fixes it to restore the intended behavior. See [1] for more
details.

1 - https://github.com/cilium/cilium/pull/15228/files#r613633603
Fixes: 2160dbf ("endpoint: Refactor init of EndpointDatapathConfiguration")
Reported-by: Joe Stringer <joe@cilium.io>
Signed-off-by: Paul Chaignon <paul@cilium.io>

daemon/cmd/endpoint.go
@@ -309,17 +309,16 @@ func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, e
 	if epTemplate.DatapathConfiguration == nil {
 		dpConfig := endpoint.NewDatapathConfiguration()
 		epTemplate.DatapathConfiguration = &dpConfig
+	}
+	if option.Config.EnableEndpointRoutes {
+		epTemplate.DatapathConfiguration.InstallEndpointRoute = true
+		epTemplate.DatapathConfiguration.RequireEgressProg = true
+		disabled := false
+		epTemplate.DatapathConfiguration.RequireRouting = &disabled
 	} else {
-		if option.Config.EnableEndpointRoutes {
-			epTemplate.DatapathConfiguration.InstallEndpointRoute = true
-			epTemplate.DatapathConfiguration.RequireEgressProg = true
-			disabled := false
-			epTemplate.DatapathConfiguration.RequireRouting = &disabled
-		} else {
-			epTemplate.DatapathConfiguration.InstallEndpointRoute = false
-			epTemplate.DatapathConfiguration.RequireEgressProg = false
-			epTemplate.DatapathConfiguration.RequireRouting = nil
-		}
+		epTemplate.DatapathConfiguration.InstallEndpointRoute = false
+		epTemplate.DatapathConfiguration.RequireEgressProg = false
+		epTemplate.DatapathConfiguration.RequireRouting = nil
 	}
 
 	log.WithFields(logrus.Fields{