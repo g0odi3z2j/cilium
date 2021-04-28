g0odi3z2j/cilium

commit 1c4c81e670a2022466694dc1513514f0673c0f51

contrib: add dual-stack support for dev VMs
With Kubernetes 1.21 enabling dual-stack support by default we can
configure the dev VMs to also have this feature enabled.

Signed-off-by: André Martins <andre@cilium.io>

contrib/vagrant/scripts/helpers.bash
@@ -54,27 +54,18 @@ esac
 
 kubernetes_master="${controllers_ips[0]}"
 
-# Default values for IPv4
+# Default values for IPv4 and IPv6
 #
 # CIDR Range for Pods in cluster.
-k8s_cluster_cidr=${K8S_CLUSTER_CIDR:-"10.16.0.0/12"} # 10.16.0.1-10.31.255.255
+k8s_cluster_cidr=${K8S_CLUSTER_CIDR:-"10.16.0.0/12,FD02::/96"} # 10.16.0.1-10.31.255.255
 # Mask size for node cidr in cluster.
-k8s_node_cidr_mask_size=${K8S_NODE_CIDR_MASK_SIZE:-"16"} # 1st Node: 10.16.0.1-10.16.255.254, 2nd Node: 10.17.0.1-10.17.255.254...
+k8s_node_cidr_v4_mask_size=${K8S_NODE_CIDR_MASK_SIZE:-"16"} # 1st Node: 10.16.0.1-10.16.255.254, 2nd Node: 10.17.0.1-10.17.255.254...
+k8s_node_cidr_v6_mask_size=${K8S_NODE_CIDR_MASK_SIZE:-"112"} # 1st Node: 10.16.0.1-10.16.255.254, 2nd Node: 10.17.0.1-10.17.255.254...
 # CIDR Range for Services in cluster.
-k8s_service_cluster_ip_range=${K8S_SERVICE_CLUSTER_IP_RANGE:-"172.20.0.0/24"}
-cluster_dns_ip=${K8S_CLUSTER_DNS_IP:-"172.20.0.10"}
-cluster_api_server_ip=${K8S_CLUSTER_API_SERVER_IP:-"172.20.0.1"}
-
-# Default values for IPv6
-#
-# CIDR Range for Pods in cluster.
-#k8s_cluster_cidr=${K8S_CLUSTER_CIDR:-"FD02::/96"} # 10.0.0.1-10.63.255.254
-# Mask size for node cidr in cluster.
-#k8s_node_cidr_mask_size=${K8S_NODE_CIDR_MASK_SIZE:-"112"} # 1st Node: 10.0.0.1-10.0.255.254, 2nd Node: 10.1.0.1-10.1.255.254...
-# CIDR Range for Services in cluster.
-#k8s_service_cluster_ip_range=${K8S_SERVICE_CLUSTER_IP_RANGE:-"FD03::/112"}
-#cluster_dns_ip=${K8S_CLUSTER_DNS_IP:-"FD03::A"}
-#cluster_api_server_ip=${K8S_CLUSTER_API_SERVER_IP:-"FD03::1"}
+k8s_service_cluster_ip_range=${K8S_SERVICE_CLUSTER_IP_RANGE:-"172.20.0.0/24,FD03::/112"}
+cluster_dns_ip=${K8S_CLUSTER_DNS_IP:-"172.20.0.10,FD03::A"}
+cluster_api_server_ipv4=${K8S_CLUSTER_API_SERVER_IPV4:-"172.20.0.1"}
+cluster_api_server_ipv6=${K8S_CLUSTER_API_SERVER_IPV6:-"FD03::1"}
 
 k8s_version="v1.21.0"
 etcd_version="v3.4.13"

contrib/vagrant/scripts/02-install-kubernetes-master.sh
@@ -136,7 +136,8 @@ ExecStart=/usr/bin/kube-controller-manager \\
   --configure-cloud-routes=false \\
   --kubeconfig='/var/lib/kubernetes/controller-manager.kubeconfig' \\
   --leader-elect=true \\
-  --node-cidr-mask-size ${k8s_node_cidr_mask_size} \\
+  --node-cidr-mask-size-ipv4=${k8s_node_cidr_v4_mask_size} \\
+  --node-cidr-mask-size-ipv6=${k8s_node_cidr_v6_mask_size} \\
   --use-service-account-credentials \\
   --service-account-private-key-file='/var/lib/kubernetes/k8s-controller-manager-sa-key.pem' \\
   --service-cluster-ip-range=${k8s_service_cluster_ip_range} \\

contrib/vagrant/scripts/cert-gen-helpers.bash
@@ -74,7 +74,8 @@ generate_server_certs() {
     "${cli_name}",
     "${master_hostname}",
     "${master_ip}",
-    "${cluster_api_server_ip}",
+    "${cluster_api_server_ipv4}",
+    "${cluster_api_server_ipv6}",
     "${cli_name}.cluster.default"
   ],
   "key": {

commit f5550de2a8761e0e426531b45702f7ee26a00c3b

contrib: fix dual-stack support in dev VMs
Fixes: 1c4c81e670a2 ("contrib: add dual-stack support for dev VMs")
Signed-off-by: André Martins <andre@cilium.io>

contrib/vagrant/deployments/coredns-svc.yaml.sed
@@ -17,7 +17,13 @@ metadata:
 spec:
   selector:
     k8s-app: kube-dns
+  ipFamilies:
+    - IPv4
+    - IPv6
   clusterIP: $DNS_SERVER_IP
+  clusterIPs:
+  - $DNS_SERVER_IP
+  - $DNS_SERVER_IPV6
   ports:
   - name: dns
     port: 53

contrib/vagrant/scripts/03-install-kubernetes-worker.sh
@@ -333,7 +333,7 @@ ExecStartPre=/bin/bash -c ' \\
 ExecStart=/usr/bin/kubelet \\
   --client-ca-file=/var/lib/kubelet/ca-k8s.pem \\
   --cloud-provider= \\
-  --cluster-dns=${cluster_dns_ip} \\
+  --cluster-dns=${cluster_dns_ip},${cluster_dns_ipv6} \\
   --cluster-domain=cluster.local \\
   --container-runtime=${container_runtime_kubelet} \\
   ${container_runtime_endpoint} \\

contrib/vagrant/scripts/06-install-coredns.sh
@@ -18,7 +18,7 @@ svc_file="${deployments_dir}/coredns-svc.yaml"
 controller_file="${deployments_dir}/coredns-controller.yaml"
 sa_file="${deployments_dir}/coredns-sa.yaml"
 
-sed "s/\$DNS_SERVER_IP/${cluster_dns_ip}/" "${svc_file}.sed" > "${svc_file}"
+sed "s/\$DNS_SERVER_IPV6/${cluster_dns_ipv6}/;s/\$DNS_SERVER_IP/${cluster_dns_ip}/" "${svc_file}.sed" > "${svc_file}"
 
 sed -e "s/\$DNS_DOMAIN/cluster.local/" \
     "${cm_file}.sed" > "${cm_file}"

contrib/vagrant/scripts/helpers.bash
@@ -10,12 +10,16 @@
 #   K8S_CLUSTER_CIDR the cluster cidr to be used in kube-controller-manager
 #       cluster-cidr option
 #   K8S_NODE_CIDR_MASK_SIZE the node cidr to be used in kube-controller-manager
-#       node-cidr-mask-size option
+#       node-cidr-mask-size-ipv4 option
+#   K8S_NODE_CIDR_V6_MASK_SIZE the node cidr to be used in kube-controller-manager
+#       node-cidr-mask-size-ipv6 option
 #   K8S_SERVICE_CLUSTER_IP_RANGE the service cluster IP range to be used in
 #       kube-controller-manager service-cluster-ip-range option
 #   K8S_CLUSTER_DNS_IP the kubedns service IP to be set up in kube-dns service
 #       spec file
 #   K8S_CLUSTER_API_SERVER_IP the cluster api service IP to be set up in the
+#       certificates generated
+#   K8S_CLUSTER_API_SERVER_IPV6 the cluster api service IPv6 to be set up in the
 #       certificates generated
 #   WGET, if set https_proxy, it will set https_proxy for the command's wget,
 #       otherwise alias for wget
@@ -60,10 +64,11 @@ kubernetes_master="${controllers_ips[0]}"
 k8s_cluster_cidr=${K8S_CLUSTER_CIDR:-"10.16.0.0/12,FD02::/96"} # 10.16.0.1-10.31.255.255
 # Mask size for node cidr in cluster.
 k8s_node_cidr_v4_mask_size=${K8S_NODE_CIDR_MASK_SIZE:-"16"} # 1st Node: 10.16.0.1-10.16.255.254, 2nd Node: 10.17.0.1-10.17.255.254...
-k8s_node_cidr_v6_mask_size=${K8S_NODE_CIDR_MASK_SIZE:-"112"} # 1st Node: 10.16.0.1-10.16.255.254, 2nd Node: 10.17.0.1-10.17.255.254...
+k8s_node_cidr_v6_mask_size=${K8S_NODE_CIDR_V6_MASK_SIZE:-"112"} # 1st Node: 10.16.0.1-10.16.255.254, 2nd Node: 10.17.0.1-10.17.255.254...
 # CIDR Range for Services in cluster.
 k8s_service_cluster_ip_range=${K8S_SERVICE_CLUSTER_IP_RANGE:-"172.20.0.0/24,FD03::/112"}
-cluster_dns_ip=${K8S_CLUSTER_DNS_IP:-"172.20.0.10,FD03::A"}
+cluster_dns_ip=${K8S_CLUSTER_DNS_IP:-"172.20.0.10"}
+cluster_dns_ipv6=${K8S_CLUSTER_DNS_IPV6:-"FD03::A"}
 cluster_api_server_ipv4=${K8S_CLUSTER_API_SERVER_IPV4:-"172.20.0.1"}
 cluster_api_server_ipv6=${K8S_CLUSTER_API_SERVER_IPV6:-"FD03::1"}
 

contrib/vagrant/start.sh
@@ -200,24 +200,14 @@ function write_k8s_install() {
     k8s_dir="${1}"
     filename="${2}"
     filename_2nd_half="${3}"
-    if [[ -n "${IPV6_EXT}" ]]; then
-        # The k8s cluster cidr will be /80
-        # it can be any value as long it's lower than /96
-        # k8s will assign each node a cidr for example:
-        #   master  : FD02::0:0:0/96
-        #   worker 1: FD02::1:0:0/96
-        #   worker 1: FD02::2:0:0/96
-        k8s_cluster_cidr+="FD02::/80"
-        k8s_node_cidr_mask_size="96"
-        k8s_service_cluster_ip_range="FD03::/112"
-        k8s_cluster_api_server_ip="FD03::1"
-        k8s_cluster_dns_ip="FD03::A"
-    fi
-    k8s_cluster_cidr=${k8s_cluster_cidr:-"10.16.0.0/12"}
+    k8s_cluster_cidr=${k8s_cluster_cidr:-"10.16.0.0/12,FD02::/80"}
     k8s_node_cidr_mask_size=${k8s_node_cidr_mask_size:-"16"}
-    k8s_service_cluster_ip_range=${k8s_service_cluster_ip_range:-"172.20.0.0/24"}
+    k8s_node_cidr_v6_mask_size=${k8s_node_cidr_v6_mask_size:-"96"}
+    k8s_service_cluster_ip_range=${k8s_service_cluster_ip_range:-"172.20.0.0/24,FD03::/112"}
     k8s_cluster_api_server_ip=${k8s_cluster_api_server_ip:-"172.20.0.1"}
+    k8s_cluster_api_server_ipv6=${k8s_cluster_api_server_ipv6:-"FD03::1"}
     k8s_cluster_dns_ip=${k8s_cluster_dns_ip:-"172.20.0.10"}
+    k8s_cluster_dns_ipv6=${k8s_cluster_dns_ipv6:-"FD03::A"}
 
     cat <<EOF >> "${filename}"
 # K8s
@@ -225,9 +215,12 @@ k8s_path="/home/vagrant/go/src/github.com/cilium/cilium/contrib/vagrant/scripts"
 export IPV6_EXT="${IPV6_EXT}"
 export K8S_CLUSTER_CIDR="${k8s_cluster_cidr}"
 export K8S_NODE_CIDR_MASK_SIZE="${k8s_node_cidr_mask_size}"
+export K8S_NODE_CIDR_V6_MASK_SIZE="${k8s_node_cidr_v6_mask_size}"
 export K8S_SERVICE_CLUSTER_IP_RANGE="${k8s_service_cluster_ip_range}"
 export K8S_CLUSTER_API_SERVER_IP="${k8s_cluster_api_server_ip}"
+export K8S_CLUSTER_API_SERVER_IPV6="${k8s_cluster_api_server_ipv6}"
 export K8S_CLUSTER_DNS_IP="${k8s_cluster_dns_ip}"
+export K8S_CLUSTER_DNS_IPV6="${k8s_cluster_dns_ipv6}"
 export RUNTIME="${RUNTIME}"
 export INSTALL="${INSTALL}"
 # Always do installation if RELOAD is not set
@@ -271,9 +264,12 @@ k8s_path="/home/vagrant/go/src/github.com/cilium/cilium/contrib/vagrant/scripts"
 export IPV6_EXT="${IPV6_EXT}"
 export K8S_CLUSTER_CIDR="${k8s_cluster_cidr}"
 export K8S_NODE_CIDR_MASK_SIZE="${k8s_node_cidr_mask_size}"
+export K8S_NODE_CIDR_V6_MASK_SIZE="${k8s_node_cidr_v6_mask_size}"
 export K8S_SERVICE_CLUSTER_IP_RANGE="${k8s_service_cluster_ip_range}"
 export K8S_CLUSTER_API_SERVER_IP="${k8s_cluster_api_server_ip}"
+export K8S_CLUSTER_API_SERVER_IPV6="${k8s_cluster_api_server_ipv6}"
 export K8S_CLUSTER_DNS_IP="${k8s_cluster_dns_ip}"
+export K8S_CLUSTER_DNS_IPV6="${k8s_cluster_dns_ipv6}"
 export RUNTIME="${RUNTIME}"
 export K8STAG="${VM_BASENAME}"
 export NWORKERS="${NWORKERS}"