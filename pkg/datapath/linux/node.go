g0odi3z2j/cilium

commit c5492b5430f04a48f28216b547b78d4e902c1340

cilium: auto-discovery pod subnets for ENI IPAM
When encryption is enabled with an IPAM using a single IP pool for the
entire cluster instead of per node we need to know those CIDRs. This is
required so we can place a decrypt/encrypt rules that covers the subnet.

In this patch lookup the CIDRs from the routeInfo and use those. Otherwise
users would need to manually modify helm charts, yamls, etc.

Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/loader/base.go
@@ -398,14 +398,24 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 
 		// For the ENI ipam mode on EKS, this will be the interface that
 		// the router (cilium_host) IP is associated to.
-		if len(interfaces) == 0 && option.Config.IPAM == ipamOption.IPAMENI {
-			if info := node.GetRouterInfo(); info != nil {
-				mac := info.GetMac()
-				iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
-				if err != nil {
-					log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
+		if option.Config.IPAM == ipamOption.IPAMENI {
+			if len(interfaces) == 0 {
+				if info := node.GetRouterInfo(); info != nil {
+					mac := info.GetMac()
+					iface, err := linuxrouting.RetrieveIfaceNameFromMAC(mac.String())
+					if err != nil {
+						log.WithError(err).WithField("mac", mac).Fatal("Failed to set encrypt interface in the ENI ipam mode")
+					}
+					interfaces = append(interfaces, iface)
 				}
-				interfaces = append(interfaces, iface)
+			}
+			if len(option.Config.IPv4PodSubnets) == 0 {
+				if info := node.GetRouterInfo(); info != nil {
+					for _, c := range info.GetIPv4CIDRs() {
+						option.Config.IPv4PodSubnets = append(option.Config.IPv4PodSubnets, &c)
+					}
+				}
+				log.Info("Encryption IPv4PodSubnets in-use")
 			}
 		}
 		if err := l.replaceNetworkDatapath(ctx, interfaces); err != nil {

commit 02e46746a3f852c06e8a10d581e8991cf794eeb0

cilium: encryption, nodeSubnets are populated before auto-discovery
In order to push auto-discovery of the subnet into the nodeSubent fields we
need to do the discovery early enough in the setup that the subnets exist.
At the moment we do the auto-discovery too late and the values are not
pushed into the nodeConfiguration so we miss updating ipsec route table
correctly.

Rather than overwrite the config options this patch pushes the code into
the nodeConfigurationChanged() and updates the node configuration directly.
This has two advantages: we avoid stomping on user config and then
we also catch any subnet updates.

Fixes: c5492b5430f04 ("cilium: auto-discovery pod subnets for ENI IPAM")
Signed-off-by: John Fastabend <john.fastabend@gmail.com>

pkg/datapath/linux/node.go
@@ -30,11 +30,13 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/linux/ipsec"
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
+	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/neighborsmap"
 	"github.com/cilium/cilium/pkg/maps/tunnel"
 	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/node"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 
@@ -1373,6 +1375,19 @@ func (n *linuxNodeHandler) NodeConfigurationChanged(newConfig datapath.LocalNode
 	n.updateOrRemoveNodeRoutes(prevConfig.AuxiliaryPrefixes, newConfig.AuxiliaryPrefixes, true)
 
 	if newConfig.EnableIPSec {
+		// For the ENI ipam mode on EKS, this will be the interface that
+		// the router (cilium_host) IP is associated to.
+		if option.Config.IPAM == ipamOption.IPAMENI && len(option.Config.IPv4PodSubnets) == 0 {
+			if info := node.GetRouterInfo(); info != nil {
+				var ipv4PodSubnets []*net.IPNet
+				for _, c := range info.GetIPv4CIDRs() {
+					cidr := c // create a copy to be able to take a reference
+					ipv4PodSubnets = append(ipv4PodSubnets, &cidr)
+				}
+				n.nodeConfig.IPv4PodSubnets = ipv4PodSubnets
+			}
+		}
+
 		if err := n.replaceHostRules(); err != nil {
 			log.WithError(err).Warning("Cannot replace Host rules")
 		}

pkg/datapath/loader/base.go
@@ -198,17 +198,6 @@ func (l *Loader) reinitializeIPSec(ctx context.Context) error {
 			}
 			option.Config.EncryptInterface = interfaces
 		}
-
-		// For the ENI ipam mode on EKS, this will be the interface that
-		// the router (cilium_host) IP is associated to.
-		if len(option.Config.IPv4PodSubnets) == 0 {
-			if info := node.GetRouterInfo(); info != nil {
-				for _, c := range info.GetIPv4CIDRs() {
-					cidr := c // create a copy to be able to take a reference
-					option.Config.IPv4PodSubnets = append(option.Config.IPv4PodSubnets, &cidr)
-				}
-			}
-		}
 	}
 
 	// No interfaces is valid in tunnel disabled case