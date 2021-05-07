g0odi3z2j/cilium

commit 9a2cfd69e492580ec5b4f94c752b8c011d260d30

node-neigh: Do not inc neighbor refcount for the same node
(*linuxNodeHandler).NodeUpdate() can be called multiple times for the
same remote node. This could lead to the neighbor refcount being
incremented if the nextHop of the node had not changed. The consequence
of the invalid refcount is that upon (*linuxNodeHandler).NodeDelete(),
a related neighbor entry might not be deleted.

Fix this by checking whether the node has been previously considered for
arpinging. An alternative solution would be to pass the firstAddition
param to insertNeighbor(). However, as we are going to merge
refreshNeighbor(), the taken approach is more robust and requires less
assertions in the upcoming merged method.

Fixes: e970929ecf6 ("node: Remove permanent ARP entry when remote node is deleted")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/datapath/linux/node.go
@@ -609,10 +609,36 @@ func (n *linuxNodeHandler) insertNeighbor(newNode *nodeTypes.Node, ifaceName str
 	}
 
 	nextHopStr := nextHopIPv4.String()
+	if existingNextHopStr, found := n.neighNextHopByNode[newNode.Identity()]; found {
+		if existingNextHopStr == nextHopStr {
+			// We already know about the nextHop of the given newNode. Can happen
+			// when insertNeighbor is called by NodeUpdate multiple times for
+			// the same node.
+			return
+		}
+		// nextHop has changed, so remove the old one.
+		if n.neighNextHopRefCount.Delete(existingNextHopStr) {
+			neigh, found := n.neighByNextHop[existingNextHopStr]
+			if found {
+				delete(n.neighByNextHop, nextHopStr)
+				if err := netlink.NeighDel(neigh); err != nil {
+					log.WithFields(logrus.Fields{
+						logfields.IPAddr:       neigh.IP,
+						logfields.HardwareAddr: neigh.HardwareAddr,
+						logfields.LinkIndex:    neigh.LinkIndex,
+					}).WithError(err).Warn("Failed to remove neighbor entry")
+				}
+				if option.Config.NodePortHairpin {
+					neighborsmap.NeighRetire(net.ParseIP(existingNextHopStr))
+				}
+			}
+		}
+	}
+
 	n.neighNextHopByNode[newNode.Identity()] = nextHopStr
 	_, found := n.neighByNextHop[nextHopStr]
 
-	// nextHop hasn't been arpinged before OR the arping failed
+	// nextHop hasn't been arpinged before OR it was but the arping failed
 	if n.neighNextHopRefCount.Add(nextHopStr) || !found {
 		linkAttr, err := netlink.LinkByName(ifaceName)
 		if err != nil {

pkg/datapath/linux/node_linux_test.go
@@ -1025,6 +1025,11 @@ func (s *linuxPrivilegedIPv4OnlyTestSuite) TestArpPingHandling(c *check.C) {
 	}
 	err = linuxNodeHandler.NodeAdd(nodev1)
 	c.Assert(err, check.IsNil)
+	// Insert the same node second time. This should not increment refcount for
+	// the same nextHop. We test it by checking that NodeDelete has removed the
+	// related neigh entry.
+	err = linuxNodeHandler.NodeAdd(nodev1)
+	c.Assert(err, check.IsNil)
 
 	// Check whether an arp entry for nodev1 IP addr (=veth1) was added
 	neighs, err := netlink.NeighList(veth0.Attrs().Index, netlink.FAMILY_V4)

commit 07409ddb7f741e24e6606e0eedd55016db7d12bd

node-neigh: Fix obsolete nextHop removal
The impact of the wrong removal was very small - unnecessary neigh
insert via netlink and dangling unused value. This could have happened
under very rare circumstances when a next hop to a node had changed.

Fixes: 9a2cfd69e492 ("node-neigh: Do not inc neighbor refcount for the same node")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/datapath/linux/node.go
@@ -719,7 +719,7 @@ func (n *linuxNodeHandler) insertNeighbor(ctx context.Context, newNode *nodeType
 						logfields.LinkIndex:    neigh.LinkIndex,
 					}).WithError(err).Info("Unable to remove neighbor entry")
 				}
-				delete(n.neighByNextHop, nextHopStr)
+				delete(n.neighByNextHop, existingNextHopStr)
 				delete(n.neighLastPingByNextHop, existingNextHopStr)
 				if option.Config.NodePortHairpin {
 					neighborsmap.NeighRetire(net.ParseIP(existingNextHopStr))