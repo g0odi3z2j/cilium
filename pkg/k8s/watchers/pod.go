g0odi3z2j/cilium

commit 5a6777960d75767e481e261927f832d9a2ecd19a

pkg/k8s: ignore certain ipcache errors
Certain errors can be ignored when upserting IP addresses into the
ipcache. Thus, there is need to populate the logs with some red herring
warnings.

Fixes: 0fd36979bf24 ("k8s/watchers: don't silently ignore (*K8sWatcher).updatePodHostData error")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/pod.go
@@ -755,7 +755,21 @@ func (k *K8sWatcher) updatePodHostData(oldPod, newPod *slim_corev1.Pod, oldPodIP
 			namedPortsChanged = true
 		}
 		if err != nil {
-			errs = append(errs, fmt.Sprintf("ipcache entry for podIP %s owned by kvstore or agent: %s", podIP, err))
+			// It is expected to receive error overwrites where the existing
+			// source is the KVStore, this can happen as KVStore event
+			// propagation can usually be faster than k8s event propagation.
+			// It is also expected to receive an error overwrite where the
+			// existing source is local since cilium-agent receives events for
+			// local pods.
+			if !errors.Is(err, &ipcache.ErrOverwrite{
+				ExistingSrc: source.KVStore,
+				NewSrc:      source.Kubernetes,
+			}) || !errors.Is(err, &ipcache.ErrOverwrite{
+				ExistingSrc: source.Local,
+				NewSrc:      source.Kubernetes,
+			}) {
+				errs = append(errs, fmt.Sprintf("ipcache entry for podIP %s: %s", podIP, err))
+			}
 		}
 	}
 	if len(errs) != 0 {

daemon/cmd/status_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2021 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

pkg/ipcache/ipcache_test.go
@@ -17,6 +17,7 @@
 package ipcache
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"sort"
@@ -70,7 +71,7 @@ func (s *IPCacheTestSuite) TestIPCache(c *C) {
 		ID:     identity,
 		Source: source.Kubernetes,
 	})
-	c.Assert(err, Not(IsNil))
+	c.Assert(errors.Is(err, &ErrOverwrite{NewSrc: source.Kubernetes}), Equals, true)
 
 	IPIdentityCache.Upsert(endpointIP, nil, 0, nil, Identity{
 		ID:     identity,

pkg/k8s/watchers/cilium_endpoint.go
@@ -194,7 +194,7 @@ func (k *K8sWatcher) endpointUpdated(oldEndpoint, endpoint *types.CiliumEndpoint
 	for _, pair := range endpoint.Networking.Addressing {
 		if pair.IPV4 != "" {
 			ipsAdded = append(ipsAdded, pair.IPV4)
-				portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV4, nodeIP, encryptionKey, k8sMeta,
+			portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV4, nodeIP, encryptionKey, k8sMeta,
 				ipcache.Identity{ID: id, Source: source.CustomResource})
 			if portsChanged {
 				namedPortsChanged = true
@@ -203,7 +203,7 @@ func (k *K8sWatcher) endpointUpdated(oldEndpoint, endpoint *types.CiliumEndpoint
 
 		if pair.IPV6 != "" {
 			ipsAdded = append(ipsAdded, pair.IPV6)
-				portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV6, nodeIP, encryptionKey, k8sMeta,
+			portsChanged, _ := ipcache.IPIdentityCache.Upsert(pair.IPV6, nodeIP, encryptionKey, k8sMeta,
 				ipcache.Identity{ID: id, Source: source.CustomResource})
 			if portsChanged {
 				namedPortsChanged = true

commit 6eac7f26f83f390aa0c96ad37c280e479eeb6605

pkg/k8s: set right logic to ignore ipcache errors
The logic to ignore errors is inverted and the errors are still being
printed as warnings. This commit inverts the logic so that only relevant
warnings are printed.

Fixes: 5a6777960d75 ("pkg/k8s: ignore certain ipcache errors")
Fixes: 1cb2c908ed21 ("pkg/k8s: ignore overwrite source "custom-resource" with "k8s" errors")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/pod.go
@@ -765,10 +765,10 @@ func (k *K8sWatcher) updatePodHostData(oldPod, newPod *slim_corev1.Pod, oldPodIP
 			if !errors.Is(err, &ipcache.ErrOverwrite{
 				ExistingSrc: source.KVStore,
 				NewSrc:      source.Kubernetes,
-			}) || !errors.Is(err, &ipcache.ErrOverwrite{
+			}) && !errors.Is(err, &ipcache.ErrOverwrite{
 				ExistingSrc: source.Local,
 				NewSrc:      source.Kubernetes,
-			}) || !errors.Is(err, &ipcache.ErrOverwrite{
+			}) && !errors.Is(err, &ipcache.ErrOverwrite{
 				ExistingSrc: source.CustomResource,
 				NewSrc:      source.Kubernetes,
 			}) {