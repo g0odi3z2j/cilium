g0odi3z2j/cilium

commit 88cd1f7bb5520c8701225d0cc94628f261f3be13

pkg/k8s: remove node.cilium.io/agent-not-ready taint from nodes
In some environments, usually in cloud providers, Cilium is deployed on
a cluster that has a pre-installed CNI plugin installed. This default
installation can cause Pods to have their network managed by that CNI
plugin. This results in certain Pods from being unmanaged by Cilium
which are often undetected by users.

To guarantee that Pods are only scheduled when Cilium has configured the
node, Cilium will remove an existing taint used for this purpose:
'node.cilium.io/agent-not-ready'. This taint should be created on
cluster creation for users that want to use this functionality.

Suggested-by: Timo Beckers <timo@isovalent.com>
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/node.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020 Authors of Cilium
+// Copyright 2016-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@ import (
 	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/controller"
+	ciliumio "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node/addressing"
@@ -212,11 +213,29 @@ func ParseNode(k8sNode *slim_corev1.Node, source source.Source) *nodeTypes.Node 
 // setNodeNetworkUnavailableFalse sets Kubernetes NodeNetworkUnavailable to
 // false as Cilium is managing the network connectivity.
 // https://kubernetes.io/docs/concepts/architecture/nodes/#condition
-func setNodeNetworkUnavailableFalse(c kubernetes.Interface, nodeName string) error {
+func setNodeNetworkUnavailableFalse(ctx context.Context, c kubernetes.Interface, nodeGetter nodeGetter, nodeName string) error {
+	n, err := nodeGetter.GetK8sNode(ctx, nodeName)
+	if err != nil {
+		return err
+	}
+
+	const reason = "CiliumIsUp"
+
+	for _, condition := range n.Status.Conditions {
+		if condition.Type == corev1.NodeNetworkUnavailable &&
+			condition.Status == corev1.ConditionFalse &&
+			condition.Reason == reason {
+
+			// No need to update node condition as it is already available in
+			// the node status.
+			return nil
+		}
+	}
+
 	condition := corev1.NodeCondition{
 		Type:               corev1.NodeNetworkUnavailable,
 		Status:             corev1.ConditionFalse,
-		Reason:             "CiliumIsUp",
+		Reason:             reason,
 		Message:            "Cilium is running on this node",
 		LastTransitionTime: metav1.Now(),
 		LastHeartbeatTime:  metav1.Now(),
@@ -230,15 +249,68 @@ func setNodeNetworkUnavailableFalse(c kubernetes.Interface, nodeName string) err
 	return err
 }
 
+// removeNodeTaint removes the AgentNotReadyNodeTaint allowing for pods to be
+// scheduled once Cilium is setup. Mostly used in cloud providers to prevent
+// existing CNI plugins from managing pods.
+func removeNodeTaint(ctx context.Context, c kubernetes.Interface, nodeGetter nodeGetter, nodeName string) error {
+	k8sNode, err := nodeGetter.GetK8sNode(ctx, nodeName)
+	if err != nil {
+		return err
+	}
+
+	var taintFound bool
+
+	var taints []corev1.Taint
+	for _, taint := range k8sNode.Spec.Taints {
+		if taint.Key != ciliumio.AgentNotReadyNodeTaint {
+			taints = append(taints, taint)
+		} else {
+			taintFound = true
+		}
+	}
+
+	// No cilium taints found
+	if !taintFound {
+		log.WithFields(logrus.Fields{
+			logfields.NodeName: nodeName,
+			"taint":            ciliumio.AgentNotReadyNodeTaint,
+		}).Debug("Taint not found in node")
+		return nil
+	}
+	log.WithFields(logrus.Fields{
+		logfields.NodeName: nodeName,
+		"taint":            ciliumio.AgentNotReadyNodeTaint,
+	}).Debug("Removing Node Taint")
+
+	k8sNode.Spec.Taints = taints
+
+	_, err = c.CoreV1().Nodes().Update(ctx, k8sNode, metav1.UpdateOptions{})
+	return err
+}
+
+const (
+	markK8sNodeReadyControllerName = "mark-k8s-node-as-available"
+)
+
 // MarkNodeReady marks the Kubernetes node resource as ready from a networking
 // perspective
-func (k8sCli K8sClient) MarkNodeReady(nodeName string) {
+func (k8sCli K8sClient) MarkNodeReady(nodeGetter nodeGetter, nodeName string) {
 	log.WithField(logfields.NodeName, nodeName).Debug("Setting NetworkUnavailable=false")
 
-	controller.NewManager().UpdateController("mark-k8s-node-as-available",
+	controller.NewManager().UpdateController(markK8sNodeReadyControllerName,
 		controller.ControllerParams{
-			DoFunc: func(_ context.Context) error {
-				return setNodeNetworkUnavailableFalse(k8sCli, nodeName)
+			DoFunc: func(ctx context.Context) error {
+				err := removeNodeTaint(ctx, k8sCli, nodeGetter, nodeName)
+				if err != nil {
+					return err
+				}
+				return setNodeNetworkUnavailableFalse(ctx, k8sCli, nodeGetter, nodeName)
 			},
 		})
+}
+
+// ReMarkNodeReady re-triggers the controller set by 'MarkNodeReady'. If
+// 'MarkNodeReady' has not been executed yet, calling this function be a no-op.
+func (k8sCli K8sClient) ReMarkNodeReady() {
+	controller.NewManager().TriggerController(markK8sNodeReadyControllerName)
 }

pkg/k8s/watchers/node.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/comparator"
 	"github.com/cilium/cilium/pkg/k8s"
+	ciliumio "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	"github.com/cilium/cilium/pkg/node"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
@@ -31,7 +32,6 @@ import (
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -43,7 +43,7 @@ var (
 
 // NodesInit initializes a k8s watcher for node events belonging to the node
 // where Cilium is running.
-func (k *K8sWatcher) NodesInit(k8sClient kubernetes.Interface) {
+func (k *K8sWatcher) NodesInit(k8sClient *k8s.K8sClient) {
 	onceNodeInitStart.Do(func() {
 		nodeStore, nodeController := informer.NewInformer(
 			cache.NewListWatchFromClient(k8sClient.CoreV1().RESTClient(),
@@ -55,6 +55,9 @@ func (k *K8sWatcher) NodesInit(k8sClient kubernetes.Interface) {
 					var valid bool
 					if node := k8s.ObjToV1Node(obj); node != nil {
 						valid = true
+						if hasAgentNotReadyTaint(node) {
+							k8sClient.ReMarkNodeReady()
+						}
 						err := k.updateK8sNodeV1(nil, node)
 						k.K8sEventProcessed(metricNode, metricCreate, err == nil)
 					}
@@ -65,6 +68,10 @@ func (k *K8sWatcher) NodesInit(k8sClient kubernetes.Interface) {
 					if oldNode := k8s.ObjToV1Node(oldObj); oldNode != nil {
 						valid = true
 						if newNode := k8s.ObjToV1Node(newObj); newNode != nil {
+							if hasAgentNotReadyTaint(newNode) {
+								k8sClient.ReMarkNodeReady()
+							}
+
 							oldNodeLabels := oldNode.GetLabels()
 							newNodeLabels := newNode.GetLabels()
 							if comparator.MapStringEquals(oldNodeLabels, newNodeLabels) {
@@ -87,6 +94,17 @@ func (k *K8sWatcher) NodesInit(k8sClient kubernetes.Interface) {
 		go nodeController.Run(wait.NeverStop)
 		k.k8sAPIGroups.AddAPI(k8sAPIGroupNodeV1Core)
 	})
+}
+
+// hasAgentNotReadyTaint returns true if the given node has the Cilium Agen
+// Not Ready Node Taint.
+func hasAgentNotReadyTaint(k8sNode *v1.Node) bool {
+	for _, taint := range k8sNode.Spec.Taints {
+		if taint.Key == ciliumio.AgentNotReadyNodeTaint {
+			return true
+		}
+	}
+	return false
 }
 
 func (k *K8sWatcher) updateK8sNodeV1(oldK8sNode, newK8sNode *v1.Node) error {

daemon/cmd/daemon_main.go
@@ -1722,7 +1722,7 @@ func runDaemon() {
 
 	if k8s.IsEnabled() {
 		bootstrapStats.k8sInit.Start()
-		k8s.Client().MarkNodeReady(nodeTypes.GetName())
+		k8s.Client().MarkNodeReady(d.k8sWatcher, nodeTypes.GetName())
 		bootstrapStats.k8sInit.End(true)
 	}
 

pkg/k8s/apis/cilium.io/const.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 Authors of Cilium
+// Copyright 2016-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -64,6 +64,12 @@ const (
 	CiliumK8sAnnotationPrefix = "cilium.io/"
 	// CiliumIdentityAnnotationDeprecated is the previous annotation key used to map to an endpoint's security identity.
 	CiliumIdentityAnnotationDeprecated = "cilium-identity"
+
+	// AgentNotReadyNodeTaint is a node taint which prevents pods from being
+	// scheduled. Once cilium is setup it is removed from the node. Mostly
+	// used in cloud providers to prevent existing CNI plugins from managing
+	// pods.
+	AgentNotReadyNodeTaint = "node." + CiliumK8sAnnotationPrefix + "agent-not-ready"
 )
 
 const (

commit 0ea7b5d5c9c9d16c0c3544e66ff8935ae8f0e457

pkg/k8s: re-add CiliumIsUp Node condition even if removed
If the CiliumIsUp Node condition is removed either by accident or due
a concurrency issue when updating the node from different entities,
Cilium is now able to re-add the node condition back to the node again.

Fixes: 88cd1f7bb552 ("pkg/k8s: remove node.cilium.io/agent-not-ready taint from nodes")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/node.go
@@ -38,6 +38,12 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+const (
+	// ciliumNodeConditionReason is the condition name used by Cilium to set
+	// when the Network is setup in the node.
+	ciliumNodeConditionReason = "CiliumIsUp"
+)
+
 // ParseNodeAddressType converts a Kubernetes NodeAddressType to a Cilium
 // NodeAddressType. If the Kubernetes NodeAddressType does not have a
 // corresponding Cilium AddressType, returns an error.
@@ -219,23 +225,14 @@ func setNodeNetworkUnavailableFalse(ctx context.Context, c kubernetes.Interface,
 		return err
 	}
 
-	const reason = "CiliumIsUp"
-
-	for _, condition := range n.Status.Conditions {
-		if condition.Type == corev1.NodeNetworkUnavailable &&
-			condition.Status == corev1.ConditionFalse &&
-			condition.Reason == reason {
-
-			// No need to update node condition as it is already available in
-			// the node status.
-			return nil
-		}
+	if HasCiliumIsUpCondition(n) {
+		return nil
 	}
 
 	condition := corev1.NodeCondition{
 		Type:               corev1.NodeNetworkUnavailable,
 		Status:             corev1.ConditionFalse,
-		Reason:             reason,
+		Reason:             ciliumNodeConditionReason,
 		Message:            "Cilium is running on this node",
 		LastTransitionTime: metav1.Now(),
 		LastHeartbeatTime:  metav1.Now(),
@@ -247,6 +244,19 @@ func setNodeNetworkUnavailableFalse(ctx context.Context, c kubernetes.Interface,
 	patch := []byte(fmt.Sprintf(`{"status":{"conditions":%s}}`, raw))
 	_, err = c.CoreV1().Nodes().PatchStatus(context.TODO(), nodeName, patch)
 	return err
+}
+
+// HasCiliumIsUpCondition returns true if the given k8s node has the cilium node
+// condition set.
+func HasCiliumIsUpCondition(n *corev1.Node) bool {
+	for _, condition := range n.Status.Conditions {
+		if condition.Type == corev1.NodeNetworkUnavailable &&
+			condition.Status == corev1.ConditionFalse &&
+			condition.Reason == ciliumNodeConditionReason {
+			return true
+		}
+	}
+	return false
 }
 
 // removeNodeTaint removes the AgentNotReadyNodeTaint allowing for pods to be

pkg/k8s/watchers/node.go
@@ -55,7 +55,7 @@ func (k *K8sWatcher) NodesInit(k8sClient *k8s.K8sClient) {
 					var valid bool
 					if node := k8s.ObjToV1Node(obj); node != nil {
 						valid = true
-						if hasAgentNotReadyTaint(node) {
+						if hasAgentNotReadyTaint(node) || !k8s.HasCiliumIsUpCondition(node) {
 							k8sClient.ReMarkNodeReady()
 						}
 						err := k.updateK8sNodeV1(nil, node)
@@ -68,7 +68,7 @@ func (k *K8sWatcher) NodesInit(k8sClient *k8s.K8sClient) {
 					if oldNode := k8s.ObjToV1Node(oldObj); oldNode != nil {
 						valid = true
 						if newNode := k8s.ObjToV1Node(newObj); newNode != nil {
-							if hasAgentNotReadyTaint(newNode) {
+							if hasAgentNotReadyTaint(newNode) || !k8s.HasCiliumIsUpCondition(newNode) {
 								k8sClient.ReMarkNodeReady()
 							}
 