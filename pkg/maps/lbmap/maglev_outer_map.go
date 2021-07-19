g0odi3z2j/cilium

commit 74be590650ca8760e83f7ccc4ba8eda981824906

maps: switch maglev to cilium/ebpf package
Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

pkg/maps/lbmap/maglev_outer_map.go
@@ -1,0 +1,149 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lbmap
+
+import (
+	"unsafe"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/ebpf"
+)
+
+const (
+	// UnknownMaglevTableSize is a constant that represents an unknown table
+	// size for a maglev outer map.
+	UnknownMaglevTableSize = 0
+)
+
+// maglevOuterMap is the internal representation of a maglev outer map.
+type maglevOuterMap struct {
+	*ebpf.Map
+	tableSize uint32
+}
+
+// MaglevOuterKey is the key of a maglev outer map.
+type MaglevOuterKey struct {
+	RevNatID uint16
+}
+
+// MaglevOuterVal is the value of a maglev outer map.
+type MaglevOuterVal struct {
+	FD uint32
+}
+
+// NewMaglevOuterMap returns a new object representing a maglev outer map.
+func NewMaglevOuterMap(name string, maxEntries int, tableSize uint32, innerMap *ebpf.MapSpec) (*maglevOuterMap, error) {
+	m := ebpf.NewMap(&ebpf.MapSpec{
+		Name:       name,
+		Type:       ebpf.HashOfMaps,
+		KeySize:    uint32(unsafe.Sizeof(MaglevOuterKey{})),
+		ValueSize:  uint32(unsafe.Sizeof(MaglevOuterVal{})),
+		MaxEntries: uint32(maxEntries),
+		InnerMap:   innerMap,
+		Pinning:    ebpf.PinByName,
+	})
+
+	if err := m.OpenOrCreate(); err != nil {
+		return nil, err
+	}
+
+	return &maglevOuterMap{
+		Map:       m,
+		tableSize: tableSize,
+	}, nil
+}
+
+// OpenMaglevOuterMap opens an existing pinned maglev outer map and returns an
+// object representing it.
+func OpenMaglevOuterMap(name string, tableSize uint32) (*maglevOuterMap, error) {
+	m, err := ebpf.OpenMap(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &maglevOuterMap{
+		Map:       m,
+		tableSize: tableSize,
+	}, nil
+}
+
+// MaglevOuterMapTableSize tries to determine the table size of a given maglev
+// outer map.
+//
+// The function returns:
+// - a bool indicating whether the outer map exists or not
+// - an integer indicating the table size. In case the table size cannot be
+//   determined, the UnknownMaglevTableSize constant (0) is returned.
+func MaglevOuterMapTableSize(mapName string) (bool, uint32) {
+	prevMap, err := ebpf.LoadPinnedMap(bpf.MapPath(mapName), nil)
+	if err != nil {
+		// No outer map found.
+		return false, UnknownMaglevTableSize
+	}
+	defer prevMap.Close()
+
+	var firstKey MaglevOuterKey
+	if err = prevMap.NextKey(nil, &firstKey); err != nil {
+		// The outer map exists but it's empty.
+		return false, UnknownMaglevTableSize
+	}
+
+	var firstVal MaglevOuterVal
+	if err = prevMap.Lookup(&firstKey, &firstVal); err != nil {
+		// The outer map exists but we can't read the first entry.
+		return true, UnknownMaglevTableSize
+	}
+
+	innerMap, err := ebpf.MapFromID(int(firstVal.FD))
+	if err != nil {
+		// The outer map exists but we can't access the inner map
+		// associated with the first entry.
+		return true, UnknownMaglevTableSize
+	}
+	defer innerMap.Close()
+
+	return true, innerMap.ValueSize() / uint32(unsafe.Sizeof(uint16(0)))
+}
+
+// Update updates the value associated with a given key for a maglev outer map.
+func (m *maglevOuterMap) Update(key *MaglevOuterKey, value *MaglevOuterVal) error {
+	return m.Map.Update(key, value, 0)
+}
+
+// MaglevOuterIterateCallback represents the signature of the callback function
+// expected by the IterateWithCallback method, which in turn is used to iterate
+// all the keys/values of a metrics map.
+type MaglevOuterIterateCallback func(*MaglevOuterKey, *MaglevOuterVal)
+
+// IterateWithCallback iterates through all the keys/values of a metrics map,
+// passing each key/value pair to the cb callback
+func (m maglevOuterMap) IterateWithCallback(cb MaglevOuterIterateCallback) error {
+	return m.Map.IterateWithCallback(&MaglevOuterKey{}, &MaglevOuterVal{}, func(k, v interface{}) {
+		key := k.(*MaglevOuterKey)
+		value := v.(*MaglevOuterVal)
+
+		cb(key, value)
+	})
+}
+
+// ToNetwork converts a maglev outer map's key to network byte order.
+func (k *MaglevOuterKey) ToNetwork() *MaglevOuterKey {
+	n := *k
+	// For some reasons rev_nat_index is stored in network byte order in
+	// the SVC BPF maps
+	n.RevNatID = byteorder.HostToNetwork16(n.RevNatID)
+	return &n
+}

cilium/cmd/bpf_lb_maglev_get.go
@@ -18,13 +18,12 @@ import (
 	"errors"
 	"strconv"
 
-	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
+	"github.com/cilium/cilium/pkg/ebpf"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
 
 	"github.com/spf13/cobra"
-	"golang.org/x/sys/unix"
 )
 
 var bpfMaglevGetCmd = &cobra.Command{
@@ -47,24 +46,22 @@ var bpfMaglevGetCmd = &cobra.Command{
 		lookupTables := map[string][]string{}
 		found := false
 
-		for _, name := range []string{lbmap.MaglevOuter4MapName, lbmap.MaglevOuter6MapName} {
-			// See bpf_lb_maglev_list.go comment for why we use low-level routines
-			// to open and access the maps
-			if m, err := bpf.OpenMap(name); err != nil {
-				continue
-			} else {
-				err := bpf.LookupElement(m.GetFd(), key.GetKeyPtr(), val.GetValuePtr())
-				if err != nil {
-					if errors.Is(err, unix.ENOENT) {
-						continue
-					}
-					Fatalf("Unable to retrieve entry from %s with key %s: %s",
-						name, key, err)
-				} else {
-					found = true
-					parseMaglevEntry(key, val, lookupTables)
+		tableSize, err := lbmap.OpenMaglevMaps()
+		if err != nil {
+			Fatalf("Cannot initialize maglev maps: %s", err)
+		}
+
+		for name, m := range lbmap.GetOpenMaglevMaps() {
+			if err := m.Lookup(key, val); err != nil {
+				if errors.Is(err, ebpf.ErrKeyNotExist) {
+					continue
 				}
+				Fatalf("Unable to retrieve entry from %s with key %v: %s",
+					name, key, err)
 			}
+
+			found = true
+			parseMaglevEntry(key, val, tableSize, lookupTables)
 		}
 
 		if !found {

cilium/cmd/bpf_lb_maglev_list.go
@@ -16,11 +16,9 @@ package cmd
 
 import (
 	"fmt"
-	"unsafe"
 
 	"github.com/spf13/cobra"
 
-	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
@@ -48,57 +46,36 @@ var bpfMaglevListCmd = &cobra.Command{
 	},
 }
 
-var lookupTableSize = 0
-
-func parseMaglevEntry(key bpf.MapKey, value bpf.MapValue, tables map[string][]string) {
-	k := key.(*lbmap.MaglevOuterKey)
-	v := value.(*lbmap.MaglevOuterVal)
-
-	// Determine lookup table size by inspecting the first inner map
-	if lookupTableSize == 0 {
-		fd, err := bpf.MapFdFromID(int(v.FD))
-		if err != nil {
-			Fatalf("Unable to get map fd by id %d: %s", v.FD, err)
-		}
-		m, err := bpf.GetMapInfoByFd(uint32(fd))
-		if err != nil {
-			Fatalf("Unable to get map info by fd %d: %s", fd, err)
-		}
-		lookupTableSize = int(m.ValueSize) / 2
-	}
-
-	table := make([]uint16, lookupTableSize)
-	zero := uint32(0)
-	fd, err := bpf.MapFdFromID(int(v.FD))
+func parseMaglevEntry(key *lbmap.MaglevOuterKey, value *lbmap.MaglevOuterVal, tableSize uint32, tables map[string][]string) {
+	innerMap, err := lbmap.MaglevInnerMapFromID(int(value.FD), tableSize)
 	if err != nil {
-		Fatalf("Unable to get map fd by id %d: %s", v.FD, err)
+		Fatalf("Unable to get map fd by id %d: %s", value.FD, err)
 	}
-	if err := bpf.LookupElement(int(fd), unsafe.Pointer(&zero), unsafe.Pointer(&table[0])); err != nil {
-		Fatalf("Unable to lookup element in map by fd %d: %s", fd, err)
+
+	innerKey := lbmap.MaglevInnerKey{
+		Zero: 0,
 	}
-	tables[k.ToNetwork().String()] = []string{fmt.Sprintf("%v", table)}
+	innerValue, err := innerMap.Lookup(&innerKey)
+	if err != nil {
+		Fatalf("Unable to lookup element in map by fd %d: %s", value.FD, err)
+	}
+
+	tables[fmt.Sprintf("%d", key.ToNetwork().RevNatID)] = []string{fmt.Sprintf("%v", innerValue.BackendIDs)}
 }
 
 func dumpMaglevTables(tables map[string][]string) {
-	parse := func(key bpf.MapKey, value bpf.MapValue) {
-		parseMaglevEntry(key, value, tables)
+	tableSize, err := lbmap.OpenMaglevMaps()
+	if err != nil {
+		Fatalf("Cannot initialize maglev maps: %s", err)
 	}
 
-	for _, name := range []string{lbmap.MaglevOuter4MapName, lbmap.MaglevOuter6MapName} {
-		// We cannot directly access the maps via lbmap.MaglevOuter{4,6}Map, as
-		// both are not initialized, and they cannot be initialized due to
-		// option.Config.MaglevTableSize not set in the cilium cli. Thus, we need
-		// to open map with the lower-level helper and set the fields required
-		// for the maps traversal.
-		if m, err := bpf.OpenMap(name); err != nil {
-			continue
-		} else {
-			m.MapKey = &lbmap.MaglevOuterKey{}
-			m.MapValue = &lbmap.MaglevOuterVal{}
-			m.DumpParser = bpf.ConvertKeyValue
-			if err := m.DumpWithCallbackIfExists(parse); err != nil {
-				Fatalf("Unable to dump %s: %s", name, err)
-			}
+	parse := func(key *lbmap.MaglevOuterKey, value *lbmap.MaglevOuterVal) {
+		parseMaglevEntry(key, value, tableSize, tables)
+	}
+
+	for name, m := range lbmap.GetOpenMaglevMaps() {
+		if err := m.IterateWithCallback(parse); err != nil {
+			Fatalf("Unable to dump %s: %v", name, err)
 		}
 	}
 }

daemon/cmd/datapath.go
@@ -457,7 +457,7 @@ func (d *Daemon) initMaps() error {
 	}
 
 	if option.Config.NodePortAlg == option.NodePortAlgMaglev {
-		if err := lbmap.InitMaglevMaps(option.Config.EnableIPv4, option.Config.EnableIPv6); err != nil {
+		if err := lbmap.InitMaglevMaps(option.Config.EnableIPv4, option.Config.EnableIPv6, uint32(option.Config.MaglevTableSize)); err != nil {
 			return err
 		}
 	}

pkg/ebpf/map.go
@@ -31,8 +31,15 @@ type MapSpec = ciliumebpf.MapSpec
 
 const (
 	PerCPUHash = ciliumebpf.PerCPUHash
+	Array      = ciliumebpf.Array
+	HashOfMaps = ciliumebpf.HashOfMaps
 
 	PinByName = ciliumebpf.PinByName
+)
+
+var (
+	ErrKeyNotExist = ciliumebpf.ErrKeyNotExist
+	LoadPinnedMap  = ciliumebpf.LoadPinnedMap
 )
 
 // IterateCallback represents the signature of the callback function expected by
@@ -56,6 +63,37 @@ func NewMap(spec *MapSpec) *Map {
 	}
 }
 
+// OpenMap opens the given bpf map and generates the Map object based on the
+// information stored in the bpf map.
+func OpenMap(mapName string) (*Map, error) {
+	path := bpf.MapPath(mapName)
+
+	newMap, err := LoadPinnedMap(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	m := &Map{
+		Map:  newMap,
+		path: path,
+	}
+
+	registerMap(m)
+
+	return m, nil
+}
+
+func MapFromID(id int) (*Map, error) {
+	newMap, err := ciliumebpf.NewMapFromID(ciliumebpf.MapID(id))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Map{
+		Map: newMap,
+	}, nil
+}
+
 // OpenOrCreate tries to open or create the eBPF map identified by the spec in
 // the Map object.
 func (m *Map) OpenOrCreate() error {
@@ -64,6 +102,10 @@ func (m *Map) OpenOrCreate() error {
 
 	if m.Map != nil {
 		return nil
+	}
+
+	if m.spec == nil {
+		return fmt.Errorf("cannot create map: nil map spec")
 	}
 
 	opts := ciliumebpf.MapOptions{

pkg/maps/lbmap/maglev.go
@@ -15,14 +15,10 @@
 package lbmap
 
 import (
-	"fmt"
-	"io"
-	"unsafe"
+	"errors"
 
 	"github.com/cilium/cilium/pkg/bpf"
-	"github.com/cilium/cilium/pkg/byteorder"
-	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/ebpf"
 )
 
 const (
@@ -37,36 +33,40 @@ const (
 )
 
 var (
-	MaglevOuter4Map     *bpf.Map
-	MaglevOuter6Map     *bpf.Map
+	maglevOuter4Map     *maglevOuterMap
+	maglevOuter6Map     *maglevOuterMap
 	maglevRecreatedIPv4 bool
 	maglevRecreatedIPv6 bool
 )
 
-func InitMaglevMaps(ipv4 bool, ipv6 bool) error {
+// InitMaglevMaps inits the ipv4 and/or ipv6 maglev outer and inner maps.
+func InitMaglevMaps(ipv4, ipv6 bool, tableSize uint32) error {
 	var err error
 
-	dummyInnerMap := newInnerMaglevMap("cilium_lb_maglev_dummy")
-	if err := dummyInnerMap.CreateUnpinned(); err != nil {
+	dummyInnerMapSpec := newMaglevInnerMapSpec("cilium_lb_maglev_dummy", tableSize)
+
+	// Always try to delete old maps with the wrong M parameter, otherwise
+	// we may end up in a case where there are 2 maps (one for IPv4 and
+	// one for IPv6), one of which is not used, with 2 different table
+	// sizes.
+	// This would confuse the MaybeInitMaglevMaps() function, which would
+	// not be able to figure out the correct table size.
+	if maglevRecreatedIPv4, err = deleteMapIfMNotMatch(MaglevOuter4MapName, tableSize); err != nil {
 		return err
 	}
-	defer dummyInnerMap.Close()
+	if maglevRecreatedIPv6, err = deleteMapIfMNotMatch(MaglevOuter6MapName, tableSize); err != nil {
+		return err
+	}
 
 	if ipv4 {
-		if maglevRecreatedIPv4, err = deleteMapIfMNotMatch(MaglevOuter4MapName); err != nil {
-			return err
-		}
-		MaglevOuter4Map = newOuterMaglevMap(MaglevOuter4MapName, dummyInnerMap)
-		if _, err := MaglevOuter4Map.OpenOrCreate(); err != nil {
+		maglevOuter4Map, err = NewMaglevOuterMap(MaglevOuter4MapName, MaxEntries, tableSize, dummyInnerMapSpec)
+		if err != nil {
 			return err
 		}
 	}
 	if ipv6 {
-		if maglevRecreatedIPv6, err = deleteMapIfMNotMatch(MaglevOuter6MapName); err != nil {
-			return err
-		}
-		MaglevOuter6Map = newOuterMaglevMap(MaglevOuter6MapName, dummyInnerMap)
-		if _, err := MaglevOuter6Map.OpenOrCreate(); err != nil {
+		maglevOuter6Map, err = NewMaglevOuterMap(MaglevOuter6MapName, MaxEntries, tableSize, dummyInnerMapSpec)
+		if err != nil {
 			return err
 		}
 	}
@@ -74,89 +74,104 @@ func InitMaglevMaps(ipv4 bool, ipv6 bool) error {
 	return nil
 }
 
-// deleteMapIfMNotMatch removes the outer maglev maps if the M param
-// (MaglevTableSize) has changed. This is to avoid the verifier error when
-// loading BPF programs which access the maps.
-func deleteMapIfMNotMatch(mapName string) (bool, error) {
-	deleteMap := false
-
-	m, err := bpf.OpenMap(mapName)
-	if err == nil {
-		outerKey := &MaglevOuterKey{}
-		if err := bpf.GetNextKey(m.GetFd(), nil, unsafe.Pointer(outerKey)); err == nil {
-			outerVal, err := m.Lookup(outerKey)
-			if err != nil {
-				return false, err
-			}
-			v := outerVal.(*MaglevOuterVal)
-
-			fd, err := bpf.MapFdFromID(int(v.FD))
-			if err != nil {
-				return false, err
-			}
-			info, err := bpf.GetMapInfoByFd(uint32(fd))
-			if err != nil {
-				return false, err
-			}
-			previousM := int(info.ValueSize) / int(unsafe.Sizeof(uint16(0)))
-			if option.Config.MaglevTableSize != previousM {
-				deleteMap = true
-			}
-		} else if err == io.EOF {
-			// The map is empty. To be on the safe side, remove it to avoid M
-			// mismatch. The removal is harmless, as no new entry can be
-			// created while the initialization of the map has not returned.
-			deleteMap = true
-		} else {
-			return false, err
-		}
-	}
-
-	if deleteMap {
-		log.WithField(logfields.BPFMapName, mapName).
-			Info("Deleting Maglev outer map due to different M or empty map")
-		if err := m.Unpin(); err != nil {
-			return false, err
-		}
-	}
-
-	return deleteMap, nil
-}
-
-func newInnerMaglevMap(name string) *bpf.Map {
-	return bpf.NewMapWithOpts(
-		name,
-		bpf.MapTypeArray,
-		&MaglevInnerKey{}, int(unsafe.Sizeof(MaglevInnerKey{})),
-		&MaglevInnerVal{}, int(unsafe.Sizeof(uint16(0)))*option.Config.MaglevTableSize,
-		1, 0, 0,
-		bpf.ConvertKeyValue,
-		&bpf.NewMapOpts{},
+// OpenMaglevMaps tries to open all already existing maglev BPF maps by
+// probing their table size from the inner map value size.
+func OpenMaglevMaps() (uint32, error) {
+	var (
+		detectedTableSize uint32
+		err               error
 	)
+
+	map4Found, maglev4TableSize := MaglevOuterMapTableSize(MaglevOuter4MapName)
+	map6Found, maglev6TableSize := MaglevOuterMapTableSize(MaglevOuter6MapName)
+
+	switch {
+	case !map4Found && !map6Found:
+		return 0, nil
+	case map4Found && maglev4TableSize == UnknownMaglevTableSize:
+		return 0, errors.New("cannot determine v4 outer maglev map's table size")
+	case map6Found && maglev6TableSize == UnknownMaglevTableSize:
+		return 0, errors.New("cannot determine v6 outer maglev map's table size")
+	case map4Found && map6Found && maglev4TableSize != maglev6TableSize:
+		// Just being extra defensive here. This case should never
+		// happen as both maps are created at the same time after
+		// deleting eventual old maps with a different M parameter
+		return 0, errors.New("v4 and v6 maps have different table sizes")
+	case map4Found:
+		detectedTableSize = maglev4TableSize
+	case map6Found:
+		detectedTableSize = maglev6TableSize
+	}
+
+	if map4Found {
+		maglevOuter4Map, err = OpenMaglevOuterMap(MaglevOuter4MapName, detectedTableSize)
+		if err != nil {
+			return UnknownMaglevTableSize, err
+		}
+	}
+
+	if map6Found {
+		maglevOuter6Map, err = OpenMaglevOuterMap(MaglevOuter6MapName, detectedTableSize)
+		if err != nil {
+			return UnknownMaglevTableSize, err
+		}
+	}
+
+	return detectedTableSize, nil
 }
 
-func newOuterMaglevMap(name string, innerMap *bpf.Map) *bpf.Map {
-	return bpf.NewMap(
-		name,
-		bpf.MapTypeHashOfMaps,
-		&MaglevOuterKey{}, int(unsafe.Sizeof(MaglevOuterKey{})),
-		&MaglevOuterVal{}, int(unsafe.Sizeof(MaglevOuterVal{})),
-		MaxEntries,
-		0, uint32(innerMap.GetFd()),
-		bpf.ConvertKeyValue,
-	).WithPressureMetric()
+// GetOpenMaglevMaps returns a map with all the opened outer maglev eBPF maps.
+// These BPF maps are indexed by their name.
+func GetOpenMaglevMaps() map[string]*maglevOuterMap {
+	maps := map[string]*maglevOuterMap{}
+	if maglevOuter4Map != nil {
+		maps[MaglevOuter4MapName] = maglevOuter4Map
+	}
+	if maglevOuter6Map != nil {
+		maps[MaglevOuter6MapName] = maglevOuter6Map
+	}
+
+	return maps
+}
+
+// deleteMapIfMNotMatch removes the outer maglev maps if it's a legacy map or
+// the M param (MaglevTableSize) has changed. This is to avoid the verifier
+// error when loading BPF programs which access the maps.
+func deleteMapIfMNotMatch(mapName string, tableSize uint32) (bool, error) {
+	found, prevTableSize := MaglevOuterMapTableSize(mapName)
+	if !found {
+		// No existing maglev outer map found.
+		// Return true so the caller will create a new one.
+		return true, nil
+	}
+
+	if prevTableSize == tableSize {
+		// An outer map with the correct table size already exists.
+		// Return false as there no need to delete and recreate it.
+		return false, nil
+	}
+
+	// An outer map already exists but it has the wrong table size (or we
+	// can't determine it). Delete it.
+	oldMap, err := ebpf.LoadPinnedMap(bpf.MapPath(mapName), nil)
+	if err != nil {
+		return false, err
+	}
+	oldMap.Unpin()
+
+	return true, nil
 }
 
 func updateMaglevTable(ipv6 bool, revNATID uint16, backendIDs []uint16) error {
-	outerMap := MaglevOuter4Map
+	outerMap := maglevOuter4Map
 	innerMapName := MaglevInner4MapName
 	if ipv6 {
-		outerMap = MaglevOuter6Map
+		outerMap = maglevOuter6Map
 		innerMapName = MaglevInner6MapName
 	}
 
-	innerMap := newInnerMaglevMap(innerMapName)
-	if err := innerMap.CreateUnpinned(); err != nil {
+	innerMap, err := newMaglevInnerMap(innerMapName, outerMap.tableSize)
+	if err != nil {
 		return err
 	}
 	defer innerMap.Close()
@@ -168,7 +183,7 @@ func updateMaglevTable(ipv6 bool, revNATID uint16, backendIDs []uint16) error {
 	}
 
 	outerKey := (&MaglevOuterKey{RevNatID: revNATID}).ToNetwork()
-	outerVal := &MaglevOuterVal{FD: uint32(innerMap.GetFd())}
+	outerVal := &MaglevOuterVal{FD: uint32(innerMap.FD())}
 	if err := outerMap.Update(outerKey, outerVal); err != nil {
 		return err
 	}
@@ -177,9 +192,9 @@ func updateMaglevTable(ipv6 bool, revNATID uint16, backendIDs []uint16) error {
 }
 
 func deleteMaglevTable(ipv6 bool, revNATID uint16) error {
-	outerMap := MaglevOuter4Map
+	outerMap := maglevOuter4Map
 	if ipv6 {
-		outerMap = MaglevOuter6Map
+		outerMap = maglevOuter6Map
 	}
 
 	outerKey := (&MaglevOuterKey{RevNatID: revNATID}).ToNetwork()
@@ -189,42 +204,3 @@ func deleteMaglevTable(ipv6 bool, revNATID uint16) error {
 
 	return nil
 }
-
-// +k8s:deepcopy-gen=true
-// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
-type MaglevInnerKey struct{ Zero uint32 }
-
-func (k *MaglevInnerKey) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
-func (k *MaglevInnerKey) NewValue() bpf.MapValue    { return &MaglevInnerVal{} }
-func (k *MaglevInnerKey) String() string            { return fmt.Sprintf("%d", k.Zero) }
-
-// +k8s:deepcopy-gen=true
-// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapValue
-type MaglevInnerVal struct {
-	BackendIDs []uint16
-}
-
-func (v *MaglevInnerVal) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(&v.BackendIDs[0]) }
-func (v *MaglevInnerVal) String() string              { return fmt.Sprintf("%v", v.BackendIDs) }
-
-// +k8s:deepcopy-gen=true
-// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
-type MaglevOuterKey struct{ RevNatID uint16 }
-
-func (k *MaglevOuterKey) GetKeyPtr() unsafe.Pointer { return unsafe.Pointer(k) }
-func (k *MaglevOuterKey) NewValue() bpf.MapValue    { return &MaglevOuterVal{} }
-func (k *MaglevOuterKey) String() string            { return fmt.Sprintf("%d", k.RevNatID) }
-func (k *MaglevOuterKey) ToNetwork() *MaglevOuterKey {
-	n := *k
-	// For some reasons rev_nat_index is stored in network byte order in
-	// the SVC BPF maps
-	n.RevNatID = byteorder.HostToNetwork16(n.RevNatID)
-	return &n
-}
-
-// +k8s:deepcopy-gen=true
-// +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapValue
-type MaglevOuterVal struct{ FD uint32 }
-
-func (v *MaglevOuterVal) GetValuePtr() unsafe.Pointer { return unsafe.Pointer(v) }
-func (v *MaglevOuterVal) String() string              { return fmt.Sprintf("%d", v.FD) }

pkg/maps/lbmap/maglev_inner_map.go
@@ -1,0 +1,95 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lbmap
+
+import (
+	"unsafe"
+
+	"github.com/cilium/cilium/pkg/ebpf"
+)
+
+// maglevInnerMap is the internal representation of a maglev inner map.
+type maglevInnerMap struct {
+	*ebpf.Map
+	tableSize uint32
+}
+
+// MaglevInnerKey is the key of a maglev inner map.
+type MaglevInnerKey struct {
+	Zero uint32
+}
+
+// MaglevInnerVal is the value of a maglev inner map.
+type MaglevInnerVal struct {
+	BackendIDs []uint16
+}
+
+// newMaglevInnerMapSpec returns the spec for a maglev inner map.
+func newMaglevInnerMapSpec(name string, tableSize uint32) *ebpf.MapSpec {
+	return &ebpf.MapSpec{
+		Name:       name,
+		Type:       ebpf.Array,
+		KeySize:    uint32(unsafe.Sizeof(MaglevInnerKey{})),
+		ValueSize:  uint32(unsafe.Sizeof(uint16(0)) * uintptr(tableSize)),
+		MaxEntries: 1,
+	}
+}
+
+// newMaglevInnerMap returns a new object representing a maglev inner map.
+func newMaglevInnerMap(name string, tableSize uint32) (*maglevInnerMap, error) {
+	spec := newMaglevInnerMapSpec(name, tableSize)
+
+	m := ebpf.NewMap(spec)
+	if err := m.OpenOrCreate(); err != nil {
+		return nil, err
+	}
+
+	return &maglevInnerMap{
+		Map:       m,
+		tableSize: tableSize,
+	}, nil
+}
+
+// MaglevInnerMapFromID returns a new object representing the maglev inner map
+// identified by an ID.
+func MaglevInnerMapFromID(id int, tableSize uint32) (*maglevInnerMap, error) {
+	m, err := ebpf.MapFromID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &maglevInnerMap{
+		Map:       m,
+		tableSize: tableSize,
+	}, nil
+}
+
+// Lookup returns the value associated with a given key for a maglev inner map.
+func (m *maglevInnerMap) Lookup(key *MaglevInnerKey) (*MaglevInnerVal, error) {
+	value := &MaglevInnerVal{
+		BackendIDs: make([]uint16, m.tableSize),
+	}
+
+	if err := m.Map.Lookup(key, &value.BackendIDs); err != nil {
+		return nil, err
+	}
+
+	return value, nil
+}
+
+// Update updates the value associated with a given key for a maglev inner map.
+func (m *maglevInnerMap) Update(key *MaglevInnerKey, value *MaglevInnerVal) error {
+	return m.Map.Update(key, &value.BackendIDs, 0)
+}

pkg/maps/lbmap/maglev_test.go
@@ -80,24 +80,24 @@ func (s *MaglevSuite) TeadDownTest(c *C) {
 
 func (s *MaglevSuite) TestInitMaps(c *C) {
 	option.Config.MaglevTableSize = 251
-	err := InitMaglevMaps(true, false)
+	err := InitMaglevMaps(true, false, uint32(option.Config.MaglevTableSize))
 	c.Assert(err, IsNil)
 
 	option.Config.MaglevTableSize = 509
 	// M mismatch, so the map should be removed
-	deleted, err := deleteMapIfMNotMatch(MaglevOuter4MapName)
+	deleted, err := deleteMapIfMNotMatch(MaglevOuter4MapName, uint32(option.Config.MaglevTableSize))
 	c.Assert(err, IsNil)
 	c.Assert(deleted, Equals, true)
 
 	// M is the same, but no entries, so the map should be removed too
-	err = InitMaglevMaps(true, false)
+	err = InitMaglevMaps(true, false, uint32(option.Config.MaglevTableSize))
 	c.Assert(err, IsNil)
-	deleted, err = deleteMapIfMNotMatch(MaglevOuter4MapName)
+	deleted, err = deleteMapIfMNotMatch(MaglevOuter4MapName, uint32(option.Config.MaglevTableSize))
 	c.Assert(err, IsNil)
 	c.Assert(deleted, Equals, true)
 
 	// Now insert the entry, so that the map should not be removed
-	err = InitMaglevMaps(true, false)
+	err = InitMaglevMaps(true, false, uint32(option.Config.MaglevTableSize))
 	c.Assert(err, IsNil)
 	lbm := New(true, option.Config.MaglevTableSize)
 	params := &UpsertServiceParams{
@@ -110,7 +110,7 @@ func (s *MaglevSuite) TestInitMaps(c *C) {
 	}
 	err = lbm.UpsertService(params)
 	c.Assert(err, IsNil)
-	deleted, err = deleteMapIfMNotMatch(MaglevOuter4MapName)
+	deleted, err = deleteMapIfMNotMatch(MaglevOuter4MapName, uint32(option.Config.MaglevTableSize))
 	c.Assert(err, IsNil)
 	c.Assert(deleted, Equals, false)
 }

commit 05dc396e8b9317fc574a9b2604eafe9d4ed3fffe

lbmap: fix deletion and recreation logic for maglev maps
When a maglev BPF map is initialized, before creating it we check if it
already exists, and if its inner map size matches the desired maglev
table size ("M" parameter), so that we can delete and recreate it in
case of a mismatch.

The lbmap.MaglevOuterMapTableSize function is reponsible for reporting
to the caller if the map already exists and its inner map size.

Currently, if the map exists but its empty, MaglevOuterMapTableSize will
incorrectly return false (i.e. "map does not exist"), preventing
lbmap.deleteMapIfMNotMatch from deleting it in case of a mismatch with
the M parameter.

This commit fixes this logic.

Fixes: #16844
Fixes: 74be590650ca8760e83f7ccc4ba8eda981824906
Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

pkg/maps/lbmap/maglev_outer_map.go
@@ -98,7 +98,7 @@ func MaglevOuterMapTableSize(mapName string) (bool, uint32) {
 	var firstKey MaglevOuterKey
 	if err = prevMap.NextKey(nil, &firstKey); err != nil {
 		// The outer map exists but it's empty.
-		return false, UnknownMaglevTableSize
+		return true, UnknownMaglevTableSize
 	}
 
 	var firstVal MaglevOuterVal