g0odi3z2j/cilium

commit 53231f5161c1f74387e0cf1788c1b371f4d4c36c

routing: Fix incorrect interface selection for pod routes
The Configure method relies on the MAC address to select the proper
egress interface for new pods (EKS and AKS). Several interfaces can
however have the same MAC address in the case of slave devices. In
such a case, the wrong interface may be selected.

To avoid this, we skip Linux slave devices during the lookup by MAC
address. Thus, in case of slave devices, we will select the master
device.

Fixes: 75b2a293 ("Implement support for cilium-health in ENI mode")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/routing/routing.go
@@ -275,9 +275,9 @@ func deleteRule(r route.Rule) error {
 }
 
 // retrieveIfIndexFromMAC finds the corresponding device index (ifindex) for a
-// given MAC address. This is useful for creating rules and routes in order to
-// specify the table. When the ifindex is found, the device is brought up and
-// its MTU is set.
+// given MAC address, excluding Linux slave devices. This is useful for
+// creating rules and routes in order to specify the table. When the ifindex is
+// found, the device is brought up and its MTU is set.
 func retrieveIfIndexFromMAC(mac mac.MAC, mtu int) (int, error) {
 	var link netlink.Link
 
@@ -287,6 +287,11 @@ func retrieveIfIndexFromMAC(mac mac.MAC, mtu int) (int, error) {
 	}
 
 	for _, l := range links {
+		// Linux slave devices have the same MAC address as their master
+		// device, but we want the master device.
+		if l.Attrs().Slave != nil {
+			continue
+		}
 		if l.Attrs().HardwareAddr.String() == mac.String() {
 			if link != nil {
 				return -1, fmt.Errorf("several interfaces found with MAC %s: %s and %s", mac, link.Attrs().Name, l.Attrs().Name)

commit d45b1e0ede52ac4219ecd765a1d00f6677609c11

routing: Fix incorrect detection of Linux slave devices
Using method Slave() exposed by the netlink package doesn't always work.
In particular, it doesn't work on AKS, maybe because there's no master
bond interface in that case. We should instead rely on the flags passed
by Linux's netlink API.

Fixes: 53231f51 ("routing: Fix incorrect interface selection for pod routes")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/routing/routing.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
 )
 
 var (
@@ -289,7 +290,7 @@ func retrieveIfIndexFromMAC(mac mac.MAC, mtu int) (int, error) {
 	for _, l := range links {
 		// Linux slave devices have the same MAC address as their master
 		// device, but we want the master device.
-		if l.Attrs().Slave != nil {
+		if l.Attrs().RawFlags&unix.IFF_SLAVE != 0 {
 			continue
 		}
 		if l.Attrs().HardwareAddr.String() == mac.String() {