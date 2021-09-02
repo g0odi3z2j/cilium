g0odi3z2j/cilium

commit baa4c797042371e4d15d9368d9e962eed023e207

datapath: Include the host endpoint ID in all endpoint headers
We need the host endpoint ID in bpf_lxc to be able to tail call into the
bpf_host program enforcing host policies. Thus, this commit includes the
host endpoint ID in the header files for all endpoints, not just the
host endpoint.

For that to work, we also need to make it available globally in the agent.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/node/host_endpoint.go
@@ -14,8 +14,13 @@
 
 package node
 
+const (
+	templateHostEndpointID = uint64(0xffff)
+)
+
 var (
-	labels map[string]string
+	labels     map[string]string
+	endpointID = templateHostEndpointID
 )
 
 // GetLabels returns the labels of this node.
@@ -26,4 +31,14 @@ func GetLabels() map[string]string {
 // SetLabels sets the labels of this node.
 func SetLabels(l map[string]string) {
 	labels = l
+}
+
+// GetEndpointID returns the ID of the host endpoint for this node.
+func GetEndpointID() uint64 {
+	return endpointID
+}
+
+// SetLabels sets the ID of the host endpoint this node.
+func SetEndpointID(id uint64) {
+	endpointID = id
 }

pkg/datapath/linux/config/config.go
@@ -631,7 +631,6 @@ func (h *HeaderfileWriter) writeStaticData(fw io.Writer, e datapath.EndpointConf
 		}
 		// Dummy value to avoid being optimized when 0
 		fmt.Fprint(fw, defineUint32("SECCTX_FROM_IPCACHE", 1))
-		fmt.Fprint(fw, defineUint32("HOST_EP_ID", uint32(e.GetID())))
 
 		// Use templating for ETH_HLEN only if there is any L2-less device
 		if !mac.HaveMACAddr(option.Config.Devices) {
@@ -661,6 +660,8 @@ func (h *HeaderfileWriter) writeStaticData(fw io.Writer, e datapath.EndpointConf
 		fmt.Fprint(fw, defineIPv4("LXC_IPV4", e.IPv4Address()))
 		fmt.Fprint(fw, defineUint32("LXC_ID", uint32(e.GetID())))
 	}
+
+	fmt.Fprint(fw, defineUint32("HOST_EP_ID", uint32(node.GetEndpointID())))
 
 	fmt.Fprint(fw, defineMAC("NODE_MAC", e.GetNodeMAC()))
 

pkg/datapath/loader/template.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cilium/cilium/pkg/maps/callsmap"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
 	"github.com/cilium/cilium/pkg/maps/policymap"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 )
 
@@ -241,10 +242,11 @@ func elfVariableSubstitutions(ep datapath.Endpoint) map[string]uint32 {
 			}
 		}
 		result["SECCTX_FROM_IPCACHE"] = uint32(SecctxFromIpcacheDisabled)
-		result["HOST_EP_ID"] = uint32(ep.GetID())
 	} else {
 		result["LXC_ID"] = uint32(ep.GetID())
 	}
+
+	result["HOST_EP_ID"] = uint32(node.GetEndpointID())
 
 	identity := ep.GetIdentity().Uint32()
 	result["SECLABEL"] = identity

pkg/endpoint/restore.go
@@ -39,6 +39,7 @@ import (
 	"github.com/cilium/cilium/pkg/labelsfilter"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/mac"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 
 	"github.com/sirupsen/logrus"
@@ -175,6 +176,12 @@ func ReadEPsFromDirNames(ctx context.Context, owner regeneration.Owner, basePath
 			}
 		} else {
 			possibleEPs[ep.ID] = ep
+		}
+
+		// We need to save the host endpoint ID as we'll need it to regenerate
+		// other endpoints.
+		if isHost {
+			node.SetEndpointID(ep.GetID())
 		}
 	}
 	return possibleEPs

pkg/endpointmanager/manager.go
@@ -630,6 +630,8 @@ func (mgr *EndpointManager) AddHostEndpoint(ctx context.Context, owner regenerat
 		return err
 	}
 
+	node.SetEndpointID(ep.GetID())
+
 	epLabels := labels.Labels{}
 	epLabels.MergeLabels(labels.LabelHost)
 

commit 7401b570f8a34055ced72fb6816d4d4cf7be3d88

node: Fix race condition on labels' getter/setter
Our race detection pipeline detected a race condition between the setter
and getter for the local node's labels:

    WARNING: DATA RACE
    Write at 0x000005882150 by goroutine 154:
      github.com/cilium/cilium/pkg/node.SetLabels()
          /go/src/github.com/cilium/cilium/pkg/node/host_endpoint.go:33 +0x1dc
      github.com/cilium/cilium/pkg/k8s/watchers.(*K8sWatcher).updateK8sNodeV1()
          /go/src/github.com/cilium/cilium/pkg/k8s/watchers/node.go:89 +0x1cc
      github.com/cilium/cilium/pkg/k8s/watchers.(*K8sWatcher).nodesInit.func1()
          /go/src/github.com/cilium/cilium/pkg/k8s/watchers/node.go:44 +0xd1
      k8s.io/client-go/tools/cache.ResourceEventHandlerFuncs.OnAdd()
          /go/src/github.com/cilium/cilium/vendor/k8s.io/client-go/tools/cache/controller.go:231 +0x8f
      k8s.io/client-go/tools/cache.(*ResourceEventHandlerFuncs).OnAdd()
          <autogenerated>:1 +0x2a
      github.com/cilium/cilium/pkg/k8s/informer.NewInformerWithStore.func1()
          /go/src/github.com/cilium/cilium/pkg/k8s/informer/informer.go:119 +0x297
      k8s.io/client-go/tools/cache.(*DeltaFIFO).Pop()
          /go/src/github.com/cilium/cilium/vendor/k8s.io/client-go/tools/cache/delta_fifo.go:544 +0x50d
      k8s.io/client-go/tools/cache.(*controller).processLoop()
          /go/src/github.com/cilium/cilium/vendor/k8s.io/client-go/tools/cache/controller.go:183 +0x83
      k8s.io/client-go/tools/cache.(*controller).processLoop-fm()
          /go/src/github.com/cilium/cilium/vendor/k8s.io/client-go/tools/cache/controller.go:181 +0x4a
      k8s.io/apimachinery/pkg/util/wait.BackoffUntil.func1()
          /go/src/github.com/cilium/cilium/vendor/k8s.io/apimachinery/pkg/util/wait/wait.go:155 +0x75
      k8s.io/apimachinery/pkg/util/wait.BackoffUntil()
          /go/src/github.com/cilium/cilium/vendor/k8s.io/apimachinery/pkg/util/wait/wait.go:156 +0xba
      k8s.io/apimachinery/pkg/util/wait.JitterUntil()
          /go/src/github.com/cilium/cilium/vendor/k8s.io/apimachinery/pkg/util/wait/wait.go:133 +0x114
      k8s.io/apimachinery/pkg/util/wait.Until()
          /go/src/github.com/cilium/cilium/vendor/k8s.io/apimachinery/pkg/util/wait/wait.go:90 +0x507
      k8s.io/client-go/tools/cache.(*controller).Run()
          /go/src/github.com/cilium/cilium/vendor/k8s.io/client-go/tools/cache/controller.go:154 +0x4a9

    Previous read at 0x000005882150 by main goroutine:
      github.com/cilium/cilium/pkg/nodediscovery.(*NodeDiscovery).StartDiscovery()
          /go/src/github.com/cilium/cilium/pkg/nodediscovery/nodediscovery.go:182 +0x3ab
      github.com/cilium/cilium/daemon/cmd.NewDaemon()
          /go/src/github.com/cilium/cilium/daemon/cmd/daemon.go:762 +0x34d6
      github.com/cilium/cilium/daemon/cmd.runDaemon()
          /go/src/github.com/cilium/cilium/daemon/cmd/daemon_main.go:1558 +0xb8b
      github.com/cilium/cilium/daemon/cmd.glob..func1()
          /go/src/github.com/cilium/cilium/daemon/cmd/daemon_main.go:137 +0x3a4
      github.com/spf13/cobra.(*Command).execute()
          /go/src/github.com/cilium/cilium/vendor/github.com/spf13/cobra/command.go:854 +0x8cb
      github.com/spf13/cobra.(*Command).ExecuteC()
          /go/src/github.com/cilium/cilium/vendor/github.com/spf13/cobra/command.go:958 +0x4b2
      github.com/spf13/cobra.(*Command).Execute()
          /go/src/github.com/cilium/cilium/vendor/github.com/spf13/cobra/command.go:895 +0x88
      github.com/cilium/cilium/daemon/cmd.Execute()
          /go/src/github.com/cilium/cilium/daemon/cmd/daemon_main.go:162 +0x69
      main.main()
          /go/src/github.com/cilium/cilium/daemon/main.go:22 +0x2f
      runtime.main()
          /usr/local/go/src/runtime/proc.go:225 +0x255
      github.com/cilium/ebpf.init()
          /go/src/github.com/cilium/cilium/vendor/github.com/cilium/ebpf/syscalls.go:437 +0x58b
      github.com/cilium/ebpf.init()
          /go/src/github.com/cilium/cilium/vendor/github.com/cilium/ebpf/prog.go:453 +0x518
      github.com/cilium/ebpf.init()
          /go/src/github.com/cilium/cilium/vendor/github.com/cilium/ebpf/syscalls.go:419 +0x4ab
      github.com/cilium/ebpf.init()
          /go/src/github.com/cilium/cilium/vendor/github.com/cilium/ebpf/syscalls.go:235 +0x439
      github.com/cilium/ebpf.init()
          /go/src/github.com/cilium/cilium/vendor/github.com/cilium/ebpf/syscalls.go:217 +0x3cc
      runtime.doInit()
          /usr/local/go/src/runtime/proc.go:6309 +0xeb
      github.com/cilium/ebpf/internal/btf.init()
          /go/src/github.com/cilium/cilium/vendor/github.com/cilium/ebpf/internal/btf/btf.go:728 +0x1cc

This commit fixes it.

Fixes: baa4c797 ("datapath: Include the host endpoint ID in all endpoint headers")

pkg/node/host_endpoint.go
@@ -3,22 +3,31 @@
 
 package node
 
+import (
+	"github.com/cilium/cilium/pkg/lock"
+)
+
 const (
 	templateHostEndpointID = uint64(0xffff)
 )
 
 var (
 	labels     map[string]string
+	labelsMu   lock.RWMutex
 	endpointID = templateHostEndpointID
 )
 
 // GetLabels returns the labels of this node.
 func GetLabels() map[string]string {
+	labelsMu.RLock()
+	defer labelsMu.RUnlock()
 	return labels
 }
 
 // SetLabels sets the labels of this node.
 func SetLabels(l map[string]string) {
+	labelsMu.Lock()
+	defer labelsMu.Unlock()
 	labels = l
 }
 
@@ -27,7 +36,7 @@ func GetEndpointID() uint64 {
 	return endpointID
 }
 
-// SetLabels sets the ID of the host endpoint this node.
+// SetEndpointID sets the ID of the host endpoint this node.
 func SetEndpointID(id uint64) {
 	endpointID = id
 }