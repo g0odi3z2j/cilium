g0odi3z2j/cilium

commit 9af40702901f4a1edd71ac05221427bd4a626f4a

contrib: Support prereleases in release prep scripts
Add support for vX.Y.0-rcN and vX.Y.0-snapshotN prereleases into the
start-release.sh, submit-release.sh and tag-release.sh scripts.

Signed-off-by: Joe Stringer <joe@cilium.io>

contrib/release/submit-release.sh
@@ -10,8 +10,10 @@ REMOTE="$(get_remote)"
 BRANCH="${1:-""}"
 if [ "$BRANCH" = "" ]; then
     BRANCH=$(git symbolic-ref --short HEAD | sed 's/.*\(v[0-9]\+\.[0-9]\+\).*/\1/')
+    if [ -z "$(git ls-remote --heads $REMOTE $BRANCH)" ]; then
+        BRANCH=master
+    fi
 fi
-BRANCH=$(echo "$BRANCH" | sed 's/^v//')
 
 RELEASE="v$(cat VERSION)"
 SUMMARY=${2:-}
@@ -23,10 +25,10 @@ fi
 
 USER_REMOTE=$(get_user_remote ${3:-})
 
-if ! git branch -a | grep -q "$REMOTE/v$BRANCH$" || [ ! -e $SUMMARY ]; then
+if ! git branch -a | grep -q "$REMOTE/$BRANCH$" || [ ! -e $SUMMARY ]; then
     echo "usage: $0 [branch version] [release-summary] [your remote]" 1>&2
     echo 1>&2
-    echo "Ensure 'branch version' is available in '$REMOTE' and the summary file exists" 1>&2
+    echo "Ensure '$BRANCH' is available in '$REMOTE' and the summary file exists" 1>&2
     exit 1
 fi
 
@@ -51,12 +53,21 @@ if $GENERATE_SUMMARY; then
     CHANGELOG=$SUMMARY
     SUMMARY="$RELEASE-pr-$(date --rfc-3339=date).txt"
     echo "Prepare for release $RELEASE" > $SUMMARY
-    tail -n+4 $CHANGELOG >> $SUMMARY
+    if [ "$BRANCH" = "master" ]; then
+        echo "" >> $SUMMARY
+        echo "See the included CHANGELOG.md for a full list of changes." >> $SUMMARY
+    else
+        tail -n+4 $CHANGELOG >> $SUMMARY
+    fi
 fi
 
-echo -e "Sending PR for branch v$BRANCH:\n" 1>&2
+echo -e "Sending PR for branch $BRANCH:\n" 1>&2
 cat $SUMMARY 1>&2
 echo -e "\nSending pull request..." 2>&1
 PR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git push $USER_REMOTE "$PR_BRANCH"
-hub pull-request -b "v$BRANCH" -l kind/release,backport/$BRANCH -F $SUMMARY
+LABELS="kind/release"
+if [ "$BRANCH" != "master" ]; then
+    labels = "$LABELS,backport/$(echo $BRANCH | sed 's/^v//')"
+fi
+hub pull-request -b "$BRANCH" -l "$LABELS" -F $SUMMARY

contrib/release/start-release.sh
@@ -7,6 +7,7 @@ source $DIR/lib/common.sh
 source $DIR/../backporting/common.sh
 
 PROJECTS_REGEX='s/.*projects\/\([0-9]\+\).*/\1/'
+VERSION_TO_BRANCH_REGEX='s/[^0-9]*\([0-9]\+\.[0-9]\+\).*/\1/'
 ACTS_YAML=".github/maintainers-little-helper.yaml"
 REMOTE="$(get_remote)"
 
@@ -16,6 +17,18 @@ usage() {
     logecho "GH-PROJECT Project Number for next (X.Y.Z+1) development release"
     logecho
     logecho "--help     Print this help message"
+}
+
+# $1 - VERSION
+version_is_prerelease() {
+    case "$1" in
+        *rc*|*snapshot*)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
 }
 
 handle_args() {
@@ -29,12 +42,12 @@ handle_args() {
         common::exit 0
     fi
 
-    if ! echo "$1" | grep -q "[0-9]\+\.[0-9]\+\.[0-9]\+"; then
+    if ! echo "$1" | grep -q "[0-9]\+\.[0-9]\+\.[0-9]\+\(-\(\(rc\)\|\(snapshot\)\)\(\.\)\?[0-9]\+\)\?$"; then
         usage 2>&1
         common::exit 1 "Invalid VERSION ARG \"$1\"; Expected X.Y.Z"
     fi
 
-    if ! echo "$2" | grep -q "^[0-9]\+"; then
+    if ! echo "$2" | grep -q "^[0-9]\+" && ! version_is_prerelease "$1"; then
         usage 2>&1
         common::exit 1 "Invalid GH-PROJECT ID argument. Expected [0-9]+"
     fi
@@ -54,25 +67,37 @@ main() {
 
     local ersion="$(echo $1 | sed 's/^v//')"
     local version="v$ersion"
-    local branch="v$(echo $ersion | sed 's/[^0-9]*\([0-9]\+\.[0-9]\+\).*/\1/')"
+    local branch="v$(echo $ersion | sed $VERSION_TO_BRANCH_REGEX)"
     local new_proj="$2"
+    local old_version=""
 
     git fetch $REMOTE
-    git checkout -b pr/prepare-$version $REMOTE/$branch
-    local old_version="$(cat VERSION)"
+    if [ -n "$(git ls-remote --heads $REMOTE $branch)" ]; then
+        git checkout -b pr/prepare-$version $REMOTE/$branch
+        old_version="$(cat VERSION)"
+    else
+        logecho "Cannot find $REMOTE/$branch. Basing release on master."
+        branch="master"
+        git checkout -b pr/prepare-$version $REMOTE/$branch
+        local old_branch="$(cat VERSION | sed $VERSION_TO_BRANCH_REGEX)"
+        old_version="$(git show v$old_branch:VERSION)"
+    fi
 
     logecho "Updating VERSION, AUTHORS.md, $ACTS_YAML, helm templates"
     echo $ersion > VERSION
     sed -i 's/"[^"]*"/""/g' install/kubernetes/Makefile.digests
     logrun make -C install/kubernetes all USE_DIGESTS=false
     logrun make update-authors
-    old_proj=$(grep "projects" $ACTS_YAML | sed "$PROJECTS_REGEX")
-    sed -i 's/\(projects\/\)[0-9]\+/\1'$new_proj'/g' $ACTS_YAML
+    if ! version_is_prerelease "$version"; then
+        old_proj=$(grep "projects" $ACTS_YAML | sed "$PROJECTS_REGEX")
+        sed -i 's/\(projects\/\)[0-9]\+/\1'$new_proj'/g' $ACTS_YAML
+    fi
 
     $DIR/prep-changelog.sh "$old_version" "$version"
 
     logecho "Next steps:"
     logecho "* Check all changes and add to a new commit"
+    logecho "  * If this is a prerelease, create a revert commit"
     logecho "* Push the PR to Github for review ('submit-release.sh')"
     logecho "* Close https://github.com/cilium/cilium/projects/$old_proj"
     logecho "* (After PR merge) Use 'tag-release.sh' to prepare tags/release"

contrib/release/tag-release.sh
@@ -61,6 +61,9 @@ main() {
 
     local commit="$(git rev-parse HEAD)"
     BRANCH="$(git symbolic-ref --short HEAD | sed 's/.*\(v[0-9]\+\.[0-9]\+\).*/\1/')"
+    if [ -z "$(git ls-remote --heads $REMOTE $BRANCH)" ]; then
+        BRANCH="master"
+    fi
     echo "Current HEAD is:"
     git log --oneline -1 "$commit"
     if ! commit_in_upstream "$commit" "$BRANCH"; then

commit 49008d8309dea12667f8e56440b01993cab24b5f

contrib: Fix submit-release.sh regression
Commit 9af40702901f ("contrib: Support prereleases in release prep
scripts") accidentally broke this script because the variable wasn't
being set properly for regular releases. Fix up the variable assignment.

Fixes this error:

    Sending pull request...
    Enumerating objects: 37, done.
    Counting objects: 100% (37/37), done.
    Delta compression using up to 8 threads
    Compressing objects: 100% (17/17), done.
    Writing objects: 100% (19/19), 4.10 KiB | 839.00 KiB/s, done.
    Total 19 (delta 15), reused 0 (delta 0)
    remote: Resolving deltas: 100% (15/15), completed with 15 local objects.
    remote:
    remote: Create a pull request for 'pr/prepare-v1.10.5' on GitHub by
    visiting:
    remote:
    https://github.com/joestringer/cilium/pull/new/pr/prepare-v1.10.5
    remote:
    To github.com:joestringer/cilium
     * [new branch]                pr/prepare-v1.10.5 -> pr/prepare-v1.10.5
     /home/joe/git/cilium/contrib/release/submit-release.sh: line 68:
     labels: command not found

     Signal ERR caught!

     Traceback (line function script):
     68 main /home/joe/git/cilium/contrib/release/submit-release.sh

Signed-off-by: Joe Stringer <joe@cilium.io>

contrib/release/submit-release.sh
@@ -65,6 +65,6 @@ PR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git push $USER_REMOTE "$PR_BRANCH"
 LABELS="kind/release"
 if [ "$BRANCH" != "master" ]; then
-    labels = "$LABELS,backport/$(echo $BRANCH | sed 's/^v//')"
+    LABELS="$LABELS,backport/$(echo $BRANCH | sed 's/^v//')"
 fi
 hub pull-request -b "$BRANCH" -l "$LABELS" -F $SUMMARY