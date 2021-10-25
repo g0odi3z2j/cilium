g0odi3z2j/cilium

commit 88bb6335ec89be18ae40f75e58e208da4dc25a37

lb: Use more neutral terminology
This commit replaces "svc master" by "svc frontend" and "slave" by
"backend slot".

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/monitor/datapath_debug.go
@@ -65,24 +65,24 @@ const (
 	DbgPktHash
 	DbgLb6LookupMaster
 	DbgLb6LookupMasterFail
-	DbgLb6LookupSlave
-	DbgLb6LookupSlaveSuccess
-	DbgLb6LookupSlaveV2Fail
+	DbgLb6LookupBackendSlot
+	DbgLb6LookupBackendSlotSuccess
+	DbgLb6LookupBackendSlotV2Fail
 	DbgLb6LookupBackendFail
 	DbgLb6ReverseNatLookup
 	DbgLb6ReverseNat
 	DbgLb4LookupMaster
 	DbgLb4LookupMasterFail
-	DbgLb4LookupSlave
-	DbgLb4LookupSlaveSuccess
-	DbgLb4LookupSlaveV2Fail
+	DbgLb4LookupBackendSlot
+	DbgLb4LookupBackendSlotSuccess
+	DbgLb4LookupBackendSlotV2Fail
 	DbgLb4LookupBackendFail
 	DbgLb4ReverseNatLookup
 	DbgLb4ReverseNat
 	DbgLb4LoopbackSnat
 	DbgLb4LoopbackSnatRev
 	DbgCtLookup4
-	DbgRRSlaveSel
+	DbgRRBackendSlotSel
 	DbgRevProxyLookup
 	DbgRevProxyFound
 	DbgRevProxyUpdate
@@ -288,20 +288,20 @@ func (n *DebugMsg) subTypeString() string {
 		return fmt.Sprintf("Going to the stack, policy-skip=%d", n.Arg1)
 	case DbgPktHash:
 		return fmt.Sprintf("Packet hash=%d (%#x), selected_service=%d", n.Arg1, n.Arg1, n.Arg2)
-	case DbgRRSlaveSel:
-		return fmt.Sprintf("RR slave selection hash=%d (%#x), selected_service=%d", n.Arg1, n.Arg1, n.Arg2)
+	case DbgRRBackendSlotSel:
+		return fmt.Sprintf("RR backend slot selection hash=%d (%#x), selected_service=%d", n.Arg1, n.Arg1, n.Arg2)
 	case DbgLb6LookupMaster:
 		return fmt.Sprintf("Master service lookup, addr.p4=%x key.dport=%d", n.Arg1, byteorder.NetworkToHost(uint16(n.Arg2)))
 	case DbgLb6LookupMasterFail:
 		return fmt.Sprintf("Master service lookup failed, addr.p2=%x addr.p3=%x", n.Arg1, n.Arg2)
-	case DbgLb6LookupSlave, DbgLb4LookupSlave:
-		return fmt.Sprintf("Slave service lookup: slave=%d, dport=%d", n.Arg1, byteorder.NetworkToHost(uint16(n.Arg2)))
-	case DbgLb6LookupSlaveV2Fail, DbgLb4LookupSlaveV2Fail:
-		return fmt.Sprintf("Slave service lookup failed: slave=%d, dport=%d", n.Arg1, byteorder.NetworkToHost(uint16(n.Arg2)))
+	case DbgLb6LookupBackendSlot, DbgLb4LookupBackendSlot:
+		return fmt.Sprintf("Service backend slot lookup: slot=%d, dport=%d", n.Arg1, byteorder.NetworkToHost(uint16(n.Arg2)))
+	case DbgLb6LookupBackendSlotV2Fail, DbgLb4LookupBackendSlotV2Fail:
+		return fmt.Sprintf("Service backend slot lookup failed: slot=%d, dport=%d", n.Arg1, byteorder.NetworkToHost(uint16(n.Arg2)))
 	case DbgLb6LookupBackendFail, DbgLb4LookupBackendFail:
 		return fmt.Sprintf("Backend service lookup failed: backend_id=%d", n.Arg1)
-	case DbgLb6LookupSlaveSuccess:
-		return fmt.Sprintf("Slave service lookup result: target.p4=%x port=%d", n.Arg1, byteorder.NetworkToHost(uint16(n.Arg2)))
+	case DbgLb6LookupBackendSlotSuccess:
+		return fmt.Sprintf("Service backend slot lookup result: target.p4=%x port=%d", n.Arg1, byteorder.NetworkToHost(uint16(n.Arg2)))
 	case DbgLb6ReverseNatLookup, DbgLb4ReverseNatLookup:
 		return fmt.Sprintf("Reverse NAT lookup, index=%d", byteorder.NetworkToHost(uint16(n.Arg1)))
 	case DbgLb6ReverseNat:
@@ -310,8 +310,8 @@ func (n *DebugMsg) subTypeString() string {
 		return fmt.Sprintf("Master service lookup, addr=%s key.dport=%d", ip4Str(n.Arg1), byteorder.NetworkToHost(uint16(n.Arg2)))
 	case DbgLb4LookupMasterFail:
 		return "Master service lookup failed"
-	case DbgLb4LookupSlaveSuccess:
-		return fmt.Sprintf("Slave service lookup result: target=%s port=%d", ip4Str(n.Arg1), byteorder.NetworkToHost(uint16(n.Arg2)))
+	case DbgLb4LookupBackendSlotSuccess:
+		return fmt.Sprintf("Service backend slot lookup result: target=%s port=%d", ip4Str(n.Arg1), byteorder.NetworkToHost(uint16(n.Arg2)))
 	case DbgLb4ReverseNat:
 		return fmt.Sprintf("Performing reverse NAT, address=%s port=%d", ip4Str(n.Arg1), byteorder.NetworkToHost(uint16(n.Arg2)))
 	case DbgLb4LoopbackSnat:

bpf/bpf_sock.c
@@ -270,7 +270,7 @@ static __always_inline int __sock4_xlate_fwd(struct bpf_sock_addr *ctx,
 		.address	= ctx->user_ip4,
 		.dport		= ctx_dst_port(ctx),
 	}, orig_key = key;
-	struct lb4_service *slave_svc;
+	struct lb4_service *backend_slot;
 	bool backend_from_affinity = false;
 	__u32 backend_id = 0;
 
@@ -323,14 +323,14 @@ static __always_inline int __sock4_xlate_fwd(struct bpf_sock_addr *ctx,
 	if (backend_id == 0) {
 		backend_from_affinity = false;
 
-		key.slave = (sock_local_cookie(ctx_full) % svc->count) + 1;
-		slave_svc = __lb4_lookup_slave(&key);
-		if (!slave_svc) {
-			update_metrics(0, METRIC_EGRESS, REASON_LB_NO_SLAVE);
+		key.backend_slot = (sock_local_cookie(ctx_full) % svc->count) + 1;
+		backend_slot = __lb4_lookup_backend_slot(&key);
+		if (!backend_slot) {
+			update_metrics(0, METRIC_EGRESS, REASON_LB_NO_BACKEND_SLOT);
 			return -ENOENT;
 		}
 
-		backend_id = slave_svc->backend_id;
+		backend_id = backend_slot->backend_id;
 		backend = __lb4_lookup_backend(backend_id);
 	}
 
@@ -712,7 +712,7 @@ static __always_inline int __sock6_xlate_fwd(struct bpf_sock_addr *ctx,
 	struct lb6_key key = {
 		.dport		= ctx_dst_port(ctx),
 	}, orig_key;
-	struct lb6_service *slave_svc;
+	struct lb6_service *backend_slot;
 	bool backend_from_affinity = false;
 	__u32 backend_id = 0;
 
@@ -745,14 +745,14 @@ static __always_inline int __sock6_xlate_fwd(struct bpf_sock_addr *ctx,
 	if (backend_id == 0) {
 		backend_from_affinity = false;
 
-		key.slave = (sock_local_cookie(ctx) % svc->count) + 1;
-		slave_svc = __lb6_lookup_slave(&key);
-		if (!slave_svc) {
-			update_metrics(0, METRIC_EGRESS, REASON_LB_NO_SLAVE);
+		key.backend_slot = (sock_local_cookie(ctx) % svc->count) + 1;
+		backend_slot = __lb6_lookup_backend_slot(&key);
+		if (!backend_slot) {
+			update_metrics(0, METRIC_EGRESS, REASON_LB_NO_BACKEND_SLOT);
 			return -ENOENT;
 		}
 
-		backend_id = slave_svc->backend_id;
+		backend_id = backend_slot->backend_id;
 		backend = __lb6_lookup_backend(backend_id);
 	}
 

bpf/lib/common.h
@@ -361,13 +361,13 @@ enum {
  * If reason is larger than below then this is a drop reason and
  * value corresponds to -(DROP_*), see above.
  */
-#define REASON_FORWARDED	0
-#define REASON_PLAINTEXT	3
-#define REASON_DECRYPT		4
-#define REASON_LB_NO_SLAVE	5
-#define REASON_LB_NO_BACKEND	6
-#define REASON_LB_REVNAT_UPDATE	7
-#define REASON_LB_REVNAT_STALE	8
+#define REASON_FORWARDED		0
+#define REASON_PLAINTEXT		3
+#define REASON_DECRYPT			4
+#define REASON_LB_NO_BACKEND_SLOT	5
+#define REASON_LB_NO_BACKEND		6
+#define REASON_LB_REVNAT_UPDATE		7
+#define REASON_LB_REVNAT_STALE		8
 
 /* Lookup scope for externalTrafficPolicy=Local */
 #define LB_LOOKUP_SCOPE_EXT	0
@@ -501,7 +501,7 @@ enum {
 #define TUPLE_F_OUT		0	/* Outgoing flow */
 #define TUPLE_F_IN		1	/* Incoming flow */
 #define TUPLE_F_RELATED		2	/* Flow represents related packets */
-#define TUPLE_F_SERVICE		4	/* Flow represents service/slave map */
+#define TUPLE_F_SERVICE		4	/* Flow represents packets to service */
 
 #define CT_EGRESS 0
 #define CT_INGRESS 1
@@ -600,7 +600,7 @@ struct ct_entry {
 struct lb6_key {
 	union v6addr address;	/* Service virtual IPv6 address */
 	__be16 dport;		/* L4 port filter, if unset, all ports apply */
-	__u16 slave;		/* Backend iterator, 0 indicates the master service */
+	__u16 backend_slot;	/* Backend iterator, 0 indicates the svc frontend */
 	__u8 proto;		/* L4 protocol, currently not used (set to 0) */
 	__u8 scope;		/* LB_LOOKUP_SCOPE_* for externalTrafficPolicy=Local */
 	__u8 pad[2];
@@ -610,7 +610,7 @@ struct lb6_key {
 struct lb6_service {
 	union {
 		__u32 backend_id;	/* Backend ID in lb6_backends */
-		__u32 affinity_timeout;	/* In seconds, only for master svc */
+		__u32 affinity_timeout;	/* In seconds, only for svc frontend */
 	};
 	__u16 count;
 	__u16 rev_nat_index;
@@ -647,7 +647,7 @@ struct ipv6_revnat_entry {
 struct lb4_key {
 	__be32 address;		/* Service virtual IPv4 address */
 	__be16 dport;		/* L4 port filter, if unset, all ports apply */
-	__u16 slave;		/* Backend iterator, 0 indicates the master service */
+	__u16 backend_slot;	/* Backend iterator, 0 indicates the svc frontend */
 	__u8 proto;		/* L4 protocol, currently not used (set to 0) */
 	__u8 scope;		/* LB_LOOKUP_SCOPE_* for externalTrafficPolicy=Local */
 	__u8 pad[2];
@@ -656,10 +656,10 @@ struct lb4_key {
 struct lb4_service {
 	union {
 		__u32 backend_id;		/* Backend ID in lb4_backends */
-		__u32 affinity_timeout;		/* In seconds, only for master svc */
+		__u32 affinity_timeout;		/* In seconds, only for svc frontend */
 	};
-	/* For the master service, count denotes number of service endpoints.
-	 * For service endpoints, zero. (Previously, legacy service ID)
+	/* For the service frontend, count denotes number of service backend
+	 * slots (otherwise zero).
 	 */
 	__u16 count;
 	__u16 rev_nat_index;	/* Reverse NAT ID in lb4_reverse_nat */

bpf/lib/dbg.h
@@ -28,26 +28,26 @@ enum {
 	DBG_TO_HOST,
 	DBG_TO_STACK,
 	DBG_PKT_HASH,
-	DBG_LB6_LOOKUP_MASTER,
-	DBG_LB6_LOOKUP_MASTER_FAIL,
-	DBG_LB6_LOOKUP_SLAVE,
-	DBG_LB6_LOOKUP_SLAVE_SUCCESS,
-	DBG_LB6_LOOKUP_SLAVE_V2_FAIL,
+	DBG_LB6_LOOKUP_FRONTEND,
+	DBG_LB6_LOOKUP_FRONTEND_FAIL,
+	DBG_LB6_LOOKUP_BACKEND_SLOT,
+	DBG_LB6_LOOKUP_BACKEND_SLOT_SUCCESS,
+	DBG_LB6_LOOKUP_BACKEND_SLOT_V2_FAIL,
 	DBG_LB6_LOOKUP_BACKEND_FAIL,
 	DBG_LB6_REVERSE_NAT_LOOKUP,
 	DBG_LB6_REVERSE_NAT,
-	DBG_LB4_LOOKUP_MASTER,
-	DBG_LB4_LOOKUP_MASTER_FAIL,
-	DBG_LB4_LOOKUP_SLAVE,
-	DBG_LB4_LOOKUP_SLAVE_SUCCESS,
-	DBG_LB4_LOOKUP_SLAVE_V2_FAIL,
+	DBG_LB4_LOOKUP_FRONTEND,
+	DBG_LB4_LOOKUP_FRONTEND_FAIL,
+	DBG_LB4_LOOKUP_BACKEND_SLOT,
+	DBG_LB4_LOOKUP_BACKEND_SLOT_SUCCESS,
+	DBG_LB4_LOOKUP_BACKEND_SLOT_V2_FAIL,
 	DBG_LB4_LOOKUP_BACKEND_FAIL,
 	DBG_LB4_REVERSE_NAT_LOOKUP,
 	DBG_LB4_REVERSE_NAT,
 	DBG_LB4_LOOPBACK_SNAT,
 	DBG_LB4_LOOPBACK_SNAT_REV,
 	DBG_CT_LOOKUP4,
-	DBG_RR_SLAVE_SEL,
+	DBG_RR_BACKEND_SLOT_SEL,
 	DBG_REV_PROXY_LOOKUP,
 	DBG_REV_PROXY_FOUND,
 	DBG_REV_PROXY_UPDATE,

bpf/lib/lb.h
@@ -249,15 +249,15 @@ bool lb6_svc_is_routable(const struct lb6_service *svc)
 	return __lb_svc_is_routable(svc->flags);
 }
 
-static __always_inline int lb6_select_slave(__u16 count)
+static __always_inline int lb6_select_backend_slot(__u16 count)
 {
-	/* Slave 0 is reserved for the master slot */
+	/* Backend slot 0 is reserved for the service frontend */
 	return (get_prandom_u32() % count) + 1;
 }
 
-static __always_inline int lb4_select_slave(__u16 count)
+static __always_inline int lb4_select_backend_slot(__u16 count)
 {
-	/* Slave 0 is reserved for the master slot */
+	/* Backend slot 0 is reserved for the service frontend */
 	return (get_prandom_u32() % count) + 1;
 }
 
@@ -444,7 +444,7 @@ struct lb6_service *lb6_lookup_service(struct lb6_key *key,
 	struct lb6_service *svc;
 
 	key->scope = LB_LOOKUP_SCOPE_EXT;
-	key->slave = 0;
+	key->backend_slot = 0;
 	svc = map_lookup_elem(&LB6_SERVICES_MAP_V2, key);
 	if (svc) {
 		if (!scope_switch || !lb6_svc_is_local_scope(svc))
@@ -476,24 +476,24 @@ lb6_lookup_backend(struct __ctx_buff *ctx __maybe_unused, __u16 backend_id)
 }
 
 static __always_inline
-struct lb6_service *__lb6_lookup_slave(struct lb6_key *key)
+struct lb6_service *__lb6_lookup_backend_slot(struct lb6_key *key)
 {
 	return map_lookup_elem(&LB6_SERVICES_MAP_V2, key);
 }
 
 static __always_inline
-struct lb6_service *lb6_lookup_slave(struct __ctx_buff *ctx __maybe_unused,
-				     struct lb6_key *key, __u16 slave)
+struct lb6_service *lb6_lookup_backend_slot(struct __ctx_buff *ctx __maybe_unused,
+					    struct lb6_key *key, __u16 slot)
 {
 	struct lb6_service *svc;
 
-	key->slave = slave;
-	cilium_dbg_lb(ctx, DBG_LB6_LOOKUP_SLAVE, key->slave, key->dport);
-	svc = __lb6_lookup_slave(key);
+	key->backend_slot = slot;
+	cilium_dbg_lb(ctx, DBG_LB6_LOOKUP_BACKEND_SLOT, key->backend_slot, key->dport);
+	svc = __lb6_lookup_backend_slot(key);
 	if (svc)
 		return svc;
 
-	cilium_dbg_lb(ctx, DBG_LB6_LOOKUP_SLAVE_V2_FAIL, key->slave, key->dport);
+	cilium_dbg_lb(ctx, DBG_LB6_LOOKUP_BACKEND_SLOT_V2_FAIL, key->backend_slot, key->dport);
 
 	return NULL;
 }
@@ -634,8 +634,8 @@ static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
 	union v6addr *addr;
 	__u8 flags = tuple->flags;
 	struct lb6_backend *backend;
-	struct lb6_service *slave_svc;
-	int slave;
+	struct lb6_service *backend_slot;
+	int slot;
 	__u32 backend_id = 0;
 	int ret;
 #ifdef ENABLE_SESSION_AFFINITY
@@ -659,14 +659,14 @@ static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
 		}
 #endif
 		if (backend_id == 0) {
-			slave = lb6_select_slave(svc->count);
-			slave_svc = lb6_lookup_slave(ctx, key, slave);
-			if (!slave_svc)
+			slot = lb6_select_backend_slot(svc->count);
+			backend_slot = lb6_lookup_backend_slot(ctx, key, slot);
+			if (!backend_slot)
 				goto drop_no_service;
 
-			backend_id = slave_svc->backend_id;
+			backend_id = backend_slot->backend_id;
 
-			backend = lb6_lookup_backend(ctx, slave_svc->backend_id);
+			backend = lb6_lookup_backend(ctx, backend_slot->backend_id);
 			if (backend == NULL)
 				goto drop_no_service;
 		}
@@ -702,11 +702,11 @@ static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
 								     &client_id);
 #endif
 		if (backend_id == 0) {
-			slave = lb6_select_slave(svc->count);
-			slave_svc = lb6_lookup_slave(ctx, key, slave);
-			if (!slave_svc)
+			slot = lb6_select_backend_slot(svc->count);
+			backend_slot = lb6_lookup_backend_slot(ctx, key, slot);
+			if (!backend_slot)
 				goto drop_no_service;
-			backend_id = slave_svc->backend_id;
+			backend_id = backend_slot->backend_id;
 		}
 
 		state->backend_id = backend_id;
@@ -720,18 +720,18 @@ static __always_inline int lb6_local(const void *map, struct __ctx_buff *ctx,
 	 */
 	backend = lb6_lookup_backend(ctx, state->backend_id);
 	if (!backend) {
-		key->slave = 0;
+		key->backend_slot = 0;
 		svc = lb6_lookup_service(key, false);
 		if (!svc)
 			goto drop_no_service;
-		slave = lb6_select_slave(svc->count);
-		slave_svc = lb6_lookup_slave(ctx, key, slave);
-		if (!slave_svc)
+		slot = lb6_select_backend_slot(svc->count);
+		backend_slot = lb6_lookup_backend_slot(ctx, key, slot);
+		if (!backend_slot)
 			goto drop_no_service;
-		backend = lb6_lookup_backend(ctx, slave_svc->backend_id);
+		backend = lb6_lookup_backend(ctx, backend_slot->backend_id);
 		if (!backend)
 			goto drop_no_service;
-		state->backend_id = slave_svc->backend_id;
+		state->backend_id = backend_slot->backend_id;
 		ct_update6_backend_id(map, tuple, state);
 	}
 
@@ -768,7 +768,7 @@ struct lb6_service *lb6_lookup_service(struct lb6_key *key __maybe_unused,
 }
 
 static __always_inline
-struct lb6_service *__lb6_lookup_slave(struct lb6_key *key __maybe_unused)
+struct lb6_service *__lb6_lookup_backend_slot(struct lb6_key *key __maybe_unused)
 {
 	return NULL;
 }
@@ -911,7 +911,7 @@ struct lb4_service *lb4_lookup_service(struct lb4_key *key,
 	struct lb4_service *svc;
 
 	key->scope = LB_LOOKUP_SCOPE_EXT;
-	key->slave = 0;
+	key->backend_slot = 0;
 	svc = map_lookup_elem(&LB4_SERVICES_MAP_V2, key);
 	if (svc) {
 		if (!scope_switch || !lb4_svc_is_local_scope(svc))
@@ -943,24 +943,24 @@ lb4_lookup_backend(struct __ctx_buff *ctx __maybe_unused, __u16 backend_id)
 }
 
 static __always_inline
-struct lb4_service *__lb4_lookup_slave(struct lb4_key *key)
+struct lb4_service *__lb4_lookup_backend_slot(struct lb4_key *key)
 {
 	return map_lookup_elem(&LB4_SERVICES_MAP_V2, key);
 }
 
 static __always_inline
-struct lb4_service *lb4_lookup_slave(struct __ctx_buff *ctx __maybe_unused,
-				     struct lb4_key *key, __u16 slave)
+struct lb4_service *lb4_lookup_backend_slot(struct __ctx_buff *ctx __maybe_unused,
+					    struct lb4_key *key, __u16 slot)
 {
 	struct lb4_service *svc;
 
-	key->slave = slave;
-	cilium_dbg_lb(ctx, DBG_LB4_LOOKUP_SLAVE, key->slave, key->dport);
-	svc = __lb4_lookup_slave(key);
+	key->backend_slot = slot;
+	cilium_dbg_lb(ctx, DBG_LB4_LOOKUP_BACKEND_SLOT, key->backend_slot, key->dport);
+	svc = __lb4_lookup_backend_slot(key);
 	if (svc)
 		return svc;
 
-	cilium_dbg_lb(ctx, DBG_LB4_LOOKUP_SLAVE_V2_FAIL, key->slave, key->dport);
+	cilium_dbg_lb(ctx, DBG_LB4_LOOKUP_BACKEND_SLOT_V2_FAIL, key->backend_slot, key->dport);
 
 	return NULL;
 }
@@ -1118,8 +1118,8 @@ static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
 	__be32 new_saddr = 0, new_daddr;
 	__u8 flags = tuple->flags;
 	struct lb4_backend *backend;
-	struct lb4_service *slave_svc;
-	int slave;
+	struct lb4_service *backend_slot;
+	int slot;
 	__u32 backend_id = 0;
 	int ret;
 #ifdef ENABLE_SESSION_AFFINITY
@@ -1142,12 +1142,12 @@ static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
 #endif
 		if (backend_id == 0) {
 			/* No CT entry has been found, so select a svc endpoint */
-			slave = lb4_select_slave(svc->count);
-			slave_svc = lb4_lookup_slave(ctx, key, slave);
-			if (!slave_svc)
+			slot = lb4_select_backend_slot(svc->count);
+			backend_slot = lb4_lookup_backend_slot(ctx, key, slot);
+			if (!backend_slot)
 				goto drop_no_service;
 
-			backend_id = slave_svc->backend_id;
+			backend_id = backend_slot->backend_id;
 
 			backend = lb4_lookup_backend(ctx, backend_id);
 			if (backend == NULL)
@@ -1196,12 +1196,12 @@ static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
 								     &client_id);
 #endif
 		if (backend_id == 0) {
-			slave = lb4_select_slave(svc->count);
-			slave_svc = lb4_lookup_slave(ctx, key, slave);
-			if (!slave_svc)
+			slot = lb4_select_backend_slot(svc->count);
+			backend_slot = lb4_lookup_backend_slot(ctx, key, slot);
+			if (!backend_slot)
 				goto drop_no_service;
 
-			backend_id = slave_svc->backend_id;
+			backend_id = backend_slot->backend_id;
 		}
 
 		state->backend_id = backend_id;
@@ -1215,18 +1215,18 @@ static __always_inline int lb4_local(const void *map, struct __ctx_buff *ctx,
 	 */
 	backend = lb4_lookup_backend(ctx, state->backend_id);
 	if (!backend) {
-		key->slave = 0;
+		key->backend_slot = 0;
 		svc = lb4_lookup_service(key, false);
 		if (!svc)
 			goto drop_no_service;
-		slave = lb4_select_slave(svc->count);
-		slave_svc = lb4_lookup_slave(ctx, key, slave);
-		if (!slave_svc)
+		slot = lb4_select_backend_slot(svc->count);
+		backend_slot = lb4_lookup_backend_slot(ctx, key, slot);
+		if (!backend_slot)
 			goto drop_no_service;
-		backend = lb4_lookup_backend(ctx, slave_svc->backend_id);
+		backend = lb4_lookup_backend(ctx, backend_slot->backend_id);
 		if (!backend)
 			goto drop_no_service;
-		state->backend_id = slave_svc->backend_id;
+		state->backend_id = backend_slot->backend_id;
 		ct_update4_backend_id(map, tuple, state);
 	}
 

cilium/cmd/bpf_lb_list.go
@@ -71,7 +71,7 @@ func dumpSVC(serviceList map[string][]string) {
 		backendID := svcVal.GetBackendID()
 		flags := loadbalancer.ServiceFlags(svcVal.GetFlags())
 
-		if svcKey.GetSlave() == 0 {
+		if svcKey.GetBackendSlot() == 0 {
 			ip := "0.0.0.0"
 			if svcKey.IsIPv6() {
 				ip = "[::]"

pkg/logging/logfields/logfields.go
@@ -238,8 +238,8 @@ const (
 	// BackendName is the name of the backend
 	BackendName = "backendName"
 
-	// SlaveSlot is the slot number in a service BPF map
-	SlaveSlot = "slaveSlot"
+	// BackendSlot is the backend slot number in a service BPF map
+	BackendSlot = "backendSlot"
 
 	// CiliumNetworkPolicy is a cilium specific NetworkPolicy
 	CiliumNetworkPolicy = "ciliumNetworkPolicy"

pkg/maps/lbmap/ipv4.go
@@ -149,20 +149,20 @@ func (in *pad2uint8) DeepCopyInto(out *pad2uint8) {
 // +k8s:deepcopy-gen=true
 // +k8s:deepcopy-gen:interfaces=github.com/cilium/cilium/pkg/bpf.MapKey
 type Service4Key struct {
-	Address types.IPv4 `align:"address"`
-	Port    uint16     `align:"dport"`
-	Slave   uint16     `align:"slave"`
-	Proto   uint8      `align:"proto"`
-	Scope   uint8      `align:"scope"`
-	Pad     pad2uint8  `align:"pad"`
+	Address     types.IPv4 `align:"address"`
+	Port        uint16     `align:"dport"`
+	BackendSlot uint16     `align:"backend_slot"`
+	Proto       uint8      `align:"proto"`
+	Scope       uint8      `align:"scope"`
+	Pad         pad2uint8  `align:"pad"`
 }
 
-func NewService4Key(ip net.IP, port uint16, proto u8proto.U8proto, scope uint8, slave uint16) *Service4Key {
+func NewService4Key(ip net.IP, port uint16, proto u8proto.U8proto, scope uint8, slot uint16) *Service4Key {
 	key := Service4Key{
-		Port:  port,
-		Proto: uint8(proto),
-		Scope: scope,
-		Slave: slave,
+		Port:        port,
+		Proto:       uint8(proto),
+		Scope:       scope,
+		BackendSlot: slot,
 	}
 
 	copy(key.Address[:], ip.To4())
@@ -183,8 +183,8 @@ func (k *Service4Key) NewValue() bpf.MapValue    { return &Service4Value{} }
 func (k *Service4Key) IsIPv6() bool              { return false }
 func (k *Service4Key) IsSurrogate() bool         { return k.GetAddress().IsUnspecified() }
 func (k *Service4Key) Map() *bpf.Map             { return Service4MapV2 }
-func (k *Service4Key) SetSlave(slave int)        { k.Slave = uint16(slave) }
-func (k *Service4Key) GetSlave() int             { return int(k.Slave) }
+func (k *Service4Key) SetBackendSlot(slot int)   { k.BackendSlot = uint16(slot) }
+func (k *Service4Key) GetBackendSlot() int       { return int(k.BackendSlot) }
 func (k *Service4Key) SetScope(scope uint8)      { k.Scope = scope }
 func (k *Service4Key) GetScope() uint8           { return k.Scope }
 func (k *Service4Key) GetAddress() net.IP        { return k.Address.IP() }

commit 39b3db513c9c138ea6a0f2e7e255e1ffeadea0f8

monitor: Fix mismatching frontend service debug trace types
It seems they were missed in commit 88bb6335ec8 ("lb: Use more neutral
terminology"). However, this was harmless as these debug types are no
longer used as of commit ec3241e0ad ("datapath: Do not log if svc is not
found"). This commit is essentially cosmetic.

Fixes: 88bb6335ec8 ("lb: Use more neutral terminology")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/monitor/datapath_debug.go
@@ -56,16 +56,16 @@ const (
 	DbgToHost
 	DbgToStack
 	DbgPktHash
-	DbgLb6LookupMaster
-	DbgLb6LookupMasterFail
+	DbgLb6LookupFrontend
+	DbgLb6LookupFrontendFail
 	DbgLb6LookupBackendSlot
 	DbgLb6LookupBackendSlotSuccess
 	DbgLb6LookupBackendSlotV2Fail
 	DbgLb6LookupBackendFail
 	DbgLb6ReverseNatLookup
 	DbgLb6ReverseNat
-	DbgLb4LookupMaster
-	DbgLb4LookupMasterFail
+	DbgLb4LookupFrontend
+	DbgLb4LookupFrontendFail
 	DbgLb4LookupBackendSlot
 	DbgLb4LookupBackendSlotSuccess
 	DbgLb4LookupBackendSlotV2Fail
@@ -294,10 +294,10 @@ func (n *DebugMsg) Message() string {
 		return fmt.Sprintf("Packet hash=%d (%#x), selected_service=%d", n.Arg1, n.Arg1, n.Arg2)
 	case DbgRRBackendSlotSel:
 		return fmt.Sprintf("RR backend slot selection hash=%d (%#x), selected_service=%d", n.Arg1, n.Arg1, n.Arg2)
-	case DbgLb6LookupMaster:
-		return fmt.Sprintf("Master service lookup, addr.p4=%x key.dport=%d", n.Arg1, byteorder.NetworkToHost16(uint16(n.Arg2)))
-	case DbgLb6LookupMasterFail:
-		return fmt.Sprintf("Master service lookup failed, addr.p2=%x addr.p3=%x", n.Arg1, n.Arg2)
+	case DbgLb6LookupFrontend:
+		return fmt.Sprintf("Frontend service lookup, addr.p4=%x key.dport=%d", n.Arg1, byteorder.NetworkToHost16(uint16(n.Arg2)))
+	case DbgLb6LookupFrontendFail:
+		return fmt.Sprintf("Frontend service lookup failed, addr.p2=%x addr.p3=%x", n.Arg1, n.Arg2)
 	case DbgLb6LookupBackendSlot, DbgLb4LookupBackendSlot:
 		return fmt.Sprintf("Service backend slot lookup: slot=%d, dport=%d", n.Arg1, byteorder.NetworkToHost16(uint16(n.Arg2)))
 	case DbgLb6LookupBackendSlotV2Fail, DbgLb4LookupBackendSlotV2Fail:
@@ -310,10 +310,10 @@ func (n *DebugMsg) Message() string {
 		return fmt.Sprintf("Reverse NAT lookup, index=%d", byteorder.NetworkToHost16(uint16(n.Arg1)))
 	case DbgLb6ReverseNat:
 		return fmt.Sprintf("Performing reverse NAT, address.p4=%x port=%d", n.Arg1, byteorder.NetworkToHost16(uint16(n.Arg2)))
-	case DbgLb4LookupMaster:
-		return fmt.Sprintf("Master service lookup, addr=%s key.dport=%d", ip4Str(n.Arg1), byteorder.NetworkToHost16(uint16(n.Arg2)))
-	case DbgLb4LookupMasterFail:
-		return "Master service lookup failed"
+	case DbgLb4LookupFrontend:
+		return fmt.Sprintf("Frontend service lookup, addr=%s key.dport=%d", ip4Str(n.Arg1), byteorder.NetworkToHost16(uint16(n.Arg2)))
+	case DbgLb4LookupFrontendFail:
+		return "Frontend service lookup failed"
 	case DbgLb4LookupBackendSlotSuccess:
 		return fmt.Sprintf("Service backend slot lookup result: target=%s port=%d", ip4Str(n.Arg1), byteorder.NetworkToHost16(uint16(n.Arg2)))
 	case DbgLb4ReverseNat: