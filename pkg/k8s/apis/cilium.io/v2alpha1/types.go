g0odi3z2j/cilium

commit 6ef88473c6cc8b56ae1eb19522d8f6a0b6167c58

Add custom resource for egress nat gateway policy
Adds CiliumEgressNATPolicy crds for egress nat gateway configuration
apis. Specifically each policy consists of a list of endpoint selectors,
a list of destination cidrs and SNAT ip. This information
is used to update egress ebpf map in the datapath. The related api is
currently in v2alpha1.

Signed-off-by: Bolun Zhao <blzhao@google.com>

pkg/k8s/apis/cilium.io/v2alpha1/types.go
@@ -1,0 +1,82 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v2alpha1
+
+import (
+	slimv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:singular="ciliumegressnatpolicy",path="ciliumegressnatpolicies",scope="Cluster"
+// +kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",description="The age of the identity",name="Age",type=date
+// +kubebuilder:subresource:status
+// +kubebuilder:storageversion
+
+type CiliumEgressNATPolicy struct {
+	// +k8s:openapi-gen=false
+	// +deepequal-gen=false
+	metav1.TypeMeta `json:",inline"`
+	// +k8s:openapi-gen=false
+	// +deepequal-gen=false
+	metav1.ObjectMeta `json:"metadata"`
+
+	Spec CiliumEgressNATPolicySpec `json:"spec,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:openapi-gen=false
+// +deepequal-gen=false
+
+// CiliumEgressNATPolicyList is a list of CiliumEgressNATPolicy objects.
+type CiliumEgressNATPolicyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	// Items is a list of CiliumEgressNATPolicy.
+	Items []CiliumEgressNATPolicy `json:"items"`
+}
+
+type CiliumEgressNATPolicySpec struct {
+	// Egress represents a list of rules by which egress traffic is
+	// filtered from the source pods.
+	Egress []EgressRule `json:"egress"`
+
+	// DestinationCIDRs is a list of destination CIDRs for destination IP addresses.
+	// If a destination IP matches any one CIDR, it will be selected.
+	DestinationCIDRs []string `json:"destinationCidrs"`
+
+	// EgressSourceIP is a source ip address that the egress traffic is
+	// redirected to and SNATed with.
+	//
+	// Example:
+	// When it is set to "192.168.1.100", matched egress packets will be
+	// redirected to node with ip 192.168.1.100 and SNAT’ed with IP address 192.168.1.100.
+	//
+	// +kubebuilder:validation:Pattern=`((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))`
+	EgressSourceIP string `json:"egressSourceIp"`
+}
+
+type EgressRule struct {
+	// Selects Namespaces using cluster-scoped labels. This field follows standard label
+	// selector semantics; if present but empty, it selects all namespaces.
+	NamespaceSelector *slimv1.LabelSelector `json:"namespaceSelector,omitempty"`
+
+	// This is a label selector which selects Pods. This field follows standard label
+	// selector semantics; if present but empty, it selects all pods.
+	PodSelector *slimv1.LabelSelector `json:"podSelector,omitempty"`
+}

examples/crds/ciliumegressnatpolicies.yaml
@@ -1,0 +1,190 @@
+
+---
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  annotations:
+    controller-gen.kubebuilder.io/version: v0.3.1-0.20200716001835-4a903ddb7005
+  creationTimestamp: null
+  name: ciliumegressnatpolicies.cilium.io
+spec:
+  group: cilium.io
+  names:
+    kind: CiliumEgressNATPolicy
+    listKind: CiliumEgressNATPolicyList
+    plural: ciliumegressnatpolicies
+    singular: ciliumegressnatpolicy
+  scope: Cluster
+  versions:
+  - additionalPrinterColumns:
+    - description: The age of the identity
+      jsonPath: .metadata.creationTimestamp
+      name: Age
+      type: date
+    name: v2alpha1
+    schema:
+      openAPIV3Schema:
+        properties:
+          apiVersion:
+            description: 'APIVersion defines the versioned schema of this representation
+              of an object. Servers should convert recognized schemas to the latest
+              internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources'
+            type: string
+          kind:
+            description: 'Kind is a string value representing the REST resource this
+              object represents. Servers may infer this from the endpoint the client
+              submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds'
+            type: string
+          metadata:
+            type: object
+          spec:
+            properties:
+              destinationCidrs:
+                description: DestinationCIDRs is a list of destination CIDRs for destination
+                  IP addresses. If a destination IP matches any one CIDR, it will
+                  be selected.
+                items:
+                  type: string
+                type: array
+              egress:
+                description: Egress represents a list of rules by which egress traffic
+                  is filtered from the source pods.
+                items:
+                  properties:
+                    namespaceSelector:
+                      description: Selects Namespaces using cluster-scoped labels.
+                        This field follows standard label selector semantics; if present
+                        but empty, it selects all namespaces.
+                      properties:
+                        matchExpressions:
+                          description: matchExpressions is a list of label selector
+                            requirements. The requirements are ANDed.
+                          items:
+                            description: A label selector requirement is a selector
+                              that contains values, a key, and an operator that relates
+                              the key and values.
+                            properties:
+                              key:
+                                description: key is the label key that the selector
+                                  applies to.
+                                type: string
+                              operator:
+                                description: operator represents a key's relationship
+                                  to a set of values. Valid operators are In, NotIn,
+                                  Exists and DoesNotExist.
+                                enum:
+                                - In
+                                - NotIn
+                                - Exists
+                                - DoesNotExist
+                                type: string
+                              values:
+                                description: values is an array of string values.
+                                  If the operator is In or NotIn, the values array
+                                  must be non-empty. If the operator is Exists or
+                                  DoesNotExist, the values array must be empty. This
+                                  array is replaced during a strategic merge patch.
+                                items:
+                                  type: string
+                                type: array
+                            required:
+                            - key
+                            - operator
+                            type: object
+                          type: array
+                        matchLabels:
+                          additionalProperties:
+                            description: MatchLabelsValue represents the value from
+                              the MatchLabels {key,value} pair.
+                            maxLength: 63
+                            pattern: ^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?$
+                            type: string
+                          description: matchLabels is a map of {key,value} pairs.
+                            A single {key,value} in the matchLabels map is equivalent
+                            to an element of matchExpressions, whose key field is
+                            "key", the operator is "In", and the values array contains
+                            only "value". The requirements are ANDed.
+                          type: object
+                      type: object
+                    podSelector:
+                      description: This is a label selector which selects Pods. This
+                        field follows standard label selector semantics; if present
+                        but empty, it selects all pods.
+                      properties:
+                        matchExpressions:
+                          description: matchExpressions is a list of label selector
+                            requirements. The requirements are ANDed.
+                          items:
+                            description: A label selector requirement is a selector
+                              that contains values, a key, and an operator that relates
+                              the key and values.
+                            properties:
+                              key:
+                                description: key is the label key that the selector
+                                  applies to.
+                                type: string
+                              operator:
+                                description: operator represents a key's relationship
+                                  to a set of values. Valid operators are In, NotIn,
+                                  Exists and DoesNotExist.
+                                enum:
+                                - In
+                                - NotIn
+                                - Exists
+                                - DoesNotExist
+                                type: string
+                              values:
+                                description: values is an array of string values.
+                                  If the operator is In or NotIn, the values array
+                                  must be non-empty. If the operator is Exists or
+                                  DoesNotExist, the values array must be empty. This
+                                  array is replaced during a strategic merge patch.
+                                items:
+                                  type: string
+                                type: array
+                            required:
+                            - key
+                            - operator
+                            type: object
+                          type: array
+                        matchLabels:
+                          additionalProperties:
+                            description: MatchLabelsValue represents the value from
+                              the MatchLabels {key,value} pair.
+                            maxLength: 63
+                            pattern: ^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?$
+                            type: string
+                          description: matchLabels is a map of {key,value} pairs.
+                            A single {key,value} in the matchLabels map is equivalent
+                            to an element of matchExpressions, whose key field is
+                            "key", the operator is "In", and the values array contains
+                            only "value". The requirements are ANDed.
+                          type: object
+                      type: object
+                  type: object
+                type: array
+              egressSourceIp:
+                description: "EgressSourceIP is a source ip address that the egress
+                  traffic is redirected to and SNATed with. \n Example: When it is
+                  set to \"192.168.1.100\", matched egress packets will be redirected
+                  to node with ip 192.168.1.100 and SNAT’ed with IP address 192.168.1.100."
+                pattern: ((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))
+                type: string
+            required:
+            - destinationCidrs
+            - egress
+            - egressSourceIp
+            type: object
+        required:
+        - metadata
+        type: object
+    served: true
+    storage: true
+    subresources:
+      status: {}
+status:
+  acceptedNames:
+    kind: ""
+    plural: ""
+  conditions: []
+  storedVersions: []

Makefile
@@ -305,7 +305,7 @@ CRD_OPTIONS ?= "crd:crdVersions=v1"
 manifests: ## Generate K8s manifests e.g. CRD, RBAC etc.
 	$(eval TMPDIR := $(shell mktemp -d))
 	cd "./vendor/sigs.k8s.io/controller-tools/cmd/controller-gen" && \
-	go run ./... $(CRD_OPTIONS) paths="$(PWD)/pkg/k8s/apis/cilium.io/v2" output:crd:artifacts:config="$(TMPDIR)";
+	go run ./... $(CRD_OPTIONS) paths="$(PWD)/pkg/k8s/apis/cilium.io/v2;$(PWD)/pkg/k8s/apis/cilium.io/v2alpha1" output:crd:artifacts:config="$(TMPDIR)";
 	mv ${TMPDIR}/cilium.io_ciliumnetworkpolicies.yaml ./examples/crds/ciliumnetworkpolicies.yaml
 	mv ${TMPDIR}/cilium.io_ciliumclusterwidenetworkpolicies.yaml ./examples/crds/ciliumclusterwidenetworkpolicies.yaml
 	mv ${TMPDIR}/cilium.io_ciliumendpoints.yaml ./examples/crds/ciliumendpoints.yaml
@@ -313,6 +313,7 @@ manifests: ## Generate K8s manifests e.g. CRD, RBAC etc.
 	mv ${TMPDIR}/cilium.io_ciliumnodes.yaml ./examples/crds/ciliumnodes.yaml
 	mv ${TMPDIR}/cilium.io_ciliumexternalworkloads.yaml ./examples/crds/ciliumexternalworkloads.yaml
 	mv ${TMPDIR}/cilium.io_ciliumlocalredirectpolicies.yaml ./examples/crds/ciliumlocalredirectpolicies.yaml
+	mv ${TMPDIR}/cilium.io_ciliumegressnatpolicies.yaml ./examples/crds/ciliumegressnatpolicies.yaml
 	rm -rf $(TMPDIR)
 
 generate-api: api/v1/openapi.yaml ## Generate cilium-agent client, model and server code from openapi spec.
@@ -417,7 +418,7 @@ generate-k8s-api: ## Generate Cilium k8s API client, deepcopy and deepequal Go s
 	policy:api\
 	service:store")
 	$(call generate_k8s_api_deepcopy_deepequal,github.com/cilium/cilium/pkg/policy,"api:kafka")
-	$(call generate_k8s_api_all,github.com/cilium/cilium/pkg/k8s/apis,"cilium.io:v2")
+	$(call generate_k8s_api_all,github.com/cilium/cilium/pkg/k8s/apis,"cilium.io:v2 cilium.io:v2alpha1")
 	$(call generate_k8s_api_deepcopy_deepequal,github.com/cilium/cilium/pkg/aws,"eni:types")
 	$(call generate_k8s_api_deepcopy_deepequal,github.com/cilium/cilium/api,"v1:models")
 	$(call generate_k8s_api_deepcopy_deepequal,github.com/cilium/cilium,"$\

pkg/k8s/apis/cilium.io/v2/client/bindata.go
@@ -2,6 +2,7 @@
 
  //Package client generated by go-bindata.// sources:
 // ../../../../../../examples/crds/ciliumclusterwidenetworkpolicies.yaml
+// ../../../../../../examples/crds/ciliumegressnatpolicies.yaml
 // ../../../../../../examples/crds/ciliumendpoints.yaml
 // ../../../../../../examples/crds/ciliumexternalworkloads.yaml
 // ../../../../../../examples/crds/ciliumidentities.yaml
@@ -100,6 +101,26 @@ func examplesCrdsCiliumclusterwidenetworkpoliciesYaml() (*asset, error) {
 	}
 
 	info := bindataFileInfo{name: "examples/crds/ciliumclusterwidenetworkpolicies.yaml", size: 305264, mode: os.FileMode(416), modTime: time.Unix(1450269211, 0)}
+	a := &asset{bytes: bytes, info: info}
+	return a, nil
+}
+
+var _examplesCrdsCiliumegressnatpoliciesYaml = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xec\x5a\xdd\x6e\xe3\xb8\xf5\xbf\xf7\x53\x1c\xf8\xbf\x7f\x8c\x3d\xb5\x14\xdb\x49\xe6\x43\xbd\x18\x04\x49\x0a\x18\x9d\x0d\x82\x49\x90\x02\x8d\xb3\x05\x2d\x1e\xdb\xdc\x50\xa4\x4a\x52\x49\xbc\x49\x80\xbe\x46\x5f\xaf\x4f\x52\x90\x94\x6c\xd9\x96\x6c\x67\x31\x3b\x40\x01\xf3\x66\x62\xf2\xf0\xf0\xc7\xf3\x7d\x86\x6a\x04\x41\xd0\x20\x29\xbb\x41\xa5\x99\x14\x11\x90\x94\xe1\x93\x41\x61\x7f\xe9\xf0\xfe\x93\x0e\x99\x3c\x78\xe8\x35\xee\x99\xa0\x11\x9c\x66\xda\xc8\xe4\x1b\x6a\x99\xa9\x18\xcf\x70\xcc\x04\x33\x4c\x8a\x46\x82\x86\x50\x62\x48\xd4\x00\x20\x42\x48\x43\xec\xb4\xb6\x3f\x01\x62\x29\x8c\x92\x9c\xa3\x0a\x26\x28\xc2\xfb\x6c\x84\xa3\x8c\x71\x8a\xca\x31\x2f\x8e\x7e\xe8\x86\x87\x61\x2f\xe8\x86\xfd\x6e\xbf\xdb\xfd\xd8\xfb\xd0\xed\xf6\x3e\x1d\x1e\x07\x47\xe4\x73\xf7\x90\xd2\xd1\xc7\x6e\xf7\xb8\x01\x10\x2b\x74\xcc\xaf\x59\x82\xda\x90\x24\x8d\x40\x64\x9c\x37\x00\x04\x49\x30\x82\x98\x71\x96\x25\x38\x51\xa8\xb5\x20\x26\x95\x9c\xc5\x0c\x75\xe8\xe7\x43\x26\x1b\x3a\xc5\xd8\x22\x9b\x28\x99\xa5\xc5\x06\xbb\xe0\x59\xe4\xa8\xf3\x1b\xbb\xc5\x73\xc7\xed\xe2\xe4\xfa\xd2\x72\x9b\xb9\x75\xce\xb4\xf9\x6b\x3d\xcd\x57\xa6\x8d\xa3\x4b\x79\xa6\x08\xaf\xc5\xe5\x68\x34\x13\x93\x8c\x13\x55\x4d\x65\xcf\xd3\xb1\x4c\x31\x82\x53\x9e\x69\x83\xaa\x01\x90\x8b\xcd\x81\x0d\x80\x50\xea\x14\x41\xf8\xa5\x62\xc2\xa0\x3a\x95\x3c\x4b\x0a\x05\x04\x40\x51\xc7\x8a\xa5\xc6\x09\xfa\x7a\x8a\x40\x26\x08\x72\x0c\x66\x8a\xc0\x28\x0a\xc3\x8c\xbf\x15\xc0\xaf\x5a\x8a\x4b\x62\xa6\x11\x84\x85\x56\xc3\x35\x99\xe7\xb4\x5e\xe2\x27\x13\xcc\x7f\x9b\x99\x05\x49\x89\xf1\x13\x7e\xf9\xa1\x4f\x78\x3a\x25\x3d\x7f\xd3\x78\x8a\x09\x89\x72\x7a\x99\xa2\x38\xb9\x1c\xdc\x1c\x5e\x2d\x4d\x03\xa4\x4a\xa6\xa8\x0c\x2b\x94\xe1\x47\xc9\x50\x4b\xb3\xb0\x7c\xbb\x77\x96\xa1\xa7\x02\x6a\x2d\x14\xb5\xbb\x66\x2e\x30\xa4\x39\x06\x7f\x7d\xa6\x41\x61\xaa\x50\xa3\xf0\x36\xbb\xc4\x18\x2c\x11\x11\x20\x47\xbf\x62\x6c\x42\xb8\x42\x65\xd9\x80\x9e\xca\x8c\x53\x6b\xd8\x0f\xa8\x0c\x28\x8c\xe5\x44\xb0\xdf\xe6\xbc\x35\x18\xe9\x0e\xe5\xc4\x60\x6e\x07\x8b\xe1\x14\x24\x08\x87\x07\xc2\x33\xec\x00\x11\x14\x12\x32\x03\x85\xf6\x14\xc8\x44\x89\x9f\x23\xd1\x21\xfc\x2c\x15\x02\x13\x63\x19\xc1\xd4\x98\x54\x47\x07\x07\x13\x66\x0a\x07\x8d\x65\x92\x64\x82\x99\xd9\x81\xf3\x35\x36\xca\x8c\x54\xfa\x80\xe2\x03\xf2\x03\xcd\x26\x01\x51\xf1\x94\x19\x8c\x4d\xa6\xf0\x80\xa4\x2c\x70\xd0\x85\x73\xd2\x30\xa1\xff\xa7\x72\x97\xd6\xef\x96\xb0\x7a\x7d\x6a\xa3\x98\x98\x94\x16\x9c\x6b\x6c\xd0\x80\x75\x0b\x60\x1a\x48\xbe\xd5\xdf\x62\x21\x68\x3b\x65\xa5\xf3\xed\xfc\xea\x1a\x8a\xa3\x9d\x32\x56\xa5\xef\xe4\xbe\xd8\xa8\x17\x2a\xb0\x02\x63\x62\x8c\xca\x2b\x71\xac\x64\xe2\x78\xa2\xa0\xa9\x64\xc2\xb8\x1f\x31\x67\x28\x56\xc5\xaf\xb3\x51\xc2\x8c\xd5\xfb\x3f\x33\xd4\xc6\xea\x2a\x84\x53\x17\xb5\x60\x84\x90\xa5\xd6\x7e\x69\x08\x03\x01\xa7\x24\x41\x7e\x4a\x34\xfe\xe1\x0a\xb0\x92\xd6\x81\x15\xec\x6e\x2a\x28\x07\xdc\x55\x62\x2f\xb5\xd2\x42\x11\xf0\x16\xa3\xda\xbf\x72\x4d\x1a\x26\x9c\x2b\x9c\x32\xaa\xd6\xd6\x57\x74\x7d\x56\x22\x1f\x9c\x7d\xd3\x5e\xed\x36\x36\x5a\xd7\x29\x31\x03\xbf\x3c\x96\xaa\x3c\xbb\xc6\x1c\x60\x70\x69\xa3\x99\x8d\x7f\xd6\xee\x07\x63\x20\x4b\x6c\x06\x97\x90\x10\x13\x4f\x51\x03\x11\x33\x90\x02\x1d\xe7\x0e\x30\x03\x8f\xcc\xe5\x81\xd5\x31\x42\xd0\xc8\x31\xb6\x5a\x5d\x5b\x66\x06\x93\x8a\x4b\xd6\x0a\xbe\xbc\x48\x94\x22\xb3\x95\x35\x1f\xba\xb7\x48\xcd\xe7\x8a\x92\x5d\x97\x64\xa6\x32\x8e\x1a\x46\x33\x78\x9c\xb2\x78\x9a\xf3\x03\xa3\xc8\x78\xcc\xe2\x0a\x9c\xd6\xf8\x19\x37\xa8\x90\x2e\xbc\x20\x77\xa9\x54\x52\xfd\x86\x2b\xd7\x5b\x85\x1f\x2e\x3b\xa6\x24\xc6\x2b\x27\x4e\xa9\xaa\xc9\x56\x2e\xeb\x89\x35\x5c\x14\xbb\x35\x64\x36\xe1\x41\xec\x93\x59\xe0\x72\x1b\x05\x4e\x46\xc8\x2b\xe0\x16\xe3\xda\xf9\x39\x43\x4e\x61\x2c\x39\x97\x8f\x1a\xb4\x21\x82\x12\x95\xef\xcd\xb5\x2c\x15\x68\x4c\x88\x30\x2c\xd6\x7f\x06\x36\x86\x5c\xc8\xb5\x8c\x47\x99\x01\x4c\x52\x33\x73\x46\xa4\x73\xb8\x84\xf3\xc5\x85\x6b\x61\x6d\x13\x99\x1d\xce\x5e\xcf\x9f\x2c\x0c\xbd\xa8\x8a\x76\x90\xdc\xea\xc6\x65\xe7\x5a\xbe\xf3\x06\x9e\xe0\x42\x1d\x53\x98\xf8\x10\x6a\xf3\x7f\x79\x06\x88\x42\x38\xb9\x38\xab\x72\x8f\xc5\xa8\xb5\x9a\x1a\xf0\x27\xab\x4a\x29\x1d\x99\x27\x87\x5d\xa0\x03\x98\x29\x31\xae\x86\x24\x4c\xe8\x3c\x21\x76\x80\xc0\x3d\xce\x7c\xee\xb4\x09\x3a\x45\x45\xec\x21\x8e\x58\xa1\xcb\xbb\x5b\xf9\xa2\xe5\xe1\x58\xe4\x69\x76\xe3\x8e\x5d\x54\xed\xc7\x3d\xce\xb6\x91\xac\x08\xcb\xe2\x60\x3a\x2f\x19\xac\xd4\xec\x84\xbb\x8b\xf3\xe6\xdd\x04\x05\xae\x44\x4a\x39\x43\x97\xd3\xb6\x52\x6f\x8c\x70\xcb\xa3\x90\xef\x1b\xaf\x35\x57\xcb\x52\xa4\xbb\xc7\xd9\x3b\xed\x95\x64\xad\x7a\xca\xd2\x1d\x2e\x66\xa4\xb3\x18\x67\xf9\x45\x51\x74\x43\x38\xa3\xf3\x43\xbc\x1d\x0f\x44\x07\x2e\xa4\x19\x88\xce\x0e\x4c\xcf\x9f\x98\x2d\x00\xac\x05\x9c\x49\xd4\x17\xd2\xb8\x99\xed\xa2\x43\x91\x25\xdb\x65\x11\xc0\xa0\x2a\xc3\xad\x12\x39\xb8\x3b\xd0\x79\xb4\x3b\x10\x96\xef\xf2\x3d\xad\xc0\xcb\xfd\x8d\x36\xe0\x37\x39\x8f\x17\x3e\x63\x5a\x15\x96\x0b\xc3\x2d\x7e\xe7\xc7\xc0\x37\x2b\x73\x8b\x62\xda\x16\x68\x52\xe5\xca\xf6\x25\xbe\x3f\xaa\x2a\x2d\x57\x8d\x24\xd3\xae\xe2\x13\x52\x04\x2e\xfe\x87\x55\xa7\xe4\x36\xb2\x93\xf7\x95\x05\xbf\x0e\x69\x7e\x60\x7e\xd8\xf5\x7a\xc1\x5b\x35\xfc\x5e\xdf\xa8\x70\x12\x23\x05\x9a\x39\xe1\xb9\xf2\x9a\x18\x9c\xb0\x18\x12\x54\x13\x84\xd4\x26\x8c\xed\xd2\xdc\x21\x94\xfb\xf1\x06\xdb\xd8\x54\x13\x2d\x8f\x3c\x0f\xd0\xcd\x00\x02\x1b\x26\xb6\x50\x14\x6a\xda\x48\x56\x53\x13\xff\x1e\xf4\x2e\x1f\x7f\x75\x65\xca\x26\xf0\xe5\x66\x7c\xb7\x94\xb1\xe4\x30\x3f\x2f\x4e\xb9\x59\xee\x9b\xf4\xc2\xa4\x5c\xa9\xb7\x43\x82\x2b\x31\x83\x67\x9b\x31\xdd\xee\x57\x48\x09\x53\x9b\x0d\x25\x21\x4f\x5f\x51\x4c\xcc\x34\x82\x0f\x87\x9b\x13\x23\x31\xb6\xa1\x8d\xe0\x97\x56\xeb\xf6\x24\xf8\x3b\x09\x7e\xeb\x06\x9f\xef\x6e\x83\xf9\xdf\xff\x08\xef\xde\xb7\xbf\x94\xd6\xda\x5f\x7e\xda\x41\x69\x5b\x0d\x6f\xbd\x5a\xca\xaf\xea\xea\x8b\x84\xa4\x36\xd4\xac\x5e\x7b\x4b\xb8\x39\x71\xff\x1b\xc3\x71\x69\x1f\x13\x4e\x9a\xe5\x23\x2c\x77\xa6\xc1\x1a\xf3\x03\xe1\x9b\xea\x4b\xc8\xd3\x97\x00\xe4\xbe\xfc\x91\xe3\xb5\xda\xae\x03\x8f\x53\xa9\x7d\x45\xe2\x8b\xdc\x2d\xc1\xa1\x79\x8f\xb3\x66\x67\x2d\x5c\x35\x07\xa2\xe9\xcb\xa2\xb5\xf0\x53\xd4\x50\x1b\xd9\x4a\xc1\x67\xd0\x74\xfb\x9a\xbf\xb7\x54\xdc\xc1\xe5\xb6\x92\xa4\x92\xbe\xa9\xcb\x70\xdd\x81\xaf\x8f\x97\xcb\x4e\xdf\x46\x15\x55\xfd\xa5\xed\x88\x36\x47\xde\x1f\xdb\x62\x54\xb7\x68\xb9\x0c\xf6\xcd\x45\x0d\xf8\x7d\x73\xb1\x79\xec\x9b\x8b\x7d\x73\x01\xfb\xe6\x62\xdf\x5c\x6c\x1b\xfb\xe6\xa2\x8e\x62\xdf\x5c\x6c\x1c\xfb\xe6\x62\xdf\x5c\xfc\x8f\x36\x17\x1b\x97\xb7\xbd\xec\x5c\xb9\x97\x95\x41\xba\xe5\x85\xa7\x79\x5e\xa6\xbe\xcc\x6b\x52\xff\x2a\xc3\xd2\xe2\x85\x6b\x51\x85\x79\xe6\x55\x60\xfd\xc3\x8f\x0f\xd1\x94\x29\xf7\x8c\xe5\x35\x4e\xe1\xea\xe2\xe4\x1a\x29\x3c\x32\x33\x0d\x61\x28\xe0\xfc\x89\x24\x29\xc7\x08\xfe\x36\x45\x61\x3b\x8d\x4a\x35\xdb\x42\xc7\x48\x18\x36\x7b\x9f\xfb\x61\xef\xc3\xa7\xb0\x17\xf6\xba\xdd\x61\xb3\x93\xbf\xac\xd1\xe2\xd1\x29\x25\xf1\x3d\x1a\xed\xde\xd5\x6c\x6a\x59\x00\xa8\x02\x2a\x41\x48\x8a\x0e\x8c\xbd\xe2\x12\xf3\x39\xda\xff\xfc\xeb\xdf\x39\xe0\xd2\x43\xdf\x32\x6d\xd8\x5c\xe3\x3e\x0f\x02\xad\xd6\x2f\x43\xfd\xbe\xd5\x6a\xdd\x5a\x47\x7f\xb9\xed\xd9\x50\xe0\xfe\xec\xb9\x7f\x9e\xfb\xaf\x2f\xfd\xdb\x6e\x70\x94\xcf\xf6\x8f\x6f\xbb\xc1\xf1\x5d\x7b\x18\xb6\x9f\x0f\x5f\xdf\xba\xab\x3d\xd4\xef\x7f\x6a\xbf\x94\xcf\x3c\x09\xfe\x42\x82\xf1\xdd\x73\xaf\x73\xf4\x1a\xb5\x9f\x3f\xbe\xae\xcd\xbe\x44\xed\xf6\x4b\x25\xf1\x87\xd7\x56\xb4\x46\xdd\x6a\xe5\xa7\xe5\x08\x86\xf4\xa5\x37\xa4\x43\xea\x51\x7e\x19\xd2\x76\x6b\x18\x6e\xa5\xb1\xb7\x6b\xd7\x9f\x7c\xfc\xda\x6a\xad\x9f\xdd\x7e\xee\x75\xfa\x76\xdb\x0f\xc1\x70\x54\x8b\xc1\x6e\xab\x5a\xfa\xf2\x3d\x81\x6d\x40\x76\x58\x8b\xec\xa8\x06\xd9\x73\xb7\xd3\x7f\xfd\x41\xe8\xfa\xb5\xe8\x8e\xeb\xd1\x1d\xfe\x28\x74\xbd\x5a\x74\x1f\xea\xd1\x1d\xfd\x11\xe8\xa2\x3a\x20\x1f\xeb\x81\x1c\x7f\x7f\x20\xed\xd6\xff\x87\x7f\x6a\x7f\x71\x91\xa3\x5d\x93\x5d\x2a\x4a\x8f\xea\xba\x35\x58\xfb\x1c\x63\x65\xb9\x22\x6d\x04\x2b\x89\xaa\xb1\x7e\xfe\x4a\xea\x5b\x3f\x3b\x98\x7f\x5f\xd2\xa8\xdd\xa9\x51\x3d\x20\x8d\xc0\xa8\xcc\x7f\xed\xa5\x8d\x54\x64\x82\xe5\x99\x6c\x34\xff\xb0\xa8\x60\xae\x0d\x31\x99\x8e\xe0\xf9\xb5\x91\xff\xd9\x00\x20\x71\x8c\xa9\x41\x7a\xb1\xfa\xf9\x5d\xd3\xe7\x82\xe2\x1b\x3a\xf7\x33\x96\xc2\x97\xc0\x3a\x82\xdb\xbb\x86\x3f\x18\xe9\x4d\xf1\x41\x9c\x9d\xfc\x6f\x00\x00\x00\xff\xff\x56\xcb\xc2\x4e\xd8\x28\x00\x00")
+
+func examplesCrdsCiliumegressnatpoliciesYamlBytes() ([]byte, error) {
+	return bindataRead(
+		_examplesCrdsCiliumegressnatpoliciesYaml,
+		"examples/crds/ciliumegressnatpolicies.yaml",
+	)
+}
+
+func examplesCrdsCiliumegressnatpoliciesYaml() (*asset, error) {
+	bytes, err := examplesCrdsCiliumegressnatpoliciesYamlBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	info := bindataFileInfo{name: "examples/crds/ciliumegressnatpolicies.yaml", size: 10456, mode: os.FileMode(416), modTime: time.Unix(1450269211, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
@@ -277,6 +298,7 @@ func AssetNames() []string {
 // _bindata is a table, holding each asset generator, mapped to its name.
 var _bindata = map[string]func() (*asset, error){
 	"examples/crds/ciliumclusterwidenetworkpolicies.yaml": examplesCrdsCiliumclusterwidenetworkpoliciesYaml,
+	"examples/crds/ciliumegressnatpolicies.yaml":          examplesCrdsCiliumegressnatpoliciesYaml,
 	"examples/crds/ciliumendpoints.yaml":                  examplesCrdsCiliumendpointsYaml,
 	"examples/crds/ciliumexternalworkloads.yaml":          examplesCrdsCiliumexternalworkloadsYaml,
 	"examples/crds/ciliumidentities.yaml":                 examplesCrdsCiliumidentitiesYaml,
@@ -329,6 +351,7 @@ var _bintree = &bintree{nil, map[string]*bintree{
 	"examples": &bintree{nil, map[string]*bintree{
 		"crds": &bintree{nil, map[string]*bintree{
 			"ciliumclusterwidenetworkpolicies.yaml": &bintree{examplesCrdsCiliumclusterwidenetworkpoliciesYaml, map[string]*bintree{}},
+			"ciliumegressnatpolicies.yaml":          &bintree{examplesCrdsCiliumegressnatpoliciesYaml, map[string]*bintree{}},
 			"ciliumendpoints.yaml":                  &bintree{examplesCrdsCiliumendpointsYaml, map[string]*bintree{}},
 			"ciliumexternalworkloads.yaml":          &bintree{examplesCrdsCiliumexternalworkloadsYaml, map[string]*bintree{}},
 			"ciliumidentities.yaml":                 &bintree{examplesCrdsCiliumidentitiesYaml, map[string]*bintree{}},

commit e416c54032694a57a7ba33116a80dd56d0a8c298

k8s: Remove incorrect Age description for CENP
The CiliumEgressNatPolicy CRD had an incorrect Age description, copy
pasted from the CiliumIdentity CRD.

Fixes: 6ef88473c6c ("Add custom resource for egress nat gateway policy")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/k8s/apis/cilium.io/client/crds/v2alpha1/ciliumegressnatpolicies.yaml
@@ -20,8 +20,7 @@ spec:
   scope: Cluster
   versions:
   - additionalPrinterColumns:
-    - description: The age of the identity
-      jsonPath: .metadata.creationTimestamp
+    - jsonPath: .metadata.creationTimestamp
       name: Age
       type: date
     name: v2alpha1

pkg/k8s/apis/cilium.io/v2alpha1/types.go
@@ -13,7 +13,7 @@ import (
 // +genclient:nonNamespaced
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:resource:categories={cilium,ciliumpolicy},singular="ciliumegressnatpolicy",path="ciliumegressnatpolicies",scope="Cluster"
-// +kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",description="The age of the identity",name="Age",type=date
+// +kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",name="Age",type=date
 // +kubebuilder:storageversion
 
 type CiliumEgressNATPolicy struct {