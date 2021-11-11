g0odi3z2j/cilium

commit f2214539fe338cac7d56f9bbabc0f1a7e31b2750

daemon: Split IPAM bootstrap, join cluster in between
Split IPAM bootstrap into two phases:
1. Configure IPAM (configureIPAM())
2. Begin using the IPAM configuration (startIPAM())

This allows overriding the IPAM configuration set in phase 1
before starting the use of IPAM configuration in phase 2.

Split the --join-cluster feature init into two phases:

1. Get config from clustermesh-apiserver by resetting node
registration to only contain the node name. clustermesh-apiserver
updates the key with configuration for this node derived from the
cluster state.

2. Update node registration after node IPAM is started with full node
information.

When node registration is started, the local cluster name may not be
known yet. To accomodate this only the node name is used to identify
the node registration.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/nodediscovery/nodediscovery.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/identity"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	"github.com/cilium/cilium/pkg/k8s"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
@@ -124,6 +125,46 @@ func NewNodeDiscovery(manager *nodemanager.Manager, mtuConfig mtu.Configuration,
 	}
 }
 
+// JoinCluster passes the node name to the kvstore and updates the local configuration on response.
+// This allows cluster configuration to override local configuration.
+// Must be called on agent startup after IPAM is configured, but before the configuration is used.
+// nodeName is the name to be used in the local agent.
+func (n *NodeDiscovery) JoinCluster(nodeName string) {
+	var resp *nodeTypes.Node
+	maxRetryCount := 50
+	retryCount := 0
+	for retryCount < maxRetryCount {
+		log.WithFields(
+			logrus.Fields{
+				logfields.Node: nodeName,
+			}).Info("Joining local node to cluster")
+
+		var err error
+		if resp, err = n.Registrar.JoinCluster(nodeName); err != nil || resp == nil {
+			if retryCount >= maxRetryCount {
+				log.Fatalf("Unable to join cluster")
+			}
+			retryCount++
+			log.WithError(err).Error("Unable to initialize local node. Retrying...")
+			time.Sleep(time.Second)
+		} else {
+			break
+		}
+	}
+
+	// Override local config based on the response
+	option.Config.ClusterID = resp.ClusterID
+	option.Config.ClusterName = resp.Cluster
+	node.SetLabels(resp.Labels)
+	if resp.IPv4AllocCIDR != nil {
+		node.SetIPv4AllocRange(resp.IPv4AllocCIDR)
+	}
+	if resp.IPv6AllocCIDR != nil {
+		node.SetIPv6NodeRange(resp.IPv6AllocCIDR.IPNet)
+	}
+	identity.SetLocalNodeID(resp.NodeIdentity)
+}
+
 // start configures the local node and starts node discovery. This is called on
 // agent startup to configure the local node based on the configuration options
 // passed to the agent. nodeName is the name to be used in the local agent.
@@ -136,6 +177,7 @@ func (n *NodeDiscovery) StartDiscovery(nodeName string) {
 	n.LocalNode.ClusterID = option.Config.ClusterID
 	n.LocalNode.EncryptionKey = node.GetIPsecKeyIdentity()
 	n.LocalNode.Labels = node.GetLabels()
+	n.LocalNode.NodeIdentity = identity.GetLocalNodeID().Uint32()
 
 	if node.GetExternalIPv4() != nil {
 		n.LocalNode.IPAddresses = append(n.LocalNode.IPAddresses, nodeTypes.Address{
@@ -181,24 +223,13 @@ func (n *NodeDiscovery) StartDiscovery(nodeName string) {
 		close(n.Registered)
 	}()
 
-	if option.Config.JoinCluster {
-		// Wait for successful node registration before
-		// informing the manager if registering to a k8s
-		// namespace as it can update node information
+	go func() {
 		select {
 		case <-n.Registered:
 		case <-time.NewTimer(defaults.NodeInitTimeout).C:
 			log.Fatalf("Unable to initialize local node due to timeout")
 		}
-	} else {
-		go func() {
-			select {
-			case <-n.Registered:
-			case <-time.NewTimer(defaults.NodeInitTimeout).C:
-				log.Fatalf("Unable to initialize local node due to timeout")
-			}
-		}()
-	}
+	}()
 
 	n.Manager.NodeUpdated(n.LocalNode)
 

daemon/cmd/daemon.go
@@ -491,23 +491,6 @@ func NewDaemon(ctx context.Context, epMgr *endpointmanager.EndpointManager, dp d
 		bootstrapStats.k8sInit.End(true)
 	}
 
-	if option.Config.JoinCluster {
-		if k8s.IsEnabled() {
-			log.Fatalf("Cannot join a Cilium cluster (--%s) when configured as a Kubernetes node", option.JoinClusterName)
-		}
-		if option.Config.KVStore == "" {
-			log.Fatalf("Joining a Cilium cluster (--%s) requires kvstore (--%s) be set", option.JoinClusterName, option.KVStore)
-		}
-		agentLabels := labels.NewLabelsFromModel(option.Config.AgentLabels).K8sStringMap()
-		if option.Config.K8sNamespace != "" {
-			agentLabels[k8sConst.PodNamespaceLabel] = option.Config.K8sNamespace
-		}
-		agentLabels[k8sConst.PodNameLabel] = nodeTypes.GetName()
-		agentLabels[k8sConst.PolicyLabelCluster] = option.Config.ClusterName
-		// Set configured agent labels to local node for node registration
-		node.SetLabels(agentLabels)
-	}
-
 	// Perform an early probe on the underlying kernel on whether BandwidthManager
 	// can be supported or not. This needs to be done before detectNativeDevices()
 	// as BandwidthManager needs these to be available for setup.
@@ -591,7 +574,41 @@ func NewDaemon(ctx context.Context, epMgr *endpointmanager.EndpointManager, dp d
 		log.WithError(err).Warning("Unable to clean stale endpoint interfaces")
 	}
 
-	d.bootstrapIPAM()
+	// Must init kvstore before starting node discovery
+	if option.Config.KVStore == "" {
+		log.Info("Skipping kvstore configuration")
+	} else {
+		bootstrapStats.kvstore.Start()
+		d.initKVStore()
+		bootstrapStats.kvstore.End(true)
+	}
+
+	// Configure IPAM without using the configuration yet.
+	d.configureIPAM()
+
+	if option.Config.JoinCluster {
+		if k8s.IsEnabled() {
+			log.Fatalf("Cannot join a Cilium cluster (--%s) when configured as a Kubernetes node", option.JoinClusterName)
+		}
+		if option.Config.KVStore == "" {
+			log.Fatalf("Joining a Cilium cluster (--%s) requires kvstore (--%s) be set", option.JoinClusterName, option.KVStore)
+		}
+		agentLabels := labels.NewLabelsFromModel(option.Config.AgentLabels).K8sStringMap()
+		if option.Config.K8sNamespace != "" {
+			agentLabels[k8sConst.PodNamespaceLabel] = option.Config.K8sNamespace
+		}
+		agentLabels[k8sConst.PodNameLabel] = nodeTypes.GetName()
+		agentLabels[k8sConst.PolicyLabelCluster] = option.Config.ClusterName
+		// Set configured agent labels to local node for node registration
+		node.SetLabels(agentLabels)
+
+		// This can override node addressing config, so do this before starting IPAM
+		log.WithField(logfields.NodeName, nodeTypes.GetName()).Debug("Calling JoinCluster()")
+		d.nodeDiscovery.JoinCluster(nodeTypes.GetName())
+	}
+
+	// Start IPAM
+	d.startIPAM()
 
 	// restore endpoints before any IPs are allocated to avoid eventual IP
 	// conflicts later on, otherwise any IP conflict will result in the
@@ -605,6 +622,8 @@ func NewDaemon(ctx context.Context, epMgr *endpointmanager.EndpointManager, dp d
 	if err := d.allocateIPs(); err != nil {
 		return nil, nil, err
 	}
+
+	d.nodeDiscovery.StartDiscovery(nodeTypes.GetName())
 
 	// Annotation of the k8s node must happen after discovery of the
 	// PodCIDR range and allocation of the health IPs.
@@ -631,17 +650,6 @@ func NewDaemon(ctx context.Context, epMgr *endpointmanager.EndpointManager, dp d
 	} else if !option.Config.AnnotateK8sNode {
 		log.Debug("Annotate k8s node is disabled.")
 	}
-
-	// Must init kvstore before starting node discovery
-	if option.Config.KVStore == "" {
-		log.Info("Skipping kvstore configuration")
-	} else {
-		bootstrapStats.kvstore.Start()
-		d.initKVStore()
-		bootstrapStats.kvstore.End(true)
-	}
-
-	d.nodeDiscovery.StartDiscovery(nodeTypes.GetName())
 
 	// Trigger refresh and update custom resource in the apiserver with all restored endpoints.
 	// Trigger after nodeDiscovery.StartDiscovery to avoid custom resource update conflict.

daemon/cmd/ipam.go
@@ -324,7 +324,7 @@ func (d *Daemon) allocateIPs() error {
 	return d.allocateHealthIPs()
 }
 
-func (d *Daemon) bootstrapIPAM() {
+func (d *Daemon) configureIPAM() {
 	// If the device has been specified, the IPv4AllocPrefix and the
 	// IPv6AllocPrefix were already allocated before the k8s.Init().
 	//
@@ -337,9 +337,6 @@ func (d *Daemon) bootstrapIPAM() {
 	//
 	// Then, we will calculate the IPv4 or IPv6 alloc prefix based on the IPv6
 	// or IPv4 alloc prefix, respectively, retrieved by k8s node annotations.
-	bootstrapStats.ipam.Start()
-	log.Info("Initializing node addressing")
-
 	if option.Config.IPv4Range != AutoCIDR {
 		allocCIDR, err := cidr.ParseCIDR(option.Config.IPv4Range)
 		if err != nil {
@@ -360,7 +357,11 @@ func (d *Daemon) bootstrapIPAM() {
 	if err := node.AutoComplete(); err != nil {
 		log.WithError(err).Fatal("Cannot autocomplete node addresses")
 	}
+}
 
+func (d *Daemon) startIPAM() {
+	bootstrapStats.ipam.Start()
+	log.Info("Initializing node addressing")
 	// Set up ipam conf after init() because we might be running d.conf.KVStoreIPv4Registration
 	d.ipam = ipam.NewIPAM(d.datapath.LocalNodeAddressing(), option.Config, d.nodeDiscovery, d.k8sWatcher)
 	bootstrapStats.ipam.End(true)

pkg/node/store/logfields.go
@@ -1,0 +1,22 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package store
+
+import (
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
+
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "node")

pkg/node/store/store.go
@@ -21,7 +21,6 @@ import (
 	"time"
 
 	"github.com/cilium/cilium/pkg/defaults"
-	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
@@ -36,6 +35,12 @@ var (
 	// break backwards compatibility
 	NodeStorePrefix = path.Join(kvstore.BaseKeyPrefix, "state", "nodes", "v1")
 
+	// KeyCreator creates a node for a shared store
+	KeyCreator = func() store.Key {
+		n := nodeTypes.Node{}
+		return &n
+	}
+
 	// NodeRegisterStorePrefix is the kvstore prefix of the shared
 	// store for node registration
 	//
@@ -43,9 +48,9 @@ var (
 	// break backwards compatibility
 	NodeRegisterStorePrefix = path.Join(kvstore.BaseKeyPrefix, "state", "noderegister", "v1")
 
-	// KeyCreator creates a node for a shared store
-	KeyCreator = func() store.Key {
-		n := nodeTypes.Node{}
+	// RegisterKeyCreator creates a node for a shared store
+	RegisterKeyCreator = func() store.Key {
+		n := nodeTypes.RegisterNode{}
 		return &n
 	}
 )
@@ -101,34 +106,83 @@ type NodeRegistrar struct {
 // RegisterObserver implements the store.Observer interface and sends
 // named node's identity updates on a channel.
 type RegisterObserver struct {
-	name   string
-	idChan chan uint32
+	name    string
+	updates chan *nodeTypes.RegisterNode
 }
 
 // NewRegisterObserver returns a new RegisterObserver
-func NewRegisterObserver(name string, idChan chan uint32) *RegisterObserver {
+func NewRegisterObserver(name string, updateChan chan *nodeTypes.RegisterNode) *RegisterObserver {
 	return &RegisterObserver{
-		name:   name,
-		idChan: idChan,
+		name:    name,
+		updates: updateChan,
 	}
 }
 
 func (o *RegisterObserver) OnUpdate(k store.Key) {
-	if n, ok := k.(*nodeTypes.Node); ok {
-		if n.NodeIdentity != 0 && n.Fullname() == o.name {
+	if n, ok := k.(*nodeTypes.RegisterNode); ok {
+		log.Debugf("noderegister update on key %s while waiting for %s: %v", n.GetKeyName(), o.name, n)
+		if n.NodeIdentity != 0 && n.GetKeyName() == o.name {
 			select {
-			case o.idChan <- n.NodeIdentity:
+			case o.updates <- n:
 			default:
-				// Register Node idChan would block, not sending
+				// Register Node updateChan would block, not sending
 			}
 		}
 	}
 }
 
 func (o *RegisterObserver) OnDelete(k store.NamedKey) {
+	log.Debugf("noderegister key %s deleted while registering %s", k.GetKeyName(), o.name)
 }
 
-// RegisterNode registers the local node in the cluster
+// JoinCluster registers the local node in the cluster.
+// Blocks until timeout occurs or an updated Node is received from the kv-store and returns it.
+// Otherwise this does not block and returns nil.
+func (nr *NodeRegistrar) JoinCluster(name string) (*nodeTypes.Node, error) {
+	n := &nodeTypes.RegisterNode{
+		Node: nodeTypes.Node{
+			Name:   name,
+			Source: source.Local,
+		},
+	}
+
+	registerObserver := NewRegisterObserver(n.GetKeyName(), make(chan *nodeTypes.RegisterNode, 10))
+	// Join the shared store for node registrations
+	registerStore, err := store.JoinSharedStore(store.Configuration{
+		Prefix:     NodeRegisterStorePrefix,
+		KeyCreator: RegisterKeyCreator,
+		Observer:   registerObserver,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Drain the channel of old updates first
+	for len(registerObserver.updates) > 0 {
+		dump := <-registerObserver.updates
+		log.Debugf("bypassing stale noderegister key: %s", dump.GetKeyName())
+	}
+
+	log.Debugf("updating noderegister key %s with: %v", n.GetKeyName(), n)
+	err = registerStore.UpdateLocalKeySync(context.TODO(), n)
+	if err != nil {
+		registerStore.Release()
+		return nil, err
+	}
+
+	// Wait until an updated key is received from the kvstore
+	select {
+	case n = <-registerObserver.updates:
+	case <-time.After(defaults.NodeInitTimeout / 10):
+		registerStore.Release()
+		return nil, fmt.Errorf("timed out waiting for node identity")
+	}
+
+	nr.registerStore = registerStore
+	return &n.Node, nil
+}
+
+// RegisterNode registers the local node in the cluster.
 func (nr *NodeRegistrar) RegisterNode(n *nodeTypes.Node, manager NodeManager) error {
 	if option.Config.KVStore == "" {
 		return nil
@@ -140,58 +194,22 @@ func (nr *NodeRegistrar) RegisterNode(n *nodeTypes.Node, manager NodeManager) er
 		KeyCreator: KeyCreator,
 		Observer:   NewNodeObserver(manager),
 	})
-
 	if err != nil {
 		return err
 	}
 
-	var registerStore *store.SharedStore
-	if option.Config.JoinCluster {
-		nodeId := make(chan uint32, 1)
-
-		// Join the shared store for node registrations
-		registerStore, err := store.JoinSharedStore(store.Configuration{
-			Prefix:     NodeRegisterStorePrefix,
-			KeyCreator: KeyCreator,
-			Observer:   NewRegisterObserver(n.Fullname(), nodeId),
-		})
-		if err != nil {
-			nodeStore.Release()
-			return err
-		}
-
-		// drain the channel of old updates first
-		for len(nodeId) > 0 {
-			<-nodeId
-		}
-		err = registerStore.UpdateLocalKeySync(context.TODO(), n)
-		if err == nil {
-			// Wait until node identity can has been allocated by the KV store
-			select {
-			case id := <-nodeId:
-				n.NodeIdentity = id
-				identity.SetLocalNodeID(id)
-			case <-time.After(defaults.NodeInitTimeout / 10):
-				registerStore.Release()
-				nodeStore.Release()
-				return fmt.Errorf("timed out waiting for node identity")
-			}
-		}
+	// Use nodeTypes.RegisterNode for updating local node info if not nil, but keep nodeStore for cluster node updates
+	if nr.registerStore != nil {
+		err = nr.registerStore.UpdateLocalKeySync(context.TODO(), &nodeTypes.RegisterNode{Node: *n})
 	} else {
 		err = nodeStore.UpdateLocalKeySync(context.TODO(), n)
 	}
-
 	if err != nil {
-		if registerStore != nil {
-			registerStore.Release()
-		}
 		nodeStore.Release()
 		return err
 	}
 
-	nr.registerStore = registerStore
 	nr.SharedStore = nodeStore
-
 	return nil
 }
 
@@ -199,7 +217,7 @@ func (nr *NodeRegistrar) RegisterNode(n *nodeTypes.Node, manager NodeManager) er
 // SharedStore.
 func (nr *NodeRegistrar) UpdateLocalKeySync(n *nodeTypes.Node) error {
 	if nr.registerStore != nil {
-		return nr.registerStore.UpdateLocalKeySync(context.TODO(), n)
+		return nr.registerStore.UpdateLocalKeySync(context.TODO(), &nodeTypes.RegisterNode{Node: *n})
 	}
 	return nr.SharedStore.UpdateLocalKeySync(context.TODO(), n)
 }

pkg/node/types/node.go
@@ -134,6 +134,23 @@ func (n *Node) ToCiliumNode() *ciliumv2.CiliumNode {
 	}
 }
 
+// RegisterNode overloads GetKeyName to ignore the cluster name, as cluster name may not be stable during node registration.
+//
+// +k8s:deepcopy-gen=true
+type RegisterNode struct {
+	Node
+}
+
+// GetKeyName Overloaded key name w/o cluster name
+func (n *RegisterNode) GetKeyName() string {
+	return n.Name
+}
+
+// DeepKeyCopy creates a deep copy of the LocalKey
+func (n *RegisterNode) DeepKeyCopy() store.LocalKey {
+	return n.DeepCopy()
+}
+
 // Node contains the nodes name, the list of addresses to this address
 //
 // +k8s:deepcopy-gen=true

pkg/node/types/zz_generated.deepcopy.go
@@ -90,3 +90,20 @@ func (in *Node) DeepCopy() *Node {
 	in.DeepCopyInto(out)
 	return out
 }
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *RegisterNode) DeepCopyInto(out *RegisterNode) {
+	*out = *in
+	in.Node.DeepCopyInto(&out.Node)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new RegisterNode.
+func (in *RegisterNode) DeepCopy() *RegisterNode {
+	if in == nil {
+		return nil
+	}
+	out := new(RegisterNode)
+	in.DeepCopyInto(out)
+	return out
+}

commit d8594549ce8a33e441886b800d6bb301ad3568e4

nodediscovery: Fix local host identity propagation
The local NodeDiscovery implementation was previously informing the rest
of the Cilium agent that the local node's identity is "Remote Node"
because of the statically initialized "identity.GetLocalNodeID" value.
However, that value should only ever be used for external workloads
cases in order to prepare the source identity used for transmitting
traffic to other Cilium nodes. It should not be used for locally
determining the identity of traffic coming from the host itself.

Fix this by hardcoding the identity to "Host" identity.

Fixes: f2214539fe33 ("daemon: Split IPAM bootstrap, join cluster in between")

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/nodediscovery/nodediscovery.go
@@ -176,7 +176,7 @@ func (n *NodeDiscovery) StartDiscovery(nodeName string) {
 	n.LocalNode.EncryptionKey = node.GetIPsecKeyIdentity()
 	n.LocalNode.WireguardPubKey = node.GetWireguardPubKey()
 	n.LocalNode.Labels = node.GetLabels()
-	n.LocalNode.NodeIdentity = identity.GetLocalNodeID().Uint32()
+	n.LocalNode.NodeIdentity = uint32(identity.ReservedIdentityHost)
 
 	if node.GetK8sExternalIPv4() != nil {
 		n.LocalNode.IPAddresses = append(n.LocalNode.IPAddresses, nodeTypes.Address{