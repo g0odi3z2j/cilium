g0odi3z2j/cilium

commit f0c7a81bc66c63b73a15663c062cd2fd52856a40

cleanup the cilium helm chart: * remove .Values.Capabilities in favor of --kube-version in helm template * using `cilium.image` template to unify image render * support `priorityClassName` in all components * move `hubble-metrics` service & `hubble` ServiceMonitor to seperated file (in hubble folder) * make `name` as the first attribute of object * remove unnecessary parentheses * using `nindent` instead of `indent` * using `default` and `ternary` instead of `if-else` * using `with` instead of `if` when it's possible
Signed-off-by: Đặng Minh Dũng <dungdm93@live.com>

install/kubernetes/cilium/templates/cilium-operator/_helpers.tpl
@@ -1,0 +1,20 @@
+{{- define "cilium.operator.cloud" -}}
+{{- $cloud := "generic" -}}
+{{- if .Values.eni.enabled -}}
+  {{- $cloud = "aws" -}}
+{{- else if .Values.azure.enabled -}}
+  {{- $cloud = "azure" -}}
+{{- else if .Values.alibabacloud.enabled -}}
+  {{- $cloud = "alibabacloud" -}}
+{{- end -}}
+{{- $cloud -}}
+{{- end -}}
+
+{{/*
+Return cilium operator image
+*/}}
+{{- define "cilium.operator.image" -}}
+{{- $cloud := include "cilium.operator.cloud" . }}
+{{- $digest := (.Values.operator.image.useDigest | default false) | ternary (printf "@%s" .Values.operator.image.digest) "" -}}
+{{- printf "%s-%s%s:%s%s" .Values.operator.image.repository $cloud .Values.operator.image.suffix .Values.operator.image.tag $digest -}}
+{{- end -}}

install/kubernetes/cilium/templates/_helpers.tpl
@@ -6,6 +6,48 @@ Create chart name and version as used by the chart label.
 {{- end }}
 
 {{/*
+Render full image name from given values, e.g:
+```
+image:
+  repository: quay.io/cilium/cilium
+  tag: v1.10.1
+  useDigest: true
+  digest: abcdefgh
+```
+then `include "cilium.image" .Values.image`
+will return `quay.io/cilium/cilium:v1.10.1@abcdefgh`
+*/}}
+{{- define "cilium.image" -}}
+{{- $digest := (.useDigest | default false) | ternary (printf "@%s" .digest) "" -}}
+{{- printf "%s:%s%s" .repository .tag $digest -}}
+{{- end -}}
+
+{{/*
+Return user specify priorityClass or default criticalPriorityClass
+Usage:
+  include "cilium.priorityClass" (list $ <priorityClass> <criticalPriorityClass>)
+where:
+* `priorityClass`: is user specify priorityClass e.g `.Values.operator.priorityClassName`
+* `criticalPriorityClass`: default criticalPriorityClass, e.g `"system-cluster-critical"`
+  This value is used when `priorityClass` is `nil` and 
+  `.Values.enableCriticalPriorityClass=true` and kubernetes supported it.
+*/}}
+{{- define "cilium.priorityClass" -}}
+{{- $root := index . 0 -}}
+{{- $priorityClass := index . 1 -}}
+{{- $criticalPriorityClass := index . 2 -}}
+{{- if $priorityClass }}
+  {{- $priorityClass }}
+{{- else if and $root.Values.enableCriticalPriorityClass $criticalPriorityClass -}}
+  {{- if and (eq $root.Release.Namespace "kube-system") (semverCompare ">=1.10-0" $root.Capabilities.KubeVersion.Version) -}}
+    {{- $criticalPriorityClass }}
+  {{- else if semverCompare ">=1.17-0" $root.Capabilities.KubeVersion.Version -}}
+    {{- $criticalPriorityClass }}
+  {{- end -}}
+{{- end -}}
+{{- end -}}
+
+{{/*
 Return the appropriate apiVersion for ingress.
 */}}
 {{- define "ingress.apiVersion" -}}

.github/workflows/conformance-k8s-network-policies.yaml
@@ -27,7 +27,7 @@ jobs:
           echo "Checking for differences between preflight and agent clusterrole"
           diff \
              -I '^[ ]\{2\}name: cilium.*' \
-             -I '^Keep file in synced with.*' \
+             -I '^Keep file in sync with.*' \
              -I '{{- if.*' \
              cilium-agent/clusterrole.yaml \
              cilium-preflight/clusterrole.yaml

.github/workflows/tests-smoke.yaml
@@ -53,7 +53,7 @@ jobs:
           echo "Checking for differences between preflight and agent clusterrole"
           diff \
              -I '^[ ]\{2\}name: cilium.*' \
-             -I '^Keep file in synced with.*' \
+             -I '^Keep file in sync with.*' \
              -I '{{- if.*' \
              cilium-agent/clusterrole.yaml \
              cilium-preflight/clusterrole.yaml

install/kubernetes/cilium/templates/cilium-agent/clusterrole.yaml
@@ -1,6 +1,6 @@
-{{- if and (.Values.agent) (not .Values.preflight.enabled) }}
+{{- if and .Values.agent (not .Values.preflight.enabled) }}
 {{- /*
-Keep file in synced with cilium-preflight-clusterrole.yaml
+Keep file in sync with cilium-preflight/clusterrole.yaml
 */ -}}
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole

install/kubernetes/cilium/templates/cilium-agent/clusterrolebinding.yaml
@@ -1,4 +1,4 @@
-{{- if and (.Values.agent) (not .Values.preflight.enabled) .Values.serviceAccounts.cilium.create }}
+{{- if and .Values.agent (not .Values.preflight.enabled) .Values.serviceAccounts.cilium.create }}
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:

install/kubernetes/cilium/templates/cilium-agent/service.yaml
@@ -1,6 +1,7 @@
-{{- if and (.Values.agent) (not .Values.preflight.enabled) (.Values.prometheus.enabled) (.Values.prometheus.serviceMonitor.enabled) }}
-kind: Service
+{{- if and .Values.agent (not .Values.preflight.enabled) .Values.prometheus.enabled }}
+{{- if .Values.prometheus.serviceMonitor.enabled }}
 apiVersion: v1
+kind: Service
 metadata:
   name: cilium-agent
   namespace: {{ .Release.Namespace }}
@@ -9,6 +10,8 @@ metadata:
 spec:
   clusterIP: None
   type: ClusterIP
+  selector:
+    k8s-app: cilium
   ports:
   - name: metrics
     port: {{ .Values.prometheus.port }}
@@ -18,50 +21,26 @@ spec:
     port: {{ .Values.proxy.prometheus.port }}
     protocol: TCP
     targetPort: envoy-metrics
-  selector:
-    k8s-app: cilium
-{{- else if and (.Values.prometheus.enabled) (.Values.proxy.prometheus.enabled) }}
-kind: Service
+{{- else if .Values.proxy.prometheus.enabled }}
 apiVersion: v1
+kind: Service
 metadata:
   name: cilium-agent
   namespace: {{ .Release.Namespace }}
   annotations:
-    prometheus.io/scrape: 'true'
+    prometheus.io/scrape: "true"
     prometheus.io/port: {{ .Values.proxy.prometheus.port | quote }}
   labels:
     k8s-app: cilium
 spec:
   clusterIP: None
   type: ClusterIP
+  selector:
+    k8s-app: cilium
   ports:
   - name: envoy-metrics
     port: {{ .Values.proxy.prometheus.port }}
     protocol: TCP
     targetPort: envoy-metrics
-  selector:
-    k8s-app: cilium
 {{- end }}
-{{- if and .Values.hubble.metrics.enabled }}
----
-kind: Service
-apiVersion: v1
-metadata:
-  name: hubble-metrics
-  namespace: {{ .Release.Namespace }}
-  annotations:
-    prometheus.io/scrape: 'true'
-    prometheus.io/port: {{ .Values.hubble.metrics.port | quote }}
-  labels:
-    k8s-app: hubble
-spec:
-  clusterIP: None
-  type: ClusterIP
-  ports:
-  - name: hubble-metrics
-    port: {{ .Values.hubble.metrics.port }}
-    protocol: TCP
-    targetPort: hubble-metrics
-  selector:
-    k8s-app: cilium
 {{- end }}

commit 21e35264e552de2316b911802d8e764db59ec070

install/kubernetes: fix helm generation for operator image digest
This commit fixes the image digest as part of the operator deployment.

Fixes: f0c7a81bc66c ("cleanup the cilium helm chart:")
Signed-off-by: André Martins <andre@cilium.io>

install/kubernetes/cilium/templates/cilium-operator/_helpers.tpl
@@ -10,11 +10,23 @@
 {{- $cloud -}}
 {{- end -}}
 
+{{- define "cilium.operator.imageDigestName" -}}
+{{- $imageDigest := (.Values.operator.image.useDigest | default false) | ternary (printf "@%s" .Values.operator.image.genericDigest) "" -}}
+{{- if .Values.eni.enabled -}}
+  {{- $imageDigest := (.Values.operator.image.useDigest | default false) | ternary (printf "@%s" .Values.operator.image.awsDigest) "" -}}
+{{- else if .Values.azure.enabled -}}
+  {{- $imageDigest := (.Values.operator.image.useDigest | default false) | ternary (printf "@%s" .Values.operator.image.azureDigest) "" -}}
+{{- else if .Values.alibabacloud.enabled -}}
+  {{- $imageDigest := (.Values.operator.image.useDigest | default false) | ternary (printf "@%s" .Values.operator.image.alibabacloudDigest) "" -}}
+{{- end -}}
+{{- $imageDigest -}}
+{{- end -}}
+
 {{/*
 Return cilium operator image
 */}}
 {{- define "cilium.operator.image" -}}
 {{- $cloud := include "cilium.operator.cloud" . }}
-{{- $digest := (.Values.operator.image.useDigest | default false) | ternary (printf "@%s" .Values.operator.image.digest) "" -}}
-{{- printf "%s-%s%s:%s%s" .Values.operator.image.repository $cloud .Values.operator.image.suffix .Values.operator.image.tag $digest -}}
+{{- $imageDigest := include "cilium.operator.imageDigestName" . }}
+{{- printf "%s-%s%s:%s%s" .Values.operator.image.repository $cloud .Values.operator.image.suffix .Values.operator.image.tag $imageDigest -}}
 {{- end -}}