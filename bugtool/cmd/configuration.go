g0odi3z2j/cilium

commit 7e2dd3f97d939278b3d749e5c8449fb7f393848c

bugtool: avoid allocation on conversion of execCommand result to string
Converting []byte to string can cause allocation due to the fact that
strings are immutable in Go. By letting execCommand return []byte
instead of string we can avoid some of these allocations, i.e also the
allcoation caused by further processing of that return value.

Signed-off-by: Tobias Klauser <tobias@cilium.io>

bugtool/cmd/configuration.go
@@ -4,6 +4,7 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -258,7 +259,7 @@ func routeCommands() []string {
 	commands := []string{}
 	routes, _ := execCommand("ip route show table all | grep -E --only-matching 'table [0-9]+'")
 
-	for _, r := range strings.Split(strings.TrimSuffix(routes, "\n"), "\n") {
+	for _, r := range bytes.Split(bytes.TrimSuffix(routes, []byte("\n")), []byte("\n")) {
 		routeTablev4 := fmt.Sprintf("ip -4 route show %v", r)
 		routeTablev6 := fmt.Sprintf("ip -6 route show %v", r)
 		commands = append(commands, routeTablev4, routeTablev6)
@@ -287,7 +288,7 @@ func copyConfigCommands(confDir string, k8sPods []string) []string {
 	// path. This should be refactored.
 	if len(k8sPods) == 0 {
 		kernel, _ := execCommand("uname -r")
-		kernel = strings.TrimSpace(kernel)
+		kernel = bytes.TrimSpace(kernel)
 		// Append the boot config for the current kernel
 		l := Location{fmt.Sprintf("/boot/config-%s", kernel),
 			fmt.Sprintf("%s/kernel-config-%s", confDir, kernel)}
@@ -306,7 +307,7 @@ func copyConfigCommands(confDir string, k8sPods []string) []string {
 		for _, pod := range k8sPods {
 			prompt := podPrefix(pod, "uname -r")
 			kernel, _ := execCommand(prompt)
-			kernel = strings.TrimSpace(kernel)
+			kernel = bytes.TrimSpace(kernel)
 			l := Location{fmt.Sprintf("/boot/config-%s", kernel),
 				fmt.Sprintf("%s/kernel-config-%s", confDir, kernel)}
 			locations = append(locations, l)

bugtool/cmd/root.go
@@ -4,6 +4,7 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -334,18 +335,18 @@ func runAll(commands []string, cmdDir string, k8sPods []string) {
 	}
 }
 
-func execCommand(prompt string) (string, error) {
+func execCommand(prompt string) ([]byte, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), execTimeout)
 	defer cancel()
 	output, err := exec.CommandContext(ctx, "bash", "-c", prompt).CombinedOutput()
 	if errors.Is(ctx.Err(), context.DeadlineExceeded) {
-		return "", fmt.Errorf("exec timeout")
+		return nil, fmt.Errorf("exec timeout")
 	}
-	return string(output), err
+	return output, err
 }
 
 // writeCmdToFile will execute command and write markdown output to a file
-func writeCmdToFile(cmdDir, prompt string, k8sPods []string, enableMarkdown bool, postProcess func(output string) string) {
+func writeCmdToFile(cmdDir, prompt string, k8sPods []string, enableMarkdown bool, postProcess func(output []byte) []byte) {
 	// Clean up the filename
 	name := strings.Replace(prompt, "/", " ", -1)
 	name = strings.Replace(name, " ", "-", -1)
@@ -388,7 +389,7 @@ func writeCmdToFile(cmdDir, prompt string, k8sPods []string, enableMarkdown bool
 	}
 	// We deliberately continue in case there was a error but the output
 	// produced might have useful information
-	if strings.Contains(output, "```") || !enableMarkdown {
+	if bytes.Contains(output, []byte("```")) || !enableMarkdown {
 		// Already contains Markdown, print as is.
 		fmt.Fprint(f, output)
 	} else if enableMarkdown && len(output) > 0 {
@@ -420,17 +421,17 @@ func getCiliumPods(namespace, label string) ([]string, error) {
 		return nil, err
 	}
 
-	lines := strings.Split(output, "\n")
+	lines := bytes.Split(output, []byte("\n"))
 	ciliumPods := make([]string, 0, len(lines))
 	for _, l := range lines {
-		if !strings.HasPrefix(l, "cilium") {
+		if !bytes.HasPrefix(l, []byte("cilium")) {
 			continue
 		}
 		// NAME           READY     STATUS    RESTARTS   AGE
 		// cilium-cfmww   0/1       Running   0          3m
 		// ^
-		pod := strings.Split(l, " ")[0]
-		ciliumPods = append(ciliumPods, pod)
+		pod := bytes.Split(l, []byte(" "))[0]
+		ciliumPods = append(ciliumPods, string(pod))
 	}
 
 	return ciliumPods, nil

bugtool/cmd/helper.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"archive/tar"
+	"bytes"
 	"compress/gzip"
 	"crypto/sha256"
 	"encoding/hex"
@@ -164,12 +165,11 @@ var isEncryptionKey = regexp.MustCompile("(auth|enc|aead|comp)(.*[[:blank:]](0[x
 
 // hashEncryptionKeys processes the buffer containing the output of `ip -s xfrm state`.
 // It searches for IPsec keys in the output and replaces them by their hash.
-func hashEncryptionKeys(output string) string {
-	var finalOutput []string
-	// Split the result as a slice of strings.
-	lines := strings.Split(output, "\n")
+func hashEncryptionKeys(output []byte) []byte {
+	var b bytes.Buffer
+	lines := bytes.Split(output, []byte("\n"))
 	// Search for lines containing encryption keys.
-	for _, line := range lines {
+	for i, line := range lines {
 		// isEncryptionKey.FindStringSubmatchIndex(line) will return:
 		// - [], if the global pattern is not found
 		// - a slice of integers, if the global pattern is found. The
@@ -182,18 +182,23 @@ func hashEncryptionKeys(output string) string {
 		// the hexadecimal string (the third submatch) will be at index
 		// 6 and 7 in the slice. They may be equal to -1 if the
 		// submatch, marked as optional ('?'), is not found.
-		matched := isEncryptionKey.FindStringSubmatchIndex(line)
+		matched := isEncryptionKey.FindSubmatchIndex(line)
 		if matched != nil && matched[6] > 0 {
 			key := line[matched[6]:matched[7]]
 			h := sha256.New()
-			h.Write([]byte(key))
-			hashedKey := hex.EncodeToString(h.Sum(nil))
-			line = line[:matched[6]] + "[hash:" + hashedKey + "]" + line[matched[7]:]
+			h.Write(key)
+			sum := h.Sum(nil)
+			hashedKey := make([]byte, hex.EncodedLen(len(sum)))
+			hex.Encode(hashedKey, sum)
+			fmt.Fprintf(&b, "%s[hash:%s]%s", line[:matched[6]], hashedKey, line[matched[7]:])
 		} else if matched != nil && matched[6] < 0 {
-			line = "[redacted]"
+			b.WriteString("[redacted]")
+		} else {
+			b.Write(line)
 		}
-		finalOutput = append(finalOutput, line)
+		if i < len(lines)-1 {
+			b.WriteByte('\n')
+		}
 	}
-
-	return strings.Join(finalOutput, "\n")
+	return b.Bytes()
 }

bugtool/cmd/helper_test.go
@@ -146,7 +146,7 @@ func (b *BugtoolSuite) TestHashEncryptionKeys(c *C) {
 	}
 
 	for _, v := range testdata {
-		modifiedString := hashEncryptionKeys(v.input)
-		c.Assert(modifiedString, Equals, v.output)
+		modifiedString := hashEncryptionKeys([]byte(v.input))
+		c.Assert(string(modifiedString), Equals, v.output)
 	}
 }

commit 77e533a7c5d7fbf5e9fd29414f35b6ca07540d32

bugtool: fix IP route debug gathering commands
Commit 7e2dd3f97d93 ("bugtool: avoid allocation on conversion of
execCommand result to string") broke the `ip route show` commands
because the change from `[]byte` to `string` causes the `%v` formatting
verb to emit the raw byte slice, not the string. Fix this by using the
`%s` formatting verb to make sure the argument gets interpreted as a
string.

Also fix another instance in `writeCmdToFile` where `fmt.Fprint` is now
invoked with a byte slice.

Grepping for `%v` in bugtool sources and manually inspecting all changes
from commit 7e2dd3f97d93 showed no other instances where a byte slice
could potentially end up being formatted in a wrong way.

Fixes: 7e2dd3f97d93 ("bugtool: avoid allocation on conversion of execCommand result to string")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

bugtool/cmd/configuration.go
@@ -258,8 +258,8 @@ func routeCommands() []string {
 	routes, _ := execCommand("ip route show table all | grep -E --only-matching 'table [0-9]+'")
 
 	for _, r := range bytes.Split(bytes.TrimSuffix(routes, []byte("\n")), []byte("\n")) {
-		routeTablev4 := fmt.Sprintf("ip -4 route show %v", r)
-		routeTablev6 := fmt.Sprintf("ip -6 route show %v", r)
+		routeTablev4 := fmt.Sprintf("ip -4 route show %s", r)
+		routeTablev6 := fmt.Sprintf("ip -6 route show %s", r)
 		commands = append(commands, routeTablev4, routeTablev6)
 	}
 	return commands

bugtool/cmd/root.go
@@ -399,7 +399,7 @@ func writeCmdToFile(cmdDir, prompt string, k8sPods []string, enableMarkdown bool
 		// produced might have useful information
 		if bytes.Contains(output, []byte("```")) || !enableMarkdown {
 			// Already contains Markdown, print as is.
-			fmt.Fprint(f, output)
+			fmt.Fprint(f, string(output))
 		} else if enableMarkdown && len(output) > 0 {
 			// Write prompt as header and the output as body, and/or error but delete empty output.
 			fmt.Fprint(f, fmt.Sprintf("# %s\n\n```\n%s\n```\n", prompt, output))