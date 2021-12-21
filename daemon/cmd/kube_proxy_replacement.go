g0odi3z2j/cilium

commit 1d526ecb4d04bae2a6276473d7fd770ac6fda08b

datapath,daemon: Enable multi-dev XDP
This commit enables XDP (bpf_xdp) on all devices specified by --devices.
Previously, only --direct-routing-device (or --devices if dev count = 1)
could have been used to run bpf_xdp.

To do so, we rely on the bpf_redirect() helper which performance
(>= 5.5 Linux kernel) recently became on par with bpf_redirect_map()
[1]. Therefore, it does not make sense to use the latter.

The side effect of the enablement is that the --prefilter-device no
longer makes sense. This will be addressed in a following commit.

[1]: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=1d233886dd904edbf239eeffe435c3308ae97625

Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/cmd/kube_proxy_replacement.go
@@ -557,13 +557,6 @@ func finishKubeProxyReplacementInit(isKubeProxyReplacementStrict bool) error {
 	}
 
 	if option.Config.NodePortAcceleration != option.NodePortAccelerationDisabled {
-		if option.Config.XDPDevice != "undefined" &&
-			(option.Config.DirectRoutingDevice == "" ||
-				option.Config.XDPDevice != option.Config.DirectRoutingDevice) {
-			return fmt.Errorf("Cannot set NodePort acceleration device: mismatch between Prefilter device %s and NodePort device %s",
-				option.Config.XDPDevice, option.Config.DirectRoutingDevice)
-		}
-		option.Config.XDPDevice = option.Config.DirectRoutingDevice
 		if err := loader.SetXDPMode(option.Config.NodePortAcceleration); err != nil {
 			return fmt.Errorf("Cannot set NodePort acceleration")
 		}

bpf/include/bpf/ctx/xdp.h
@@ -249,17 +249,13 @@ ctx_adjust_hroom(struct xdp_md *ctx, const __s32 len_diff, const __u32 mode,
 	return ret;
 }
 
-#define redirect			redirect__stub
-#define redirect_peer			redirect
-
 static __always_inline __maybe_unused int
 ctx_redirect(const struct xdp_md *ctx, int ifindex, const __u32 flags)
 {
-	if (unlikely(flags))
-		return -ENOTSUPP;
-	if ((__u32)ifindex != ctx->ingress_ifindex)
-		return -ENOTSUPP;
-	return XDP_TX;
+	if ((__u32)ifindex == ctx->ingress_ifindex)
+		return XDP_TX;
+
+	return redirect(ifindex, flags);
 }
 
 static __always_inline __maybe_unused __u64

bpf/include/bpf/helpers_xdp.h
@@ -18,7 +18,7 @@ static int BPF_FUNC(xdp_adjust_head, struct xdp_md *xdp, int delta);
 static int BPF_FUNC(xdp_adjust_tail, struct xdp_md *xdp, int delta);
 
 /* Packet redirection */
-static int BPF_STUB(redirect, int ifindex, __u32 flags);
+static int BPF_FUNC(redirect, int ifindex, __u32 flags);
 
 /* Packet manipulation */
 static int BPF_STUB(xdp_load_bytes, struct xdp_md *xdp, __u32 off,

bpf/lib/common.h
@@ -918,7 +918,16 @@ static __always_inline int redirect_ep(struct __ctx_buff *ctx __maybe_unused,
 		 */
 		ctx_change_type(ctx, PACKET_HOST);
 # endif /* ENCAP_IFINDEX */
+#if __ctx_is == __ctx_skb
 		return redirect_peer(ifindex, 0);
+#else
+		/* bpf_redirect_peer() is available only in TC BPF. However,
+		 * this path is not used by bpf_xdp. So to avoid compilation
+		 * errors protect it with #if until we have replaced all usage
+		 * of redirect{,_peer}() with ctx_redirect{,_peer}().
+		 */
+		return -ENOTSUP;
+#endif /* __ctx_is == __ctx_skb */
 	}
 #else
 	return CTX_ACT_OK;

daemon/cmd/daemon_main.go
@@ -1201,7 +1201,16 @@ func initEnv(cmd *cobra.Command) {
 	}
 
 	if option.Config.DevicePreFilter != "undefined" {
-		option.Config.XDPDevice = option.Config.DevicePreFilter
+		found := false
+		for _, dev := range option.Config.Devices {
+			if dev == option.Config.DevicePreFilter {
+				found = true
+				break
+			}
+		}
+		if !found {
+			option.Config.Devices = append(option.Config.Devices, option.Config.DevicePreFilter)
+		}
 		if err := loader.SetXDPMode(option.Config.ModePreFilter); err != nil {
 			scopedLog.WithError(err).Fatal("Cannot set prefilter XDP mode")
 		}

pkg/datapath/loader/base.go
@@ -101,7 +101,7 @@ func writePreFilterHeader(preFilter *prefilter.PreFilter, dir string) error {
 	defer f.Close()
 	fw := bufio.NewWriter(f)
 	fmt.Fprint(fw, "/*\n")
-	fmt.Fprintf(fw, " * XDP device: %s\n", option.Config.DevicePreFilter)
+	fmt.Fprintf(fw, " * XDP devices: %s\n", strings.Join(option.Config.Devices, " "))
 	fmt.Fprintf(fw, " * XDP mode: %s\n", option.Config.ModePreFilter)
 	fmt.Fprint(fw, " */\n\n")
 	preFilter.WriteConfig(fw)
@@ -206,9 +206,12 @@ func (l *Loader) reinitializeIPSec(ctx context.Context) error {
 }
 
 func (l *Loader) reinitializeXDPLocked(ctx context.Context, extraCArgs []string) error {
-	maybeUnloadObsoleteXDPPrograms(option.Config.XDPDevice, option.Config.XDPMode)
-	if option.Config.XDPDevice != "undefined" {
-		if err := compileAndLoadXDPProg(ctx, option.Config.XDPDevice, option.Config.XDPMode, extraCArgs); err != nil {
+	maybeUnloadObsoleteXDPPrograms(option.Config.Devices, option.Config.XDPMode)
+	if option.Config.XDPMode == option.XDPModeDisabled {
+		return nil
+	}
+	for _, dev := range option.Config.Devices {
+		if err := compileAndLoadXDPProg(ctx, dev, option.Config.XDPMode, extraCArgs); err != nil {
 			return err
 		}
 	}
@@ -256,7 +259,7 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 	}
 
 	if option.Config.DevicePreFilter != "undefined" {
-		scopedLog := log.WithField(logfields.XDPDevice, option.Config.XDPDevice)
+		scopedLog := log.WithField(logfields.Devices, option.Config.Devices)
 
 		preFilter, err := prefilter.NewPreFilter()
 		if err != nil {

pkg/datapath/loader/xdp.go
@@ -32,7 +32,7 @@ func xdpModeToFlag(xdpMode string) uint32 {
 }
 
 // maybeUnloadObsoleteXDPPrograms removes bpf_xdp.o from previously used devices.
-func maybeUnloadObsoleteXDPPrograms(xdpDev, xdpMode string) {
+func maybeUnloadObsoleteXDPPrograms(xdpDevs []string, xdpMode string) {
 	links, err := netlink.LinkList()
 	if err != nil {
 		log.WithError(err).Warn("Failed to list links for XDP unload")
@@ -47,15 +47,21 @@ func maybeUnloadObsoleteXDPPrograms(xdpDev, xdpMode string) {
 			// Ignore devices created by cilium-agent
 			continue
 		}
-		if link.Attrs().Name == xdpDev && xdpDev != "" {
-			if link.Attrs().Xdp.Flags&xdpModeToFlag(xdpMode) != 0 {
+
+		used := false
+		for _, xdpDev := range xdpDevs {
+			if link.Attrs().Name == xdpDev &&
+				link.Attrs().Xdp.Flags&xdpModeToFlag(xdpMode) != 0 {
 				// XDP mode matches; don't unload, otherwise we might introduce
 				// intermittent connectivity problems
-				continue
+				used = true
+				break
 			}
 		}
-		netlink.LinkSetXdpFdWithFlags(link, -1, int(xdpModeToFlag(option.XDPModeLinkGeneric)))
-		netlink.LinkSetXdpFdWithFlags(link, -1, int(xdpModeToFlag(option.XDPModeLinkDriver)))
+		if !used {
+			netlink.LinkSetXdpFdWithFlags(link, -1, int(xdpModeToFlag(option.XDPModeLinkGeneric)))
+			netlink.LinkSetXdpFdWithFlags(link, -1, int(xdpModeToFlag(option.XDPModeLinkDriver)))
+		}
 	}
 }
 

pkg/logging/logfields/logfields.go
@@ -283,9 +283,6 @@ const (
 	// BPFMapValue is a value from a BPF map
 	BPFMapValue = "bpfMapValue"
 
-	// XDPDevice is the device name
-	XDPDevice = "xdpDevice"
-
 	// Device is the device name
 	Device = "device"
 

commit 73a4e52570ba3b37f8f78c38c269e508163e7021

daemon: Fix multi-dev XDP check
The 1d526ecb4d commit didn't remove all guards which previously were
used to check that XDP is enabled only with |--devices| == 1.

The last guard was not visible on v1.11 due to
https://github.com/cilium/cilium/pull/18304, while on v1.10 it failed on
the multi-dev XDP setups.

Fixes: 1d526ecb4d ("datapath,daemon: Enable multi-dev XDP")
Reported-by: Tobias Klauser <tobias@cilium.io>
Reported-by: Paul Chaignon <paul@cilium.io>
Signed-off-by: Martynas Pumputis <m@lambda.lt>

daemon/cmd/kube_proxy_replacement.go
@@ -628,10 +628,6 @@ func finishKubeProxyReplacementInit(isKubeProxyReplacementStrict bool) error {
 		}
 	}
 
-	if option.Config.NodePortAcceleration != option.NodePortAccelerationDisabled &&
-		len(option.Config.Devices) != 1 {
-		return fmt.Errorf("Cannot set NodePort acceleration due to multi-device setup (%q). Specify --%s with a single device to enable NodePort acceleration.", option.Config.Devices, option.Devices)
-	}
 	return nil
 }
 