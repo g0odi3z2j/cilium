g0odi3z2j/cilium

commit a14efe46b844520b7115cb5854f2cec8dff4e114

ipcache, policy: Inject labels from identity metadata
Any update to the IdentityMetadata (IDMD) map must trigger an update to
the ipcache. Since the IdentityMetadata maps IPS / CIDRs to labels, if
an update was made, we must to check if that update was related to the
labels. If so, that means it might affect identities. Hence, we must to
allocate a new identity, trigger policy updating, and push it into the
ipcache. This commit provides the mechanism to do so (InjectLabels()).
In future commits, this will be triggered by other subsystems as they
encounter IPs / CIDRs for which we need to associate labels with.

Corresponding functions to find and delete IPs / CIDRs with specific
labels is also provided, which include releasing any allocated
identities and deletion from the ipcache.

Here's a diagram of how this works for the kube-apiserver policy
matching:

  +------------+  (1)        (1)  +------------+
  | EP Watcher +-----+      +-----+ CN Watcher |
  +-----+------+   W |      | W   +------+-----+
        |            |      |            |
        |            v      v            |
        |            +------+            |
        |            | IDMD |            |
        |            +------+            |
        |               ^                |
        |               |                |
        |           (3) |R               |
        | (2)    +------+--------+   (2) |
        +------> |Label Injector |<------+
       Trigger   +-------+-------+ Trigger
                     (4) |W
                         |
                         v
                       +---+
                       |IPC|
                       +---+
  legend:
  * W means write
  * R means read

Signed-off-by: Chris Tarazi <chris@isovalent.com>
Co-authored-by: Joe Stringer <joe@cilium.io>
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/ipcache/metadata.go
@@ -4,8 +4,24 @@
 package ipcache
 
 import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"sync"
+
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/labels"
+	cidrlabels "github.com/cilium/cilium/pkg/labels/cidr"
 	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/source"
+
+	"github.com/sirupsen/logrus"
 )
 
 var (
@@ -17,6 +33,10 @@ var (
 	idMDMU lock.RWMutex
 	// IdentityMetadata maps IP prefixes (x.x.x.x/32) to their labels.
 	IdentityMetadata = make(map[string]labels.Labels)
+
+	// ErrLocalIdentityAllocatorUninitialized is an error that's returned when
+	// the local identity allocator is uninitialized.
+	ErrLocalIdentityAllocatorUninitialized = errors.New("local identity allocator uninitialized")
 )
 
 // UpsertMetadata upserts a given IP and its corresponding labels associated
@@ -41,4 +61,377 @@ func GetIDMetadataByIP(prefix string) labels.Labels {
 	idMDMU.RLock()
 	defer idMDMU.RUnlock()
 	return IdentityMetadata[prefix]
+}
+
+// InjectLabels injects labels from the IdentityMetadata (IDMD) map into the
+// identities used for the prefixes in the IPCache. The given source is the
+// source of the caller, as inserting into the IPCache requires knowing where
+// this updated information comes from.
+//
+// Note that as this function iterates through the IDMD, if it detects a change
+// in labels for a given prefix, then this might allocate a new identity. If a
+// prefix was previously associated with an identity, it will get deallocated,
+// so a balance is kept.
+func InjectLabels(src source.Source, updater identityUpdater, triggerer policyTriggerer) error {
+	if IdentityAllocator == nil || !IdentityAllocator.IsLocalIdentityAllocatorInitialized() {
+		return ErrLocalIdentityAllocatorUninitialized
+	}
+
+	if IPIdentityCache.k8sSyncedChecker != nil &&
+		!IPIdentityCache.k8sSyncedChecker.K8sCacheIsSynced() {
+		return errors.New("k8s cache not fully synced")
+	}
+
+	var (
+		// trigger is true when we need to trigger policy recalculations.
+		trigger bool
+		// toUpsert stores IPKeyPairs to upsert into the ipcache.
+		toUpsert = make(map[string]Identity)
+		// idsToPropagate stores the identities that must be updated via the
+		// selector cache.
+		idsToPropagate = make(map[identity.NumericIdentity]labels.LabelArray)
+	)
+
+	idMDMU.Lock()
+	defer idMDMU.Unlock()
+
+	for prefix, lbls := range IdentityMetadata {
+		id, isNew, err := injectLabels(prefix, lbls)
+		if err != nil {
+			return fmt.Errorf("failed to allocate new identity for IP %v: %w", prefix, err)
+		}
+
+		// Reserved IDs should always be upserted if there was a change to
+		// their labels. This is especially important for IDs such as
+		// kube-apiserver which is can be accompanied by other labels such as
+		// remote-node, host, or CIDR labels.
+		// Also, any new identity should be upserted.
+		if id.IsReserved() || isNew {
+			if lbls.Has(labels.LabelKubeAPIServer[labels.IDNameKubeAPIServer]) {
+				// Overwrite the source because any IP associated with the
+				// kube-apiserver takes the strongest precedence. This is
+				// because we need to overwrite Local if only the local node IP
+				// has been upserted into the ipcache first.
+				//
+				// Also, trigger policy recalculations to update kube-apiserver
+				// identity.
+				src = source.KubeAPIServer
+				trigger = true
+				if id.IsReserved() {
+					identity.AddReservedIdentityWithLabels(id.ID, lbls)
+					idsToPropagate[id.ID] = lbls.LabelArray()
+				}
+			}
+
+			toUpsert[prefix] = Identity{
+				ID:     id.ID,
+				Source: src,
+			}
+		} else {
+			// Unlikely, but to balance the allocation / release
+			// we must either add the identity to `toUpsert`, or
+			// immediately release it again. Otherwise it will leak.
+			if _, err := IdentityAllocator.Release(context.TODO(), id); err != nil {
+				log.WithError(err).WithFields(logrus.Fields{
+					logfields.IPAddr: prefix,
+					logfields.Labels: lbls,
+				}).Error(
+					"Failed to release assigned identity during label injection, this might be a leak.",
+				)
+			}
+		}
+	}
+
+	// Recalculate policy first before upserting into the ipcache.
+	if trigger {
+		// Accumulate the desired policy map changes as the identities have
+		// been updated with new labels.
+		var wg sync.WaitGroup
+		updater.UpdateIdentities(idsToPropagate, nil, &wg)
+		wg.Wait()
+
+		// This will take the accumulated policy map changes from the above,
+		// and realizes it into the datapath.
+		triggerer.TriggerPolicyUpdates(false, "kube-apiserver identity updated")
+	}
+
+	IPIdentityCache.mutex.Lock()
+	defer IPIdentityCache.mutex.Unlock()
+	for ip, id := range toUpsert {
+		hIP, key := IPIdentityCache.getHostIPCache(ip)
+		meta := IPIdentityCache.getK8sMetadata(ip)
+		if _, err := IPIdentityCache.upsertLocked(ip, hIP, key, meta, Identity{
+			ID:     id.ID,
+			Source: src,
+		}); err != nil {
+			return fmt.Errorf("failed to upsert %s into ipcache with identity %d: %w", ip, id.ID, err)
+		}
+	}
+
+	return nil
+}
+
+func injectLabels(prefix string, lbls labels.Labels) (*identity.Identity, bool, error) {
+	// Before allocating an identity, check if we should deallocate the old one
+	// first, if it exists.
+	if id, exists := IPIdentityCache.LookupByIP(prefix); exists {
+		// But not if it's only the kube-apiserver reserved identity. The
+		// kube-apiserver label can be associated with the host and CIDR
+		// labels.
+		realID := IdentityAllocator.LookupIdentityByID(context.TODO(), id.ID)
+		if realID != nil && !realID.Labels.Equals(labels.LabelKubeAPIServer) {
+			scopedLog := log.WithFields(logrus.Fields{
+				logfields.IPAddr:         prefix,
+				logfields.OldIdentity:    realID,
+				logfields.IdentityLabels: realID.Labels,
+			})
+
+			released, err := IdentityAllocator.Release(context.TODO(), realID)
+			if err != nil {
+				scopedLog.WithError(err).Warn(
+					"Failed to release previously assigned identity to IP, this might be a leak.",
+				)
+			} else {
+				scopedLog.WithFields(logrus.Fields{
+					"released":       released,
+					logfields.Labels: lbls,
+				}).Debug(
+					"Releasing old identity with previously assigned to IP and updating with new set of labels",
+				)
+			}
+		}
+	}
+
+	// If no other labels are associated with this IP, we assume that it's
+	// outside of the cluster and hence needs a CIDR identity. This might be a
+	// temporary identity.
+	if lbls.Equals(labels.LabelKubeAPIServer) {
+		// The release of the identitiy allocated has been handled above. This
+		// can happen if we discover that the prefix is also associated with
+		// other labels besides kube-apiserver, i.e. if the kube-apiserver is
+		// not running outside of the cluster.
+		return injectLabelsForCIDR(prefix, lbls)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), option.Config.IPAllocationTimeout)
+	defer cancel()
+	return IdentityAllocator.AllocateIdentity(ctx, lbls, false)
+}
+
+// injectLabelsForCIDR will allocate a CIDR identity for the given prefix. The
+// release of the identity must be managed by the caller.
+func injectLabelsForCIDR(p string, lbls labels.Labels) (*identity.Identity, bool, error) {
+	var prefix string
+
+	ip := net.ParseIP(p)
+	if ip == nil {
+		return nil, false, fmt.Errorf("Invalid IP inserted into IdentityMetadata: %s", prefix)
+	} else if ip.To4() != nil {
+		prefix = p + "/32"
+	} else {
+		prefix = p + "/128"
+	}
+
+	_, cidr, err := net.ParseCIDR(prefix)
+	if err != nil {
+		return nil, false, err
+	}
+
+	allLbls := cidrlabels.GetCIDRLabels(cidr)
+	allLbls.MergeLabels(lbls)
+
+	log.WithFields(logrus.Fields{
+		logfields.CIDR:   cidr,
+		logfields.Labels: lbls, // omitting allLbls as CIDR labels would make this massive
+	}).Debug(
+		"Injecting CIDR labels for prefix",
+	)
+
+	return allocate(cidr, allLbls)
+}
+
+// FilterMetadataByLabels returns all the prefixes inside the IdentityMetadata
+// map which contain the given labels. Note that `filter` is a subset match,
+// not a full match.
+func FilterMetadataByLabels(filter labels.Labels) []string {
+	idMDMU.RLock()
+	defer idMDMU.RUnlock()
+
+	var matching []string
+	sortedFilter := filter.SortedList()
+	for prefix, lbls := range IdentityMetadata {
+		if bytes.Contains(lbls.SortedList(), sortedFilter) {
+			matching = append(matching, prefix)
+		}
+	}
+	return matching
+}
+
+// RemoveLabelsFromIPs wraps RemoveLabels to provide a convenient method for
+// the caller to remove all given prefixes at once. This function will trigger
+// policy update and recalculation if necessary on behalf of the caller if any
+// changes to the kube-apiserver were detected.
+//
+// Identities allocated by InjectLabels() may be released by RemoveLabels().
+//
+// A prefix will only be removed from the IDMD if the set of labels becomes
+// empty.
+func RemoveLabelsFromIPs(
+	m map[string]labels.Labels,
+	src source.Source,
+	updater identityUpdater,
+	triggerer policyTriggerer,
+) {
+	var (
+		idsToAdd    = make(map[identity.NumericIdentity]labels.LabelArray)
+		idsToDelete = make(map[identity.NumericIdentity]labels.LabelArray)
+	)
+	for prefix, lbls := range m {
+		id, exists := IPIdentityCache.LookupByIP(prefix)
+		if !exists {
+			continue
+		}
+		// Insert to propagate the updated set of labels after removal.
+		var la labels.LabelArray
+		if l := RemoveLabels(prefix, lbls, src); l != nil {
+			la = l.LabelArray()
+		}
+		idsToDelete[id.ID] = la
+		if len(lbls) > 0 {
+			// If for example kube-apiserver label is removed from
+			// a remote-node, then RemoveLabels() will return a
+			// non-empty set representing the new full set of
+			// labels to associate with the node. In order to
+			// propagate the new identity, we must emit a delete
+			// event for the old identity and then an add event for
+			// the new identity.
+			idsToAdd[id.ID] = la
+		}
+	}
+	if len(idsToDelete) > 0 {
+		var wg sync.WaitGroup
+		// SelectorCache.UpdateIdentities() asks for callers to avoid
+		// handing the same identity in both 'adds' and 'deletes'
+		// parameters here, so make two calls. These changes will not
+		// be propagated to the datapath until later.
+		updater.UpdateIdentities(nil, idsToDelete, &wg)
+		updater.UpdateIdentities(idsToAdd, nil, &wg)
+		wg.Wait()
+
+		triggerer.TriggerPolicyUpdates(false, "kube-apiserver identity updated by removal")
+	}
+}
+
+// RemoveLabels removes the given labels association with the given prefix. The
+// leftover labels are returned, if any.
+//
+// Identities are deallocated and their subequent entry in the IPCache is
+// removed if the prefix is no longer associated with any labels.
+//
+// It is the responsibility of the caller to trigger policy recalculation after
+// calling this function.
+func RemoveLabels(prefix string, lbls labels.Labels, src source.Source) labels.Labels {
+	idMDMU.Lock()
+	defer idMDMU.Unlock()
+
+	l, ok := IdentityMetadata[prefix]
+	if !ok {
+		return nil
+	}
+
+	l = l.Remove(lbls)
+	if len(l) != 0 { // Labels left over, do not deallocate
+		IdentityMetadata[prefix] = l
+		return l
+	}
+
+	// No labels left, perform deallocation
+
+	IPIdentityCache.Lock()
+	defer IPIdentityCache.Unlock()
+	delete(IdentityMetadata, prefix)
+	id, exists := IPIdentityCache.LookupByIPRLocked(prefix)
+	if !exists {
+		return nil
+	}
+	realID := IdentityAllocator.LookupIdentityByID(context.TODO(), id.ID)
+	if realID == nil {
+		return nil
+	}
+	released, err := IdentityAllocator.Release(context.TODO(), realID)
+	if err != nil {
+		log.WithError(err).WithFields(logrus.Fields{
+			logfields.IPAddr:         prefix,
+			logfields.Labels:         lbls,
+			logfields.Identity:       realID,
+			logfields.IdentityLabels: realID.Labels,
+		}).Error(
+			"Failed to release assigned identity to IP while removing label association, this might be a leak.",
+		)
+	}
+	if released {
+		if lbls.Has(labels.LabelKubeAPIServer[labels.IDNameKubeAPIServer]) {
+			src = source.KubeAPIServer
+		}
+		IPIdentityCache.deleteLocked(prefix, src)
+	}
+	return nil
+}
+
+// TriggerLabelInjection triggers the label injection controller to iterate
+// through the IDMD and potentially allocate new identities based on any label
+// changes.
+//
+// The following diagram describes the relationship between the label injector
+// triggered here and the callers/callees.
+//
+//       +------------+  (1)        (1)  +------------+
+//      | EP Watcher +-----+      +-----+ CN Watcher |
+//      +-----+------+   W |      | W   +------+-----+
+//            |            |      |            |
+//            |            v      v            |
+//            |            +------+            |
+//            |            | IDMD |            |
+//            |            +------+            |
+//            |               ^                |
+//            |               |                |
+//            |           (3) |R               |
+//            | (2)    +------+--------+   (2) |
+//            +------> |Label Injector |<------+
+//           Trigger   +-------+-------+ Trigger
+//                         (4) |W
+//                             |
+//                             v
+//                           +---+
+//                           |IPC|
+//                           +---+
+//      legend:
+//      * W means write
+//      * R means read
+func (ipc *IPCache) TriggerLabelInjection(src source.Source, sc identityUpdater, pt policyTriggerer) {
+	// GH-17829: Would also be nice to have an end-to-end test to validate
+	//           on upgrade that there are no connectivity drops when this
+	//           channel is preventing transient BPF entries.
+
+	// This controller is for retrying this operation in case it fails. It
+	// should eventually succeed.
+	ipc.UpdateController(
+		"ipcache-inject-labels",
+		controller.ControllerParams{
+			DoFunc: func(context.Context) error {
+				if err := InjectLabels(src, sc, pt); err != nil {
+					return fmt.Errorf("failed to inject labels into ipcache: %w", err)
+				}
+				return nil
+			},
+		},
+	)
+}
+
+type identityUpdater interface {
+	UpdateIdentities(added, deleted cache.IdentityCache, wg *sync.WaitGroup)
+}
+
+type policyTriggerer interface {
+	TriggerPolicyUpdates(bool, string)
 }

pkg/ipcache/metadata_test.go
@@ -1,0 +1,91 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 Authors of Cilium
+
+//go:build !privileged_tests
+// +build !privileged_tests
+
+package ipcache
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/source"
+	testidentity "github.com/cilium/cilium/pkg/testutils/identity"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInjectLabels(t *testing.T) {
+	setupTest(t)
+
+	assert.Len(t, IdentityMetadata, 1)
+	assert.NoError(t, InjectLabels(source.Local, &mockUpdater{}, &mockTriggerer{}))
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 1)
+
+	// Insert kube-apiserver IP from outside of the cluster. This should create
+	// a CIDR ID for this IP.
+	UpsertMetadata("10.0.0.4", labels.LabelKubeAPIServer)
+	assert.Len(t, IdentityMetadata, 2)
+	assert.NoError(t, InjectLabels(source.Local, &mockUpdater{}, &mockTriggerer{}))
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 2)
+	assert.True(t, IPIdentityCache.ipToIdentityCache["10.0.0.4"].ID.HasLocalScope())
+
+	// Upsert node labels to the kube-apiserver to validate that the CIDR ID is
+	// deallocated and the kube-apiserver reserved ID is associated with this
+	// IP now.
+	UpsertMetadata("10.0.0.4", labels.LabelRemoteNode)
+	assert.Len(t, IdentityMetadata, 2)
+	assert.NoError(t, InjectLabels(source.Local, &mockUpdater{}, &mockTriggerer{}))
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 2)
+	assert.False(t, IPIdentityCache.ipToIdentityCache["10.0.0.4"].ID.HasLocalScope())
+}
+
+func TestFilterMetadataByLabels(t *testing.T) {
+	setupTest(t)
+
+	UpsertMetadata("2.1.1.1", labels.LabelWorld)
+	UpsertMetadata("3.1.1.1", labels.LabelWorld)
+
+	assert.Len(t, FilterMetadataByLabels(labels.LabelKubeAPIServer), 1)
+	assert.Len(t, FilterMetadataByLabels(labels.LabelWorld), 2)
+}
+
+func TestRemoveLabelsFromIPs(t *testing.T) {
+	setupTest(t)
+
+	assert.Len(t, IdentityMetadata, 1)
+	assert.NoError(t, InjectLabels(source.Local, &mockUpdater{}, &mockTriggerer{}))
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 1)
+
+	RemoveLabelsFromIPs(map[string]labels.Labels{
+		"1.1.1.1": labels.LabelKubeAPIServer,
+	}, source.Local, &mockUpdater{}, &mockTriggerer{})
+	assert.Len(t, IdentityMetadata, 1)
+	assert.Equal(t, labels.LabelHost, IdentityMetadata["1.1.1.1"])
+}
+
+func setupTest(t *testing.T) {
+	IPIdentityCache = NewIPCache()
+	IPIdentityCache.k8sSyncedChecker = &mockK8sSyncedChecker{}
+
+	IdentityAllocator = testidentity.NewMockIdentityAllocator(nil)
+	IdentityMetadata = make(map[string]labels.Labels)
+
+	UpsertMetadata("1.1.1.1", labels.LabelKubeAPIServer)
+	UpsertMetadata("1.1.1.1", labels.LabelHost)
+}
+
+type mockK8sSyncedChecker struct{}
+
+func (m *mockK8sSyncedChecker) K8sCacheIsSynced() bool { return true }
+
+type mockUpdater struct{}
+
+func (m *mockUpdater) UpdateIdentities(_, _ cache.IdentityCache, _ *sync.WaitGroup) {}
+
+type mockTriggerer struct{}
+
+func (m *mockTriggerer) TriggerPolicyUpdates(bool, string) {}

daemon/cmd/daemon.go
@@ -454,6 +454,9 @@ func NewDaemon(ctx context.Context, cancel context.CancelFunc, epMgr *endpointma
 		option.Config,
 	)
 	nd.RegisterK8sNodeGetter(d.k8sWatcher)
+	// GH-17849: The daemon does not have a reference to the ipcache,
+	// instead we rely on the global.
+	ipcache.IPIdentityCache.RegisterK8sSyncedChecker(&d)
 
 	d.k8sWatcher.NodeChain.Register(d.endpointManager)
 	if option.Config.BGPAnnounceLBIP || option.Config.BGPAnnouncePodCIDR {

daemon/cmd/datapath.go
@@ -19,6 +19,7 @@ import (
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/ipcache"
+	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
 	"github.com/cilium/cilium/pkg/maps/egressmap"
@@ -243,9 +244,13 @@ func (d *Daemon) syncEndpointsAndHostIPs() error {
 
 		// Upsert will not propagate (reserved:foo->ID) mappings across the cluster,
 		// and we specifically don't want to do so.
+		//
+		// This upsert will fail with ErrOverwrite continuously as long as the
+		// EP / CN watcher have found an apiserver IP and upserted it into the
+		// ipcache. Until then, it is expected to succeed.
 		ipcache.IPIdentityCache.Upsert(ipIDPair.PrefixString(), nil, hostKey, nil, ipcache.Identity{
 			ID:     ipIDPair.ID,
-			Source: source.Local,
+			Source: sourceByIP(ipIDPair.IP.String(), source.Local),
 		})
 	}
 
@@ -259,11 +264,20 @@ func (d *Daemon) syncEndpointsAndHostIPs() error {
 				log.Debugf("Removed outdated host ip %s from endpoint map", hostIP)
 			}
 
-			ipcache.IPIdentityCache.Delete(hostIP, source.Local)
+			ipcache.IPIdentityCache.Delete(hostIP, sourceByIP(hostIP, source.Local))
 		}
 	}
 
 	return nil
+}
+
+func sourceByIP(prefix string, defaultSrc source.Source) source.Source {
+	if lbls := ipcache.GetIDMetadataByIP(prefix); lbls.Has(
+		labels.LabelKubeAPIServer[labels.IDNameKubeAPIServer],
+	) {
+		return source.KubeAPIServer
+	}
+	return defaultSrc
 }
 
 // initMaps opens all BPF maps (and creates them if they do not exist). This

pkg/ipcache/cidr.go
@@ -58,14 +58,16 @@ func AllocateCIDRs(
 			continue
 		}
 
-		prefixStr := p.String()
+		lbls := cidr.GetCIDRLabels(p)
+		lbls.MergeLabels(GetIDMetadataByIP(p.IP.String()))
 
-		id, isNew, err := allocate(prefixStr, cidr.GetCIDRLabels(p))
+		id, isNew, err := allocate(p, lbls)
 		if err != nil {
 			IdentityAllocator.ReleaseSlice(context.Background(), nil, usedIdentities)
 			return nil, err
 		}
 
+		prefixStr := p.String()
 		usedIdentities = append(usedIdentities, id)
 		allocatedIdentities[prefixStr] = id
 		if isNew {
@@ -106,7 +108,21 @@ func UpsertGeneratedIdentities(newlyAllocatedIdentities map[string]*identity.Ide
 	}
 }
 
-func allocate(prefix string, lbls labels.Labels) (*identity.Identity, bool, error) {
+// allocate will allocate a new identity for the given prefix based on the
+// given set of labels. This function performs both global and local (CIDR)
+// identity allocation and the set of labels determine which identity
+// allocation type is to occur.
+//
+// If the identity is a CIDR identity, then its corresponding Identity will
+// have its CIDR labels set correctly.
+//
+// It is up to the caller to provide the full set of labels for identity
+// allocation.
+func allocate(prefix *net.IPNet, lbls labels.Labels) (*identity.Identity, bool, error) {
+	if prefix == nil {
+		return nil, false, nil
+	}
+
 	allocateCtx, cancel := context.WithTimeout(context.Background(), option.Config.IPAllocationTimeout)
 	defer cancel()
 
@@ -116,7 +132,7 @@ func allocate(prefix string, lbls labels.Labels) (*identity.Identity, bool, erro
 	}
 
 	if lbls.Has(labels.LabelWorld[labels.IDNameWorld]) {
-		id.CIDRLabel = labels.NewLabelsFromModel([]string{labels.LabelSourceCIDR + ":" + prefix})
+		id.CIDRLabel = labels.NewLabelsFromModel([]string{labels.LabelSourceCIDR + ":" + prefix.String()})
 	}
 
 	return id, isNew, err

commit 9bdcacae2a94534dde22c80c235d835f2bbd8608

ipcache, k8s/watchers: Fix non-atomic window when removing labels
Prevously, this code suffered from a TOCTOU bug. The IDMD lock would be
taken by two different calls into the IDMD logic and assumed that the
underlying IDMD would stay in the same state across both calls. However,
that is incorrect because of two separate calls took the IDMD lock, so
it's possible for the IDMD to change in between the two calls, hence
TOCTOU.

This commit fixes this by amending the API of the IDMD logic to expose
one function to perform the work of the previous two functions. Those
two functions are now unexported.

Fixes: a14efe46b84 ("ipcache, policy: Inject labels from identity
metadata")

Reported-by: Joe Stringer <joe@cilium.io>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ipcache/metadata.go
@@ -267,13 +267,37 @@ func injectLabelsForCIDR(p string, lbls labels.Labels) (*identity.Identity, bool
 	return allocate(cidr, allLbls)
 }
 
-// FilterMetadataByLabels returns all the prefixes inside the identityMetadata
+// RemoveLabelsExcluded removes the given labels from all IPs inside the IDMD
+// except for the IPs / prefixes inside the given excluded set. This may cause
+// updates to the ipcache, as well as to the identity and policy logic via
+// 'updater' and 'triggerer'.
+func RemoveLabelsExcluded(
+	lbls labels.Labels,
+	toExclude map[string]struct{},
+	src source.Source,
+	updater identityUpdater,
+	triggerer policyTriggerer,
+) {
+	idMDMU.Lock()
+	defer idMDMU.Unlock()
+
+	oldSet := filterMetadataByLabels(lbls)
+	toRemove := make(map[string]labels.Labels)
+	for _, ip := range oldSet {
+		if _, ok := toExclude[ip]; !ok {
+			toRemove[ip] = lbls
+		}
+	}
+
+	removeLabelsFromIPs(toRemove, src, updater, triggerer)
+}
+
+// filterMetadataByLabels returns all the prefixes inside the identityMetadata
 // map which contain the given labels. Note that `filter` is a subset match,
 // not a full match.
-func FilterMetadataByLabels(filter labels.Labels) []string {
-	idMDMU.RLock()
-	defer idMDMU.RUnlock()
-
+//
+// Assumes that the IDMD read lock is taken!
+func filterMetadataByLabels(filter labels.Labels) []string {
 	var matching []string
 	sortedFilter := filter.SortedList()
 	for prefix, lbls := range identityMetadata {
@@ -290,7 +314,9 @@ func FilterMetadataByLabels(filter labels.Labels) []string {
 //
 // A prefix will only be removed from the IDMD if the set of labels becomes
 // empty.
-func RemoveLabelsFromIPs(
+//
+// Assumes that the IDMD lock is taken!
+func removeLabelsFromIPs(
 	m map[string]labels.Labels,
 	src source.Source,
 	updater identityUpdater,
@@ -302,9 +328,6 @@ func RemoveLabelsFromIPs(
 		// toReplace stores the identity to replace in the ipcache.
 		toReplace = make(map[string]Identity)
 	)
-
-	idMDMU.Lock()
-	defer idMDMU.Unlock()
 
 	IPIdentityCache.Lock()
 	defer IPIdentityCache.Unlock()

pkg/k8s/watchers/endpoint.go
@@ -131,15 +131,9 @@ func (k *K8sWatcher) handleKubeAPIServerServiceEPChanges(desiredIPs map[string]s
 	//     an update event.
 	//   * if the entire object is deleted, then it will quickly be recreated
 	//     and this will be in the form of an add event.
-	oldAPIServerIPs := ipcache.FilterMetadataByLabels(labels.LabelKubeAPIServer)
-	toRemove := make(map[string]labels.Labels)
-	for _, ip := range oldAPIServerIPs {
-		if _, ok := desiredIPs[ip]; !ok {
-			toRemove[ip] = labels.LabelKubeAPIServer
-		}
-	}
-	ipcache.RemoveLabelsFromIPs(
-		toRemove,
+	ipcache.RemoveLabelsExcluded(
+		labels.LabelKubeAPIServer,
+		desiredIPs,
 		src,
 		k.policyRepository.GetSelectorCache(),
 		k.policyManager,

pkg/ipcache/metadata_test.go
@@ -52,8 +52,8 @@ func TestFilterMetadataByLabels(t *testing.T) {
 	UpsertMetadata("2.1.1.1", labels.LabelWorld)
 	UpsertMetadata("3.1.1.1", labels.LabelWorld)
 
-	assert.Len(t, FilterMetadataByLabels(labels.LabelKubeAPIServer), 1)
-	assert.Len(t, FilterMetadataByLabels(labels.LabelWorld), 2)
+	assert.Len(t, filterMetadataByLabels(labels.LabelKubeAPIServer), 1)
+	assert.Len(t, filterMetadataByLabels(labels.LabelWorld), 2)
 }
 
 func TestRemoveLabelsFromIPs(t *testing.T) {
@@ -63,7 +63,7 @@ func TestRemoveLabelsFromIPs(t *testing.T) {
 	assert.NoError(t, InjectLabels(source.Local, &mockUpdater{}, &mockTriggerer{}))
 	assert.Len(t, IPIdentityCache.ipToIdentityCache, 1)
 
-	RemoveLabelsFromIPs(map[string]labels.Labels{
+	removeLabelsFromIPs(map[string]labels.Labels{
 		"1.1.1.1": labels.LabelKubeAPIServer,
 	}, source.Local, &mockUpdater{}, &mockTriggerer{})
 	assert.Len(t, identityMetadata, 1)
@@ -90,7 +90,7 @@ func TestRemoveLabelsFromIPs(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Len(t, ids, 1)
 	assert.Equal(t, 2, id.ReferenceCount)
-	RemoveLabelsFromIPs(map[string]labels.Labels{ // remove kube-apiserver policy
+	removeLabelsFromIPs(map[string]labels.Labels{ // remove kube-apiserver policy
 		"1.1.1.1": labels.LabelKubeAPIServer,
 	}, source.Local, &mockUpdater{}, &mockTriggerer{})
 	assert.NotContains(t, identityMetadata["1.1.1.1"], labels.LabelKubeAPIServer)