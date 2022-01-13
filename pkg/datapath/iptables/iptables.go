g0odi3z2j/cilium

commit 0a04790dbf4939b443d6f74b300e1d5aef14cfae

iptables: Don't masquerade traffic to cluster nodes
When using BPF masquerading, we don't masquerade traffic destined to
cluster nodes in native routing mode. We detect those destination
using the security identity.

When using iptables masquerading, we cannot implement the exact same
because we can't match on security identities. Instead, we need to
maintain an ipset of IP addresses belonging to cluster nodes and skip
the iptables masquerading rules when a packet is destined to an IP in
the ipset.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/iptables/iptables.go
@@ -7,6 +7,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"net"
 	"regexp"
 	"strconv"
 	"strings"
@@ -18,6 +19,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/ip"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	lb "github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -46,6 +48,8 @@ const (
 	ciliumForwardChain    = "CILIUM_FORWARD"
 	feederDescription     = "cilium-feeder:"
 	xfrmDescription       = "cilium-xfrm-notrack:"
+	ciliumNodeIpsetV4     = "cilium_node_set_v4"
+	ciliumNodeIpsetV6     = "cilium_node_set_v6"
 )
 
 // Minimum iptables versions supporting the -w and -w<seconds> flags
@@ -80,23 +84,30 @@ type customChain struct {
 
 type iptablesInterface interface {
 	getProg() string
+	getIpset() string
 	getVersion() (semver.Version, error)
 	runProgCombinedOutput(args []string, quiet bool) ([]byte, error)
 	runProg(args []string, quiet bool) error
 }
 
 type ipt struct {
-	prog string
+	prog  string
+	ipset string
 }
 
 // package name is iptables so we use ip4tables internally for "iptables"
 var (
-	ip4tables = &ipt{prog: "iptables"}
-	ip6tables = &ipt{prog: "ip6tables"}
+	ip4tables = &ipt{prog: "iptables", ipset: ciliumNodeIpsetV4}
+	ip6tables = &ipt{prog: "ip6tables", ipset: ciliumNodeIpsetV6}
+	ipset     = &ipt{prog: "ipset"}
 )
 
 func (ipt *ipt) getProg() string {
 	return ipt.prog
+}
+
+func (ipt *ipt) getIpset() string {
+	return ipt.ipset
 }
 
 func (ipt *ipt) getVersion() (semver.Version, error) {
@@ -1035,12 +1046,62 @@ func (m *IptablesManager) installForwardChainRulesIpX(prog iptablesInterface, if
 	return nil
 }
 
+// AddToNodeIpset adds an IP address to the ipset for cluster nodes. It creates
+// the ipset if it doesn't already exist and doesn't error if either the ipset
+// or the IP already exist.
+func AddToNodeIpset(nodeIP net.IP) {
+	scopedLog := log.WithField(logfields.IPAddr, nodeIP.String())
+	ciliumNodeIpset := ciliumNodeIpsetV4
+	if ip.IsIPv6(nodeIP) {
+		ciliumNodeIpset = ciliumNodeIpsetV6
+	}
+	scopedLog.Debugf("Adding IP to ipset %s", ciliumNodeIpset)
+	createIpset(ciliumNodeIpset, ip.IsIPv6(nodeIP))
+	progArgs := []string{"add", ciliumNodeIpset, nodeIP.String(), "-exist"}
+	if err := ipset.runProg(progArgs, false); err != nil {
+		scopedLog.WithError(err).Errorf("Failed to add IP to ipset %s", ciliumNodeIpset)
+	}
+}
+
+// RemoveFromBodeIpset removes an IP address from the ipset for cluster nodes.
+func RemoveFromNodeIpset(nodeIP net.IP) {
+	scopedLog := log.WithField(logfields.IPAddr, nodeIP.String())
+	ciliumNodeIpset := ciliumNodeIpsetV4
+	if ip.IsIPv6(nodeIP) {
+		ciliumNodeIpset = ciliumNodeIpsetV6
+	}
+	scopedLog.Debugf("Removing IP from ipset %s", ciliumNodeIpset)
+	progArgs := []string{"del", ciliumNodeIpset, nodeIP.String()}
+	if err := ipset.runProg(progArgs, false); err != nil {
+		scopedLog.WithError(err).Errorf("Failed to remove IP from ipset %s", ciliumNodeIpset)
+	}
+}
+
 func (m *IptablesManager) installMasqueradeRules(prog iptablesInterface, ifName, localDeliveryInterface,
 	snatDstExclusionCIDR, allocRange, hostMasqueradeIP string) error {
+	if option.Config.Tunnel == option.TunnelDisabled {
+		// Exclude traffic to nodes from masquerade.
+		if err := createIpset(prog.getIpset(), prog.getProg() == "ip6tables"); err != nil {
+			return err
+		}
+		progArgs := append(
+			m.waitArgs,
+			"-t", "nat",
+			"-A", ciliumPostNatChain,
+			"-s", allocRange,
+			"-m", "set", "--match-set", prog.getIpset(), "dst",
+			"-m", "comment", "--comment", "exclude traffic to cluster nodes from masquerade",
+			"-j", "ACCEPT",
+		)
+		if err := prog.runProg(progArgs, false); err != nil {
+			return err
+		}
+	}
+
 	// Masquerade all egress traffic leaving the node
 	//
-	// This rule must be first as it has different exclusion criteria
-	// than the other rules in this table.
+	// This rule must be first as the node ipset rule as it has different
+	// exclusion criteria than the other rules in this table.
 	//
 	// The following conditions must be met:
 	// * May not leave on a cilium_ interface, this excludes all
@@ -1171,6 +1232,15 @@ func (m *IptablesManager) installMasqueradeRules(prog iptablesInterface, ifName,
 	}
 
 	return nil
+}
+
+func createIpset(name string, ipv6 bool) error {
+	ipsetFamily := "inet"
+	if ipv6 {
+		ipsetFamily = "inet6"
+	}
+	progArgs := []string{"create", name, "iphash", "family", ipsetFamily, "-exist"}
+	return ipset.runProg(progArgs, false)
 }
 
 func (m *IptablesManager) installHostTrafficMarkRule(prog iptablesInterface) error {

pkg/node/manager/manager.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/datapath"
+	"github.com/cilium/cilium/pkg/datapath/iptables"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/inctimer"
 	"github.com/cilium/cilium/pkg/ipcache"
@@ -360,6 +361,10 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 			tunnelIP = nodeIP
 		}
 
+		if address.Type != addressing.NodeCiliumInternalIP {
+			iptables.AddToNodeIpset(address.IP)
+		}
+
 		if skipIPCache(address) {
 			continue
 		}
@@ -514,6 +519,10 @@ func (m *Manager) NodeDeleted(n nodeTypes.Node) {
 	}
 
 	for _, address := range entry.node.IPAddresses {
+		if address.Type != addressing.NodeCiliumInternalIP {
+			iptables.RemoveFromNodeIpset(address.IP)
+		}
+
 		if m.legacyNodeIpBehavior() && address.Type != addressing.NodeCiliumInternalIP {
 			continue
 		}

pkg/datapath/iptables/iptables_test.go
@@ -34,6 +34,7 @@ type expectation struct {
 type mockIptables struct {
 	c            *check.C
 	prog         string
+	ipset        string
 	expectations []expectation
 	index        int
 }
@@ -44,6 +45,10 @@ func (ipt *mockIptables) addExpectation(args string, out []byte, err error) {
 
 func (ipt *mockIptables) getProg() string {
 	return ipt.prog
+}
+
+func (ipt *mockIptables) getIpset() string {
+	return ipt.ipset
 }
 
 func (ipt *mockIptables) getVersion() (semver.Version, error) {

commit db66a3599571ed74d6debd8a890419a921110634

iptables, node: Use ipset only with iptables masquerading
In commit 0a04790d ("iptables: Don't masquerade traffic to cluster
nodes"), we introduced an ipset to track the set of node IP addresses
and avoid masquerading traffic to those IP in native routing mode.

This is however only needed if we're using iptables-based masquerading
(vs. BPF-based). When that's not the case, there's no reason to track
node IP addresses in this way, or even to require the ipset binary as a
dependency.

Fixes: 0a04790d ("iptables: Don't masquerade traffic to cluster nodes")
Reported-by: Daniel Borkmann <daniel@iogearbox.net>
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/iptables/iptables.go
@@ -1074,9 +1074,16 @@ func RemoveFromNodeIpset(nodeIP net.IP) {
 	}
 }
 
+// useNodeIpset returns true if the ipset for node IP addresses should be
+// used to skip masquerading.
+func useNodeIpset() bool {
+	return option.Config.Tunnel == option.TunnelDisabled &&
+		option.Config.IptablesMasqueradingEnabled()
+}
+
 func (m *IptablesManager) installMasqueradeRules(prog iptablesInterface, ifName, localDeliveryInterface,
 	snatDstExclusionCIDR, allocRange, hostMasqueradeIP string) error {
-	if option.Config.Tunnel == option.TunnelDisabled {
+	if useNodeIpset() {
 		// Exclude traffic to nodes from masquerade.
 		if err := createIpset(prog.getIpset(), prog.getProg() == "ip6tables"); err != nil {
 			return err

pkg/node/manager/manager.go
@@ -411,7 +411,8 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 			tunnelIP = nodeIP
 		}
 
-		if address.Type != addressing.NodeCiliumInternalIP {
+		if option.Config.IptablesMasqueradingEnabled() &&
+			address.Type != addressing.NodeCiliumInternalIP {
 			iptables.AddToNodeIpset(address.IP)
 		}
 
@@ -594,7 +595,8 @@ func (m *Manager) NodeDeleted(n nodeTypes.Node) {
 	}
 
 	for _, address := range entry.node.IPAddresses {
-		if address.Type != addressing.NodeCiliumInternalIP {
+		if option.Config.IptablesMasqueradingEnabled() &&
+			address.Type != addressing.NodeCiliumInternalIP {
 			iptables.RemoveFromNodeIpset(address.IP)
 		}
 