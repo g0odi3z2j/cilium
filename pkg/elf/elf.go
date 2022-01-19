g0odi3z2j/cilium

commit 763b290a01f045efb30728b7b733c17cce9b58fe

Add ELF package for templating constants
Add a new package that is capable of reading ELF files and substituting
32-bit values in the data section and strings in the string table, based
on a caller-specified set of options.

Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/elf/elf.go
@@ -1,0 +1,279 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package elf
+
+import (
+	"debug/elf"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"unsafe"
+
+	"github.com/cilium/cilium/pkg/lock"
+
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	subsystem = "elf"
+)
+
+var (
+	ignoredPrefixes []string
+)
+
+// ELF is an in-memory representation of a BPF ELF object from the filesystem.
+type ELF struct {
+	metadata *elf.File
+	file     *os.File
+	symbols  symbols
+	log      *logrus.Entry
+
+	// lock concurrent writes of the ELF. This library probably isn't far
+	// off from allowing concurrent Write() execution, but it's just not
+	// supported for now.
+	lock.Mutex
+}
+
+// newReader creates a new reader that can safely seek the input in parallel.
+func newReader(ra io.ReaderAt) *io.SectionReader {
+	// If 1<<63-1 is good enough for pkg/debug/elf, it's good enough for us.
+	return io.NewSectionReader(ra, 0, 1<<63-1)
+}
+
+// NewELF returns a new object from the specified reader.
+//
+// The ELF binary is expected to start at position 0 in the specified reader.
+func NewELF(ra io.ReaderAt, scopedLog *logrus.Entry) (*ELF, error) {
+	ef, err := elf.NewFile(ra)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open ELF: %s", err)
+	}
+
+	// EM_NONE is generated by older Clang (eg 3.8.x), which we currently
+	// use in Travis. We should be able to drop that part pretty soon.
+	if ef.Machine != elf.EM_NONE && ef.Machine != elf.EM_BPF {
+		return nil, fmt.Errorf("unsupported ELF machine type %s", ef.Machine)
+	}
+
+	result := &ELF{
+		metadata: ef,
+		log:      scopedLog,
+	}
+	if err := result.symbols.extractFrom(ef); err != nil {
+		return nil, fmt.Errorf("unable to read ELF symbols: %s", err)
+	}
+
+	return result, nil
+}
+
+// Open an ELF file from the specified path.
+func Open(path string) (*ELF, error) {
+	scopedLog := log.WithField(srcPath, path)
+
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := NewELF(f, scopedLog)
+	if err != nil {
+		if err2 := f.Close(); err2 != nil {
+			scopedLog.WithError(err).Warning("Failed to close ELF")
+		}
+		return nil, err
+	}
+	result.file = f
+	return result, err
+}
+
+// Close closes the ELF. If the File was created using NewELF directly instead
+// of Open, Close has no effect.
+func (elf *ELF) Close() (err error) {
+	if elf.file != nil {
+		err = elf.file.Close()
+	}
+	return err
+}
+
+func (elf *ELF) readValue(offset int64, size int64) ([]byte, error) {
+	reader := io.NewSectionReader(elf.file, offset, size)
+	result := make([]byte, size)
+	if err := binary.Read(reader, elf.metadata.ByteOrder, &result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (elf *ELF) readOption(key string) (result uint32, err error) {
+	opt, exists := elf.symbols.data[key]
+	if !exists {
+		return 0, fmt.Errorf("no such option %q in ELF", key)
+	}
+	value, err := elf.readValue(int64(opt.offset), int64(opt.size))
+	if err != nil {
+		return 0, err
+	}
+	return elf.metadata.ByteOrder.Uint32(value), err
+}
+
+func (elf *ELF) findString(key string) error {
+	opt, exists := elf.symbols.strings[key]
+	if !exists {
+		return fmt.Errorf("no such string %q in ELF", key)
+	}
+	if _, err := elf.readValue(int64(opt.offset), int64(opt.size)); err != nil {
+		return err
+	}
+	return nil
+}
+
+// copy the ELF from the reader to the writer, substituting the constants with
+// names specified in 'intOptions' with their corresponding values, and the
+// strings specified in 'strOptions' with their corresponding values.
+//
+// Keys in the 'intOptions' / 'strOptions' maps are case-sensitive.
+func (elf *ELF) copy(w io.Writer, r *io.SectionReader, intOptions map[string]uint32, strOptions map[string]string) error {
+	if len(intOptions) == 0 && len(strOptions) == 0 {
+		// Copy the remaining portion of the file
+		if _, err := io.Copy(w, r); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	globalOff := uint64(0) // current position in file
+	processedOptions := make(map[string]struct{}, len(intOptions)+len(strOptions))
+processSymbols:
+	for _, symbol := range elf.symbols.sort() {
+		scopedLog := log.WithField("symbol", symbol.name)
+
+		// Figure out the value to substitute
+		var value []byte
+		switch symbol.kind {
+		case symbolUint32:
+			v, exists := intOptions[symbol.name]
+			if exists {
+				value = make([]byte, unsafe.Sizeof(v))
+				elf.metadata.ByteOrder.PutUint32(value, v)
+			}
+		case symbolString:
+			v, exists := strOptions[symbol.name]
+			if exists {
+				if uint64(len(v)) != symbol.size {
+					return fmt.Errorf("symbol substitution value %q (len %d) must equal length of symbol name %q (len %d)", v, len(v), symbol.name, symbol.size)
+				}
+				value = []byte(v)
+			}
+		}
+		if value == nil {
+			for _, prefix := range ignoredPrefixes {
+				if strings.HasPrefix(symbol.name, prefix) {
+					continue processSymbols
+				}
+			}
+			scopedLog.Warning("Skipping symbol substitution")
+			continue processSymbols
+		}
+
+		// Copy data up until this symbol into the new file;
+		// Write the new value and seek past it.
+		dataToCopy := int64(symbol.offset - globalOff)
+		if _, err := io.CopyN(w, r, dataToCopy); err != nil {
+			return err
+		}
+		if err := binary.Write(w, elf.metadata.ByteOrder, value); err != nil {
+			return fmt.Errorf("failed to substitute %s: %s", symbol.name, err)
+		}
+		if _, err := r.Seek(int64(symbol.size), io.SeekCurrent); err != nil {
+			return err
+		}
+		processedOptions[symbol.name] = struct{}{}
+		globalOff = symbol.offset + symbol.size
+	}
+
+	// Check for additional options that weren't applied
+	for symbol := range strOptions {
+		if _, processed := processedOptions[symbol]; !processed {
+			return fmt.Errorf("no such string %q in ELF", symbol)
+		}
+	}
+	for symbol := range intOptions {
+		if _, processed := processedOptions[symbol]; !processed {
+			return fmt.Errorf("no such symbol %q in ELF", symbol)
+		}
+	}
+
+	// Copy the remaining portion of the file
+	if _, err := io.Copy(w, r); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Write the received ELF to a new file at the specified location, with the
+// specified options (indexed by name) substituted:
+// - intOptions: 32-bit values substituted in the data section.
+// - strOptions: strings susbtituted in the string table. For each key/value
+//               pair, both key and value must be same length.
+//
+// Only one goroutine may Write() the same *ELF concurrently.
+//
+// On success, writes the new file to the specified path.
+// On failure, returns an error and no file is left on the filesystem.
+func (elf *ELF) Write(path string, intOptions map[string]uint32, strOptions map[string]string) error {
+	elf.Lock()
+	defer elf.Unlock()
+
+	scopedLog := elf.log.WithField(dstPath, path)
+
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err2 := f.Close(); err2 != nil {
+			scopedLog.WithError(err).Warning("Failed to close new ELF")
+		}
+		if err != nil {
+			if err2 := os.RemoveAll(path); err2 != nil {
+				scopedLog.WithError(err).Warning("Failed to clean up new ELF path on error")
+			}
+		}
+	}()
+
+	reader := newReader(elf.file)
+	if err = elf.copy(f, reader, intOptions, strOptions); err != nil {
+		return err
+	}
+	if err = f.Sync(); err != nil {
+		return err
+	}
+
+	scopedLog.WithError(err).Debugf("Finished writing ELF")
+	return nil
+}
+
+// IgnoreSymbolPrefixes configures the ELF package to ignore symbols that have
+// any of the specified prefixes. It must be called by only one thread at a time.
+//
+// This slice will be iterated once per ELF.Write(), so try not to let it grow
+// out of hand...
+func IgnoreSymbolPrefixes(prefixes []string) {
+	ignoredPrefixes = prefixes
+}

pkg/elf/symbols.go
@@ -1,0 +1,243 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package elf
+
+import (
+	"bufio"
+	"debug/elf"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"sort"
+	"strings"
+	"unsafe"
+)
+
+const (
+	dataSection       = ".data"
+	mapSection        = "maps"
+	endpointMapPrefix = "1/0x"     // CILIUM_MAP_POLICY ## / ## LXC_ID
+	endpointRelPrefix = ".rel1/0x" // .rel ## endpointMapPrefix
+
+	// nMapRelocations is an approximation of the number of offsets in an
+	// ELF relating to map names that need to be adjusted. It's used for
+	// map initialization within 'Symbols'.
+	//
+	// $ readelf -a bpf/bpf_lxc.o | grep "cilium_.*_" | grep "^0000" | wc -l
+	// 51
+	nMapRelocations = 64
+
+	nullTerminator     = byte(0)
+	relocSectionPrefix = ".rel"
+)
+
+type symbolKind uint32
+
+var (
+	symbolUint32 = symbolKind(1)
+	symbolString = symbolKind(2)
+)
+
+func (k symbolKind) String() string {
+	switch k {
+	case symbolUint32:
+		return "uint32"
+	case symbolString:
+		return "string"
+	}
+	return "unknown"
+}
+
+// symbol stores the location and type of a symbol within the ELF file.
+type symbol struct {
+	name   string
+	kind   symbolKind
+	offset uint64
+	size   uint64
+}
+
+func newSymbol(name string, kind symbolKind, offset, size uint64) symbol {
+	return symbol{
+		name:   name,
+		kind:   kind,
+		offset: offset,
+		size:   size,
+	}
+}
+
+func newVariable(name string, offset uint64) symbol {
+	size := uint64(unsafe.Sizeof(symbolUint32))
+	return newSymbol(name, symbolUint32, offset, size)
+}
+
+func newString(name string, offset uint64) symbol {
+	return newSymbol(name, symbolString, offset, uint64(len(name)))
+}
+
+type symbolSlice []symbol
+
+// sort a slice of symbols by offset.
+func (c symbolSlice) sort() symbolSlice {
+	sort.Slice(c, func(i, j int) bool { return c[i].offset < c[j].offset })
+	return c
+}
+
+type symbols struct {
+	// data caches static 32-bit variables by name.
+	data map[string]symbol
+	// strings caches string symbols by name.
+	strings map[string]symbol
+}
+
+func (s *symbols) sort() symbolSlice {
+	result := make(symbolSlice, 0)
+	for _, c := range s.data {
+		result = append(result, c)
+	}
+	for _, c := range s.strings {
+		result = append(result, c)
+	}
+	return result.sort()
+}
+
+func isGlobalData(sym elf.Symbol) bool {
+	return elf.ST_TYPE(sym.Info) == elf.STT_NOTYPE &&
+		elf.ST_BIND(sym.Info) == elf.STB_GLOBAL &&
+		elf.ST_VISIBILITY(sym.Other) == elf.STV_DEFAULT
+}
+
+func getSectionHeaderSize(e *elf.File) uint64 {
+	switch e.Class {
+	case elf.ELFCLASS32:
+		return uint64(unsafe.Sizeof(elf.Section32{}))
+	case elf.ELFCLASS64:
+		return uint64(unsafe.Sizeof(elf.Section64{}))
+	}
+	return 0
+}
+
+func readStringOffset(e *elf.File, r io.ReadSeeker, symbolOffset int64) (uint64, error) {
+	if _, err := r.Seek(symbolOffset, io.SeekStart); err != nil {
+		return 0, err
+	}
+
+	switch e.Class {
+	case elf.ELFCLASS32:
+		var sym32 elf.Sym32
+		if err := binary.Read(r, e.ByteOrder, &sym32); err != nil {
+			return 0, err
+		}
+		return uint64(sym32.Name), nil
+	case elf.ELFCLASS64:
+		var sym64 elf.Sym64
+		if err := binary.Read(r, e.ByteOrder, &sym64); err != nil {
+			return 0, err
+		}
+		return uint64(sym64.Name), nil
+	}
+	return 0, fmt.Errorf("Unsupported ELF type %d", e.Class)
+}
+
+// extractFrom processes the specified ELF and populates the received symbols
+// object with data and string offsets in the file, for later substitution.
+func (s *symbols) extractFrom(e *elf.File) error {
+	dataOffsets := make(map[string]symbol)
+	stringOffsets := make(map[string]symbol)
+
+	symbols, err := e.Symbols()
+	if err != nil {
+		return err
+	}
+	symtab := e.SectionByType(elf.SHT_SYMTAB)
+	strtab := e.Sections[symtab.Link]
+
+	// Scan symbol table for offsets of static data and symbol names.
+	symbolReader := symtab.Open()
+	for i, sym := range symbols {
+		section := e.Sections[sym.Section]
+		switch {
+		case section.Flags&elf.SHF_COMPRESSED > 0:
+			return fmt.Errorf("compressed %s section not supported", section.Name)
+		case !isGlobalData(sym):
+			// LBB is a common llvm symbol prefix (basic block);
+			// Don't flood the logs with messages about it.
+			if !strings.HasPrefix(sym.Name, "LBB") {
+				log.Debugf("Skipping %s", sym.Name)
+			}
+			continue
+		case section.Name == dataSection:
+			// Offset from start of binary to variable inside .data
+			offset := section.Offset + sym.Value
+			dataOffsets[sym.Name] = newVariable(sym.Name, offset)
+			log.WithField(fieldSymbol, sym.Name).Debugf("Found variable with offset %d", offset)
+		case section.Name == mapSection:
+			// From the Golang Documentation:
+			//   "For compatibility with Go 1.0, Symbols omits the
+			//   the null symbol at index 0."
+			// We must reverse this when reading directly.
+			symbolOffset := int64(i+1) * int64(symtab.Entsize)
+			symOffsetInStrtab, err := readStringOffset(e, symbolReader, symbolOffset)
+			if err != nil {
+				return err
+			}
+			// Offset from start of binary to name inside .strtab
+			symOffset := strtab.Offset + symOffsetInStrtab
+			stringOffsets[sym.Name] = newString(sym.Name, symOffset)
+			log.WithField(fieldSymbol, sym.Name).Debugf("Found symbol with offset %d", symOffset)
+		default:
+			log.WithField(fieldSymbol, sym.Name).Debugf("Found symbol with unknown section reference %d", sym.Section)
+		}
+	}
+
+	// Scan string table for offsets of section names.
+	stringReader := bufio.NewReader(strtab.Open())
+	var elfString string
+	for off := uint64(0); off < strtab.Size; off += uint64(len(elfString)) {
+		// off is the offset within the string table.
+		elfString, err = stringReader.ReadString(nullTerminator)
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		// We only need to worry about sections with relocations.
+		if !strings.HasPrefix(elfString, relocSectionPrefix) {
+			if err == io.EOF {
+				break
+			}
+			continue
+		}
+
+		elfEnd := len(elfString)
+		if elfString[elfEnd-1] == nullTerminator {
+			elfEnd--
+		}
+		relocOffset := uint64(len(relocSectionPrefix))
+		secName := elfString[relocOffset:elfEnd]
+		if sec := e.Section(secName); sec != nil {
+			// Offset from start of binary to name inside .strtab
+			globalOffset := strtab.Offset + off + relocOffset
+			stringOffsets[secName] = newString(secName, globalOffset)
+			log.WithField(fieldSymbol, secName).Debugf("Found section with offset %d", globalOffset)
+		}
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	s.data = dataOffsets
+	s.strings = stringOffsets
+	return nil
+}

CODEOWNERS
@@ -74,6 +74,7 @@ pkg/datapath @cilium/bpf
 pkg/datapath/ipcache/ @cilium/ipcache
 pkg/debugdetection @cilium/agent
 pkg/defaults @cilium/agent
+pkg/elf @cilium/bpf
 pkg/endpoint/ @cilium/endpoint
 pkg/endpointmanager/ @cilium/endpoint
 pkg/envoy/ @cilium/proxy

Makefile
@@ -107,6 +107,7 @@ generate-cov:
 	$(QUIET) rm coverage.out coverage-all-tmp.out
 
 unit-tests: start-kvstores
+	$(QUIET) $(MAKE) -C test/bpf/
 	$(QUIET) $(MAKE) -C daemon/ check-bindata
 	$(QUIET) echo "mode: count" > coverage-all-tmp.out
 	$(QUIET) echo "mode: count" > coverage.out

pkg/elf/doc.go
@@ -1,0 +1,17 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package elf provides utility functions for manipulating ELF files
+// on the filesystem.
+package elf

pkg/elf/elf_test.go
@@ -1,0 +1,216 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package elf
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/cilium/cilium/pkg/testutils"
+)
+
+// Hook up gocheck into the "go test" runner.
+type ELFTestSuite struct{}
+
+var (
+	_ = Suite(&ELFTestSuite{})
+
+	elfObjCopy  = "elf-demo-copy.o"
+	baseObjPath = filepath.Join(testutils.CiliumRootDir, "test", "bpf", "elf-demo.o")
+)
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+func hash(path string) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, f); err != nil {
+		return nil, err
+	}
+
+	return hash.Sum(nil), nil
+}
+
+var errDifferentFiles = fmt.Errorf("File hashes differ")
+
+func compareFiles(path1, path2 string) error {
+	h1, err := hash(path1)
+	if err != nil {
+		return err
+	}
+	h2, err := hash(path2)
+	if err != nil {
+		return err
+	}
+
+	if !bytes.Equal(h1, h2) {
+		return errDifferentFiles
+	}
+	return nil
+}
+
+func (s *ELFTestSuite) TestWrite(c *C) {
+	tmpDir, err := ioutil.TempDir("", "cilium_")
+	c.Assert(err, IsNil)
+	defer os.RemoveAll(tmpDir)
+
+	elf, err := Open(baseObjPath)
+	c.Assert(err, IsNil)
+	defer elf.Close()
+
+	validOptions := IsNil
+	notValidOptions := Not(validOptions)
+	type testOption struct {
+		description  string
+		key          string
+		kind         symbolKind
+		intValue     uint32
+		strValue     string
+		elfValid     Checker
+		elfChangeErr error
+	}
+	testOptions := []testOption{
+		{
+			description: "test direct copy",
+			elfValid:    validOptions,
+		},
+		{
+			description:  "test constant substitution 1",
+			key:          "FOO",
+			kind:         symbolUint32,
+			intValue:     42,
+			elfValid:     validOptions,
+			elfChangeErr: errDifferentFiles,
+		},
+		{
+			description:  "test constant substitution 2",
+			key:          "BAR",
+			kind:         symbolUint32,
+			intValue:     42,
+			elfValid:     validOptions,
+			elfChangeErr: errDifferentFiles,
+		},
+		{
+			description:  "test map name substitution name",
+			key:          "test_cilium_calls_4278124286",
+			kind:         symbolString,
+			strValue:     "test_cilium_calls_0000000042",
+			elfValid:     validOptions,
+			elfChangeErr: errDifferentFiles,
+		},
+		{
+			description: "test invalid map name substitution",
+			key:         "test_cilium_calls_4278124286",
+			kind:        symbolString,
+			strValue:    "test_cilium_calls_00",
+			elfValid:    notValidOptions,
+		},
+	}
+
+	for i := 1; i <= 2; i++ {
+		testOptions = append(testOptions, testOption{
+			description:  fmt.Sprintf("test mac substitution %d", i),
+			key:          fmt.Sprintf("LOCAL_MAC_%d", i),
+			kind:         symbolUint32,
+			intValue:     42,
+			elfValid:     validOptions,
+			elfChangeErr: errDifferentFiles,
+		})
+	}
+
+	for i, test := range testOptions {
+		c.Logf("%s", test.description)
+
+		// Create the copy of the ELF with an optional substitution
+		intOptions := make(map[string]uint32)
+		strOptions := make(map[string]string)
+		switch test.kind {
+		case symbolUint32:
+			intOptions[test.key] = test.intValue
+		case symbolString:
+			strOptions[test.key] = test.strValue
+		}
+		objectCopy := filepath.Join(tmpDir, fmt.Sprintf("%d_%s", i, elfObjCopy))
+		err = elf.Write(objectCopy, intOptions, strOptions)
+		c.Assert(err, test.elfValid)
+		if test.elfValid == notValidOptions {
+			continue
+		}
+		err = compareFiles(baseObjPath, objectCopy)
+		c.Assert(err, Equals, test.elfChangeErr)
+
+		// Test that the written ELF matches expectations
+		modifiedElf, err := Open(objectCopy)
+		c.Assert(err, IsNil)
+		defer modifiedElf.Close()
+
+		switch test.kind {
+		case symbolUint32:
+			value, err := modifiedElf.readOption(test.key)
+			c.Assert(err, IsNil)
+			c.Assert(value, Equals, test.intValue)
+		case symbolString:
+			err := modifiedElf.findString(test.strValue)
+			c.Assert(err, IsNil)
+		default:
+			_, err = modifiedElf.readOption("unknown")
+			c.Assert(err, NotNil)
+		}
+		modifiedElf.Close()
+	}
+}
+
+// BenchmarkWriteELF benchmarks writing a very simple elf demo program.
+func BenchmarkWriteELF(b *testing.B) {
+	tmpDir, err := ioutil.TempDir("", "cilium_")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	elf, err := Open(baseObjPath)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer elf.Close()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		intOptions := make(map[string]uint32)
+		strOptions := make(map[string]string)
+
+		objectCopy := filepath.Join(tmpDir, fmt.Sprintf("%d_%s", i, elfObjCopy))
+		if err = elf.Write(objectCopy, intOptions, strOptions); err != nil {
+			b.Fatal(err)
+		}
+	}
+}

pkg/elf/log.go
@@ -1,0 +1,31 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package elf
+
+import (
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
+
+var (
+	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "elf")
+
+	fieldName   = "name"
+	fieldSize   = "size"
+	fieldSymbol = "symbol"
+
+	srcPath = "template-path"
+	dstPath = "new-elf-path"
+)

test/bpf/Makefile
@@ -1,6 +1,6 @@
 include ../../Makefile.defs
 
-FLAGS := -I../../bpf/include -I. -D__NR_CPUS__=$(shell nproc) -O2
+FLAGS := -I../../bpf/ -I../../bpf/include -I. -D__NR_CPUS__=$(shell nproc) -O2
 BPF_CC_FLAGS :=  ${FLAGS} -target bpf -emit-llvm
 BPF_LLC_FLAGS   := -march=bpf -mcpu=probe -filetype=obj
 
@@ -9,7 +9,8 @@ LIB := $(shell find ../../bpf/ -name '*.h')
 CLANG ?= $(QUIET) clang
 LLC ?= llc
 
-TARGETS := perf-event-test bpf-event-test.o unit-test
+BPF_TARGETS := bpf-event-test.o elf-demo.o
+TARGETS := $(BPF_TARGETS) perf-event-test unit-test
 all: $(TARGETS)
 
 perf-event-test: perf-event-test.go
@@ -17,6 +18,10 @@ perf-event-test: perf-event-test.go
 	$(QUIET)$(GO) build $(GOBUILD) -o $@ $<
 
 bpf-event-test.o: bpf-event-test.c
+	@$(ECHO_CC)
+	$(CLANG) ${BPF_CC_FLAGS} -c $< -o - | $(LLC) ${BPF_LLC_FLAGS} -o $@
+
+elf-demo.o: elf-demo.c
 	@$(ECHO_CC)
 	$(CLANG) ${BPF_CC_FLAGS} -c $< -o - | $(LLC) ${BPF_LLC_FLAGS} -o $@
 

commit 4abc32e6b16449d670a6fdee8913f7afadb9e999

elf: Don't assume data symbols are 4-bytes long
The size of data symbols is currently hardcoded to 4 bytes when we parse
them in elf.extractFrom(). Instead, we should retrieve the actual size
from the object file and skip any symbol that doesn't have the expected
size (our symbol substitution logic currently assumes 4-bytes long
symbols for data).

Fixes: 763b290a01f ("Add ELF package for templating constants")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/elf/elf.go
@@ -139,10 +139,11 @@ processSymbols:
 		var value []byte
 		switch symbol.kind {
 		case symbolUint32:
-			v, exists := intOptions[symbol.name]
-			if exists {
-				value = make([]byte, unsafe.Sizeof(v))
-				elf.metadata.ByteOrder.PutUint32(value, v)
+			if symbol.size == uint64(unsafe.Sizeof(uint32(0))) {
+				if v, exists := intOptions[symbol.name]; exists {
+					value = make([]byte, symbol.size)
+					elf.metadata.ByteOrder.PutUint32(value, v)
+				}
 			}
 
 		case symbolString:

pkg/elf/symbols.go
@@ -12,7 +12,6 @@ import (
 	"io"
 	"sort"
 	"strings"
-	"unsafe"
 )
 
 const (
@@ -67,8 +66,7 @@ func newSymbol(name string, kind symbolKind, offset, size uint64) symbol {
 	}
 }
 
-func newVariable(name string, offset uint64) symbol {
-	size := uint64(unsafe.Sizeof(symbolUint32))
+func newVariable(name string, offset, size uint64) symbol {
 	return newSymbol(name, symbolUint32, offset, size)
 }
 
@@ -179,7 +177,7 @@ func (s *symbols) extractFrom(e *elf.File) error {
 		case section.Name == dataSection:
 			// Offset from start of binary to variable inside .data
 			offset := section.Offset + sym.Value
-			dataOffsets[sym.Name] = newVariable(sym.Name, offset)
+			dataOffsets[sym.Name] = newVariable(sym.Name, offset, sym.Size)
 			log.WithField(fieldSymbol, sym.Name).Debugf("Found variable with offset %d", offset)
 
 		// Find the absolute offset to the map's entry in .strtab.