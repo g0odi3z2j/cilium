g0odi3z2j/cilium

commit 080c73ed41830b9ee5e811f8a1e2c99e5fda3d9b

release: Generate helm values docs
Generate the helm values docs during the release process.

Signed-off-by: Joe Stringer <joe@cilium.io>

contrib/release/post-release.sh
@@ -65,10 +65,11 @@ main() {
 
     git checkout -b pr/$version-digests $version
     ${DIR}/pull-docker-manifests.sh "$@"
+    logrun make -C Documentation update-helm-values
     logecho
     logecho "Check that the following changes look correct:"
     # TODO: Make this less interactive when we have used it enough
-    git add --patch install/kubernetes
+    git add --patch install/kubernetes Documentation/
     git commit -se -m "install: Update image digests for $version" -m "Generated from $1." -m "$(cat digest-$version.txt)"
     echo "Create PR for v$branch with these changes"
     if ! common::askyorn ; then

contrib/release/start-release.sh
@@ -88,6 +88,7 @@ main() {
     echo $ersion > VERSION
     sed -i 's/"[^"]*"/""/g' install/kubernetes/Makefile.digests
     logrun make -C install/kubernetes all USE_DIGESTS=false
+    logrun make -C Documentation update-helm-values
     logrun make update-authors
     if ! version_is_prerelease "$version"; then
         old_proj=$(grep "projects" $ACTS_YAML | sed "$PROJECTS_REGEX")

commit b87bf646b83bda6c6c4bb7e33d8a799b87735962

contrib: Fix release script helm value generation
Commit 080c73ed4183 ("release: Generate helm values docs") broke the
release scripts for branches v1.10 and earlier, as it introduced a
dependency on a new Makefile target that has not been backported.
Grep for the dependency and skip it silently if it's not available.

Fixes: 080c73ed4183 ("release: Generate helm values docs")
Signed-off-by: Joe Stringer <joe@cilium.io>

contrib/release/post-release.sh
@@ -65,7 +65,9 @@ main() {
 
     git checkout -b pr/$version-digests $version
     ${DIR}/pull-docker-manifests.sh "$@"
-    logrun make -C Documentation update-helm-values
+    if grep -q update-helm-values Documentation/Makefile; then
+        logrun make -C Documentation update-helm-values
+    fi
     logecho
     logecho "Check that the following changes look correct:"
     # TODO: Make this less interactive when we have used it enough

contrib/release/start-release.sh
@@ -88,7 +88,9 @@ main() {
     echo $ersion > VERSION
     sed -i 's/"[^"]*"/""/g' install/kubernetes/Makefile.digests
     logrun make -C install/kubernetes all USE_DIGESTS=false
-    logrun make -C Documentation update-helm-values
+    if grep -q update-helm-values Documentation/Makefile; then
+        logrun make -C Documentation update-helm-values
+    fi
     logrun make update-authors
     if ! version_is_prerelease "$version"; then
         old_proj=$(grep "projects" $ACTS_YAML | sed "$PROJECTS_REGEX")