g0odi3z2j/cilium

commit e9317d3fcd68381bfd0140eb109e1387d9c00058

byteorder: Simplify byte order functions
The previous HostToNetwork and NetworkToHost functions used a
combination of the unsafe package, reflection, unchecked type
assertions, and panics.

Replace them with simple functions controlled by build tags.

Signed-off-by: Tom Payne <tom@isovalent.com>

pkg/byteorder/byteorder_littleendian.go
@@ -1,0 +1,34 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//+build 386 amd64 arm arm64 mips64le ppc64le riscv64 wasm
+
+package byteorder
+
+import "encoding/binary"
+
+var Native binary.ByteOrder = binary.LittleEndian
+
+func HostToNetwork16(u uint16) uint16 { return swap16(u) }
+func HostToNetwork32(u uint32) uint32 { return swap32(u) }
+func NetworkToHost16(u uint16) uint16 { return swap16(u) }
+func NetworkToHost32(u uint32) uint32 { return swap32(u) }
+
+func swap16(u uint16) uint16 {
+	return (u&0xff00)>>8 | (u&0xff)<<8
+}
+
+func swap32(u uint32) uint32 {
+	return (u&0xff000000)>>24 | (u&0xff0000)>>8 | (u&0xff00)<<8 | (u&0xff)<<24
+}

bpf/tests/prog_test/prog_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Authors of Cilium
+// Copyright 2020-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -161,8 +161,8 @@ func testMap(spec *ebpf.Collection) error {
 			TupleKey4: tuple.TupleKey4{
 				SourceAddr: types.IPv4{1, 1, 1, 1},
 				DestAddr:   types.IPv4{1, 1, 1, 2},
-				SourcePort: byteorder.HostToNetwork(uint16(1001)).(uint16),
-				DestPort:   byteorder.HostToNetwork(uint16(1002)).(uint16),
+				SourcePort: byteorder.HostToNetwork16(1001),
+				DestPort:   byteorder.HostToNetwork16(1002),
 				NextHeader: 0,
 				Flags:      0,
 			},
@@ -429,8 +429,8 @@ func testCt4Rst(spec *ebpf.Collection) error {
 			TupleKey4: tuple.TupleKey4{
 				SourceAddr: types.IPv4{10, 3, 0, 20},
 				DestAddr:   types.IPv4{10, 3, 0, 10},
-				SourcePort: byteorder.HostToNetwork(uint16(3010)).(uint16),
-				DestPort:   byteorder.HostToNetwork(uint16(3020)).(uint16),
+				SourcePort: byteorder.HostToNetwork16(3010),
+				DestPort:   byteorder.HostToNetwork16(3020),
 				NextHeader: 6,
 				Flags:      0,
 			},

cilium/cmd/bpf_ct_list_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Authors of Cilium
+// Copyright 2020-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -45,8 +45,8 @@ var (
 		TupleKey4: tuple.TupleKey4{
 			DestAddr:   types.IPv4{10, 10, 10, 1},
 			SourceAddr: types.IPv4{10, 10, 10, 2},
-			DestPort:   byteorder.HostToNetwork(uint16(80)).(uint16),
-			SourcePort: byteorder.HostToNetwork(uint16(13579)).(uint16),
+			DestPort:   byteorder.HostToNetwork16(80),
+			SourcePort: byteorder.HostToNetwork16(13579),
 			NextHeader: 6,
 			Flags:      123,
 		},
@@ -55,8 +55,8 @@ var (
 		TupleKey6: tuple.TupleKey6{
 			DestAddr:   types.IPv6{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
 			SourceAddr: types.IPv6{1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 121, 98, 219, 61},
-			DestPort:   byteorder.HostToNetwork(uint16(443)).(uint16),
-			SourcePort: byteorder.HostToNetwork(uint16(7878)).(uint16),
+			DestPort:   byteorder.HostToNetwork16(443),
+			SourcePort: byteorder.HostToNetwork16(7878),
 			NextHeader: 17,
 			Flags:      31,
 		},
@@ -68,7 +68,7 @@ var (
 		TxBytes:          2048,
 		Lifetime:         12345,
 		Flags:            3,
-		RevNAT:           byteorder.HostToNetwork(uint16(27)).(uint16),
+		RevNAT:           byteorder.HostToNetwork16(27),
 		TxFlagsSeen:      88,
 		RxFlagsSeen:      99,
 		SourceSecurityID: 6789,

cilium/cmd/bpf_nat_list_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Authors of Cilium
+// Copyright 2020-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -39,8 +39,8 @@ var (
 			TupleKey4: tuple.TupleKey4{
 				DestAddr:   types.IPv4{10, 10, 10, 1},
 				SourceAddr: types.IPv4{10, 10, 10, 2},
-				DestPort:   byteorder.HostToNetwork(uint16(80)).(uint16),
-				SourcePort: byteorder.HostToNetwork(uint16(13579)).(uint16),
+				DestPort:   byteorder.HostToNetwork16(80),
+				SourcePort: byteorder.HostToNetwork16(13579),
 				NextHeader: 6,
 				Flags:      123,
 			},
@@ -51,8 +51,8 @@ var (
 			TupleKey6: tuple.TupleKey6{
 				DestAddr:   types.IPv6{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
 				SourceAddr: types.IPv6{1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 121, 98, 219, 61},
-				DestPort:   byteorder.HostToNetwork(uint16(443)).(uint16),
-				SourcePort: byteorder.HostToNetwork(uint16(7878)).(uint16),
+				DestPort:   byteorder.HostToNetwork16(443),
+				SourcePort: byteorder.HostToNetwork16(7878),
 				NextHeader: 17,
 				Flags:      31,
 			},
@@ -62,13 +62,13 @@ var (
 		Created:   12345,
 		HostLocal: 6789,
 		Addr:      types.IPv4{10, 10, 10, 3},
-		Port:      byteorder.HostToNetwork(uint16(53)).(uint16),
+		Port:      byteorder.HostToNetwork16(53),
 	}
 	natValue6 = nat.NatEntry6{
 		Created:   12345,
 		HostLocal: 6789,
 		Addr:      types.IPv6{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53},
-		Port:      byteorder.HostToNetwork(uint16(53)).(uint16),
+		Port:      byteorder.HostToNetwork16(53),
 	}
 )
 

cilium/cmd/bpf_policy_get.go
@@ -1,4 +1,4 @@
-// Copyright 2017-2020 Authors of Cilium
+// Copyright 2017-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -159,13 +159,13 @@ func formatMap(w io.Writer, statsMap []policymap.PolicyEntryDump) {
 		trafficDirectionString := trafficDirection.String()
 		port := models.PortProtocolANY
 		if stat.Key.DestPort != 0 {
-			dport := byteorder.NetworkToHost(stat.Key.DestPort).(uint16)
+			dport := byteorder.NetworkToHost16(stat.Key.DestPort)
 			proto := u8proto.U8proto(stat.Key.Nexthdr)
 			port = fmt.Sprintf("%d/%s", dport, proto.String())
 		}
 		proxyPort := "NONE"
 		if stat.ProxyPort != 0 {
-			proxyPort = strconv.FormatUint(uint64(byteorder.NetworkToHost(stat.ProxyPort).(uint16)), 10)
+			proxyPort = strconv.FormatUint(uint64(byteorder.NetworkToHost16(stat.ProxyPort)), 10)
 		}
 		var policyStr string
 		if policymap.PolicyEntryFlags(stat.Flags).IsDeny() {

pkg/byteorder/byteorder.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2017-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,41 +18,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"reflect"
-	"unsafe"
 )
-
-// Native is set to the host ByteOrder type. This should normally be either
-// BigEndian or LittleEndian.
-var Native = getNativeEndian()
-var nativeEndian binary.ByteOrder
-
-func getNativeEndian() binary.ByteOrder {
-	if nativeEndian == nil {
-		var x uint32 = 0x01020304
-		if *(*byte)(unsafe.Pointer(&x)) == 0x01 {
-			nativeEndian = binary.BigEndian
-		} else {
-			nativeEndian = binary.LittleEndian
-		}
-	}
-	return nativeEndian
-}
-
-// Byte swap a 16 bit value if we aren't big endian
-func swap16(i uint16) uint16 {
-	if Native == binary.BigEndian {
-		return i
-	}
-	return (i&0xff00)>>8 | (i&0xff)<<8
-}
-
-// Byte swap a 32 bit value if aren't big endian
-func swap32(i uint32) uint32 {
-	if Native == binary.BigEndian {
-		return i
-	}
-	return (i&0xff000000)>>24 | (i&0xff0000)>>8 | (i&0xff00)<<8 | (i&0xff)<<24
-}
 
 // reverse returns a reversed slice of b.
 func reverse(b []byte) []byte {
@@ -64,30 +30,6 @@ func reverse(b []byte) []byte {
 	}
 
 	return c
-}
-
-// HostToNetwork converts b to the networking byte order.
-func HostToNetwork(b interface{}) interface{} {
-	switch bt := b.(type) {
-	case uint16:
-		return swap16(bt)
-	case uint32:
-		return swap32(bt)
-	default:
-		panic(unsupported(b))
-	}
-}
-
-// NetworkToHost converts n to host byte order.
-func NetworkToHost(n interface{}) interface{} {
-	switch nt := n.(type) {
-	case uint16:
-		return swap16(nt)
-	case uint32:
-		return swap32(nt)
-	default:
-		panic(unsupported(n))
-	}
 }
 
 // HostToNetworkSlice converts b to the networking byte order.

pkg/byteorder/byteorder_bigendian.go
@@ -1,0 +1,24 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//+build armbe arm64be mips mips64 ppc64
+
+package byteorder
+
+var Native binary.ByteOrder = binary.BigEndian
+
+func HostToNetwork16(u uint16) uint16 { return u }
+func HostToNetwork32(u uint32) uint32 { return u }
+func NetworkToHost16(u uint16) uint16 { return u }
+func NetworkToHost32(u uint32) uint32 { return u }

pkg/byteorder/byteorder_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2017-2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 package byteorder
 
 import (
+	"encoding/binary"
 	"net"
 	"reflect"
 	"testing"
@@ -52,11 +53,14 @@ func (b *ByteorderSuite) TestHostToNetworkPutShort(c *C) {
 }
 
 func (b *ByteorderSuite) TestHostToNetwork(c *C) {
-	c.Assert(HostToNetwork(uint16(0xAABB)), Equals, uint16(0xBBAA),
-		Commentf("TestHostToNetwork failed: HostToNetwork(0xAABB) != 0xBBAA"))
-
-	c.Assert(HostToNetwork(uint32(0xAABBCCDD)), Equals, uint32(0xDDCCBBAA),
-		Commentf("TestHostToNetwork failed: HostToNetwork(0xAABBCCDD) != 0xDDCCBBAA"))
+	switch Native {
+	case binary.LittleEndian:
+		c.Assert(HostToNetwork16(0xAABB), Equals, uint16(0xBBAA))
+		c.Assert(HostToNetwork32(0xAABBCCDD), Equals, uint32(0xDDCCBBAA))
+	case binary.BigEndian:
+		c.Assert(HostToNetwork16(0xAABB), Equals, uint16(0xAABB))
+		c.Assert(HostToNetwork32(0xAABBCCDD), Equals, uint32(0xAABBCCDD))
+	}
 }
 
 func (b *ByteorderSuite) TestHostSliceToNetworkU32(c *C) {

commit 8ceebcc58d523c62849f820ed52aa9e87f6f33c9

byteorder: use native instructions in host/network order conversion
In the current implementation of HostToNetwork{16,32} and
NetworkToHost{16,32} for big endian architectures the Go compiler isn't
able to convert these operations into native assembly instructions, e.g.
BSWAP on x86_64.

Use ReverseBytes{16,32} from the math/bits stdlib package to implement
these functions which allows the Go compiler to emit the respective
assembly instructions which generally yield better performance.

The Go toolchain test suite checks that the correct native assembly
instructions are generated for the respective math/bits functions:
https://github.com/golang/go/blob/16d6a5233a183be7264295c66167d35c689f9372/test/codegen/mathbits.go#L194-L208

Example output of `go tool compile -S byteorder_littleendian.go` on
x86_64 for the HostToNetwork{16,32} functions below.
NetworkToHost{16,32} look the same. Note the ROLW and BSWAPL
instructions emitted after this change.

Before:

```
"".HostToNetwork16 STEXT nosplit size=20 args=0x8 locals=0x0 funcid=0x0
        0x0000 00000 (byteorder_littleendian.go:13)     TEXT    "".HostToNetwork16(SB), NOSPLIT|ABIInternal, $0-8
        0x0000 00000 (byteorder_littleendian.go:13)     FUNCDATA        $0, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
        0x0000 00000 (byteorder_littleendian.go:13)     FUNCDATA        $1, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
        0x0000 00000 (byteorder_littleendian.go:13)     FUNCDATA        $5, "".HostToNetwork16.arginfo1(SB)
        0x0000 00000 (<unknown line number>)    NOP
        0x0000 00000 (byteorder_littleendian.go:13)     MOVL    AX, CX
        0x0002 00002 (byteorder_littleendian.go:19)     ANDL    $-256, AX
        0x0007 00007 (byteorder_littleendian.go:19)     SHRW    $8, AX
        0x000b 00011 (byteorder_littleendian.go:19)     MOVBLZX CL, CX
        0x000e 00014 (byteorder_littleendian.go:19)     SHLL    $8, CX
        0x0011 00017 (byteorder_littleendian.go:19)     ORL     CX, AX
        0x0013 00019 (byteorder_littleendian.go:13)     RET
        0x0000 89 c1 25 00 ff ff ff 66 c1 e8 08 0f b6 c9 c1 e1  ..%....f........
        0x0010 08 09 c8 c3                                      ....
"".HostToNetwork32 STEXT nosplit size=45 args=0x8 locals=0x0 funcid=0x0
        0x0000 00000 (byteorder_littleendian.go:14)     TEXT    "".HostToNetwork32(SB), NOSPLIT|ABIInternal, $0-8
        0x0000 00000 (byteorder_littleendian.go:14)     FUNCDATA        $0, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
        0x0000 00000 (byteorder_littleendian.go:14)     FUNCDATA        $1, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
        0x0000 00000 (byteorder_littleendian.go:14)     FUNCDATA        $5, "".HostToNetwork32.arginfo1(SB)
        0x0000 00000 (<unknown line number>)    NOP
        0x0000 00000 (byteorder_littleendian.go:14)     MOVL    AX, CX
        0x0002 00002 (byteorder_littleendian.go:23)     ANDL    $-16777216, AX
        0x0007 00007 (byteorder_littleendian.go:23)     SHRL    $24, AX
        0x000a 00010 (byteorder_littleendian.go:23)     MOVL    CX, DX
        0x000c 00012 (byteorder_littleendian.go:23)     ANDL    $16711680, CX
        0x0012 00018 (byteorder_littleendian.go:23)     SHRL    $8, CX
        0x0015 00021 (byteorder_littleendian.go:23)     ORL     CX, AX
        0x0017 00023 (byteorder_littleendian.go:23)     MOVL    DX, CX
        0x0019 00025 (byteorder_littleendian.go:23)     ANDL    $65280, DX
        0x001f 00031 (byteorder_littleendian.go:23)     SHLL    $8, DX
        0x0022 00034 (byteorder_littleendian.go:23)     ORL     DX, AX
        0x0024 00036 (byteorder_littleendian.go:23)     MOVBLZX CL, CX
        0x0027 00039 (byteorder_littleendian.go:23)     SHLL    $24, CX
        0x002a 00042 (byteorder_littleendian.go:23)     ORL     CX, AX
        0x002c 00044 (byteorder_littleendian.go:14)     RET
        0x0000 89 c1 25 00 00 00 ff c1 e8 18 89 ca 81 e1 00 00  ..%.............
        0x0010 ff 00 c1 e9 08 09 c8 89 d1 81 e2 00 ff 00 00 c1  ................
        0x0020 e2 08 09 d0 0f b6 c9 c1 e1 18 09 c8 c3           .............
```

After:

```
"".HostToNetwork16 STEXT nosplit size=5 args=0x8 locals=0x0 funcid=0x0
        0x0000 00000 (byteorder_littleendian.go:16)     TEXT    "".HostToNetwork16(SB), NOSPLIT|ABIInternal, $0-8
        0x0000 00000 (byteorder_littleendian.go:16)     FUNCDATA        $0, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
        0x0000 00000 (byteorder_littleendian.go:16)     FUNCDATA        $1, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
        0x0000 00000 (byteorder_littleendian.go:16)     FUNCDATA        $5, "".HostToNetwork16.arginfo1(SB)
        0x0000 00000 (<unknown line number>)    NOP
        0x0000 00000 (byteorder_littleendian.go:16)     ROLW    $8, AX
        0x0004 00004 (byteorder_littleendian.go:16)     RET
        0x0000 66 c1 c0 08 c3                                   f....
"".HostToNetwork32 STEXT nosplit size=3 args=0x8 locals=0x0 funcid=0x0
        0x0000 00000 (byteorder_littleendian.go:17)     TEXT    "".HostToNetwork32(SB), NOSPLIT|ABIInternal, $0-8
        0x0000 00000 (byteorder_littleendian.go:17)     FUNCDATA        $0, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
        0x0000 00000 (byteorder_littleendian.go:17)     FUNCDATA        $1, gclocals·33cdeccccebe80329f1fdbee7f5874cb(SB)
        0x0000 00000 (byteorder_littleendian.go:17)     FUNCDATA        $5, "".HostToNetwork32.arginfo1(SB)
        0x0000 00000 (byteorder_littleendian.go:17)     BSWAPL  AX
        0x0002 00002 (byteorder_littleendian.go:17)     RET
        0x0000 0f c8 c3                                         ...
```

Fixes: e9317d3fcd68 ("byteorder: Simplify byte order functions")

pkg/byteorder/byteorder_littleendian.go
@@ -6,19 +6,14 @@
 
 package byteorder
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"math/bits"
+)
 
 var Native binary.ByteOrder = binary.LittleEndian
 
-func HostToNetwork16(u uint16) uint16 { return swap16(u) }
-func HostToNetwork32(u uint32) uint32 { return swap32(u) }
-func NetworkToHost16(u uint16) uint16 { return swap16(u) }
-func NetworkToHost32(u uint32) uint32 { return swap32(u) }
-
-func swap16(u uint16) uint16 {
-	return (u&0xff00)>>8 | (u&0xff)<<8
-}
-
-func swap32(u uint32) uint32 {
-	return (u&0xff000000)>>24 | (u&0xff0000)>>8 | (u&0xff00)<<8 | (u&0xff)<<24
-}
+func HostToNetwork16(u uint16) uint16 { return bits.ReverseBytes16(u) }
+func HostToNetwork32(u uint32) uint32 { return bits.ReverseBytes32(u) }
+func NetworkToHost16(u uint16) uint16 { return bits.ReverseBytes16(u) }
+func NetworkToHost32(u uint32) uint32 { return bits.ReverseBytes32(u) }