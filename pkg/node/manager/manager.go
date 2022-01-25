g0odi3z2j/cilium

commit 090f9d8b3078fb9441d6bf46bead347df29cd27f

node: Don't skip masquerading for External node IPs
Commit 0a04790dbf4 ("iptables: Don't masquerade traffic to cluster
nodes") introduced ipsets in our iptables rules, to skip masquerading
traffic to cluster nodes when iptables-based masquerading is used.
BPF-based masquerading already implemented this logic.

In practice, traffic to both Internal and External node IPs would skip
masquerading. However, Kubernetes doesn't state that External Node IPs
are routable within the cluster. Masquerading may thus be required for
External Node IPs, as reported by several users.

Hence, this commit changes the logic to only skip masquerading in
iptables for Internal node IPs. This commit doesn't affect the BPF-based
masquerading logic, as unfortunately we can't easily distinguish between
Internal and External node IPs in our datapath today (both are
identified as host or remote-node).

Fixes: 0a04790dbf4 ("iptables: Don't masquerade traffic to cluster nodes")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/node/manager/manager.go
@@ -412,7 +412,7 @@ func (m *Manager) NodeUpdated(n nodeTypes.Node) {
 		}
 
 		if option.Config.IptablesMasqueradingEnabled() &&
-			address.Type != addressing.NodeCiliumInternalIP {
+			address.Type == addressing.NodeInternalIP {
 			iptables.AddToNodeIpset(address.IP)
 		}
 
@@ -596,7 +596,7 @@ func (m *Manager) NodeDeleted(n nodeTypes.Node) {
 
 	for _, address := range entry.node.IPAddresses {
 		if option.Config.IptablesMasqueradingEnabled() &&
-			address.Type != addressing.NodeCiliumInternalIP {
+			address.Type != addressing.NodeInternalIP {
 			iptables.RemoveFromNodeIpset(address.IP)
 		}
 

commit dabc567245f6076508c62b6c0b5e7c675e13600b

node: Fix bug where node ipsets are never cleaned
Due to a typo in the condition for the removal of IP addresses from the
node ipset in commit 090f9d8b3 ("node: Don't skip masquerading for
External node IPs"), the ipsets would have never been cleaned up. This
commit fixes it.

Fixes: 090f9d8b3 ("node: Don't skip masquerading for External node IPs")
Reported-by: Kornilios Kourtis <kornilios@isovalent.com>
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/node/manager/manager.go
@@ -596,7 +596,7 @@ func (m *Manager) NodeDeleted(n nodeTypes.Node) {
 
 	for _, address := range entry.node.IPAddresses {
 		if option.Config.IptablesMasqueradingEnabled() &&
-			address.Type != addressing.NodeInternalIP {
+			address.Type == addressing.NodeInternalIP {
 			iptables.RemoveFromNodeIpset(address.IP)
 		}
 