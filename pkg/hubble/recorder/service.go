g0odi3z2j/cilium

commit 7c7bbff7f7facbc7449f5fb87b7a2d82c940d323

hubble: Hubble node_name field should contain cluster name
Description:
This field node_name currently only contains the node name,
without it's cluster-mesh prefix.  If the cluster name is not
the default cluster name then it should show the node_name along
with prefixed cluster name.

Its should be shown as below:
cluster<id>/<node_name>
Eg: cluster1/node-ip.compute.internal

$ hubble observe --last 2 -o compact --print-node-name
Nov  3 11:40:45.407 [cluster5/ip-172-0-51-175.us-west-2.compute.internal]: [...]
Nov  3 11:40:46.554 [cluster7/ip-172-0-121-242.us-west-2.compute.internal ]: [...]

Tested Output:
vagrant@k8s1:~/go/src/github.com/cilium/cilium$ hubble observe --print-node-name
TIMESTAMP             NODE        SOURCE              DESTINATION        TYPE        VERDICT     SUMMARY
May  3 15:47:10.176   maha/k8s1   10.11.1.226:51792   10.11.0.190:4240   to-stack    FORWARDED   TCP Flags: SYN
May  3 15:47:10.177   maha/k8s1   10.11.1.226:51792   10.11.0.190:4240   from-host   FORWARDED   TCP Flags: SYN
May  3 15:47:10.177   maha/k8s1   10.11.1.226:51792   10.11.0.190:4240   to-stack    FORWARDED   TCP Flags: SYN

$ hubble observe --last 2 -o compact --print-node-name
May  3 15:50:18.290 [maha/k8s1]: [fd00::1:8665]:52188 -> [fd00::ebfa]:4240 to-endpoint FORWARDED (TCP Flags: ACK)
May  3 15:50:22.638 [maha/k8s1]: fe80::436:9bff:fedc:1763 -> ff02::2 from-endpoint FORWARDED (ICMPv6 RouterSolicitation)

Fixes: #13872

Signed-off-by: Maddy007-maha <mahadev.panchal@accuknox.com>

pkg/hubble/recorder/service.go
@@ -124,7 +124,7 @@ func (s *Service) Record(stream recorderpb.Recorder_RecordServer) error {
 	}
 
 	err = stream.Send(&recorderpb.RecordResponse{
-		NodeName: nodeTypes.GetName(),
+		NodeName: nodeTypes.GetAbsoluteNodeName(),
 		Time:     timestamppb.Now(),
 		ResponseType: &recorderpb.RecordResponse_Stopped{
 			Stopped: resp,
@@ -144,7 +144,7 @@ func createPcapFile(basedir, prefix string) (f *os.File, filePath string, err er
 	for {
 		startTime := time.Now().Unix()
 		random := rand.Uint32()
-		nodeName := nodeTypes.GetName()
+		nodeName := nodeTypes.GetAbsoluteNodeName()
 		name := fmt.Sprintf("%s_%d_%d_%s.pcap", prefix, startTime, random, nodeName)
 		filePath = path.Join(basedir, name)
 		f, err = os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
@@ -313,7 +313,7 @@ func (s *Service) watchRecording(ctx context.Context, h *sink.Handle, stream rec
 	for {
 		stats := h.Stats()
 		err := stream.Send(&recorderpb.RecordResponse{
-			NodeName: nodeTypes.GetName(),
+			NodeName: nodeTypes.GetAbsoluteNodeName(),
 			Time:     timestamppb.Now(),
 			ResponseType: &recorderpb.RecordResponse_Running{
 				Running: &recorderpb.RecordingRunningResponse{

pkg/hubble/monitor/consumer.go
@@ -51,7 +51,7 @@ func NewConsumer(observer Observer) monitorConsumer.MonitorConsumer {
 func (c *consumer) sendNumLostEvents() {
 	numEventsLostNotification := &observerTypes.MonitorEvent{
 		Timestamp: time.Now(),
-		NodeName:  nodeTypes.GetName(),
+		NodeName:  nodeTypes.GetAbsoluteNodeName(),
 		Payload: &observerTypes.LostEvent{
 			Source:        observerTypes.LostEventSourceEventsQueue,
 			NumLostEvents: c.numEventsLost,
@@ -90,7 +90,7 @@ func (c *consumer) sendEvent(event *observerTypes.MonitorEvent) {
 func (c *consumer) NotifyAgentEvent(typ int, message interface{}) {
 	c.sendEvent(&observerTypes.MonitorEvent{
 		Timestamp: time.Now(),
-		NodeName:  nodeTypes.GetName(),
+		NodeName:  nodeTypes.GetAbsoluteNodeName(),
 		Payload: &observerTypes.AgentEvent{
 			Type:    typ,
 			Message: message,
@@ -102,7 +102,7 @@ func (c *consumer) NotifyAgentEvent(typ int, message interface{}) {
 func (c *consumer) NotifyPerfEvent(data []byte, cpu int) {
 	c.sendEvent(&observerTypes.MonitorEvent{
 		Timestamp: time.Now(),
-		NodeName:  nodeTypes.GetName(),
+		NodeName:  nodeTypes.GetAbsoluteNodeName(),
 		Payload: &observerTypes.PerfEvent{
 			Data: data,
 			CPU:  cpu,
@@ -114,7 +114,7 @@ func (c *consumer) NotifyPerfEvent(data []byte, cpu int) {
 func (c *consumer) NotifyPerfEventLost(numLostEvents uint64, cpu int) {
 	c.sendEvent(&observerTypes.MonitorEvent{
 		Timestamp: time.Now(),
-		NodeName:  nodeTypes.GetName(),
+		NodeName:  nodeTypes.GetAbsoluteNodeName(),
 		Payload: &observerTypes.LostEvent{
 			Source:        observerTypes.LostEventSourcePerfRingBuffer,
 			NumLostEvents: numLostEvents,

pkg/hubble/observer/local_observer.go
@@ -305,7 +305,7 @@ nextEvent:
 		case *flowpb.LostEvent:
 			resp = &observerpb.GetFlowsResponse{
 				Time:     e.Timestamp,
-				NodeName: nodeTypes.GetName(),
+				NodeName: nodeTypes.GetAbsoluteNodeName(),
 				ResponseTypes: &observerpb.GetFlowsResponse_LostEvents{
 					LostEvents: ev,
 				},
@@ -383,7 +383,7 @@ func (s *LocalObserverServer) GetAgentEvents(
 			eventsReader.eventCount++
 			resp := &observerpb.GetAgentEventsResponse{
 				Time:       e.Timestamp,
-				NodeName:   nodeTypes.GetName(),
+				NodeName:   nodeTypes.GetAbsoluteNodeName(),
 				AgentEvent: ev,
 			}
 			err = server.Send(resp)
@@ -444,7 +444,7 @@ func (s *LocalObserverServer) GetDebugEvents(
 			eventsReader.eventCount++
 			resp := &observerpb.GetDebugEventsResponse{
 				Time:       e.Timestamp,
-				NodeName:   nodeTypes.GetName(),
+				NodeName:   nodeTypes.GetAbsoluteNodeName(),
 				DebugEvent: ev,
 			}
 			err = server.Send(resp)

pkg/hubble/relay/observer/observer.go
@@ -136,7 +136,7 @@ func nodeStatusError(err error, nodeNames ...string) *observerpb.GetFlowsRespons
 	}
 
 	return &observerpb.GetFlowsResponse{
-		NodeName: nodeTypes.GetName(),
+		NodeName: nodeTypes.GetAbsoluteNodeName(),
 		Time:     timestamppb.New(time.Now()),
 		ResponseTypes: &observerpb.GetFlowsResponse_NodeStatus{
 			NodeStatus: &relaypb.NodeStatusEvent{
@@ -150,7 +150,7 @@ func nodeStatusError(err error, nodeNames ...string) *observerpb.GetFlowsRespons
 
 func nodeStatusEvent(state relaypb.NodeState, nodeNames ...string) *observerpb.GetFlowsResponse {
 	return &observerpb.GetFlowsResponse{
-		NodeName: nodeTypes.GetName(),
+		NodeName: nodeTypes.GetAbsoluteNodeName(),
 		Time:     timestamppb.New(time.Now()),
 		ResponseTypes: &observerpb.GetFlowsResponse_NodeStatus{
 			NodeStatus: &relaypb.NodeStatusEvent{

pkg/node/types/nodename.go
@@ -17,8 +17,10 @@ package types
 import (
 	"os"
 
+	"github.com/cilium/cilium/pkg/defaults"
 	k8sConsts "github.com/cilium/cilium/pkg/k8s/constants"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/option"
 )
 
 var (
@@ -40,6 +42,18 @@ func SetName(name string) {
 // resort is hardcoded to "localhost".
 func GetName() string {
 	return nodeName
+}
+
+// GetAbsoluteNodeName returns the absolute node name combined of both
+// (prefixed)cluster name and the local node name in case of
+// clustered environments otherwise returns the name of the local node.
+func GetAbsoluteNodeName() string {
+	if option.Config.ClusterName != "" &&
+		option.Config.ClusterName != defaults.ClusterName {
+		return option.Config.ClusterName + "/" + nodeName
+	} else {
+		return nodeName
+	}
 }
 
 func init() {

commit 1081cbb8ade0c560faa78460aa3f631ff42e98d8

hubble/recorder: Sanitize pcap filename
This removes any special characters from the generated pcap filename.

This fixes a bug where we accidentally added a slash to the filename
when we added support for the cluster name. This broke file creation, as
file names cannot contain slashes.

Fixes: 7c7bbff7f7fa ("hubble: Hubble node_name field should contain cluster name")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/hubble/recorder/service.go
@@ -213,6 +213,8 @@ func (s *Service) Record(stream recorderpb.Recorder_RecordServer) error {
 
 const fileExistsRetries = 100
 
+var allowedFileChars = regexp.MustCompile("[^a-zA-Z0-9_.-]")
+
 func createPcapFile(basedir, prefix string) (f *os.File, filePath string, err error) {
 	try := 0
 	for {
@@ -220,7 +222,8 @@ func createPcapFile(basedir, prefix string) (f *os.File, filePath string, err er
 		random := rand.Uint32()
 		nodeName := nodeTypes.GetAbsoluteNodeName()
 		name := fmt.Sprintf("%s_%d_%d_%s.pcap", prefix, startTime, random, nodeName)
-		filePath = path.Join(basedir, name)
+		sanitizedName := allowedFileChars.ReplaceAllLiteralString(name, "_")
+		filePath = path.Join(basedir, sanitizedName)
 		f, err = os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
 		if err != nil {
 			if os.IsExist(err) {