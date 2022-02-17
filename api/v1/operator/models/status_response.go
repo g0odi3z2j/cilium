g0odi3z2j/cilium

commit 3371408e812e03ae57a5060cbfda24a570878558

api: add openapi config for the API served by the Operator
Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

api/v1/operator/models/status_response.go
@@ -1,0 +1,46 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright 2017-2020 Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package models
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
+)
+
+// StatusResponse Health information for the operator
+//
+// swagger:model StatusResponse
+type StatusResponse struct {
+
+	// Status of Cilium operator
+	Status string `json:"status,omitempty"`
+}
+
+// Validate validates this status response
+func (m *StatusResponse) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// MarshalBinary interface implementation
+func (m *StatusResponse) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation
+func (m *StatusResponse) UnmarshalBinary(b []byte) error {
+	var res StatusResponse
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+	*m = res
+	return nil
+}

Makefile
@@ -345,6 +345,23 @@ generate-health-api: api/v1/health/openapi.yaml
 	@# sort goimports automatically
 	-$(QUIET) find api/v1/health/ -type f -name "*.go" -print | PATH="$(PWD)/tools:$(PATH)" xargs goimports -w
 
+generate-operator-api: api/v1/operator/openapi.yaml
+	@$(ECHO_GEN)api/v1/operator/openapi.yaml
+	-$(QUIET)$(SWAGGER) generate server -s server -a restapi \
+		-t api/v1 \
+		-t api/v1/operator/ \
+		-f api/v1/operator/openapi.yaml \
+		--default-scheme=http \
+		-C api/v1/cilium-server.yml \
+		-r hack/spdx-copyright-header.txt
+	-$(QUIET)$(SWAGGER) generate client -a restapi \
+		-t api/v1 \
+		-t api/v1/operator/ \
+		-f api/v1/operator/openapi.yaml \
+		-r hack/spdx-copyright-header.txt
+	@# sort goimports automatically
+	-$(QUIET) find api/v1/operator/ -type f -name "*.go" -print | PATH="$(PWD)/tools:$(PATH)" xargs goimports -w
+
 generate-hubble-api: api/v1/flow/flow.proto api/v1/peer/peer.proto api/v1/observer/observer.proto api/v1/relay/relay.proto
 	$(QUIET) $(MAKE) $(SUBMAKEOPTS) -C api/v1
 
@@ -602,5 +619,5 @@ dev-doctor:
 	$(QUIET)$(GO) version 2>/dev/null || ( echo "go not found, see https://golang.org/doc/install" ; false )
 	$(QUIET)$(GO) run ./tools/dev-doctor
 
-.PHONY: build-context-update clean-build clean clean-container dev-doctor force generate-api generate-health-api generate-hubble-api install licenses-all veryclean
+.PHONY: build-context-update clean-build clean clean-container dev-doctor force generate-api generate-health-api generate-operator-api generate-hubble-api install licenses-all veryclean
 force :;

api/v1/operator/client/cilium_operator_client.go
@@ -1,0 +1,120 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright 2017-2020 Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package client
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"github.com/go-openapi/runtime"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+
+	"github.com/cilium/cilium/api/v1/operator/client/metrics"
+	"github.com/cilium/cilium/api/v1/operator/client/operator"
+)
+
+// Default cilium operator HTTP client.
+var Default = NewHTTPClient(nil)
+
+const (
+	// DefaultHost is the default Host
+	// found in Meta (info) section of spec file
+	DefaultHost string = "localhost"
+	// DefaultBasePath is the default BasePath
+	// found in Meta (info) section of spec file
+	DefaultBasePath string = "/v1"
+)
+
+// DefaultSchemes are the default schemes found in Meta (info) section of spec file
+var DefaultSchemes = []string{"http"}
+
+// NewHTTPClient creates a new cilium operator HTTP client.
+func NewHTTPClient(formats strfmt.Registry) *CiliumOperator {
+	return NewHTTPClientWithConfig(formats, nil)
+}
+
+// NewHTTPClientWithConfig creates a new cilium operator HTTP client,
+// using a customizable transport config.
+func NewHTTPClientWithConfig(formats strfmt.Registry, cfg *TransportConfig) *CiliumOperator {
+	// ensure nullable parameters have default
+	if cfg == nil {
+		cfg = DefaultTransportConfig()
+	}
+
+	// create transport and client
+	transport := httptransport.New(cfg.Host, cfg.BasePath, cfg.Schemes)
+	return New(transport, formats)
+}
+
+// New creates a new cilium operator client
+func New(transport runtime.ClientTransport, formats strfmt.Registry) *CiliumOperator {
+	// ensure nullable parameters have default
+	if formats == nil {
+		formats = strfmt.Default
+	}
+
+	cli := new(CiliumOperator)
+	cli.Transport = transport
+	cli.Metrics = metrics.New(transport, formats)
+	cli.Operator = operator.New(transport, formats)
+	return cli
+}
+
+// DefaultTransportConfig creates a TransportConfig with the
+// default settings taken from the meta section of the spec file.
+func DefaultTransportConfig() *TransportConfig {
+	return &TransportConfig{
+		Host:     DefaultHost,
+		BasePath: DefaultBasePath,
+		Schemes:  DefaultSchemes,
+	}
+}
+
+// TransportConfig contains the transport related info,
+// found in the meta section of the spec file.
+type TransportConfig struct {
+	Host     string
+	BasePath string
+	Schemes  []string
+}
+
+// WithHost overrides the default host,
+// provided by the meta section of the spec file.
+func (cfg *TransportConfig) WithHost(host string) *TransportConfig {
+	cfg.Host = host
+	return cfg
+}
+
+// WithBasePath overrides the default basePath,
+// provided by the meta section of the spec file.
+func (cfg *TransportConfig) WithBasePath(basePath string) *TransportConfig {
+	cfg.BasePath = basePath
+	return cfg
+}
+
+// WithSchemes overrides the default schemes,
+// provided by the meta section of the spec file.
+func (cfg *TransportConfig) WithSchemes(schemes []string) *TransportConfig {
+	cfg.Schemes = schemes
+	return cfg
+}
+
+// CiliumOperator is a client for cilium operator
+type CiliumOperator struct {
+	Metrics metrics.ClientService
+
+	Operator operator.ClientService
+
+	Transport runtime.ClientTransport
+}
+
+// SetTransport changes the transport on the client and all its subresources
+func (c *CiliumOperator) SetTransport(transport runtime.ClientTransport) {
+	c.Transport = transport
+	c.Metrics.SetTransport(transport)
+	c.Operator.SetTransport(transport)
+}

api/v1/operator/client/metrics/get_metrics_parameters.go
@@ -1,0 +1,115 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright 2017-2020 Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package metrics
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	cr "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+)
+
+// NewGetMetricsParams creates a new GetMetricsParams object
+// with the default values initialized.
+func NewGetMetricsParams() *GetMetricsParams {
+
+	return &GetMetricsParams{
+
+		timeout: cr.DefaultTimeout,
+	}
+}
+
+// NewGetMetricsParamsWithTimeout creates a new GetMetricsParams object
+// with the default values initialized, and the ability to set a timeout on a request
+func NewGetMetricsParamsWithTimeout(timeout time.Duration) *GetMetricsParams {
+
+	return &GetMetricsParams{
+
+		timeout: timeout,
+	}
+}
+
+// NewGetMetricsParamsWithContext creates a new GetMetricsParams object
+// with the default values initialized, and the ability to set a context for a request
+func NewGetMetricsParamsWithContext(ctx context.Context) *GetMetricsParams {
+
+	return &GetMetricsParams{
+
+		Context: ctx,
+	}
+}
+
+// NewGetMetricsParamsWithHTTPClient creates a new GetMetricsParams object
+// with the default values initialized, and the ability to set a custom HTTPClient for a request
+func NewGetMetricsParamsWithHTTPClient(client *http.Client) *GetMetricsParams {
+
+	return &GetMetricsParams{
+		HTTPClient: client,
+	}
+}
+
+/*GetMetricsParams contains all the parameters to send to the API endpoint
+for the get metrics operation typically these are written to a http.Request
+*/
+type GetMetricsParams struct {
+	timeout    time.Duration
+	Context    context.Context
+	HTTPClient *http.Client
+}
+
+// WithTimeout adds the timeout to the get metrics params
+func (o *GetMetricsParams) WithTimeout(timeout time.Duration) *GetMetricsParams {
+	o.SetTimeout(timeout)
+	return o
+}
+
+// SetTimeout adds the timeout to the get metrics params
+func (o *GetMetricsParams) SetTimeout(timeout time.Duration) {
+	o.timeout = timeout
+}
+
+// WithContext adds the context to the get metrics params
+func (o *GetMetricsParams) WithContext(ctx context.Context) *GetMetricsParams {
+	o.SetContext(ctx)
+	return o
+}
+
+// SetContext adds the context to the get metrics params
+func (o *GetMetricsParams) SetContext(ctx context.Context) {
+	o.Context = ctx
+}
+
+// WithHTTPClient adds the HTTPClient to the get metrics params
+func (o *GetMetricsParams) WithHTTPClient(client *http.Client) *GetMetricsParams {
+	o.SetHTTPClient(client)
+	return o
+}
+
+// SetHTTPClient adds the HTTPClient to the get metrics params
+func (o *GetMetricsParams) SetHTTPClient(client *http.Client) {
+	o.HTTPClient = client
+}
+
+// WriteToRequest writes these params to a swagger request
+func (o *GetMetricsParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Registry) error {
+
+	if err := r.SetTimeout(o.timeout); err != nil {
+		return err
+	}
+	var res []error
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}

api/v1/operator/client/metrics/get_metrics_responses.go
@@ -1,0 +1,97 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright 2017-2020 Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package metrics
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
+
+	"github.com/cilium/cilium/api/v1/operator/models"
+)
+
+// GetMetricsReader is a Reader for the GetMetrics structure.
+type GetMetricsReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *GetMetricsReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+	case 200:
+		result := NewGetMetricsOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+	case 500:
+		result := NewGetMetricsFailed()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return nil, result
+
+	default:
+		return nil, runtime.NewAPIError("response status code does not match any response statuses defined for this endpoint in the swagger spec", response, response.Code())
+	}
+}
+
+// NewGetMetricsOK creates a GetMetricsOK with default headers values
+func NewGetMetricsOK() *GetMetricsOK {
+	return &GetMetricsOK{}
+}
+
+/*GetMetricsOK handles this case with default header values.
+
+Success
+*/
+type GetMetricsOK struct {
+	Payload []*models.Metric
+}
+
+func (o *GetMetricsOK) Error() string {
+	return fmt.Sprintf("[GET /metrics/][%d] getMetricsOK  %+v", 200, o.Payload)
+}
+
+func (o *GetMetricsOK) GetPayload() []*models.Metric {
+	return o.Payload
+}
+
+func (o *GetMetricsOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	// response payload
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+// NewGetMetricsFailed creates a GetMetricsFailed with default headers values
+func NewGetMetricsFailed() *GetMetricsFailed {
+	return &GetMetricsFailed{}
+}
+
+/*GetMetricsFailed handles this case with default header values.
+
+Metrics cannot be retrieved
+*/
+type GetMetricsFailed struct {
+}
+
+func (o *GetMetricsFailed) Error() string {
+	return fmt.Sprintf("[GET /metrics/][%d] getMetricsFailed ", 500)
+}
+
+func (o *GetMetricsFailed) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	return nil
+}

api/v1/operator/client/metrics/metrics_client.go
@@ -1,0 +1,75 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright 2017-2020 Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package metrics
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
+)
+
+// New creates a new metrics API client.
+func New(transport runtime.ClientTransport, formats strfmt.Registry) ClientService {
+	return &Client{transport: transport, formats: formats}
+}
+
+/*
+Client for metrics API
+*/
+type Client struct {
+	transport runtime.ClientTransport
+	formats   strfmt.Registry
+}
+
+// ClientService is the interface for Client methods
+type ClientService interface {
+	GetMetrics(params *GetMetricsParams) (*GetMetricsOK, error)
+
+	SetTransport(transport runtime.ClientTransport)
+}
+
+/*
+  GetMetrics retrieves cilium operator metrics
+*/
+func (a *Client) GetMetrics(params *GetMetricsParams) (*GetMetricsOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewGetMetricsParams()
+	}
+
+	result, err := a.transport.Submit(&runtime.ClientOperation{
+		ID:                 "GetMetrics",
+		Method:             "GET",
+		PathPattern:        "/metrics/",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &GetMetricsReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	})
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*GetMetricsOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for GetMetrics: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+// SetTransport changes the transport on the client
+func (a *Client) SetTransport(transport runtime.ClientTransport) {
+	a.transport = transport
+}

api/v1/operator/client/operator/get_healthz_parameters.go
@@ -1,0 +1,115 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright 2017-2020 Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package operator
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	cr "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+)
+
+// NewGetHealthzParams creates a new GetHealthzParams object
+// with the default values initialized.
+func NewGetHealthzParams() *GetHealthzParams {
+
+	return &GetHealthzParams{
+
+		timeout: cr.DefaultTimeout,
+	}
+}
+
+// NewGetHealthzParamsWithTimeout creates a new GetHealthzParams object
+// with the default values initialized, and the ability to set a timeout on a request
+func NewGetHealthzParamsWithTimeout(timeout time.Duration) *GetHealthzParams {
+
+	return &GetHealthzParams{
+
+		timeout: timeout,
+	}
+}
+
+// NewGetHealthzParamsWithContext creates a new GetHealthzParams object
+// with the default values initialized, and the ability to set a context for a request
+func NewGetHealthzParamsWithContext(ctx context.Context) *GetHealthzParams {
+
+	return &GetHealthzParams{
+
+		Context: ctx,
+	}
+}
+
+// NewGetHealthzParamsWithHTTPClient creates a new GetHealthzParams object
+// with the default values initialized, and the ability to set a custom HTTPClient for a request
+func NewGetHealthzParamsWithHTTPClient(client *http.Client) *GetHealthzParams {
+
+	return &GetHealthzParams{
+		HTTPClient: client,
+	}
+}
+
+/*GetHealthzParams contains all the parameters to send to the API endpoint
+for the get healthz operation typically these are written to a http.Request
+*/
+type GetHealthzParams struct {
+	timeout    time.Duration
+	Context    context.Context
+	HTTPClient *http.Client
+}
+
+// WithTimeout adds the timeout to the get healthz params
+func (o *GetHealthzParams) WithTimeout(timeout time.Duration) *GetHealthzParams {
+	o.SetTimeout(timeout)
+	return o
+}
+
+// SetTimeout adds the timeout to the get healthz params
+func (o *GetHealthzParams) SetTimeout(timeout time.Duration) {
+	o.timeout = timeout
+}
+
+// WithContext adds the context to the get healthz params
+func (o *GetHealthzParams) WithContext(ctx context.Context) *GetHealthzParams {
+	o.SetContext(ctx)
+	return o
+}
+
+// SetContext adds the context to the get healthz params
+func (o *GetHealthzParams) SetContext(ctx context.Context) {
+	o.Context = ctx
+}
+
+// WithHTTPClient adds the HTTPClient to the get healthz params
+func (o *GetHealthzParams) WithHTTPClient(client *http.Client) *GetHealthzParams {
+	o.SetHTTPClient(client)
+	return o
+}
+
+// SetHTTPClient adds the HTTPClient to the get healthz params
+func (o *GetHealthzParams) SetHTTPClient(client *http.Client) {
+	o.HTTPClient = client
+}
+
+// WriteToRequest writes these params to a swagger request
+func (o *GetHealthzParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Registry) error {
+
+	if err := r.SetTimeout(o.timeout); err != nil {
+		return err
+	}
+	var res []error
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}

api/v1/operator/client/operator/get_healthz_responses.go
@@ -1,0 +1,105 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+// Copyright 2017-2020 Authors of Cilium
+// SPDX-License-Identifier: Apache-2.0
+
+package operator
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
+)
+
+// GetHealthzReader is a Reader for the GetHealthz structure.
+type GetHealthzReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *GetHealthzReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+	case 200:
+		result := NewGetHealthzOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+	case 500:
+		result := NewGetHealthzInternalServerError()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return nil, result
+
+	default:
+		return nil, runtime.NewAPIError("response status code does not match any response statuses defined for this endpoint in the swagger spec", response, response.Code())
+	}
+}
+
+// NewGetHealthzOK creates a GetHealthzOK with default headers values
+func NewGetHealthzOK() *GetHealthzOK {
+	return &GetHealthzOK{}
+}
+
+/*GetHealthzOK handles this case with default header values.
+
+Cilium operator is healthy
+*/
+type GetHealthzOK struct {
+	Payload string
+}
+
+func (o *GetHealthzOK) Error() string {
+	return fmt.Sprintf("[GET /healthz][%d] getHealthzOK  %+v", 200, o.Payload)
+}
+
+func (o *GetHealthzOK) GetPayload() string {
+	return o.Payload
+}
+
+func (o *GetHealthzOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	// response payload
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+// NewGetHealthzInternalServerError creates a GetHealthzInternalServerError with default headers values
+func NewGetHealthzInternalServerError() *GetHealthzInternalServerError {
+	return &GetHealthzInternalServerError{}
+}
+
+/*GetHealthzInternalServerError handles this case with default header values.
+
+Cilium operator is not healthy
+*/
+type GetHealthzInternalServerError struct {
+	Payload string
+}
+
+func (o *GetHealthzInternalServerError) Error() string {
+	return fmt.Sprintf("[GET /healthz][%d] getHealthzInternalServerError  %+v", 500, o.Payload)
+}
+
+func (o *GetHealthzInternalServerError) GetPayload() string {
+	return o.Payload
+}
+
+func (o *GetHealthzInternalServerError) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	// response payload
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

commit a443a197785ce3e8451aca23be01fc947bf3381f

api: remove unused file
It seems that api/v1/operator/models/status_response.go was committed by
mistake, and is not actually needed. Tobias noted (offline) that “The
OpenAPI definition api/v1/operator/openapi.yaml doesn't contain the type
contained in the file and it isn't used by any importers of
api/v1/operator/models”. Let's get rid of it.

Fixes: 3371408e812e ("api: add openapi config for the API served by the Operator")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

api/v1/operator/models/status_response.go
@@ -1,46 +1,0 @@
-// Code generated by go-swagger; DO NOT EDIT.
-
-// Copyright 2017-2020 Authors of Cilium
-// SPDX-License-Identifier: Apache-2.0
-
-package models
-
-// This file was generated by the swagger tool.
-// Editing this file might prove futile when you re-run the swagger generate command
-
-import (
-	"github.com/go-openapi/strfmt"
-	"github.com/go-openapi/swag"
-)
-
-// StatusResponse Health information for the operator
-//
-// swagger:model StatusResponse
-type StatusResponse struct {
-
-	// Status of Cilium operator
-	Status string `json:"status,omitempty"`
-}
-
-// Validate validates this status response
-func (m *StatusResponse) Validate(formats strfmt.Registry) error {
-	return nil
-}
-
-// MarshalBinary interface implementation
-func (m *StatusResponse) MarshalBinary() ([]byte, error) {
-	if m == nil {
-		return nil, nil
-	}
-	return swag.WriteJSON(m)
-}
-
-// UnmarshalBinary interface implementation
-func (m *StatusResponse) UnmarshalBinary(b []byte) error {
-	var res StatusResponse
-	if err := swag.ReadJSON(b, &res); err != nil {
-		return err
-	}
-	*m = res
-	return nil
-}