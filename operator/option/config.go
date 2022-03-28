g0odi3z2j/cilium

commit 8397bc72f8eac2ca1adee75c50dcade61a0a51dd

cmd: Fix issue reading string map type via config map
As mentioned in below upstream issue, there is a discrepancy in viper
while reading string map string data type i.e. kv pair format was not
supported, only `{"k":"v"}` format is allowed. This commit is to wrap
GetStringMapString implementation to handle such case.

Also, during the bootstrap, if there is any flag having invalid value,
fatal log will be printed for early detection and awareness.

Relates https://github.com/spf13/viper/issues/911
Fixes https://github.com/cilium/cilium/issues/18328

Signed-off-by: Tam Mach <tam.mach@isovalent.com>

operator/option/config.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/spf13/viper"
 
+	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 )
@@ -446,15 +447,21 @@ func (c *OperatorConfig) Populate() {
 		c.IPAMSubnetsIDs = m
 	}
 
-	if m := viper.GetStringMapString(IPAMSubnetsTags); len(m) != 0 {
+	if m, err := command.GetStringMapStringE(viper.GetViper(), IPAMSubnetsTags); err != nil {
+		log.Fatalf("unable to parse %s: %s", IPAMSubnetsTags, err)
+	} else if len(m) != 0 {
 		c.IPAMSubnetsTags = m
 	}
 
-	if m := viper.GetStringMapString(AWSInstanceLimitMapping); len(m) != 0 {
+	if m, err := command.GetStringMapStringE(viper.GetViper(), AWSInstanceLimitMapping); err != nil {
+		log.Fatalf("unable to parse %s: %s", AWSInstanceLimitMapping, err)
+	} else if len(m) != 0 {
 		c.AWSInstanceLimitMapping = m
 	}
 
-	if m := viper.GetStringMapString(ENITags); len(m) != 0 {
+	if m, err := command.GetStringMapStringE(viper.GetViper(), ENITags); err != nil {
+		log.Fatalf("unable to parse %s: %s", ENITags, err)
+	} else if len(m) != 0 {
 		c.ENITags = m
 	}
 }

pkg/option/config.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/cidr"
 	clustermeshTypes "github.com/cilium/cilium/pkg/clustermesh/types"
+	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/ip"
@@ -2782,21 +2783,29 @@ func (c *DaemonConfig) Populate() {
 	c.MonitorAggregationFlags = ctMonitorReportFlags
 
 	// Map options
-	if m := viper.GetStringMapString(FixedIdentityMapping); len(m) != 0 {
+	if m := command.GetStringMapString(viper.GetViper(), FixedIdentityMapping); err != nil {
+		log.Fatalf("unable to parse %s: %s", FixedIdentityMapping, err)
+	} else if len(m) != 0 {
 		c.FixedIdentityMapping = m
 	}
 
 	c.ConntrackGCInterval = viper.GetDuration(ConntrackGCInterval)
 
-	if m := viper.GetStringMapString(KVStoreOpt); len(m) != 0 {
+	if m, err := command.GetStringMapStringE(viper.GetViper(), KVStoreOpt); err != nil {
+		log.Fatalf("unable to parse %s: %s", KVStoreOpt, err)
+	} else if len(m) != 0 {
 		c.KVStoreOpt = m
 	}
 
-	if m := viper.GetStringMapString(LogOpt); len(m) != 0 {
+	if m, err := command.GetStringMapStringE(viper.GetViper(), LogOpt); err != nil {
+		log.Fatalf("unable to parse %s: %s", LogOpt, err)
+	} else if len(m) != 0 {
 		c.LogOpt = m
 	}
 
-	if m := viper.GetStringMapString(APIRateLimitName); len(m) != 0 {
+	if m, err := command.GetStringMapStringE(viper.GetViper(), APIRateLimitName); err != nil {
+		log.Fatalf("unable to parse %s: %s", APIRateLimitName, err)
+	} else if len(m) != 0 {
 		c.APIRateLimit = m
 	}
 

Makefile
@@ -523,6 +523,8 @@ endif
 	$(QUIET) contrib/scripts/check-assert-deep-equals.sh
 	@$(ECHO_CHECK) contrib/scripts/lock-check.sh
 	$(QUIET) contrib/scripts/lock-check.sh
+	@$(ECHO_CHECK) contrib/scripts/check-viper-get-string-map-string.sh
+	$(QUIET) contrib/scripts/check-viper-get-string-map-string.sh
 ifeq ($(SKIP_CUSTOMVET_CHECK),"false")
 	@$(ECHO_CHECK) contrib/scripts/custom-vet-check.sh
 	$(QUIET) contrib/scripts/custom-vet-check.sh

contrib/scripts/check-viper-get-string-map-string.sh
@@ -1,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright 2022 Authors of Cilium
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Simple script to make sure viper.GetStringMapString should not be used.
+# Related upstream issue https://github.com/spf13/viper/issues/911
+if grep -r --exclude-dir={.git,_build,vendor,contrib} -i --include \*.go "viper.GetStringMapString" .; then
+  echo "Found viper.GetStringMapString(key) usage. Please use command.GetStringMapString(viper.GetViper(), key) instead";
+  exit 1
+fi

go.mod
@@ -64,6 +64,7 @@ require (
 	github.com/servak/go-fastping v0.0.0-20160802140958-5718d12e20a0
 	github.com/shirou/gopsutil/v3 v3.21.12
 	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/cast v1.4.1
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.10.1
@@ -194,7 +195,6 @@ require (
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
-	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.9 // indirect

pkg/command/map_string.go
@@ -1,0 +1,80 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Authors of Cilium
+
+package command
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode"
+
+	"github.com/spf13/cast"
+	"github.com/spf13/viper"
+)
+
+var keyValueRegex = regexp.MustCompile(`([\w-:./]+=[\w-:./]+,)*([\w-:./]+=[\w-:./]+)$`)
+
+// GetStringMapString contains one enhancement to support k1=v2,k2=v2 compared to original
+// implementation of GetStringMapString function
+// Related upstream issue https://github.com/spf13/viper/issues/911
+func GetStringMapString(vp *viper.Viper, key string) map[string]string {
+	v, _ := GetStringMapStringE(vp, key)
+	return v
+}
+
+// GetStringMapStringE is same as GetStringMapString, but with error
+func GetStringMapStringE(vp *viper.Viper, key string) (map[string]string, error) {
+	data := vp.Get(key)
+	if data == nil {
+		return map[string]string{}, nil
+	}
+	v, err := cast.ToStringMapStringE(data)
+	if err != nil {
+		var syntaxErr *json.SyntaxError
+		if !errors.As(err, &syntaxErr) {
+			return v, err
+		}
+
+		switch s := data.(type) {
+		case string:
+			if len(s) == 0 {
+				return map[string]string{}, nil
+			}
+
+			// if the input is starting with either '{' or '[', just preserve original json parsing error.
+			firstIndex := strings.IndexFunc(s, func(r rune) bool {
+				return !unicode.IsSpace(r)
+			})
+			if firstIndex != -1 && (s[firstIndex] == '{' || s[firstIndex] == '[') {
+				return v, err
+			}
+
+			if !isValidKeyValuePair(s) {
+				return map[string]string{}, fmt.Errorf("'%s' is not formatted as key=value,key1=value1", s)
+			}
+
+			var v = map[string]string{}
+			kvs := strings.Split(s, ",")
+			for _, kv := range kvs {
+				temp := strings.Split(kv, "=")
+				if len(temp) != 2 {
+					return map[string]string{}, fmt.Errorf("'%s' is not formatted as key=value,key1=value1", s)
+				}
+				v[temp[0]] = temp[1]
+			}
+			return v, nil
+		}
+	}
+	return v, nil
+}
+
+// isValidKeyValuePair returns true if the input is following key1=value1,key2=value2,...,keyN=valueN format.
+func isValidKeyValuePair(str string) bool {
+	if len(str) == 0 {
+		return true
+	}
+	return len(keyValueRegex.ReplaceAllString(str, "")) == 0
+}

pkg/command/map_string_test.go
@@ -1,0 +1,286 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Authors of Cilium
+
+//go:build !privileged_tests
+// +build !privileged_tests
+
+package command
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetStringMapString(t *testing.T) {
+	expectedResult := map[string]string{
+		"k1": "v1",
+		"k2": "v2",
+	}
+	type args struct {
+		key   string
+		value string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]string
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "valid json format",
+			args: args{
+				key:   "FOO_BAR",
+				value: `{"k1":"v1","k2":"v2"}`,
+			},
+			want:    expectedResult,
+			wantErr: assert.NoError,
+		},
+		{
+			name: "invalid json format with extra comma at the end",
+			args: args{
+				key:   "FOO_BAR",
+				value: `{"k1":"v1","k2":"v2",}`,
+			},
+			want:    map[string]string{},
+			wantErr: assertErrorString("invalid character '}' looking for beginning of object key string"),
+		},
+		{
+			name: "valid single kv format",
+			args: args{
+				key:   "FOO_BAR",
+				value: "k1=v1",
+			},
+			want:    map[string]string{"k1": "v1"},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "valid kv format",
+			args: args{
+				key:   "FOO_BAR",
+				value: "k1=v1,k2=v2",
+			},
+			want:    expectedResult,
+			wantErr: assert.NoError,
+		},
+		{
+			name: "valid kv format with forward slash",
+			args: args{
+				key:   "FOO_BAR",
+				value: "kubernetes.io/cluster/piano-eks-general-blue-01=owned,kubernetes.io/role/internal-elb=1",
+			},
+			want: map[string]string{
+				"kubernetes.io/cluster/piano-eks-general-blue-01": "owned",
+				"kubernetes.io/role/internal-elb":                 "1",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "valid kv format with hyphens",
+			args: args{
+				key:   "FOO_BAR",
+				value: "cluster=my-cluster",
+			},
+			want: map[string]string{
+				"cluster": "my-cluster",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "invalid kv format with extra comma",
+			args: args{
+				key:   "FOO_BAR",
+				value: "k1=v1,k2=v2,",
+			},
+			want:    map[string]string{},
+			wantErr: assertErrorString("'k1=v1,k2=v2,' is not formatted as key=value,key1=value1"),
+		},
+		{
+			name: "invalid kv format with extra equal",
+			args: args{
+				key:   "FOO_BAR",
+				value: "k1=v1,k2==v2",
+			},
+			want:    map[string]string{},
+			wantErr: assertErrorString("'k1=v1,k2==v2' is not formatted as key=value,key1=value1"),
+		},
+		{
+			name: "invalid kv format with wrong space in between",
+			args: args{
+				key:   "FOO_BAR",
+				value: "k1=v1, k2=v2",
+			},
+			want:    map[string]string{},
+			wantErr: assertErrorString("'k1=v1, k2=v2' is not formatted as key=value,key1=value1"),
+		},
+		{
+			name: "malformed json format",
+			args: args{
+				key:   "FOO_BAR",
+				value: `{"k1": "v1",=sdlkfj`,
+			},
+			want:    map[string]string{},
+			wantErr: assertErrorString("invalid character '=' looking for beginning of object key string"),
+		},
+		{
+			name: "staring with valid json beginning value e.g. t, f, n, 0, -, \"",
+			args: args{
+				key:   "FOO_BAR",
+				value: "this is a sentence used in test",
+			},
+			want:    map[string]string{},
+			wantErr: assertErrorString("'this is a sentence used in test' is not formatted as key=value,key1=value1"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			viper.Reset()
+			viper.AutomaticEnv()
+			t.Setenv(strings.ToUpper(tt.args.key), tt.args.value)
+			v, err := GetStringMapStringE(viper.GetViper(), strings.ToLower(tt.args.key))
+			tt.wantErr(t, err)
+			assert.Equal(t, tt.want, v)
+		})
+	}
+}
+
+func TestGetStringMapStringConversion(t *testing.T) {
+	viper.Reset()
+	viper.Set("foo_bar", struct{}{})
+	v, err := GetStringMapStringE(viper.GetViper(), "foo_bar")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "unable to cast struct {}{} of type struct {} to map[string]string")
+	assert.Equal(t, map[string]string{}, v)
+}
+
+func Test_isValidKeyValuePair(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "valid format with one pair",
+			args: args{
+				str: "k1=v1",
+			},
+			want: true,
+		},
+		{
+			name: "valid format with hyphen in k and v",
+			args: args{
+				str: "k-1=v-1,k-2=v-2",
+			},
+			want: true,
+		},
+		{
+			name: "valid format with multiple hyphens",
+			args: args{
+				str: "Cluster=piano-eks-general-blue-01",
+			},
+			want: true,
+		},
+		{
+			name: "valid format with colon",
+			args: args{
+				str: "consul.address=127.0.0.1:8500",
+			},
+			want: true,
+		},
+		{
+			name: "valid format with forward slash",
+			args: args{
+				str: "kubernetes.io/cluster/piano-eks-general-blue-01=owned",
+			},
+			want: true,
+		},
+
+		{
+			name: "valid format with multiple pairs",
+			args: args{
+				str: "k1=v1,k2=v2,k3=v3,k4=v4,k4=v4,k4=v4",
+			},
+			want: true,
+		},
+		{
+			name: "empty value",
+			args: args{
+				str: "",
+			},
+			want: true,
+		},
+		{
+			name: "space in between",
+			args: args{
+				str: "k1=v1, k2=v2",
+			},
+			want: false,
+		},
+		{
+			name: "insufficient value",
+			args: args{
+				str: "k1=v1,k2,=v2",
+			},
+			want: false,
+		},
+		{
+			name: "no pair at all",
+			args: args{
+				str: "here is the test",
+			},
+			want: false,
+		},
+		{
+			name: "ending with command",
+			args: args{
+				str: "k1=v1,k2=v2,",
+			},
+			want: false,
+		},
+		{
+			name: "ending with equal",
+			args: args{
+				str: "k1=v1,k2=v2=",
+			},
+			want: false,
+		},
+		{
+			name: "kv separator as space",
+			args: args{
+				str: "k1=v1 k2=v2=",
+			},
+			want: false,
+		},
+		{
+			name: "space in key",
+			args: args{
+				str: "k1=v1, k2=v2",
+			},
+			want: false,
+		},
+		{
+			name: "space in value",
+			args: args{
+				str: "k1=v1,k2= v2",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, isValidKeyValuePair(tt.args.str), "isValidKeyValuePair(%v)", tt.args.str)
+		})
+	}
+}
+
+func assertErrorString(errString string) assert.ErrorAssertionFunc {
+	return func(t assert.TestingT, err error, msgAndArgs ...interface{}) bool {
+		return assert.EqualError(t, err, errString, msgAndArgs)
+	}
+}

commit bd3250f1e60de3c680e5921ae744b10ceb81e1eb

cmd: Always use GetStringMapString parse result
Because string map options are configured `flags.Var`, they are assigned
twice: Once in `flags.Var`, and then again in `option.Config.Populate()`
with `GetStringMapString`. The latter is needed because it works around
a viper bug where ConfigMap options were not properly parsed
(cilium/cilium#18328). This commit ensures we're always using the result
of the fallback parser, which fixes a bug where a ConfigMap value of
`{}` was parsed as `map["{}":""]`.

Fixes: 8397bc72f8ea ("cmd: Fix issue reading string map type via config map")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

operator/option/config.go
@@ -449,19 +449,19 @@ func (c *OperatorConfig) Populate() {
 
 	if m, err := command.GetStringMapStringE(viper.GetViper(), IPAMSubnetsTags); err != nil {
 		log.Fatalf("unable to parse %s: %s", IPAMSubnetsTags, err)
-	} else if len(m) != 0 {
+	} else {
 		c.IPAMSubnetsTags = m
 	}
 
 	if m, err := command.GetStringMapStringE(viper.GetViper(), AWSInstanceLimitMapping); err != nil {
 		log.Fatalf("unable to parse %s: %s", AWSInstanceLimitMapping, err)
-	} else if len(m) != 0 {
+	} else {
 		c.AWSInstanceLimitMapping = m
 	}
 
 	if m, err := command.GetStringMapStringE(viper.GetViper(), ENITags); err != nil {
 		log.Fatalf("unable to parse %s: %s", ENITags, err)
-	} else if len(m) != 0 {
+	} else {
 		c.ENITags = m
 	}
 }

pkg/option/config.go
@@ -2881,19 +2881,19 @@ func (c *DaemonConfig) Populate() {
 
 	if m, err := command.GetStringMapStringE(viper.GetViper(), KVStoreOpt); err != nil {
 		log.Fatalf("unable to parse %s: %s", KVStoreOpt, err)
-	} else if len(m) != 0 {
+	} else {
 		c.KVStoreOpt = m
 	}
 
 	if m, err := command.GetStringMapStringE(viper.GetViper(), LogOpt); err != nil {
 		log.Fatalf("unable to parse %s: %s", LogOpt, err)
-	} else if len(m) != 0 {
+	} else {
 		c.LogOpt = m
 	}
 
 	if m, err := command.GetStringMapStringE(viper.GetViper(), APIRateLimitName); err != nil {
 		log.Fatalf("unable to parse %s: %s", APIRateLimitName, err)
-	} else if len(m) != 0 {
+	} else {
 		c.APIRateLimit = m
 	}
 