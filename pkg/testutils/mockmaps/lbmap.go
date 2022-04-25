g0odi3z2j/cilium

commit 688167ee823d383cb30200cafb16e5e245f1ee14

cilium, tests: Add NAT46 and NAT64 upsert test cases
Add various test cases for the agent's service management as well as the
BPF service map for NAT46/64 entries.

  root@zh-lab-node-1:~/go/src/github.com/cilium/cilium/pkg/service# go test -v -check.vv -check.f TestUpsertAndDeleteServiceNat
  === RUN   Test
  START: service_test.go:103: ManagerTestSuite.TestUpsertAndDeleteServiceNat46
  START: service_test.go:38: ManagerTestSuite.SetUpTest
  PASS: service_test.go:38: ManagerTestSuite.SetUpTest	0.000s

  START: service_test.go:62: ManagerTestSuite.TearDownTest
  PASS: service_test.go:62: ManagerTestSuite.TearDownTest	0.000s

  PASS: service_test.go:103: ManagerTestSuite.TestUpsertAndDeleteServiceNat46	0.000s

  START: service_test.go:109: ManagerTestSuite.TestUpsertAndDeleteServiceNat64
  START: service_test.go:38: ManagerTestSuite.SetUpTest
  PASS: service_test.go:38: ManagerTestSuite.SetUpTest	0.000s

  START: service_test.go:62: ManagerTestSuite.TearDownTest
  PASS: service_test.go:62: ManagerTestSuite.TearDownTest	0.000s

  PASS: service_test.go:109: ManagerTestSuite.TestUpsertAndDeleteServiceNat64	0.000s

  OK: 2 passed
  --- PASS: Test (0.00s)
  PASS
  ok  	github.com/cilium/cilium/pkg/service	0.009s
  [...]

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

pkg/testutils/mockmaps/lbmap.go
@@ -88,7 +88,10 @@ func (m *LBMockMap) DeleteService(addr lb.L3n4AddrID, backendCount int, maglev b
 }
 
 func (m *LBMockMap) AddBackend(id lb.BackendID, ip net.IP, port uint16, ipv6 bool) error {
-	if _, found := m.BackendByID[id]; found {
+	if be, found := m.BackendByID[id]; found {
+		if be.L3n4Addr.IP.Equal(ip) && be.L4Addr.Port == port {
+			return nil
+		}
 		return fmt.Errorf("Backend %d already exists", id)
 	}
 

pkg/service/service_test.go
@@ -99,6 +99,100 @@ func (m *ManagerTestSuite) TestUpsertAndDeleteServiceWithoutIPv6(c *C) {
 	m.testUpsertAndDeleteService(c)
 }
 
+func (m *ManagerTestSuite) TestUpsertAndDeleteServiceNat46(c *C) {
+	option.Config.EnableIPv4 = true
+	option.Config.EnableIPv6 = true
+	option.Config.NodePortNat46X64 = true
+	m.testUpsertAndDeleteService46(c)
+}
+
+func (m *ManagerTestSuite) TestUpsertAndDeleteServiceNat64(c *C) {
+	option.Config.EnableIPv4 = true
+	option.Config.EnableIPv6 = true
+	option.Config.NodePortNat46X64 = true
+	m.testUpsertAndDeleteService64(c)
+}
+
+func (m *ManagerTestSuite) testUpsertAndDeleteService46(c *C) {
+	// Should create a new v4 service with two v6 backends
+	p := &lb.SVC{
+		Frontend:      frontend1,
+		Backends:      backends3,
+		Type:          lb.SVCTypeNodePort,
+		TrafficPolicy: lb.SVCTrafficPolicyCluster,
+		Name:          "svc1",
+		Namespace:     "ns1",
+	}
+	created, id1, err := m.svc.UpsertService(p)
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, true)
+	c.Assert(id1, Equals, lb.ID(1))
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id1)].Backends), Equals, 2)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
+	c.Assert(m.svc.svcByID[id1].svcName, Equals, "svc1")
+	c.Assert(m.svc.svcByID[id1].svcNamespace, Equals, "ns1")
+	c.Assert(m.svc.svcByID[id1].svcNatPolicy, Equals, lb.SVCNatPolicyNat46)
+
+	// Should delete both backends of service
+	p.Backends = nil
+	created, id2, err := m.svc.UpsertService(p)
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, false)
+	c.Assert(id2, Equals, id1)
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id2)].Backends), Equals, 0)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 0)
+	c.Assert(m.svc.svcByID[id2].svcName, Equals, "svc1")
+	c.Assert(m.svc.svcByID[id2].svcNamespace, Equals, "ns1")
+	c.Assert(m.svc.svcByID[id2].svcNatPolicy, Equals, lb.SVCNatPolicyNone)
+
+	// Should delete the remaining service
+	found, err := m.svc.DeleteServiceByID(lb.ServiceID(id1))
+	c.Assert(err, IsNil)
+	c.Assert(found, Equals, true)
+	c.Assert(len(m.lbmap.ServiceByID), Equals, 0)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 0)
+}
+
+func (m *ManagerTestSuite) testUpsertAndDeleteService64(c *C) {
+	// Should create a new v6 service with two v4 backends
+	p := &lb.SVC{
+		Frontend:      frontend3,
+		Backends:      backends1,
+		Type:          lb.SVCTypeNodePort,
+		TrafficPolicy: lb.SVCTrafficPolicyCluster,
+		Name:          "svc1",
+		Namespace:     "ns1",
+	}
+	created, id1, err := m.svc.UpsertService(p)
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, true)
+	c.Assert(id1, Equals, lb.ID(1))
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id1)].Backends), Equals, 2)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 2)
+	c.Assert(m.svc.svcByID[id1].svcName, Equals, "svc1")
+	c.Assert(m.svc.svcByID[id1].svcNamespace, Equals, "ns1")
+	c.Assert(m.svc.svcByID[id1].svcNatPolicy, Equals, lb.SVCNatPolicyNat64)
+
+	// Should delete both backends of service
+	p.Backends = nil
+	created, id2, err := m.svc.UpsertService(p)
+	c.Assert(err, IsNil)
+	c.Assert(created, Equals, false)
+	c.Assert(id2, Equals, id1)
+	c.Assert(len(m.lbmap.ServiceByID[uint16(id2)].Backends), Equals, 0)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 0)
+	c.Assert(m.svc.svcByID[id2].svcName, Equals, "svc1")
+	c.Assert(m.svc.svcByID[id2].svcNamespace, Equals, "ns1")
+	c.Assert(m.svc.svcByID[id2].svcNatPolicy, Equals, lb.SVCNatPolicyNone)
+
+	// Should delete the remaining service
+	found, err := m.svc.DeleteServiceByID(lb.ServiceID(id1))
+	c.Assert(err, IsNil)
+	c.Assert(found, Equals, true)
+	c.Assert(len(m.lbmap.ServiceByID), Equals, 0)
+	c.Assert(len(m.lbmap.BackendByID), Equals, 0)
+}
+
 func (m *ManagerTestSuite) testUpsertAndDeleteService(c *C) {
 	// Should create a new service with two backends and session affinity
 	p := &lb.SVC{
@@ -402,7 +496,6 @@ func (m *ManagerTestSuite) TestSyncWithK8sFinished(c *C) {
 	for _, b := range lbmap.ServiceByID[uint16(id2)].Backends {
 		c.Assert(m.lbmap.AffinityMatch[uint16(id2)][b.ID], Equals, struct{}{})
 	}
-
 }
 
 func (m *ManagerTestSuite) TestHealthCheckNodePort(c *C) {

commit 0a268ded90c6a7364a521ffd657562883c6b41e8

testutils/mockmaps: Add duplicate backend calls check back
Commit 688167ee82 rendered the duplicate calls to add a backend
check ineffective. Until we have a better mock code
to differentiate between v4 and v6 backend maps, let's
bring back the original check with some special checks
with respect to NAT46/64 policies.

Fixes: 688167ee82 ("cilium, tests: Add NAT46 and NAT64 upsert test cases")
Signed-off-by: Aditi Ghag <aditi@cilium.io>

pkg/testutils/mockmaps/lbmap.go
@@ -95,10 +95,9 @@ func (m *LBMockMap) AddBackend(b lb.Backend, ipv6 bool) error {
 	ip := b.IP
 	port := b.Port
 
-	if be, found := m.BackendByID[id]; found {
-		if be.L3n4Addr.IP.Equal(ip) && be.L4Addr.Port == port {
-			return nil
-		}
+	// Backends can be added to both v4 and v6 lb maps (when nat64 policies
+	// are enabled).
+	if _, found := m.BackendByID[id]; found && !b.L3n4Addr.IsIPv6() && !ipv6 {
 		return fmt.Errorf("Backend %d already exists", id)
 	}
 