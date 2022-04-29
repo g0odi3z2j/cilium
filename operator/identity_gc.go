g0odi3z2j/cilium

commit 706d41ca8935a7502838eadb375d5b411f199d31

operator: only GC identity keys of its own cluster
Fix: #16805

Signed-off-by: ArthurChiao <arthurchiao@hotmail.com>

pkg/allocator/allocator.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/cilium/cilium/pkg/backoff"
+	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/idpool"
 	"github.com/cilium/cilium/pkg/inctimer"
 	"github.com/cilium/cilium/pkg/kvstore"
@@ -237,7 +238,7 @@ type Backend interface {
 	// by cilium-agent.
 	// Note: not all Backend implemenations rely on this, such as the kvstore
 	// backends, and may use leases to expire keys.
-	RunGC(ctx context.Context, rateLimit *rate.Limiter, staleKeysPrevRound map[string]uint64) (map[string]uint64, *GCStats, error)
+	RunGC(ctx context.Context, rateLimit *rate.Limiter, staleKeysPrevRound map[string]uint64, minID idpool.ID, maxID idpool.ID) (map[string]uint64, *GCStats, error)
 
 	// RunLocksGC reaps stale or unused locks within the Backend. It is used by
 	// the cilium-operator and is not invoked by cilium-agent. Returns
@@ -265,11 +266,15 @@ type Backend interface {
 // After creation, IDs can be allocated with Allocate() and released with
 // Release()
 func NewAllocator(typ AllocatorKey, backend Backend, opts ...AllocatorOption) (*Allocator, error) {
+	// Calculate initial ID range with cluster ID
+	minID := option.Config.ClusterID<<identity.ClusterIDShift + 1
+	maxID := minID + (1<<identity.ClusterIDShift - 1)
+
 	a := &Allocator{
 		keyType:      typ,
 		backend:      backend,
-		min:          idpool.ID(1),
-		max:          idpool.ID(^uint64(0)),
+		min:          idpool.ID(minID),
+		max:          idpool.ID(maxID),
 		localKeys:    newLocalKeys(),
 		stopGC:       make(chan struct{}),
 		suffix:       uuid.New().String()[:10],
@@ -335,12 +340,20 @@ func WithEvents(events AllocatorEventChan) AllocatorOption {
 
 // WithMin sets the minimum identifier to be allocated
 func WithMin(id idpool.ID) AllocatorOption {
-	return func(a *Allocator) { a.min = id }
+	return func(a *Allocator) {
+		if id > a.min {
+			a.min = id
+		}
+	}
 }
 
 // WithMax sets the maximum identifier to be allocated
 func WithMax(id idpool.ID) AllocatorOption {
-	return func(a *Allocator) { a.max = id }
+	return func(a *Allocator) {
+		if id < a.max {
+			a.max = id
+		}
+	}
 }
 
 // WithPrefixMask sets the prefix used for all ID allocations. If set, the mask
@@ -797,7 +810,7 @@ func (a *Allocator) Release(ctx context.Context, key AllocatorKey) (lastUse bool
 
 // RunGC scans the kvstore for unused master keys and removes them
 func (a *Allocator) RunGC(rateLimit *rate.Limiter, staleKeysPrevRound map[string]uint64) (map[string]uint64, *GCStats, error) {
-	return a.backend.RunGC(context.TODO(), rateLimit, staleKeysPrevRound)
+	return a.backend.RunGC(context.TODO(), rateLimit, staleKeysPrevRound, a.min, a.max)
 }
 
 // RunLocksGC scans the kvstore for stale locks and removes them

pkg/kvstore/allocator/allocator.go
@@ -426,7 +426,13 @@ func (k *kvstoreBackend) RunLocksGC(ctx context.Context, staleKeysPrevRound map[
 }
 
 // RunGC scans the kvstore for unused master keys and removes them
-func (k *kvstoreBackend) RunGC(ctx context.Context, rateLimit *rate.Limiter, staleKeysPrevRound map[string]uint64) (map[string]uint64, *allocator.GCStats, error) {
+func (k *kvstoreBackend) RunGC(
+	ctx context.Context,
+	rateLimit *rate.Limiter,
+	staleKeysPrevRound map[string]uint64,
+	minID, maxID idpool.ID,
+) (map[string]uint64, *allocator.GCStats, error) {
+
 	// fetch list of all /id/ keys
 	allocated, err := k.backend.ListPrefix(ctx, k.idPrefix)
 	if err != nil {
@@ -438,11 +444,36 @@ func (k *kvstoreBackend) RunGC(ctx context.Context, rateLimit *rate.Limiter, sta
 
 	staleKeys := map[string]uint64{}
 
+	min := uint64(minID)
+	max := uint64(maxID)
+	reasonOutOfRange := fmt.Sprintf("out of local cluster identity range [%d,%d]", min, max)
+
 	// iterate over /id/
 	for key, v := range allocated {
 		// if k.lockless {
 		// FIXME: Add DeleteOnZeroCount support
 		// }
+
+		// Parse identity ID
+		items := strings.Split(key, "/")
+		if len(items) == 0 {
+			log.WithField(fieldKey, key).WithError(err).Warning("Unknown identity key found, skipping")
+			continue
+		}
+
+		if identityID, err := strconv.ParseUint(items[len(items)-1], 10, 32); err != nil {
+			log.WithField(fieldKey, key).WithError(err).Warning("Parse identity failed, skipping")
+			continue
+		} else {
+			// We should not GC those identities that are out of our scope
+			if identityID < min || identityID > max {
+				log.WithFields(logrus.Fields{
+					fieldKey: key,
+					"reason": reasonOutOfRange,
+				}).Debug("Skipping this key")
+				continue
+			}
+		}
 
 		lock, err := k.lockPath(ctx, key)
 		if err != nil {

pkg/allocator/allocator_test.go
@@ -171,7 +171,7 @@ func (d *dummyBackend) RunLocksGC(_ context.Context, _ map[string]kvstore.Value)
 	return nil, nil
 }
 
-func (d *dummyBackend) RunGC(context.Context, *rate.Limiter, map[string]uint64) (map[string]uint64, *GCStats, error) {
+func (d *dummyBackend) RunGC(context.Context, *rate.Limiter, map[string]uint64, idpool.ID, idpool.ID) (map[string]uint64, *GCStats, error) {
 	return nil, nil, nil
 }
 

pkg/k8s/identitybackend/identity.go
@@ -115,7 +115,7 @@ func (c *crdBackend) RunLocksGC(_ context.Context, _ map[string]kvstore.Value) (
 	return nil, nil
 }
 
-func (c *crdBackend) RunGC(context.Context, *rate.Limiter, map[string]uint64) (map[string]uint64, *allocator.GCStats, error) {
+func (c *crdBackend) RunGC(context.Context, *rate.Limiter, map[string]uint64, idpool.ID, idpool.ID) (map[string]uint64, *allocator.GCStats, error) {
 	return nil, nil, nil
 }
 

pkg/kvstore/allocator/allocator_test.go
@@ -300,6 +300,82 @@ func (s *AllocatorSuite) TestGC(c *C) {
 	c.Assert(key, IsNil)
 }
 
+func (s *AllocatorSuite) TestGC_ShouldSkipOutOfRangeIdentites(c *C) {
+
+	// Allocator1: allocator under test
+	backend, err := NewKVStoreBackend(randomTestName(), "a", TestAllocatorKey(""), kvstore.Client())
+	c.Assert(err, IsNil)
+
+	maxID1 := idpool.ID(4 + c.N)
+	allocator1, err := allocator.NewAllocator(TestAllocatorKey(""), backend, allocator.WithMax(maxID1), allocator.WithoutGC())
+	c.Assert(err, IsNil)
+	c.Assert(allocator1, Not(IsNil))
+
+	defer allocator1.DeleteAllKeys()
+	defer allocator1.Delete()
+
+	allocator1.DeleteAllKeys()
+
+	shortKey1 := TestAllocatorKey("1;")
+	shortID1, _, _, err := allocator1.Allocate(context.Background(), shortKey1)
+	c.Assert(err, IsNil)
+	c.Assert(shortID1, Not(Equals), 0)
+
+	allocator1.Release(context.Background(), shortKey1)
+
+	// Alloctor2: with a non-overlapping range compared with allocator1
+	backend2, err := NewKVStoreBackend(randomTestName(), "a", TestAllocatorKey(""), kvstore.Client())
+	c.Assert(err, IsNil)
+
+	minID2 := maxID1 + 1
+	maxID2 := minID2 + 4
+	allocator2, err := allocator.NewAllocator(TestAllocatorKey(""), backend2, allocator.WithMin(minID2), allocator.WithMax(maxID2), allocator.WithoutGC())
+	c.Assert(err, IsNil)
+	c.Assert(allocator2, Not(IsNil))
+
+	shortKey2 := TestAllocatorKey("2;")
+	shortID2, _, _, err := allocator2.Allocate(context.Background(), shortKey2)
+	c.Assert(err, IsNil)
+	c.Assert(shortID2, Not(Equals), 0)
+
+	defer allocator2.DeleteAllKeys()
+	defer allocator2.Delete()
+
+	allocator2.Release(context.Background(), shortKey2)
+
+	// Perform GC with allocator1: there are two entries in kvstore currently
+	rateLimiter := rate.NewLimiter(10*time.Second, 100)
+
+	keysToDelete := map[string]uint64{}
+	keysToDelete, _, err = allocator1.RunGC(rateLimiter, keysToDelete)
+	c.Assert(err, IsNil)
+	// But, only one will be filtered out and GC'ed
+	c.Assert(len(keysToDelete), Equals, 1)
+	keysToDelete, _, err = allocator1.RunGC(rateLimiter, keysToDelete)
+	c.Assert(err, IsNil)
+	c.Assert(len(keysToDelete), Equals, 0)
+
+	// Wait for cache to be updated via delete notification
+	c.Assert(testutils.WaitUntil(func() bool {
+		key, err := allocator1.GetByID(context.TODO(), shortID1)
+		if err != nil {
+			c.Error(err)
+			return false
+		}
+		return key == nil
+	}, 5*time.Second), IsNil)
+
+	// The key created with allocator1 should be GC'd
+	key, err := allocator1.GetByID(context.TODO(), shortID1)
+	c.Assert(err, IsNil)
+	c.Assert(key, IsNil)
+
+	// The key created with allocator2 should NOT be GC'd
+	key2, err := allocator2.GetByID(context.TODO(), shortID2)
+	c.Assert(err, IsNil)
+	c.Assert(key2, Not(IsNil))
+}
+
 func testAllocator(c *C, maxID idpool.ID, allocatorName string, suffix string) {
 	backend, err := NewKVStoreBackend(allocatorName, "a", TestAllocatorKey(""), kvstore.Client())
 	c.Assert(err, IsNil)

commit 3fccbddd8ab98088c8730f431d1d5744efdca809

operator: fix identity GC collection
As the operator uses a different constructor for the GC of the
allocator, this constructor was not being initialized with the min and
max values of the identities that it should be GC. This commit adds
those options making it possible for the Operator to GC those
identities.

Fixes: 706d41ca8935 ("operator: only GC identity keys of its own cluster")
Signed-off-by: André Martins <andre@cilium.io>

operator/identity_gc.go
@@ -11,7 +11,9 @@ import (
 	"github.com/cilium/cilium/operator/metrics"
 	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/allocator"
+	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/idpool"
 	"github.com/cilium/cilium/pkg/inctimer"
 	"github.com/cilium/cilium/pkg/kvstore"
 	kvstoreallocator "github.com/cilium/cilium/pkg/kvstore/allocator"
@@ -24,7 +26,9 @@ func startKvstoreIdentityGC() {
 	if err != nil {
 		log.WithError(err).Fatal("Unable to initialize kvstore backend for identity allocation")
 	}
-	a := allocator.NewAllocatorForGC(backend)
+	minID := idpool.ID(identity.MinimalAllocationIdentity)
+	maxID := idpool.ID(identity.MaximumAllocationIdentity)
+	a := allocator.NewAllocatorForGC(backend, allocator.WithMin(minID), allocator.WithMax(maxID))
 
 	successfulRuns := 0
 	failedRuns := 0

operator/kvstore_watchdog.go
@@ -10,7 +10,9 @@ import (
 
 	"github.com/cilium/cilium/pkg/allocator"
 	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/idpool"
 	"github.com/cilium/cilium/pkg/inctimer"
 	"github.com/cilium/cilium/pkg/kvstore"
 	kvstoreallocator "github.com/cilium/cilium/pkg/kvstore/allocator"
@@ -61,7 +63,10 @@ func startKvstoreWatchdog() {
 	if err != nil {
 		log.WithError(err).Fatal("Unable to initialize kvstore backend for identity garbage collection")
 	}
-	a := allocator.NewAllocatorForGC(backend)
+
+	minID := idpool.ID(identity.MinimalAllocationIdentity)
+	maxID := idpool.ID(identity.MaximumAllocationIdentity)
+	a := allocator.NewAllocatorForGC(backend, allocator.WithMin(minID), allocator.WithMax(maxID))
 
 	keysToDelete := map[string]kvstore.Value{}
 	go func() {

pkg/allocator/allocator.go
@@ -155,7 +155,21 @@ type Allocator struct {
 type AllocatorOption func(*Allocator)
 
 // NewAllocatorForGC returns an allocator that can be used to run RunGC()
-func NewAllocatorForGC(backend Backend) *Allocator {
+//
+// The allocator can be configured by passing in additional options:
+//  - WithMin(id) - minimum ID to allocate (default: 1)
+//  - WithMax(id) - maximum ID to allocate (default max(uint64))
+func NewAllocatorForGC(backend Backend, opts ...AllocatorOption) *Allocator {
+	a := &Allocator{
+		backend: backend,
+		min:     idpool.ID(1),
+		max:     idpool.ID(^uint64(0)),
+	}
+
+	for _, fn := range opts {
+		fn(a)
+	}
+
 	return &Allocator{backend: backend}
 }
 