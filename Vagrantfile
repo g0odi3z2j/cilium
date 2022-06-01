g0odi3z2j/cilium

commit 4721721f61f326c28be8b3ba9612fcc327c5e9be

vagrant: Don't recreate natnetworks
We are hitting issues with the natnetwork creations, both in CI and in
the dev. VM setup. In both cases, it fails to create because the
natnetwork apparently already exists. For the dev. VM, that happens
because the removal of natnetworks sometimes fail, maybe because it's
still in use while being removed. For the CI, it's unclear why it fails
as a previous listing of natnetworks shows nothing.

In any case, one way to work around this is to avoid recreating the
natnetworks each time the VMs are booted. Instead, we can create only
the natnetworks we need and delete the rest. If the creations fail, we
assume it's because they already exist and ignore the failure.

To be able to ignore the creation failures, we must move them outside of
the Vagrant interface configuration and into a proper Shell script,
which we run in a pre-provisioning step.

Signed-off-by: Paul Chaignon <paul@cilium.io>

Vagrantfile
@@ -46,6 +46,11 @@ end
 
 $cleanup = <<SCRIPT
 i=1
+while [ "$i" -le "$((num_workers+1))" ]; do
+    VBoxManage natnetwork add --netname natnet$i --network fd08::/64 --ipv6 on --enable
+    i=$((i+1))
+done 2>/dev/null
+
 res=0
 while [ "$res" == "0" ]; do
     VBoxManage natnetwork remove --netname natnet$i
@@ -217,7 +222,6 @@ Vagrant.configure(2) do |config|
         cm.vm.network "private_network",
             ip: "192.168.59.15"
         cm.vm.provider "virtualbox" do |vb|
-            vb.customize ["natnetwork", "add", "--netname", "natnet1", "--network", "fd08::/64", "--ipv6", "on", "--enable"]
             vb.customize ["modifyvm", :id, "--nic4", "natnetwork"]
             vb.customize ["modifyvm", :id, "--nat-network4", "natnet1"]
         end
@@ -280,7 +284,6 @@ Vagrant.configure(2) do |config|
             node.vm.network "private_network",
                 ip: "192.168.59.15"
             node.vm.provider "virtualbox" do |vb|
-                vb.customize ["natnetwork", "add", "--netname", "natnet#{n+2}", "--network", "fd08::/64", "--ipv6", "on", "--enable"]
                 vb.customize ["modifyvm", :id, "--nic4", "natnetwork"]
                 vb.customize ["modifyvm", :id, "--nat-network4", "natnet#{n+2}"]
             end

test/Vagrantfile
@@ -59,6 +59,11 @@ end
 
 $cleanup = <<SCRIPT
 i=1
+while [ "$i" -le "$K8S_NODES" ]; do
+    VBoxManage natnetwork add --netname natnet$i --network fd08::/64 --ipv6 on --enable
+    i=$((i+1))
+done 2>/dev/null
+
 res=0
 while [ "$res" == "0" ]; do
     VBoxManage natnetwork remove --netname natnet$i
@@ -218,7 +223,6 @@ Vagrant.configure("2") do |config|
             server.vm.network "private_network",
                 ip: "192.168.59.15"
             server.vm.provider "virtualbox" do |vb|
-                vb.customize ["natnetwork", "add", "--netname", "natnet#{i}", "--network", "fd08::/64", "--ipv6", "on", "--enable"]
                 vb.customize ["modifyvm", :id, "--nic5", "natnetwork"]
                 vb.customize ["modifyvm", :id, "--nat-network5", "natnet#{i}"]
             end

commit 8d465c1d669426c52af80af72c4bb3bfe9b2b9b0

vagrant: Fix IPv6 NAT setup
Commit 85bc9e8f ("vagrant: Enable IPv6 connectivity to the outside
world") added a VirtualBox NAT service to the dev. and test VMs to allow
IPv6 connectivity to the outside world. This service includes a new
device and associated routes. Commit 4721721f ("vagrant: Don't recreate
natnetworks") moved the device setup much earlier in the VM
provisioning, as a Bash script instead of Ruby code.

However, this Bash script refers to Ruby variable $K8S_NODES. That
variable doesn't exist in Bash unless it is set as an environment
variable. That's always the case in CI, but not necessarily when running
locally. Hence, the NAT service setup would fail locally and IPv6
traffic wouldn't egress the VM.

In addition, it seems the VirtualBox doesn't accept an IPv6 address for
the --network argument when setting the NAT service. As a result of
this, it seems the NAT service setup partially fails and doesn't
recognize IPv6 traffic.

Tests k8s-1.{17,18,19,20,21,22}-kernel-4.9 started to fail because of
this second issue. Unfortunately, these tests are not run on pull
requests anymore so we missed it. In addition, since we've taken the
habit of disabling and forgetting about any test that's flaky, the IPv6
masquerading tests were also disabled.

Fixes: 4721721f ("vagrant: Don't recreate natnetworks")
Signed-off-by: Paul Chaignon <paul@cilium.io>

Vagrantfile
@@ -47,7 +47,7 @@ end
 $cleanup = <<SCRIPT
 i=1
 while [ "$i" -le "$((num_workers+1))" ]; do
-    VBoxManage natnetwork add --netname natnet$i --network fd08::/64 --ipv6 on --enable
+    VBoxManage natnetwork add --netname natnet$i --network 192.168.0.0/16 --ipv6 on --enable
     i=$((i+1))
 done 2>/dev/null
 

test/Vagrantfile
@@ -59,8 +59,9 @@ end
 
 $cleanup = <<SCRIPT
 i=1
-while [ "$i" -le "$K8S_NODES" ]; do
-    VBoxManage natnetwork add --netname natnet$i --network fd08::/64 --ipv6 on --enable
+k8s_nodes="${K8S_NODES:-2}"
+while [ "$i" -le "$k8s_nodes" ]; do
+    VBoxManage natnetwork add --netname natnet$i --network 192.168.0.0/16 --ipv6 on --enable
     i=$((i+1))
 done 2>/dev/null
 