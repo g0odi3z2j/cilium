g0odi3z2j/cilium

commit 9482960cb90e8f422faa9125e60c6f3a7a853556

vtep: VTEP policy routing setup when L7 policy enabled
L7 policies redirect packets on egress of bpf_lxc to userspace
proxy. When packets leave the proxy, they don't go back through
bpf_lxc again, so the VTEP redirectons won't be enforced. VTEP
redirection is only affected for pod with L7 egress policy and
the pod send request to VTEP, non-VTEP request is not affected.

Create VTEP specific policy routing rule and route table so
updating VTEP policy routing does not intefere with other policy
routing managed by Cilium agent, the VTEP specific policy routing
has lower priority than L7 fwmark and local route rules, otherwise
traffic with L7 policy to VTEP device will be interrupted.
see https://github.com/cilium/cilium/issues/19708

Joe Stringer helped with code suggestion to reduce code duplicate
on VTEP route removal.

For example:

ip rule list

9:	from all fwmark 0x200/0xf00 lookup 2004
10:	from all fwmark 0xa00/0xf00 lookup 2005
100:	from all lookup local
112:	from all to 10.1.34.0/24 lookup 202
112:	from all to 10.1.1.0/24 lookup 202
32766:	from all lookup main
32767:	from all lookup default

ip r s table 202

10.1.1.0/24 dev cilium_host scope link
10.1.34.0/24 dev cilium_host scope link

Users could change VTEP CIDR at runtime with cilium cli:

cilium config set vtep-cidr "10.1.32.0/24 10.1.1.0/24"

✨ Patching ConfigMap cilium-config with vtep-cidr=10.1.32.0/24 10.1.1.0/24...
♻️  Restarted Cilium pods

The rule and route will be updated properly

ip rule list
9:	from all fwmark 0x200/0xf00 lookup 2004
10:	from all fwmark 0xa00/0xf00 lookup 2005
100:	from all lookup local
112:	from all to 10.1.1.0/24 lookup 202
112:	from all to 10.1.32.0/24 lookup 202
32766:	from all lookup main
32767:	from all lookup default

ip r s table 202

10.1.1.0/24 dev cilium_host scope link
10.1.32.0/24 dev cilium_host scope link

Suggested-by: Joe Stringer <joe@cilium.io>
Signed-off-by: Vincent Li <v.li@f5.com>

daemon/cmd/datapath.go
@@ -13,12 +13,15 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
 
+	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/datapath"
 	datapathIpcache "github.com/cilium/cilium/pkg/datapath/ipcache"
 	"github.com/cilium/cilium/pkg/datapath/linux/ipsec"
+	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/probes"
+	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/endpointmanager"
 	"github.com/cilium/cilium/pkg/identity"
@@ -40,6 +43,7 @@ import (
 	"github.com/cilium/cilium/pkg/maps/signalmap"
 	"github.com/cilium/cilium/pkg/maps/tunnel"
 	"github.com/cilium/cilium/pkg/maps/vtep"
+	"github.com/cilium/cilium/pkg/mtu"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/source"
@@ -275,12 +279,11 @@ func (d *Daemon) syncEndpointsAndHostIPs() error {
 		if err != nil {
 			return err
 		}
-		if option.Config.EnableL7Proxy {
-			log.WithField(logfields.URL, "https://github.com/cilium/cilium/issues/19699").
-				Warningf("Both VTEP redirection (%s) and L7 proxy (--%s) are enabled. This is currently not fully supported: "+
-					"if the endpoint has a L7 policy and send request to VTEP devices , endpoint traffic will not go through VTEP devices.", option.EnableVTEP, option.EnableL7Proxy)
-
+		err = setupRouteToVtepCidr()
+		if err != nil {
+			return err
 		}
+
 	}
 
 	return nil
@@ -512,6 +515,96 @@ func setupVTEPMapping() error {
 	}
 	return nil
 
+}
+
+func setupRouteToVtepCidr() error {
+	routeCidrs := []*cidr.CIDR{}
+
+	filter := &netlink.Route{
+		Table: linux_defaults.RouteTableVtep,
+	}
+
+	routes, err := netlink.RouteListFiltered(netlink.FAMILY_V4, filter, netlink.RT_FILTER_TABLE)
+	if err != nil {
+		return fmt.Errorf("failed to list routes: %w", err)
+	}
+	for _, rt := range routes {
+		log.WithFields(logrus.Fields{
+			logfields.IPAddr: rt.Dst.String(),
+		}).Info("VTEP route")
+		rtCIDR, err := cidr.ParseCIDR(rt.Dst.String())
+		if err != nil {
+			return fmt.Errorf("Invalid VTEP Route CIDR: %w", err)
+		}
+		routeCidrs = append(routeCidrs, rtCIDR)
+	}
+
+	addedVtepRoutes, removedVtepRoutes := cidr.DiffCIDRLists(routeCidrs, option.Config.VtepCIDRs)
+	vtepMTU := mtu.EthernetMTU - mtu.TunnelOverhead
+
+	if option.Config.EnableL7Proxy {
+		for _, prefix := range addedVtepRoutes {
+			ip4 := prefix.IP.To4()
+			if ip4 == nil {
+				return fmt.Errorf("Invalid VTEP CIDR IPv4 address: %v", ip4)
+			}
+			r := route.Route{
+				Device: defaults.HostDevice,
+				Prefix: *prefix.IPNet,
+				Scope:  netlink.SCOPE_LINK,
+				MTU:    vtepMTU,
+				Table:  linux_defaults.RouteTableVtep,
+			}
+			if _, err := route.Upsert(r); err != nil {
+				return fmt.Errorf("Update VTEP CIDR route error: %w", err)
+			}
+			log.WithFields(logrus.Fields{
+				logfields.IPAddr: r.Prefix.String(),
+			}).Info("VTEP route added")
+
+			rule := route.Rule{
+				Priority: linux_defaults.RulePriorityVtep,
+				To:       prefix.IPNet,
+				Table:    linux_defaults.RouteTableVtep,
+			}
+			if err := route.ReplaceRule(rule); err != nil {
+				return fmt.Errorf("Update VTEP CIDR rule error: %w", err)
+			}
+		}
+	} else {
+		removedVtepRoutes = routeCidrs
+	}
+
+	for _, prefix := range removedVtepRoutes {
+		ip4 := prefix.IP.To4()
+		if ip4 == nil {
+			return fmt.Errorf("Invalid VTEP CIDR IPv4 address: %v", ip4)
+		}
+		r := route.Route{
+			Device: defaults.HostDevice,
+			Prefix: *prefix.IPNet,
+			Scope:  netlink.SCOPE_LINK,
+			MTU:    vtepMTU,
+			Table:  linux_defaults.RouteTableVtep,
+		}
+		if err := route.Delete(r); err != nil {
+			return fmt.Errorf("Delete VTEP CIDR route error: %w", err)
+		}
+		log.WithFields(logrus.Fields{
+			logfields.IPAddr: r.Prefix.String(),
+		}).Info("VTEP route removed")
+
+		rule := route.Rule{
+			Priority: linux_defaults.RulePriorityVtep,
+			To:       prefix.IPNet,
+			Table:    linux_defaults.RouteTableVtep,
+		}
+		if err := route.DeleteRule(rule); err != nil {
+			return fmt.Errorf("Delete VTEP CIDR rule error: %w", err)
+		}
+	}
+
+	return nil
 }
 
 // Datapath returns a reference to the datapath implementation.

pkg/datapath/linux/linux_defaults/linux_defaults.go
@@ -16,6 +16,9 @@ const (
 	// rules
 	RouteTableWireguard = 201
 
+	// RouteTableVtep is the default table ID to use for VTEP routing rules
+	RouteTableVtep = 202
+
 	// RouteTableEgressGatewayInterfacesOffset is the offset for the per-ENI
 	// egress gateway routing tables.
 	// Each ENI interface will have its own table starting with this offset. It
@@ -93,6 +96,9 @@ const (
 	// associated, but from the main routing table instead.
 	// This priority is before the egress priority.
 	RulePriorityNodeport = RulePriorityEgress - 1
+
+	// RulePriorityVtep is the priority of the rule used for routing packets to VTEP device
+	RulePriorityVtep = 112
 
 	// TunnelDeviceName the default name of the tunnel device when using vxlan
 	TunnelDeviceName = "cilium_vxlan"

commit 5397943eef93e6edbc93bf4eb9dbe312597360e4

daemon: Fix build after VTEP routes conflict
Commit 9482960cb90e ("vtep: VTEP policy routing setup when L7 policy
enabled") was recently merged without rebasing on top of master, which
introduced a compile-time conflict vs commit d591ad6d9c55
("datapath/linux/route: remove unused Upsert bool return value"). Fix it
by updating the Upsert() call.

CC: Vincent Li <v.li@f5.com>
Fixes: 9482960cb90e ("vtep: VTEP policy routing setup when L7 policy enabled")
Signed-off-by: Joe Stringer <joe@cilium.io>

daemon/cmd/datapath.go
@@ -555,7 +555,7 @@ func setupRouteToVtepCidr() error {
 				MTU:    vtepMTU,
 				Table:  linux_defaults.RouteTableVtep,
 			}
-			if _, err := route.Upsert(r); err != nil {
+			if err := route.Upsert(r); err != nil {
 				return fmt.Errorf("Update VTEP CIDR route error: %w", err)
 			}
 			log.WithFields(logrus.Fields{