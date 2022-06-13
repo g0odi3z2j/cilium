g0odi3z2j/cilium

commit c31857177f6bf2d76fcdf435173307b40974d39a

operator: merge duplicate k8s watchers
In certain cases the Cilium Operator could end up with two CiliumNode
k8s watchers. This commit re-writes the code in a way that a single
watcher will be used for all cases.

Signed-off-by: André Martins <andre@cilium.io>

operator/cilium_node.go
@@ -1,16 +1,21 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2019-2020 Authors of Cilium
+// Copyright 2019-2021 Authors of Cilium
 
 package main
 
 import (
 	"context"
+	"strings"
 
 	"github.com/cilium/cilium/pkg/ipam/allocator"
 	"github.com/cilium/cilium/pkg/k8s"
 	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/informer"
 	v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	"github.com/cilium/cilium/pkg/kvstore/store"
+	nodeStore "github.com/cilium/cilium/pkg/node/store"
+	nodeTypes "github.com/cilium/cilium/pkg/node/types"
+	"github.com/cilium/cilium/pkg/option"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -20,16 +25,181 @@ import (
 
 var (
 	// ciliumNodeStore contains all CiliumNodes present in k8s.
-	// Warning: The CiliumNodes stored in the cache are not intended to be
-	// used for Update operations in k8s as some of its fields are not
-	// populated.
 	ciliumNodeStore cache.Store
 
 	k8sCiliumNodesCacheSynced = make(chan struct{})
 )
 
-func startSynchronizingCiliumNodes(nodeManager allocator.NodeEventHandler) {
-	log.Info("Starting to synchronize CiliumNode custom resources")
+func startSynchronizingCiliumNodes(nodeManager allocator.NodeEventHandler, withKVStore bool) error {
+	var (
+		resourceEventHandler  = cache.ResourceEventHandlerFuncs{}
+		ciliumNodeConvertFunc = k8s.ConvertToCiliumNode
+	)
+
+	if withKVStore {
+		log.Info("Starting to synchronize CiliumNode custom resources to KVStore")
+
+		ciliumNodeKVStore, err := store.JoinSharedStore(store.Configuration{
+			Prefix:     nodeStore.NodeStorePrefix,
+			KeyCreator: nodeStore.KeyCreator,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Start a go routine to GC all CiliumNodes from the KVStore that are
+		// no longer running.
+		go func() {
+			<-k8sCiliumNodesCacheSynced
+			// Since we processed all events received from k8s we know that
+			// at this point the list in ciliumNodeStore should be the source of
+			// truth and we need to delete all nodes in the kvNodeStore that are
+			// *not* present in the ciliumNodeStore.
+			listOfCiliumNodes := ciliumNodeStore.ListKeys()
+
+			kvStoreNodes := ciliumNodeKVStore.SharedKeysMap()
+			for _, ciliumNode := range listOfCiliumNodes {
+				// The remaining kvStoreNodes are leftovers that need to be GCed
+				kvStoreNodeName := nodeTypes.GetKeyNodeName(option.Config.ClusterName, ciliumNode)
+				delete(kvStoreNodes, kvStoreNodeName)
+			}
+
+			for _, kvStoreNode := range kvStoreNodes {
+				// Only delete the nodes that belong to our cluster
+				if strings.HasPrefix(kvStoreNode.GetKeyName(), option.Config.ClusterName) {
+					ciliumNodeKVStore.DeleteLocalKey(context.TODO(), kvStoreNode)
+				}
+			}
+		}()
+
+		// nodeManager is nil so we don't need to handle any events managed by
+		// the nodeManager and the events from Kubernetes will only be relevant
+		// for KVStore.
+		if nodeManager == nil {
+			resourceEventHandler = cache.ResourceEventHandlerFuncs{
+				AddFunc: func(obj interface{}) {
+					if ciliumNode := k8s.ObjToCiliumNode(obj); ciliumNode != nil {
+						nodeNew := nodeTypes.ParseCiliumNode(ciliumNode)
+						ciliumNodeKVStore.UpdateKeySync(context.TODO(), &nodeNew)
+					} else {
+						log.Warningf("Unknown CiliumNode object type %T received: %+v", obj, obj)
+					}
+				},
+				UpdateFunc: func(oldObj, newObj interface{}) {
+					if oldNode := k8s.ObjToCiliumNode(oldObj); oldNode != nil {
+						if newNode := k8s.ObjToCiliumNode(newObj); newNode != nil {
+							if oldNode.DeepEqual(newNode) {
+								return
+							}
+							nodeNew := nodeTypes.ParseCiliumNode(newNode)
+							ciliumNodeKVStore.UpdateKeySync(context.TODO(), &nodeNew)
+						} else {
+							log.Warningf("Unknown CiliumNode object type %T received: %+v", newNode, newNode)
+						}
+					} else {
+						log.Warningf("Unknown CiliumNode object type %T received: %+v", oldNode, oldNode)
+					}
+				},
+				DeleteFunc: func(obj interface{}) {
+					if ciliumNode := k8s.ObjToCiliumNode(obj); ciliumNode != nil {
+						deletedNode := nodeTypes.ParseCiliumNode(ciliumNode)
+						ciliumNodeKVStore.DeleteLocalKey(context.TODO(), &deletedNode)
+					} else {
+						log.Warningf("Unknown CiliumNode object type %T received: %+v", obj, obj)
+					}
+				},
+			}
+		} else {
+			// nodeManager not nil thus the events will be processed by
+			// ciliumNodeKVStore and the nodeManager
+			resourceEventHandler = cache.ResourceEventHandlerFuncs{
+				AddFunc: func(obj interface{}) {
+					if ciliumNode := k8s.ObjToCiliumNode(obj); ciliumNode != nil {
+						nodeNew := nodeTypes.ParseCiliumNode(ciliumNode)
+						ciliumNodeKVStore.UpdateKeySync(context.TODO(), &nodeNew)
+						// node is deep copied before it is stored in pkg/aws/eni
+						nodeManager.Create(ciliumNode)
+					} else {
+						log.Warningf("Unknown CiliumNode object type %T received: %+v", obj, obj)
+					}
+				},
+				UpdateFunc: func(oldObj, newObj interface{}) {
+					if oldNode := k8s.ObjToCiliumNode(oldObj); oldNode != nil {
+						if newNode := k8s.ObjToCiliumNode(newObj); newNode != nil {
+							if oldNode.DeepEqual(newNode) {
+								return
+							}
+							nodeNew := nodeTypes.ParseCiliumNode(newNode)
+							ciliumNodeKVStore.UpdateKeySync(context.TODO(), &nodeNew)
+							// node is deep copied before it is stored in pkg/aws/eni
+							nodeManager.Update(newNode)
+						} else {
+							log.Warningf("Unknown CiliumNode object type %T received: %+v", newNode, newNode)
+						}
+					} else {
+						log.Warningf("Unknown CiliumNode object type %T received: %+v", oldNode, oldNode)
+					}
+				},
+				DeleteFunc: func(obj interface{}) {
+					if ciliumNode := k8s.ObjToCiliumNode(obj); ciliumNode != nil {
+						deletedNode := nodeTypes.ParseCiliumNode(ciliumNode)
+						ciliumNodeKVStore.DeleteLocalKey(context.TODO(), &deletedNode)
+						nodeManager.Delete(ciliumNode.Name)
+					} else {
+						log.Warningf("Unknown CiliumNode object type %T received: %+v", obj, obj)
+					}
+				},
+			}
+		}
+
+	} else {
+		log.Info("Starting to synchronize CiliumNode custom resources")
+		if nodeManager == nil {
+			// Both nodeManager and KVStore are nil. We don't need to handle
+			// any watcher events, but we will need to keep all CiliumNodes in
+			// memory because 'ciliumNodeStore' is used across the operator
+			// to get the latest state of a CiliumNode.
+
+			// Since we won't be handling any events we don't need to convert
+			// objects.
+			ciliumNodeConvertFunc = nil
+		} else {
+			// nodeManager not nil but the KVStore is nil thus the events
+			// will only be relevant to the nodeManager.
+			resourceEventHandler = cache.ResourceEventHandlerFuncs{
+				AddFunc: func(obj interface{}) {
+					if ciliumNode := k8s.ObjToCiliumNode(obj); ciliumNode != nil {
+						// node is deep copied before it is stored in pkg/aws/eni
+						nodeManager.Create(ciliumNode)
+					} else {
+						log.Warningf("Unknown CiliumNode object type %T received: %+v", obj, obj)
+					}
+				},
+				UpdateFunc: func(oldObj, newObj interface{}) {
+					if oldNode := k8s.ObjToCiliumNode(oldObj); oldNode != nil {
+						if newNode := k8s.ObjToCiliumNode(newObj); newNode != nil {
+							if oldNode.DeepEqual(newNode) {
+								return
+							}
+							// node is deep copied before it is stored in pkg/aws/eni
+							nodeManager.Update(newNode)
+						} else {
+							log.Warningf("Unknown CiliumNode object type %T received: %+v", newNode, newNode)
+						}
+					} else {
+						log.Warningf("Unknown CiliumNode object type %T received: %+v", oldNode, oldNode)
+					}
+				},
+				DeleteFunc: func(obj interface{}) {
+					if ciliumNode := k8s.ObjToCiliumNode(obj); ciliumNode != nil {
+						nodeManager.Delete(ciliumNode.Name)
+					} else {
+						log.Warningf("Unknown CiliumNode object type %T received: %+v", obj, obj)
+					}
+				},
+			}
+		}
+	}
 
 	// TODO: The operator is currently storing a full copy of the
 	// CiliumNode resource, as the resource grows, we may want to consider
@@ -40,33 +210,8 @@ func startSynchronizingCiliumNodes(nodeManager allocator.NodeEventHandler) {
 			v2.CNPluralName, v1.NamespaceAll, fields.Everything()),
 		&v2.CiliumNode{},
 		0,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				if node := k8s.ObjToCiliumNode(obj); node != nil {
-					// node is deep copied before it is stored in pkg/aws/eni
-					nodeManager.Create(node)
-				} else {
-					log.Warningf("Unknown CiliumNode object type %T received: %+v", obj, obj)
-				}
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				if oldNode := k8s.ObjToCiliumNode(oldObj); oldNode != nil {
-					if node := k8s.ObjToCiliumNode(newObj); node != nil {
-						if oldNode.DeepEqual(node) {
-							return
-						}
-						// node is deep copied before it is stored in pkg/aws/eni
-						nodeManager.Update(node)
-					}
-				}
-			},
-			DeleteFunc: func(obj interface{}) {
-				if node := k8s.ObjToCiliumNode(obj); node != nil {
-					nodeManager.Delete(node.Name)
-				}
-			},
-		},
-		k8s.ConvertToCiliumNode,
+		resourceEventHandler,
+		ciliumNodeConvertFunc,
 	)
 
 	go func() {
@@ -75,12 +220,8 @@ func startSynchronizingCiliumNodes(nodeManager allocator.NodeEventHandler) {
 	}()
 
 	go ciliumNodeInformer.Run(wait.NeverStop)
-}
 
-func deleteCiliumNode(nodeManager *allocator.NodeEventHandler, name string) {
-	if nodeManager != nil {
-		(*nodeManager).Delete(name)
-	}
+	return nil
 }
 
 type ciliumNodeUpdateImplementation struct{}

operator/allocator_providers.go
@@ -1,32 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2020 Authors of Cilium
+// Copyright 2020-2021 Authors of Cilium
 
 package main
 
 import (
-	"context"
-	"time"
-
 	"github.com/cilium/cilium/pkg/ipam/allocator"
-	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 )
 
 var (
 	allocatorProviders = make(map[string]allocator.AllocatorProvider)
-
-	// NOPNodeManager is a NodeEventHandler that does nothing
-	NOPNodeManager allocator.NodeEventHandler = &nopNodeManager{}
 )
-
-// nopNodeManager is used for allocation types that need no node management.
-// This is needed to allow node watcher maintain a CiliumNode store for the
-// use in CEP GC even when operator does not do anything for IPAM.
-type nopNodeManager struct{}
-
-func (nm *nopNodeManager) Create(resource *v2.CiliumNode) bool { return true }
-
-func (nm *nopNodeManager) Update(resource *v2.CiliumNode) bool { return true }
-
-func (nm *nopNodeManager) Delete(nodeName string) {}
-
-func (nm *nopNodeManager) Resync(context.Context, time.Time) {}

operator/k8s_node.go
@@ -1,128 +1,29 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2019-2020 Authors of Cilium
+// Copyright 2019-2021 Authors of Cilium
 
 package main
 
 import (
 	"context"
 	"encoding/json"
-	"strings"
 	"sync"
 
 	operatorOption "github.com/cilium/cilium/operator/option"
 	"github.com/cilium/cilium/pkg/controller"
-	"github.com/cilium/cilium/pkg/ipam/allocator"
 	"github.com/cilium/cilium/pkg/k8s"
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	v2 "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/typed/cilium.io/v2"
-	"github.com/cilium/cilium/pkg/k8s/informer"
 	v1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	"github.com/cilium/cilium/pkg/k8s/utils"
-	"github.com/cilium/cilium/pkg/kvstore/store"
-	nodeStore "github.com/cilium/cilium/pkg/node/store"
-	nodeTypes "github.com/cilium/cilium/pkg/node/types"
-	"github.com/cilium/cilium/pkg/option"
-
 	core_v1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/cache"
 )
 
-func runNodeWatcher() error {
-	if operatorOption.Config.CNPNodeStatusGCInterval != 0 {
-		go runCNPNodeStatusGC("cnp-node-gc", false, ciliumNodeStore)
-		go runCNPNodeStatusGC("ccnp-node-gc", true, ciliumNodeStore)
-	}
-	return nil
-}
-
-func runNodeWatcherKVStore(nodeManager *allocator.NodeEventHandler) error {
-
-	log.Info("Starting to synchronize k8s nodes to kvstore")
-
-	ciliumNodeKVStore, err := store.JoinSharedStore(store.Configuration{
-		Prefix:     nodeStore.NodeStorePrefix,
-		KeyCreator: nodeStore.KeyCreator,
-	})
-	if err != nil {
-		return err
-	}
-
-	resourceEventHandler := cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			if ciliumNode := k8s.ObjToCiliumNode(obj); ciliumNode != nil {
-				nodeNew := nodeTypes.ParseCiliumNode(ciliumNode)
-				ciliumNodeKVStore.UpdateKeySync(context.TODO(), &nodeNew)
-			}
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			if oldNode := k8s.ObjToCiliumNode(oldObj); oldNode != nil {
-				if newNode := k8s.ObjToCiliumNode(newObj); newNode != nil {
-					if oldNode.DeepEqual(newNode) {
-						return
-					}
-
-					nodeNew := nodeTypes.ParseCiliumNode(newNode)
-					ciliumNodeKVStore.UpdateKeySync(context.TODO(), &nodeNew)
-				}
-			}
-		},
-		DeleteFunc: func(obj interface{}) {
-			n := k8s.ObjToCiliumNode(obj)
-			if n == nil {
-				return
-			}
-			deletedNode := nodeTypes.ParseCiliumNode(n)
-			ciliumNodeKVStore.DeleteLocalKey(context.TODO(), &deletedNode)
-			deleteCiliumNode(nodeManager, n.Name)
-		},
-	}
-
-	nodeStore, nodeController := informer.NewInformer(
-		cache.NewListWatchFromClient(k8s.CiliumClient().CiliumV2().RESTClient(),
-			cilium_v2.CNPluralName, v1.NamespaceAll, fields.Everything()),
-		&cilium_v2.CiliumNode{},
-		0,
-		resourceEventHandler,
-		nil,
-	)
-
-	go nodeController.Run(wait.NeverStop)
-
-	go func() {
-		cache.WaitForCacheSync(wait.NeverStop, nodeController.HasSynced)
-
-		// Since we processed all events received from k8s we know that
-		// at this point the list in ciliumNodeStore should be the source of
-		// truth and we need to delete all nodes in the kvNodeStore that are
-		// *not* present in the ciliumNodeStore.
-		listOfCiliumNodes := ciliumNodeStore.ListKeys()
-
-		kvStoreNodes := ciliumNodeKVStore.SharedKeysMap()
-		for _, ciliumNode := range listOfCiliumNodes {
-			// The remaining kvStoreNodes are leftovers that need to be GCed
-			kvStoreNodeName := nodeTypes.GetKeyNodeName(option.Config.ClusterName, ciliumNode)
-			delete(kvStoreNodes, kvStoreNodeName)
-		}
-
-		for _, kvStoreNode := range kvStoreNodes {
-			// Only delete the nodes that belong to our cluster
-			if strings.HasPrefix(kvStoreNode.GetKeyName(), option.Config.ClusterName) {
-				ciliumNodeKVStore.DeleteLocalKey(context.TODO(), kvStoreNode)
-			}
-		}
-
-	}()
-
-	if operatorOption.Config.CNPNodeStatusGCInterval != 0 {
-		go runCNPNodeStatusGC("cnp-node-gc", false, nodeStore)
-		go runCNPNodeStatusGC("ccnp-node-gc", true, nodeStore)
-	}
-
-	return nil
+func RunCNPNodeStatusGC(nodeStore cache.Store) {
+	go runCNPNodeStatusGC("cnp-node-gc", false, nodeStore)
+	go runCNPNodeStatusGC("ccnp-node-gc", true, nodeStore)
 }
 
 // runCNPNodeStatusGC runs the node status garbage collector for cilium network

operator/main.go
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2018-2020 Authors of Cilium
+// Copyright 2018-2021 Authors of Cilium
 
 // Ensure build fails on versions of Go that are not supported by Cilium.
 // This build tag should be kept in sync with the version specified in go.mod.
@@ -376,8 +376,9 @@ func onOperatorStartLeading(ctx context.Context) {
 	}
 
 	var (
-		nodeManager *allocator.NodeEventHandler
+		nodeManager allocator.NodeEventHandler
 		err         error
+		withKVStore bool
 	)
 
 	log.WithField(logfields.Mode, option.Config.IPAM).Info("Initializing IPAM")
@@ -398,40 +399,12 @@ func onOperatorStartLeading(ctx context.Context) {
 			log.WithError(err).Fatalf("Unable to start %s allocator", ipamMode)
 		}
 
-		startSynchronizingCiliumNodes(nm)
-		nodeManager = &nm
-
-		switch ipamMode {
-		case ipamOption.IPAMClusterPool:
-			// We will use CiliumNodes as the source of truth for the podCIDRs.
-			// Once the CiliumNodes are synchronized with the operator we will
-			// be able to watch for K8s Node events which they will be used
-			// to create the remaining CiliumNodes.
-			<-k8sCiliumNodesCacheSynced
-
-			// We don't want CiliumNodes that don't have podCIDRs to be
-			// allocated with a podCIDR already being used by another node.
-			// For this reason we will call Resync after all CiliumNodes are
-			// synced with the operator to signal the node manager, since it
-			// knows all podCIDRs that are currently set in the cluster, that
-			// it can allocate podCIDRs for the nodes that don't have a podCIDR
-			// set.
-			nm.Resync(context.Background(), time.Time{})
-		}
-	default:
-		startSynchronizingCiliumNodes(NOPNodeManager)
-		nodeManager = &NOPNodeManager
+		nodeManager = nm
 	}
 
 	if operatorOption.Config.BGPAnnounceLBIP {
 		log.Info("Starting LB IP allocator")
 		operatorWatchers.StartLBIPAllocator(ctx, option.Config)
-	}
-
-	if operatorOption.Config.CNPNodeStatusGCInterval != 0 {
-		if err := runNodeWatcher(); err != nil {
-			log.WithError(err).Error("Unable to setup node watcher")
-		}
 	}
 
 	if kvstoreEnabled() {
@@ -509,12 +482,35 @@ func onOperatorStartLeading(ctx context.Context) {
 		}
 
 		if operatorOption.Config.SyncK8sNodes {
-			if err := runNodeWatcherKVStore(nodeManager); err != nil {
-				log.WithError(err).Error("Unable to setup node watcher")
-			}
+			withKVStore = true
 		}
 
 		startKvstoreWatchdog()
+	}
+
+	if err := startSynchronizingCiliumNodes(nodeManager, withKVStore); err != nil {
+		log.WithError(err).Fatal("Unable to setup node watcher")
+	}
+
+	if operatorOption.Config.CNPNodeStatusGCInterval != 0 {
+		RunCNPNodeStatusGC(ciliumNodeStore)
+	}
+
+	if option.Config.IPAM == ipamOption.IPAMClusterPool {
+		// We will use CiliumNodes as the source of truth for the podCIDRs.
+		// Once the CiliumNodes are synchronized with the operator we will
+		// be able to watch for K8s Node events which they will be used
+		// to create the remaining CiliumNodes.
+		<-k8sCiliumNodesCacheSynced
+
+		// We don't want CiliumNodes that don't have podCIDRs to be
+		// allocated with a podCIDR already being used by another node.
+		// For this reason we will call Resync after all CiliumNodes are
+		// synced with the operator to signal the node manager, since it
+		// knows all podCIDRs that are currently set in the cluster, that
+		// it can allocate podCIDRs for the nodes that don't have a podCIDR
+		// set.
+		nodeManager.Resync(context.Background(), time.Time{})
 	}
 
 	if operatorOption.Config.IdentityGCInterval != 0 {

commit 370535638ce79736c5e96ac00bfe1702dc2984f8

operator: connect to the KVStore asynchronously
In some clusters, the KVStore is using etcd-operator which depends on
Cilium agent to be up and ready. However, the Cilium agent, when running
with "ipam: cluster-pool", depends on the Cilium Operator to be
up-and-ready. Cilium Operator will not allocate any addresses because it
depends on the KVStore to be up and ready. To prevent this
chicken-and-egg problem, we will start the KVStore client of Cilium
Operator asynchronously which will then be able to allocate addresses to
Cilium Nodes, unblock Cilium which will be able to start the KVStore.

Fixes: c31857177f6b ("operator: merge duplicate k8s watchers")
Signed-off-by: André Martins <andre@cilium.io>

operator/cilium_node.go
@@ -51,9 +51,10 @@ var (
 
 func startSynchronizingCiliumNodes(ctx context.Context, nodeManager allocator.NodeEventHandler, withKVStore bool) error {
 	var (
-		ciliumNodeKVStore *store.SharedStore
-		err               error
-		syncHandler       func(key string) error
+		ciliumNodeKVStore  *store.SharedStore
+		err                error
+		syncHandler        func(key string) error
+		connectedToKVStore = make(chan struct{})
 
 		resourceEventHandler  = cache.ResourceEventHandlerFuncs{}
 		ciliumNodeConvertFunc = k8s.ConvertToCiliumNode
@@ -63,19 +64,23 @@ func startSynchronizingCiliumNodes(ctx context.Context, nodeManager allocator.No
 	// KVStore is enabled -> we will run the event handler to sync objects into
 	// KVStore.
 	if withKVStore {
-		log.Info("Starting to synchronize CiliumNode custom resources to KVStore")
-
-		ciliumNodeKVStore, err = store.JoinSharedStore(store.Configuration{
-			Prefix:     nodeStore.NodeStorePrefix,
-			KeyCreator: nodeStore.KeyCreator,
-		})
-		if err != nil {
-			return err
-		}
-
+		// Connect to the KVStore asynchronously so that we are able to start
+		// the operator without relying on the KVStore to be up.
 		// Start a go routine to GC all CiliumNodes from the KVStore that are
 		// no longer running.
 		go func() {
+			log.Info("Starting to synchronize CiliumNode custom resources to KVStore")
+
+			ciliumNodeKVStore, err = store.JoinSharedStore(store.Configuration{
+				Prefix:     nodeStore.NodeStorePrefix,
+				KeyCreator: nodeStore.KeyCreator,
+			})
+
+			if err != nil {
+				log.WithError(err).Fatal("Unable to setup node watcher")
+			}
+			close(connectedToKVStore)
+
 			<-k8sCiliumNodesCacheSynced
 			// Since we processed all events received from k8s we know that
 			// at this point the list in ciliumNodeStore should be the source of
@@ -203,6 +208,9 @@ func startSynchronizingCiliumNodes(ctx context.Context, nodeManager allocator.No
 	go func() {
 		cache.WaitForCacheSync(wait.NeverStop, ciliumNodeInformer.HasSynced)
 		close(k8sCiliumNodesCacheSynced)
+		if withKVStore {
+			<-connectedToKVStore
+		}
 		// Only handle events if syncHandler is not nil. If it is nil then
 		// there isn't any event handler set for CiliumNodes events.
 		if syncHandler != nil {