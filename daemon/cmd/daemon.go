g0odi3z2j/cilium

commit 9cad166b696a05df339c970823788b574cd29a41

daemon, node: Remove old, discarded router IPs from `cilium_host`
In the previous commit (referenced below), we forgot to remove the old
router IPs from the actual interface (`cilium_host`). This caused
connectivity issues in user environments where the discarded, stale IPs
were reassigned to pods, causing the ipcache entries for those IPs to
have `remote-node` identity.

To fix this, we remove all IPs from the `cilium_host` interface that
weren't restored during the router IP restoration process. This step
correctly finalizes the restoration process for router IPs.

Fixes: 823433a7a24 ("daemon, node: Fix faulty router IP restoration
logic")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

daemon/cmd/daemon.go
@@ -79,6 +79,7 @@ import (
 	cnitypes "github.com/cilium/cilium/plugins/cilium-cni/types"
 
 	"github.com/sirupsen/logrus"
+	"github.com/vishvananda/netlink"
 	"golang.org/x/sync/semaphore"
 )
 
@@ -274,7 +275,50 @@ func restoreCiliumHostIPs(ipv6 bool, fromK8s net.IP) {
 		fromFS = node.GetInternalIPv4Router()
 	}
 
-	node.RestoreHostIPs(ipv6, fromK8s, fromFS, cidr)
+	restoredIP := node.RestoreHostIPs(ipv6, fromK8s, fromFS, cidr)
+	if err := removeOldRouterState(restoredIP); err != nil {
+		log.WithError(err).Warnf(
+			"Failed to remove old router IPs (restored IP: %s) from cilium_host. Manual intervention is required to remove all other old IPs.",
+			restoredIP,
+		)
+	}
+}
+
+// removeOldRouterState will try to ensure that the only IP assigned to the
+// `cilium_host` interface is the given restored IP. If the given IP is nil,
+// then it attempts to clear all IPs from the interface.
+func removeOldRouterState(restoredIP net.IP) error {
+	l, err := netlink.LinkByName(defaults.HostDevice)
+	if err != nil {
+		return err
+	}
+
+	family := netlink.FAMILY_V6
+	if restoredIP.To4() != nil {
+		family = netlink.FAMILY_V4
+	}
+	addrs, err := netlink.AddrList(l, family)
+	if err != nil {
+		return err
+	}
+	if len(addrs) > 1 {
+		log.Info("More than one router IP was found on the cilium_host device after restoration, cleaning up old router IPs.")
+	}
+
+	var errs []error
+	for _, a := range addrs {
+		if restoredIP != nil && restoredIP.Equal(a.IP) {
+			continue
+		}
+		if err := netlink.AddrDel(l, &a); err != nil {
+			errs = append(errs, fmt.Errorf("failed to remove IP %s: %w", a.IP, err))
+		}
+	}
+	if len(errs) > 0 {
+		return fmt.Errorf("failed to remove all old router IPs: %v", errs)
+	}
+
+	return nil
 }
 
 // NewDaemon creates and returns a new Daemon with the parameters set in c.

pkg/node/address.go
@@ -375,9 +375,11 @@ func AutoComplete() error {
 // filesystem to be the most up-to-date source of truth. The chosen router IP
 // is then checked whether it is contained inside node CIDR (pod CIDR) range.
 // If not, then the router IP is discarded and not restored.
-func RestoreHostIPs(ipv6 bool, fromK8s, fromFS net.IP, cidr *cidr.CIDR) {
+//
+// The restored IP is returned.
+func RestoreHostIPs(ipv6 bool, fromK8s, fromFS net.IP, cidr *cidr.CIDR) net.IP {
 	if !option.Config.EnableHostIPRestore {
-		return
+		return nil
 	}
 
 	var (
@@ -398,6 +400,9 @@ func RestoreHostIPs(ipv6 bool, fromK8s, fromFS net.IP, cidr *cidr.CIDR) {
 			"The router IP (%s) considered for restoration does not belong in the Pod CIDR of the node. Discarding old router IP.",
 			ip,
 		)
+		// Indicate that this IP will not be restored by setting to nil after
+		// we've used it to log above.
+		ip = nil
 		setter(nil)
 	case err != nil && errors.Is(err, errMismatch):
 		log.Warnf(
@@ -408,6 +413,8 @@ func RestoreHostIPs(ipv6 bool, fromK8s, fromFS net.IP, cidr *cidr.CIDR) {
 	case err == nil:
 		setter(ip)
 	}
+
+	return ip
 }
 
 func chooseHostIPsToRestore(ipv6 bool, fromK8s, fromFS net.IP, cidr *cidr.CIDR) (ip net.IP, err error) {

commit 32d8631db6611201ddc1b7afbfe03361978ccbb5

daemon: Fix issues where stale router IPs were not cleaned up
Previously, removeOldRouterState would only try to clean up stale router
IPs if the `cilium_host` device had more than one address assigned to
it. The implementation however is incorrect if router IP restoration
failed, because if restoration failed, then `restoredIP` would be nil
and the function would assume the IPv6 address family, thus only trying
to remove IPv6 addresses.

But even with the proper address family, the existing logic seems to
have a bug if there is only one single stale IP on the `cilium_host`
device: If restoration failed and there is only one IP on the host
device, then the IP assigned to the device cannot be the restored one,
thus it needs to be removed.

If the single stale IP is not removed and remains on the device, once a
new router IP is allocated by `allocateRouterIPv{4,6}`, and after a new
router IP is added to the interface in `init.sh` (via `Daemon.init`),
then `cilium_host` device will wrongly end up with two router IPs (one
of which is stale).

Fixes: 9cad166b696a ("daemon, node: Remove old, discarded router IPs from `cilium_host`")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

daemon/cmd/daemon.go
@@ -307,7 +307,7 @@ func (d *Daemon) restoreCiliumHostIPs(ipv6 bool, fromK8s net.IP) {
 	}
 
 	restoredIP := node.RestoreHostIPs(ipv6, fromK8s, fromFS, cidrs)
-	if err := removeOldRouterState(restoredIP); err != nil {
+	if err := removeOldRouterState(ipv6, restoredIP); err != nil {
 		log.WithError(err).Warnf(
 			"Failed to remove old router IPs (restored IP: %s) from cilium_host. Manual intervention is required to remove all other old IPs.",
 			restoredIP,
@@ -318,27 +318,33 @@ func (d *Daemon) restoreCiliumHostIPs(ipv6 bool, fromK8s net.IP) {
 // removeOldRouterState will try to ensure that the only IP assigned to the
 // `cilium_host` interface is the given restored IP. If the given IP is nil,
 // then it attempts to clear all IPs from the interface.
-func removeOldRouterState(restoredIP net.IP) error {
+func removeOldRouterState(ipv6 bool, restoredIP net.IP) error {
 	l, err := netlink.LinkByName(defaults.HostDevice)
 	if err != nil {
 		return err
 	}
 
-	family := netlink.FAMILY_V6
-	if restoredIP.To4() != nil {
-		family = netlink.FAMILY_V4
+	family := netlink.FAMILY_V4
+	if ipv6 {
+		family = netlink.FAMILY_V6
 	}
 	addrs, err := netlink.AddrList(l, family)
 	if err != nil {
 		return err
 	}
-	if len(addrs) > 1 {
-		log.Info("More than one router IP was found on the cilium_host device after restoration, cleaning up old router IPs.")
+
+	isRestoredIP := func(a netlink.Addr) bool {
+		return restoredIP != nil && restoredIP.Equal(a.IP)
 	}
+	if len(addrs) == 0 || (len(addrs) == 1 && isRestoredIP(addrs[0])) {
+		return nil // nothing to clean up
+	}
+
+	log.Info("More than one stale router IP was found on the cilium_host device after restoration, cleaning up old router IPs.")
 
 	var errs []error
 	for _, a := range addrs {
-		if restoredIP != nil && restoredIP.Equal(a.IP) {
+		if isRestoredIP(a) {
 			continue
 		}
 		if err := netlink.AddrDel(l, &a); err != nil {