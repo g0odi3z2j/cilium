g0odi3z2j/cilium

commit c533f0eaeead7c4ce6ee3a8e79ddf9853a470749

cmd: Allow more complicated patterns in map string type.
The previous PR #18478 wraps existing viper GetStringMapString function
to get around upstream bugs, however, it's unintentionally restricted a
few formats, which supported before in cilium, such as:

- --aws-instance-limit-mapping=c6a.2xlarge=4,15,15,m4.xlarge=1,5,10
- --api-rate-limit=endpoint-create=rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true

For complicated attribute, we are allowing comma character in value part
of key value pair. As golang didn't support look-ahead functionalities in
built-in regex library, this commit is to replace string.Split function
by custom implementation to handle such scenario.

Relates: #18478
Fixes: #18973
Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/command/map_string.go
@@ -15,7 +15,12 @@ import (
 	"github.com/spf13/viper"
 )
 
-var keyValueRegex = regexp.MustCompile(`([\w-:./@]+=[\w-:./@]*,)*([\w-:./@]+=[\w-:./@]*)$`)
+const (
+	comma = ','
+	equal = '='
+)
+
+var keyValueRegex = regexp.MustCompile(`([\w-:./@]+=[\w-:,./@]*,)*([\w-:./@]+=[\w-,:./@]*[\w-:./@]+)$`)
 
 // GetStringMapString contains one enhancement to support k1=v2,k2=v2 compared to original
 // implementation of GetStringMapString function
@@ -57,9 +62,9 @@ func GetStringMapStringE(vp *viper.Viper, key string) (map[string]string, error)
 			}
 
 			var v = map[string]string{}
-			kvs := strings.Split(s, ",")
+			kvs := splitKeyValue(s, comma, equal)
 			for _, kv := range kvs {
-				temp := strings.Split(kv, "=")
+				temp := strings.Split(kv, string(equal))
 				if len(temp) != 2 {
 					return map[string]string{}, fmt.Errorf("'%s' is not formatted as key=value,key1=value1", s)
 				}
@@ -77,4 +82,41 @@ func isValidKeyValuePair(str string) bool {
 		return true
 	}
 	return len(keyValueRegex.ReplaceAllString(str, "")) == 0
+}
+
+// splitKeyValue is similar to strings.Split, but looks ahead to make sure
+// that sep character is allowed in value component of key-value pair.
+//
+// Example: with the input "c6a.2xlarge=4,15,15,m4.xlarge=2,4,8",
+// - strings.Split function will return []string{"c6a.2xlarge=4", "15", "15", "m4.xlarge=2", "4", "8"}.
+// - splitKeyValue function will return []string{"c6a.2xlarge=4,15,15", "m4.xlarge=2,4,8"} instead.
+func splitKeyValue(str string, sep rune, keyValueSep rune) []string {
+	var sepIndexes []int
+	// find all indexes of separator character
+	for i := 0; i < len(str); i++ {
+		if int32(str[i]) == sep {
+			sepIndexes = append(sepIndexes, i)
+		}
+	}
+
+	if len(sepIndexes) == 0 {
+		return []string{str}
+	}
+
+	if len(sepIndexes) == 1 {
+		index := sepIndexes[0]
+		return []string{str[:index], str[index+1:]}
+	}
+
+	var res []string
+	var start = 0
+	for i := 0; i < len(sepIndexes)-1; i++ {
+		if strings.Contains(str[sepIndexes[i]:sepIndexes[i+1]], string(keyValueSep)) {
+			res = append(res, str[start:sepIndexes[i]])
+			start = sepIndexes[i] + 1
+		}
+	}
+	// append the remaining for last sep index
+	res = append(res, str[start:])
+	return res
 }

pkg/command/map_string_test.go
@@ -98,6 +98,29 @@ func TestGetStringMapString(t *testing.T) {
 			wantErr: assert.NoError,
 		},
 		{
+			name: "valid kv format with comma in value",
+			args: args{
+				key:   "API_RATE_LIMIT",
+				value: "endpoint-create=rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true,endpoint-delete=rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true",
+			},
+			want: map[string]string{
+				"endpoint-create": "rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true",
+				"endpoint-delete": "rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "another valid kv format with comma in value",
+			args: args{
+				key:   "AWS_INSTANCE_LIMIT_MAPPING",
+				value: "c6a.2xlarge=4,15,15",
+			},
+			want: map[string]string{
+				"c6a.2xlarge": "4,15,15",
+			},
+			wantErr: assert.NoError,
+		},
+		{
 			name: "valid kv format with forward slash",
 			args: args{
 				key:   "FOO_BAR",

commit 1d76840ca7eaca0b6cc10b6f57b4ec2207936c71

command: fix parsing of string map strings with multiple separators
We need to check the last substring, i.e. the string after the last
separator for the key/value separator as well. Otherwise a string such
as a=b,c=d,e=f,g=h with more than two separators will not be considered
a valid string map string.

Fixes: #20666
Fixes: c533f0eaeead ("cmd: Allow more complicated patterns in map string type.")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/command/map_string.go
@@ -66,7 +66,7 @@ func GetStringMapStringE(vp *viper.Viper, key string) (map[string]string, error)
 			for _, kv := range kvs {
 				temp := strings.Split(kv, string(equal))
 				if len(temp) != 2 {
-					return map[string]string{}, fmt.Errorf("'%s' is not formatted as key=value,key1=value1", s)
+					return map[string]string{}, fmt.Errorf("'%s' in '%s' is not formatted as key=value,key1=value1", kv, s)
 				}
 				v[temp[0]] = temp[1]
 			}
@@ -110,8 +110,12 @@ func splitKeyValue(str string, sep rune, keyValueSep rune) []string {
 
 	var res []string
 	var start = 0
-	for i := 0; i < len(sepIndexes)-1; i++ {
-		if strings.Contains(str[sepIndexes[i]:sepIndexes[i+1]], string(keyValueSep)) {
+	for i := 0; i < len(sepIndexes); i++ {
+		last := len(str)
+		if i < len(sepIndexes)-1 {
+			last = sepIndexes[i+1]
+		}
+		if strings.ContainsRune(str[sepIndexes[i]:last], keyValueSep) {
 			res = append(res, str[start:sepIndexes[i]])
 			start = sepIndexes[i] + 1
 		}

pkg/command/map_string_test.go
@@ -113,10 +113,11 @@ func TestGetStringMapString(t *testing.T) {
 			name: "another valid kv format with comma in value",
 			args: args{
 				key:   "AWS_INSTANCE_LIMIT_MAPPING",
-				value: "c6a.2xlarge=4,15,15",
+				value: "c6a.2xlarge=4,15,15,m4.large=1,5,10",
 			},
 			want: map[string]string{
 				"c6a.2xlarge": "4,15,15",
+				"m4.large":    "1,5,10",
 			},
 			wantErr: assert.NoError,
 		},
@@ -140,6 +141,20 @@ func TestGetStringMapString(t *testing.T) {
 			},
 			want: map[string]string{
 				"cluster": "my-cluster",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "valid kv format from issue #20666",
+			args: args{
+				key:   "FOO_BAR",
+				value: "a=b,c=d,E=F,G=h",
+			},
+			want: map[string]string{
+				"a": "b",
+				"c": "d",
+				"E": "F",
+				"G": "h",
 			},
 			wantErr: assert.NoError,
 		},
@@ -255,7 +270,6 @@ func Test_isValidKeyValuePair(t *testing.T) {
 			},
 			want: true,
 		},
-
 		{
 			name: "valid format with multiple pairs",
 			args: args{