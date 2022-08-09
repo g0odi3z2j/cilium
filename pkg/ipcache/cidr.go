g0odi3z2j/cilium

commit ca37993352f4cbfc140921b87e2c77c656a2eacb

ipcache: Fix race in identity/ipcache release
Create a critical section for identity release + removal from ipcache.
Otherwise, it's possible to trigger the following race condition:

    Goroutine 1                | Goroutine 2
    ---------------------------+--------------------------------------
    releaseCIDRIdentities()    | AllocateCIDRs()
    -> Release(..., id, ...)   |
                               | -> allocate(...)
                               | -> ipc.UpsertGeneratedIdentities(...)
    -> ipc.deleteLocked(...)   |

In this case, the expectation from Goroutine 2 is that a new identity
is allocated and that identity is inserted into the ipcache, but the
result is that the identity is allocated but the ipcache entry is
missing. This is partly because the identity released in goroutine 1 is
different from the newly allocated identity in goroutine 2, however
goroutine 1 will delete the ipcache entry based on the prefix and not
the identity. Therefore it's possible for goroutine 1 to delete the
ipcache entry corresponding to the identity allocated in goroutine 2.

Note that for balancing the upsert / release, we perhaps should cover
the entire allocation + ipcache push in Upsert() with the same locking.
However, on upsert there is an optional feature of the API to defer the
ipcache upsert to a later point, governed by the caller. There is
currently no way to extend the locking over that much longer time
period, so we only cover the allocation step there. This should still be
safe, as one of the following cases should occur:

    Goroutine 1                | Goroutine 2
    ---------------------------+--------------------------------------
    Lock                       |
    Release()                  |
    deleteLocked()             |
    Unlock                     |
                               | Lock
                               | ipc.allocate()
                               | Unlock
                               | .... (repeat below)

pkg/ipcache/cidr.go
@@ -47,6 +47,7 @@ func (ipc *IPCache) AllocateCIDRs(
 		newlyAllocatedIdentities = map[string]*identity.Identity{}
 	}
 
+	ipc.Lock()
 	allocatedIdentities := make(map[string]*identity.Identity, len(prefixes))
 	for i, p := range prefixes {
 		if p == nil {
@@ -72,6 +73,7 @@ func (ipc *IPCache) AllocateCIDRs(
 			newlyAllocatedIdentities[prefixStr] = id
 		}
 	}
+	ipc.Unlock()
 
 	// Only upsert into ipcache if identity wasn't allocated
 	// before and the caller does not care doing this
@@ -141,6 +143,21 @@ func (ipc *IPCache) allocate(prefix *net.IPNet, lbls labels.Labels, oldNID ident
 }
 
 func (ipc *IPCache) releaseCIDRIdentities(ctx context.Context, identities map[string]*identity.Identity) {
+	// Create a critical section for identity release + removal from ipcache.
+	// Otherwise, it's possible to trigger the following race condition:
+	//
+	// Goroutine 1                | Goroutine 2
+	// releaseCIDRIdentities()    | AllocateCIDRs()
+	// -> Release(..., id, ...)   |
+	//                            | -> allocate(...)
+	//                            | -> ipc.UpsertGeneratedIdentities(...)
+	// -> ipc.deleteLocked(...)   |
+	//
+	// In this case, the expectation from Goroutine 2 is that an identity
+	// is allocated and that identity is in the ipcache, but the result
+	// is that the identity is allocated but the ipcache entry is missing.
+	ipc.Lock()
+	defer ipc.Unlock()
 	for prefix, id := range identities {
 		released, err := ipc.IdentityAllocator.Release(ctx, id, false)
 		if err != nil {
@@ -151,7 +168,7 @@ func (ipc *IPCache) releaseCIDRIdentities(ctx context.Context, identities map[st
 		}
 
 		if released {
-			ipc.Delete(prefix, source.Generated)
+			ipc.deleteLocked(prefix, source.Generated)
 		}
 	}
 }

commit 7a92669900cf90b78c961f4cd433146c213327b4

ipcache: Fix lock leak
Commit ca37993352f4 ("ipcache: Fix race in identity/ipcache release")
unintentionally took the lock on the IPCache and failed to release it if
the loop returned in the middle. This case is a bit unusual given that
allocation fails in this case. Fix it.

Found by inspection.

Fixes: ca37993352f4 ("ipcache: Fix race in identity/ipcache release")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/ipcache/cidr.go
@@ -64,6 +64,7 @@ func (ipc *IPCache) AllocateCIDRs(
 		id, isNew, err := ipc.allocate(p, lbls, oldNID)
 		if err != nil {
 			ipc.IdentityAllocator.ReleaseSlice(context.Background(), nil, usedIdentities)
+			ipc.Unlock()
 			return nil, err
 		}
 