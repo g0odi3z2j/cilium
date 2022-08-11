g0odi3z2j/cilium

commit cb043cbb357be89927d9e431a3de259ad2a98caf

pkg/ipcache: only log if not running in debug
Saves 6.79% of memory allocations done when inserting 35k ipcache
entries.

Signed-off-by: André Martins <andre@cilium.io>

pkg/ipcache/kvstore.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/option"
 
 	"github.com/sirupsen/logrus"
 )
@@ -204,6 +205,8 @@ func NewIPIdentityWatcher(backend kvstore.BackendOperations) *IPIdentityWatcher 
 // function returns when IPIdentityWatcher.Close() is called. The watcher will
 // automatically restart as required.
 func (iw *IPIdentityWatcher) Watch() {
+
+	var scopedLog *logrus.Entry
 restart:
 	watcher := iw.backend.ListAndWatch("endpointIPWatcher", IPIdentitiesPath, 512)
 
@@ -217,8 +220,10 @@ restart:
 				goto restart
 			}
 
-			scopedLog := log.WithFields(logrus.Fields{"kvstore-event": event.Typ.String(), "key": event.Key})
-			scopedLog.Debug("Received event")
+			if option.Config.Debug {
+				scopedLog = log.WithFields(logrus.Fields{"kvstore-event": event.Typ.String(), "key": event.Key})
+				scopedLog.Debug("Received event")
+			}
 
 			// Synchronize local caching of endpoint IP to ipIDPair mapping with
 			// operation key-value store has informed us about.
@@ -251,7 +256,8 @@ restart:
 				var ipIDPair identity.IPIdentityPair
 				err := json.Unmarshal(event.Value, &ipIDPair)
 				if err != nil {
-					scopedLog.WithError(err).Errorf("Not adding entry to ip cache; error unmarshaling data from key-value store")
+					log.WithFields(logrus.Fields{"kvstore-event": event.Typ.String(), "key": event.Key}).
+						WithError(err).Error("Not adding entry to ip cache; error unmarshaling data from key-value store")
 					continue
 				}
 				ip := ipIDPair.PrefixString()
@@ -270,7 +276,8 @@ restart:
 				// need to convert kvstore key to IP.
 				ipnet, isHost, err := keyToIPNet(event.Key)
 				if err != nil {
-					scopedLog.WithError(err).Error("Error parsing IP from key")
+					log.WithFields(logrus.Fields{"kvstore-event": event.Typ.String(), "key": event.Key}).
+						WithError(err).Error("Error parsing IP from key")
 					continue
 				}
 				var ip string

commit b71279bb0bafd71b5a87075d19538337286773c5

ipcache/kvstore: fix panic when processing ip=<nil> entries
This problem was introduced in cb043cbb357, which results in
a "nil pointer dereference" panic.

Signed-off-by: ArthurChiao <arthurchiao@hotmail.com>

pkg/ipcache/kvstore.go
@@ -220,7 +220,8 @@ func NewIPIdentityWatcher(ipc *IPCache, backend kvstore.BackendOperations) *IPId
 // automatically restart as required.
 func (iw *IPIdentityWatcher) Watch(ctx context.Context) {
 
-	var scopedLog *logrus.Entry
+	scopedLog := log
+
 restart:
 	watcher := iw.backend.ListAndWatch(ctx, "endpointIPWatcher", IPIdentitiesPath, 512)
 
@@ -276,7 +277,9 @@ restart:
 				}
 				ip := ipIDPair.PrefixString()
 				if ip == "<nil>" {
-					scopedLog.Debug("Ignoring entry with nil IP")
+					if option.Config.Debug {
+						scopedLog.Debug("Ignoring entry with nil IP")
+					}
 					continue
 				}
 				var k8sMeta *K8sMetadata