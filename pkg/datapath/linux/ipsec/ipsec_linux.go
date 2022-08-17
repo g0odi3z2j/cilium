g0odi3z2j/cilium

commit 8e35a60125785704cdf360cc68f75300d33114fa

ipsec: add ipSecXfrmMark{Set,Get}SPI helpers
which allows to encode and extract an SPI value in and from a XfrmMark

Signed-off-by: Gilberto Bertin <jibi@cilium.io>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -53,6 +53,10 @@ const (
 	offsetEncKey   = 4
 	offsetIP       = 5
 	maxOffset      = offsetIP
+
+	// ipSecXfrmMarkSPIShift defines how many bits the SPI is shifted when
+	// encoded in a XfrmMark
+	ipSecXfrmMarkSPIShift = 12
 )
 
 type ipSecKey struct {
@@ -157,13 +161,12 @@ func ipSecReplaceStateOut(remoteIP, localIP net.IP) (uint8, error) {
 	if key == nil {
 		return 0, fmt.Errorf("IPSec key missing")
 	}
-	spiWide := uint32(key.Spi)
 	state := ipSecNewState()
 	ipSecJoinState(state, key)
 	state.Src = localIP
 	state.Dst = remoteIP
 	state.Mark = &netlink.XfrmMark{
-		Value: ((spiWide << 12) | linux_defaults.RouteMarkEncrypt),
+		Value: ipSecXfrmMarkSetSPI(linux_defaults.RouteMarkEncrypt, key.Spi),
 		Mask:  linux_defaults.IPsecMarkMask,
 	}
 	state.OutputMark = &netlink.XfrmMark{
@@ -232,15 +235,24 @@ func IpSecReplacePolicyFwd(src, dst, tmplSrc, tmplDst *net.IPNet) error {
 	return _ipSecReplacePolicyInFwd(src, dst, tmplSrc, tmplDst, false, netlink.XFRM_DIR_FWD)
 }
 
+// ipSecXfrmMarkSetSPI takes a XfrmMark base value, an SPI, returns the mark
+// value with the SPI value encoded in it
+func ipSecXfrmMarkSetSPI(markValue uint32, spi uint8) uint32 {
+	return markValue | (uint32(spi) << ipSecXfrmMarkSPIShift)
+}
+
+// ipSecXfrmMarkGetSPI extracts from a XfrmMark value the encoded SPI
+func ipSecXfrmMarkGetSPI(markValue uint32) uint8 {
+	return uint8(markValue >> ipSecXfrmMarkSPIShift)
+}
+
 func ipSecReplacePolicyOut(src, dst, tmplSrc, tmplDst *net.IPNet, dir IPSecDir) error {
 	// TODO: Remove old policy pointing to target net
-	var spiWide uint32
 
 	key := getIPSecKeys(dst.IP)
 	if key == nil {
 		return fmt.Errorf("IPSec key missing")
 	}
-	spiWide = uint32(key.Spi)
 
 	policy := ipSecNewPolicy()
 	if dir == IPSecDirOutNode {
@@ -253,7 +265,7 @@ func ipSecReplacePolicyOut(src, dst, tmplSrc, tmplDst *net.IPNet, dir IPSecDir) 
 	policy.Dst = &net.IPNet{IP: dst.IP.Mask(dst.Mask), Mask: dst.Mask}
 	policy.Dir = netlink.XFRM_DIR_OUT
 	policy.Mark = &netlink.XfrmMark{
-		Value: ((spiWide << 12) | linux_defaults.RouteMarkEncrypt),
+		Value: ipSecXfrmMarkSetSPI(linux_defaults.RouteMarkEncrypt, key.Spi),
 		Mask:  linux_defaults.IPsecMarkMask,
 	}
 	ipSecAttachPolicyTempl(policy, key, tmplSrc.IP, tmplDst.IP, true, 0)

commit 1b7affb917944cb87800d2df0f78069eb78051d1

ipsec: Fix incorrect parsing of SPI from mark
Commit 8e35a601257 introduced helpers to set and retrieve the SPI from
the XFRM mark. The SPI number is encoded into 4 bits at positions 12-15
(LSB first).

However, function ipSecXfrmMarkGetSPI extracts 8 bits at positions 12-19
instead of 4. This commit fixes it. This bug would only have an impact
if some other software used the 4 upper bits; no such case is currently
known.

Fixes: 8e35a601257 ("ipsec: add ipSecXfrmMark{Set,Get}SPI helpers")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -254,7 +254,7 @@ func ipSecXfrmMarkSetSPI(markValue uint32, spi uint8) uint32 {
 
 // ipSecXfrmMarkGetSPI extracts from a XfrmMark value the encoded SPI
 func ipSecXfrmMarkGetSPI(markValue uint32) uint8 {
-	return uint8(markValue >> ipSecXfrmMarkSPIShift)
+	return uint8(markValue >> ipSecXfrmMarkSPIShift & 0xF)
 }
 
 func getSPIFromXfrmPolicy(policy *netlink.XfrmPolicy) uint8 {