g0odi3z2j/cilium

commit 9f92d32da666d5c53eff3c971bb391595983abda

kubectl get cep returns empty columns of policies statuses
This patch set "disabled" for the fields INGRESS ENFORCEMENT, EGRESS ENFORCEMENT, VISIBILITY POLICY if Cilium run with --endpoint-status disabled.
$ kubectl get cep
NAME                        ENDPOINT ID   IDENTITY ID   INGRESS ENFORCEMENT   EGRESS ENFORCEMENT   VISIBILITY POLICY   ENDPOINT STATE   IPV4          IPV6
deathstar-f694cf746-f2tf4   1239          28002         disabled              disabled             disabled            ready            10.11.0.52    fd04::6e90
deathstar-f694cf746-gnrg6   3603          28002         disabled              disabled             disabled            ready            10.11.0.78    fd04::6dd9
tiefighter                  810           35802         disabled              disabled             disabled            ready            10.11.0.72    fd04::3ccb
xwing                       1629          58767         disabled              disabled             disabled            ready            10.11.0.136   fd04::bc93                    56            58767         Status N.A.           Status N.A.          Status N.A.         ready            10.11.0.53    fd04::d70d

Fixes: #17880

Signed-off-by: Roman Ptitcyn <romanspb@yahoo.com>

pkg/endpoint/endpoint_status.go
@@ -20,6 +20,13 @@ import (
 	"github.com/cilium/cilium/pkg/policy"
 )
 
+// StatusNA is value of fields in the output of 'kubectl get cep' in case of disabled "--endpoint-status"
+const (
+	EndpointPolicyStateEnforcing    cilium_v2.EndpointPolicyState = "enforcing"
+	EndpointPolicyStateNonEnforcing cilium_v2.EndpointPolicyState = "non-enforcing"
+	EndpointPolicyStateDisabled     cilium_v2.EndpointPolicyState = "disabled"
+)
+
 func getEndpointStatusControllers(mdlControllers models.ControllerStatuses) (controllers cilium_v2.ControllerList) {
 	for _, c := range mdlControllers {
 		if c.Status == nil {
@@ -227,6 +234,16 @@ func (e *Endpoint) getEndpointPolicy() (ep *cilium_v2.EndpointPolicy) {
 				e.desiredPolicy.EgressPolicyEnabled,
 		},
 	}
+	if ep.Ingress.Enforcing {
+		ep.Ingress.State = EndpointPolicyStateEnforcing
+	} else {
+		ep.Ingress.State = EndpointPolicyStateNonEnforcing
+	}
+	if ep.Egress.Enforcing {
+		ep.Egress.State = EndpointPolicyStateEnforcing
+	} else {
+		ep.Egress.State = EndpointPolicyStateNonEnforcing
+	}
 
 	// Handle allow-all cases
 	allowsAllIngress, allowsAllEgress := e.desiredPolicy.AllowsIdentity(identity.IdentityUnknown)
@@ -279,6 +296,19 @@ func (e *Endpoint) getEndpointPolicy() (ep *cilium_v2.EndpointPolicy) {
 	return
 }
 
+func (e *Endpoint) getEndpointPolicyStateDisabled() (ep *cilium_v2.EndpointPolicy) {
+	ep = &cilium_v2.EndpointPolicy{
+		Ingress: &cilium_v2.EndpointPolicyDirection{
+			State: EndpointPolicyStateDisabled,
+		},
+		Egress: &cilium_v2.EndpointPolicyDirection{
+			State: EndpointPolicyStateDisabled,
+		},
+	}
+
+	return
+}
+
 func (e *Endpoint) getEndpointVisibilityPolicyStatus() *string {
 	if e.visibilityPolicy == nil {
 		return nil
@@ -290,6 +320,11 @@ func (e *Endpoint) getEndpointVisibilityPolicyStatus() *string {
 		str = e.visibilityPolicy.Error.Error()
 	}
 	return &str
+}
+
+func (e *Endpoint) getEndpointVisibilityPolicyStatusDisabled() *string {
+	str := EndpointPolicyStateDisabled
+	return (*string)(&str)
 }
 
 // EndpointStatusConfiguration is the configuration interface that a caller of
@@ -335,6 +370,9 @@ func (e *Endpoint) GetCiliumEndpointStatus(conf EndpointStatusConfiguration) *ci
 	if conf.EndpointStatusIsEnabled(option.EndpointStatusPolicy) {
 		status.Policy = e.getEndpointPolicy()
 		status.VisibilityPolicyStatus = e.getEndpointVisibilityPolicyStatus()
+	} else {
+		status.Policy = e.getEndpointPolicyStateDisabled()
+		status.VisibilityPolicyStatus = e.getEndpointVisibilityPolicyStatusDisabled()
 	}
 
 	if conf.EndpointStatusIsEnabled(option.EndpointStatusHealth) {

pkg/k8s/apis/cilium.io/client/crds/v2/ciliumendpoints.yaml
@@ -31,13 +31,13 @@ spec:
       name: Identity ID
       type: integer
     - description: Ingress enforcement in the endpoint
-      jsonPath: .status.policy.ingress.enforcing
+      jsonPath: .status.policy.ingress.state
       name: Ingress Enforcement
-      type: boolean
+      type: string
     - description: Egress enforcement in the endpoint
-      jsonPath: .status.policy.egress.enforcing
+      jsonPath: .status.policy.egress.state
       name: Egress Enforcement
-      type: boolean
+      type: string
     - description: Status of visibility policy in the endpoint
       jsonPath: .status.visibility-policy-status
       name: Visibility Policy
@@ -343,6 +343,10 @@ spec:
                               type: integer
                           type: object
                         type: array
+                      state:
+                        description: 'EndpointPolicyState defines the state of the
+                          Policy mode: "enforcing", "non-enforcing", "disabled"'
+                        type: string
                     required:
                     - enforcing
                     type: object
@@ -430,6 +434,10 @@ spec:
                               type: integer
                           type: object
                         type: array
+                      state:
+                        description: 'EndpointPolicyState defines the state of the
+                          Policy mode: "enforcing", "non-enforcing", "disabled"'
+                        type: string
                     required:
                     - enforcing
                     type: object

pkg/k8s/apis/cilium.io/v2/register.go
@@ -23,7 +23,7 @@ const (
 	//
 	// Maintainers: Run ./Documentation/check-crd-compat-table.sh for each release
 	// Developers: Bump patch for each change in the CRD schema.
-	CustomResourceDefinitionSchemaVersion = "1.25.5"
+	CustomResourceDefinitionSchemaVersion = "1.25.6"
 
 	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
 	CustomResourceDefinitionSchemaVersionKey = "io.cilium.k8s.crd.schema.version"

pkg/k8s/apis/cilium.io/v2/types.go
@@ -22,8 +22,8 @@ import (
 // +kubebuilder:resource:categories={cilium},singular="ciliumendpoint",path="ciliumendpoints",scope="Namespaced",shortName={cep,ciliumep}
 // +kubebuilder:printcolumn:JSONPath=".status.id",description="Cilium endpoint id",name="Endpoint ID",type=integer
 // +kubebuilder:printcolumn:JSONPath=".status.identity.id",description="Cilium identity id",name="Identity ID",type=integer
-// +kubebuilder:printcolumn:JSONPath=".status.policy.ingress.enforcing",description="Ingress enforcement in the endpoint",name="Ingress Enforcement",type=boolean
-// +kubebuilder:printcolumn:JSONPath=".status.policy.egress.enforcing",description="Egress enforcement in the endpoint",name="Egress Enforcement",type=boolean
+// +kubebuilder:printcolumn:JSONPath=".status.policy.ingress.state",description="Ingress enforcement in the endpoint",name="Ingress Enforcement",type=string
+// +kubebuilder:printcolumn:JSONPath=".status.policy.egress.state",description="Egress enforcement in the endpoint",name="Egress Enforcement",type=string
 // +kubebuilder:printcolumn:JSONPath=".status.visibility-policy-status",description="Status of visibility policy in the endpoint",name="Visibility Policy",type=string
 // +kubebuilder:printcolumn:JSONPath=".status.state",description="Endpoint current state",name="Endpoint State",type=string
 // +kubebuilder:printcolumn:JSONPath=".status.networking.addressing[0].ipv4",description="Endpoint IPv4 address",name="IPv4",type=string
@@ -40,6 +40,9 @@ type CiliumEndpoint struct {
 	// +kubebuilder:validation:Optional
 	Status EndpointStatus `json:"status"`
 }
+
+// EndpointPolicyState defines the state of the Policy mode: "enforcing", "non-enforcing", "disabled"
+type EndpointPolicyState string
 
 // EndpointStatus is the status of a Cilium endpoint.
 type EndpointStatus struct {
@@ -141,6 +144,7 @@ type EndpointPolicyDirection struct {
 	Removing AllowedIdentityList `json:"removing,omitempty"`
 	// Deprecated
 	Adding AllowedIdentityList `json:"adding,omitempty"`
+	State  EndpointPolicyState `json:"state,omitempty"`
 }
 
 // IdentityTuple specifies a peer by identity, destination port and protocol.

pkg/k8s/apis/cilium.io/v2/zz_generated.deepequal.go
@@ -812,6 +812,10 @@ func (in *EndpointPolicyDirection) DeepEqual(other *EndpointPolicyDirection) boo
 		}
 	}
 
+	if in.State != other.State {
+		return false
+	}
+
 	return true
 }
 

commit 7fa3c873d637f9397c890c9edc42ff16162a0e5d

pkg/endpoint: change CEP policy status message
To make it clear to users that only the status of the policy enforcement
is not enabled this commit changes the message wording from "disabled"
to "<status disabled>".

Fixes: 9f92d32da666 ("kubectl get cep returns empty columns of policies statuses")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/endpoint_status.go
@@ -24,7 +24,7 @@ import (
 const (
 	EndpointPolicyStateEnforcing    cilium_v2.EndpointPolicyState = "enforcing"
 	EndpointPolicyStateNonEnforcing cilium_v2.EndpointPolicyState = "non-enforcing"
-	EndpointPolicyStateDisabled     cilium_v2.EndpointPolicyState = "disabled"
+	EndpointPolicyStateDisabled     cilium_v2.EndpointPolicyState = "<status disabled>"
 )
 
 func getEndpointStatusControllers(mdlControllers models.ControllerStatuses) (controllers cilium_v2.ControllerList) {