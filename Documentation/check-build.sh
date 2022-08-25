g0odi3z2j/cilium

commit 8a4b742afa9cc0670ca5ef4b1fb0aefa2a484b75

docs: Mark Git repo as safe (if in container) before spell checks
In commit 16fd709ef1ae ("docs: Run builder container as host UID/GID"),
we updated the Dockerfile for building the documentation to create a
specific directory required by the versionwarning extensions. This
command required in turn to run the container with host UID/GID.

On some setups, in particular with macOS, this may result in running
with UID/GID different from what Linux Alpine usually "expects". For
example, the GID can be 20 (typically on macOS), which corresponds to
the "dialout" group on Alpine. In such a case, Git fails to verify that
the ownership of the Cilium repository corresponds to the user running
the commands. With recent versions of Git, this translates into an
error, because Git refuses to work in "unsafe" repositories as this
would create security issues [0].

To work around this, let's mark the repository as "safe" before running
the spell checks. This will update the ~/.gitconfig file, so we only run
this from inside a container.

Another attempt at fixing the issue was initially committed as
72112259580d ("docs: Mark Git repo as safe in docs-builder, for GitHub
workflow too"), but marking the repository as safe while buildilng the
image does not work in the case where we mount the repository at a
different location, or if we change the HOME environment variable (as
the CI does) and make Git fail to find its .gitconfig.

[0] https://github.blog/2022-04-12-git-security-vulnerability-announced

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

Documentation/check-build.sh
@@ -62,6 +62,24 @@ describe_spelling_errors() {
 }
 
 build_with_spellchecker() {
+    # The spell checker runs some Git commands to retreive the name of authors
+    # and consider them as acceptable words.
+    #
+    # Recent Git versions refuse to work by default if the repository owner is
+    # different from the user. This is the case when we run this script in a
+    # container on macOS, because pass --user "uid:gid", and these values
+    # differ from what Linux is used to (The gid from macOS seems to be 20,
+    # which corresponds to the "dialout" group in the container). We pass
+    # --user "uid:gid" to have the "install" command work in the workaround for
+    # versionwarning above.
+    #
+    # If running in a container, tell Git that the repository is safe.
+    set +o nounset
+    if [[ -n "$MAKE_GIT_REPO_SAFE" ]]; then
+        git config --global --add safe.directory "${script_dir}/.."
+    fi
+    set -o nounset
+
     rm -rf "${spelldir}"
     # Call with -q -W --keep-going: suppresses regular output (keeps warning;
     # -Q would suppress warnings as well including those we write to a file),

Documentation/Dockerfile
@@ -25,6 +25,7 @@ RUN pip install -r /tmp/requirements.txt
 
 ENV HOME=/tmp
 ENV READTHEDOCS_VERSION=$READTHEDOCS_VERSION
+ENV MAKE_GIT_REPO_SAFE=1
 
 ## Workaround odd behaviour of sphinx versionwarning extension. It wants to
 ## write runtime data inside a system directory.

commit b6b3915cdddce7618abd90c3407054095b0a79ca

docs: Fix repository path for telling git it's safe
It seems that git does not expand paths when looking for safe
repositories in its config file. So the following does not work:

    git config --global --add safe.directory /src/Documentation/..

Instead we need the regular path:

    git config --global --add safe.directory /src

Take $(dirname Documentation) to mark the correct path.

Fixes: 8a4b742afa9c ("docs: Mark Git repo as safe (if in container) before spell checks")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

Documentation/check-build.sh
@@ -12,6 +12,7 @@ set -o pipefail
 # times anyway.
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+root_dir="$(dirname "${script_dir}")"
 
 build_dir="${script_dir}/_build"
 warnings="${build_dir}/warnings.txt"
@@ -76,7 +77,7 @@ build_with_spellchecker() {
     # If running in a container, tell Git that the repository is safe.
     set +o nounset
     if [[ -n "$MAKE_GIT_REPO_SAFE" ]]; then
-        git config --global --add safe.directory "${script_dir}/.."
+        git config --global --add safe.directory "${root_dir}"
     fi
     set -o nounset
 