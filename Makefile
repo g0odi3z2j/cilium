g0odi3z2j/cilium

commit 91dab362fb5c6d9cecd12801d41967272c614889

Makefile: Split agent and operator kind targets
Split the image build targets for building the agent vs. the operator,
so that developers who only want to update one or the other during their
local debugging can do so. For more information, see the output of
'make help | grep kind'.

Signed-off-by: Joe Stringer <joe@cilium.io>

Makefile
@@ -513,20 +513,33 @@ kind: ## Create a kind cluster for Cilium development.
 kind-down: ## Destroy a kind cluster for Cilium development.
 	$(QUIET)./contrib/scripts/kind-down.sh
 
-kind-image: export DOCKER_REGISTRY=localhost:5000
-kind-image: export LOCAL_AGENT_IMAGE=$(DOCKER_REGISTRY)/$(DOCKER_DEV_ACCOUNT)/cilium-dev:$(LOCAL_IMAGE_TAG)
-kind-image: export LOCAL_OPERATOR_IMAGE=$(DOCKER_REGISTRY)/$(DOCKER_DEV_ACCOUNT)/operator:$(LOCAL_IMAGE_TAG)
-kind-image: ## Build cilium-dev docker image and import it into kind.
+.PHONY: kind-ready
+kind-ready:
 	@$(ECHO_CHECK) kind is ready...
 	@kind get clusters >/dev/null
+
+.PHONY: kind-image-agent
+kind-image-agent: export DOCKER_REGISTRY=localhost:5000
+kind-image-agent: export LOCAL_AGENT_IMAGE=$(DOCKER_REGISTRY)/$(DOCKER_DEV_ACCOUNT)/cilium-dev:$(LOCAL_IMAGE_TAG)
+kind-image-agent: kind-ready ## Build cilium-dev docker image and import it into kind.
 	$(QUIET)$(MAKE) dev-docker-image DOCKER_IMAGE_TAG=$(LOCAL_IMAGE_TAG)
 	@echo "  DEPLOY image to kind ($(LOCAL_AGENT_IMAGE))"
 	$(QUIET)$(CONTAINER_ENGINE) push $(LOCAL_AGENT_IMAGE)
 	$(QUIET)kind load docker-image $(LOCAL_AGENT_IMAGE)
+
+.PHONY: kind-image-operator
+kind-image-operator: export DOCKER_REGISTRY=localhost:5000
+kind-image-operator: export LOCAL_OPERATOR_IMAGE=$(DOCKER_REGISTRY)/$(DOCKER_DEV_ACCOUNT)/operator:$(LOCAL_IMAGE_TAG)
+kind-image-operator: kind-ready ## Build cilium-operator-dev docker image and import it into kind.
 	$(QUIET)$(MAKE) dev-docker-operator-image DOCKER_IMAGE_TAG=$(LOCAL_IMAGE_TAG)
 	@echo "  DEPLOY image to kind ($(LOCAL_OPERATOR_IMAGE))"
 	$(QUIET)$(CONTAINER_ENGINE) push $(LOCAL_OPERATOR_IMAGE)
 	$(QUIET)kind load docker-image $(LOCAL_OPERATOR_IMAGE)
+
+.PHONY: kind-image
+kind-image: ## Build cilium and operator images and import them into kind.
+	$(MAKE) kind-image-agent
+	$(MAKE) kind-image-operator
 
 precheck: check-go-version logging-subsys-field ## Peform build precheck for the source code.
 ifeq ($(SKIP_K8S_CODE_GEN_CHECK),"false")

commit e9ae9cf924cd99dc066ca9b64d87a9f39d6c9494

make: fix kind-image-operator target to build the operator-generic image
Build the cilium/operator-generic instead of the cilium/operator
(all-in-one) image in the kind-image-operator. Otherwise loading the
image will fail with:

      DEPLOY image to kind (localhost:5000/cilium/operator-generic:local)
    docker push localhost:5000/cilium/operator-generic:local
    The push refers to repository [localhost:5000/cilium/operator-generic]
    An image does not exist locally with the tag: localhost:5000/cilium/operator-generic
    make[1]: *** [Makefile:542: kind-image-operator] Error 1
    make[1]: Leaving directory '/home/tklauser/go/src/github.com/cilium/cilium'
    make: *** [Makefile:548: kind-image] Error 2

Fixes: 91dab362fb5c ("Makefile: Split agent and operator kind targets")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

Makefile
@@ -537,7 +537,7 @@ kind-image-agent: kind-ready ## Build cilium-dev docker image and import it into
 
 $(eval $(call KIND_ENV,kind-image-operator))
 kind-image-operator: kind-ready ## Build cilium-operator-dev docker image and import it into kind.
-	$(QUIET)$(MAKE) dev-docker-operator-image DOCKER_IMAGE_TAG=$(LOCAL_IMAGE_TAG)
+	$(QUIET)$(MAKE) dev-docker-operator-generic-image DOCKER_IMAGE_TAG=$(LOCAL_IMAGE_TAG)
 	@echo "  DEPLOY image to kind ($(LOCAL_OPERATOR_IMAGE))"
 	$(QUIET)$(CONTAINER_ENGINE) push $(LOCAL_OPERATOR_IMAGE)
 	$(QUIET)kind load docker-image $(LOCAL_OPERATOR_IMAGE)