g0odi3z2j/cilium

commit ac2106e589085a7e81211bad3197f877e089af0b

datapath: use right file number in drop notifications
The 3c559bf6fdef09 ("datapath: add file and line info to drop notifications")
commit added source file name and source line information to drop
notifications.  While this was convenient, the source file didn't always
correspond to the source line, for example, in case a header with the actual
send_notify_error* call was included in a C file, the source file referred to
the C file, and the line—to the header file. Fix this by always providing the
right file.

Another change, based on feedback from devs, reduces noise, namely, instead of
printing the source file name, we just print <file-id>:<line>, like here:

    xx drop (Invalid packet) flow 0x0 to endpoint 0, ifindex 12, file 101:80, ...
    xx drop (Policy denied) flow 0xeea42740 to endpoint 0, ifindex 14, file 2:1167, ...

To find the <file-id> refer to the bpf/source_names_to_ids.h file.

Signed-off-by: Anton Protopopov <aspsk@isovalent.com>

pkg/datapath/loader/check-sources.sh
@@ -1,24 +1,50 @@
 #! /bin/bash -efu
 
 #
-# This simple awk command extracts file names from the sourceNameToId map definition
+# This simple awk command extracts file names from the definition of the
+# __source_file_name_to_id function. This is the list of file names which
+# are currently assigned a numerical ID.
 #
 defined_files=$(
-  awk -F: '/^var sourceNameToId/{found=1; next} {if (!found) next} /^}/{exit} {gsub(/"|\t/, "", $1); print $1}' pkg/datapath/loader/compile.go
+	awk -F: '/^__source_file_name_to_id/{found=1; next}
+		/return 0/{exit}
+		{if (!found || !/_strcase_/) next}
+		{gsub(/.* |"|)|;/, "", $1); print $1}
+		' bpf/source_names_to_ids.h
 )
 
+
 #
-# Now we need to find all the C files in the bpf/ directory
-# and check if all of them were defined in the map
+# Now we need to find all the C files in the bpf/ directory and all header
+# files in the bpf/lib/ directory which are using one of send_drop_notify*
+# functions.
 #
+all_files=$(find bpf/ -maxdepth 1 -name '*.c'; find bpf/lib/ -maxdepth 1 -name '*.h')
 required_files=$(
-	find bpf/ -maxdepth 1 -name '*.c' | xargs -n1 basename
+	grep -e '\<send_drop_notify\(\|_error\|_ext\|_error_ext\)\>' $all_files |
+	cut -f1 -d: |
+	sort -u |
+	xargs -n1 basename
 )
 
+#
+# Check that all files which use send_drop_notify* are defined
+#
 retval=0
 for f in $required_files; do
 	if ! grep --silent -w "$f" <<<"$defined_files"; then
-		echo "$0: $f is not defined, add its mapping to the sourceNameToId map" >&2
+		echo "$0: $f is not defined, please add its mapping to bpf/source_names_to_ids.h" >&2
+		retval=1
+	fi
+done
+
+#
+# Check that all defined files actually use send_drop_notify*
+#
+retval=0
+for f in $defined_files; do
+	if ! grep --silent -w "$f" <<<"$required_files"; then
+		echo "$0: $f is not using send_drop_notify*, please remove it from bpf/source_names_to_ids.h" >&2
 		retval=1
 	fi
 done

bpf/lib/drop.h
@@ -135,10 +135,6 @@ int _send_drop_notify(__u8 file __maybe_unused, __u16 line __maybe_unused,
  * original error returned by fib_lookup (if reason == DROP_NO_FIB).
  */
 
-#ifndef __MAGIC_FILE__
-#define __MAGIC_FILE__ 0
-#endif
-
 /*
  * Cilium errors are greater than absolute errno values, so we just pass
  * a positive value here
@@ -159,6 +155,10 @@ int _send_drop_notify(__u8 file __maybe_unused, __u16 line __maybe_unused,
 	typeof(ext_err) __ext_err = (ext_err); \
 	__DROP_REASON(err) | ((__u8)(__ext_err < -128 ? 0 : __ext_err) << 8); \
 })
+
+#include "../source_names_to_ids.h"
+
+#define __MAGIC_FILE__ (__u8)__source_file_name_to_id(__FILE_NAME__)
 
 #define send_drop_notify(ctx, src, dst, dst_id, reason, exitcode, direction) \
 	_send_drop_notify(__MAGIC_FILE__, __LINE__, ctx, src, dst, dst_id, \

bpf/source_names_to_ids.h
@@ -1,0 +1,40 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright Authors of Cilium */
+#ifndef HEADER_NAMES_TO_IDS_H_
+#define HEADER_NAMES_TO_IDS_H_
+
+#define _strcase_(id, known_name) do {			\
+	if (!__builtin_strcmp(header_name, known_name))	\
+		return id;				\
+	} while (0)
+
+/*
+ * The __source_file_name_to_id function is used inside lib/drop.h to encode
+ * source file information with drop info messages. It must be always inlined,
+ * otherwise clang won't translate this to a constexpr.
+ *
+ * The following list of files is static, but it is validated during build with
+ * the pkg/datapath/loader/check-sources.sh tool.
+ */
+static __always_inline int
+__source_file_name_to_id(const char *const header_name)
+{
+	/* source files from bpf/ */
+	_strcase_(1, "bpf_host.c");
+	_strcase_(2, "bpf_lxc.c");
+	_strcase_(3, "bpf_overlay.c");
+	_strcase_(4, "bpf_xdp.c");
+
+	/* header files from bpf/lib/ */
+	_strcase_(101, "arp.h");
+	_strcase_(102, "drop.h");
+	_strcase_(103, "egress_policies.h");
+	_strcase_(104, "icmp6.h");
+	_strcase_(105, "nodeport.h");
+
+	return 0;
+}
+
+#undef _strcase_
+
+#endif /* HEADER_NAMES_TO_IDS_H_ */

pkg/datapath/loader/compile.go
@@ -293,44 +293,6 @@ func progCFlags(prog *progInfo, dir *directoryInfo) []string {
 	)
 }
 
-// We need to map all bpf/*.c files to ids (an id value doesn't matter, but
-// should be a unique non-zero __u8 number).  A build will fail if there is
-// a file found for which there is no a corresponding id.
-var sourceNameToId map[string]int = map[string]int{
-	"bpf_network.c":            1,
-	"bpf_sock.c":               2,
-	"bpf_host.c":               3,
-	"bpf_overlay.c":            4,
-	"bpf_xdp.c":                5,
-	"bpf_alignchecker.c":       6,
-	"cilium-probe-kernel-hz.c": 7,
-	"bpf_lxc.c":                8,
-}
-
-var idToSourceName map[int]string
-
-func encodeSourceName(sourceName string) int {
-	if id, ok := sourceNameToId[sourceName]; ok {
-		return id
-	}
-	return 0
-}
-
-func DecodeSourceName(id int) string {
-	// auto-generate idToSourceName to simplify updates of the list
-	if idToSourceName == nil {
-		idToSourceName = make(map[int]string)
-		for sourceName, id := range sourceNameToId {
-			idToSourceName[id] = sourceName
-		}
-	}
-
-	if sourceName, ok := idToSourceName[id]; ok {
-		return sourceName
-	}
-	return "unknown"
-}
-
 // compile and link a program.
 func compile(ctx context.Context, prog *progInfo, dir *directoryInfo) (err error) {
 	args := make([]string, 0, 16)
@@ -343,7 +305,6 @@ func compile(ctx context.Context, prog *progInfo, dir *directoryInfo) (err error
 
 	args = append(args, standardCFlags...)
 	args = append(args, prog.Options...)
-	args = append(args, fmt.Sprintf("-D__MAGIC_FILE__=%d", encodeSourceName(prog.Source)))
 	args = append(args, progCFlags(prog, dir)...)
 
 	// Compilation is split between two exec calls. First clang generates

pkg/monitor/datapath_drop.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/cilium/cilium/pkg/datapath/loader"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/monitor/api"
 )
@@ -50,8 +49,8 @@ func (n *DropNotify) dumpIdentity(buf *bufio.Writer, numeric DisplayFormat) {
 // DumpInfo prints a summary of the drop messages.
 func (n *DropNotify) DumpInfo(data []byte, numeric DisplayFormat) {
 	buf := bufio.NewWriter(os.Stdout)
-	fmt.Fprintf(buf, "xx drop (%s) flow %#x to endpoint %d, ifindex %d, file %s line %d, ",
-		api.DropReasonExt(n.SubType, n.ExtError), n.Hash, n.DstID, n.Ifindex, loader.DecodeSourceName(int(n.File)), int(n.Line))
+	fmt.Fprintf(buf, "xx drop (%s) flow %#x to endpoint %d, ifindex %d, file %d:%d, ",
+		api.DropReasonExt(n.SubType, n.ExtError), n.Hash, n.DstID, n.Ifindex, int(n.File), int(n.Line))
 	n.dumpIdentity(buf, numeric)
 	fmt.Fprintf(buf, ": %s\n", GetConnectionSummary(data[DropNotifyLen:]))
 	buf.Flush()

commit 2f3bd6f224d7c69be05feb05037f33cf2e16ccda

Fix improper regex in check-sources.sh awk command
Parens need to be escaped with `\` in regex. Some versions of awk will
fail without it.

Fixes: #21284
Fixes: ac2106e58908
Signed-off-by: Nathan Perkins <nperkins487@gmail.com>

pkg/datapath/loader/check-sources.sh
@@ -9,7 +9,7 @@ defined_files=$(
 	awk -F: '/^__source_file_name_to_id/{found=1; next}
 		/return 0/{exit}
 		{if (!found || !/_strcase_/) next}
-		{gsub(/.* |"|)|;/, "", $1); print $1}
+		{gsub(/.* |"|\)|;/, "", $1); print $1}
 		' bpf/source_names_to_ids.h
 )
 