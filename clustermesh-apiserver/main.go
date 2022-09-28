g0odi3z2j/cilium

commit 69156a52be5d1452b25a91d996b94bd536349d9f

gops: Create the gops cell for the gops agent
Add a hive cell for running the gops agent.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/root.go
@@ -8,11 +8,11 @@ import (
 	"os"
 	"time"
 
-	gops "github.com/google/gops/agent"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"go.uber.org/fx"
 
+	"github.com/cilium/cilium/pkg/gops"
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/version"
@@ -58,6 +58,7 @@ func init() {
 	agentHive = hive.New(
 		Vp, RootCmd.Flags(),
 
+		gops.Cell,
 		hive.NewCell("daemon", fx.Invoke(registerDaemonHooks)),
 	)
 }
@@ -69,17 +70,6 @@ func runApp(cmd *cobra.Command, args []string) {
 		fmt.Printf("%s %s\n", cmd.Name(), version.Version)
 		os.Exit(0)
 	}
-
-	// Open socket for using gops to get stacktraces of the agent.
-	addr := fmt.Sprintf("127.0.0.1:%d", Vp.GetInt(option.GopsPort))
-	addrField := logrus.Fields{"address": addr}
-	if err := gops.Listen(gops.Options{
-		Addr:                   addr,
-		ReuseSocketAddrAndPort: true,
-	}); err != nil {
-		log.WithError(err).WithFields(addrField).Fatal("Cannot start gops server")
-	}
-	log.WithFields(addrField).Info("Started gops server")
 
 	agentHive.Run()
 }

pkg/gops/cell.go
@@ -1,0 +1,54 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package gops
+
+import (
+	"context"
+	"fmt"
+
+	gopsAgent "github.com/google/gops/agent"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+	"go.uber.org/fx"
+
+	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/option"
+)
+
+// Gops runs the gops agent, a tool to list and diagnose Go processes.
+// See https://github.com/google/gops.
+var Cell = hive.NewCellWithConfig[GopsConfig](
+	"gops",
+	fx.Invoke(registerGopsHooks),
+)
+
+type GopsConfig struct {
+	GopsPort uint16 // Port for gops server to listen on
+}
+
+func (GopsConfig) CellFlags(flags *pflag.FlagSet) {
+	flags.Uint16(option.GopsPort, defaults.GopsPortAgent, "Port for gops server to listen on")
+}
+
+func registerGopsHooks(lc fx.Lifecycle, log logrus.FieldLogger, cfg GopsConfig) {
+	addr := fmt.Sprintf("127.0.0.1:%d", cfg.GopsPort)
+	addrField := logrus.Fields{"address": addr, logfields.LogSubsys: "gops"}
+	log = log.WithFields(addrField)
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			log.Info("Started gops server")
+			return gopsAgent.Listen(gopsAgent.Options{
+				Addr:                   addr,
+				ReuseSocketAddrAndPort: true,
+			})
+		},
+		OnStop: func(context.Context) error {
+			gopsAgent.Close()
+			log.Info("Stopped gops server")
+			return nil
+		},
+	})
+}

daemon/cmd/daemon_main.go
@@ -817,9 +817,6 @@ func initializeFlags() {
 	flags.MarkHidden(option.CMDRef)
 	regOpts.BindEnv(option.CMDRef)
 
-	flags.Int(option.GopsPort, defaults.GopsPortAgent, "Port for gops server to listen on")
-	regOpts.BindEnv(option.GopsPort)
-
 	flags.Int(option.ToFQDNsMinTTL, 0, fmt.Sprintf("The minimum time, in seconds, to use DNS data for toFQDNs policies. (default %d )", defaults.ToFQDNsMinTTL))
 	regOpts.BindEnv(option.ToFQDNsMinTTL)
 

commit acdbfa4c3d710d7f81e494390191e717ca7bb22e

gops: Fix the gops default port
The gops cell cannot default to GopsPortAgent, but rather requires
that each application supplies the right default so they do not overlap.

Fixes: 69156a52be ("gops: Create the gops cell for the gops agent")

Signed-off-by: Jussi Maki <jussi@isovalent.com>

clustermesh-apiserver/main.go
@@ -99,6 +99,8 @@ var (
 )
 
 func init() {
+	gops.DefaultGopsPort = defaults.GopsPortApiserver
+
 	rootHive = hive.New(
 		vp, rootCmd.Flags(),
 

daemon/cmd/root.go
@@ -13,6 +13,7 @@ import (
 	"github.com/spf13/pflag"
 	"go.uber.org/fx"
 
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/gops"
 	"github.com/cilium/cilium/pkg/hive"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
@@ -67,6 +68,8 @@ func init() {
 	setupSleepBeforeFatal()
 	registerBootstrapMetrics()
 	initializeFlags()
+
+	gops.DefaultGopsPort = defaults.GopsPortAgent
 
 	agentHive = hive.New(
 		Vp,

operator/cmd/root.go
@@ -30,6 +30,7 @@ import (
 	operatorWatchers "github.com/cilium/cilium/operator/watchers"
 
 	"github.com/cilium/cilium/pkg/components"
+	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/gops"
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/ipam/allocator"
@@ -133,6 +134,8 @@ func registerOperatorHooks(lc fx.Lifecycle, clientset k8sClient.Clientset, shutd
 
 func init() {
 	rootCmd.AddCommand(MetricsCmd)
+
+	gops.DefaultGopsPort = defaults.GopsPortOperator
 
 	operatorHive = hive.New(
 		Vp,

pkg/gops/cell.go
@@ -12,17 +12,21 @@ import (
 	"github.com/spf13/pflag"
 	"go.uber.org/fx"
 
-	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 )
 
-// Gops runs the gops agent, a tool to list and diagnose Go processes.
-// See https://github.com/google/gops.
-var Cell = hive.NewCellWithConfig[GopsConfig](
-	"gops",
-	fx.Invoke(registerGopsHooks),
+var (
+	// DefaultGopsPort is the default for --gops-port option.
+	DefaultGopsPort = uint16(0)
+
+	// Cell runs the gops agent, a tool to list and diagnose Go processes.
+	// See https://github.com/google/gops.
+	Cell = hive.NewCellWithConfig[GopsConfig](
+		"gops",
+		fx.Invoke(registerGopsHooks),
+	)
 )
 
 type GopsConfig struct {
@@ -30,7 +34,7 @@ type GopsConfig struct {
 }
 
 func (GopsConfig) CellFlags(flags *pflag.FlagSet) {
-	flags.Uint16(option.GopsPort, defaults.GopsPortAgent, "Port for gops server to listen on")
+	flags.Uint16(option.GopsPort, DefaultGopsPort, "Port for gops server to listen on")
 }
 
 func registerGopsHooks(lc fx.Lifecycle, log logrus.FieldLogger, cfg GopsConfig) {