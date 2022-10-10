g0odi3z2j/cilium

commit 06d83b6bc63dae11b0becff7461f39f87fd13079

node: Add LocalNodeStore and convert getters/setters to it
This adds LocalNodeStore, a canonical owner of 'types.Node' for the local
node. The supported operations on the store are Observe, Update and Get.

With this construction we can simplify the persisting of the local state
and the code that modify the local node state. Typical example is ipsecKeyIdentity
updated from pkg/ipsec, where not only we need to update the global variable,
but also inform both nodediscovery (to push change to k8s/kvstore) and datapath.
Now instead we would call 'LocalNodeStore.Update' instead and can drop the IPsec
dependency to nodediscovery and NodeHandler.

Since this is a large change to push through, this starts by simply converting
the existing getters and setters over to using LocalNodeStore, without yet
observing it.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/root.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cilium/cilium/pkg/gops"
 	"github.com/cilium/cilium/pkg/hive"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/version"
 )
@@ -75,6 +76,14 @@ func init() {
 		k8sClient.Cell,
 
 		hive.NewCellWithConfig[DaemonCellConfig]("daemon", fx.Invoke(registerDaemonHooks)),
+
+		node.LocalNodeStoreCell,
+		hive.Invoke(func(store node.LocalNodeStore) {
+			// Set the global LocalNodeStore. This is to retain the API of getters and setters
+			// defined in pkg/node/address.go until uses of them have been converted to use
+			// LocalNodeStore directly.
+			node.SetLocalNodeStore(store)
+		}),
 	)
 }
 

pkg/datapath/linux/devices_test.go
@@ -32,6 +32,7 @@ type DevicesSuite struct {
 	prevConfigTunnel              string
 	prevConfigEnableIPv6NDP       bool
 	prevK8sNodeIP                 net.IP
+	prevK8sNodeIPv6               net.IP
 }
 
 var _ = Suite(&DevicesSuite{})
@@ -47,7 +48,8 @@ func (s *DevicesSuite) SetUpSuite(c *C) {
 	s.prevConfigTunnel = option.Config.Tunnel
 	s.prevConfigEnableIPv6NDP = option.Config.EnableIPv6NDP
 	s.prevConfigIPv6MCastDevice = option.Config.IPv6MCastDevice
-	s.prevK8sNodeIP = node.GetK8sNodeIP()
+	s.prevK8sNodeIP = node.GetIPv4()
+	s.prevK8sNodeIPv6 = node.GetIPv6()
 	s.currentNetNS, err = netns.Get()
 	c.Assert(err, IsNil)
 }
@@ -61,7 +63,8 @@ func (s *DevicesSuite) TearDownTest(c *C) {
 	option.Config.Tunnel = s.prevConfigTunnel
 	option.Config.EnableIPv6NDP = s.prevConfigEnableIPv6NDP
 	option.Config.IPv6MCastDevice = s.prevConfigIPv6MCastDevice
-	node.SetK8sNodeIP(s.prevK8sNodeIP)
+	node.SetIPv4(s.prevK8sNodeIP)
+	node.SetIPv6(s.prevK8sNodeIPv6)
 }
 
 func (s *DevicesSuite) TestDetect(c *C) {
@@ -80,7 +83,7 @@ func (s *DevicesSuite) TestDetect(c *C) {
 		// 2. Node IP not set, can still detect. Direct routing device shouldn't be detected.
 		option.Config.EnableNodePort = true
 		c.Assert(createDummy("dummy0", "192.168.0.1/24", false), IsNil)
-		node.SetK8sNodeIP(nil)
+		node.SetIPv4(nil)
 
 		devices, err = dm.Detect()
 		c.Assert(err, IsNil)
@@ -90,7 +93,7 @@ func (s *DevicesSuite) TestDetect(c *C) {
 
 		// 3. Manually specified devices, no detection is performed
 		option.Config.EnableNodePort = true
-		node.SetK8sNodeIP(net.ParseIP("192.168.0.1"))
+		node.SetIPv4(net.ParseIP("192.168.0.1"))
 		c.Assert(createDummy("dummy1", "192.168.1.1/24", false), IsNil)
 		option.Config.SetDevices([]string{"dummy0"})
 
@@ -106,7 +109,7 @@ func (s *DevicesSuite) TestDetect(c *C) {
 		c.Assert(createDummy("dummy2", "192.168.2.1/24", false), IsNil)
 		c.Assert(createDummy("dummy3", "192.168.3.1/24", false), IsNil)
 		c.Assert(delRoutes("dummy3"), IsNil) // Delete routes so it won't be detected
-		node.SetK8sNodeIP(net.ParseIP("192.168.1.1"))
+		node.SetIPv4(net.ParseIP("192.168.1.1"))
 		option.Config.EnableIPv4 = true
 		option.Config.EnableIPv6 = false
 		option.Config.Tunnel = option.TunnelDisabled
@@ -123,7 +126,8 @@ func (s *DevicesSuite) TestDetect(c *C) {
 		option.Config.EnableIPv6 = true
 		option.Config.EnableIPv6NDP = true
 		c.Assert(createDummy("cilium_foo", "2001:db8::face/64", true), IsNil)
-		node.SetK8sNodeIP(net.ParseIP("2001:db8::face"))
+		node.SetIPv4(nil)
+		node.SetIPv6(net.ParseIP("2001:db8::face"))
 		devices, err = dm.Detect()
 		c.Assert(err, IsNil)
 		c.Assert(devices, checker.DeepEquals, []string{"cilium_foo", "dummy0", "dummy1", "dummy2"})

pkg/hive/hive.go
@@ -76,6 +76,13 @@ func New(v *viper.Viper, flags *pflag.FlagSet, cells ...*Cell) *Hive {
 	return h
 }
 
+// NewForTests returns a new hive for testing that does not register
+// the flags declared by the cells. Use in combination with TestApp()
+// to provide cell configurations.
+func NewForTests(cells ...*Cell) *Hive {
+	return New(viper.New(), pflag.NewFlagSet("", pflag.ContinueOnError), cells...)
+}
+
 func (h *Hive) SetTimeouts(start, stop time.Duration) {
 	h.startTimeout, h.stopTimeout = start, stop
 }

pkg/node/address.go
@@ -22,6 +22,7 @@ import (
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/mac"
+	"github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 )
 
@@ -33,45 +34,30 @@ var (
 	// about lock copying.
 	addrsMu lock.RWMutex
 	addrs   addresses
+
+	// localNode holds the current state of the local "types.Node".
+	// This is defined here until all uses of the getters and
+	// setters in this file have been migrated to use LocalNodeStore
+	// directly.
+	localNode LocalNodeStore = defaultLocalNodeStore()
 )
 
 type addresses struct {
 	ipv4Loopback      net.IP
-	ipv4Address       net.IP
-	ipv4RouterAddress net.IP
 	ipv4NodePortAddrs map[string]net.IP // iface name => ip addr
 	ipv4MasqAddrs     map[string]net.IP // iface name => ip addr
-	ipv6Address       net.IP
-	ipv6RouterAddress net.IP
 	ipv6NodePortAddrs map[string]net.IP // iface name => ip addr
-	ipv4AllocRange    *cidr.CIDR
-	ipv6AllocRange    *cidr.CIDR
 	routerInfo        RouterInfo
-
-	// k8s Node External IP
-	ipv4ExternalAddress net.IP
-	ipv6ExternalAddress net.IP
-
-	// Addresses of the cilium-health endpoint located on the node
-	ipv4HealthIP net.IP
-	ipv6HealthIP net.IP
-
-	// Addresses of the Cilium Ingress located on the node
-	ipv4IngressIP net.IP
-	ipv6IngressIP net.IP
-
-	// k8s Node IP (either InternalIP or ExternalIP or nil; the former is preferred)
-	k8sNodeIP net.IP
-
-	ipsecKeyIdentity uint8
-
-	wireguardPubKey string
 }
 
 type RouterInfo interface {
 	GetIPv4CIDRs() []net.IPNet
 	GetMac() mac.MAC
 	GetInterfaceNumber() int
+}
+
+func SetLocalNodeStore(s LocalNodeStore) {
+	localNode = s
 }
 
 func makeIPv6HostIP() net.IP {
@@ -89,29 +75,30 @@ func makeIPv6HostIP() net.IP {
 // network device of the system in which case the first address with global
 // scope will be regarded as the system's node address.
 func InitDefaultPrefix(device string) {
-	addrsMu.Lock()
-	defer addrsMu.Unlock()
-
 	if option.Config.EnableIPv4 {
-		ip, err := firstGlobalV4Addr(device, addrs.ipv4RouterAddress, preferPublicIP)
+		ip, err := firstGlobalV4Addr(device, GetInternalIPv4Router(), preferPublicIP)
 		if err != nil {
 			return
 		}
 
-		if addrs.ipv4Address == nil {
-			addrs.ipv4Address = ip
+		if GetIPv4() == nil {
+			SetIPv4(ip)
 		}
 
-		if addrs.ipv4AllocRange == nil {
+		ipv4range := GetIPv4AllocRange()
+		ipv6range := GetIPv6AllocRange()
+
+		if ipv4range == nil {
 			// If the IPv6AllocRange is not nil then the IPv4 allocation should be
 			// derived from the IPv6AllocRange.
 			//                     vvvv vvvv
 			// FD00:0000:0000:0000:0000:0000:0000:0000
-			if addrs.ipv6AllocRange != nil {
-				ip = net.IPv4(addrs.ipv6AllocRange.IP[8],
-					addrs.ipv6AllocRange.IP[9],
-					addrs.ipv6AllocRange.IP[10],
-					addrs.ipv6AllocRange.IP[11])
+			if ipv6range != nil {
+				ip = net.IPv4(
+					ipv6range.IP[8],
+					ipv6range.IP[9],
+					ipv6range.IP[10],
+					ipv6range.IP[11])
 			}
 			v4range := fmt.Sprintf(defaults.DefaultIPv4Prefix+"/%d",
 				ip.To4()[3], defaults.DefaultIPv4PrefixLen)
@@ -120,23 +107,27 @@ func InitDefaultPrefix(device string) {
 				log.WithError(err).WithField(logfields.V4Prefix, v4range).Panic("BUG: Invalid default IPv4 prefix")
 			}
 
-			addrs.ipv4AllocRange = cidr.NewCIDR(ip4net)
-			log.WithField(logfields.V4Prefix, addrs.ipv4AllocRange).Info("Using autogenerated IPv4 allocation range")
+			SetIPv4AllocRange(cidr.NewCIDR(ip4net))
+			log.WithField(logfields.V4Prefix, GetIPv4AllocRange()).Info("Using autogenerated IPv4 allocation range")
 		}
 	}
 
 	if option.Config.EnableIPv6 {
-		if addrs.ipv6Address == nil {
+		ipv4range := GetIPv4AllocRange()
+		ipv6range := GetIPv6AllocRange()
+
+		if GetIPv6() == nil {
 			// Find a IPv6 node address first
-			addrs.ipv6Address, _ = firstGlobalV6Addr(device, addrs.ipv6RouterAddress, preferPublicIP)
-			if addrs.ipv6Address == nil {
-				addrs.ipv6Address = makeIPv6HostIP()
+			addr, _ := firstGlobalV6Addr(device, GetIPv6Router(), preferPublicIP)
+			if addr == nil {
+				addr = makeIPv6HostIP()
 			}
+			SetIPv6(addr)
 		}
 
-		if addrs.ipv6AllocRange == nil && addrs.ipv4AllocRange != nil {
+		if ipv6range == nil && ipv4range != nil {
 			// The IPv6 allocation should be derived from the IPv4 allocation.
-			ip := addrs.ipv4AllocRange.IP
+			ip := ipv4range.IP
 			v6range := fmt.Sprintf("%s%02x%02x:%02x%02x:0:0/%d",
 				option.Config.IPv6ClusterAllocCIDRBase, ip[0], ip[1], ip[2], ip[3], 96)
 
@@ -145,8 +136,8 @@ func InitDefaultPrefix(device string) {
 				log.WithError(err).WithField(logfields.V6Prefix, v6range).Panic("BUG: Invalid default IPv6 prefix")
 			}
 
-			addrs.ipv6AllocRange = cidr.NewCIDR(ip6net)
-			log.WithField(logfields.V6Prefix, addrs.ipv6AllocRange).Info("Using autogenerated IPv6 allocation range")
+			SetIPv6NodeRange(cidr.NewCIDR(ip6net))
+			log.WithField(logfields.V6Prefix, GetIPv6AllocRange()).Info("Using autogenerated IPv6 allocation range")
 		}
 	}
 }
@@ -163,7 +154,7 @@ func InitNodePortAddrs(devices []string, inheritIPAddrFromDevice string) error {
 
 	if option.Config.EnableIPv4 {
 		if inheritIPAddrFromDevice != "" {
-			inheritedIP, err = firstGlobalV4Addr(inheritIPAddrFromDevice, addrs.k8sNodeIP, !preferPublicIP)
+			inheritedIP, err = firstGlobalV4Addr(inheritIPAddrFromDevice, GetK8sNodeIP(), !preferPublicIP)
 			if err != nil {
 				return fmt.Errorf("failed to determine IPv4 of %s for NodePort", inheritIPAddrFromDevice)
 			}
@@ -173,7 +164,7 @@ func InitNodePortAddrs(devices []string, inheritIPAddrFromDevice string) error {
 			if inheritIPAddrFromDevice != "" {
 				addrs.ipv4NodePortAddrs[device] = inheritedIP
 			} else {
-				ip, err := firstGlobalV4Addr(device, addrs.k8sNodeIP, !preferPublicIP)
+				ip, err := firstGlobalV4Addr(device, GetK8sNodeIP(), !preferPublicIP)
 				if err != nil {
 					return fmt.Errorf("failed to determine IPv4 of %s for NodePort", device)
 				}
@@ -184,7 +175,7 @@ func InitNodePortAddrs(devices []string, inheritIPAddrFromDevice string) error {
 
 	if option.Config.EnableIPv6 {
 		if inheritIPAddrFromDevice != "" {
-			inheritedIP, err = firstGlobalV6Addr(inheritIPAddrFromDevice, addrs.k8sNodeIP, !preferPublicIP)
+			inheritedIP, err = firstGlobalV6Addr(inheritIPAddrFromDevice, GetK8sNodeIP(), !preferPublicIP)
 			if err != nil {
 				return fmt.Errorf("Failed to determine IPv6 of %s for NodePort", inheritIPAddrFromDevice)
 			}
@@ -194,7 +185,7 @@ func InitNodePortAddrs(devices []string, inheritIPAddrFromDevice string) error {
 			if inheritIPAddrFromDevice != "" {
 				addrs.ipv6NodePortAddrs[device] = inheritedIP
 			} else {
-				ip, err := firstGlobalV6Addr(device, addrs.k8sNodeIP, !preferPublicIP)
+				ip, err := firstGlobalV6Addr(device, GetK8sNodeIP(), !preferPublicIP)
 				if err != nil {
 					return fmt.Errorf("Failed to determine IPv6 of %s for NodePort", device)
 				}
@@ -226,7 +217,7 @@ func InitBPFMasqueradeAddrs(devices []string) error {
 		}
 
 		for _, device := range devices {
-			ip, err := firstGlobalV4Addr(device, addrs.k8sNodeIP, preferPublicIP)
+			ip, err := firstGlobalV4Addr(device, GetK8sNodeIP(), preferPublicIP)
 			if err != nil {
 				return fmt.Errorf("Failed to determine IPv4 of %s for BPF masq", device)
 			}
@@ -267,16 +258,12 @@ func SetIPv4Loopback(ip net.IP) {
 
 // GetIPv4AllocRange returns the IPv4 allocation prefix of this node
 func GetIPv4AllocRange() *cidr.CIDR {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return addrs.ipv4AllocRange.DeepCopy()
+	return localNode.Get().IPv4AllocCIDR.DeepCopy()
 }
 
 // GetIPv6AllocRange returns the IPv6 allocation prefix of this node
 func GetIPv6AllocRange() *cidr.CIDR {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return addrs.ipv6AllocRange.DeepCopy()
+	return localNode.Get().IPv6AllocCIDR.DeepCopy()
 }
 
 // SetIPv4 sets the IPv4 node address. It must be reachable on the network.
@@ -285,9 +272,9 @@ func GetIPv6AllocRange() *cidr.CIDR {
 // - NodeExternalIP
 // - other IP address type
 func SetIPv4(ip net.IP) {
-	addrsMu.Lock()
-	addrs.ipv4Address = clone(ip)
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.SetNodeInternalIP(ip)
+	})
 }
 
 // GetIPv4 returns one of the IPv4 node address available with the following
@@ -297,9 +284,8 @@ func SetIPv4(ip net.IP) {
 // - other IP address type.
 // It must be reachable on the network.
 func GetIPv4() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv4Address)
+	n := localNode.Get()
+	return clone(n.GetNodeIP(false))
 }
 
 // SetInternalIPv4Router sets the cilium internal IPv4 node address, it is allocated from the node prefix.
@@ -307,34 +293,32 @@ func GetIPv4() net.IP {
 // Cilium-managed network (this means within the node for direct routing mode and on the overlay
 // for tunnel mode).
 func SetInternalIPv4Router(ip net.IP) {
-	addrsMu.Lock()
-	addrs.ipv4RouterAddress = clone(ip)
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.SetCiliumInternalIP(ip)
+	})
 }
 
 // GetInternalIPv4Router returns the cilium internal IPv4 node address. This must not be conflated with
 // k8s internal IP as this IP address is only relevant within the Cilium-managed network (this means
 // within the node for direct routing mode and on the overlay for tunnel mode).
 func GetInternalIPv4Router() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv4RouterAddress)
+	n := localNode.Get()
+	return n.GetCiliumInternalIP(false)
 }
 
 // SetK8sExternalIPv4 sets the external IPv4 node address. It must be a public IP that is routable
 // on the network as well as the internet.
 func SetK8sExternalIPv4(ip net.IP) {
-	addrsMu.Lock()
-	addrs.ipv4ExternalAddress = clone(ip)
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.SetNodeExternalIP(ip)
+	})
 }
 
 // GetK8sExternalIPv4 returns the external IPv4 node address. It must be a public IP that is routable
 // on the network as well as the internet. It can return nil if no External IPv4 address is assigned.
 func GetK8sExternalIPv4() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv4ExternalAddress)
+	n := localNode.Get()
+	return n.GetExternalIP(false)
 }
 
 // GetRouterInfo returns additional information for the router, the cilium_host interface.
@@ -354,17 +338,15 @@ func SetRouterInfo(info RouterInfo) {
 // GetHostMasqueradeIPv4 returns the IPv4 address to be used for masquerading
 // any traffic that is being forwarded from the host into the Cilium cluster.
 func GetHostMasqueradeIPv4() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv4RouterAddress)
+	return GetInternalIPv4Router()
 }
 
 // SetIPv4AllocRange sets the IPv4 address pool to use when allocating
 // addresses for local endpoints
 func SetIPv4AllocRange(net *cidr.CIDR) {
-	addrsMu.Lock()
-	addrs.ipv4AllocRange = net.DeepCopy()
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.IPv4AllocCIDR = net
+	})
 }
 
 // Uninitialize resets this package to the default state, for use in
@@ -420,9 +402,9 @@ func GetMasqIPv4AddrsWithDevices() map[string]net.IP {
 
 // SetIPv6NodeRange sets the IPv6 address pool to be used on this node
 func SetIPv6NodeRange(net *cidr.CIDR) {
-	addrsMu.Lock()
-	addrs.ipv6AllocRange = net.DeepCopy()
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.IPv6AllocCIDR = net
+	})
 }
 
 // AutoComplete completes the parts of addressing that can be auto derived
@@ -435,11 +417,11 @@ func AutoComplete() error {
 
 	InitDefaultPrefix(option.Config.DirectRoutingDevice)
 
-	if option.Config.EnableIPv6 && addrs.ipv6AllocRange == nil {
+	if option.Config.EnableIPv6 && GetIPv6AllocRange() == nil {
 		return fmt.Errorf("IPv6 allocation CIDR is not configured. Please specificy --%s", option.IPv6Range)
 	}
 
-	if option.Config.EnableIPv4 && addrs.ipv4AllocRange == nil {
+	if option.Config.EnableIPv4 && GetIPv4AllocRange() == nil {
 		return fmt.Errorf("IPv4 allocation CIDR is not configured. Please specificy --%s", option.IPv4Range)
 	}
 
@@ -563,15 +545,13 @@ const mismatchRouterIPsMsg = "Mismatch of router IPs found during restoration. T
 // ValidatePostInit validates the entire addressing setup and completes it as
 // required
 func ValidatePostInit() error {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
 	if option.Config.EnableIPv4 || option.Config.Tunnel != option.TunnelDisabled {
-		if addrs.ipv4Address == nil {
+		if GetIPv4() == nil {
 			return fmt.Errorf("external IPv4 node address could not be derived, please configure via --ipv4-node")
 		}
 	}
 
-	if option.Config.EnableIPv4 && addrs.ipv4RouterAddress == nil {
+	if option.Config.EnableIPv4 && GetInternalIPv4Router() == nil {
 		return fmt.Errorf("BUG: Internal IPv4 node address was not configured")
 	}
 
@@ -580,52 +560,50 @@ func ValidatePostInit() error {
 
 // SetIPv6 sets the IPv6 address of the node
 func SetIPv6(ip net.IP) {
-	addrsMu.Lock()
-	addrs.ipv6Address = clone(ip)
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.SetNodeInternalIP(ip)
+	})
 }
 
 // GetIPv6 returns the IPv6 address of the node
 func GetIPv6() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv6Address)
+	n := localNode.Get()
+	return clone(n.GetNodeIP(true))
 }
 
 // GetHostMasqueradeIPv6 returns the IPv6 address to be used for masquerading
 // any traffic that is being forwarded from the host into the Cilium cluster.
 func GetHostMasqueradeIPv6() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv6Address)
+	return GetIPv6()
 }
 
-// GetIPv6Router returns the IPv6 address of the node
+// GetIPv6Router returns the IPv6 address of the router, e.g. address
+// of cilium_host device.
 func GetIPv6Router() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv6RouterAddress)
+	n := localNode.Get()
+	return clone(n.GetCiliumInternalIP(true))
 }
 
-// SetIPv6Router returns the IPv6 address of the node
+// SetIPv6Router sets the IPv6 address of the router address, e.g. address
+// of cilium_host device.
 func SetIPv6Router(ip net.IP) {
-	addrsMu.Lock()
-	addrs.ipv6RouterAddress = clone(ip)
-	addrsMu.Unlock()
-}
-
-// SetK8sExternalIPv6 sets the external IPv6 node address. It must be a public IP.
-func SetK8sExternalIPv6(ip net.IP) {
-	addrsMu.Lock()
-	addrs.ipv6ExternalAddress = clone(ip)
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.SetCiliumInternalIP(ip)
+	})
 }
 
 // GetK8sExternalIPv6 returns the external IPv6 node address.
 func GetK8sExternalIPv6() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv6ExternalAddress)
+	n := localNode.Get()
+	return clone(n.GetExternalIP(false))
+}
+
+// SetK8sExternalIPv6 sets the external IPv6 node address. It must be a public IP that is routable
+// on the network as well as the internet.
+func SetK8sExternalIPv6(ip net.IP) {
+	localNode.Update(func(n *types.Node) {
+		n.SetNodeExternalIP(ip)
+	})
 }
 
 // GetNodeAddressing returns the NodeAddressing model for the local IPs.
@@ -745,98 +723,85 @@ func getCiliumHostIPs() (ipv4GW, ipv6Router net.IP) {
 // SetIPsecKeyIdentity sets the IPsec key identity an opaque value used to
 // identity encryption keys used on the node.
 func SetIPsecKeyIdentity(id uint8) {
-	addrsMu.Lock()
-	addrs.ipsecKeyIdentity = id
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.EncryptionKey = id
+	})
 }
 
 // GetIPsecKeyIdentity returns the IPsec key identity of the node
 func GetIPsecKeyIdentity() uint8 {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return addrs.ipsecKeyIdentity
+	return localNode.Get().EncryptionKey
 }
 
 // GetK8sNodeIPs returns k8s Node IP addr.
 func GetK8sNodeIP() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.k8sNodeIP)
+	n := localNode.Get()
+	return n.GetK8sNodeIP()
 }
 
 // SetK8sNodeIP sets k8s Node IP addr.
 func SetK8sNodeIP(ip net.IP) {
-	addrsMu.Lock()
-	addrs.k8sNodeIP = clone(ip)
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.SetNodeInternalIP(ip)
+	})
 }
 
 func SetWireguardPubKey(key string) {
-	addrsMu.Lock()
-	addrs.wireguardPubKey = key
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.WireguardPubKey = key
+	})
 }
 
 func GetWireguardPubKey() string {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return addrs.wireguardPubKey
+	return localNode.Get().WireguardPubKey
 }
 
 // SetEndpointHealthIPv4 sets the IPv4 cilium-health endpoint address.
 func SetEndpointHealthIPv4(ip net.IP) {
-	addrsMu.Lock()
-	addrs.ipv4HealthIP = clone(ip)
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.IPv4HealthIP = ip
+	})
 }
 
 // GetEndpointHealthIPv4 returns the IPv4 cilium-health endpoint address.
 func GetEndpointHealthIPv4() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv4HealthIP)
+	return localNode.Get().IPv4HealthIP
 }
 
 // SetEndpointHealthIPv6 sets the IPv6 cilium-health endpoint address.
 func SetEndpointHealthIPv6(ip net.IP) {
-	addrsMu.Lock()
-	addrs.ipv6HealthIP = clone(ip)
-	addrsMu.Unlock()
+	localNode.Update(func(n *types.Node) {
+		n.IPv6HealthIP = ip
+	})
 }
 
 // GetEndpointHealthIPv6 returns the IPv6 cilium-health endpoint address.
 func GetEndpointHealthIPv6() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv6HealthIP)
+	return localNode.Get().IPv6HealthIP
 }
 
 // SetIngressIPv4 sets the local IPv4 source address for Cilium Ingress.
 func SetIngressIPv4(ip net.IP) {
-	addrsMu.RLock()
-	addrs.ipv4IngressIP = clone(ip)
-	addrsMu.RUnlock()
+	localNode.Update(func(n *types.Node) {
+		n.IPv4IngressIP = ip
+	})
 }
 
 // GetIngressIPv4 returns the local IPv4 source address for Cilium Ingress.
 func GetIngressIPv4() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv4IngressIP)
+	return localNode.Get().IPv4IngressIP
 }
 
 // SetIngressIPv6 sets the local IPv6 source address for Cilium Ingress.
 func SetIngressIPv6(ip net.IP) {
-	addrsMu.RLock()
-	addrs.ipv6IngressIP = clone(ip)
-	addrsMu.RUnlock()
+	localNode.Update(func(n *types.Node) {
+		n.IPv6IngressIP = ip
+	})
 }
 
 // GetIngressIPv6 returns the local IPv6 source address for Cilium Ingress.
 func GetIngressIPv6() net.IP {
-	addrsMu.RLock()
-	defer addrsMu.RUnlock()
-	return clone(addrs.ipv6IngressIP)
+	return localNode.Get().IPv6IngressIP
 }
 
 func copyStringToNetIPMap(in map[string]net.IP) map[string]net.IP {

pkg/node/address_test.go
@@ -114,21 +114,8 @@ func (s *NodeSuite) Test_chooseHostIPsToRestore(c *C) {
 	for _, tt := range tests {
 		c.Log("Test: " + tt.name)
 		got, err := chooseHostIPsToRestore(tt.ipv6, tt.fromK8s, tt.fromFS, []*cidr.CIDR{tt.cidr})
-		if tt.expect == nil {
-			// If we don't expect to change it, set it to what's currently the
-			// router IP.
-			if tt.ipv6 {
-				tt.expect = GetIPv6Router()
-			} else {
-				tt.expect = GetInternalIPv4Router()
-			}
-		}
 		c.Assert(err, checker.DeepEquals, tt.err)
-		if tt.ipv6 {
-			c.Assert(got, checker.DeepEquals, tt.expect)
-		} else {
-			c.Assert(got, checker.DeepEquals, tt.expect)
-		}
+		c.Assert(got, checker.DeepEquals, tt.expect)
 	}
 }
 

pkg/node/host_endpoint.go
@@ -3,32 +3,26 @@
 
 package node
 
-import (
-	"github.com/cilium/cilium/pkg/lock"
-)
+import "github.com/cilium/cilium/pkg/node/types"
 
 const (
 	templateHostEndpointID = uint64(0xffff)
 )
 
 var (
-	labels     map[string]string
-	labelsMu   lock.RWMutex
 	endpointID = templateHostEndpointID
 )
 
 // GetLabels returns the labels of this node.
 func GetLabels() map[string]string {
-	labelsMu.RLock()
-	defer labelsMu.RUnlock()
-	return labels
+	return localNode.Get().Labels
 }
 
 // SetLabels sets the labels of this node.
 func SetLabels(l map[string]string) {
-	labelsMu.Lock()
-	defer labelsMu.Unlock()
-	labels = l
+	localNode.Update(func(n *types.Node) {
+		n.Labels = l
+	})
 }
 
 // GetEndpointID returns the ID of the host endpoint for this node.

commit 30c4384359942879ca10f5a103eca9230e99c674

daemon: Move SetLocalNodeStore to registerDaemonHooks for correct order
We need to make sure that LocalNodeStore gets started before registerDaemonHooks
as otherwise Get/Update blocks. Fix this by making registerDaemonHooks depend
on LocalNodeStore and move the setting of node.localNode to it.

Since the list of parameters for registerDaemonHooks is getting quite long,
add daemonParams struct.

Fixes: 06d83b6bc6 ("node: Add LocalNodeStore and convert getters/setters to it")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

daemon/cmd/daemon_main.go
@@ -46,6 +46,7 @@ import (
 	"github.com/cilium/cilium/pkg/envoy"
 	"github.com/cilium/cilium/pkg/flowdebug"
 	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
 	"github.com/cilium/cilium/pkg/hubble/exporter/exporteroption"
 	"github.com/cilium/cilium/pkg/hubble/observer/observeroption"
 	"github.com/cilium/cilium/pkg/identity"
@@ -1538,6 +1539,16 @@ func (d *Daemon) initKVStore() {
 	}
 }
 
+type daemonParams struct {
+	cell.In
+
+	Lifecycle      hive.Lifecycle
+	Shutdowner     hive.Shutdowner
+	Config         DaemonCellConfig
+	LocalNodeStore node.LocalNodeStore
+	Clientset      k8sClient.Clientset
+}
+
 // registerDaemonHooks registers the lifecycle hooks for the part of the cilium-agent that has
 // not yet been modularized.
 //
@@ -1548,8 +1559,8 @@ func (d *Daemon) initKVStore() {
 //
 // If an object still owned by Daemon is required in a module, it should be provided indirectly, e.g. via
 // a callback.
-func registerDaemonHooks(lc hive.Lifecycle, shutdowner hive.Shutdowner, cfg DaemonCellConfig, clientset k8sClient.Clientset) error {
-	if cfg.SkipDaemon {
+func registerDaemonHooks(params daemonParams) error {
+	if params.Config.SkipDaemon {
 		return nil
 	}
 
@@ -1560,15 +1571,20 @@ func registerDaemonHooks(lc hive.Lifecycle, shutdowner hive.Shutdowner, cfg Daem
 		// In lb-only mode the k8s client is not used, even if its configuration
 		// is available, so disable it here before it starts. Using GetString
 		// directly as option.Config not populated yet.
-		clientset.Disable()
+		params.Clientset.Disable()
 	}
 
-	lc.Append(hive.Hook{
+	// Set the global LocalNodeStore. This is to retain the API of getters and setters
+	// defined in pkg/node/address.go until uses of them have been converted to use
+	// LocalNodeStore directly.
+	node.SetLocalNodeStore(params.LocalNodeStore)
+
+	params.Lifecycle.Append(hive.Hook{
 		OnStart: func(context.Context) error {
 			// Set the k8s clients provided by the K8s client cell. The global clients will be refactored out
 			// by later commits.
-			if clientset.IsEnabled() {
-				k8s.SetClients(clientset, clientset.Slim(), clientset, clientset)
+			if params.Clientset.IsEnabled() {
+				k8s.SetClients(params.Clientset, params.Clientset.Slim(), params.Clientset, params.Clientset)
 			}
 
 			bootstrapStats.earlyInit.Start()
@@ -1577,7 +1593,7 @@ func registerDaemonHooks(lc hive.Lifecycle, shutdowner hive.Shutdowner, cfg Daem
 
 			// Start running the daemon in the background (blocks on API server's Serve()) to allow rest
 			// of the start hooks to run.
-			go runDaemon(ctx, cleaner, shutdowner, clientset)
+			go runDaemon(ctx, cleaner, params.Shutdowner, params.Clientset)
 			return nil
 		},
 		OnStop: func(context.Context) error {

daemon/cmd/root.go
@@ -81,12 +81,6 @@ func init() {
 		cell.Invoke(registerDaemonHooks),
 
 		node.LocalNodeStoreCell,
-		cell.Invoke(func(store node.LocalNodeStore) {
-			// Set the global LocalNodeStore. This is to retain the API of getters and setters
-			// defined in pkg/node/address.go until uses of them have been converted to use
-			// LocalNodeStore directly.
-			node.SetLocalNodeStore(store)
-		}),
 	)
 	Vp = agentHive.Viper()
 	agentHive.RegisterFlags(RootCmd.PersistentFlags())