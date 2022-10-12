g0odi3z2j/cilium

commit 8cbcc7a4b539dfbdf79a255bf9e97ffd33176b38

ipcache: Fix metadata access from CIDR allocation
The locking order for metadata -> IPCache should be first grabbing the
metadata lock then the IPCache lock, according to the documentation in
the metadata structure. Correct this lock ordering to conform to the
documented pattern.

This sort of improper lock ordering could theoretically cause a deadlock
if for instance the label injector ran at the same time as this function
is called.

Found by inspection.

Fixes: ca37993352f4 ("ipcache: Fix race in identity/ipcache release")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/ipcache/cidr.go
@@ -52,11 +52,12 @@ func (ipc *IPCache) AllocateCIDRs(
 	allocateCtx, cancel := context.WithTimeout(context.Background(), option.Config.IPAllocationTimeout)
 	defer cancel()
 
+	ipc.metadata.RLock()
 	ipc.Lock()
 	allocatedIdentities := make(map[netip.Prefix]*identity.Identity, len(prefixes))
 	for i, prefix := range prefixes {
 		lbls := cidr.GetCIDRLabels(prefix)
-		lbls.MergeLabels(ipc.metadata.get(prefix).ToLabels())
+		lbls.MergeLabels(ipc.metadata.getLocked(prefix).ToLabels())
 		oldNID := identity.InvalidIdentity
 		if oldNIDs != nil && len(oldNIDs) > i {
 			oldNID = oldNIDs[i]
@@ -75,6 +76,7 @@ func (ipc *IPCache) AllocateCIDRs(
 		}
 	}
 	ipc.Unlock()
+	ipc.metadata.RUnlock()
 
 	// Only upsert into ipcache if identity wasn't allocated
 	// before and the caller does not care doing this

commit 097534da991f58289ded62efe82fd109eb119427

ipcache: Release metadata mutex in loop error condition
Found while backporting the following commit to older branches.

Fixes: 8cbcc7a4b539 ("ipcache: Fix metadata access from CIDR allocation")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/ipcache/cidr.go
@@ -66,6 +66,7 @@ func (ipc *IPCache) AllocateCIDRs(
 		if err != nil {
 			ipc.IdentityAllocator.ReleaseSlice(context.Background(), nil, usedIdentities)
 			ipc.Unlock()
+			ipc.metadata.RUnlock()
 			return nil, err
 		}
 