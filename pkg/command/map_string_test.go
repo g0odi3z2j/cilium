g0odi3z2j/cilium

commit c533f0eaeead7c4ce6ee3a8e79ddf9853a470749

cmd: Allow more complicated patterns in map string type.
The previous PR #18478 wraps existing viper GetStringMapString function
to get around upstream bugs, however, it's unintentionally restricted a
few formats, which supported before in cilium, such as:

- --aws-instance-limit-mapping=c6a.2xlarge=4,15,15,m4.xlarge=1,5,10
- --api-rate-limit=endpoint-create=rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true

For complicated attribute, we are allowing comma character in value part
of key value pair. As golang didn't support look-ahead functionalities in
built-in regex library, this commit is to replace string.Split function
by custom implementation to handle such scenario.

Relates: #18478
Fixes: #18973
Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/command/map_string.go
@@ -15,7 +15,12 @@ import (
 	"github.com/spf13/viper"
 )
 
-var keyValueRegex = regexp.MustCompile(`([\w-:./@]+=[\w-:./@]*,)*([\w-:./@]+=[\w-:./@]*)$`)
+const (
+	comma = ','
+	equal = '='
+)
+
+var keyValueRegex = regexp.MustCompile(`([\w-:./@]+=[\w-:,./@]*,)*([\w-:./@]+=[\w-,:./@]*[\w-:./@]+)$`)
 
 // GetStringMapString contains one enhancement to support k1=v2,k2=v2 compared to original
 // implementation of GetStringMapString function
@@ -57,9 +62,9 @@ func GetStringMapStringE(vp *viper.Viper, key string) (map[string]string, error)
 			}
 
 			var v = map[string]string{}
-			kvs := strings.Split(s, ",")
+			kvs := splitKeyValue(s, comma, equal)
 			for _, kv := range kvs {
-				temp := strings.Split(kv, "=")
+				temp := strings.Split(kv, string(equal))
 				if len(temp) != 2 {
 					return map[string]string{}, fmt.Errorf("'%s' is not formatted as key=value,key1=value1", s)
 				}
@@ -77,4 +82,41 @@ func isValidKeyValuePair(str string) bool {
 		return true
 	}
 	return len(keyValueRegex.ReplaceAllString(str, "")) == 0
+}
+
+// splitKeyValue is similar to strings.Split, but looks ahead to make sure
+// that sep character is allowed in value component of key-value pair.
+//
+// Example: with the input "c6a.2xlarge=4,15,15,m4.xlarge=2,4,8",
+// - strings.Split function will return []string{"c6a.2xlarge=4", "15", "15", "m4.xlarge=2", "4", "8"}.
+// - splitKeyValue function will return []string{"c6a.2xlarge=4,15,15", "m4.xlarge=2,4,8"} instead.
+func splitKeyValue(str string, sep rune, keyValueSep rune) []string {
+	var sepIndexes []int
+	// find all indexes of separator character
+	for i := 0; i < len(str); i++ {
+		if int32(str[i]) == sep {
+			sepIndexes = append(sepIndexes, i)
+		}
+	}
+
+	if len(sepIndexes) == 0 {
+		return []string{str}
+	}
+
+	if len(sepIndexes) == 1 {
+		index := sepIndexes[0]
+		return []string{str[:index], str[index+1:]}
+	}
+
+	var res []string
+	var start = 0
+	for i := 0; i < len(sepIndexes)-1; i++ {
+		if strings.Contains(str[sepIndexes[i]:sepIndexes[i+1]], string(keyValueSep)) {
+			res = append(res, str[start:sepIndexes[i]])
+			start = sepIndexes[i] + 1
+		}
+	}
+	// append the remaining for last sep index
+	res = append(res, str[start:])
+	return res
 }

pkg/command/map_string_test.go
@@ -98,6 +98,29 @@ func TestGetStringMapString(t *testing.T) {
 			wantErr: assert.NoError,
 		},
 		{
+			name: "valid kv format with comma in value",
+			args: args{
+				key:   "API_RATE_LIMIT",
+				value: "endpoint-create=rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true,endpoint-delete=rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true",
+			},
+			want: map[string]string{
+				"endpoint-create": "rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true",
+				"endpoint-delete": "rate-limit:10/s,rate-burst:10,parallel-requests:10,auto-adjust:true",
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "another valid kv format with comma in value",
+			args: args{
+				key:   "AWS_INSTANCE_LIMIT_MAPPING",
+				value: "c6a.2xlarge=4,15,15",
+			},
+			want: map[string]string{
+				"c6a.2xlarge": "4,15,15",
+			},
+			wantErr: assert.NoError,
+		},
+		{
 			name: "valid kv format with forward slash",
 			args: args{
 				key:   "FOO_BAR",

commit 6eeb92a90adccd61da8d37cfa62df3f3e5a93e05

cli: Update regex for key value validation
The previous pattern did not allow spaces in values.

Fixes: #19793
Fixes: c533f0eaeea (cmd: Allow more complicated patterns in map string type.)
Signed-off-by: John Gardiner Myers <jgmyers@proofpoint.com>

pkg/command/map_string.go
@@ -20,7 +20,7 @@ const (
 	equal = '='
 )
 
-var keyValueRegex = regexp.MustCompile(`([\w-:./@]+=[\w-:,./@]*,)*([\w-:./@]+=[\w-,:./@]*[\w-:./@]+)$`)
+var keyValueRegex = regexp.MustCompile(`([\w-:./@]+=([\w-:,./@][\w-:,./@ ]*[\w-:,./@])?[\w-:,./@]*,)*([\w-:./@]+=([\w-:,./@][\w-:,./@ ]*)?[\w-:./@]+)$`)
 
 // GetStringMapString contains one enhancement to support k1=v2,k2=v2 compared to original
 // implementation of GetStringMapString function

pkg/command/map_string_test.go
@@ -143,6 +143,17 @@ func TestGetStringMapString(t *testing.T) {
 			wantErr: assert.NoError,
 		},
 		{
+			name: "valid kv format with space",
+			args: args{
+				key:   "FOO_BAR",
+				value: "cluster=my cluster",
+			},
+			want: map[string]string{
+				"cluster": "my cluster",
+			},
+			wantErr: assert.NoError,
+		},
+		{
 			name: "valid kv format from issue #20666",
 			args: args{
 				key:   "FOO_BAR",
@@ -276,6 +287,13 @@ func Test_isValidKeyValuePair(t *testing.T) {
 			want: true,
 		},
 		{
+			name: "valid format with multiple pairs with commas",
+			args: args{
+				str: "k1=v,1,k2=v2,,k3=,v3,k4=v,4,k4=v4,k4=v,4",
+			},
+			want: true,
+		},
+		{
 			name: "empty value",
 			args: args{
 				str: "",
@@ -299,12 +317,12 @@ func Test_isValidKeyValuePair(t *testing.T) {
 		{
 			name: "no pair at all",
 			args: args{
-				str: "here is the test",
+				str: "here-is-the-test",
 			},
 			want: false,
 		},
 		{
-			name: "ending with command",
+			name: "ending with comma",
 			args: args{
 				str: "k1=v1,k2=v2,",
 			},
@@ -332,9 +350,30 @@ func Test_isValidKeyValuePair(t *testing.T) {
 			want: false,
 		},
 		{
-			name: "space in value",
+			name: "value starts with space",
+			args: args{
+				str: "k1= v1,k2=v2",
+			},
+			want: false,
+		},
+		{
+			name: "last value starts with space",
 			args: args{
 				str: "k1=v1,k2= v2",
+			},
+			want: false,
+		},
+		{
+			name: "value ends with space",
+			args: args{
+				str: "k1=v1 ,k2=v2",
+			},
+			want: false,
+		},
+		{
+			name: "last value ends with space",
+			args: args{
+				str: "k1=v1,k2=v2 ",
 			},
 			want: false,
 		},