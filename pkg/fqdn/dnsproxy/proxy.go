g0odi3z2j/cilium

commit daf920aaac936ef56f046560d75d2bd6e7dd0f9c

fqdn: dnsproxy: forward the original security identity (tunnel case)
Consider a situation in which a pod, which is also a subject to an egress
policy, performs a DNS request. This request is redirected to the DNS proxy,
which performs address resolutoin. The DNS proxy runs in the host network
namespace, and thus the DNS request has the host identity. In case when the DNS
server is a subject to an ingress policy, this request may be denied, because
the DNS server will see a request from a 'remote-node' identity.

Here is an example configuration wich will not let pods labeled woo=hoo to
access DNS servers running on a different host, while this should be allowed:

    apiVersion: "cilium.io/v2"
    kind: CiliumClusterwideNetworkPolicy
    metadata:
      name: "egress-dns"
    spec:
      endpointSelector:
        matchLabels:
          woo: hoo
      egress:
      - toEndpoints:
        - matchLabels:
            io.kubernetes.pod.namespace: kube-system
            k8s-app: kube-dns
        toPorts:
        - ports:
          - port: "53"
            protocol: UDP
          rules:
            dns:
              - matchPattern: "*"
    ---
    apiVersion: "cilium.io/v2"
    kind: CiliumNetworkPolicy
    metadata:
      name: "ingress-dns"
      namespace: kube-system
    spec:
      endpointSelector:
        matchLabels:
          k8s-app: kube-dns
      ingress:
      - fromEndpoints:
        - {}
        toPorts:
          - ports:
            - port: "53"
              protocol: UDP

Patch the DNS proxy to pass the original security identity with the DNS
request using the SO_MARK socket option.

Signed-off-by: Anton Protopopov <aspsk@isovalent.com>

pkg/fqdn/dnsproxy/proxy.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"math"
 	"net"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -18,6 +19,7 @@ import (
 	"github.com/miekg/dns"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/semaphore"
+	"golang.org/x/sys/unix"
 
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/endpoint"
@@ -560,6 +562,33 @@ func (p *DNSProxy) CheckAllowed(endpointID uint64, destPort uint16, destID ident
 	return false, nil
 }
 
+func configureConnection(conn *net.Conn, secId identity.NumericIdentity) error {
+	var file *os.File
+	var err error
+
+	switch l4conn := (*conn).(type) {
+	case *net.UDPConn:
+		if file, err = l4conn.File(); err != nil {
+			return fmt.Errorf("can't get file from %v: %w", l4conn, err)
+		}
+	case *net.TCPConn:
+		if file, err = l4conn.File(); err != nil {
+			return fmt.Errorf("can't get file from %v: %w", l4conn, err)
+		}
+	default:
+		return fmt.Errorf("unsupported type %T", l4conn)
+	}
+
+	defer file.Close()
+
+	mark := int(uint32(secId)<<16 | uint32(linux_defaults.MagicMarkIdentity))
+	err = unix.SetsockoptInt(int(file.Fd()), unix.SOL_SOCKET, unix.SO_MARK, mark)
+	if err != nil {
+		return fmt.Errorf("error setting SO_MARK: %w", err)
+	}
+	return nil
+}
+
 // ServeDNS handles individual DNS requests forwarded to the proxy, and meets
 // the dns.Handler interface.
 // It will:
@@ -694,8 +723,28 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 	stat.ProcessingTime.End(true)
 	stat.UpstreamTime.Start()
 
+	conn, err := client.Dial(targetServerAddr)
+	if err != nil {
+		err := fmt.Errorf("failed to dial connection to %v: %w", targetServerAddr, err)
+		stat.Err = err
+		scopedLog.WithError(err).Error("Failed to dial connection to the upstream DNS server, cannot service DNS request")
+		p.NotifyOnDNSMsg(time.Now(), ep, epIPPort, targetServerID, targetServerAddr, request, protocol, false, &stat)
+		p.sendRefused(scopedLog, w, request)
+		return
+	}
+	defer conn.Close()
+
+	if err = configureConnection(&conn.Conn, ep.GetIdentity()); err != nil {
+		err := fmt.Errorf("failed to set socket options: %w", err)
+		stat.Err = err
+		scopedLog.WithError(err).Error("Failed to configure connection to the upstream DNS server, cannot service DNS request")
+		p.NotifyOnDNSMsg(time.Now(), ep, epIPPort, targetServerID, targetServerAddr, request, protocol, false, &stat)
+		p.sendRefused(scopedLog, w, request)
+		return
+	}
+
 	request.Id = dns.Id() // force a random new ID for this request
-	response, _, err := client.Exchange(request, targetServerAddr)
+	response, _, err := client.ExchangeWithConn(request, conn)
 	stat.UpstreamTime.End(err == nil)
 	if err != nil {
 		stat.Err = err

pkg/fqdn/dnsproxy/udp.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"net"
 	"strconv"
@@ -177,7 +178,7 @@ func (f *sessionUDPFactory) ReadRequest(conn *net.UDPConn) ([]byte, dns.SessionU
 }
 
 func (f *sessionUDPFactory) ReadRequestConn(conn net.PacketConn) ([]byte, net.Addr, error) {
-	return []byte{}, nil, fmt.Errorf("ReadRequestConn is not supported")
+	return []byte{}, nil, errors.New("ReadRequestConn is not supported")
 }
 
 // Discard returns 's' to the factory pool

commit 6e74c46ce5e939fcaea662b5899e13cc8eb7f28f

fqdn: dnsproxy: fix forwarding of the security identity for cluster mesh
The commit daf920aaac93 wrongly forwarded only lower 16 bits of the original
identity. This might corrupt identities when cluster-id is not zero (as the
cluster-id is encoded in bits 16..23 of the identity) and leads to policy drops
due to unknown identity, e.g.

    xx drop (Policy denied) flow 0xd1a7add4 to endpoint 3966, file bpf_lxc.c line 2032, , identity 47657->157516: 10.2.3.223:55853 -> 10.2.3.206:53 udp

(Here the security identity 47657 doesn't exist, as it should actually be equal
to 0x10000|47657 = 113193.)

Fix this by also storing bits 16..23 of the identity in the skb mark according
to the datapath ABI, i.e., skb mark should be equal to (id << 16) | (id >> 16).

Fixes: daf920aaac93 ("fqdn: dnsproxy: forward the original security identity")

Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>
Signed-off-by: Anton Protopopov <aspsk@isovalent.com>

pkg/fqdn/dnsproxy/proxy.go
@@ -751,7 +751,8 @@ func configureConnection(conn *net.Conn, secId identity.NumericIdentity) error {
 
 	defer file.Close()
 
-	mark := int(uint32(secId)<<16 | uint32(linux_defaults.MagicMarkIdentity))
+	mark := linux_defaults.MagicMarkIdentity
+	mark |= int(uint32(secId&0xFFFF)<<16 | uint32((secId&0xFF0000)>>16))
 	err = unix.SetsockoptInt(int(file.Fd()), unix.SOL_SOCKET, unix.SO_MARK, mark)
 	if err != nil {
 		return fmt.Errorf("error setting SO_MARK: %w", err)
@@ -826,7 +827,10 @@ func (p *DNSProxy) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
 		return
 	}
 
-	scopedLog = scopedLog.WithField(logfields.EndpointID, ep.StringID())
+	scopedLog = scopedLog.WithFields(logrus.Fields{
+		logfields.EndpointID: ep.StringID(),
+		logfields.Identity:   ep.GetIdentity(),
+	})
 
 	targetServerIP, targetServerPort, targetServerAddr, err := p.lookupTargetDNSServer(w)
 	if err != nil {