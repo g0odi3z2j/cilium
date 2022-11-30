g0odi3z2j/cilium

commit 5b30c1776fb9d6a1b86c5351827d88bcb2ca2b19

pkg/k8s: execute heartbeat only if cluster is quiet
If Cilium has interactions with k8s, Cilium does not need to
perform heatbeats to the kube-apiserver. Unless those interactions
result in an error for which we should perform a heartbeat to
kube-apiserver to check if the connection is still alive. In case the
response from kube-apiserver is an error Cilium should then close all
connections with the kube-apiserver.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/watcher.go
@@ -20,6 +20,7 @@ import (
 	"net"
 	"net/url"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -240,6 +241,16 @@ func (*k8sMetrics) Observe(verb string, u url.URL, latency time.Duration) {
 
 func (*k8sMetrics) Increment(code string, method string, host string) {
 	metrics.KubernetesAPICalls.WithLabelValues(host, method, code).Inc()
+	// The 'code' is set to '<error>' in case an error is returned from k8s
+	// more info:
+	// https://github.com/kubernetes/client-go/blob/v0.18.0-rc.1/rest/request.go#L700-L703
+	if code != "<error>" {
+		// Consider success if status code is 2xx or 4xx
+		if strings.HasPrefix(code, "2") ||
+			strings.HasPrefix(code, "4") {
+			k8smetrics.LastSuccessInteraction.Reset()
+		}
+	}
 	k8smetrics.LastInteraction.Reset()
 }
 

pkg/k8s/client.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
+	k8smetrics "github.com/cilium/cilium/pkg/k8s/metrics"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/version"
@@ -122,21 +123,31 @@ func setDialer(config *rest.Config) func() {
 }
 
 func runHeartbeat(heartBeat func(context.Context) error, timeout time.Duration, closeAllConns ...func()) {
+	expireDate := time.Now().Add(-timeout)
+	// Don't even perform a health check if we have received a successful
+	// k8s event in the last 'timeout' duration
+	if k8smetrics.LastSuccessInteraction.Time().After(expireDate) {
+		return
+	}
+
 	done := make(chan error)
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	go func() {
+		// If we have reached up to this point to perform a heartbeat to
+		// kube-apiserver then we should close the connections if we receive
+		// any error at all except if we receive a http.StatusTooManyRequests
+		// which means the server is overloaded and only for this reason we
+		// will not close all connections.
 		err := heartBeat(ctx)
 		switch t := err.(type) {
 		case *errors.StatusError:
-			switch t.ErrStatus.Code {
-			case http.StatusGatewayTimeout,
-				http.StatusRequestTimeout,
-				http.StatusBadGateway:
-				done <- t
+			if t.ErrStatus.Code != http.StatusTooManyRequests {
+				done <- err
 			}
+		default:
+			done <- err
 		}
-
 		close(done)
 	}()
 

pkg/k8s/client_test.go
@@ -17,19 +17,23 @@
 package k8s
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net"
+	"net/http"
 	"time"
 
 	"github.com/cilium/cilium/pkg/annotation"
 	"github.com/cilium/cilium/pkg/checker"
+	k8smetrics "github.com/cilium/cilium/pkg/k8s/metrics"
 	"github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/node"
 	"github.com/cilium/cilium/pkg/source"
 
 	. "gopkg.in/check.v1"
 	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
@@ -173,5 +177,127 @@ func (s *K8sSuite) TestUseNodeCIDR(c *C) {
 	case <-time.Tick(10 * time.Second):
 		c.Errorf("d.fakeK8sClient.CoreV1().Nodes().Update() was not called")
 		c.FailNow()
+	}
+}
+
+func (s *K8sSuite) Test_runHeartbeat(c *C) {
+	// k8s api server never replied back in the expected time. We should close all connections
+	k8smetrics.LastSuccessInteraction.Reset()
+	time.Sleep(2 * time.Millisecond)
+
+	called := make(chan struct{})
+	runHeartbeat(
+		func(ctx context.Context) error {
+			<-ctx.Done()
+			return nil
+		},
+		time.Millisecond,
+		func() {
+			close(called)
+		},
+	)
+
+	select {
+	case <-time.After(5 * time.Millisecond):
+		c.Error("Heartbeat should have closed all connections")
+	case <-called:
+	}
+
+	// There are some connectivity issues, cilium is trying to reach kube-apiserver
+	// but it's only receiving errors. We should close all connections!
+
+	// Wait the double amount of time than the timeout to make sure
+	// LastSuccessInteraction is not taken into account and we will see that we
+	// will close all connections.
+	time.Sleep(200 * time.Millisecond)
+
+	called = make(chan struct{})
+	runHeartbeat(
+		func(ctx context.Context) error {
+			<-ctx.Done()
+			return nil
+		},
+		100*time.Millisecond,
+		func() {
+			close(called)
+		},
+	)
+
+	select {
+	case <-time.After(200 * time.Millisecond):
+		c.Error("Heartbeat should have closed all connections")
+	case <-called:
+	}
+
+	// Cilium is successfully talking with kube-apiserver, we should not do
+	// anything.
+	k8smetrics.LastSuccessInteraction.Reset()
+
+	called = make(chan struct{})
+	runHeartbeat(
+		func(ctx context.Context) error {
+			close(called)
+			return nil
+		},
+		100*time.Millisecond,
+		func() {
+			c.Error("This should not have been called!")
+		},
+	)
+
+	select {
+	case <-time.After(200 * time.Millisecond):
+	case <-called:
+		c.Error("Heartbeat should have closed all connections")
+	}
+
+	// Cilium had the last interaction with kube-apiserver a long time ago.
+	// We should perform a heartbeat
+	k8smetrics.LastInteraction.Reset()
+	time.Sleep(500 * time.Millisecond)
+
+	called = make(chan struct{})
+	runHeartbeat(
+		func(ctx context.Context) error {
+			close(called)
+			return nil
+		},
+		100*time.Millisecond,
+		func() {
+			c.Error("This should not have been called!")
+		},
+	)
+
+	select {
+	case <-time.After(200 * time.Millisecond):
+		c.Error("Heartbeat should have closed all connections")
+	case <-called:
+	}
+
+	// Cilium had the last interaction with kube-apiserver a long time ago.
+	// We should perform a heartbeat but the heart beat will return
+	// an error so we should close all connections
+	k8smetrics.LastInteraction.Reset()
+	time.Sleep(500 * time.Millisecond)
+
+	called = make(chan struct{})
+	runHeartbeat(
+		func(ctx context.Context) error {
+			return &errors.StatusError{
+				ErrStatus: metav1.Status{
+					Code: http.StatusRequestTimeout,
+				},
+			}
+		},
+		100*time.Millisecond,
+		func() {
+			close(called)
+		},
+	)
+
+	select {
+	case <-time.After(200 * time.Millisecond):
+		c.Error("Heartbeat should have closed all connections")
+	case <-called:
 	}
 }

pkg/k8s/metrics/metrics.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Authors of Cilium
+// Copyright 2019-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,6 +24,9 @@ var (
 	// LastInteraction is the time at which the last apiserver interaction
 	// occurred
 	LastInteraction eventTimestamper
+	// LastSuccessInteraction is the time at which we have received a successful
+	// k8s apiserver reply (i.e. a response code 2xx or 4xx).
+	LastSuccessInteraction eventTimestamper
 )
 
 type eventTimestamper struct {

commit c9f4f64672ce877fc7a4efd0b6247bc377d168c0

k8s: don't consider 4xx a successful interaction
While a 404 Not Found or a 409 Conflict can be considered successful
interactions with the k8s API, a blanket accept for all 4xx codes is
problematic. Since LastSuccessInteraction is exclusively used as an
optimisation, we should err on the cautious side: accept the potential
increase in heartbeats to avoid missing being unable to effecticely
communicate with the k8s API.

As an example of how this can go wrong, in #20915 we have an issue
around receiving 401 Unauthorized from the EKS control plane. At
sufficient scale, we never see a need to run the heartbeat. Running the
heartbeat, however, would close and reopen the connections on receiving
a 401, and thus restore connectivity to the k8s API.

We currently only use the LastSuccessInteraction to as an optimisation
to not perform unnecessary k8s API heartbeats, this "metric" (possibly a
misnomer) is not used or exposed and changing its semantics is
acceptable.

Fixes: 5b30c1776fb9d6a1b86c5351827d88bcb2ca2b19

Signed-off-by: David Bimmler <david.bimmler@isovalent.com>
Co-authored-by: Sebastian Wicki <gandro@gmx.net>

pkg/k8s/watchers/watcher.go
@@ -337,9 +337,8 @@ func (*k8sMetrics) Increment(_ context.Context, code string, method string, host
 	// more info:
 	// https://github.com/kubernetes/client-go/blob/v0.18.0-rc.1/rest/request.go#L700-L703
 	if code != "<error>" {
-		// Consider success if status code is 2xx or 4xx
-		if strings.HasPrefix(code, "2") ||
-			strings.HasPrefix(code, "4") {
+		// Consider success only if status code is 2xx
+		if strings.HasPrefix(code, "2") {
 			k8smetrics.LastSuccessInteraction.Reset()
 		}
 	}