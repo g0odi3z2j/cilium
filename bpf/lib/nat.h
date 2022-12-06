g0odi3z2j/cilium

commit 0d898a1b9975167bea5c5873db95261a2a846ba9

bpf/nat: remove unnecessary redondante nexthdr variable
It's redondante variable that does not look to bring any value.

Signed-off-by: Sahid Orentino Ferdjaoui <sahid.ferdjaoui@industrialdiscipline.com>

bpf/lib/nat.h
@@ -1137,7 +1137,6 @@ snat_v6_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 		__be16 sport;
 		__be16 dport;
 	} l4hdr;
-	__u8 nexthdr;
 	__u32 off;
 	bool icmp_echoreply = false;
 
@@ -1146,8 +1145,7 @@ snat_v6_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
 		return DROP_INVALID;
 
-	nexthdr = ip6->nexthdr;
-	hdrlen = ipv6_hdrlen(ctx, &nexthdr);
+	hdrlen = ipv6_hdrlen(ctx, &ip6->nexthdr);
 	if (hdrlen < 0)
 		return hdrlen;
 
@@ -1212,7 +1210,6 @@ snat_v6_rev_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 		__be16 sport;
 		__be16 dport;
 	} l4hdr;
-	__u8 nexthdr;
 	__u32 off;
 
 	build_bug_on(sizeof(struct ipv6_nat_entry) > 64);
@@ -1220,8 +1217,7 @@ snat_v6_rev_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
 		return DROP_INVALID;
 
-	nexthdr = ip6->nexthdr;
-	hdrlen = ipv6_hdrlen(ctx, &nexthdr);
+	hdrlen = ipv6_hdrlen(ctx, &ip6->nexthdr);
 	if (hdrlen < 0)
 		return hdrlen;
 

commit 3001cd8655773416ef2a3d26605302e14542558f

bpf: nat: fix usage of ipv6_hdrlen() with unhandled Extension headers
When the packet contains an unhandled Extension header, ipv6_hdrlen()
returns the header type in the *nexthdr parameter. If we don't use a
separate nexthdr variable, this overwrites ip6->nexthdr.

Follow the same pattern as in snat_v6_create_dsr(), and just use
tuple.nexthdr instead.

Fixes: 0d898a1b9975 ("bpf/nat: remove unnecessary redondante nexthdr variable")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nat.h
@@ -1315,7 +1315,6 @@ static __always_inline void snat_v6_init_tuple(const struct ipv6hdr *ip6,
 					       enum nat_dir dir,
 					       struct ipv6_ct_tuple *tuple)
 {
-	tuple->nexthdr = ip6->nexthdr;
 	ipv6_addr_copy(&tuple->daddr, (union v6addr *)&ip6->daddr);
 	ipv6_addr_copy(&tuple->saddr, (union v6addr *)&ip6->saddr);
 	tuple->flags = dir;
@@ -1366,7 +1365,8 @@ snat_v6_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
 		return DROP_INVALID;
 
-	hdrlen = ipv6_hdrlen(ctx, &ip6->nexthdr);
+	tuple.nexthdr = ip6->nexthdr;
+	hdrlen = ipv6_hdrlen(ctx, &tuple.nexthdr);
 	if (hdrlen < 0)
 		return hdrlen;
 
@@ -1438,7 +1438,8 @@ snat_v6_rev_nat(struct __ctx_buff *ctx, const struct ipv6_nat_target *target)
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
 		return DROP_INVALID;
 
-	hdrlen = ipv6_hdrlen(ctx, &ip6->nexthdr);
+	tuple.nexthdr = ip6->nexthdr;
+	hdrlen = ipv6_hdrlen(ctx, &tuple.nexthdr);
 	if (hdrlen < 0)
 		return hdrlen;
 