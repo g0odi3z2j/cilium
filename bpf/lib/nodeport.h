g0odi3z2j/cilium

commit defc73c98202b64f113b933982dd9d9c42b8f575

bpf: pipe forwarding reason into traces for TO_OVERLAY
Add an additional argument to __encap_with_nodeid() so we can pass the
reason (conntrack status) for forwarding the packet when calling
send_trace_notify(). This allows us to get the "is_reply" field in the
flows visible in Hubble.

We also need to let Hubble know that the TraceToOverlay observability
point now has reply information available.

Note that the reason remains at zero at most locations where
__encap_with_nodeid() is called: only the trivial cases where we have
the info were addressed in this commit, others will be updated as a
follow-up.

In the near future, we want to clean up the confusion between an
intended 0 value for the reason (TRACE_REASON_POLICY) and an unspecified
reason. This is not addressed in the current work. However, in order to
avoid complicating things further, we introduce a new macro
TRACE_REASON_UNSPECIFIED, currently set to 0, to make it easier to
rework the reason values in the future.

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

bpf/lib/nodeport.h
@@ -607,7 +607,9 @@ int tail_nodeport_nat_ipv6(struct __ctx_buff *ctx)
 		info = ipcache_lookup6(&IPCACHE_MAP, dst, V6_CACHE_KEY_LEN);
 		if (info != NULL && info->tunnel_endpoint != 0) {
 			ret = __encap_with_nodeid(ctx, info->tunnel_endpoint,
-						  WORLD_ID, TRACE_PAYLOAD_LEN);
+						  WORLD_ID,
+						  TRACE_REASON_UNKNOWN,
+						  TRACE_PAYLOAD_LEN);
 			if (ret)
 				goto drop_err;
 
@@ -914,7 +916,9 @@ static __always_inline int rev_nodeport_lb6(struct __ctx_buff *ctx, int *ifindex
 			info = ipcache_lookup6(&IPCACHE_MAP, dst, V6_CACHE_KEY_LEN);
 			if (info != NULL && info->tunnel_endpoint != 0) {
 				ret = __encap_with_nodeid(ctx, info->tunnel_endpoint,
-							  SECLABEL, TRACE_PAYLOAD_LEN);
+							  SECLABEL,
+							  TRACE_REASON_CT_REPLY,
+							  TRACE_PAYLOAD_LEN);
 				if (ret)
 					return ret;
 
@@ -1628,7 +1632,9 @@ int tail_nodeport_nat_ipv4(struct __ctx_buff *ctx)
 			 * outside.
 			 */
 			ret = __encap_with_nodeid(ctx, info->tunnel_endpoint,
-						  WORLD_ID, TRACE_PAYLOAD_LEN);
+						  WORLD_ID,
+						  TRACE_REASON_UNKNOWN,
+						  TRACE_PAYLOAD_LEN);
 			if (ret)
 				goto drop_err;
 
@@ -1902,7 +1908,8 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, int *ifindex
 	int ret, fib_ret, ret2, l3_off = ETH_HLEN, l4_off;
 	struct ct_state ct_state = {};
 	struct bpf_fib_lookup fib_params = {};
-	__u32 monitor = 0;
+	enum trace_reason __maybe_unused reason = TRACE_REASON_UNKNOWN;
+	__u32 monitor = TRACE_PAYLOAD_LEN;
 	bool l2_hdr_required = true;
 	__u32 tunnel_endpoint __maybe_unused = 0;
 
@@ -1945,6 +1952,7 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, int *ifindex
 			 &monitor);
 
 	if (ret == CT_REPLY && ct_state.node_port == 1 && ct_state.rev_nat_index != 0) {
+		reason = TRACE_REASON_CT_REPLY;
 		ret2 = lb4_rev_nat(ctx, l3_off, l4_off, &csum_off,
 				   &ct_state, &tuple,
 				   REV_NAT_F_TUPLE_SADDR, ipv4_has_l4_header(ip4));
@@ -2036,7 +2044,8 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, int *ifindex
 
 #if defined(ENABLE_EGRESS_GATEWAY) || defined(TUNNEL_MODE)
 encap_redirect:
-	ret = __encap_with_nodeid(ctx, tunnel_endpoint, SECLABEL, TRACE_PAYLOAD_LEN);
+	ret = __encap_with_nodeid(ctx, tunnel_endpoint, SECLABEL,
+				  reason, monitor);
 	if (ret)
 		return ret;
 

bpf/lib/encap.h
@@ -114,7 +114,7 @@ encap_remap_v6_host_address(struct __ctx_buff *ctx __maybe_unused,
 
 static __always_inline int
 __encap_with_nodeid(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
-		    __u32 seclabel, __u32 monitor)
+		    __u32 seclabel, enum trace_reason ct_reason, __u32 monitor)
 {
 	struct bpf_tunnel_key key = {};
 	__u32 node_id;
@@ -139,7 +139,7 @@ __encap_with_nodeid(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 		return DROP_WRITE_ERROR;
 
 	send_trace_notify(ctx, TRACE_TO_OVERLAY, seclabel, 0, 0, ENCAP_IFINDEX,
-			  TRACE_REASON_UNKNOWN, monitor);
+			  ct_reason, monitor);
 	return 0;
 }
 
@@ -147,7 +147,8 @@ static __always_inline int
 __encap_and_redirect_with_nodeid(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 				 __u32 seclabel, __u32 monitor)
 {
-	int ret = __encap_with_nodeid(ctx, tunnel_endpoint, seclabel, monitor);
+	int ret = __encap_with_nodeid(ctx, tunnel_endpoint, seclabel,
+				      TRACE_REASON_UNKNOWN, monitor);
 	if (ret != 0)
 		return ret;
 
@@ -202,7 +203,8 @@ encap_and_redirect_lxc(struct __ctx_buff *ctx, __u32 tunnel_endpoint,
 		 * the tunnel, to apply the correct reverse DNAT.
 		 * See #14674 for details.
 		 */
-		return __encap_with_nodeid(ctx, tunnel_endpoint, seclabel, monitor);
+		return __encap_with_nodeid(ctx, tunnel_endpoint, seclabel,
+					   TRACE_REASON_UNKNOWN, monitor);
 #else
 		return __encap_and_redirect_with_nodeid(ctx, tunnel_endpoint, seclabel, monitor);
 #endif /* !ENABLE_NODEPORT && (ENABLE_IPSEC || ENABLE_HOST_FIREWALL) */

commit 7a1106664c58c982e6153870717b3d7ad190db55

bpf: nodeport: wire up trace aggregation for rev_nodeport_lb6()
Pass the `monitor` feedback from the CT lookup to __encap_with_nodeid(). A
previous commit already added this for rev_nodeport_lb4(), so aim for
commonality here.

Fixes: defc73c98202 ("bpf: pipe forwarding reason into traces for TO_OVERLAY")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -1061,8 +1061,7 @@ static __always_inline int rev_nodeport_lb6(struct __ctx_buff *ctx, __u32 *ifind
 							   SECLABEL, info->sec_label,
 							   NOT_VTEP_DST,
 							   TRACE_REASON_CT_REPLY,
-							   TRACE_PAYLOAD_LEN,
-							   ifindex);
+							   monitor, ifindex);
 			}
 		}
 #endif