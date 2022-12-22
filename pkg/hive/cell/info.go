g0odi3z2j/cilium

commit 6ea13663277bdf4e5ae15776315a7261c857466f

hive: Detect terminal width for use in InfoLeaf line wrapping
Detect the width with term.GetSize() instead of using a hard-coded width
for wrapping the list of input and output objects.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hive/cell/info.go
@@ -7,29 +7,43 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 	"strings"
 
 	"github.com/davecgh/go-spew/spew"
+	"golang.org/x/term"
 )
 
 const (
 	// indentBy is the number of spaces nested elements should be indented by
 	indentBy = 4
-
-	// maxLineLength is the maximum line length after which the InfoLeaf line
-	// wraps.
-	maxLineLength = 120
 )
+
+type InfoPrinter struct {
+	io.Writer
+	width int
+}
+
+func NewInfoPrinter() *InfoPrinter {
+	width, _, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		width = 120
+	}
+	return &InfoPrinter{
+		Writer: os.Stdout,
+		width:  width,
+	}
+}
 
 // Info provides a simple way of printing cells hierarchically in
 // textual form.
 type Info interface {
-	Print(indent int, w io.Writer)
+	Print(indent int, w *InfoPrinter)
 }
 
 type InfoLeaf string
 
-func (l InfoLeaf) Print(indent int, w io.Writer) {
+func (l InfoLeaf) Print(indent int, w *InfoPrinter) {
 	buf := bufio.NewWriter(w)
 	currentLineLength := indent
 	indentString := strings.Repeat(" ", indent)
@@ -40,7 +54,7 @@ func (l InfoLeaf) Print(indent int, w io.Writer) {
 		buf.WriteByte(' ')
 		currentLineLength += len(f)
 
-		if currentLineLength >= maxLineLength {
+		if currentLineLength >= w.width {
 			buf.WriteByte('\n')
 			if !wrapped {
 				// Increase the indent for the wrapped lines so it's clear we
@@ -70,11 +84,6 @@ func NewInfoNode(header string) *InfoNode {
 	return &InfoNode{header: header}
 }
 
-func (n *InfoNode) Condensed() *InfoNode {
-	n.condensed = true
-	return n
-}
-
 func (n *InfoNode) Add(child Info) {
 	n.children = append(n.children, child)
 }
@@ -83,7 +92,7 @@ func (n *InfoNode) AddLeaf(format string, args ...any) {
 	n.Add(InfoLeaf(fmt.Sprintf(format, args...)))
 }
 
-func (n *InfoNode) Print(indent int, w io.Writer) {
+func (n *InfoNode) Print(indent int, w *InfoPrinter) {
 	if n.header != "" {
 		fmt.Fprintf(w, "%s%s:\n", strings.Repeat(" ", indent), n.header)
 		indent += indentBy
@@ -101,7 +110,7 @@ type InfoStruct struct {
 	value any
 }
 
-func (n *InfoStruct) Print(indent int, w io.Writer) {
+func (n *InfoStruct) Print(indent int, w *InfoPrinter) {
 	scs := spew.ConfigState{Indent: strings.Repeat(" ", indentBy), SortKeys: true}
 	indentString := strings.Repeat(" ", indent)
 	for i, line := range strings.Split(scs.Sdump(n.value), "\n") {

go.mod
@@ -94,6 +94,7 @@ require (
 	golang.org/x/net v0.1.0
 	golang.org/x/sync v0.1.0
 	golang.org/x/sys v0.2.0
+	golang.org/x/term v0.1.0
 	golang.org/x/time v0.2.0
 	golang.org/x/tools v0.2.0
 	golang.zx2c4.com/wireguard v0.0.0-20211017052713-f87e87af0d9a
@@ -229,7 +230,6 @@ require (
 	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/mod v0.6.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
-	golang.org/x/term v0.1.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect

pkg/hive/cell/provide.go
@@ -40,7 +40,7 @@ func (p *provider) Info(container) Info {
 		}
 
 		ctorNode := NewInfoNode(fmt.Sprintf("🚧%s %s", privateSymbol, internal.FuncNameAndLocation(ctor)))
-		ctorNode = ctorNode.Condensed()
+		ctorNode.condensed = true
 
 		var ins, outs []string
 		for _, input := range info.Inputs {

pkg/hive/hive.go
@@ -288,8 +288,9 @@ func (h *Hive) PrintObjects() {
 	}
 
 	fmt.Printf("Cells:\n\n")
+	ip := cell.NewInfoPrinter()
 	for _, c := range h.cells {
-		c.Info(h.container).Print(2, os.Stdout)
+		c.Info(h.container).Print(2, ip)
 		fmt.Println()
 	}
 	h.lifecycle.PrintHooks()

commit f30d070f927950bb7b45d5253ecdfcda735874f8

hive: Fix line wrapping in PrintObjects
The initial implementation was broken as it checked if the line length exceeds width
after printing. Tested manually with long list of objects.

Fixes: 6ea1366327 ("hive: Detect terminal width for use in InfoLeaf line wrapping")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hive/cell/info.go
@@ -45,16 +45,13 @@ type InfoLeaf string
 
 func (l InfoLeaf) Print(indent int, w *InfoPrinter) {
 	buf := bufio.NewWriter(w)
-	currentLineLength := indent
 	indentString := strings.Repeat(" ", indent)
 	buf.WriteString(indentString)
+	currentLineLength := len(indentString)
 	wrapped := false
 	for _, f := range strings.Fields(string(l)) {
-		buf.WriteString(f)
-		buf.WriteByte(' ')
-		currentLineLength += len(f)
-
-		if currentLineLength >= w.width {
+		newLineLength := currentLineLength + len(f) + 1
+		if newLineLength >= w.width {
 			buf.WriteByte('\n')
 			if !wrapped {
 				// Increase the indent for the wrapped lines so it's clear we
@@ -65,7 +62,11 @@ func (l InfoLeaf) Print(indent int, w *InfoPrinter) {
 			}
 			buf.WriteString(indentString)
 			currentLineLength = indent
+		} else {
+			currentLineLength = newLineLength
 		}
+		buf.WriteString(f)
+		buf.WriteByte(' ')
 	}
 	buf.WriteByte('\n')
 	buf.Flush()