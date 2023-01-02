g0odi3z2j/cilium

commit 488f63544da9ceac9c9854e3b243674d2b5fe14b

envoy: API update
NetworkPolicy `name` field has been modified to a repeated `endpoint_ips`
field.

The `name` was always the endpoint's IP, but only having one caused the
policy to be duplicated for both IPv4 and IPv6. Having all endpoint's IPs
explicitly in the policy spec removes the need to cache and sync the same
policy for IPv4 and IPv6.

L7 LB feature needs access to both endpoint IP addresses, when available,
so consolidating the policies also enables this.

Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

proxylib/proxylib/instance.go
@@ -178,26 +178,36 @@ func (ins *Instance) PolicyUpdate(resp *envoy_service_discovery.DiscoveryRespons
 			return fmt.Errorf("NPDS: Policy unmarshal error: %v", err)
 		}
 
-		policyName := config.GetName()
-
+		ips := config.GetEndpointIps()
+		if len(ips) == 0 {
+			return fmt.Errorf("NPDS: Policy has no endpoint_ips")
+		}
+		for _, ip := range ips {
+			logrus.Infof("NPDS: Endpoint IP: %s", ip)
+		}
 		// Locate the old version, if any
-		oldPolicy, found := oldMap[policyName]
+		oldPolicy, found := oldMap[ips[0]]
 		if found {
 			// Check if the new policy is the same as the old one
 			if proto.Equal(&config, oldPolicy.protobuf) {
-				logrus.Debugf("NPDS: New policy for %s is equal to the old one, no need to change", policyName)
-				newMap[policyName] = oldPolicy
+				logrus.Debugf("NPDS: New policy for Endpoint %d is equal to the old one, no need to change", config.GetEndpointId())
+				for _, ip := range ips {
+					newMap[ip] = oldPolicy
+				}
 				continue
 			}
 		}
 
 		// Validate new config
 		if err = config.Validate(); err != nil {
-			return fmt.Errorf("NPDS: Policy validation error for %s: %v", policyName, err)
+			return fmt.Errorf("NPDS: Policy validation error for Endpoint %d: %v", config.GetEndpointId(), err)
 		}
 
 		// Create new PolicyInstance, may panic. Takes ownership of 'config'.
-		newMap[policyName] = newPolicyInstance(&config)
+		newPolicy := newPolicyInstance(&config)
+		for _, ip := range ips {
+			newMap[ip] = newPolicy
+		}
 	}
 
 	// Store the new policy map

daemon/cmd/policy_test.go
@@ -56,7 +56,8 @@ var (
 	lblJoe  = labels.ParseLabel("user=joe")
 	lblPete = labels.ParseLabel("user=pete")
 
-	testEndpointID = uint16(1)
+	testQAEndpointID   = uint16(1)
+	testProdEndpointID = uint16(2)
 
 	regenerationMetadata = &regeneration.ExternalRegenerationMetadata{
 		Reason:            "test",
@@ -176,19 +177,32 @@ func (ds *DaemonSuite) getXDSNetworkPolicies(c *C, resourceNames []string) map[s
 }
 
 func prepareEndpointDirs() (cleanup func(), err error) {
-	testEPDir := fmt.Sprintf("%d", testEndpointID)
-	if err = os.Mkdir(testEPDir, 0755); err != nil {
-		return func() {}, err
+	var testDirs []string
+	for testEndpointID := range []uint16{testQAEndpointID, testProdEndpointID} {
+		testEPDir := fmt.Sprintf("%d", testEndpointID)
+		if err = os.Mkdir(testEPDir, 0755); err != nil {
+			for _, dir := range testDirs {
+				os.RemoveAll(dir)
+			}
+			return func() {}, err
+		}
+		testDirs = append(testDirs, testEPDir)
 	}
 	return func() {
-		os.RemoveAll(fmt.Sprintf("%s/ep_config.h", testEPDir))
-		time.Sleep(1 * time.Second)
-		os.RemoveAll(testEPDir)
-		os.RemoveAll(fmt.Sprintf("%s_backup", testEPDir))
+		for _, testEPDir := range testDirs {
+			os.RemoveAll(fmt.Sprintf("%s/ep_config.h", testEPDir))
+			time.Sleep(1 * time.Second)
+			os.RemoveAll(testEPDir)
+			os.RemoveAll(fmt.Sprintf("%s_backup", testEPDir))
+		}
 	}, nil
 }
 
 func (ds *DaemonSuite) prepareEndpoint(c *C, identity *identity.Identity, qa bool) *endpoint.Endpoint {
+	testEndpointID := testProdEndpointID
+	if qa {
+		testEndpointID = testQAEndpointID
+	}
 	e := endpoint.NewEndpointWithState(ds.d, ds.d, ipcache.NewIPCache(nil), ds.d.l7Proxy, ds.d.identityAllocator, testEndpointID, endpoint.StateWaitingForIdentity)
 	if qa {
 		e.IPv6 = QAIPv6Addr
@@ -333,7 +347,7 @@ func (ds *DaemonSuite) TestUpdateConsumerMap(c *C) {
 		return expectedRemotePolicies[i] < expectedRemotePolicies[j]
 	})
 	expectedNetworkPolicy := &cilium.NetworkPolicy{
-		Name:             QAIPv4Addr.String(),
+		EndpointIps:      []string{QAIPv6Addr.String(), QAIPv4Addr.String()},
 		EndpointId:       uint64(eQABar.ID),
 		ConntrackMapName: "global",
 		IngressPerPortPolicies: []*cilium.PortNetworkPolicy{
@@ -376,7 +390,7 @@ func (ds *DaemonSuite) TestUpdateConsumerMap(c *C) {
 	})
 
 	expectedNetworkPolicy = &cilium.NetworkPolicy{
-		Name:             ProdIPv4Addr.String(),
+		EndpointIps:      []string{ProdIPv6Addr.String(), ProdIPv4Addr.String()},
 		EndpointId:       uint64(eProdBar.ID),
 		ConntrackMapName: "global",
 		IngressPerPortPolicies: []*cilium.PortNetworkPolicy{
@@ -464,7 +478,7 @@ func (ds *DaemonSuite) TestL4_L7_Shadowing(c *C) {
 
 	qaBarNetworkPolicy := networkPolicies[QAIPv4Addr.String()]
 	expectedNetworkPolicy := &cilium.NetworkPolicy{
-		Name:             QAIPv4Addr.String(),
+		EndpointIps:      []string{QAIPv6Addr.String(), QAIPv4Addr.String()},
 		EndpointId:       uint64(eQABar.ID),
 		ConntrackMapName: "global",
 		IngressPerPortPolicies: []*cilium.PortNetworkPolicy{
@@ -548,7 +562,7 @@ func (ds *DaemonSuite) TestL4_L7_ShadowingShortCircuit(c *C) {
 
 	qaBarNetworkPolicy := networkPolicies[QAIPv4Addr.String()]
 	expectedNetworkPolicy := &cilium.NetworkPolicy{
-		Name:             QAIPv4Addr.String(),
+		EndpointIps:      []string{QAIPv6Addr.String(), QAIPv4Addr.String()},
 		EndpointId:       uint64(eQABar.ID),
 		ConntrackMapName: "global",
 		IngressPerPortPolicies: []*cilium.PortNetworkPolicy{
@@ -636,7 +650,7 @@ func (ds *DaemonSuite) TestL3_dependent_L7(c *C) {
 
 	qaBarNetworkPolicy := networkPolicies[QAIPv4Addr.String()]
 	expectedNetworkPolicy := &cilium.NetworkPolicy{
-		Name:             QAIPv4Addr.String(),
+		EndpointIps:      []string{QAIPv6Addr.String(), QAIPv4Addr.String()},
 		EndpointId:       uint64(eQABar.ID),
 		ConntrackMapName: "global",
 		IngressPerPortPolicies: []*cilium.PortNetworkPolicy{
@@ -928,7 +942,7 @@ func (ds *DaemonSuite) TestIncrementalPolicy(c *C) {
 	}, time.Second*1)
 	c.Assert(err, IsNil)
 	c.Assert(qaBarNetworkPolicy, checker.ExportedEquals, &cilium.NetworkPolicy{
-		Name:             QAIPv4Addr.String(),
+		EndpointIps:      []string{QAIPv6Addr.String(), QAIPv4Addr.String()},
 		EndpointId:       uint64(eQABar.ID),
 		ConntrackMapName: "global",
 		IngressPerPortPolicies: []*cilium.PortNetworkPolicy{

go.mod
@@ -21,7 +21,7 @@ require (
 	github.com/cilium/ebpf v0.8.1
 	github.com/cilium/ipam v0.0.0-20211026130907-54a76012817c
 	github.com/cilium/lumberjack/v2 v2.2.2
-	github.com/cilium/proxy v0.0.0-20220310090216-617157adcd74
+	github.com/cilium/proxy v0.0.0-20220525133153-3b70fad0b951
 	github.com/cilium/workerpool v1.1.3
 	github.com/containernetworking/cni v1.1.0
 	github.com/containernetworking/plugins v1.1.1

go.sum
@@ -186,8 +186,8 @@ github.com/cilium/lumberjack/v2 v2.2.2 h1:RKTdhb63DY0Xu7pE1pipMj7Zq28LyvBGSrCneH
 github.com/cilium/lumberjack/v2 v2.2.2/go.mod h1:yfbtPGmg4i//5oEqzaMxDqSWqgfZFmMoV70Mc2k6v0A=
 github.com/cilium/metallb v0.1.1-0.20210831235406-48667b93284d h1:skjwi8X3DdLWRI4AFmiAn83ruNoAw4f2kvdBlM8EI8c=
 github.com/cilium/metallb v0.1.1-0.20210831235406-48667b93284d/go.mod h1:8nydvUTW+/9nVywCQ9bE/YGzb4EISALP4lKNpK3fFqo=
-github.com/cilium/proxy v0.0.0-20220310090216-617157adcd74 h1:y6fqnpfQ4ePgJ56DztfsOUgCJTiCtY2R0gYGcbghQO4=
-github.com/cilium/proxy v0.0.0-20220310090216-617157adcd74/go.mod h1:ontBl/RX7G0GwcR38YQVp6d75MjIsL1FbBidVpn+F8I=
+github.com/cilium/proxy v0.0.0-20220525133153-3b70fad0b951 h1:PpsSavy+lbfzVnMhJe2E+cuBniKqnW0BGl3Sdd3Uc88=
+github.com/cilium/proxy v0.0.0-20220525133153-3b70fad0b951/go.mod h1:ontBl/RX7G0GwcR38YQVp6d75MjIsL1FbBidVpn+F8I=
 github.com/cilium/workerpool v1.1.3 h1:GB5H495r6AXg8kYklLAOn7N4PDR/djeE4WYPbq8U+yY=
 github.com/cilium/workerpool v1.1.3/go.mod h1:GOYJhwlnIjR+jWSDNBb5kw47G1H/XA9X4WOBpgr4pQU=
 github.com/circonus-labs/circonus-gometrics v2.3.1+incompatible/go.mod h1:nmEj6Dob7S7YxXgwXpfOuvO54S+tGdZdw9fuRZt25Ag=

pkg/envoy/server.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -139,8 +140,7 @@ type XDSServer struct {
 	// Exported for testing only!
 	NetworkPolicyMutator xds.AckingResourceMutator
 
-	// networkPolicyEndpoints maps each network policy's name to the info on
-	// the local endpoint.
+	// networkPolicyEndpoints maps endpoint IP to the info on the local endpoint.
 	// mutex must be held when accessing this.
 	networkPolicyEndpoints map[string]logger.EndpointUpdater
 
@@ -1543,11 +1543,11 @@ func getDirectionNetworkPolicy(ep logger.EndpointUpdater, l4Policy policy.L4Poli
 }
 
 // getNetworkPolicy converts a network policy into a cilium.NetworkPolicy.
-func getNetworkPolicy(ep logger.EndpointUpdater, vis *policy.VisibilityPolicy, name string, l4Policy *policy.L4Policy,
+func getNetworkPolicy(ep logger.EndpointUpdater, vis *policy.VisibilityPolicy, ips []string, l4Policy *policy.L4Policy,
 	ingressPolicyEnforced, egressPolicyEnforced bool) *cilium.NetworkPolicy {
 	p := &cilium.NetworkPolicy{
-		Name:             name,
-		EndpointId:       uint64(ep.GetID()),
+		EndpointIps:      ips,
+		EndpointId:       ep.GetID(),
 		ConntrackMapName: ep.ConntrackNameLocked(),
 	}
 	// If no policy, deny all traffic. Otherwise, convert the policies for ingress and egress.
@@ -1602,22 +1602,31 @@ func (s *XDSServer) UpdateNetworkPolicy(ep logger.EndpointUpdater, vis *policy.V
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	// First, validate all policies
-	ips := []string{
-		ep.GetIPv6Address(),
-		ep.GetIPv4Address(),
+	// Put IPv4 last for compatibility with sidecar containers
+	var ips []string
+	if ipv6 := ep.GetIPv6Address(); ipv6 != "" {
+		ips = append(ips, ipv6)
 	}
-	policies := make([]*cilium.NetworkPolicy, 0, len(ips))
-	for _, ip := range ips {
-		if ip == "" {
-			continue
-		}
-		networkPolicy := getNetworkPolicy(ep, vis, ip, policy, ingressPolicyEnforced, egressPolicyEnforced)
-		err := networkPolicy.Validate()
-		if err != nil {
-			return fmt.Errorf("error validating generated NetworkPolicy for %s: %s", ip, err), nil
-		}
-		policies = append(policies, networkPolicy)
+	if ipv4 := ep.GetIPv4Address(); ipv4 != "" {
+		ips = append(ips, ipv4)
+	}
+	if len(ips) == 0 {
+		// It looks like the "host EP" (identity == 1) has no IPs, so it is possible to find
+		// there are no IPs here. In this case just skip without updating a policy, as
+		// policies are always keyed by an IP.
+		//
+		// TODO: When L7 policy support for the host is needed, all host IPs should be
+		// considered here?
+		log.WithField(logfields.EndpointID, ep.GetID()).Debug("Endpoint has no IP addresses")
+		return nil, func() error { return nil }
+	}
+
+	networkPolicy := getNetworkPolicy(ep, vis, ips, policy, ingressPolicyEnforced, egressPolicyEnforced)
+
+	// First, validate the policy
+	err := networkPolicy.Validate()
+	if err != nil {
+		return fmt.Errorf("error validating generated NetworkPolicy for Endpoint %d: %s", ep.GetID(), err), nil
 	}
 
 	nodeIDs := getNodeIDs(ep, policy)
@@ -1631,22 +1640,23 @@ func (s *XDSServer) UpdateNetworkPolicy(ep logger.EndpointUpdater, vis *policy.V
 		}
 	}
 
-	// When successful, push them into the cache.
-	revertFuncs := make(xds.AckingResourceMutatorRevertFuncList, 0, len(policies))
-	revertUpdatedNetworkPolicyEndpoints := make(map[string]logger.EndpointUpdater, len(policies))
-	for _, p := range policies {
-		var callback func(error)
-		if policy != nil {
-			policyRevision := policy.Revision
-			callback = func(err error) {
-				if err == nil {
-					go ep.OnProxyPolicyUpdate(policyRevision)
-				}
+	// When successful, push policy into the cache.
+	var callback func(error)
+	if policy != nil {
+		policyRevision := policy.Revision
+		callback = func(err error) {
+			if err == nil {
+				go ep.OnProxyPolicyUpdate(policyRevision)
 			}
 		}
-		revertFuncs = append(revertFuncs, s.NetworkPolicyMutator.Upsert(NetworkPolicyTypeURL, p.Name, p, nodeIDs, wg, callback))
-		revertUpdatedNetworkPolicyEndpoints[p.Name] = s.networkPolicyEndpoints[p.Name]
-		s.networkPolicyEndpoints[p.Name] = ep
+	}
+	epID := ep.GetID()
+	resourceName := strconv.FormatUint(epID, 10)
+	revertFunc := s.NetworkPolicyMutator.Upsert(NetworkPolicyTypeURL, resourceName, networkPolicy, nodeIDs, wg, callback)
+	revertUpdatedNetworkPolicyEndpoints := make(map[string]logger.EndpointUpdater, len(ips))
+	for _, ip := range ips {
+		revertUpdatedNetworkPolicyEndpoints[ip] = s.networkPolicyEndpoints[ip]
+		s.networkPolicyEndpoints[ip] = ep
 	}
 
 	return nil, func() error {
@@ -1655,17 +1665,17 @@ func (s *XDSServer) UpdateNetworkPolicy(ep logger.EndpointUpdater, vis *policy.V
 		s.mutex.Lock()
 		defer s.mutex.Unlock()
 
-		for name, ep := range revertUpdatedNetworkPolicyEndpoints {
+		for ip, ep := range revertUpdatedNetworkPolicyEndpoints {
 			if ep == nil {
-				delete(s.networkPolicyEndpoints, name)
+				delete(s.networkPolicyEndpoints, ip)
 			} else {
-				s.networkPolicyEndpoints[name] = ep
+				s.networkPolicyEndpoints[ip] = ep
 			}
 		}
 
 		// Don't wait for an ACK for the reverted xDS updates.
 		// This is best-effort.
-		revertFuncs.Revert(nil)
+		revertFunc(nil)
 
 		log.Debug("Finished reverting xDS network policy update")
 
@@ -1697,17 +1707,19 @@ func (s *XDSServer) RemoveNetworkPolicy(ep logger.EndpointInfoSource) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	name := ep.GetIPv6Address()
-	if name != "" {
-		s.networkPolicyCache.Delete(NetworkPolicyTypeURL, name)
-		delete(s.networkPolicyEndpoints, name)
+	epID := ep.GetID()
+	resourceName := strconv.FormatUint(epID, 10)
+	s.networkPolicyCache.Delete(NetworkPolicyTypeURL, resourceName)
+
+	ip := ep.GetIPv6Address()
+	if ip != "" {
+		delete(s.networkPolicyEndpoints, ip)
 	}
-	name = ep.GetIPv4Address()
-	if name != "" {
-		s.networkPolicyCache.Delete(NetworkPolicyTypeURL, name)
-		delete(s.networkPolicyEndpoints, name)
+	ip = ep.GetIPv4Address()
+	if ip != "" {
+		delete(s.networkPolicyEndpoints, ip)
 		// Delete node resources held in the cache for the endpoint (e.g., sidecar)
-		s.NetworkPolicyMutator.DeleteNode(name)
+		s.NetworkPolicyMutator.DeleteNode(ip)
 	}
 }
 
@@ -1720,6 +1732,8 @@ func (s *XDSServer) RemoveAllNetworkPolicies() {
 // GetNetworkPolicies returns the current version of the network policies with
 // the given names.
 // If resourceNames is empty, all resources are returned.
+//
+// Only used for testing
 func (s *XDSServer) GetNetworkPolicies(resourceNames []string) (map[string]*cilium.NetworkPolicy, error) {
 	resources, err := s.networkPolicyCache.GetResources(context.Background(), NetworkPolicyTypeURL, 0, "", resourceNames)
 	if err != nil {
@@ -1728,16 +1742,18 @@ func (s *XDSServer) GetNetworkPolicies(resourceNames []string) (map[string]*cili
 	networkPolicies := make(map[string]*cilium.NetworkPolicy, len(resources.Resources))
 	for _, res := range resources.Resources {
 		networkPolicy := res.(*cilium.NetworkPolicy)
-		networkPolicies[networkPolicy.Name] = networkPolicy
+		for _, ip := range networkPolicy.EndpointIps {
+			networkPolicies[ip] = networkPolicy
+		}
 	}
 	return networkPolicies, nil
 }
 
 // getLocalEndpoint returns the endpoint info for the local endpoint on which
 // the network policy of the given name if enforced, or nil if not found.
-func (s *XDSServer) getLocalEndpoint(networkPolicyName string) logger.EndpointUpdater {
+func (s *XDSServer) getLocalEndpoint(endpointIP string) logger.EndpointUpdater {
 	s.mutex.RLock()
 	defer s.mutex.RUnlock()
 
-	return s.networkPolicyEndpoints[networkPolicyName]
+	return s.networkPolicyEndpoints[endpointIP]
 }

pkg/envoy/server_test.go
@@ -438,9 +438,9 @@ func (s *ServerSuite) TestGetDirectionNetworkPolicy(c *C) {
 }
 
 func (s *ServerSuite) TestGetNetworkPolicy(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, L4Policy1, true, true)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, L4Policy1, true, true)
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: ExpectedPerPortPolicies12Wildcard,
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
@@ -450,9 +450,9 @@ func (s *ServerSuite) TestGetNetworkPolicy(c *C) {
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyWildcard(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, L4Policy2, true, true)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, L4Policy2, true, true)
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: ExpectedPerPortPolicies12Wildcard,
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
@@ -462,9 +462,9 @@ func (s *ServerSuite) TestGetNetworkPolicyWildcard(c *C) {
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyDeny(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, L4Policy1RequiresV2, true, true)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, L4Policy1RequiresV2, true, true)
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: ExpectedPerPortPolicies12RequiresV2,
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
@@ -474,9 +474,9 @@ func (s *ServerSuite) TestGetNetworkPolicyDeny(c *C) {
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyWildcardDeny(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, L4Policy1RequiresV2, true, true)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, L4Policy1RequiresV2, true, true)
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: ExpectedPerPortPolicies12RequiresV2,
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
@@ -486,9 +486,9 @@ func (s *ServerSuite) TestGetNetworkPolicyWildcardDeny(c *C) {
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyNil(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, nil, true, true)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, nil, true, true)
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: nil,
 		EgressPerPortPolicies:  nil,
@@ -498,9 +498,9 @@ func (s *ServerSuite) TestGetNetworkPolicyNil(c *C) {
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyIngressNotEnforced(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, L4Policy2, false, true)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, L4Policy2, false, true)
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: allowAllPortNetworkPolicy,
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
@@ -510,9 +510,9 @@ func (s *ServerSuite) TestGetNetworkPolicyIngressNotEnforced(c *C) {
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyEgressNotEnforced(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, L4Policy1RequiresV2, true, false)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, L4Policy1RequiresV2, true, false)
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: ExpectedPerPortPolicies12RequiresV2,
 		EgressPerPortPolicies:  allowAllPortNetworkPolicy,
@@ -568,9 +568,9 @@ var ExpectedPerPortPoliciesL7 = []*cilium.PortNetworkPolicy{
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyL7(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, L4PolicyL7, true, true)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, L4PolicyL7, true, true)
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: ExpectedPerPortPoliciesL7,
 		ConntrackMapName:       "global",
@@ -623,9 +623,9 @@ var ExpectedPerPortPoliciesKafka = []*cilium.PortNetworkPolicy{
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyKafka(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, L4PolicyKafka, true, true)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, L4PolicyKafka, true, true)
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: ExpectedPerPortPoliciesKafka,
 		ConntrackMapName:       "global",
@@ -694,9 +694,9 @@ var ExpectedPerPortPoliciesMySQL = []*cilium.PortNetworkPolicy{
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyMySQL(c *C) {
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, L4PolicyMySQL, true, true)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, L4PolicyMySQL, true, true)
 	expected := &cilium.NetworkPolicy{
-		Name:                  IPv4Addr,
+		EndpointIps:           []string{IPv4Addr},
 		EndpointId:            uint64(ep.GetID()),
 		EgressPerPortPolicies: ExpectedPerPortPoliciesMySQL,
 		ConntrackMapName:      "global",
@@ -720,10 +720,10 @@ var kafkaIngressVisibilityPolicy = &policy.VisibilityPolicy{
 
 func (s *ServerSuite) TestGetNetworkPolicyProxylibVisibility(c *C) {
 	// No visibility gets allow-all policies
-	obtained := getNetworkPolicy(ep, nil, IPv4Addr, emptyL4Policy, false, false)
+	obtained := getNetworkPolicy(ep, nil, []string{IPv4Addr}, emptyL4Policy, false, false)
 
 	expected := &cilium.NetworkPolicy{
-		Name:                   IPv4Addr,
+		EndpointIps:            []string{IPv4Addr},
 		EndpointId:             uint64(ep.GetID()),
 		IngressPerPortPolicies: allowAllPortNetworkPolicy,
 		EgressPerPortPolicies:  allowAllPortNetworkPolicy,
@@ -732,12 +732,12 @@ func (s *ServerSuite) TestGetNetworkPolicyProxylibVisibility(c *C) {
 
 	c.Assert(obtained, checker.ExportedEquals, expected)
 
-	obtained = getNetworkPolicy(ep, kafkaIngressVisibilityPolicy, IPv4Addr, emptyL4Policy, false, false)
+	obtained = getNetworkPolicy(ep, kafkaIngressVisibilityPolicy, []string{IPv4Addr}, emptyL4Policy, false, false)
 
 	// Visibility policies still contain the allow-all policies, when policy is not enforced
 	expected = &cilium.NetworkPolicy{
-		Name:       IPv4Addr,
-		EndpointId: uint64(ep.GetID()),
+		EndpointIps: []string{IPv4Addr},
+		EndpointId:  uint64(ep.GetID()),
 		IngressPerPortPolicies: []*cilium.PortNetworkPolicy{
 			allowAllTCPPortNetworkPolicy,
 			{

commit 21dc97a65746fbc5ba2572b02c78b8d191e00932

proxylib: Downgrade noisy log msg to debug level
The log msg doesn't provide any useful / actionable information anyway,
presumably unless you're debugging.

Fixes: 488f63544da ("envoy: API update")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

proxylib/proxylib/instance.go
@@ -183,7 +183,7 @@ func (ins *Instance) PolicyUpdate(resp *envoy_service_discovery.DiscoveryRespons
 			return fmt.Errorf("NPDS: Policy has no endpoint_ips")
 		}
 		for _, ip := range ips {
-			logrus.Infof("NPDS: Endpoint IP: %s", ip)
+			logrus.Debugf("NPDS: Endpoint IP: %s", ip)
 		}
 		// Locate the old version, if any
 		oldPolicy, found := oldMap[ips[0]]