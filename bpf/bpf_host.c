g0odi3z2j/cilium

commit 60b63a6708050a81825a03874785b5c6a228eb9a

bpf: xdp: enable tunnel encap for EgressGW reply traffic
When XDP needs to tunnel-encap a packet, pass it up the stack and redirect
it to the tunnel interface from the netdev's TC ingress. In contrast to a
pure XDP solution, this lets us re-use all the existing tunnel
infrastructure in the kernel and host.

Achieve this by having ctx_set_encap_info() store the tunnel information
into our internal metadata, and transfer it over when going from XDP to TC.

For now we only use this for the case of EgressGW reply traffic - so the
change in tail_rev_nodeport_lb6() is just for commonality, we never hit the
ret != CTX_ACT_REDIRECT case there.

But it should be possible to extend this for the general case of running
XDP with TUNNEL_MODE. The relevant code paths just need to learn how to
handle CTX_ACT_OK and CTX_ACT_REDIRECT return values when calling encap
helpers.

Note that if all the xfer data becomes too much, we can probably also just
have a second tunnel look-up in cil_from_netdev() to get all that stuff.

Fixes: #19717
Fixes: #19624
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/bpf_host.c
@@ -1135,6 +1135,20 @@ int cil_from_netdev(struct __ctx_buff *ctx)
 {
 	__u32 __maybe_unused vlan_id;
 
+#if defined(ENABLE_NODEPORT_ACCELERATION) && defined(ENABLE_EGRESS_GATEWAY)
+	struct trace_ctx trace = {
+		.reason = TRACE_REASON_UNKNOWN,
+		.monitor = TRACE_PAYLOAD_LEN,
+	};
+
+	if (ctx_get_xfer(ctx, XFER_FLAGS) == XFER_PKT_ENCAP) {
+		return __encap_and_redirect_with_nodeid(ctx, ctx_get_xfer(ctx, XFER_ENCAP_NODEID),
+							ctx_get_xfer(ctx, XFER_ENCAP_SECLABEL),
+							ctx_get_xfer(ctx, XFER_ENCAP_DSTID),
+							NOT_VTEP_DST, &trace);
+	}
+#endif
+
 	/* Filter allowed vlan id's and pass them back to kernel.
 	 */
 	if (ctx->vlan_present) {

bpf/lib/common.h
@@ -64,10 +64,14 @@
 
 /* XDP to SKB transferred meta data. */
 #define XFER_PKT_NO_SVC		1 /* Skip upper service handling. */
+#define XFER_PKT_ENCAP		2 /* needs encap, tunnel info is in metadata. */
 
 /* For use in ctx_get_xfer(), after XDP called ctx_move_xfer(). */
 enum {
 	XFER_FLAGS = 0,		/* XFER_PKT_* */
+	XFER_ENCAP_NODEID = 1,
+	XFER_ENCAP_SECLABEL = 2,
+	XFER_ENCAP_DSTID = 3,
 };
 
 /* These are shared with test/bpf/check-complexity.sh, when modifying any of
@@ -631,7 +635,8 @@ static __always_inline __u32 or_encrypt_key(__u8 key)
 #define TC_INDEX_F_SKIP_HOST_FIREWALL	16
 
 /*
- * For use in ctx_{load,store}_meta(), which operates on sk_buff->cb.
+ * For use in ctx_{load,store}_meta(), which operates on sk_buff->cb or
+ * the cilium_xdp_scratch pad.
  * The verifier only exposes the first 5 slots in cb[], so this enum
  * only contains 5 entries. Aliases are added to the slots to re-use
  * them under different names in different parts of the datapath.
@@ -648,16 +653,19 @@ enum {
 #define	CB_ENCRYPT_MAGIC	CB_SRC_LABEL	/* Alias, non-overlapping */
 #define	CB_DST_ENDPOINT_ID	CB_SRC_LABEL    /* Alias, non-overlapping */
 #define CB_SRV6_SID_1		CB_SRC_LABEL	/* Alias, non-overlapping */
+#define CB_ENCAP_NODEID		CB_SRC_LABEL	/* XDP */
 	CB_IFINDEX,
 #define	CB_ADDR_V4		CB_IFINDEX	/* Alias, non-overlapping */
 #define	CB_ADDR_V6_1		CB_IFINDEX	/* Alias, non-overlapping */
 #define	CB_ENCRYPT_IDENTITY	CB_IFINDEX	/* Alias, non-overlapping */
 #define	CB_IPCACHE_SRC_LABEL	CB_IFINDEX	/* Alias, non-overlapping */
 #define CB_SRV6_SID_2		CB_IFINDEX	/* Alias, non-overlapping */
+#define CB_ENCAP_SECLABEL	CB_IFINDEX	/* XDP */
 	CB_POLICY,
 #define	CB_ADDR_V6_2		CB_POLICY	/* Alias, non-overlapping */
 #define	CB_BACKEND_ID		CB_POLICY	/* Alias, non-overlapping */
 #define CB_SRV6_SID_3		CB_POLICY	/* Alias, non-overlapping */
+#define CB_ENCAP_DSTID		CB_POLICY	/* XDP */
 	CB_NAT,
 #define	CB_ADDR_V6_3		CB_NAT		/* Alias, non-overlapping */
 #define	CB_FROM_HOST		CB_NAT		/* Alias, non-overlapping */

bpf/lib/nodeport.h
@@ -1052,6 +1052,7 @@ int tail_rev_nodeport_lb6(struct __ctx_buff *ctx)
 	if (ret == CTX_ACT_REDIRECT)
 		return ctx_redirect(ctx, ifindex, 0);
 
+	ctx_move_xfer(ctx);
 	return ret;
 
 drop:
@@ -1987,8 +1988,7 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, __u32 *ifind
 	if (!revalidate_data(ctx, &data, &data_end, &ip4))
 		return DROP_INVALID;
 
-#if defined(ENABLE_EGRESS_GATEWAY) && !defined(TUNNEL_MODE) && \
-	__ctx_is != __ctx_xdp
+#if defined(ENABLE_EGRESS_GATEWAY) && !defined(TUNNEL_MODE)
 	/* Traffic from clients to egress gateway nodes reaches said gateways
 	 * by a vxlan tunnel. If we are not using TUNNEL_MODE, we need to
 	 * identify reverse traffic from the gateway to clients and also steer
@@ -1997,10 +1997,6 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, __u32 *ifind
 	 * egress gateway map using a reverse address tuple. A match means that
 	 * the corresponding forward traffic was forwarded to the egress gateway
 	 * via the tunnel.
-	 *
-	 * Currently, we don't support redirect to a tunnel netdev / encap on
-	 * XDP. Thus, the problem mentioned above is present when using the
-	 * egress gw feature with bpf_xdp.
 	 */
 	{
 		struct egress_gw_policy_entry *egress_policy;
@@ -2043,7 +2039,7 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, __u32 *ifind
 		bpf_mark_snat_done(ctx);
 
 		*ifindex = ct_state.ifindex;
-#if defined(TUNNEL_MODE) && __ctx_is != __ctx_xdp
+#if defined(TUNNEL_MODE)
 		{
 			struct remote_endpoint_info *info;
 
@@ -2125,8 +2121,7 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, __u32 *ifind
 
 	return CTX_ACT_REDIRECT;
 
-#if (defined(ENABLE_EGRESS_GATEWAY) || defined(TUNNEL_MODE)) && \
-	__ctx_is != __ctx_xdp
+#if (defined(ENABLE_EGRESS_GATEWAY) || defined(TUNNEL_MODE))
 encap_redirect:
 	return __encap_with_nodeid(ctx, tunnel_endpoint, SECLABEL, dst_id,
 				   NOT_VTEP_DST, reason, monitor, ifindex);
@@ -2169,6 +2164,7 @@ int tail_rev_nodeport_lb4(struct __ctx_buff *ctx)
 	if (ret == CTX_ACT_REDIRECT)
 		return ctx_redirect(ctx, ifindex, 0);
 
+	ctx_move_xfer(ctx);
 	return ret;
 }
 

bpf/lib/overloadable_xdp.h
@@ -96,13 +96,28 @@ static __always_inline __maybe_unused void ctx_move_xfer(struct xdp_md *ctx)
 	/* We transfer data from XFER_MARKER. This specifically
 	 * does not break packet trains in GRO.
 	 */
-	if (meta_xfer) {
+	if (meta_xfer == XFER_PKT_NO_SVC) {
 		if (!ctx_adjust_meta(ctx, -(int)sizeof(meta_xfer))) {
 			__u32 *data_meta = ctx_data_meta(ctx);
 			__u32 *data = ctx_data(ctx);
 
 			if (!ctx_no_room(data_meta + 1, data))
 				data_meta[XFER_FLAGS] = meta_xfer;
+		}
+	} else if (meta_xfer == XFER_PKT_ENCAP) {
+		if (!ctx_adjust_meta(ctx, -(int)(4 * sizeof(__u32)))) {
+			__u32 *data_meta = ctx_data_meta(ctx);
+			__u32 *data = ctx_data(ctx);
+
+			if (!ctx_no_room(data_meta + 4, data)) {
+				data_meta[XFER_FLAGS] = meta_xfer;
+				data_meta[XFER_ENCAP_NODEID] =
+					ctx_load_meta(ctx, CB_ENCAP_NODEID);
+				data_meta[XFER_ENCAP_SECLABEL] =
+					ctx_load_meta(ctx, CB_ENCAP_SECLABEL);
+				data_meta[XFER_ENCAP_DSTID] =
+					ctx_load_meta(ctx, CB_ENCAP_DSTID);
+			}
 		}
 	}
 }
@@ -123,8 +138,17 @@ ctx_set_encap_info(struct xdp_md *ctx __maybe_unused,
 		   __u32 dstid __maybe_unused,
 		   __u32 vni __maybe_unused, __u32 *ifindex __maybe_unused)
 {
+/* needs more work in the callers: */
+#ifdef TUNNEL_MODE
 	return DROP_INVALID;
-	/* return CTX_ACT_OK; */
+#else
+	ctx_store_meta(ctx, CB_ENCAP_NODEID, bpf_ntohl(node_id));
+	ctx_store_meta(ctx, CB_ENCAP_SECLABEL, seclabel);
+	ctx_store_meta(ctx, CB_ENCAP_DSTID, dstid);
+	ctx_set_xfer(ctx, XFER_PKT_ENCAP);
+
+	return CTX_ACT_OK;
+#endif /* TUNNEL_MODE */
 }
 #endif /* HAVE_ENCAP */
 

commit f8369c4575dd86f299ebd5264b7d597bfb0febf4

bpf: nodeport: reset EDT aggregate ID for XDP-to-TC tunnel punt
If a punted skb enters from-netdev on TC-Ingress with an RX queue mapping,
this can collide with an aggregate in the THROTTLE_MAP when to-overlay
calls edt_sched_departure().

So reset the aggregate ID before redirecting into the tunnel, same as we
would do in the pure TC nodeport path.

Fixes: 60b63a670805 ("bpf: xdp: enable tunnel encap for EgressGW reply traffic")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/bpf_host.c
@@ -1054,6 +1054,8 @@ int cil_from_netdev(struct __ctx_buff *ctx)
 		ctx_snat_done_set(ctx);
 
 	if (flags & XFER_PKT_ENCAP) {
+		edt_set_aggregate(ctx, 0);
+
 		return __encap_and_redirect_with_nodeid(ctx, ctx_get_xfer(ctx, XFER_ENCAP_NODEID),
 							ctx_get_xfer(ctx, XFER_ENCAP_SECLABEL),
 							ctx_get_xfer(ctx, XFER_ENCAP_DSTID),