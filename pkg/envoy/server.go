g0odi3z2j/cilium

commit 894a0f8a5b8b155b722c2459615228763a7c5b21

envoy: Add xDS resource validation
Make sure Envoy xDS resources have names and validate xDS resources
before adding them to xDS cache. This validation can not be done at
the k8s CRD apply time, so k8s CRD can contain invalid Envoy
configurations.

Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

pkg/envoy/server.go
@@ -555,6 +555,11 @@ func (s *XDSServer) addListener(name string, listenerConf func() *envoy_config_l
 		// Note that this may degrade Envoy performance.
 		listenerConfig.EnableReusePort = &wrapperspb.BoolValue{Value: false}
 	}
+	if err := listenerConfig.Validate(); err != nil {
+		log.Errorf("Envoy: Could not validate Listener (%s): %s", err, listenerConfig.String())
+		return
+	}
+
 	s.listenerMutator.Upsert(ListenerTypeURL, name, listenerConfig, []string{"127.0.0.1"}, wg,
 		func(err error) {
 			// listener might have already been removed, so we can't look again

pkg/envoy/ciliumenvoyconfig.go
@@ -44,7 +44,7 @@ type PortAllocator interface {
 }
 
 // ParseResources parses all supported Envoy resource types from CiliumEnvoyConfig CRD to Resources type
-func ParseResources(namePrefix string, anySlice []cilium_v2alpha1.XDSResource, portAllocator PortAllocator) (Resources, error) {
+func ParseResources(namePrefix string, anySlice []cilium_v2alpha1.XDSResource, validate bool, portAllocator PortAllocator) (Resources, error) {
 	resources := Resources{}
 	for _, r := range anySlice {
 		message, err := r.UnmarshalNew()
@@ -57,6 +57,15 @@ func ParseResources(namePrefix string, anySlice []cilium_v2alpha1.XDSResource, p
 			listener, ok := message.(*envoy_config_listener.Listener)
 			if !ok {
 				return Resources{}, fmt.Errorf("Invalid type for Listener: %T", message)
+			}
+			// Check that a listener name is provided and that it is unique within this CEC
+			if listener.Name == "" {
+				return Resources{}, fmt.Errorf("'Listener name not provided")
+			}
+			for i := range resources.Listeners {
+				if listener.Name == resources.Listeners[i].Name {
+					return Resources{}, fmt.Errorf("Duplicate Listener name %q", listener.Name)
+				}
 			}
 
 			if option.Config.EnableBPFTProxy {
@@ -144,24 +153,51 @@ func ParseResources(namePrefix string, anySlice []cilium_v2alpha1.XDSResource, p
 			// listener.Name = namePrefix + "/" + listener.Name // Prepend listener name with k8s resource name
 			resources.Listeners = append(resources.Listeners, listener)
 
-			log.Debugf("ParseResources: Parsed listener %s: %v", name, listener)
+			log.Debugf("ParseResources: Parsed listener %q: %v", name, listener)
 
 		case RouteTypeURL:
 			route, ok := message.(*envoy_config_route.RouteConfiguration)
 			if !ok {
 				return Resources{}, fmt.Errorf("Invalid type for Route: %T", message)
 			}
-
+			// Check that a Route name is provided and that it is unique within this CEC
+			if route.Name == "" {
+				return Resources{}, fmt.Errorf("RouteConfiguration name not provided")
+			}
+			for i := range resources.Routes {
+				if route.Name == resources.Routes[i].Name {
+					return Resources{}, fmt.Errorf("Duplicate Route name %q", route.Name)
+				}
+			}
+			if validate {
+				if err := route.Validate(); err != nil {
+					return Resources{}, fmt.Errorf("ParseResources: Could not validate RouteConfiguration (%s): %s", err, route.String())
+				}
+			}
 			name := route.Name
 			// route.Name = namePrefix + "/" + route.Name // Prepend route name with k8s resource name
 			resources.Routes = append(resources.Routes, route)
 
-			log.Debugf("ParseResources: Parsed route %s: %v", name, route)
+			log.Debugf("ParseResources: Parsed route %q: %v", name, route)
 
 		case ClusterTypeURL:
 			cluster, ok := message.(*envoy_config_cluster.Cluster)
 			if !ok {
 				return Resources{}, fmt.Errorf("Invalid type for Route: %T", message)
+			}
+			// Check that a Cluster name is provided and that it is unique within this CEC
+			if cluster.Name == "" {
+				return Resources{}, fmt.Errorf("Cluster name not provided")
+			}
+			for i := range resources.Clusters {
+				if cluster.Name == resources.Clusters[i].Name {
+					return Resources{}, fmt.Errorf("Duplicate Cluster name %q", cluster.Name)
+				}
+			}
+			if validate {
+				if err := cluster.Validate(); err != nil {
+					return Resources{}, fmt.Errorf("ParseResources: Could not validate Cluster %q (%s): %s", cluster.Name, err, cluster.String())
+				}
 			}
 			// Fill in EDS config source if unset
 			if enum := cluster.GetType(); enum == envoy_config_cluster.Cluster_EDS {
@@ -177,21 +213,49 @@ func ParseResources(namePrefix string, anySlice []cilium_v2alpha1.XDSResource, p
 			// cluster.Name = namePrefix + "/" + cluster.Name // Prepend cluster name with k8s resource name
 			resources.Clusters = append(resources.Clusters, cluster)
 
-			log.Debugf("ParseResources: Parsed cluster %s: %v", name, cluster)
+			log.Debugf("ParseResources: Parsed cluster %q: %v", name, cluster)
 
 		case EndpointTypeURL:
-			endpoint, ok := message.(*envoy_config_endpoint.ClusterLoadAssignment)
+			endpoints, ok := message.(*envoy_config_endpoint.ClusterLoadAssignment)
 			if !ok {
 				return Resources{}, fmt.Errorf("Invalid type for Route: %T", message)
 			}
-			resources.Endpoints = append(resources.Endpoints, endpoint)
+			// Check that a Cluster name is provided and that it is unique within this CEC
+			if endpoints.ClusterName == "" {
+				return Resources{}, fmt.Errorf("ClusterLoadAssignment cluster_name not provided")
+			}
+			for i := range resources.Endpoints {
+				if endpoints.ClusterName == resources.Endpoints[i].ClusterName {
+					return Resources{}, fmt.Errorf("Duplicate cluster_name %q", endpoints.ClusterName)
+				}
+			}
+			if validate {
+				if err := endpoints.Validate(); err != nil {
+					return Resources{}, fmt.Errorf("ParseResources: Could not validate ClusterLoadAssignment for cluster %q (%s): %s", endpoints.ClusterName, err, endpoints.String())
+				}
+			}
+			resources.Endpoints = append(resources.Endpoints, endpoints)
 
-			log.Debugf("ParseResources: Parsed endpoint: %v", endpoint)
+			log.Debugf("ParseResources: Parsed endpoints for cluster %q: %v", endpoints.ClusterName, endpoints)
 
 		case SecretTypeURL:
 			secret, ok := message.(*envoy_config_tls.Secret)
 			if !ok {
 				return Resources{}, fmt.Errorf("Invalid type for Secret: %T", message)
+			}
+			// Check that a Secret name is provided and that it is unique within this CEC
+			if secret.Name == "" {
+				return Resources{}, fmt.Errorf("Secret name not provided")
+			}
+			for i := range resources.Secrets {
+				if secret.Name == resources.Secrets[i].Name {
+					return Resources{}, fmt.Errorf("Duplicate Secret name %q", secret.Name)
+				}
+			}
+			if validate {
+				if err := secret.Validate(); err != nil {
+					return Resources{}, fmt.Errorf("ParseResources: Could not validate Secret for cluster %q (%s): %s", secret.Name, err, secret.String())
+				}
 			}
 			resources.Secrets = append(resources.Secrets, secret)
 
@@ -208,7 +272,7 @@ func ParseResources(namePrefix string, anySlice []cilium_v2alpha1.XDSResource, p
 		if listener.GetAddress() == nil {
 			port, err := portAllocator.AllocateProxyPort(listener.Name, false)
 			if err != nil || port == 0 {
-				return Resources{}, fmt.Errorf("Listener port allocation failed: %s", err)
+				return Resources{}, fmt.Errorf("Listener port allocation for %q failed: %s", listener.Name, err)
 			}
 			listener.Address = getListenerAddress(port, option.Config.IPv4Enabled(), option.Config.IPv6Enabled())
 			if resources.portAllocations == nil {
@@ -218,6 +282,11 @@ func ParseResources(namePrefix string, anySlice []cilium_v2alpha1.XDSResource, p
 
 			// Inject Transparent to work with TPROXY
 			listener.Transparent = &wrapperspb.BoolValue{Value: true}
+		}
+		if validate {
+			if err := listener.Validate(); err != nil {
+				return Resources{}, fmt.Errorf("ParseResources: Could not validate Listener %q (%s): %s", listener.Name, err, listener.String())
+			}
 		}
 	}
 

pkg/envoy/ciliumenvoyconfig_test.go
@@ -7,7 +7,6 @@
 package envoy
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 
@@ -144,9 +143,9 @@ func (s *JSONSuite) TestCiliumEnvoyConfig(c *C) {
 	portAllocator := NewMockPortAllocator()
 	jsonBytes, err := yaml.YAMLToJSON([]byte(ciliumEnvoyConfig))
 	c.Assert(err, IsNil)
-	var buf bytes.Buffer
-	json.Indent(&buf, jsonBytes, "", "\t")
-	fmt.Printf("JSON spec:\n%s\n", buf.String())
+	// var buf bytes.Buffer
+	// json.Indent(&buf, jsonBytes, "", "\t")
+	// fmt.Printf("JSON spec:\n%s\n", buf.String())
 	cec := &cilium_v2alpha1.CiliumEnvoyConfig{}
 	err = json.Unmarshal(jsonBytes, cec)
 	c.Assert(err, IsNil)
@@ -154,7 +153,7 @@ func (s *JSONSuite) TestCiliumEnvoyConfig(c *C) {
 	c.Assert(cec.Spec.Resources, HasLen, 1)
 	c.Assert(cec.Spec.Resources[0].TypeUrl, Equals, "type.googleapis.com/envoy.config.listener.v3.Listener")
 
-	resources, err := ParseResources("prefix", cec.Spec.Resources, portAllocator)
+	resources, err := ParseResources("prefix", cec.Spec.Resources, true, portAllocator)
 	c.Assert(err, IsNil)
 	c.Assert(resources.Listeners, HasLen, 1)
 	c.Assert(resources.Listeners[0].Address.GetSocketAddress().GetPortValue(), Equals, uint32(10000))
@@ -193,6 +192,90 @@ func (s *JSONSuite) TestCiliumEnvoyConfig(c *C) {
 	c.Assert(hcm.HttpFilters[0].Name, Equals, "envoy.filters.http.router")
 }
 
+var ciliumEnvoyConfigInvalid = `apiVersion: cilium.io/v2alpha1
+kind: CiliumEnvoyConfig
+metadata:
+  name: envoy-prometheus-metrics-listener
+spec:
+  version_info: "0"
+  resources:
+  - "@type": type.googleapis.com/envoy.config.listener.v3.Listener
+    name: envoy-prometheus-metrics-listener
+    address:
+      socket_address:
+        address: 127.0.0.1
+    filter_chains:
+    - filters:
+      - name: envoy.filters.network.http_connection_manager
+        typed_config:
+          "@type": type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager
+          stat_prefix: ingress_http
+          codec_type: AUTO
+          rds:
+            route_config_name: local_route
+          http_filters:
+          - name: envoy.filters.http.router
+`
+
+func (s *JSONSuite) TestCiliumEnvoyConfigValidation(c *C) {
+	portAllocator := NewMockPortAllocator()
+	jsonBytes, err := yaml.YAMLToJSON([]byte(ciliumEnvoyConfigInvalid))
+	c.Assert(err, IsNil)
+	// var buf bytes.Buffer
+	// json.Indent(&buf, jsonBytes, "", "\t")
+	// fmt.Printf("JSON spec:\n%s\n", buf.String())
+	cec := &cilium_v2alpha1.CiliumEnvoyConfig{}
+	err = json.Unmarshal(jsonBytes, cec)
+	c.Assert(err, IsNil)
+	c.Assert(cec.Spec.Resources, Not(IsNil))
+	c.Assert(cec.Spec.Resources, HasLen, 1)
+	c.Assert(cec.Spec.Resources[0].TypeUrl, Equals, "type.googleapis.com/envoy.config.listener.v3.Listener")
+
+	resources, err := ParseResources("prefix", cec.Spec.Resources, false, portAllocator)
+	c.Assert(err, IsNil)
+	c.Assert(resources.Listeners, HasLen, 1)
+	c.Assert(resources.Listeners[0].Address.GetSocketAddress().GetPortValue(), Equals, uint32(0)) // invalid listener port number
+	c.Assert(resources.Listeners[0].FilterChains, HasLen, 1)
+	chain := resources.Listeners[0].FilterChains[0]
+	c.Assert(chain.Filters, HasLen, 1)
+	c.Assert(chain.Filters[0].Name, Equals, "envoy.filters.network.http_connection_manager")
+	message, err := chain.Filters[0].GetTypedConfig().UnmarshalNew()
+	c.Assert(err, IsNil)
+	c.Assert(message, Not(IsNil))
+	hcm, ok := message.(*envoy_config_http.HttpConnectionManager)
+	c.Assert(ok, Equals, true)
+	c.Assert(hcm, Not(IsNil))
+
+	//
+	// Check that missing RDS config source is automatically filled in
+	//
+	rds := hcm.GetRds()
+	c.Assert(rds, Not(IsNil))
+	cs := rds.GetConfigSource()
+	c.Assert(cs, Not(IsNil))
+	acs := cs.GetApiConfigSource()
+	c.Assert(acs, Not(IsNil))
+	c.Assert(acs.ApiType, Equals, envoy_config_core.ApiConfigSource_GRPC)
+	c.Assert(acs.TransportApiVersion, Equals, envoy_config_core.ApiVersion_V3)
+	c.Assert(acs.SetNodeOnFirstMessageOnly, Equals, true)
+	c.Assert(acs.GrpcServices, HasLen, 1)
+	eg := acs.GrpcServices[0].GetEnvoyGrpc()
+	c.Assert(eg, Not(IsNil))
+	c.Assert(eg.ClusterName, Equals, "xds-grpc-cilium")
+
+	//
+	// Check that HTTP filters are parsed
+	//
+	c.Assert(hcm.HttpFilters, HasLen, 1)
+	c.Assert(hcm.HttpFilters[0].Name, Equals, "envoy.filters.http.router")
+
+	//
+	// Same with validation fails
+	//
+	resources, err = ParseResources("prefix", cec.Spec.Resources, true, portAllocator)
+	c.Assert(err, Not(IsNil))
+}
+
 var ciliumEnvoyConfigNoAddress = `apiVersion: cilium.io/v2alpha1
 kind: CiliumEnvoyConfig
 metadata:
@@ -219,9 +302,9 @@ func (s *JSONSuite) TestCiliumEnvoyConfigNoAddress(c *C) {
 	portAllocator := NewMockPortAllocator()
 	jsonBytes, err := yaml.YAMLToJSON([]byte(ciliumEnvoyConfigNoAddress))
 	c.Assert(err, IsNil)
-	var buf bytes.Buffer
-	json.Indent(&buf, jsonBytes, "", "\t")
-	fmt.Printf("JSON spec:\n%s\n", buf.String())
+	// var buf bytes.Buffer
+	// json.Indent(&buf, jsonBytes, "", "\t")
+	// fmt.Printf("JSON spec:\n%s\n", buf.String())
 	cec := &cilium_v2alpha1.CiliumEnvoyConfig{}
 	err = json.Unmarshal(jsonBytes, cec)
 	c.Assert(err, IsNil)
@@ -229,7 +312,7 @@ func (s *JSONSuite) TestCiliumEnvoyConfigNoAddress(c *C) {
 	c.Assert(cec.Spec.Resources, HasLen, 1)
 	c.Assert(cec.Spec.Resources[0].TypeUrl, Equals, "type.googleapis.com/envoy.config.listener.v3.Listener")
 
-	resources, err := ParseResources("prefix", cec.Spec.Resources, portAllocator)
+	resources, err := ParseResources("prefix", cec.Spec.Resources, true, portAllocator)
 	c.Assert(err, IsNil)
 	c.Assert(resources.Listeners, HasLen, 1)
 	c.Assert(resources.Listeners[0].Address, Not(IsNil))
@@ -280,6 +363,7 @@ spec:
   version_info: "0"
   resources:
   - "@type": type.googleapis.com/envoy.config.listener.v3.Listener
+    name: multi-resource-listener
     address:
       socket_address:
         address: 127.0.0.1
@@ -341,7 +425,7 @@ func (s *JSONSuite) TestCiliumEnvoyConfigMulti(c *C) {
 	c.Assert(cec.Spec.Resources, HasLen, 5)
 	c.Assert(cec.Spec.Resources[0].TypeUrl, Equals, "type.googleapis.com/envoy.config.listener.v3.Listener")
 
-	resources, err := ParseResources("prefix", cec.Spec.Resources, portAllocator)
+	resources, err := ParseResources("prefix", cec.Spec.Resources, true, portAllocator)
 	c.Assert(err, IsNil)
 	c.Assert(resources.Listeners, HasLen, 1)
 	c.Assert(resources.Listeners[0].Address.GetSocketAddress().GetPortValue(), Equals, uint32(10000))

pkg/k8s/watchers/cilium_clusterwide_envoy_config.go
@@ -89,7 +89,7 @@ func (k *K8sWatcher) addCiliumClusterwideEnvoyConfig(ccec *cilium_v2alpha1.Ciliu
 		logfields.K8sAPIVersion: ccec.TypeMeta.APIVersion,
 	})
 
-	resources, err := envoy.ParseResources("", ccec.Spec.Resources, k.envoyConfigManager)
+	resources, err := envoy.ParseResources("", ccec.Spec.Resources, true, k.envoyConfigManager)
 	if err != nil {
 		scopedLog.WithError(err).Warn("Failed to add CiliumClusterwideEnvoyConfig: malformed Envoy config")
 		return err
@@ -119,12 +119,12 @@ func (k *K8sWatcher) updateCiliumClusterwideEnvoyConfig(oldCCEC *cilium_v2alpha1
 		logfields.K8sAPIVersion: newCCEC.TypeMeta.APIVersion,
 	})
 
-	oldResources, err := envoy.ParseResources("", oldCCEC.Spec.Resources, k.envoyConfigManager)
+	oldResources, err := envoy.ParseResources("", oldCCEC.Spec.Resources, false, k.envoyConfigManager)
 	if err != nil {
 		scopedLog.WithError(err).Warn("Failed to update CiliumClusterwideEnvoyConfig: malformed old Envoy config")
 		return err
 	}
-	newResources, err := envoy.ParseResources("", newCCEC.Spec.Resources, k.envoyConfigManager)
+	newResources, err := envoy.ParseResources("", newCCEC.Spec.Resources, true, k.envoyConfigManager)
 	if err != nil {
 		scopedLog.WithError(err).Warn("Failed to update CiliumClusterwideEnvoyConfig: malformed new Envoy config")
 		return err
@@ -158,7 +158,7 @@ func (k *K8sWatcher) deleteCiliumClusterwideEnvoyConfig(ccec *cilium_v2alpha1.Ci
 		logfields.K8sAPIVersion: ccec.TypeMeta.APIVersion,
 	})
 
-	resources, err := envoy.ParseResources("", ccec.Spec.Resources, k.envoyConfigManager)
+	resources, err := envoy.ParseResources("", ccec.Spec.Resources, false, k.envoyConfigManager)
 	if err != nil {
 		scopedLog.WithError(err).Warn("Failed to delete CiliumClusterwideEnvoyConfig: parsing rersource names failed")
 		return err

pkg/k8s/watchers/cilium_envoy_config.go
@@ -91,7 +91,7 @@ func (k *K8sWatcher) addCiliumEnvoyConfig(cec *cilium_v2alpha1.CiliumEnvoyConfig
 		logfields.K8sAPIVersion:         cec.TypeMeta.APIVersion,
 	})
 
-	resources, err := envoy.ParseResources(cec.ObjectMeta.Namespace, cec.Spec.Resources, k.envoyConfigManager)
+	resources, err := envoy.ParseResources(cec.ObjectMeta.Namespace, cec.Spec.Resources, true, k.envoyConfigManager)
 	if err != nil {
 		scopedLog.WithError(err).Warn("Failed to add CiliumEnvoyConfig: malformed Envoy config")
 		return err
@@ -179,12 +179,12 @@ func (k *K8sWatcher) updateCiliumEnvoyConfig(oldCEC *cilium_v2alpha1.CiliumEnvoy
 		logfields.K8sAPIVersion:         newCEC.TypeMeta.APIVersion,
 	})
 
-	oldResources, err := envoy.ParseResources(oldCEC.ObjectMeta.Namespace, oldCEC.Spec.Resources, k.envoyConfigManager)
+	oldResources, err := envoy.ParseResources(oldCEC.ObjectMeta.Namespace, oldCEC.Spec.Resources, false, k.envoyConfigManager)
 	if err != nil {
 		scopedLog.WithError(err).Warn("Failed to update CiliumEnvoyConfig: malformed old Envoy config")
 		return err
 	}
-	newResources, err := envoy.ParseResources(newCEC.ObjectMeta.Namespace, newCEC.Spec.Resources, k.envoyConfigManager)
+	newResources, err := envoy.ParseResources(newCEC.ObjectMeta.Namespace, newCEC.Spec.Resources, true, k.envoyConfigManager)
 	if err != nil {
 		scopedLog.WithError(err).Warn("Failed to update CiliumEnvoyConfig: malformed new Envoy config")
 		return err
@@ -278,7 +278,7 @@ func (k *K8sWatcher) deleteCiliumEnvoyConfig(cec *cilium_v2alpha1.CiliumEnvoyCon
 		logfields.K8sAPIVersion:         cec.TypeMeta.APIVersion,
 	})
 
-	resources, err := envoy.ParseResources(cec.ObjectMeta.Namespace, cec.Spec.Resources, k.envoyConfigManager)
+	resources, err := envoy.ParseResources(cec.ObjectMeta.Namespace, cec.Spec.Resources, false, k.envoyConfigManager)
 	if err != nil {
 		scopedLog.WithError(err).Warn("Failed to delete CiliumEnvoyConfig: parsing rersource names failed")
 		return err

pkg/k8s/watchers/cilium_envoy_config_test.go
@@ -68,7 +68,7 @@ func (s *K8sWatcherSuite) TestParseEnvoySpec(c *C) {
 	c.Assert(cec.Spec.Resources, HasLen, 1)
 	c.Assert(cec.Spec.Resources[0].TypeUrl, Equals, "type.googleapis.com/envoy.config.listener.v3.Listener")
 
-	resources, err := envoy.ParseResources("prefix", cec.Spec.Resources, nil)
+	resources, err := envoy.ParseResources("prefix", cec.Spec.Resources, true, nil)
 	c.Assert(err, IsNil)
 	c.Assert(resources.Listeners, HasLen, 1)
 	c.Assert(resources.Listeners[0].Address.GetSocketAddress().GetPortValue(), Equals, uint32(10000))

commit eebb5135e0a4e7e2cfc9d130667fdb44369e8982

envoy: Fix lock leak in config validation failure
If XDS listener configuration validation failed, addListener() would not
unlock the XDSServer.mutex, leading to the lock being locked forever.
Fix it by using a standard defer() approach.

CC: Jarno Rajahalme <jarno@isovalent.com>
Fixes: 894a0f8a5b8b ("envoy: Add xDS resource validation")
Signed-off-by: Joe Stringer <joe@cilium.io>

pkg/envoy/server.go
@@ -536,6 +536,8 @@ func (s *XDSServer) AddMetricsListener(port uint16, wg *completion.WaitGroup) {
 // 'listenerConf()' is only called if a new listener is being created.
 func (s *XDSServer) addListener(name string, listenerConf func() *envoy_config_listener.Listener, wg *completion.WaitGroup, cb func(err error), isProxyListener bool) {
 	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	listener := s.listeners[name]
 	if listener == nil {
 		listener = &Listener{}
@@ -554,7 +556,6 @@ func (s *XDSServer) addListener(name string, listenerConf func() *envoy_config_l
 			listener.waiters = append(listener.waiters, wg.AddCompletion())
 		}
 		listener.mutex.Unlock()
-		s.mutex.Unlock()
 		return
 	}
 	// Try again after a NACK, potentially with a different port number, etc.
@@ -600,7 +601,6 @@ func (s *XDSServer) addListener(name string, listenerConf func() *envoy_config_l
 				cb(err)
 			}
 		})
-	s.mutex.Unlock()
 }
 
 // upsertListener either updates an existing LDS listener with 'name', or creates a new one.