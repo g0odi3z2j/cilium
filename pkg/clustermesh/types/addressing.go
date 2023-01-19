g0odi3z2j/cilium

commit a68386bae3cffd616ee05771123815f491146bd3

clustermesh: Add some helper functions to PrefixCluster
Implement required helpers to implement tunnel map with cluster-aware
addressing.

Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

pkg/clustermesh/types/addressing.go
@@ -10,6 +10,8 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/cilium/cilium/pkg/cidr"
+	"github.com/cilium/cilium/pkg/ip"
 	ippkg "github.com/cilium/cilium/pkg/ip"
 )
 
@@ -234,6 +236,32 @@ func PrefixClusterFrom(addr netip.Addr, bits int, clusterID uint32) PrefixCluste
 		prefix:    netip.PrefixFrom(addr, bits),
 		clusterID: clusterID,
 	}
+}
+
+func PrefixClusterFromCIDR(c *cidr.CIDR, clusterID uint32) PrefixCluster {
+	if c == nil {
+		return PrefixCluster{}
+	}
+
+	addr := ip.MustAddrFromIP(c.IP)
+	ones, _ := c.Mask.Size()
+
+	return PrefixCluster{
+		prefix:    netip.PrefixFrom(addr, ones),
+		clusterID: clusterID,
+	}
+}
+
+func (pc0 PrefixCluster) Equal(pc1 PrefixCluster) bool {
+	return pc0.prefix == pc1.prefix && pc0.clusterID == pc1.clusterID
+}
+
+func (pc PrefixCluster) IsValid() bool {
+	return pc.prefix.IsValid()
+}
+
+func (pc PrefixCluster) AddrCluster() AddrCluster {
+	return AddrClusterFrom(pc.prefix.Addr(), pc.clusterID)
 }
 
 func (pc PrefixCluster) String() string {

commit f33b7abc1db764ef04aea7e6e65d6b89206861f3

clustermesh/types: don't panic on invalid IP in PrefixClusterFromCIDR
ip.MustAddrFromIP will panic in case of an invalid address. These types
of conversion functions should usually only be used in test code. Return
an empty PrefixCluster in this case instead, like in other error cases.

Fixes: a68386bae3cf ("clustermesh: Add some helper functions to PrefixCluster")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/clustermesh/types/addressing.go
@@ -11,7 +11,6 @@ import (
 	"strings"
 
 	"github.com/cilium/cilium/pkg/cidr"
-	"github.com/cilium/cilium/pkg/ip"
 	ippkg "github.com/cilium/cilium/pkg/ip"
 )
 
@@ -243,7 +242,10 @@ func PrefixClusterFromCIDR(c *cidr.CIDR, clusterID uint32) PrefixCluster {
 		return PrefixCluster{}
 	}
 
-	addr := ip.MustAddrFromIP(c.IP)
+	addr, ok := ippkg.AddrFromIP(c.IP)
+	if !ok {
+		return PrefixCluster{}
+	}
 	ones, _ := c.Mask.Size()
 
 	return PrefixCluster{