g0odi3z2j/cilium

commit fca06ad5ee80a9124490b06b0f04be3fbcbfba11

datapath: always generate BTF debug information
Enable by default the generation of the debug symbols for the BPF
program.

The --bpf-compile-debug option does not have any effect anymore and will
be deprecated in version 1.11.

Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

pkg/datapath/loader/compile.go
@@ -274,17 +274,13 @@ func progCFlags(prog *progInfo, dir *directoryInfo) []string {
 }
 
 // compile and link a program.
-func compile(ctx context.Context, prog *progInfo, dir *directoryInfo, debug bool) (err error) {
+func compile(ctx context.Context, prog *progInfo, dir *directoryInfo) (err error) {
 	args := make([]string, 0, 16)
 	if prog.OutputType == outputSource {
 		args = append(args, "-E") // Preprocessor
 	} else {
 		args = append(args, "-emit-llvm")
-		if debug {
-			// FIXME: Latest clang/llvm generates incompatible BTF that
-			// iproute2 cannot load.
-			//args = append(args, "-g")
-		}
+		args = append(args, "-g")
 	}
 
 	args = append(args, standardCFlags...)
@@ -298,11 +294,11 @@ func compile(ctx context.Context, prog *progInfo, dir *directoryInfo, debug bool
 	}).Debug("Launching compiler")
 	if prog.OutputType == outputSource {
 		compileCmd := exec.CommandContext(ctx, compiler, args...)
-		_, err = compileCmd.CombinedOutput(log, debug)
+		_, err = compileCmd.CombinedOutput(log, true)
 	} else {
 		switch prog.OutputType {
 		case outputObject:
-			err = compileAndLink(ctx, prog, dir, debug, args...)
+			err = compileAndLink(ctx, prog, dir, true, args...)
 		case outputAssembly:
 			err = compileAndLink(ctx, prog, dir, false, args...)
 		default:
@@ -316,20 +312,20 @@ func compile(ctx context.Context, prog *progInfo, dir *directoryInfo, debug bool
 // compileDatapath invokes the compiler and linker to create all state files for
 // the BPF datapath, with the primary target being the BPF ELF binary.
 //
-// If debug is enabled, create also the following output files:
+// It also creates the following output files:
 // * Preprocessed C
 // * Assembly
 // * Object compiled with debug symbols
-func compileDatapath(ctx context.Context, dirs *directoryInfo, isHost, debug bool, logger *logrus.Entry) error {
-	scopedLog := logger.WithField(logfields.Debug, debug)
+func compileDatapath(ctx context.Context, dirs *directoryInfo, isHost bool, logger *logrus.Entry) error {
+	scopedLog := logger.WithField(logfields.Debug, true)
 
 	versionCmd := exec.CommandContext(ctx, compiler, "--version")
-	compilerVersion, err := versionCmd.CombinedOutput(scopedLog, debug)
+	compilerVersion, err := versionCmd.CombinedOutput(scopedLog, true)
 	if err != nil {
 		return err
 	}
 	versionCmd = exec.CommandContext(ctx, linker, "--version")
-	linkerVersion, err := versionCmd.CombinedOutput(scopedLog, debug)
+	linkerVersion, err := versionCmd.CombinedOutput(scopedLog, true)
 	if err != nil {
 		return err
 	}
@@ -339,22 +335,20 @@ func compileDatapath(ctx context.Context, dirs *directoryInfo, isHost, debug boo
 	}).Debug("Compiling datapath")
 
 	// Write out assembly and preprocessing files for debugging purposes
-	if debug {
-		progs := debugProgs
-		if isHost {
-			progs = debugHostProgs
-		}
-		for _, p := range progs {
-			if err := compile(ctx, p, dirs, debug); err != nil {
-				// Only log an error here if the context was not canceled or not
-				// timed out; this log message should only represent failures
-				// with respect to compiling the program.
-				if ctx.Err() == nil {
-					scopedLog.WithField(logfields.Params, logfields.Repr(p)).
-						WithError(err).Debug("JoinEP: Failed to compile")
-				}
-				return err
+	progs := debugProgs
+	if isHost {
+		progs = debugHostProgs
+	}
+	for _, p := range progs {
+		if err := compile(ctx, p, dirs); err != nil {
+			// Only log an error here if the context was not canceled or not
+			// timed out; this log message should only represent failures
+			// with respect to compiling the program.
+			if ctx.Err() == nil {
+				scopedLog.WithField(logfields.Params, logfields.Repr(p)).
+					WithError(err).Debug("JoinEP: Failed to compile")
 			}
+			return err
 		}
 	}
 
@@ -363,7 +357,7 @@ func compileDatapath(ctx context.Context, dirs *directoryInfo, isHost, debug boo
 	if isHost {
 		prog = hostEpProg
 	}
-	if err := compile(ctx, prog, dirs, debug); err != nil {
+	if err := compile(ctx, prog, dirs); err != nil {
 		// Only log an error here if the context was not canceled or not timed
 		// out; this log message should only represent failures with respect to
 		// compiling the program.
@@ -379,7 +373,6 @@ func compileDatapath(ctx context.Context, dirs *directoryInfo, isHost, debug boo
 
 // Compile compiles a BPF program generating an object file.
 func Compile(ctx context.Context, src string, out string) error {
-	debug := option.Config.BPFCompilationDebug
 	prog := progInfo{
 		Source:     src,
 		Output:     out,
@@ -391,7 +384,7 @@ func Compile(ctx context.Context, src string, out string) error {
 		Output:  option.Config.StateDir,
 		State:   option.Config.StateDir,
 	}
-	return compile(ctx, &prog, &dirs, debug)
+	return compile(ctx, &prog, &dirs)
 }
 
 // compileTemplate compiles a BPF program generating a template object file.
@@ -402,5 +395,5 @@ func compileTemplate(ctx context.Context, out string, isHost bool) error {
 		Output:  out,
 		State:   out,
 	}
-	return compileDatapath(ctx, &dirs, isHost, option.Config.BPFCompilationDebug, log)
+	return compileDatapath(ctx, &dirs, isHost, log)
 }

bpf/init.sh
@@ -233,7 +233,7 @@ function bpf_compile()
 	EXTRA_OPTS=$4
 
 	clang -O2 -target bpf -std=gnu89 -nostdinc -emit-llvm	\
-	      -Wall -Wextra -Werror -Wshadow			\
+	      -g -Wall -Wextra -Werror -Wshadow			\
 	      -Wno-address-of-packed-member			\
 	      -Wno-unknown-warning-option			\
 	      -Wno-gnu-variable-sized-type-not-at-end		\
@@ -631,4 +631,4 @@ fi
 
 # Compile dummy BPF file containing all shared struct definitions used by
 # pkg/alignchecker to validate C and Go equivalent struct alignments
-bpf_compile bpf_alignchecker.c bpf_alignchecker.o obj "-g"
+bpf_compile bpf_alignchecker.c bpf_alignchecker.o obj ""

daemon/cmd/daemon_main.go
@@ -224,7 +224,8 @@ func init() {
 	flags.String(option.CGroupRoot, "", "Path to Cgroup2 filesystem")
 	option.BindEnv(option.CGroupRoot)
 
-	flags.Bool(option.BPFCompileDebugName, false, "Enable debugging of the BPF compilation process")
+	flags.Bool(option.BPFCompileDebugName, true, "Enable debugging of the BPF compilation process. ")
+	flags.MarkDeprecated(option.BPFCompileDebugName, "This flag is no longer available and will be removed in v1.11")
 	option.BindEnv(option.BPFCompileDebugName)
 
 	flags.Bool(option.SockopsEnableName, defaults.SockopsEnable, "Enable sockops when kernel supported")

pkg/datapath/loader/cache.go
@@ -65,6 +65,13 @@ var ignoredELFPrefixes = []string{
 	"from-host",                  // Prog name
 	"to-netdev",                  // Prog name
 	"to-host",                    // Prog name
+	".BTF",                       // Debug
+	".BTF.ext",                   // Debug
+	".debug_ranges",              // Debug
+	".debug_info",                // Debug
+	".debug_line",                // Debug
+	".debug_frame",               // Debug
+	".debug_loc",                 // Debug
 	// Endpoint IPv6 address. It's possible for the template object to have
 	// these symbols while the endpoint doesn't, if IPv6 was just enabled and
 	// the endpoint restored.

pkg/datapath/loader/loader.go
@@ -367,9 +367,8 @@ func (l *Loader) reloadDatapath(ctx context.Context, ep datapath.Endpoint, dirs 
 }
 
 func (l *Loader) compileAndLoad(ctx context.Context, ep datapath.Endpoint, dirs *directoryInfo, stats *metrics.SpanStat) error {
-	debug := option.Config.BPFCompilationDebug
 	stats.BpfCompilation.Start()
-	err := compileDatapath(ctx, dirs, ep.IsHost(), debug, ep.Logger(Subsystem))
+	err := compileDatapath(ctx, dirs, ep.IsHost(), ep.Logger(Subsystem))
 	stats.BpfCompilation.End(err == nil)
 	if err != nil {
 		return err

pkg/option/config.go
@@ -1528,10 +1528,6 @@ type DaemonConfig struct {
 	// ProxyPrometheusPort specifies the port to serve Envoy metrics on.
 	ProxyPrometheusPort int
 
-	// BPFCompilationDebug specifies whether to compile BPF programs compilation
-	// debugging enabled.
-	BPFCompilationDebug bool
-
 	// EnvoyLogPath specifies where to store the Envoy proxy logs when Envoy
 	// runs in the same container as Cilium.
 	EnvoyLogPath string
@@ -2454,7 +2450,6 @@ func (c *DaemonConfig) Populate() {
 	c.AllowLocalhost = viper.GetString(AllowLocalhost)
 	c.AnnotateK8sNode = viper.GetBool(AnnotateK8sNode)
 	c.AutoCreateCiliumNodeResource = viper.GetBool(AutoCreateCiliumNodeResource)
-	c.BPFCompilationDebug = viper.GetBool(BPFCompileDebugName)
 	c.BPFRoot = viper.GetString(BPFRoot)
 	c.CertDirectory = viper.GetString(CertsDirectory)
 	c.CGroupRoot = viper.GetString(CGroupRoot)

commit 56b3cec46ea324655f4b045aeb760a83e159a741

cilium: Fix missing error log dump from compilation
When compilation fails in Cilium, it currently does not dump the actual
compilation error. This is in particular annoying given the developer
experience suffers from it if an error cannot be reproduced otherwise.

Turns out this got changed via fca06ad5ee80 ("datapath: always generate
BTF debug information") where it switched from `compileAndLink(ctx, prog,
dir, debug, args...)` to unconditional `compileAndLink(ctx, prog, dir,
true, args...)` which then accidentially made the warning messages
disappear from regular (non-debug) mode in the scopedLog selector.

Before looking into this, I was not aware that these would show up in
debug mode. Make this a plain log.Warn given this is something we should
warn about and have easy introspection no matter if in debug or non-debug
mode.

Before fix:

  [...]
  level=error msg="Failed to compile bpf_host.dbg.o: exit status 1" compiler-pid=3097802 linker-pid=3097803 subsys=datapath-loader
  level=error msg="BPF template object creation failed" bpfHeaderfileHash=b67b2d6708b2cb48f42dc6305e5802be6fba717325a2006082c3a0b396e67630 error="failed to compile template program /var/run/cilium/state/templates/b67b2d6708b2cb48f42dc6305e5802be6fba717325a2006082c3a0b396e67630: Failed to compile bpf_host.dbg.o: exit status 1" subsys=datapath-loader
  [...]

After fix:

  [...]
  level=error msg="Failed to compile bpf_host.dbg.o: exit status 1" compiler-pid=3086249 linker-pid=3086250 subsys=datapath-loader
  level=warning msg="In file included from /var/lib/cilium/bpf/bpf_host.c:55:" subsys=datapath-loader
  level=warning msg="/var/lib/cilium/bpf/lib/nodeport.h:570:8: error: implicit declaration of function 'fib_redirect_v6' [-Werror,-Wimplicit-function-declaration]" subsys=datapath-loader
  level=warning msg="        ret = fib_redirect_v6(ctx, l3_off, ip6, ctx_get_ifindex(ctx), &oif);" subsys=datapath-loader
  level=warning msg="              ^" subsys=datapath-loader
  level=warning msg="/var/lib/cilium/bpf/lib/nodeport.h:601:8: error: implicit declaration of function 'fib_redirect_v4' [-Werror,-Wimplicit-function-declaration]" subsys=datapath-loader
  level=warning msg="        ret = fib_redirect_v4(ctx, l3_off, ip4, ctx_get_ifindex(ctx), &oif);" subsys=datapath-loader
  level=warning msg="              ^" subsys=datapath-loader
  level=warning msg="2 errors generated." subsys=datapath-loader
  level=error msg="BPF template object creation failed" bpfHeaderfileHash=b67b2d6708b2cb48f42dc6305e5802be6fba717325a2006082c3a0b396e67630 error="failed to compile template program /var/run/cilium/state/templates/b67b2d6708b2cb48f42dc6305e5802be6fba717325a2006082c3a0b396e67630: Failed to compile bpf_host.dbg.o: exit status 1" subsys=datapath-loader
  [...]

Fixes: fca06ad5ee80 ("datapath: always generate BTF debug information")

pkg/datapath/loader/compile.go
@@ -218,7 +218,7 @@ func pidFromProcess(proc *os.Process) string {
 
 // compileAndLink links the specified program from the specified path to the
 // intermediate representation, to the output specified in the prog's info.
-func compileAndLink(ctx context.Context, prog *progInfo, dir *directoryInfo, debug bool, compileArgs ...string) error {
+func compileAndLink(ctx context.Context, prog *progInfo, dir *directoryInfo, compileArgs ...string) error {
 	compileCmd, cancelCompile := exec.WithCancel(ctx, compiler, compileArgs...)
 	defer cancelCompile()
 	compilerStdout, compilerStderr, err := prepareCmdPipes(compileCmd)
@@ -257,13 +257,9 @@ func compileAndLink(ctx context.Context, prog *progInfo, dir *directoryInfo, deb
 		}
 
 		if compileOut != nil {
-			scopedLog := log.Warn
-			if debug {
-				scopedLog = log.Debug
-			}
 			scanner := bufio.NewScanner(bytes.NewReader(compileOut))
 			for scanner.Scan() {
-				scopedLog(scanner.Text())
+				log.Warn(scanner.Text())
 			}
 		}
 	}
@@ -315,10 +311,8 @@ func compile(ctx context.Context, prog *progInfo, dir *directoryInfo) (err error
 	case outputSource:
 		compileCmd := exec.CommandContext(ctx, compiler, args...)
 		_, err = compileCmd.CombinedOutput(log, true)
-	case outputObject:
-		err = compileAndLink(ctx, prog, dir, true, args...)
-	case outputAssembly:
-		err = compileAndLink(ctx, prog, dir, false, args...)
+	case outputObject, outputAssembly:
+		err = compileAndLink(ctx, prog, dir, args...)
 	default:
 		log.Fatalf("Unhandled progInfo.OutputType %s", prog.OutputType)
 	}