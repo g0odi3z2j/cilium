g0odi3z2j/cilium

commit c65f4cf596aed56476922cbd63ed8e9265540f59

hubble: Add "syn-only" option to flows-to-world metric
Use this option to exclude non-SYN drops that could occur transiently
from the flows-to-world metric.

Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

pkg/hubble/metrics/flows-to-world/handler.go
@@ -23,6 +23,7 @@ type flowsToWorldHandler struct {
 	worldLabel   string
 	anyDrop      bool
 	port         bool
+	synOnly      bool
 }
 
 func (h *flowsToWorldHandler) Init(registry *prometheus.Registry, options api.Options) error {
@@ -37,6 +38,8 @@ func (h *flowsToWorldHandler) Init(registry *prometheus.Registry, options api.Op
 			h.anyDrop = true
 		case "port":
 			h.port = true
+		case "syn-only":
+			h.synOnly = true
 		}
 	}
 	labels := []string{"protocol", "verdict"}
@@ -85,6 +88,12 @@ func (h *flowsToWorldHandler) ProcessFlow(_ context.Context, flow *flowpb.Flow) 
 	if flow.GetVerdict() != flowpb.Verdict_DROPPED && isReply {
 		return nil
 	}
+
+	// if "syn-only" option is set, only count non-reply SYN packets for TCP.
+	if h.synOnly && (!l4.GetTCP().GetFlags().GetSYN() || isReply) {
+		return nil
+	}
+
 	labelValues, err := h.context.GetLabelValues(flow)
 	if err != nil {
 		return err

pkg/hubble/metrics/flows-to-world/handler_test.go
@@ -195,3 +195,43 @@ hubble_flows_to_world_total{destination="cilium.io",port="80",protocol="TCP",sou
 `)
 	assert.NoError(t, testutil.CollectAndCompare(h.flowsToWorld, expected))
 }
+
+func TestFlowsToWorldHandler_SynOnly(t *testing.T) {
+	registry := prometheus.NewRegistry()
+	opts := api.Options{"sourceContext": "namespace", "destinationContext": "dns|ip", "syn-only": ""}
+	h := &flowsToWorldHandler{}
+	assert.NoError(t, h.Init(registry, opts))
+	assert.NoError(t, testutil.CollectAndCompare(h.flowsToWorld, strings.NewReader("")))
+	flow := flowpb.Flow{
+		Verdict:        flowpb.Verdict_DROPPED,
+		DropReasonDesc: flowpb.DropReason_POLICY_DENIED,
+		EventType:      &flowpb.CiliumEventType{Type: monitorAPI.MessageTypeDrop},
+		L4: &flowpb.Layer4{
+			Protocol: &flowpb.Layer4_TCP{
+				TCP: &flowpb.TCP{DestinationPort: 80, Flags: &flowpb.TCPFlags{SYN: true}},
+			},
+		},
+		Source: &flowpb.Endpoint{Namespace: "src-a"},
+		Destination: &flowpb.Endpoint{
+			Labels: []string{"reserved:world"},
+		},
+		DestinationNames: []string{"cilium.io"},
+		IsReply:          wrapperspb.Bool(false),
+	}
+	h.ProcessFlow(context.Background(), &flow)
+
+	// reply flows should not be counted
+	flow.IsReply = wrapperspb.Bool(true)
+	h.ProcessFlow(context.Background(), &flow)
+
+	// Non-SYN should not be counted
+	flow.IsReply = wrapperspb.Bool(false)
+	flow.L4.GetTCP().Flags = &flowpb.TCPFlags{ACK: true}
+	h.ProcessFlow(context.Background(), &flow)
+
+	expected := strings.NewReader(`# HELP hubble_flows_to_world_total Total number of flows to reserved:world
+# TYPE hubble_flows_to_world_total counter
+hubble_flows_to_world_total{destination="cilium.io",protocol="TCP",source="src-a",verdict="DROPPED"} 1
+`)
+	assert.NoError(t, testutil.CollectAndCompare(h.flowsToWorld, expected))
+}

pkg/hubble/metrics/flows-to-world/plugin.go
@@ -24,7 +24,8 @@ Options:
  any-drop - By default, this metric counts dropped flows if and only
             if the drop_reason is "Policy denied". Set this option to
             count any dropped flows to reserved:world.
- port     - Include destination port as a label.` +
+ port     - Include destination port as a label.
+ syn-only - Only count non-reply SYNs for TCP flows.` +
 		api.ContextOptionsHelp
 }
 

commit 2e4f48dacbbb599ac9811df279a8e8812cf1f52a

hubble: Fix the condition to apply for syn-only flag
It turned out that is_reply field is not set for dropped flows, so don't
ignore flows unless it's explicitly set to true.

Fixes: c65f4cf596ae ("hubble: Add "syn-only" option to flows-to-world metric")
Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

pkg/hubble/metrics/flows-to-world/handler.go
@@ -94,7 +94,7 @@ func (h *flowsToWorldHandler) ProcessFlow(_ context.Context, flow *flowpb.Flow) 
 		return nil
 	}
 	// if this is potentially a forwarded reply packet, ignore it to avoid collecting statistics about ephemeral ports
-	isReply := flow.GetIsReply() == nil || flow.GetIsReply().GetValue()
+	isReply := flow.GetIsReply() != nil && flow.GetIsReply().GetValue()
 	if flow.GetVerdict() != flowpb.Verdict_DROPPED && isReply {
 		return nil
 	}

pkg/hubble/metrics/flows-to-world/handler_test.go
@@ -220,6 +220,10 @@ func TestFlowsToWorldHandler_SynOnly(t *testing.T) {
 	}
 	h.ProcessFlow(context.Background(), &flow)
 
+	// flows without is_reply field should be counted.
+	flow.IsReply = nil
+	h.ProcessFlow(context.Background(), &flow)
+
 	// reply flows should not be counted
 	flow.IsReply = wrapperspb.Bool(true)
 	h.ProcessFlow(context.Background(), &flow)
@@ -231,7 +235,7 @@ func TestFlowsToWorldHandler_SynOnly(t *testing.T) {
 
 	expected := strings.NewReader(`# HELP hubble_flows_to_world_total Total number of flows to reserved:world
 # TYPE hubble_flows_to_world_total counter
-hubble_flows_to_world_total{destination="cilium.io",protocol="TCP",source="src-a",verdict="DROPPED"} 1
+hubble_flows_to_world_total{destination="cilium.io",protocol="TCP",source="src-a",verdict="DROPPED"} 2
 `)
 	assert.NoError(t, testutil.CollectAndCompare(h.flowsToWorld, expected))
 }