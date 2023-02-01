g0odi3z2j/cilium

commit 7330ea14a4aa4d5bc50b3868ebb67e281a7b2d98

clustermesh: Implement a basic connect-time validation
Implement a basic connect-time validation using CiliumClusterConfig.
clustermesh-apiserver is modified to set local CiliumClusterConfig on
start-up time and cilium-agent is modified to get CiliumClusterConfig
of remote clusters and validates it.

Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

pkg/clustermesh/remote_cluster.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/allocator"
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/kvstore"
@@ -29,6 +30,10 @@ import (
 type remoteCluster struct {
 	// name is the name of the cluster
 	name string
+
+	// clusterConfig is a configuration of the remote cluster taken
+	// from remote kvstore.
+	config *cmtypes.CiliumClusterConfig
 
 	// configPath is the path to the etcd configuration to be used to
 	// connect to the etcd cluster of the remote cluster
@@ -123,6 +128,8 @@ func (rc *remoteCluster) releaseOldConnection() {
 	backend := rc.backend
 	rc.backend = nil
 
+	rc.config = nil
+
 	rc.mesh.metricTotalNodes.WithLabelValues(rc.mesh.conf.Name, rc.mesh.conf.NodeName, rc.name).Set(0.0)
 	rc.mesh.metricReadinessStatus.WithLabelValues(rc.mesh.conf.Name, rc.mesh.conf.NodeName, rc.name).Set(metrics.BoolToFloat64(rc.isReadyLocked()))
 
@@ -177,6 +184,21 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 
 				rc.getLogger().Info("Connection to remote cluster established")
 
+				config, err := GetClusterConfig(rc.name, backend)
+				if err == nil && config == nil {
+					rc.getLogger().Warning("Remote cluster doesn't have cluster configuration, falling back to the old behavior. This is expected when connecting to the old cluster running Cilium without cluster configuration feature.")
+				} else if err == nil {
+					rc.getLogger().Info("Found remote cluster configuration")
+				} else {
+					rc.getLogger().WithError(err).Warning("Unable to get remote cluster configuration")
+					return err
+				}
+
+				if err := rc.mesh.canConnect(rc.name, config); err != nil {
+					rc.getLogger().WithError(err).Error("Unable to connect to the remote cluster")
+					return err
+				}
+
 				remoteNodes, err := store.JoinSharedStore(store.Configuration{
 					Prefix:                  path.Join(nodeStore.NodeStorePrefix, rc.name),
 					KeyCreator:              rc.mesh.conf.NodeKeyCreator,
@@ -226,6 +248,7 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 				rc.remoteNodes = remoteNodes
 				rc.remoteServices = remoteServices
 				rc.backend = backend
+				rc.config = config
 				rc.ipCacheWatcher = ipCacheWatcher
 				rc.remoteIdentityCache = remoteIdentityCache
 				rc.mesh.metricTotalNodes.WithLabelValues(rc.mesh.conf.Name, rc.mesh.conf.NodeName, rc.name).Set(float64(rc.remoteNodes.NumEntries()))

clustermesh-apiserver/main.go
@@ -29,6 +29,8 @@ import (
 	"k8s.io/client-go/tools/cache"
 
 	operatorWatchers "github.com/cilium/cilium/operator/watchers"
+	"github.com/cilium/cilium/pkg/clustermesh"
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/defaults"
 	"github.com/cilium/cilium/pkg/gops"
 	"github.com/cilium/cilium/pkg/hive"
@@ -588,6 +590,14 @@ func startServer(startCtx hive.HookContext, clientset k8sClient.Clientset, servi
 	var err error
 	if err = kvstore.Setup(context.Background(), "etcd", option.Config.KVStoreOpt, nil); err != nil {
 		log.WithError(err).Fatal("Unable to connect to etcd")
+	}
+
+	config := cmtypes.CiliumClusterConfig{
+		ID: clusterID,
+	}
+
+	if err := clustermesh.SetClusterConfig(cfg.clusterName, &config, kvstore.Client()); err != nil {
+		log.WithError(err).Fatal("Unable to set local cluster config on kvstore")
 	}
 
 	_, err = store.JoinSharedStore(store.Configuration{

pkg/clustermesh/clustermesh.go
@@ -106,6 +106,11 @@ func GetClusterConfig(clusterName string, backend kvstore.BackendOperations) (*c
 		return nil, err
 	}
 
+	// Cluster configuration missing, but it's not an error
+	if val == nil {
+		return nil, nil
+	}
+
 	if err := json.Unmarshal(val, &config); err != nil {
 		return nil, err
 	}
@@ -322,6 +327,32 @@ func (cm *ClusterMesh) NumReadyClusters() int {
 	}
 
 	return nready
+}
+
+func (cm *ClusterMesh) canConnect(name string, config *cmtypes.CiliumClusterConfig) error {
+	cm.mutex.RLock()
+	defer cm.mutex.RUnlock()
+
+	for n, rc := range cm.clusters {
+		if err := func() error {
+			rc.mutex.RLock()
+			defer rc.mutex.RUnlock()
+
+			if rc.name == name || !rc.isReadyLocked() || rc.config == nil {
+				return nil
+			}
+
+			if err := rc.config.IsCompatible(config); err != nil {
+				return err
+			}
+
+			return nil
+		}(); err != nil {
+			return fmt.Errorf("configuration of %s is not compatible with %s: %w", name, n, err)
+		}
+	}
+
+	return nil
 }
 
 // ClustersSynced returns after all clusters were synchronized with the bpf

pkg/clustermesh/clustermesh_test.go
@@ -16,6 +16,7 @@ import (
 
 	. "gopkg.in/check.v1"
 
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/ipcache"
@@ -103,6 +104,15 @@ func (s *ClusterMeshTestSuite) TestClusterMesh(c *C) {
 	defer os.RemoveAll(dir)
 
 	etcdConfig := []byte(fmt.Sprintf("endpoints:\n- %s\n", kvstore.EtcdDummyAddress()))
+
+	for i, name := range []string{"test2", "cluster1", "cluster2"} {
+		config := cmtypes.CiliumClusterConfig{
+			ID: uint32(i),
+		}
+
+		err = SetClusterConfig(name, &config, kvstore.Client())
+		c.Assert(err, IsNil)
+	}
 
 	config1 := path.Join(dir, "cluster1")
 	err = os.WriteFile(config1, etcdConfig, 0644)

pkg/clustermesh/services_test.go
@@ -55,6 +55,8 @@ func (s *ClusterMeshServicesTestSuite) SetUpTest(c *C) {
 	kvstore.SetupDummy("etcd")
 
 	s.randomName = rand.RandomString()
+	clusterName1 := s.randomName + "1"
+	clusterName2 := s.randomName + "2"
 
 	kvstore.Client().DeletePrefix(context.TODO(), "cilium/state/services/v1/"+s.randomName)
 	s.svcCache = k8s.NewServiceCache(fakeDatapath.NewNodeAddressing())
@@ -67,11 +69,19 @@ func (s *ClusterMeshServicesTestSuite) SetUpTest(c *C) {
 	s.testDir = dir
 	c.Assert(err, IsNil)
 
-	config1 := path.Join(dir, s.randomName+"1")
+	for i, cluster := range []string{clusterName1, clusterName2} {
+		config := cmtypes.CiliumClusterConfig{
+			ID: uint32(i),
+		}
+		err := SetClusterConfig(cluster, &config, kvstore.Client())
+		c.Assert(err, IsNil)
+	}
+
+	config1 := path.Join(dir, clusterName1)
 	err = os.WriteFile(config1, etcdConfig, 0644)
 	c.Assert(err, IsNil)
 
-	config2 := path.Join(dir, s.randomName+"2")
+	config2 := path.Join(dir, clusterName2)
 	err = os.WriteFile(config2, etcdConfig, 0644)
 	c.Assert(err, IsNil)
 

pkg/clustermesh/types/types.go
@@ -3,6 +3,10 @@
 
 package types
 
+import (
+	"fmt"
+)
+
 const (
 	// ClusterIDMin is the minimum value of the cluster ID
 	ClusterIDMin = 0
@@ -13,4 +17,26 @@ const (
 
 type CiliumClusterConfig struct {
 	ID uint32 `json:"id,omitempty"`
+}
+
+func (c0 *CiliumClusterConfig) IsCompatible(c1 *CiliumClusterConfig) error {
+	if c1 == nil {
+		// When remote cluster doesn't have cluster config, we
+		// currently just bypass the validation for compatibility.
+		// Otherwise, we cannot connect with older cluster which
+		// doesn't support cluster config feature.
+		//
+		// When we introduce a new cluster config can't be ignored,
+		// we should properly check it here and return error. Now
+		// we only have ClusterID which used to be ignored.
+		return nil
+	} else {
+		// Remote cluster has cluster config. Do validations.
+
+		// ID shouldn't be duplicated
+		if c0.ID == c1.ID {
+			return fmt.Errorf("duplicated cluster id")
+		}
+	}
+	return nil
 }

commit 666f08846f619d1381a80ddf94ebb0753c7acf34

clustermesh: close etcd connection on config retrieval error
Ensure that the backend connection to a remote etcd server in the
context of clustermesh gets properly closed when the connect-time
validation check fails due to an error, to prevent leaking the
connection itself and associated resources, such as the goroutine
performing the watch of 'cilium/.heartbeat'

Fixes: 7330ea14a4aa ("clustermesh: Implement a basic connect-time validation")

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/clustermesh/remote_cluster.go
@@ -191,11 +191,13 @@ func (rc *remoteCluster) restartRemoteConnection(allocator RemoteIdentityWatcher
 					rc.getLogger().Info("Found remote cluster configuration")
 				} else {
 					rc.getLogger().WithError(err).Warning("Unable to get remote cluster configuration")
+					backend.Close(ctx)
 					return err
 				}
 
 				if err := rc.mesh.canConnect(rc.name, config); err != nil {
 					rc.getLogger().WithError(err).Error("Unable to connect to the remote cluster")
+					backend.Close(ctx)
 					return err
 				}
 