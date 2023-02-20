g0odi3z2j/cilium

commit b5943c196b8e09d7e7c22461056663b6a72b2638

hive: Reimplement on top of dig
This reimplements hive directly on top of the uber/dig library. This
simplifies dealing with lifecycles and logging, and most importantly
allows implementing the configuration flag registration in a
straightforward way with an invoke.

The usage is also simplified as now there's only one library to
learn. The API is now very similar to uber/fx, but with the extra
Config() option.

To simplify use of hive.New the flags are now registered afterwards
with RegisterFlags(), and hive creates its own viper instance that
can be retrieved with Viper().

See pkg/hive/example/main.go for summary of the API changes.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hive/hive.go
@@ -5,22 +5,20 @@ package hive
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"path"
-	"reflect"
-	"runtime"
+	"os"
+	"os/signal"
 	"strings"
 	"time"
 
-	"github.com/mitchellh/mapstructure"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
-	"go.uber.org/fx"
-	"go.uber.org/fx/fxevent"
-	"go.uber.org/fx/fxtest"
+	"go.uber.org/dig"
+	"golang.org/x/sys/unix"
 
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/hive/internal"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 )
@@ -42,17 +40,22 @@ const (
 	defaultEnvPrefix = "CILIUM_"
 )
 
-// Hive is a modular application built from cells.
+// Hive is a framework building modular applications.
+//
+// It implements dependency injection using the dig library.
+//
+// See pkg/hive/example for a runnable example application.
 type Hive struct {
-	app                       *fx.App
-	cells                     []*Cell
-	dotGraph                  fx.DotGraph
+	container                 *dig.Container
+	cells                     []cell.Cell
+	shutdown                  chan error
 	envPrefix                 string
-	flags                     *pflag.FlagSet
-	fxLogger                  *logging.FxLogger
-	lifecycle                 lifecycleProxy
 	startTimeout, stopTimeout time.Duration
+	flags                     *pflag.FlagSet
 	viper                     *viper.Viper
+	lifecycle                 *DefaultLifecycle
+	populated                 bool
+	invokes                   []func() error
 }
 
 // New returns a new hive that can be run, or inspected.
@@ -60,27 +63,89 @@ type Hive struct {
 //
 // The object graph is not constructed until methods of the hive are
 // invoked.
-func New(v *viper.Viper, flags *pflag.FlagSet, cells ...*Cell) *Hive {
+//
+// Applications should call RegisterFlags() to register the hive's command-line
+// flags. Likewise if configuration settings come from configuration files, then
+// the Viper() method can be used to populate the hive's viper instance.
+func New(cells ...cell.Cell) *Hive {
 	h := &Hive{
+		container:    dig.New(),
 		envPrefix:    defaultEnvPrefix,
-		fxLogger:     logging.NewFxLogger(log),
 		cells:        cells,
-		viper:        v,
-		flags:        pflag.NewFlagSet("", pflag.ContinueOnError),
+		viper:        viper.New(),
 		startTimeout: defaultStartTimeout,
 		stopTimeout:  defaultStopTimeout,
+		flags:        pflag.NewFlagSet("", pflag.ContinueOnError),
+		lifecycle:    &DefaultLifecycle{},
+		shutdown:     make(chan error, 1),
 	}
-	if err := h.registerFlags(flags); err != nil {
-		log.Fatal(err)
+
+	if err := h.provideDefaults(); err != nil {
+		log.WithError(err).Fatal("Failed to provide default objects")
 	}
+
+	// Apply all cells to the container. This registers all constructors
+	// and adds all config flags. Invokes are delayed until Start() is
+	// called.
+	for _, cell := range cells {
+		if err := cell.Apply(h.container); err != nil {
+			log.WithError(err).Fatal("Failed to apply cell")
+		}
+	}
+
+	// Bind the newly registered flags to viper.
+	h.flags.VisitAll(func(f *pflag.Flag) {
+		if err := h.viper.BindPFlag(f.Name, f); err != nil {
+			log.Fatalf("BindPFlag: %s", err)
+		}
+		if err := h.viper.BindEnv(f.Name, h.getEnvName(f.Name)); err != nil {
+			log.Fatalf("BindEnv: %s", err)
+		}
+	})
+
 	return h
 }
 
-// NewForTests returns a new hive for testing that does not register
-// the flags declared by the cells. Use in combination with TestApp()
-// to provide cell configurations.
-func NewForTests(cells ...*Cell) *Hive {
-	return New(viper.New(), pflag.NewFlagSet("", pflag.ContinueOnError), cells...)
+// RegisterFlags adds all flags in the hive to the given flag set.
+// Fatals if a flag already exists in the given flag set.
+// Use with e.g. cobra.Command:
+//
+//	cmd := &cobra.Command{...}
+//	h.RegisterFlags(cmd.Flags())
+func (h *Hive) RegisterFlags(flags *pflag.FlagSet) {
+	h.flags.VisitAll(func(f *pflag.Flag) {
+		if flags.Lookup(f.Name) != nil {
+			log.Fatalf("Error registering flag: '%s' already registered", f.Name)
+		}
+		flags.AddFlag(f)
+	})
+}
+
+// Viper returns the hive's viper instance.
+func (h *Hive) Viper() *viper.Viper {
+	return h.viper
+}
+
+type defaults struct {
+	dig.Out
+
+	Flags       *pflag.FlagSet
+	Lifecycle   Lifecycle
+	Logger      logrus.FieldLogger
+	Shutdowner  Shutdowner
+	InvokerList cell.InvokerList
+}
+
+func (h *Hive) provideDefaults() error {
+	return h.container.Provide(func() defaults {
+		return defaults{
+			Flags:       h.flags,
+			Lifecycle:   h.lifecycle,
+			Logger:      log,
+			Shutdowner:  h,
+			InvokerList: h,
+		}
+	})
 }
 
 func (h *Hive) SetTimeouts(start, stop time.Duration) {
@@ -92,46 +157,141 @@ func (h *Hive) SetEnvPrefix(prefix string) {
 }
 
 // Run populates the cell configurations and runs the hive cells.
-// If an error occurs when populating cell configurations this
-// method panics.
-// Interrupt signal will cause the hive to stop.
-func (h *Hive) Run() {
-	if h.app == nil {
-		if err := h.createApp(); err != nil {
-			log.Fatalf("Run failed: %s", err)
-		}
+// Interrupt signal or call to Shutdowner.Shutdown() will cause the hive to stop.
+func (h *Hive) Run() error {
+	startCtx, cancel := context.WithTimeout(context.Background(), h.startTimeout)
+	defer cancel()
+
+	if err := h.Start(startCtx); err != nil {
+		return fmt.Errorf("failed to start: %w", err)
 	}
-	h.app.Run()
+
+	shutdownErr := h.waitForSignalOrShutdown()
+
+	stopCtx, cancel := context.WithTimeout(context.Background(), h.stopTimeout)
+	defer cancel()
+
+	if err := h.Stop(stopCtx); err != nil {
+		return fmt.Errorf("failed to stop: %w", err)
+	}
+
+	return shutdownErr
 }
 
+func (h *Hive) waitForSignalOrShutdown() error {
+	signals := make(chan os.Signal, 1)
+	defer signal.Stop(signals)
+	signal.Notify(signals, os.Interrupt, unix.SIGINT, unix.SIGTERM)
+	select {
+	case <-signals:
+		log.Error("Interrupt received")
+		return nil
+	case err := <-h.shutdown:
+		return err
+	}
+}
+
+func (h *Hive) populate() error {
+	if h.populated {
+		return nil
+	}
+	h.populated = true
+
+	// Provide all the parsed settings to the config cells.
+	err := h.container.Provide(
+		func() cell.AllSettings {
+			return cell.AllSettings(h.viper.AllSettings())
+		})
+	if err != nil {
+		return err
+	}
+
+	// Execute the invoke functions to construct the objects.
+	for _, invoke := range h.invokes {
+		if err := invoke(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (h *Hive) AppendInvoke(invoke func() error) {
+	h.invokes = append(h.invokes, invoke)
+}
+
+// Start starts the hive. The context allows cancelling the start.
+// If context is cancelled and the start hooks do not respect the cancellation
+// then after 5 more seconds the process will be terminated forcefully.
 func (h *Hive) Start(ctx context.Context) error {
-	if h.app == nil {
-		if err := h.createApp(); err != nil {
-			log.Fatalf("Start failed: %s", err)
-		}
+	if err := h.populate(); err != nil {
+		return err
 	}
-	return h.app.Start(ctx)
+
+	defer close(h.fatalOnTimeout(ctx))
+
+	return h.lifecycle.Start(ctx)
 }
 
+// Stop stops the hive. The context allows cancelling the stop.
+// If context is cancelled and the stop hooks do not respect the cancellation
+// then after 5 more seconds the process will be terminated forcefully.
 func (h *Hive) Stop(ctx context.Context) error {
-	if h.app == nil {
-		return errors.New("Hive was not started")
+	defer close(h.fatalOnTimeout(ctx))
+	return h.lifecycle.Stop(ctx)
+}
+
+func (h *Hive) fatalOnTimeout(ctx context.Context) chan struct{} {
+	terminated := make(chan struct{}, 1)
+	go func() {
+		select {
+		case <-terminated:
+			// Start/stop terminated in time, nothing to do.
+			return
+
+		case <-ctx.Done():
+		}
+
+		// Context was cancelled. Give 5 more seconds and then
+		// go fatal.
+		time.Sleep(5 * time.Second)
+
+		select {
+		case <-terminated:
+		default:
+			log.Fatal("Start or stop failed to finish on time, aborting forcefully.")
+		}
+	}()
+	return terminated
+}
+
+// Shutdown implements the Shutdowner interface and is provided
+// for the cells to use for triggering a early shutdown.
+func (h *Hive) Shutdown(opts ...ShutdownOption) {
+	var o shutdownOptions
+	for _, opt := range opts {
+		opt.apply(&o)
 	}
-	return h.app.Stop(ctx)
+	h.shutdown <- o.err
+	close(h.shutdown)
 }
 
 func (h *Hive) PrintObjects() {
-	if err := h.createApp(); err != nil {
-		log.Fatal(err)
+	fmt.Printf("Cells:\n\n")
+	for _, c := range h.cells {
+		c.Info().Print(2, os.Stdout)
+		fmt.Println()
 	}
-	h.fxLogger.PrintObjects()
+
+	if err := h.populate(); err != nil {
+		log.WithError(err).Fatal("Failed to populate object graph")
+	}
 
 	fmt.Printf("Start hooks:\n\n")
 	for _, hook := range h.lifecycle.hooks {
 		if hook.OnStart == nil {
 			continue
 		}
-		fmt.Printf("  • %s\n", funcNameAndLocation(hook.OnStart))
+		fmt.Printf("  • %s\n", internal.FuncNameAndLocation(hook.OnStart))
 	}
 
 	fmt.Printf("\nStop hooks:\n\n")
@@ -140,25 +300,18 @@ func (h *Hive) PrintObjects() {
 		if hook.OnStop == nil {
 			continue
 		}
-		fmt.Printf("  • %s\n", funcNameAndLocation(hook.OnStop))
-	}
-
-	fmt.Printf("\nConfigurations:\n\n")
-	allSettings := h.viper.AllSettings()
-	for _, cell := range h.cells {
-		if cell.newConfig != nil {
-			cfg := cell.newConfig()
-			h.unmarshalConfig(allSettings, cell, &cfg)
-			fmt.Printf("  ⚙ %s: %#v\n", cell.name, cfg)
-		}
+		fmt.Printf("  • %s\n", internal.FuncNameAndLocation(hook.OnStop))
 	}
 }
 
 func (h *Hive) PrintDotGraph() {
-	if err := h.createApp(); err != nil {
-		log.Fatal(err)
+	if err := h.populate(); err != nil {
+		log.WithError(err).Fatal("Failed to populate object graph")
 	}
-	fmt.Print(h.dotGraph)
+
+	if err := dig.Visualize(h.container, os.Stdout); err != nil {
+		log.WithError(err).Fatal("Failed to Visualize()")
+	}
 }
 
 // getEnvName returns the environment variable to be used for the given option name.
@@ -166,183 +319,4 @@ func (h *Hive) getEnvName(option string) string {
 	under := strings.Replace(option, "-", "_", -1)
 	upper := strings.ToUpper(under)
 	return h.envPrefix + upper
-}
-
-// registerCells registers the command-line flags from all the cells.
-func (h *Hive) registerFlags(parent *pflag.FlagSet) error {
-	for _, cell := range h.cells {
-		flags := pflag.NewFlagSet("", pflag.ContinueOnError)
-		cell.registerFlags(flags)
-		h.flags.AddFlagSet(flags)
-		flags.VisitAll(func(f *pflag.Flag) {
-			cell.flags = append(cell.flags, f.Name)
-		})
-	}
-	var err error
-	h.flags.VisitAll(func(f *pflag.Flag) {
-		if err != nil {
-			return
-		}
-		if parent.Lookup(f.Name) != nil {
-			err = fmt.Errorf("error registering flags: '%s' already registered", f.Name)
-		} else {
-			viper.BindEnv(f.Name, h.getEnvName(f.Name))
-			parent.AddFlag(f)
-		}
-	})
-	if err != nil {
-		return err
-	}
-	return h.viper.BindPFlags(h.flags)
-}
-
-// populate creates the cell configurations from viper and returns the combined
-// fx option for all cells and their configurations.
-func (h *Hive) populate(overrides ...any) (fx.Option, error) {
-	allSettings := h.viper.AllSettings()
-
-	overridesMap := map[reflect.Type]any{}
-	for _, cfg := range overrides {
-		overridesMap[reflect.TypeOf(cfg)] = cfg
-	}
-
-	allOpts := []fx.Option{}
-	for _, cell := range h.cells {
-		cell := cell
-
-		cellOpts := cell.opts
-		if cell.newConfig != nil {
-			cfg := cell.newConfig()
-
-			if override, ok := overridesMap[reflect.TypeOf(cfg)]; ok {
-				cfg = override
-			} else {
-				if err := h.unmarshalConfig(allSettings, cell, &cfg); err != nil {
-					return nil, err
-				}
-			}
-			cellOpts = append(cellOpts, fx.Supply(cfg))
-		}
-
-		var cellOpt fx.Option
-		if cell.name != "" {
-			// Provide a logger to the cell that has subsys set to the cell name.
-			cellLogger := fx.Decorate(
-				func(log logrus.FieldLogger) logrus.FieldLogger {
-					return log.WithField(logfields.LogSubsys, cell.name)
-				})
-			cellOpts = append(cellOpts, cellLogger)
-			cellOpt = fx.Module(cell.name, cellOpts...)
-		} else {
-			cellOpt = fx.Options(cellOpts...)
-		}
-		allOpts = append(allOpts, cellOpt)
-	}
-	return fx.Options(allOpts...), nil
-}
-
-// createApp creates the fx application, unless already created.
-func (h *Hive) createApp() error {
-	if h.app != nil {
-		return nil
-	}
-	opts, err := h.populate()
-	if err != nil {
-		return err
-	}
-	h.app = fx.New(
-		fx.WithLogger(func() fxevent.Logger { return h.fxLogger }),
-		fx.Supply(fx.Annotate(log, fx.As(new(logrus.FieldLogger)))),
-		fx.StartTimeout(h.startTimeout),
-		fx.StopTimeout(h.stopTimeout),
-		fx.Populate(&h.dotGraph),
-		fx.Decorate(func(parent fx.Lifecycle) fx.Lifecycle {
-			h.lifecycle.parent = parent
-			return &h.lifecycle
-		}),
-		opts,
-	)
-	return h.app.Err()
-}
-
-// TestApp constructs a test application for the hive with given configuration
-// overrides.
-func (h *Hive) TestApp(tb fxtest.TB, configs ...any) (*fxtest.App, error) {
-	opts, err := h.populate(configs...)
-	if err != nil {
-		return nil, err
-	}
-	return fxtest.New(tb,
-		fx.Supply(fx.Annotate(log, fx.As(new(logrus.FieldLogger)))),
-		opts), nil
-}
-
-func (h *Hive) unmarshalConfig(allSettings map[string]any, cell *Cell, target *any) error {
-	decoder, err := mapstructure.NewDecoder(decoderConfig(target))
-	if err != nil {
-		return fmt.Errorf("failed to create config decoder: %w", err)
-	}
-
-	// As input, only consider the flags declared by CellFlags.
-	input := make(map[string]any)
-	for _, flag := range cell.flags {
-		if v, ok := allSettings[flag]; ok {
-			input[flag] = v
-		} else {
-			return fmt.Errorf("internal error: cell flag %s not found from settings", flag)
-		}
-	}
-
-	if err := decoder.Decode(input); err != nil {
-		return fmt.Errorf("failed to unmarshal config struct %T: %w.\n"+
-			"Hint: field 'FooBar' matches flag 'foo-bar', or use tag `mapstructure:\"flag-name\"` to match field with flag",
-			*target, err)
-	}
-	return nil
-}
-
-func decoderConfig(target *any) *mapstructure.DecoderConfig {
-	return &mapstructure.DecoderConfig{
-		Metadata:         nil,
-		Result:           target,
-		WeaklyTypedInput: true,
-		DecodeHook: mapstructure.ComposeDecodeHookFunc(
-			mapstructure.StringToTimeDurationHookFunc(),
-			mapstructure.StringToSliceHookFunc(","),
-		),
-		ZeroFields: true,
-		// Error out if the config struct has fields that are
-		// not found from input.
-		ErrorUnset: true,
-		// Error out also if settings from input are not used.
-		ErrorUnused: true,
-		// Match field FooBarBaz with "foo-bar-baz" by removing
-		// the dashes from the flag.
-		MatchName: func(mapKey, fieldName string) bool {
-			return strings.EqualFold(
-				strings.ReplaceAll(mapKey, "-", ""),
-				fieldName)
-		},
-	}
-
-}
-
-// lifecycleProxy collects the appended hooks so they can be shown
-// in PrintObjects() as fx doesn't provide access to the hooks.
-type lifecycleProxy struct {
-	parent fx.Lifecycle
-	hooks  []fx.Hook
-}
-
-func (p *lifecycleProxy) Append(hook fx.Hook) {
-	p.hooks = append(p.hooks, hook)
-	p.parent.Append(hook)
-}
-
-var _ fx.Lifecycle = &lifecycleProxy{}
-
-func funcNameAndLocation(fn any) string {
-	f := runtime.FuncForPC(reflect.ValueOf(fn).Pointer())
-	file, line := f.FileLine(f.Entry())
-	return fmt.Sprintf("%s (.../%s/%s:%d)", f.Name(), path.Base(path.Dir(file)), path.Base(file), line)
 }

commit 885734d6f1fd4e2899ee67a6bc1b81c93541dba3

hive: Don't log interrupt signal as error
Hive can be stopped by sending the process a INT or TERM signal to
initiate the stop of the application. Hive was logging an error
"Interrupt received" when this happened and this was causing CI
jobs to fail as they were checking that no errors were logged.
Since stopping via SIGINT/SIGTERM is not errornous, log the event
at info level instead.

Fixes: #23637
Fixes: b5943c196b8e ("hive: Reimplement on top of dig")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hive/hive.go
@@ -190,8 +190,8 @@ func (h *Hive) waitForSignalOrShutdown() error {
 	defer signal.Stop(signals)
 	signal.Notify(signals, os.Interrupt, unix.SIGINT, unix.SIGTERM)
 	select {
-	case <-signals:
-		log.Error("Interrupt received")
+	case sig := <-signals:
+		log.WithField("signal", sig).Info("Signal received")
 		return nil
 	case err := <-h.shutdown:
 		return err
@@ -238,6 +238,8 @@ func (h *Hive) Start(ctx context.Context) error {
 
 	defer close(h.fatalOnTimeout(ctx))
 
+	log.Info("Starting")
+
 	return h.lifecycle.Start(ctx)
 }
 
@@ -246,6 +248,7 @@ func (h *Hive) Start(ctx context.Context) error {
 // then after 5 more seconds the process will be terminated forcefully.
 func (h *Hive) Stop(ctx context.Context) error {
 	defer close(h.fatalOnTimeout(ctx))
+	log.Info("Stopping")
 	return h.lifecycle.Stop(ctx)
 }
 