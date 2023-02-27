g0odi3z2j/cilium

commit f5c4842822a97edead47b2fabf8b65c6a7344664

k8s: Multi cluster service discovery support
This makes use of the already introduced cilium operator to synchronize
Kubernetes services into the kvstore and maintains a shared store to
synchronize remote services of all connected cluster into individual agents.

Upon presence of the annotation io.cilium/global-service=true, a Kubernetes
service will be completed with remote endpoints which match the namespace and
service name.

The ClusterIP of the remote services is ignored. The selector is only
implemented in the local cluster and the already expanded list of endpoints
(backend IPs and ports) is synchronized. The selector and labels are
synchronized into the kvstore for troubleshooting.

Signed-off-by: Thomas Graf <thomas@cilium.io>

pkg/clustermesh/services.go
@@ -1,0 +1,127 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clustermesh
+
+import (
+	"github.com/cilium/cilium/pkg/kvstore/store"
+	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/service"
+)
+
+// ServiceMerger is the interface to be implemented by the owner of local
+// services. The functions have to merge service updates and deletions with
+// local services to provide a shared view.
+type ServiceMerger interface {
+	MergeExternalServiceUpdate(service *service.ClusterService)
+	MergeExternalServiceDelete(service *service.ClusterService)
+}
+
+type globalService struct {
+	clusterServices map[string]*service.ClusterService
+}
+
+func newGlobalService() *globalService {
+	return &globalService{
+		clusterServices: map[string]*service.ClusterService{},
+	}
+}
+
+type globalServiceCache struct {
+	mutex  lock.RWMutex
+	byName map[string]*globalService
+}
+
+func newGlobalServiceCache() *globalServiceCache {
+	return &globalServiceCache{
+		byName: map[string]*globalService{},
+	}
+}
+
+func (c *globalServiceCache) onUpdate(svc *service.ClusterService) {
+	c.mutex.Lock()
+
+	// Validate that the global service is known
+	globalSvc, ok := c.byName[svc.NamespaceServiceName()]
+	if !ok {
+		globalSvc = newGlobalService()
+		c.byName[svc.NamespaceServiceName()] = globalSvc
+	}
+
+	globalSvc.clusterServices[svc.Cluster] = svc
+	c.mutex.Unlock()
+}
+
+// must be called with c.mutex held
+func (c *globalServiceCache) delete(globalService *globalService, clusterName, serviceName string) {
+	if _, ok := globalService.clusterServices[clusterName]; !ok {
+		return
+	}
+
+	delete(globalService.clusterServices, clusterName)
+
+	// After the last cluster service is removed, remove the
+	// global service
+	if len(globalService.clusterServices) == 0 {
+		delete(c.byName, serviceName)
+	}
+}
+
+func (c *globalServiceCache) onDelete(svc *service.ClusterService) {
+	c.mutex.Lock()
+	if globalService, ok := c.byName[svc.NamespaceServiceName()]; ok {
+		c.delete(globalService, svc.NamespaceServiceName(), svc.Cluster)
+	}
+	c.mutex.Unlock()
+}
+
+func (c *globalServiceCache) onClusterDelete(clusterName string) {
+	c.mutex.Lock()
+	for serviceName, globalService := range c.byName {
+		c.delete(globalService, serviceName, clusterName)
+	}
+	c.mutex.Unlock()
+}
+
+type remoteServiceObserver struct {
+	remoteCluster *remoteCluster
+}
+
+// OnUpdate is called when a service in a remote cluster is updated
+func (r *remoteServiceObserver) OnUpdate(key store.Key) {
+	if svc, ok := key.(*service.ClusterService); ok {
+		mesh := r.remoteCluster.mesh
+		mesh.globalServices.onUpdate(svc)
+
+		if merger := mesh.conf.ServiceMerger; merger != nil {
+			merger.MergeExternalServiceUpdate(svc)
+		}
+	} else {
+		log.Warningf("Received unexpected remote service update object %+v", key)
+	}
+}
+
+// OnDelete is called when a service in a remote cluster is deleted
+func (r *remoteServiceObserver) OnDelete(key store.Key) {
+	if svc, ok := key.(*service.ClusterService); ok {
+		mesh := r.remoteCluster.mesh
+		mesh.globalServices.onDelete(svc)
+
+		if merger := mesh.conf.ServiceMerger; merger != nil {
+			merger.MergeExternalServiceDelete(svc)
+		}
+	} else {
+		log.Warningf("Received unexpected remote service delete object %+v", key)
+	}
+}

pkg/clustermesh/services_test.go
@@ -1,0 +1,261 @@
+// Copyright 2018 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !privileged_tests
+
+package clustermesh
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"time"
+
+	"github.com/cilium/cilium/pkg/identity/cache"
+	"github.com/cilium/cilium/pkg/k8s"
+	"github.com/cilium/cilium/pkg/kvstore"
+	"github.com/cilium/cilium/pkg/loadbalancer"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/testutils"
+
+	"github.com/sirupsen/logrus"
+	. "gopkg.in/check.v1"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var etcdConfig = []byte(fmt.Sprintf("endpoints:\n- %s\n", kvstore.EtcdDummyAddress()))
+
+func (s *ClusterMeshServicesTestSuite) prepareServiceUpdate(clusterSuffix, backendIP, portName, port string) (string, []byte) {
+	return "cilium/state/services/v1/" + s.randomName + clusterSuffix + "/default/foo",
+		[]byte(`{"cluster":"` + s.randomName + clusterSuffix + `","namespace":"default","name":"foo","frontends":{"172.20.0.177":{"port":{"protocol":"TCP","port":80}}},"backends":{"` + backendIP + `":{"` + portName + `":{"protocol":"TCP","port":` + port + `}}},"labels":{},"selector":{"name":"foo"}}`)
+
+}
+
+type ClusterMeshServicesTestSuite struct {
+	svcCache   k8s.ServiceCache
+	testDir    string
+	mesh       *ClusterMesh
+	randomName string
+}
+
+var (
+	_ = Suite(&ClusterMeshServicesTestSuite{})
+)
+
+func (s *ClusterMeshServicesTestSuite) SetUpTest(c *C) {
+	kvstore.SetupDummy("etcd")
+
+	s.randomName = testutils.RandomRune()
+
+	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName)
+	s.svcCache = k8s.NewServiceCache()
+	logging.DefaultLogger.SetLevel(logrus.DebugLevel)
+	cache.InitIdentityAllocator(&identityAllocatorOwnerMock{})
+	dir, err := ioutil.TempDir("", "multicluster")
+	s.testDir = dir
+	c.Assert(err, IsNil)
+
+	config1 := path.Join(dir, s.randomName+"1")
+	err = ioutil.WriteFile(config1, etcdConfig, 0644)
+	c.Assert(err, IsNil)
+
+	config2 := path.Join(dir, s.randomName+"2")
+	err = ioutil.WriteFile(config2, etcdConfig, 0644)
+	c.Assert(err, IsNil)
+
+	cm, err := NewClusterMesh(Configuration{
+		Name:            "test2",
+		ConfigDirectory: dir,
+		NodeKeyCreator:  testNodeCreator,
+		nodeObserver:    &testObserver{},
+		ServiceMerger:   &s.svcCache,
+	})
+	c.Assert(err, IsNil)
+	c.Assert(cm, Not(IsNil))
+
+	s.mesh = cm
+
+	// wait for both clusters to appear in the list of cm clusters
+	c.Assert(testutils.WaitUntil(func() bool {
+		return cm.NumReadyClusters() == 2
+	}, 10*time.Second), IsNil)
+}
+
+func (s *ClusterMeshServicesTestSuite) TearDownTest(c *C) {
+	if s.mesh != nil {
+		s.mesh.Close()
+	}
+
+	cache.Close()
+
+	os.RemoveAll(s.testDir)
+	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName)
+	kvstore.Close()
+}
+
+func (s *ClusterMeshServicesTestSuite) expectEvent(c *C, action k8s.CacheAction, id k8s.ServiceID, fn func(event k8s.ServiceEvent) bool) {
+	c.Assert(testutils.WaitUntil(func() bool {
+		var event k8s.ServiceEvent
+		select {
+		case event = <-s.svcCache.Events:
+		case <-time.After(time.Second * 10):
+			c.Errorf("Timeout while waiting for event to be received")
+			return false
+		}
+
+		c.Assert(event.Action, Equals, action)
+		c.Assert(event.ID, Equals, id)
+
+		if fn != nil {
+			return fn(event)
+		}
+
+		return true
+	}, 2*time.Second), IsNil)
+}
+
+func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesGlobal(c *C) {
+	kvstore.Set(s.prepareServiceUpdate("1", "10.0.185.196", "http", "80"))
+	kvstore.Set(s.prepareServiceUpdate("2", "20.0.185.196", "http2", "90"))
+
+	k8sSvc := &v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+			Annotations: map[string]string{
+				"io.cilium/global-service": "true",
+			},
+		},
+		Spec: v1.ServiceSpec{
+			ClusterIP: "127.0.0.1",
+			Type:      v1.ServiceTypeClusterIP,
+		},
+	}
+
+	svcID := s.svcCache.UpdateService(k8sSvc)
+
+	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
+		return event.Endpoints.Backends["10.0.185.196"] != nil &&
+			event.Endpoints.Backends["20.0.185.196"] != nil
+	})
+
+	k8sEndpoints := &v1.Endpoints{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Subsets: []v1.EndpointSubset{
+			{
+				Addresses: []v1.EndpointAddress{{IP: "30.0.185.196"}},
+				Ports: []v1.EndpointPort{
+					{
+						Name:     "http",
+						Port:     100,
+						Protocol: v1.ProtocolTCP,
+					},
+				},
+			},
+		},
+	}
+
+	s.svcCache.UpdateEndpoints(k8sEndpoints)
+	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
+		return event.Endpoints.Backends["30.0.185.196"] != nil
+	})
+
+	s.svcCache.DeleteEndpoints(k8sEndpoints)
+	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
+		return event.Endpoints.Backends["30.0.185.196"] == nil
+	})
+
+	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName + "1")
+	s.expectEvent(c, k8s.UpdateService, svcID, nil)
+
+	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName + "2")
+	s.expectEvent(c, k8s.DeleteService, svcID, nil)
+}
+
+func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesUpdate(c *C) {
+	kvstore.Set(s.prepareServiceUpdate("1", "10.0.185.196", "http", "80"))
+	kvstore.Set(s.prepareServiceUpdate("2", "20.0.185.196", "http2", "90"))
+
+	k8sSvc := &v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+			Annotations: map[string]string{
+				"io.cilium/global-service": "true",
+			},
+		},
+		Spec: v1.ServiceSpec{
+			ClusterIP: "127.0.0.1",
+			Type:      v1.ServiceTypeClusterIP,
+		},
+	}
+
+	svcID := s.svcCache.UpdateService(k8sSvc)
+
+	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
+		return event.Endpoints.Backends["10.0.185.196"]["http"].Equals(
+			loadbalancer.NewL4Addr(loadbalancer.TCP, 80)) &&
+			event.Endpoints.Backends["20.0.185.196"]["http2"].Equals(
+				loadbalancer.NewL4Addr(loadbalancer.TCP, 90))
+	})
+
+	kvstore.Set(s.prepareServiceUpdate("1", "80.0.185.196", "http", "8080"))
+	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
+		return event.Endpoints.Backends["80.0.185.196"] != nil &&
+			event.Endpoints.Backends["20.0.185.196"] != nil
+	})
+
+	kvstore.Set(s.prepareServiceUpdate("2", "90.0.185.196", "http", "8080"))
+	s.expectEvent(c, k8s.UpdateService, svcID, func(event k8s.ServiceEvent) bool {
+		return event.Endpoints.Backends["80.0.185.196"] != nil &&
+			event.Endpoints.Backends["90.0.185.196"] != nil
+	})
+
+	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName + "1")
+	s.expectEvent(c, k8s.UpdateService, svcID, nil)
+
+	kvstore.DeletePrefix("cilium/state/services/v1/" + s.randomName + "2")
+	s.expectEvent(c, k8s.DeleteService, svcID, nil)
+}
+
+func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesNonGlobal(c *C) {
+	kvstore.Set(s.prepareServiceUpdate("1", "10.0.185.196", "http", "80"))
+	kvstore.Set(s.prepareServiceUpdate("2", "20.0.185.196", "http2", "90"))
+
+	k8sSvc := &v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+			// shared annotation is NOT set
+		},
+		Spec: v1.ServiceSpec{
+			ClusterIP: "127.0.0.1",
+			Type:      v1.ServiceTypeClusterIP,
+		},
+	}
+
+	s.svcCache.UpdateService(k8sSvc)
+
+	time.Sleep(100 * time.Millisecond)
+	select {
+	case event := <-s.svcCache.Events:
+		c.Errorf("Unexpected service event received: %+v", event)
+	default:
+	}
+}

daemon/daemon.go
@@ -1016,6 +1016,7 @@ func NewDaemon() (*Daemon, *endpointRestoreState, error) {
 				Name:            "clustermesh",
 				ConfigDirectory: path,
 				NodeKeyCreator:  nodeStore.KeyCreator,
+				ServiceMerger:   &d.k8sSvcCache,
 			})
 			if err != nil {
 				log.WithError(err).Fatal("Unable to initialize ClusterMesh")

pkg/annotation/k8s.go
@@ -15,26 +15,33 @@
 package annotation
 
 const (
+	// Prefix is the common prefix for all annotations
+	Prefix = "io.cilium"
+
 	// Name is an optional annotation to the NetworkPolicy
 	// resource which specifies the name of the policy node to which all
 	// rules should be applied to.
-	Name = "io.cilium.name"
+	Name = Prefix + ".name"
 
 	// V4CIDRName is the annotation name used to store the IPv4
 	// pod CIDR in the node's annotations.
-	V4CIDRName = "io.cilium.network.ipv4-pod-cidr"
+	V4CIDRName = Prefix + ".network.ipv4-pod-cidr"
 	// V6CIDRName is the annotation name used to store the IPv6
 	// pod CIDR in the node's annotations.
-	V6CIDRName = "io.cilium.network.ipv6-pod-cidr"
+	V6CIDRName = Prefix + ".network.ipv6-pod-cidr"
 
 	// V4HealthName is the annotation name used to store the IPv4
 	// address of the cilium-health endpoint in the node's annotations.
-	V4HealthName = "io.cilium.network.ipv4-health-ip"
+	V4HealthName = Prefix + ".network.ipv4-health-ip"
 	// V6HealthName is the annotation name used to store the IPv6
 	// address of the cilium-health endpoint in the node's annotations.
-	V6HealthName = "io.cilium.network.ipv6-health-ip"
+	V6HealthName = Prefix + ".network.ipv6-health-ip"
 
 	// CiliumHostIP is the annotation name used to store the IPv4 address
 	// of the cilium host interface in the node's annotations.
-	CiliumHostIP = "io.cilium.network.ipv4-cilium-host"
+	CiliumHostIP = Prefix + ".network.ipv4-cilium-host"
+
+	// GlobalService if set to true, marks a service to become a global
+	// service
+	GlobalService = Prefix + "/global-service"
 )

pkg/clustermesh/clustermesh.go
@@ -20,6 +20,7 @@ import (
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	"github.com/cilium/cilium/pkg/lock"
+	nodeStore "github.com/cilium/cilium/pkg/node/store"
 	"github.com/cilium/cilium/pkg/option"
 )
 
@@ -44,7 +45,20 @@ type Configuration struct {
 	// nodes are being discovered in remote clusters
 	NodeKeyCreator store.KeyCreator
 
-	observer store.Observer
+	// ServiceMerger is the interface responsible to merge service and
+	// endpoints into an existing cache
+	ServiceMerger ServiceMerger
+
+	nodeObserver store.Observer
+}
+
+// NodeObserver returns the node store observer of the configuration
+func (c *Configuration) NodeObserver() store.Observer {
+	if c.nodeObserver != nil {
+		return c.nodeObserver
+	}
+
+	return &nodeStore.NodeObserver{}
 }
 
 // ClusterMesh is a cache of multiple remote clusters
@@ -56,15 +70,20 @@ type ClusterMesh struct {
 	clusters      map[string]*remoteCluster
 	controllers   *controller.Manager
 	configWatcher *configDirectoryWatcher
+
+	// globalServices is a list of all global services. The datastructure
+	// is protected by its own mutex inside of the structure.
+	globalServices *globalServiceCache
 }
 
 // NewClusterMesh creates a new remote cluster cache based on the
 // provided configuration
 func NewClusterMesh(c Configuration) (*ClusterMesh, error) {
 	cm := &ClusterMesh{
-		conf:        c,
-		clusters:    map[string]*remoteCluster{},
-		controllers: controller.NewManager(),
+		conf:           c,
+		clusters:       map[string]*remoteCluster{},
+		controllers:    controller.NewManager(),
+		globalServices: newGlobalServiceCache(),
 	}
 
 	w, err := createConfigDirectoryWatcher(c.ConfigDirectory, cm)
@@ -140,6 +159,8 @@ func (cm *ClusterMesh) remove(name string) {
 	if cluster, ok := cm.clusters[name]; ok {
 		cluster.onRemove()
 		delete(cm.clusters, name)
+
+		cm.globalServices.onClusterDelete(name)
 	}
 	cm.mutex.Unlock()
 

pkg/clustermesh/clustermesh_test.go
@@ -128,7 +128,7 @@ func (s *ClusterMeshTestSuite) TestClusterMesh(c *C) {
 		Name:            "test2",
 		ConfigDirectory: dir,
 		NodeKeyCreator:  testNodeCreator,
-		observer:        &testObserver{},
+		nodeObserver:    &testObserver{},
 	})
 	c.Assert(err, IsNil)
 	c.Assert(cm, Not(IsNil))

pkg/clustermesh/remote_cluster.go
@@ -27,6 +27,7 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	"github.com/cilium/cilium/pkg/lock"
 	nodeStore "github.com/cilium/cilium/pkg/node/store"
+	"github.com/cilium/cilium/pkg/service"
 
 	"github.com/sirupsen/logrus"
 )
@@ -63,6 +64,10 @@ type remoteCluster struct {
 
 	// store is the shared store representing all nodes in the remote cluster
 	remoteNodes *store.SharedStore
+
+	// remoteServices is the shared store representing services in remote
+	// clusters
+	remoteServices *store.SharedStore
 
 	// ipCacheWatcher is the watcher that notifies about IP<->identity
 	// changes in the remote cluster
@@ -111,19 +116,32 @@ func (rc *remoteCluster) restartRemoteConnection() {
 					return err
 				}
 
-				observer := rc.mesh.conf.observer
-				if observer == nil {
-					observer = &nodeStore.NodeObserver{}
-				}
-
 				remoteNodes, err := store.JoinSharedStore(store.Configuration{
 					Prefix:                  path.Join(nodeStore.NodeStorePrefix, rc.name),
 					KeyCreator:              rc.mesh.conf.NodeKeyCreator,
 					SynchronizationInterval: time.Minute,
 					Backend:                 backend,
-					Observer:                observer,
+					Observer:                rc.mesh.conf.NodeObserver(),
 				})
 				if err != nil {
+					backend.Close()
+					return err
+				}
+
+				remoteServices, err := store.JoinSharedStore(store.Configuration{
+					Prefix: path.Join(service.ServiceStorePrefix, rc.name),
+					KeyCreator: func() store.Key {
+						svc := service.ClusterService{}
+						return &svc
+					},
+					SynchronizationInterval: time.Minute,
+					Backend:                 backend,
+					Observer: &remoteServiceObserver{
+						remoteCluster: rc,
+					},
+				})
+				if err != nil {
+					remoteNodes.Close()
 					backend.Close()
 					return err
 				}
@@ -135,6 +153,7 @@ func (rc *remoteCluster) restartRemoteConnection() {
 
 				rc.mutex.Lock()
 				rc.remoteNodes = remoteNodes
+				rc.remoteServices = remoteServices
 				rc.backend = backend
 				rc.ipCacheWatcher = ipCacheWatcher
 				rc.remoteIdentityCache = remoteIdentityCache
@@ -153,11 +172,14 @@ func (rc *remoteCluster) restartRemoteConnection() {
 				if rc.remoteNodes != nil {
 					rc.remoteNodes.Close()
 				}
-				if rc.backend != nil {
-					rc.backend.Close()
-				}
 				if rc.remoteIdentityCache != nil {
 					rc.remoteIdentityCache.Close()
+				}
+				if rc.remoteServices != nil {
+					rc.remoteServices.Close()
+				}
+				if rc.backend != nil {
+					rc.backend.Close()
 				}
 				rc.mutex.Unlock()
 

pkg/k8s/endpoints.go
@@ -42,6 +42,10 @@ type Endpoints struct {
 // String returns the string representation of an endpoints resource, with
 // backends and ports sorted.
 func (e *Endpoints) String() string {
+	if e == nil {
+		return ""
+	}
+
 	backends := []string{}
 	for ip, ports := range e.Backends {
 		for _, port := range ports {
@@ -133,4 +137,17 @@ func ParseEndpoints(ep *v1.Endpoints) (ServiceID, *Endpoints) {
 	}
 
 	return ParseEndpointsID(ep), endpoints
+}
+
+// externalEndpoints is the collection of external endpoints in all remote
+// clusters. The map key is the name of the remote cluster.
+type externalEndpoints struct {
+	endpoints map[string]*Endpoints
+}
+
+// newExternalEndpoints returns a new ExternalEndpoints
+func newExternalEndpoints() externalEndpoints {
+	return externalEndpoints{
+		endpoints: map[string]*Endpoints{},
+	}
 }

commit 01a64a25014f84de7d1b1155d65fb90f0501351c

clustermesh: fix deletion of services from global cache
Due to a swap in the order of the parameters, deleted global services
were never removed from the cache, which continued to grow in size. This
commit fixes the issue and adds a new test to prevent regressions.

Fixes: f5c4842822a9 ("k8s: Multi cluster service discovery support")

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/clustermesh/services.go
@@ -113,7 +113,7 @@ func (c *globalServiceCache) onDelete(svc *serviceStore.ClusterService) {
 
 	c.mutex.Lock()
 	if globalService, ok := c.byName[svc.NamespaceServiceName()]; ok {
-		c.delete(globalService, svc.NamespaceServiceName(), svc.Cluster)
+		c.delete(globalService, svc.Cluster, svc.NamespaceServiceName())
 	} else {
 		scopedLog.Debugf("Ignoring delete request for unknown global service")
 	}
@@ -126,7 +126,7 @@ func (c *globalServiceCache) onClusterDelete(clusterName string) {
 
 	c.mutex.Lock()
 	for serviceName, globalService := range c.byName {
-		c.delete(globalService, serviceName, clusterName)
+		c.delete(globalService, clusterName, serviceName)
 	}
 	c.mutex.Unlock()
 }

pkg/clustermesh/services_test.go
@@ -14,6 +14,7 @@ import (
 
 	. "gopkg.in/check.v1"
 
+	"github.com/cilium/cilium/pkg/checker"
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	fakeDatapath "github.com/cilium/cilium/pkg/datapath/fake"
 	"github.com/cilium/cilium/pkg/defaults"
@@ -28,6 +29,7 @@ import (
 	"github.com/cilium/cilium/pkg/lock"
 	fakeConfig "github.com/cilium/cilium/pkg/option/fake"
 	"github.com/cilium/cilium/pkg/rand"
+	serviceStore "github.com/cilium/cilium/pkg/service/store"
 	"github.com/cilium/cilium/pkg/testutils"
 	testidentity "github.com/cilium/cilium/pkg/testutils/identity"
 )
@@ -328,4 +330,61 @@ func (s *ClusterMeshServicesTestSuite) TestClusterMeshServicesNonGlobal(c *C) {
 		swgSvcs.Wait()
 		return true
 	}, 2*time.Second), IsNil)
+}
+
+type fakeServiceMerger struct {
+	updated map[string]int
+	deleted map[string]int
+}
+
+func (f *fakeServiceMerger) init() {
+	f.updated = make(map[string]int)
+	f.deleted = make(map[string]int)
+}
+
+func (f *fakeServiceMerger) MergeExternalServiceUpdate(service *serviceStore.ClusterService, _ *lock.StoppableWaitGroup) {
+	f.updated[service.String()]++
+}
+
+func (f *fakeServiceMerger) MergeExternalServiceDelete(service *serviceStore.ClusterService, _ *lock.StoppableWaitGroup) {
+	f.deleted[service.String()]++
+}
+
+func (s *ClusterMeshServicesTestSuite) TestRemoteServiceObserver(c *C) {
+	svc1 := serviceStore.ClusterService{Cluster: "remote", Namespace: "namespace", Name: "name", IncludeExternal: true, Shared: true}
+	cache := newGlobalServiceCache("cluster", "node")
+	merger := fakeServiceMerger{}
+
+	observer := remoteServiceObserver{
+		remoteCluster: &remoteCluster{
+			mesh: &ClusterMesh{
+				globalServices: cache,
+				conf:           Configuration{ServiceMerger: &merger},
+			},
+		},
+		swg: lock.NewStoppableWaitGroup(),
+	}
+
+	// Observe a new service update (for a shared service), and assert it is correctly added to the cache
+	merger.init()
+	observer.OnUpdate(&svc1)
+
+	c.Assert(merger.updated[svc1.String()], Equals, 1)
+	c.Assert(merger.deleted[svc1.String()], Equals, 0)
+
+	c.Assert(cache.byName, HasLen, 1)
+	gs, ok := cache.byName[svc1.NamespaceServiceName()]
+	c.Assert(ok, Equals, true)
+	c.Assert(gs.clusterServices, HasLen, 1)
+	found, ok := gs.clusterServices[svc1.Cluster]
+	c.Assert(ok, Equals, true)
+	c.Assert(found, checker.DeepEquals, &svc1)
+
+	// Observe a new service deletion, and assert it is correctly removed from the cache
+	merger.init()
+	observer.OnDelete(&svc1)
+
+	c.Assert(merger.updated[svc1.String()], Equals, 0)
+	c.Assert(merger.deleted[svc1.String()], Equals, 1)
+	c.Assert(cache.byName, HasLen, 0)
 }