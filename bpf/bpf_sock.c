g0odi3z2j/cilium

commit a892b4504c2b3ce2755df49bb02c37eb593cb409

bpf: Block service connections if no backend is available
Similar to commit b8b30eeb7548 ("bpf: drop SVC traffic if no backend is
available"), make socket load balancer block connections to the services
that have no backends (either no backends at all, or no local backends
when the internalTrafficPolicy is set to Local).

Currently socket load balancer allows all connections, even if it was a
service endpoint without available backends. In such case, packets fall
back to per-packet load balancing, instead of being dropped, which has
an undesired effect: if internalTrafficPolicy is Local and there are no
local backends, per-packet load balancer will retry the lookup in the
external scope, and if externalTrafficPolicy is Cluster, the client will
end up connecting to some remote backend, contrary to the internal
traffic policy.

Fix it by making syscalls return an error when there are no local
backends available.

Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

bpf/bpf_sock.c
@@ -203,7 +203,7 @@ sock4_wildcard_lookup(struct lb4_key *key __maybe_unused,
 	return NULL;
 wildcard_lookup:
 	key->address = 0;
-	return lb4_lookup_service(key, true, true);
+	return lb4_lookup_service(key, true, false);
 }
 #endif /* ENABLE_NODEPORT */
 
@@ -316,11 +316,13 @@ static __always_inline int __sock4_xlate_fwd(struct bpf_sock_addr *ctx,
 	 * service entries via wildcarded lookup for NodePort and
 	 * HostPort services.
 	 */
-	svc = lb4_lookup_service(&key, true, true);
+	svc = lb4_lookup_service(&key, true, false);
 	if (!svc)
 		svc = sock4_wildcard_lookup_full(&key, in_hostns);
 	if (!svc)
 		return -ENXIO;
+	if (svc->count == 0)
+		return -EHOSTUNREACH;
 
 	send_trace_sock_notify4(ctx_full, XLATE_PRE_DIRECTION_FWD, dst_ip,
 				bpf_ntohs(dst_port));
@@ -394,7 +396,7 @@ static __always_inline int __sock4_xlate_fwd(struct bpf_sock_addr *ctx,
 		backend_slot = __lb4_lookup_backend_slot(&key);
 		if (!backend_slot) {
 			update_metrics(0, METRIC_EGRESS, REASON_LB_NO_BACKEND_SLOT);
-			return -ENOENT;
+			return -EHOSTUNREACH;
 		}
 
 		backend_id = backend_slot->backend_id;
@@ -403,7 +405,7 @@ static __always_inline int __sock4_xlate_fwd(struct bpf_sock_addr *ctx,
 
 	if (!backend) {
 		update_metrics(0, METRIC_EGRESS, REASON_LB_NO_BACKEND);
-		return -ENOENT;
+		return -EHOSTUNREACH;
 	}
 
 	if (lb4_svc_is_localredirect(svc) &&
@@ -452,10 +454,17 @@ __sock4_health_fwd(struct bpf_sock_addr *ctx __maybe_unused)
 __section("cgroup/connect4")
 int cil_sock4_connect(struct bpf_sock_addr *ctx)
 {
+	int err;
+
 	if (sock_is_health_check(ctx))
 		return __sock4_health_fwd(ctx);
 
-	__sock4_xlate_fwd(ctx, ctx, false);
+	err = __sock4_xlate_fwd(ctx, ctx, false);
+	if (err == -EHOSTUNREACH || err == -ENOMEM) {
+		try_set_retval(err);
+		return SYS_REJECT;
+	}
+
 	return SYS_PROCEED;
 }
 
@@ -473,7 +482,7 @@ static __always_inline int __sock4_post_bind(struct bpf_sock *ctx,
 	    !ctx_in_hostns(ctx_full, NULL))
 		return 0;
 
-	svc = lb4_lookup_service(&key, true, true);
+	svc = lb4_lookup_service(&key, true, false);
 	if (!svc)
 		/* Perform a wildcard lookup for the case where the caller
 		 * tries to bind to loopback or an address with host identity
@@ -569,11 +578,11 @@ static __always_inline int __sock4_xlate_rev(struct bpf_sock_addr *ctx,
 			.dport		= val->port,
 		};
 
-		svc = lb4_lookup_service(&svc_key, true, true);
+		svc = lb4_lookup_service(&svc_key, true, false);
 		if (!svc)
 			svc = sock4_wildcard_lookup_full(&svc_key,
 						ctx_in_hostns(ctx_full, NULL));
-		if (!svc || svc->rev_nat_index != val->rev_nat_index) {
+		if (!svc || svc->rev_nat_index != val->rev_nat_index || svc->count == 0) {
 			map_delete_elem(&LB4_REVERSE_NAT_SK_MAP, &key);
 			update_metrics(0, METRIC_INGRESS, REASON_LB_REVNAT_STALE);
 			return -ENOENT;
@@ -592,7 +601,14 @@ static __always_inline int __sock4_xlate_rev(struct bpf_sock_addr *ctx,
 __section("cgroup/sendmsg4")
 int cil_sock4_sendmsg(struct bpf_sock_addr *ctx)
 {
-	__sock4_xlate_fwd(ctx, ctx, true);
+	int err;
+
+	err = __sock4_xlate_fwd(ctx, ctx, true);
+	if (err == -EHOSTUNREACH || err == -ENOMEM) {
+		try_set_retval(err);
+		return SYS_REJECT;
+	}
+
 	return SYS_PROCEED;
 }
 
@@ -735,7 +751,7 @@ sock6_wildcard_lookup(struct lb6_key *key __maybe_unused,
 	return NULL;
 wildcard_lookup:
 	memset(&key->address, 0, sizeof(key->address));
-	return lb6_lookup_service(key, true, true);
+	return lb6_lookup_service(key, true, false);
 }
 #endif /* ENABLE_NODEPORT */
 
@@ -830,7 +846,7 @@ static __always_inline int __sock6_post_bind(struct bpf_sock *ctx)
 
 	ctx_get_v6_src_address(ctx, &key.address);
 
-	svc = lb6_lookup_service(&key, true, true);
+	svc = lb6_lookup_service(&key, true, false);
 	if (!svc) {
 		svc = sock6_wildcard_lookup(&key, false, false, true);
 		if (!svc)
@@ -958,11 +974,13 @@ static __always_inline int __sock6_xlate_fwd(struct bpf_sock_addr *ctx,
 	ctx_get_v6_address(ctx, &key.address);
 	memcpy(&orig_key, &key, sizeof(key));
 
-	svc = lb6_lookup_service(&key, true, true);
+	svc = lb6_lookup_service(&key, true, false);
 	if (!svc)
 		svc = sock6_wildcard_lookup_full(&key, in_hostns);
 	if (!svc)
 		return sock6_xlate_v4_in_v6(ctx, udp_only);
+	if (svc->count == 0)
+		return -EHOSTUNREACH;
 
 	send_trace_sock_notify6(ctx, XLATE_PRE_DIRECTION_FWD, &key.address,
 				bpf_ntohs(dst_port));
@@ -1005,7 +1023,7 @@ static __always_inline int __sock6_xlate_fwd(struct bpf_sock_addr *ctx,
 		backend_slot = __lb6_lookup_backend_slot(&key);
 		if (!backend_slot) {
 			update_metrics(0, METRIC_EGRESS, REASON_LB_NO_BACKEND_SLOT);
-			return -ENOENT;
+			return -EHOSTUNREACH;
 		}
 
 		backend_id = backend_slot->backend_id;
@@ -1014,7 +1032,7 @@ static __always_inline int __sock6_xlate_fwd(struct bpf_sock_addr *ctx,
 
 	if (!backend) {
 		update_metrics(0, METRIC_EGRESS, REASON_LB_NO_BACKEND);
-		return -ENOENT;
+		return -EHOSTUNREACH;
 	}
 
 	if (lb6_svc_is_affinity(svc) && !backend_from_affinity)
@@ -1071,10 +1089,17 @@ __sock6_health_fwd(struct bpf_sock_addr *ctx __maybe_unused)
 __section("cgroup/connect6")
 int cil_sock6_connect(struct bpf_sock_addr *ctx)
 {
+	int err;
+
 	if (sock_is_health_check(ctx))
 		return __sock6_health_fwd(ctx);
 
-	__sock6_xlate_fwd(ctx, false);
+	err = __sock6_xlate_fwd(ctx, false);
+	if (err == -EHOSTUNREACH || err == -ENOMEM) {
+		try_set_retval(err);
+		return SYS_REJECT;
+	}
+
 	return SYS_PROCEED;
 }
 
@@ -1130,11 +1155,11 @@ static __always_inline int __sock6_xlate_rev(struct bpf_sock_addr *ctx)
 			.dport		= val->port,
 		};
 
-		svc = lb6_lookup_service(&svc_key, true, true);
+		svc = lb6_lookup_service(&svc_key, true, false);
 		if (!svc)
 			svc = sock6_wildcard_lookup_full(&svc_key,
 						ctx_in_hostns(ctx, NULL));
-		if (!svc || svc->rev_nat_index != val->rev_nat_index) {
+		if (!svc || svc->rev_nat_index != val->rev_nat_index || svc->count == 0) {
 			map_delete_elem(&LB6_REVERSE_NAT_SK_MAP, &key);
 			update_metrics(0, METRIC_INGRESS, REASON_LB_REVNAT_STALE);
 			return -ENOENT;
@@ -1154,7 +1179,14 @@ static __always_inline int __sock6_xlate_rev(struct bpf_sock_addr *ctx)
 __section("cgroup/sendmsg6")
 int cil_sock6_sendmsg(struct bpf_sock_addr *ctx)
 {
-	__sock6_xlate_fwd(ctx, true);
+	int err;
+
+	err = __sock6_xlate_fwd(ctx, true);
+	if (err == -EHOSTUNREACH || err == -ENOMEM) {
+		try_set_retval(err);
+		return SYS_REJECT;
+	}
+
 	return SYS_PROCEED;
 }
 

bpf/include/bpf/errno.h
@@ -30,6 +30,15 @@
 #ifndef EADDRINUSE
 # define EADDRINUSE	98
 #endif
+#ifndef ECONNRESET
+# define ECONNRESET	104
+#endif
+#ifndef ENOTCONN
+# define ENOTCONN	107
+#endif
+#ifndef EHOSTUNREACH
+# define EHOSTUNREACH	113
+#endif
 #ifndef ENOTSUPP
 # define ENOTSUPP	524
 #endif

bpf/include/bpf/helpers.h
@@ -106,4 +106,15 @@ static int BPF_FUNC_REMAP(get_socket_opt, void *ctx, int level, int optname,
 
 static __u64 BPF_FUNC(get_current_cgroup_id);
 
+static int BPF_FUNC(set_retval, int retval);
+
+static inline int try_set_retval(int retval __maybe_unused)
+{
+#ifdef HAVE_SET_RETVAL
+	return set_retval(retval);
+#else
+	return 0;
+#endif
+}
+
 #endif /* __BPF_HELPERS__ */

pkg/datapath/linux/probes/probes.go
@@ -500,6 +500,7 @@ func ExecuteHeaderProbes() *FeatureProbes {
 		{ebpf.CGroupSockAddr, asm.FnSkLookupTcp},
 		{ebpf.CGroupSockAddr, asm.FnSkLookupUdp},
 		{ebpf.CGroupSockAddr, asm.FnGetCurrentCgroupId},
+		{ebpf.CGroupSock, asm.FnSetRetval},
 
 		// skb related probes
 		{ebpf.SchedCLS, asm.FnSkbChangeTail},
@@ -540,6 +541,7 @@ func writeCommonHeader(writer io.Writer, probes *FeatureProbes) error {
 		// introduced in the same release.
 		"HAVE_DIRECT_ACCESS_TO_MAP_VALUES": probes.ProgramHelpers[ProgramHelper{ebpf.SchedCLS, asm.FnFibLookup}],
 		"HAVE_LARGE_INSN_LIMIT":            probes.Misc.HaveLargeInsnLimit,
+		"HAVE_SET_RETVAL":                  probes.ProgramHelpers[ProgramHelper{ebpf.CGroupSock, asm.FnSetRetval}],
 	}
 
 	return writeFeatureHeader(writer, features, true)

commit 298cbbe5cd8cd27d84dc7e52594d965925afa7df

ingress: Bypass service count check
As L7 LB service is having dummy endpoint (e.g. 192.192.192.192), the
in-cluster traffic will be dropped due to no healthy backend. This commit
is to make sure that the proper exclusion rule is done.

Relates: https://github.com/cilium/cilium/pull/21871
Fixes: a892b4504c

Signed-off-by: Tam Mach <tam.mach@cilium.io>

bpf/bpf_sock.c
@@ -321,7 +321,7 @@ static __always_inline int __sock4_xlate_fwd(struct bpf_sock_addr *ctx,
 		svc = sock4_wildcard_lookup_full(&key, in_hostns);
 	if (!svc)
 		return -ENXIO;
-	if (svc->count == 0)
+	if (svc->count == 0 && !lb4_svc_is_l7loadbalancer(svc))
 		return -EHOSTUNREACH;
 
 	send_trace_sock_notify4(ctx_full, XLATE_PRE_DIRECTION_FWD, dst_ip,
@@ -594,7 +594,8 @@ static __always_inline int __sock4_xlate_rev(struct bpf_sock_addr *ctx,
 		if (!svc)
 			svc = sock4_wildcard_lookup_full(&svc_key,
 						ctx_in_hostns(ctx_full, NULL));
-		if (!svc || svc->rev_nat_index != val->rev_nat_index || svc->count == 0) {
+		if (!svc || svc->rev_nat_index != val->rev_nat_index ||
+		    (svc->count == 0 && !lb4_svc_is_l7loadbalancer(svc))) {
 			map_delete_elem(&LB4_REVERSE_NAT_SK_MAP, &key);
 			update_metrics(0, METRIC_INGRESS, REASON_LB_REVNAT_STALE);
 			return -ENOENT;
@@ -998,7 +999,7 @@ static __always_inline int __sock6_xlate_fwd(struct bpf_sock_addr *ctx,
 		svc = sock6_wildcard_lookup_full(&key, in_hostns);
 	if (!svc)
 		return sock6_xlate_v4_in_v6(ctx, udp_only);
-	if (svc->count == 0)
+	if (svc->count == 0 && !lb6_svc_is_l7loadbalancer(svc))
 		return -EHOSTUNREACH;
 
 	send_trace_sock_notify6(ctx, XLATE_PRE_DIRECTION_FWD, &key.address,
@@ -1183,7 +1184,8 @@ static __always_inline int __sock6_xlate_rev(struct bpf_sock_addr *ctx)
 		if (!svc)
 			svc = sock6_wildcard_lookup_full(&svc_key,
 						ctx_in_hostns(ctx, NULL));
-		if (!svc || svc->rev_nat_index != val->rev_nat_index || svc->count == 0) {
+		if (!svc || svc->rev_nat_index != val->rev_nat_index ||
+		    (svc->count == 0 && !lb6_svc_is_l7loadbalancer(svc))) {
 			map_delete_elem(&LB6_REVERSE_NAT_SK_MAP, &key);
 			update_metrics(0, METRIC_INGRESS, REASON_LB_REVNAT_STALE);
 			return -ENOENT;