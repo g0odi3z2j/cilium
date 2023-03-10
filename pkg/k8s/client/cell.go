g0odi3z2j/cilium

commit 68261dc32e854f96dce891d7cedf4f2a70afce8f

k8s: Add k8s-client cell
This adds a cell for the kubernetes client that provides a Clientset
for the application for accessing Kubernetes resources.

The integration of the new clientset to the codebase is done gradually.
In this commit the APIs provided by pkg/k8s are kept mostly as is, with
Configure and Init still functioning. Daemon is modified to take in the
clientset as parameter which is then used to populate the existing
global client variables via k8s.SetClients. Minimal changes were made
to cilium command and the operator.

New command-line flag, --skip-daemon, was added to allow experimenting
with new cells without having the daemon start up.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/k8s/client/cell.go
@@ -1,0 +1,492 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package client
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"go.uber.org/fx"
+	apiext_clientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	apiext_fake "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilnet "k8s.io/apimachinery/pkg/util/net"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/connrotation"
+
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/hive"
+	cilium_clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
+	cilium_fake "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/fake"
+	k8smetrics "github.com/cilium/cilium/pkg/k8s/metrics"
+	slim_apiextclientsetscheme "github.com/cilium/cilium/pkg/k8s/slim/k8s/apiextensions-client/clientset/versioned/scheme"
+	slim_apiext_clientset "github.com/cilium/cilium/pkg/k8s/slim/k8s/apiextensions-clientset"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	slim_metav1beta1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1beta1"
+	slim_clientset "github.com/cilium/cilium/pkg/k8s/slim/k8s/client/clientset/versioned"
+	slim_fake "github.com/cilium/cilium/pkg/k8s/slim/k8s/client/clientset/versioned/fake"
+	k8sversion "github.com/cilium/cilium/pkg/k8s/version"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/version"
+)
+
+// client.Cell provides Clientset, a composition of clientsets to Kubernetes resources
+// used by Cilium.
+var Cell = hive.NewCellWithConfig[Config](
+	"k8s-client",
+	fx.Provide(newClientset),
+)
+
+// Type aliases for the clientsets to avoid name collision on 'Clientset' when composing them.
+type (
+	KubernetesClientset = kubernetes.Clientset
+	SlimClientset       = slim_clientset.Clientset
+	APIExtClientset     = slim_apiext_clientset.Clientset
+	CiliumClientset     = cilium_clientset.Clientset
+)
+
+// Clientset is a composition of the different client sets used by Cilium.
+type Clientset interface {
+	kubernetes.Interface
+	apiext_clientset.Interface
+	cilium_clientset.Interface
+
+	// Slim returns the slim client, which contains some of the same APIs as the
+	// normal kubernetes client, but with slimmed down messages to reduce memory
+	// usage. Prefer the slim version when caching messages.
+	Slim() slim_clientset.Interface
+
+	// IsEnabled returns true if Kubernetes support is enabled and the
+	// clientset can be used.
+	IsEnabled() bool
+
+	// Disable disables the client. Panics if called after the clientset has been
+	// started.
+	Disable()
+
+	// Config returns the configuration used to create this client.
+	Config() Config
+}
+
+// compositeClientset implements the Clientset using real clients.
+type compositeClientset struct {
+	started  bool
+	disabled bool
+
+	*KubernetesClientset
+	*APIExtClientset
+	*CiliumClientset
+
+	controller    *controller.Manager
+	slim          *SlimClientset
+	config        Config
+	log           logrus.FieldLogger
+	closeAllConns func()
+	restConfig    *rest.Config
+}
+
+func newClientset(lc fx.Lifecycle, log logrus.FieldLogger, cfg Config) (Clientset, error) {
+	if !cfg.isEnabled() {
+		return &compositeClientset{disabled: true}, nil
+	}
+
+	if cfg.K8sAPIServer != "" &&
+		!strings.HasPrefix(cfg.K8sAPIServer, "http") {
+		cfg.K8sAPIServer = "http://" + cfg.K8sAPIServer // default to HTTP
+	}
+
+	client := compositeClientset{
+		log:        log,
+		controller: controller.NewManager(),
+		config:     cfg,
+	}
+
+	lc.Append(fx.Hook{
+		OnStart: client.onStart,
+		OnStop:  client.onStop,
+	})
+
+	return &client, nil
+}
+
+func (c *compositeClientset) Slim() slim_clientset.Interface {
+	return c.slim
+}
+
+func (c *compositeClientset) Discovery() discovery.DiscoveryInterface {
+	return c.KubernetesClientset.Discovery()
+}
+
+func (c *compositeClientset) IsEnabled() bool {
+	return c.config.isEnabled() && !c.disabled
+}
+
+func (c *compositeClientset) Disable() {
+	if c.started {
+		panic("Clientset.Disable() called after it had been started")
+	}
+	c.disabled = true
+}
+
+func (c *compositeClientset) Config() Config {
+	return c.config
+}
+
+func (c *compositeClientset) onStart(startCtx context.Context) error {
+	if !c.IsEnabled() {
+		return nil
+	}
+
+	cfg := c.config
+
+	restConfig, err := createConfig(cfg.K8sAPIServer, cfg.K8sKubeConfigPath, cfg.K8sClientQPS, cfg.K8sClientBurst)
+	if err != nil {
+		return fmt.Errorf("unable to create k8s client rest configuration: %w", err)
+	}
+	c.restConfig = restConfig
+	defaultCloseAllConns := setDialer(cfg, restConfig)
+
+	httpClient, err := rest.HTTPClientFor(restConfig)
+	if err != nil {
+		return fmt.Errorf("unable to create k8s REST client: %w", err)
+	}
+
+	// We are implementing the same logic as Kubelet, see
+	// https://github.com/kubernetes/kubernetes/blob/v1.24.0-beta.0/cmd/kubelet/app/server.go#L852.
+	if s := os.Getenv("DISABLE_HTTP2"); len(s) > 0 {
+		c.closeAllConns = defaultCloseAllConns
+	} else {
+		c.closeAllConns = func() {
+			utilnet.CloseIdleConnectionsFor(restConfig.Transport)
+		}
+	}
+
+	// Slim and K8s clients use protobuf marshalling.
+	restConfig.ContentConfig.ContentType = `application/vnd.kubernetes.protobuf`
+
+	c.slim, err = slim_clientset.NewForConfigAndClient(restConfig, httpClient)
+	if err != nil {
+		return fmt.Errorf("unable to create slim k8s client: %w", err)
+	}
+
+	c.APIExtClientset, err = slim_apiext_clientset.NewForConfigAndClient(restConfig, httpClient)
+	if err != nil {
+		return fmt.Errorf("unable to create apiext k8s client: %w", err)
+	}
+
+	c.KubernetesClientset, err = kubernetes.NewForConfigAndClient(restConfig, httpClient)
+	if err != nil {
+		return fmt.Errorf("unable to create k8s client: %w", err)
+	}
+
+	// The cilium client uses JSON marshalling.
+	restConfig.ContentConfig.ContentType = `application/json`
+	c.CiliumClientset, err = cilium_clientset.NewForConfigAndClient(restConfig, httpClient)
+	if err != nil {
+		return fmt.Errorf("unable to create cilium k8s client: %w", err)
+	}
+
+	if err := c.waitForConn(startCtx); err != nil {
+		return err
+	}
+	c.startHeartbeat()
+
+	// Update the global K8s clients, K8s version and the capabilities.
+	if err := k8sversion.Update(c, c.Config()); err != nil {
+		return err
+	}
+
+	if !k8sversion.Capabilities().MinimalVersionMet {
+		return fmt.Errorf("k8s version (%v) is not meeting the minimal requirement (%v)",
+			k8sversion.Version(), k8sversion.MinimalVersionConstraint)
+	}
+
+	c.started = true
+
+	return nil
+}
+
+func (c *compositeClientset) onStop(ctx context.Context) error {
+	if c.IsEnabled() {
+		c.controller.RemoveAllAndWait()
+		c.closeAllConns()
+
+		// Drop references created in onStart
+		c.closeAllConns = nil
+		c.slim = nil
+		c.APIExtClientset = nil
+		c.CiliumClientset = nil
+		c.KubernetesClientset = nil
+	}
+	c.started = false
+	return nil
+}
+
+func (c *compositeClientset) startHeartbeat() {
+	restClient := c.KubernetesClientset.RESTClient()
+
+	timeout := c.config.K8sHeartbeatTimeout
+	if timeout == 0 {
+		return
+	}
+
+	heartBeat := func(ctx context.Context) error {
+		// Kubernetes does a get node of the node that kubelet is running [0]. This seems excessive in
+		// our case because the amount of data transferred is bigger than doing a Get of /healthz.
+		// For this reason we have picked to perform a get on `/healthz` instead a get of a node.
+		//
+		// [0] https://github.com/kubernetes/kubernetes/blob/v1.17.3/pkg/kubelet/kubelet_node_status.go#L423
+		res := restClient.Get().Resource("healthz").Do(ctx)
+		return res.Error()
+	}
+
+	c.controller.UpdateController("k8s-heartbeat",
+		controller.ControllerParams{
+			DoFunc: func(context.Context) error {
+				runHeartbeat(
+					c.log,
+					heartBeat,
+					timeout,
+					c.closeAllConns,
+				)
+				return nil
+			},
+			RunInterval: timeout,
+		})
+}
+
+// createConfig creates a rest.Config for connecting to k8s api-server.
+//
+// The precedence of the configuration selection is the following:
+// 1. kubeCfgPath
+// 2. apiServerURL (https if specified)
+// 3. rest.InClusterConfig().
+func createConfig(apiServerURL, kubeCfgPath string, qps float32, burst int) (*rest.Config, error) {
+	var (
+		config *rest.Config
+		err    error
+	)
+	cmdName := "cilium"
+	if len(os.Args[0]) != 0 {
+		cmdName = filepath.Base(os.Args[0])
+	}
+	userAgent := fmt.Sprintf("%s/%s", cmdName, version.Version)
+
+	switch {
+	// If the apiServerURL and the kubeCfgPath are empty then we can try getting
+	// the rest.Config from the InClusterConfig
+	case apiServerURL == "" && kubeCfgPath == "":
+		if config, err = rest.InClusterConfig(); err != nil {
+			return nil, err
+		}
+	case kubeCfgPath != "":
+		if config, err = clientcmd.BuildConfigFromFlags("", kubeCfgPath); err != nil {
+			return nil, err
+		}
+	case strings.HasPrefix(apiServerURL, "https://"):
+		if config, err = rest.InClusterConfig(); err != nil {
+			return nil, err
+		}
+		config.Host = apiServerURL
+	default:
+		config = &rest.Config{Host: apiServerURL, UserAgent: userAgent}
+	}
+
+	return config, nil
+}
+
+func (c *compositeClientset) waitForConn(ctx context.Context) error {
+	stop := make(chan struct{})
+	timeout := time.NewTimer(time.Minute)
+	defer timeout.Stop()
+	var err error
+	wait.Until(func() {
+		c.log.WithField("host", c.restConfig.Host).Info("Establishing connection to apiserver")
+		err = isConnReady(c)
+		if err == nil {
+			close(stop)
+			return
+		}
+
+		select {
+		case <-ctx.Done():
+		case <-timeout.C:
+		default:
+			return
+		}
+
+		c.log.WithError(err).WithField(logfields.IPAddr, c.restConfig.Host).Error("Unable to contact k8s api-server")
+		close(stop)
+	}, 5*time.Second, stop)
+	if err == nil {
+		c.log.Info("Connected to apiserver")
+	}
+	return err
+}
+
+func setDialer(cfg Config, restConfig *rest.Config) func() {
+	if cfg.K8sHeartbeatTimeout == 0 {
+		return func() {}
+	}
+	ctx := (&net.Dialer{
+		Timeout:   cfg.K8sHeartbeatTimeout,
+		KeepAlive: cfg.K8sHeartbeatTimeout,
+	}).DialContext
+	dialer := connrotation.NewDialer(ctx)
+	restConfig.Dial = dialer.DialContext
+	return dialer.CloseAll
+}
+
+func runHeartbeat(log logrus.FieldLogger, heartBeat func(context.Context) error, timeout time.Duration, closeAllConns ...func()) {
+	expireDate := time.Now().Add(-timeout)
+	// Don't even perform a health check if we have received a successful
+	// k8s event in the last 'timeout' duration
+	if k8smetrics.LastSuccessInteraction.Time().After(expireDate) {
+		return
+	}
+
+	done := make(chan error)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	go func() {
+		// If we have reached up to this point to perform a heartbeat to
+		// kube-apiserver then we should close the connections if we receive
+		// any error at all except if we receive a http.StatusTooManyRequests
+		// which means the server is overloaded and only for this reason we
+		// will not close all connections.
+		err := heartBeat(ctx)
+		switch t := err.(type) {
+		case *errors.StatusError:
+			if t.ErrStatus.Code != http.StatusTooManyRequests {
+				done <- err
+			}
+		default:
+			done <- err
+		}
+		close(done)
+	}()
+
+	select {
+	case err := <-done:
+		if err != nil {
+			log.WithError(err).Warn("Network status error received, restarting client connections")
+			for _, fn := range closeAllConns {
+				fn()
+			}
+		}
+	case <-ctx.Done():
+		log.Warn("Heartbeat timed out, restarting client connections")
+		for _, fn := range closeAllConns {
+			fn()
+		}
+	}
+}
+
+// isConnReady returns the err for the kube-system namespace get
+func isConnReady(c kubernetes.Interface) error {
+	_, err := c.CoreV1().Namespaces().Get(context.TODO(), "kube-system", metav1.GetOptions{})
+	return err
+}
+
+var FakeClientCell = hive.NewCell(
+	"k8s-fake-client",
+	fx.Provide(NewFakeClientset),
+)
+
+type (
+	KubernetesFakeClientset = fake.Clientset
+	SlimFakeClientset       = slim_fake.Clientset
+	CiliumFakeClientset     = cilium_fake.Clientset
+	APIExtFakeClientset     = apiext_fake.Clientset
+)
+
+type FakeClientset struct {
+	*KubernetesFakeClientset
+	*CiliumFakeClientset
+	*APIExtFakeClientset
+
+	SlimClientset *SlimFakeClientset
+}
+
+var _ Clientset = &FakeClientset{}
+
+func (c *FakeClientset) Slim() slim_clientset.Interface {
+	return c.SlimClientset
+}
+
+func (c *FakeClientset) Discovery() discovery.DiscoveryInterface {
+	return c.KubernetesFakeClientset.Discovery()
+}
+
+func (c *FakeClientset) IsEnabled() bool {
+	return true
+}
+
+func (c *FakeClientset) Disable() {
+	/* nop */
+}
+
+func (c *FakeClientset) Config() Config {
+	return Config{}
+}
+
+func NewFakeClientset() (*FakeClientset, Clientset) {
+	client := FakeClientset{
+		SlimClientset:           slim_fake.NewSimpleClientset(),
+		CiliumFakeClientset:     cilium_fake.NewSimpleClientset(),
+		APIExtFakeClientset:     apiext_fake.NewSimpleClientset(),
+		KubernetesFakeClientset: fake.NewSimpleClientset(),
+	}
+	return &client, &client
+}
+
+type standaloneLifecycle struct {
+	hooks []fx.Hook
+}
+
+func (s *standaloneLifecycle) Append(hook fx.Hook) {
+	s.hooks = append(s.hooks, hook)
+}
+
+// NewStandaloneClientset creates a clientset outside hive. To be removed once
+// remaining uses of k8s.Init()/k8s.Client()/etc. have been converted.
+func NewStandaloneClientset(cfg Config) (Clientset, error) {
+	log := logging.DefaultLogger
+	lc := &standaloneLifecycle{}
+
+	clientset, err := newClientset(lc, log, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, hook := range lc.hooks {
+		if err := hook.OnStart(context.Background()); err != nil {
+			return nil, err
+		}
+	}
+
+	return clientset, err
+}
+
+func init() {
+	// Register the metav1.Table and metav1.PartialObjectMetadata for the
+	// apiextclientset.
+	utilruntime.Must(slim_metav1.AddMetaToScheme(slim_apiextclientsetscheme.Scheme))
+	utilruntime.Must(slim_metav1beta1.AddMetaToScheme(slim_apiextclientsetscheme.Scheme))
+}

cilium/cmd/policy_trace.go
@@ -20,8 +20,7 @@ import (
 	endpointid "github.com/cilium/cilium/pkg/endpoint/id"
 	"github.com/cilium/cilium/pkg/iana"
 	"github.com/cilium/cilium/pkg/identity"
-	"github.com/cilium/cilium/pkg/k8s"
-	clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/policy/trace"
 )
 
@@ -225,16 +224,17 @@ func getSecIDFromK8s(podName string) (string, error) {
 	if err != nil {
 		Fatalf("Error while retrieving configuration: %s", err)
 	}
-	restConfig, err := k8s.CreateConfigFromAgentResponse(resp)
+
+	clientset, err := k8sClient.NewStandaloneClientset(
+		k8sClient.Config{
+			K8sAPIServer:      resp.Status.K8sEndpoint,
+			K8sKubeConfigPath: resp.Status.K8sConfiguration,
+		})
 	if err != nil {
-		return "", fmt.Errorf("unable to create rest configuration: %s", err)
-	}
-	ciliumK8sClient, err := clientset.NewForConfig(restConfig)
-	if err != nil {
-		return "", fmt.Errorf("unable to create k8s client: %s", err)
+		return "", fmt.Errorf("unable to construct client: %s", err)
 	}
 
-	ep, err := ciliumK8sClient.CiliumV2().CiliumEndpoints(namespace).Get(context.TODO(), pod, meta_v1.GetOptions{})
+	ep, err := clientset.CiliumV2().CiliumEndpoints(namespace).Get(context.TODO(), pod, meta_v1.GetOptions{})
 	if err != nil {
 		return "", fmt.Errorf("unable to get pod %s in namespace %s", pod, namespace)
 	}

cilium/cmd/preflight_k8s_valid_cnp.go
@@ -61,14 +61,7 @@ func validateCNPs() error {
 		log.WithError(err).Fatal("Unable to connect to Kubernetes apiserver")
 	}
 
-	restConfig, err := k8s.CreateConfig()
-	if err != nil {
-		return fmt.Errorf("Unable to create rest configuration for k8s CRD: %w", err)
-	}
-	apiExtensionsClient, err := apiextensionsclient.NewForConfig(restConfig)
-	if err != nil {
-		return fmt.Errorf("Unable to create API extensions clientset for k8s CRD: %w", err)
-	}
+	apiExtensionsClient := k8s.APIExtClient()
 
 	npValidator, err := v2_validation.NewNPValidator()
 	if err != nil {

clustermesh-apiserver/main.go
@@ -585,7 +585,7 @@ func runServer(cmd *cobra.Command) {
 	}).Info("Starting clustermesh-apiserver...")
 
 	if mockFile == "" {
-		k8s.Configure("", option.Config.K8sKubeConfigPath, 0.0, 0)
+		k8s.Configure("", viper.GetString(option.K8sKubeConfigPath), 0.0, 0)
 		if err := k8s.Init(k8sconfig.NewDefaultConfiguration()); err != nil {
 			log.WithError(err).Fatal("Unable to connect to Kubernetes apiserver")
 		}

daemon/cmd/daemon_main.go
@@ -51,6 +51,7 @@ import (
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	"github.com/cilium/cilium/pkg/ipmasq"
 	"github.com/cilium/cilium/pkg/k8s"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/k8s/watchers/resources"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/labels"
@@ -322,9 +323,6 @@ func initializeFlags() {
 	flags.Bool(option.K8sEnableEndpointSlice, defaults.K8sEnableEndpointSlice, "Enables k8s EndpointSlice feature in Cilium if the k8s cluster supports it")
 	option.BindEnv(Vp, option.K8sEnableEndpointSlice)
 
-	flags.Bool(option.K8sEnableAPIDiscovery, defaults.K8sEnableAPIDiscovery, "Enable discovery of Kubernetes API groups and resources with the discovery API")
-	option.BindEnv(Vp, option.K8sEnableAPIDiscovery)
-
 	flags.Bool(option.EnableL7Proxy, defaults.EnableL7Proxy, "Enable L7 proxy for L7 policy enforcement")
 	option.BindEnv(Vp, option.EnableL7Proxy)
 
@@ -433,12 +431,6 @@ func initializeFlags() {
 
 	flags.Bool(option.K8sEventHandover, defaults.K8sEventHandover, "Enable k8s event handover to kvstore for improved scalability")
 	option.BindEnv(Vp, option.K8sEventHandover)
-
-	flags.String(option.K8sAPIServer, "", "Kubernetes API server URL")
-	option.BindEnv(Vp, option.K8sAPIServer)
-
-	flags.String(option.K8sKubeConfigPath, "", "Absolute path of the kubernetes kubeconfig file")
-	option.BindEnv(Vp, option.K8sKubeConfigPath)
 
 	flags.String(option.K8sNamespaceName, "", "Name of the Kubernetes namespace in which Cilium is deployed in")
 	option.BindEnv(Vp, option.K8sNamespaceName)
@@ -952,9 +944,6 @@ func initializeFlags() {
 	flags.StringSlice(option.DisableIptablesFeederRules, []string{}, "Chains to ignore when installing feeder rules.")
 	option.BindEnv(Vp, option.DisableIptablesFeederRules)
 
-	flags.Duration(option.K8sHeartbeatTimeout, 30*time.Second, "Configures the timeout for api-server heartbeat, set to 0 to disable")
-	option.BindEnv(Vp, option.K8sHeartbeatTimeout)
-
 	flags.Bool(option.EnableIPv4FragmentsTrackingName, defaults.EnableIPv4FragmentsTracking, "Enable IPv4 fragments tracking for L4-based lookups")
 	option.BindEnv(Vp, option.EnableIPv4FragmentsTrackingName)
 
@@ -1066,7 +1055,9 @@ func initializeFlags() {
 	flags.Bool(option.EnableBGPControlPlane, false, "Enable the BGP control plane.")
 	option.BindEnv(Vp, option.EnableBGPControlPlane)
 
-	Vp.BindPFlags(flags)
+	if err := Vp.BindPFlags(flags); err != nil {
+		log.Fatalf("BindPFlags failed: %s", err)
+	}
 }
 
 // restoreExecPermissions restores file permissions to 0740 of all files inside
@@ -1118,12 +1109,6 @@ func initEnv() {
 	option.LogRegisteredOptions(Vp, log)
 
 	sysctl.SetProcfs(option.Config.ProcFs)
-
-	// Configure k8s as soon as possible so that k8s.IsEnabled() has the right
-	// behavior.
-	bootstrapStats.k8sInit.Start()
-	k8s.Configure(option.Config.K8sAPIServer, option.Config.K8sKubeConfigPath, defaults.K8sClientQPSLimit, defaults.K8sClientBurst)
-	bootstrapStats.k8sInit.End(true)
 
 	for _, grp := range option.Config.DebugVerbose {
 		switch grp {
@@ -1567,18 +1552,37 @@ func (d *Daemon) initKVStore() {
 //
 // If an object still owned by Daemon is required in a module, it should be provided indirectly, e.g. via
 // a callback.
-func registerDaemonHooks(lc fx.Lifecycle, shutdowner fx.Shutdowner) error {
+func registerDaemonHooks(lc fx.Lifecycle, shutdowner fx.Shutdowner, cfg DaemonCellConfig, clientset k8sClient.Clientset) error {
+
+	if cfg.SkipDaemon {
+		return nil
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	cleaner := NewDaemonCleanup()
+
+	if Vp.GetString(option.DatapathMode) == datapathOption.DatapathModeLBOnly {
+		// In lb-only mode the k8s client is not used, even if its configuration
+		// is available, so disable it here before it starts. Using GetString
+		// directly as option.Config not populated yet.
+		clientset.Disable()
+	}
+
 	lc.Append(fx.Hook{
 		OnStart: func(context.Context) error {
+			// Set the k8s clients provided by the K8s client cell. The global clients will be refactored out
+			// by later commits.
+			if clientset.IsEnabled() {
+				k8s.SetClients(clientset, clientset.Slim(), clientset, clientset)
+			}
+
 			bootstrapStats.earlyInit.Start()
 			initEnv()
 			bootstrapStats.earlyInit.End(true)
 
 			// Start running the daemon in the background (blocks on API server's Serve()) to allow rest
 			// of the start hooks to run.
-			go runDaemon(ctx, cleaner, shutdowner)
+			go runDaemon(ctx, cleaner, shutdowner, clientset)
 			return nil
 		},
 		OnStop: func(context.Context) error {
@@ -1591,7 +1595,7 @@ func registerDaemonHooks(lc fx.Lifecycle, shutdowner fx.Shutdowner) error {
 }
 
 // runDaemon runs the old unmodular part of the cilium-agent.
-func runDaemon(ctx context.Context, cleaner *daemonCleanup, shutdowner fx.Shutdowner) {
+func runDaemon(ctx context.Context, cleaner *daemonCleanup, shutdowner fx.Shutdowner, clientset k8sClient.Clientset) {
 	datapathConfig := linuxdatapath.DatapathConfiguration{
 		HostDevice: defaults.HostDevice,
 		ProcFs:     option.Config.ProcFs,
@@ -1632,14 +1636,6 @@ func runDaemon(ctx context.Context, cleaner *daemonCleanup, shutdowner fx.Shutdo
 	} else {
 		// Delete wireguard device from previous run (if such exists)
 		link.DeleteByName(wireguardTypes.IfaceName)
-	}
-
-	if k8s.IsEnabled() {
-		bootstrapStats.k8sInit.Start()
-		if err := k8s.Init(option.Config); err != nil {
-			log.Fatalf("unable to initialize Kubernetes subsystem: %s", err)
-		}
-		bootstrapStats.k8sInit.End(true)
 	}
 
 	d, restoredEndpoints, err := NewDaemon(ctx, cleaner,

daemon/cmd/root.go
@@ -10,10 +10,12 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"go.uber.org/fx"
 
 	"github.com/cilium/cilium/pkg/gops"
 	"github.com/cilium/cilium/pkg/hive"
+	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/version"
 )
@@ -26,9 +28,10 @@ var (
 	}
 
 	cmdrefCmd = &cobra.Command{
-		Use:   "cmdref [output directory]",
-		Short: "Generate command reference for cilium-agent to given output directory",
-		Args:  cobra.ExactArgs(1),
+		Use:    "cmdref [output directory]",
+		Short:  "Generate command reference for cilium-agent to given output directory",
+		Args:   cobra.ExactArgs(1),
+		Hidden: true,
 		Run: func(cmd *cobra.Command, args []string) {
 			genMarkdown(RootCmd, args[0])
 		},
@@ -49,6 +52,15 @@ var (
 	agentHive *hive.Hive
 )
 
+type DaemonCellConfig struct {
+	SkipDaemon bool
+}
+
+func (DaemonCellConfig) CellFlags(flags *pflag.FlagSet) {
+	flags.Bool("skip-daemon", false, "Skip running of the daemon, only start normal cells")
+	flags.MarkHidden("skip-daemon")
+}
+
 func init() {
 	cobra.OnInitialize(option.InitConfig(RootCmd, "cilium-agent", "cilium", Vp))
 	setupSleepBeforeFatal()
@@ -56,10 +68,13 @@ func init() {
 	initializeFlags()
 
 	agentHive = hive.New(
-		Vp, RootCmd.Flags(),
+		Vp,
+		RootCmd.PersistentFlags(),
 
 		gops.Cell,
-		hive.NewCell("daemon", fx.Invoke(registerDaemonHooks)),
+		k8sClient.Cell,
+
+		hive.NewCellWithConfig[DaemonCellConfig]("daemon", fx.Invoke(registerDaemonHooks)),
 	)
 }
 

operator/cmd/root.go
@@ -148,10 +148,11 @@ func initEnv() {
 	option.LogRegisteredOptions(Vp, log)
 	// Enable fallback to direct API probing to check for support of Leases in
 	// case Discovery API fails.
-	option.Config.EnableK8sLeasesFallbackDiscovery()
+	//option.Config.EnableK8sLeasesFallbackDiscovery()
 }
 
 func initK8s(k8sInitDone chan struct{}) {
+	/* TODO: Fixed in next commit.
 	k8s.Configure(
 		option.Config.K8sAPIServer,
 		option.Config.K8sKubeConfigPath,
@@ -161,7 +162,7 @@ func initK8s(k8sInitDone chan struct{}) {
 
 	if err := k8s.Init(option.Config); err != nil {
 		log.WithError(err).Fatal("Unable to connect to Kubernetes apiserver")
-	}
+	}*/
 
 	close(k8sInitDone)
 }
@@ -370,8 +371,10 @@ func OnOperatorStartLeading(ctx context.Context) {
 		cesController := ces.NewCESController(k8s.CiliumClient(),
 			operatorOption.Config.CESMaxCEPsInCES,
 			operatorOption.Config.CESSlicingMode,
+			0.0, 0,
+			/* TODO: Fixed in next commit.
 			option.Config.K8sClientQPSLimit,
-			option.Config.K8sClientBurst)
+			option.Config.K8sClientBurst)*/)
 		stopCh := make(chan struct{})
 		// Start CEP watcher
 		operatorWatchers.CiliumEndpointsSliceInit(k8s.CiliumClient().CiliumV2(), cesController)

pkg/hive/hive.go
@@ -246,13 +246,13 @@ func (h *Hive) createApp() error {
 	h.app = fx.New(
 		fx.WithLogger(func() fxevent.Logger { return h.fxLogger }),
 		fx.Supply(fx.Annotate(log, fx.As(new(logrus.FieldLogger)))),
+		fx.StartTimeout(h.startTimeout),
+		fx.StopTimeout(h.stopTimeout),
+		fx.Populate(&h.dotGraph),
 		fx.Decorate(func(parent fx.Lifecycle) fx.Lifecycle {
 			h.lifecycle.parent = parent
 			return &h.lifecycle
 		}),
-		fx.StartTimeout(h.startTimeout),
-		fx.StopTimeout(h.stopTimeout),
-		fx.Populate(&h.dotGraph),
 		opts,
 	)
 	return h.app.Err()

commit 188f3c93aedf7a915ad13ef14bb3c3a780922db5

pkg/k8s: set user-agent in k8s client
The user agent for the k8s client was never set and it was defaulting
to some non-specific which would make it difficult to distinguish the Cilium
requests in the kube-apiserver logs.

This looks a regression introduced in 68261dc32e85 and, similarly to the
user-agent, we should make sure we are not overwriting any default QPS
nor Burt values with '0'.

Fixes: 68261dc32e85 ("k8s: Add k8s-client cell")
Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/client/cell.go
@@ -306,9 +306,20 @@ func createConfig(apiServerURL, kubeCfgPath string, qps float32, burst int) (*re
 		config = &rest.Config{Host: apiServerURL, UserAgent: userAgent}
 	}
 
-	config.QPS = qps
-	config.Burst = burst
+	setConfig(config, userAgent, qps, burst)
 	return config, nil
+}
+
+func setConfig(config *rest.Config, userAgent string, qps float32, burst int) {
+	if userAgent != "" {
+		config.UserAgent = userAgent
+	}
+	if qps != 0.0 {
+		config.QPS = qps
+	}
+	if burst != 0 {
+		config.Burst = burst
+	}
 }
 
 func (c *compositeClientset) waitForConn(ctx context.Context) error {