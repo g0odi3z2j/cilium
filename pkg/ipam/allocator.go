g0odi3z2j/cilium

commit fedeff4cbddb2429f705901eb4c43c23a083ebf0

ipam: track IP owner per pool
Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/ipam/allocator.go
@@ -30,11 +30,11 @@ var (
 	ErrIPv6Disabled = errors.New("IPv6 allocation disabled")
 )
 
-func (ipam *IPAM) lookupIPsByOwner(owner string) (ips []net.IP) {
+func (ipam *IPAM) lookupIPsByOwner(owner string, pool Pool) (ips []net.IP) {
 	ipam.allocatorMutex.RLock()
 	defer ipam.allocatorMutex.RUnlock()
 
-	for ip, o := range ipam.owner {
+	for ip, o := range ipam.owner[pool] {
 		if o == owner {
 			if parsedIP := net.ParseIP(ip); parsedIP != nil {
 				ips = append(ips, parsedIP)
@@ -115,7 +115,7 @@ func (ipam *IPAM) allocateIP(ip net.IP, owner string, pool Pool, needSyncUpstrea
 		"owner": owner,
 	}).Debugf("Allocated specific IP")
 
-	ipam.owner[ip.String()] = owner
+	ipam.registerIPOwner(ip, owner, pool)
 	metrics.IpamEvent.WithLabelValues(metricAllocate, string(family)).Inc()
 	return
 }
@@ -154,7 +154,7 @@ func (ipam *IPAM) allocateNextFamily(family Family, owner string, pool Pool, nee
 				"pool":  pool.String(),
 				"owner": owner,
 			}).Debugf("Allocated random IP")
-			ipam.owner[result.IP.String()] = owner
+			ipam.registerIPOwner(result.IP, owner, pool)
 			metrics.IpamEvent.WithLabelValues(metricAllocate, string(family)).Inc()
 			return
 		}
@@ -162,7 +162,7 @@ func (ipam *IPAM) allocateNextFamily(family Family, owner string, pool Pool, nee
 		// The allocated IP is excluded, do not use it. The excluded IP
 		// is now allocated so it won't be allocated in the next
 		// iteration.
-		ipam.owner[result.IP.String()] = fmt.Sprintf("%s (excluded)", owner)
+		ipam.registerIPOwner(result.IP, fmt.Sprintf("%s (excluded)", owner), pool)
 	}
 }
 
@@ -263,12 +263,11 @@ func (ipam *IPAM) releaseIPLocked(ip net.IP, pool Pool) error {
 		}
 	}
 
-	owner := ipam.owner[ip.String()]
+	owner := ipam.releaseIPOwner(ip, pool)
 	log.WithFields(logrus.Fields{
 		"ip":    ip.String(),
 		"owner": owner,
 	}).Debugf("Released IP")
-	delete(ipam.owner, ip.String())
 	delete(ipam.expirationTimers, ip.String())
 
 	metrics.IpamEvent.WithLabelValues(metricRelease, string(family)).Inc()
@@ -291,7 +290,7 @@ func (ipam *IPAM) ReleaseIPString(releaseArg string, pool Pool) (err error) {
 
 	ip := net.ParseIP(releaseArg)
 	if ip == nil {
-		ips = ipam.lookupIPsByOwner(releaseArg)
+		ips = ipam.lookupIPsByOwner(releaseArg, pool)
 		if len(ips) == 0 {
 			return fmt.Errorf("Invalid IP address or owner name: %s", releaseArg)
 		}
@@ -319,7 +318,8 @@ func (ipam *IPAM) Dump() (allocv4 map[string]string, allocv6 map[string]string, 
 		allocv4, st4 = ipam.IPv4Allocator.Dump()
 		st4 = "IPv4: " + st4
 		for ip := range allocv4 {
-			owner, _ := ipam.owner[ip]
+			// XXX: only consider default pool for now
+			owner, _ := ipam.owner[PoolDefault][ip]
 			// If owner is not available, report IP but leave owner empty
 			allocv4[ip] = owner
 		}
@@ -329,7 +329,8 @@ func (ipam *IPAM) Dump() (allocv4 map[string]string, allocv6 map[string]string, 
 		allocv6, st6 = ipam.IPv6Allocator.Dump()
 		st6 = "IPv6: " + st6
 		for ip := range allocv6 {
-			owner, _ := ipam.owner[ip]
+			// XXX: only consider default pool for now
+			owner, _ := ipam.owner[PoolDefault][ip]
 			// If owner is not available, report IP but leave owner empty
 			allocv6[ip] = owner
 		}

pkg/ipam/ipam.go
@@ -107,7 +107,7 @@ func NewIPAM(nodeAddressing types.NodeAddressing, c Configuration, owner Owner, 
 	ipam := &IPAM{
 		nodeAddressing:   nodeAddressing,
 		config:           c,
-		owner:            map[string]string{},
+		owner:            map[Pool]map[string]string{},
 		expirationTimers: map[string]string{},
 		excludedIPs:      map[string]string{},
 	}
@@ -157,6 +157,28 @@ func NewIPAM(nodeAddressing types.NodeAddressing, c Configuration, owner Owner, 
 	}
 
 	return ipam
+}
+
+// registerIPOwner registers a new owner for an IP in a particular pool.
+func (ipam *IPAM) registerIPOwner(ip net.IP, owner string, pool Pool) {
+	if _, ok := ipam.owner[pool]; !ok {
+		ipam.owner[pool] = make(map[string]string)
+	}
+	ipam.owner[pool][ip.String()] = owner
+}
+
+// releaseIPOwner releases ip from pool and returns the previous owner.
+func (ipam *IPAM) releaseIPOwner(ip net.IP, pool Pool) string {
+	var owner string
+	if m, ok := ipam.owner[pool]; ok {
+		ipStr := ip.String()
+		owner = m[ipStr]
+		delete(m, ipStr)
+		if len(m) == 0 {
+			delete(ipam.owner, pool)
+		}
+	}
+	return owner
 }
 
 // ExcludeIP ensures that a certain IP is never allocated. It is preferred to

pkg/ipam/types.go
@@ -83,8 +83,8 @@ type IPAM struct {
 	IPv6Allocator Allocator
 	IPv4Allocator Allocator
 
-	// owner maps an IP to the owner
-	owner map[string]string
+	// owner maps an IP to the owner per pool.
+	owner map[Pool]map[string]string
 
 	// expirationTimers is a map of all expiration timers. Each entry
 	// represents a IP allocation which is protected by an expiration

commit 2f3afb18a108db60fbb239eef7ae704965a55a00

ipam: add method to get IP owner per pool
Avoid accessing a nil map once we use more than just the default pool.

Fixes: fedeff4cbddb ("ipam: track IP owner per pool")
Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/ipam/allocator.go
@@ -319,7 +319,7 @@ func (ipam *IPAM) Dump() (allocv4 map[string]string, allocv6 map[string]string, 
 		st4 = "IPv4: " + st4
 		for ip := range allocv4 {
 			// XXX: only consider default pool for now
-			owner, _ := ipam.owner[PoolDefault][ip]
+			owner := ipam.getIPOwner(ip, PoolDefault)
 			// If owner is not available, report IP but leave owner empty
 			allocv4[ip] = owner
 		}
@@ -330,7 +330,7 @@ func (ipam *IPAM) Dump() (allocv4 map[string]string, allocv6 map[string]string, 
 		st6 = "IPv6: " + st6
 		for ip := range allocv6 {
 			// XXX: only consider default pool for now
-			owner, _ := ipam.owner[PoolDefault][ip]
+			owner := ipam.getIPOwner(ip, PoolDefault)
 			// If owner is not available, report IP but leave owner empty
 			allocv6[ip] = owner
 		}

pkg/ipam/ipam.go
@@ -159,6 +159,15 @@ func NewIPAM(nodeAddressing types.NodeAddressing, c Configuration, owner Owner, 
 	return ipam
 }
 
+// getIPOwner returns the owner for an IP in a particular pool or the empty
+// string in case the pool or IP is not registered.
+func (ipam *IPAM) getIPOwner(ip string, pool Pool) string {
+	if p, ok := ipam.owner[pool]; ok {
+		return p[ip]
+	}
+	return ""
+}
+
 // registerIPOwner registers a new owner for an IP in a particular pool.
 func (ipam *IPAM) registerIPOwner(ip net.IP, owner string, pool Pool) {
 	if _, ok := ipam.owner[pool]; !ok {