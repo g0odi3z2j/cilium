g0odi3z2j/cilium

commit d381b43648164bfe9fca57cc6505f8ef6cf409c3

checker: Add ExportedEquals checker
The updated protobuf implementation contains cyclical data structures
and unexported fields that can not be compared.

Add a new checker "ExportedEquals" that only compares exported fields
of the given data structures. This ignoring of unexported fields is
not safe for comparing arbitrary data structures which may store
internal state in unexported fields.

Use this new "ExportedEquals" checker to compare protobuf
Messages. This avoids comparing global data structures pointer to by
protobuf implementation specific fields.

Avoid infinite recursion by keeping track which pointers have already
been followed.

Change existing use of checker.Equals to use simple Equals or HasLen
instead when possible.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

pkg/checker/checker.go
@@ -22,6 +22,7 @@ import (
 	"github.com/cilium/cilium/pkg/comparator"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 
 	"gopkg.in/check.v1"
 )
@@ -138,6 +139,96 @@ func structTypes(v reflect.Value, m map[reflect.Type]struct{}) {
 		m[v.Type()] = struct{}{}
 		for i := 0; i < v.NumField(); i++ {
 			structTypes(v.Field(i), m)
+		}
+	}
+}
+
+type cmpExportedChecker struct {
+	*check.CheckerInfo
+}
+
+// ExportedEquals is a GoCheck checker that does a diff between two objects and
+// pretty-prints any difference between the two. Note that unexported struct
+// fields are NOT included in the comparison! Equals can act as a substitute
+// for DeepEquals with the exception that unexported struct fields are ignored.
+var ExportedEquals check.Checker = &cmpExportedChecker{&check.CheckerInfo{Name: "ExportedEquals", Params: cmpParams}}
+
+// Check performs a diff between two objects provided as parameters, and
+// returns either true if the objects are identical, or false otherwise. If
+// it returns false, it also returns the unified diff between the expected
+// and obtained output.
+func (checker *cmpExportedChecker) Check(params []interface{}, _ []string) (result bool, error string) {
+	if len(params) < 2 {
+		return false, "Parameter missing"
+	}
+
+	// Diff expects to receive parameters in order ("expected",
+	// "obtained"), but our convention is to pass them as
+	// ("obtained", "expected"), so reverse them here.
+	diff := cmp.Diff(params[1], params[0], DeepIgnoreUnexported(params[1], params[0]))
+
+	return diff == "", diff
+}
+
+// ExportedEqual tests whether two parameters are deeply equal, when considering
+// only exported fields, and returns true if they are. If the objects are not
+// deeply equal, then the second return value includes a json representation of
+// the difference between the parameters.
+func ExportedEqual(params ...interface{}) (bool, string) {
+	return Equals.Check(params, cmpParams)
+}
+
+func DeepIgnoreUnexported(vs ...interface{}) cmp.Option {
+	m := make(map[reflect.Type]struct{})
+	for _, v := range vs {
+		exportedStructTypes(reflect.ValueOf(v), m)
+	}
+	typs := make([]interface{}, 0, len(m))
+	for t := range m {
+		typs = append(typs, reflect.New(t).Elem().Interface())
+	}
+	return cmpopts.IgnoreUnexported(typs...)
+}
+
+func exportedStructTypes(v reflect.Value, m map[reflect.Type]struct{}) {
+	exportedUniqueStructTypes(v, m, make(map[uintptr]struct{}))
+}
+
+func exportedUniqueStructTypes(v reflect.Value, m map[reflect.Type]struct{}, s map[uintptr]struct{}) {
+	if !v.IsValid() {
+		return
+	}
+	switch v.Kind() {
+	case reflect.Ptr:
+		if !v.IsNil() {
+			addr := v.Pointer()
+			if _, found := s[addr]; found {
+				return // prevent infinite loops
+			}
+			// mark pointer as seen
+			s[addr] = struct{}{}
+			exportedUniqueStructTypes(v.Elem(), m, s)
+		}
+	case reflect.Interface:
+		if !v.IsNil() {
+			exportedUniqueStructTypes(v.Elem(), m, s)
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			exportedUniqueStructTypes(v.Index(i), m, s)
+		}
+	case reflect.Map:
+		for _, k := range v.MapKeys() {
+			exportedUniqueStructTypes(v.MapIndex(k), m, s)
+		}
+	case reflect.Struct:
+		// mark type's unexported fields to be ignored
+		m[v.Type()] = struct{}{}
+		for i := 0; i < v.NumField(); i++ {
+			// Only descend to exported fields
+			if v.Field(i).CanInterface() {
+				exportedUniqueStructTypes(v.Field(i), m, s)
+			}
 		}
 	}
 }

daemon/cmd/policy_test.go
@@ -372,7 +372,7 @@ func (ds *DaemonSuite) TestUpdateConsumerMap(c *C) {
 			{Protocol: envoy_config_core.SocketAddress_TCP},
 		},
 	}
-	c.Assert(qaBarNetworkPolicy, checker.Equals, expectedNetworkPolicy)
+	c.Assert(qaBarNetworkPolicy, checker.ExportedEquals, expectedNetworkPolicy)
 
 	prodBarNetworkPolicy := networkPolicies[ProdIPv4Addr.String()]
 	c.Assert(prodBarNetworkPolicy, Not(IsNil))
@@ -415,7 +415,7 @@ func (ds *DaemonSuite) TestUpdateConsumerMap(c *C) {
 			{Protocol: envoy_config_core.SocketAddress_TCP},
 		},
 	}
-	c.Assert(prodBarNetworkPolicy, checker.Equals, expectedNetworkPolicy)
+	c.Assert(prodBarNetworkPolicy, checker.ExportedEquals, expectedNetworkPolicy)
 }
 
 func (ds *DaemonSuite) TestL4_L7_Shadowing(c *C) {
@@ -497,7 +497,7 @@ func (ds *DaemonSuite) TestL4_L7_Shadowing(c *C) {
 			{Protocol: envoy_config_core.SocketAddress_TCP},
 		},
 	}
-	c.Assert(qaBarNetworkPolicy, checker.Equals, expectedNetworkPolicy)
+	c.Assert(qaBarNetworkPolicy, checker.ExportedEquals, expectedNetworkPolicy)
 }
 
 // HTTP rules here have no side effects, so the L4 allow-all rule is
@@ -576,7 +576,7 @@ func (ds *DaemonSuite) TestL4_L7_ShadowingShortCircuit(c *C) {
 			{Protocol: envoy_config_core.SocketAddress_TCP},
 		},
 	}
-	c.Assert(qaBarNetworkPolicy, checker.Equals, expectedNetworkPolicy)
+	c.Assert(qaBarNetworkPolicy, checker.ExportedEquals, expectedNetworkPolicy)
 }
 
 func (ds *DaemonSuite) TestL3_dependent_L7(c *C) {
@@ -677,7 +677,7 @@ func (ds *DaemonSuite) TestL3_dependent_L7(c *C) {
 			{Protocol: envoy_config_core.SocketAddress_TCP},
 		},
 	}
-	c.Assert(qaBarNetworkPolicy, checker.Equals, expectedNetworkPolicy)
+	c.Assert(qaBarNetworkPolicy, checker.ExportedEquals, expectedNetworkPolicy)
 }
 
 func (ds *DaemonSuite) TestReplacePolicy(c *C) {
@@ -951,7 +951,7 @@ func (ds *DaemonSuite) TestIncrementalPolicy(c *C) {
 			},
 		},
 	}
-	c.Assert(qaBarNetworkPolicy.IngressPerPortPolicies[0], checker.Equals, expectedIngressPolicy)
+	c.Assert(qaBarNetworkPolicy.IngressPerPortPolicies[0], checker.ExportedEquals, expectedIngressPolicy)
 
 	// Allocate identities needed for this test
 	qaFooLbls := labels.Labels{lblFoo.Key: lblFoo, lblQA.Key: lblQA}
@@ -975,7 +975,7 @@ func (ds *DaemonSuite) TestIncrementalPolicy(c *C) {
 		return qaBarNetworkPolicy != nil && len(qaBarNetworkPolicy.IngressPerPortPolicies) == 2
 	}, time.Second*1)
 	c.Assert(err, IsNil)
-	c.Assert(qaBarNetworkPolicy, checker.Equals, &cilium.NetworkPolicy{
+	c.Assert(qaBarNetworkPolicy, checker.ExportedEquals, &cilium.NetworkPolicy{
 		Name:             QAIPv4Addr.String(),
 		Policy:           uint64(qaBarSecLblsCtx.ID),
 		ConntrackMapName: "global",

pkg/envoy/server_test.go
@@ -407,44 +407,44 @@ var L4Policy2 = &policy.L4Policy{
 
 func (s *ServerSuite) TestGetHTTPRule(c *C) {
 	obtained, canShortCircuit := getHTTPRule(nil, PortRuleHTTP1, "")
-	c.Assert(obtained.Headers, checker.Equals, ExpectedHeaders1)
-	c.Assert(canShortCircuit, checker.Equals, true)
+	c.Assert(obtained.Headers, checker.ExportedEquals, ExpectedHeaders1)
+	c.Assert(canShortCircuit, Equals, true)
 }
 
 func (s *ServerSuite) TestGetPortNetworkPolicyRule(c *C) {
 	obtained, canShortCircuit := getPortNetworkPolicyRule(cachedSelector1, cachedSelector1.IsWildcard(), policy.ParserTypeHTTP, L7Rules12)
-	c.Assert(obtained, checker.Equals, ExpectedPortNetworkPolicyRule12)
-	c.Assert(canShortCircuit, checker.Equals, true)
+	c.Assert(obtained, checker.ExportedEquals, ExpectedPortNetworkPolicyRule12)
+	c.Assert(canShortCircuit, Equals, true)
 
 	obtained, canShortCircuit = getPortNetworkPolicyRule(cachedSelector1, cachedSelector1.IsWildcard(), policy.ParserTypeHTTP, L7Rules12HeaderMatch)
-	c.Assert(obtained, checker.Equals, ExpectedPortNetworkPolicyRule122HeaderMatch)
-	c.Assert(canShortCircuit, checker.Equals, false)
+	c.Assert(obtained, checker.ExportedEquals, ExpectedPortNetworkPolicyRule122HeaderMatch)
+	c.Assert(canShortCircuit, Equals, false)
 
 	obtained, canShortCircuit = getPortNetworkPolicyRule(cachedSelector2, cachedSelector2.IsWildcard(), policy.ParserTypeHTTP, L7Rules1)
-	c.Assert(obtained, checker.Equals, ExpectedPortNetworkPolicyRule1)
-	c.Assert(canShortCircuit, checker.Equals, true)
+	c.Assert(obtained, checker.ExportedEquals, ExpectedPortNetworkPolicyRule1)
+	c.Assert(canShortCircuit, Equals, true)
 }
 
 func (s *ServerSuite) TestGetDirectionNetworkPolicy(c *C) {
 	// L4+L7
 	obtained := getDirectionNetworkPolicy(ep, L4PolicyMap1, true)
-	c.Assert(obtained, checker.Equals, ExpectedPerPortPolicies12Wildcard)
+	c.Assert(obtained, checker.ExportedEquals, ExpectedPerPortPolicies12Wildcard)
 
 	// L4+L7 with header mods
 	obtained = getDirectionNetworkPolicy(ep, L4PolicyMap1HeaderMatch, true)
-	c.Assert(obtained, checker.Equals, ExpectedPerPortPolicies122HeaderMatchWildcard)
+	c.Assert(obtained, checker.ExportedEquals, ExpectedPerPortPolicies122HeaderMatchWildcard)
 
 	// L4+L7
 	obtained = getDirectionNetworkPolicy(ep, L4PolicyMap2, true)
-	c.Assert(obtained, checker.Equals, ExpectedPerPortPolicies1Wildcard)
+	c.Assert(obtained, checker.ExportedEquals, ExpectedPerPortPolicies1Wildcard)
 
 	// L4-only
 	obtained = getDirectionNetworkPolicy(ep, L4PolicyMap4, true)
-	c.Assert(obtained, checker.Equals, ExpectedPerPortPoliciesWildcard)
+	c.Assert(obtained, checker.ExportedEquals, ExpectedPerPortPoliciesWildcard)
 
 	// L4-only
 	obtained = getDirectionNetworkPolicy(ep, L4PolicyMap5, true)
-	c.Assert(obtained, checker.Equals, ExpectedPerPortPoliciesWildcard)
+	c.Assert(obtained, checker.ExportedEquals, ExpectedPerPortPoliciesWildcard)
 }
 
 func (s *ServerSuite) TestGetNetworkPolicy(c *C) {
@@ -456,7 +456,7 @@ func (s *ServerSuite) TestGetNetworkPolicy(c *C) {
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
 		ConntrackMapName:       "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyWildcard(c *C) {
@@ -468,7 +468,7 @@ func (s *ServerSuite) TestGetNetworkPolicyWildcard(c *C) {
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
 		ConntrackMapName:       "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyDeny(c *C) {
@@ -480,7 +480,7 @@ func (s *ServerSuite) TestGetNetworkPolicyDeny(c *C) {
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
 		ConntrackMapName:       "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyWildcardDeny(c *C) {
@@ -492,7 +492,7 @@ func (s *ServerSuite) TestGetNetworkPolicyWildcardDeny(c *C) {
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
 		ConntrackMapName:       "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyNil(c *C) {
@@ -504,7 +504,7 @@ func (s *ServerSuite) TestGetNetworkPolicyNil(c *C) {
 		EgressPerPortPolicies:  nil,
 		ConntrackMapName:       "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyIngressNotEnforced(c *C) {
@@ -516,7 +516,7 @@ func (s *ServerSuite) TestGetNetworkPolicyIngressNotEnforced(c *C) {
 		EgressPerPortPolicies:  ExpectedPerPortPolicies1Wildcard,
 		ConntrackMapName:       "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }
 
 func (s *ServerSuite) TestGetNetworkPolicyEgressNotEnforced(c *C) {
@@ -528,7 +528,7 @@ func (s *ServerSuite) TestGetNetworkPolicyEgressNotEnforced(c *C) {
 		EgressPerPortPolicies:  allowAllPortNetworkPolicy,
 		ConntrackMapName:       "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }
 
 var L4PolicyL7 = &policy.L4Policy{
@@ -585,7 +585,7 @@ func (s *ServerSuite) TestGetNetworkPolicyL7(c *C) {
 		IngressPerPortPolicies: ExpectedPerPortPoliciesL7,
 		ConntrackMapName:       "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }
 
 var L4PolicyKafka = &policy.L4Policy{
@@ -640,7 +640,7 @@ func (s *ServerSuite) TestGetNetworkPolicyKafka(c *C) {
 		IngressPerPortPolicies: ExpectedPerPortPoliciesKafka,
 		ConntrackMapName:       "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }
 
 var L4PolicyMySQL = &policy.L4Policy{
@@ -711,5 +711,5 @@ func (s *ServerSuite) TestGetNetworkPolicyMySQL(c *C) {
 		EgressPerPortPolicies: ExpectedPerPortPoliciesMySQL,
 		ConntrackMapName:      "global",
 	}
-	c.Assert(obtained, checker.Equals, expected)
+	c.Assert(obtained, checker.ExportedEquals, expected)
 }

pkg/fqdn/cache_test.go
@@ -572,7 +572,7 @@ func (ds *DNSCacheTestSuite) TestOverlimitEntriesWithoutLimit(c *C) {
 		cache.Update(now, "test.com", []net.IP{net.ParseIP(fmt.Sprintf("1.1.1.%d", i))}, i)
 	}
 	affectedNames, _ := cache.cleanupOverLimitEntries()
-	c.Assert(len(affectedNames), checker.Equals, 0)
+	c.Assert(affectedNames, HasLen, 0)
 	c.Assert(cache.Lookup("test.com"), HasLen, 5)
 }
 
@@ -594,7 +594,7 @@ func (ds *DNSCacheTestSuite) TestGCOverlimitAfterTTLCleanup(c *C) {
 
 	// Due all entries are deleted on TTL, the overlimit should return 0 entries.
 	affectedNames, _ := cache.cleanupOverLimitEntries()
-	c.Assert(len(affectedNames), checker.Equals, 0)
+	c.Assert(affectedNames, HasLen, 0)
 }
 
 func (ds *DNSCacheTestSuite) TestOverlimitAfterDeleteForwardEntry(c *C) {
@@ -603,7 +603,7 @@ func (ds *DNSCacheTestSuite) TestOverlimitAfterDeleteForwardEntry(c *C) {
 	dnsCache := NewDNSCache(0)
 	dnsCache.overLimit["test.com"] = true
 	affectedNames, _ := dnsCache.cleanupOverLimitEntries()
-	c.Assert(len(affectedNames), checker.Equals, 0)
+	c.Assert(affectedNames, HasLen, 0)
 }
 
 func assertZombiesContain(c *C, zombies []*DNSZombieMapping, mappings map[string][]string) {
@@ -912,7 +912,7 @@ func (ds *DNSCacheTestSuite) TestZombiesDumpAlive(c *C) {
 
 	cidrMatcher := func(ip net.IP) bool { return false }
 	alive = zombies.DumpAlive(cidrMatcher)
-	c.Assert(len(alive), checker.Equals, 0)
+	c.Assert(alive, HasLen, 0)
 
 	cidrMatcher = func(ip net.IP) bool { return true }
 	alive = zombies.DumpAlive(cidrMatcher)
@@ -941,5 +941,5 @@ func (ds *DNSCacheTestSuite) TestZombiesDumpAlive(c *C) {
 	c.Assert(err, IsNil)
 	cidrMatcher = func(ip net.IP) bool { return cidr.Contains(ip) }
 	alive = zombies.DumpAlive(cidrMatcher)
-	c.Assert(len(alive), checker.Equals, 0)
+	c.Assert(alive, HasLen, 0)
 }

pkg/ipam/allocator/podcidr/podcidr_test.go
@@ -246,7 +246,7 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_Create(c *C) {
 						op: k8sOpCreate,
 					},
 				})
-				c.Assert(atomic.LoadInt32(&reSyncCalls), checker.Equals, int32(1))
+				c.Assert(atomic.LoadInt32(&reSyncCalls), Equals, int32(1))
 			},
 			args: args{
 				node: &v2.CiliumNode{
@@ -301,7 +301,7 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_Create(c *C) {
 						op: k8sOpCreate,
 					},
 				})
-				c.Assert(atomic.LoadInt32(&reSyncCalls), checker.Equals, int32(1))
+				c.Assert(atomic.LoadInt32(&reSyncCalls), Equals, int32(1))
 			},
 			args: args{
 				node: &v2.CiliumNode{
@@ -392,7 +392,7 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_Create(c *C) {
 						op: k8sOpCreate,
 					},
 				})
-				c.Assert(atomic.LoadInt32(&reSyncCalls), checker.Equals, int32(1))
+				c.Assert(atomic.LoadInt32(&reSyncCalls), Equals, int32(1))
 			},
 			args: args{
 				node: &v2.CiliumNode{
@@ -456,7 +456,7 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_Create(c *C) {
 						op: k8sOpCreate,
 					},
 				})
-				c.Assert(atomic.LoadInt32(&reSyncCalls), checker.Equals, int32(1))
+				c.Assert(atomic.LoadInt32(&reSyncCalls), Equals, int32(1))
 			},
 			args: args{
 				node: &v2.CiliumNode{
@@ -548,7 +548,7 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_Delete(c *C) {
 						op: k8sOpDelete,
 					},
 				})
-				c.Assert(atomic.LoadInt32(&reSyncCalls), checker.Equals, int32(1))
+				c.Assert(atomic.LoadInt32(&reSyncCalls), Equals, int32(1))
 			},
 			args: args{
 				nodeName: "node-1",
@@ -565,7 +565,7 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_Delete(c *C) {
 			},
 			testPostRun: func(fields *fields) {
 				c.Assert(fields.ciliumNodesToK8s, checker.DeepEquals, map[string]*ciliumNodeK8sOp{})
-				c.Assert(atomic.LoadInt32(&reSyncCalls), checker.Equals, int32(0))
+				c.Assert(atomic.LoadInt32(&reSyncCalls), Equals, int32(0))
 			},
 			args: args{
 				nodeName: "node-1",
@@ -615,7 +615,7 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_Resync(c *C) {
 			},
 			testPostRun: func(fields *fields) {
 				time.Sleep(2 * time.Millisecond)
-				c.Assert(atomic.LoadInt32(&reSyncCalls), checker.Equals, int32(1))
+				c.Assert(atomic.LoadInt32(&reSyncCalls), Equals, int32(1))
 			},
 		},
 	}
@@ -1010,13 +1010,13 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_allocateIPNets(c *C) {
 						v6PodCIDRs: mustNewCIDRs("fd00::/80"),
 					},
 				})
-				c.Assert(onIsAllocatedCallsv4, checker.Equals, 1)
-				c.Assert(onOccupyCallsv4, checker.Equals, 1)
-				c.Assert(releaseCallsv4, checker.Equals, 0)
+				c.Assert(onIsAllocatedCallsv4, Equals, 1)
+				c.Assert(onOccupyCallsv4, Equals, 1)
+				c.Assert(releaseCallsv4, Equals, 0)
 
-				c.Assert(onIsAllocatedCallsv6, checker.Equals, 1)
-				c.Assert(onOccupyCallsv6, checker.Equals, 1)
-				c.Assert(releaseCallsv6, checker.Equals, 0)
+				c.Assert(onIsAllocatedCallsv6, Equals, 1)
+				c.Assert(onOccupyCallsv6, Equals, 1)
+				c.Assert(releaseCallsv6, Equals, 0)
 			},
 			args: args{
 				nodeName: "node-1",
@@ -1076,13 +1076,13 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_allocateIPNets(c *C) {
 			},
 			testPostRun: func(fields *fields) {
 				c.Assert(fields.nodes, checker.DeepEquals, map[string]*nodeCIDRs{})
-				c.Assert(onIsAllocatedCallsv4, checker.Equals, 1)
-				c.Assert(onOccupyCallsv4, checker.Equals, 1)
-				c.Assert(releaseCallsv4, checker.Equals, 1)
+				c.Assert(onIsAllocatedCallsv4, Equals, 1)
+				c.Assert(onOccupyCallsv4, Equals, 1)
+				c.Assert(releaseCallsv4, Equals, 1)
 
-				c.Assert(onIsAllocatedCallsv6, checker.Equals, 0)
-				c.Assert(onOccupyCallsv6, checker.Equals, 0)
-				c.Assert(releaseCallsv6, checker.Equals, 0)
+				c.Assert(onIsAllocatedCallsv6, Equals, 0)
+				c.Assert(onOccupyCallsv6, Equals, 0)
+				c.Assert(releaseCallsv6, Equals, 0)
 			},
 			args: args{
 				nodeName: "node-1",
@@ -1195,7 +1195,7 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_allocateIPNets(c *C) {
 						v6PodCIDRs: mustNewCIDRs("fd00::/80"),
 					},
 				})
-				c.Assert(onAllocateNextv6, checker.Equals, 1)
+				c.Assert(onAllocateNextv6, Equals, 1)
 			},
 			args: args{
 				nodeName: "node-1",
@@ -1216,8 +1216,8 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_allocateIPNets(c *C) {
 		}
 		newNodeCIDRs, gotAllocated, err := n.reuseIPNets(tt.args.nodeName, tt.args.v4CIDR, tt.args.v6CIDR)
 		gotErr := err != nil
-		c.Assert(gotErr, checker.Equals, tt.wantErr, Commentf("Test Name: %s", tt.name))
-		c.Assert(gotAllocated, checker.Equals, tt.wantAllocated, Commentf("Test Name: %s", tt.name))
+		c.Assert(gotErr, Equals, tt.wantErr, Commentf("Test Name: %s", tt.name))
+		c.Assert(gotAllocated, Equals, tt.wantAllocated, Commentf("Test Name: %s", tt.name))
 		c.Assert(newNodeCIDRs, checker.DeepEquals, tt.fields.newNodeCIDRs, Commentf("Test Name: %s", tt.name))
 
 		if tt.testPostRun != nil {
@@ -1327,8 +1327,8 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_allocateNext(c *C) {
 						v6PodCIDRs: mustNewCIDRs("fd00::/80"),
 					},
 				})
-				c.Assert(allocateNextCallsv4, checker.Equals, 1)
-				c.Assert(allocateNextCallsv6, checker.Equals, 1)
+				c.Assert(allocateNextCallsv4, Equals, 1)
+				c.Assert(allocateNextCallsv6, Equals, 1)
 			},
 			args: args{
 				nodeName: "node-1",
@@ -1382,8 +1382,8 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_allocateNext(c *C) {
 			},
 			testPostRun: func(fields *fields) {
 				c.Assert(fields.nodes, checker.DeepEquals, map[string]*nodeCIDRs{})
-				c.Assert(allocateNextCallsv4, checker.Equals, 1)
-				c.Assert(releaseCallsv4, checker.Equals, 1)
+				c.Assert(allocateNextCallsv4, Equals, 1)
+				c.Assert(releaseCallsv4, Equals, 1)
 			},
 			args: args{
 				nodeName: "node-1",
@@ -1421,7 +1421,7 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_allocateNext(c *C) {
 		nodeCIDRs, gotAllocated, err := n.allocateNext(tt.args.nodeName)
 		c.Assert(err, checker.DeepEquals, tt.wantErr, Commentf("Test Name: %s", tt.name))
 		c.Assert(nodeCIDRs, checker.DeepEquals, tt.nodeCIDRs, Commentf("Test Name: %s", tt.name))
-		c.Assert(gotAllocated, checker.Equals, tt.wantAllocated, Commentf("Test Name: %s", tt.name))
+		c.Assert(gotAllocated, Equals, tt.wantAllocated, Commentf("Test Name: %s", tt.name))
 
 		if tt.testPostRun != nil {
 			tt.testPostRun(tt.fields)
@@ -1487,8 +1487,8 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_releaseIPNets(c *C) {
 				}
 			},
 			testPostRun: func(fields *fields) {
-				c.Assert(len(fields.nodes), checker.Equals, 0)
-				c.Assert(onReleaseCalls, checker.Equals, 1)
+				c.Assert(fields.nodes, HasLen, 0)
+				c.Assert(onReleaseCalls, Equals, 1)
 			},
 			args: args{
 				nodeName: "node-1",
@@ -1522,8 +1522,8 @@ func (s *PodCIDRSuite) TestNodesPodCIDRManager_releaseIPNets(c *C) {
 				}
 			},
 			testPostRun: func(fields *fields) {
-				c.Assert(len(fields.nodes), checker.Equals, 0)
-				c.Assert(onReleaseCalls, checker.Equals, 1)
+				c.Assert(fields.nodes, HasLen, 0)
+				c.Assert(onReleaseCalls, Equals, 1)
 			},
 			args: args{
 				nodeName: "node-1",
@@ -1634,7 +1634,7 @@ func (s *PodCIDRSuite) Test_parsePodCIDRs(c *C) {
 	for _, tt := range tests {
 		nodeCIDRs, err := parsePodCIDRs(tt.args.podCIDRs)
 		gotErr := err != nil
-		c.Assert(gotErr, checker.Equals, tt.wantErr, Commentf("Test Name: %s", tt.name))
+		c.Assert(gotErr, Equals, tt.wantErr, Commentf("Test Name: %s", tt.name))
 		c.Assert(nodeCIDRs, checker.DeepEquals, tt.want, Commentf("Test Name: %s", tt.name))
 	}
 }
@@ -1732,7 +1732,7 @@ func (s *PodCIDRSuite) Test_syncToK8s(c *C) {
 					},
 					OnGet: func(nodeName string) (node *v2.CiliumNode, err error) {
 						calls[k8sOpGet]++
-						c.Assert(nodeName, checker.Equals, "node-1")
+						c.Assert(nodeName, Equals, "node-1")
 						return &v2.CiliumNode{
 							ObjectMeta: v1.ObjectMeta{
 								Name: "node-1",
@@ -1821,7 +1821,7 @@ func (s *PodCIDRSuite) Test_syncToK8s(c *C) {
 					},
 					OnGet: func(nodeName string) (node *v2.CiliumNode, err error) {
 						calls[k8sOpGet]++
-						c.Assert(nodeName, checker.Equals, "node-1")
+						c.Assert(nodeName, Equals, "node-1")
 						return nil, &k8sErrors.StatusError{
 							ErrStatus: v1.Status{
 								Reason: v1.StatusReasonNotFound,
@@ -2043,7 +2043,7 @@ func (s *PodCIDRSuite) Test_syncToK8s(c *C) {
 	for _, tt := range tests {
 		tt.testSetup()
 		gotErr := syncToK8s(tt.args.nodeGetter, tt.args.ciliumNodesToK8s) != nil
-		c.Assert(gotErr, checker.Equals, tt.wantErr, Commentf("Test Name: %s", tt.name))
+		c.Assert(gotErr, Equals, tt.wantErr, Commentf("Test Name: %s", tt.name))
 		if tt.testPostRun != nil {
 			tt.testPostRun(tt.args)
 		}
@@ -2076,7 +2076,7 @@ func (s *PodCIDRSuite) TestNewNodesPodCIDRManager(c *C) {
 	nm.k8sReSync.Trigger()
 	// Waiting 2 times the amount of time set in the trigger
 	time.Sleep(2 * time.Second)
-	c.Assert(onDeleteCalls, checker.Equals, 0)
+	c.Assert(onDeleteCalls, Equals, 0)
 
 	nm.Mutex.Lock()
 	nm.ciliumNodesToK8s = map[string]*ciliumNodeK8sOp{
@@ -2096,5 +2096,5 @@ func (s *PodCIDRSuite) TestNewNodesPodCIDRManager(c *C) {
 	// Wait for the controller to try more times, the number of deletedCalls
 	// should not be different because we have successfully deleted the node.
 	time.Sleep(2 * time.Second)
-	c.Assert(onDeleteCalls, checker.Equals, 2)
+	c.Assert(onDeleteCalls, Equals, 2)
 }

pkg/policy/visibility_test.go
@@ -136,7 +136,7 @@ func (ds *PolicyTestSuite) TestVisibilityPolicyCreation(c *C) {
 	anno = "<Egress/53/ANY/DNS>"
 	vp, err = NewVisibilityPolicy(anno)
 	c.Assert(err, IsNil)
-	c.Assert(len(vp.Egress), checker.Equals, 2)
+	c.Assert(vp.Egress, HasLen, 2)
 	udp, ok := vp.Egress["53/UDP"]
 	c.Assert(ok, Equals, true)
 	c.Assert(udp.Proto, Equals, u8proto.UDP)

commit 86e4b524a0eb54b96f79da30aeb18cbdbb59b8e5

checker: Fix incorrect checker for ExportedEqual()
When using checker.ExportedEqual(), it was using the standard Equals
checker under-the-hood, but this is incorrect. Fix it to use the correct
checker.

In the commit introducing the bug, there was no direct usage of
checker.ExportedEqual(), but rather checker.ExportedEqual (note the
plural). Subtle!

Discovered while working on improving policy unit tests.

Fixes: d381b436481 ("checker: Add ExportedEquals checker")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/checker/checker.go
@@ -163,7 +163,7 @@ func (checker *cmpExportedChecker) Check(params []interface{}, _ []string) (resu
 // deeply equal, then the second return value includes a json representation of
 // the difference between the parameters.
 func ExportedEqual(params ...interface{}) (bool, string) {
-	return Equals.Check(params, cmpParams)
+	return ExportedEquals.Check(params, cmpParams)
 }
 
 func DeepIgnoreUnexported(vs ...interface{}) cmp.Option {