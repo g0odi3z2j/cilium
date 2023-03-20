g0odi3z2j/cilium

commit 68e827cee4079e7f512018ec7c963a7b994a4009

coccinelle: Run coccicheck with GitHub Actions
Runs coccicheck as a GitHub Action on all pull request synchronize and
open events, with Coccinelle packaged as a Docker image based on Alpine.

Signed-off-by: Paul Chaignon <paul@cilium.io>

contrib/coccinelle/check-cocci.sh
@@ -1,0 +1,4 @@
+#!/bin/bash
+
+make -C bpf coccicheck | tee /tmp/stdout
+exit $(grep -c "^* file " /tmp/stdout)

.github/workflows/coccicheck.yaml
@@ -1,0 +1,12 @@
+name: BPF checks
+
+on:
+  - pull_request
+
+jobs:
+    coccicheck:
+        name: coccicheck
+        runs-on: ubuntu-latest
+        steps:
+            - uses: actions/checkout@v2
+            - uses: docker://cilium/coccicheck:latest

contrib/coccinelle/Dockerfile
@@ -1,0 +1,19 @@
+FROM docker.io/library/alpine:3.11
+
+LABEL maintainer="maintainer@cilium.io"
+
+ENV COCCINELLE_VERSION 1.0.8
+
+RUN apk add -t .build_apks curl autoconf automake gcc libc-dev ocaml ocaml-dev ocaml-ocamldoc ocaml-findlib && \
+    apk add make python bash && \
+    curl -sS -L https://github.com/coccinelle/coccinelle/archive/$COCCINELLE_VERSION.tar.gz -o coccinelle.tar.gz && \
+    tar xvzf coccinelle.tar.gz && rm coccinelle.tar.gz && \
+    cd coccinelle-$COCCINELLE_VERSION && \
+    ./autogen && \
+    ./configure --disable-ocaml --disable-pcre-syntax && \
+    make && make install-spatch install-python && \
+    cd .. && rm -r coccinelle-$COCCINELLE_VERSION && \
+    strip `which spatch` && \
+    apk del .build_apks
+
+ENTRYPOINT ["./contrib/coccinelle/check-cocci.sh"]

commit 5cc541444bb3fc18d67aca957427a8c729745832

cocci: Make check-cocci.sh fail loudly
We recently discovered that coccinelle checks have been failing locally
and in CI for some (yet unknown) time, because of a Python import error.
Ideally, the CI should have caught it. But the failures have been
silent, because check-cocci.sh only returns an error if a coccicheck
raises an issue, not if the checks fail to launch.

To ensure we catch similar errors in the future, set errexit and
pipefail in check-cocci.sh, to make sure that we get an error on
failures to run "make -C bpf coccicheck".

Fixes: 68e827cee407 ("coccinelle: Run coccicheck with GitHub Actions")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

contrib/coccinelle/check-cocci.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -o errexit
+set -o pipefail
+
 make -C bpf coccicheck | tee /tmp/stdout
 exit $(grep -c "^* file " /tmp/stdout)