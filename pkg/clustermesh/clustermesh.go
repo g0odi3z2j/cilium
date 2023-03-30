g0odi3z2j/cilium

commit 1898cae0be4fc76273b2ff8ef832d89a01e2d35d

clustermesh: Add helper functions to set/get cluster configuration
Add hepler functions to set/get cluster information on kvstore.

Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

pkg/clustermesh/clustermesh.go
@@ -5,13 +5,16 @@ package clustermesh
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"path"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/allocator"
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/kvstore"
@@ -71,6 +74,43 @@ type Configuration struct {
 	// NodesSharedKeyDeleteDelay is the delay before a shared node delete event
 	// is handled. This parameter is optional.
 	NodesSharedKeyDeleteDelay *time.Duration
+}
+
+func SetClusterConfig(clusterName string, config *cmtypes.CiliumClusterConfig, backend kvstore.BackendOperations) error {
+	key := path.Join(kvstore.ClusterConfigPrefix, clusterName)
+
+	val, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	_, err = kvstore.Client().UpdateIfDifferent(ctx, key, val, true)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetClusterConfig(clusterName string, backend kvstore.BackendOperations) (*cmtypes.CiliumClusterConfig, error) {
+	var config cmtypes.CiliumClusterConfig
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	val, err := backend.Get(ctx, path.Join(kvstore.ClusterConfigPrefix, clusterName))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(val, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
 }
 
 // RemoteIdentityWatcher is any type which provides identities that have been

commit 156608a8bb31c9a08d5ade27fc888802a54f09b5

clustermesh: fix client usage in SetClusterConfig
Previously, the SetClusterConfig function always used the default kvstore
client to update the cluster configuration, neglecting the one specified
as parameter. This commit fixes it.

Fixes: 1898cae0be4f ("clustermesh: Add helper functions to set/get cluster configuration")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/clustermesh/clustermesh.go
@@ -79,7 +79,7 @@ func SetClusterConfig(clusterName string, config *cmtypes.CiliumClusterConfig, b
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	_, err = kvstore.Client().UpdateIfDifferent(ctx, key, val, true)
+	_, err = backend.UpdateIfDifferent(ctx, key, val, true)
 	if err != nil {
 		return err
 	}