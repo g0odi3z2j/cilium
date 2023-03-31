g0odi3z2j/cilium

commit 69e750ea8b55c848be0a1d574864c1f93cb44de3

operator: Refactor cilium identities GC to a cell
Add a new cell.Module to implement the CiliumIdentity GC.

This improves the testability of the GC code through unit tests, without
relying on more complicated (and slow) controlplane tests.
Besides, the modularization of the GC cleans up the operator bootstrap
code, since all the GC specific parts are now moved to their cell.

The cell supports both GC modes: CRD allocated identities or kvstore
allocated identities.
The code is moved inside the `identity` package and has the following
structure:

operator/identity/
├── cell.go
├── crd_gc.go
├── gc.go
├── heartbeat.go
└── kvstore_gc.go

where:

- `cell.go` contains all the exported API for the new cell, alongside
  its dependencies and configuration.
- `gc.go` contains the common part of both gc modes.
- `crd_gc.go` and `heartbeat.go` contain the CRD allocation mode
  specific GC part.
- `kvstore`contains the kvstore allocation mode specific GC part.

The new cell relies on `resource.Resource[*v2.CiliumIdentity]` to listen
for CiliumIdentity related events and react accordingly.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

operator/cmd/root.go
@@ -24,6 +24,7 @@ import (
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 
 	"github.com/cilium/cilium/operator/api"
+	"github.com/cilium/cilium/operator/identity"
 	operatorMetrics "github.com/cilium/cilium/operator/metrics"
 	operatorOption "github.com/cilium/cilium/operator/option"
 	ces "github.com/cilium/cilium/operator/pkg/ciliumendpointslice"
@@ -49,7 +50,6 @@ import (
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/pprof"
 	"github.com/cilium/cilium/pkg/rand"
-	"github.com/cilium/cilium/pkg/rate"
 	"github.com/cilium/cilium/pkg/version"
 )
 
@@ -68,18 +68,6 @@ var (
 	// Use a Go context so we can tell the leaderelection code when we
 	// want to step down
 	leaderElectionCtx, leaderElectionCtxCancel = context.WithCancel(context.Background())
-
-	// identityRateLimiter is a rate limiter to rate limit the number of
-	// identities being GCed by the operator. See the documentation of
-	// rate.Limiter to understand its difference than 'x/time/rate.Limiter'.
-	//
-	// With our rate.Limiter implementation Cilium will be able to handle bursts
-	// of identities being garbage collected with the help of the functionality
-	// provided by the 'policy-trigger-interval' in the cilium-agent. With the
-	// policy-trigger even if we receive N identity changes over the interval
-	// set, Cilium will only need to process all of them at once instead of
-	// processing each one individually.
-	identityRateLimiter *rate.Limiter
 
 	operatorAddr string
 
@@ -101,10 +89,28 @@ var (
 			return option.Config
 		}),
 
+		cell.Provide(func() *operatorOption.OperatorConfig {
+			return operatorOption.Config
+		}),
+
+		cell.Provide(func(
+			daemonCfg *option.DaemonConfig,
+			operatorCfg *operatorOption.OperatorConfig,
+		) identity.GCSharedConfig {
+			return identity.GCSharedConfig{
+				IdentityAllocationMode: daemonCfg.IdentityAllocationMode,
+				EnableMetrics:          operatorCfg.EnableMetrics,
+				ClusterName:            daemonCfg.LocalClusterName(),
+				K8sNamespace:           daemonCfg.CiliumNamespaceName(),
+				ClusterID:              daemonCfg.LocalClusterID(),
+			}
+		}),
+
 		// These cells are started only after the operator is elected leader.
 		WithLeaderLifecycle(
 			k8s.SharedResourcesCell,
 			lbipam.Cell,
+			identity.GCCell,
 
 			legacyCell,
 		),
@@ -621,33 +627,12 @@ func (legacy *legacyOnLeader) onStart(_ hive.HookContext) error {
 		nodeManager.Resync(legacy.ctx, time.Time{})
 	}
 
-	if operatorOption.Config.IdentityGCInterval != 0 {
-		identityRateLimiter = rate.NewLimiter(
-			operatorOption.Config.IdentityGCRateInterval,
-			operatorOption.Config.IdentityGCRateLimit,
-		)
-		legacy.wg.Add(1)
-		go func() {
-			defer legacy.wg.Done()
-			<-legacy.ctx.Done()
-			identityRateLimiter.Stop()
-		}()
-	}
-
-	switch option.Config.IdentityAllocationMode {
-	case option.IdentityAllocationModeCRD:
+	if option.Config.IdentityAllocationMode == option.IdentityAllocationModeCRD {
 		if !legacy.clientset.IsEnabled() {
 			log.Fatal("CRD Identity allocation mode requires k8s to be configured.")
 		}
-
-		startManagingK8sIdentities(legacy.ctx, &legacy.wg, legacy.clientset)
-
-		if operatorOption.Config.IdentityGCInterval != 0 {
-			go startCRDIdentityGC(legacy.ctx, &legacy.wg, legacy.clientset)
-		}
-	case option.IdentityAllocationModeKVstore:
-		if operatorOption.Config.IdentityGCInterval != 0 {
-			startKvstoreIdentityGC()
+		if operatorOption.Config.EndpointGCInterval == 0 {
+			log.Fatal("Cilium Identity garbage collector requires the CiliumEndpoint garbage collector to be enabled")
 		}
 	}
 

operator/cmd/flags.go
@@ -191,9 +191,6 @@ func init() {
 		return nil
 	}
 
-	flags.Duration(operatorOption.IdentityHeartbeatTimeout, 2*defaults.KVstoreLeaseTTL, "Timeout after which identity expires on lack of heartbeat")
-	option.BindEnv(Vp, operatorOption.IdentityHeartbeatTimeout)
-
 	flags.Bool(option.EnableIPv4Name, defaults.EnableIPv4, "Enable IPv4 support")
 	option.BindEnv(Vp, option.EnableIPv4Name)
 
@@ -226,17 +223,6 @@ func init() {
 
 	flags.String(option.IdentityAllocationMode, option.IdentityAllocationModeKVstore, "Method to use for identity allocation")
 	option.BindEnv(Vp, option.IdentityAllocationMode)
-
-	flags.Duration(operatorOption.IdentityGCInterval, defaults.KVstoreLeaseTTL, "GC interval for security identities")
-	option.BindEnv(Vp, operatorOption.IdentityGCInterval)
-
-	flags.Duration(operatorOption.IdentityGCRateInterval, time.Minute,
-		"Interval used for rate limiting the GC of security identities")
-	option.BindEnv(Vp, operatorOption.IdentityGCRateInterval)
-
-	flags.Int(operatorOption.IdentityGCRateLimit, 2500,
-		fmt.Sprintf("Maximum number of security identities that will be deleted within the %s", operatorOption.IdentityGCRateInterval))
-	option.BindEnv(Vp, operatorOption.IdentityGCRateLimit)
 
 	flags.String(option.KVStore, "", "Key-value store type")
 	option.BindEnv(Vp, option.KVStore)

operator/cmd/identity_gc.go
@@ -1,90 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-package cmd
-
-import (
-	"time"
-
-	"github.com/sirupsen/logrus"
-
-	"github.com/cilium/cilium/operator/metrics"
-	operatorOption "github.com/cilium/cilium/operator/option"
-	"github.com/cilium/cilium/pkg/allocator"
-	"github.com/cilium/cilium/pkg/identity"
-	"github.com/cilium/cilium/pkg/identity/cache"
-	"github.com/cilium/cilium/pkg/idpool"
-	"github.com/cilium/cilium/pkg/inctimer"
-	"github.com/cilium/cilium/pkg/kvstore"
-	kvstoreallocator "github.com/cilium/cilium/pkg/kvstore/allocator"
-	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/option"
-)
-
-func startKvstoreIdentityGC() {
-	log.WithField(logfields.Interval, operatorOption.Config.IdentityGCInterval).Info("Starting kvstore identity garbage collector")
-	backend, err := kvstoreallocator.NewKVStoreBackend(cache.IdentitiesPath, "", nil, kvstore.Client())
-	if err != nil {
-		log.WithError(err).Fatal("Unable to initialize kvstore backend for identity allocation")
-	}
-
-	identity.InitMinMaxIdentityAllocation(option.Config)
-
-	minID := idpool.ID(identity.MinimalAllocationIdentity)
-	maxID := idpool.ID(identity.MaximumAllocationIdentity)
-
-	log.WithFields(map[string]interface{}{
-		"min":        minID,
-		"max":        maxID,
-		"cluster-id": option.Config.ClusterID,
-	}).Info("Garbage Collecting identities between range")
-	a := allocator.NewAllocatorForGC(backend, allocator.WithMin(minID), allocator.WithMax(maxID))
-
-	successfulRuns := 0
-	failedRuns := 0
-	keysToDelete := map[string]uint64{}
-	go func() {
-		gcTimer, gcTimerDone := inctimer.New()
-		defer gcTimerDone()
-		for {
-			now := time.Now()
-			keysToDelete2, gcStats, err := a.RunGC(identityRateLimiter, keysToDelete)
-			gcDuration := time.Since(now)
-			if err != nil {
-				log.WithError(err).Warning("Unable to run security identity garbage collector")
-
-				if operatorOption.Config.EnableMetrics {
-					failedRuns++
-					metrics.IdentityGCRuns.WithLabelValues(metrics.LabelValueOutcomeFail).Set(float64(failedRuns))
-				}
-			} else {
-				keysToDelete = keysToDelete2
-
-				if operatorOption.Config.EnableMetrics {
-					successfulRuns++
-					metrics.IdentityGCRuns.WithLabelValues(metrics.LabelValueOutcomeSuccess).Set(float64(successfulRuns))
-
-					metrics.IdentityGCSize.WithLabelValues("alive").Set(float64(gcStats.Alive))
-					metrics.IdentityGCSize.WithLabelValues("deleted").Set(float64(gcStats.Deleted))
-				}
-			}
-
-			sleep := operatorOption.Config.IdentityGCInterval
-			if operatorOption.Config.IdentityGCInterval <= gcDuration {
-				log.WithFields(logrus.Fields{
-					logfields.Interval: operatorOption.Config.IdentityGCInterval,
-					logfields.Duration: gcDuration,
-					logfields.Hint:     "Is there a ratelimit configured on the kvstore client or server?",
-				}).Warning("Identity garbage collection took longer than the GC interval")
-				// Don't sleep because we have a lot of work to do.
-			} else {
-				sleep -= gcDuration
-				<-gcTimer.After(sleep)
-			}
-
-			log.WithFields(logrus.Fields{
-				"identities-to-delete": keysToDelete,
-			}).Debug("Will delete identities if they are still unused")
-		}
-	}()
-}

operator/cmd/k8s_identity.go
@@ -1,247 +1,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Authors of Cilium
-
-package cmd
-
-import (
-	"context"
-	"sync"
-	"time"
-
-	"github.com/sirupsen/logrus"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/tools/cache"
-
-	"github.com/cilium/cilium/operator/identity"
-	"github.com/cilium/cilium/operator/metrics"
-	operatorOption "github.com/cilium/cilium/operator/option"
-	"github.com/cilium/cilium/operator/watchers"
-	"github.com/cilium/cilium/pkg/controller"
-	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
-	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
-	"github.com/cilium/cilium/pkg/k8s/identitybackend"
-	"github.com/cilium/cilium/pkg/k8s/informer"
-	"github.com/cilium/cilium/pkg/k8s/utils"
-	"github.com/cilium/cilium/pkg/logging/logfields"
-)
-
-var identityStore cache.Store
-
-// deleteIdentity deletes an identity. It includes the resource version and
-// will error if the object has since been changed.
-func deleteIdentity(ctx context.Context, clientset k8sClient.Clientset, identity *v2.CiliumIdentity) error {
-	// Wait until we can delete an identity
-	err := identityRateLimiter.Wait(ctx)
-	if err != nil {
-		return err
-	}
-	err = clientset.CiliumV2().CiliumIdentities().Delete(
-		ctx,
-		identity.Name,
-		metav1.DeleteOptions{
-			Preconditions: &metav1.Preconditions{
-				UID:             &identity.UID,
-				ResourceVersion: &identity.ResourceVersion,
-			},
-		})
-	if err != nil {
-		log.WithError(err).Error("Unable to delete identity")
-	} else {
-		log.WithField(logfields.Identity, identity.GetName()).Info("Garbage collected identity")
-	}
-
-	return err
-}
-
-func updateIdentity(ctx context.Context, clientset k8sClient.Clientset, identity *v2.CiliumIdentity) error {
-	_, err := clientset.CiliumV2().CiliumIdentities().Update(
-		ctx,
-		identity,
-		metav1.UpdateOptions{})
-	if err != nil {
-		log.WithError(err).Error("Updating Identity")
-	} else {
-		log.WithField(logfields.Identity, identity.GetName()).Debug("Updated identity")
-	}
-
-	return err
-}
-
-var identityHeartbeat *identity.IdentityHeartbeatStore
-
-// counters for GC failed/successful runs
-var (
-	failedRuns     = 0
-	successfulRuns = 0
-)
-
-// identityGCIteration is a single iteration of a garbage collection. It will
-// delete identities that have not had its heartbeat lifesign updated since
-// option.Config.IdentityHeartbeatTimeout
-func identityGCIteration(ctx context.Context, clientset k8sClient.Clientset) {
-	log.Debug("Running CRD identity garbage collector")
-
-	if identityStore == nil {
-		log.Debug("Identity store cache is not ready yet")
-		return
-	}
-	select {
-	case <-watchers.CiliumEndpointsSynced:
-	case <-ctx.Done():
-		return
-	}
-
-	identityStoreList := identityStore.List()
-	totalEntries := len(identityStoreList)
-	deletedEntries := 0
-
-	timeNow := time.Now()
-	for _, identityObject := range identityStoreList {
-		identity, ok := identityObject.(*v2.CiliumIdentity)
-		if !ok {
-			log.WithField(logfields.Object, identityObject).
-				Errorf("Saw %T object while expecting k8s/types.Identity", identityObject)
-			continue
-		}
-
-		// The identity is definitely alive if there's a CE using it.
-		if watchers.HasCEWithIdentity(identity.Name) {
-			// If the identity is alive then mark it as alive
-			identityHeartbeat.MarkAlive(identity.Name, timeNow)
-			continue
-		}
-		if !identityHeartbeat.IsAlive(identity.Name) {
-			ts, ok := identity.Annotations[identitybackend.HeartBeatAnnotation]
-			if !ok {
-				identity = identity.DeepCopy()
-				if identity.Annotations == nil {
-					identity.Annotations = make(map[string]string)
-				}
-				log.WithFields(logrus.Fields{
-					logfields.Identity: identity.Name,
-					logfields.K8sUID:   identity.UID,
-				}).Info("Marking identity for later deletion")
-				identity.Annotations[identitybackend.HeartBeatAnnotation] = timeNow.Format(time.RFC3339Nano)
-				err := updateIdentity(ctx, clientset, identity)
-				if err != nil {
-					log.WithError(err).
-						WithField(logfields.Identity, identity).
-						Error("Marking identity for later deletion")
-				}
-				continue
-			}
-
-			log.WithFields(logrus.Fields{
-				logfields.Identity: identity,
-			}).Debugf("Deleting unused identity; marked for deletion at %s", ts)
-			if err := deleteIdentity(ctx, clientset, identity); err != nil {
-				log.WithError(err).WithFields(logrus.Fields{
-					logfields.Identity: identity,
-				}).Error("Deleting unused identity")
-				// If Context was canceled we should break
-				if ctx.Err() != nil {
-					break
-				}
-			} else {
-				deletedEntries++
-			}
-		}
-	}
-
-	if operatorOption.Config.EnableMetrics {
-		if ctx.Err() == nil {
-			successfulRuns++
-			metrics.IdentityGCRuns.WithLabelValues(metrics.LabelValueOutcomeSuccess).Set(float64(successfulRuns))
-		} else {
-			failedRuns++
-			metrics.IdentityGCRuns.WithLabelValues(metrics.LabelValueOutcomeFail).Set(float64(failedRuns))
-		}
-		aliveEntries := totalEntries - deletedEntries
-		metrics.IdentityGCSize.WithLabelValues(metrics.LabelValueOutcomeAlive).Set(float64(aliveEntries))
-		metrics.IdentityGCSize.WithLabelValues(metrics.LabelValueOutcomeDeleted).Set(float64(deletedEntries))
-	}
-
-	identityHeartbeat.GC()
-}
-
-func startCRDIdentityGC(ctx context.Context, wg *sync.WaitGroup, clientset k8sClient.Clientset) {
-	if operatorOption.Config.EndpointGCInterval == 0 {
-		log.Fatal("The CiliumIdentity garbage collector requires the CiliumEndpoint garbage collector to be enabled")
-	}
-
-	log.WithField(logfields.Interval, operatorOption.Config.IdentityGCInterval).Info("Starting CRD identity garbage collector")
-
-	mgr := controller.NewManager()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		<-ctx.Done()
-		mgr.RemoveAllAndWait()
-	}()
-
-	mgr.UpdateController("crd-identity-gc",
-		controller.ControllerParams{
-			RunInterval: operatorOption.Config.IdentityGCInterval,
-			DoFunc: func(ctx context.Context) error {
-				identityGCIteration(ctx, clientset)
-				return ctx.Err()
-			},
-		})
-}
-
-func startManagingK8sIdentities(ctx context.Context, wg *sync.WaitGroup, clientset k8sClient.Clientset) {
-	identityHeartbeat = identity.NewIdentityHeartbeatStore(operatorOption.Config.IdentityHeartbeatTimeout)
-
-	identityStore = cache.NewStore(cache.DeletionHandlingMetaNamespaceKeyFunc)
-	identityInformer := informer.NewInformerWithStore(
-		utils.ListerWatcherFromTyped[*v2.CiliumIdentityList](clientset.CiliumV2().CiliumIdentities()),
-		&v2.CiliumIdentity{},
-		0,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				if identity, ok := obj.(*v2.CiliumIdentity); ok {
-					// A new identity is always alive
-					identityHeartbeat.MarkAlive(identity.Name, time.Now())
-				}
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				if oldIdty, ok := oldObj.(*v2.CiliumIdentity); ok {
-					if newIdty, ok := newObj.(*v2.CiliumIdentity); ok {
-						if oldIdty.DeepEqual(newIdty) {
-							return
-						}
-						// Any update to the identity marks it as alive
-						identityHeartbeat.MarkAlive(newIdty.Name, time.Now())
-					}
-				}
-			},
-			DeleteFunc: func(obj interface{}) {
-				identity, ok := obj.(*v2.CiliumIdentity)
-				if !ok {
-					deletedObj, ok := obj.(cache.DeletedFinalStateUnknown)
-					if ok {
-						identity, ok = deletedObj.Obj.(*v2.CiliumIdentity)
-					}
-					if !ok {
-						return
-					}
-				}
-				// When the identity is deleted, delete the
-				// heartbeat entry as well. This will not be
-				// 100% accurate as the CiliumEndpoint can live
-				// longer than the CiliumIdentity. See
-				// identityHeartbeat.GC()
-				identityHeartbeat.Delete(identity.Name)
-			},
-		},
-		nil,
-		identityStore,
-	)
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		identityInformer.Run(ctx.Done())
-	}()
-}

operator/identity/cell.go
@@ -1,0 +1,96 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package identity
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/pflag"
+
+	"github.com/cilium/cilium/pkg/defaults"
+	"github.com/cilium/cilium/pkg/hive/cell"
+)
+
+const (
+	// GCInterval is the interval in which allocator identities are
+	// attempted to be collected
+	GCInterval = "identity-gc-interval"
+
+	// GCRateInterval is the interval used for rate limiting the GC of
+	// identities.
+	GCRateInterval = "identity-gc-rate-interval"
+
+	// GCRateLimit is the maximum identities used for rate limiting the
+	// GC of identities.
+	GCRateLimit = "identity-gc-rate-limit"
+
+	// HeartbeatTimeout is the timeout used to GC identities from k8s
+	HeartbeatTimeout = "identity-heartbeat-timeout"
+)
+
+// GCCell is a cell that implements a periodic Cilium identities
+// garbage collector.
+// The GC subscribes to identities events to mark all the related identities
+// as alive. If an identity has no activity for a prolonged interval,
+// it is first marked for deletion and eventually deleted.
+var GCCell = cell.Module(
+	"k8s-identities-gc",
+	"Cilium identities garbage collector",
+
+	cell.Config(defaultGCConfig),
+
+	cell.Provide(newGC),
+
+	// Invoke forces the instantiation of the identity gc
+	cell.Invoke(func(*GC) {}),
+)
+
+// GCConfig contains the configuration for the identity-gc.
+type GCConfig struct {
+	GCInterval       time.Duration `mapstructure:"identity-gc-interval"`
+	HeartbeatTimeout time.Duration `mapstructure:"identity-heartbeat-timeout"`
+
+	GCRateInterval time.Duration `mapstructure:"identity-gc-rate-interval"`
+	GCRateLimit    int64         `mapstructure:"identity-gc-rate-limit"`
+}
+
+var defaultGCConfig = GCConfig{
+	GCInterval:       defaults.KVstoreLeaseTTL,
+	HeartbeatTimeout: 2 * defaults.KVstoreLeaseTTL,
+
+	GCRateInterval: time.Minute,
+	GCRateLimit:    2500,
+}
+
+func (def GCConfig) Flags(flags *pflag.FlagSet) {
+	flags.Duration(GCInterval, def.GCInterval, "GC interval for security identities")
+	flags.Duration(HeartbeatTimeout, def.HeartbeatTimeout, "Timeout after which identity expires on lack of heartbeat")
+	flags.Duration(GCRateInterval, def.GCRateInterval,
+		"Interval used for rate limiting the GC of security identities")
+	flags.Int64(GCRateLimit, def.GCRateLimit,
+		fmt.Sprintf("Maximum number of security identities that will be deleted within the %s", GCRateInterval))
+}
+
+// GCSharedConfig contains the configuration that is shared between
+// this module and others.
+// It is a temporary solution meant to avoid polluting this module with a direct
+// dependency on global operator and daemon configurations.
+type GCSharedConfig struct {
+	// IdentityAllocationMode specifies what mode to use for identity allocation
+	IdentityAllocationMode string
+
+	// EnableMetrics enables prometheus metrics
+	EnableMetrics bool
+
+	// ClusterName is the name of the cluster
+	ClusterName string
+
+	// K8sNamespace is the name of the namespace in which Cilium is
+	// deployed in when running in Kubernetes mode
+	K8sNamespace string
+
+	// ClusterID is the unique identifier of the cluster
+	ClusterID uint32
+}

operator/identity/crd_gc.go
@@ -1,0 +1,188 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package identity
+
+import (
+	"context"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/cilium/cilium/operator/metrics"
+	"github.com/cilium/cilium/operator/watchers"
+	"github.com/cilium/cilium/pkg/controller"
+	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/identitybackend"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
+
+func (igc *GC) startCRDModeGC(ctx context.Context) error {
+	if igc.gcInterval != 0 {
+		igc.logger.WithField(logfields.Interval, igc.gcInterval).Info("Starting CRD identity garbage collector")
+
+		igc.mgr = controller.NewManager()
+		igc.mgr.UpdateController("crd-identity-gc",
+			controller.ControllerParams{
+				RunInterval:  igc.gcInterval,
+				DoFunc:       igc.gc,
+				NoErrorRetry: true,
+			})
+	}
+
+	return igc.wp.Submit("heartbeat-updater", igc.runHeartbeatUpdater)
+}
+
+func (igc *GC) runHeartbeatUpdater(ctx context.Context) error {
+	for event := range igc.identity.Events(ctx) {
+		switch event.Kind {
+		case resource.Upsert:
+			// Identity is marked as alive if it is new or it has
+			// been updated.
+			igc.heartbeatStore.MarkAlive(event.Object.Name, time.Now())
+		case resource.Delete:
+			// When the identity is deleted, delete the
+			// heartbeat entry as well. This will not be
+			// 100% accurate as the CiliumEndpoint can live
+			// longer than the CiliumIdentity. See
+			// heartbeatStore.gc()
+			igc.heartbeatStore.Delete(event.Object.Name)
+		}
+		event.Done(nil)
+	}
+	return nil
+}
+
+// gc is a single iteration of a garbage collection. It will
+// delete identities that have not had its heartbeat lifesign updated
+// since HeartbeatTimeout.
+func (igc *GC) gc(ctx context.Context) error {
+	igc.logger.Debug("Running CRD identity garbage collector")
+
+	select {
+	case <-watchers.CiliumEndpointsSynced:
+	case <-ctx.Done():
+		return nil
+	}
+
+	identitiesStore, err := igc.identity.Store(ctx)
+	if err != nil {
+		igc.logger.WithError(err).Error("unable to get Cilium identities from local store")
+		return err
+	}
+	identities := identitiesStore.List()
+	totalEntries := len(identities)
+	deletedEntries := 0
+
+	timeNow := time.Now()
+	for _, identity := range identities {
+		// The identity is definitely alive if there's a CE using it.
+		if watchers.HasCEWithIdentity(identity.Name) {
+			igc.heartbeatStore.MarkAlive(identity.Name, timeNow)
+			continue
+		}
+
+		if !igc.heartbeatStore.IsAlive(identity.Name) {
+			ts, ok := identity.Annotations[identitybackend.HeartBeatAnnotation]
+			if !ok {
+				log.WithFields(logrus.Fields{
+					logfields.Identity: identity.Name,
+					logfields.K8sUID:   identity.UID,
+				}).Info("Marking identity for later deletion")
+
+				if identity.Annotations == nil {
+					identity.Annotations = make(map[string]string)
+				}
+
+				identity.Annotations[identitybackend.HeartBeatAnnotation] = timeNow.Format(time.RFC3339Nano)
+				if err := igc.updateIdentity(ctx, identity); err != nil {
+					log.WithError(err).
+						WithField(logfields.Identity, identity).
+						Error("Marking identity for later deletion")
+					return err
+				}
+
+				continue
+			}
+
+			log.WithFields(logrus.Fields{
+				logfields.Identity: identity,
+			}).Debugf("Deleting unused identity; marked for deletion at %s", ts)
+
+			if err := igc.deleteIdentity(ctx, identity); err != nil {
+				log.WithError(err).WithFields(logrus.Fields{
+					logfields.Identity: identity,
+				}).Error("Deleting unused identity")
+				return err
+			} else {
+				deletedEntries++
+			}
+		}
+
+		// If Context was canceled we should break
+		if ctx.Err() != nil {
+			break
+		}
+	}
+
+	if igc.enableMetrics {
+		if ctx.Err() == nil {
+			igc.successfulRuns++
+			metrics.IdentityGCRuns.WithLabelValues(metrics.LabelValueOutcomeSuccess).Set(igc.successfulRuns)
+		} else {
+			igc.failedRuns++
+			metrics.IdentityGCRuns.WithLabelValues(metrics.LabelValueOutcomeFail).Set(igc.failedRuns)
+		}
+		aliveEntries := totalEntries - deletedEntries
+		metrics.IdentityGCSize.WithLabelValues(metrics.LabelValueOutcomeAlive).Set(float64(aliveEntries))
+		metrics.IdentityGCSize.WithLabelValues(metrics.LabelValueOutcomeDeleted).Set(float64(deletedEntries))
+	}
+
+	igc.heartbeatStore.GC()
+
+	return nil
+}
+
+// deleteIdentity deletes an identity. It includes the resource version and
+// will error if the object has since been changed.
+func (igc *GC) deleteIdentity(ctx context.Context, identity *v2.CiliumIdentity) error {
+	// Wait until we can delete an identity
+	if err := igc.rateLimiter.Wait(ctx); err != nil {
+		return err
+	}
+
+	if err := igc.clientset.Delete(
+		ctx,
+		identity.Name,
+		metav1.DeleteOptions{
+			Preconditions: &metav1.Preconditions{
+				UID:             &identity.UID,
+				ResourceVersion: &identity.ResourceVersion,
+			},
+		},
+	); err != nil {
+		log.WithError(err).Error("Unable to delete identity")
+		return err
+	}
+
+	log.WithField(logfields.Identity, identity.GetName()).Info("Garbage collected identity")
+
+	return nil
+}
+
+func (igc *GC) updateIdentity(ctx context.Context, identity *v2.CiliumIdentity) error {
+	if _, err := igc.clientset.Update(
+		ctx,
+		identity,
+		metav1.UpdateOptions{},
+	); err != nil {
+		log.WithError(err).Error("Updating Identity")
+		return err
+	}
+
+	log.WithField(logfields.Identity, identity.GetName()).Debug("Updated identity")
+
+	return nil
+}

commit 7bdc992106815fb1c81d4804364c4cddaaa3326c

operator: fix deadlock when running in kvstore mode
When running in kvstore mode, the start hook of the identity GC cell
blocks until the kvstore client has been initialized, which is performed
by the legacyCell start hook. Given that the identity GC cell was
registered first, and there are no explicit dependencies among the two,
its start hook was also executed first, causing a deadlock.

This commit changes the order in which the cells are registered as a
workaround, until the kvstore is refactored into a proper cell.

The current hooks execution order is the following:

function="gops.registerGopsHooks.func1 (cell.go:44)"
function="client.(*compositeClientset).onStart"
function="cmd.registerOperatorHooks.func1 (root.go:142)"
function="*resource.resource[*github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1.CiliumLoadBalancerIPPool].Start"
function="*resource.resource[*github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1.Service].Start"
function="*lbipam.LBIPAM.Start"
function="*resource.resource[*k8s.io/api/core/v1.Node].Start"
function="*resource.resource[*github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2.CiliumNode].Start"
function="*resource.resource[*github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1.Namespace].Start"
function="*resource.resource[*github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2.CiliumIdentity].Start"
function="cmd.(*legacyOnLeader).onStart"
function="identitygc.registerGC.func1 (gc.go:107)"

Fixes: 69e750ea8b55 ("operator: Refactor cilium identities GC to a cell")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

operator/cmd/root.go
@@ -112,9 +112,16 @@ var (
 			client.RegisterCRDsCell,
 			k8s.SharedResourcesCell,
 			lbipam.Cell,
-			identitygc.Cell,
 
 			legacyCell,
+
+			// When running in kvstore mode, the start hook of the identity GC
+			// cell blocks until the kvstore client has been initialized, which
+			// is performed by the legacyCell start hook. Hence, the identity GC
+			// cell is registered afterwards, to ensure the ordering of the
+			// setup operations. This is a hacky workaround until the kvstore is
+			// refactored into a proper cell.
+			identitygc.Cell,
 		),
 	)
 