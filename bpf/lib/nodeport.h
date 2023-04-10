g0odi3z2j/cilium

commit 479ba216303784841e10adc0c900802ac9634fcd

datapath: Support multiple IPv6 extensions with DSR
Iterate over extensions instead of considering the first IPv6 extensions
when searching for the DSR-related extension. This makes the IPv6 DSR
implementation to play nicely with other extensions.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/lib/nodeport.h
@@ -157,30 +157,73 @@ static __always_inline int set_dsr_ext6(struct __sk_buff *skb,
 	return 0;
 }
 
+static __always_inline int find_dsr_v6(struct __sk_buff *skb, __u8 nexthdr,
+				       struct dsr_opt_v6 *dsr_opt, bool *found)
+{
+	int i, len = sizeof(struct ipv6hdr);
+	struct ipv6_opt_hdr opthdr;
+	__u8 nh = nexthdr;
+
+#pragma unroll
+	for (i = 0; i < IPV6_MAX_HEADERS; i++) {
+		switch (nh) {
+		case NEXTHDR_NONE:
+			return DROP_INVALID_EXTHDR;
+
+		case NEXTHDR_FRAGMENT:
+			return DROP_FRAG_NOSUPPORT;
+
+		case NEXTHDR_HOP:
+		case NEXTHDR_ROUTING:
+		case NEXTHDR_AUTH:
+		case NEXTHDR_DEST:
+			if (skb_load_bytes(skb, ETH_HLEN + len, &opthdr, sizeof(opthdr)) < 0)
+				return DROP_INVALID;
+
+			if (nh == NEXTHDR_DEST && opthdr.hdrlen == DSR_IPV6_EXT_LEN) {
+				if (skb_load_bytes(skb, ETH_HLEN + len, dsr_opt,
+						   sizeof(*dsr_opt)) < 0)
+					return DROP_INVALID;
+				if (dsr_opt->opt_type == DSR_IPV6_OPT_TYPE &&
+				    dsr_opt->opt_len == DSR_IPV6_OPT_LEN) {
+					*found = true;
+					return 0;
+				}
+			}
+
+			nh = opthdr.nexthdr;
+			if (nh == NEXTHDR_AUTH)
+				len += ipv6_authlen(&opthdr);
+			else
+				len += ipv6_optlen(&opthdr);
+			break;
+
+		default:
+			return len;
+		}
+	}
+
+	/* Reached limit of supported extension headers */
+	return DROP_INVALID_EXTHDR;
+}
+
 static __always_inline int handle_dsr_v6(struct __sk_buff *skb, bool *dsr)
 {
 	void *data, *data_end;
 	struct ipv6hdr *ip6;
-	struct dst_opt_v6 opt = {};
+	struct dsr_opt_v6 opt = {};
+	int ret;
 
 	if (!revalidate_data(skb, &data, &data_end, &ip6))
 		return DROP_INVALID;
 
-	// TODO(brb) iterate over all extension headers
-	if (ip6->nexthdr == NEXTHDR_DEST) {
-		if (skb_load_bytes(skb, ETH_HLEN + sizeof(*ip6), &opt,
-				   sizeof(opt)) < 0) {
-			return DROP_INVALID;
-		}
-		if (opt.len == DSR_IPV6_EXT_LEN &&
-		    opt.opt_type == DSR_IPV6_OPT_TYPE &&
-		    opt.opt_len == DSR_IPV6_OPT_LEN) {
-			*dsr = true;
+	ret = find_dsr_v6(skb, ip6->nexthdr, &opt, dsr);
+	if (ret != 0)
+		return ret;
 
-			if (snat_v6_create_dsr(skb, &opt.addr,
-					       opt.port) < 0) {
-				return DROP_INVALID;
-			}
+	if (*dsr) {
+		if (snat_v6_create_dsr(skb, &opt.addr, opt.port) < 0) {
+			return DROP_INVALID;
 		}
 	}
 

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=91198f28aa65ff6bd833a3bfee7ac8e513f3ed6e
+GO_BINDATA_SHA1SUM=d9c731f4c00feef1c9117c611049dd9f4efb25ae
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/conntrack_test.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/ipv6_test.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/signal.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_max_insn.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

commit b42fa3d924201fa63e9f98de6c265c072a878e9c

bpf: dsr: fix parsing of IPv6 AUTH extension header
When walking an IPv6 packet's extension headers to find a DSR extension,
only advance the `nh` variable _after_ calculating the current extension
header's length. For AUTH headers we otherwise fail to determine the
correct header length.

We recently fixed the same issue in the generic IPv6 code with
commit 0ca10b364534 ("bpf: fix ipv6 extension header parsing error"), but
missed that the nodeport code has its own IPv6 extension header parsing
logic.

Fixes: 479ba2163037 ("datapath: Support multiple IPv6 extensions with DSR")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/nodeport.h
@@ -347,11 +347,12 @@ static __always_inline int find_dsr_v6(struct __ctx_buff *ctx, __u8 nexthdr,
 				}
 			}
 
-			nh = opthdr.nexthdr;
 			if (nh == NEXTHDR_AUTH)
 				len += ipv6_authlen(&opthdr);
 			else
 				len += ipv6_optlen(&opthdr);
+
+			nh = opthdr.nexthdr;
 			break;
 
 		default: