g0odi3z2j/cilium

commit e26b9a025dd60ce4d6b07b2955321aeca12d1712

policy: Add bpf ICMP policy support with the "ENABLE_ICMP_POLICY" flag
ICMP types are set to the destination port in the policy map's key struct, when "ENABLE_ICMP_POLICY" flag is enabled.

Signed-off-by: Tomoki Sugiura <cheztomo513@gmail.com>

bpf/lib/policy.h
@@ -123,6 +123,44 @@ __policy_can_access(const void *map, struct __ctx_buff *ctx, __u32 local_id,
 	}
 #endif /* ALLOW_ICMP_FRAG_NEEDED */
 
+#ifdef ENABLE_ICMP_RULE
+	if (proto == IPPROTO_ICMP) {
+		void *data, *data_end;
+		struct iphdr *ip4;
+		struct icmphdr icmphdr __align_stack_8;
+		__u32 off;
+
+		if (!revalidate_data(ctx, &data, &data_end, &ip4))
+			return DROP_INVALID;
+		off = ((void *)ip4 - data) + ipv4_hdrlen(ip4);
+		if (ctx_load_bytes(ctx, off, &icmphdr, sizeof(icmphdr)) < 0)
+			return DROP_INVALID;
+
+		/* Convert from unsigned char to unsigned short considering byte order(little-endian).
+		 * In the little-endian case, for example, 2byte data "AB" convert to "BA".
+		 * Therefore, the "icmp_type" should be shifted not just casting.
+		 */
+		key.dport = (__u16)(icmphdr.type << 8);
+	} else if (proto == IPPROTO_ICMPV6) {
+		void *data, *data_end;
+		struct ipv6hdr *ip6;
+		__u32 off;
+		__u8 icmp_type;
+
+		if (!revalidate_data(ctx, &data, &data_end, &ip6))
+			return DROP_INVALID;
+		off = ((void *)ip6 - data) + ipv6_hdrlen(ctx, ETH_HLEN, &ip6->nexthdr);
+		if (ctx_load_bytes(ctx, off, &icmp_type, sizeof(icmp_type)) < 0)
+			return DROP_INVALID;
+
+		/* Convert from unsigned char to unsigned short considering byte order(little-endian).
+		 * In the little-endian case, for example, 2byte data "AB" convert to "BA".
+		 * Therefore, the "icmp_type" should be shifted not just casting.
+		 */
+		key.dport = (__u16)(icmp_type << 8);
+	}
+#endif /* ENABLE_ICMP_RULE */
+
 	/* L4 lookup can't be done on untracked fragments. */
 	if (!is_untracked_fragment) {
 		/* Start with L3/L4 lookup. */

commit 42c5976f40cdcf7ede396a2cf470ff38c092c820

bpf: policy: fix handling of ICMPv6 packet with extension headers
After walking the IPv6 extension headers, ipv6_hdrlen() returns the L4
proto type in `nexthdr` parameter. If we pass in a pointer to the IPv6
header's nexthdr field, then the actual packet content is changed and
subsequent processing of the packet is broken (because we treat the first
IPv6 extension header as an ICMPv6 header).

So even if we don't care about the L4 proto type (because we already know
that it's ICMPv6), we still need to provide some stack space to store the
`nexthdr`.

This only becomes relevant when ENABLE_ICMP_RULE is set, which is
currently controlled by a hidden agent flag.

Fixes: e26b9a025dd6 ("policy: Add bpf ICMP policy support with the "ENABLE_ICMP_POLICY" flag")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

bpf/lib/policy.h
@@ -82,10 +82,13 @@ __policy_can_access(const void *map, struct __ctx_buff *ctx, __u32 local_id,
 		struct ipv6hdr *ip6;
 		__u32 off;
 		__u8 icmp_type;
+		__u8 nexthdr;
 
 		if (!revalidate_data(ctx, &data, &data_end, &ip6))
 			return DROP_INVALID;
-		off = ((void *)ip6 - data) + ipv6_hdrlen(ctx, &ip6->nexthdr);
+
+		nexthdr = ip6->nexthdr;
+		off = ((void *)ip6 - data) + ipv6_hdrlen(ctx, &nexthdr);
 		if (ctx_load_bytes(ctx, off, &icmp_type, sizeof(icmp_type)) < 0)
 			return DROP_INVALID;
 