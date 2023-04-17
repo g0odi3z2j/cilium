g0odi3z2j/cilium

commit 1c5272f44da35e1240e7ce2953570677e87feeb8

bpf/wireguard: Skip encryption for cluster-external traffic
Egress gateway traffic travels from a client pod to a gateway node via
the overlay to be SNATed at the gateway node's native device. The reply
traffic should then take the same path back to the client pod.

When WireGuard is also enabled, however, we observe that the reply
traffic goes through the WireGuard device instead of the overlay device.
More specifically, after being reverse SNATed in bpf_host's handle_ipv4,
this reply traffic goes through encap_and_redirect_with_nodeid to be
redirected to the overlay device. There, it runs through
wg_maybe_redirect_to_encrypt, which was never taught to skip traffic from
outside the cluster.

Without the egress gateway, this scenario doesn't happen for WireGuard
because all traffic coming from outside the cluster is either:
- destined to the local node and not subject to WireGuard encryption, or
- destined to a service with a remote backend, but redirected in the
  BPF NodePort logic.

This commit fixes this bug by adding a new case in
wg_maybe_redirect_to_encrypt, to skip encryption for any traffic that
originates from outside the cluster.

Fixes: da4b3689da7 ("datapath: Change WG integration to support host2host case")
Signed-off-by: Paul Chaignon <paul@cilium.io>

bpf/lib/wireguard.h
@@ -92,6 +92,15 @@ wg_maybe_redirect_to_encrypt(struct __ctx_buff *ctx)
 		goto out;
 #endif /* ENABLE_NODE_ENCRYPTION */
 
+	/* We don't want to encrypt any traffic that originates from outside
+	 * the cluster.
+	 * Without this check, that may happen for the egress gateway, when
+	 * reply traffic arrives from the cluster-external server and goes to
+	 * the client pod.
+	 */
+	if (!src || !identity_is_cluster(src->sec_label))
+		goto out;
+
 	/* Redirect to the WireGuard tunnel device if the encryption is
 	 * required.
 	 */

commit 2242152a0fbe7495f6030b4b8071c925c46aeba4

bpf/wireguard: always lookup src identity
When WireGuard node encryption is enabled, src is never set. Therefore,
the unguarded check in line 97 always evaluates to true and no traffic
is encrypted.

This commit fixes this bug by always looking up the src identity,
independent of if WireGuard node encryption is enabled or not.

Fixes: 1c5272f ("bpf/wireguard: Skip encryption for cluster-external traffic")
Signed-off-by: Leonard Cohnen <lc@edgeless.systems>

bpf/lib/wireguard.h
@@ -48,9 +48,7 @@ wg_maybe_redirect_to_encrypt(struct __ctx_buff *ctx)
 		}
 #endif
 		dst = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr, 0);
-#ifndef ENABLE_NODE_ENCRYPTION
 		src = lookup_ip6_remote_endpoint((union v6addr *)&ip6->saddr, 0);
-#endif /* ENABLE_NODE_ENCRYPTION */
 		break;
 #endif
 #ifdef ENABLE_IPV4
@@ -58,9 +56,7 @@ wg_maybe_redirect_to_encrypt(struct __ctx_buff *ctx)
 		if (!revalidate_data(ctx, &data, &data_end, &ip4))
 			return DROP_INVALID;
 		dst = lookup_ip4_remote_endpoint(ip4->daddr, 0);
-#ifndef ENABLE_NODE_ENCRYPTION
 		src = lookup_ip4_remote_endpoint(ip4->saddr, 0);
-#endif /* ENABLE_NODE_ENCRYPTION */
 		break;
 #endif
 	default: