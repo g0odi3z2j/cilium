g0odi3z2j/cilium

commit 90948dbcbc991e2447c13219462693d3b95885e9

k8s: Add support for cidrGroupRef field in CNP/CCNP
Add support for cidrGroupRef field in Ingress rules for Cilium Network
Policies.  The new field allows to reference a list of CiliumCIDRGroup
objects that contain the CIDRs from which we wants to allow traffic
from.

The feature is implemented as a wrapper to the existing FromCIDRSet
Ingress rule. Each policy with a reference to a cidr group is translated
in an equivalent "FromCIDRSet policy" after looking up the CIDRs in the
referenced CiliumCIDRGroup objects.

To do that, we use a single handler for CiliumNetworkPolicy,
CiliumClusterwideNetworkPolicy and CiliumCIDRGroup in the k8s watcher,
in order to avoid further locking when accessing the CNP/CCNP cache or
the CIDRGroup one.

Every time the referenced CiliumCIDRGroups are updated, the CNP rules
are recalculated, in order to append to the Ingress.FromCIDRSet field
the updated lists of CIDRs.

In case the referenced CiliumCIDRGroups k8s objects are all empty or
deleted, the policy behaviour is default-deny.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/k8s/watchers/cilium_cidr_group.go
@@ -1,0 +1,224 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package watchers
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	cilium_v2_alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/client"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	"github.com/cilium/cilium/pkg/k8s/types"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/policy/api"
+)
+
+func (k *K8sWatcher) onUpsertCIDRGroup(
+	cidrGroup *cilium_v2_alpha1.CiliumCIDRGroup,
+	cidrGroupCache map[string]*cilium_v2_alpha1.CiliumCIDRGroup,
+	cnpCache map[resource.Key]*types.SlimCNP,
+	cs client.Clientset,
+) error {
+	oldCidrGroup, ok := cidrGroupCache[cidrGroup.Name]
+	if ok && oldCidrGroup.Spec.DeepEqual(&cidrGroup.Spec) {
+		return nil
+	}
+
+	cidrGroupCpy := cidrGroup.DeepCopy()
+	cidrGroupCache[cidrGroup.Name] = cidrGroupCpy
+
+	return k.updateCIDRGroupRefPolicies(cidrGroup.Name, cidrGroupCache, cnpCache, cs)
+}
+
+func (k *K8sWatcher) onDeleteCIDRGroup(
+	cidrGroupName string,
+	cidrGroupCache map[string]*cilium_v2_alpha1.CiliumCIDRGroup,
+	cnpCache map[resource.Key]*types.SlimCNP,
+	cs client.Clientset,
+) error {
+	delete(cidrGroupCache, cidrGroupName)
+
+	return k.updateCIDRGroupRefPolicies(cidrGroupName, cidrGroupCache, cnpCache, cs)
+}
+
+func (k *K8sWatcher) updateCIDRGroupRefPolicies(
+	cidrGroup string,
+	cidrGroupCache map[string]*cilium_v2_alpha1.CiliumCIDRGroup,
+	cnpCache map[resource.Key]*types.SlimCNP,
+	cs client.Clientset,
+) error {
+	var errs []error
+	for key, cnp := range cnpCache {
+		if !hasCIDRGroupRef(cnp, cidrGroup) {
+			continue
+		}
+
+		log.WithFields(logrus.Fields{
+			logfields.CiliumNetworkPolicyName: cnp.Name,
+			logfields.K8sAPIVersion:           cnp.APIVersion,
+			logfields.K8sNamespace:            cnp.Namespace,
+			logfields.CIDRGroupRef:            cidrGroup,
+		}).Info("Referenced CiliumCIDRGroup updated or deleted, recalculating CiliumNetworkPolicy rules")
+
+		initialRecvTime := time.Now()
+
+		// We need to deepcopy this structure because we are writing
+		// fields.
+		// See https://github.com/cilium/cilium/blob/27fee207f5422c95479422162e9ea0d2f2b6c770/pkg/policy/api/ingress.go#L112-L134
+		cnpCpy := cnp.DeepCopy()
+
+		translatedCNP := resolveCIDRGroupRef(cnpCpy, cidrGroupCache)
+
+		err := k.updateCiliumNetworkPolicyV2(cs, cnpCpy, translatedCNP, initialRecvTime)
+		if err == nil {
+			cnpCache[key] = cnpCpy
+		}
+
+		errs = append(errs, err)
+	}
+	return errors.Join(errs...)
+}
+
+func resolveCIDRGroupRef(cnp *types.SlimCNP, cidrGroupCache map[string]*cilium_v2_alpha1.CiliumCIDRGroup) *types.SlimCNP {
+	refs := getCIDRGroupRefs(cnp)
+	if len(refs) == 0 {
+		return cnp
+	}
+
+	cidrsSets, err := cidrGroupRefsToCIDRsSets(refs, cidrGroupCache)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			logfields.K8sAPIVersion:           cnp.TypeMeta.APIVersion,
+			logfields.CiliumNetworkPolicyName: cnp.ObjectMeta.Name,
+			logfields.K8sNamespace:            cnp.ObjectMeta.Namespace,
+			logfields.CIDRGroupRef:            refs,
+		}).WithError(err).Warning("Unable to translate all cidr groups to cidrs")
+	}
+
+	translated := translateCIDRGroupRefs(cnp, cidrsSets)
+
+	return translated
+}
+
+func hasCIDRGroupRef(cnp *types.SlimCNP, cidrGroup string) bool {
+	if specHasCIDRGroupRef(cnp.Spec, cidrGroup) {
+		return true
+	}
+	for _, spec := range cnp.Specs {
+		if specHasCIDRGroupRef(spec, cidrGroup) {
+			return true
+		}
+	}
+	return false
+}
+
+func specHasCIDRGroupRef(spec *api.Rule, cidrGroup string) bool {
+	if spec == nil {
+		return false
+	}
+	for _, ingress := range spec.Ingress {
+		for _, rule := range ingress.FromCIDRSet {
+			if string(rule.CIDRGroupRef) == cidrGroup {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func getCIDRGroupRefs(cnp *types.SlimCNP) []string {
+	if cnp.Spec == nil && cnp.Specs == nil {
+		return nil
+	}
+
+	specs := cnp.Specs
+	if cnp.Spec != nil {
+		specs = append(specs, cnp.Spec)
+	}
+
+	var cidrGroupRefs []string
+	for _, spec := range specs {
+		for _, ingress := range spec.Ingress {
+			for _, rule := range ingress.FromCIDRSet {
+				cidrGroupRefs = append(cidrGroupRefs, string(rule.CIDRGroupRef))
+			}
+		}
+	}
+
+	return cidrGroupRefs
+}
+
+func cidrGroupRefsToCIDRsSets(cidrGroupRefs []string, cache map[string]*cilium_v2_alpha1.CiliumCIDRGroup) (map[string][]api.CIDR, error) {
+	var errs []error
+	cidrsSet := make(map[string][]api.CIDR)
+	for _, cidrGroupRef := range cidrGroupRefs {
+		var found bool
+		for cidrGroupName, cidrGroup := range cache {
+			if cidrGroupName == cidrGroupRef {
+				cidrs := make([]api.CIDR, 0, len(cidrGroup.Spec.ExternalCIDRs))
+				for _, cidr := range cidrGroup.Spec.ExternalCIDRs {
+					cidrs = append(cidrs, api.CIDR(cidr))
+				}
+				cidrsSet[cidrGroupRef] = cidrs
+				found = true
+				break
+			}
+		}
+		if !found {
+			errs = append(errs, fmt.Errorf("cidr group %q not found, skipping translation", cidrGroupRef))
+		}
+	}
+	return cidrsSet, errors.Join(errs...)
+}
+
+func translateCIDRGroupRefs(cnp *types.SlimCNP, cidrsSets map[string][]api.CIDR) *types.SlimCNP {
+	cnpCpy := cnp.DeepCopy()
+
+	if cnpCpy.Spec != nil {
+		translateSpec(cnpCpy.Spec, cidrsSets)
+	}
+	for i := range cnpCpy.Specs {
+		if cnpCpy.Specs[i] != nil {
+			translateSpec(cnpCpy.Specs[i], cidrsSets)
+		}
+	}
+	return cnpCpy
+}
+
+func translateSpec(spec *api.Rule, cidrsSets map[string][]api.CIDR) {
+	for i := range spec.Ingress {
+		var (
+			oldRules api.CIDRRuleSlice
+			refRules []api.CIDRRule
+		)
+
+		for _, rule := range spec.Ingress[i].FromCIDRSet {
+			if rule.CIDRGroupRef == "" {
+				// keep rules without a cidr group reference
+				oldRules = append(oldRules, rule)
+				continue
+			}
+			// collect all rules with references to a cidr group
+			refRules = append(refRules, rule)
+		}
+
+		// add rules for each cidr in the referenced cidr groups
+		var newRules api.CIDRRuleSlice
+		for _, refRule := range refRules {
+			cidrs, found := cidrsSets[string(refRule.CIDRGroupRef)]
+			if !found || len(cidrs) == 0 {
+				continue
+			}
+			for _, cidr := range cidrs {
+				newRules = append(newRules, api.CIDRRule{Cidr: cidr, ExceptCIDRs: refRule.ExceptCIDRs})
+			}
+		}
+
+		spec.Ingress[i].FromCIDRSet = append(oldRules, newRules...)
+	}
+}

pkg/k8s/watchers/cilium_network_policy.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/k8s"
 	cilium_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	cilium_v2_alpha1 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
 	"github.com/cilium/cilium/pkg/k8s/client"
 	clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
 	"github.com/cilium/cilium/pkg/k8s/resource"
@@ -82,16 +83,21 @@ func (r *ruleImportMetadataCache) get(cnp *types.SlimCNP) (policyImportMetadata,
 }
 
 func (k *K8sWatcher) ciliumNetworkPoliciesInit(ctx context.Context, cs client.Clientset) {
-	var cnpSynced, ccnpSynced atomic.Bool
+	var cnpSynced, ccnpSynced, cidrGroupSynced atomic.Bool
 	go func() {
 		cnpEvents := k.sharedResources.CiliumNetworkPolicies.Events(ctx)
 		ccnpEvents := k.sharedResources.CiliumClusterwideNetworkPolicies.Events(ctx)
 
-		// cache contains both CNPs and CCNPs, stored using a common intermediate
+		// cnpCache contains both CNPs and CCNPs, stored using a common intermediate
 		// representation (*types.SlimCNP). The cache is indexed on resource.Key,
 		// that contains both the name and namespace of the resource, in order to
 		// avoid key clashing between CNPs and CCNPs.
-		cache := make(map[resource.Key]*types.SlimCNP)
+		// The cache contains CNPs and CCNPs in their "original form"
+		// (i.e: pre-translation of each CIDRGroupRef to a CIDRSet).
+		cnpCache := make(map[resource.Key]*types.SlimCNP)
+
+		cidrGroupCache := make(map[string]*cilium_v2_alpha1.CiliumCIDRGroup)
+		cidrGroupEvents := k.sharedResources.CIDRGroups.Events(ctx)
 
 		for {
 			select {
@@ -119,9 +125,9 @@ func (k *K8sWatcher) ciliumNetworkPoliciesInit(ctx context.Context, cs client.Cl
 				var err error
 				switch event.Kind {
 				case resource.Upsert:
-					err = k.onUpsert(slimCNP, cache, event.Key, cs, k8sAPIGroupCiliumNetworkPolicyV2, resources.MetricCNP)
+					err = k.onUpsert(slimCNP, cnpCache, event.Key, cidrGroupCache, cs, k8sAPIGroupCiliumNetworkPolicyV2, resources.MetricCNP)
 				case resource.Delete:
-					err = k.onDelete(slimCNP, cache, event.Key, k8sAPIGroupCiliumNetworkPolicyV2, resources.MetricCNP)
+					err = k.onDelete(slimCNP, cnpCache, event.Key, k8sAPIGroupCiliumNetworkPolicyV2, resources.MetricCNP)
 				}
 				reportCNPChangeMetrics(err)
 				event.Done(err)
@@ -149,26 +155,54 @@ func (k *K8sWatcher) ciliumNetworkPoliciesInit(ctx context.Context, cs client.Cl
 				var err error
 				switch event.Kind {
 				case resource.Upsert:
-					err = k.onUpsert(slimCNP, cache, event.Key, cs, k8sAPIGroupCiliumClusterwideNetworkPolicyV2, resources.MetricCCNP)
+					err = k.onUpsert(slimCNP, cnpCache, event.Key, cidrGroupCache, cs, k8sAPIGroupCiliumClusterwideNetworkPolicyV2, resources.MetricCCNP)
 				case resource.Delete:
-					err = k.onDelete(slimCNP, cache, event.Key, k8sAPIGroupCiliumClusterwideNetworkPolicyV2, resources.MetricCCNP)
+					err = k.onDelete(slimCNP, cnpCache, event.Key, k8sAPIGroupCiliumClusterwideNetworkPolicyV2, resources.MetricCCNP)
+				}
+				event.Done(err)
+			case event, ok := <-cidrGroupEvents:
+				if !ok {
+					cidrGroupEvents = nil
+					break
+				}
+
+				if event.Kind == resource.Sync {
+					cidrGroupSynced.Store(true)
+					event.Done(nil)
+					continue
+				}
+
+				var err error
+				switch event.Kind {
+				case resource.Upsert:
+					err = k.onUpsertCIDRGroup(event.Object, cidrGroupCache, cnpCache, cs)
+				case resource.Delete:
+					err = k.onDeleteCIDRGroup(event.Object.Name, cidrGroupCache, cnpCache, cs)
 				}
 				event.Done(err)
 			}
-			if cnpEvents == nil && ccnpEvents == nil {
+			if cnpEvents == nil && ccnpEvents == nil && cidrGroupEvents == nil {
 				return
 			}
 		}
 	}()
 
-	k.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumNetworkPolicyV2, cnpSynced.Load)
-	k.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumClusterwideNetworkPolicyV2, ccnpSynced.Load)
+	k.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumNetworkPolicyV2, func() bool {
+		return cnpSynced.Load() && cidrGroupSynced.Load()
+	})
+	k.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumClusterwideNetworkPolicyV2, func() bool {
+		return ccnpSynced.Load() && cidrGroupSynced.Load()
+	})
+	k.registerResourceWithSyncFn(ctx, k8sAPIGroupCiliumCIDRGroupV2Alpha1, func() bool {
+		return cidrGroupSynced.Load()
+	})
 }
 
 func (k *K8sWatcher) onUpsert(
 	cnp *types.SlimCNP,
-	cache map[resource.Key]*types.SlimCNP,
+	cnpCache map[resource.Key]*types.SlimCNP,
 	key resource.Key,
+	cidrGroupCache map[string]*cilium_v2_alpha1.CiliumCIDRGroup,
 	cs client.Clientset,
 	apiGroup string,
 	metricLabel string,
@@ -185,7 +219,7 @@ func (k *K8sWatcher) onUpsert(
 		k.K8sEventReceived(apiGroup, metricLabel, action, true, equal)
 	}()
 
-	oldCNP, ok := cache[key]
+	oldCNP, ok := cnpCache[key]
 	if !ok {
 		action = resources.MetricCreate
 	} else {
@@ -205,14 +239,16 @@ func (k *K8sWatcher) onUpsert(
 	// See https://github.com/cilium/cilium/blob/27fee207f5422c95479422162e9ea0d2f2b6c770/pkg/policy/api/ingress.go#L112-L134
 	cnpCpy := cnp.DeepCopy()
 
+	translatedCNP := resolveCIDRGroupRef(cnpCpy, cidrGroupCache)
+
 	var err error
 	if ok {
-		err = k.updateCiliumNetworkPolicyV2(cs, oldCNP, cnpCpy, initialRecvTime)
+		err = k.updateCiliumNetworkPolicyV2(cs, oldCNP, translatedCNP, initialRecvTime)
 	} else {
-		err = k.addCiliumNetworkPolicyV2(cs, cnpCpy, initialRecvTime)
+		err = k.addCiliumNetworkPolicyV2(cs, translatedCNP, initialRecvTime)
 	}
 	if err == nil {
-		cache[key] = cnpCpy
+		cnpCache[key] = cnpCpy
 	}
 
 	k.K8sEventProcessed(metricLabel, action, err == nil)

pkg/k8s/watchers/watcher.go
@@ -67,6 +67,7 @@ const (
 	k8sAPIGroupNetworkingV1Core                 = "networking.k8s.io/v1::NetworkPolicy"
 	k8sAPIGroupCiliumNetworkPolicyV2            = "cilium/v2::CiliumNetworkPolicy"
 	k8sAPIGroupCiliumClusterwideNetworkPolicyV2 = "cilium/v2::CiliumClusterwideNetworkPolicy"
+	k8sAPIGroupCiliumCIDRGroupV2Alpha1          = "cilium/v2alpha1::CiliumCIDRGroup"
 	k8sAPIGroupCiliumNodeV2                     = "cilium/v2::CiliumNode"
 	k8sAPIGroupCiliumEndpointV2                 = "cilium/v2::CiliumEndpoint"
 	k8sAPIGroupCiliumLocalRedirectPolicyV2      = "cilium/v2::CiliumLocalRedirectPolicy"
@@ -421,7 +422,7 @@ var ciliumResourceToGroupMapping = map[string]watcherInfo{
 	synced.CRDResourceName(v2alpha1.BGPPName):     {skip, ""}, // Handled in BGP control plane
 	synced.CRDResourceName(v2alpha1.LBIPPoolName): {skip, ""}, // Handled in LB IPAM
 	synced.CRDResourceName(v2alpha1.CNCName):      {skip, ""}, // Handled by init directly
-	synced.CRDResourceName(v2alpha1.CCGName):      {skip, ""},
+	synced.CRDResourceName(v2alpha1.CCGName):      {start, k8sAPIGroupCiliumCIDRGroupV2Alpha1},
 }
 
 // resourceGroups are all of the core Kubernetes and Cilium resource groups
@@ -540,7 +541,7 @@ func (k *K8sWatcher) enableK8sWatchers(ctx context.Context, resourceNames []stri
 		return fmt.Errorf("error creating service list option modifier: %w", err)
 	}
 
-	// CNP and CCNP resources are handled together.
+	// CNP, CCNP, and CCG resources are handled together.
 	var cnpOnce sync.Once
 	for _, r := range resourceNames {
 		switch r {
@@ -572,7 +573,7 @@ func (k *K8sWatcher) enableK8sWatchers(ctx context.Context, resourceNames []stri
 			// only watch secrets in specific namespaces
 			k.tlsSecretInit(k.clientset.Slim(), option.Config.EnvoySecretNamespaces, swgSecret)
 		// Custom resource definitions
-		case k8sAPIGroupCiliumNetworkPolicyV2, k8sAPIGroupCiliumClusterwideNetworkPolicyV2:
+		case k8sAPIGroupCiliumNetworkPolicyV2, k8sAPIGroupCiliumClusterwideNetworkPolicyV2, k8sAPIGroupCiliumCIDRGroupV2Alpha1:
 			cnpOnce.Do(func() { k.ciliumNetworkPoliciesInit(ctx, k.clientset) })
 		case k8sAPIGroupCiliumEndpointV2:
 			k.initCiliumEndpointOrSlices(k.clientset, asyncControllers)

pkg/logging/logfields/logfields.go
@@ -681,4 +681,7 @@ const (
 
 	// User identifies a given user
 	User = "user"
+
+	// CIDRGroupRef is a references to a CiliumCIDRGroup object.
+	CIDRGroupRef = "cidrGroupRef"
 )

commit 06629124b8bd0ad7a39d35fdb25784c78173f0a4

k8s/watchers: Fix erroneous warning logs due to empty CIDRGroupRef
Previously, the logic to detect whether a CIDRGroupRef has been set did
not consider CNP policies which only reference a CIDR. This is relevant
because CIDRs and CIDRGroupRefs are mutually exclusive, i.e.  oneOf
validation is applied on them.

This triggered the following warning log to be triggered quite often as
each update to the CNP would run through the translation logic:

```
level=warning msg="Unable to translate all cidr groups to cidrs" cidrGroupRef="[group-1 ]" ciliumNetworkPolicyName=tofqdn-dns-visibility error="cidr group \"group-1\" not found, skipping translation; cidr group \"\" not found, skipping translation" k8sApiVersion= k8sNamespace=default subsys=k8s-watcher
```

For example, the following will trigger the buggy behavior:

```
ingress:
  - fromCIDRSet:
    - cidr: "3.3.3.3/32"
```

With the fix, we detect that a CIDR has been set and therefore, we avoid
returning an empty CIDRGroupRef name.

Fixes: 90948dbcbc9 ("k8s: Add support for cidrGroupRef field in
CNP/CCNP")
Fixes: https://github.com/cilium/cilium/issues/25020

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/k8s/watchers/cilium_cidr_group.go
@@ -127,7 +127,7 @@ func resolveCIDRGroupRef(cnp *types.SlimCNP, cidrGroupCache map[string]*cilium_v
 			logfields.CiliumNetworkPolicyName: cnp.ObjectMeta.Name,
 			logfields.K8sNamespace:            cnp.ObjectMeta.Namespace,
 			logfields.CIDRGroupRef:            refs,
-		}).WithError(err).Warning("Unable to translate all cidr groups to cidrs")
+		}).WithError(err).Warning("Unable to translate all CIDR groups to CIDRs")
 	}
 
 	translated := translateCIDRGroupRefs(cnp, cidrsSets)
@@ -175,7 +175,12 @@ func getCIDRGroupRefs(cnp *types.SlimCNP) []string {
 	for _, spec := range specs {
 		for _, ingress := range spec.Ingress {
 			for _, rule := range ingress.FromCIDRSet {
-				cidrGroupRefs = append(cidrGroupRefs, string(rule.CIDRGroupRef))
+				// If CIDR is not set, then we assume CIDRGroupRef is set due
+				// to OneOf, even if CIDRGroupRef is empty, as that's still a
+				// valid reference (although useless from a user perspective).
+				if len(rule.Cidr) == 0 {
+					cidrGroupRefs = append(cidrGroupRefs, string(rule.CIDRGroupRef))
+				}
 			}
 		}
 	}

pkg/k8s/watchers/cilium_cidr_group_test.go
@@ -177,6 +177,51 @@ func TestHasCIDRGroupRef(t *testing.T) {
 			expected:  true,
 		},
 		{
+			name: "CIDR in Spec",
+			cnp: &types.SlimCNP{
+				CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "cilium.io/v2",
+						Kind:       "CiliumNetworkPolicy",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-policy",
+						Namespace: "test-namespace",
+					},
+					Spec: &api.Rule{
+						Ingress: []api.IngressRule{
+							{
+								IngressCommonRule: api.IngressCommonRule{
+									FromCIDRSet: api.CIDRRuleSlice{
+										{
+											CIDRGroupRef: "cidr-group-1",
+										},
+									},
+								},
+							},
+						},
+					},
+					Specs: api.Rules{
+						{
+							Ingress: []api.IngressRule{
+								{
+									IngressCommonRule: api.IngressCommonRule{
+										FromCIDRSet: api.CIDRRuleSlice{
+											{
+												Cidr: "1.1.1.1/32",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			cidrGroup: "cidr-group-1",
+			expected:  true,
+		},
+		{
 			name: "CIDRGroupRef in Specs",
 			cnp: &types.SlimCNP{
 				CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
@@ -373,6 +418,50 @@ func TestCIDRGroupRefsGet(t *testing.T) {
 				},
 			},
 			expected: []string{"cidr-group-1", "cidr-group-2"},
+		},
+		{
+			name: "single FromCidrSet rule with only CIDR",
+			cnp: &types.SlimCNP{
+				CiliumNetworkPolicy: &cilium_v2.CiliumNetworkPolicy{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "cilium.io/v2",
+						Kind:       "CiliumNetworkPolicy",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-policy",
+						Namespace: "test-namespace",
+					},
+					Spec: &api.Rule{
+						Ingress: []api.IngressRule{
+							{
+								IngressCommonRule: api.IngressCommonRule{
+									FromCIDRSet: api.CIDRRuleSlice{
+										{
+											CIDRGroupRef: "cidr-group-1",
+										},
+									},
+								},
+							},
+						},
+					},
+					Specs: api.Rules{
+						{
+							Ingress: []api.IngressRule{
+								{
+									IngressCommonRule: api.IngressCommonRule{
+										FromCIDRSet: api.CIDRRuleSlice{
+											{
+												Cidr: "1.1.1.1/32",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: []string{"cidr-group-1"},
 		},
 		{
 			name: "multiple FromCidrSet rules",