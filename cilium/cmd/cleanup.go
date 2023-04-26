g0odi3z2j/cilium

commit 0bb62de7725e54f88163cd0a5fff8bd7c25fe70f

cilium: Remove attached bpf_xdp upon "cilium cleanup"
cilium cleanup doesn't remove a previously attached XDP progs by Cilium,
this commit is an complementary.

Fixes: #19586

Signed-off-by: zhang he <zhanghe9702@163.com>

cilium/cmd/cleanup.go
@@ -14,6 +14,8 @@ import (
 	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
 
+	"github.com/cilium/ebpf"
+
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/common"
 	"github.com/cilium/cilium/pkg/defaults"
@@ -126,6 +128,7 @@ type ciliumCleanup struct {
 	routes    map[int]netlink.Route
 	links     map[int]netlink.Link
 	tcFilters map[string][]*netlink.BpfFilter
+	xdpLinks  []netlink.Link
 	netNSs    []string
 	bpfOnly   bool
 }
@@ -161,8 +164,17 @@ func newCiliumCleanup(bpfOnly bool) ciliumCleanup {
 			}
 		}
 	}
+	xdpLinks := []netlink.Link{}
 
-	return ciliumCleanup{routes, ciliumLinks, tcFilters, netNSs, bpfOnly}
+	for _, link := range links {
+		if ok, err := isCiliumXDPAttachedToLink(link); ok && err == nil {
+			xdpLinks = append(xdpLinks, link)
+		} else if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		}
+	}
+
+	return ciliumCleanup{routes, ciliumLinks, tcFilters, xdpLinks, netNSs, bpfOnly}
 }
 
 func (c ciliumCleanup) whatWillBeRemoved() []string {
@@ -172,6 +184,13 @@ func (c ciliumCleanup) whatWillBeRemoved() []string {
 		section := "tc filters\n"
 		for linkName, f := range c.tcFilters {
 			section += fmt.Sprintf("%s %v\n", linkName, f)
+		}
+		toBeRemoved = append(toBeRemoved, section)
+	}
+	if len(c.xdpLinks) > 0 {
+		section := "xdp programs\n"
+		for _, l := range c.xdpLinks {
+			section += fmt.Sprintf("%s: xdp/prog id %v\n", l.Attrs().Name, l.Attrs().Xdp.ProgId)
 		}
 		toBeRemoved = append(toBeRemoved, section)
 	}
@@ -223,13 +242,16 @@ func (c ciliumCleanup) cleanupFuncs() []cleanupFunc {
 	cleanupNamedNetNSs := func() error {
 		return removeNamedNetNSs(c.netNSs)
 	}
-
+	cleanupXDPs := func() error {
+		return removeXDPs(c.xdpLinks)
+	}
 	cleanupTCFilters := func() error {
 		return removeTCFilters(c.tcFilters)
 	}
 
 	funcs := []cleanupFunc{
 		cleanupTCFilters,
+		cleanupXDPs,
 	}
 	if !c.bpfOnly {
 		funcs = append(funcs, cleanupRoutesAndLinks)
@@ -514,4 +536,48 @@ func removeTCFilters(linkAndFilters map[string][]*netlink.BpfFilter) error {
 	}
 
 	return nil
+}
+
+func removeXDPs(links []netlink.Link) error {
+	for _, link := range links {
+		err := netlink.LinkSetXdpFd(link, -1)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("removed cilium xdp of %s\n", link.Attrs().Name)
+	}
+	return nil
+}
+
+func isCiliumXDPAttachedToLink(link netlink.Link) (bool, error) {
+	linkxdp := link.Attrs().Xdp
+	if linkxdp == nil || !linkxdp.Attached {
+		return false, nil
+	}
+
+	ok, err := isCiliumXDP(linkxdp.ProgId)
+	if ok && err == nil {
+		return true, nil
+	}
+
+	return false, err
+}
+
+func isCiliumXDP(progId uint32) (bool, error) {
+	xdp, err := ebpf.NewProgramFromID(ebpf.ProgramID(progId))
+	if err != nil {
+		return false, err
+	}
+
+	info, err := xdp.Info()
+	if err != nil {
+		return false, err
+	}
+
+	if strings.Contains(info.Name, "cil_xdp_entry") {
+		return true, nil
+	}
+
+	return false, nil
+
 }

bpf/bpf_xdp.c
@@ -278,7 +278,7 @@ static __always_inline int check_filters(struct __ctx_buff *ctx)
 }
 
 __section("from-netdev")
-int bpf_xdp_entry(struct __ctx_buff *ctx)
+int cil_xdp_entry(struct __ctx_buff *ctx)
 {
 	return check_filters(ctx);
 }

bpf/tests/xdp_nodeport_lb4_test.c
@@ -32,7 +32,7 @@ struct {
 	__array(values, int());
 } entry_call_map __section(".maps") = {
 	.values = {
-		[0] = &bpf_xdp_entry,
+		[0] = &cil_xdp_entry,
 	},
 };
 

commit 89c2ce78ea0c24e0057bbea4d7c4c2e3ec65fe23

cmd/cleanup: Fix cleanup of generic XDP programs
The 'cilium cleanup' command wasn't actually removing XDP programs
attached to the generic hook point. This was causing issues when an XDP
test in ginkgo was immediatly followed by the upgrade test. The upgrade
test would run 'cilium cleanup' and remove everything except for the
XDP programs. That would isolate the Cilium-managed nodes from the
outside network (both SSH and intra-cluster connectivity).

Obviously, this works when we change the Cilium configuration to disable
XDP. There we have some logic to remove any leftover XDP programs on
agent startup. Comparing that logic with the 'cilium cleanup' logic, we
can see that the difference is that the agent removes XDP programs
explicitely for each hook point (generic and driver). Let's do the same.

This fix was tested manually on a setup with Cilium's generic XDP
programs installed.

Fixes: 0bb62de772 ("cilium: Remove attached bpf_xdp upon "cilium cleanup"")
Signed-off-by: Paul Chaignon <paul@cilium.io>

cilium/cmd/cleanup.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/vishvananda/netlink"
+	"github.com/vishvananda/netlink/nl"
 	"golang.org/x/sys/unix"
 
 	"github.com/cilium/ebpf"
@@ -543,7 +544,11 @@ func removeTCFilters(linkAndFilters map[string][]*netlink.BpfFilter) error {
 
 func removeXDPs(links []netlink.Link) error {
 	for _, link := range links {
-		err := netlink.LinkSetXdpFd(link, -1)
+		err := netlink.LinkSetXdpFdWithFlags(link, -1, int(nl.XDP_FLAGS_DRV_MODE))
+		if err != nil {
+			return err
+		}
+		err = netlink.LinkSetXdpFdWithFlags(link, -1, int(nl.XDP_FLAGS_SKB_MODE))
 		if err != nil {
 			return err
 		}