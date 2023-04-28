g0odi3z2j/cilium

commit f52120db9cb457a141f0bd97b7bc61cebf42711b

service: Add methods to acquire backend ID for svc v2
This commit does two things:

1. Restructure the local ID acquisition code in a way that it can
   be reused by other entities to acquire node-local IDs.
2. Add methods to acquire node-local backend IDs for svc v2.

The backend IDs are going to be used in BPF maps to uniquely
identify service v2 endpoints. So, they cannot be of the
`BackendAddrID` type. Therefore, we reuse the mechanism used
by `ServiceID` to generate them. Luckily, the backend ID's
scope is node-local, so the ID generation does not have to
rely on kvstore which would make their acquisition much more
slow.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

pkg/service/id.go
@@ -15,6 +15,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/cilium/cilium/pkg/loadbalancer"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 )
@@ -27,7 +29,7 @@ func AcquireID(l3n4Addr loadbalancer.L3n4Addr, baseID uint32) (*loadbalancer.L3n
 		return acquireGlobalID(l3n4Addr, baseID)
 	}
 
-	return acquireLocalID(l3n4Addr, baseID)
+	return serviceIDAlloc.acquireLocalID(l3n4Addr, baseID)
 }
 
 // RestoreID restores  previously used service ID
@@ -41,7 +43,7 @@ func RestoreID(l3n4Addr loadbalancer.L3n4Addr, baseID uint32) (*loadbalancer.L3n
 		return acquireGlobalID(l3n4Addr, 0)
 	}
 
-	return acquireLocalID(l3n4Addr, baseID)
+	return serviceIDAlloc.acquireLocalID(l3n4Addr, baseID)
 }
 
 // GetID returns the L3n4AddrID that belongs to the given id.
@@ -50,7 +52,7 @@ func GetID(id uint32) (*loadbalancer.L3n4AddrID, error) {
 		return getGlobalID(id)
 	}
 
-	return getLocalID(id)
+	return serviceIDAlloc.getLocalID(id)
 }
 
 // DeleteID deletes the L3n4AddrID belonging to the given id from the kvstore.
@@ -61,7 +63,7 @@ func DeleteID(id uint32) error {
 		return deleteGlobalID(id)
 	}
 
-	return deleteLocalID(id)
+	return serviceIDAlloc.deleteLocalID(id)
 }
 
 func setIDSpace(next, max uint32) error {
@@ -69,7 +71,7 @@ func setIDSpace(next, max uint32) error {
 		return setGlobalIDSpace(next, max)
 	}
 
-	return setLocalIDSpace(next, max)
+	return serviceIDAlloc.setLocalIDSpace(next, max)
 }
 
 func getMaxServiceID() (uint32, error) {
@@ -77,5 +79,44 @@ func getMaxServiceID() (uint32, error) {
 		return getGlobalMaxServiceID()
 	}
 
-	return getLocalMaxServiceID()
+	return serviceIDAlloc.getLocalMaxServiceID()
+}
+
+// AcquireBackendID acquires a new local ID for the given backend.
+func AcquireBackendID(l3n4Addr loadbalancer.L3n4Addr) (uint16, error) {
+	return restoreBackendID(l3n4Addr, 0)
+}
+
+// RestoreBackendID tries to restore the given local ID for the given backend.
+//
+// If ID cannot be restored (ID already taken), returns an error.
+func RestoreBackendID(l3n4Addr loadbalancer.L3n4Addr, id uint16) error {
+	newID, err := restoreBackendID(l3n4Addr, id)
+	if err != nil {
+		return err
+	}
+
+	// TODO(brb) This shouldn't happen (otherwise, there is a bug in the code).
+	//           But maybe it makes sense to delete all svc v2 in this case.
+	if newID != id {
+		DeleteBackendID(newID)
+		return fmt.Errorf("Restored backend ID for %+v does not match (%d != %d)",
+			l3n4Addr, newID, id)
+	}
+
+	return nil
+}
+
+// DeleteBackendID releases the given backend ID.
+// TODO(brb) maybe provide l3n4Addr as an arg for the extra safety.
+func DeleteBackendID(id uint16) {
+	backendIDAlloc.deleteLocalID(uint32(id))
+}
+
+func restoreBackendID(l3n4Addr loadbalancer.L3n4Addr, id uint16) (uint16, error) {
+	l3n4AddrID, err := backendIDAlloc.acquireLocalID(l3n4Addr, uint32(id))
+	if err != nil {
+		return 0, err
+	}
+	return uint16(l3n4AddrID.ID), nil
 }

pkg/service/id_local.go
@@ -21,119 +21,135 @@ import (
 	"github.com/cilium/cilium/pkg/lock"
 )
 
-var (
-	// mutex protects servicesID, services, nextID and maxID
-	mutex lock.RWMutex
+// IDAllocator contains an internal state of the ID allocator.
+type IDAllocator struct {
+	// Protects entitiesID, entities, nextID and maxID
+	lock.RWMutex
 
-	// servicesID is a map of all services indexed by service ID
-	servicesID = map[uint32]*loadbalancer.L3n4AddrID{}
+	// entitiesID is a map of all entities indexed by service ID
+	entitiesID map[uint32]*loadbalancer.L3n4AddrID
 
-	// services is a map of all services indexed by L3n4Addr.StringID()
-	services = map[string]uint32{}
+	// entities is a map of all entities indexed by L3n4Addr.StringID()
+	entities map[string]uint32
 
 	// nextID is the next service ID to attempt to allocate
-	nextID = FirstFreeServiceID
+	nextID uint32
 
 	// maxID is the maximum service ID available for allocation
-	maxID = MaxSetOfServiceID
+	maxID uint32
+}
+
+var (
+	serviceIDAlloc = NewIDAllocator()
+	backendIDAlloc = NewIDAllocator()
 )
 
-func newServiceID(svc loadbalancer.L3n4Addr, id uint32) *loadbalancer.L3n4AddrID {
-	return &loadbalancer.L3n4AddrID{
-		L3n4Addr: svc,
-		ID:       loadbalancer.ServiceID(id),
+// NewIDAllocator creates a new ID allocator instance.
+func NewIDAllocator() *IDAllocator {
+	return &IDAllocator{
+		entitiesID: map[uint32]*loadbalancer.L3n4AddrID{},
+		entities:   map[string]uint32{},
+		nextID:     FirstFreeServiceID,
+		maxID:      MaxSetOfServiceID,
 	}
 }
 
-func addServiceID(svc loadbalancer.L3n4Addr, id uint32) *loadbalancer.L3n4AddrID {
+func (alloc *IDAllocator) addServiceID(svc loadbalancer.L3n4Addr, id uint32) *loadbalancer.L3n4AddrID {
 	svcID := newServiceID(svc, id)
-	servicesID[id] = svcID
-	services[svc.StringID()] = id
+	alloc.entitiesID[id] = svcID
+	alloc.entities[svc.StringID()] = id
 
 	return svcID
 }
 
-func acquireLocalID(svc loadbalancer.L3n4Addr, desiredID uint32) (*loadbalancer.L3n4AddrID, error) {
-	mutex.Lock()
-	defer mutex.Unlock()
+func (alloc *IDAllocator) acquireLocalID(svc loadbalancer.L3n4Addr, desiredID uint32) (*loadbalancer.L3n4AddrID, error) {
+	alloc.Lock()
+	defer alloc.Unlock()
 
-	if svcID, ok := services[svc.StringID()]; ok {
-		if svc, ok := servicesID[svcID]; ok {
+	if svcID, ok := alloc.entities[svc.StringID()]; ok {
+		if svc, ok := alloc.entitiesID[svcID]; ok {
 			return svc, nil
 		}
 	}
 
 	if desiredID != 0 {
-		if _, ok := servicesID[desiredID]; !ok {
-			return addServiceID(svc, desiredID), nil
+		if _, ok := alloc.entitiesID[desiredID]; !ok {
+			return alloc.addServiceID(svc, desiredID), nil
 		}
 	}
 
-	startingID := nextID
+	startingID := alloc.nextID
 	rollover := false
 	for {
-		if nextID == startingID && rollover {
+		if alloc.nextID == startingID && rollover {
 			break
-		} else if nextID == maxID {
-			nextID = FirstFreeServiceID
+		} else if alloc.nextID == alloc.maxID {
+			alloc.nextID = FirstFreeServiceID
 			rollover = true
 		}
 
-		if _, ok := servicesID[nextID]; !ok {
-			svcID := addServiceID(svc, nextID)
-			nextID++
+		if _, ok := alloc.entitiesID[alloc.nextID]; !ok {
+			svcID := alloc.addServiceID(svc, alloc.nextID)
+			alloc.nextID++
 			return svcID, nil
 		}
 
-		nextID++
+		alloc.nextID++
 	}
 
 	return nil, fmt.Errorf("no service ID available")
 }
 
-func getLocalID(id uint32) (*loadbalancer.L3n4AddrID, error) {
-	mutex.RLock()
-	defer mutex.RUnlock()
+func (alloc *IDAllocator) getLocalID(id uint32) (*loadbalancer.L3n4AddrID, error) {
+	alloc.RLock()
+	defer alloc.RUnlock()
 
-	if svc, ok := servicesID[id]; ok {
+	if svc, ok := alloc.entitiesID[id]; ok {
 		return svc, nil
 	}
 
 	return nil, nil
 }
 
-func deleteLocalID(id uint32) error {
-	mutex.Lock()
-	defer mutex.Unlock()
+func (alloc *IDAllocator) deleteLocalID(id uint32) error {
+	alloc.Lock()
+	defer alloc.Unlock()
 
-	if svc, ok := servicesID[id]; ok {
-		delete(servicesID, id)
-		delete(services, svc.StringID())
+	if svc, ok := alloc.entitiesID[id]; ok {
+		delete(alloc.entitiesID, id)
+		delete(alloc.entities, svc.StringID())
 	}
 
 	return nil
 }
 
-func setLocalIDSpace(next, max uint32) error {
-	mutex.Lock()
-	nextID = next
-	maxID = max
-	mutex.Unlock()
+func (alloc *IDAllocator) setLocalIDSpace(next, max uint32) error {
+	alloc.Lock()
+	alloc.nextID = next
+	alloc.maxID = max
+	alloc.Unlock()
 
 	return nil
 }
 
-func getLocalMaxServiceID() (uint32, error) {
-	mutex.RLock()
-	defer mutex.RUnlock()
-	return nextID, nil
+func (alloc *IDAllocator) getLocalMaxServiceID() (uint32, error) {
+	alloc.RLock()
+	defer alloc.RUnlock()
+	return alloc.nextID, nil
 }
 
-func resetLocalID() {
-	mutex.Lock()
-	servicesID = map[uint32]*loadbalancer.L3n4AddrID{}
-	services = map[string]uint32{}
-	nextID = FirstFreeServiceID
-	maxID = MaxSetOfServiceID
-	mutex.Unlock()
+func (alloc *IDAllocator) resetLocalID() {
+	alloc.Lock()
+	alloc.entitiesID = map[uint32]*loadbalancer.L3n4AddrID{}
+	alloc.entities = map[string]uint32{}
+	alloc.nextID = FirstFreeServiceID
+	alloc.maxID = MaxSetOfServiceID
+	alloc.Unlock()
+}
+
+func newServiceID(svc loadbalancer.L3n4Addr, id uint32) *loadbalancer.L3n4AddrID {
+	return &loadbalancer.L3n4AddrID{
+		L3n4Addr: svc,
+		ID:       loadbalancer.ServiceID(id),
+	}
 }

pkg/service/service_test.go
@@ -73,9 +73,9 @@ var _ = Suite(&ServiceLocalSuite{})
 
 func (e *ServiceLocalSuite) SetUpTest(c *C) {
 	EnableGlobalServiceID(false)
-	resetLocalID()
+	serviceIDAlloc.resetLocalID()
 }
 
 func (e *ServiceLocalSuite) TearDownTest(c *C) {
-	resetLocalID()
+	serviceIDAlloc.resetLocalID()
 }

commit fd585d15dd97a19fe8562be10fb89f6a2866e216

pkg/service: Fix premature release of backend ID
During backends restore, the agent previously released
backend ID in case of conflict. More importantly, the
ID that was released was already restored for another
backend. This will release conflicted IDs from the ID
allocator, while still leaving backend entries in the
backends map. The ID allocator can then assign the
released ID to another backend once the restore is
complete.
This commit removes releasing conflicted backend IDs.
The proper clean-up would happen in certain cases when
backends are deleted as duplicate or orphan where they
are not associated with any of the services.

Fixes: f52120db9c ("service: Add methods to acquire backend ID for svc v2")
Signed-off-by: Aditi Ghag <aditi@cilium.io>

pkg/service/id.go
@@ -61,7 +61,8 @@ func RestoreBackendID(l3n4Addr loadbalancer.L3n4Addr, id loadbalancer.BackendID)
 	// TODO(brb) This shouldn't happen (otherwise, there is a bug in the code).
 	//           But maybe it makes sense to delete all svc v2 in this case.
 	if newID != id {
-		DeleteBackendID(newID)
+		// The newID belongs to another backend that was previously restored,
+		// so don't release the ID here in case of conflicts.
 		return fmt.Errorf("restored backend ID for %+v does not match (%d != %d)",
 			l3n4Addr, newID, id)
 	}