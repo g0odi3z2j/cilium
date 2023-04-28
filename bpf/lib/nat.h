g0odi3z2j/cilium

commit fd4ca511216473a919a70225a4429c12f63eb6d8

bpf: nat: always track egress gateway connections
In order to reclaim orphan entries in the NAT table the agent uses the
connection tracking state: if a NAT entry is not backed by a connection,
it should be considered orphaned and can be reclaimed.

Currently the SNAT logic assumes that all connections that needs to be
translated are already tracked. The only exception for this is
connections originating from the local host, which are tracked by
snat_v4_track_local().

In practice egress gateway is another case that requires special
attention: in fact, all connections tunneled from a different node to a
gateway node are currently not tracked. This means that whenever the CT
garbage collector kicks in, all NAT entries for egress gateway will be
deleted, potentially leading to connections breakage.

This commit fix this by making sure we always track these connections.

While at it, also rename snat_v4_track_local() to
snat_v4_track_connection().

Towards: #21346
Signed-off-by: Gilberto Bertin <jibi@cilium.io>

bpf/lib/nat.h
@@ -108,6 +108,7 @@ struct ipv4_nat_target {
 	const __u16 min_port; /* host endianness */
 	const __u16 max_port; /* host endianness */
 	bool src_from_world;
+	bool egress_gateway; /* NAT is needed because of an egress gateway policy */
 };
 
 #if defined(ENABLE_IPV4) && defined(ENABLE_NODEPORT)
@@ -257,11 +258,11 @@ static __always_inline int snat_v4_new_mapping(struct __ctx_buff *ctx,
 	return !ret ? 0 : DROP_NAT_NO_MAPPING;
 }
 
-static __always_inline int snat_v4_track_local(struct __ctx_buff *ctx,
-					       const struct ipv4_ct_tuple *tuple,
-					       const struct ipv4_nat_entry *state,
-					       enum nat_dir dir, __u32 off,
-					       const struct ipv4_nat_target *target)
+static __always_inline int snat_v4_track_connection(struct __ctx_buff *ctx,
+						    const struct ipv4_ct_tuple *tuple,
+						    const struct ipv4_nat_entry *state,
+						    enum nat_dir dir, __u32 off,
+						    const struct ipv4_nat_target *target)
 {
 	struct ct_state ct_state;
 	struct ipv4_ct_tuple tmp;
@@ -275,6 +276,14 @@ static __always_inline int snat_v4_track_local(struct __ctx_buff *ctx,
 	} else if (!state && dir == NAT_DIR_EGRESS) {
 		if (tuple->saddr == target->addr)
 			needs_ct = true;
+#if defined(ENABLE_EGRESS_GATEWAY)
+		/* Track egress gateway connections, but only if they are
+		 * related to a remote endpoint (if the endpoint is local then
+		 * the connection is already tracked).
+		 */
+		else if (target->egress_gateway && !__lookup_ip4_endpoint(tuple->saddr))
+			needs_ct = true;
+#endif
 	}
 	if (!needs_ct)
 		return 0;
@@ -309,7 +318,7 @@ snat_v4_nat_handle_mapping(struct __ctx_buff *ctx,
 	int ret;
 
 	*state = snat_v4_lookup(tuple);
-	ret = snat_v4_track_local(ctx, tuple, *state, NAT_DIR_EGRESS, off, target);
+	ret = snat_v4_track_connection(ctx, tuple, *state, NAT_DIR_EGRESS, off, target);
 	if (ret < 0)
 		return ret;
 	else if (*state)
@@ -328,7 +337,7 @@ snat_v4_rev_nat_handle_mapping(struct __ctx_buff *ctx,
 	int ret;
 
 	*state = snat_v4_lookup(tuple);
-	ret = snat_v4_track_local(ctx, tuple, *state, NAT_DIR_INGRESS, off, target);
+	ret = snat_v4_track_connection(ctx, tuple, *state, NAT_DIR_INGRESS, off, target);
 	if (ret < 0)
 		return ret;
 	else if (*state)

bpf/lib/nodeport.h
@@ -1102,7 +1102,8 @@ static __always_inline bool nodeport_uses_dsr4(const struct ipv4_ct_tuple *tuple
  * Callers should treat contents of "from_endpoint" and "addr" as undetermined,
  * if function returns false.
  */
-static __always_inline bool snat_v4_needed(struct __ctx_buff *ctx, __be32 *addr,
+static __always_inline bool snat_v4_needed(struct __ctx_buff *ctx,
+					   struct ipv4_nat_target *target,
 					   bool *from_endpoint __maybe_unused)
 {
 	void *data, *data_end;
@@ -1121,7 +1122,7 @@ static __always_inline bool snat_v4_needed(struct __ctx_buff *ctx, __be32 *addr,
 	 */
 #if defined(TUNNEL_MODE) && defined(IS_BPF_OVERLAY)
 	if (ip4->saddr == IPV4_GATEWAY) {
-		*addr = IPV4_GATEWAY;
+		target->addr = IPV4_GATEWAY;
 		return true;
 	}
 #else
@@ -1131,12 +1132,12 @@ static __always_inline bool snat_v4_needed(struct __ctx_buff *ctx, __be32 *addr,
      */
 	if (DIRECT_ROUTING_DEV_IFINDEX == NATIVE_DEV_IFINDEX &&
 	    ip4->saddr == IPV4_DIRECT_ROUTING) {
-		*addr = IPV4_DIRECT_ROUTING;
+		target->addr = IPV4_DIRECT_ROUTING;
 		return true;
 	}
 # ifdef ENABLE_MASQUERADE
 	if (ip4->saddr == IPV4_MASQUERADE) {
-		*addr = IPV4_MASQUERADE;
+		target->addr = IPV4_MASQUERADE;
 		return true;
 	}
 # endif
@@ -1197,7 +1198,8 @@ static __always_inline bool snat_v4_needed(struct __ctx_buff *ctx, __be32 *addr,
 	if (!egress_gw_policy)
 		goto skip_egress_gateway;
 
-	*addr = egress_gw_policy->egress_ip;
+	target->addr = egress_gw_policy->egress_ip;
+	target->egress_gateway = true;
 	*from_endpoint = true;
 
 	return true;
@@ -1253,7 +1255,7 @@ skip_egress_gateway:
 		 */
 		if (!is_reply && local_ep) {
 			*from_endpoint = true;
-			*addr = IPV4_MASQUERADE;
+			target->addr = IPV4_MASQUERADE;
 			return true;
 		}
 	}
@@ -1269,10 +1271,11 @@ static __always_inline int nodeport_nat_ipv4_fwd(struct __ctx_buff *ctx)
 		.min_port = NODEPORT_PORT_MIN_NAT,
 		.max_port = NODEPORT_PORT_MAX_NAT,
 		.addr = 0,
+		.egress_gateway = 0,
 	};
 	int ret = CTX_ACT_OK;
 
-	if (snat_v4_needed(ctx, &target.addr, &from_endpoint))
+	if (snat_v4_needed(ctx, &target, &from_endpoint))
 		ret = snat_v4_nat(ctx, &target, from_endpoint);
 	if (ret == NAT_PUNT_TO_STACK)
 		ret = CTX_ACT_OK;

bpf/tests/nat_test.c
@@ -90,10 +90,10 @@ int bpf_test(__maybe_unused struct xdp_md *ctx)
 	/* can simply assume it to be -1 because the actually value does not matter. */
 	mock_ct_lookup4_response = -1;
 
-	/* So snat_v4_track_local will return exactly the same value which means */
-	/* an error occurs when snat_v4_track_local is looking for the ipv4_ct_tuple. */
+	/* So snat_v4_track_connection will return exactly the same value which means */
+	/* an error occurs when snat_v4_track_connection is looking for the ipv4_ct_tuple. */
 	TEST("return -1 on error", {
-		if (snat_v4_track_local(&ctx_buff, &tuple, &state, NAT_DIR_EGRESS, 0, &target)
+		if (snat_v4_track_connection(&ctx_buff, &tuple, &state, NAT_DIR_EGRESS, 0, &target)
 		    != -1){
 			test_fail();
 		}
@@ -103,10 +103,10 @@ int bpf_test(__maybe_unused struct xdp_md *ctx)
 	/* also assume it to be 1 because the actually value does not matter. */
 	mock_ct_lookup4_response = 1;
 
-	/* So snat_v4_track_local will return 0 which means snat_v4_track_local */
+	/* So snat_v4_track_connection will return 0 which means snat_v4_track_connection */
 	/* successfully tracks ipv4_ct_tuple. */
 	TEST("return 0 on track", {
-		if (snat_v4_track_local(&ctx_buff, &tuple, &state, NAT_DIR_EGRESS, 0, &target)
+		if (snat_v4_track_connection(&ctx_buff, &tuple, &state, NAT_DIR_EGRESS, 0, &target)
 		    != 0){
 			test_fail();
 		}
@@ -119,10 +119,10 @@ int bpf_test(__maybe_unused struct xdp_md *ctx)
 	mock_ct_lookup4_response = CT_NEW;
 	mock_ct_create4_response = -1;
 
-	/* So snat_v4_track_local will return that value which means an error occurs */
-	/* when snat_v4_track_local is trying to create the ipv4_ct_tuple. */
+	/* So snat_v4_track_connection will return that value which means an error occurs */
+	/* when snat_v4_track_connection is trying to create the ipv4_ct_tuple. */
 	TEST("return -1 on create error", {
-		if (snat_v4_track_local(&ctx_buff, &tuple, &state, NAT_DIR_EGRESS, 0, &target)
+		if (snat_v4_track_connection(&ctx_buff, &tuple, &state, NAT_DIR_EGRESS, 0, &target)
 		    != -1){
 			test_fail();
 		}
@@ -134,10 +134,10 @@ int bpf_test(__maybe_unused struct xdp_md *ctx)
 	mock_ct_lookup4_response = CT_NEW;
 	mock_ct_create4_response = 0;
 
-	/* So snat_v4_track_local will return 0 which means snat_v4_track_local */
+	/* So snat_v4_track_connection will return 0 which means snat_v4_track_connection */
 	/* successfully creates the ipv4_ct_tuple. */
 	TEST("return 0 on create success", {
-		if (snat_v4_track_local(&ctx_buff, &tuple, &state, NAT_DIR_EGRESS, 0, &target)
+		if (snat_v4_track_connection(&ctx_buff, &tuple, &state, NAT_DIR_EGRESS, 0, &target)
 		    != 0) {
 			test_fail();
 		}

test/k8s/egress.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
 
 	. "github.com/onsi/gomega"
@@ -76,8 +77,23 @@ var _ = SkipDescribeIf(func() bool {
 		res.ExpectSuccess()
 	}
 
-	BeforeAll(func() {
+	// ctEntriesOnNode returns the number of CT entries matching destination
+	// IP and port on a given node
+	ctEntriesOnNode := func(node, dstIP, dstPort string) int {
+		ciliumPod, err := kubectl.GetCiliumPodOnNode(node)
+		Expect(err).Should(BeNil(), "Unable to determine cilium pod on node %s", node)
 
+		cmd := fmt.Sprintf("cilium bpf ct list global | grep '\\-> %s:%s\\b' | wc -l", dstIP, dstPort)
+		res := kubectl.ExecPodCmd(helpers.CiliumNamespace, ciliumPod, cmd)
+		res.ExpectSuccess()
+
+		n, err := strconv.Atoi(strings.TrimSpace(res.Stdout()))
+		Expect(err).Should(BeNil(), "Cannot parse output of '%s' command", cmd)
+
+		return n
+	}
+
+	BeforeAll(func() {
 		kubectl = helpers.CreateKubectl(helpers.K8s1VMName(), logger)
 
 		k8s1Name, k8s1IP = kubectl.GetNodeInfo(helpers.K8s1)
@@ -128,9 +144,14 @@ var _ = SkipDescribeIf(func() bool {
 
 		for _, src := range []string{srcPod, srcPod2} {
 			By("Testing that a request from pod %s to outside is SNATed with the egressIP %s", src, egressIP)
+			ctEntriesBeforeConnection := ctEntriesOnNode(helpers.K8s2, outsideIP, "80")
 			res := kubectl.ExecPodCmd(randomNamespace, src, helpers.CurlFail("http://%s:80", outsideIP))
+
 			res.ExpectSuccess()
 			res.ExpectMatchesRegexp(fmt.Sprintf("client_address=::ffff:%s\n", egressIP))
+
+			ctEntriesAfterConnection := ctEntriesOnNode(helpers.K8s2, outsideIP, "80")
+			Expect(ctEntriesAfterConnection - ctEntriesBeforeConnection).Should(Equal(1))
 		}
 	}
 

commit bf05d7c753eceb908121f7834be386511e33ca1d

bpf: Extend the CT expiration timeout for egress gateway packets
When the SNAT entry already exists for a connection that goes through
the egress gateway (i.e. it's not the first packet), ct_lookup4 is
skipped. That means that ct_update_timeout is not called, and the CT
entry will be garbage collected while the connection is still active.

To fix this bug, do the CT lookups for the second and further packets of
connections using egress gateway as well.

Fixes: fd4ca5112164 ("bpf: nat: always track egress gateway connections")
Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

bpf/lib/nat.h
@@ -319,17 +319,17 @@ static __always_inline int snat_v4_track_connection(struct __ctx_buff *ctx,
 
 	if (state && state->common.host_local) {
 		needs_ct = true;
-	} else if (!state && dir == NAT_DIR_EGRESS) {
-		if (tuple->saddr == target->addr)
-			needs_ct = true;
 #if defined(ENABLE_EGRESS_GATEWAY)
-		/* Track egress gateway connections, but only if they are
-		 * related to a remote endpoint (if the endpoint is local then
-		 * the connection is already tracked).
-		 */
-		else if (target->egress_gateway && !target->from_local_endpoint)
-			needs_ct = true;
+	/* Track egress gateway connections, but only if they are related to a
+	 * remote endpoint (if the endpoint is local then the connection is
+	 * already tracked). NAT_DIR_EGRESS is implied. Do ct_lookup4 also for
+	 * established connections to extend the expiration timeout.
+	 */
+	} else if (target->egress_gateway && !target->from_local_endpoint) {
+		needs_ct = true;
 #endif
+	} else if (!state && dir == NAT_DIR_EGRESS && tuple->saddr == target->addr) {
+		needs_ct = true;
 	}
 	if (!needs_ct)
 		return 0;