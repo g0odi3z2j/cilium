g0odi3z2j/cilium

commit 463dde17681ffe691ea7f75b898b9e739df158d8

datapath: Introduce node IDs
Node IDs are node-scoped unique identities for all cluster nodes. This
commit introduces two methods, to allocate and release such IDs.

They will be used in a subsequent patchset to identify remote nodes in
our datapath.

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/node_ids.go
@@ -1,0 +1,124 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package linux
+
+import (
+	"net"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/cilium/cilium/pkg/idpool"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/node"
+	nodeTypes "github.com/cilium/cilium/pkg/node/types"
+)
+
+const (
+	minNodeID = 1
+	maxNodeID = idpool.ID(^uint16(0))
+)
+
+// AllocateNodeID allocates a new ID for the given node (by IP) if one wasn't
+// already assigned.
+func (n *linuxNodeHandler) AllocateNodeID(nodeIP net.IP) uint16 {
+	if len(nodeIP) == 0 || nodeIP.IsUnspecified() {
+		// This should never happen. If it ever does, we may have an unexpected
+		// call to AllocateNodeID.
+		log.Warning("Attempt to allocate a node ID for an empty node IP address")
+		return 0
+	}
+
+	// Don't allocate a node ID for the local node.
+	localNode := node.GetIPv4()
+	if localNode.Equal(nodeIP) {
+		return 0
+	}
+
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+
+	if nodeID, exists := n.nodeIDsByIPs[nodeIP.String()]; exists {
+		return nodeID
+	}
+
+	nodeID := uint16(n.nodeIDs.AllocateID())
+	if nodeID == uint16(idpool.NoID) {
+		log.Error("No more IDs available for nodes")
+	} else {
+		log.WithFields(logrus.Fields{
+			logfields.NodeID: nodeID,
+			logfields.IPAddr: nodeIP,
+		}).Debug("Allocated new node ID for node IP address")
+	}
+	n.nodeIDsByIPs[nodeIP.String()] = nodeID
+	return nodeID
+}
+
+// allocateIDForNode allocates a new ID for the given node if one hasn't already
+// been assigned. If any of the node IPs have an ID associated, then all other
+// node IPs receive the same. This might happen if we allocated a node ID from
+// the ipcache, where we don't have all node IPs but only one.
+func (n *linuxNodeHandler) allocateIDForNode(node *nodeTypes.Node) uint16 {
+	nodeID := uint16(0)
+
+	// Did we already allocate a node ID for any IP of that node?
+	for _, addr := range node.IPAddresses {
+		if id, exists := n.nodeIDsByIPs[addr.IP.String()]; exists {
+			nodeID = id
+		}
+	}
+
+	if nodeID == 0 {
+		nodeID = uint16(n.nodeIDs.AllocateID())
+		if nodeID == uint16(idpool.NoID) {
+			log.WithField(logfields.NodeName, node.Name).Error("No more IDs available for nodes")
+		} else {
+			log.WithFields(logrus.Fields{
+				logfields.NodeID:   nodeID,
+				logfields.NodeName: node.Name,
+			}).Debug("Allocated new node ID for node")
+		}
+	}
+
+	for _, addr := range node.IPAddresses {
+		n.nodeIDsByIPs[addr.IP.String()] = nodeID
+	}
+	return nodeID
+}
+
+// deallocateIDForNode deallocates the node ID for the given node, if it was allocated.
+func (n *linuxNodeHandler) deallocateIDForNode(oldNode *nodeTypes.Node) {
+	nodeID := n.nodeIDsByIPs[oldNode.IPAddresses[0].IP.String()]
+	for _, addr := range oldNode.IPAddresses {
+		id := n.nodeIDsByIPs[addr.IP.String()]
+		if nodeID != id {
+			log.WithFields(logrus.Fields{
+				logfields.NodeName: oldNode.Name,
+				logfields.IPAddr:   addr.IP,
+			}).Errorf("Found two node IDs (%d and %d) for the same node", id, nodeID)
+		}
+	}
+
+	n.deallocateNodeIDLocked(nodeID)
+}
+
+// DeallocateNodeID deallocates the given node ID, if it was allocated.
+func (n *linuxNodeHandler) DeallocateNodeID(nodeID uint16) {
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+	n.deallocateNodeIDLocked(nodeID)
+}
+
+func (n *linuxNodeHandler) deallocateNodeIDLocked(nodeID uint16) {
+	for ip, id := range n.nodeIDsByIPs {
+		if nodeID == id {
+			delete(n.nodeIDsByIPs, ip)
+		}
+	}
+
+	if !n.nodeIDs.Insert(idpool.ID(nodeID)) {
+		log.WithField(logfields.NodeID, nodeID).Warn("Attempted to deallocate a node ID that wasn't allocated")
+	}
+	log.WithField(logfields.NodeID, nodeID).Debug("Deallocate node ID")
+}

daemon/cmd/status.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"net"
 	"strings"
 	"time"
 
@@ -529,6 +530,11 @@ func (c *clusterNodesClient) NodeNeighborRefresh(ctx context.Context, node nodeT
 func (c *clusterNodesClient) NodeCleanNeighbors(migrateOnly bool) {
 	// no-op
 	return
+}
+
+func (c *clusterNodesClient) AllocateNodeID(_ net.IP) uint16 {
+	// no-op
+	return 0
 }
 
 func (h *getNodes) cleanupClients() {

pkg/datapath/fake/node.go
@@ -5,6 +5,7 @@ package fake
 
 import (
 	"context"
+	"net"
 
 	"github.com/cilium/cilium/pkg/datapath"
 	"github.com/cilium/cilium/pkg/lock"
@@ -61,4 +62,8 @@ func (n *FakeNodeHandler) NodeNeighborRefresh(ctx context.Context, node nodeType
 
 func (n *FakeNodeHandler) NodeCleanNeighbors(migrateOnly bool) {
 	return
+}
+
+func (n *FakeNodeHandler) AllocateNodeID(_ net.IP) uint16 {
+	return 0
 }

pkg/datapath/linux/node.go
@@ -28,6 +28,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/datapath/types"
+	"github.com/cilium/cilium/pkg/idpool"
 	ipamOption "github.com/cilium/cilium/pkg/ipam/option"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
@@ -41,9 +42,7 @@ import (
 const (
 	wildcardIPv4 = "0.0.0.0"
 	wildcardIPv6 = "0::0"
-)
 
-const (
 	neighFileName = "neigh-link.json"
 )
 
@@ -70,6 +69,11 @@ type linuxNodeHandler struct {
 	neighLastPingByNextHop map[string]time.Time      // key = string(net.IP)
 	wgAgent                datapath.WireguardAgent
 
+	// Pool of available IDs for nodes.
+	nodeIDs idpool.IDPool
+	// Node-scoped unique IDs for the nodes.
+	nodeIDsByIPs map[string]uint16
+
 	ipsecMetricCollector prometheus.Collector
 }
 
@@ -86,6 +90,8 @@ func NewNodeHandler(datapathConfig DatapathConfiguration, nodeAddressing types.N
 		neighByNextHop:         map[string]*netlink.Neigh{},
 		neighLastPingByNextHop: map[string]time.Time{},
 		wgAgent:                wgAgent,
+		nodeIDs:                idpool.NewIDPool(minNodeID, maxNodeID),
+		nodeIDsByIPs:           map[string]uint16{},
 		ipsecMetricCollector:   ipsec.NewXFRMCollector(),
 	}
 }
@@ -1234,6 +1240,8 @@ func (n *linuxNodeHandler) nodeDelete(oldNode *nodeTypes.Node) error {
 			return err
 		}
 	}
+
+	n.deallocateIDForNode(oldNode)
 
 	return nil
 }

pkg/datapath/node.go
@@ -134,4 +134,8 @@ type NodeHandler interface {
 	// NodeCleanNeighbors cleans all neighbor entries for the direct routing device
 	// and the encrypt interface.
 	NodeCleanNeighbors(migrateOnly bool)
+
+	// AllocateNodeID allocates a new ID for the given node (by IP) if one wasn't
+	// already assigned.
+	AllocateNodeID(net.IP) uint16
 }

pkg/hubble/peer/handler.go
@@ -5,6 +5,7 @@ package peer
 
 import (
 	"context"
+	"net"
 	"strings"
 
 	peerpb "github.com/cilium/cilium/api/v1/peer"
@@ -130,6 +131,11 @@ func (h handler) NodeCleanNeighbors(migrateOnly bool) {
 // Close frees handler resources.
 func (h *handler) Close() {
 	close(h.stop)
+}
+
+func (h *handler) AllocateNodeID(_ net.IP) uint16 {
+	// no-op
+	return 0
 }
 
 // newChangeNotification creates a new change notification with the provided

pkg/logging/logfields/logfields.go
@@ -17,6 +17,9 @@ const (
 	// Node is a host machine in the cluster, running cilium
 	Node = "node"
 
+	// NodeID is the node-scoped ID of the node as allocated by the agent
+	NodeID = "nodeID"
+
 	// NodeName is a human readable name for the node
 	NodeName = "nodeName"
 

pkg/node/manager/manager_test.go
@@ -172,6 +172,10 @@ func (n *signalNodeHandler) NodeCleanNeighbors(migrateOnly bool) {
 	return
 }
 
+func (n *signalNodeHandler) AllocateNodeID(_ net.IP) uint16 {
+	return 0
+}
+
 func (s *managerTestSuite) SetUpSuite(c *check.C) {
 }
 

commit a2fd0aa4b9c51e589ef70dadee83969ecd23875d

linux/node_id: do not attempt to map NoID
We correctly detect that we failed to allocate a new node ID (due to
exhaustion of the idpool), but then still go ahead and map it. This
leads to spurious errors which include "Failed to map node IP address to
allocated ID".

Instead, don't try to map NoID and return it directly.

Fixes: 463dde1768 (datapath: Introduce node IDs)

Suggested-by: Paul Chaignon <paul@cilium.io>
Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

pkg/datapath/linux/node_ids.go
@@ -49,6 +49,7 @@ func (n *linuxNodeHandler) AllocateNodeID(nodeIP net.IP) uint16 {
 	nodeID := uint16(n.nodeIDs.AllocateID())
 	if nodeID == uint16(idpool.NoID) {
 		log.Error("No more IDs available for nodes")
+		return nodeID
 	} else {
 		log.WithFields(logrus.Fields{
 			logfields.NodeID: nodeID,