g0odi3z2j/cilium

commit 521d2d501d57b7e851186c52c8a4820d3277484a

clustermesh: extend CiliumClusterConfig with capabilities
Extend the CiliumClusterConfig struct to allow expressing cluster-specific
capabilities. Currently, the only supported capability concerns the
support of per-prefix "synced" canaries. When enabled, the clustermesh-apiserver
writes keys in the form `cilium/synced/$name/$prefix` into etcd to
state that the initial synchronization of the keys under the `$prefix`
prefix for cluster `$name` has completed successfully.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/clustermesh/types/types.go
@@ -29,6 +29,13 @@ func ValidateClusterID(clusterID uint32) error {
 
 type CiliumClusterConfig struct {
 	ID uint32 `json:"id,omitempty"`
+
+	Capabilities CiliumClusterConfigCapabilities `json:"capabilities,omitempty"`
+}
+
+type CiliumClusterConfigCapabilities struct {
+	// Supports per-prefix "synced" canaries
+	SyncedCanaries bool `json:"syncedFlags,omitempty"`
 }
 
 func (c0 *CiliumClusterConfig) IsCompatible(c1 *CiliumClusterConfig) error {

commit 0f64909eacfffb0704b41f3099be28b884a0353f

clustermesh: fix SyncedCanaries capability name mismatch
521d2d501d57 ("clustermesh: extend CiliumClusterConfig with capabilities")
just introduced the SyncedCanaries capability. Yet, there was a mismatch
between the variable name and the JSON field name, which is now fixed.

Fixes: 521d2d501d57 ("clustermesh: extend CiliumClusterConfig with capabilities")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/clustermesh/types/types.go
@@ -35,7 +35,7 @@ type CiliumClusterConfig struct {
 
 type CiliumClusterConfigCapabilities struct {
 	// Supports per-prefix "synced" canaries
-	SyncedCanaries bool `json:"syncedFlags,omitempty"`
+	SyncedCanaries bool `json:"syncedCanaries,omitempty"`
 }
 
 func (c0 *CiliumClusterConfig) IsCompatible(c1 *CiliumClusterConfig) error {