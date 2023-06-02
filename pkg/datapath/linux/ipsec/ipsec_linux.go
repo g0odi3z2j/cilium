g0odi3z2j/cilium

commit c0d062d19a97934019456f408849029999773003

ipsec: Custom check for XFRM state existence
UpsertIPsecEndpoint is currently unable to replace stale XFRM states. We
use XfrmStateAdd, which fails with EEXIST if a state with the same key
(IPs, SPI, and mark) already exists. We can't use XfrmStateUpdate because
it fails with ESRCH is no state with the specified key exist.

Note we don't have the same issue for XFRM policies because
XfrmPolicyUpdate doesn't return ESRCH if no such policy already exists.
No idea why the two APIs are not consistent.

We therefore need to implement a proper 'update or insert' logic
for XFRM states ourselves.

To that end, we first check if the state we want to add already exists.
If it doesn't, we attempt to add it. If it fails with EEXIST, we know
that some other state is conflicting. In that case, we attempt to remove
any conflicting XFRM states that are found and then attempt to add the
new state again. To find conflicting XFRM states, we use the same logic
as the kernel does (cf. __xfrm_state_lookup).

Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -149,6 +149,96 @@ func ipSecJoinState(state *netlink.XfrmState, keys *ipSecKey) {
 	state.Reqid = keys.ReqID
 }
 
+// xfrmStateReplace attempts to add a new XFRM state only if one doesn't
+// already exist. If it doesn't but some other XFRM state conflicts, then
+// we attempt to remove the conflicting state before trying to add again.
+func xfrmStateReplace(new *netlink.XfrmState) error {
+	states, err := netlink.XfrmStateList(netlink.FAMILY_ALL)
+	if err != nil {
+		return fmt.Errorf("Cannot get XFRM state: %s", err)
+	}
+
+	// Check if the XFRM state already exists
+	for _, s := range states {
+		if xfrmIPEqual(s.Src, new.Src) && xfrmIPEqual(s.Dst, new.Dst) &&
+			xfrmMarkEqual(s.OutputMark, new.OutputMark) &&
+			xfrmMarkEqual(s.Mark, new.Mark) && s.Spi == new.Spi {
+			return nil
+		}
+	}
+
+	// It doesn't exist so let's attempt to add it.
+	firstAttemptErr := netlink.XfrmStateAdd(new)
+	if !os.IsExist(firstAttemptErr) {
+		return firstAttemptErr
+	}
+	log.WithFields(logrus.Fields{
+		logfields.SPI:           new.Spi,
+		logfields.SourceIP:      new.Src,
+		logfields.DestinationIP: new.Dst,
+	}).Warn("Failed to add XFRM state due to conflicting state")
+
+	// An existing state conflicts with this one. We need to remove the
+	// existing one first.
+	deletedSomething, err := xfrmDeleteConflictingState(states, new)
+	if err != nil {
+		return err
+	}
+
+	// If no conflicting state was found and deleted, there's no point in
+	// attempting to add again.
+	if !deletedSomething {
+		return firstAttemptErr
+	}
+	return netlink.XfrmStateAdd(new)
+}
+
+// Attempt to remove any XFRM state that conflicts with the state we just tried
+// to add. To find those conflicting states, we need to use the same logic that
+// the kernel used to reject our check with EEXIST. That logic is upstream in
+// __xfrm_state_lookup.
+func xfrmDeleteConflictingState(states []netlink.XfrmState, new *netlink.XfrmState) (bool, error) {
+	deletedSomething := false
+	for _, s := range states {
+		if new.Spi == s.Spi && (new.Mark == nil) == (s.Mark == nil) &&
+			(new.Mark == nil || new.Mark.Value&new.Mark.Mask&s.Mark.Mask == s.Mark.Value) &&
+			xfrmIPEqual(new.Src, s.Src) && xfrmIPEqual(new.Dst, s.Dst) {
+			err := netlink.XfrmStateDel(&s)
+			if err == nil {
+				deletedSomething = true
+				log.WithFields(logrus.Fields{
+					logfields.SPI:           s.Spi,
+					logfields.SourceIP:      s.Src,
+					logfields.DestinationIP: s.Dst,
+				}).Info("Removed a conflicting XFRM state")
+			} else {
+				return deletedSomething, fmt.Errorf("Failed to remove a conflicting XFRM state: %w", err)
+			}
+		}
+	}
+	return deletedSomething, nil
+}
+
+// This function compares two IP addresses and returns true if they are equal.
+// This is unfortunately necessary because our netlink library returns nil IPv6
+// addresses as nil IPv4 addresses and net.IP.Equal rightfully considers those
+// are different.
+func xfrmIPEqual(ip1, ip2 net.IP) bool {
+	if ip1.IsUnspecified() && ip2.IsUnspecified() {
+		return true
+	}
+	return ip1.Equal(ip2)
+}
+
+// Returns true if two XFRM marks are identical. They should be either both nil
+// or have the same mark value and mask.
+func xfrmMarkEqual(mark1, mark2 *netlink.XfrmMark) bool {
+	if (mark1 == nil) != (mark2 == nil) {
+		return false
+	}
+	return mark1 == nil || (mark1.Value == mark2.Value && mark1.Mask == mark2.Mask)
+}
+
 func ipSecReplaceStateIn(localIP, remoteIP net.IP, zeroMark bool) (uint8, error) {
 	key := getIPSecKeys(remoteIP)
 	if key == nil {
@@ -174,7 +264,7 @@ func ipSecReplaceStateIn(localIP, remoteIP net.IP, zeroMark bool) (uint8, error)
 		}
 	}
 
-	return key.Spi, netlink.XfrmStateAdd(state)
+	return key.Spi, xfrmStateReplace(state)
 }
 
 func ipSecReplaceStateOut(localIP, remoteIP net.IP, nodeID uint16) (uint8, error) {
@@ -191,7 +281,7 @@ func ipSecReplaceStateOut(localIP, remoteIP net.IP, nodeID uint16) (uint8, error
 		Value: linux_defaults.RouteMarkEncrypt,
 		Mask:  linux_defaults.RouteMarkMask,
 	}
-	return key.Spi, netlink.XfrmStateAdd(state)
+	return key.Spi, xfrmStateReplace(state)
 }
 
 func _ipSecReplacePolicyInFwd(src, dst *net.IPNet, tmplSrc, tmplDst net.IP, proxyMark bool, dir netlink.Dir) error {
@@ -393,9 +483,7 @@ func UpsertIPsecEndpoint(local, remote *net.IPNet, outerLocal, outerRemote net.I
 	if !outerLocal.Equal(outerRemote) {
 		if dir == IPSecDirIn || dir == IPSecDirBoth {
 			if spi, err = ipSecReplaceStateIn(outerLocal, outerRemote, outputMark); err != nil {
-				if !os.IsExist(err) {
-					return 0, fmt.Errorf("unable to replace local state: %s", err)
-				}
+				return 0, fmt.Errorf("unable to replace local state: %s", err)
 			}
 			if err = ipSecReplacePolicyIn(remote, local, outerRemote, outerLocal); err != nil {
 				if !os.IsExist(err) {
@@ -411,9 +499,7 @@ func UpsertIPsecEndpoint(local, remote *net.IPNet, outerLocal, outerRemote net.I
 
 		if dir == IPSecDirOut || dir == IPSecDirOutNode || dir == IPSecDirBoth {
 			if spi, err = ipSecReplaceStateOut(outerLocal, outerRemote, remoteNodeID); err != nil {
-				if !os.IsExist(err) {
-					return 0, fmt.Errorf("unable to replace remote state: %s", err)
-				}
+				return 0, fmt.Errorf("unable to replace remote state: %s", err)
 			}
 
 			if err = ipSecReplacePolicyOut(local, remote, outerLocal, outerRemote, remoteNodeID, dir); err != nil {

pkg/logging/logfields/logfields.go
@@ -630,6 +630,8 @@ const (
 	// SourceIP is a source IP
 	SourceIP = "sourceIP"
 
+	DestinationIP = "destinationIP"
+
 	// DestinationCIDR is a destination CIDR
 	DestinationCIDR = "destinationCIDR"
 

commit 52c454f97aea66a009ba98afd4412cc97f3cdcad

ipsec: Allow old and new XFRM OUT states to coexist for upgrade
Commit 72629bb746 ("ipsec: Match OUT XFRM states & policies using node
IDs") changed our XFRM states to match on packet marks of the form
0xXXXXYe00/0xffffff00 where XXXX is the node ID and Y is the SPI. The
previous format for the packet mark in XFRM states was 0xYe00/0xff00.

According to the Linux kernel these two states conflict (because
0xXXXXYe00/0xffffff00 ∈ 0xYe00/0xff00). That means we can't add the new
state while the old one is around.

Thus, in commit c0d062d19 ("ipsec: Custom check for XFRM state
existence"), we removed any old conflicting XFRM state before adding
the new ones. That however causes packet drops on upgrades because we
may remove the old XFRM state before bpf_lxc has been updated to use the
new 0xXXXXYe00/0xffffff00 mark. Instead, we would need both XFRM state
formats to coexist for the duration of the upgrade.

Impossible, you say! Don't despair. Things are actually a bit more
complicated (it's IPsec and Linux after all).

While Linux doesn't allow us to add 0xXXXXYe00/0xffffff00 when
0xYe00/0xff00 exists, it does allow adding in the reverse order. That
seems to be because 0xXXXXYe00/0xffffff00 ∈ 0xYe00/0xff00 but
0xYe00/0xff00 ∉ 0xXXXXYe00/0xffffff00 [1].

Therefore, to have both XFRM states coexist, we can remove the old
state, add the new one, then re-add the old state. That is allowed
because we never try to add the new state when the old is present.

During the short period of time when we have removed the old XFRM
state, we can have a packet drops due to the missing state. These drops
should be limited to the specific node pair this XFRM state is
handling. This will also only happen on upgrades. Finally, this
shouldn't happen with ENI and Azure IPAM modes because they don't have
such old conflicting states.

I tested this upgrade path on a 20-nodes GKE cluster running our
drop-sensitive application, migrate-svc, scaled up to 50 clients and 30
backends. I didn't get a single packet drop despite the application
consistently sending packets back and forth between nodes. Thus, I think
the window for drops to happen is really small.

Diff before/after the upgrade (v1.13.0 -> thi patch, GKE):

      src 10.24.1.77 dst 10.24.2.207
          proto esp spi 0x00000003 reqid 1 mode tunnel
          replay-window 0
          mark 0x3e00/0xff00 output-mark 0xe00/0xf00
          aead rfc4106(gcm(aes)) 0xfc2d0c4e646b87ff2d0801b57997e3598eab0d6b 128
    -     anti-replay context: seq 0x0, oseq 0x2c, bitmap 0x00000000
    +     anti-replay context: seq 0x0, oseq 0x16, bitmap 0x00000000
          sel src 0.0.0.0/0 dst 0.0.0.0/0
    + src 10.24.1.77 dst 10.24.2.207
    +     proto esp spi 0x00000003 reqid 1 mode tunnel
    +     replay-window 0
    +     mark 0x713f3e00/0xffffff00 output-mark 0xe00/0xf00
    +     aead rfc4106(gcm(aes)) 0xfc2d0c4e646b87ff2d0801b57997e3598eab0d6b 128
    +     anti-replay context: seq 0x0, oseq 0x0, bitmap 0x00000000
    +     sel src 0.0.0.0/0 dst 0.0.0.0/0

We can notice that the counters for the existing XFRM state also
changed (decreased). That's expected since the state got recreated.

1 - I think this is because XFRM states don't have priorities. So when
    two XFRM states would match a given packet (in our case a packet
    with mark XXXXYe00), the oldest XFRM state is taken. Thus, by not
    allowing to add a more specific match after a more generic one, the
    kernel ensures that the more specific match is always taken when
    both match a packet. That likely corresponds to user expectations.
    That is, if both 0xXXXXYe00/0xffffff00 and 0xYe00/0xff00 match a
    packet, we would probably expect 0xXXXXYe00/0xffffff00 to be used.
Fixes: c0d062d19 ("ipsec: Custom check for XFRM state existence")

pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -188,6 +188,12 @@ func xfrmStateReplace(new *netlink.XfrmState) error {
 		return fmt.Errorf("Cannot get XFRM state: %s", err)
 	}
 
+	scopedLog := log.WithFields(logrus.Fields{
+		logfields.SPI:           new.Spi,
+		logfields.SourceIP:      new.Src,
+		logfields.DestinationIP: new.Dst,
+	})
+
 	// Check if the XFRM state already exists
 	for _, s := range states {
 		if xfrmIPEqual(s.Src, new.Src) && xfrmIPEqual(s.Dst, new.Dst) &&
@@ -197,16 +203,47 @@ func xfrmStateReplace(new *netlink.XfrmState) error {
 		}
 	}
 
+	oldXFRMMark := &netlink.XfrmMark{
+		Value: ipSecXfrmMarkSetSPI(linux_defaults.RouteMarkEncrypt, uint8(new.Spi)),
+		Mask:  linux_defaults.IPsecOldMarkMaskOut,
+	}
+	for _, s := range states {
+		// This is the XFRM OUT state from a previous Cilium version.
+		// Because its mark matches the new mark (0xXXXX3e00/0xffffff00 ∈
+		// 0x3e00/0xff00), the kernel considers the two states conflict and we
+		// won't be able to add the new one until the old one is removed.
+		//
+		// Thus, we temporarily remove the old, conflicting XFRM state and
+		// re-add it in a defer. In between the removal of the old state and
+		// the addition of the new, we can have a packet drops due to the
+		// missing state. These drops should be limited to the specific node
+		// pair we are handling here and the window during which they can
+		// happen should be really small. This is also specific to the upgrade
+		// and can be removed in v1.15. Finally, this shouldn't happen with ENI
+		// and Azure IPAM modes because they don't have such conflicting states.
+		if xfrmIPEqual(s.Src, new.Src) && xfrmIPEqual(s.Dst, new.Dst) &&
+			xfrmMarkEqual(s.OutputMark, new.OutputMark) &&
+			xfrmMarkEqual(s.Mark, oldXFRMMark) && s.Spi == new.Spi {
+			err := netlink.XfrmStateDel(&s)
+			if err != nil {
+				scopedLog.WithError(err).Error("Failed to remove old XFRM state")
+			} else {
+				scopedLog.Infof("Temporarily removed old XFRM state")
+				defer func(oldXFRMState netlink.XfrmState) {
+					if err := netlink.XfrmStateAdd(&oldXFRMState); err != nil {
+						scopedLog.WithError(err).Errorf("Failed to re-add old XFRM state")
+					}
+				}(s)
+			}
+		}
+	}
+
 	// It doesn't exist so let's attempt to add it.
 	firstAttemptErr := netlink.XfrmStateAdd(new)
 	if !os.IsExist(firstAttemptErr) {
 		return firstAttemptErr
 	}
-	log.WithFields(logrus.Fields{
-		logfields.SPI:           new.Spi,
-		logfields.SourceIP:      new.Src,
-		logfields.DestinationIP: new.Dst,
-	}).Warn("Failed to add XFRM state due to conflicting state")
+	scopedLog.Error("Failed to add XFRM state due to conflicting state")
 
 	// An existing state conflicts with this one. We need to remove the
 	// existing one first.