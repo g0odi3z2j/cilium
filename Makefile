g0odi3z2j/cilium

commit 2cb41f589dce1bb2d8eeaedb3392dde051710b32

Set SED binary from Makefile in kind.sh
Our Makefiles and scripts largely assume GNU sed, and kind.sh uses `sed
-z` which requires GNU sed. MacOS ships with BSD sed, so to make the
existing `kind.sh` script work, make the sed binary configurable, and
pass through the Makefile SED to the script.

Signed-off-by: Chance Zibolski <chance.zibolski@gmail.com>

Makefile
@@ -411,7 +411,7 @@ microk8s: check-microk8s ## Build cilium-dev docker image and import to microk8s
 	$(QUIET)./contrib/scripts/microk8s-import.sh $(LOCAL_OPERATOR_IMAGE)
 
 kind: ## Create a kind cluster for Cilium development.
-	$(QUIET)./contrib/scripts/kind.sh
+	SED=$(SED) $(QUIET)./contrib/scripts/kind.sh
 
 kind-down: ## Destroy a kind cluster for Cilium development.
 	$(QUIET)./contrib/scripts/kind-down.sh

Makefile.defs
@@ -39,7 +39,7 @@ DOCKER_BUILD_FLAGS?=
 
 # use gsed if avaiable, otherwise use sed.
 # gsed is needed for MacOS to make in-place replacement work correctly.
-SED ?= $(if $(shell command -v gsed), gsed, sed)
+SED ?= $(if $(shell command -v gsed),gsed,sed)
 
 # Set DOCKER_DEV_ACCOUNT with "cilium" by default
 ifeq ($(DOCKER_DEV_ACCOUNT),)

contrib/scripts/kind.sh
@@ -18,6 +18,8 @@ default_network="kind-cilium"
 
 PROG=${0}
 
+SED="${SED:-sed}"
+
 xdp=false
 if [ "${1:-}" = "--xdp" ]; then
   xdp=true
@@ -169,7 +171,7 @@ fi
 # Replace "forward . /etc/resolv.conf" in the coredns cm with "forward . 8.8.8.8".
 # This is required because in case of BPF Host Routing we bypass iptables thus
 # breaking DNS. See https://github.com/cilium/cilium/issues/23330
-NewCoreFile=$(kubectl get cm -n kube-system coredns -o jsonpath='{.data.Corefile}' | sed 's,forward . /etc/resolv.conf,forward . 8.8.8.8,' | sed -z 's/\n/\\n/g')
+NewCoreFile=$(kubectl get cm -n kube-system coredns -o jsonpath='{.data.Corefile}' | "${SED}" 's,forward . /etc/resolv.conf,forward . 8.8.8.8,' | "${SED}" -z 's/\n/\\n/g')
 kubectl patch configmap/coredns -n kube-system --type merge -p '{"data":{"Corefile": "'"$NewCoreFile"'"}}'
 
 set +e

commit 9558cbf41af18896335dd54a22e3fd91ad08c274

Makefile: Fix kind deployment in quiet mode
The "$(QUIET)" directive is a Makefile syntax converting to something
like "@" when env V=0, so as a Makefile directive it must be at the
beginning of the line, or "make kind" will fail. Fix it.

CC: Chance Zibolski <chance.zibolski@gmail.com>
Fixes: 2cb41f589dce ("Set SED binary from Makefile in kind.sh")
Signed-off-by: Joe Stringer <joe@cilium.io>

Makefile
@@ -416,7 +416,7 @@ microk8s: check-microk8s ## Build cilium-dev docker image and import to microk8s
 	$(QUIET)./contrib/scripts/microk8s-import.sh $(LOCAL_OPERATOR_IMAGE)
 
 kind: ## Create a kind cluster for Cilium development.
-	SED=$(SED) $(QUIET)./contrib/scripts/kind.sh
+	$(QUIET)SED=$(SED) ./contrib/scripts/kind.sh
 
 kind-down: ## Destroy a kind cluster for Cilium development.
 	$(QUIET)./contrib/scripts/kind-down.sh