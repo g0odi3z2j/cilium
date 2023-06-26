g0odi3z2j/cilium

commit c2bfc629f54513bb3a9d22d13df9a9c794d17932

ipmasq: Implement ip-masq-agent support for IPv6
Add the relevant map definition and the corresponding handling in
package ipmasq for supporting IPv6 with the ip-masq-agent.

We get a new BPF map for storing entries that we do not want to
masquerade. The maximum number of entries is the same as for the
IPv4-related map, and will be made customisable in a future commit.
Handling of the map is nearly identical to the one for IPv4.

From the outside of the ipmasq package, the IPMasqBPFMap abstraction
hides the fact that the maps are distinct, its methods take care of
identifying the IP version in use for the CIDRs passed in arguments and
to update the relevant map.

Option "nonMasqueradeCIDRs" is supported transparently as well. Support
for option "masqLinkLocalIPv6" is added. By default, the ip-masq-agent
prevents masquerading to CIDR fe80::/10, as described in [0], based on
RFC 4291.

However, at this point, the Agent still refuses to accept configurations
using IPv6 and ip-masq-agent, so this code is not used just yet.

[0] https://github.com/kubernetes-sigs/ip-masq-agent#configuring-the-agent

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

pkg/datapath/maps/map.go
@@ -141,6 +141,7 @@ func (ms *MapSweeper) RemoveDisabledMaps() {
 			lbmap.Affinity6MapName,
 			lbmap.SourceRange6MapName,
 			lbmap.HealthProbe6MapName,
+			ipmasq.MapNameIPv6,
 			cidrmap.MapName + "v6_dyn",
 			cidrmap.MapName + "v6_fix",
 		}...)
@@ -204,7 +205,12 @@ func (ms *MapSweeper) RemoveDisabledMaps() {
 	}
 
 	if !option.Config.EnableIPMasqAgent {
-		maps = append(maps, ipmasq.MapNameIPv4)
+		if option.Config.EnableIPv4 {
+			maps = append(maps, ipmasq.MapNameIPv4)
+		}
+		if option.Config.EnableIPv6 {
+			maps = append(maps, ipmasq.MapNameIPv6)
+		}
 	}
 
 	if !option.Config.EnableXDPPrefilter {

pkg/ipmasq/ipmasq.go
@@ -39,6 +39,8 @@ var (
 	}
 	linkLocalCIDRIPv4Str = "169.254.0.0/16"
 	linkLocalCIDRIPv4    = mustParseCIDR(linkLocalCIDRIPv4Str)
+	linkLocalCIDRIPv6Str = "fe80::/10"
+	linkLocalCIDRIPv6    = mustParseCIDR(linkLocalCIDRIPv6Str)
 )
 
 // ipnet is a wrapper type for net.IPNet to enable de-serialization of CIDRs
@@ -51,7 +53,7 @@ func (c *Ipnet) UnmarshalJSON(json []byte) error {
 		return fmt.Errorf("Invalid CIDR: %s", str)
 	}
 
-	n, err := parseCIDRv4(strings.Trim(str, `"`))
+	n, err := parseCIDR(strings.Trim(str, `"`))
 	if err != nil {
 		return err
 	}
@@ -60,7 +62,7 @@ func (c *Ipnet) UnmarshalJSON(json []byte) error {
 	return nil
 }
 
-func parseCIDRv4(c string) (*net.IPNet, error) {
+func parseCIDR(c string) (*net.IPNet, error) {
 	ip, n, err := net.ParseCIDR(c)
 	if err != nil {
 		return nil, fmt.Errorf("Invalid CIDR %s: %s", c, err)
@@ -75,6 +77,7 @@ func parseCIDRv4(c string) (*net.IPNet, error) {
 type config struct {
 	NonMasqCIDRs      []Ipnet `json:"nonMasqueradeCIDRs"`
 	MasqLinkLocalIPv4 bool    `json:"masqLinkLocal"`
+	MasqLinkLocalIPv6 bool    `json:"masqLinkLocalIPv6"`
 }
 
 // IPMasqMap is an interface describing methods for manipulating an ipmasq map
@@ -88,6 +91,7 @@ type IPMasqMap interface {
 type IPMasqAgent struct {
 	configPath             string
 	masqLinkLocalIPv4      bool
+	masqLinkLocalIPv6      bool
 	nonMasqCIDRsFromConfig map[string]net.IPNet
 	nonMasqCIDRsInMap      map[string]net.IPNet
 	ipMasqMap              IPMasqMap
@@ -192,6 +196,10 @@ func (a *IPMasqAgent) Update() error {
 		a.nonMasqCIDRsFromConfig[linkLocalCIDRIPv4Str] = linkLocalCIDRIPv4
 	}
 
+	if !a.masqLinkLocalIPv6 {
+		a.nonMasqCIDRsFromConfig[linkLocalCIDRIPv6Str] = linkLocalCIDRIPv6
+	}
+
 	for cidrStr, cidr := range a.nonMasqCIDRsFromConfig {
 		if _, ok := a.nonMasqCIDRsInMap[cidrStr]; !ok {
 			log.WithField(logfields.CIDR, cidrStr).Info("Adding CIDR")
@@ -222,6 +230,7 @@ func (a *IPMasqAgent) readConfig() (bool, error) {
 			log.WithField(logfields.Path, a.configPath).Info("Config file not found")
 			a.nonMasqCIDRsFromConfig = map[string]net.IPNet{}
 			a.masqLinkLocalIPv4 = false
+			a.masqLinkLocalIPv6 = false
 			return true, nil
 		}
 		return false, fmt.Errorf("Failed to read %s: %s", a.configPath, err)
@@ -230,6 +239,7 @@ func (a *IPMasqAgent) readConfig() (bool, error) {
 	if len(raw) == 0 {
 		a.nonMasqCIDRsFromConfig = map[string]net.IPNet{}
 		a.masqLinkLocalIPv4 = false
+		a.masqLinkLocalIPv6 = false
 		return true, nil
 	}
 
@@ -249,6 +259,7 @@ func (a *IPMasqAgent) readConfig() (bool, error) {
 	}
 	a.nonMasqCIDRsFromConfig = nonMasqCIDRs
 	a.masqLinkLocalIPv4 = cfg.MasqLinkLocalIPv4
+	a.masqLinkLocalIPv6 = cfg.MasqLinkLocalIPv6
 
 	return false, nil
 }
@@ -271,7 +282,7 @@ func (a *IPMasqAgent) restore() error {
 }
 
 func mustParseCIDR(c string) net.IPNet {
-	n, err := parseCIDRv4(c)
+	n, err := parseCIDR(c)
 	if err != nil {
 		panic(err)
 	}

pkg/ipmasq/ipmasq_test.go
@@ -12,6 +12,7 @@ import (
 
 	check "github.com/cilium/checkmate"
 
+	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/lock"
 )
 
@@ -22,6 +23,7 @@ func Test(t *testing.T) {
 type ipMasqMapMock struct {
 	lock.RWMutex
 	cidrsIPv4 map[string]net.IPNet
+	cidrsIPv6 map[string]net.IPNet
 }
 
 func (m *ipMasqMapMock) Update(cidr net.IPNet) error {
@@ -29,10 +31,17 @@ func (m *ipMasqMapMock) Update(cidr net.IPNet) error {
 	defer m.Unlock()
 
 	cidrStr := cidr.String()
-	if _, ok := m.cidrsIPv4[cidrStr]; ok {
-		return fmt.Errorf("CIDR already exists: %s", cidrStr)
+	if ip.IsIPv4(cidr.IP) {
+		if _, ok := m.cidrsIPv4[cidrStr]; ok {
+			return fmt.Errorf("CIDR already exists: %s", cidrStr)
+		}
+		m.cidrsIPv4[cidrStr] = cidr
+	} else {
+		if _, ok := m.cidrsIPv6[cidrStr]; ok {
+			return fmt.Errorf("CIDR already exists: %s", cidrStr)
+		}
+		m.cidrsIPv6[cidrStr] = cidr
 	}
-	m.cidrsIPv4[cidrStr] = cidr
 
 	return nil
 }
@@ -42,10 +51,17 @@ func (m *ipMasqMapMock) Delete(cidr net.IPNet) error {
 	defer m.Unlock()
 
 	cidrStr := cidr.String()
-	if _, ok := m.cidrsIPv4[cidrStr]; !ok {
-		return fmt.Errorf("CIDR not found: %s", cidrStr)
+	if ip.IsIPv4(cidr.IP) {
+		if _, ok := m.cidrsIPv4[cidrStr]; !ok {
+			return fmt.Errorf("CIDR not found: %s", cidrStr)
+		}
+		delete(m.cidrsIPv4, cidrStr)
+	} else {
+		if _, ok := m.cidrsIPv6[cidrStr]; !ok {
+			return fmt.Errorf("CIDR not found: %s", cidrStr)
+		}
+		delete(m.cidrsIPv6, cidrStr)
 	}
-	delete(m.cidrsIPv4, cidrStr)
 
 	return nil
 }
@@ -58,6 +74,9 @@ func (m *ipMasqMapMock) Dump() ([]net.IPNet, error) {
 	for _, cidr := range m.cidrsIPv4 {
 		cidrs = append(cidrs, cidr)
 	}
+	for _, cidr := range m.cidrsIPv6 {
+		cidrs = append(cidrs, cidr)
+	}
 
 	return cidrs, nil
 }
@@ -66,8 +85,11 @@ func (m *ipMasqMapMock) dumpToSet() map[string]struct{} {
 	m.RLock()
 	defer m.RUnlock()
 
-	cidrs := make(map[string]struct{}, len(m.cidrsIPv4))
+	cidrs := make(map[string]struct{}, len(m.cidrsIPv4)+len(m.cidrsIPv6))
 	for cidrStr := range m.cidrsIPv4 {
+		cidrs[cidrStr] = struct{}{}
+	}
+	for cidrStr := range m.cidrsIPv6 {
 		cidrs[cidrStr] = struct{}{}
 	}
 

pkg/maps/ipmasq/ipmasq.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/ebpf"
+	"github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/types"
 )
@@ -17,6 +18,8 @@ import (
 const (
 	MapNameIPv4    = "cilium_ipmasq_v4"
 	MaxEntriesIPv4 = 16384
+	MapNameIPv6    = "cilium_ipmasq_v6"
+	MaxEntriesIPv6 = 16384
 )
 
 type Key4 struct {
@@ -26,6 +29,14 @@ type Key4 struct {
 
 func (k *Key4) String() string  { return fmt.Sprintf("%s", k.Address) }
 func (k *Key4) New() bpf.MapKey { return &Key4{} }
+
+type Key6 struct {
+	PrefixLen uint32
+	Address   types.IPv6
+}
+
+func (k *Key6) String() string  { return fmt.Sprintf("%s", k.Address) }
+func (k *Key6) New() bpf.MapKey { return &Key6{} }
 
 type Value struct {
 	Pad uint8 // not used
@@ -37,6 +48,8 @@ func (v *Value) New() bpf.MapValue { return &Value{} }
 var (
 	ipMasq4Map *bpf.Map
 	onceIPv4   sync.Once
+	ipMasq6Map *bpf.Map
+	onceIPv6   sync.Once
 )
 
 func IPMasq4Map() *bpf.Map {
@@ -54,23 +67,56 @@ func IPMasq4Map() *bpf.Map {
 	return ipMasq4Map
 }
 
+func IPMasq6Map() *bpf.Map {
+	onceIPv6.Do(func() {
+		ipMasq6Map = bpf.NewMap(
+			MapNameIPv6,
+			ebpf.LPMTrie,
+			&Key6{},
+			&Value{},
+			MaxEntriesIPv6,
+			bpf.BPF_F_NO_PREALLOC,
+		).WithCache().WithPressureMetric().
+			WithEvents(option.Config.GetEventBufferConfig(MapNameIPv6))
+	})
+	return ipMasq6Map
+}
+
 type IPMasqBPFMap struct{}
 
 func (*IPMasqBPFMap) Update(cidr net.IPNet) error {
-	return IPMasq4Map().Update(keyIPv4(cidr), &Value{})
+	if ip.IsIPv4(cidr.IP) {
+		return IPMasq4Map().Update(keyIPv4(cidr), &Value{})
+	} else {
+		return IPMasq6Map().Update(keyIPv6(cidr), &Value{})
+	}
 }
 
 func (*IPMasqBPFMap) Delete(cidr net.IPNet) error {
-	return IPMasq4Map().Delete(keyIPv4(cidr))
+	if ip.IsIPv4(cidr.IP) {
+		return IPMasq4Map().Delete(keyIPv4(cidr))
+	} else {
+		return IPMasq6Map().Delete(keyIPv6(cidr))
+	}
 }
 
 func (*IPMasqBPFMap) Dump() ([]net.IPNet, error) {
 	cidrs := []net.IPNet{}
-	if err := IPMasq4Map().DumpWithCallback(
-		func(keyIPv4 bpf.MapKey, _ bpf.MapValue) {
-			cidrs = append(cidrs, keyToIPNetIPv4(keyIPv4.(*Key4)))
-		}); err != nil {
-		return nil, err
+	if ipMasq4Map != nil {
+		if err := ipMasq4Map.DumpWithCallback(
+			func(keyIPv4 bpf.MapKey, _ bpf.MapValue) {
+				cidrs = append(cidrs, keyToIPNetIPv4(keyIPv4.(*Key4)))
+			}); err != nil {
+			return nil, err
+		}
+	}
+	if ipMasq6Map != nil {
+		if err := ipMasq6Map.DumpWithCallback(
+			func(keyIPv6 bpf.MapKey, _ bpf.MapValue) {
+				cidrs = append(cidrs, keyToIPNetIPv6(keyIPv6.(*Key6)))
+			}); err != nil {
+			return nil, err
+		}
 	}
 	return cidrs, nil
 }
@@ -89,6 +135,26 @@ func keyToIPNetIPv4(key *Key4) net.IPNet {
 	)
 
 	cidr.Mask = net.CIDRMask(int(key.PrefixLen), 32)
+	key.Address.DeepCopyInto(&ip)
+	cidr.IP = ip.IP()
+
+	return cidr
+}
+
+func keyIPv6(cidr net.IPNet) *Key6 {
+	ones, _ := cidr.Mask.Size()
+	key := &Key6{PrefixLen: uint32(ones)}
+	copy(key.Address[:], cidr.IP.To16())
+	return key
+}
+
+func keyToIPNetIPv6(key *Key6) net.IPNet {
+	var (
+		cidr net.IPNet
+		ip   types.IPv6
+	)
+
+	cidr.Mask = net.CIDRMask(int(key.PrefixLen), 128)
 	key.Address.DeepCopyInto(&ip)
 	cidr.IP = ip.IP()
 

commit cffa8779f7328f460734cd8db365f1d4d281f8d8

maps: Fix conditions for disabling ip-masq-agent related maps
When adding support for IPv6 ip-masq-agent, the related maps were not
handled correctly in RemoveDisabledMaps(), resulting in maps being kept,
even if option.Config.EnableIPv(4|6)Masquerade is disabled. Let's fix
the conditions to sweep these maps.

Fixes: c2bfc629f545 ("ipmasq: Implement ip-masq-agent support for IPv6")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

pkg/datapath/maps/map.go
@@ -204,13 +204,12 @@ func (ms *MapSweeper) RemoveDisabledMaps() {
 		maps = append(maps, lbmap.SourceRange6MapName, lbmap.SourceRange4MapName)
 	}
 
-	if !option.Config.EnableIPMasqAgent {
-		if option.Config.EnableIPv4 {
-			maps = append(maps, ipmasq.MapNameIPv4)
-		}
-		if option.Config.EnableIPv6 {
-			maps = append(maps, ipmasq.MapNameIPv6)
-		}
+	if !(option.Config.EnableIPMasqAgent && option.Config.EnableIPv4Masquerade) {
+		maps = append(maps, ipmasq.MapNameIPv4)
+	}
+
+	if !(option.Config.EnableIPMasqAgent && option.Config.EnableIPv6Masquerade) {
+		maps = append(maps, ipmasq.MapNameIPv6)
 	}
 
 	if !option.Config.EnableXDPPrefilter {