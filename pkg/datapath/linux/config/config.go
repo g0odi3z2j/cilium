g0odi3z2j/cilium

commit 26fe8ce68357231da86cf92eaf2a8e8530ffd876

ipmasq: Enable IPv6 support for ip-masq-agent
Enable ip-masq-agent support for IPv6 in the Cilium Agent:

- Make the Agent accept IPv6 _and_ ip-masq-agent in its configuration.
- Similarly, make the daemon accept IPv6 with the ip-masq-agent.
- Create the IPv6-related BPF map on Agent startup.
- When IPv6 and the ip-masq-agent are selected, pass the relevant macro
  to the datapath to process packets accordingly.

Related updates:

- Display the maximum number of entries for the IPv6-related map in the
  status command.
- Update the documentation.
- Add the (commented) Helm value.

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

pkg/datapath/linux/config/config.go
@@ -608,8 +608,14 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 
 				// ip-masq-agent depends on bpf-masq
 				if option.Config.EnableIPMasqAgent {
-					cDefinesMap["ENABLE_IP_MASQ_AGENT_IPV4"] = "1"
-					cDefinesMap["IP_MASQ_AGENT_IPV4"] = ipmasq.MapNameIPv4
+					if option.Config.EnableIPv4 {
+						cDefinesMap["ENABLE_IP_MASQ_AGENT_IPV4"] = "1"
+						cDefinesMap["IP_MASQ_AGENT_IPV4"] = ipmasq.MapNameIPv4
+					}
+					if option.Config.EnableIPv6 {
+						cDefinesMap["ENABLE_IP_MASQ_AGENT_IPV6"] = "1"
+						cDefinesMap["IP_MASQ_AGENT_IPV6"] = ipmasq.MapNameIPv6
+					}
 				}
 			}
 			if option.Config.EnableIPv6Masquerade {

daemon/cmd/daemon.go
@@ -1005,8 +1005,8 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 			return nil, nil, fmt.Errorf("failed to determine BPF masquerade addrs: %w", err)
 		}
 	} else if option.Config.EnableIPMasqAgent {
-		log.WithError(err).Errorf("BPF ip-masq-agent requires --%s=\"true\" and --%s=\"true\"", option.EnableIPv4Masquerade, option.EnableBPFMasquerade)
-		return nil, nil, fmt.Errorf("BPF ip-masq-agent requires --%s=\"true\" and --%s=\"true\"", option.EnableIPv4Masquerade, option.EnableBPFMasquerade)
+		log.WithError(err).Errorf("BPF ip-masq-agent requires (--%s=\"true\" or --%s=\"true\") and --%s=\"true\"", option.EnableIPv4Masquerade, option.EnableIPv6Masquerade, option.EnableBPFMasquerade)
+		return nil, nil, fmt.Errorf("BPF ip-masq-agent requires (--%s=\"true\" or --%s=\"true\") and --%s=\"true\"", option.EnableIPv4Masquerade, option.EnableIPv6Masquerade, option.EnableBPFMasquerade)
 	} else if option.Config.EnableIPv4EgressGateway {
 		log.WithError(err).Errorf("egress gateway requires --%s=\"true\" and --%s=\"true\"", option.EnableIPv4Masquerade, option.EnableBPFMasquerade)
 		return nil, nil, fmt.Errorf("egress gateway requires --%s=\"true\" and --%s=\"true\"", option.EnableIPv4Masquerade, option.EnableBPFMasquerade)
@@ -1014,12 +1014,6 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		log.Infof("Auto-disabling %q feature since IPv4 and IPv6 masquerading are disabled",
 			option.EnableBPFMasquerade)
 		option.Config.EnableBPFMasquerade = false
-	}
-	if option.Config.EnableIPMasqAgent {
-		if !option.Config.EnableIPv4 {
-			log.WithError(err).Errorf("BPF ip-masq-agent requires IPv4 support (--%s=\"true\")", option.EnableIPv4Name)
-			return nil, nil, fmt.Errorf("BPF ip-masq-agent requires IPv4 support (--%s=\"true\")", option.EnableIPv4Name)
-		}
 	}
 	if len(option.Config.GetDevices()) == 0 {
 		if option.Config.EnableHostFirewall {

daemon/cmd/datapath.go
@@ -416,7 +416,12 @@ func (d *Daemon) initMaps() error {
 
 	if option.Config.EnableIPv4 && option.Config.EnableIPMasqAgent {
 		if err := ipmasq.IPMasq4Map().OpenOrCreate(); err != nil {
-			return fmt.Errorf("initializing masquerading map: %w", err)
+			return fmt.Errorf("initializing IPv4 masquerading map: %w", err)
+		}
+	}
+	if option.Config.EnableIPv6 && option.Config.EnableIPMasqAgent {
+		if err := ipmasq.IPMasq6Map().OpenOrCreate(); err != nil {
+			return fmt.Errorf("initializing IPv6 masquerading map: %w", err)
 		}
 	}
 

daemon/cmd/status.go
@@ -336,6 +336,10 @@ func (d *Daemon) getBPFMapStatus() *models.BPFMapStatus {
 				Size: int64(ipmasqmap.MaxEntriesIPv4),
 			},
 			{
+				Name: "IPv6 masquerading agent",
+				Size: int64(ipmasqmap.MaxEntriesIPv6),
+			},
+			{
 				Name: "IPv4 fragmentation",
 				Size: int64(option.Config.FragmentsMapEntries),
 			},

install/kubernetes/cilium/values.yaml
@@ -1487,6 +1487,7 @@ ipMasqAgent:
 # config:
   # nonMasqueradeCIDRs: []
   # masqLinkLocal: false
+  # masqLinkLocalIPv6: false
 
 # iptablesLockTimeout defines the iptables "--wait" option when invoked from Cilium.
 # iptablesLockTimeout: "5s"

install/kubernetes/cilium/values.yaml.tmpl
@@ -1484,6 +1484,7 @@ ipMasqAgent:
 # config:
   # nonMasqueradeCIDRs: []
   # masqLinkLocal: false
+  # masqLinkLocalIPv6: false
 
 # iptablesLockTimeout defines the iptables "--wait" option when invoked from Cilium.
 # iptablesLockTimeout: "5s"

pkg/ipmasq/ipmasq.go
@@ -63,12 +63,9 @@ func (c *Ipnet) UnmarshalJSON(json []byte) error {
 }
 
 func parseCIDR(c string) (*net.IPNet, error) {
-	ip, n, err := net.ParseCIDR(c)
+	_, n, err := net.ParseCIDR(c)
 	if err != nil {
 		return nil, fmt.Errorf("Invalid CIDR %s: %s", c, err)
-	}
-	if ip.To4() == nil {
-		return nil, fmt.Errorf("Invalid CIDR %s: only IPv4 is supported", c)
 	}
 	return n, nil
 }

commit 771c5961f6829f1bdfb059ae0968112250b3e725

config: Do not define IPv6 ip-masq-agent macros under IPv4 config
Fix a bug introduced with the support for IPv6 ip-masq-agent. The macros
for IPv6 are not at the right location, and they will be set if IPv4
masquerading is set, even if it is disabled for IPv6. Let's move the
code to its right location.

Fixes: 26fe8ce68357 ("ipmasq: Enable IPv6 support for ip-masq-agent")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

pkg/datapath/linux/config/config.go
@@ -608,14 +608,8 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 
 				// ip-masq-agent depends on bpf-masq
 				if option.Config.EnableIPMasqAgent {
-					if option.Config.EnableIPv4 {
-						cDefinesMap["ENABLE_IP_MASQ_AGENT_IPV4"] = "1"
-						cDefinesMap["IP_MASQ_AGENT_IPV4"] = ipmasq.MapNameIPv4
-					}
-					if option.Config.EnableIPv6 {
-						cDefinesMap["ENABLE_IP_MASQ_AGENT_IPV6"] = "1"
-						cDefinesMap["IP_MASQ_AGENT_IPV6"] = ipmasq.MapNameIPv6
-					}
+					cDefinesMap["ENABLE_IP_MASQ_AGENT_IPV4"] = "1"
+					cDefinesMap["IP_MASQ_AGENT_IPV4"] = ipmasq.MapNameIPv4
 				}
 			}
 			if option.Config.EnableIPv6Masquerade {
@@ -625,6 +619,11 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 				fw.WriteString(FmtDefineAddress("IPV6_SNAT_EXCLUSION_DST_CIDR", cidr.IP))
 				extraMacrosMap["IPV6_SNAT_EXCLUSION_DST_CIDR_MASK"] = cidr.Mask.String()
 				fw.WriteString(FmtDefineAddress("IPV6_SNAT_EXCLUSION_DST_CIDR_MASK", cidr.Mask))
+
+				if option.Config.EnableIPMasqAgent {
+					cDefinesMap["ENABLE_IP_MASQ_AGENT_IPV6"] = "1"
+					cDefinesMap["IP_MASQ_AGENT_IPV6"] = ipmasq.MapNameIPv6
+				}
 			}
 		}
 