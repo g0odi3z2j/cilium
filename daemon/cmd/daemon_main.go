g0odi3z2j/cilium

commit 007fce495190f5de33b7549c327232773f362128

clustermesh: use custom dialer for service resolution
When kvstoremesh is enabled, the agent connects to the local kvstore,
rather to remote ones. Hence, it targets the corresponding service.
Yet, since agents run in host network, service resolution requires that
the DNSPolicy is set to ClusterFirstWithHostNet, introducing a
dependency on CoreDNS. To prevent this requirement, let's configure a
custom dialer responsible for service resolution based on the service
cached information.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

pkg/clustermesh/internal/remote_cluster.go
@@ -13,12 +13,14 @@ import (
 	"github.com/go-openapi/strfmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 
 	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/clustermesh/types"
 	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	cmutils "github.com/cilium/cilium/pkg/clustermesh/utils"
 	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/metrics"
@@ -46,6 +48,9 @@ type remoteCluster struct {
 
 	// clusterSizeDependantInterval allows to calculate intervals based on cluster size.
 	clusterSizeDependantInterval kvstore.ClusterSizeDependantIntervalFunc
+
+	// serviceIPGetter, if not nil, is used to create a custom dialer for service resolution.
+	serviceIPGetter k8s.ServiceIPGetter
 
 	// changed receives an event when the remote cluster configuration has
 	// changed and is closed when the configuration file was removed
@@ -287,10 +292,18 @@ func (rc *remoteCluster) makeEtcdOpts() map[string]string {
 }
 
 func (rc *remoteCluster) makeExtraOpts() kvstore.ExtraOptions {
+	var dialOpts []grpc.DialOption
+	if rc.serviceIPGetter != nil {
+		// Allow to resolve service names without depending on the DNS. This prevents the need
+		// for setting the DNSPolicy to ClusterFirstWithHostNet when running in host network.
+		dialOpts = append(dialOpts, grpc.WithContextDialer(k8s.CreateCustomDialer(rc.serviceIPGetter, rc.getLogger())))
+	}
+
 	return kvstore.ExtraOptions{
 		NoLockQuorumCheck:            true,
 		ClusterName:                  rc.name,
 		ClusterSizeDependantInterval: rc.clusterSizeDependantInterval,
+		DialOption:                   dialOpts,
 	}
 }
 

pkg/clustermesh/cell.go
@@ -23,7 +23,7 @@ var Cell = cell.Module(
 	cell.Provide(NewClusterMesh),
 
 	// Convert concrete objects into more restricted interfaces used by clustermesh.
-	cell.ProvidePrivate(func(sc *k8s.ServiceCache) ServiceMerger { return sc }),
+	cell.ProvidePrivate(func(sc *k8s.ServiceCache) (ServiceMerger, k8s.ServiceIPGetter) { return sc, sc }),
 	cell.ProvidePrivate(func(ipcache *ipcache.IPCache) ipcache.IPCacher { return ipcache }),
 	cell.ProvidePrivate(func(mgr nodemanager.NodeManager) (store.Observer, kvstore.ClusterSizeDependantIntervalFunc) {
 		return nodeStore.NewNodeObserver(mgr), mgr.ClusterSizeDependantInterval

pkg/clustermesh/clustermesh.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/hive/cell"
 	"github.com/cilium/cilium/pkg/ipcache"
+	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/kvstore/store"
 	"github.com/cilium/cilium/pkg/lock"
@@ -56,6 +57,9 @@ type Configuration struct {
 
 	// ClusterSizeDependantInterval allows to calculate intervals based on cluster size.
 	ClusterSizeDependantInterval kvstore.ClusterSizeDependantIntervalFunc
+
+	// ServiceIPGetter, if not nil, is used to create a custom dialer for service resolution.
+	ServiceIPGetter k8s.ServiceIPGetter
 
 	Metrics         Metrics
 	InternalMetrics internal.Metrics
@@ -110,6 +114,7 @@ func NewClusterMesh(lifecycle hive.Lifecycle, c Configuration) *ClusterMesh {
 		Config:                       c.Config,
 		ClusterIDName:                c.ClusterIDName,
 		ClusterSizeDependantInterval: c.ClusterSizeDependantInterval,
+		ServiceIPGetter:              c.ServiceIPGetter,
 
 		NewRemoteCluster: cm.newRemoteCluster,
 

pkg/clustermesh/internal/clustermesh.go
@@ -12,6 +12,7 @@ import (
 	"github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/lock"
 )
@@ -50,6 +51,9 @@ type Configuration struct {
 
 	// ClusterSizeDependantInterval allows to calculate intervals based on cluster size.
 	ClusterSizeDependantInterval kvstore.ClusterSizeDependantIntervalFunc
+
+	// ServiceIPGetter, if not nil, is used to create a custom dialer for service resolution.
+	ServiceIPGetter k8s.ServiceIPGetter
 
 	// Metrics holds the different clustermesh metrics.
 	Metrics Metrics
@@ -112,6 +116,7 @@ func (cm *ClusterMesh) newRemoteCluster(name, path string) *remoteCluster {
 		name:                         name,
 		configPath:                   path,
 		clusterSizeDependantInterval: cm.conf.ClusterSizeDependantInterval,
+		serviceIPGetter:              cm.conf.ServiceIPGetter,
 
 		changed:     make(chan bool, configNotificationsChannelSize),
 		controllers: controller.NewManager(),

commit 368f68e297f0139770405ea394ec4fe2bc814ab5

clustermesh: silence custom dialer failure log messages
007fce495190 ("clustermesh: use custom dialer for service resolution")
configured a custom dialer to perform service resolution when the etcd
address refers to a local service (mainly in the kvstoremesh case).

Yet, the custom dialer function outputs quite verbose log messages, which
can be misleading when the address does not point to a local service (as
it is correct that the address cannot be parsed). One example being:

level=error msg="Unable to parse etcd service URL"
  error="parse \"172.19.0.2:2379\": first path segment in URL cannot contain colon"

Hence, let's silence them, as they do more harm than good in this specific
situation. We still print (at debug level) the outcome of the translation,
so that we know to whom we are actually connecting.

Fixes: 007fce495190 ("clustermesh: use custom dialer for service resolution")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

daemon/cmd/daemon_main.go
@@ -1566,7 +1566,7 @@ func (d *Daemon) initKVStore() {
 		)
 		log := log.WithField(logfields.LogSubsys, "etcd")
 		goopts.DialOption = []grpc.DialOption{
-			grpc.WithContextDialer(k8s.CreateCustomDialer(d.k8sWatcher.K8sSvcCache, log)),
+			grpc.WithContextDialer(k8s.CreateCustomDialer(d.k8sWatcher.K8sSvcCache, log, true)),
 		}
 	}
 

operator/cmd/root.go
@@ -570,7 +570,7 @@ func (legacy *legacyOnLeader) onStart(_ hive.HookContext) error {
 					log := log.WithField(logfields.LogSubsys, "etcd")
 					goopts = &kvstore.ExtraOptions{
 						DialOption: []grpc.DialOption{
-							grpc.WithContextDialer(k8s.CreateCustomDialer(svcGetter, log)),
+							grpc.WithContextDialer(k8s.CreateCustomDialer(svcGetter, log, true)),
 						},
 					}
 				}

pkg/clustermesh/internal/remote_cluster.go
@@ -311,7 +311,8 @@ func (rc *remoteCluster) makeExtraOpts() kvstore.ExtraOptions {
 	if rc.serviceIPGetter != nil {
 		// Allow to resolve service names without depending on the DNS. This prevents the need
 		// for setting the DNSPolicy to ClusterFirstWithHostNet when running in host network.
-		dialOpts = append(dialOpts, grpc.WithContextDialer(k8s.CreateCustomDialer(rc.serviceIPGetter, rc.getLogger())))
+		logger := log.WithField(fieldClusterName, rc.name)
+		dialOpts = append(dialOpts, grpc.WithContextDialer(k8s.CreateCustomDialer(rc.serviceIPGetter, logger, false)))
 	}
 
 	return kvstore.ExtraOptions{

pkg/k8s/service.go
@@ -689,9 +689,10 @@ type ServiceIPGetter interface {
 }
 
 // CreateCustomDialer returns a custom dialer that picks the service IP,
-// from the given ServiceIPGetter, if the address the used to dial is a k8s
-// service.
-func CreateCustomDialer(b ServiceIPGetter, log *logrus.Entry) func(ctx context.Context, addr string) (conn net.Conn, e error) {
+// from the given ServiceIPGetter, if the address used to dial is a k8s
+// service. If verboseLogs is set, a log message is output when the
+// address to service IP translation fails.
+func CreateCustomDialer(b ServiceIPGetter, log *logrus.Entry, verboseLogs bool) func(ctx context.Context, addr string) (conn net.Conn, e error) {
 	return func(ctx context.Context, s string) (conn net.Conn, e error) {
 		// If the service is available, do the service translation to
 		// the service IP. Otherwise dial with the original service
@@ -713,19 +714,20 @@ func CreateCustomDialer(b ServiceIPGetter, log *logrus.Entry) func(ctx context.C
 				svcIP := b.GetServiceIP(*svc)
 				if svcIP != nil {
 					s = svcIP.String()
-				} else {
+				} else if verboseLogs {
 					log.Debug("Service not found in the service IP getter")
 				}
-			} else {
+			} else if verboseLogs {
 				log.WithFields(logrus.Fields{
 					"url-host": u.Host,
 					"url":      s,
 				}).Debug("Unable to parse etcd service URL into a service ID")
 			}
-			log.Debugf("custom dialer based on k8s service backend is dialing to %q", s)
-		} else {
+		} else if verboseLogs {
 			log.WithError(err).Error("Unable to parse etcd service URL")
 		}
+
+		log.Debugf("Custom dialer based on k8s service backend is dialing to %q", s)
 		return net.Dial("tcp", s)
 	}
 }