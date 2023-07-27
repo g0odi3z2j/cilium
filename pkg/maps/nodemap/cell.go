g0odi3z2j/cilium

commit 79bee301980ff11e01d131980936edc1831947c1

nodeid map: provide map via hive cell
This commit introduces a hive cell for the nodeid map.

The datapath cell has been refactored to depend on the nodeid map -
which introduces proper init order before accessing the map during
nodeid restoration (currently only working due to the fact, that the map
gets implicitly opened when iterating over its entries).

Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

pkg/maps/nodemap/cell.go
@@ -1,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package nodemap
+
+import (
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+)
+
+// Cell provides the nodemap.Map which contains information about node IDs and their IP addresses.
+var Cell = cell.Module(
+	"node-map",
+	"eBPF map which contains information about node IDs and their IP addresses",
+
+	cell.Provide(newNodeMap),
+)
+
+func newNodeMap(lifecycle hive.Lifecycle) bpf.MapOut[Map] {
+	nodeMap := newMap()
+
+	lifecycle.Append(hive.Hook{
+		OnStart: func(context hive.HookContext) error {
+			return nodeMap.init()
+		},
+		OnStop: func(context hive.HookContext) error {
+			return nodeMap.close()
+		},
+	})
+
+	return bpf.NewMapOut(Map(nodeMap))
+}

pkg/maps/nodemap/node_map.go
@@ -4,8 +4,8 @@
 package nodemap
 
 import (
+	"fmt"
 	"net"
-	"sync"
 	"unsafe"
 
 	"golang.org/x/sys/unix"
@@ -20,25 +20,37 @@ const (
 	MaxEntries = 16384
 )
 
-var (
-	nodeMap     *Map
-	nodeMapInit = &sync.Once{}
-)
+// Map provides access to the eBPF map node.
+type Map interface {
+	// Update inserts or updates the node map object associated with the provided
+	// IP and node id.
+	Update(ip net.IP, nodeID uint16) error
 
-// SetNodeMap sets the node map. Only used for testing.
-func SetNodeMap(m *Map) {
-	nodeMap = m
+	// Delete deletes the node map object associated with the provided
+	// IP.
+	Delete(ip net.IP) error
+
+	// IterateWithCallback iterates through all the keys/values of a node map,
+	// passing each key/value pair to the cb callback.
+	IterateWithCallback(cb NodeIterateCallback) error
 }
 
-func NodeMap() *Map {
-	nodeMapInit.Do(func() {
-		nodeMap = NewNodeMap(MapName)
-	})
-	return nodeMap
+type nodeMap struct {
+	bpfMap *ebpf.Map
 }
 
-type Map struct {
-	*ebpf.Map
+func newMap() *nodeMap {
+	return &nodeMap{
+		bpfMap: ebpf.NewMap(&ebpf.MapSpec{
+			Name:       MapName,
+			Type:       ebpf.Hash,
+			KeySize:    uint32(unsafe.Sizeof(NodeKey{})),
+			ValueSize:  uint32(unsafe.Sizeof(NodeValue{})),
+			MaxEntries: uint32(MaxEntries),
+			Flags:      unix.BPF_F_NO_PREALLOC,
+			Pinning:    ebpf.PinByName,
+		}),
+	}
 }
 
 type NodeKey struct {
@@ -75,27 +87,15 @@ type NodeValue struct {
 	NodeID uint16
 }
 
-func NewNodeMap(mapName string) *Map {
-	return &Map{Map: ebpf.NewMap(&ebpf.MapSpec{
-		Name:       mapName,
-		Type:       ebpf.Hash,
-		KeySize:    uint32(unsafe.Sizeof(NodeKey{})),
-		ValueSize:  uint32(unsafe.Sizeof(NodeValue{})),
-		MaxEntries: uint32(MaxEntries),
-		Flags:      unix.BPF_F_NO_PREALLOC,
-		Pinning:    ebpf.PinByName,
-	})}
-}
-
-func (m *Map) Update(ip net.IP, nodeID uint16) error {
+func (m *nodeMap) Update(ip net.IP, nodeID uint16) error {
 	key := newNodeKey(ip)
 	val := NodeValue{NodeID: nodeID}
-	return m.Map.Update(key, val, 0)
+	return m.bpfMap.Update(key, val, 0)
 }
 
-func (m *Map) Delete(ip net.IP) error {
+func (m *nodeMap) Delete(ip net.IP) error {
 	key := newNodeKey(ip)
-	return m.Map.Delete(key)
+	return m.bpfMap.Map.Delete(key)
 }
 
 // NodeIterateCallback represents the signature of the callback function
@@ -103,14 +103,40 @@ func (m *Map) Delete(ip net.IP) error {
 // all the keys/values of a node map.
 type NodeIterateCallback func(*NodeKey, *NodeValue)
 
-// IterateWithCallback iterates through all the keys/values of a node map,
-// passing each key/value pair to the cb callback.
-func (m Map) IterateWithCallback(cb NodeIterateCallback) error {
-	return m.Map.IterateWithCallback(&NodeKey{}, &NodeValue{},
+func (m *nodeMap) IterateWithCallback(cb NodeIterateCallback) error {
+	return m.bpfMap.IterateWithCallback(&NodeKey{}, &NodeValue{},
 		func(k, v interface{}) {
 			key := k.(*NodeKey)
 			value := v.(*NodeValue)
 
 			cb(key, value)
 		})
+}
+
+// LoadNodeMap loads the pre-initialized node map for access.
+// This should only be used from components which aren't capable of using hive - mainly the Cilium CLI.
+// It needs to initialized beforehand via the Cilium Agent.
+func LoadNodeMap() (Map, error) {
+	bpfMap, err := ebpf.LoadRegisterMap(MapName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load bpf map: %w", err)
+	}
+
+	return &nodeMap{bpfMap: bpfMap}, nil
+}
+
+func (m *nodeMap) init() error {
+	if err := m.bpfMap.OpenOrCreate(); err != nil {
+		return fmt.Errorf("failed to init bpf map: %w", err)
+	}
+
+	return nil
+}
+
+func (m *nodeMap) close() error {
+	if err := m.bpfMap.Close(); err != nil {
+		return fmt.Errorf("failed to close bpf map: %w", err)
+	}
+
+	return nil
 }

pkg/maps/nodemap/node_map_privileged_test.go
@@ -1,0 +1,71 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package nodemap
+
+import (
+	"net"
+	"testing"
+
+	. "github.com/cilium/checkmate"
+
+	"github.com/cilium/ebpf/rlimit"
+
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/testutils"
+)
+
+// Hook up gocheck into the "go test" runner.
+type NodeMapTestSuite struct{}
+
+var _ = Suite(&NodeMapTestSuite{})
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+func (k *NodeMapTestSuite) SetUpSuite(c *C) {
+	testutils.PrivilegedTest(c)
+
+	bpf.CheckOrMountFS("")
+	err := rlimit.RemoveMemlock()
+	c.Assert(err, IsNil)
+}
+
+func (k *NodeMapTestSuite) TestNodeMap(c *C) {
+	nodeMap := newMap()
+	err := nodeMap.init()
+	c.Assert(err, IsNil)
+	defer nodeMap.bpfMap.Unpin()
+
+	bpfNodeIDMap := map[uint16]string{}
+	toMap := func(key *NodeKey, val *NodeValue) {
+		address := key.IP.String()
+		if key.Family == bpf.EndpointKeyIPv4 {
+			address = net.IP(key.IP[:net.IPv4len]).String()
+		}
+		bpfNodeIDMap[val.NodeID] = address
+	}
+
+	err = nodeMap.IterateWithCallback(toMap)
+	c.Assert(err, IsNil)
+	c.Assert(bpfNodeIDMap, HasLen, 0)
+
+	err = nodeMap.Update(net.ParseIP("10.1.0.0"), 10)
+	c.Assert(err, IsNil)
+	err = nodeMap.Update(net.ParseIP("10.1.0.1"), 20)
+	c.Assert(err, IsNil)
+
+	bpfNodeIDMap = map[uint16]string{}
+	err = nodeMap.IterateWithCallback(toMap)
+	c.Assert(err, IsNil)
+	c.Assert(bpfNodeIDMap, HasLen, 2)
+
+	err = nodeMap.Delete(net.ParseIP("10.1.0.0"))
+	c.Assert(err, IsNil)
+
+	bpfNodeIDMap = map[uint16]string{}
+	err = nodeMap.IterateWithCallback(toMap)
+	c.Assert(err, IsNil)
+	c.Assert(bpfNodeIDMap, HasLen, 1)
+}

pkg/maps/nodemap/fake/node_map.go
@@ -1,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package fake
+
+import (
+	"net"
+
+	"github.com/cilium/cilium/pkg/maps/nodemap"
+)
+
+type fakeNodeMap struct {
+}
+
+var _ nodemap.Map = &fakeNodeMap{}
+
+func NewFakeNodeMap() *fakeNodeMap {
+	return &fakeNodeMap{}
+}
+
+func (f fakeNodeMap) Update(ip net.IP, nodeID uint16) error {
+	return nil
+}
+
+func (f fakeNodeMap) Delete(ip net.IP) error {
+	return nil
+}
+
+func (f fakeNodeMap) IterateWithCallback(cb nodemap.NodeIterateCallback) error {
+	return nil
+}

cilium/cmd/bpf_nodeid_list.go
@@ -4,7 +4,9 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"net"
 	"os"
 	"text/tabwriter"
@@ -45,7 +47,17 @@ var bpfNodeIDListCmd = &cobra.Command{
 			})
 		}
 
-		if err := nodemap.NodeMap().IterateWithCallback(parse); err != nil {
+		nodeMap, err := nodemap.LoadNodeMap()
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				fmt.Fprintln(os.Stderr, "Cannot find node bpf map")
+				return
+			}
+
+			Fatalf("Cannot load node bpf map: %s", err)
+		}
+
+		if err := nodeMap.IterateWithCallback(parse); err != nil {
 			Fatalf("Error dumping contents of the node ID map: %s\n", err)
 		}
 

daemon/cmd/datapath.go
@@ -36,7 +36,6 @@ import (
 	"github.com/cilium/cilium/pkg/maps/metricsmap"
 	"github.com/cilium/cilium/pkg/maps/nat"
 	"github.com/cilium/cilium/pkg/maps/neighborsmap"
-	"github.com/cilium/cilium/pkg/maps/nodemap"
 	"github.com/cilium/cilium/pkg/maps/policymap"
 	"github.com/cilium/cilium/pkg/maps/srv6map"
 	"github.com/cilium/cilium/pkg/maps/tunnel"
@@ -323,10 +322,6 @@ func (d *Daemon) initMaps() error {
 	// are re-allocated on startup.
 	if err := ipcachemap.IPCacheMap().Recreate(); err != nil {
 		return fmt.Errorf("initializing ipcache map: %w", err)
-	}
-
-	if err := nodemap.NodeMap().OpenOrCreate(); err != nil {
-		return err
 	}
 
 	if err := metricsmap.Metrics.OpenOrCreate(); err != nil {

pkg/datapath/cells.go
@@ -19,6 +19,7 @@ import (
 	ipcache "github.com/cilium/cilium/pkg/ipcache/types"
 	"github.com/cilium/cilium/pkg/maps"
 	"github.com/cilium/cilium/pkg/maps/eventsmap"
+	"github.com/cilium/cilium/pkg/maps/nodemap"
 	monitorAgent "github.com/cilium/cilium/pkg/monitor/agent"
 	"github.com/cilium/cilium/pkg/option"
 	wg "github.com/cilium/cilium/pkg/wireguard/agent"
@@ -100,7 +101,7 @@ func newDatapath(params datapathParams) types.Datapath {
 			return nil
 		}})
 
-	datapath := linuxdatapath.NewDatapath(datapathConfig, iptablesManager, params.WgAgent)
+	datapath := linuxdatapath.NewDatapath(datapathConfig, iptablesManager, params.WgAgent, params.NodeMap)
 
 	params.LC.Append(hive.Hook{
 		OnStart: func(hive.HookContext) error {
@@ -121,4 +122,6 @@ type datapathParams struct {
 	// Force map initialisation before loader. You should not use these otherwise.
 	// Some of the entries in this slice may be nil.
 	BpfMaps []bpf.BpfMap `group:"bpf-maps"`
+
+	NodeMap nodemap.Map
 }

pkg/datapath/linux/datapath.go
@@ -8,6 +8,7 @@ import (
 	"github.com/cilium/cilium/pkg/datapath/loader"
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/maps/lbmap"
+	"github.com/cilium/cilium/pkg/maps/nodemap"
 )
 
 // DatapathConfiguration is the static configuration of the datapath. The
@@ -31,7 +32,7 @@ type linuxDatapath struct {
 }
 
 // NewDatapath creates a new Linux datapath
-func NewDatapath(cfg DatapathConfiguration, ruleManager datapath.IptablesManager, wgAgent datapath.WireguardAgent) datapath.Datapath {
+func NewDatapath(cfg DatapathConfiguration, ruleManager datapath.IptablesManager, wgAgent datapath.WireguardAgent, nodeMap nodemap.Map) datapath.Datapath {
 	dp := &linuxDatapath{
 		ConfigWriter:    &config.HeaderfileWriter{},
 		IptablesManager: ruleManager,
@@ -42,7 +43,7 @@ func NewDatapath(cfg DatapathConfiguration, ruleManager datapath.IptablesManager
 		lbmap:           lbmap.New(),
 	}
 
-	dp.node = NewNodeHandler(cfg, dp.nodeAddressing)
+	dp.node = NewNodeHandler(cfg, dp.nodeAddressing, nodeMap)
 	return dp
 }
 

commit 77740a16859f5967fafd1c8f56a21bfccd7cb879

nodemap: Run tests on dedicated map
We typically don't execute privileged integration tests on the usual BPF
maps, but instead use copies prefixed with "test_". Let's do the same
for the node ID map tests.

Fixes: 79bee301980 ("nodeid map: provide map via hive cell")
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

pkg/maps/nodemap/cell.go
@@ -18,7 +18,7 @@ var Cell = cell.Module(
 )
 
 func newNodeMap(lifecycle hive.Lifecycle) bpf.MapOut[Map] {
-	nodeMap := newMap()
+	nodeMap := newMap(MapName)
 
 	lifecycle.Append(hive.Hook{
 		OnStart: func(context hive.HookContext) error {

pkg/maps/nodemap/node_map.go
@@ -39,10 +39,10 @@ type nodeMap struct {
 	bpfMap *ebpf.Map
 }
 
-func newMap() *nodeMap {
+func newMap(mapName string) *nodeMap {
 	return &nodeMap{
 		bpfMap: ebpf.NewMap(&ebpf.MapSpec{
-			Name:       MapName,
+			Name:       mapName,
 			Type:       ebpf.Hash,
 			KeySize:    uint32(unsafe.Sizeof(NodeKey{})),
 			ValueSize:  uint32(unsafe.Sizeof(NodeValue{})),

pkg/maps/nodemap/node_map_privileged_test.go
@@ -33,7 +33,7 @@ func (k *NodeMapTestSuite) SetUpSuite(c *C) {
 }
 
 func (k *NodeMapTestSuite) TestNodeMap(c *C) {
-	nodeMap := newMap()
+	nodeMap := newMap("test_cilium_node_map")
 	err := nodeMap.init()
 	c.Assert(err, IsNil)
 	defer nodeMap.bpfMap.Unpin()