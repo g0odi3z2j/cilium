g0odi3z2j/cilium

commit 3c356667532232ce6dbdeac6e1ebdd85ea1b8fe1

kvstoremesh: add helm configuration
This commit extends the helm chart to allow configuring kvstoremesh. In
particular, the clustermesh-apiserver deployment is enriched with the
additional kvstoremesh sidecar container (when kvstoremesh is enabled),
appropriately mounting the secret containing the remote kvstore
configurations. Additionally, the configuration used by the agents is
modified to connect to the local kvstore instance (through the
corresponding service) instead of the remote ones.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

install/kubernetes/cilium/templates/clustermesh-config/_helpers.tpl
@@ -2,11 +2,14 @@
 {{- $cluster := index . 0 -}}
 {{- $domain := index . 1 -}}
 {{- $hasCustomCACert := index . 2 -}}
+{{- $override := index . 3 -}}
 {{- /* The parenthesis around $cluster.tls are required, since it can be null: https://stackoverflow.com/a/68807258 */}}
 {{- $prefix := ternary "common-" (printf "%s." $cluster.name) (or (empty ($cluster.tls).cert) (empty ($cluster.tls).key)) -}}
 
 endpoints:
-{{- if $cluster.ips }}
+{{- if $override }}
+- {{ $override }}
+{{- else if $cluster.ips }}
 - https://{{ $cluster.name }}.{{ $domain }}:{{ $cluster.port }}
 {{- else }}
 - https://{{ $cluster.address | required "missing clustermesh.apiserver.config.clusters.address" }}:{{ $cluster.port }}

install/kubernetes/cilium/templates/clustermesh-config/kvstoremesh-secret.yaml
@@ -1,20 +1,17 @@
-{{- if and .Values.clustermesh.useAPIServer .Values.clustermesh.config.enabled }}
+{{- if and .Values.clustermesh.useAPIServer .Values.clustermesh.config.enabled .Values.clustermesh.apiserver.kvstoremesh.enabled }}
 ---
 apiVersion: v1
 kind: Secret
 metadata:
-  name: cilium-clustermesh
+  name: cilium-kvstoremesh
   namespace: {{ .Release.Namespace }}
 data:
-  {{- $override := ternary (printf "https://clustermesh-apiserver.%s.svc:2379" .Release.Namespace) "" $.Values.clustermesh.apiserver.kvstoremesh.enabled }}
   {{- range .Values.clustermesh.config.clusters }}
   {{- $hasCustomCACert := or (.tls).caCert $.Values.clustermesh.apiserver.tls.ca.cert }}
-  {{ .name }}: {{ include "clustermesh-config-generate-etcd-cfg" (list . $.Values.clustermesh.config.domain $hasCustomCACert $override) | b64enc }}
+  {{ .name }}: {{ include "clustermesh-config-generate-etcd-cfg" (list . $.Values.clustermesh.config.domain $hasCustomCACert "") | b64enc }}
   {{- /* The parenthesis around .tls are required, since it can be null: https://stackoverflow.com/a/68807258 */}}
   {{- if and (.tls).cert (.tls).key }}
-  {{- if $hasCustomCACert }}
   {{ .name }}.etcd-client-ca.crt: {{ .tls.caCert | default $.Values.clustermesh.apiserver.tls.ca.cert }}
-  {{- end }}
   {{ .name }}.etcd-client.key: {{ .tls.key }}
   {{ .name }}.etcd-client.crt: {{ .tls.cert }}
   {{- end }}

install/kubernetes/Makefile.digests
@@ -6,6 +6,7 @@ export CILIUM_DIGEST := ""
 export CLUSTERMESH_APISERVER_DIGEST := ""
 export DOCKER_PLUGIN_DIGEST := ""
 export HUBBLE_RELAY_DIGEST := ""
+export KVSTOREMESH_DIGEST := ""
 export OPERATOR_AWS_DIGEST := ""
 export OPERATOR_AZURE_DIGEST := ""
 export OPERATOR_ALIBABACLOUD_DIGEST := ""

install/kubernetes/Makefile.values
@@ -15,6 +15,7 @@ ifeq ($(RELEASE),yes)
     export CILIUM_OPERATOR_BASE_REPO:=${RELEASE_REGISTRY}/${RELEASE_ORG}/operator
     export CLUSTERMESH_APISERVER_REPO:=${RELEASE_REGISTRY}/${RELEASE_ORG}/clustermesh-apiserver
     export HUBBLE_RELAY_REPO:=${RELEASE_REGISTRY}/${RELEASE_ORG}/hubble-relay
+    export KVSTOREMESH_REPO:=${RELEASE_REGISTRY}/${RELEASE_ORG}/kvstoremesh
 else
     export CILIUM_BRANCH:=main
     export PULL_POLICY:=Always
@@ -24,6 +25,7 @@ else
     export CILIUM_VERSION:=latest
     export CLUSTERMESH_APISERVER_REPO:=${CI_REGISTRY}/${CI_ORG}/clustermesh-apiserver-ci
     export HUBBLE_RELAY_REPO:=${CI_REGISTRY}/${CI_ORG}/hubble-relay-ci
+    export KVSTOREMESH_REPO:=${CI_REGISTRY}/${CI_ORG}/kvstoremesh-ci
 endif
 
 ifndef CILIUM_BRANCH

install/kubernetes/cilium/templates/cilium-agent/daemonset.yaml
@@ -716,7 +716,7 @@ spec:
       tolerations:
         {{- toYaml . | trim | nindent 8 }}
       {{- end }}
-      {{- if and .Values.clustermesh.useAPIServer .Values.clustermesh.config.enabled }}
+      {{- if and .Values.clustermesh.useAPIServer .Values.clustermesh.config.enabled (not .Values.clustermesh.apiserver.kvstoremesh.enabled) }}
       hostAliases:
       {{- range $cluster := .Values.clustermesh.config.clusters }}
       {{- range $ip := $cluster.ips }}

install/kubernetes/cilium/templates/clustermesh-apiserver/deployment.yaml
@@ -218,6 +218,68 @@ spec:
         securityContext:
           {{- toYaml . | nindent 10 }}
         {{- end }}
+      {{- if .Values.clustermesh.apiserver.kvstoremesh.enabled }}
+      - name: kvstoremesh
+        image: {{ include "cilium.image" .Values.clustermesh.apiserver.kvstoremesh.image | quote }}
+        imagePullPolicy: {{ .Values.clustermesh.apiserver.kvstoremesh.image.pullPolicy }}
+        command:
+        - /usr/bin/kvstoremesh
+        args:
+        {{- if .Values.debug.enabled }}
+        - --debug
+        {{- end }}
+        - --cluster-name=$(CLUSTER_NAME)
+        - --cluster-id=$(CLUSTER_ID)
+        - --kvstore-opt=etcd.config=/var/lib/cilium/etcd-config.yaml
+        - --kvstore-opt=etcd.qps=100
+        - --kvstore-opt=etcd.maxInflight=10
+        - --clustermesh-config=/var/lib/cilium/clustermesh
+        {{- if .Values.clustermesh.apiserver.metrics.kvstoremesh.enabled }}
+        - --prometheus-serve-addr=:{{ .Values.clustermesh.apiserver.metrics.kvstoremesh.port }}
+        {{- end }}
+        {{- with .Values.clustermesh.apiserver.kvstoremesh.extraArgs }}
+        {{- toYaml . | trim | nindent 8 }}
+        {{- end }}
+        env:
+        - name: CLUSTER_NAME
+          valueFrom:
+            configMapKeyRef:
+              name: cilium-config
+              key: cluster-name
+        - name: CLUSTER_ID
+          valueFrom:
+            configMapKeyRef:
+              name: cilium-config
+              key: cluster-id
+        {{- with .Values.clustermesh.apiserver.kvstoremesh.extraEnv }}
+        {{- toYaml . | trim | nindent 8 }}
+        {{- end }}
+        {{- if .Values.clustermesh.apiserver.metrics.kvstoremesh.enabled }}
+        ports:
+        - name: kvmesh-metrics
+          containerPort: {{ .Values.clustermesh.apiserver.metrics.kvstoremesh.port }}
+          protocol: TCP
+        {{- end }}
+        {{- with .Values.clustermesh.apiserver.kvstoremesh.resources }}
+        resources:
+          {{- toYaml . | nindent 10 }}
+        {{- end }}
+        volumeMounts:
+        - name: etcd-admin-client
+          mountPath: /var/lib/cilium/etcd-secrets
+          readOnly: true
+        - name: kvstoremesh-secrets
+          mountPath: /var/lib/cilium/clustermesh
+          readOnly: true
+        {{- with .Values.clustermesh.apiserver.kvstoremesh.extraVolumeMounts }}
+        {{- toYaml . | nindent 8 }}
+        {{- end }}
+        terminationMessagePolicy: FallbackToLogsOnError
+        {{- with .Values.clustermesh.apiserver.kvstoremesh.securityContext }}
+        securityContext:
+          {{- toYaml . | nindent 10 }}
+        {{- end }}
+      {{- end }}
       volumes:
       - name: etcd-server-secrets
         projected:
@@ -272,6 +334,29 @@ spec:
       {{- end }}
       - name: etcd-data-dir
         emptyDir: {}
+      {{- if .Values.clustermesh.apiserver.kvstoremesh.enabled }}
+      - name: kvstoremesh-secrets
+        projected:
+          # note: the leading zero means this number is in octal representation: do not remove it
+          defaultMode: 0400
+          sources:
+          - secret:
+              name: cilium-kvstoremesh
+              # note: items are not explicitly listed here, since the entries of this secret
+              # depend on the peers configured, and that would cause a restart of this pod
+              # at every addition/removal. Leaving the field empty makes each secret entry
+              # to be automatically projected into the volume as a file whose name is the key.
+          - secret:
+              name: clustermesh-apiserver-remote-cert
+              optional: true
+              items:
+              - key: tls.key
+                path: common-etcd-client.key
+              - key: tls.crt
+                path: common-etcd-client.crt
+              - key: ca.crt
+                path: common-etcd-client-ca.crt
+      {{- end }}
       {{- with .Values.clustermesh.apiserver.extraVolumes }}
       {{- toYaml . | nindent 6 }}
       {{- end }}
@@ -302,5 +387,14 @@ spec:
       {{- with .Values.clustermesh.apiserver.tolerations }}
       tolerations:
         {{- toYaml . | nindent 8 }}
+      {{- end }}
+      {{- if and .Values.clustermesh.config.enabled .Values.clustermesh.apiserver.kvstoremesh.enabled }}
+      hostAliases:
+      {{- range $cluster := .Values.clustermesh.config.clusters }}
+      {{- range $ip := $cluster.ips }}
+      - ip: {{ $ip }}
+        hostnames: [ "{{ $cluster.name }}.{{ $.Values.clustermesh.config.domain }}" ]
+      {{- end }}
+      {{- end }}
       {{- end }}
 {{- end }}

install/kubernetes/cilium/templates/clustermesh-apiserver/metrics-service.yaml
@@ -1,6 +1,6 @@
 {{- if and
   (or .Values.externalWorkloads.enabled .Values.clustermesh.useAPIServer)
-  (or .Values.clustermesh.apiserver.metrics.enabled .Values.clustermesh.apiserver.metrics.etcd.enabled) }}
+  (or .Values.clustermesh.apiserver.metrics.enabled .Values.clustermesh.apiserver.metrics.kvstoremesh.enabled .Values.clustermesh.apiserver.metrics.etcd.enabled) }}
 apiVersion: v1
 kind: Service
 metadata:
@@ -20,6 +20,12 @@ spec:
     port: {{ .Values.clustermesh.apiserver.metrics.port }}
     protocol: TCP
     targetPort: apiserv-metrics
+  {{- end }}
+  {{- if .Values.clustermesh.apiserver.metrics.kvstoremesh.enabled }}
+  - name: kvmesh-metrics
+    port: {{ .Values.clustermesh.apiserver.metrics.kvstoremesh.port }}
+    protocol: TCP
+    targetPort: kvmesh-metrics
   {{- end }}
   {{- if .Values.clustermesh.apiserver.metrics.etcd.enabled }}
   - name: etcd-metrics

install/kubernetes/cilium/templates/clustermesh-apiserver/servicemonitor.yaml
@@ -1,6 +1,6 @@
 {{- if and
   (or .Values.externalWorkloads.enabled .Values.clustermesh.useAPIServer)
-  (or .Values.clustermesh.apiserver.metrics.enabled .Values.clustermesh.apiserver.metrics.etcd.enabled)
+  (or .Values.clustermesh.apiserver.metrics.enabled .Values.clustermesh.apiserver.metrics.kvstoremesh.enabled .Values.clustermesh.apiserver.metrics.etcd.enabled)
   .Values.clustermesh.apiserver.metrics.serviceMonitor.enabled }}
 ---
 apiVersion: monitoring.coreos.com/v1
@@ -36,6 +36,20 @@ spec:
     {{- toYaml . | nindent 4 }}
     {{- end }}
     {{- with .Values.clustermesh.apiserver.metrics.serviceMonitor.metricRelabelings }}
+    metricRelabelings:
+    {{- toYaml . | nindent 4 }}
+    {{- end }}
+  {{- end }}
+  {{- if .Values.clustermesh.apiserver.metrics.kvstoremesh.enabled }}
+  - port: kvmesh-metrics
+    interval: {{ .Values.clustermesh.apiserver.metrics.serviceMonitor.kvstoremesh.interval | quote }}
+    honorLabels: true
+    path: /metrics
+    {{- with .Values.clustermesh.apiserver.metrics.serviceMonitor.kvstoremesh.relabelings }}
+    relabelings:
+    {{- toYaml . | nindent 4 }}
+    {{- end }}
+    {{- with .Values.clustermesh.apiserver.metrics.serviceMonitor.kvstoremesh.metricRelabelings }}
     metricRelabelings:
     {{- toYaml . | nindent 4 }}
     {{- end }}

commit 9bef480c504cc0291f2df16a434d6097d2b1f1d2

helm: use local etcd client certificate when kvstoremesh is enabled
When kvstoremesh is enabled, local agents connect to the local
clustermesh-apiserver instance, rather than the remote one. Yet,
when the TLS key/certificate pair of a remote cluster is manually
specified, the helm chart currently configures it both to connect
to the remote clustermesh-apiserver instance (which is correct)
and to the local one (which prevents the connection from being
established correctly as it is signed with a different CA).

Let's fix this making sure that we always use the local TLS
key/certificate pair when connecting to the local
clustermesh-apiserver instance.

Fixes: 3c3566675322 ("kvstoremesh: add helm configuration")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

install/kubernetes/cilium/templates/clustermesh-config/_helpers.tpl
@@ -4,10 +4,10 @@
 {{- $hasCustomCACert := index . 2 -}}
 {{- $override := index . 3 -}}
 {{- /* The parenthesis around $cluster.tls are required, since it can be null: https://stackoverflow.com/a/68807258 */}}
-{{- $prefix := ternary "common-" (printf "%s." $cluster.name) (or (empty ($cluster.tls).cert) (empty ($cluster.tls).key)) -}}
+{{- $prefix := ternary "common-" (printf "%s." $cluster.name) (or (ne $override "") (empty ($cluster.tls).cert) (empty ($cluster.tls).key)) -}}
 
 endpoints:
-{{- if $override }}
+{{- if ne $override "" }}
 - {{ $override }}
 {{- else if $cluster.ips }}
 - https://{{ $cluster.name }}.{{ $domain }}:{{ $cluster.port }}

install/kubernetes/cilium/templates/clustermesh-config/clustermesh-secret.yaml
@@ -11,7 +11,7 @@ data:
   {{- $hasCustomCACert := or (.tls).caCert $.Values.clustermesh.apiserver.tls.ca.cert }}
   {{ .name }}: {{ include "clustermesh-config-generate-etcd-cfg" (list . $.Values.clustermesh.config.domain $hasCustomCACert $override) | b64enc }}
   {{- /* The parenthesis around .tls are required, since it can be null: https://stackoverflow.com/a/68807258 */}}
-  {{- if and (.tls).cert (.tls).key }}
+  {{- if and (eq $override "") (.tls).cert (.tls).key }}
   {{- if $hasCustomCACert }}
   {{ .name }}.etcd-client-ca.crt: {{ .tls.caCert | default $.Values.clustermesh.apiserver.tls.ca.cert }}
   {{- end }}

install/kubernetes/cilium/templates/clustermesh-config/kvstoremesh-secret.yaml
@@ -11,7 +11,9 @@ data:
   {{ .name }}: {{ include "clustermesh-config-generate-etcd-cfg" (list . $.Values.clustermesh.config.domain $hasCustomCACert "") | b64enc }}
   {{- /* The parenthesis around .tls are required, since it can be null: https://stackoverflow.com/a/68807258 */}}
   {{- if and (.tls).cert (.tls).key }}
+  {{- if $hasCustomCACert }}
   {{ .name }}.etcd-client-ca.crt: {{ .tls.caCert | default $.Values.clustermesh.apiserver.tls.ca.cert }}
+  {{- end }}
   {{ .name }}.etcd-client.key: {{ .tls.key }}
   {{ .name }}.etcd-client.crt: {{ .tls.cert }}
   {{- end }}