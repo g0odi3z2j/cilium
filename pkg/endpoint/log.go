g0odi3z2j/cilium

commit f761832659008a9af545dc7174110003c342c0bb

endpoint: Allow multiple endpoints per pod
This commit removes the restriction that there is a 1:1 mapping between
a pod and an endpoint. To achieve this, we remove the `LookupPodName`
helper in the agent and replace it with a `LookupCEPName` (for
correlating with a CEP) and `GetEndpointsByPodName` (for correlating
with a pod) helpers instead.

Lookups on the pod name via API continue to be supported for backwards
compatibility, if the endpoint has not opted out of legacy identifiers.
The API also allows lookup by `cep-name` with this change, but similar
to other mostly internal identifiers (such as the `cilium-global`
prefix) we do not announce this ID in the `EndpointIdentifiers` model.

Co-authored-by: Tobias Klauser <tobias@isovalent.com>
Signed-off-by: Tobias Klauser <tobias@isovalent.com>
Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/endpointmanager/manager.go
@@ -274,6 +274,9 @@ func (mgr *endpointManager) Lookup(id string) (*endpoint.Endpoint, error) {
 	case endpointid.PodNamePrefix:
 		return mgr.lookupPodNameLocked(eid), nil
 
+	case endpointid.CEPNamePrefix:
+		return mgr.lookupCEPNameLocked(eid), nil
+
 	case endpointid.IPv4Prefix:
 		return mgr.lookupIPv4(eid), nil
 
@@ -330,12 +333,26 @@ func (mgr *endpointManager) LookupIP(ip netip.Addr) (ep *endpoint.Endpoint) {
 	return ep
 }
 
-// LookupPodName looks up endpoint by namespace + pod name
-func (mgr *endpointManager) LookupPodName(name string) *endpoint.Endpoint {
+// LookupCEPName looks up an endpoint by its K8s namespace + cep name
+func (mgr *endpointManager) LookupCEPName(namespacedName string) *endpoint.Endpoint {
 	mgr.mutex.RLock()
-	ep := mgr.lookupPodNameLocked(name)
+	ep := mgr.lookupCEPNameLocked(namespacedName)
 	mgr.mutex.RUnlock()
 	return ep
+}
+
+// GetEndpointsByPodName looks up endpoints by namespace + pod name
+func (mgr *endpointManager) GetEndpointsByPodName(namespacedName string) []*endpoint.Endpoint {
+	mgr.mutex.RLock()
+	defer mgr.mutex.RUnlock()
+	eps := make([]*endpoint.Endpoint, 0, 1)
+	for _, ep := range mgr.endpoints {
+		if ep.GetK8sNamespaceAndPodName() == namespacedName {
+			eps = append(eps, ep)
+		}
+	}
+
+	return eps
 }
 
 // ReleaseID releases the ID of the specified endpoint from the endpointManager.
@@ -371,7 +388,7 @@ func (mgr *endpointManager) unexpose(ep *endpoint.Endpoint) {
 			log.WithError(err).WithFields(logrus.Fields{
 				"state":                   previousState,
 				logfields.CNIAttachmentID: identifiers[endpointid.CNIAttachmentIdPrefix],
-				logfields.K8sPodName:      identifiers[endpointid.PodNamePrefix],
+				logfields.CEPName:         identifiers[endpointid.CEPNamePrefix],
 			}).Warning("Unable to release endpoint ID")
 		}
 	}
@@ -426,6 +443,13 @@ func (mgr *endpointManager) lookupDockerEndpoint(id string) *endpoint.Endpoint {
 
 func (mgr *endpointManager) lookupPodNameLocked(name string) *endpoint.Endpoint {
 	if ep, ok := mgr.endpointsAux[endpointid.NewID(endpointid.PodNamePrefix, name)]; ok {
+		return ep
+	}
+	return nil
+}
+
+func (mgr *endpointManager) lookupCEPNameLocked(name string) *endpoint.Endpoint {
+	if ep, ok := mgr.endpointsAux[endpointid.NewID(endpointid.CEPNamePrefix, name)]; ok {
 		return ep
 	}
 	return nil

daemon/cmd/ciliumendpoints.go
@@ -21,7 +21,7 @@ import (
 )
 
 type localEndpointCache interface {
-	LookupPodName(name string) *endpoint.Endpoint
+	LookupCEPName(namespacedName string) *endpoint.Endpoint
 }
 
 // This must only be run after K8s Pod and CES/CEP caches are synced and local endpoint restoration is complete.
@@ -45,7 +45,7 @@ func (d *Daemon) cleanStaleCEPs(ctx context.Context, eps localEndpointCache, cil
 				return fmt.Errorf("unexpected object type returned from ciliumendpointslice store: %T", cesObj)
 			}
 			for _, cep := range ces.Endpoints {
-				if cep.Networking.NodeIP == node.GetCiliumEndpointNodeIP() && eps.LookupPodName(ces.Namespace+"/"+cep.Name) == nil {
+				if cep.Networking.NodeIP == node.GetCiliumEndpointNodeIP() && eps.LookupCEPName(ces.Namespace+"/"+cep.Name) == nil {
 					d.deleteCiliumEndpoint(ctx, ces.Namespace, cep.Name, nil, ciliumClient, eps,
 						enableCiliumEndpointSlice)
 				}
@@ -58,7 +58,7 @@ func (d *Daemon) cleanStaleCEPs(ctx context.Context, eps localEndpointCache, cil
 				return fmt.Errorf("unexpected object type returned from ciliumendpoint store: %T", cepObj)
 			}
 
-			if cep.Networking.NodeIP == node.GetCiliumEndpointNodeIP() && eps.LookupPodName(cep.Namespace+"/"+cep.Name) == nil {
+			if cep.Networking.NodeIP == node.GetCiliumEndpointNodeIP() && eps.LookupCEPName(cep.Namespace+"/"+cep.Name) == nil {
 				d.deleteCiliumEndpoint(ctx, cep.Namespace, cep.Name, &cep.ObjectMeta.UID, ciliumClient, eps,
 					enableCiliumEndpointSlice)
 			}

daemon/cmd/ciliumendpoints_test.go
@@ -194,11 +194,11 @@ func Test_cleanStaleCEP(t *testing.T) {
 }
 
 type fakeEPManager struct {
-	byPodName map[string]*endpoint.Endpoint
+	byCEPName map[string]*endpoint.Endpoint
 }
 
-func (epm *fakeEPManager) LookupPodName(name string) *endpoint.Endpoint {
-	ep, ok := epm.byPodName[name]
+func (epm *fakeEPManager) LookupCEPName(namespacedName string) *endpoint.Endpoint {
+	ep, ok := epm.byCEPName[namespacedName]
 	if !ok {
 		return nil
 	}

daemon/cmd/endpoint.go
@@ -251,18 +251,18 @@ func (m *endpointCreationManager) NewCreateRequest(ep *endpoint.Endpoint, cancel
 		return
 	}
 
-	podName := ep.GetK8sNamespaceAndPodName()
+	cepName := ep.GetK8sNamespaceAndCEPName()
 
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 
-	if req, ok := m.requests[podName]; ok {
-		ep.Logger(daemonSubsys).Warning("Cancelling obsolete endpoint creating due to new create for same pod")
+	if req, ok := m.requests[cepName]; ok {
+		ep.Logger(daemonSubsys).Warning("Cancelling obsolete endpoint creating due to new create for same cep name")
 		req.cancel()
 	}
 
 	ep.Logger(daemonSubsys).Debug("New create request")
-	m.requests[podName] = &endpointCreationRequest{
+	m.requests[cepName] = &endpointCreationRequest{
 		cancel:   cancel,
 		endpoint: ep,
 		started:  time.Now(),
@@ -274,15 +274,15 @@ func (m *endpointCreationManager) EndCreateRequest(ep *endpoint.Endpoint) bool {
 		return false
 	}
 
-	podName := ep.GetK8sNamespaceAndPodName()
+	cepName := ep.GetK8sNamespaceAndCEPName()
 
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 
-	if req, ok := m.requests[podName]; ok {
+	if req, ok := m.requests[cepName]; ok {
 		if req.endpoint == ep {
 			ep.Logger(daemonSubsys).Debug("End of create request")
-			delete(m.requests, podName)
+			delete(m.requests, cepName)
 			return true
 		}
 	}

pkg/endpoint/api.go
@@ -145,15 +145,15 @@ func (e *Endpoint) getModelEndpointIdentitiersRLocked() *models.EndpointIdentifi
 		CniAttachmentID:  e.getCNIAttachmentIDLocked(),
 		DockerEndpointID: e.dockerEndpointID,
 		DockerNetworkID:  e.dockerNetworkID,
-		PodName:          e.GetK8sNamespaceAndPodName(),
-		K8sPodName:       e.K8sPodName,
-		K8sNamespace:     e.K8sNamespace,
 	}
 
 	// Use legacy endpoint identifiers only if the endpoint has not opted out
 	if !e.disableLegacyIdentifiers {
 		identifiers.ContainerID = e.containerID
 		identifiers.ContainerName = e.containerName
+		identifiers.PodName = e.GetK8sNamespaceAndPodName()
+		identifiers.K8sPodName = e.K8sPodName
+		identifiers.K8sNamespace = e.K8sNamespace
 	}
 
 	return identifiers

pkg/endpoint/id/id.go
@@ -58,9 +58,17 @@ const (
 	// Deprecated. Use CNIAttachmentIdPrefix instead
 	ContainerNamePrefix PrefixType = "container-name"
 
+	// CEPNamePrefix is used to address an endpoint via its Kubernetes
+	// CiliumEndpoint resource name. This addressing only works if the endpoint
+	// is represented as a Kubernetes CiliumEndpoint resource.
+	CEPNamePrefix PrefixType = "cep-name"
+
 	// PodNamePrefix is used to address an endpoint via the Kubernetes pod
 	// name. This addressing only works if the endpoint represents as
 	// Kubernetes pod.
+	// This can only be used to look up endpoints which have not opted-out of
+	// legacy identifiers.
+	// Deprecated. May not be unique. Use CEPNamePrefix instead.
 	PodNamePrefix PrefixType = "pod-name"
 
 	// IPv4Prefix is used to address an endpoint via the endpoint's IPv4
@@ -140,6 +148,7 @@ func Parse(id string) (PrefixType, string, error) {
 		ContainerIdPrefix,
 		DockerEndpointPrefix,
 		ContainerNamePrefix,
+		CEPNamePrefix,
 		PodNamePrefix,
 		IPv4Prefix,
 		IPv6Prefix:

pkg/endpoint/id/id_test.go
@@ -61,6 +61,16 @@ func (s *IDSuite) TestSplitID(c *C) {
 			},
 		},
 		{
+			name: "ID with CEPNamePrefix prefix",
+			id:   string(CEPNamePrefix) + ":default:baz-net1",
+			want: want{
+				prefixType:      CEPNamePrefix,
+				prefixTypeCheck: Equals,
+				id:              "default:baz-net1",
+				idCheck:         Equals,
+			},
+		},
+		{
 			name: "ID with ':'",
 			id:   ":",
 			want: want{

pkg/endpoint/identifiers.go
@@ -27,8 +27,8 @@ func (e *Endpoint) GetK8sPodName() string {
 
 // HumanString returns the endpoint's most human readable identifier as string
 func (e *Endpoint) HumanString() string {
-	if pod := e.GetK8sNamespaceAndPodName(); pod != "" {
-		return pod
+	if cep := e.GetK8sNamespaceAndCEPName(); cep != "" {
+		return cep
 	}
 
 	return e.StringID()
@@ -39,6 +39,27 @@ func (e *Endpoint) HumanString() string {
 func (e *Endpoint) GetK8sNamespaceAndPodName() string {
 	// both fields are const after creation
 	return e.K8sNamespace + "/" + e.K8sPodName
+}
+
+// GetK8sCEPName returns the corresponding K8s CiliumEndpoint resource name
+// for this endpoint (without the namespace)
+// Returns an empty string if the endpoint does not belong to a pod.
+func (e *Endpoint) GetK8sCEPName() string {
+	// all fields are const after creation
+
+	// Endpoints which have not opted out of legacy identifiers will continue
+	// to use just the pod name as the cep name for backwards compatibility reasons.
+	if e.disableLegacyIdentifiers && e.K8sPodName != "" && e.containerIfName != "" {
+		return e.K8sPodName + "-" + e.containerIfName
+	}
+	return e.K8sPodName
+}
+
+// GetK8sNamespaceAndCEPName returns the corresponding namespace and
+// K8s CiliumEndpoint resource name for this endpoint.
+func (e *Endpoint) GetK8sNamespaceAndCEPName() string {
+	// all fields are const after creation
+	return e.K8sNamespace + "/" + e.GetK8sCEPName()
 }
 
 // getCNIAttachmentIDLocked returns the endpoint's unique CNI attachment ID
@@ -95,7 +116,7 @@ func (e *Endpoint) GetDockerEndpointID() string {
 // IdentifiersLocked fetches the set of attributes that uniquely identify the
 // endpoint. The caller must hold exclusive control over the endpoint.
 func (e *Endpoint) IdentifiersLocked() id.Identifiers {
-	refs := make(id.Identifiers, 7)
+	refs := make(id.Identifiers, 8)
 	if cniID := e.getCNIAttachmentIDLocked(); cniID != "" {
 		refs[id.CNIAttachmentIdPrefix] = cniID
 	}
@@ -120,9 +141,14 @@ func (e *Endpoint) IdentifiersLocked() id.Identifiers {
 		refs[id.ContainerNamePrefix] = e.containerName
 	}
 
-	if podName := e.GetK8sNamespaceAndPodName(); podName != "" {
+	if podName := e.GetK8sNamespaceAndPodName(); !e.disableLegacyIdentifiers && podName != "" {
 		refs[id.PodNamePrefix] = podName
 	}
+
+	if cepName := e.GetK8sNamespaceAndCEPName(); cepName != "" {
+		refs[id.CEPNamePrefix] = cepName
+	}
+
 	return refs
 }
 

commit 756242af991ca5935a5caf945aeb7e164ce65e7b

endpointmanager: log endpoint name alongside pod name
After commit f76183265900 ("endpoint: Allow multiple endpoints per
pod"), there might be multiple endpoints per podi, i.e. there no longer
is a clear 1:1 mapping from pod <-> CEP. Thus also log the endpoint
name when adding and deleting endpoints.

Fixes: f76183265900 ("endpoint: Allow multiple endpoints per pod")
Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/endpoint/log.go
@@ -117,6 +117,7 @@ func (e *Endpoint) UpdateLogger(fields map[string]interface{}) {
 		logfields.IPv4:                   e.GetIPv4Address(),
 		logfields.IPv6:                   e.GetIPv6Address(),
 		logfields.K8sPodName:             e.GetK8sNamespaceAndPodName(),
+		logfields.CEPName:                e.GetK8sNamespaceAndCEPName(),
 	}
 
 	if e.SecurityIdentity != nil {

pkg/endpointmanager/gc.go
@@ -76,6 +76,7 @@ func (mgr *endpointManager) sweepEndpoints(markedEndpoints []uint16) {
 			logfields.EndpointID:  ep.StringID(),
 			logfields.ContainerID: ep.GetShortContainerID(),
 			logfields.K8sPodName:  ep.GetK8sNamespaceAndPodName(),
+			logfields.CEPName:     ep.GetK8sNamespaceAndCEPName(),
 			logfields.URL:         "https://github.com/kubernetes/kubernetes/issues/86944",
 		}).Warning("Stray endpoint found. You may be affected by upstream Kubernetes issue #86944.")
 		errs := mgr.RemoveEndpoint(ep, endpoint.DeleteConfig{

pkg/endpointmanager/manager.go
@@ -654,6 +654,7 @@ func (mgr *endpointManager) AddEndpoint(owner regeneration.Owner, ep *endpoint.E
 		logfields.IPv4:        ep.GetIPv4Address(),
 		logfields.IPv6:        ep.GetIPv6Address(),
 		logfields.K8sPodName:  ep.GetK8sNamespaceAndPodName(),
+		logfields.CEPName:     ep.GetK8sNamespaceAndCEPName(),
 	})
 
 	err = mgr.expose(ep)