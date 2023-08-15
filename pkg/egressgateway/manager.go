g0odi3z2j/cilium

commit 70a41afda36e35c976816a1fbaadfabcf8cde68c

egressgw: detect conflicting configurations in ENI mode
In ENI mode the manager creates EgressGW-specific routing tables for each
selected interface. Currently we create one default route in this
table, that sends all traffic to its nexthop.

If multiple policies with different egressIPs (== different nexthop) select
the same interface, the resulting default routes would collide.

Detect such a configuration by tracking what egressIP is used on each
interface, and reject if the egressIPs would differ.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

pkg/egressgateway/manager.go
@@ -683,8 +683,10 @@ func (manager *Manager) policyMatchesMinusExcludedCIDRs(sourceIP net.IP, f func(
 }
 
 func (manager *Manager) regenerateGatewayConfigs() {
+	policyByInterfaceIndex := make(map[int]*PolicyConfig)
+
 	for _, policyConfig := range manager.policyConfigs {
-		policyConfig.regenerateGatewayConfig(manager)
+		policyConfig.regenerateGatewayConfig(manager, policyByInterfaceIndex)
 	}
 }
 

pkg/egressgateway/policy.go
@@ -4,6 +4,7 @@
 package egressgateway
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 
@@ -98,7 +99,7 @@ func (config *policyGatewayConfig) selectsNodeAsGateway(node nodeTypes.Node) boo
 	return config.nodeSelector.Matches(k8sLabels.Set(node.Labels))
 }
 
-func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
+func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager, policyByInterfaceIndex map[int]*PolicyConfig) {
 	gwc := gatewayConfig{
 		egressIP:  net.IPNet{IP: net.IPv4zero, Mask: net.CIDRMask(0, 0)},
 		gatewayIP: GatewayNotFoundIPv4,
@@ -115,15 +116,37 @@ func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 
 		if node.IsLocal() {
 			err := gwc.deriveFromPolicyGatewayConfig(policyGwc)
-			if err != nil {
-				logger := log.WithFields(logrus.Fields{
-					logfields.CiliumEgressGatewayPolicyName: config.id,
-					logfields.Interface:                     policyGwc.iface,
-					logfields.EgressIP:                      policyGwc.egressIP,
-				})
+			logger := log.WithFields(logrus.Fields{
+				logfields.CiliumEgressGatewayPolicyName: config.id,
+				logfields.Interface:                     policyGwc.iface,
+				logfields.EgressIP:                      policyGwc.egressIP,
+			})
 
+			if err != nil {
 				logger.WithError(err).Error("Failed to derive policy gateway configuration")
+				break
 			}
+
+			if !manager.installRoutes {
+				break
+			}
+
+			// We can only have one default route per interface. Enforce that all
+			// policies on an interface use the same EgressIP (== default route).
+			currentPolicy := policyByInterfaceIndex[gwc.ifaceIndex]
+			if currentPolicy == nil {
+				policyByInterfaceIndex[gwc.ifaceIndex] = config
+				break
+			}
+
+			currentEgressIP := currentPolicy.gatewayConfig.egressIP
+			if gwc.egressIP.IP.Equal(currentEgressIP.IP) && bytes.Equal(gwc.egressIP.Mask, currentEgressIP.Mask) {
+				break
+			}
+
+			gwc.localNodeConfiguredAsGateway = false
+			gwc.gatewayIP = GatewayNotFoundIPv4
+			logger.Errorf("Conflict with policy %s: Selects the same egress interface but uses a different egress IP (%s).", currentPolicy.id, currentEgressIP.String())
 		}
 
 		break

pkg/egressgateway/helpers_test.go
@@ -68,6 +68,16 @@ func addPolicy(tb testing.TB, policies fakeResource[*Policy], params *policyPara
 	})
 }
 
+func deletePolicy(tb testing.TB, policies fakeResource[*Policy], params *policyParams) {
+	tb.Helper()
+
+	policy, _ := newCEGP(params)
+	policies.process(tb, resource.Event[*Policy]{
+		Kind:   resource.Delete,
+		Object: policy,
+	})
+}
+
 type policyParams struct {
 	name            string
 	endpointLabels  map[string]string

pkg/egressgateway/manager_privileged_test.go
@@ -46,6 +46,7 @@ const (
 
 	ep1IP = "10.0.0.1"
 	ep2IP = "10.0.0.2"
+	ep3IP = "10.0.0.3"
 
 	destCIDR      = "1.1.1.0/24"
 	excludedCIDR1 = "1.1.1.22/32"
@@ -55,6 +56,8 @@ const (
 	egressCIDR1 = "192.168.101.1/24"
 	egressIP2   = "192.168.102.1"
 	egressCIDR2 = "192.168.102.1/24"
+	egressIP3   = "192.168.103.1"
+	egressCIDR3 = "192.168.103.1/24"
 
 	zeroIP4 = "0.0.0.0"
 
@@ -66,6 +69,7 @@ const (
 var (
 	ep1Labels = map[string]string{"test-key": "test-value-1"}
 	ep2Labels = map[string]string{"test-key": "test-value-2"}
+	ep3Labels = map[string]string{"test-key": "test-value-3"}
 
 	identityAllocator = testidentity.NewMockIdentityAllocator(nil)
 
@@ -236,6 +240,48 @@ func (k *EgressGatewayTestSuite) TestEgressGatewayManager(c *C) {
 	assertIPRules(c, []ipRule{
 		{ep1IP, destCIDR, egressCIDR1, testInterface1Idx},
 	})
+
+	// Add an additional Egress IP on the first interface, and a policy
+	// that selects this IP. The conflicting policy should be ignored,
+	// resulting in a "no gateway found" egress entry and no IP rule
+	// for ep3.
+	link, err := netlink.LinkByName(testInterface1)
+	if err != nil {
+		panic(err)
+	}
+
+	a, _ := netlink.ParseAddr(egressCIDR3)
+	err = netlink.AddrAdd(link, a)
+	c.Assert(err, IsNil)
+
+	policy3 := policyParams{
+		name:            "policy-3",
+		endpointLabels:  ep3Labels,
+		destinationCIDR: destCIDR,
+		nodeLabels:      nodeGroup1Labels,
+		egressIP:        egressIP3,
+	}
+
+	addPolicy(c, k.policies, &policy3)
+
+	// Add a new endpoint and ID which matches policy-3
+	ep3, _ := newEndpointAndIdentity("ep-3", ep3IP, ep3Labels)
+	egressGatewayManager.OnUpdateEndpoint(&ep3)
+
+	assertEgressRules(c, policyMap, []egressRule{
+		{ep1IP, destCIDR, egressIP1, node1IP},
+		{ep2IP, destCIDR, zeroIP4, node2IP},
+		{ep3IP, destCIDR, egressIP3, gatewayNotFoundValue},
+	})
+	assertIPRules(c, []ipRule{
+		{ep1IP, destCIDR, egressCIDR1, testInterface1Idx},
+	})
+
+	// Delete the conflicting policy and IP again.
+	deletePolicy(c, k.policies, &policy3)
+
+	err = netlink.AddrDel(link, a)
+	c.Assert(err, IsNil)
 
 	// Test if disabling the --install-egress-gateway-routes agent option
 	// will result in stale IP routes/rules getting removed

commit c4e53754dfd8abce95f88c96eb0e71a53c8e5e83

egressgw: refactor check for conflicting egress IPs
The egressgateway code currently doesn't handle some configurations
correctly when using ip rules and routes to steer traffic. We've
recently added some logic to detect this error. The approach was
to only install rules for the first non-conflicting policy.

The problem with this approach is that the egress gateway doesn't
process policies in a deterministic fashion. So in the case of a
conflict we will choose different "first" policies for each
reconciliation. The end result is no better than what we had
before, where the "last" policy would win.
The check also can't address the fact that other nodes in the
cluster will still send traffic to the "misconfigured" gatewaty.

Refactor the existing check into a separate piece of code, which
makes it easier to understand. Also drop the logic which attempts
to remove conflicting policies, since it doesn't have the desired
effect and complicates behaviour.

Fixes: 70a41afda3 ("egressgw: detect conflicting configurations in ENI mode")
Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

pkg/egressgateway/manager.go
@@ -4,6 +4,7 @@
 package egressgateway
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -702,10 +703,40 @@ func (manager *Manager) policyMatchesMinusExcludedCIDRs(sourceIP net.IP, f func(
 }
 
 func (manager *Manager) regenerateGatewayConfigs() {
-	policyByInterfaceIndex := make(map[int]*PolicyConfig)
-
 	for _, policyConfig := range manager.policyConfigs {
-		policyConfig.regenerateGatewayConfig(manager, policyByInterfaceIndex)
+		policyConfig.regenerateGatewayConfig(manager)
+	}
+
+	if !manager.installRoutes {
+		return
+	}
+
+	// We can only have one default route per interface. Warn if there are
+	// conflicts on the desired egress IP per interface.
+	policyByInterfaceIndex := make(map[int]*PolicyConfig)
+	for _, policyConfig := range manager.policyConfigs {
+		gwc := policyConfig.gatewayConfig
+
+		if !gwc.localNodeConfiguredAsGateway {
+			continue
+		}
+
+		currentPolicy := policyByInterfaceIndex[gwc.ifaceIndex]
+		if currentPolicy == nil {
+			policyByInterfaceIndex[gwc.ifaceIndex] = policyConfig
+			continue
+		}
+
+		currentEgressIP := currentPolicy.gatewayConfig.egressIP
+		if gwc.egressIP.IP.Equal(currentEgressIP.IP) && bytes.Equal(gwc.egressIP.Mask, currentEgressIP.Mask) {
+			continue
+		}
+
+		log.WithFields(logrus.Fields{
+			logfields.CiliumEgressGatewayPolicyName: policyConfig.id,
+			logfields.Interface:                     policyConfig.policyGwConfig.iface,
+			logfields.EgressIP:                      policyConfig.policyGwConfig.egressIP,
+		}).Errorf("Conflict with policy %s: Selects the same egress interface but uses a different egress IP (%s).", currentPolicy.id, currentEgressIP.String())
 	}
 }
 

pkg/egressgateway/policy.go
@@ -4,7 +4,6 @@
 package egressgateway
 
 import (
-	"bytes"
 	"fmt"
 	"net"
 
@@ -99,7 +98,7 @@ func (config *policyGatewayConfig) selectsNodeAsGateway(node nodeTypes.Node) boo
 	return config.nodeSelector.Matches(k8sLabels.Set(node.Labels))
 }
 
-func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager, policyByInterfaceIndex map[int]*PolicyConfig) {
+func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 	gwc := gatewayConfig{
 		egressIP:  net.IPNet{IP: net.IPv4zero, Mask: net.CIDRMask(0, 0)},
 		gatewayIP: GatewayNotFoundIPv4,
@@ -116,37 +115,14 @@ func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager, policyByIn
 
 		if node.IsLocal() {
 			err := gwc.deriveFromPolicyGatewayConfig(policyGwc)
-			logger := log.WithFields(logrus.Fields{
-				logfields.CiliumEgressGatewayPolicyName: config.id,
-				logfields.Interface:                     policyGwc.iface,
-				logfields.EgressIP:                      policyGwc.egressIP,
-			})
-
 			if err != nil {
+				logger := log.WithFields(logrus.Fields{
+					logfields.CiliumEgressGatewayPolicyName: config.id,
+					logfields.Interface:                     policyGwc.iface,
+					logfields.EgressIP:                      policyGwc.egressIP,
+				})
 				logger.WithError(err).Error("Failed to derive policy gateway configuration")
-				break
 			}
-
-			if !manager.installRoutes {
-				break
-			}
-
-			// We can only have one default route per interface. Enforce that all
-			// policies on an interface use the same EgressIP (== default route).
-			currentPolicy := policyByInterfaceIndex[gwc.ifaceIndex]
-			if currentPolicy == nil {
-				policyByInterfaceIndex[gwc.ifaceIndex] = config
-				break
-			}
-
-			currentEgressIP := currentPolicy.gatewayConfig.egressIP
-			if gwc.egressIP.IP.Equal(currentEgressIP.IP) && bytes.Equal(gwc.egressIP.Mask, currentEgressIP.Mask) {
-				break
-			}
-
-			gwc.localNodeConfiguredAsGateway = false
-			gwc.gatewayIP = GatewayNotFoundIPv4
-			logger.Errorf("Conflict with policy %s: Selects the same egress interface but uses a different egress IP (%s).", currentPolicy.id, currentEgressIP.String())
 		}
 
 		break