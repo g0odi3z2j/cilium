g0odi3z2j/cilium

commit 42c4a829d68501949dfc99fef0095b43482024cf

cilium, iptables: Support option to masquerade source IP from routing layer
Add a new `--enable-masquerade-to-route-source` option to the agent which
supports more fine-grained masquerading for advanced use cases. For example,
a routing agent installs several CIDRs for a given interface and each of
the CIDRs have a different source address selection criteria. Moreover, the
next hops could even be part of a next hop group where the underlying next
hops are reachable via different interfaces which may not be known to Cilium.

Anyway, if the option is enabled, then the regular MASQUERADE target will
only act as a catchall/fallback, and we install several j SNAT --to-source
rules before that based on the routing setup (destination CIDR & source IP
to use), so that Pod traffic can be masqueraded differenty. For the default
route we keep relying on MASQUERADE target. Note that the user needs to
ensure that there are no overlapping destination prefixes since iptables is
only processing its rules linearly as opposed to routing layer.

Example:

  --enable-masquerade-to-route-source=false (default):
  [...]
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 -m set --match-set cilium_node_set_v4 dst -m comment --comment "exclude traffic to cluster nodes from masquerade" -j ACCEPT
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 ! -d 10.244.0.0/16 ! -o cilium_+ -m comment --comment "cilium masquerade non-cluster" -j MASQUERADE
  [...]

  --enable-masquerade-to-route-source=true:
  [...]
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 -m set --match-set cilium_node_set_v4 dst -m comment --comment "exclude traffic to cluster nodes from masquerade" -j ACCEPT
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 -d 1.1.1.1/32 -m comment --comment "cilium snat non-cluster via source route" -j SNAT --to-source 192.168.2.99
  [0:0] -A CILIUM_POST_nat -s 10.244.0.0/24 -d 192.168.2.13/32 -o enp5s0 -m comment --comment "cilium snat non-cluster via source route" -j SNAT --to-source 192.168.2.99

pkg/cidr/cidr.go
@@ -7,6 +7,8 @@ import (
 	"bytes"
 	"fmt"
 	"net"
+
+	"github.com/vishvananda/netlink"
 )
 
 // NewCIDR returns a new CIDR using a net.IPNet
@@ -88,7 +90,7 @@ func (n *CIDR) Equal(o *CIDR) bool {
 	return Equal(n.IPNet, o.IPNet)
 }
 
-// Equal returns true if the n and o net.IPNet CIDRs arr Equal.
+// Equal returns true if the n and o net.IPNet CIDRs are Equal.
 func Equal(n, o *net.IPNet) bool {
 	if n == nil || o == nil {
 		return n == o
@@ -98,6 +100,23 @@ func Equal(n, o *net.IPNet) bool {
 	}
 	return n.IP.Equal(o.IP) &&
 		bytes.Equal(n.Mask, o.Mask)
+}
+
+// ZeroNet generates a zero net.IPNet object for the given address family
+func ZeroNet(family int) *net.IPNet {
+	switch family {
+	case netlink.FAMILY_V4:
+		return &net.IPNet{
+			IP:   net.IPv4zero,
+			Mask: net.CIDRMask(0, 8*net.IPv4len),
+		}
+	case netlink.FAMILY_V6:
+		return &net.IPNet{
+			IP:   net.IPv6zero,
+			Mask: net.CIDRMask(0, 8*net.IPv6len),
+		}
+	}
+	return nil
 }
 
 // ContainsAll returns true if 'ipNets1' contains all net.IPNet of 'ipNets2'

daemon/cmd/daemon.go
@@ -959,6 +959,10 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 	}
 
 	if option.Config.MasqueradingEnabled() && option.Config.EnableBPFMasquerade {
+		if option.Config.EnableMasqueradeRouteSource {
+			log.Error("BPF masquerading does not yet support masquerading to source IP from routing layer")
+			return nil, nil, fmt.Errorf("BPF masquerading to route source (--%s=\"true\") currently not supported with BPF-based masquerading (--%s=\"true\")", option.EnableMasqueradeRouteSource, option.EnableBPFMasquerade)
+		}
 		// TODO(brb) nodeport constraints will be lifted once the SNAT BPF code has been refactored
 		if err := node.InitBPFMasqueradeAddrs(option.Config.GetDevices()); err != nil {
 			log.WithError(err).Error("failed to determine BPF masquerade addrs")

daemon/cmd/daemon_main.go
@@ -683,6 +683,9 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.Bool(option.EnableBPFMasquerade, false, "Masquerade packets from endpoints leaving the host with BPF instead of iptables")
 	option.BindEnv(vp, option.EnableBPFMasquerade)
 
+	flags.Bool(option.EnableMasqueradeRouteSource, false, "Masquerade packets to the source IP provided from the routing layer rather than interface address")
+	option.BindEnv(vp, option.EnableMasqueradeRouteSource)
+
 	flags.String(option.DeriveMasqIPAddrFromDevice, "", "Device name from which Cilium derives the IP addr for BPF masquerade")
 	flags.MarkHidden(option.DeriveMasqIPAddrFromDevice)
 	option.BindEnv(vp, option.DeriveMasqIPAddrFromDevice)

install/kubernetes/cilium/templates/cilium-configmap.yaml
@@ -556,6 +556,7 @@ data:
 {{- else if eq $defaultBpfMasquerade "true" }}
   enable-bpf-masquerade: {{ $defaultBpfMasquerade | quote }}
 {{- end }}
+  enable-masquerade-to-route-source: {{ .Values.enableMasqueradeRouteSource | quote }}
 {{- if hasKey .Values "egressMasqueradeInterfaces" }}
   egress-masquerade-interfaces: {{ .Values.egressMasqueradeInterfaces }}
 {{- end }}

install/kubernetes/cilium/values.yaml
@@ -1652,14 +1652,17 @@ maglev: {}
 # -- Enables masquerading of IPv4 traffic leaving the node from endpoints.
 enableIPv4Masquerade: true
 
-# -- Enables IPv6 BIG TCP support which increases maximum IPv6 GSO/GRO limits for nodes and pods
-enableIPv6BIGTCP: false
+# -- Enables masquerading of IPv6 traffic leaving the node from endpoints.
+enableIPv6Masquerade: true
+
+# -- Enables masquerading to the source of the route for traffic leaving the node from endpoints.
+enableMasqueradeRouteSource: false
 
 # -- Enables IPv4 BIG TCP support which increases maximum IPv4 GSO/GRO limits for nodes and pods
 enableIPv4BIGTCP: false
 
-# -- Enables masquerading of IPv6 traffic leaving the node from endpoints.
-enableIPv6Masquerade: true
+# -- Enables IPv6 BIG TCP support which increases maximum IPv6 GSO/GRO limits for nodes and pods
+enableIPv6BIGTCP: false
 
 # -- Enables egress gateway to redirect and SNAT the traffic that leaves the
 # cluster.

install/kubernetes/cilium/values.yaml.tmpl
@@ -1649,14 +1649,17 @@ maglev: {}
 # -- Enables masquerading of IPv4 traffic leaving the node from endpoints.
 enableIPv4Masquerade: true
 
-# -- Enables IPv6 BIG TCP support which increases maximum IPv6 GSO/GRO limits for nodes and pods
-enableIPv6BIGTCP: false
+# -- Enables masquerading of IPv6 traffic leaving the node from endpoints.
+enableIPv6Masquerade: true
+
+# -- Enables masquerading to the source of the route for traffic leaving the node from endpoints.
+enableMasqueradeRouteSource: false
 
 # -- Enables IPv4 BIG TCP support which increases maximum IPv4 GSO/GRO limits for nodes and pods
 enableIPv4BIGTCP: false
 
-# -- Enables masquerading of IPv6 traffic leaving the node from endpoints.
-enableIPv6Masquerade: true
+# -- Enables IPv6 BIG TCP support which increases maximum IPv6 GSO/GRO limits for nodes and pods
+enableIPv6BIGTCP: false
 
 # -- Enables egress gateway to redirect and SNAT the traffic that leaves the
 # cluster.

pkg/datapath/iptables/iptables.go
@@ -17,9 +17,11 @@ import (
 	"github.com/blang/semver/v4"
 	"github.com/mattn/go-shellwords"
 	"github.com/sirupsen/logrus"
+	"github.com/vishvananda/netlink"
 
 	"github.com/cilium/cilium/pkg/backoff"
 	"github.com/cilium/cilium/pkg/byteorder"
+	"github.com/cilium/cilium/pkg/cidr"
 	"github.com/cilium/cilium/pkg/command/exec"
 	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
@@ -1217,7 +1219,81 @@ func (m *IptablesManager) installMasqueradeRules(prog iptablesInterface, ifName,
 		}
 	}
 
-	// Masquerade all egress traffic leaving the node
+	// Masquerade egress traffic leaving the node based on source routing
+	//
+	// If this option is enabled, then it takes precedence over the catch-all
+	// MASQUERADE further below.
+	if option.Config.EnableMasqueradeRouteSource {
+		devices := option.Config.GetDevices()
+		if len(option.Config.MasqueradeInterfaces) > 0 {
+			devices = option.Config.MasqueradeInterfaces
+		}
+		family := netlink.FAMILY_V4
+		if prog == ip6tables {
+			family = netlink.FAMILY_V6
+		}
+		if routes, err := netlink.RouteList(nil, family); err == nil {
+			for _, r := range routes {
+				var link netlink.Link
+				match := false
+				if r.LinkIndex > 0 {
+					link, err = netlink.LinkByIndex(r.LinkIndex)
+					if err != nil {
+						continue
+					}
+					// Routes are dedicated to the specific interface, so we
+					// need to install the SNAT rules also for that interface
+					// via -o. If we cannot correlate to anything because no
+					// devices were specified, we need to bail out.
+					if len(devices) == 0 {
+						return fmt.Errorf("cannot correlate source route device for generating masquerading rules")
+					}
+					for _, device := range devices {
+						if device == link.Attrs().Name {
+							match = true
+							break
+						}
+					}
+				} else {
+					// There might be next hop groups where ifindex is zero
+					// and the underlying next hop devices might not be known
+					// to Cilium. In this case, assume match and don't encode
+					// -o device.
+					match = true
+				}
+				_, exclusionCIDR, err := net.ParseCIDR(snatDstExclusionCIDR)
+				if !match || r.Src == nil ||
+					cidr.Equal(r.Dst, cidr.ZeroNet(r.Family)) ||
+					(err == nil && cidr.Equal(r.Dst, exclusionCIDR)) {
+					continue
+				}
+				progArgs := []string{
+					"-t", "nat",
+					"-A", ciliumPostNatChain,
+					"-s", allocRange,
+					"-d", r.Dst.String(),
+				}
+				if link != nil {
+					progArgs = append(
+						progArgs,
+						"-o", link.Attrs().Name)
+				}
+				progArgs = append(
+					progArgs,
+					"-m", "comment", "--comment", "cilium snat non-cluster via source route",
+					"-j", "SNAT",
+					"--to-source", r.Src.String())
+				if option.Config.IPTablesRandomFully {
+					progArgs = append(progArgs, "--random-fully")
+				}
+				if err := prog.runProg(progArgs); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	// Masquerade all egress traffic leaving the node (catch-all)
 	//
 	// This rule must be first as the node ipset rule as it has different
 	// exclusion criteria than the other rules in this table.

pkg/option/config.go
@@ -372,6 +372,9 @@ const (
 	// EnableBPFMasquerade masquerades packets from endpoints leaving the host with BPF instead of iptables
 	EnableBPFMasquerade = "enable-bpf-masquerade"
 
+	// EnableMasqueradeRouteSource masquerades to the source route IP address instead of the interface one
+	EnableMasqueradeRouteSource = "enable-masquerade-to-route-source"
+
 	// DeriveMasqIPAddrFromDevice is device name which IP addr is used for BPF masquerades
 	DeriveMasqIPAddrFromDevice = "derive-masquerade-ip-addr-from-device"
 
@@ -1797,29 +1800,31 @@ type DaemonConfig struct {
 
 	// Masquerade specifies whether or not to masquerade packets from endpoints
 	// leaving the host.
-	EnableIPv4Masquerade       bool
-	EnableIPv6Masquerade       bool
-	EnableBPFMasquerade        bool
-	DeriveMasqIPAddrFromDevice string
-	EnableBPFClockProbe        bool
-	EnableIPMasqAgent          bool
-	EnableIPv4EgressGateway    bool
-	EnableEnvoyConfig          bool
-	EnableIngressController    bool
-	EnableGatewayAPI           bool
-	EnvoyConfigTimeout         time.Duration
-	IPMasqAgentConfigPath      string
-	InstallIptRules            bool
-	MonitorAggregation         string
-	PreAllocateMaps            bool
-	IPv6NodeAddr               string
-	IPv4NodeAddr               string
-	SidecarIstioProxyImage     string
-	SocketPath                 string
-	TracePayloadlen            int
-	Version                    string
-	PrometheusServeAddr        string
-	ToFQDNsMinTTL              int
+	EnableIPv4Masquerade        bool
+	EnableIPv6Masquerade        bool
+	EnableBPFMasquerade         bool
+	EnableMasqueradeRouteSource bool
+	EnableIPMasqAgent           bool
+	DeriveMasqIPAddrFromDevice  string
+	IPMasqAgentConfigPath       string
+
+	EnableBPFClockProbe     bool
+	EnableIPv4EgressGateway bool
+	EnableEnvoyConfig       bool
+	EnableIngressController bool
+	EnableGatewayAPI        bool
+	EnvoyConfigTimeout      time.Duration
+	InstallIptRules         bool
+	MonitorAggregation      string
+	PreAllocateMaps         bool
+	IPv6NodeAddr            string
+	IPv4NodeAddr            string
+	SidecarIstioProxyImage  string
+	SocketPath              string
+	TracePayloadlen         int
+	Version                 string
+	PrometheusServeAddr     string
+	ToFQDNsMinTTL           int
 
 	// DNSMaxIPsPerRestoredRule defines the maximum number of IPs to maintain
 	// for each FQDN selector in endpoint's restored DNS rules
@@ -3193,6 +3198,7 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 	c.EnableIPv4Masquerade = vp.GetBool(EnableIPv4Masquerade) && c.EnableIPv4
 	c.EnableIPv6Masquerade = vp.GetBool(EnableIPv6Masquerade) && c.EnableIPv6
 	c.EnableBPFMasquerade = vp.GetBool(EnableBPFMasquerade)
+	c.EnableMasqueradeRouteSource = vp.GetBool(EnableMasqueradeRouteSource)
 	c.DeriveMasqIPAddrFromDevice = vp.GetString(DeriveMasqIPAddrFromDevice)
 	c.EnablePMTUDiscovery = vp.GetBool(EnablePMTUDiscovery)
 	c.IPv6NAT46x64CIDR = defaults.IPv6NAT46x64CIDR

commit eb9bee6b24be67dade706a1c054d8168e5359b9f

pkg/cidr: Move linux specific variable references from netlink
The netlink consts are linux specific, so move them accordingly.

Reported-by: Tim Horner <timothy.horner@isovalent.com>
Fixes: 42c4a829d6 "cilium, iptables: Support option to masquerade source IP from routing layer"
Signed-off-by: Aditi Ghag <aditi@cilium.io>

pkg/cidr/cidr.go
@@ -7,8 +7,6 @@ import (
 	"bytes"
 	"fmt"
 	"net"
-
-	"github.com/vishvananda/netlink"
 )
 
 // NewCIDR returns a new CIDR using a net.IPNet
@@ -105,12 +103,12 @@ func Equal(n, o *net.IPNet) bool {
 // ZeroNet generates a zero net.IPNet object for the given address family
 func ZeroNet(family int) *net.IPNet {
 	switch family {
-	case netlink.FAMILY_V4:
+	case FAMILY_V4:
 		return &net.IPNet{
 			IP:   net.IPv4zero,
 			Mask: net.CIDRMask(0, 8*net.IPv4len),
 		}
-	case netlink.FAMILY_V6:
+	case FAMILY_V6:
 		return &net.IPNet{
 			IP:   net.IPv6zero,
 			Mask: net.CIDRMask(0, 8*net.IPv6len),

pkg/cidr/cidr_linux.go
@@ -1,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cidr
+
+import "github.com/vishvananda/netlink/nl"
+
+// Family type definitions
+const (
+	FAMILY_ALL  = nl.FAMILY_ALL
+	FAMILY_V4   = nl.FAMILY_V4
+	FAMILY_V6   = nl.FAMILY_V6
+	FAMILY_MPLS = nl.FAMILY_MPLS
+)

pkg/cidr/cidr_unspecified.go
@@ -1,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+//go:build !linux
+
+package cidr
+
+// Dummy values on non-linux platform
+const (
+	FAMILY_V4 = iota
+	FAMILY_V6
+)