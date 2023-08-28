g0odi3z2j/cilium

commit 498d475c399bd30db2e15c2b12e97fe577b912cf

cli: add helper functions for `cilium encrypt`
Signed-off-by: Gaurav Genani <h3llix.pvt@gmail.com>

cilium/cmd/encrypt_status.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,19 +16,40 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
+	"reflect"
+	"regexp"
+	"strings"
 
+	"github.com/cilium/cilium/api/v1/client/daemon"
+	"github.com/cilium/cilium/api/v1/models"
 	"github.com/cilium/cilium/pkg/command"
 	"github.com/cilium/cilium/pkg/common"
+	"github.com/prometheus/procfs"
+	"github.com/vishvananda/netlink"
 
 	"github.com/spf13/cobra"
 )
 
+const (
+	// Cilium uses reqid 1 to tie the IPsec security policies to their matching state
+	ciliumReqId = "1"
+)
+
+type void struct{}
+
+var (
+	voidType    void
+	countErrors int
+	regex       = regexp.MustCompile("oseq[[:blank:]](0[xX][[:xdigit:]]+)?")
+)
+
 var encryptStatusCmd = &cobra.Command{
 	Use:   "status",
-	Short: "Display current state of IPSec configurations",
+	Short: "Display the current encryption state",
 	Run: func(cmd *cobra.Command, args []string) {
 		common.RequireRootPrivilege("cilium encrypt status")
-		dumpStatus()
+		getEncryptionMode()
 	},
 }
 
@@ -37,6 +58,94 @@ func init() {
 	command.AddJSONOutput(encryptStatusCmd)
 }
 
-func dumpStatus() {
-	fmt.Print("Worked")
+func getXfrmStats() (int, map[string]int) {
+	fs, err := procfs.NewDefaultFS()
+	if err != nil {
+		Fatalf("Cannot get a new proc FS: %s", err)
+	}
+	stats, err := fs.NewXfrmStat()
+	if err != nil {
+		Fatalf("Failed to read xfrm statistics: %s", err)
+	}
+	v := reflect.ValueOf(stats)
+	errorMap := make(map[string]int)
+	if v.Type().Kind() == reflect.Struct {
+		for i := 0; i < v.NumField(); i++ {
+			name := v.Type().Field(i).Name
+			value := v.Field(i).Interface().(int)
+			if value != 0 {
+				countErrors += value
+				errorMap[name] = value
+			}
+		}
+	}
+	return countErrors, errorMap
+}
+
+func countUniqueIPsecKeys() int {
+	// trying to mimic set type data structure
+	// using void data type as struct{} because it does not use any memory
+	keys := make(map[string]void)
+	xfrmStates, err := netlink.XfrmStateList(netlink.FAMILY_ALL)
+	if err != nil {
+		Fatalf("Cannot get xfrm state: %s", err)
+	}
+	for _, v := range xfrmStates {
+		keys[string(v.Aead.Key)] = voidType
+	}
+
+	return len(keys)
+}
+
+func maxSequenceNumber() string {
+	maxSeqNum := "0"
+	out, err := exec.Command("ip", "xfrm", "state", "list", "reqid", ciliumReqId).Output()
+	if err != nil {
+		Fatalf("Cannot get xfrm states: %s", err)
+	}
+	commandOutput := string(out)
+	lines := strings.Split(commandOutput, "\n")
+	for _, line := range lines {
+		matched := regex.FindStringSubmatchIndex(line)
+		if matched != nil {
+			oseq := line[matched[2]:matched[3]]
+			if oseq > maxSeqNum {
+				maxSeqNum = oseq
+			}
+		}
+	}
+	if maxSeqNum == "0" {
+		return "N/A"
+	}
+	return fmt.Sprintf("%s/0xffffffff", maxSeqNum)
+}
+
+func getEncryptionMode() {
+	params := daemon.NewGetHealthzParamsWithTimeout(timeout)
+	params.SetBrief(&brief)
+	resp, err := client.Daemon.GetHealthz(params)
+	if err != nil {
+		Fatalf("Cannot get daemon encryption status: %s", err)
+	}
+	encryptionStatusResponse := resp.Payload.Encryption
+	fmt.Printf("Encryption: %-26s\n", encryptionStatusResponse.Mode)
+	switch encryptionStatusResponse.Mode {
+	case models.EncryptionStatusModeIPsec:
+		dumpIPsecStatus()
+	}
+
+}
+
+func dumpIPsecStatus() {
+	keys := countUniqueIPsecKeys()
+	oseq := maxSequenceNumber()
+	fmt.Printf("Keys in use: %-26d\n", keys)
+	fmt.Printf("Max Seq. Number: %s/%s\n", oseq, "0xffffffff")
+	errCount, errMap := getXfrmStats()
+	fmt.Printf("Errors: %-26d\n", errCount)
+	if errCount != 0 {
+		for k, v := range errMap {
+			fmt.Printf("\t%s: %-26d\n", k, v)
+		}
+	}
 }

cilium/cmd/encrypt.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ import (
 // encryptCmd represents the encrypt command
 var encryptCmd = &cobra.Command{
 	Use:   "encrypt",
-	Short: "Access to Ipsec state of node",
+	Short: "Manage transparent encryption",
 }
 
 func init() {

cilium/cmd/encrypt_flush.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Authors of Cilium
+// Copyright 2021 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,7 +24,8 @@ import (
 
 var encryptFlushCmd = &cobra.Command{
 	Use:   "flush",
-	Short: "Flushes the current IPSec states",
+	Short: "Flushes the current IPsec state",
+	Long:  "Will cause a short connectivity disruption",
 	Run: func(cmd *cobra.Command, args []string) {
 		common.RequireRootPrivilege("cilium encrypt flush")
 		netlink.XfrmPolicyFlush()

go.mod
@@ -67,6 +67,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0
 	github.com/prometheus/client_golang v1.11.0
 	github.com/prometheus/client_model v0.2.1-0.20200623203004-60555c9708c7
+	github.com/prometheus/procfs v0.2.0
 	github.com/russross/blackfriday/v2 v2.1.0
 	github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa
 	github.com/servak/go-fastping v0.0.0-20160802140958-5718d12e20a0

commit 59733f76922de81bdc6f62aeeed522b606852cca

cmd: Fix the computed IPsec max. sequence number
maxSequenceNumber currently iterates over all XFRM states in the ip xfrm
state list output to find the largest sequence number. It however does
so while keeping the parsed sequence numbers as hexadecimal strings.
Hence, a number like "0xc1" is understood as being larger than e.g.
"0x1234".

This commit fixes it by parsing the sequence numbers into int64 before
comparing them.

We also need to adapt the regular expression slightly to avoid
considering the "0x" prefix as part of the number, given
strconv.ParseInt doesn't support it.

Fixes: 498d475c39 ("cli: add helper functions for `cilium encrypt`")
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

cilium/cmd/encrypt_status.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"reflect"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/prometheus/procfs"
@@ -30,7 +31,7 @@ type void struct{}
 var (
 	voidType    void
 	countErrors int
-	regex       = regexp.MustCompile("oseq[[:blank:]](0[xX][[:xdigit:]]+)?")
+	regex       = regexp.MustCompile("oseq[[:blank:]]0[xX]([[:xdigit:]]+)")
 )
 
 var encryptStatusCmd = &cobra.Command{
@@ -89,13 +90,17 @@ func countUniqueIPsecKeys() int {
 	return len(keys)
 }
 
-func extractMaxSequenceNumber(ipOutput string) string {
-	maxSeqNum := "0"
+func extractMaxSequenceNumber(ipOutput string) int64 {
+	maxSeqNum := int64(0)
 	lines := strings.Split(ipOutput, "\n")
 	for _, line := range lines {
 		matched := regex.FindStringSubmatchIndex(line)
 		if matched != nil {
-			oseq := line[matched[2]:matched[3]]
+			oseq, err := strconv.ParseInt(line[matched[2]:matched[3]], 16, 64)
+			if err != nil {
+				Fatalf("Failed to parse sequence number '%s': %s",
+					line[matched[2]:matched[3]], err)
+			}
 			if oseq > maxSeqNum {
 				maxSeqNum = oseq
 			}
@@ -111,10 +116,10 @@ func maxSequenceNumber() string {
 	}
 	commandOutput := string(out)
 	maxSeqNum := extractMaxSequenceNumber(commandOutput)
-	if maxSeqNum == "0" {
+	if maxSeqNum == 0 {
 		return "N/A"
 	}
-	return fmt.Sprintf("%s/0xffffffff", maxSeqNum)
+	return fmt.Sprintf("0x%x/0xffffffff", maxSeqNum)
 }
 
 func getEncryptionMode() {