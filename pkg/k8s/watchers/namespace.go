g0odi3z2j/cilium

commit 91251e9d2ae4f8311b37d99dc911dc39770d4fb3

pkg/k8s: ignore namespace events that do not change labels
As we can receive different type of namespace events, like difference in
the annotations. We can ignore all of these events unless the labels are
different.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/namespace.go
@@ -92,6 +92,12 @@ func (k *K8sWatcher) updateK8sV1Namespace(oldNS, newNS *slim_corev1.Namespace) e
 	oldIdtyLabels, _ := labelsfilter.Filter(oldLabels)
 	newIdtyLabels, _ := labelsfilter.Filter(newLabels)
 
+	// Do not perform any other operations the the old labels are the same as
+	// the new labels
+	if oldIdtyLabels.DeepEqual(&newIdtyLabels) {
+		return nil
+	}
+
 	eps := k.endpointManager.GetEndpoints()
 	failed := false
 	for _, ep := range eps {

commit 12108d78a92adf98a026a4aa9a8594413ca3efb9

k8s/watchers: fix propagation of namespace labels to endpoint labels
Currently (*namespaceUpdater).oldLabels is never updated with the
namespace's labels after they have been synced to endpoints. This can
e.g. lead to updates mistakenly being skipped. This in turn can lead to
situations as described in https://github.com/cilium/cilium/issues/27626
where namespace derived labels are not removed from CEPs when the
corresponding label is removed from the namespace.

Fix this by always updating (*namespaceUpdater).oldLabels on successful
endpoint label update.

Fixes: 91251e9d2ae4 ("pkg/k8s: ignore namespace events that do not change labels")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/k8s/watchers/namespace.go
@@ -86,8 +86,8 @@ func (u *namespaceUpdater) update(newNS *slim_corev1.Namespace) error {
 	oldIdtyLabels, _ := labelsfilter.Filter(oldLabels)
 	newIdtyLabels, _ := labelsfilter.Filter(newLabels)
 
-	// Do not perform any other operations the the old labels are the same as
-	// the new labels
+	// Do not perform any other operations if the old labels are the same as
+	// the new labels.
 	if oldIdtyLabels.DeepEqual(&newIdtyLabels) {
 		return nil
 	}
@@ -100,7 +100,7 @@ func (u *namespaceUpdater) update(newNS *slim_corev1.Namespace) error {
 			err := ep.ModifyIdentityLabels(newIdtyLabels, oldIdtyLabels)
 			if err != nil {
 				log.WithError(err).WithField(logfields.EndpointID, ep.ID).
-					Warningf("unable to update endpoint with new namespace labels")
+					Warning("unable to update endpoint with new identity labels from namespace labels")
 				failed = true
 			}
 		}
@@ -108,6 +108,7 @@ func (u *namespaceUpdater) update(newNS *slim_corev1.Namespace) error {
 	if failed {
 		return errors.New("unable to update some endpoints with new namespace labels")
 	}
+	u.oldLabels[newNS.Name] = newLabels
 	return nil
 }
 