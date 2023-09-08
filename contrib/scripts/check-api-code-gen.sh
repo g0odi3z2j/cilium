g0odi3z2j/cilium

commit 46140e28c9cb825e60e2e807168e39eee6347115

contrib: add check for new files in check-(api|k8s)-code-gen
Currently, the `check-(api|k8s)-code-gen` scripts verify that generated
files are up to date, running a full regeneration and checking the
resulting output of `git diff`. Yet, `git diff` does not include
unstaged files, meaning that brand new files would be missed.

Let's fix this running `git add --intent-to-add .` first to make them
appear in the diff.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

contrib/scripts/check-api-code-gen.sh
@@ -34,6 +34,9 @@ make generate-operator-api
 # Generate all hubble api files
 make generate-hubble-api
 
+# Ensure new files are also considered in the diff
+git add --intent-to-add .
+
 # Check for diff
 diff="$(git diff)"
 

contrib/scripts/check-k8s-code-gen.sh
@@ -18,6 +18,9 @@ rm -rf ./pkg/k8s/slim/k8s/{client,apiextensions-client}
 # Generate all files
 make generate-k8s-api manifests
 
+# Ensure new files are also considered in the diff
+git add --intent-to-add .
+
 # Check for diff
 diff="$(git diff)"
 

commit a8a8ddec38f473ba0b99c01c62e1b34f1c804a1b

contrib/scripts: check diff of staged files
With the change made in 46140e28c9cb, we need to check the diff of the
staged files, otherwise we will not detect files that got deleted and
not regenerated back.

Fixes: 46140e28c9cb ("contrib: add check for new files in check-(api|k8s)-code-gen")

Reported-by: Harsimran Pabla <hpabla@isovalent.com>
Signed-off-by: André Martins <andre@cilium.io>

contrib/scripts/check-api-code-gen.sh
@@ -39,10 +39,12 @@ git add --intent-to-add .
 
 # Check for diff
 diff="$(git diff)"
+diff_staged="$(git diff --staged)"
 
-if [ -n "$diff" ]; then
+if [ -n "$diff" ] || [ -n "$diff_staged" ]; then
 	echo "Ungenerated api source code:"
 	echo "$diff"
+	echo "$diff_staged"
 	echo "Please run 'make generate-api generate-health-api generate-hubble-api generate-operator-api' and submit your changes"
 	exit 1
 fi

contrib/scripts/check-k8s-code-gen.sh
@@ -23,10 +23,12 @@ git add --intent-to-add .
 
 # Check for diff
 diff="$(git diff)"
+diff_staged="$(git diff --staged)"
 
-if [ -n "$diff" ]; then
+if [ -n "$diff" ] || [ -n "$diff_staged" ]; then
 	echo "Ungenerated source code:"
 	echo "$diff"
+	echo "$diff_staged"
 	echo "Please run make 'generate-k8s-api && make manifests' and submit your changes"
 	exit 1
 fi