g0odi3z2j/cilium

commit a0fe078d4f74d669c78073f9efcb1d498ec84a34

contrib: Move github release to post-release
There's a few suboptimal things with the current release process:
- If one contributor prepares part of the release but another person
  tags the release, then later steps in the process can fail because
  early scripts create local files that are required to complete the
  release.
- Despite programmatically creating the release text, the workflow
  expects that a release preparer will manually pull the docker digests
  and insert it into the release text.

We can fix this by deferring the github release creation until the
post-release phase. At this point, the digests are known for the target
releases, so we can pull those from the specified target URL. At this
point, the changelog also includes all of the remaining details that we
need to prepare the release text.

As a side effect, this allows us to align the processes between the
patch versions and prereleases more closely.

Signed-off-by: Joe Stringer <joe@cilium.io>

contrib/release/start-release.sh
@@ -123,8 +123,6 @@ main() {
     logecho "  * If this is a prerelease, create a revert commit"
     logecho "* Push the PR to Github for review ('submit-release.sh')"
     logecho "* (After PR merge) Use 'tag-release.sh' to prepare tags/release"
-
-    # Leave $version-changes.txt around for prep-release.sh usage later
 }
 
 main "$@"

contrib/release/post-release.sh
@@ -44,9 +44,9 @@ handle_args() {
         exit 1
     fi
 
-    if [ ! -z "$2" ] && ! echo "$2" | grep -q "[0-9]\+\.[0-9]\+\.[0-9]\+"; then
+    if [ ! -z "$2" ] && ! echo "$2" | grep -q "$RELEASE_REGEX"; then
         usage 2>&1
-        common::exit 1 "Invalid VERSION ARG \"$2\"; Expected X.Y.Z"
+        common::exit 1 "Invalid VERSION ARG \"$2\"; $RELEASE_FORMAT_MSG"
     fi
 
     if [ -z "${GITHUB_TOKEN}" ]; then
@@ -65,6 +65,21 @@ main() {
 
     git checkout -b pr/$version-digests $version
     ${DIR}/pull-docker-manifests.sh "$@"
+
+    echo -e "$ersion\n" > $version-release-summary.txt
+    # Grab the release notes for the current release, stop before the next
+    # release. The start of line `## vX.Y.Z` lines will match in command.
+    tail -n+4 CHANGELOG.md | sed '/^## v.*$/q' >> $version-release-summary.txt
+    tail -n+2 digest-$version.txt >> $version-release-summary.txt
+    logecho "Creating Github draft release"
+    logrun hub release create -d -F $version-release-summary.txt $version
+    logecho "Browse to $RELEASES_URL to see the draft release"
+
+    if version_is_prerelease "$version"; then
+        # No digest updates for main branch for prereleases.
+        return
+    fi
+
     if grep -q update-helm-values Documentation/Makefile; then
         logrun make -C Documentation update-helm-values
     fi

contrib/release/tag-release.sh
@@ -53,10 +53,6 @@ main() {
     fi
     local version="v$ersion"
 
-    if [[ ! -e $version-changes.txt ]]; then
-        common::exit 1 "Generate release notes via contrib/release/start-release.sh"
-    fi
-
     git fetch $REMOTE
 
     local commit="$(git rev-parse HEAD)"
@@ -75,14 +71,6 @@ main() {
     logrun -s git tag -a $ersion -s -m "Release $version"
     logrun -s git tag -a $version -s -m "Release $version"
     logrun -s git push $REMOTE $version $ersion
-
-    # Leave $version-changes.txt around so we can generate release notes later
-    echo -e "$ersion\n" > $version-release-summary.txt
-    echo "We are pleased to release Cilium $version." >>  $version-release-summary.txt
-    tail -n+4 $version-changes.txt >> $version-release-summary.txt
-    logecho "Creating Github draft release"
-    logrun hub release create -d -F $version-release-summary.txt $version
-    logecho "Browse to $RELEASES_URL to see the draft release"
 
     logecho
     logecho "Next steps:"

commit 771eeaf5321b7a6b397283b17fabee8f84b80436

contrib: Fix missing function in post-release.sh
Commit a0fe078d4f74 ("contrib: Move github release to post-release")
introduced a usage of version_is_prerelease() in post-release.sh, but
this was defined in start-release.sh so the function couldn't be found.
Move it to common.sh so it can be reused across scripts.

Fixes: a0fe078d4f74 ("contrib: Move github release to post-release")

Signed-off-by: Joe Stringer <joe@cilium.io>

contrib/backporting/common.sh
@@ -82,3 +82,15 @@ get_branch_from_version() {
     fi
     echo "$branch"
 }
+
+# $1 - VERSION
+version_is_prerelease() {
+    case "$1" in
+        *pre*|*rc*|*snapshot*)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}

contrib/release/start-release.sh
@@ -22,18 +22,6 @@ usage() {
     logecho "--help     Print this help message"
 }
 
-# $1 - VERSION
-version_is_prerelease() {
-    case "$1" in
-        *pre*|*rc*|*snapshot*)
-            return 0
-            ;;
-        *)
-            return 1
-            ;;
-    esac
-}
-
 handle_args() {
     if [ "$#" -gt 3 ]; then
         usage 2>&1