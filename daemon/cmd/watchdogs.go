g0odi3z2j/cilium

commit f2c2cf62cc0e4514c56eaea4b2601d2de1af03c8

Resiliency: Checks endpoints BPF programs remain loaded
We've seen instance where BPF programs are deleted by third party
software (aka Ddog). When this occurrs either agents could fail to
restart or connectivity may be compromised.

This PR attempts to reconciliate BPF program state with their associated
endpoints by ensuring endpoints programs are reloaded when necessary.

- Add a watchdog to ensure endpoints bpf program remain loaded
- Checks for tc qdisk/ingress filter presence
- Add configuration to enable/disable this feature and configure
  watchdog rate
- Add call to terminate controllers upon daemon shutting down

Signed-off-by: Fernand Galiana <fernand.galiana@isovalent.com>

daemon/cmd/watchdogs.go
@@ -1,0 +1,136 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cmd
+
+import (
+	"context"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+
+	"github.com/cilium/cilium/pkg/controller"
+	"github.com/cilium/cilium/pkg/datapath/loader"
+	"github.com/cilium/cilium/pkg/endpoint"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/promise"
+)
+
+const epBPFProgWatchdog = "ep-bpf-prog-watchdog"
+
+type epBPFProgWatchdogConfig struct {
+	EndpointBPFProgWatchdogInterval time.Duration
+}
+
+func (c epBPFProgWatchdogConfig) Flags(flags *pflag.FlagSet) {
+	flags.Duration("endpoint-bpf-prog-watchdog-interval", c.EndpointBPFProgWatchdogInterval,
+		"Interval to trigger endpoint BPF programs load check watchdog")
+}
+
+type epBPFProgWatchdogParams struct {
+	cell.In
+
+	Config        epBPFProgWatchdogConfig
+	Logger        logrus.FieldLogger
+	Lifecycle     hive.Lifecycle
+	DaemonPromise promise.Promise[*Daemon]
+}
+
+var (
+	// endpointBPFrogWatchdogCell triggers a job to ensure device tc programs remain loaded.
+	endpointBPFrogWatchdogCell = cell.Module(
+		epBPFProgWatchdog,
+		"Periodically checks that endpoint BPF programs remain loaded",
+
+		cell.Config(epBPFProgWatchdogConfigDefault),
+		cell.Invoke(registerEndpointBPFProgWatchdog),
+	)
+
+	epBPFProgWatchdogConfigDefault = epBPFProgWatchdogConfig{
+		EndpointBPFProgWatchdogInterval: 30 * time.Second,
+	}
+)
+
+func registerEndpointBPFProgWatchdog(p epBPFProgWatchdogParams) {
+	if p.Config.EndpointBPFProgWatchdogInterval == 0 {
+		return
+	}
+
+	var (
+		ctx, cancel = context.WithCancel(context.Background())
+		mgr         = controller.NewManager()
+	)
+	p.Lifecycle.Append(hive.Hook{
+		OnStart: func(hive.HookContext) error {
+			mgr.UpdateController(
+				epBPFProgWatchdog,
+				controller.ControllerParams{
+					Group: controller.NewGroup(epBPFProgWatchdog),
+					DoFunc: func(ctx context.Context) error {
+						d, err := p.DaemonPromise.Await(ctx)
+						if err != nil {
+							return err
+						}
+						return d.checkEndpointBPFPrograms(ctx, p)
+					},
+					RunInterval: p.Config.EndpointBPFProgWatchdogInterval,
+					Context:     ctx,
+				},
+			)
+
+			return nil
+		},
+		OnStop: func(hive.HookContext) error {
+			cancel()
+			mgr.RemoveAllAndWait()
+			return nil
+		},
+	},
+	)
+}
+
+func (d *Daemon) checkEndpointBPFPrograms(ctx context.Context, p epBPFProgWatchdogParams) error {
+	var (
+		loaded = true
+		err    error
+		eps    = d.endpointManager.GetEndpoints()
+	)
+	for _, ep := range eps {
+		if ep.GetState() != endpoint.StateReady {
+			continue
+		}
+		loaded, err = loader.DeviceHasTCProgramLoaded(ep.HostInterface(), ep.RequireEgressProg())
+		if err != nil {
+			log.WithField(logfields.Endpoint, ep.HostInterface()).
+				WithError(err).
+				Error("Unable to assert if endpoint BPF programs need to be reloaded")
+			return err
+		}
+		// We've detected missing bpf progs for this endpoint.
+		// Break and trigger bpf progs reload.
+		if !loaded {
+			break
+		}
+	}
+	if loaded {
+		return nil
+	}
+
+	log.WithField(logfields.Count, len(eps)).
+		Warn(
+			"Detected unexpected endpoint BPF program removal. " +
+				"Consider investigating whether other software running on this machine is removing Cilium's endpoint BPF programs. " +
+				"If endpoint BPF programs are removed, the associated pods will lose connectivity and only reinstating the programs will restore connectivity.",
+		)
+	wg, err := d.TriggerReloadWithoutCompile(epBPFProgWatchdog)
+	if err != nil {
+		log.WithError(err).Error("Failed to reload Cilium endpoints BPF programs")
+	} else {
+		wg.Wait()
+	}
+
+	return err
+}

daemon/cmd/daemon.go
@@ -1276,6 +1276,8 @@ func (d *Daemon) Close() {
 	}
 	identitymanager.RemoveAll()
 	d.cgroupManager.Close()
+
+	// Ensures all controllers are stopped!
 	d.controllers.RemoveAllAndWait()
 }
 

daemon/cmd/daemon_main.go
@@ -1627,6 +1627,7 @@ var daemonCell = cell.Module(
 	// by the debuginfo API
 	cell.ProvidePrivate(daemonSettings),
 	cell.Invoke(func(promise.Promise[*Daemon]) {}), // Force instantiation.
+	endpointBPFrogWatchdogCell,
 )
 
 type daemonParams struct {

pkg/datapath/loader/netlink.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/vishvananda/netlink"
@@ -24,6 +25,8 @@ import (
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/sysctl"
 )
+
+const qdiscClsact = "clsact"
 
 func directionToParent(dir string) uint32 {
 	switch dir {
@@ -44,7 +47,7 @@ func replaceQdisc(link netlink.Link) error {
 
 	qdisc := &netlink.GenericQdisc{
 		QdiscAttrs: attrs,
-		QdiscType:  "clsact",
+		QdiscType:  qdiscClsact,
 	}
 
 	return netlink.QdiscReplace(qdisc)
@@ -691,4 +694,62 @@ func renameDevice(from, to string) error {
 	}
 
 	return nil
+}
+
+// DeviceHasTCProgramLoaded checks whether a given device has tc filter/qdisc progs attached.
+func DeviceHasTCProgramLoaded(hostInterface string, checkEgress bool) (bool, error) {
+	const bpfProgPrefix = "cil_"
+
+	l, err := netlink.LinkByName(hostInterface)
+	if err != nil {
+		return false, fmt.Errorf("unable to find endpoint link by name: %w", err)
+	}
+
+	dd, err := netlink.QdiscList(l)
+	if err != nil {
+		return false, fmt.Errorf("unable to fetch qdisc list for endpoint: %w", err)
+	}
+	var found bool
+	for _, d := range dd {
+		if d.Type() == qdiscClsact {
+			found = true
+			break
+		}
+	}
+	if !found {
+		return false, nil
+	}
+
+	ff, err := netlink.FilterList(l, netlink.HANDLE_MIN_INGRESS)
+	if err != nil {
+		return false, fmt.Errorf("unable to fetch ingress filter list: %w", err)
+	}
+	var filtersCount int
+	for _, f := range ff {
+		if filter, ok := f.(*netlink.BpfFilter); ok {
+			if strings.HasPrefix(filter.Name, bpfProgPrefix) {
+				filtersCount++
+			}
+		}
+	}
+	if filtersCount == 0 {
+		return false, nil
+	}
+	if !checkEgress {
+		return true, nil
+	}
+
+	ff, err = netlink.FilterList(l, netlink.HANDLE_MIN_EGRESS)
+	if err != nil {
+		return false, fmt.Errorf("unable to fetch egress filter list: %w", err)
+	}
+	filtersCount = 0
+	for _, f := range ff {
+		if filter, ok := f.(*netlink.BpfFilter); ok {
+			if strings.HasPrefix(filter.Name, bpfProgPrefix) {
+				filtersCount++
+			}
+		}
+	}
+	return len(ff) > 0 && filtersCount > 0, nil
 }

commit 1d40c254180a8457f364bb34d542eddcfa8242f2

report endpoint ID on endpoint BPF program
Presenting only the endpoint interface name into the logs might not be
enough to understand which endpoint has the problem. Thus, we should
print the endpoint ID well.

Fixes: f2c2cf62cc0e ("Resiliency: Checks endpoints BPF programs remain loaded")
Signed-off-by: André Martins <andre@cilium.io>

daemon/cmd/watchdogs.go
@@ -109,6 +109,7 @@ func (d *Daemon) checkEndpointBPFPrograms(ctx context.Context, p epBPFProgWatchd
 		loaded, err = loader.DeviceHasTCProgramLoaded(ep.HostInterface(), ep.RequireEgressProg())
 		if err != nil {
 			log.WithField(logfields.Endpoint, ep.HostInterface()).
+				WithField(logfields.EndpointID, ep.ID).
 				WithError(err).
 				Error("Unable to assert if endpoint BPF programs need to be reloaded")
 			return err