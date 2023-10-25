g0odi3z2j/cilium

commit 14411d270cb81cd76bedf2f24e1bbd3864199b97

daemon: Convert host IP sync to async ipcache API
Switch over from using direct ipcache insertion with raw identities to a
label-based model, see GH-21142.

Signed-off-by: Chris Tarazi <chris@isovalent.com>

daemon/cmd/datapath.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"net"
+	"net/netip"
 	"os"
 	"strings"
 	"time"
@@ -25,6 +26,7 @@ import (
 	"github.com/cilium/cilium/pkg/identity"
 	ippkg "github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/ipcache"
+	ipcachetypes "github.com/cilium/cilium/pkg/ipcache/types"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ctmap"
@@ -163,7 +165,11 @@ func (d *Daemon) syncHostIPs() error {
 		return nil
 	}
 
-	specialIdentities := []identity.IPIdentityPair{}
+	type ipIDLabel struct {
+		identity.IPIdentityPair
+		labels.Labels
+	}
+	specialIdentities := make([]ipIDLabel, 0, 2)
 
 	if option.Config.EnableIPv4 {
 		addrs, err := d.datapath.LocalNodeAddressing().IPv4().LocalAddresses()
@@ -177,20 +183,24 @@ func (d *Daemon) syncHostIPs() error {
 			}
 
 			if len(ip) > 0 {
-				specialIdentities = append(specialIdentities,
+				specialIdentities = append(specialIdentities, ipIDLabel{
 					identity.IPIdentityPair{
 						IP: ip,
 						ID: identity.ReservedIdentityHost,
-					})
+					},
+					labels.LabelHost,
+				})
 			}
 		}
 
-		specialIdentities = append(specialIdentities,
+		specialIdentities = append(specialIdentities, ipIDLabel{
 			identity.IPIdentityPair{
 				IP:   net.IPv4zero,
 				Mask: net.CIDRMask(0, net.IPv4len*8),
 				ID:   identity.ReservedIdentityWorld,
-			})
+			},
+			labels.LabelWorld,
+		})
 	}
 
 	if option.Config.EnableIPv6 {
@@ -206,20 +216,24 @@ func (d *Daemon) syncHostIPs() error {
 			}
 
 			if len(ip) > 0 {
-				specialIdentities = append(specialIdentities,
+				specialIdentities = append(specialIdentities, ipIDLabel{
 					identity.IPIdentityPair{
 						IP: ip,
 						ID: identity.ReservedIdentityHost,
-					})
+					},
+					labels.LabelHost,
+				})
 			}
 		}
 
-		specialIdentities = append(specialIdentities,
+		specialIdentities = append(specialIdentities, ipIDLabel{
 			identity.IPIdentityPair{
 				IP:   net.IPv6zero,
 				Mask: net.CIDRMask(0, net.IPv6len*8),
 				ID:   identity.ReservedIdentityWorld,
-			})
+			},
+			labels.LabelWorld,
+		})
 	}
 
 	existingEndpoints, err := lxcmap.DumpToMap()
@@ -227,30 +241,31 @@ func (d *Daemon) syncHostIPs() error {
 		return err
 	}
 
-	for _, ipIDPair := range specialIdentities {
-		isHost := ipIDPair.ID == identity.ReservedIdentityHost
+	daemonResourceID := ipcachetypes.NewResourceID(ipcachetypes.ResourceKindDaemon, "", "")
+	for _, ipIDLblsPair := range specialIdentities {
+		isHost := ipIDLblsPair.ID == identity.ReservedIdentityHost
 		if isHost {
-			added, err := lxcmap.SyncHostEntry(ipIDPair.IP)
+			added, err := lxcmap.SyncHostEntry(ipIDLblsPair.IP)
 			if err != nil {
 				return fmt.Errorf("Unable to add host entry to endpoint map: %s", err)
 			}
 			if added {
-				log.WithField(logfields.IPAddr, ipIDPair.IP).Debugf("Added local ip to endpoint map")
+				log.WithField(logfields.IPAddr, ipIDLblsPair.IP).Debugf("Added local ip to endpoint map")
 			}
 		}
 
-		delete(existingEndpoints, ipIDPair.IP.String())
+		delete(existingEndpoints, ipIDLblsPair.IP.String())
 
-		// Upsert will not propagate (reserved:foo->ID) mappings across the cluster,
-		// and we specifically don't want to do so.
-		//
-		// This upsert will fail with ErrOverwrite continuously as long as the
-		// EP / CN watcher have found an apiserver IP and upserted it into the
-		// ipcache. Until then, it is expected to succeed.
-		d.ipcache.Upsert(ipIDPair.PrefixString(), nil, 0, nil, ipcache.Identity{
-			ID:     ipIDPair.ID,
-			Source: d.sourceByIP(ipIDPair.IP, source.Local),
-		})
+		lbls := ipIDLblsPair.Labels
+		if ipIDLblsPair.ID == identity.ReservedIdentityWorld {
+			p := netip.PrefixFrom(ippkg.MustAddrFromIP(ipIDLblsPair.IP), 0)
+			d.ipcache.OverrideIdentity(p, lbls, source.Local, daemonResourceID)
+		} else {
+			d.ipcache.UpsertLabels(ippkg.IPToNetPrefix(ipIDLblsPair.IP),
+				lbls,
+				source.Local, daemonResourceID,
+			)
+		}
 	}
 
 	// existingEndpoints is a map from endpoint IP to endpoint info. Referring
@@ -265,7 +280,7 @@ func (d *Daemon) syncHostIPs() error {
 				log.Debugf("Removed outdated host IP %s from endpoint map", hostIP)
 			}
 
-			d.ipcache.Delete(hostIP, d.sourceByIP(ip, source.Local))
+			d.ipcache.RemoveLabels(ippkg.IPToNetPrefix(ip), labels.LabelHost, daemonResourceID)
 		}
 	}
 
@@ -278,24 +293,9 @@ func (d *Daemon) syncHostIPs() error {
 		if err != nil {
 			return err
 		}
-
 	}
 
 	return nil
-}
-
-func (d *Daemon) sourceByIP(ip net.IP, defaultSrc source.Source) source.Source {
-	if addr, ok := ippkg.AddrFromIP(ip); ok {
-		lbls := d.ipcache.GetMetadataLabelsByIP(addr)
-		if lbls.Has(labels.LabelKubeAPIServer[labels.IDNameKubeAPIServer]) {
-			return source.KubeAPIServer
-		}
-	} else {
-		log.WithFields(logrus.Fields{
-			logfields.IPAddr: ip,
-		}).Warning("BUG: Invalid addr detected in host stack. Please report this bug to the Cilium developers.")
-	}
-	return defaultSrc
 }
 
 // initMaps opens all BPF maps (and creates them if they do not exist). This

pkg/ipcache/metadata_test.go
@@ -25,6 +25,7 @@ var (
 	inClusterPrefix  = netip.MustParsePrefix("10.0.0.4/32")
 	inClusterPrefix2 = netip.MustParsePrefix("10.0.0.5/32")
 	aPrefix          = netip.MustParsePrefix("100.4.16.32/32")
+	allCIDRsPrefix   = netip.MustParsePrefix("0.0.0.0/0")
 )
 
 func TestInjectLabels(t *testing.T) {
@@ -112,6 +113,17 @@ func TestInjectLabels(t *testing.T) {
 	assert.Len(t, remaining, 0)
 	assert.Len(t, IPIdentityCache.ipToIdentityCache, 3)
 	assert.False(t, IPIdentityCache.ipToIdentityCache["100.4.16.32/32"].ID.HasLocalScope())
+
+	// Assert that an upsert for reserved:world label results in only the
+	// reserved world ID.
+	IPIdentityCache.metadata.upsertLocked(allCIDRsPrefix, source.Local, "daemon-uid", labels.LabelWorld)
+	assert.Len(t, IPIdentityCache.metadata.m, 4)
+	remaining, err = IPIdentityCache.InjectLabels(ctx, []netip.Prefix{allCIDRsPrefix})
+	assert.NoError(t, err)
+	assert.Len(t, remaining, 0)
+	assert.Len(t, IPIdentityCache.ipToIdentityCache, 4)
+	assert.False(t, IPIdentityCache.ipToIdentityCache["0.0.0.0/0"].ID.HasLocalScope())
+	assert.Equal(t, identity.ReservedIdentityWorld, IPIdentityCache.ipToIdentityCache["0.0.0.0/0"].ID)
 }
 
 // TestInjectExisting tests "upgrading" an existing identity to the apiserver.
@@ -159,7 +171,6 @@ func TestInjectExisting(t *testing.T) {
 	id, ok = IPIdentityCache.LookupByIP(prefix.String())
 	assert.True(t, ok)
 	assert.Equal(t, source.KubeAPIServer, id.Source)
-
 }
 
 func TestFilterMetadataByLabels(t *testing.T) {

pkg/ipcache/types/types.go
@@ -42,6 +42,7 @@ type ResourceKind string
 var (
 	ResourceKindCNP      = ResourceKind("cnp")
 	ResourceKindCCNP     = ResourceKind("ccnp")
+	ResourceKindDaemon   = ResourceKind("daemon")
 	ResourceKindEndpoint = ResourceKind("ep")
 	ResourceKindNetpol   = ResourceKind("netpol")
 	ResourceKindNode     = ResourceKind("node")

commit 585877a7fd6a49b4f4233180af54b4689131d52c

daemon: Uniquely identify daemon ipcache upserts
Each callpath from the daemon should use a unique resource ID to ensure
that only code corresponding to that specific callpath will add/remove
entries with the same resourceID. This can prevent bugs where either:
- The same resource gets overwritten by another caller, or
- Identities get prematurely released because the daemon thinks that the
  last remaining user for the identity has been removed.

This doesn't look like a bug yet, because the IPs for ingress and
reserved ipcache entries are disjoint sets today. However, this could
cause problems with new PRs that are out for review.

Fixes: 14411d270cb8 ("daemon: Convert host IP sync to async ipcache API")
Fixes: 32d658603533 ("dameon: Convert ingress restoration code to async ipcache API")
Signed-off-by: Joe Stringer <joe@cilium.io>

daemon/cmd/daemon.go
@@ -703,7 +703,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 			ingressIP,
 			labels.LabelIngress,
 			source.Restored,
-			ipcachetypes.NewResourceID(ipcachetypes.ResourceKindDaemon, "", ""),
+			ipcachetypes.NewResourceID(ipcachetypes.ResourceKindDaemon, "", "ingress"),
 		)
 	}
 	if len(oldIngressIPs) > 0 {

daemon/cmd/datapath.go
@@ -256,7 +256,7 @@ func (d *Daemon) syncHostIPs() error {
 		return err
 	}
 
-	daemonResourceID := ipcachetypes.NewResourceID(ipcachetypes.ResourceKindDaemon, "", "")
+	daemonResourceID := ipcachetypes.NewResourceID(ipcachetypes.ResourceKindDaemon, "", "reserved")
 	for _, ipIDLblsPair := range specialIdentities {
 		isHost := ipIDLblsPair.ID == identity.ReservedIdentityHost
 		if isHost {