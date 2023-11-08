g0odi3z2j/cilium

commit 8b553913aa5d4806224d4b61cd4276e5440b4c2a

devices: Flush routes on device delete
As Linux does not send comprehensive set of route delete messages on device
deletion, flush the routes on the link delete message.

Since the link and route messages are coming over separate sockets
they may come out of order, so keep track of what link indexes have been deleted
and ignore route updates for deleted links. Linux does reuse the index after the
32 bits roll around, so remove the "dead link index" on link creation.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/datapath/linux/devices_controller.go
@@ -106,15 +106,21 @@ type devicesController struct {
 	filter         deviceFilter
 	l3DevSupported bool
 
+	// deadLinkIndexes tracks the set of links that have been deleted. This is needed
+	// to avoid processing route or address updates after a link delete as they may
+	// arrive out of order due to the use of separate netlink sockets.
+	deadLinkIndexes sets.Set[int]
+
 	cancel context.CancelFunc // controller's context is cancelled when stopped.
 }
 
 func newDevicesController(lc hive.Lifecycle, p devicesControllerParams) (*devicesController, statedb.Table[*tables.Device], statedb.Table[*tables.Route]) {
 	dc := &devicesController{
-		params:      p,
-		initialized: make(chan struct{}),
-		filter:      deviceFilter(p.Config.Devices),
-		log:         p.Log,
+		params:          p,
+		initialized:     make(chan struct{}),
+		filter:          deviceFilter(p.Config.Devices),
+		log:             p.Log,
+		deadLinkIndexes: sets.New[int](),
 	}
 	lc.Append(dc)
 	return dc, p.DeviceTable, p.RouteTable
@@ -391,10 +397,14 @@ func (dc *devicesController) processBatch(txn statedb.WriteTxn, batch map[int][]
 			d = d.DeepCopy()
 		}
 		deviceDeleted := false
+		deviceUpdated := false // Set to true if the batch contained an address or link update.
 
 		for _, u := range updates {
 			switch u := u.(type) {
 			case netlink.AddrUpdate:
+				if dc.deadLinkIndexes.Has(u.LinkIndex) {
+					continue
+				}
 				addr := deviceAddressFromAddrUpdate(u)
 				if u.NewAddr {
 					d.Addrs = append(d.Addrs, addr)
@@ -404,7 +414,15 @@ func (dc *devicesController) processBatch(txn statedb.WriteTxn, batch map[int][]
 						d.Addrs = slices.Delete(d.Addrs, i, i+1)
 					}
 				}
+				deviceUpdated = true
 			case netlink.RouteUpdate:
+				if dc.deadLinkIndexes.Has(u.LinkIndex) {
+					// Ignore route updates for a device that has been removed
+					// to avoid processing an out of order route create after
+					// link delete (Linux won't send complete set of messages
+					// of routes deleted when link is deleted).
+					continue
+				}
 				r := tables.Route{
 					Table:     u.Table,
 					LinkIndex: index,
@@ -419,7 +437,7 @@ func (dc *devicesController) processBatch(txn statedb.WriteTxn, batch map[int][]
 					if err != nil {
 						dc.log.WithError(err).WithField(logfields.Route, r).Warn("Failed to insert route")
 					}
-				} else {
+				} else if u.Type == unix.RTM_DELROUTE {
 					_, _, err := dc.params.RouteTable.Delete(txn, &r)
 					if err != nil {
 						dc.log.WithError(err).WithField(logfields.Route, r).Warn("Failed to delete route")
@@ -427,21 +445,29 @@ func (dc *devicesController) processBatch(txn statedb.WriteTxn, batch map[int][]
 				}
 			case netlink.LinkUpdate:
 				if u.Header.Type == unix.RTM_DELLINK {
-					// Mark for deletion. This may be undone if
-					// the ifindex is reused.
+					// Mark for deletion.
+					dc.deadLinkIndexes.Insert(d.Index)
 					deviceDeleted = true
 				} else {
+					dc.deadLinkIndexes.Delete(d.Index)
 					deviceDeleted = false
 					populateFromLink(d, u.Link)
 				}
+				deviceUpdated = true
 			}
 		}
 
 		if deviceDeleted {
-			// Remove the deleted device. The routes table will be cleaned up from the
-			// route updates.
+			// Remove the deleted device.
 			dc.params.DeviceTable.Delete(txn, d)
-		} else {
+
+			// Remove all routes for the device. For a deleted device netlink does not
+			// send complete set of route delete messages.
+			iter, _ := dc.params.RouteTable.Get(txn, tables.RouteLinkIndex.Query(d.Index))
+			for r, _, ok := iter.Next(); ok; r, _, ok = iter.Next() {
+				dc.params.RouteTable.Delete(txn, r)
+			}
+		} else if deviceUpdated {
 			// Recheck the viability of the device after the updates have been applied.
 			d.Selected, d.NotSelectedReason = dc.isSelectedDevice(d, txn)
 

pkg/datapath/linux/devices_controller_test.go
@@ -89,6 +89,21 @@ func TestDevicesController(t *testing.T) {
 		return false
 	}
 
+	orphanRoutes := func(devs []*tables.Device, routes []*tables.Route) bool {
+		indexes := map[int]bool{}
+		for _, dev := range devs {
+			indexes[dev.Index] = true
+		}
+		for _, r := range routes {
+			if !indexes[r.LinkIndex] {
+				// A route exists without a device.
+				t.Logf("orphan route: %+v", r)
+				return true
+			}
+		}
+		return false
+	}
+
 	// The test steps perform an action, wait for devices table to change
 	// and then validate the change. Since we may see intermediate states
 	// in the devices table (as there's multiple netlink updates that may
@@ -150,7 +165,8 @@ func TestDevicesController(t *testing.T) {
 			func(t *testing.T, devs []*tables.Device, routes []*tables.Route) bool {
 				return len(devs) == 1 &&
 					"dummy1" == devs[0].Name &&
-					containsAddress(devs[0], "192.168.1.1")
+					containsAddress(devs[0], "192.168.1.1") &&
+					!orphanRoutes(devs, routes)
 			},
 		},
 
@@ -248,6 +264,8 @@ func TestDevicesController(t *testing.T) {
 			// Get the new set of devices
 			for {
 				txn := db.ReadTxn()
+				allDevsIter, _ := devicesTable.All(txn)
+				allDevs := statedb.Collect(allDevsIter)
 				devs, devsInvalidated := tables.SelectedDevices(devicesTable, txn)
 
 				routesIter, routesIterInvalidated := routesTable.All(txn)
@@ -257,13 +275,18 @@ func TestDevicesController(t *testing.T) {
 					break
 				}
 
+				// Check that there are no orphan routes
+				if !orphanRoutes(allDevs, routes) {
+					break
+				}
+
 				// Wait for a changes and try again.
 				select {
 				case <-routesIterInvalidated:
 				case <-devsInvalidated:
 				case <-ctx.Done():
 					txn.WriteJSON(os.Stdout)
-					t.Fatalf("Test case %q timed out while waiting for devices. Last devices seen: %+v", step.name, devs)
+					t.Fatalf("Test case %q timed out while waiting for devices", step.name)
 				}
 			}
 

commit c86962286d64fdd1aee3018889b72bfc7abab7d7

datapath/devices: Fix selection on route changes and orphanRoutes check
The addition of the check to see if routes exists without corresponding
device to the devices controller tests caused it to stop the test too
early before the test case had passed.

As the tests were faulty they didn't catch the bug where the 'Selected'
status wasn't rechecked and thus the 'veth-with-default-route' test
passed when it shouldn't have. Fix this by always checking if selected
status updates.

Fixes: 8b553913aa ("devices: Flush routes on device delete")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/datapath/linux/devices_controller.go
@@ -398,7 +398,10 @@ func (dc *devicesController) processBatch(txn statedb.WriteTxn, batch map[int][]
 			d = d.DeepCopy()
 		}
 		deviceDeleted := false
-		deviceUpdated := false // Set to true if the batch contained an address or link update.
+
+		// Set to true if the device was modified. This is done to avoid unnecessary
+		// modifications to the device that would wake up watchers.
+		deviceUpdated := false
 
 		for _, u := range updates {
 			switch u := u.(type) {
@@ -458,6 +461,18 @@ func (dc *devicesController) processBatch(txn statedb.WriteTxn, batch map[int][]
 			}
 		}
 
+		// Recheck the viability of the device after the updates have been applied.
+		// Since route changes may cause device to be selected (e.g. veth device that
+		// has default route), always recheck viability if device is not selected.
+		if deviceUpdated || !d.Selected {
+			oldSelected := d.Selected
+			oldReason := d.NotSelectedReason
+			d.Selected, d.NotSelectedReason = dc.isSelectedDevice(d, txn)
+			if d.Selected != oldSelected || d.NotSelectedReason != oldReason {
+				deviceUpdated = true
+			}
+		}
+
 		if deviceDeleted {
 			// Remove the deleted device.
 			dc.params.DeviceTable.Delete(txn, d)
@@ -469,9 +484,6 @@ func (dc *devicesController) processBatch(txn statedb.WriteTxn, batch map[int][]
 				dc.params.RouteTable.Delete(txn, r)
 			}
 		} else if deviceUpdated {
-			// Recheck the viability of the device after the updates have been applied.
-			d.Selected, d.NotSelectedReason = dc.isSelectedDevice(d, txn)
-
 			// Create or update the device.
 			_, _, err := dc.params.DeviceTable.Insert(txn, d)
 			if err != nil {

pkg/datapath/linux/devices_controller_test.go
@@ -60,10 +60,15 @@ func devicesControllerTestSetup(t *testing.T) {
 	})
 }
 
-func containsAddress(dev *tables.Device, addrStr string) bool {
+const (
+	secondaryAddress = true
+	primaryAddress   = false
+)
+
+func containsAddress(dev *tables.Device, addrStr string, secondary bool) bool {
 	addr := netip.MustParseAddr(addrStr)
 	for _, a := range dev.Addrs {
-		if a.Addr == addr {
+		if a.Addr == addr && a.Secondary == secondary {
 			return true
 		}
 	}
@@ -154,10 +159,8 @@ func TestDevicesController(t *testing.T) {
 				return len(devs) == 2 &&
 					"dummy1" == devs[1].Name &&
 					devs[1].Selected &&
-					devs[1].Addrs[0].Addr == netip.MustParseAddr("192.168.1.1") &&
-					!devs[1].Addrs[0].Secondary &&
-					devs[1].Addrs[1].Addr == netip.MustParseAddr("192.168.1.2") &&
-					devs[1].Addrs[1].Secondary
+					containsAddress(devs[1], "192.168.1.1", primaryAddress) &&
+					containsAddress(devs[1], "192.168.1.2", secondaryAddress)
 			},
 		},
 
@@ -182,8 +185,7 @@ func TestDevicesController(t *testing.T) {
 			func(t *testing.T, devs []*tables.Device, routes []*tables.Route) bool {
 				return len(devs) == 1 &&
 					"dummy1" == devs[0].Name &&
-					containsAddress(devs[0], "192.168.1.1") &&
-					!orphanRoutes(devs, routes)
+					containsAddress(devs[0], "192.168.1.1", primaryAddress)
 			},
 		},
 
@@ -198,7 +200,7 @@ func TestDevicesController(t *testing.T) {
 					devs[0].Name == "dummy1" &&
 					devs[0].Selected &&
 					devs[1].Name == "veth0" &&
-					containsAddress(devs[1], "192.168.4.1") &&
+					containsAddress(devs[1], "192.168.4.1", primaryAddress) &&
 					devs[1].Selected
 			},
 		},
@@ -290,12 +292,9 @@ func TestDevicesController(t *testing.T) {
 				routesIter, routesIterInvalidated := routesTable.All(txn)
 				routes := statedb.Collect(routesIter)
 
-				if step.check(t, devs, routes) {
-					break
-				}
-
-				// Check that there are no orphan routes
-				if !orphanRoutes(allDevs, routes) {
+				// Stop if the test case passes and there are no orphan routes left in the
+				// route table.
+				if step.check(t, devs, routes) && !orphanRoutes(allDevs, routes) {
 					break
 				}
 