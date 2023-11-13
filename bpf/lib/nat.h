g0odi3z2j/cilium

commit 37e737a41b817a89765349a825e0ef9ea172daef

bpf/nat: introduce snat_v4_nat() and snat_v4_rev_nat functions
This split snat_v4_process() to introduce two functions: snat_v4_nat()
and snat_v4_rev_nat().  This does not change logic, it is expected to
reduce verifier complexity and improve code maintenability.

Fixes: #19660
Signed-off-by: Sahid Orentino Ferdjaoui <sahid.ferdjaoui@industrialdiscipline.com>

bpf/lib/nat.h
@@ -516,8 +516,8 @@ static __always_inline __maybe_unused int snat_v4_create_dsr(struct __ctx_buff *
 }
 
 static __always_inline __maybe_unused int
-snat_v4_process(struct __ctx_buff *ctx, enum nat_dir dir,
-		const struct ipv4_nat_target *target, bool from_endpoint)
+snat_v4_nat(struct __ctx_buff *ctx, const struct ipv4_nat_target *target,
+	    bool from_endpoint)
 {
 	struct icmphdr icmphdr __align_stack_8;
 	struct ipv4_nat_entry *state, tmp;
@@ -540,7 +540,7 @@ snat_v4_process(struct __ctx_buff *ctx, enum nat_dir dir,
 	tuple.nexthdr = ip4->protocol;
 	tuple.daddr = ip4->daddr;
 	tuple.saddr = ip4->saddr;
-	tuple.flags = dir;
+	tuple.flags = NAT_DIR_EGRESS;
 	off = ((void *)ip4 - data) + ipv4_hdrlen(ip4);
 	switch (tuple.nexthdr) {
 	case IPPROTO_TCP:
@@ -572,22 +572,95 @@ snat_v4_process(struct __ctx_buff *ctx, enum nat_dir dir,
 		return NAT_PUNT_TO_STACK;
 	};
 
-	if (snat_v4_can_skip(target, &tuple, dir, from_endpoint, icmp_echoreply))
+	if (snat_v4_can_skip(target, &tuple, NAT_DIR_EGRESS, from_endpoint, icmp_echoreply))
 		return NAT_PUNT_TO_STACK;
-	ret = snat_v4_handle_mapping(ctx, &tuple, &state, &tmp, dir, off, target);
+	ret = snat_v4_handle_mapping(ctx, &tuple, &state, &tmp, NAT_DIR_EGRESS, off, target);
 	if (ret > 0)
 		return CTX_ACT_OK;
 	if (ret < 0)
 		return ret;
 
-	return dir == NAT_DIR_EGRESS ?
-	       snat_v4_rewrite_egress(ctx, &tuple, state, off, ipv4_has_l4_header(ip4)) :
-	       snat_v4_rewrite_ingress(ctx, &tuple, state, off);
+	return snat_v4_rewrite_egress(ctx, &tuple, state, off, ipv4_has_l4_header(ip4));
+}
+
+static __always_inline __maybe_unused int
+snat_v4_rev_nat(struct __ctx_buff *ctx, const struct ipv4_nat_target *target,
+		bool from_endpoint)
+{
+	struct icmphdr icmphdr __align_stack_8;
+	struct ipv4_nat_entry *state, tmp;
+	struct ipv4_ct_tuple tuple = {};
+	void *data, *data_end;
+	struct iphdr *ip4;
+	struct {
+		__be16 sport;
+		__be16 dport;
+	} l4hdr;
+	bool icmp_echoreply = false;
+	__u64 off;
+	int ret;
+
+	build_bug_on(sizeof(struct ipv4_nat_entry) > 64);
+
+	if (!revalidate_data(ctx, &data, &data_end, &ip4))
+		return DROP_INVALID;
+
+	tuple.nexthdr = ip4->protocol;
+	tuple.daddr = ip4->daddr;
+	tuple.saddr = ip4->saddr;
+	tuple.flags = NAT_DIR_INGRESS;
+	off = ((void *)ip4 - data) + ipv4_hdrlen(ip4);
+	switch (tuple.nexthdr) {
+	case IPPROTO_TCP:
+	case IPPROTO_UDP:
+#ifdef ENABLE_SCTP
+	case IPPROTO_SCTP:
+#endif  /* ENABLE_SCTP */
+		if (ctx_load_bytes(ctx, off, &l4hdr, sizeof(l4hdr)) < 0)
+			return DROP_INVALID;
+		tuple.dport = l4hdr.dport;
+		tuple.sport = l4hdr.sport;
+		break;
+	case IPPROTO_ICMP:
+		if (ctx_load_bytes(ctx, off, &icmphdr, sizeof(icmphdr)) < 0)
+			return DROP_INVALID;
+		if (icmphdr.type != ICMP_ECHO &&
+		    icmphdr.type != ICMP_ECHOREPLY)
+			return DROP_NAT_UNSUPP_PROTO;
+		if (icmphdr.type == ICMP_ECHO) {
+			tuple.dport = 0;
+			tuple.sport = icmphdr.un.echo.id;
+		} else {
+			tuple.dport = icmphdr.un.echo.id;
+			tuple.sport = 0;
+			icmp_echoreply = true;
+		}
+		break;
+	default:
+		return NAT_PUNT_TO_STACK;
+	};
+
+	if (snat_v4_can_skip(target, &tuple, NAT_DIR_INGRESS, from_endpoint, icmp_echoreply))
+		return NAT_PUNT_TO_STACK;
+	ret = snat_v4_handle_mapping(ctx, &tuple, &state, &tmp, NAT_DIR_INGRESS, off, target);
+	if (ret > 0)
+		return CTX_ACT_OK;
+	if (ret < 0)
+		return ret;
+
+	return snat_v4_rewrite_ingress(ctx, &tuple, state, off);
 }
 #else
 static __always_inline __maybe_unused
-int snat_v4_process(struct __ctx_buff *ctx __maybe_unused,
-		    enum nat_dir dir __maybe_unused,
+int snat_v4_nat(struct __ctx_buff *ctx __maybe_unused,
+		const struct ipv4_nat_target *target __maybe_unused,
+		bool from_endpoint __maybe_unused)
+{
+	return CTX_ACT_OK;
+}
+
+static __always_inline __maybe_unused
+int snat_v4_rev_nat(struct __ctx_buff *ctx __maybe_unused,
 		    const struct ipv4_nat_target *target __maybe_unused,
 		    bool from_endpoint __maybe_unused)
 {

bpf/lib/nodeport.h
@@ -1277,8 +1277,7 @@ static __always_inline int nodeport_nat_ipv4_fwd(struct __ctx_buff *ctx)
 	int ret = CTX_ACT_OK;
 
 	if (snat_v4_needed(ctx, &target.addr, &from_endpoint))
-		ret = snat_v4_process(ctx, NAT_DIR_EGRESS, &target,
-				      from_endpoint);
+		ret = snat_v4_nat(ctx, &target, from_endpoint);
 	if (ret == NAT_PUNT_TO_STACK)
 		ret = CTX_ACT_OK;
 
@@ -1646,7 +1645,7 @@ int tail_nodeport_nat_ingress_ipv4(struct __ctx_buff *ctx)
 	 */
 	target.addr = IPV4_DIRECT_ROUTING;
 
-	ret = snat_v4_process(ctx, NAT_DIR_INGRESS, &target, false);
+	ret = snat_v4_rev_nat(ctx, &target, false);
 	if (IS_ERR(ret)) {
 		/* In case of no mapping, recircle back to main path. SNAT is very
 		 * expensive in terms of instructions (since we don't have BPF to
@@ -1738,7 +1737,7 @@ int tail_nodeport_nat_egress_ipv4(struct __ctx_buff *ctx)
 		use_tunnel = true;
 	}
 #endif
-	ret = snat_v4_process(ctx, NAT_DIR_EGRESS, &target, false);
+	ret = snat_v4_nat(ctx, &target, false);
 	if (IS_ERR(ret) && ret != NAT_PUNT_TO_STACK)
 		goto drop_err;
 

commit 7a81f9ea3027c3c7ed3b9479f716b7468fa3d911

bpf: Handle fragments in SNAT flows
The code that extracts ports in snat_v4_nat and snat_v4_rev_nat doesn't
take into account that the packet may be a second or further fragment.
In this case, garbage will be read instead of the real port numbers. Fix
this by using the existing ipv4_ct_extract_l4_ports that takes into
account fragmentation.

This change makes the behavior of the switch inside the aforementioned
functions closer to ct_extract_ports4.

Consider fragments in snat_v4_rewrite_ingress, avoid rewriting ports if
it's not the first fragment.

Fixes: 37e737a41b81 ("bpf/nat: introduce snat_v4_nat() and snat_v4_rev_nat functions")
Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

bpf/lib/nat.h
@@ -482,7 +482,7 @@ snat_v4_icmp_rewrite_egress_embedded(struct __ctx_buff *ctx,
 static __always_inline int snat_v4_rewrite_ingress(struct __ctx_buff *ctx,
 						   struct ipv4_ct_tuple *tuple,
 						   struct ipv4_nat_entry *state,
-						   __u32 off)
+						   __u32 off, bool has_l4_header)
 {
 	int ret, flags = BPF_F_PSEUDO_HDR;
 	struct csum_offset csum = {};
@@ -492,43 +492,47 @@ static __always_inline int snat_v4_rewrite_ingress(struct __ctx_buff *ctx,
 	    state->to_dport == tuple->dport)
 		return 0;
 	sum = csum_diff(&tuple->daddr, 4, &state->to_daddr, 4, 0);
-	csum_l4_offset_and_flags(tuple->nexthdr, &csum);
-	if (state->to_dport != tuple->dport) {
-		switch (tuple->nexthdr) {
-		case IPPROTO_TCP:
-		case IPPROTO_UDP:
-			ret = l4_modify_port(ctx, off,
-					     offsetof(struct tcphdr, dest),
-					     &csum, state->to_dport,
-					     tuple->dport);
-			if (ret < 0)
-				return ret;
-			break;
-#ifdef ENABLE_SCTP
-		case IPPROTO_SCTP:
-			return DROP_CSUM_L4;
-#endif  /* ENABLE_SCTP */
-		case IPPROTO_ICMP: {
-			__u8 type = 0;
+	if (has_l4_header) {
+		csum_l4_offset_and_flags(tuple->nexthdr, &csum);
 
-			if (ctx_load_bytes(ctx, off +
-					   offsetof(struct icmphdr, type),
-					   &type, 1) < 0)
-				return DROP_INVALID;
-			if (type == ICMP_ECHOREPLY) {
-				if (ctx_store_bytes(ctx, off +
-						    offsetof(struct icmphdr, un.echo.id),
-						    &state->to_dport,
-						    sizeof(state->to_dport), 0) < 0)
-					return DROP_WRITE_ERROR;
-				if (l4_csum_replace(ctx, off + offsetof(struct icmphdr, checksum),
-						    tuple->dport,
-						    state->to_dport,
-						    sizeof(tuple->dport)) < 0)
-					return DROP_CSUM_L4;
-			}
-			break;
-		}}
+		if (state->to_dport != tuple->dport) {
+			switch (tuple->nexthdr) {
+			case IPPROTO_TCP:
+			case IPPROTO_UDP:
+				ret = l4_modify_port(ctx, off,
+						     offsetof(struct tcphdr, dest),
+						     &csum, state->to_dport,
+						     tuple->dport);
+				if (ret < 0)
+					return ret;
+				break;
+#ifdef ENABLE_SCTP
+			case IPPROTO_SCTP:
+				return DROP_CSUM_L4;
+#endif  /* ENABLE_SCTP */
+			case IPPROTO_ICMP: {
+				__u8 type = 0;
+
+				if (ctx_load_bytes(ctx, off +
+						   offsetof(struct icmphdr, type),
+						   &type, 1) < 0)
+					return DROP_INVALID;
+				if (type == ICMP_ECHOREPLY) {
+					if (ctx_store_bytes(ctx, off +
+							    offsetof(struct icmphdr, un.echo.id),
+							    &state->to_dport,
+							    sizeof(state->to_dport), 0) < 0)
+						return DROP_WRITE_ERROR;
+					if (l4_csum_replace(ctx, off +
+							    offsetof(struct icmphdr, checksum),
+							    tuple->dport,
+							    state->to_dport,
+							    sizeof(tuple->dport)) < 0)
+						return DROP_CSUM_L4;
+				}
+				break;
+			}}
+		}
 	}
 	if (ctx_store_bytes(ctx, ETH_HLEN + offsetof(struct iphdr, daddr),
 			    &state->to_daddr, 4, 0) < 0)
@@ -867,10 +871,6 @@ snat_v4_nat(struct __ctx_buff *ctx, struct ipv4_ct_tuple *tuple, int off,
 	    struct trace_ctx *trace, __s8 *ext_err)
 {
 	struct icmphdr icmphdr __align_stack_8;
-	struct {
-		__be16 sport;
-		__be16 dport;
-	} l4hdr;
 	__u16 port_off;
 
 	build_bug_on(sizeof(struct ipv4_nat_entry) > 64);
@@ -881,11 +881,11 @@ snat_v4_nat(struct __ctx_buff *ctx, struct ipv4_ct_tuple *tuple, int off,
 #ifdef ENABLE_SCTP
 	case IPPROTO_SCTP:
 #endif  /* ENABLE_SCTP */
-		if (ctx_load_bytes(ctx, off, &l4hdr, sizeof(l4hdr)) < 0)
+		if (ipv4_load_l4_ports(ctx, NULL, off, CT_EGRESS,
+				       &tuple->dport, &has_l4_header) < 0)
 			return DROP_INVALID;
 
-		tuple->dport = l4hdr.dport;
-		tuple->sport = l4hdr.sport;
+		ipv4_ct_tuple_swap_ports(tuple);
 		port_off = TCP_SPORT_OFF;
 		break;
 	case IPPROTO_ICMP:
@@ -1000,10 +1000,6 @@ snat_v4_rev_nat(struct __ctx_buff *ctx, const struct ipv4_nat_target *target,
 	struct ipv4_ct_tuple tuple = {};
 	void *data, *data_end;
 	struct iphdr *ip4;
-	struct {
-		__be16 sport;
-		__be16 dport;
-	} l4hdr;
 	bool has_l4_header = true;
 	__u64 off, inner_l3_off;
 	int ret;
@@ -1022,10 +1018,10 @@ snat_v4_rev_nat(struct __ctx_buff *ctx, const struct ipv4_nat_target *target,
 #ifdef ENABLE_SCTP
 	case IPPROTO_SCTP:
 #endif  /* ENABLE_SCTP */
-		if (ctx_load_bytes(ctx, off, &l4hdr, sizeof(l4hdr)) < 0)
+		if (ipv4_load_l4_ports(ctx, ip4, off, CT_INGRESS,
+				       &tuple.dport, &has_l4_header) < 0)
 			return DROP_INVALID;
-		tuple.dport = l4hdr.dport;
-		tuple.sport = l4hdr.sport;
+		ipv4_ct_tuple_swap_ports(&tuple);
 		break;
 	case IPPROTO_ICMP:
 		if (ctx_load_bytes(ctx, off, &icmphdr, sizeof(icmphdr)) < 0)
@@ -1047,6 +1043,7 @@ snat_v4_rev_nat(struct __ctx_buff *ctx, const struct ipv4_nat_target *target,
 			if (IS_ERR(ret))
 				return ret;
 
+			has_l4_header = true;
 			goto rewrite;
 		default:
 			return NAT_PUNT_TO_STACK;
@@ -1064,7 +1061,7 @@ snat_v4_rev_nat(struct __ctx_buff *ctx, const struct ipv4_nat_target *target,
 		return ret;
 
 rewrite:
-	return snat_v4_rewrite_ingress(ctx, &tuple, state, off);
+	return snat_v4_rewrite_ingress(ctx, &tuple, state, off, has_l4_header);
 }
 #else
 static __always_inline __maybe_unused