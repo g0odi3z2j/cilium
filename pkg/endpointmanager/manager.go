g0odi3z2j/cilium

commit ddf543badf249ed56d30c2e3fb514ca0b5512786

endpointmanager: Use netip.Addr instead of net.IP in LookupIP
It's preparing to migrate Hubble getters (calling LookupIP) from net.IP to
netip.Addr.

Signed-off-by: Anna Kapuscinska <anna@isovalent.com>

pkg/endpointmanager/manager.go
@@ -7,7 +7,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net"
 	"net/netip"
 	"sync"
 	"time"
@@ -316,13 +315,13 @@ func (mgr *endpointManager) LookupIPv6(ipv6 string) *endpoint.Endpoint {
 }
 
 // LookupIP looks up endpoint by IP address
-func (mgr *endpointManager) LookupIP(ip net.IP) (ep *endpoint.Endpoint) {
-	addr := ip.String()
+func (mgr *endpointManager) LookupIP(ip netip.Addr) (ep *endpoint.Endpoint) {
+	ipStr := ip.String()
 	mgr.mutex.RLock()
-	if ip.To4() != nil {
-		ep = mgr.lookupIPv4(addr)
+	if ip.Is4() {
+		ep = mgr.lookupIPv4(ipStr)
 	} else {
-		ep = mgr.lookupIPv6(addr)
+		ep = mgr.lookupIPv6(ipStr)
 	}
 	mgr.mutex.RUnlock()
 	return ep

daemon/cmd/fqdn.go
@@ -34,7 +34,7 @@ import (
 	"github.com/cilium/cilium/pkg/fqdn/re"
 	"github.com/cilium/cilium/pkg/identity"
 	secIDCache "github.com/cilium/cilium/pkg/identity/cache"
-	"github.com/cilium/cilium/pkg/ip"
+	ippkg "github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/metrics"
 	"github.com/cilium/cilium/pkg/option"
@@ -398,7 +398,11 @@ func (d *Daemon) updateSelectors(ctx context.Context, selectorWithIPsToUpdate ma
 
 // lookupEPByIP returns the endpoint that this IP belongs to
 func (d *Daemon) lookupEPByIP(endpointIP net.IP) (endpoint *endpoint.Endpoint, err error) {
-	e := d.endpointManager.LookupIP(endpointIP)
+	endpointAddr, ok := ippkg.AddrFromIP(endpointIP)
+	if !ok {
+		return nil, fmt.Errorf("invalid IP %s for endpoint lookup", endpointIP)
+	}
+	e := d.endpointManager.LookupIP(endpointAddr)
 	if e == nil {
 		return nil, fmt.Errorf("Cannot find endpoint with IP %s", endpointIP.String())
 	}
@@ -581,7 +585,7 @@ func (d *Daemon) notifyOnDNSMsg(lookupTime time.Time, ep *endpoint.Endpoint, epI
 		// doesn't happen in the case, we play it safe and don't purge the zombie
 		// in case of races.
 		log.WithField(logfields.EndpointID, ep.ID).Debug("Recording DNS lookup in endpoint specific cache")
-		if updated := ep.DNSHistory.Update(lookupTime, qname, ip.MustAddrsFromIPs(responseIPs), int(TTL)); updated {
+		if updated := ep.DNSHistory.Update(lookupTime, qname, ippkg.MustAddrsFromIPs(responseIPs), int(TTL)); updated {
 			ep.DNSZombies.ForceExpireByNameIP(lookupTime, qname, responseIPs...)
 			ep.SyncEndpointHeaderFile()
 		}

daemon/cmd/hubble.go
@@ -312,7 +312,11 @@ func (d *Daemon) GetIdentity(securityIdentity uint32) (*identity.Identity, error
 //
 //   - EndpointGetter: https://github.com/cilium/hubble/blob/04ab72591faca62a305ce0715108876167182e04/pkg/parser/getters/getters.go#L34
 func (d *Daemon) GetEndpointInfo(ip net.IP) (endpoint v1.EndpointInfo, ok bool) {
-	ep := d.endpointManager.LookupIP(ip)
+	addr, ok := ippkg.AddrFromIP(ip)
+	if !ok {
+		return nil, false
+	}
+	ep := d.endpointManager.LookupIP(addr)
 	if ep == nil {
 		return nil, false
 	}

pkg/endpointmanager/cell.go
@@ -5,7 +5,7 @@ package endpointmanager
 
 import (
 	"context"
-	"net"
+	"net/netip"
 	"sync"
 	"time"
 
@@ -51,7 +51,7 @@ type EndpointsLookup interface {
 	LookupIPv6(ipv6 string) *endpoint.Endpoint
 
 	// LookupIP looks up endpoint by IP address
-	LookupIP(ip net.IP) (ep *endpoint.Endpoint)
+	LookupIP(ip netip.Addr) (ep *endpoint.Endpoint)
 
 	// LookupPodName looks up endpoint by namespace + pod name, e.g. "prod/pod-0"
 	LookupPodName(name string) *endpoint.Endpoint

pkg/proxy/epinfo.go
@@ -6,10 +6,12 @@ package proxy
 import (
 	"context"
 	"net"
+	"net/netip"
 
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
+	ippkg "github.com/cilium/cilium/pkg/ip"
 	"github.com/cilium/cilium/pkg/proxy/accesslog"
 )
 
@@ -24,7 +26,7 @@ var (
 
 // EndpointLookup is any type which maps from IP to the endpoint owning that IP.
 type EndpointLookup interface {
-	LookupIP(ip net.IP) (ep *endpoint.Endpoint)
+	LookupIP(ip netip.Addr) (ep *endpoint.Endpoint)
 }
 
 // endpointInfoRegistry provides a default implementation of the
@@ -49,7 +51,8 @@ func (r *endpointInfoRegistry) FillEndpointInfo(info *accesslog.EndpointInfo, ip
 		}
 
 		// Get (local) endpoint identifier to be reported by cilium monitor
-		ep = endpointManager.LookupIP(ip)
+		addr, _ := ippkg.AddrFromIP(ip)
+		ep = endpointManager.LookupIP(addr)
 		if ep != nil {
 			info.ID = ep.GetID()
 		}

commit 947dedadd5b31ae72cb7a4cf71b972be9ae07e02

endpointmanager: unmap ip for lookup
In case of an IPv4-mapped IPv6 address we'd lookup the address as an
IPv4 address with a ::ffff: prefix, leading to endpoint lookup errors
such as

    cannot find endpoint with IP ::ffff:10.0.1.19

Fix this by explicitly unmapping the address before lookup which leads
to 10.0.1.19 to be looked up (and found) in the above case.

Fixes: ddf543badf24 ("endpointmanager: Use netip.Addr instead of net.IP in LookupIP")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

pkg/endpointmanager/manager.go
@@ -325,7 +325,7 @@ func (mgr *endpointManager) LookupIPv6(ipv6 string) *endpoint.Endpoint {
 
 // LookupIP looks up endpoint by IP address
 func (mgr *endpointManager) LookupIP(ip netip.Addr) (ep *endpoint.Endpoint) {
-	ipStr := ip.String()
+	ipStr := ip.Unmap().String()
 	mgr.mutex.RLock()
 	if ip.Is4() {
 		ep = mgr.lookupIPv4(ipStr)