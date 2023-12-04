g0odi3z2j/cilium

commit 4d6f5c7c4acc32bb1676e6e3ddd44e3a7f0c670c

cilium-health: Remove unnecessary goroutine
Prior to this commit, we had an additional goroutine that wrapped the
pinger/prober that waited until they both complete to print a "finished"
message. This goroutine was in charge of building the 'prober' object,
which would be accessed from the main thread to halt the thread via
Stop(). If the logic actually followed to loop around and recreate the
prober, then this could race with server shutdown from the main thread
and have undefined behaviour. Removing the goroutine removes this data
race and simplifies the code.

Signed-off-by: Joe Stringer <joe@covalent.io>

pkg/health/server/server.go
@@ -185,39 +185,24 @@ func (s *Server) FetchStatusResponse() (*healthModels.HealthStatusResponse, erro
 // Blocks indefinitely, or returns any errors that occur hosting the Unix
 // socket API server.
 func (s *Server) runActiveServices() error {
-	var prober *prober
-
 	// Run it once at the start so we get some initial status
 	s.FetchStatusResponse()
 
-	s.waitgroup.Add(1)
-	go func() {
-		defer s.waitgroup.Done()
-
-		nodes, _ := s.getNodes()
-		prober = newProber(s, nodes)
-		prober.MaxRTT = s.ProbeInterval
-		prober.OnIdle = func() {
-			// Fetch results and update set of nodes to probe every
-			// ProbeInterval
-			s.updateCluster(prober.getResults())
-			if nodes, err := s.getNodes(); err == nil {
-				prober.setNodes(nodes)
-			}
+	nodes, _ := s.getNodes()
+	prober := newProber(s, nodes)
+	prober.MaxRTT = s.ProbeInterval
+	prober.OnIdle = func() {
+		// Fetch results and update set of nodes to probe every
+		// ProbeInterval
+		s.updateCluster(prober.getResults())
+		if nodes, err := s.getNodes(); err == nil {
+			prober.setNodes(nodes)
 		}
-		prober.RunLoop()
-
-		if <-prober.Done() {
-			if err := prober.Err(); err != nil {
-				log.WithError(err).Debug("Received prober finish")
-			}
-		}
-	}()
+	}
+	prober.RunLoop()
 
 	err := s.Server.Serve()
-	if prober != nil {
-		prober.Stop()
-	}
+	prober.Stop()
 
 	return err
 }

commit aef506a85e972b246e472f7d2d5c7a08207df0ee

health/server: Fix stale references to old nodes during health probe
Given the order of operations in prober.OnIdle, it is possible for the
health probe to have a stale references to a deleted nodes. When that
occurs, node connectivity metrics which were previously deleted [1]
would be brought back, causing confusion. If users defined alerts for
node connectivity health checks metrics (see example below), then this
would erroneously trigger because the old nodes would appear in the
metric labels as a failing health check.

Example given deletion of "kind-worker2" node:

```
cilium_node_connectivity_status                          source_cluster="kind-kind" source_node_name="kind-worker" target_cluster="kind-kind" target_node_name="kind-control-plane" target_nod
e_type="remote_intra_cluster" type="endpoint"                                                                        1.000000
cilium_node_connectivity_status                          source_cluster="kind-kind" source_node_name="kind-worker" target_cluster="kind-kind" target_node_name="kind-control-plane" target_nod
e_type="remote_intra_cluster" type="node"                                                                            1.000000
cilium_node_connectivity_status                          source_cluster="kind-kind" source_node_name="kind-worker" target_cluster="kind-kind" target_node_name="kind-worker" target_node_type=
"local_node" type="endpoint"                                                                                         1.000000
cilium_node_connectivity_status                          source_cluster="kind-kind" source_node_name="kind-worker" target_cluster="kind-kind" target_node_name="kind-worker" target_node_type=
"local_node" type="node"                                                                                             1.000000

cilium_node_connectivity_status                          source_cluster="kind-kind" source_node_name="kind-worker" target_cluster="kind-kind" target_node_name="kind-worker2" target_node_type
="remote_intra_cluster" type="endpoint"                                                                              0.000000
```

Fixes: 4d6f5c7c4a ("cilium-health: Remove unnecessary goroutine")

pkg/health/server/server.go
@@ -338,14 +338,14 @@ func (s *Server) runActiveServices() error {
 	prober := newProber(s, nodesAdded)
 	prober.MaxRTT = s.ProbeInterval
 	prober.OnIdle = func() {
-		// Fetch results and update set of nodes to probe every
-		// ProbeInterval
-		s.updateCluster(prober.getResults())
+		// Update set of nodes to probe every ProbeInterval and then fetch
+		// results
 		if nodesAdded, nodesRemoved, err := s.getNodes(); err != nil {
 			log.WithError(err).Error("unable to get cluster nodes")
 		} else {
 			prober.setNodes(nodesAdded, nodesRemoved)
 		}
+		s.updateCluster(prober.getResults())
 	}
 	prober.RunLoop()
 	defer prober.Stop()