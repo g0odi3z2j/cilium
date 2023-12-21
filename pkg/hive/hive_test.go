g0odi3z2j/cilium

commit b5943c196b8e09d7e7c22461056663b6a72b2638

hive: Reimplement on top of dig
This reimplements hive directly on top of the uber/dig library. This
simplifies dealing with lifecycles and logging, and most importantly
allows implementing the configuration flag registration in a
straightforward way with an invoke.

The usage is also simplified as now there's only one library to
learn. The API is now very similar to uber/fx, but with the extra
Config() option.

To simplify use of hive.New the flags are now registered afterwards
with RegisterFlags(), and hive creates its own viper instance that
can be retrieved with Viper().

See pkg/hive/example/main.go for summary of the API changes.

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hive/cell/config.go
@@ -1,0 +1,141 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cell
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/pflag"
+	"go.uber.org/dig"
+)
+
+// Config constructs a new config cell.
+//
+// The configuration struct `T` needs to implement the Flags method that
+// registers the flags. The structure is populated and provided via dependency
+// injection by Hive.Run(). The underlying mechanism for populating the struct
+// is viper's Unmarshal().
+func Config[Cfg Flagger](def Cfg) Cell {
+	c := &config[Cfg]{defaultConfig: def, flags: pflag.NewFlagSet("", pflag.ContinueOnError)}
+	def.Flags(c.flags)
+	return c
+}
+
+// Flagger is implemented by configuration structs to provide configuration
+// for a cell.
+type Flagger interface {
+	// Flags registers the configuration options as command-line flags.
+	//
+	// By convention a flag name matches the field name
+	// if they're the same under case-insensitive comparison when dashes are
+	// removed. E.g. "my-config-flag" matches field "MyConfigFlag". The
+	// correspondence to the flag can be also specified with the mapstructure
+	// tag: MyConfigFlag `mapstructure:"my-config-flag"`.
+	//
+	// Exported fields that are not found from the viper settings will cause
+	// hive.Run() to fail. Unexported fields are ignored.
+	//
+	// See https://pkg.go.dev/github.com/mitchellh/mapstructure for more info.
+	Flags(*pflag.FlagSet)
+}
+
+type AllSettings map[string]any
+
+// config is a cell for configuration. It registers the config's command-line
+// flags and provides the parsed config to the hive.
+type config[Cfg Flagger] struct {
+	defaultConfig Cfg
+	flags         *pflag.FlagSet
+}
+
+func (c *config[Cfg]) provideConfig(settings AllSettings) (Cfg, error) {
+	target := c.defaultConfig
+	decoder, err := mapstructure.NewDecoder(decoderConfig(&target))
+	if err != nil {
+		return target, fmt.Errorf("failed to create config decoder: %w", err)
+	}
+
+	// As input, only consider the declared flags.
+	input := make(map[string]any)
+
+	c.flags.VisitAll(func(f *pflag.Flag) {
+		if v, ok := settings[f.Name]; ok {
+			input[f.Name] = v
+		} else {
+			err = fmt.Errorf("internal error: %s not found from settings", f.Name)
+		}
+	})
+	if err != nil {
+		return target, err
+	}
+	if err := decoder.Decode(input); err != nil {
+		return target, fmt.Errorf("failed to unmarshal config struct %T: %w.\n"+
+			"Hint: field 'FooBar' matches flag 'foo-bar', or use tag `mapstructure:\"flag-name\"` to match field with flag",
+			target, err)
+	}
+	return target, nil
+}
+
+func decoderConfig(target any) *mapstructure.DecoderConfig {
+	return &mapstructure.DecoderConfig{
+		Metadata:         nil,
+		Result:           target,
+		WeaklyTypedInput: true,
+		DecodeHook: mapstructure.ComposeDecodeHookFunc(
+			mapstructure.StringToTimeDurationHookFunc(),
+			mapstructure.StringToSliceHookFunc(","),
+		),
+		ZeroFields: true,
+		// Error out if the config struct has fields that are
+		// not found from input.
+		ErrorUnset: true,
+		// Error out also if settings from input are not used.
+		ErrorUnused: true,
+		// Match field FooBarBaz with "foo-bar-baz" by removing
+		// the dashes from the flag.
+		MatchName: func(mapKey, fieldName string) bool {
+			return strings.EqualFold(
+				strings.ReplaceAll(mapKey, "-", ""),
+				fieldName)
+		},
+	}
+}
+
+func (c *config[Cfg]) Apply(cont container) error {
+	// Register the flags to the global set of all flags.
+	err := cont.Invoke(
+		func(allFlags *pflag.FlagSet) {
+			allFlags.AddFlagSet(c.flags)
+		})
+	if err != nil {
+		return err
+	}
+	// And provide the constructor for the config.
+	return cont.Provide(c.provideConfig, dig.Export(true))
+}
+
+type configInfoEntry struct {
+	nameAndType string
+	defValue    string
+}
+
+func (c *config[Cfg]) Info() Info {
+	n := NewInfoNode(fmt.Sprintf("⚙️ %T", c.defaultConfig))
+
+	entries := []configInfoEntry{}
+	maxWidth := 0
+	c.flags.VisitAll(func(f *pflag.Flag) {
+		nameAndType := fmt.Sprintf("%s[%s]", f.Name, f.Value.Type())
+		entries = append(entries, configInfoEntry{nameAndType, f.DefValue})
+		if len(nameAndType) > maxWidth {
+			maxWidth = len(nameAndType)
+		}
+	})
+	for _, entry := range entries {
+		n.AddLeaf(fmt.Sprintf("• %-*s = %s", maxWidth, entry.nameAndType, entry.defValue))
+	}
+	return n
+}

pkg/hive/hive_test.go
@@ -1,101 +1,233 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium
 
-package hive
+package hive_test
 
 import (
-	"strings"
+	"context"
+	"errors"
 	"testing"
 
 	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
-	"go.uber.org/fx"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
 )
 
 type Config struct {
-	Hello string
+	Foo string
+	Bar int
 }
 
-func (Config) CellFlags(flags *pflag.FlagSet) {
-	flags.String("hello", "hello world", "sets the greeting")
+func (Config) Flags(flags *pflag.FlagSet) {
+	flags.String("foo", "hello world", "sets the greeting")
+	flags.Int("bar", 123, "bar")
 }
 
-func TestHive(t *testing.T) {
-	flags := pflag.NewFlagSet("", pflag.ContinueOnError)
-	viper := viper.New()
-
+func TestHiveGoodConfig(t *testing.T) {
 	var cfg Config
-	cell := NewCellWithConfig[Config](
-		"test-cell",
-		fx.Populate(&cfg),
+	testCell := cell.Module(
+		"test",
+		cell.Config(Config{}),
+		cell.Invoke(func(c Config) {
+			cfg = c
+		}),
 	)
-	var receivedConfig bool
 
-	hive := New(viper, flags,
-		cell,
-		OnStart(func(c Config) error {
-			receivedConfig = true
-			return nil
-		}))
+	hive := hive.New(testCell)
 
-	flags.Set("hello", "test")
+	flags := pflag.NewFlagSet("", pflag.ContinueOnError)
+	hive.RegisterFlags(flags)
 
-	// Test with config coming from flags.
-	app, err := hive.TestApp(t)
-	if err != nil {
-		t.Fatalf("TestApp(): %s", err)
-	}
-	app.RequireStart().RequireStop()
-	if cfg.Hello != "test" {
-		t.Fatalf("Config not set correctly, expected 'test', got %v", cfg)
-	}
+	// Test the two ways of setting it
+	flags.Set("foo", "test")
+	hive.Viper().Set("bar", 13)
 
-	if !receivedConfig {
-		t.Fatal("OnStart hook not called")
-	}
+	err := hive.Start(context.TODO())
+	assert.NoError(t, err, "expected Start to succeed")
 
-	// Test with config override
-	app, err = hive.TestApp(t, Config{Hello: "override"})
-	if err != nil {
-		t.Fatalf("TestApp(): %s", err)
-	}
-	app.RequireStart().RequireStop()
-	if cfg.Hello != "override" {
-		t.Fatalf("Config not set correctly, expected 'override', got %v", cfg)
-	}
+	err = hive.Stop(context.TODO())
+	assert.NoError(t, err, "expected Stop to succeed")
 
+	assert.Equal(t, cfg.Foo, "test", "Config.Foo not set correctly")
+	assert.Equal(t, cfg.Bar, 13, "Config.Bar not set correctly")
 }
 
-// BadConfig has a field that matches no flags, and CellFlags
+// BadConfig has a field that matches no flags, and Flags
 // declares a flag that matches no field.
 type BadConfig struct {
 	Bar string
 }
 
-func (BadConfig) CellFlags(flags *pflag.FlagSet) {
+func (BadConfig) Flags(flags *pflag.FlagSet) {
 	flags.String("foo", "foobar", "foo")
 }
 
 func TestHiveBadConfig(t *testing.T) {
-	flags := pflag.NewFlagSet("", pflag.ContinueOnError)
-	viper := viper.New()
-
-	var cfg BadConfig
-	cell := NewCellWithConfig[BadConfig](
-		"test-cell",
-		fx.Populate(&cfg),
+	testCell := cell.Module(
+		"test",
+		cell.Config(BadConfig{}),
+		cell.Invoke(func(c BadConfig) {}),
 	)
 
-	hive := New(viper, flags, cell)
-	_, err := hive.TestApp(t)
-	if err == nil {
-		t.Fatal("Expected TestApp() to fail")
+	hive := hive.New(testCell)
+
+	err := hive.Start(context.TODO())
+	assert.ErrorContains(t, err, "has invalid keys: foo", "expected 'invalid keys' error")
+	assert.ErrorContains(t, err, "has unset fields: Bar", "expected 'unset fields' error")
+}
+
+type SomeObject struct {
+	X int
+}
+
+func TestProvideInvoke(t *testing.T) {
+	invoked := false
+
+	testCell := cell.Module(
+		"test",
+		cell.Provide(func() *SomeObject { return &SomeObject{10} }),
+		cell.Invoke(func(*SomeObject) { invoked = true }),
+	)
+
+	err := hive.New(
+		testCell,
+		shutdownOnStartCell,
+	).Run()
+	assert.NoError(t, err, "expected Run to succeed")
+
+	assert.True(t, invoked, "expected invoke to be called, but it was not")
+}
+
+func TestProvidePrivate(t *testing.T) {
+	invoked := false
+
+	testCell := cell.Module(
+		"test",
+		cell.ProvidePrivate(func() *SomeObject { return &SomeObject{10} }),
+		cell.Invoke(func(*SomeObject) { invoked = true }),
+	)
+
+	// Test happy path.
+	err := hive.New(
+		testCell,
+		shutdownOnStartCell,
+	).Run()
+	assert.NoError(t, err, "expected Start to succeed")
+
+	if !invoked {
+		t.Fatal("expected invoke to be called, but it was not")
 	}
 
-	if !strings.Contains(err.Error(), "has invalid keys: foo") {
-		t.Fatalf("Expected 'invalid keys' error, got: %s", err)
-	}
-	if !strings.Contains(err.Error(), "has unset fields: Bar") {
-		t.Fatalf("Expected 'unset fields' error, got: %s", err)
-	}
+	// Now test that we can't access it from root scope.
+	h := hive.New(
+		testCell,
+		cell.Invoke(func(*SomeObject) {}),
+		shutdownOnStartCell,
+	)
+	err = h.Start(context.TODO())
+	assert.ErrorContains(t, err, "missing type: *hive_test.SomeObject", "expected Start to fail to find *SomeObject")
 }
+
+func TestDecorate(t *testing.T) {
+	invoked := false
+
+	testCell := cell.Decorate(
+		func(o *SomeObject) *SomeObject {
+			return &SomeObject{X: o.X + 1}
+		},
+		cell.Invoke(
+			func(o *SomeObject) error {
+				if o.X != 2 {
+					return errors.New("X != 2")
+				}
+				invoked = true
+				return nil
+			}),
+	)
+
+	hive := hive.New(
+		cell.Provide(func() *SomeObject { return &SomeObject{1} }),
+
+		// Here *SomeObject is not decorated.
+		cell.Invoke(func(o *SomeObject) error {
+			if o.X != 1 {
+				return errors.New("X != 1")
+			}
+			return nil
+		}),
+
+		testCell,
+
+		shutdownOnStartCell,
+	)
+
+	assert.NoError(t, hive.Run(), "expected Run() to succeed")
+	assert.True(t, invoked, "expected decorated invoke function to be called")
+}
+
+func TestShutdown(t *testing.T) {
+	//
+	// Happy paths without a shutdown error:
+	//
+
+	// Test from a start hook
+	h := hive.New(
+		cell.Invoke(func(lc hive.Lifecycle, shutdowner hive.Shutdowner) {
+			lc.Append(hive.Hook{
+				OnStart: func(context.Context) error {
+					shutdowner.Shutdown()
+					return nil
+				}})
+		}),
+	)
+	assert.NoError(t, h.Run(), "expected Run() to succeed")
+
+	// Test from a goroutine forked from start hook
+	h = hive.New(
+		cell.Invoke(func(lc hive.Lifecycle, shutdowner hive.Shutdowner) {
+			lc.Append(hive.Hook{
+				OnStart: func(context.Context) error {
+					go shutdowner.Shutdown()
+					return nil
+				}})
+		}),
+	)
+	assert.NoError(t, h.Run(), "expected Run() to succeed")
+
+	// Test from an invoke. Shouldn't really be used, but should still work.
+	h = hive.New(
+		cell.Invoke(func(lc hive.Lifecycle, shutdowner hive.Shutdowner) {
+			shutdowner.Shutdown()
+		}),
+	)
+	assert.NoError(t, h.Run(), "expected Run() to succeed")
+
+	//
+	// Unhappy paths that fatal with an error:
+	//
+
+	shutdownErr := errors.New("shutdown error")
+
+	// Test from a start hook
+	h = hive.New(
+		cell.Invoke(func(lc hive.Lifecycle, shutdowner hive.Shutdowner) {
+			lc.Append(hive.Hook{
+				OnStart: func(context.Context) error {
+					shutdowner.Shutdown(hive.ShutdownWithError(shutdownErr))
+					return nil
+				}})
+		}),
+	)
+	assert.ErrorIs(t, h.Run(), shutdownErr, "expected Run() to fail with shutdownErr")
+
+}
+
+var shutdownOnStartCell = cell.Invoke(func(lc hive.Lifecycle, shutdowner hive.Shutdowner) {
+	lc.Append(hive.Hook{
+		OnStart: func(context.Context) error {
+			shutdowner.Shutdown()
+			return nil
+		}})
+})

pkg/k8s/client/config.go
@@ -30,13 +30,22 @@ type Config struct {
 	EnableK8sAPIDiscovery bool
 }
 
-func (cfg Config) CellFlags(flags *pflag.FlagSet) {
-	flags.String(option.K8sAPIServer, "", "Kubernetes API server URL")
-	flags.String(option.K8sKubeConfigPath, "", "Absolute path of the kubernetes kubeconfig file")
-	flags.Float32(option.K8sClientQPSLimit, defaults.K8sClientQPSLimit, "Queries per second limit for the K8s client")
-	flags.Int(option.K8sClientBurst, defaults.K8sClientBurst, "Burst value allowed for the K8s client")
-	flags.Duration(option.K8sHeartbeatTimeout, 30*time.Second, "Configures the timeout for api-server heartbeat, set to 0 to disable")
-	flags.Bool(option.K8sEnableAPIDiscovery, defaults.K8sEnableAPIDiscovery, "Enable discovery of Kubernetes API groups and resources with the discovery API")
+var defaultConfig = Config{
+	K8sAPIServer:          "",
+	K8sKubeConfigPath:     "",
+	K8sClientQPS:          defaults.K8sClientQPSLimit,
+	K8sClientBurst:        defaults.K8sClientBurst,
+	K8sHeartbeatTimeout:   30 * time.Second,
+	EnableK8sAPIDiscovery: defaults.K8sEnableAPIDiscovery,
+}
+
+func (def Config) Flags(flags *pflag.FlagSet) {
+	flags.String(option.K8sAPIServer, def.K8sAPIServer, "Kubernetes API server URL")
+	flags.String(option.K8sKubeConfigPath, def.K8sKubeConfigPath, "Absolute path of the kubernetes kubeconfig file")
+	flags.Float32(option.K8sClientQPSLimit, def.K8sClientQPS, "Queries per second limit for the K8s client")
+	flags.Int(option.K8sClientBurst, def.K8sClientBurst, "Burst value allowed for the K8s client")
+	flags.Duration(option.K8sHeartbeatTimeout, def.K8sHeartbeatTimeout, "Configures the timeout for api-server heartbeat, set to 0 to disable")
+	flags.Bool(option.K8sEnableAPIDiscovery, def.EnableK8sAPIDiscovery, "Enable discovery of Kubernetes API groups and resources with the discovery API")
 }
 
 func (cfg Config) isEnabled() bool {

commit 8e411295b3c54cc8f62ba9665750f691fb1c6d73

hive: Unify parsing of string slices
Hive uses pflag and viper to parse configuration flags from multiple sources.
If a flag is set via command-line then the pflag parser is invoked to get to
the destination type as defined in the FlagSet ("flags.StringSlice" [1]), however
if the flag comes from environment or config-map, then the parsing was done by
a mapstructure hook [2].

This is all well and good as long as these two ways of parsing into say []string
are aligned with each other. And they were. Unfortunately these were not aligned
with the pre-Hive way of parsing which used viper.GetStringSlice and similar
methods. Specifically viper.GetStringSlice is implemented ([3]) via cast.ToStringSlice,
which uses strings.Fields that splits by whitespace instead of by commas.

So to summarize the different ways a StringSlice can be parsed:

- [1]: flags.StringSlice: parses with csv.Reader (split by comma)
- [2]: stringToSliceHookFunc: splits by comma
- [3]: viper.GetStringSlice: splits by whitespaces

So while arguably the first two are more consistent, we can't just flip
from splitting by spaces to splitting by commas as that creates a huge foot-gun
when fields are moved from option.Config to individual hive.Config structs.

To solve this we allow splitting both ways by using two mapstructure
hooks that process the values before they're pushed to the config struct:

- mapstructure.StringToSliceHookFunc(",") splits first string by commas.
  This only impacts input coming from environment, configmap or flags.String and
  going to []string.

- fixupStringSliceHookFunc takes []string coming from flags.StringSlice or from
  the StringToSliceHookFunc and resplits it by whitespace if it was of length 1.

With this, we have unified the parsing of []string across all the config input
methods:

  "foo,bar,baz" => []string{"foo", "bar", "baz"}
  "foo bar baz" => []string{"foo", "bar", "baz"}
  "foo bar,baz" => []string{"foo bar", "baz"}

[1]: https://github.com/spf13/pflag/blob/master/string_slice.go#L27
[2]: https://github.com/mitchellh/mapstructure/blob/main/decode_hooks.go#L104
[3]: https://github.com/spf13/viper/blob/9154b900c34ad9d88897f7e5288ce43f457f698b/viper.go#L1067

Fixes: #29210
Fixes: b5943c196b ("hive: Reimplement on top of dig")

pkg/hive/cell/config.go
@@ -104,8 +104,24 @@ func decoderConfig(target any) *mapstructure.DecoderConfig {
 		Result:           target,
 		WeaklyTypedInput: true,
 		DecodeHook: mapstructure.ComposeDecodeHookFunc(
+			// To unify the splitting of fields of a []string field across the input coming
+			// from environment, configmap and pflag (command-line), we first split a string
+			// (env/configmap) by comma, and then for all input methods we split a single
+			// value []string by whitespace. Thus the following all result in the same slice:
+			//
+			// --string-slice=foo,bar,baz
+			// --string-slice="foo bar baz"
+			// CILIUM_STRING_SLICE="foo,bar,baz"
+			// CILIUM_STRING_SLICE="foo bar baz"
+			// /.../configmap/string_slice: "foo bar baz"
+			// /.../configmap/string_slice: "foo,bar,baz"
+			//
+			// If both commas and whitespaces are present the commas take precedence:
+			// "foo,bar baz" => []string{"foo", "bar baz"}
+			mapstructure.StringToSliceHookFunc(","), // string->[]string is split by comma
+			fixupStringSliceHookFunc,                // []string of length 1 is split again by whitespace
+
 			mapstructure.StringToTimeDurationHookFunc(),
-			mapstructure.StringToSliceHookFunc(","),
 			stringToCIDRHookFunc,
 			stringToMapHookFunc,
 		),
@@ -164,4 +180,24 @@ func stringToCIDRHookFunc(from reflect.Type, to reflect.Type, data interface{}) 
 		return data, nil
 	}
 	return cidr.ParseCIDR(s)
+}
+
+// fixupStringSliceHookFunc takes a []string and if it's a single element splits it again
+// by whitespace. This unifies the flag parsing behavior with StringSlice
+// values coming from environment or configmap where both spaces or commas can be used to split.
+func fixupStringSliceHookFunc(from reflect.Type, to reflect.Type, data interface{}) (interface{}, error) {
+	if from.Kind() != reflect.Slice || to.Kind() != reflect.Slice {
+		return data, nil
+	}
+	if from.Elem().Kind() != reflect.String || to.Elem().Kind() != reflect.String {
+		return data, nil
+	}
+
+	raw := data.([]string)
+	if len(raw) == 1 {
+		// Flag was already split by commas (the default behavior), so split it
+		// now by spaces.
+		return strings.Fields(raw[0]), nil
+	}
+	return raw, nil
 }

pkg/hive/hive_test.go
@@ -232,6 +232,78 @@ func TestHiveCIDRSlice(t *testing.T) {
 	require.Equal(t, cidr.MustParseCIDR("2001:db8::/64"), cfg.Foo[1], "Config.Foo not set correctly")
 }
 
+type StringSliceConfig struct {
+	SpacesFlag, CommasFlag []string
+	SpacesMap, CommasMap   []string
+	Mixed                  []string
+	StringFlag             string
+}
+
+func (StringSliceConfig) Flags(flags *pflag.FlagSet) {
+	flags.StringSlice("spaces-flag", nil, "split by spaces via pflag")
+	flags.StringSlice("commas-flag", nil, "split by commas via pflag")
+	flags.StringSlice("spaces-map", nil, "split by spaces via configmap")
+	flags.StringSlice("commas-map", nil, "split by commas via configmap")
+
+	// One can also use just flags.String against a []string config field and
+	// it will be parsed the same way as via configmap.
+	flags.String("mixed", "", "mixed")
+
+	flags.String("string-flag", "", "plain string untouched")
+}
+
+func TestHiveStringSlice(t *testing.T) {
+	var cfg StringSliceConfig
+	testCell := cell.Module(
+		"test",
+		"Test Module",
+		cell.Config(StringSliceConfig{}),
+		cell.Invoke(func(c StringSliceConfig) {
+			cfg = c
+		}),
+	)
+	hive := hive.New(testCell)
+
+	flags := pflag.NewFlagSet("", pflag.ContinueOnError)
+	hive.RegisterFlags(flags)
+
+	spaces := "foo bar baz"
+	commas := "foo,bar,baz"
+	expected := []string{"foo", "bar", "baz"}
+
+	// When the values are provided via the flags then the value given
+	// to Viper is already a []string as parsed by pflag. This will be
+	// processed by stringSliceToStringSliceHookFunc to re-split if needed.
+	flags.Set("spaces-flag", spaces)
+	flags.Set("commas-flag", commas)
+
+	// Plain string flags are not split in any way.
+	flags.Set("string-flag", "foo, bar, baz")
+
+	// If spaces and commas are mixed then commas take precedence.
+	flags.Set("mixed", "foo bar,baz")
+
+	// When the values are provided via the configmap or environment they're
+	// given to Viper as plain strings that will be processed by stringToSliceHookFunc.
+	hive.Viper().MergeConfigMap(
+		map[string]any{
+			"spaces-map": spaces,
+			"commas-map": commas,
+		})
+
+	err := hive.Start(context.TODO())
+	require.NoError(t, err, "expected Start to succeed")
+	err = hive.Stop(context.TODO())
+	require.NoError(t, err, "expected Stop to succeed")
+
+	assert.ElementsMatch(t, cfg.SpacesFlag, expected, "unexpected SpacesFlag")
+	assert.ElementsMatch(t, cfg.SpacesMap, expected, "unexpected SpacesMap")
+	assert.ElementsMatch(t, cfg.CommasFlag, expected, "unexpected CommasFlag")
+	assert.ElementsMatch(t, cfg.CommasMap, expected, "unexpected CommasMap")
+	assert.ElementsMatch(t, cfg.Mixed, []string{"foo bar", "baz"}, "unexpected Mixed")
+	assert.Equal(t, cfg.StringFlag, "foo, bar, baz", "unexpected StringFlag")
+}
+
 type SomeObject struct {
 	X int
 }