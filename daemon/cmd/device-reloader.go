g0odi3z2j/cilium

commit 982ca6d014ec619575dc6e326f6b1f4846ca2f94

daemon: add a device reloader that isn't as racy
With the introduction of the NodeAddress table, and the reimplementation
of the NodeAddressing on top of it, we have introduced a race in the
current, experimental runtime device detection. It's possible that the
datapath is reloaded with a new set of devices which is not reflected in
the node addressing interface yet.

We want to get rid of device manager anyway, so use this opportunity to
replace this functionality with a device reloader (this patch), and kill
the racy Listen (next patch).

Fixes: 706cf98d64 (datapath: Reimplement NodeAddressing around Table[NodeAddress])

Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

daemon/cmd/device-reloader.go
@@ -1,0 +1,143 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package cmd
+
+import (
+	"context"
+	"slices"
+
+	"github.com/cilium/cilium/pkg/datapath/tables"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+	"github.com/cilium/cilium/pkg/hive/job"
+	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/option"
+	"github.com/cilium/cilium/pkg/promise"
+	"github.com/cilium/cilium/pkg/statedb"
+	"github.com/cilium/cilium/pkg/time"
+)
+
+type deviceReloaderParams struct {
+	cell.In
+
+	Jobs          job.Registry
+	Scope         cell.Scope
+	DB            *statedb.DB
+	Daemon        promise.Promise[*Daemon]
+	Config        *option.DaemonConfig
+	Devices       statedb.Table[*tables.Device]
+	NodeAddresses statedb.Table[tables.NodeAddress]
+}
+
+type deviceReloader struct {
+	params       deviceReloaderParams
+	addrsChanged <-chan struct{}
+	devsChanged  <-chan struct{}
+	prevDevices  []string
+	jg           job.Group
+}
+
+// registerDeviceReloader provides the device reloader to the hive. The device reloader reloads the
+// datapath when the runtime devices (i.e. links) change. This is a legacy component. New code
+// should NOT be added here, but should directly use Table[*Device], Table[NodeAddress] or both.
+//
+// The current functionality of this reloader is to reload the datapath when either the node address
+// or the devices change. This leads to potentially inconsistent, observable state when the device
+// changes have not yet propagated to the NodeAddress changes. Components which depend on both need
+// to live with this fact and come up with component-specific strategies to deal with it.
+func registerDeviceReloader(lc hive.Lifecycle, p deviceReloaderParams) {
+	if !p.Config.EnableRuntimeDeviceDetection {
+		return
+	}
+
+	lc.Append(&deviceReloader{params: p})
+}
+
+// Start listening to changed devices if requested.
+func (d *deviceReloader) Start(ctx hive.HookContext) error {
+	if !d.params.Config.AreDevicesRequired() {
+		log.Info("Runtime device detection requested, but no feature requires it. Disabling detection.")
+		return nil
+	}
+
+	// Force an initial reload by supplying a closed channel.
+	c := make(chan struct{})
+	close(c)
+	d.devsChanged = c
+
+	jg := d.params.Jobs.NewGroup(d.params.Scope)
+	jg.Add(job.Timer("device-reloader", d.reload, time.Second))
+	d.jg = jg
+	return jg.Start(ctx)
+}
+
+func (d *deviceReloader) Stop(ctx hive.HookContext) error {
+	if d.jg != nil {
+		return d.jg.Stop(ctx)
+	}
+	return nil
+}
+
+func (d *deviceReloader) query() []string {
+	rxn := d.params.DB.ReadTxn()
+	_, d.addrsChanged = d.params.NodeAddresses.All(rxn)
+
+	var devices []*tables.Device
+	devices, d.devsChanged = tables.SelectedDevices(d.params.Devices, rxn)
+	return tables.DeviceNames(devices)
+}
+
+func (d *deviceReloader) reload(ctx context.Context) error {
+	var addrsChanged bool
+	select {
+	case <-d.addrsChanged:
+		addrsChanged = true
+	case <-d.devsChanged:
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+
+	// Note that the consumers may see inconsistent state in between updates to
+	// the devices and the node addresses, but that we are eventually
+	// consistent.
+
+	// Setup new watch channels.
+	devices := d.query()
+	// Don't do any work if we don't need to.
+	if slices.Equal(d.prevDevices, devices) && !addrsChanged {
+		return nil
+	}
+
+	d.params.Config.SetDevices(devices)
+
+	if d.params.Config.MasqueradingEnabled() && option.Config.EnableBPFMasquerade {
+		if err := node.InitBPFMasqueradeAddrs(devices); err != nil {
+			log.Warnf("InitBPFMasqueradeAddrs failed: %s", err)
+		}
+	}
+
+	daemon, err := d.params.Daemon.Await(ctx)
+	if err != nil {
+		return err
+	}
+	if daemon.l2announcer != nil {
+		daemon.l2announcer.DevicesChanged(devices)
+	}
+
+	if d.params.Config.EnableNodePort {
+		// Synchronize services and endpoints to reflect new addresses onto lbmap.
+		daemon.svc.SyncServicesOnDeviceChange(daemon.Datapath().LocalNodeAddressing())
+		daemon.controllers.TriggerController(syncHostIPsController)
+	}
+
+	// Reload the datapath.
+	wg, err := daemon.TriggerReloadWithoutCompile("devices changed")
+	if err != nil {
+		log.WithError(err).Warn("Failed to reload datapath")
+	} else {
+		wg.Wait()
+	}
+	d.prevDevices = devices
+	return nil
+}

daemon/cmd/cells.go
@@ -201,6 +201,9 @@ var (
 
 		// RegeneratorCell provides extra options and utilities for endpoints regeneration.
 		endpoint.RegeneratorCell,
+
+		// The device reloader reloads the datapath when the devices change at runtime.
+		cell.Invoke(registerDeviceReloader),
 	)
 )
 

daemon/cmd/daemon.go
@@ -1032,58 +1032,11 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 	d.ipcache.InitIPIdentityWatcher(d.ctx, params.StoreFactory)
 	identitymanager.Subscribe(d.policy)
 
-	// Start listening to changed devices if requested.
-	if option.Config.EnableRuntimeDeviceDetection && d.deviceManager != nil {
-		if option.Config.AreDevicesRequired() {
-			devicesChan, err := d.deviceManager.Listen(ctx)
-			if err != nil {
-				log.WithError(err).Warn("Runtime device detection failed to start")
-			}
-			go func() {
-				for devices := range devicesChan {
-					d.ReloadOnDeviceChange(devices)
-				}
-			}()
-		} else {
-			log.Info("Runtime device detection requested, but no feature requires it. Disabling detection.")
-		}
-	}
-
 	if err := params.IPsecKeyCustodian.StartBackgroundJobs(d.Datapath().Node()); err != nil {
 		log.WithError(err).Error("Unable to start IPsec key watcher")
 	}
 
 	return &d, restoredEndpoints, nil
-}
-
-// ReloadOnDeviceChange regenerates device related information and reloads the datapath.
-// The devices is the new set of devices that replaces the old set.
-func (d *Daemon) ReloadOnDeviceChange(devices []string) {
-	option.Config.SetDevices(devices)
-
-	if option.Config.MasqueradingEnabled() && option.Config.EnableBPFMasquerade {
-		if err := node.InitBPFMasqueradeAddrs(devices); err != nil {
-			log.Warnf("InitBPFMasqueradeAddrs failed: %s", err)
-		}
-	}
-
-	if d.l2announcer != nil {
-		d.l2announcer.DevicesChanged(devices)
-	}
-
-	if option.Config.EnableNodePort {
-		// Synchronize services and endpoints to reflect new addresses onto lbmap.
-		d.svc.SyncServicesOnDeviceChange(d.Datapath().LocalNodeAddressing())
-		d.controllers.TriggerController(syncHostIPsController)
-	}
-
-	// Reload the datapath.
-	wg, err := d.TriggerReloadWithoutCompile("devices changed")
-	if err != nil {
-		log.WithError(err).Warn("Failed to reload datapath")
-		return
-	}
-	wg.Wait()
 }
 
 // Close shuts down a daemon

commit 7d641347d8dd2085649f4262e471b602b9643d7c

daemon: fix device reloader race condition
The device reloader introduced a very similar variant of the race it was
created to avoid. Specifically, if the node addressing table was updated
after a device change, but before the device reloader re-queriyed the
table, the reloader would miss the update.

This results in node-port services potentially not being available on
newly added devices, even though runtime device detection is enabled.

Fixes: 982ca6d014 (daemon: add a device reloader that isn't as racy)

Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

daemon/cmd/device-reloader.go
@@ -64,7 +64,7 @@ func (d *deviceReloader) Start(ctx hive.HookContext) error {
 	// Force an initial reload by supplying a closed channel.
 	c := make(chan struct{})
 	close(c)
-	d.devsChanged = c
+	d.addrsChanged = c
 
 	jg := d.params.Jobs.NewGroup(d.params.Scope)
 	jg.Add(job.Timer("device-reloader", d.reload, time.Second))
@@ -79,13 +79,10 @@ func (d *deviceReloader) Stop(ctx hive.HookContext) error {
 	return nil
 }
 
-func (d *deviceReloader) query() []string {
-	rxn := d.params.DB.ReadTxn()
-	_, d.addrsChanged = d.params.NodeAddresses.All(rxn)
-
-	var devices []*tables.Device
-	devices, d.devsChanged = tables.SelectedDevices(d.params.Devices, rxn)
-	return tables.DeviceNames(devices)
+func (d *deviceReloader) queryDevices(rxn statedb.ReadTxn) []string {
+	var nativeDevices []*tables.Device
+	nativeDevices, d.devsChanged = tables.SelectedDevices(d.params.Devices, rxn)
+	return tables.DeviceNames(nativeDevices)
 }
 
 func (d *deviceReloader) reload(ctx context.Context) error {
@@ -103,7 +100,12 @@ func (d *deviceReloader) reload(ctx context.Context) error {
 	// consistent.
 
 	// Setup new watch channels.
-	devices := d.query()
+	rxn := d.params.DB.ReadTxn()
+	if addrsChanged {
+		_, d.addrsChanged = d.params.NodeAddresses.All(rxn)
+	}
+	devices := d.queryDevices(rxn)
+
 	// Don't do any work if we don't need to.
 	if slices.Equal(d.prevDevices, devices) && !addrsChanged {
 		return nil